ae4a6ff905710456653552f2328f466d90bf3bb5
Alexey Goncharuk <agoncharuk@gridgain.com>
2014-02-27 13:58:45 -0800

    GG-7688 - User ordered messages should be skipped on timeout.

--- modules/core/java/org/gridgain/grid/kernal/managers/communication/GridIoManager.java
+++ modules/core/java/org/gridgain/grid/kernal/managers/communication/GridIoManager.java
@@ -1135,7 +1135,7 @@ public class GridIoManager extends GridManagerAdapter<GridCommunicationSpi<Seria
         if (ordered) {
             long msgId = nextMessageId(TOPIC_COMM_USER, locNodeId);
 
-            sendOrderedMessage(nodes, TOPIC_COMM_USER, msgId, ioMsg, PUBLIC_POOL, timeout, false);
+            sendOrderedMessage(nodes, TOPIC_COMM_USER, msgId, ioMsg, PUBLIC_POOL, timeout, true);
         }
         else if (loc)
             send(F.first(nodes), TOPIC_COMM_USER, ioMsg, PUBLIC_POOL);
