459c410d62a4c7f6b92b792a1a826e8684514e8d
vozerov <vladimir.ozerov@domograph.ru>
2014-02-28 11:36:32 +0400

    # GG-7691: Done.

---
+++ examples/java/org/gridgain/examples/compute/ComputeFailoverCheckpointNodeStartup.java
@@ -0,0 +1,81 @@
+// @java.file.header
+
+/*  _________        _____ __________________        _____
+ *  __  ____/___________(_)______  /__  ____/______ ____(_)_______
+ *  _  / __  __  ___/__  / _  __  / _  / __  _  __ `/__  / __  __ \
+ *  / /_/ /  _  /    _  /  / /_/ /  / /_/ /  / /_/ / _  /  _  / / /
+ *  \____/   /_/     /_/   \_,__/   \____/   \__,_/  /_/   /_/ /_/
+ */
+
+package org.gridgain.examples.compute;
+
+import org.gridgain.grid.*;
+import org.gridgain.grid.marshaller.optimized.*;
+import org.gridgain.grid.spi.checkpoint.sharedfs.*;
+import org.gridgain.grid.spi.discovery.tcp.*;
+import org.gridgain.grid.spi.discovery.tcp.ipfinder.vm.*;
+
+import java.util.*;
+
+/**
+ * Starts up an empty node with checkpoint-enabled configuration.
+ * <p>
+ * The difference is that running this class from IDE adds all example classes to classpath
+ * but running from command line doesn't.
+ *
+ * @author @java.author
+ * @version @java.version
+ */
+public class ComputeFailoverCheckpointNodeStartup {
+    /**
+     * Start up an empty node with specified cache configuration.
+     *
+     * @param args Command line arguments, none required.
+     * @throws GridException If example execution failed.
+     */
+    public static void main(String[] args) throws GridException {
+        GridGain.start(configuration());
+    }
+
+    /**
+     * Create Grid configuration with configured checkpoints.
+     *
+     * @return Grid configuration.
+     * @throws GridException If configuration creation failed.
+     */
+    public static GridConfiguration configuration() throws GridException {
+        GridConfiguration cfg = new GridConfiguration();
+
+        cfg.setLocalHost("127.0.0.1");
+        cfg.setPeerClassLoadingEnabled(true);
+
+        GridOptimizedMarshaller marsh = new GridOptimizedMarshaller();
+
+        marsh.setRequireSerializable(false);
+
+        cfg.setMarshaller(marsh);
+
+        GridSharedFsCheckpointSpi checkpointSpi = new GridSharedFsCheckpointSpi();
+
+        checkpointSpi.setDirectoryPaths(Collections.singletonList("/work/checkpoint/sharedfs"));
+
+        cfg.setCheckpointSpi(checkpointSpi);
+
+        GridTcpDiscoverySpi discoSpi = new GridTcpDiscoverySpi();
+
+        GridTcpDiscoveryVmIpFinder ipFinder = new GridTcpDiscoveryVmIpFinder();
+
+        Collection<String> addrs = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++)
+            addrs.add("127.0.0.1:" + (47500 + i));
+
+        ipFinder.setAddresses(addrs);
+
+        discoSpi.setIpFinder(ipFinder);
+
+        cfg.setDiscoverySpi(discoSpi);
+
+        return cfg;
+    }
+}
