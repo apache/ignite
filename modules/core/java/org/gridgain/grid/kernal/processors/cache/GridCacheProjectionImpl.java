5fd528f852129551b2fd4d1ff069883f47847674
Valentin Kulichenko <vkulichenko@gridgain.com>
2014-02-28 12:33:42 +0400

    # GG-7625 - Fixing tests

--- modules/core/java/org/gridgain/grid/kernal/processors/cache/GridCacheProjectionImpl.java
+++ modules/core/java/org/gridgain/grid/kernal/processors/cache/GridCacheProjectionImpl.java
@@ -1209,6 +1209,8 @@ public class GridCacheProjectionImpl<K, V> extends GridMetadataAwareAdapter impl
         flags = U.readSet(in);
 
         cache = cctx.cache();
+
+        qry = new GridCacheQueriesImpl<>(cctx, this);
     }
 
     /** {@inheritDoc} */
