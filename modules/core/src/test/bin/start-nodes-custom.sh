141e86e9de6bfd2be876c9ca11fc59a890b26815
sboikov <sboikov@gridgain.com>
2014-03-09 12:32:58 +0400

    # GG-7725 Need to modularize tests

--- modules/core/src/test/bin/start-nodes-custom.sh
+++ modules/core/src/test/bin/start-nodes-custom.sh
@@ -9,17 +9,8 @@
 #  \____/   /_/     /_/   \_,__/   \____/   \__,_/  /_/   /_/ /_/
 #
 
-osname=`uname`
+SCRIPT_DIR=$(cd $(dirname "$0"); pwd)
 
-case $osname in
-    Darwin*)
-        export MODULE_DIR=$(dirname $(dirname $(cd ${0%/*} && echo $PWD/${0##*/})))
-        ;;
-    *)
-        export MODULE_DIR="$(dirname $(readlink -f $0))"/..
-        ;;
-esac
+export GRIDGAIN_HOME="${SCRIPT_DIR}"/../../../../..
 
-export GRIDGAIN_HOME="${MODULE_DIR}"/../..
-
-"${GRIDGAIN_HOME}"/bin/ggstart.sh -v modules/tests/config/spring-start-nodes-attr.xml
+"${GRIDGAIN_HOME}"/bin/ggstart.sh -v modules/core/src/test/config/spring-start-nodes-attr.xml
