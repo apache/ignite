4528cf03d1f8db8ec1ecde34471b2f56e7a5143a
AKuznetsov <akuznetsov@gridgain.com>
2014-03-14 22:42:57 +0800

    # GG-7856 Fix compilation.

--- modules/visor-console/tests/org/gridgain/visor/VisorScalarSpec.scala
+++ modules/visor-console/tests/org/gridgain/visor/VisorScalarSpec.scala
@@ -11,6 +11,7 @@
 
 package org.gridgain.visor
 
+import org.scalatest.matchers._
 import org.scalatest._
 import org.gridgain.scalar._
 import org.gridgain.grid._
--- modules/visor-console/tests/org/gridgain/visor/VisorTextTableSpec.scala
+++ modules/visor-console/tests/org/gridgain/visor/VisorTextTableSpec.scala
@@ -11,6 +11,7 @@
 
 package org.gridgain.visor
 
+import org.scalatest.matchers._
 import org.scalatest._
 import collection._
 import org.gridgain.visor.commands.VisorTextTable
