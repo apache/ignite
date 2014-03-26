9c2b35b47e095ef0e668d8ed966de3c41dd895ff
Anton Irinev <airinev@gridgain.com>
2014-03-26 17:30:12 +0400

    # GG-7926 Minor fix

--- modules/core/java/org/gridgain/grid/kernal/GridKernal.java
+++ modules/core/java/org/gridgain/grid/kernal/GridKernal.java
@@ -96,7 +96,7 @@ import static org.gridgain.grid.util.nodestart.GridNodeStartUtils.*;
  */
 public class GridKernal extends GridProjectionAdapter implements GridEx, GridKernalMBean {
     /** Enterprise release flag. */
-    private static final boolean ENT;
+    private static final boolean ent;
 
     /** Compound GridGain version. */
     private static final String COMPOUND_VERSION;
@@ -114,9 +114,9 @@ public class GridKernal extends GridProjectionAdapter implements GridEx, GridKer
             ent0 = false;
         }
 
-        ENT = ent0;
+        ent = ent0;
 
-        COMPOUND_VERSION = EDITION + (ENT ? "ent" : "os") + VER;
+        COMPOUND_VERSION = EDITION + (ent ? "ent" : "os") + VER;
     }
 
     /** Ant-augmented compatible versions. */
@@ -618,7 +618,7 @@ public class GridKernal extends GridProjectionAdapter implements GridEx, GridKer
 
         // Spin out SPIs & managers.
         try {
-            GridKernalContextImpl ctx = new GridKernalContextImpl(this, cfg, gw, ENT);
+            GridKernalContextImpl ctx = new GridKernalContextImpl(this, cfg, gw, ent);
 
             nodeLoc = new GridNodeLocalMapImpl(ctx);
 
