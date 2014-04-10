434dfc411f39b108b66dc943db989bbaca77922f
sboikov <semen.boikov@inria.fr>
2014-04-10 12:46:06 +0400

    # GG-8063, added test.

--- modules/core/src/test/java/org/gridgain/grid/kernal/processors/cache/GridCacheTxMultiThreadedAbstractTest.java
+++ modules/core/src/test/java/org/gridgain/grid/kernal/processors/cache/GridCacheTxMultiThreadedAbstractTest.java
@@ -14,6 +14,7 @@ import org.gridgain.grid.cache.*;
 import org.gridgain.testframework.*;
 import org.jetbrains.annotations.*;
 
+import java.util.*;
 import java.util.concurrent.*;
 
 import static org.gridgain.grid.cache.GridCacheTxConcurrency.*;
@@ -191,5 +192,83 @@ public abstract class GridCacheTxMultiThreadedAbstractTest extends GridCacheTxAb
         checkRollbackMultithreaded(OPTIMISTIC, SERIALIZABLE);
 
         finalChecks();
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    // TODO: GG-8063, enabled when fixed.
+    public void _testOptimisticSerializableConsistency() throws Exception {
+        final GridCache<Integer, Long> cache = grid(0).cache(null);
+
+        final int THREADS = 2;
+
+        final int ITERATIONS = 100;
+
+        final int key = 0;
+
+        cache.put(key, 0L);
+
+        List<GridFuture<Collection<Long>>> futs = new ArrayList<>(THREADS);
+
+        for (int i = 0; i < THREADS; i++) {
+            futs.add(GridTestUtils.runAsync(new Callable<Collection<Long>>() {
+                @Override public Collection<Long> call() throws Exception {
+                    Collection<Long> res = new ArrayList<>();
+
+                    for (int i = 0; i < ITERATIONS; i++) {
+                        while (true) {
+                            try (GridCacheTx tx = cache.txStart(OPTIMISTIC, SERIALIZABLE)) {
+                                long val = cache.get(key);
+
+                                cache.put(key, val + 1);
+
+                                tx.commit();
+
+                                assertTrue(res.add(val + 1));
+
+                                break;
+                            }
+                            catch(GridCacheTxOptimisticException e) {
+                                log.info("Got error, will retry: " + e);
+                            }
+                        }
+                    }
+
+                    return res;
+                }
+            }));
+        }
+
+        List<Collection<Long>> cols = new ArrayList<>(THREADS);
+
+        for (GridFuture<Collection<Long>> fut : futs) {
+            Collection<Long> col = fut.get();
+
+            assertEquals(ITERATIONS, col.size());
+
+            cols.add(col);
+        }
+
+        Set<Long> duplicates = new HashSet<>();
+
+        for (Collection<Long> col1 : cols) {
+            for (Long val1 : col1) {
+                for (Collection<Long> col2 : cols) {
+                    if (col1 == col2)
+                        continue;
+
+                    for (Long val2 : col2) {
+                        if (val1.equals(val2)) {
+                            duplicates.add(val2);
+
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        assertTrue("Found duplicated values: " + duplicates, duplicates.isEmpty());
     }
 }
