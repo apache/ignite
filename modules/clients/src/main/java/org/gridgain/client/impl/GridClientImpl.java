15f0050d4db1899aeab01e19eacbd7eb111600a7
anovikov <anovikov@gridgain.com>
2014-04-18 10:37:50 +0800

    #GG-8129 Move unreachable localhost endpoint check.

--- modules/clients/src/main/java/org/gridgain/client/impl/GridClientImpl.java
+++ modules/clients/src/main/java/org/gridgain/client/impl/GridClientImpl.java
@@ -15,7 +15,6 @@ import org.gridgain.client.impl.connection.*;
 import org.gridgain.client.ssl.*;
 import org.gridgain.grid.*;
 import org.gridgain.grid.lang.*;
-import org.gridgain.grid.util.typedef.F;
 import org.gridgain.grid.util.typedef.internal.*;
 import org.jetbrains.annotations.*;
 
@@ -25,7 +24,6 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.*;
 import java.util.logging.*;
-import static org.gridgain.grid.kernal.GridNodeAttributes.*;
 
 /**
  * Client implementation.
@@ -397,19 +395,8 @@ public class GridClientImpl implements GridClient {
         if (hasSrvs) {
             // Add REST endpoints for all nodes from previous topology snapshot.
             try {
-                for (GridClientNodeImpl node : top.nodes()) {
-                    Collection<InetSocketAddress> endpoints = node.availableAddresses(cfg.getProtocol());
-
-                    boolean onSameHost = node.attributes().isEmpty() ||
-                        F.containsAny(U.allLocalMACs(), node.attribute(ATTR_MACS).toString().split(", "));
-
-                    if (onSameHost)
-                        connSrvs.addAll(endpoints);
-                    else
-                        for(InetSocketAddress endpoint : endpoints)
-                            if (!endpoint.getAddress().isLoopbackAddress())
-                                connSrvs.add(endpoint);
-                }
+                for (GridClientNodeImpl node : top.nodes())
+                    connSrvs.addAll(node.availableAddresses(cfg.getProtocol()));
             }
             catch (GridClientDisconnectedException ignored) {
                 // Ignore if latest topology update failed.
--- modules/clients/src/main/java/org/gridgain/client/impl/GridClientNodeImpl.java
+++ modules/clients/src/main/java/org/gridgain/client/impl/GridClientNodeImpl.java
@@ -17,6 +17,8 @@ import java.net.*;
 import java.util.*;
 import java.util.concurrent.atomic.*;
 
+import static org.gridgain.grid.kernal.GridNodeAttributes.*;
+
 /**
  * Client node implementation.
  */
@@ -213,6 +215,19 @@ public class GridClientNodeImpl implements GridClientNode {
             return addrs0;
 
         addrs0 = U.toSocketAddresses(addrs, hostNames, port);
+
+        boolean onDifferentHost = !attributes().isEmpty() &&
+            !F.containsAny(U.allLocalMACs(), attribute(ATTR_MACS).toString().split(", "));
+
+        if (onDifferentHost) {
+            Collection<InetSocketAddress> reachableEndpoints = new ArrayList<>(addrs0.size());
+
+            for (InetSocketAddress endpoint : addrs0)
+                if (!endpoint.getAddress().isLoopbackAddress())
+                    reachableEndpoints.add(endpoint);
+
+            addrs0 = U.sealList(reachableEndpoints);
+        }
 
         if (!addrsCache.compareAndSet(null, addrs0))
             return addrsCache.get();
