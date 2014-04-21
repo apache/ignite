3fa4f502c2551dc066d5cc1b0c7cfb42b4cb26d7
vozerov <vozerov@gridgain.com>
2014-04-21 10:18:24 +0400

    # gg-8123: Isolating the bug (9). See JVMFailure class.

--- modules/core/src/main/java/bug/JVMFailure.java
+++ modules/core/src/main/java/bug/JVMFailure.java
@@ -6,6 +6,10 @@ import java.lang.reflect.*;
 import java.util.*;
 
 public class JVMFailure {
+    private static final int KEY_SIZE_OFF = 0;
+    private static final int VAL_SIZE_OFF = 4;
+    private static final int HEADER_SIZE = 8;
+
     private static final Unsafe UNSAFE = unsafe();
     private static final long BYTE_ARR_OFF = UNSAFE.arrayBaseOffset(byte[].class);
 
@@ -40,36 +44,36 @@ public class JVMFailure {
     private Deque<Long> ptrs = new ArrayDeque<>();
 
     private void put(byte[] keyBytes, byte[] valBytes) {
-        long ptr = UNSAFE.allocateMemory(12 + keyBytes.length + valBytes.length);
+        long ptr = UNSAFE.allocateMemory(HEADER_SIZE + keyBytes.length + valBytes.length);
 
-        UNSAFE.putInt(ptr + 4, keyBytes.length);
-        UNSAFE.putInt(ptr + 8, valBytes.length);
-        UNSAFE.copyMemory(keyBytes, BYTE_ARR_OFF, null, ptr + 12, keyBytes.length);
-        UNSAFE.copyMemory(valBytes, BYTE_ARR_OFF, null, ptr + 12 + keyBytes.length, valBytes.length);
+        UNSAFE.putInt(ptr + KEY_SIZE_OFF, keyBytes.length);
+        UNSAFE.putInt(ptr + VAL_SIZE_OFF, valBytes.length);
+        UNSAFE.copyMemory(keyBytes, BYTE_ARR_OFF, null, ptr + HEADER_SIZE, keyBytes.length);
+        UNSAFE.copyMemory(valBytes, BYTE_ARR_OFF, null, ptr + HEADER_SIZE + keyBytes.length, valBytes.length);
 
         ptrs.addLast(ptr);
 
         if (ptrs.size() > 100) {
-            long ptr0 = ptrs.pollFirst();
+            long oldPtr = ptrs.pollFirst();
 
-            byte[] valBytes0 = faultyMethod(ptr0);
+            byte[] valBytes0 = faultyMethod(oldPtr);
 
             if (valBytes0[0] != 0)
-                System.out.println(Arrays.toString(valBytes0));
+                System.out.println(oldPtr + " " + Arrays.toString(valBytes0));
 
-            int keySize = UNSAFE.getInt(ptr + 4);
-            int valSize = UNSAFE.getInt(ptr + 8);
+            int keySize = UNSAFE.getInt(oldPtr + KEY_SIZE_OFF);
+            int valSize = UNSAFE.getInt(oldPtr + VAL_SIZE_OFF);
 
-            UNSAFE.setMemory(ptr, 12 + keySize + valSize, (byte)0xAB);
-            UNSAFE.freeMemory(ptr);
+            UNSAFE.setMemory(oldPtr, HEADER_SIZE + keySize + valSize, (byte)0xAB);
+            UNSAFE.freeMemory(oldPtr);
         }
     }
 
     private byte[] faultyMethod(long ptr) {
-        int keySize = readInt(ptr + 4);
-        int valSize = readInt(ptr + 8);
+        int keySize = readInt(ptr + KEY_SIZE_OFF);
+        int valSize = readInt(ptr + VAL_SIZE_OFF);
 
-        return readBytes(ptr + 12 + keySize, new byte[valSize]);
+        return readBytes(ptr + HEADER_SIZE + keySize, new byte[valSize]);
     }
 
     public static void main(String[] args) {
@@ -77,8 +81,8 @@ public class JVMFailure {
 
         byte[] keyBytes = new byte[] { 127, 127, 127, 127 };
 
-        byte[] valBytes = new byte[74];
-        for (int i = 0; i < 74; i++)
+        byte[] valBytes = new byte[8];
+        for (int i = 0; i < 8; i++)
             valBytes[i] = (byte)i;
 
         for (;;)
