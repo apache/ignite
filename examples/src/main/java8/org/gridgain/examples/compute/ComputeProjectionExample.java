085a770baf34a6a7b67886a9bd7b2155897f374b
Anton Irinev <airinev@gridgain.com>
2014-05-06 19:23:11 +0400

    GG-8255 Add java8 examples

---
+++ examples/src/main/java8/org/gridgain/examples/compute/ComputeProjectionExample.java
@@ -0,0 +1,81 @@
+/* @java.file.header */
+
+/*  _________        _____ __________________        _____
+ *  __  ____/___________(_)______  /__  ____/______ ____(_)_______
+ *  _  / __  __  ___/__  / _  __  / _  / __  _  __ `/__  / __  __ \
+ *  / /_/ /  _  /    _  /  / /_/ /  / /_/ /  / /_/ / _  /  _  / / /
+ *  \____/   /_/     /_/   \_,__/   \____/   \__,_/  /_/   /_/ /_/
+ */
+
+package org.gridgain.examples.compute;
+
+import org.gridgain.grid.*;
+import org.gridgain.grid.lang.*;
+
+/**
+ * Demonstrates new functional APIs.
+ * <p>
+ * Remote nodes should always be started with special configuration file which
+ * enables P2P class loading: {@code 'ggstart.{sh|bat} examples/config/example-compute.xml'}.
+ * <p>
+ * Alternatively you can run {@link ComputeNodeStartup} in another JVM which will start GridGain node
+ * with {@code examples/config/example-compute.xml} configuration.
+ */
+public class ComputeProjectionExample {
+    /**
+     * Executes example.
+     *
+     * @param args Command line arguments, none required.
+     * @throws GridException If example execution failed.
+     */
+    public static void main(String[] args) throws Exception {
+        try (Grid grid = GridGain.start("examples/config/example-compute.xml")) {
+            System.out.println(grid.configuration().getDeploymentMode());
+
+            int prjSize = grid.nodes().size();
+
+            if (prjSize < 2) {
+                System.out.println();
+                System.out.println(">>> Please start at least 2 grid nodes to run example.");
+                System.out.println();
+
+                return;
+            }
+
+            System.out.println();
+            System.out.println("Compute projection example started.");
+
+            // Say hello to all nodes in the grid, including local node.
+            // Note, that Grid itself also implements GridProjection.
+            sayHello(grid);
+
+            // Say hello to all remote nodes.
+            sayHello(grid.forRemotes());
+
+            // Pick random node out of remote nodes.
+            GridProjection randomNode = grid.forRemotes().forRandom();
+
+            // Say hello to a random node.
+            sayHello(randomNode);
+
+            // Say hello to all nodes residing on the same host with random node.
+            sayHello(grid.forHost(randomNode.node()));
+
+            // Say hello to all nodes that have current CPU load less than 50%.
+            sayHello(grid.forPredicate((GridPredicate<GridNode>) n -> n.metrics().getCurrentCpuLoad() < 0.5));
+        }
+    }
+
+    /**
+     * Print 'Hello' message on remote grid nodes.
+     *
+     * @param g Grid instance.
+     * @throws GridException If failed.
+     */
+    private static void sayHello(final GridProjection g) throws GridException {
+        // Print out hello message on all remote projection nodes.
+        g.forRemotes().compute().broadcast(
+            (GridRunnable)() -> System.out.println(">>> Hello Node: " + g.grid().localNode().id())
+        ).get();
+    }
+}
---
+++ examples/src/main/java8/org/gridgain/examples/compute/ComputeRunnableExample.java
@@ -0,0 +1,58 @@
+/* @java.file.header */
+
+/*  _________        _____ __________________        _____
+ *  __  ____/___________(_)______  /__  ____/______ ____(_)_______
+ *  _  / __  __  ___/__  / _  __  / _  / __  _  __ `/__  / __  __ \
+ *  / /_/ /  _  /    _  /  / /_/ /  / /_/ /  / /_/ / _  /  _  / / /
+ *  \____/   /_/     /_/   \_,__/   \____/   \__,_/  /_/   /_/ /_/
+ */
+
+package org.gridgain.examples.compute;
+
+import org.gridgain.grid.*;
+import org.gridgain.grid.lang.*;
+
+import java.util.*;
+
+/**
+ * Demonstrates a simple use of {@link org.gridgain.grid.lang.GridRunnable}.
+ * <p>
+ * Remote nodes should always be started with special configuration file which
+ * enables P2P class loading: {@code 'ggstart.{sh|bat} examples/config/example-compute.xml'}.
+ * <p>
+ * Alternatively you can run {@link ComputeNodeStartup} in another JVM which will start GridGain node
+ * with {@code examples/config/example-compute.xml} configuration.
+ */
+public class ComputeRunnableExample {
+    /**
+     * Executes example.
+     *
+     * @param args Command line arguments, none required.
+     * @throws GridException If example execution failed.
+     */
+    public static void main(String[] args) throws GridException {
+        try (Grid g = GridGain.start("examples/config/example-compute.xml")) {
+            System.out.println();
+            System.out.println("Compute runnable example started.");
+
+            Collection<GridFuture> futs = new ArrayList<>();
+
+            // Iterate through all words in the sentence and create callable jobs.
+            for (final String word : "Print words using runnable".split(" ")) {
+                // Execute runnable on some node.
+                futs.add(g.compute().run((GridRunnable)() -> {
+                    System.out.println();
+                    System.out.println(">>> Printing '" + word + "' on this node from grid job.");
+                }));
+            }
+
+            // Wait for all futures to complete.
+            for (GridFuture<?> f : futs)
+                f.get();
+
+            System.out.println();
+            System.out.println(">>> Finished printing words using runnable execution.");
+            System.out.println(">>> Check all nodes for output (this node is also part of the grid).");
+        }
+    }
+}
