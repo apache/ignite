4ee37a094ee15299dab094d85bd996b899ab0df6
vozerov <vozerov@gridgain.com>
2014-05-16 10:40:18 +0400

    # gg-8204: WIP.

---
+++ modules/hadoop/src/main/java/org/gridgain/grid/kernal/ggfs/hadoop/impl/NewGridGgfsHadoopWrapper.java
@@ -0,0 +1,30 @@
+/* @java.file.header */
+
+/*  _________        _____ __________________        _____
+ *  __  ____/___________(_)______  /__  ____/______ ____(_)_______
+ *  _  / __  __  ___/__  / _  __  / _  / __  _  __ `/__  / __  __ \
+ *  / /_/ /  _  /    _  /  / /_/ /  / /_/ /  / /_/ / _  /  _  / / /
+ *  \____/   /_/     /_/   \_,__/   \____/   \__,_/  /_/   /_/ /_/
+ */
+
+package org.gridgain.grid.kernal.ggfs.hadoop.impl;
+
+import org.gridgain.grid.*;
+import org.gridgain.grid.ggfs.*;
+import org.gridgain.grid.kernal.ggfs.hadoop.*;
+import org.gridgain.grid.kernal.processors.ggfs.*;
+import org.gridgain.grid.util.lang.*;
+import org.jetbrains.annotations.*;
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.atomic.*;
+
+public class NewGridGgfsHadoopWrapper {
+
+    private final AtomicReference<NewGridGgfsHadoop> hadoop
+
+    private <T> T withReconnectHandling(final GridClosureX<GridGgfsHadoopIpcIo, T> clo) throws IOException {
+
+    }
+}
