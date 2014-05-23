c98eaa4f8e4ace4461ea3f4621872c4ee3b1cbc1
Anton Shakhmin <ashakhmin@gridgain.com>
2014-05-23 19:12:28 +0400

    # GG-8407 - comparison benchmark for dot net

--- modules/clients/src/test/dotnet/client-lib-load-test/Program.cs
+++ modules/clients/src/test/dotnet/client-lib-load-test/Program.cs
@@ -13,204 +13,166 @@ namespace GridGain {
     using System.Text;
     using System.Threading;
     using System.Collections.Generic;
-    using System.Security.Cryptography.X509Certificates;
     using GridGain.Client;
-    using GridGain.Client.Ssl;
+    using System.Diagnostics;
 
     using G = GridGain.Client.GridClientFactory;
-    using Dbg = System.Diagnostics.Debug;
+    using X = System.Console;
 
-    /** <summary>Java client load tests.</summary> */
+    /** <summary>Client load tests.</summary> */
     class GridClientLoadTest {
         static void Main(string[] args) {
-            Dbg.Listeners.Add(new System.Diagnostics.TextWriterTraceListener(System.Console.Out));
-            Dbg.AutoFlush = true;
+            var cfg = CreateConfig(GridClientProtocol.Tcp, "127.0.0.1:10080");
 
-            // Bypass all certificates.
-            System.Net.ServicePointManager.ServerCertificateValidationCallback = (sender, cert, chain, error) => true;
+            IGridClient client = G.Start(cfg);
 
-            var clientsCount = 20;
-            var threadsCount = 20;
+            IGridClientData cache = client.Data("atomic");
 
-            var sslCtx = new GridClientSslContext(true);
+            X.WriteLine("Client is started.");
 
-            sslCtx.ClientCertificates.Add(new X509Certificate2("cert\\client.pfx", "123456"));
+            X.WriteLine(">>>");
+            X.WriteLine(">>> Running test: PUT_GET");
+            X.WriteLine(">>>");
 
-            var cfgs = new IGridClientConfiguration[] {
-                CreateConfig(GridClientProtocol.Tcp, null, "127.0.0.1:10080"),
-                CreateConfig(GridClientProtocol.Tcp, sslCtx, "127.0.0.1:10443"),
-                CreateConfig(GridClientProtocol.Http, null, "127.0.0.1:11080"),
-                CreateConfig(GridClientProtocol.Http, sslCtx, "127.0.0.1:11443"),
-            };
+            var threadsCount = 16;
 
-            var clients = new List<IGridClient>();
+            var warmup = 20;
 
-            try {
-                Dbg.WriteLine("Start clients");
+            var duration = 40;
 
-                while (clients.Count < clientsCount)
-                    foreach (var cfg in cfgs)
-                        clients.Add(G.Start(cfg));
+            long adder = 0;
 
-                Dbg.WriteLine("Started clients: {0}", clients.Count);
+            long operationPerSecond = 0;
 
-                var taskName = "org.gridgain.client.GridClientHttpTask";
-                var taskArgs = "[\"asdf\"]";
+            var closed = false;
 
-                var threads = new List<Thread>();
-                var closed = false;
+            var timer = new Thread(() => {
+                Stopwatch testStart = new Stopwatch();
+                Stopwatch start = new Stopwatch();
 
-                while (threads.Count < threadsCount) {
-                    var t = new Thread(() => {
-                        Random rnd = new Random();
-                        IGridClient client;
+                testStart.Restart();
+          
+                int i = 0;
 
-                        while (!closed) {
-                            lock (clients) {
-                                client = clients[rnd.Next(clients.Count)];
-                            }
+                while (!closed) {
+                    i++;
 
-                            try {
-                                var data = client.Data();
-                                var exec = client.Compute();
+                    if (i == warmup) {
+                        Interlocked.Exchange(ref adder, 0);
 
-                                var futs = new List<IGridClientFuture>();
+                        testStart.Restart();
 
-                                //Dbg.WriteLine(Thread.CurrentThread.Name);
-
-                                futs.Add(data.PutAsync<String, String>("key", "value"));
-                                futs.Add(data.GetAsync<String, String>("key"));
-                                futs.Add(exec.ExecuteAsync<int>(taskName, taskArgs));
-                                futs.Add(exec.ExecuteAsync<int>(taskName, taskArgs));
-
-                                foreach (var fut in futs)
-                                    fut.WaitDone();
-                            }
-                            catch (GridClientServerUnreachableException e) {
-                                Dbg.WriteLine("Server unavailable: " + e.Message);
-
-                                lock (clients) {
-                                    clients.Remove(client);
-                                }
-
-                                G.Stop(client.Id);
-                            }
-                            catch (GridClientClosedException e) {
-                                Dbg.WriteLine("Client closed [clientId={0}, e={1}]", client.Id, e);
-
-                                lock (clients) {
-                                    clients.Remove(client);
-                                }
-
-                                G.Stop(client.Id);
-                            }
-                            catch (GridClientException e) {
-                                Dbg.WriteLine("Grid exception happens: " + e);
-                            }
-                            catch (Exception e) {
-                                Dbg.WriteLine("Unexpected exception happens: " + e);
-
-                                closed = true;
-
-                                break;
-                            }
-                        }
-                    });
-
-                    t.Name = "test-thread-" + threads.Count;
-
-                    threads.Add(t);
-                }
-
-                var stat = new Thread(() => {
-                    while (true) {
-                        Thread.Sleep(TimeSpan.FromSeconds(10));
-
-                        IDictionary<ThreadState, int> map = new Dictionary<ThreadState, int>();
-
-                        foreach (var state in (ThreadState[])Enum.GetValues(typeof(ThreadState)))
-                            map[state] = 0;
-
-                        foreach (var t in threads)
-                            map[t.ThreadState]++;
-
-                        StringBuilder b = new StringBuilder(DateTime.Now.ToString());
-
-                        foreach (var pair in map)
-                            b.Append("  ").Append(pair.Key).Append("=").Append(pair.Value);
-
-                        Dbg.WriteLine(b.ToString());
+                        X.WriteLine("Warm up finished...");
                     }
+
+                    start.Restart();
+
+                    long opers = adder;
+
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+
+                    long diff = adder - opers;
+
+                    start.Stop();
+
+                    // Convert to seconds.
+                    double tm = start.Elapsed.TotalSeconds;
+
+                    X.WriteLine("Operations/second: " + (long)(diff / tm));
+                }                
+
+                testStart.Stop();
+
+                Interlocked.Exchange(ref operationPerSecond, (long)(adder / testStart.Elapsed.TotalSeconds));
+            });
+
+            timer.IsBackground = true;
+            timer.Start();
+
+            long ops = 0;
+            long time = 0;
+                
+            var threads = new List<Thread>();
+
+            Stopwatch testStart2 = new Stopwatch();
+
+            testStart2.Start();
+
+            while (threads.Count < threadsCount) {
+                var t = new Thread(() => {
+                    Random rnd = new Random();
+ 
+                    int i = 0;
+
+                    Stopwatch start = new Stopwatch();
+
+                    while(true) {
+                        start.Restart();
+
+                        // Operation begin.
+                        Object key = rnd.Next(1000000);
+
+                        Object o = cache.GetItem<Object, Object>(key);
+
+                        if (o != null)
+                            key = rnd.Next(1000000);
+
+                        cache.Put(key, key);
+                        // Operation end.
+
+                        start.Stop();
+
+                        Interlocked.Add(ref time, (long)(start.Elapsed.TotalMilliseconds));
+
+                        Interlocked.Increment(ref adder);
+
+                        testStart2.Stop();
+
+                        if (testStart2.Elapsed.TotalSeconds > (duration + warmup))
+                            break;
+                        else 
+                            testStart2.Start();
+
+                        i++;
+                    }
+
+                    Interlocked.Add(ref ops, i);                  
                 });
 
-                stat.IsBackground = true;
-                stat.Start();
+                t.Name = "comparison-worker-" + threads.Count;
 
-                Dbg.WriteLine("Start threads: {0}", threads.Count);
-
-                foreach (var t in threads)
-                    t.Start();
-
-                Dbg.WriteLine("Threads started: {0}", threads.Count);
-
-                while (true) {
-                    Console.Out.WriteLine("Press 'q' key to exit...");
-
-                    if (Console.ReadKey(true).Key == ConsoleKey.Q) {
-                        Console.Out.WriteLine("\nAre you sure (y/n)?");
-
-                        if (Console.ReadKey(false).Key == ConsoleKey.Y)
-                            break;
-                    }
-                }
-
-                closed = true;
-
-                Dbg.WriteLine("\nStop threads: {0}", threads.Count);
-
-                for (int i = 0; i < threads.Count; i++) {
-                    var t = threads[i];
-                    if (t.IsAlive) {
-                        Dbg.WriteLine("Join thread #{0}", i);
-
-                        //t.Join();
-                        if (!t.Join(TimeSpan.FromSeconds(5))) {
-                            Dbg.WriteLine("Thread #{0} join timed out. Thread aborted.", i);
-                            t.Abort();
-                        }
-
-                        Dbg.WriteLine("Thread #{0} stopped", i);
-                    }
-                    else
-                        Dbg.WriteLine("Thread #{0} already stopped", i);
-                }
-
-                Dbg.WriteLine("\nAll threads stopped");
+                threads.Add(t);
             }
-            finally {
-                Dbg.WriteLine("Stop clients: {0}", clients.Count);
-                G.StopAll(true);
-                Dbg.WriteLine("Stoped clients: {0}", clients.Count);
-            }
+
+            foreach (var t in threads)
+                t.Start();
+
+            // Wait threads finish their job.
+            foreach (var t in threads)
+                t.Join();
+
+            closed = true;
+
+            timer.Join();
+
+            X.WriteLine("Average operations/second: " + operationPerSecond);
+
+            double latency = 1d * time / ops;
+
+            X.WriteLine("Average latency for PUT_GET test: " + latency + "ms");
+
+            G.StopAll();
         }
 
-        private static IGridClientConfiguration CreateConfig(GridClientProtocol proto, IGridClientSslContext sslCtx, String srv) {
-            var replicated = new GridClientDataConfiguration();
-            replicated.Name = "replicated";
-
-            var partitoned = new GridClientDataConfiguration();
-            partitoned.Name = "partitioned";
-            partitoned.Affinity = new GridClientPartitionAffinity();
+        private static IGridClientConfiguration CreateConfig(GridClientProtocol proto, String srv) {
+            var atomic = new GridClientDataConfiguration();
+            atomic.Name = "atomic";
+            atomic.Affinity = new GridClientPartitionAffinity();
 
             GridClientConfiguration cfg = new GridClientConfiguration();
 
-            cfg.DataConfigurations.Add(replicated);
-            cfg.DataConfigurations.Add(partitoned);
-
-            cfg.Protocol = proto;
-            cfg.SslContext = sslCtx;
+            cfg.DataConfigurations.Add(atomic);
             cfg.Servers.Add(srv);
-            cfg.Credentials = "s3cret";
 
             return cfg;
         }
