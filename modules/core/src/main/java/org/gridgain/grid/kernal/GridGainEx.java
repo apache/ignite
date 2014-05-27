b0008b83f23374310aa3b0622983689c1ccdcfdf
sboikov <sboikov@gridgain.com>
2014-05-27 17:00:14 +0400

    # GG-8416 Use NIOServer in java client

--- modules/core/src/main/java/org/gridgain/grid/kernal/GridGainEx.java
+++ modules/core/src/main/java/org/gridgain/grid/kernal/GridGainEx.java
@@ -1515,8 +1515,6 @@ public class GridGainEx {
                     DFLT_REST_KEEP_ALIVE_TIME,
                     new LinkedBlockingQueue<Runnable>(DFLT_REST_THREADPOOL_QUEUE_CAP)
                 );
-
-                myCfg.setRestExecutorService(restExecSvc);
             }
 
             execSvcShutdown = cfg.getExecutorServiceShutdown();
