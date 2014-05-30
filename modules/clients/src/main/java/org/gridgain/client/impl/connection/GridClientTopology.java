101e746f24911ff7865aeaff22ce076429cae457
Yakov Zhdanov <yzhdanov@gridgain.com>
2014-05-30 21:57:52 +0400

    sp-23 fixes

--- modules/clients/src/main/java/org/gridgain/client/impl/connection/GridClientTopology.java
+++ modules/clients/src/main/java/org/gridgain/client/impl/connection/GridClientTopology.java
@@ -11,7 +11,6 @@ package org.gridgain.client.impl.connection;
 import org.gridgain.client.*;
 import org.gridgain.client.impl.*;
 import org.gridgain.client.util.*;
-import org.gridgain.grid.lang.*;
 import org.gridgain.grid.util.typedef.*;
 import org.gridgain.grid.util.typedef.internal.*;
 
