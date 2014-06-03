1f1afa70799ac2238678a253dad8075a2eaca6c4
Valentin Kulichenko <vkulichenko@gridgain.com>
2014-06-03 16:25:39 +0400

    # GG-8489 - Fixed header

--- modules/core/src/main/java/org/gridgain/grid/kernal/GridKernal.java
+++ modules/core/src/main/java/org/gridgain/grid/kernal/GridKernal.java
@@ -1,17 +1,4 @@
-/* ${line.separator}
-Copyright (C) ${biz.name.full}. All Rights Reserved.${line.separator}
-${line.separator}
-Licensed under the Apache License, Version 2.0 (the &quot;License&quot;);${line.separator}
-you may not use this file except in compliance with the License.${line.separator}
-You may obtain a copy of the License at${line.separator}${line.separator}
-    http://www.apache.org/licenses/LICENSE-2.0${line.separator}
-${line.separator}
-Unless required by applicable law or agreed to in writing, software${line.separator}
-distributed under the License is distributed on an &quot;AS IS&quot; BASIS,${line.separator}
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.${line.separator}
-See the License for the specific language governing permissions and${line.separator}
-limitations under the License.${line.separator}
-*/
+/* @java.file.header */
 
 /*  _________        _____ __________________        _____
  *  __  ____/___________(_)______  /__  ____/______ ____(_)_______
