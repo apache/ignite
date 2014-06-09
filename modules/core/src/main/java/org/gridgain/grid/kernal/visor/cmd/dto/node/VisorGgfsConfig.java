eaf33573cd2fa0663216c9b623d82303da902d89
AKuznetsov <akuznetsov@gridgain.com>
2014-06-09 17:09:50 +0800

    # GG-8022 Endpoint now configured via map.

--- modules/core/src/main/java/org/gridgain/grid/kernal/visor/cmd/dto/node/VisorGgfsConfig.java
+++ modules/core/src/main/java/org/gridgain/grid/kernal/visor/cmd/dto/node/VisorGgfsConfig.java
@@ -134,7 +134,10 @@ public class VisorGgfsConfig implements Serializable {
         cfg.fragmentizerEnabled(ggfs.isFragmentizerEnabled());
         cfg.fragmentizerThrottlingBlockLength(ggfs.getFragmentizerThrottlingBlockLength());
         cfg.fragmentizerThrottlingDelay(ggfs.getFragmentizerThrottlingDelay());
-        cfg.ipcEndpointConfiguration("TODO"); // GG-8022 JSON WAS REMOVED ggfs.getIpcEndpointConfiguration());
+
+        Map<String, String> endpointCfg = ggfs.getIpcEndpointConfiguration();
+        cfg.ipcEndpointConfiguration(endpointCfg != null ? endpointCfg.toString() : null);
+
         cfg.ipcEndpointEnabled(ggfs.isIpcEndpointEnabled());
         cfg.maxSpace(ggfs.getMaxSpaceSize());
         cfg.managementPort(ggfs.getManagementPort());
