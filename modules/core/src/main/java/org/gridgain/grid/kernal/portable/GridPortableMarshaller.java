e0a94ffdef4e6f744d1576e445f1f9cfd89ba6cd
Valentin Kulichenko <vkulichenko@gridgain.com>
2014-06-10 19:27:13 +0400

    # GG-8491 - Portable objects

---
+++ modules/core/src/main/java/org/gridgain/grid/kernal/portable/GridPortableMarshaller.java
@@ -0,0 +1,26 @@
+/* @java.file.header */
+
+/*  _________        _____ __________________        _____
+ *  __  ____/___________(_)______  /__  ____/______ ____(_)_______
+ *  _  / __  __  ___/__  / _  __  / _  / __  _  __ `/__  / __  __ \
+ *  / /_/ /  _  /    _  /  / /_/ /  / /_/ /  / /_/ / _  /  _  / / /
+ *  \____/   /_/     /_/   \_,__/   \____/   \__,_/  /_/   /_/ /_/
+ */
+
+package org.gridgain.grid.kernal.portable;
+
+import org.gridgain.grid.portable.*;
+import org.jetbrains.annotations.*;
+
+/**
+ * Portable objects marshaller.
+ */
+public class GridPortableMarshaller {
+    public GridPortableObject marshal(@Nullable GridPortable portable) throws GridPortableException {
+        return null;
+    }
+
+    public GridPortable unmarshal(@Nullable GridPortableObject portableObj) throws GridPortableException {
+        return null;
+    }
+}
