17b1cb1eaa5f5e296e6c337f6d67f1db866599ba
Valentin Kulichenko <vkulichenko@gridgain.com>
2014-06-11 18:09:58 +0400

    # GG-8491 - Portable marshaller implementation

--- modules/core/src/main/java/org/gridgain/grid/kernal/portable/GridPortableMarshaller.java
+++ modules/core/src/main/java/org/gridgain/grid/kernal/portable/GridPortableMarshaller.java
@@ -16,11 +16,40 @@ import org.jetbrains.annotations.*;
  * Portable objects marshaller.
  */
 public class GridPortableMarshaller {
-    public GridPortableObject marshal(@Nullable GridPortable portable) throws GridPortableException {
-        return null;
+    /** */
+    static final byte NULL = (byte)0x80;
+
+    /** */
+    static final byte HANDLE = (byte)0x81;
+
+    /** */
+    static final byte OBJ = (byte)0x82;
+
+    /** */
+    private static final byte[] NULL_ARR = new byte[] {NULL};
+
+    /**
+     * @param portable Portable object.
+     * @return Byte array.
+     * @throws GridPortableException In case of error.
+     */
+    public byte[] marshal(@Nullable GridPortable portable) throws GridPortableException {
+        if (portable == null)
+            return NULL_ARR;
+
+        GridPortableWriterAdapter writer = new GridUnsafePortableWriter();
+
+        writer.writeObject(portable);
+
+        return writer.array();
     }
 
-    public GridPortable unmarshal(@Nullable GridPortableObject portableObj) throws GridPortableException {
+    /**
+     * @param arr Byte array.
+     * @return Portable object.
+     * @throws GridPortableException
+     */
+    @Nullable public GridPortable unmarshal(byte[] arr) throws GridPortableException {
         return null;
     }
 }
