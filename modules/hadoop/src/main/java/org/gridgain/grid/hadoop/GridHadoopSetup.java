88866ba7fcf65dd553686cc3d8c19736053d34fb
S.Vladykin <svladykin@gridgain.com>
2014-06-25 18:23:08 +0400

    #gg-8022-zz - setup

--- modules/hadoop/src/main/java/org/gridgain/grid/hadoop/GridHadoopSetup.java
+++ modules/hadoop/src/main/java/org/gridgain/grid/hadoop/GridHadoopSetup.java
@@ -39,9 +39,19 @@ public class GridHadoopSetup {
      * @param msg Exit message.
      */
     private static void exit(String msg) {
-        X.println("ERROR: " + msg);
+        X.println("### " + msg);
+        X.println("### Setup failed, exiting... ");
 
         System.exit(1);
+    }
+
+    /**
+     * Prints message.
+     *
+     * @param msg Message.
+     */
+    private static void println(String msg) {
+        X.println(">>> " + msg);
     }
 
     /**
@@ -56,6 +66,10 @@ public class GridHadoopSetup {
      * </ul>
      */
     private static void configureHadoop() {
+        String gridgainHome = U.getGridGainHome();
+
+        println("GRIDGAIN_HOME is set to '" + gridgainHome + "'.");
+
         String hadoopHome = System.getenv("HADOOP_HOME");
 
         if (hadoopHome == null || hadoopHome.isEmpty())
@@ -64,7 +78,7 @@ public class GridHadoopSetup {
 
         hadoopHome = hadoopHome.replaceAll("\"", "");
 
-        X.println("HADOOP_HOME is set to '" + hadoopHome + "'.");
+        println("HADOOP_HOME is set to '" + hadoopHome + "'.");
 
         File hadoopDir = new File(hadoopHome);
         
@@ -84,10 +98,10 @@ public class GridHadoopSetup {
         if (F.isEmpty(hadoopCommonHome)) {
             hadoopCommonDir = new File(hadoopDir, "share/hadoop/common");
 
-            X.println("HADOOP_COMMON_HOME is not set, will use '" + hadoopCommonDir.getPath() + "'.");
+            println("HADOOP_COMMON_HOME is not set, will use '" + hadoopCommonDir.getPath() + "'.");
         }
         else {
-            X.println("HADOOP_COMMON_HOME is set to '" + hadoopCommonHome + "'.");
+            println("HADOOP_COMMON_HOME is set to '" + hadoopCommonHome + "'.");
 
             hadoopCommonDir = new File(hadoopCommonHome);
         }
@@ -108,8 +122,10 @@ public class GridHadoopSetup {
 
             File winutilsFile = new File(hadoopBinDir, WINUTILS_EXE);
 
-            if (!winutilsFile.exists() && getAnswer("File " + WINUTILS_EXE + " does not exist. " +
+            if (!winutilsFile.exists() && ask("File " + WINUTILS_EXE + " does not exist. " +
                 "It may be replaced by a stub. Create it?")) {
+                println("Creating file stub '" + winutilsFile.getAbsolutePath() + "'.");
+
                 boolean ok = false;
 
                 try {
@@ -125,10 +141,6 @@ public class GridHadoopSetup {
 
             processCmdFiles(hadoopDir, "bin", "sbin", "libexec");
         }
-
-        String gridgainHome = U.getGridGainHome();
-
-        X.println("GRIDGAIN_HOME=" + gridgainHome);
 
         File gridGainLibs = new File(new File(gridgainHome), "libs");
 
@@ -146,11 +158,14 @@ public class GridHadoopSetup {
         for (File file : jarFiles) {
             File targetFile = new File(hadoopCommonLibDir, file.getName());
 
-            if (!targetFile.exists())
+            if (!linkTargetExists(targetFile) || !targetFile.exists()) {
                 jarsExist = false;
+
+                break;
+            }
         }
 
-        if (!jarsExist && getAnswer("GridGain JAR files are not found in Hadoop 'lib' directory. " +
+        if (!jarsExist && ask("GridGain JAR files are not found in Hadoop 'lib' directory. " +
             "Create appropriate symbolic links?")) {
             File[] oldGridGainJarFiles = hadoopCommonLibDir.listFiles(new FilenameFilter() {
                 @Override public boolean accept(File dir, String name) {
@@ -159,11 +174,13 @@ public class GridHadoopSetup {
             });
 
             if (oldGridGainJarFiles.length > 0) {
-                if (!getAnswer("The Hadoop 'lib' directory contains JARs from other GridGain installation. " +
-                    "It needs to be deleted to continue. Continue?"))
+                if (!ask("The Hadoop 'lib' directory contains JARs from other GridGain installation. " +
+                    "They must be deleted to continue. Continue?"))
                     return;
 
                 for (File file : oldGridGainJarFiles) {
+                    println("Deleting '" + file.getAbsolutePath() + "'.");
+
                     if (!file.delete())
                         exit("Failed to delete file '" + file.getPath() + "'.");
                 }
@@ -171,6 +188,8 @@ public class GridHadoopSetup {
 
             for (File file : jarFiles) {
                 File targetFile = new File(hadoopCommonLibDir, file.getName());
+
+                println("Creating symbolic link '" + targetFile.getAbsolutePath() + "'.");
 
                 try {
                     Files.createSymbolicLink(targetFile.toPath(), file.toPath());
@@ -181,7 +200,36 @@ public class GridHadoopSetup {
             }
         }
 
-        X.println("Hadoop setup is successfully completed.");
+        println("Hadoop setup is complete.");
+    }
+
+    /**
+     * Checks if link target exists.
+     *
+     * @param link Symbolic link.
+     * @return {@code true} If link target exists.
+     */
+    private static boolean linkTargetExists(File link) {
+        if (!Files.isSymbolicLink(link.toPath()))
+            return true; // It is a real file or it does not exist.
+
+        Path target = null;
+
+        try {
+            target = Files.readSymbolicLink(link.toPath());
+        }
+        catch (IOException e) {
+            exit("Failed to read symbolic link: " + link.getAbsolutePath());
+        }
+
+        if (Files.notExists(target)) {
+            println("Found link pointing to non-existing location: " + link.getAbsolutePath() + " -> " +
+                target.toAbsolutePath());
+
+            return false;
+        }
+
+        return true;
     }
 
     /**
@@ -190,8 +238,8 @@ public class GridHadoopSetup {
      * @param question Question to write.
      * @return {@code true} if user inputs 'Y' or 'y', {@code false} otherwise.
      */
-    private static boolean getAnswer(String question) {
-        X.print(question + " (Y/N):");
+    private static boolean ask(String question) {
+        X.print("  < " + question + " (Y/N): ");
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -204,11 +252,16 @@ public class GridHadoopSetup {
             exit("Failed to read answer: " + e.getMessage());
         }
 
-        if (answer != null)
-            if ("Y".equals(answer.toUpperCase().trim()))
-                return true;
+        if (answer != null && "Y".equals(answer.toUpperCase().trim())) {
+            X.println("  > Yes.");
 
-        return false;
+            return true;
+        }
+        else {
+            X.println("  > No.");
+
+            return false;
+        }
     }
 
     /**
@@ -251,7 +304,7 @@ public class GridHadoopSetup {
                 }
 
                 if (invalid) {
-                    answer = answer || getAnswer("One or more *.CMD files has invalid new line character. Replace them?");
+                    answer = answer || ask("One or more *.CMD files has invalid new line character. Replace them?");
 
                     if (!answer)
                         return;
