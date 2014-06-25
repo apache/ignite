4e10abaed97f51cce98080646c7ac717643fb266
vsisko <vsisko@gridgain.com>
2014-06-25 10:52:01 +0800

    # GG-8479 - GG-8479 Security task execution.

---
+++ modules/core/src/main/java/org/gridgain/grid/kernal/visor/cmd/tasks/VisorSecurityCollectorTask.java
@@ -0,0 +1,54 @@
+/* @java.file.header */
+
+/*  _________        _____ __________________        _____
+ *  __  ____/___________(_)______  /__  ____/______ ____(_)_______
+ *  _  / __  __  ___/__  / _  __  / _  / __  _  __ `/__  / __  __ \
+ *  / /_/ /  _  /    _  /  / /_/ /  / /_/ /  / /_/ / _  /  _  / / /
+ *  \____/   /_/     /_/   \_,__/   \____/   \__,_/  /_/   /_/ /_/
+ */
+
+package org.gridgain.grid.kernal.visor.cmd.tasks;
+
+import org.gridgain.grid.*;
+import org.gridgain.grid.kernal.processors.task.*;
+import org.gridgain.grid.kernal.visor.cmd.*;
+import org.gridgain.grid.security.*;
+import org.gridgain.grid.util.typedef.internal.*;
+
+import java.util.*;
+
+/**
+ * Collects security information from node.
+ */
+@GridInternal
+public class VisorSecurityCollectorTask extends VisorOneNodeTask<UUID, Collection<GridSecuritySubject>> {
+    /** {@inheritDoc} */
+    @Override protected VisorSecurityCollectorJob job(UUID arg) {
+        return new VisorSecurityCollectorJob(arg);
+    }
+
+    /**
+     * Job that collect security information form node.
+     */
+    private static class VisorSecurityCollectorJob extends VisorJob<UUID, Collection<GridSecuritySubject>> {
+        /** */
+        private static final long serialVersionUID = 0L;
+
+        /**
+         * @param arg Node ID to collect security information.
+         */
+        protected VisorSecurityCollectorJob(UUID arg) {
+            super(arg);
+        }
+
+        /** {@inheritDoc} */
+        @Override protected Collection<GridSecuritySubject> run(UUID nodeToCollect) throws GridException {
+            return g.security().authenticatedSubjects();
+        }
+
+        /** {@inheritDoc} */
+        @Override public String toString() {
+            return S.toString(VisorSecurityCollectorJob.class, this);
+        }
+    }
+}
