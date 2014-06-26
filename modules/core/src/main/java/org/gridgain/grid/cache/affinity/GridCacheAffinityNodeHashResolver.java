c800a490d3dbc44af6fa031e57a5145e982ecab0
vozerov <vozerov@gridgain.com>
2014-06-26 13:33:50 +0400

    #gg-8491: Docs for hash resolver.

--- modules/core/src/main/java/org/gridgain/grid/cache/affinity/GridCacheAffinityNodeHashResolver.java
+++ modules/core/src/main/java/org/gridgain/grid/cache/affinity/GridCacheAffinityNodeHashResolver.java
@@ -19,6 +19,14 @@ import java.io.*;
  * Node IDs constantly change when nodes get restarted, which causes affinity mapping to change between restarts,
  * and hence causing redundant repartitioning. Providing an alternate node hash value, which survives node restarts,
  * will help to map keys to the same nodes whenever possible.
+ * <p>
+ * Note that on case clients exist they will query this object from the server and use it for affinity calculation.
+ * Therefore you must ensure that:
+ * <ul>
+ *     <li>Server and clients can marshal and unmarshal this object in portable format, i.e. all parties have
+ *     object class(es) configured as portable.</li>
+ *     <li>All parties must calculate hash code of returned object in the same way.</li>
+ * </ul>
  */
 public interface GridCacheAffinityNodeHashResolver extends Serializable {
     /**
