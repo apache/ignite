f07f3595f089b504ed7533b99ec3469f2d94074e
Valentin Kulichenko <vkulichenko@gridgain.com>
2014-07-08 17:36:08 +0400

    GG-8491 - Fixed task execution

--- modules/core/src/main/java/org/gridgain/grid/kernal/processors/rest/handlers/task/GridTaskCommandHandler.java
+++ modules/core/src/main/java/org/gridgain/grid/kernal/processors/rest/handlers/task/GridTaskCommandHandler.java
@@ -63,11 +63,16 @@ public class GridTaskCommandHandler extends GridRestCommandHandlerAdapter {
     /** Topic ID generator. */
     private final AtomicLong topicIdGen = new AtomicLong();
 
+    /** Portable enabled flag. */
+    private final boolean portable;
+
     /**
      * @param ctx Context.
      */
     public GridTaskCommandHandler(final GridKernalContext ctx) {
         super(ctx);
+
+        portable = ctx.portable().isPortableEnabled();
 
         ctx.io().addMessageListener(TOPIC_REST, new GridMessageListener() {
             @Override public void onMessage(UUID nodeId, Object msg) {
@@ -235,7 +240,8 @@ public class GridTaskCommandHandler extends GridRestCommandHandlerAdapter {
                                 if (desc.error() == null) {
                                     try {
                                         taskRestRes.setFinished(true);
-                                        taskRestRes.setResult(ctx.portable().marshalToPortable(desc.result()));
+                                        taskRestRes.setResult(
+                                            portable ? ctx.portable().marshalToPortable(desc.result()) : desc.result());
 
                                         res.setResponse(taskRestRes);
                                         fut.onDone(res);
