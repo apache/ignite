da4b57464e230a2631058335cdfb077b77448f8a
AKuznetsov <akuznetsov@gridgain.com>
2014-08-23 18:56:46 +0800

    # GG-8968 Fix for Visor plugins dependencies.

---
+++ modules/visor-plugins/src/main/java/org/gridgain/visor/plugin/api/VisorPluginComponentsFactory.java
@@ -0,0 +1,40 @@
+/* @java.file.header */
+
+/*  _________        _____ __________________        _____
+ *  __  ____/___________(_)______  /__  ____/______ ____(_)_______
+ *  _  / __  __  ___/__  / _  __  / _  / __  _  __ `/__  / __  __ \
+ *  / /_/ /  _  /    _  /  / /_/ /  / /_/ /  / /_/ / _  /  _  / / /
+ *  \____/   /_/     /_/   \_,__/   \____/   \__,_/  /_/   /_/ /_/
+ */
+
+package org.gridgain.visor.plugin.api;
+
+import java.util.*;
+
+/**
+ * Factory for creating ready Visor UI blocks like nodes panel, log panel, ....
+ *
+ * Plugins will receive factory instance from Visor.
+ */
+public interface VisorPluginComponentsFactory {
+    /**
+     * Create panel with nodes.
+     *
+     * @param pluginName Plugin name.
+     * @param title Panel title.
+     * @param ovrMsg Overlay message text.
+     * @param showHostBtn Whether or not host button should be displayed.
+     * @return Nodes panel.
+     */
+    public VisorPluginNodesPanel nodesPanel(String pluginName, String title, List<String> ovrMsg, boolean showHostBtn);
+
+    /**
+     * Create panel with list of log events.
+     *
+     * @param pluginName Plugin name.
+     * @param title Panel title.
+     * @param ovrMsg Overlay message text.
+     * @return Log panel.
+     */
+    public VisorPluginLogPanel logPanel(String pluginName, String title, List<String> ovrMsg);
+}
---
+++ modules/visor-plugins/src/main/java/org/gridgain/visor/plugin/api/VisorPluginLogPanel.java
@@ -0,0 +1,18 @@
+/* @java.file.header */
+
+/*  _________        _____ __________________        _____
+ *  __  ____/___________(_)______  /__  ____/______ ____(_)_______
+ *  _  / __  __  ___/__  / _  __  / _  / __  _  __ `/__  / __  __ \
+ *  / /_/ /  _  /    _  /  / /_/ /  / /_/ /  / /_/ / _  /  _  / / /
+ *  \____/   /_/     /_/   \_,__/   \____/   \__,_/  /_/   /_/ /_/
+ */
+
+package org.gridgain.visor.plugin.api;
+
+/**
+ * Standart Visor panel with log entries.
+ *
+ * This panel will be created by {@link VisorPluginComponentsFactory}.
+ */
+public interface VisorPluginLogPanel extends VisorPluginComponent {
+}
