f5edc751499750a4c437b18baefc01cb31ed98b0
vozerov <vozerov@gridgain.com>
2014-09-03 10:04:27 +0400

    # gg-8999: Uncommenting assertion.

--- modules/core/src/main/java/org/gridgain/grid/kernal/processors/cache/distributed/dht/atomic/GridDhtAtomicCache.java
+++ modules/core/src/main/java/org/gridgain/grid/kernal/processors/cache/distributed/dht/atomic/GridDhtAtomicCache.java
@@ -1380,8 +1380,7 @@ public class GridDhtAtomicCache<K, V> extends GridDhtCacheAdapter<K, V> {
                             if (updRes.newValue() != null || newValBytes != null) {
                                 GridFuture<Boolean> f = entry.addReader(nodeId, req.messageId(), topVer);
 
-                                // TODO: GG-7419: Enable when fixed.
-                                // assert f == null : f;
+                                assert f == null : f;
                             }
                         }
                         else if (F.contains(readers, nodeId)) // Reader became primary or backup.
