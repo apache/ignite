877c12fe6cfe33ac1e53af21180d7d77cd88d573
Valentin Kulichenko <vkulichenko@gridgain.com>
2014-09-17 11:55:08 -0700

    GG-9048 - Fixed query events

--- modules/core/src/main/java/org/gridgain/grid/kernal/processors/cache/query/GridCacheQueryManager.java
+++ modules/core/src/main/java/org/gridgain/grid/kernal/processors/cache/query/GridCacheQueryManager.java
@@ -846,7 +846,7 @@ public abstract class GridCacheQueryManager<K, V> extends GridCacheManagerAdapte
                             cctx.localNode(),
                             "SQL fields query result set row read.",
                             EVT_CACHE_QUERY_OBJECT_READ,
-                            org.gridgain.grid.cache.query.GridCacheQueryType.SQL,
+                            org.gridgain.grid.cache.query.GridCacheQueryType.SQL_FIELDS,
                             cctx.namex(),
                             null,
                             qry.clause(),
@@ -1042,11 +1042,11 @@ public abstract class GridCacheQueryManager<K, V> extends GridCacheManagerAdapte
 
                     switch (type) {
                         case SQL:
-                            if (cctx.gridEvents().isRecordable(EVT_CACHE_OBJECT_READ)) {
+                            if (cctx.gridEvents().isRecordable(EVT_CACHE_QUERY_OBJECT_READ)) {
                                 cctx.gridEvents().record(new GridCacheQueryReadEvent<>(
                                     cctx.localNode(),
                                     "SQL query entry read.",
-                                    EVT_CACHE_OBJECT_READ,
+                                    EVT_CACHE_QUERY_OBJECT_READ,
                                     org.gridgain.grid.cache.query.GridCacheQueryType.SQL,
                                     cctx.namex(),
                                     qry.queryClassName(),
@@ -1088,11 +1088,11 @@ public abstract class GridCacheQueryManager<K, V> extends GridCacheManagerAdapte
                             break;
 
                         case SCAN:
-                            if (cctx.gridEvents().isRecordable(EVT_CACHE_OBJECT_READ)) {
+                            if (cctx.gridEvents().isRecordable(EVT_CACHE_QUERY_OBJECT_READ)) {
                                 cctx.gridEvents().record(new GridCacheQueryReadEvent<>(
                                     cctx.localNode(),
                                     "Scan query entry read.",
-                                    EVT_CACHE_OBJECT_READ,
+                                    EVT_CACHE_QUERY_OBJECT_READ,
                                     org.gridgain.grid.cache.query.GridCacheQueryType.SCAN,
                                     cctx.namex(),
                                     null,
