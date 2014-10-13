8027978a463b5dd62633ad876fb95283f0281f41
sevdokimov <sevdokimov@gridgain.com>
2014-10-13 12:16:01 +0400

    # GG-9232 Move GridDotNetConfiguration to grid-core

---
+++ modules/core/src/main/java/org/gridgain/grid/dotnet/GridDotNetConfiguration.java
@@ -0,0 +1,70 @@
+/* @java.file.header */
+
+/*  _________        _____ __________________        _____
+ *  __  ____/___________(_)______  /__  ____/______ ____(_)_______
+ *  _  / __  __  ___/__  / _  __  / _  / __  _  __ `/__  / __  __ \
+ *  / /_/ /  _  /    _  /  / /_/ /  / /_/ /  / /_/ / _  /  _  / / /
+ *  \____/   /_/     /_/   \_,__/   \____/   \__,_/  /_/   /_/ /_/
+ */
+
+package org.gridgain.grid.dotnet;
+
+import org.gridgain.grid.portables.*;
+
+import java.util.*;
+
+/**
+ *
+ */
+public class GridDotNetConfiguration implements GridPortableMarshalAware {
+    /** */
+    private GridDotNetPortableConfiguration portableCfg;
+
+    /** */
+    private List<String> assemblies;
+
+    /**
+     * @return Configuration.
+     */
+    public GridDotNetPortableConfiguration getPortableCfg() {
+        return portableCfg;
+    }
+
+    /**
+     * @param portableCfg Configuration.
+     */
+    public void setPortableCfg(GridDotNetPortableConfiguration portableCfg) {
+        this.portableCfg = portableCfg;
+    }
+
+    /**
+     * @return Assemblies.
+     */
+    public List<String> getAssemblies() {
+        return assemblies;
+    }
+
+    /**
+     *
+     * @param assemblies Assemblies.
+     */
+    public void setAssemblies(List<String> assemblies) {
+        this.assemblies = assemblies;
+    }
+
+    /** {@inheritDoc} */
+    @Override public void writePortable(GridPortableWriter writer) throws GridPortableException {
+        GridPortableRawWriter rawWriter = writer.rawWriter();
+
+        rawWriter.writeCollection(assemblies);
+        rawWriter.writeObject(portableCfg);
+    }
+
+    /** {@inheritDoc} */
+    @Override public void readPortable(GridPortableReader reader) throws GridPortableException {
+        GridPortableRawReader rawReader = reader.rawReader();
+
+        assemblies = (List<String>)rawReader.<String>readCollection();
+        portableCfg = (GridDotNetPortableConfiguration)rawReader.readObject();
+    }
+}
---
+++ modules/core/src/main/java/org/gridgain/grid/dotnet/GridDotNetPortableTypeConfiguration.java
@@ -0,0 +1,160 @@
+/* @java.file.header */
+
+/*  _________        _____ __________________        _____
+ *  __  ____/___________(_)______  /__  ____/______ ____(_)_______
+ *  _  / __  __  ___/__  / _  __  / _  / __  _  __ `/__  / __  __ \
+ *  / /_/ /  _  /    _  /  / /_/ /  / /_/ /  / /_/ / _  /  _  / / /
+ *  \____/   /_/     /_/   \_,__/   \____/   \__,_/  /_/   /_/ /_/
+ */
+
+package org.gridgain.grid.dotnet;
+
+import org.gridgain.grid.portables.*;
+
+/**
+ *
+ */
+public class GridDotNetPortableTypeConfiguration implements GridPortableMarshalAware {
+    /** */
+    private String assemblyName;
+
+    /** */
+    private String typeName;
+
+    /** */
+    private String nameMapper;
+
+    /** */
+    private String idMapper;
+
+    /** */
+    private String serializer;
+
+    /** */
+    private String affinityKeyFieldName;
+
+    /** */
+    private Boolean metadataEnabled;
+
+    /**
+     * @return Assembly name.
+     */
+    public String getAssemblyName() {
+        return assemblyName;
+    }
+
+    /**
+     * @param assemblyName New assembly name.
+     */
+    public void setAssemblyName(String assemblyName) {
+        this.assemblyName = assemblyName;
+    }
+
+    /**
+     * @return Type name.
+     */
+    public String getTypeName() {
+        return typeName;
+    }
+
+    /**
+     * @param typeName New type name.
+     */
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    /**
+     * @return Name mapper.
+     */
+    public String getNameMapper() {
+        return nameMapper;
+    }
+
+    /**
+     * @param nameMapper New name mapper.
+     */
+    public void setNameMapper(String nameMapper) {
+        this.nameMapper = nameMapper;
+    }
+
+    /**
+     * @return Id mapper.
+     */
+    public String getIdMapper() {
+        return idMapper;
+    }
+
+    /**
+     * @param idMapper New id mapper.
+     */
+    public void setIdMapper(String idMapper) {
+        this.idMapper = idMapper;
+    }
+
+    /**
+     * @return Serializer.
+     */
+    public String getSerializer() {
+        return serializer;
+    }
+
+    /**
+     * @param serializer New serializer.
+     */
+    public void setSerializer(String serializer) {
+        this.serializer = serializer;
+    }
+
+    /**
+     * @return Metadata enabled.
+     */
+    public Boolean getMetadataEnabled() {
+        return metadataEnabled;
+    }
+
+    /**
+     * @param metadataEnabled New metadata enabled.
+     */
+    public void setMetadataEnabled(Boolean metadataEnabled) {
+        this.metadataEnabled = metadataEnabled;
+    }
+
+    /** {@inheritDoc} */
+    @Override public void writePortable(GridPortableWriter writer) throws GridPortableException {
+        GridPortableRawWriter rawWriter = writer.rawWriter();
+
+        rawWriter.writeString(assemblyName);
+
+        rawWriter.writeString(typeName);
+
+        rawWriter.writeString(nameMapper);
+
+        rawWriter.writeString(idMapper);
+
+        rawWriter.writeString(serializer);
+
+        rawWriter.writeString(affinityKeyFieldName);
+
+        rawWriter.writeObject(metadataEnabled);
+    }
+
+    /** {@inheritDoc} */
+    @Override public void readPortable(GridPortableReader reader) throws GridPortableException {
+        GridPortableRawReader rawReader = reader.rawReader();
+
+        assemblyName = rawReader.readString();
+
+        typeName = rawReader.readString();
+
+        nameMapper = rawReader.readString();
+
+        idMapper = rawReader.readString();
+
+        serializer = rawReader.readString();
+
+        affinityKeyFieldName = rawReader.readString();
+
+        metadataEnabled = (Boolean)rawReader.readObject();
+    }
+}
