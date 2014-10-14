72462df4d97d49004b377f2d0625f6ed0b02808c
Yakov Zhdanov <yzhdanov@gridgain.com>
2014-10-14 13:59:05 +0400

    # sp-28 minor

--- modules/clients/src/test/bin/stop-nodes.cmd
+++ modules/clients/src/test/bin/stop-nodes.cmd
@@ -1,10 +1,10 @@
 @echo OFF
 
 echo Kill all grid nodes.
-FOR /f "tokens=1,2" %%G IN ('jps') DO IF (%%H) EQU (GridCommandLineStartup) taskkill /PID %%G /T /F
+FOR /f "tokens=1,2" %%G IN ('%JAVA_HOME%/bin/jps') DO IF (%%H) EQU (GridCommandLineStartup) taskkill /PID %%G /T /F
 
 echo Kill all grid nodes.
-FOR /f "tokens=1,2" %%G IN ('jps') DO IF (%%H) EQU (GridRouterCommandLineStartup) taskkill /PID %%G /T /F
+FOR /f "tokens=1,2" %%G IN ('%JAVA_HOME%/bin/jps') DO IF (%%H) EQU (GridRouterCommandLineStartup) taskkill /PID %%G /T /F
 
 echo Wait 1 seconds while nodes stopped.
 ping -n 1 127.0.0.1 > NUL
