a38449621dc0d272d32e449886e057c4f6f53e62
anovikov <anovikov@gridgain.com>
2014-10-29 16:28:15 +0700

    # GG-9362 Add test for class not found in query example.

--- examples/src/test/java/org/gridgain/examples/GridCacheExamplesSelfTest.java
+++ examples/src/test/java/org/gridgain/examples/GridCacheExamplesSelfTest.java
@@ -13,6 +13,8 @@ import org.gridgain.examples.datagrid.starschema.*;
 import org.gridgain.examples.datagrid.store.*;
 import org.gridgain.examples.datagrid.*;
 import org.gridgain.examples.datagrid.datastructures.*;
+import org.gridgain.grid.*;
+import org.gridgain.grid.kernal.*;
 import org.gridgain.testframework.junits.common.*;
 
 /**
@@ -86,7 +88,11 @@ public class GridCacheExamplesSelfTest extends GridAbstractExamplesTest {
      * @throws Exception If failed.
      */
     public void testGridCacheQueryExample() throws Exception {
-        CacheQueryExample.main(EMPTY_ARGS);
+        try (Grid g = GridGainEx.start("examples/config/example-cache.xml", "grid1")) {
+            CacheQueryExample.main(EMPTY_ARGS);
+            CacheQueryExample.main(EMPTY_ARGS);
+            CacheQueryExample.main(EMPTY_ARGS);
+        }
     }
 
     /**
