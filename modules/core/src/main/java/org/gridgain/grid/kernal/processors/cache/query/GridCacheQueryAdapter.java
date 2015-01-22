a8f87668472e732cfc0a664acbb180087da954e0
ivasilinets <ivasilinets@gridgain.com>
2015-01-22 20:42:36 +0300

    #IGNITE-53: wip.

--- modules/core/src/main/java/org/gridgain/grid/kernal/processors/cache/query/GridCacheQueryAdapter.java
+++ modules/core/src/main/java/org/gridgain/grid/kernal/processors/cache/query/GridCacheQueryAdapter.java
@@ -469,7 +469,7 @@ public class GridCacheQueryAdapter<T> implements GridCacheQuery<T> {
                 return Collections.singletonList(cctx.localNode());
 
             case REPLICATED:
-                if (prj != null || !incBackups)
+                if (prj != null)
                     return nodes(cctx, prj);
 
                 GridCacheDistributionMode mode = cctx.config().getDistributionMode();
