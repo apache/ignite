c34d24e64244c3e2b5734ef35bee3a6f65a04d10
sevdokimov <sevdokimov@gridgain.com>
2015-02-03 15:31:29 +0300

    # IGNITE-158 Fix compilation error.

--- modules/core/src/main/java/org/apache/ignite/internal/ClusterGroupAdapter.java
+++ modules/core/src/main/java/org/apache/ignite/internal/ClusterGroupAdapter.java
@@ -21,6 +21,7 @@ import org.apache.ignite.*;
 import org.apache.ignite.cache.*;
 import org.apache.ignite.cluster.*;
 import org.apache.ignite.internal.executor.*;
+import org.apache.ignite.internal.processors.cache.*;
 import org.apache.ignite.internal.util.typedef.*;
 import org.apache.ignite.internal.util.typedef.internal.*;
 import org.apache.ignite.lang.*;
