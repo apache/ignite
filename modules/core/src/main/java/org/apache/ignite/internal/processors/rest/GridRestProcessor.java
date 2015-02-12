63502e4033c0e530d4caa2853764a2fb4a9e9fbb
ivasilinets <ivasilinets@gridgain.com>
2015-02-12 19:54:16 +0300

    #ignite-189: fix compilation GridRestProcessor.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/rest/GridRestProcessor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/rest/GridRestProcessor.java
@@ -18,6 +18,7 @@
 package org.apache.ignite.internal.processors.rest;
 
 import org.apache.ignite.*;
+import org.apache.ignite.authentication.*;
 import org.apache.ignite.configuration.*;
 import org.apache.ignite.internal.*;
 import org.apache.ignite.internal.processors.*;
