bd2ce38a73eb6fb3625684c3033eba28a2279968
ivasilinets <ivasilinets@gridgain.com>
2015-02-12 20:36:59 +0300

    #ignite-189: move AuthenticationContext from org.apache.ignite.authentication to org.apache.ignite.plugin.security package.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/security/os/GridOsSecurityProcessor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/security/os/GridOsSecurityProcessor.java
@@ -24,7 +24,6 @@ import org.apache.ignite.internal.processors.*;
 import org.apache.ignite.internal.processors.security.*;
 import org.apache.ignite.internal.util.typedef.*;
 import org.apache.ignite.plugin.security.*;
-import org.apache.ignite.authentication.*;
 import org.jetbrains.annotations.*;
 
 import java.net.*;
