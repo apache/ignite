e6beac46b50924f8a25f9ea77c20492ae846b8dc
nikolay_tikhonov <ntikhonov@gridgain.com>
2015-02-13 09:54:43 +0300

    IGNITE-236 Fixed init value.

--- modules/core/src/test/java/org/apache/ignite/internal/ClusterNodeMetricsSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/ClusterNodeMetricsSelfTest.java
@@ -228,6 +228,22 @@ public class ClusterNodeMetricsSelfTest extends GridCommonAbstractTest {
     }
 
     /**
+     * @throws Exception If failed.
+     */
+    public void testClusterNodeMetrics() throws Exception {
+        Ignite ignite0 = grid();
+        Ignite ignite1 = startGrid(1);
+
+        ClusterMetrics metrics0 = ignite0.cluster().localNode().metrics();
+        ClusterMetrics metrics1 = ignite1.cluster().localNode().metrics();
+
+        ClusterMetrics nodesMetrics =
+            ignite0.cluster().forNode(ignite0.cluster().localNode(), ignite1.cluster().localNode()).metrics();
+
+        assertEquals(metrics0.getTotalCpus() + metrics1.getTotalCpus(), nodesMetrics.getTotalCpus());
+    }
+
+    /**
      * Test message.
      */
     @SuppressWarnings("UnusedDeclaration")
