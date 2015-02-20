63c3f5494cb973c8c034a3ebc15a20a95d59cb7e
ivasilinets <ivasilinets@gridgain.com>
2015-02-20 15:09:41 +0300

    #ignite-311: small fix.

--- modules/core/src/main/java/org/apache/ignite/internal/GridProperties.java
+++ modules/core/src/main/java/org/apache/ignite/internal/GridProperties.java
@@ -36,14 +36,15 @@ public class GridProperties {
     static {
         PROPS = new Properties();
 
-        readProperties(FILE_PATH, true);
+        readProperties(FILE_PATH, PROPS, true);
     }
 
     /**
      * @param path Path.
+     * @param props Properties.
      * @param throwExc Flag indicating whether to throw an exception or not.
      */
-    public static void readProperties(String path, boolean throwExc) {
+    public static void readProperties(String path, Properties props, boolean throwExc) {
         try (InputStream is = IgniteVersionUtils.class.getClassLoader().getResourceAsStream(path)) {
             if (is == null) {
                 if (throwExc)
@@ -52,7 +53,7 @@ public class GridProperties {
                     return;
             }
 
-            PROPS.load(is);
+            props.load(is);
         }
         catch (IOException e) {
             throw new RuntimeException("Failed to read properties file: " + path, e);
