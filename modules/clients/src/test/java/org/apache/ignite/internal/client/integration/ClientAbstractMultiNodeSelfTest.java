17344b9b66d1349df52a077c4430ac59a653a640
ivasilinets <ivasilinets@gridgain.com>
2015-02-24 14:46:43 +0300

    #ignite-237: Use CacheConsistentHashAffinityFunction in client test.

--- modules/clients/src/test/java/org/apache/ignite/internal/client/integration/ClientAbstractMultiNodeSelfTest.java
+++ modules/clients/src/test/java/org/apache/ignite/internal/client/integration/ClientAbstractMultiNodeSelfTest.java
@@ -19,6 +19,7 @@ package org.apache.ignite.internal.client.integration;
 
 import org.apache.ignite.*;
 import org.apache.ignite.cache.*;
+import org.apache.ignite.cache.affinity.consistenthash.*;
 import org.apache.ignite.cluster.*;
 import org.apache.ignite.compute.*;
 import org.apache.ignite.configuration.*;
@@ -194,6 +195,8 @@ public abstract class ClientAbstractMultiNodeSelfTest extends GridCommonAbstract
         cfg.setName(cacheName);
 
         cfg.setWriteSynchronizationMode(REPLICATED_ASYNC_CACHE_NAME.equals(cacheName) ? FULL_ASYNC : FULL_SYNC);
+
+        cfg.setAffinity(new CacheConsistentHashAffinityFunction());
 
         return cfg;
     }
