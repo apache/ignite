e8ff77931dd481b96321c89280c27e4567ae9598
ivasilinets <ivasilinets@gridgain.com>
2015-02-24 18:57:19 +0300

    #ignite-237: Change affinity to CacheConsistentHashAffinityFunction in tests.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/near/GridCachePartitionedFullApiSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/near/GridCachePartitionedFullApiSelfTest.java
@@ -20,6 +20,7 @@ package org.apache.ignite.internal.processors.cache.distributed.near;
 import org.apache.ignite.*;
 import org.apache.ignite.cache.*;
 import org.apache.ignite.cache.affinity.consistenthash.*;
+import org.apache.ignite.cache.affinity.rendezvous.*;
 import org.apache.ignite.configuration.*;
 import org.apache.ignite.internal.*;
 import org.apache.ignite.internal.processors.cache.*;
@@ -73,7 +74,7 @@ public class GridCachePartitionedFullApiSelfTest extends GridCacheAbstractFullAp
             cache.put(key, i);
         }
 
-        CacheConsistentHashAffinityFunction aff = (CacheConsistentHashAffinityFunction)cache.configuration().getAffinity();
+        CacheRendezvousAffinityFunction aff = (CacheRendezvousAffinityFunction)cache.configuration().getAffinity();
 
         for (int i = 0 ; i < aff.getPartitions(); i++)
             String.valueOf(cache.entrySet(i));
