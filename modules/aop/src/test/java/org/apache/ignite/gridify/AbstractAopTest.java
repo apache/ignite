1bdd8aa86dadec9706c18f9ac538ff1f835302b4
Valentin Kulichenko <vkulichenko@gridgain.com>
2015-03-05 00:00:40 -0800

    # IGNITE-141 - Marshallers refactoring

--- modules/aop/src/test/java/org/apache/ignite/gridify/AbstractAopTest.java
+++ modules/aop/src/test/java/org/apache/ignite/gridify/AbstractAopTest.java
@@ -419,8 +419,8 @@ public abstract class AbstractAopTest extends GridCommonAbstractTest {
 
             assert res == 1 : "Method gridifyDefault returned wrong value [result=" + res + ", expected=1]";
 
-            assert locDepCnt.get() == 1 : "Invalid local deployment count [expected=1, got=" + locDepCnt.get() + ']';
-            assert rmtDepCnt.get() == 1 : "Invalid remote deployment count [expected=1, got=" + rmtDepCnt.get() + ']';
+            assert locDepCnt.get() == 2 : "Invalid local deployment count [expected=2, got=" + locDepCnt.get() + ']';
+            assert rmtDepCnt.get() == 9 : "Invalid remote deployment count [expected=9, got=" + rmtDepCnt.get() + ']';
         }
         finally {
             stopAllGrids();
