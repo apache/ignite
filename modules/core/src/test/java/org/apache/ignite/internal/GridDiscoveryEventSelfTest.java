9bc57f0544fe2a69e11ace0c7ac13cb36541b2d5
Yakov Zhdanov <yzhdanov@gridgain.com>
2015-03-05 21:46:21 +0300

    sp-2 removed disco custom event test

--- modules/core/src/test/java/org/apache/ignite/internal/GridDiscoveryEventSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/GridDiscoveryEventSelfTest.java
@@ -21,7 +21,6 @@ import org.apache.ignite.*;
 import org.apache.ignite.cluster.*;
 import org.apache.ignite.configuration.*;
 import org.apache.ignite.events.*;
-import org.apache.ignite.internal.events.*;
 import org.apache.ignite.internal.util.typedef.*;
 import org.apache.ignite.internal.util.typedef.internal.*;
 import org.apache.ignite.lang.*;
