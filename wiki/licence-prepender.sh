7dbbcc4c7446bb39f9da2f7f74f8f195b1df98ff
Dmitiry Setrakyan <dsetrakyan@gridgain.com>
2015-03-06 19:11:20 -0800

    # sprint-2 - added documentation.

--- wiki/licence-prepender.sh
+++ wiki/licence-prepender.sh
@@ -17,8 +17,7 @@ license="<!--
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   See the License for the specific language governing permissions and
   limitations under the License.
--->
-"
+-->"
 
 case $osname in
     Darwin*)
