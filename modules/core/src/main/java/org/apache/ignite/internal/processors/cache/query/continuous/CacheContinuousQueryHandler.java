c72a45875b55de9f49375070ad3821a9138458bc
sboikov <sboikov@gridgain.com>
2015-03-16 17:00:33 +0300

    # sprint-2  minor

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/query/continuous/CacheContinuousQueryHandler.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/query/continuous/CacheContinuousQueryHandler.java
@@ -151,6 +151,9 @@ class CacheContinuousQueryHandler<K, V> implements GridContinuousHandler {
         assert routineId != null;
         assert ctx != null;
 
+        if (ctx.cache().internalCache(cacheName) == null)
+            return false;
+
         if (locLsnr != null)
             ctx.resource().injectGeneric(locLsnr);
 
