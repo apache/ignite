78fbe306530b37614f112b40f3fae5db9114bb98
AKuznetsov <akuznetsov@gridgain.com>
2015-03-16 20:28:50 +0700

    Merge branches 'ignite-368' and 'sprint-2' of https://git-wip-us.apache.org/repos/asf/incubator-ignite into ignite-368
    Conflicts:
    	modules/core/src/main/java/org/apache/ignite/internal/visor/node/VisorNodeDataCollectorJob.java

--- modules/core/src/main/java/org/apache/ignite/internal/visor/node/VisorNodeDataCollectorJob.java
+++ modules/core/src/main/java/org/apache/ignite/internal/visor/node/VisorNodeDataCollectorJob.java
@@ -18,8 +18,8 @@
 package org.apache.ignite.internal.visor.node;
 
 import org.apache.ignite.*;
-import org.apache.ignite.cache.*;
 import org.apache.ignite.configuration.*;
+import org.apache.ignite.internal.processors.cache.*;
 import org.apache.ignite.internal.processors.igfs.*;
 import org.apache.ignite.internal.util.ipc.*;
 import org.apache.ignite.internal.util.typedef.internal.*;
