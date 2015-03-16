7b255c09287380ff48fa6501cecbad02682cd86e
Yakov Zhdanov <yzhdanov@gridgain.com>
2015-03-16 17:20:16 +0300

    ignite-368 review

--- modules/core/src/main/java/org/apache/ignite/internal/GridKernalContextImpl.java
+++ modules/core/src/main/java/org/apache/ignite/internal/GridKernalContextImpl.java
@@ -306,9 +306,6 @@ public class GridKernalContextImpl implements GridKernalContext, Externalizable 
     /** Performance suggestions. */
     private final GridPerformanceSuggestions perf = new GridPerformanceSuggestions();
 
-    /** Exception registry. */
-    private IgniteExceptionRegistry registry;
-
     /** Marshaller context. */
     private MarshallerContextImpl marshCtx;
 
@@ -355,7 +352,6 @@ public class GridKernalContextImpl implements GridKernalContext, Externalizable 
         this.grid = grid;
         this.cfg = cfg;
         this.gw = gw;
-        this.registry = registry;
         this.utilityCachePool = utilityCachePool;
         this.marshCachePool = marshCachePool;
         this.execSvc = execSvc;
@@ -870,7 +866,7 @@ public class GridKernalContextImpl implements GridKernalContext, Externalizable 
 
     /** {@inheritDoc} */
     @Override public IgniteExceptionRegistry exceptionRegistry() {
-        return registry;
+        return IgniteExceptionRegistry.get();
     }
 
     /** {@inheritDoc} */
