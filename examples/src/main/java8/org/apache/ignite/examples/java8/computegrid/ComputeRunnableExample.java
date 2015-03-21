dea117528a8524caec5f1b7dffb09d04c904a8de
Dmitiry Setrakyan <dsetrakyan@gridgain.com>
2015-03-21 01:57:45 -0700

    # ignite-45 - fixing configs.

--- examples/src/main/java8/org/apache/ignite/examples/java8/computegrid/ComputeRunnableExample.java
+++ examples/src/main/java8/org/apache/ignite/examples/java8/computegrid/ComputeRunnableExample.java
@@ -26,7 +26,7 @@ import java.util.stream.*;
 /**
  * Demonstrates a simple use of {@link IgniteRunnable}.
  * <p>
- * Remote nodes should always be started with special configuration file which
+ * Remote nodes should always be 0started with special configuration file which
  * enables P2P class loading: {@code 'ignite.{sh|bat} examples/config/example-ignite.xml'}.
  * <p>
  * Alternatively you can run {@link ExampleNodeStartup} in another JVM which will start node
