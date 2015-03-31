4714b49fa67883138c8e868a3de2828efd9fa227
ivasilinets <ivasilinets@gridgain.com>
2015-04-01 00:21:29 +0300

    gg-9998: change classnames.properties.

---
+++ modules/core/src/main/java/org/apache/ignite/configuration/CompatibilityTest.java
@@ -0,0 +1,11 @@
+package org.apache.ignite.configuration;
+
+import java.io.Serializable;
+
+/**
+ * Class for compatibility test.
+ */
+public class CompatibilityTest implements Serializable {
+    /** */
+    private static final long serialVersionUID = 0L;
+}
