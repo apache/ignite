021ddf81e4fb492c8f21d3b81d9b89928fcae144
Andrey Novikov <anovikov@gridgain.com>
2015-04-10 14:34:21 +0700

    # ignite-715 Add check serialVersionUID for anonymous class.

--- modules/tools/src/main/java/org/apache/ignite/tools/classgen/ClassesGenerator.java
+++ modules/tools/src/main/java/org/apache/ignite/tools/classgen/ClassesGenerator.java
@@ -186,8 +186,7 @@ public class ClassesGenerator {
             Class<?> cls = Class.forName(clsName, false, ldr);
 
             if (Serializable.class.isAssignableFrom(cls) && !AbstractQueuedSynchronizer.class.isAssignableFrom(cls)) {
-                if (!cls.isInterface() && !Modifier.isAbstract(cls.getModifiers()) && !cls.isEnum() &&
-                    !cls.getSimpleName().isEmpty() && cls.getName().startsWith("org.apache.ignite")) {
+                if (!cls.isInterface() && !Modifier.isAbstract(cls.getModifiers()) && !cls.isEnum()) {
                     try {
                         Field field = cls.getDeclaredField("serialVersionUID");
 
