26368493d65162960760f322d82476d7ba4a6001
ivasilinets <ivasilinets@gridgain.com>
2015-04-16 11:59:46 +0300

    #ignite-758: Fix marshalling test

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheProxyImpl.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheProxyImpl.java
@@ -59,7 +59,7 @@ public class GridCacheProxyImpl<K, V> implements CacheProjection<K, V>, External
 
     /** Delegate object. */
     @GridToStringExclude
-    private GridCacheAdapter<K, V> delegate;
+    private CacheProjection<K, V> delegate;
 
     /** Projection. */
     @GridToStringExclude
@@ -83,7 +83,7 @@ public class GridCacheProxyImpl<K, V> implements CacheProjection<K, V>, External
      * @param delegate Delegate object.
      * @param prj Optional projection which will be passed to gateway.
      */
-    public GridCacheProxyImpl(GridCacheContext<K, V> ctx, GridCacheAdapter<K, V> delegate,
+    public GridCacheProxyImpl(GridCacheContext<K, V> ctx, CacheProjection<K, V> delegate,
         @Nullable CacheProjectionContext prj) {
         assert ctx != null;
         assert delegate != null;
--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/IgniteCacheProxy.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/IgniteCacheProxy.java
@@ -69,7 +69,7 @@ public class IgniteCacheProxy<K, V> extends AsyncSupportAdapter<IgniteCache<K, V
 
     /** Delegate. */
     @GridToStringInclude
-    private GridCacheAdapter<K, V> delegate;
+    private CacheProjection<K, V> delegate;
 
     /** Projection. */
     private CacheProjectionContext prjCtx;
@@ -97,7 +97,7 @@ public class IgniteCacheProxy<K, V> extends AsyncSupportAdapter<IgniteCache<K, V
      */
     public IgniteCacheProxy(
         GridCacheContext<K, V> ctx,
-        GridCacheAdapter<K, V> delegate,
+        CacheProjection<K, V> delegate,
         CacheProjectionContext prjCtx,
         boolean async
     ) {
@@ -1491,7 +1491,7 @@ public class IgniteCacheProxy<K, V> extends AsyncSupportAdapter<IgniteCache<K, V
     @Override public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
         ctx = (GridCacheContext<K, V>)in.readObject();
 
-        delegate = (GridCacheAdapter<K, V>)in.readObject();
+        delegate = (CacheProjection<K, V>)in.readObject();
 
         prjCtx = (CacheProjectionContext)in.readObject();
 
