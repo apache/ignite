6b032566337690f8d723ba784056ec87aca820e8
ivasilinets <ivasilinets@gridgain.com>
2015-04-20 12:03:27 +0300

    #ignite-758: fix IgfsDataManagerSelfTest.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/igfs/IgfsDataManagerSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/igfs/IgfsDataManagerSelfTest.java
@@ -189,7 +189,7 @@ public class IgfsDataManagerSelfTest extends IgfsCommonAbstractTest {
 
             for (int j = 0; j < NODES_CNT; j++) {
                 GridCacheContext<Object, Object> ctx = GridTestUtils.getFieldValue(grid(j).cachex(DATA_CACHE_NAME),
-                    GridCacheAdapter.class, "ctx");
+                    "ctx");
                 Collection<IgniteInternalTx> txs = ctx.tm().txs();
 
                 assert txs.isEmpty() : "Incomplete transactions: " + txs;
@@ -277,7 +277,7 @@ public class IgfsDataManagerSelfTest extends IgfsCommonAbstractTest {
 
             for (int j = 0; j < NODES_CNT; j++) {
                 GridCacheContext<Object, Object> ctx = GridTestUtils.getFieldValue(grid(j).cachex(DATA_CACHE_NAME),
-                    GridCacheAdapter.class, "ctx");
+                    "ctx");
                 Collection<IgniteInternalTx> txs = ctx.tm().txs();
 
                 assert txs.isEmpty() : "Incomplete transactions: " + txs;
@@ -357,7 +357,7 @@ public class IgfsDataManagerSelfTest extends IgfsCommonAbstractTest {
 
             for (int j = 0; j < NODES_CNT; j++) {
                 GridCacheContext<Object, Object> ctx = GridTestUtils.getFieldValue(grid(j).cachex(DATA_CACHE_NAME),
-                    GridCacheAdapter.class, "ctx");
+                    "ctx");
                 Collection<IgniteInternalTx> txs = ctx.tm().txs();
 
                 assert txs.isEmpty() : "Incomplete transactions: " + txs;
