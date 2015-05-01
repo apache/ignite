2cb73176d3a671cb2ac784310a4d36b2d03bad29
Dmitiry Setrakyan <dsetrakyan@gridgain.com>
2015-04-30 18:45:38 -0700

    # sprint-4 - Added javadoc for the withAsync method.

--- modules/core/src/main/java/org/apache/ignite/lang/IgniteAsyncSupport.java
+++ modules/core/src/main/java/org/apache/ignite/lang/IgniteAsyncSupport.java
@@ -22,7 +22,7 @@ package org.apache.ignite.lang;
  */
 public interface IgniteAsyncSupport {
     /**
-     * Gets component with asynchronous mode enabled.
+     * Gets instance of this component with asynchronous mode enabled.
      *
      * @return Component with asynchronous mode enabled.
      */
