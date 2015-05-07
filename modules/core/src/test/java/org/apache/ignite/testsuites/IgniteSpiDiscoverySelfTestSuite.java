7449c6056b8c0963a2a807f4f45902df0c739e2b
sevdokimov <sevdokimov@gridgain.com>
2015-05-07 16:56:58 +0300

    # IGNITE-709 Add TcpClientDiscoverySpiConfigSelfTest.

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteSpiDiscoverySelfTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteSpiDiscoverySelfTestSuite.java
@@ -52,6 +52,7 @@ public class IgniteSpiDiscoverySelfTestSuite extends TestSuite {
         suite.addTest(new TestSuite(GridTcpSpiForwardingSelfTest.class));
 
         suite.addTest(new TestSuite(TcpClientDiscoverySelfTest.class));
+        suite.addTest(new TestSuite(TcpClientDiscoverySpiConfigSelfTest.class));
 
         return suite;
     }
