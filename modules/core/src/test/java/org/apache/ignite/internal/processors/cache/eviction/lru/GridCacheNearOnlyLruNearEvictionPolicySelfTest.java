06462598ca5eeda3ea91e5ce841d3087daff3cbb
sboikov <sboikov@gridgain.com>
2015-05-15 16:12:00 +0300

    # ignite-709_3

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/eviction/lru/GridCacheNearOnlyLruNearEvictionPolicySelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/eviction/lru/GridCacheNearOnlyLruNearEvictionPolicySelfTest.java
@@ -66,17 +66,18 @@ public class GridCacheNearOnlyLruNearEvictionPolicySelfTest extends GridCommonAb
 
         if (cnt == 0)
             c.setClientMode(true);
+        else {
+            CacheConfiguration cc = new CacheConfiguration();
 
-        CacheConfiguration cc = new CacheConfiguration();
+            cc.setAtomicityMode(atomicityMode);
+            cc.setCacheMode(cacheMode);
+            cc.setWriteSynchronizationMode(PRIMARY_SYNC);
+            cc.setRebalanceMode(SYNC);
+            cc.setStartSize(100);
+            cc.setBackups(0);
 
-        cc.setAtomicityMode(atomicityMode);
-        cc.setCacheMode(cacheMode);
-        cc.setWriteSynchronizationMode(PRIMARY_SYNC);
-        cc.setRebalanceMode(SYNC);
-        cc.setStartSize(100);
-        cc.setBackups(0);
-
-        c.setCacheConfiguration(cc);
+            c.setCacheConfiguration(cc);
+        }
 
         TcpDiscoverySpi disco = new TcpDiscoverySpi();
 
