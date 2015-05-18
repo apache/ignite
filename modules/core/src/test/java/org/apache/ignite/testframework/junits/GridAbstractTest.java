c3744ba8fc571f0ec385b1e3cd1f972973250cb5
sevdokimov <sevdokimov@gridgain.com>
2015-05-18 20:04:28 +0300

    # IGNITE-709 Fix hang sests: set joinTimeout for discovery SPI.

--- modules/core/src/test/java/org/apache/ignite/testframework/junits/GridAbstractTest.java
+++ modules/core/src/test/java/org/apache/ignite/testframework/junits/GridAbstractTest.java
@@ -676,9 +676,11 @@ public abstract class GridAbstractTest extends TestCase {
     protected IgniteConfiguration optimize(IgniteConfiguration cfg) throws IgniteCheckedException {
         // TODO: IGNITE-605: propose another way to avoid network overhead in tests.
         if (cfg.getLocalHost() == null) {
-            if (cfg.getDiscoverySpi() instanceof TcpDiscoverySpi
-                || cfg.getDiscoverySpi() instanceof TcpClientDiscoverySpi)
+            if (cfg.getDiscoverySpi() instanceof TcpDiscoverySpiAdapter) {
                 cfg.setLocalHost("127.0.0.1");
+
+                ((TcpDiscoverySpiAdapter)cfg.getDiscoverySpi()).setJoinTimeout(5000);
+            }
             else
                 cfg.setLocalHost(getTestResources().getLocalHost());
         }
