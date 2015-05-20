b20bd02c5bf1f7a665ac992be5d008ea8b93e33a
sevdokimov <sevdokimov@gridgain.com>
2015-05-20 18:06:48 +0300

    # IGNITE-709 Remove incorrect test from suite.

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteSpiDiscoverySelfTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteSpiDiscoverySelfTestSuite.java
@@ -55,8 +55,6 @@ public class IgniteSpiDiscoverySelfTestSuite extends TestSuite {
         suite.addTest(new TestSuite(TcpClientDiscoverySpiConfigSelfTest.class));
         suite.addTest(new TestSuite(TcpClientDiscoveryMarshallerCheckSelfTest.class));
 
-        suite.addTest(new TestSuite(TcpDiscoveryConcurrentStartTest.class));
-
         return suite;
     }
 }
