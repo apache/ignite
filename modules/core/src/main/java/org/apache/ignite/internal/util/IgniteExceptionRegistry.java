662f7337f7d03f94373f4ebd083d308823aff50c
AKuznetsov <akuznetsov@gridgain.com>
2015-06-08 16:44:25 +0700

    # IGNITE-992 Review.

--- modules/core/src/main/java/org/apache/ignite/internal/util/IgniteExceptionRegistry.java
+++ modules/core/src/main/java/org/apache/ignite/internal/util/IgniteExceptionRegistry.java
@@ -20,6 +20,7 @@ package org.apache.ignite.internal.util;
 import org.apache.ignite.*;
 import org.apache.ignite.internal.util.tostring.*;
 import org.apache.ignite.internal.util.typedef.internal.*;
+import org.apache.ignite.internal.visor.util.*;
 
 import java.io.*;
 import java.util.*;
@@ -160,7 +161,7 @@ public class IgniteExceptionRegistry {
 
         /** */
         @GridToStringExclude
-        private final Throwable error;
+        private final VisorExceptionWrapper error;
 
         /** */
         private final long threadId;
@@ -186,7 +187,7 @@ public class IgniteExceptionRegistry {
          */
         public ExceptionInfo(long order, Throwable error, String msg, long threadId, String threadName, long time) {
             this.order = order;
-            this.error = error;
+            this.error = VisorTaskUtils.wrap(error);
             this.threadId = threadId;
             this.threadName = threadName;
             this.time = time;
@@ -210,7 +211,7 @@ public class IgniteExceptionRegistry {
         /**
          * @return Suppressed error.
          */
-        public Throwable error() {
+        public VisorExceptionWrapper error() {
             return error;
         }
 
