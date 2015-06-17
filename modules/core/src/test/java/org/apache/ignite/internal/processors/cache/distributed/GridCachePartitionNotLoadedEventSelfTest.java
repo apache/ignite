8c2553eda7ea68498c1d0235060b20f31ef55c8a
nikolay_tikhonov <ntikhonov@gridgain.com>
2015-06-17 16:12:48 +0300

    Added tests on EVT_CACHE_REBALANCE_PART_DATA_LOST event.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/GridCachePartitionNotLoadedEventSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/GridCachePartitionNotLoadedEventSelfTest.java
@@ -22,15 +22,19 @@ import org.apache.ignite.cache.*;
 import org.apache.ignite.cache.affinity.*;
 import org.apache.ignite.configuration.*;
 import org.apache.ignite.events.*;
+import org.apache.ignite.internal.*;
+import org.apache.ignite.internal.processors.cache.distributed.dht.preloader.*;
 import org.apache.ignite.lang.*;
 import org.apache.ignite.spi.discovery.tcp.*;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.*;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.*;
+import org.apache.ignite.testframework.*;
 import org.apache.ignite.testframework.junits.common.*;
 import org.apache.ignite.util.*;
 import org.eclipse.jetty.util.*;
 
 import java.util.*;
+import java.util.concurrent.*;
 
 /**
  *
@@ -145,6 +149,84 @@ public class GridCachePartitionNotLoadedEventSelfTest extends GridCommonAbstract
         awaitPartitionMapExchange();
 
         assert !jcache(1).containsKey(key);
+
+        assert !lsnr.lostParts.isEmpty();
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testStableTopology() throws Exception {
+        backupCnt = 1;
+
+        startGrid(1);
+
+        awaitPartitionMapExchange();
+
+        startGrid(0);
+
+        PartitionNotFullyLoadedListener lsnr = new PartitionNotFullyLoadedListener();
+
+        grid(1).events().localListen(lsnr, EventType.EVT_CACHE_REBALANCE_PART_DATA_LOST);
+
+        IgniteCache<Integer, Integer> cache0 = jcache(0);
+
+        int key = primaryKey(cache0);
+
+        jcache(1).put(key, key);
+
+        assert cache0.containsKey(key);
+
+        TestTcpCommunicationSpi.stop(ignite(0));
+
+        stopGrid(0, true);
+
+        awaitPartitionMapExchange();
+
+        assert jcache(1).containsKey(key);
+
+        assert lsnr.lostParts.isEmpty();
+    }
+
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testMapPartitioned() throws Exception {
+        backupCnt = 0;
+
+        startGrid(0);
+
+        startGrid(1);
+
+        PartitionNotFullyLoadedListener lsnr = new PartitionNotFullyLoadedListener();
+
+        grid(1).events().localListen(lsnr, EventType.EVT_CACHE_REBALANCE_PART_DATA_LOST);
+
+        TestTcpCommunicationSpi.skipMsgType(ignite(0), GridDhtPartitionsFullMessage.class);
+
+        IgniteInternalFuture<Object> fut = GridTestUtils.runAsync(new Callable<Object>() {
+            @Override public Object call() throws Exception {
+                startGrid(2);
+
+                return null;
+            }
+        });
+
+        boolean timeout = false;
+
+        try {
+            fut.get(1, TimeUnit.SECONDS);
+        }
+        catch (IgniteFutureTimeoutCheckedException e) {
+            timeout = true;
+        }
+
+        assert timeout;
+
+        stopGrid(0, true);
+
+        awaitPartitionMapExchange();
 
         assert !lsnr.lostParts.isEmpty();
     }
