bee6f6884ba0da9b9418842f7926d1dbd3ddd624
iveselovskiy <iveselovskiy@gridgain.com>
2015-07-09 15:05:01 +0300

    # Hive version changed in tests: 1.2.0 -> 1.2.1

--- modules/hadoop/src/test/java/org/apache/ignite/testsuites/IgniteHadoopTestSuite.java
+++ modules/hadoop/src/test/java/org/apache/ignite/testsuites/IgniteHadoopTestSuite.java
@@ -130,7 +130,7 @@ public class IgniteHadoopTestSuite extends TestSuite {
      * @throws Exception If failed.
      */
     public static void downloadHive() throws Exception {
-        String ver = IgniteSystemProperties.getString("hive.version", "1.2.0");
+        String ver = IgniteSystemProperties.getString("hive.version", "1.2.1");
 
         X.println("Will use Hive version: " + ver);
 
