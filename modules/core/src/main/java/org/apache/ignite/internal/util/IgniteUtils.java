638679451df99499b5699534b39da08b665cf5ba
ivasilinets <ivasilinets@gridgain.com>
2015-07-10 10:42:17 +0300

    #ignite-gg-10526: fix consistentId.

--- modules/core/src/main/java/org/apache/ignite/internal/util/IgniteUtils.java
+++ modules/core/src/main/java/org/apache/ignite/internal/util/IgniteUtils.java
@@ -8048,13 +8048,9 @@ public abstract class IgniteUtils {
     public static String consistentId(Collection<String> addrs, int port) {
         assert !F.isEmpty(addrs);
 
-        List<String> sortedAddrs = new ArrayList<>(addrs);
-
-        Collections.sort(sortedAddrs);
-
         StringBuilder sb = new StringBuilder();
 
-        for (String addr : sortedAddrs)
+        for (String addr : addrs)
             sb.append(addr).append(',');
 
         sb.delete(sb.length() - 1, sb.length());
