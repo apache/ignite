6894a87bf66f9de058f184197a6f03b7c01f628f
glutters <mitenick@gmail.com>
2015-07-18 14:03:47 +0200

    # ignite-788

---
+++ modules/log4j2/src/test/java/org/apache/ignite/testsuites/IgniteLog4j2TestSuite.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.testsuites;
+
+import org.apache.ignite.logger.log4j2.GridLog4j2CorrectFileNameTest;
+import org.apache.ignite.logger.log4j2.GridLog4j2InitializedTest;
+import org.apache.ignite.logger.log4j2.GridLog4j2LoggingFileTest;
+
+import junit.framework.TestSuite;
+
+/**
+ * Log4j2 logging tests.
+ */
+public class IgniteLog4j2TestSuite extends TestSuite {
+    /**
+     * @return Test suite.
+     * @throws Exception Thrown in case of the failure.
+     */
+    public static TestSuite suite() throws Exception {
+        TestSuite suite = new TestSuite("Log4j2 Logging Test Suite");
+
+        suite.addTest(new TestSuite(GridLog4j2InitializedTest.class));
+        suite.addTest(new TestSuite(GridLog4j2CorrectFileNameTest.class));
+        suite.addTest(new TestSuite(GridLog4j2LoggingFileTest.class));
+        return suite;
+    }
+
+}
