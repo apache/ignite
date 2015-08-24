2c51c56919bc5c0f61e4c96ebf4a47a4aa804986
vozerov-gridgain <vozerov@gridgain.com>
2015-08-24 12:02:34 +0300

    IGNITE-1284: Added callback utility classes.

--- modules/platform/src/main/java/org/apache/ignite/internal/platform/PlatformIgnition.java
+++ modules/platform/src/main/java/org/apache/ignite/internal/platform/PlatformIgnition.java
@@ -1,5 +1,3 @@
-package org.apache.ignite.internal.platform;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -16,6 +14,8 @@ package org.apache.ignite.internal.platform;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.apache.ignite.internal.platform;
 
 import org.apache.ignite.*;
 import org.apache.ignite.configuration.*;
