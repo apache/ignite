fb1d66d026963f04934b555f7bfd6118ffa388b5
ptupitsyn <ptupitsyn@gridgain.com>
2015-08-26 19:30:34 +0300

    Portable ifaces

---
+++ modules/platform/src/main/dotnet/Apache.Ignite.Core/Portable/PortableTypeNames.cs
@@ -0,0 +1,115 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Core.Portable
+{
+    /// <summary>
+    /// Portable type name constants.
+    /// </summary>
+    public static class PortableTypeNames
+    {
+        /** Type name: boolean. */
+        public const string Bool = "boolean";
+
+        /** Type name: byte. */
+        public const string Byte = "byte";
+
+        /** Type name: short. */
+        public const string Short = "short";
+
+        /** Type name: char. */
+        public const string Char = "char";
+
+        /** Type name: int. */
+        public const string Int = "int";
+
+        /** Type name: long. */
+        public const string Long = "long";
+
+        /** Type name: float. */
+        public const string Float = "float";
+
+        /** Type name: double. */
+        public const string Double = "double";
+
+        /** Type name: decimal. */
+        public const string Decimal = "decimal";
+
+        /** Type name: String. */
+        public const string String = "String";
+
+        /** Type name: UUID. */
+        public const string Guid = "UUID";
+
+        /** Type name: Date. */
+        public const string Date = "Date";
+
+        /** Type name: Enum. */
+        public const string Enum = "Enum";
+
+        /** Type name: Object. */
+        public const string Object = "Object";
+
+        /** Type name: boolean array. */
+        public const string ArrayBool = "boolean[]";
+
+        /** Type name: byte array. */
+        public const string ArrayByte = "byte[]";
+
+        /** Type name: short array. */
+        public const string ArrayShort = "short[]";
+
+        /** Type name: char array. */
+        public const string ArrayChar = "char[]";
+
+        /** Type name: int array. */
+        public const string ArrayInt = "int[]";
+
+        /** Type name: long array. */
+        public const string ArrayLong = "long[]";
+
+        /** Type name: float array. */
+        public const string ArrayFloat = "float[]";
+
+        /** Type name: double array. */
+        public const string ArrayDouble = "double[]";
+
+        /** Type name: decimal array. */
+        public const string ArrayDecimal = "decimal[]";
+
+        /** Type name: String array. */
+        public const string ArrayString = "String[]";
+
+        /** Type name: UUID array. */
+        public const string ArrayGuid = "UUID[]";
+
+        /** Type name: Date array. */
+        public const string ArrayDate = "Date[]";
+
+        /** Type name: Enum array. */
+        public const string ArrayEnum = "Enum[]";
+
+        /** Type name: Object array. */
+        public const string ArrayObject = "Object[]";
+
+        /** Type name: Collection. */
+        public const string Collection = "Collection";
+
+        /** Type name: Map. */
+        public const string Map = "Map";
+    }
+}
