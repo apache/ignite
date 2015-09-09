66aea2c7832442dcb077ee2b78168baf0b6473d9
ptupitsyn <ptupitsyn@gridgain.com>
2015-09-09 12:07:37 +0300

    cleanup

--- modules/platform/src/main/java/org/apache/ignite/internal/processors/platform/PlatformProcessorImpl.java
+++ modules/platform/src/main/java/org/apache/ignite/internal/processors/platform/PlatformProcessorImpl.java
@@ -114,6 +114,7 @@ public class PlatformProcessorImpl extends GridProcessorAdapter implements Platf
 
         platformCtx = new PlatformContextImpl(ctx, interopCfg.gate(), interopCfg.memory());
     }
+
     /** {@inheritDoc} */
     @Override public void start() throws IgniteCheckedException {
         try (PlatformMemory mem = platformCtx.memory().allocate()) {
@@ -145,11 +146,6 @@ public class PlatformProcessorImpl extends GridProcessorAdapter implements Platf
 
         // Add Interop node attributes.
         ctx.addNodeAttribute(PlatformUtils.ATTR_PLATFORM, interopCfg.platform());
-    }
-
-    /** {@inheritDoc} */
-    @Override public void onKernalStart() throws IgniteCheckedException {
-        // TODO: ??
     }
 
     /** {@inheritDoc} */
