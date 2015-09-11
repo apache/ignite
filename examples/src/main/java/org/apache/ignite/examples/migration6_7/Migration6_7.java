220ee836c1004d49460dc927a0282cb6470996f7
ashutak <ashutak@gridgain.com>
2015-09-11 17:08:16 +0300

    [migration from gg-6 to ignite] gg-6 client

---
+++ examples/src/main/java/org/apache/ignite/examples/migration6_7/Migration6_7.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.examples.migration6_7;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import org.gridgain.client.GridClient;
+import org.gridgain.client.GridClientConfiguration;
+import org.gridgain.client.GridClientData;
+import org.gridgain.client.GridClientDataConfiguration;
+import org.gridgain.client.GridClientFactory;
+import org.gridgain.client.GridClientPartitionAffinity;
+
+/**
+ *
+ */
+public class Migration6_7 {
+
+    public static final String PARTITIONED = "partitioned";
+
+    public static void main(String[] args) throws Exception {
+        try(GridClient ggClient = GridClientFactory.start(gg6ClientConfiguration())) {
+            GridClientData data = ggClient.data(PARTITIONED);
+
+            for (int i = 0; i < 100; i++)
+                System.out.println(">>>>> Gg6 client got [i="+i+", val="+data.get(i)+"]");
+        }
+    }
+
+    private static GridClientConfiguration gg6ClientConfiguration() {
+        GridClientConfiguration c = new GridClientConfiguration();
+
+        c.setServers(Collections.singleton("127.0.0.1:11211"));
+
+        Collection<GridClientDataConfiguration> dataCfgs = new ArrayList<>();
+
+        GridClientDataConfiguration partData = new GridClientDataConfiguration();
+        partData.setName(PARTITIONED);
+        partData.setAffinity(new GridClientPartitionAffinity());
+
+        dataCfgs.add(partData);
+
+        c.setDataConfigurations(dataCfgs);
+
+        return c;
+    }
+}
