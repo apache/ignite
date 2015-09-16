f1724c52b29b01b6350091d41cf27fe7990ef6ce
ptupitsyn <ptupitsyn@gridgain.com>
2015-09-16 13:09:57 +0300

    wip

--- modules/platform/src/main/dotnet/Apache.Ignite.Core/Events/EventBase.cs
+++ modules/platform/src/main/dotnet/Apache.Ignite.Core/Events/EventBase.cs
@@ -47,7 +47,7 @@ namespace Apache.Ignite.Core.Events
         private readonly string _name;
 
         /** */
-        private readonly DateTime _timeStamp;
+        private readonly DateTime _timestamp;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="EventBase"/> class.
@@ -64,7 +64,7 @@ namespace Apache.Ignite.Core.Events
             _message = r.ReadString();
             _type = r.ReadInt();
             _name = r.ReadString();
-            _timeStamp = r.ReadDate() ?? DateTime.Now;
+            _timestamp = r.ReadDate() ?? DateTime.Now;
         }
 
         /** <inheritDoc /> */
@@ -104,9 +104,9 @@ namespace Apache.Ignite.Core.Events
         }
 
         /** <inheritDoc /> */
-        public DateTime TimeStamp
+        public DateTime Timestamp
         {
-            get { return _timeStamp; }
+            get { return _timestamp; }
         }
 
         /** <inheritDoc /> */
@@ -143,7 +143,7 @@ namespace Apache.Ignite.Core.Events
         /** <inheritDoc /> */
         public override string ToString()
         {
-            return string.Format("CacheEntry [Name={0}, Type={1}, TimeStamp={2}, Message={3}]", Name, Type, TimeStamp,
+            return string.Format("CacheEntry [Name={0}, Type={1}, Timestamp={2}, Message={3}]", Name, Type, Timestamp,
                 Message);
         }
 
