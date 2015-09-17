1a5e5ff32bc544ae6b1a32dfe07ef6204dc2053b
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2015-09-17 17:27:05 +0300

    IGNITE-1496: Added .Net examples.

---
+++ modules/platform/src/main/dotnet/Examples/Apache.Ignite.Examples/Compute/TaskExample.cs
@@ -0,0 +1,145 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using Apache.Ignite.Core;
+using Apache.Ignite.ExamplesDll.Compute;
+using Apache.Ignite.ExamplesDll.Portable;
+
+namespace Apache.Ignite.Examples.Compute
+{
+    /// <summary>
+    /// Example demonstrating task execution.
+    /// <para />
+    /// To run the example please do the following:
+    /// 1) Build the project Apache.Ignite.ExamplesDll (select it -> right-click -> Build);
+    /// 2) Set this class as startup object (Apache.Ignite.Examples project -> right-click -> Properties ->
+    ///     Application -> Startup object);
+    /// 3) Start application (F5 or Ctrl+F5).
+    /// <para />
+    /// This example can be run in conjunction with standalone Apache Ignite .Net node.
+    /// To start standalone node please do the following:
+    /// 1) Build the project Apache.Ignite.ExamplesDll if you haven't done it yet (select it -> right-click -> Build);
+    /// 2) Locate created Apache.Ignite.ExamplesDll.dll file (Apache.Ignite.ExamplesDll project -> right-click ->
+    ///     Properties -> Build -> Output path);
+    /// 3) Go to .Net binaries folder [IGNITE_HOME]\platforms\dotnet and run Apache.Ignite.exe as follows:
+    /// Apache.Ignite.exe -IgniteHome=[path_to_IGNITE_HOME] -springConfigUrl=modules\platform\src\main\dotnet\examples\config\example-compute.xml -assembly=[path_to_Apache.Ignite.ExamplesDll.dll]
+    /// <para />
+    /// As a result you will see console jobs output on one or several nodes.
+    /// </summary>
+    public class TaskExample
+    {
+        /// <summary>
+        /// Runs the example.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            var cfg = new IgniteConfiguration
+            {
+                SpringConfigUrl = @"modules\platform\src\main\dotnet\examples\config\example-compute.xml",
+                JvmOptions = new List<string> { "-Xms512m", "-Xmx1024m" }
+            };
+
+            using (var ignite = Ignition.Start(cfg))
+            {
+                Console.WriteLine();
+                Console.WriteLine(">>> Task execution example started.");
+
+                // Generate employees to calculate average salary for.
+                ICollection<Employee> employees = Employees();
+
+                Console.WriteLine();
+                Console.WriteLine(">>> Calculating average salary for employees:");
+
+                foreach (Employee employee in employees)
+                    Console.WriteLine(">>>     " + employee);
+
+                // Execute task and get average salary.
+                var avgSalary = ignite.GetCompute().Execute(new AverageSalaryTask(), employees);
+
+                Console.WriteLine();
+                Console.WriteLine(">>> Average salary for all employees: " + avgSalary);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(">>> Example finished, press any key to exit ...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Generates collection of employees for example.
+        /// </summary>
+        /// <returns>Collection of employees.</returns>
+        private static ICollection<Employee> Employees()
+        {
+            return new []
+            {
+                new Employee(
+                    "James Wilson",
+                    12500,
+                    new Address("1096 Eddy Street, San Francisco, CA", 94109),
+                    new List<string> {"Human Resources", "Customer Service"}
+                    ),
+                new Employee(
+                    "Daniel Adams",
+                    11000,
+                    new Address("184 Fidler Drive, San Antonio, TX", 78205),
+                    new List<string> {"Development", "QA"}
+                    ),
+                new Employee(
+                    "Cristian Moss",
+                    12500,
+                    new Address("667 Jerry Dove Drive, Florence, SC", 29501),
+                    new List<string> {"Logistics"}
+                    ),
+                new Employee(
+                    "Allison Mathis",
+                    25300,
+                    new Address("2702 Freedom Lane, Hornitos, CA", 95325),
+                    new List<string> {"Development"}
+                    ),
+                new Employee(
+                    "Breana Robbin",
+                    6500,
+                    new Address("3960 Sundown Lane, Austin, TX", 78758),
+                    new List<string> {"Sales"}
+                    ),
+                new Employee(
+                    "Philip Horsley",
+                    19800,
+                    new Address("2803 Elsie Drive, Sioux Falls, SD", 57104),
+                    new List<string> {"Sales"}
+                    ),
+                new Employee(
+                    "Brian Peters",
+                    10600,
+                    new Address("1407 Pearlman Avenue, Boston, MA", 02110),
+                    new List<string> {"Development", "QA"}
+                    ),
+                new Employee(
+                    "Jack Yang",
+                    12900,
+                    new Address("4425 Parrish Avenue Smithsons Valley, TX", 78130),
+                    new List<string> {"Sales"}
+                    )
+            };
+        }
+    }
+}
---
+++ modules/platform/src/main/dotnet/Examples/Apache.Ignite.Examples/Services/ServicesExample.cs
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using Apache.Ignite.Core;
+using Apache.Ignite.ExamplesDll.Services;
+
+namespace Apache.Ignite.Examples.Services
+{
+    /// <summary>
+    /// Example demonstrating Ignite services.
+    /// <para />
+    /// This example can be run in conjunction with standalone Apache Ignite .Net node.
+    /// To start standalone node please do the following:
+    /// 1) Build the project Apache.Ignite.ExamplesDll if you havent't done it yet (select it -> right-click -> Build);
+    /// 2) Locate created Apache.Ignite.ExamplesDll.dll file (Apache.Ignite.ExamplesDll project -> right-click -> Properties -> Build -> Output path);
+    /// 3) Locate Apache.Ignite.exe file (Apache.Ignite project -> right-click -> Properties -> Build -> Output path)
+    /// Apache.Ignite.exe -IgniteHome=[path_to_IGNITE_HOME] -springConfigUrl=modules\platform\src\main\dotnet\examples\config\example-compute.xml -assembly=[path_to_Apache.Ignite.ExamplesDll.dll]
+    /// <para />
+    /// To run the example please do the following:
+    /// 1) Build the project Apache.Ignite.ExamplesDll (select it -> right-click -> Build);
+    /// 2) Set this class as startup object (Apache.Ignite.Examples project -> right-click -> Properties ->
+    ///     Application -> Startup object);
+    /// 3) Start application (F5 or Ctrl+F5).
+    /// </summary>
+    public class ServicesExample
+    {
+        /// <summary>
+        /// Runs the example.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            var cfg = new IgniteConfiguration
+            {
+                SpringConfigUrl = @"modules\platform\src\main\dotnet\examples\config\example-compute.xml",
+                JvmOptions = new List<string> {"-Xms512m", "-Xmx1024m"}
+            };
+
+            using (var ignite = Ignition.Start(cfg))
+            {
+                Console.WriteLine(">>> Services example started.");
+                Console.WriteLine();
+
+                // Deploy a service
+                var svc = new MapService<int, string>();
+                Console.WriteLine(">>> Deploying service to all nodes...");
+                ignite.GetServices().DeployNodeSingleton("service", svc);
+
+                // Get a sticky service proxy so that we will always be contacting the same remote node.
+                var prx = ignite.GetServices().GetServiceProxy<IMapService<int, string>>("service", true);
+                
+                for (var i = 0; i < 10; i++)
+                    prx.Put(i, i.ToString());
+
+                var mapSize = prx.Size;
+
+                Console.WriteLine(">>> Map service size: " + mapSize);
+
+                ignite.GetServices().CancelAll();
+            }
+        }
+    }
+}
