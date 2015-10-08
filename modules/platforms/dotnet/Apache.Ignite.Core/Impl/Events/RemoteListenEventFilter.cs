0f86b4a86e5716ee0e62aeb404fcd4bbc3a8b76d
ptupitsyn <ptupitsyn@gridgain.com>
2015-10-08 20:35:56 +0300

    fixing tests

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Events/RemoteListenEventFilter.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Events/RemoteListenEventFilter.cs
@@ -53,7 +53,7 @@ namespace Apache.Ignite.Core.Impl.Events
 
             var evt = EventReader.Read<IEvent>(reader);
 
-            var nodeId = reader.ReadGuid();
+            var nodeId = reader.ReadGuidNullable();
 
             return _filter(nodeId, evt) ? 1 : 0;
         }
