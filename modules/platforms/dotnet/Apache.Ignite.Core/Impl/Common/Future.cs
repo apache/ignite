a52090d85fd7c667e7844cb44e91302b898fa5f1
ptupitsyn <ptupitsyn@gridgain.com>
2015-10-12 12:11:27 +0300

    wip

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Common/Future.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Common/Future.cs
@@ -215,6 +215,14 @@ namespace Apache.Ignite.Core.Impl.Common
         /** <inheritdoc /> */
         public void OnNullResult()
         {
+            var type = typeof(T);
+
+            // default(IgniteNullable<>) represents null value, which is correct. All other value types can't be null.
+            if (default(T) != null && 
+                !(type.IsGenericType && type.GetGenericTypeDefinition() == typeof(IgniteNullable<>)))
+                OnError(new InvalidOperationException(string.Format("Invalid Future result. " +
+                                                                    "Expected: '{0}' But was: null", type)));
+
             OnResult(default(T));
         }
 
