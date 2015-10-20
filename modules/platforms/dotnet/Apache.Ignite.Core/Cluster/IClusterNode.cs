9879bed8026d2ac181d146e537de4ae407951fb5
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2015-10-20 15:54:41 +0300

    IGNITE-1480: Added "IsClient" flag to IClusterNode interface.

--- modules/platforms/dotnet/Apache.Ignite.Core/Cluster/IClusterNode.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Cluster/IClusterNode.cs
@@ -109,6 +109,17 @@ namespace Apache.Ignite.Core.Cluster
         bool IsDaemon { get; }
 
         /// <summary>
+        /// Gets a value indicating whether or not this node is connected to cluster as a client.
+        /// <para />
+        /// Do not confuse client in terms of discovery and client in terms of cache. 
+        /// Cache clients cannot carry data, while topology clients connect to the topology in a different way.
+        /// </summary>
+        /// <value>   
+        /// <c>true</c> if this node is a client node, <c>false otherwise.</c>.
+        /// </value>
+        bool IsClient { get; }
+
+        /// <summary>
         /// Gets metrics snapshot for this node. Note that node metrics are constantly updated
         /// and provide up to date information about nodes. For example, you can get
         /// an idea about CPU load on remote node via <see cref="IClusterMetrics.CurrentCpuLoad"/>.
