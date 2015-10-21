95ee5a80a02db9255f1991fafabb36990309de84
ptupitsyn <ptupitsyn@gridgain.com>
2015-10-21 19:37:36 +0300

    Remove unknown type ignore

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Portable/Structure/PortableStructureEntry.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Portable/Structure/PortableStructureEntry.cs
@@ -86,8 +86,7 @@ namespace Apache.Ignite.Core.Impl.Portable.Structure
         /// <param name="type">Expected type.</param>
         public void ValidateType(byte type)
         {
-            // 0 is an unknown type, does not need validation
-            if (type != 0 && _type != type)
+            if (_type != type)
             {
                 throw new PortableException("Field type mismatch detected [fieldName=" + _name +
                     ", expectedType=" + _type + ", actualType=" + type + ']');
