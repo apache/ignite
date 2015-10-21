84d9ecd142ae4989d6928c703c1628379d707557
ptupitsyn <ptupitsyn@gridgain.com>
2015-10-21 19:32:25 +0300

    wip

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Portable/PortableStructureTracker.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Portable/PortableStructureTracker.cs
@@ -56,31 +56,10 @@ namespace Apache.Ignite.Core.Impl.Portable
         }
 
         /// <summary>
-        /// Gets the field id, but does not update structure cache.
-        /// </summary>
-        public int GetFieldIdNoCache(string fieldName)
-        {
-            _curStructAction++;
-
-            if (_curStructUpdates == null)
-            {
-                var fieldId = _portStruct.GetFieldId(fieldName, 0, ref _curStructPath,
-                    _curStructAction);
-
-                if (fieldId != 0)
-                    return fieldId;
-            }
-
-            return PortableUtils.FieldId(_desc.TypeId, fieldName, _desc.NameConverter, _desc.Mapper);
-        }
-
-        /// <summary>
         /// Gets the field ID.
         /// </summary>
-        public int GetFieldId(string fieldName, byte fieldTypeId)
+        public int GetFieldId(string fieldName, byte fieldTypeId = 0)
         {
-            Debug.Assert(fieldTypeId > 0);
-
             _curStructAction++;
 
             if (_curStructUpdates == null)
