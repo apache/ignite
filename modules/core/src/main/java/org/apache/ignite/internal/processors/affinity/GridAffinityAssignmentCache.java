3ef78a3dc0d38c3cb95e9a91d3871076a76a0f82
Alexey Goncharuk <alexey.goncharuk@gmail.com>
2015-11-01 15:18:13 +0300

    IGNITE-950-new - Debug

--- modules/core/src/main/java/org/apache/ignite/internal/processors/affinity/GridAffinityAssignmentCache.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/affinity/GridAffinityAssignmentCache.java
@@ -33,6 +33,7 @@ import org.apache.ignite.cache.affinity.AffinityFunction;
 import org.apache.ignite.cache.affinity.AffinityKeyMapper;
 import org.apache.ignite.cluster.ClusterNode;
 import org.apache.ignite.events.DiscoveryEvent;
+import org.apache.ignite.igniteobject.IgniteObject;
 import org.apache.ignite.internal.GridNodeOrderComparator;
 import org.apache.ignite.internal.IgniteInternalFuture;
 import org.apache.ignite.internal.processors.cache.CacheObject;
@@ -368,7 +369,7 @@ public class GridAffinityAssignmentCache {
      * @return Affinity key.
      */
     private Object affinityKey(Object key) {
-        if (key instanceof CacheObject)
+        if (key instanceof CacheObject && !(key instanceof IgniteObject))
             key = ((CacheObject)key).value(ctx.cacheObjectContext(), false);
 
         return (key instanceof GridCacheInternal ? ctx.defaultAffMapper() : affMapper).affinityKey(key);
