2e39d74402051859aa01b676ffe76fd1036b7b8e
Alexey Goncharuk <alexey.goncharuk@gmail.com>
2015-11-03 13:12:14 +0300

    IGNITE-950 - Fixed NPE.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/atomic/GridDhtAtomicUpdateFuture.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/atomic/GridDhtAtomicUpdateFuture.java
@@ -292,7 +292,7 @@ public class GridDhtAtomicUpdateFuture extends GridFutureAdapter<Void>
                     this.updateReq.taskNameHash(),
                     forceTransformBackups ? this.updateReq.invokeArguments() : null,
                     cctx.deploymentEnabled(),
-                    updateReq.keepBinary());
+                    this.updateReq.keepBinary());
 
                 mappings.put(nodeId, updateReq);
             }
