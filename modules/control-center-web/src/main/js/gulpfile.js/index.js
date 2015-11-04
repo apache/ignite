ca6088d6d9cda0943ae914709e02ca5d9cca64ed
Dmitriyff <dmitriyff@gmail.com>
2015-11-04 15:33:58 +0700

    IGNITE-1831 added build task, set build task as default

--- modules/control-center-web/src/main/js/gulpfile.js/index.js
+++ modules/control-center-web/src/main/js/gulpfile.js/index.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var gulp_jade = require('./tasks/jade');
 var gulp_sass = require('./tasks/sass');
 
-gulp.task('default', ['jade'])
+gulp.task('default', ['build']);
+
+gulp.task('build', ['jade', 'sass']);
 
 gulp.task('watch', ['sass:watch', 'jade:watch']);
