ed36b918ca49d8c2dad30e8e34876834c2f66c87
Anton Vinogradov <av@apache.org>
2015-11-23 16:44:53 +0300

    Ignite 1924

--- modules/core/src/test/java/org/apache/ignite/spi/communication/tcp/IgniteCacheSslStartStopSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/spi/communication/tcp/IgniteCacheSslStartStopSelfTest.java
@@ -33,6 +33,7 @@ public class IgniteCacheSslStartStopSelfTest extends IgniteCachePutRetryAbstract
         IgniteConfiguration cfg = super.getConfiguration(gridName);
 
         cfg.setSslContextFactory(GridTestUtils.sslFactory());
+        ((TcpCommunicationSpi)cfg.getCommunicationSpi()).setSocketWriteTimeout(30000);
 
         return cfg;
     }
