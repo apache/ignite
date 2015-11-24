639723ffd20b2f6ad4af2fea4a327d01f9eb3db4
Anton Vinogradov <av@apache.org>
2015-11-24 12:21:27 +0300

    Ignite 1924

--- modules/core/src/test/java/org/apache/ignite/spi/communication/tcp/IgniteCacheSslStartStopSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/spi/communication/tcp/IgniteCacheSslStartStopSelfTest.java
@@ -20,6 +20,7 @@ package org.apache.ignite.spi.communication.tcp;
 import org.apache.ignite.cache.CacheAtomicityMode;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.internal.processors.cache.distributed.dht.IgniteCachePutRetryAbstractSelfTest;
+import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.apache.ignite.testframework.GridTestUtils;
 
 import static org.apache.ignite.cache.CacheAtomicityMode.ATOMIC;
@@ -33,6 +34,16 @@ public class IgniteCacheSslStartStopSelfTest extends IgniteCachePutRetryAbstract
         IgniteConfiguration cfg = super.getConfiguration(gridName);
 
         cfg.setSslContextFactory(GridTestUtils.sslFactory());
+        ((TcpCommunicationSpi)cfg.getCommunicationSpi()).setSocketWriteTimeout(Integer.MAX_VALUE);
+        ((TcpCommunicationSpi)cfg.getCommunicationSpi()).setConnectTimeout(Integer.MAX_VALUE);
+        ((TcpCommunicationSpi)cfg.getCommunicationSpi()).setMaxConnectTimeout(Integer.MAX_VALUE);
+
+        cfg.setFailureDetectionTimeout(Integer.MAX_VALUE);
+
+        ((TcpDiscoverySpi)cfg.getDiscoverySpi()).setAckTimeout(Integer.MAX_VALUE - 1);
+        ((TcpDiscoverySpi)cfg.getDiscoverySpi()).setMaxAckTimeout(Integer.MAX_VALUE);
+        ((TcpDiscoverySpi)cfg.getDiscoverySpi()).setNetworkTimeout(Integer.MAX_VALUE);
+        ((TcpDiscoverySpi)cfg.getDiscoverySpi()).setSocketTimeout(Integer.MAX_VALUE);
 
         return cfg;
     }
