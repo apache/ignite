3930809b3f614e77f4648ead91fb6db9afb4f49c
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2015-11-25 16:14:34 +0300

    wip

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Binary/BinaryEnum.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Binary/BinaryEnum.cs
@@ -59,11 +59,7 @@ namespace Apache.Ignite.Core.Impl.Binary
         /** <inheritdoc /> */
         public IBinaryType GetBinaryType()
         {
-            // TODO: Return meta. Make sure we write it!
-            // TODO: Branch from 1956
-            // TODO: Test meta
             // TODO: Should we be able to build an Enum with builder?
-            // TODO: Wrap unknown enums in Serializable
             return _marsh.GetBinaryType(_typeId);
         }
 
