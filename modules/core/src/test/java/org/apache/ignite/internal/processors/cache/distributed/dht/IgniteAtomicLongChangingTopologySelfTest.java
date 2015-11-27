712e62bd241cecac969d022461fc048453b3db6b
sboikov <sboikov@gridgain.com>
2015-11-27 13:12:55 +0300

    ignite-1732 Fixed test to catch IgniteClientDisconnectedException.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/dht/IgniteAtomicLongChangingTopologySelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/dht/IgniteAtomicLongChangingTopologySelfTest.java
@@ -31,6 +31,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReferenceArray;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteAtomicLong;
+import org.apache.ignite.IgniteClientDisconnectedException;
 import org.apache.ignite.IgniteQueue;
 import org.apache.ignite.IgniteSet;
 import org.apache.ignite.configuration.AtomicConfiguration;
@@ -214,7 +215,12 @@ public class IgniteAtomicLongChangingTopologySelfTest extends GridCommonAbstract
             while (System.currentTimeMillis() < stop) {
                 log.info("Iteration: " + iter++);
 
-                c.apply(ignite);
+                try {
+                    c.apply(ignite);
+                }
+                catch (IgniteClientDisconnectedException e) {
+                    e.reconnectFuture().get();
+                }
             }
 
             finished.set(true);
