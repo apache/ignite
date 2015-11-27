3ec6dc9f71f9d8e2fb4eaac0170568f70190e658
isapego <isapego@gridgain.com>
2015-11-27 20:36:05 +0300

    IGNITE-2021: Basic test added.

--- modules/platforms/cpp/odbc-test/src/configuration_test.cpp
+++ modules/platforms/cpp/odbc-test/src/configuration_test.cpp
@@ -21,11 +21,33 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include <ignite/odbc/configuration.h>
+
+using namespace ignite::odbc;
+
+#define DRIVER_VAL "Ignite"
+#define SERVER_VAL "testhost.com"
+#define PORT_VAL 4242
+#define PORT_SVAL "4242"
+#define CACHE_VAL "TestCache"
+
 BOOST_AUTO_TEST_SUITE(ConfigurationTestSuite)
 
-BOOST_AUTO_TEST_CASE(TestStub)
+BOOST_AUTO_TEST_CASE(TestConnectString)
 {
-    BOOST_REQUIRE(true);
+    Configuration cfg;
+
+    std::string connectStr = "DRIVER={" DRIVER_VAL "};"
+                             "SERVER=" SERVER_VAL ";"
+                             "PORT=" PORT_SVAL ";"
+                             "CACHE=" CACHE_VAL;
+
+    cfg.FillFromConnectString(connectStr.c_str(), connectStr.size());
+
+    BOOST_REQUIRE(cfg.GetDriver() == DRIVER_VAL);
+    BOOST_REQUIRE(cfg.GetHost() == SERVER_VAL);
+    BOOST_REQUIRE(cfg.GetPort() == PORT_VAL);
+    BOOST_REQUIRE(cfg.GetCache() == CACHE_VAL);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
