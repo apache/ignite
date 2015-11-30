c3830e3ec1f3f1e88c76122ae2df313b275bc7d1
isapego <isapego@gridgain.com>
2015-11-30 16:34:53 +0300

    IGNITE-2022: Configuration test implemented.

--- modules/platforms/cpp/odbc-test/src/configuration_test.cpp
+++ modules/platforms/cpp/odbc-test/src/configuration_test.cpp
@@ -16,8 +16,10 @@
  */
 
 #ifndef _MSC_VER
-    #define BOOST_TEST_DYN_LINK
+#   define BOOST_TEST_DYN_LINK
 #endif
+
+#include <iostream>
 
 #include <boost/test/unit_test.hpp>
 
@@ -28,26 +30,174 @@ using namespace ignite::odbc;
 #define DRIVER_VAL "Ignite"
 #define SERVER_VAL "testhost.com"
 #define PORT_VAL 4242
-#define PORT_SVAL "4242"
 #define CACHE_VAL "TestCache"
+#define DSN_VAL "Ignite DSN"
 
 BOOST_AUTO_TEST_SUITE(ConfigurationTestSuite)
 
-BOOST_AUTO_TEST_CASE(TestConnectString)
+void CheckConnectionConfig(const Configuration& cfg)
 {
-    Configuration cfg;
-
-    std::string connectStr = "DRIVER={" DRIVER_VAL "};"
-                             "SERVER=" SERVER_VAL ";"
-                             "PORT=" PORT_SVAL ";"
-                             "CACHE=" CACHE_VAL;
-
-    cfg.FillFromConnectString(connectStr.c_str(), connectStr.size());
-
     BOOST_REQUIRE(cfg.GetDriver() == DRIVER_VAL);
     BOOST_REQUIRE(cfg.GetHost() == SERVER_VAL);
     BOOST_REQUIRE(cfg.GetPort() == PORT_VAL);
     BOOST_REQUIRE(cfg.GetCache() == CACHE_VAL);
+    BOOST_REQUIRE(cfg.GetDsn().empty());
+
+    std::stringstream constructor;
+
+    constructor << "driver={" << DRIVER_VAL << "};"
+                << "server=" << SERVER_VAL << ";"
+                << "port=" << PORT_VAL << ";"
+                << "cache=" << CACHE_VAL << ";";
+
+    const std::string& expectedStr = constructor.str();
+
+    BOOST_REQUIRE(cfg.ToConnectString() == expectedStr);
+}
+
+void CheckDsnConfig(const Configuration& cfg)
+{
+    BOOST_REQUIRE(cfg.GetDriver() == DRIVER_VAL);
+    BOOST_REQUIRE(cfg.GetDsn() == DSN_VAL);
+    BOOST_REQUIRE(cfg.GetHost().empty());
+    BOOST_REQUIRE(cfg.GetCache().empty());
+    BOOST_REQUIRE(cfg.GetPort() == 0);
+}
+
+BOOST_AUTO_TEST_CASE(TestConnectStringUppercase)
+{
+    Configuration cfg;
+
+    std::stringstream constructor;
+
+    constructor << "DRIVER={" << DRIVER_VAL << "};"
+                << "SERVER=" << SERVER_VAL <<";"
+                << "PORT=" << PORT_VAL << ";"
+                << "CACHE=" << CACHE_VAL;
+
+    const std::string& connectStr = constructor.str();
+
+    cfg.FillFromConnectString(connectStr.c_str(), connectStr.size());
+
+    CheckConnectionConfig(cfg);
+}
+
+BOOST_AUTO_TEST_CASE(TestConnectStringLowercase)
+{
+    Configuration cfg;
+
+    std::stringstream constructor;
+
+    constructor << "driver={" << DRIVER_VAL << "};"
+                << "server=" << SERVER_VAL << ";"
+                << "port=" << PORT_VAL << ";"
+                << "cache=" << CACHE_VAL;
+
+    const std::string& connectStr = constructor.str();
+
+    cfg.FillFromConnectString(connectStr.c_str(), connectStr.size());
+
+    CheckConnectionConfig(cfg);
+}
+
+BOOST_AUTO_TEST_CASE(TestConnectStringMixed)
+{
+    Configuration cfg;
+
+    std::stringstream constructor;
+
+    constructor << "Driver={" << DRIVER_VAL << "};"
+                << "Server=" << SERVER_VAL << ";"
+                << "Port=" << PORT_VAL << ";"
+                << "Cache=" << CACHE_VAL;
+
+    const std::string& connectStr = constructor.str();
+
+    cfg.FillFromConnectString(connectStr.c_str(), connectStr.size());
+
+    CheckConnectionConfig(cfg);
+}
+
+BOOST_AUTO_TEST_CASE(TestConnectStringWhitepaces)
+{
+    Configuration cfg;
+
+    std::stringstream constructor;
+
+    constructor << "DRIVER = {" << DRIVER_VAL << "} ;\n"
+                << " SERVER =" << SERVER_VAL << " ; \n"
+                << "PORT= " << PORT_VAL << "; "
+                << "CACHE = \n\r" << CACHE_VAL;
+
+    const std::string& connectStr = constructor.str();
+
+    cfg.FillFromConnectString(connectStr.c_str(), connectStr.size());
+
+    CheckConnectionConfig(cfg);
+}
+
+BOOST_AUTO_TEST_CASE(TestDsnStringUppercase)
+{
+    Configuration cfg;
+
+    std::stringstream constructor;
+
+    constructor << "DRIVER=" << DRIVER_VAL << '\0'
+                << "DSN={" << DSN_VAL << "}" << '\0' << '\0';
+
+    const std::string& configStr = constructor.str();
+
+    cfg.FillFromConfigAttributes(configStr.data());
+
+    CheckDsnConfig(cfg);
+}
+
+BOOST_AUTO_TEST_CASE(TestDsnStrinLowercase)
+{
+    Configuration cfg;
+
+    std::stringstream constructor;
+
+    constructor << "driver=" << DRIVER_VAL << '\0'
+                << "dsn={" << DSN_VAL << "}" << '\0' << '\0';
+
+    const std::string& configStr = constructor.str();
+
+    cfg.FillFromConfigAttributes(configStr.data());
+
+    CheckDsnConfig(cfg);
+}
+
+BOOST_AUTO_TEST_CASE(TestDsnStrinMixed)
+{
+    Configuration cfg;
+
+    std::stringstream constructor;
+
+    constructor << "Driver=" << DRIVER_VAL << '\0'
+                << "Dsn={" << DSN_VAL << "}" << '\0' << '\0';
+
+    const std::string& configStr = constructor.str();
+
+    cfg.FillFromConfigAttributes(configStr.data());
+
+    CheckDsnConfig(cfg);
+}
+
+BOOST_AUTO_TEST_CASE(TestDsnStrinWhitespaces)
+{
+    Configuration cfg;
+
+    std::stringstream constructor;
+
+    constructor << " DRIVER =  " << DRIVER_VAL << "\r\n" << '\0'
+                << "DSN= {" << DSN_VAL << "} \n" << '\0' << '\0';
+
+    const std::string& configStr = constructor.str();
+
+    cfg.FillFromConfigAttributes(configStr.data());
+
+    CheckDsnConfig(cfg);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
