5d5d30e9c24489d374404b4a5e37bf25bc9363ca
Andrey <anovikov@gridgain.com>
2015-11-30 11:39:34 +0700

    IGNITE-1936 Minor fix.

--- modules/control-center-web/src/main/js/gulpfile.js/tasks/sass.js
+++ modules/control-center-web/src/main/js/gulpfile.js/tasks/sass.js
@@ -26,7 +26,7 @@ var paths = [
 gulp.task('sass', function () {
     return gulp.src(paths)
         .pipe(sass({ outputStyle: 'nested' }).on('error', sass.logError))
-        .pipe(gulp.dest('./build/public/stylesheets'));
+        .pipe(gulp.dest('./public/stylesheets'));
 });
 
 gulp.task('sass:watch', function (cb) {
