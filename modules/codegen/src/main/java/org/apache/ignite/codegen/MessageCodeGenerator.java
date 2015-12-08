16242b78dea8e99261c3aff15d571dcfb3c9fd0d
S.Vladykin <svladykin@gridgain.com>
2015-12-08 05:43:29 +0300

    ignite-split2 - msg

--- modules/codegen/src/main/java/org/apache/ignite/codegen/MessageCodeGenerator.java
+++ modules/codegen/src/main/java/org/apache/ignite/codegen/MessageCodeGenerator.java
@@ -226,6 +226,7 @@ public class MessageCodeGenerator {
 //        gen.generateAndWrite(GridH2RowRange.class);
 //        gen.generateAndWrite(GridH2RowRangeBounds.class);
 //        gen.generateAndWrite(GridH2QueryRequest.class);
+//        gen.generateAndWrite(GridH2RowMessage.class);
     }
 
     /**
