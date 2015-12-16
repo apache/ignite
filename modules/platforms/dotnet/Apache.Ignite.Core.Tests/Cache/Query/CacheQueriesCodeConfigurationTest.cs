455f23382950635a4e9c0e71986cd3b8227a1e21
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2015-12-16 17:24:22 +0300

    wip

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Cache/Query/CacheQueriesCodeConfigurationTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Cache/Query/CacheQueriesCodeConfigurationTest.cs
@@ -17,7 +17,6 @@
 
 namespace Apache.Ignite.Core.Tests.Cache.Query
 {
-    using System.Collections.Generic;
     using System.Linq;
     using Apache.Ignite.Core.Binary;
     using Apache.Ignite.Core.Cache.Query;
