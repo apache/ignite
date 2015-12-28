bbac649e89733775b02ba9af201638c59f6d373b
Ignite Teamcity <ignite@apache.org>
2015-12-28 14:39:17 +0300

    1.5.1.final-SNAPSHOT

--- modules/platforms/dotnet/Apache.Ignite.Core/Properties/AssemblyInfo.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("97db45a8-f922-456a-a819-7b3c6e5e03ba")]
 
-[assembly: AssemblyVersion("1.5.1.7796")]
-[assembly: AssemblyFileVersion("1.5.1.7796")]
+[assembly: AssemblyVersion("1.5.1.7927")]
+[assembly: AssemblyFileVersion("1.5.1.7927")]
 [assembly: AssemblyInformationalVersion("1.5.1.final")]
 
 [assembly: CLSCompliant(true)]
