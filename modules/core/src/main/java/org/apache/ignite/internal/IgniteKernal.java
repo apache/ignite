359a392f1c53217c691c4c40762c51fd330666e2
Valentin Kulichenko <valentin.kulichenko@gmail.com>
2016-01-15 09:58:41 +0300

    Update notifier fixes
    (cherry picked from commit a5c85ca)

--- modules/core/src/main/java/org/apache/ignite/internal/IgniteKernal.java
+++ modules/core/src/main/java/org/apache/ignite/internal/IgniteKernal.java
@@ -793,8 +793,6 @@ public class IgniteKernal implements IgniteEx, IgniteMXBean, Externalizable {
 
             startProcessor(clusterProc);
 
-            fillNodeAttributes(clusterProc.updateNotifierEnabled());
-
             U.onGridStart();
 
             // Start and configure resource processor first as it contains resources used
@@ -895,6 +893,8 @@ public class IgniteKernal implements IgniteEx, IgniteMXBean, Externalizable {
 
                 provider.start(ctx.plugins().pluginContextForProvider(provider));
             }
+
+            fillNodeAttributes(clusterProc.updateNotifierEnabled());
 
             gw.writeLock();
 
