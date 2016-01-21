d33bd8c8ef39edd943ef8a6e7b19b83c24e6b5d6
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-01-21 16:41:37 +0300

    fix

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Common/TypeStringConverter.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Common/TypeStringConverter.cs
@@ -92,7 +92,7 @@ namespace Apache.Ignite.Core.Impl.Common
         public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, 
             Type destinationType)
         {
-            return ConvertToString(value);
+            return Convert(value);
         }
 
         /// <summary>
