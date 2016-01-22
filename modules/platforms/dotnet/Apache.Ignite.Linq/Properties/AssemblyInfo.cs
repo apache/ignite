7585c7155e04fd8df19960d81f70485863948075
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-01-22 16:31:58 +0300

    IGNITE-1630 .Net: Create LINQ adapter for queries.

---
+++ modules/platforms/dotnet/Apache.Ignite.Linq/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Apache.Ignite.Linq")]
+[assembly: AssemblyDescription("Apache Ignite.NET Linq Provider")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Apache Software Foundation")]
+[assembly: AssemblyProduct("Apache Ignite.NET")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5b571661-17f4-4f29-8c7d-0edb38ca9b55")]
+
+[assembly: AssemblyVersion("1.5.1.7927")]
+[assembly: AssemblyFileVersion("1.5.1.7927")]
+[assembly: AssemblyInformationalVersion("1.5.1.final")]
+
+[assembly: CLSCompliant(true)]
