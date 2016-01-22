90818c8297febfe95c1ca68d2f0f6f747ff3dd7b
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-01-22 18:13:24 +0300

    wip test

---
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Cache/Query/CacheLinqTest.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Core.Tests.Cache.Query
+{
+    using System.Linq;
+    using Apache.Ignite.Core.Cache;
+    using Apache.Ignite.Linq;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests LINQ queries.
+    /// </summary>
+    public class CacheLinqTest : IgniteTestBase
+    {
+        /** Cache name. */
+        private const string CacheName = "cache";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CacheLinqTest"/> class.
+        /// </summary>
+        public CacheLinqTest() : base("config\\cache-query.xml")
+        {
+            // No-op.
+        }
+
+        [Test]
+        public void Test()
+        {
+            var cache = GetCache();
+
+            var result = cache.ToQueryable().Where(x => x.Value.Age > 10).ToArray();
+
+            Assert.IsTrue(result.Any());
+        }
+
+        private ICache<int, QueryPerson> GetCache()
+        {
+            return Grid.GetCache<int, QueryPerson>(CacheName);
+        }
+    }
+}
