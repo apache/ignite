4acc25cc52ecda1923ab311e95eb5ac02e014c17
Ilya Lantukh <ilantukh@gridgain.com>
2016-01-22 18:51:02 +0300

    gg-9149 : Removed unnecessary changes.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheAdapter.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheAdapter.java
@@ -5438,7 +5438,7 @@ public abstract class GridCacheAdapter<K, V> implements IgniteInternalCache<K, V
          * @param expiryPlc Expiry policy.
          * @return Access expire policy.
          */
-        @Nullable protected static CacheExpiryPolicy forPolicy(@Nullable final ExpiryPolicy expiryPlc) {
+        @Nullable private static CacheExpiryPolicy forPolicy(@Nullable final ExpiryPolicy expiryPlc) {
             if (expiryPlc == null)
                 return null;
 
