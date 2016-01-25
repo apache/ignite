a9a1b4f78dcb28ac05587f5431a2b89991badedd
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-01-25 17:52:55 +0300

    wip

--- modules/platforms/dotnet/Apache.Ignite.Linq/Impl/CacheQueryExecutor.cs
+++ modules/platforms/dotnet/Apache.Ignite.Linq/Impl/CacheQueryExecutor.cs
@@ -69,6 +69,10 @@ namespace Apache.Ignite.Linq.Impl
         /** <inheritdoc /> */
         public IEnumerable<T> ExecuteCollection<T>(QueryModel queryModel)
         {
+            // Check for empty query
+            if (queryModel.IsIdentityQuery() && queryModel.ResultOperators.Count == 0)
+                return (IEnumerable<T>) _cache;
+
             var queryData = CacheQueryModelVisitor.GenerateQuery(queryModel, _cache);
 
             //var query = new SqlFieldsQuery(queryData.QueryText, queryData.Parameters);
