fbe138b7649da88bd35fd52622a8485641e7babf
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-01-26 13:27:08 +0300

    wip Java sql tests

---
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/IgniteSqlTests.java
@@ -0,0 +1,55 @@
+package org.apache.ignite.internal.processors.query;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.cache.QueryEntity;
+import org.apache.ignite.cache.query.QueryCursor;
+import org.apache.ignite.cache.query.SqlQuery;
+import org.apache.ignite.cache.query.annotations.QuerySqlField;
+import org.apache.ignite.configuration.CacheConfiguration;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
+
+import javax.cache.Cache;
+import java.util.Arrays;
+import java.util.List;
+
+public class IgniteSqlTests extends GridCommonAbstractTest {
+    /** {@inheritDoc} */
+    @Override protected IgniteConfiguration getConfiguration() throws Exception {
+        IgniteConfiguration cfg = super.getConfiguration();
+
+        cfg.setPeerClassLoadingEnabled(false);
+
+        cfg.setCacheConfiguration(new CacheConfiguration("sqlCache").setIndexedTypes(QueryPerson.class));
+
+        return cfg;
+    }
+
+    public void testQueries() throws Exception {
+        Ignite i = startGrid();
+
+        IgniteCache<Integer, QueryPerson> c = i.cache("sqlCache");
+
+        QueryPerson p = new QueryPerson();
+        p.Age = 20;
+        p.Name = "vasya";
+
+        c.put(1, p);
+
+        List<Cache.Entry<Integer, QueryPerson>> results =
+            c.query(new SqlQuery<Integer, QueryPerson>("QueryPerson", "Age > 10")).getAll();
+
+        assert results.size() == 1;
+
+        stopAllGrids();
+    }
+
+    public class QueryPerson {
+        @QuerySqlField(index = false)
+        public Integer Age;
+
+        @QuerySqlField(index = false)
+        public String Name;
+    }
+}
