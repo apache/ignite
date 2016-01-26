29dc6e5e875613d4d55d88e2a200374116aeeb04
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-01-26 14:07:29 +0300

    wip Java sql tests

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/IgniteSqlTests.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/IgniteSqlTests.java
@@ -2,8 +2,6 @@ package org.apache.ignite.internal.processors.query;
 
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
-import org.apache.ignite.cache.QueryEntity;
-import org.apache.ignite.cache.query.QueryCursor;
 import org.apache.ignite.cache.query.SqlQuery;
 import org.apache.ignite.cache.query.annotations.QuerySqlField;
 import org.apache.ignite.configuration.CacheConfiguration;
@@ -11,7 +9,6 @@ import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 
 import javax.cache.Cache;
-import java.util.Arrays;
 import java.util.List;
 
 public class IgniteSqlTests extends GridCommonAbstractTest {
@@ -21,6 +18,8 @@ public class IgniteSqlTests extends GridCommonAbstractTest {
 
         cfg.setPeerClassLoadingEnabled(false);
 
+        cfg.setIgniteHome("c:\\w\\incubator-ignite");
+
         cfg.setCacheConfiguration(new CacheConfiguration("sqlCache").setIndexedTypes(QueryPerson.class));
 
         return cfg;
@@ -29,7 +28,7 @@ public class IgniteSqlTests extends GridCommonAbstractTest {
     public void testQueries() throws Exception {
         Ignite i = startGrid();
 
-        IgniteCache<Integer, QueryPerson> c = i.cache("sqlCache");
+        IgniteCache<Integer, QueryPerson> c = i.getOrCreateCache("sqlCache");
 
         QueryPerson p = new QueryPerson();
         p.Age = 20;
@@ -45,7 +44,7 @@ public class IgniteSqlTests extends GridCommonAbstractTest {
         stopAllGrids();
     }
 
-    public class QueryPerson {
+    public static class QueryPerson {
         @QuerySqlField(index = false)
         public Integer Age;
 
