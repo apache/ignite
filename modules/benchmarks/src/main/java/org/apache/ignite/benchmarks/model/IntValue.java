4edc7bdd192624a451c9ed19d4c37f26e4653a33
vozerov-gridgain <vozerov@gridgain.com>
2016-01-26 17:50:53 +0300

    Added project for benchmarks.

---
+++ modules/benchmarks/src/main/java/org/apache/ignite/benchmarks/model/IntValue.java
@@ -0,0 +1,91 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.benchmarks.model;
+
+import org.apache.ignite.binary.BinaryObjectException;
+import org.apache.ignite.binary.BinaryReader;
+import org.apache.ignite.binary.BinaryWriter;
+import org.apache.ignite.binary.Binarylizable;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+/**
+ * Simple wrapped int value.
+ */
+public class IntValue implements Externalizable, Binarylizable {
+    /** Value. */
+    private int val;
+
+    /**
+     * Default constructor.
+     */
+    public IntValue() {
+        // No-op.
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param val Value.
+     */
+    public IntValue(int val) {
+        this.val = val;
+    }
+
+    /**
+     * @param val Value.
+     */
+    public void value(int val) {
+        this.val = val;
+    }
+
+    /**
+     * @return Value.
+     */
+    public int value() {
+        return val;
+    }
+
+    /** {@inheritDoc} */
+    @Override public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        val = in.readInt();
+    }
+
+    /** {@inheritDoc} */
+    @Override public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeInt(val);
+    }
+
+    /** {@inheritDoc} */
+    @Override public void writeBinary(BinaryWriter writer) throws BinaryObjectException {
+        writer.writeInt("id", val);
+    }
+
+    /** {@inheritDoc} */
+    @Override public void readBinary(BinaryReader reader) throws BinaryObjectException {
+        val = reader.readInt("id");
+    }
+
+    /** {@inheritDoc} */
+    @Override public String toString() {
+        return "Value [id=" + val + ']';
+    }
+}
