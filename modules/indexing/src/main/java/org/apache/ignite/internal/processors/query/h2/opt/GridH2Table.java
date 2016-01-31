a83ac2c2a844ee37426e3da389e2a47eb0c18ac1
S.Vladykin <svladykin@gridgain.com>
2016-01-31 23:30:44 +0300

    sql-store-idx - fixes

--- modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/opt/GridH2Table.java
+++ modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/opt/GridH2Table.java
@@ -25,7 +25,6 @@ import java.util.Collections;
 import java.util.Set;
 import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import org.apache.ignite.IgniteCheckedException;
@@ -339,6 +338,9 @@ public class GridH2Table extends TableBase {
         // TODO use version here.
 
         GridH2Row row = desc.createRow(key, val, expirationTime);
+
+        row.key = key;
+        row.val = val;
 
         return doUpdate(row, rmv);
     }
