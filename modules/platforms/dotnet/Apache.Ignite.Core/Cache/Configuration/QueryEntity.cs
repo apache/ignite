9c357ad5b8738574e9b396a404d783a34e632a95
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-02-02 19:31:59 +0300

    wip QueryField.TypeName

--- modules/platforms/dotnet/Apache.Ignite.Core/Cache/Configuration/QueryEntity.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Cache/Configuration/QueryEntity.cs
@@ -318,7 +318,9 @@ namespace Apache.Ignite.Core.Cache.Configuration
                     if (parentPropName != null)
                         columnName = parentPropName + "." + columnName;
 
-                    fields.Add(new QueryField(columnName, memberInfo.Value));
+                    fields.Add(attr.TypeName != null
+                        ? new QueryField(columnName, attr.TypeName)
+                        : new QueryField(columnName, memberInfo.Value));
 
                     if (attr.IsIndexed)
                         indexes.Add(new QueryIndexEx(columnName, attr.IsDescending, QueryIndexType.Sorted,
