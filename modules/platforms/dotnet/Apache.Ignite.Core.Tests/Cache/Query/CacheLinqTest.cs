13dc3dce901071d312bf15496997bddcac0ed91d
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-02-02 18:31:29 +0300

    wip

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Cache/Query/CacheLinqTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Cache/Query/CacheLinqTest.cs
@@ -471,8 +471,8 @@ namespace Apache.Ignite.Core.Tests.Cache.Query
             var personsByOrg = GetPersonOrgCache().ToQueryable()
                 .Join(GetOrgCache().ToQueryable(), p => p.Value.OrganizationId, o => o.Value.Id,
                     (p, o) => new {PersonId = p.Key, PersonName = p.Value.Name, OrgName = o.Value.Name})
-                .OrderBy(x => x.OrgName)
-                .ThenBy(x => x.PersonName)
+                .OrderBy(x => x.OrgName.ToLower())
+                .ThenBy(x => x.PersonName + "_")
                 .ToArray();
 
             var expectedIds = Enumerable.Range(0, PersonCount).OrderBy(x => x%2).ThenBy(x => x).ToArray();
