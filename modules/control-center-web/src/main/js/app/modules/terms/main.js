94fea9fdde494e0b9d656cc1c92c08390b890c6c
Andrey <anovikov@gridgain.com>
2016-02-03 16:12:06 +0700

    IGNITE-843 Minor ui fix.

--- modules/control-center-web/src/main/js/app/modules/terms/main.js
+++ modules/control-center-web/src/main/js/app/modules/terms/main.js
@@ -23,7 +23,7 @@ angular
 ])
 .provider('igniteTerms', function() {
     let _rows = [
-        'Apache Ignite Web Console, version 1.0.0 beta',
+        'Apache Ignite Web Console',
         '© 2015 The Apache Software Foundation.',
         'Apache, Apache Ignite, the Apache feather and the Apache Ignite logo are trademarks of The Apache Software Foundation.'
     ];
