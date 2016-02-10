506212ba53a3f4f0b4e80cba3c5ba7a52665c968
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-02-10 19:15:03 +0300

    wip query compilation

---
+++ modules/platforms/dotnet/Apache.Ignite.Linq/CompiledQuery.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Linq
+{
+    using System;
+    using System.Linq.Expressions;
+
+    /// <summary>
+    /// Represents a compiled cache query.
+    /// </summary>
+    public static class CompiledQuery
+    {
+        /// <summary>
+        /// Creates a new delegate that represents the compiled cache query.
+        /// </summary>
+        /// <typeparam name="TR">The type of the r.</typeparam>
+        /// <param name="query">The query to compile.</param>
+        /// <returns>Delegate that represents the compiled cache query.</returns>
+        public static Func<TR> Compile<TR>(Expression<Func<TR>> query)
+        {
+            return null;
+        }
+    }
+}
