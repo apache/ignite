3dce33f5a007aa15adfc1e79b1ac96e86683e9fb
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-02-12 16:09:00 +0300

    IGNITE-2625 .NET: Fixed field offset calculation in BinaryReader.

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Binary/BinaryReader.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Binary/BinaryReader.cs
@@ -938,7 +938,7 @@ namespace Apache.Ignite.Core.Impl.Binary
                 if (!_curSchemaMap.TryGetValue(fieldId, out pos))
                     return false;
 
-                Stream.Seek(pos, SeekOrigin.Begin);
+                Stream.Seek(pos + _curPos, SeekOrigin.Begin);
             }
 
             return true;
