7d58c91959eab7845840ec6b71dc769b098b99f4
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-02-17 15:05:31 +0300

    Add console runner

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests.NuGet/TestUtil.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests.NuGet/TestUtil.cs
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 namespace Apache.Ignite.Core.Tests.NuGet
 {
     using Apache.Ignite.Core.Discovery;
