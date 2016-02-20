9bc31123c544ddce54b5e211e831f3f5ef21cd4f
dkarachentsev <dkarachentsev@gridgain.com>
2016-02-20 12:45:15 +0300

    IGNITE-2649 - Ignition.localIgnite() unreliable under Gateways and cause wrong components deserialization. Fix compilation

--- examples/src/main/java/org/apache/ignite/examples/streaming/wordcount/socket/WordsSocketStreamerServer.java
+++ examples/src/main/java/org/apache/ignite/examples/streaming/wordcount/socket/WordsSocketStreamerServer.java
@@ -36,6 +36,7 @@ import org.apache.ignite.lang.IgniteBiTuple;
 import org.apache.ignite.stream.StreamSingleTupleExtractor;
 import org.apache.ignite.stream.socket.SocketMessageConverter;
 import org.apache.ignite.stream.socket.SocketStreamer;
+import org.apache.ignite.configuration.IgniteConfiguration;
 
 /**
  * Example demonstrates streaming of data from external components into Ignite cache.
