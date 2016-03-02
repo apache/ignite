d3828bc1b0fa57e90869053dc37abf5971b89d70
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-03-02 18:41:27 +0300

    wip

--- modules/platforms/dotnet/Apache.Ignite.Core/Cache/Affinity/AffinityKey.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Cache/Affinity/AffinityKey.cs
@@ -17,11 +17,121 @@
 
 namespace Apache.Ignite.Core.Cache.Affinity
 {
+    using System;
+
     /// <summary>
     /// TODO
     /// </summary>
-    public class AffinityKey
+    public struct AffinityKey : IEquatable<AffinityKey>
     {
-        // TODO
+        /** User key. */
+        private readonly object _key;
+
+        /** Affinity key. */
+        private readonly object _affinity;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AffinityKey"/> struct.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        public AffinityKey(object key)
+        {
+            _key = key;
+            _affinity = null;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AffinityKey"/> struct.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="affinity">The affinity key.</param>
+        public AffinityKey(object key, object affinity)
+        {
+            _key = key;
+            _affinity = affinity;
+        }
+
+        /// <summary>
+        /// Gets the key.
+        /// </summary>
+        public object Key
+        {
+            get { return _key; }
+        }
+
+        /// <summary>
+        /// Gets the affinity key.
+        /// </summary>
+        [AffinityKeyMapped]
+        public object Affinity
+        {
+            get { return _affinity ?? _key; }
+        }
+
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns>
+        /// true if the current object is equal to the <paramref name="other" /> parameter; otherwise, false.
+        /// </returns>
+        public bool Equals(AffinityKey other)
+        {
+            return Equals(_key, other._key) && Equals(_affinity, other._affinity);
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="object" />, is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is AffinityKey && Equals((AffinityKey) obj);
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((_key != null ? _key.GetHashCode() : 0)*397) ^
+                       (_affinity != null ? _affinity.GetHashCode() : 0);
+            }
+        }
+
+        /// <summary>
+        /// Implements the operator ==.
+        /// </summary>
+        /// <param name="left">The left.</param>
+        /// <param name="right">The right.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static bool operator ==(AffinityKey left, AffinityKey right)
+        {
+            return left.Equals(right);
+        }
+
+        /// <summary>
+        /// Implements the operator !=.
+        /// </summary>
+        /// <param name="left">The left.</param>
+        /// <param name="right">The right.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static bool operator !=(AffinityKey left, AffinityKey right)
+        {
+            return !left.Equals(right);
+        }
     }
 }
