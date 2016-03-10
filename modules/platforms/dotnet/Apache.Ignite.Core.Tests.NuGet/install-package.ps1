91b942e9d0a1575a2b3f65f2fad4aec03e9724e4
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-03-10 18:20:11 +0300

    Fix encoding

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests.NuGet/install-package.ps1
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests.NuGet/install-package.ps1
@@ -10,6 +10,10 @@ mkdir pkg
 $ver = (Get-ChildItem nupkg\Apache.Ignite.Linq*)[0].Name -replace '\D+([\d.]+)\.\D+','$1'
 
 # Replace versions in project files
-(Get-Content packages.config) -replace 'id="Apache.Ignite(.*?)" version=".*?"', ('id="Apache.Ignite$1" version="' + $ver + '"') | Out-File packages.config
+(Get-Content packages.config) `
+    -replace 'id="Apache.Ignite(.*?)" version=".*?"', ('id="Apache.Ignite$1" version="' + $ver + '"') `
+    | Out-File packages.config -Encoding utf8
 
-(Get-Content Apache.Ignite.Core.Tests.NuGet.csproj) -replace '<HintPath>packages\\Apache.Ignite(.*?)\.[\d.]+\\', ('<HintPath>packages\Apache.Ignite$1.' + "$ver\") | Out-File Apache.Ignite.Core.Tests.NuGet.csproj
+(Get-Content Apache.Ignite.Core.Tests.NuGet.csproj) `
+    -replace '<HintPath>packages\\Apache.Ignite(.*?)\.[\d.]+\\', ('<HintPath>packages\Apache.Ignite$1.' + "$ver\") `
+    | Out-File Apache.Ignite.Core.Tests.NuGet.csproj  -Encoding utf8
