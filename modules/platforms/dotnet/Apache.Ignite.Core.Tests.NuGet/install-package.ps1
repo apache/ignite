09d73f509cb866ae5dc3f982a8243fd038b1e3e9
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-03-10 18:14:56 +0300

    Fixing nuget tests

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests.NuGet/install-package.ps1
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests.NuGet/install-package.ps1
@@ -10,3 +10,6 @@ mkdir pkg
 $ver = (Get-ChildItem nupkg\Apache.Ignite.Linq*)[0].Name -replace '\D+([\d.]+)\.\D+','$1'
 
 # Replace versions in project files
+(Get-Content packages.config) -replace 'id="Apache.Ignite(.*?)" version=".*?"', ('id="Apache.Ignite$1" version="' + $ver + '"') | Out-File packages.config
+
+(Get-Content Apache.Ignite.Core.Tests.NuGet.csproj) -replace '<HintPath>packages\\Apache.Ignite(.*?)\.[\d.]+\\', ('<HintPath>packages\Apache.Ignite$1.' + "$ver\") | Out-File Apache.Ignite.Core.Tests.NuGet.csproj
