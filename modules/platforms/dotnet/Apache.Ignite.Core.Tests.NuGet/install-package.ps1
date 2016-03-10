07ed5ef0dfd52faa8bae0406ec38c874c27f672b
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-03-10 16:39:18 +0300

    Fix nuget build config

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests.NuGet/install-package.ps1
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests.NuGet/install-package.ps1
@@ -4,8 +4,8 @@ rmdir pkg -Force -Recurse
 mkdir nupkg
 mkdir pkg
 
-& nuget pack ..\Apache.Ignite.Core\Apache.Ignite.Core.csproj -Prop Configuration=Debug -Prop Platform=x64 -OutputDirectory nupkg
-& nuget pack ..\Apache.Ignite.Linq\Apache.Ignite.Linq.csproj -Prop Configuration=Debug -Prop Platform=x64 -OutputDirectory nupkg
+& nuget pack ..\Apache.Ignite.Core\Apache.Ignite.Core.csproj -Prop Configuration=Release -Prop Platform=x64 -OutputDirectory nupkg
+& nuget pack ..\Apache.Ignite.Linq\Apache.Ignite.Linq.csproj -Prop Configuration=Release -Prop Platform=x64 -OutputDirectory nupkg
 
 $ver = (Get-ChildItem nupkg\Apache.Ignite.Linq*)[0].Name -replace '\D+([\d.]+)\.\D+','$1'
 $nugetSrc = (Get-Location).Path + "\nupkg"
