6ceec799352a196b3a358235905f0150603b5cb2
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-03-10 17:48:48 +0300

    wip

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests.NuGet/install-package.ps1
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests.NuGet/install-package.ps1
@@ -8,10 +8,5 @@ mkdir pkg
 & nuget pack ..\Apache.Ignite.Linq\Apache.Ignite.Linq.csproj -Prop Configuration=Release -Prop Platform=x64 -OutputDirectory nupkg
 
 $ver = (Get-ChildItem nupkg\Apache.Ignite.Linq*)[0].Name -replace '\D+([\d.]+)\.\D+','$1'
-$nugetSrc = (Get-Location).Path + "\nupkg"
 
-& nuget install Remotion.Linq -Version 2.0.1 -OutputDirectory pkg  # Force install to fix teamcity issues
-& nuget install Apache.Ignite.Linq -Version $ver -OutputDirectory pkg -Source $nugetSrc
-
-move ".\pkg\Apache.Ignite.$ver" ".\pkg\Apache.Ignite"
-move ".\pkg\Apache.Ignite.Linq.$ver" ".\pkg\Apache.Ignite.Linq"
+# Replace versions in project files
