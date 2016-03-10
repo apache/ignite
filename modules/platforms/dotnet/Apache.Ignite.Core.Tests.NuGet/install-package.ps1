eb716e0985c3142d48ee1dc94f3475413cfac0c4
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-03-10 14:57:43 +0300

    Simplify nuget install

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests.NuGet/install-package.ps1
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests.NuGet/install-package.ps1
@@ -4,15 +4,13 @@ rmdir pkg -Force -Recurse
 mkdir nupkg
 mkdir pkg
 
-nuget pack ..\Apache.Ignite.Core\Apache.Ignite.Core.csproj -Prop Configuration=Debug -Prop Platform=x64
-nuget pack ..\Apache.Ignite.Linq\Apache.Ignite.Linq.csproj -Prop Configuration=Debug -Prop Platform=x64
-
-copy ..\Apache.Ignite.Core\*.nupkg nupkg
-copy ..\Apache.Ignite.Linq\*.nupkg nupkg
+nuget pack ..\Apache.Ignite.Core\Apache.Ignite.Core.csproj -Prop Configuration=Debug -Prop Platform=x64 -OutputDirectory nupkg
+nuget pack ..\Apache.Ignite.Linq\Apache.Ignite.Linq.csproj -Prop Configuration=Debug -Prop Platform=x64 -OutputDirectory nupkg
 
 $ver = (Get-ChildItem nupkg\Apache.Ignite.Linq*)[0].Name -replace '\D+([\d.]+)\.\D+','$1'
+$nugetSrc = (Get-Location).Path + "\nupkg"
 
-nuget install Apache.Ignite.Linq -Version $ver -OutputDirectory pkg
+nuget install Apache.Ignite.Linq -Version $ver -OutputDirectory pkg -Source $nugetSrc
 
 move ".\pkg\Apache.Ignite.$ver" ".\pkg\Apache.Ignite"
 move ".\pkg\Apache.Ignite.Linq.$ver" ".\pkg\Apache.Ignite.Linq"
