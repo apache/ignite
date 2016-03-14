9eca5f5db86d7d23d4a20257188cef1a94e67168
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-03-14 16:56:22 +0300

    wip

---
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Binary/MarshallerContext.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Core.Impl.Binary
+{
+    using System;
+
+    /// <summary>
+    /// Marshaller context, manages dynamic type registration in the marshaller cache.
+    /// </summary>
+    internal class MarshallerContext
+    {
+        public bool RegisterType(int id, TypeResolver type)
+        {
+            // TODO: delegate to Java
+            return false;
+        }
+
+        public Type GetType(int id)
+        {
+            // TODO: delegate to Java
+            return null;
+        }
+    }
+}
