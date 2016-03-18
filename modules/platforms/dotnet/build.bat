7fd2c99b39277853c2e5993001305fb2c4c34b9d
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-03-18 13:09:29 +0300

    Fix build script

--- modules/platforms/dotnet/build.bat
+++ modules/platforms/dotnet/build.bat
@@ -24,11 +24,7 @@
 rem uncomment the following if having problems with Microsoft.Cpp.Default.props
 rem SET VCTargetsPath=C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V120
 
-set PLATFORM=%1
-if [%PLATFORM%]==[] set PLATFORM=x64
-
 set TARGET_DIR=bin
-if not [%PLATFORM%]==[x64] set TARGET_DIR=%TARGET_DIR%\%PLATFORM%
 
 rem Validate path to .Net installation.
 IF NOT EXIST %DOTNET_PATH%\MSBuild.exe SET DOTNET_PATH=c:\Windows\Microsoft.NET\Framework\v4.0.30319
@@ -42,8 +38,7 @@ cd %~dp0
 
 rem Build project.
 echo .
-echo Build project for platform: %PLATFORM%
-MSBuild.exe Apache.Ignite.sln /t:Clean;Rebuild /p:Configuration=Release /p:Platform=%PLATFORM% /val /m /nologo /ds
+MSBuild.exe Apache.Ignite.sln /t:Clean;Rebuild /p:Configuration=Release /val /m /nologo /ds
 
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
@@ -56,7 +51,7 @@ echo Copy client libraries into the distribution directory: %TARGET_DIR%
 rmdir /S /Q %TARGET_DIR%
 IF NOT EXIST %TARGET_DIR%\NUL mkdir %TARGET_DIR%
 
-copy /Y Apache.Ignite\bin\%PLATFORM%\Release\*.* %TARGET_DIR%
+copy /Y Apache.Ignite\bin\Release\*.* %TARGET_DIR%
 
 copy /Y readme.txt %TARGET_DIR%
 
