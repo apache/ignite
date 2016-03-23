ef3ccaf2b980dc72c5e3bf582536422da0fdfb2e
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-03-23 17:37:58 +0300

    wip

--- modules/platforms/dotnet/Apache.Ignite/Config/ArgsConfigurator.cs
+++ modules/platforms/dotnet/Apache.Ignite/Config/ArgsConfigurator.cs
@@ -33,7 +33,7 @@ namespace Apache.Ignite.Config
         {
             return args
                 .Select(x => x.TrimStart('-'))
-                .Select(x => x.StartsWith(Configurator.CmdJvmOpt)
+                .Select(x => x.StartsWith(Configurator.CmdJvmOpt + "-")
                     ? new[] {Configurator.CmdJvmOpt, x.Substring(Configurator.CmdJvmOpt.Length)}
                     : x.Split('='))
                 .Where(x => x.Length == 2)
