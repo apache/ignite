0368655d88c54fd6d2f6f12703aa932237f4e2fb
isapego <isapego@gridgain.com>
2016-03-25 17:05:45 +0300

    IGNITE-2851: Fix for example.

--- examples/src/main/java/org/apache/ignite/examples/datagrid/CacheQueryExample.java
+++ examples/src/main/java/org/apache/ignite/examples/datagrid/CacheQueryExample.java
@@ -63,10 +63,10 @@ import org.apache.ignite.lang.IgniteBiPredicate;
  */
 public class CacheQueryExample {
     /** Organizations cache name. */
-    private static final String ORG_CACHE =  "Organizations";
+    private static final String ORG_CACHE = CacheQueryExample.class.getSimpleName() + "Organizations";
 
     /** Persons cache name. */
-    private static final String PERSON_CACHE =  "Persons";
+    private static final String PERSON_CACHE = CacheQueryExample.class.getSimpleName() + "Persons";
 
     /**
      * Executes example.
@@ -118,10 +118,6 @@ public class CacheQueryExample {
 
                 // Example for SQL-based fields queries that uses joins.
                 sqlFieldsQueryWithJoin();
-
-                synchronized (PERSON_CACHE) {
-                    PERSON_CACHE.wait();
-                }
             }
             finally {
                 // Distributed cache could be removed from cluster only by #destroyCache() call.
