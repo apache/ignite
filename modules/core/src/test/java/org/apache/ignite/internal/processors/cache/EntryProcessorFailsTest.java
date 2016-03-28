ec15cd4753e601dc0e01fd69608833e628367fa7
ashutak <ashutak@gridgain.com>
2016-03-28 13:36:50 +0300

    ignite-2645: rewrite test

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/EntryProcessorFailsTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/EntryProcessorFailsTest.java
@@ -31,15 +31,13 @@ import javax.cache.integration.CacheWriterException;
 import javax.cache.processor.EntryProcessor;
 import javax.cache.processor.EntryProcessorException;
 import javax.cache.processor.MutableEntry;
-import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
-import org.apache.ignite.IgniteException;
-import org.apache.ignite.Ignition;
 import org.apache.ignite.cache.store.CacheStoreAdapter;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
+import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 
 import static org.apache.ignite.cache.CacheAtomicWriteOrderMode.PRIMARY;
 import static org.apache.ignite.cache.CacheAtomicityMode.ATOMIC;
@@ -50,44 +48,15 @@ import static org.apache.ignite.configuration.DeploymentMode.SHARED;
 /**
  *
  */
-public class EntryProcessorFails {
-    /**
-     * @param cacheName Cache anme.
-     * @return Cache configuration.
-     */
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-    private CacheConfiguration<String, List<Double>> createCacheConfiguration(String cacheName) {
-        CacheConfiguration<String, List<Double>> cacheConfiguration = new CacheConfiguration<>();
+public class EntryProcessorFailsTest extends GridCommonAbstractTest {
+    /** {@inheritDoc} */
+    @Override protected IgniteConfiguration getConfiguration(String gridName) throws Exception {
+        IgniteConfiguration cfg = super.getConfiguration(gridName);
 
-        cacheConfiguration.setName(cacheName);
-        cacheConfiguration.setCacheMode(PARTITIONED);
-        cacheConfiguration.setAtomicityMode(ATOMIC);
-        cacheConfiguration.setAtomicWriteOrderMode(PRIMARY);
-        cacheConfiguration.setWriteSynchronizationMode(PRIMARY_SYNC);
+        cfg.setCacheConfiguration(createCacheConfiguration());
 
-        cacheConfiguration.setReadThrough(true);
-        cacheConfiguration.setWriteThrough(true);
-
-        Factory cacheStoreFactory = new FactoryBuilder.SingletonFactory(new DummyCacheStore());
-        cacheConfiguration.setCacheStoreFactory(cacheStoreFactory);
-
-        return cacheConfiguration;
-    }
-
-    /**
-     * @param gridName Grid name.
-     * @return Ignite config.
-     * @throws IgniteException If failed.
-     */
-    private IgniteConfiguration getGridConfiguration(String gridName) throws IgniteException {
-        IgniteConfiguration gridCfg = new IgniteConfiguration();
-        gridCfg.setGridName(gridName);
-
-        CacheConfiguration<String, List<Double>> cacheCfg = createCacheConfiguration("testCache");
-        gridCfg.setCacheConfiguration(cacheCfg);
-
-        gridCfg.setLocalHost("127.0.0.1");
-        gridCfg.setDeploymentMode(SHARED);
+        cfg.setLocalHost("127.0.0.1");
+        cfg.setDeploymentMode(SHARED);
 
         TcpDiscoveryVmIpFinder ipFinder = new TcpDiscoveryVmIpFinder();
         ipFinder.setAddresses(Arrays.asList("127.0.0.1:47501", "127.0.0.1:47502"));
@@ -97,44 +66,61 @@ public class EntryProcessorFails {
         spi.setLocalAddress("127.0.0.1");
         spi.setLocalPort(47501);
 
-        gridCfg.setDiscoverySpi(spi);
+        cfg.setDiscoverySpi(spi);
 
-        return gridCfg;
+        return cfg;
+    }
+
+    /**
+     * @return Cache configuration.
+     */
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    private CacheConfiguration<String, List<Double>> createCacheConfiguration() {
+        CacheConfiguration<String, List<Double>> cacheConfiguration = new CacheConfiguration<>();
+
+        cacheConfiguration.setCacheMode(PARTITIONED);
+        cacheConfiguration.setAtomicityMode(ATOMIC);
+        cacheConfiguration.setAtomicWriteOrderMode(PRIMARY);
+        cacheConfiguration.setWriteSynchronizationMode(PRIMARY_SYNC);
+
+        cacheConfiguration.setReadThrough(true);
+        cacheConfiguration.setWriteThrough(true);
+
+        Factory cacheStoreFactory = new FactoryBuilder.SingletonFactory(new DummyCacheStore());
+
+        cacheConfiguration.setCacheStoreFactory(cacheStoreFactory);
+
+        return cacheConfiguration;
+    }
+
+    /** {@inheritDoc} */
+    @Override protected void beforeTestsStarted() throws Exception {
+        super.beforeTestsStarted();
+
+        startGridsMultiThreaded(1);
+    }
+
+    /** {@inheritDoc} */
+    @Override protected void afterTestsStopped() throws Exception {
+        stopAllGrids();
+
+        super.afterTestsStopped();
     }
 
     /**
      * @throws Exception If failed.
      */
-    public void doCacheEntryProcessor() throws Exception {
-        IgniteConfiguration gridCfg = getGridConfiguration("testGrid");
+    @SuppressWarnings("serial")
+    public void testCacheEntryProcessor() throws Exception {
+        final Set<String> testKeys = new HashSet<String>() {{
+            add("testKey_1");
+            add("testKey_2");
+            add("testKey_3");
+        }};
 
-        try (Ignite grid = Ignition.start(gridCfg)) {
-            final Set<String> testKeys = new HashSet<>();
+        final IgniteCache<String, List<Double>> cache = grid(0).cache(null);
 
-            testKeys.add("testKey_1");
-            testKeys.add("testKey_2");
-            testKeys.add("testKey_3");
-
-            final IgniteCache<String, List<Double>> testCache = grid.cache("testCache");
-
-            EntryProcessor<String, List<Double>, List<Double>> entryProcessor = new DummyEntryProcessor();
-
-            testCache.invokeAll(testKeys, entryProcessor);
-        }
-    }
-
-    /**
-     * @param args Arguments.
-     * @throws Exception If failed.
-     */
-    public static void main(String[] args) throws Exception {
-        System.out.println("Running cache entry test.");
-
-        EntryProcessorFails test = new EntryProcessorFails();
-
-        test.doCacheEntryProcessor();
-
-        System.out.println("Test complete!");
+        cache.invokeAll(testKeys, new DummyEntryProcessor());
     }
 
     /**
