c2b5cef3de13256afd087925f9d74f237f4b03a8
ashutak <ashutak@gridgain.com>
2016-03-28 12:44:13 +0300

    WebSphereTmFactory

--- modules/web/ignite-weblogic-test/src/main/java/org/apache/ignite/webtest/WebSphereTmFactory.java
+++ modules/web/ignite-weblogic-test/src/main/java/org/apache/ignite/webtest/WebSphereTmFactory.java
@@ -17,10 +17,21 @@
 
 package org.apache.ignite.webtest;
 
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import javax.cache.configuration.Factory;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.InvalidTransactionException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.Synchronization;
+import javax.transaction.SystemException;
+import javax.transaction.Transaction;
 import javax.transaction.TransactionManager;
+import javax.transaction.xa.XAResource;
 import org.apache.ignite.IgniteException;
 
 /**
@@ -28,25 +39,171 @@ import org.apache.ignite.IgniteException;
  */
 public class WebSphereTmFactory implements Factory<TransactionManager> {
     /** */
+    private static final Class<?> onePhaseXAResourceCls;
+
+    static {
+        try {
+            onePhaseXAResourceCls = Class.forName("com.ibm.tx.jta.OnePhaseXAResource");
+        }
+        catch (ClassNotFoundException e) {
+            throw new IgniteException(e);
+        }
+    }
+
+    /** */
     private static final long serialVersionUID = 0;
 
     /** {@inheritDoc} */
     @SuppressWarnings("unchecked")
     @Override public TransactionManager create() {
-        System.out.println(">>>>> DEBUG_INFO 1");
-
         try {
             Class clazz = Class.forName("com.ibm.tx.jta.impl.TranManagerSet");
 
             Method m = clazz.getMethod("instance", (Class[])null);
 
-            TransactionManager tranMgr = (TransactionManager)m.invoke((Object)null, (Object[])null);
+            TransactionManager tranMgr = (TransactionManager)m.invoke(null, (Object[])null);
 
             return new WebSphereTransactionManager(tranMgr);
         }
         catch (SecurityException | ClassNotFoundException | IllegalArgumentException | NoSuchMethodException
             | InvocationTargetException | IllegalAccessException e) {
             throw new IgniteException(e);
+        }
+    }
+    /**
+     *
+     */
+    private static class WebSphereTransactionManager implements TransactionManager {
+        /** */
+        private TransactionManager mgr;
+
+        /**
+         * @param mgr Transaction Manager.
+         */
+        WebSphereTransactionManager(TransactionManager mgr) {
+            this.mgr = mgr;
+        }
+
+        /** {@inheritDoc} */
+        @Override public void begin() throws NotSupportedException, SystemException {
+            mgr.begin();
+        }
+
+        /** {@inheritDoc} */
+        @Override public void commit() throws RollbackException, HeuristicMixedException, HeuristicRollbackException,
+            SecurityException, IllegalStateException, SystemException {
+            mgr.commit();
+        }
+
+        /** {@inheritDoc} */
+        @Override public int getStatus() throws SystemException {
+            return mgr.getStatus();
+        }
+
+        /** {@inheritDoc} */
+        @Override public Transaction getTransaction() throws SystemException {
+            Transaction tx = mgr.getTransaction();
+
+            if (tx == null)
+                return null;
+
+            return new WebSphereTransaction(tx);
+        }
+
+        /** {@inheritDoc} */
+        @Override public void resume(Transaction tobj) throws InvalidTransactionException, IllegalStateException,
+            SystemException {
+            mgr.resume(tobj);
+        }
+
+        /** {@inheritDoc} */
+        @Override public void rollback() throws IllegalStateException, SecurityException, SystemException {
+            mgr.rollback();
+        }
+
+        /** {@inheritDoc} */
+        @Override public void setRollbackOnly() throws IllegalStateException, SystemException {
+            mgr.setRollbackOnly();
+        }
+
+        /** {@inheritDoc} */
+        @Override public void setTransactionTimeout(int seconds) throws SystemException {
+            mgr.setTransactionTimeout(seconds);
+        }
+
+        /** {@inheritDoc} */
+        @Override public Transaction suspend() throws SystemException {
+            return mgr.suspend();
+        }
+    }
+
+    /**
+     *
+     */
+    private static class WebSphereTransaction implements Transaction {
+        /** */
+        private final Transaction tx;
+
+        /**
+         * @param tx Transaction.
+         */
+        WebSphereTransaction(Transaction tx) {
+            assert tx != null;
+
+            this.tx = tx;
+        }
+
+        /** {@inheritDoc} */
+        @Override public void commit() throws RollbackException, HeuristicMixedException, HeuristicRollbackException,
+            SecurityException, IllegalStateException, SystemException {
+            tx.commit();
+        }
+
+        /** {@inheritDoc} */
+        @Override public boolean delistResource(XAResource xaRes, int flag) throws IllegalStateException,
+            SystemException {
+            return tx.delistResource(xaRes, flag);
+        }
+
+        /** {@inheritDoc} */
+        @SuppressWarnings("SimplifiableIfStatement")
+        @Override public boolean enlistResource(final XAResource xaRes) throws RollbackException, IllegalStateException,
+            SystemException {
+            if (xaRes == null)
+                return false;
+
+//            final XAResource res = new IgniteOnePhaseXAResource(xaRes);
+
+            Object ibmProxy = Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(),
+                new Class[] {onePhaseXAResourceCls},
+                new InvocationHandler() {
+                    @Override public Object invoke(Object proxy, Method mtd, Object[] args) throws Throwable {
+                        return mtd.invoke(xaRes, args);
+                    }
+                });
+
+            return tx.enlistResource((XAResource)ibmProxy);
+        }
+
+        /** {@inheritDoc} */
+        @Override public int getStatus() throws SystemException {
+            return tx.getStatus();
+        }
+
+        /** {@inheritDoc} */
+        @Override public void registerSynchronization(Synchronization sync) throws RollbackException,
+            IllegalStateException, SystemException {
+            tx.registerSynchronization(sync);
+        }
+
+        /** {@inheritDoc} */
+        @Override public void rollback() throws IllegalStateException, SystemException {
+            tx.rollback();
+        }
+
+        /** {@inheritDoc} */
+        @Override public void setRollbackOnly() throws IllegalStateException, SystemException {
+            tx.setRollbackOnly();
         }
     }
 }
