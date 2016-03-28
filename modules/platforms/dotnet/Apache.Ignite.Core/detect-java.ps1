f57702ff731da8e5a3e0af6adf8de9cbbaf30adf
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-03-28 16:23:04 +0300

    wip

--- modules/platforms/dotnet/Apache.Ignite.Core/detect-java.ps1
+++ modules/platforms/dotnet/Apache.Ignite.Core/detect-java.ps1
@@ -11,8 +11,20 @@ If (Test-Path Env:\JAVA_HOME) {
 }
 
 # Next, check registry
+
 if ($x64) {
-    If (Test-Path 'Software\JavaSoft\Java Runtime Environment') {
-        
+    If (Test-Path 'HKLM:\Software\JavaSoft\Java Runtime Environment') {
+        $key = Get-Item 'HKLM:\Software\JavaSoft\Java Runtime Environment'
+        $subKeys = $key.GetSubKeyNames()
+        $curVer = $key.GetValue("CurrentVersion")
+
+        If ($curVer -eq $null -or !$subKeys.Contains($curVer)) {
+            $curVer = $subKeys[0]
+        }
+            
+        $env:JAVA_HOME64 = $key.OpenSubKey($curVer).GetValue("JavaHome")
     }
 }
+
+echo "JAVA_HOME64: $env:JAVA_HOME64"
+echo "JAVA_HOME32: $env:JAVA_HOME32"
