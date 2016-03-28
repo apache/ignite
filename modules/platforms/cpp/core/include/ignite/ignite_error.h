24f27d0c579973c1e8112c13c0c33ef11087a3dc
isapego <isapego@gridgain.com>
2016-03-28 16:16:17 +0300

    IGNITE-2902: IgniteError now implements std::exception.

--- modules/platforms/cpp/core/include/ignite/ignite_error.h
+++ modules/platforms/cpp/core/include/ignite/ignite_error.h
@@ -23,8 +23,10 @@
 #ifndef _IGNITE_ERROR
 #define _IGNITE_ERROR
 
-#include <sstream>
 #include <stdint.h>
+
+#include <exception>
+#include <sstream>
 
 #include <ignite/common/common.h>
 
@@ -75,7 +77,7 @@ namespace ignite
     /**
      * Ignite error information.
      */
-    class IGNITE_IMPORT_EXPORT IgniteError
+    class IGNITE_IMPORT_EXPORT IgniteError : public std::exception
     {
     public:
         /** Success. */
@@ -243,7 +245,14 @@ namespace ignite
          * @return Error message.
          */
         const char* GetText() const;
-        
+
+        /**
+         * Get error message. Synonim for GetText().
+         *
+         * @return Error message.
+         */
+        virtual const char* what() const;
+
         /**
          * Set error.
          *
