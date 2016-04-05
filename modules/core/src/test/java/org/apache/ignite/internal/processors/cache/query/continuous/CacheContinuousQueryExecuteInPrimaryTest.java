9a0a85d33229e5605887436dcf3377ccafd11d7d
vdpyatkov <vpyatkov@gridgain.com>
2016-04-05 14:43:33 +0300

    IGNITE-2822 Continuous query local listener can be notified with empty list of events
    Added some various cache type and modification cache methods on test case.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/query/continuous/CacheContinuousQueryExecuteInPrimaryTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/query/continuous/CacheContinuousQueryExecuteInPrimaryTest.java
@@ -19,9 +19,12 @@ package org.apache.ignite.internal.processors.cache.query.continuous;
 
 import javax.cache.Cache;
 import javax.cache.configuration.FactoryBuilder;
+
+import org.apache.commons.beanutils.converters.IntegerConverter;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.cache.CacheAtomicityMode;
 import org.apache.ignite.cache.CacheEntryEventSerializableFilter;
+import org.apache.ignite.cache.CacheMemoryMode;
 import org.apache.ignite.cache.CacheMode;
 import org.apache.ignite.cache.query.ContinuousQuery;
 import org.apache.ignite.cache.query.QueryCursor;
@@ -34,20 +37,28 @@ import org.jetbrains.annotations.NotNull;
 import javax.cache.event.CacheEntryEvent;
 import javax.cache.event.CacheEntryListenerException;
 import javax.cache.event.CacheEntryUpdatedListener;
+import javax.cache.processor.EntryProcessor;
+import javax.cache.processor.EntryProcessorException;
+import javax.cache.processor.MutableEntry;
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
-
+import static org.apache.ignite.cache.CacheAtomicWriteOrderMode.PRIMARY;
+import static org.apache.ignite.cache.CacheMode.LOCAL;
+import static org.apache.ignite.cache.CacheMode.PARTITIONED;
+import static org.apache.ignite.cache.CacheMode.REPLICATED;
+import static org.apache.ignite.cache.CacheWriteSynchronizationMode.FULL_SYNC;
 
 /**
  * Continuous queries execute in primary node tests.
  */
 public class CacheContinuousQueryExecuteInPrimaryTest extends GridCommonAbstractTest
     implements Serializable {
-
-    /** Name of cache */
-    protected static final String CACHE_NAME = "test_cache";
 
     /** Latch timeout. */
     protected static final long LATCH_TIMEOUT = 5000;
@@ -58,7 +69,6 @@ public class CacheContinuousQueryExecuteInPrimaryTest extends GridCommonAbstract
         IgniteConfiguration cfg = super.getConfiguration(gridName);
 
         cfg.setPeerClassLoadingEnabled(true);
-        cfg.setCacheConfiguration(cacheConfiguration());
         ((TcpCommunicationSpi)cfg.getCommunicationSpi()).setSharedMemoryPort(-1);
 
         return cfg;
@@ -68,14 +78,16 @@ public class CacheContinuousQueryExecuteInPrimaryTest extends GridCommonAbstract
      * @return Cache configuration.
      */
     @NotNull
-    private CacheConfiguration cacheConfiguration() {
-        CacheConfiguration cacheCfg = defaultCacheConfiguration();
+    protected CacheConfiguration<Integer, String> cacheConfiguration(CacheMode cacheMode) {
+        CacheConfiguration<Integer, String> ccfg = new CacheConfiguration<>();
 
-        cacheCfg.setName(CACHE_NAME);
-        cacheCfg.setCacheMode(CacheMode.PARTITIONED);
-        cacheCfg.setAtomicityMode(CacheAtomicityMode.ATOMIC);
+        ccfg.setAtomicityMode(CacheAtomicityMode.ATOMIC);
+        ccfg.setCacheMode(cacheMode);
+        ccfg.setMemoryMode(CacheMemoryMode.ONHEAP_TIERED);
+        ccfg.setWriteSynchronizationMode(FULL_SYNC);
+        ccfg.setAtomicWriteOrderMode(PRIMARY);
 
-        return cacheCfg;
+        return ccfg;
     }
 
     /** {@inheritDoc} */
@@ -86,73 +98,150 @@ public class CacheContinuousQueryExecuteInPrimaryTest extends GridCommonAbstract
     /**
      * @throws Exception If failed.
      */
-    public void testWithoutEventsEntries() throws Exception {
-        IgniteCache<Integer, String> cache = grid(0).cache(CACHE_NAME);
+    public void testLocalCache() throws Exception {
+        CacheConfiguration<Integer, String> ccfg = cacheConfiguration(LOCAL);
+        doTestWithoutEventsEntries(ccfg);
+        doTestWithEventsEntries(ccfg);
+    }
 
-        int ITERATION_CNT = 100;
+    /**
+     * @throws Exception If failed.
+     */
+    public void testReplicatedCache() throws Exception {
+        CacheConfiguration<Integer, String> ccfg = cacheConfiguration(REPLICATED);
+        doTestWithoutEventsEntries(ccfg);
+        doTestWithEventsEntries(ccfg);
+    }
 
-        for (int i = 0; i < ITERATION_CNT; i++) {
-            // Create new continuous query.
-            ContinuousQuery<Integer, String> qry = new ContinuousQuery<>();
+    /**
+     * @throws Exception If failed.
+     */
+    public void testPartitionedCache() throws Exception {
+        CacheConfiguration<Integer, String> ccfg = cacheConfiguration(PARTITIONED);
+        doTestWithoutEventsEntries(ccfg);
+        doTestWithEventsEntries(ccfg);
+    }
 
-            qry.setLocalListener(new CacheEntryUpdatedListener<Integer, String>() {
-                @Override public void onUpdated(Iterable<CacheEntryEvent<? extends Integer, ? extends String>> iterable)
-                    throws CacheEntryListenerException {
-                    assert false;
-                }
-            });
+    /**
+     * @throws Exception If failed.
+     */
+    private void doTestWithoutEventsEntries(CacheConfiguration<Integer, String> ccfg) throws Exception {
 
-            // This filters return always false
-            qry.setRemoteFilterFactory(FactoryBuilder.factoryOf(
-                new CacheEntryEventSerializableFilter<Integer, String>() {
-                    @Override public boolean evaluate(
-                        CacheEntryEvent<? extends Integer, ? extends String> cacheEntryEvent)
+        try (IgniteCache<Integer, String> cache = grid(0).createCache(ccfg)) {
+
+            int ITERATION_CNT = 100;
+            final AtomicBoolean noOneListen = new AtomicBoolean(true);
+
+            for (int i = 0; i < ITERATION_CNT; i++) {
+                // Create new continuous query.
+                ContinuousQuery<Integer, String> qry = new ContinuousQuery<>();
+
+                qry.setLocalListener(new CacheEntryUpdatedListener<Integer, String>() {
+                    @Override public void onUpdated(Iterable<CacheEntryEvent<? extends Integer, ? extends String>> iterable)
                         throws CacheEntryListenerException {
-                        return false;
+                        noOneListen.compareAndSet(true, false);
                     }
-                }));
+                });
 
-            // Execute query.
-            try (QueryCursor<Cache.Entry<Integer, String>> qryCursor = cache.query(qry)) {
-                for (int key = 0; key < 8; key++)
-                    cache.put(key, Integer.toString(key));
+                // This filters return always false
+                qry.setRemoteFilterFactory(FactoryBuilder.factoryOf(
+                    new CacheEntryEventSerializableFilter<Integer, String>() {
+                        @Override public boolean evaluate(
+                            CacheEntryEvent<? extends Integer, ? extends String> cacheEntryEvent)
+                            throws CacheEntryListenerException {
+                            return false;
+                        }
+                    }));
+
+                // Execute query.
+                executeQuery(cache, qry);
             }
+
+            assert noOneListen.get();
+
+        } finally {
+            ignite(0).destroyCache(ccfg.getName());
+        }
+    }
+
+    private void executeQuery(IgniteCache<Integer, String> cache, ContinuousQuery<Integer, String> qry) {
+        try (QueryCursor<Cache.Entry<Integer, String>> qryCursor = cache.query(qry)) {
+            for (int key = 0; key < 8; key++)
+                cache.put(key, Integer.toString(key));
+
+            Map<Integer, String> map = new HashMap<>(8);
+            for (int key=8; key<16; key++)
+                map.put(key, Integer.toString(key));
+            cache.putAll(map);
+
+            for (int key = 0; key < 8; key++) {
+                cache.invoke(key, new EntryProcessor<Integer, String, Object>() {
+
+                    @Override
+                    public Object process(MutableEntry<Integer, String> entry,
+                        Object... objects) throws EntryProcessorException {
+                        entry.setValue(Integer.toString(entry.getKey() + 1));
+                        return null;
+                    }
+
+                });
+            }
+
+            Map<Integer, EntryProcessor<Integer, String, Object>> invokeMap = new HashMap<>(8);
+            for (int key = 8; key < 16; key++)
+                invokeMap.put(key, new EntryProcessor<Integer, String, Object>() {
+
+                    @Override
+                    public Object process(MutableEntry<Integer, String> entry,
+                        Object... objects) throws EntryProcessorException {
+                        entry.setValue(Integer.toString(entry.getKey() - 1));
+                        return null;
+                    }
+
+                });
+            cache.invokeAll(invokeMap);
+
         }
     }
 
     /**
      * @throws Exception If failed.
      */
-    public void testWithEventsEntries() throws Exception {
-        IgniteCache<Integer, String> cache = grid(0).cache(CACHE_NAME);
+    public void doTestWithEventsEntries(CacheConfiguration<Integer, String> ccfg) throws Exception {
 
-        // Create new continuous query.
-        ContinuousQuery<Integer, String> qry = new ContinuousQuery<>();
+        try (IgniteCache<Integer, String> cache = grid(0).createCache(ccfg)) {
 
-        final CountDownLatch latch = new CountDownLatch(4);
+            // Create new continuous query.
+            ContinuousQuery<Integer, String> qry = new ContinuousQuery<>();
 
-        qry.setLocalListener(new CacheEntryUpdatedListener<Integer, String>() {
-            @Override public void onUpdated(Iterable<CacheEntryEvent<? extends Integer, ? extends String>> iterable)
-                throws CacheEntryListenerException {
-                latch.countDown();
-            }
-        });
+            final CountDownLatch latch = new CountDownLatch(16);
+            final AtomicInteger cnt = new AtomicInteger(0);
 
-        // This filters return always false
-        qry.setRemoteFilter(new CacheEntryEventSerializableFilter<Integer, String>() {
-            @Override public boolean evaluate(CacheEntryEvent<? extends Integer, ? extends String> e)
-                throws CacheEntryListenerException {
-                return e.getKey() % 2 == 0;
-            }
-        });
+            qry.setLocalListener(new CacheEntryUpdatedListener<Integer, String>() {
+                @Override public void onUpdated(Iterable<CacheEntryEvent<? extends Integer, ? extends String>> iterable)
+                    throws CacheEntryListenerException {
+                    for (CacheEntryEvent<? extends Integer, ? extends String> e : iterable)
+                        cnt.incrementAndGet();
+                        latch.countDown();
+                }
+            });
 
-        // Execute query.
-        cache.query(qry);
+            // This filters return always false
+            qry.setRemoteFilter(new CacheEntryEventSerializableFilter<Integer, String>() {
+                @Override public boolean evaluate(CacheEntryEvent<? extends Integer, ? extends String> e)
+                    throws CacheEntryListenerException {
+                    return e.getKey() % 2 == 0;
+                }
+            });
 
-        for (int i = 0; i < 8; i++)
-            cache.put(i, Integer.toString(i));
+            // Execute query.
+            executeQuery(cache, qry);
 
-        assert latch.await(LATCH_TIMEOUT, MILLISECONDS);
+            assert latch.await(LATCH_TIMEOUT, MILLISECONDS);
+            assert cnt.get() == 16;
+        } finally {
+            ignite(0).destroyCache(ccfg.getName());
+        }
 
     }
 
