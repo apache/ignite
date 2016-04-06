fae946bb9308ad81480e9d61318f74c6ca1c4e71
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-04-06 17:31:08 +0300

    dotnet types

---
+++ modules/platforms/dotnet/Apache.Ignite.Core/Cache/Configuration/NearCacheConfiguration.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Core.Cache.Configuration
+{
+    /// <summary>
+    /// Defines near cache configuration.
+    /// <para />
+    /// Distributed cache can also be fronted by a Near cache, which is a smaller local cache that stores most 
+    /// recently or most frequently accessed data. 
+    /// Just like with a partitioned cache, the user can control the size of the near cache and its eviction policies. 
+    /// </summary>
+    public class NearCacheConfiguration
+    {
+    }
+}
