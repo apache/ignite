15528cc782b0ed3852ea753029d42eb020a9e7c3
ascherbakoff <alexey.scherbakoff@gmail.com>
2016-05-03 18:15:25 +0300

    IGNITE-2982 Improving tests

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/CacheOffheapBatchIndexingTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/CacheOffheapBatchIndexingTest.java
@@ -472,10 +472,225 @@ public class CacheOffheapBatchIndexingTest extends GridCommonAbstractTest {
         try (IgniteDataStreamer<Object, Object> streamer = ignite(0).dataStreamer(name)) {
             for (int i = 0; i < 30_000; i++) {
                 if (i % 2 == 0)
-                    streamer.addData(i, new AtomicBinaryOffheapBaseBatchTest.Person(i, i + 1, String.valueOf(i), String.valueOf(i + 1), i / 0.99));
+                    streamer.addData(i, new Other.Person(i, i + 1, String.valueOf(i), String.valueOf(i + 1), i / 0.99));
                 else
-                    streamer.addData(i, new AtomicBinaryOffheapBaseBatchTest.Organization(i, String.valueOf(i)));
+                    streamer.addData(i, new Other.Organization(i, String.valueOf(i)));
             }
+        }
+    }
+}
+
+/**
+ * Define another namespace
+ */
+class Other {
+    /**
+     * Ignite cache value class.
+     */
+    protected static class Person implements Binarylizable {
+
+        /** Person ID. */
+        @QuerySqlField(index = true)
+        private int id;
+
+        /** Organization ID. */
+        @QuerySqlField(index = true)
+        private int orgId;
+
+        /** First name (not-indexed). */
+        @QuerySqlField
+        private String firstName;
+
+        /** Last name (not indexed). */
+        @QuerySqlField
+        private String lastName;
+
+        /** Salary. */
+        @QuerySqlField(index = true)
+        private double salary;
+
+        /**
+         * Constructs empty person.
+         */
+        public Person() {
+            // No-op.
+        }
+
+        /**
+         * Constructs person record.
+         *
+         * @param id Person ID.
+         * @param orgId Organization ID.
+         * @param firstName First name.
+         * @param lastName Last name.
+         * @param salary Salary.
+         */
+        public Person(int id, int orgId, String firstName, String lastName, double salary) {
+            this.id = id;
+            this.orgId = orgId;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.salary = salary;
+        }
+
+        /**
+         * @return Person id.
+         */
+        public int getId() {
+            return id;
+        }
+
+        /**
+         * @param id Person id.
+         */
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        /**
+         * @return Organization id.
+         */
+        public int getOrganizationId() {
+            return orgId;
+        }
+
+        /**
+         * @param orgId Organization id.
+         */
+        public void setOrganizationId(int orgId) {
+            this.orgId = orgId;
+        }
+
+        /**
+         * @return Person first name.
+         */
+        public String getFirstName() {
+            return firstName;
+        }
+
+        /**
+         * @param firstName Person first name.
+         */
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        /**
+         * @return Person last name.
+         */
+        public String getLastName() {
+            return lastName;
+        }
+
+        /**
+         * @param lastName Person last name.
+         */
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        /**
+         * @return Salary.
+         */
+        public double getSalary() {
+            return salary;
+        }
+
+        /**
+         * @param salary Salary.
+         */
+        public void setSalary(double salary) {
+            this.salary = salary;
+        }
+
+        /** {@inheritDoc} */
+        @Override public void writeBinary(BinaryWriter writer) throws BinaryObjectException {
+            writer.writeInt("id", id);
+            writer.writeInt("orgId", orgId);
+            writer.writeString("firstName", firstName);
+            writer.writeString("lastName", lastName);
+            writer.writeDouble("salary", salary);
+        }
+
+        /** {@inheritDoc} */
+        @Override public void readBinary(BinaryReader reader) throws BinaryObjectException {
+            id = reader.readInt("id");
+            orgId = reader.readInt("orgId");
+            firstName = reader.readString("firstName");
+            lastName = reader.readString("lastName");
+            salary = reader.readDouble("salary");
+        }
+    }
+
+    /**
+     * Ignite cache value class with indexed field.
+     */
+    protected static class Organization implements Binarylizable {
+
+        /** Organization ID. */
+        @QuerySqlField(index = true)
+        private int id;
+
+        /** Organization name. */
+        @QuerySqlField(index = true)
+        private String name;
+
+        /**
+         * Constructs empty organization.
+         */
+        public Organization() {
+            // No-op.
+        }
+
+        /**
+         * Constructs organization with given ID.
+         *
+         * @param id Organization ID.
+         * @param name Organization name.
+         */
+        public Organization(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        /**
+         * @return Organization id.
+         */
+        public int getId() {
+            return id;
+        }
+
+        /**
+         * @param id Organization id.
+         */
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        /**
+         * @return Organization name.
+         */
+        public String getName() {
+            return name;
+        }
+
+        /**
+         * @param name Organization name.
+         */
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        /** {@inheritDoc} */
+        @Override public void writeBinary(BinaryWriter writer) throws BinaryObjectException {
+            writer.writeInt("id", id);
+            writer.writeString("name", name);
+        }
+
+        /** {@inheritDoc} */
+        @Override public void readBinary(BinaryReader reader) throws BinaryObjectException {
+            id = reader.readInt("id");
+            name = reader.readString("name");
         }
     }
 }
