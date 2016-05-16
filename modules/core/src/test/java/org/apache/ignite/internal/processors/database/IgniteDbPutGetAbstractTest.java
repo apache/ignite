dd95ae1bac16ed38e717ce27cbb8b019269da838
Sergi Vladykin <sergi.vladykin@gmail.com>
2016-05-16 08:17:12 +0300

    ignite-db - more tests

--- modules/core/src/test/java/org/apache/ignite/internal/processors/database/IgniteDbPutGetAbstractTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/database/IgniteDbPutGetAbstractTest.java
@@ -27,7 +27,6 @@ import java.util.Random;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
-
 import javax.cache.Cache;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
@@ -46,6 +45,7 @@ import org.apache.ignite.configuration.DatabaseConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.internal.IgniteEx;
 import org.apache.ignite.internal.processors.cache.GridCacheAdapter;
+import org.apache.ignite.internal.processors.cache.database.tree.BPlusTree;
 import org.apache.ignite.internal.util.GridRandom;
 import org.apache.ignite.internal.util.typedef.PA;
 import org.apache.ignite.internal.util.typedef.X;
@@ -124,6 +124,12 @@ public abstract class IgniteDbPutGetAbstractTest extends GridCommonAbstractTest 
 
     /** {@inheritDoc} */
     @Override protected void beforeTest() throws Exception {
+        long seed = System.currentTimeMillis();
+
+        info("Seed: " + seed);
+
+        BPlusTree.rnd = new Random(seed);
+
         startGrids(gridCount());
 
         awaitPartitionMapExchange();
@@ -131,7 +137,70 @@ public abstract class IgniteDbPutGetAbstractTest extends GridCommonAbstractTest 
 
     /** {@inheritDoc} */
     @Override protected void afterTest() throws Exception {
+        BPlusTree.rnd = null;
+
         stopAllGrids();
+    }
+
+    public void testGradualRandomPutAllRemoveAll() {
+        IgniteEx ig = grid(0);
+
+        IgniteCache<Integer, DbValue> cache = ig.cache(null);
+
+        final int cnt = 100_000;
+
+        Random rnd = BPlusTree.rnd;
+
+        assert rnd != null;
+
+        Map<Integer, DbValue> map = new HashMap<>();
+
+        for (int i = 0; i < 5; i++) {
+            info("Iteration: " + i);
+
+            info("Grow...");
+
+            while (map.size() < cnt / 2)
+                doPutRemoveAll(rnd, cache, map, cnt, true);
+
+            info("Shrink...");
+
+            while (map.size() > cnt / 4)
+                doPutRemoveAll(rnd, cache, map, cnt, false);
+
+            info("Check...");
+
+            for (Integer key : map.keySet())
+                assertEquals(map.get(key), cache.get(key));
+        }
+    }
+
+    private void doPutRemoveAll(Random rnd, IgniteCache<Integer, DbValue> cache, Map<Integer, DbValue> map,
+        int keysCnt, boolean grow) {
+        int putCnt = grow ? 20 + rnd.nextInt(10) : 1 + rnd.nextInt(5);
+        int rmvCnt = grow ? 1 + rnd.nextInt(5) : 20 + rnd.nextInt(10);
+
+        Map<Integer, DbValue> put = new HashMap<>(putCnt);
+
+        for (int i = 0; i < putCnt; i++) {
+            int k = rnd.nextInt(keysCnt);
+
+            put.put(k, new DbValue(rnd.nextInt(500), rnd.nextInt(500) + "-value", i));
+        }
+
+        map.putAll(put);
+        cache.putAll(put);
+
+        Set<Integer> rmv = new HashSet<>();
+
+        for (int i = 0; i < rmvCnt; i++) {
+            int k = rnd.nextInt(keysCnt);
+
+            rmv.add(k);
+            map.remove(k);
+        }
+
+        cache.removeAll(rmv);
     }
 
     /**
