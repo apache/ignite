85fd683a6c9a74f3a9fa2f02cc20df0f93bdb10c
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-05-25 15:45:34 +0300

    Fix ps1 script

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests.NuGet/install-package.ps1
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests.NuGet/install-package.ps1
@@ -10,11 +10,12 @@ rmdir pkg -Force -Recurse
 mkdir nupkg
 mkdir pkg
 
+# Find all nuspec files and run 'nuget pack' on corresponding csproj files
 ls ..\*.nuspec -Recurse  `
     | % { [io.path]::ChangeExtension($_.FullName, ".csproj") } `
     | % { & $ng pack $_ -Prop Configuration=Release -Prop Platform=AnyCPU -OutputDirectory nupkg }
 
-$ver = (Get-ChildItem nupkg\Apache.Ignite.Linq*)[0].Name -replace '\D+([\d.]+)\.\D+','$1'
+$ver = (Get-ChildItem nupkg\Apache.Ignite.Linq*)[0].Name -replace 'Apache\.Ignite\.Linq\.(.*?)\.nupkg', '$1'
 
 # Replace versions in project files
 (Get-Content packages.config) `
@@ -22,5 +23,5 @@ $ver = (Get-ChildItem nupkg\Apache.Ignite.Linq*)[0].Name -replace '\D+([\d.]+)\.
     | Out-File packages.config -Encoding utf8
 
 (Get-Content Apache.Ignite.Core.Tests.NuGet.csproj) `
-    -replace '<HintPath>packages\\Apache.Ignite(.*?)\.[\d.]+\\', ('<HintPath>packages\Apache.Ignite$1.' + "$ver\") `
+    -replace 'packages\\Apache.Ignite(.*?)\.\d.*?\\', ('packages\Apache.Ignite$1.' + "$ver\") `
     | Out-File Apache.Ignite.Core.Tests.NuGet.csproj  -Encoding utf8
