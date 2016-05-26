db8851e0cffcfe0a97f62ea01e81c6d8ee9b9b8b
Igor Sapego <isapego@gridgain.com>
2016-05-26 17:30:43 +0300

    IGNITE-3205: Fix for test.

--- modules/platforms/cpp/odbc-test/src/queries_test.cpp
+++ modules/platforms/cpp/odbc-test/src/queries_test.cpp
@@ -187,7 +187,7 @@ struct QueriesTestSuiteFixture
                 BOOST_FAIL(GetOdbcErrorMessage(SQL_HANDLE_STMT, stmt));
         }
 
-        char request[] = "SELECT i8Field, i16Field, i32Field, i64Field, strField, ";
+        char request[] = "SELECT i8Field, i16Field, i32Field, i64Field, strField, "
             "floatField, doubleField, boolField, guidField, dateField, timestampField FROM TestType";
 
         ret = SQLExecDirect(stmt, (SQLCHAR*)request, SQL_NTS);
