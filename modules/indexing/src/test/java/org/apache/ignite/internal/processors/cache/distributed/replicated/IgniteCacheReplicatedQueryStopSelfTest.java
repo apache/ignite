40608efabc8b3e02da3d98a2f2577beb989869f7
Aleksei Scherbakov <alexey.scherbakoff@gmail.com>
2016-06-01 16:47:40 +0300

    IGNITE-2680 Added tests.

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/distributed/replicated/IgniteCacheReplicatedQueryStopSelfTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/distributed/replicated/IgniteCacheReplicatedQueryStopSelfTest.java
@@ -18,6 +18,9 @@
 package org.apache.ignite.internal.processors.cache.distributed.replicated;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import javax.cache.CacheException;
 import org.apache.ignite.Ignite;
@@ -26,6 +29,8 @@ import org.apache.ignite.cache.CacheMode;
 import org.apache.ignite.cache.query.QueryCursor;
 import org.apache.ignite.cache.query.SqlFieldsQuery;
 import org.apache.ignite.internal.processors.cache.IgniteCacheAbstractQuerySelfTest;
+import org.apache.ignite.internal.processors.query.h2.IgniteH2Indexing;
+import org.apache.ignite.internal.util.typedef.internal.U;
 
 import static org.apache.ignite.cache.CacheMode.PARTITIONED;
 import static org.apache.ignite.cache.CachePeekMode.ALL;
@@ -43,9 +48,9 @@ public class IgniteCacheReplicatedQueryStopSelfTest extends IgniteCacheAbstractQ
     }
 
     /**
-     * Tests stopping local query.
+     * Tests stopping two-step long query.
      */
-    public void testRemoteQueryStop() throws Exception {
+    public void testRemoteLongQueryStop() throws Exception {
         try(Ignite client = startGrid("client")) {
 
             int keyCnt = 10_000;
@@ -59,29 +64,72 @@ public class IgniteCacheReplicatedQueryStopSelfTest extends IgniteCacheAbstractQ
 
             assertEquals(0, cache.localSize(ALL));
 
-            // Produce a klller result set.
-            SqlFieldsQuery qry = new SqlFieldsQuery("select a._key, b._key from String a, String b");
-
-            //qry.setTimeout(3, TimeUnit.SECONDS);
-            final QueryCursor<List<?>> query = cache.query(qry);
+            final QueryCursor<List<?>> qry = cache.query(new SqlFieldsQuery("select a._key, b._key from String a, String b"));
 
             ignite().scheduler().runLocal(new Runnable() {
                 @Override public void run() {
-                    query.close();
+                    qry.close();
                 }
             }, 3, TimeUnit.SECONDS);
 
             // Trigger remote execution.
             try {
-                query.iterator().next();
+                qry.iterator().next();
                 fail();
             }
             catch (CacheException ex) {
                 log().error("Got expected exception", ex);
             }
 
-            // Validate if map query was cancelled.
-            Thread.sleep(10000);
+            // Validate everything was cleaned up.
+            ConcurrentMap<UUID, ConcurrentMap<Long, ?>> map = U.field(((IgniteH2Indexing)U.field(U.field(
+                grid(0).context(), "qryProc"), "idx")).mapQueryExecutor(), "qryRess");
+
+            assertEquals(1, map.size());
+
+            assertEquals(0, map.entrySet().iterator().next().getValue().size());
+        }
+    }
+
+    /**
+     * Tests stopping two step short query.
+     */
+    public void testRemoteShortQueryStop() throws Exception {
+        try(Ignite client = startGrid("client")) {
+
+            int keyCnt = 100;
+
+            IgniteCache<Object, Object> cache = client.cache(null);
+
+            assertEquals(0, cache.localSize());
+
+            for (int i = 0; i < keyCnt; i++)
+                cache.put(i, "val" + i);
+
+            assertEquals(0, cache.localSize(ALL));
+
+            final QueryCursor<List<?>> qry = cache.query(new SqlFieldsQuery("select a._key from String a"));
+
+            final CountDownLatch l = new CountDownLatch(1);
+
+            ignite().scheduler().runLocal(new Runnable() {
+                @Override public void run() {
+                    qry.close();
+                    l.countDown();
+                }
+            }, 3, TimeUnit.SECONDS);
+
+            try {
+                List<List<?>> all = qry.getAll();
+
+                assertEquals(keyCnt, all.size());
+            }
+            catch (CacheException ex) {
+                fail("Query should be finished without errors.");
+            }
+
+            // Test should complete without any exceptions.
+            l.await();
         }
     }
 }
