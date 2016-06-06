bb2092eedce5cf0dce3c649bc76148b0beb3b7bc
sboikov <sboikov@gridgain.com>
2016-06-06 12:47:38 +0300

    ignite-db-x Fixed test.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/IgniteCacheOffheapManagerImpl.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/IgniteCacheOffheapManagerImpl.java
@@ -274,7 +274,7 @@ public class IgniteCacheOffheapManagerImpl extends GridCacheManagerAdapter imple
 //                return t.get1() != null ? t : null;
 //        }
 
-        assert entry.localPartition() != null : entry;
+        assert cctx.isLocal() || entry.localPartition() != null : entry;
 
         return dataStore(entry.localPartition()).find(key);
     }
