b375faea30a91c59b5ca9b2a54d9793a13e19f81
vdpyatkov <vpyatkov@gridgain.com>
2016-06-15 13:25:11 +0300

    IGNITE-2767 Cont. query remote filter requires to be in client nodes class path

---
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/query/continuous/ContinuousQueryClassesLoading.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.processors.cache.query.continuous;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.testframework.GridStringLogger;
+import org.apache.ignite.testframework.GridTestClassLoader;
+import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
+
+/**
+ *
+ */
+public class ContinuousQueryClassesLoading extends GridCommonAbstractTest {
+
+    private GridStringLogger log;
+    private ClassLoader ldr;
+    private boolean clientMode;
+
+    @Override protected IgniteConfiguration getConfiguration() throws Exception {
+        IgniteConfiguration cfg = super.getConfiguration();
+
+        cfg.setGridLogger(log);
+        cfg.setClassLoader(ldr);
+        cfg.setClientMode(clientMode);
+
+        return cfg;
+    }
+
+    public void testTowServerWithOtherClassLoader() throws Exception {
+        log = new GridStringLogger();
+        ldr = new GridTestClassLoader();
+        clientMode = false;
+        final Ignite ignite0 = startGrid(0);
+
+
+    }
+
+    public void testServerClientWithOtherClassLoader() {
+
+    }
+
+}
