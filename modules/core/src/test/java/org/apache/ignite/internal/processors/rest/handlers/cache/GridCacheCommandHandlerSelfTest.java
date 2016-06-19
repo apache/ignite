3543270900331dd10beee8140dff6bbd4a21f700
samaitra <saikat.maitra@gmail.com>
2016-06-19 20:50:41 +0530

    IGNITE-3337 REST HTTP: metadata command returns all caches even if a cache name is passed to request

--- modules/core/src/test/java/org/apache/ignite/internal/processors/rest/handlers/cache/GridCacheCommandHandlerSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/rest/handlers/cache/GridCacheCommandHandlerSelfTest.java
@@ -46,8 +46,6 @@ import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 
-import static org.mockito.Mockito.when;
-
 /**
  * Tests command handler directly.
  */
