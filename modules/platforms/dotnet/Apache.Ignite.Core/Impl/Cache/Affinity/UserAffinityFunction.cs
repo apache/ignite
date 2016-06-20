c0cb0162052f2d42a154a7fefec7c5ca8adf6b19
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-06-20 13:19:56 +0300

    wip

---
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Cache/Affinity/UserAffinityFunction.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Core.Impl.Cache.Affinity
+{
+    using System.Diagnostics;
+    using Apache.Ignite.Core.Cache.Affinity;
+
+    /// <summary>
+    /// Wraps user-defined Affinity Function and it's handle.
+    /// </summary>
+    internal class UserAffinityFunction : IAffinityFunction
+    {
+        /** User function */
+        private readonly IAffinityFunction _function;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserAffinityFunction"/> class.
+        /// </summary>
+        /// <param name="function">The function.</param>
+        public UserAffinityFunction(IAffinityFunction function)
+        {
+            Debug.Assert(function != null);
+
+            _function = function;
+        }
+
+        /// <summary>
+        /// Gets or sets the handle. 
+        /// </summary>
+        public long Handle { get; set; }
+    }
+}
