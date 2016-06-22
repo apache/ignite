ad662ce9fc61e04413c4e9c0b3562a9a08cc664b
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-06-22 13:31:28 +0300

    AffinityFunctionContext implemented

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Cache/Affinity/AffinityFunctionContext.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Cache/Affinity/AffinityFunctionContext.cs
@@ -18,6 +18,7 @@
 namespace Apache.Ignite.Core.Impl.Cache.Affinity
 {
     using System.Collections.Generic;
+    using Apache.Ignite.Core.Binary;
     using Apache.Ignite.Core.Cache.Affinity;
     using Apache.Ignite.Core.Cluster;
     using Apache.Ignite.Core.Events;
@@ -27,32 +28,68 @@ namespace Apache.Ignite.Core.Impl.Cache.Affinity
     /// </summary>
     internal class AffinityFunctionContext : IAffinityFunctionContext
     {
+        /** */
         private readonly List<List<IClusterNode>> _previousAssignment;
+
+        /** */
         private readonly int _backups;
+
+        /** */
         private readonly ICollection<IClusterNode> _currentTopologySnapshot;
+        
+        /** */
         private readonly AffinityTopologyVersion _currentTopologyVersion;
+        
+        /** */
         private readonly DiscoveryEvent _discoveryEvent;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AffinityFunctionContext"/> class.
+        /// </summary>
+        /// <param name="reader">The reader.</param>
+        public AffinityFunctionContext(IBinaryRawReader reader)
+        {
+            var cnt = reader.ReadInt();
+
+            if (cnt > 0)
+            {
+                _previousAssignment = new List<List<IClusterNode>>(cnt);
+
+                for (int i = 0; i < cnt; i++)
+                    _previousAssignment.Add(IgniteUtils.ReadNodes(reader));
+            }
+
+            _backups = reader.ReadInt();
+            _currentTopologySnapshot = IgniteUtils.ReadNodes(reader);
+            _currentTopologyVersion = new AffinityTopologyVersion(reader.ReadLong(), reader.ReadInt());
+            _discoveryEvent = new DiscoveryEvent(reader);
+        }
+
+        /** <inheritdoc /> */
         public ICollection<IClusterNode> GetPreviousAssignment(int partition)
         {
             return _previousAssignment == null ? null : _previousAssignment[partition];
         }
 
+        /** <inheritdoc /> */
         public int Backups
         {
             get { return _backups; }
         }
 
+        /** <inheritdoc /> */
         public ICollection<IClusterNode> CurrentTopologySnapshot
         {
             get { return _currentTopologySnapshot; }
         }
 
+        /** <inheritdoc /> */
         public AffinityTopologyVersion CurrentTopologyVersion
         {
             get { return _currentTopologyVersion; }
         }
 
+        /** <inheritdoc /> */
         public DiscoveryEvent DiscoveryEvent
         {
             get { return _discoveryEvent; }
