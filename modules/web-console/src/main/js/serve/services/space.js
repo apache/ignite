579a3da66ce18f4466d034576c327c2195e6bcd1
Maxim Afanasiev <mafkus@gmail.com>
2016-06-30 10:28:33 +0700

    IGNITE-3262 Added space service.

---
+++ modules/web-console/src/main/js/serve/services/space.js
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+// Fire me up!
+
+module.exports = {
+    implements: 'services/spaceService',
+    inject: ['mongo']
+};
+
+module.exports.factory = (mongo) => {
+
+    class SpaceService {
+
+        /**
+         * Query for user spaces.
+         *
+         * @param userId User ID.
+         * @param {Boolean} demo Is need use demo space.
+         * @returns {Promise}
+         */
+        spaces(userId, demo) {
+            return mongo.Space.find({owner: userId, demo: !!demo}).lean().exec();
+        }
+
+        /**
+         * Extract IDs from user spaces.
+         *
+         * @param userId User ID.
+         * @param {Boolean} demo Is need use demo space.
+         * @returns {Promise}
+         */
+        spaceIds(userId, demo) {
+            return this.spaces(userId, demo)
+                .then((spaces) => spaces.map((space) => space._id));
+        }
+
+    }
+
+    return SpaceService;
+};
+
