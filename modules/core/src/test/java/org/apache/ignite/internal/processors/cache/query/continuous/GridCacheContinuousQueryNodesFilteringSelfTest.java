85126b3afe10738bdedb740748663085a78faa46
sboikov <sboikov@gridgain.com>
2016-07-12 09:42:16 +0300

    ignite-3413 Minor.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/query/continuous/GridCacheContinuousQueryNodesFilteringSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/query/continuous/GridCacheContinuousQueryNodesFilteringSelfTest.java
@@ -30,9 +30,10 @@ import org.apache.ignite.cache.query.ContinuousQuery;
 import org.apache.ignite.cluster.ClusterNode;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
-import org.apache.ignite.internal.util.typedef.internal.U;
 import org.apache.ignite.lang.IgnitePredicate;
-import org.apache.ignite.spi.IgniteSpiException;
+import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.TcpDiscoveryIpFinder;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 import org.apache.ignite.testframework.GridStringLogger;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 
@@ -40,51 +41,53 @@ import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 @SuppressWarnings("unused")
 public class GridCacheContinuousQueryNodesFilteringSelfTest extends GridCommonAbstractTest implements Serializable {
     /** */
+    private static final TcpDiscoveryIpFinder IP_FINDER = new TcpDiscoveryVmIpFinder(true);
+
+    /** */
     private static final String ENTRY_FILTER_CLS_NAME = "org.apache.ignite.tests.p2p.CacheDeploymentEntryEventFilter";
 
     /**
-     * Test that node not matched by filter really does not try to participate in the query.
+     * Tests that node not matched by filter really does not try to participate in the query.
+     *
      * @throws Exception if failed.
      */
+    @SuppressWarnings("EmptyTryBlock")
     public void testNodeWithoutAttributeExclusion() throws Exception {
         try (Ignite node1 = startNodeWithCache()) {
-            //noinspection EmptyTryBlock
             try (Ignite node2 = startGrid("node2", getConfiguration("node2", false, null))) {
-                // No-op.
+                assertEquals(2, node2.cluster().nodes().size());
             }
         }
     }
 
     /**
      * Test that node matched by filter and having filter instantiation problems fails for sure.
+     *
      * @throws Exception if failed.
      */
     public void testNodeWithAttributeFailure() throws Exception {
         try (Ignite node1 = startNodeWithCache()) {
             GridStringLogger log = new GridStringLogger();
-            //noinspection EmptyTryBlock
+
             try (Ignite node2 = startGrid("node2", getConfiguration("node2", true, log))) {
-                // No-op.
+                fail();
             }
-            catch (IgniteException e) { //This is what we expect...
-                assertTrue(log.toString().contains("TcpDiscoverSpi's message worker thread failed abnormally. " +
-                    "Stopping the node in order to prevent cluster wide instability." + U.nl() +
-                    "class org.apache.ignite.IgniteException: Class not found for continuous query remote filter " +
+            catch (IgniteException e) {
+                assertTrue(log.toString().contains("Class not found for continuous query remote filter " +
                     "[name=org.apache.ignite.tests.p2p.CacheDeploymentEntryEventFilter]"));
-            }
-            catch (Exception e) { // And nothing else.
-                throw new AssertionError("Unexpected exception", e);
             }
         }
     }
 
     /**
      * Start first, attribute-bearing, node, create new cache and launch continuous query on it.
-     * @return node.
+     *
+     * @return Node.
      * @throws Exception if failed.
      */
     private Ignite startNodeWithCache() throws Exception {
         Ignite node1 = startGrid("node1", getConfiguration("node1", true, null));
+
         CacheConfiguration<Integer, Integer> ccfg = new CacheConfiguration<>();
         ccfg.setName("attrsTestCache");
         ccfg.setNodeFilter(new IgnitePredicate<ClusterNode>() {
@@ -93,33 +96,46 @@ public class GridCacheContinuousQueryNodesFilteringSelfTest extends GridCommonAb
                 return "data".equals(node.attribute("node-type"));
             }
         });
+
         IgniteCache<Integer, Integer> cache = node1.createCache(ccfg);
+
         ContinuousQuery<Integer, Integer> qry = new ContinuousQuery<>();
 
         qry.setRemoteFilterFactory(new RemoteFilterFactory());
         qry.setLocalListener(new CacheEntryUpdatedListener<Integer, Integer>() {
             /** {@inheritDoc} */
             @Override public void onUpdated(Iterable<CacheEntryEvent<? extends Integer, ? extends Integer>> evts) {
+                // No-op.
             }
         });
+
         RemoteFilterFactory.clsLdr = getExternalClassLoader();
+
         cache.query(qry);
+
         // Switch class loader before starting the second node.
         RemoteFilterFactory.clsLdr = getClass().getClassLoader();
+
         return node1;
     }
 
     /**
-     * @param name grid name.
-     * @param setAttr flag indicating whether class loader and node user attribute should be tuned.
-     * @return Grid configuration w/specified name and, optionally, external class loader.
+     * @param name Node name.
+     * @param setAttr Flag indicating whether node user attribute should be set.
+     * @param log Logger.
+     * @return Node configuration w/specified name.
      * @throws Exception If failed.
      */
     private IgniteConfiguration getConfiguration(String name, boolean setAttr, GridStringLogger log) throws Exception {
         IgniteConfiguration cfg = optimize(getConfiguration(name));
+
+        ((TcpDiscoverySpi)cfg.getDiscoverySpi()).setIpFinder(IP_FINDER);
+
         if (setAttr)
             cfg.setUserAttributes(Collections.singletonMap("node-type", "data"));
+
         cfg.setGridLogger(log);
+
         return cfg;
     }
 
@@ -131,11 +147,13 @@ public class GridCacheContinuousQueryNodesFilteringSelfTest extends GridCommonAb
         private static ClassLoader clsLdr;
 
         /** {@inheritDoc} */
+        @SuppressWarnings("unchecked")
         @Override public CacheEntryEventFilter<Integer, Integer> create() {
             try {
                 Class<?> filterCls = clsLdr.loadClass(ENTRY_FILTER_CLS_NAME);
+
                 assert CacheEntryEventFilter.class.isAssignableFrom(filterCls);
-                //noinspection unchecked
+
                 return ((Class<CacheEntryEventFilter>)filterCls).newInstance();
             }
             catch (ClassNotFoundException e) {
