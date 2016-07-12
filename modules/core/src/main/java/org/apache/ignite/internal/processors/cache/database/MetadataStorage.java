c3d01f0b48f677a1e776b68409ed9229cf421397
dkarachentsev <dkarachentsev@gridgain.com>
2016-07-12 12:02:18 +0300

    GG-11220 - Cleanup metadata storage when cache/partition is destroyed.
    - Remove TODOs

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/database/MetadataStorage.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/database/MetadataStorage.java
@@ -128,14 +128,14 @@ public class MetadataStorage implements MetaStore {
             if (reuseList == null)
                 pageMem.freePage(cacheId, row.pageId);
             else
-                reuseList.add(new Bag(row.pageId)); // TODO remove when BPlusTree.destroy() will be available.
+                reuseList.add(new Bag(row.pageId));
         }
 
         return row != null ? new RootPage(new FullPageId(row.pageId, cacheId), false) : null;
     }
 
     /**
-     * TODO remove when BPlusTree.destroy() will be available.
+     *
      */
     private static class Bag implements ReuseBag {
         /** */
