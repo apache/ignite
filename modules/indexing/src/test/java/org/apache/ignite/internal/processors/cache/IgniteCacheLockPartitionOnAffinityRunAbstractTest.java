d282ce4471f03db96dd98e7d45cbde3667016bc9
tledkov-gridgain <tledkov@gridgain.com>
2016-07-15 13:17:13 +0300

    IGNITE-2310 Lock cache partition for affinityRun/affinityCall execution - revert compatibility test

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/IgniteCacheLockPartitionOnAffinityRunAbstractTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/IgniteCacheLockPartitionOnAffinityRunAbstractTest.java
@@ -144,6 +144,8 @@ public class IgniteCacheLockPartitionOnAffinityRunAbstractTest extends GridCache
             nodeRestartFut = null;
         }
 
+        Thread.sleep(3_000);
+
         awaitPartitionMapExchange();
 
         super.afterTest();
