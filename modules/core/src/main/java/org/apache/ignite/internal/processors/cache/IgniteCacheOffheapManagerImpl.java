2336229d09000ba5992821d3cff3a606d6a54b71
Alexey Goncharuk <alexey.goncharuk@gmail.com>
2016-07-19 17:16:18 +0300

    GG-11196 - Code review.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/IgniteCacheOffheapManagerImpl.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/IgniteCacheOffheapManagerImpl.java
@@ -94,87 +94,89 @@ public class IgniteCacheOffheapManagerImpl extends GridCacheManagerAdapter imple
 
         indexingEnabled = INDEXING.inClassPath() && GridQueryProcessor.isEnabled(cctx.config());
 
-        int cacheId = cctx.cacheId();
+        if (cctx.affinityNode()) {
+            int cacheId = cctx.cacheId();
 
-        long[] rootIds = null;
+            long[] rootIds = null;
 
-        long metastoreRoot = 0;
+            long metastoreRoot = 0;
 
-        boolean initNew = false;
+            boolean initNew = false;
 
-        if (!cctx.shared().database().persistenceEnabled()) {
-            try (Page metaPage = pageMem.metaPage(cacheId)) {
-                metastoreRoot = metaPage.id();
+            if (!cctx.shared().database().persistenceEnabled()) {
+                try (Page metaPage = pageMem.metaPage(cacheId)) {
+                    metastoreRoot = metaPage.id();
 
-                rootIds = allocateMetas(pageMem, cacheId, true);
+                    rootIds = allocateMetas(pageMem, cacheId, true);
 
-                initNew = true;
+                    initNew = true;
+                }
             }
-        }
-        else {
-            try (Page meta = pageMem.metaPage(cacheId)) {
-                boolean initialized = false;
+            else {
+                try (Page meta = pageMem.metaPage(cacheId)) {
+                    boolean initialized = false;
 
-                final ByteBuffer buf = meta.getForWrite();
+                    final ByteBuffer buf = meta.getForWrite();
 
-                try {
-                    final int pagesNum = buf.getInt();
+                    try {
+                        final int pagesNum = buf.getInt();
 
-                    if (pagesNum > 0) {
-                        initialized = true;
+                        if (pagesNum > 0) {
+                            initialized = true;
 
-                        assert pagesNum > 2 : "Must be at least 2 pages for reuse list";
+                            assert pagesNum > 2 : "Must be at least 2 pages for reuse list";
 
-                        metastoreRoot = buf.getLong();
+                            metastoreRoot = buf.getLong();
 
-                        rootIds = new long[pagesNum - 1];
+                            rootIds = new long[pagesNum - 1];
 
-                        for (int i = 0; i < rootIds.length; i++) {
-                            assert buf.remaining() >= 8 : "Meta page is corrupted";
+                            for (int i = 0; i < rootIds.length; i++) {
+                                assert buf.remaining() >= 8 : "Meta page is corrupted";
 
-                            rootIds[i] = buf.getLong();
+                                rootIds[i] = buf.getLong();
+                            }
+
                         }
 
+                        if (!initialized) {
+                            metastoreRoot = pageMem.allocatePage(cacheId, 0, PageMemory.FLAG_IDX);
+
+                            rootIds = allocateMetas(pageMem, cacheId, true);
+
+                            buf.rewind();
+
+                            buf.putInt(rootIds.length + 1);
+                            buf.putLong(metastoreRoot);
+
+                            for (final long rootId : rootIds)
+                                buf.putLong(rootId);
+
+                            initNew = true;
+                        }
                     }
-
-                    if (!initialized) {
-                        metastoreRoot = pageMem.allocatePage(cacheId, 0, PageMemory.FLAG_IDX);
-
-                        rootIds = allocateMetas(pageMem, cacheId, true);
-
-                        buf.rewind();
-
-                        buf.putInt(rootIds.length + 1);
-                        buf.putLong(metastoreRoot);
-
-                        for (final long rootId : rootIds)
-                            buf.putLong(rootId);
-
-                        initNew = true;
+                    finally {
+                        meta.releaseWrite(!initialized);
                     }
                 }
-                finally {
-                    meta.releaseWrite(!initialized);
+            }
+
+            cctx.shared().database().checkpointReadLock();
+
+            try {
+                reuseList = new ReuseList(cacheId, pageMem, cctx.shared().wal(), rootIds, initNew);
+                freeList = new FreeList(cctx, reuseList);
+
+                metaStore = new MetadataStorage(pageMem, cctx.shared().wal(), cacheId, reuseList, metastoreRoot, initNew);
+
+                if (cctx.affinityNode() && cctx.isLocal()) {
+                    assert cctx.cache() instanceof GridLocalCache : cctx.cache();
+
+                    locCacheDataStore = createCacheDataStore(0, (CacheDataStore.Listener)cctx.cache());
                 }
             }
-        }
-
-        cctx.shared().database().checkpointReadLock();
-
-        try {
-            reuseList = new ReuseList(cacheId, pageMem, cctx.shared().wal(), rootIds, initNew);
-            freeList = new FreeList(cctx, reuseList);
-
-            metaStore = new MetadataStorage(pageMem, cctx.shared().wal(), cacheId, reuseList, metastoreRoot, initNew);
-
-            if (cctx.affinityNode() && cctx.isLocal()) {
-                assert cctx.cache() instanceof GridLocalCache : cctx.cache();
-
-                locCacheDataStore = createCacheDataStore(0, (CacheDataStore.Listener)cctx.cache());
+            finally {
+                cctx.shared().database().checkpointReadUnlock();
             }
-        }
-        finally {
-            cctx.shared().database().checkpointReadUnlock();
         }
     }
 
