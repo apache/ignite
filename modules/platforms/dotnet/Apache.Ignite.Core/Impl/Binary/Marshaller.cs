3a7f35ea56a320cccfd275cafdef557764c59d14
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-07-21 17:36:25 +0300

    wip

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Binary/Marshaller.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Binary/Marshaller.cs
@@ -387,11 +387,11 @@ namespace Apache.Ignite.Core.Impl.Binary
         }
 
         /// <summary>
-        /// 
+        /// Gets descriptor for a type id.
         /// </summary>
-        /// <param name="userType"></param>
-        /// <param name="typeId"></param>
-        /// <returns></returns>
+        /// <param name="userType">User type flag.</param>
+        /// <param name="typeId">Type id.</param>
+        /// <returns>Descriptor.</returns>
         public IBinaryTypeDescriptor GetDescriptor(bool userType, int typeId)
         {
             IBinaryTypeDescriptor desc;
