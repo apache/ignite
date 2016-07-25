5703e255af92892de52c6d1bfcf4f3eb69e91873
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-07-25 19:20:47 +0300

    wip

--- modules/core/src/main/java/org/apache/ignite/internal/processors/platform/PlatformAbstractBootstrap.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/platform/PlatformAbstractBootstrap.java
@@ -40,7 +40,7 @@ public abstract class PlatformAbstractBootstrap implements PlatformBootstrap {
         IgniteConfiguration cfg0 = closure(envPtr).apply(cfg);
 
         // Reset work dir that was set by a temporary marshaller
-        // So that use setting takes effect
+        // So that user setting takes effect
         U.nullifyWorkDirectory();
 
         try {
