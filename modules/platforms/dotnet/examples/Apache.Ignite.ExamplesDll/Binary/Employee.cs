861c1736facdf76c71e93ddada0d8e15b5b2950c
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-08-11 17:32:13 +0300

    IGNITE-3673 .NET: Add examples for distributed joins.

--- modules/platforms/dotnet/examples/Apache.Ignite.ExamplesDll/Binary/Employee.cs
+++ modules/platforms/dotnet/examples/Apache.Ignite.ExamplesDll/Binary/Employee.cs
@@ -35,12 +35,14 @@ namespace Apache.Ignite.ExamplesDll.Binary
         /// <param name="salary">Salary.</param>
         /// <param name="address">Address.</param>
         /// <param name="departments">Departments.</param>
-        public Employee(string name, long salary, Address address, ICollection<string> departments)
+        public Employee(string name, long salary, Address address, ICollection<string> departments, 
+            int organizationId = 0)
         {
             Name = name;
             Salary = salary;
             Address = address;
             Departments = departments;
+            OrganizationId = organizationId;
         }
 
         /// <summary>
@@ -48,6 +50,12 @@ namespace Apache.Ignite.ExamplesDll.Binary
         /// </summary>
         [QuerySqlField]
         public string Name { get; set; }
+
+        /// <summary>
+        /// Organization id.
+        /// </summary>
+        [QuerySqlField(IsIndexed = true)]
+        public int OrganizationId { get; set; }
 
         /// <summary>
         /// Salary.
