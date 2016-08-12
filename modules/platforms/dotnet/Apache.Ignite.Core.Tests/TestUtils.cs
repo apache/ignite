9ecc216d3a48f924e31ac74ecf7e1f08e3cb98f6
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-08-12 15:09:01 +0300

    Add EXAMPLES_TEST category

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/TestUtils.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/TestUtils.cs
@@ -23,7 +23,6 @@ namespace Apache.Ignite.Core.Tests
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading;
-    using Apache.Ignite.Core.Discovery;
     using Apache.Ignite.Core.Discovery.Tcp;
     using Apache.Ignite.Core.Discovery.Tcp.Static;
     using Apache.Ignite.Core.Impl;
@@ -38,6 +37,9 @@ namespace Apache.Ignite.Core.Tests
     {
         /** Indicates long running and/or memory/cpu intensive test. */
         public const string CategoryIntensive = "LONG_TEST";
+
+        /** Indicates examples tests. */
+        public const string CategoryExamples = "EXAMPLES_TEST";
 
         /** */
         public const int DfltBusywaitSleepInterval = 200;
