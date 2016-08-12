497f05064cc4d297af63c4073c0ec0ba08a67d69
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-08-12 17:23:15 +0300

    ReadOnlyDictionaryTest added

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Collections/ReadOnlyDictionaryTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Collections/ReadOnlyDictionaryTest.cs
@@ -17,13 +17,54 @@
 
 namespace Apache.Ignite.Core.Tests.Collections
 {
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
     using Apache.Ignite.Core.Impl.Collections;
+    using NUnit.Framework;
 
     /// <summary>
     /// Tests for <see cref="ReadOnlyDictionary{TKey,TValue}"/>
     /// </summary>
     public class ReadOnlyDictionaryTest
     {
-        // TODO
+        /// <summary>
+        /// Tests the disctionary.
+        /// </summary>
+        [Test]
+        public void TestDictionary()
+        {
+            // Default ctor.
+            var data = Enumerable.Range(1, 5).ToDictionary(x => x, x => x.ToString());
+            var dict = new ReadOnlyDictionary<int, string>(data);
+
+            Assert.AreEqual(5, dict.Count);
+            Assert.IsTrue(dict.IsReadOnly);
+            CollectionAssert.AreEqual(data, dict);
+            CollectionAssert.AreEqual(data.Keys, dict.Keys);
+            CollectionAssert.AreEqual(data.Values, dict.Values);
+
+            Assert.IsTrue(dict.GetEnumerator().MoveNext());
+            Assert.IsTrue(((IEnumerable) dict).GetEnumerator().MoveNext());
+
+            Assert.IsTrue(dict.ContainsKey(1));
+            Assert.IsTrue(dict.Contains(new KeyValuePair<int, string>(4, "4")));
+            Assert.AreEqual("3", dict[3]);
+
+            string val;
+            Assert.IsTrue(dict.TryGetValue(2, out val));
+            Assert.AreEqual("2", val);
+
+            var arr = new KeyValuePair<int, string>[5];
+            dict.CopyTo(arr, 0);
+            CollectionAssert.AreEqual(data, arr);
+
+            Assert.Throws<NotSupportedException>(() => dict.Add(1, "2"));
+            Assert.Throws<NotSupportedException>(() => dict.Add(new KeyValuePair<int, string>(1, "2")));
+            Assert.Throws<NotSupportedException>(() => dict.Clear());
+            Assert.Throws<NotSupportedException>(() => dict.Remove(1));
+            Assert.Throws<NotSupportedException>(() => dict.Remove(new KeyValuePair<int, string>(1, "2")));
+        }
     }
 }
