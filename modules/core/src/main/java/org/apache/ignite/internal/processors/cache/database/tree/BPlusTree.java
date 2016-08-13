884496fe09ad44c44edf132c10ac2c467af30678
Sergi Vladykin <sergi.vladykin@gmail.com>
2016-08-13 13:25:39 +0300

    Merge branch 'ignite-db-x' of github.com:ggprivate/ggprivate into ignite-gg-11276
    # Conflicts:
    #	modules/core/src/test/java/org/gridgain/testsuites/GridDbTestSuite.java

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/database/tree/BPlusTree.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/database/tree/BPlusTree.java
@@ -101,7 +101,7 @@ public abstract class BPlusTree<L, T extends L> {
     private final IgniteWriteAheadLogManager wal;
 
     /** */
-    private final PageMemory pageMem;
+    protected final PageMemory pageMem;
 
     /** */
     private final ReuseList reuseList;
