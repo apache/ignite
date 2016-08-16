c3442a30ea729add00b8782932ebee0b03107de2
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-08-16 16:19:49 +0300

    wip

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Binary/BinaryReaderWriterTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Binary/BinaryReaderWriterTest.cs
@@ -40,69 +40,103 @@ namespace Apache.Ignite.Core.Tests.Binary
 
         private class ReadWriteAll : IBinarizable
         {
+            private static readonly DateTime Date = DateTime.UtcNow;
+
+            private static readonly Guid Guid = Guid.NewGuid();
+
             public void WriteBinary(IBinaryWriter writer)
             {
                 writer.WriteByte("Byte", 1);
-                writer.WriteByteArray("ByteArray", new byte[] { 1});
+                writer.WriteByteArray("ByteArray", new byte[] {1});
                 writer.WriteChar("Char", '1');
-                writer.WriteCharArray("CharArray", new[] { '1' });
+                writer.WriteCharArray("CharArray", new[] {'1'});
                 writer.WriteShort("Short", 1);
                 writer.WriteShortArray("ShortArray", new short[] {1});
                 writer.WriteInt("Int", 1);
                 writer.WriteIntArray("IntArray", new[] {1});
                 writer.WriteLong("Long", 1);
-                writer.WriteLongArray("LongArray", new long[] { 1 });
+                writer.WriteLongArray("LongArray", new long[] {1});
                 writer.WriteBoolean("Boolean", true);
-                writer.WriteBooleanArray("BooleanArray", new [] {true});
+                writer.WriteBooleanArray("BooleanArray", new[] {true});
                 writer.WriteFloat("Float", 1);
                 writer.WriteFloatArray("FloatArray", new float[] {1});
                 writer.WriteDouble("Double", 1);
                 writer.WriteDoubleArray("DoubleArray", new double[] {1});
                 writer.WriteDecimal("Decimal", 1);
                 writer.WriteDecimalArray("DecimalArray", new decimal?[] {1});
-                writer.WriteTimestamp("Timestamp", DateTime.UtcNow);
-                writer.WriteTimestampArray("TimestampArray", new DateTime?[] {DateTime.UtcNow});
+                writer.WriteTimestamp("Timestamp", Date);
+                writer.WriteTimestampArray("TimestampArray", new DateTime?[] {Date});
                 writer.WriteString("String", "1");
                 writer.WriteStringArray("StringArray", new[] {"1"});
-                writer.WriteGuid("Guid", Guid.Empty);
-                writer.WriteGuidArray("GuidArray", new Guid?[] {Guid.Empty});
-                writer.WriteEnum("Enum", 1);
-                writer.WriteEnumArray("EnumArray", new[] {1});
+                writer.WriteGuid("Guid", Guid);
+                writer.WriteGuidArray("GuidArray", new Guid?[] {Guid});
+                writer.WriteEnum("Enum", MyEnum.Bar);
+                writer.WriteEnumArray("EnumArray", new[] {MyEnum.Bar});
 
                 var raw = writer.GetRawWriter();
 
                 raw.WriteByte(1);
-                raw.WriteByteArray(new byte[] { 1 });
+                raw.WriteByteArray(new byte[] {1});
                 raw.WriteChar('1');
-                raw.WriteCharArray(new[] { '1' });
+                raw.WriteCharArray(new[] {'1'});
                 raw.WriteShort(1);
-                raw.WriteShortArray(new short[] { 1 });
+                raw.WriteShortArray(new short[] {1});
                 raw.WriteInt(1);
-                raw.WriteIntArray(new[] { 1 });
+                raw.WriteIntArray(new[] {1});
                 raw.WriteLong(1);
-                raw.WriteLongArray(new long[] { 1 });
+                raw.WriteLongArray(new long[] {1});
                 raw.WriteBoolean(true);
-                raw.WriteBooleanArray(new[] { true });
+                raw.WriteBooleanArray(new[] {true});
                 raw.WriteFloat(1);
-                raw.WriteFloatArray(new float[] { 1 });
+                raw.WriteFloatArray(new float[] {1});
                 raw.WriteDouble(1);
-                raw.WriteDoubleArray(new double[] { 1 });
+                raw.WriteDoubleArray(new double[] {1});
                 raw.WriteDecimal(1);
-                raw.WriteDecimalArray(new decimal?[] { 1 });
-                raw.WriteTimestamp(DateTime.UtcNow);
-                raw.WriteTimestampArray(new DateTime?[] { DateTime.UtcNow });
+                raw.WriteDecimalArray(new decimal?[] {1});
+                raw.WriteTimestamp(Date);
+                raw.WriteTimestampArray(new DateTime?[] {Date});
                 raw.WriteString("1");
-                raw.WriteStringArray(new[] { "1" });
-                raw.WriteGuid(Guid.Empty);
-                raw.WriteGuidArray(new Guid?[] { Guid.Empty });
-                raw.WriteEnum(1);
-                raw.WriteEnumArray(new[] { 1 });
+                raw.WriteStringArray(new[] {"1"});
+                raw.WriteGuid(Guid);
+                raw.WriteGuidArray(new Guid?[] {Guid});
+                raw.WriteEnum(MyEnum.Bar);
+                raw.WriteEnumArray(new[] {MyEnum.Bar});
             }
 
             public void ReadBinary(IBinaryReader reader)
             {
-                Assert.AreEqual(1, reader.ReadInt("int"));
+                Assert.AreEqual(1, reader.ReadByte("Byte"));
+                Assert.AreEqual(new byte[] {1}, reader.ReadByteArray("ByteArray"));
+                Assert.AreEqual('1', reader.ReadChar("Char"));
+                Assert.AreEqual(new[] {'1'}, reader.ReadCharArray("CharArray"));
+                Assert.AreEqual(1, reader.ReadShort("Short"));
+                Assert.AreEqual(new short[] {1}, reader.ReadShortArray("ShortArray"));
+                Assert.AreEqual(1, reader.ReadInt("Int"));
+                Assert.AreEqual(new[] {1}, reader.ReadIntArray("IntArray"));
+                Assert.AreEqual(1, reader.ReadLong("Long"));
+                Assert.AreEqual(new long[] {1}, reader.ReadLongArray("LongArray"));
+                Assert.AreEqual(true, reader.ReadBoolean("Boolean"));
+                Assert.AreEqual(new[] {true}, reader.ReadBooleanArray("BooleanArray"));
+                Assert.AreEqual(1, reader.ReadFloat("Float"));
+                Assert.AreEqual(new float[] {1}, reader.ReadFloatArray("FloatArray"));
+                Assert.AreEqual(1, reader.ReadDouble("Double"));
+                Assert.AreEqual(new double[] {1}, reader.ReadDoubleArray("DoubleArray"));
+                Assert.AreEqual(1, reader.ReadDecimal("Decimal"));
+                Assert.AreEqual(new decimal?[] {1}, reader.ReadDecimalArray("DecimalArray"));
+                Assert.AreEqual(Date, reader.ReadTimestamp("Timestamp"));
+                Assert.AreEqual(new DateTime?[] {Date}, reader.ReadTimestampArray("TimestampArray"));
+                Assert.AreEqual("1", reader.ReadString("String"));
+                Assert.AreEqual(new[] {"1"}, reader.ReadStringArray("StringArray"));
+                Assert.AreEqual(Guid, reader.ReadGuid("Guid"));
+                Assert.AreEqual(new Guid?[] {Guid}, reader.ReadGuidArray("GuidArray"));
+                Assert.AreEqual(MyEnum.Bar, reader.ReadEnum<MyEnum>("Enum"));
+                Assert.AreEqual(new[] {MyEnum.Bar}, reader.ReadEnumArray<MyEnum>("EnumArray"));
             }
+        }
+
+        private enum MyEnum
+        {
+            Bar
         }
     }
 }
