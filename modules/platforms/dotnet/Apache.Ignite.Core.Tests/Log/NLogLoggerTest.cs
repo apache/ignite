61df358e9e5e0d8d257ff075d7aadffa35231851
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-08-17 12:05:03 +0300

    Adding tests

---
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Log/NLogLoggerTest.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Core.Tests.Log
+{
+    using Apache.Ignite.Core.Log;
+    using Apache.Ignite.NLog;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests the NLog integration.
+    /// </summary>
+    public class NLogLoggerTest
+    {
+        /// <summary>
+        /// Tests the log level conversion.
+        /// </summary>
+        [Test]
+        public void TestLogLevelConversion()
+        {
+            var levels = new[] { LogLevel.Trace, LogLevel.Info, LogLevel.Debug, LogLevel.Warn, LogLevel.Error };
+
+            foreach (var igniteLevel in levels)
+            {
+                var nlogLevel = IgniteNLogLogger.ConvertLogLevel(igniteLevel);
+
+                Assert.AreEqual(igniteLevel.ToString(), nlogLevel.ToString());
+            }
+        }
+    }
+}
