4e460c6bb8d71d03de03c26791c1c7e48454d113
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-08-25 12:15:04 +0300

    wip

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/EntityFramework/EntityFrameworkSecondLevelCacheTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/EntityFramework/EntityFrameworkSecondLevelCacheTest.cs
@@ -17,11 +17,47 @@
 
 namespace Apache.Ignite.Core.Tests.EntityFramework
 {
+    using System.Collections.Generic;
+    using System.Data.Common;
+    using System.Data.Entity;
+
     /// <summary>
     /// Integration test with in-memory database.
     /// </summary>
     public class EntityFrameworkSecondLevelCacheTest
     {
         // https://www.stevefenton.co.uk/2015/11/using-an-in-memory-database-as-a-test-double-with-entity-framework/
+
+        public class BloggingContext : DbContext
+        {
+            internal BloggingContext(DbConnection connection)
+                : base(connection, true)
+            {
+                // No-op.
+            }
+
+
+            public virtual DbSet<Blog> Blogs { get; set; }
+            public virtual DbSet<Post> Posts { get; set; }
+        }
+
+        public class Blog
+        {
+            public int BlogId { get; set; }
+            public string Name { get; set; }
+            public string Url { get; set; }
+
+            public virtual List<Post> Posts { get; set; }
+        }
+
+        public class Post
+        {
+            public int PostId { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+
+            public int BlogId { get; set; }
+            public virtual Blog Blog { get; set; }
+        }
     }
 }
