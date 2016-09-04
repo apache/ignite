d06eaa2344a753e08d1e3cb00e6b4ab83c6a9a01
isapego <isapego@gridgain.com>
2016-09-04 16:52:04 +0300

    IGNITE-3801: ODBC: Added tests for OUTER JOIN. This closes #1027.

--- modules/platforms/cpp/odbc/src/config/connection_info.cpp
+++ modules/platforms/cpp/odbc/src/config/connection_info.cpp
@@ -298,9 +298,7 @@ namespace ignite
 #ifdef SQL_OJ_CAPABILITIES
                 // Bitmask enumerating the types of outer joins supported by the 
                 // driver and data source.
-                intParams[SQL_OJ_CAPABILITIES] = SQL_OJ_LEFT | SQL_OJ_RIGHT |
-                    SQL_OJ_FULL | SQL_OJ_NESTED | SQL_OJ_INNER |
-                    SQL_OJ_ALL_COMPARISON_OPS;
+                intParams[SQL_OJ_CAPABILITIES] = SQL_OJ_LEFT | SQL_OJ_NOT_ORDERED | SQL_OJ_ALL_COMPARISON_OPS;
 #endif // SQL_OJ_CAPABILITIES
 
 #ifdef SQL_POS_OPERATIONS
