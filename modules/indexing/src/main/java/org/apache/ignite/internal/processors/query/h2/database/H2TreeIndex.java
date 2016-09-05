f4d5c1946e0aa9805ce1d4c6183329c98a5ca7d2
Alexey Goncharuk <alexey.goncharuk@gmail.com>
2016-09-05 11:13:58 +0300

    GG-11478 - Fixed memory leak.

--- modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/database/H2TreeIndex.java
+++ modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/database/H2TreeIndex.java
@@ -196,6 +196,8 @@ public class H2TreeIndex extends GridH2IndexBase {
         catch (IgniteCheckedException e) {
             throw new IgniteException(e);
         }
+
+        super.destroy();
     }
 
     /** {@inheritDoc} */
