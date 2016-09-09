59527c5649afd3576d944d42299cf49416972562
vozerov-gridgain <vozerov@gridgain.com>
2016-09-09 12:27:55 +0300

    IGNITE-2649: Ensured correct local Ignite instance processing during serialization and deserialization.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheAdapter.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheAdapter.java
@@ -4639,7 +4639,7 @@ public abstract class GridCacheAdapter<K, V> implements IgniteInternalCache<K, V
         try {
             IgniteBiTuple<String, String> t = stash.get();
 
-            return IgnitionEx.gridx(t.get1()).cachex(t.get2());
+            return IgnitionEx.localIgnite().cachex(t.get2());
         }
         catch (IllegalStateException e) {
             throw U.withCause(new InvalidObjectException(e.getMessage()), e);
