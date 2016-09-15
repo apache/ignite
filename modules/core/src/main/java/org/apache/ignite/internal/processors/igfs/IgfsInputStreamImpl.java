ba744f3f4e1c2e8db610d815af7d7a83bdc6a409
tledkov-gridgain <tledkov@gridgain.com>
2016-09-15 16:50:41 +0500

    IGNITE-3859 IGFS: Support direct PROXY mode invocation in method: open

--- modules/core/src/main/java/org/apache/ignite/internal/processors/igfs/IgfsInputStreamImpl.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/igfs/IgfsInputStreamImpl.java
@@ -18,96 +18,28 @@
 package org.apache.ignite.internal.processors.igfs;
 
 import org.apache.ignite.IgniteCheckedException;
-import org.apache.ignite.IgniteLogger;
-import org.apache.ignite.events.IgfsEvent;
 import org.apache.ignite.igfs.IgfsCorruptedFileException;
-import org.apache.ignite.igfs.IgfsInputStream;
 import org.apache.ignite.igfs.IgfsPath;
 import org.apache.ignite.igfs.IgfsPathNotFoundException;
 import org.apache.ignite.igfs.secondary.IgfsSecondaryFileSystemPositionedReadable;
 import org.apache.ignite.internal.IgniteInternalFuture;
-import org.apache.ignite.internal.managers.eventstorage.GridEventStorageManager;
-import org.apache.ignite.internal.util.GridConcurrentHashSet;
 import org.apache.ignite.internal.util.typedef.internal.S;
-import org.apache.ignite.internal.util.typedef.internal.U;
-import org.apache.ignite.lang.IgniteInClosure;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.EOFException;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
-import static org.apache.ignite.events.EventType.EVT_IGFS_FILE_CLOSED_READ;
 
 /**
  * Input stream to read data from grid cache with separate blocks.
  */
-public class IgfsInputStreamImpl extends IgfsInputStream implements IgfsSecondaryFileSystemPositionedReadable {
-    /** Empty chunks result. */
-    private static final byte[][] EMPTY_CHUNKS = new byte[0][];
+public class IgfsInputStreamImpl extends IgfsAbstractInputStream {
+    /** Meta manager. */
+    private final IgfsMetaManager meta;
 
-    /** IGFS context. */
-    private final IgfsContext igfsCtx;
-
-    /** Secondary file system reader. */
-    @SuppressWarnings("FieldAccessedSynchronizedAndUnsynchronized")
-    private final IgfsSecondaryFileSystemPositionedReadable secReader;
-
-    /** Logger. */
-    private IgniteLogger log;
-
-    /** Path to file. */
-    protected final IgfsPath path;
+    /** Data manager. */
+    private final IgfsDataManager data;
 
     /** File descriptor. */
     private volatile IgfsEntryInfo fileInfo;
-
-    /** The number of already read bytes. Important! Access to the property is guarded by this object lock. */
-    private long pos;
-
-    /** Local cache. */
-    private final Map<Long, IgniteInternalFuture<byte[]>> locCache;
-
-    /** Maximum local cache size. */
-    private final int maxLocCacheSize;
-
-    /** Pending data read futures which were evicted from the local cache before completion. */
-    private final Set<IgniteInternalFuture<byte[]>> pendingFuts;
-
-    /** Pending futures lock. */
-    private final Lock pendingFutsLock = new ReentrantLock();
-
-    /** Pending futures condition. */
-    private final Condition pendingFutsCond = pendingFutsLock.newCondition();
-
-    /** Closed flag. */
-    private boolean closed;
-
-    /** Number of blocks to prefetch asynchronously. */
-    private int prefetchBlocks;
-
-    /** Numbed of blocks that must be read sequentially before prefetch is triggered. */
-    private int seqReadsBeforePrefetch;
-
-    /** Bytes read. */
-    private long bytes;
-
-    /** Index of the previously read block. Initially it is set to -1 indicating that no reads has been made so far. */
-    private long prevBlockIdx = -1;
-
-    /** Amount of sequential reads performed. */
-    private int seqReads;
-
-    /** Time consumed on reading. */
-    private long time;
 
     /**
      * Constructs file output stream.
@@ -121,35 +53,13 @@ public class IgfsInputStreamImpl extends IgfsInputStream implements IgfsSecondar
      */
     IgfsInputStreamImpl(IgfsContext igfsCtx, IgfsPath path, IgfsEntryInfo fileInfo, int prefetchBlocks,
         int seqReadsBeforePrefetch, @Nullable IgfsSecondaryFileSystemPositionedReadable secReader) {
-        assert igfsCtx != null;
-        assert path != null;
+        super(igfsCtx, path, prefetchBlocks, seqReadsBeforePrefetch, secReader);
+
         assert fileInfo != null;
 
-        this.igfsCtx = igfsCtx;
-        this.path = path;
         this.fileInfo = fileInfo;
-        this.prefetchBlocks = prefetchBlocks;
-        this.seqReadsBeforePrefetch = seqReadsBeforePrefetch;
-        this.secReader = secReader;
-
-        log = igfsCtx.kernalContext().log(IgfsInputStream.class);
-
-        maxLocCacheSize = (prefetchBlocks > 0 ? prefetchBlocks : 1) * 3 / 2;
-
-        locCache = new LinkedHashMap<>(maxLocCacheSize, 1.0f);
-
-        pendingFuts = new GridConcurrentHashSet<>(prefetchBlocks > 0 ? prefetchBlocks : 1);
-
-        igfsCtx.metrics().incrementFilesOpenedForRead();
-    }
-
-    /**
-     * Gets bytes read.
-     *
-     * @return Bytes read.
-     */
-    public synchronized long bytes() {
-        return bytes;
+        meta = igfsCtx.meta();
+        data = igfsCtx.data();
     }
 
     /** {@inheritDoc} */
@@ -158,250 +68,17 @@ public class IgfsInputStreamImpl extends IgfsInputStream implements IgfsSecondar
     }
 
     /** {@inheritDoc} */
-    @Override public synchronized int read() throws IOException {
-        byte[] buf = new byte[1];
-
-        int read = read(buf, 0, 1);
-
-        if (read == -1)
-            return -1; // EOF.
-
-        return buf[0] & 0xFF; // Cast to int and cut to *unsigned* byte value.
+    @Override protected int blockSize() {
+        return fileInfo.blockSize();
     }
 
     /** {@inheritDoc} */
-    @Override public synchronized int read(@NotNull byte[] b, int off, int len) throws IOException {
-        int read = readFromStore(pos, b, off, len);
-
-        if (read != -1)
-            pos += read;
-
-        return read;
+    @Override protected long blocksCount() {
+        return fileInfo.blocksCount();
     }
 
     /** {@inheritDoc} */
-    @Override public synchronized void seek(long pos) throws IOException {
-        if (pos < 0)
-            throw new IOException("Seek position cannot be negative: " + pos);
-
-
-        this.pos = pos;
-    }
-
-    /** {@inheritDoc} */
-    @Override public synchronized long position() throws IOException {
-        return pos;
-    }
-
-    /** {@inheritDoc} */
-    @Override public synchronized int available() throws IOException {
-        long l = fileInfo.length() - pos;
-
-        if (l < 0)
-            return 0;
-
-        if (l > Integer.MAX_VALUE)
-            return Integer.MAX_VALUE;
-
-        return (int)l;
-    }
-
-    /** {@inheritDoc} */
-    @Override public synchronized void readFully(long pos, byte[] buf) throws IOException {
-        readFully(pos, buf, 0, buf.length);
-    }
-
-    /** {@inheritDoc} */
-    @Override public synchronized void readFully(long pos, byte[] buf, int off, int len) throws IOException {
-        for (int readBytes = 0; readBytes < len; ) {
-            int read = readFromStore(pos + readBytes, buf, off + readBytes, len - readBytes);
-
-            if (read == -1)
-                throw new EOFException("Failed to read stream fully (stream ends unexpectedly)" +
-                    "[pos=" + pos + ", buf.length=" + buf.length + ", off=" + off + ", len=" + len + ']');
-
-            readBytes += read;
-        }
-    }
-
-    /** {@inheritDoc} */
-    @Override public synchronized int read(long pos, byte[] buf, int off, int len) throws IOException {
-        return readFromStore(pos, buf, off, len);
-    }
-
-    /**
-     * Reads bytes from given position.
-     *
-     * @param pos Position to read from.
-     * @param len Number of bytes to read.
-     * @return Array of chunks with respect to chunk file representation.
-     * @throws IOException If read failed.
-     */
-    @SuppressWarnings("IfMayBeConditional")
-    public synchronized byte[][] readChunks(long pos, int len) throws IOException {
-        // Readable bytes in the file, starting from the specified position.
-        long readable = fileInfo.length() - pos;
-
-        if (readable <= 0)
-            return EMPTY_CHUNKS;
-
-        long startTime = System.nanoTime();
-
-        if (readable < len)
-            len = (int)readable; // Truncate expected length to available.
-
-        assert len > 0;
-
-        bytes += len;
-
-        int start = (int)(pos / fileInfo.blockSize());
-        int end = (int)((pos + len - 1) / fileInfo.blockSize());
-
-        int chunkCnt = end - start + 1;
-
-        byte[][] chunks = new byte[chunkCnt][];
-
-        for (int i = 0; i < chunkCnt; i++) {
-            byte[] block = blockFragmentizerSafe(start + i);
-
-            int blockOff = (int)(pos % fileInfo.blockSize());
-            int blockLen = Math.min(len, block.length - blockOff);
-
-            // If whole block can be used as result, do not do array copy.
-            if (blockLen == block.length)
-                chunks[i] = block;
-            else {
-                // Only first or last block can have non-full data.
-                assert i == 0 || i == chunkCnt - 1;
-
-                chunks[i] = Arrays.copyOfRange(block, blockOff, blockOff + blockLen);
-            }
-
-            len -= blockLen;
-            pos += blockLen;
-        }
-
-        assert len == 0;
-
-        time += System.nanoTime() - startTime;
-
-        return chunks;
-    }
-
-    /** {@inheritDoc} */
-    @Override public synchronized void close() throws IOException {
-        if (!closed) {
-            try {
-                if (secReader != null) {
-                    // Close secondary input stream.
-                    secReader.close();
-
-                    // Ensuring local cache futures completion.
-                    for (IgniteInternalFuture<byte[]> fut : locCache.values()) {
-                        try {
-                            fut.get();
-                        }
-                        catch (IgniteCheckedException ignore) {
-                            // No-op.
-                        }
-                    }
-
-                    // Ensuring pending evicted futures completion.
-                    while (!pendingFuts.isEmpty()) {
-                        pendingFutsLock.lock();
-
-                        try {
-                            pendingFutsCond.await(100, TimeUnit.MILLISECONDS);
-                        }
-                        catch (InterruptedException ignore) {
-                            // No-op.
-                        }
-                        finally {
-                            pendingFutsLock.unlock();
-                        }
-                    }
-                }
-            }
-            catch (Exception e) {
-                throw new IOException("File to close the file: " + path, e);
-            }
-            finally {
-                closed = true;
-
-                IgfsLocalMetrics metrics = igfsCtx.metrics();
-
-                metrics.addReadBytesTime(bytes, time);
-                metrics.decrementFilesOpenedForRead();
-
-                locCache.clear();
-
-                GridEventStorageManager evts = igfsCtx.kernalContext().event();
-
-                if (evts.isRecordable(EVT_IGFS_FILE_CLOSED_READ))
-                    evts.record(new IgfsEvent(path, igfsCtx.localNode(), EVT_IGFS_FILE_CLOSED_READ, bytes()));
-            }
-        }
-    }
-
-    /**
-     * @param pos Position to start reading from.
-     * @param buf Data buffer to save read data to.
-     * @param off Offset in the buffer to write data from.
-     * @param len Length of the data to read from the stream.
-     * @return Number of actually read bytes.
-     * @throws IOException In case of any IO exception.
-     */
-    private int readFromStore(long pos, byte[] buf, int off, int len) throws IOException {
-        if (pos < 0)
-            throw new IllegalArgumentException("Read position cannot be negative: " + pos);
-
-        if (buf == null)
-            throw new NullPointerException("Destination buffer cannot be null.");
-
-        if (off < 0 || len < 0 || buf.length < len + off)
-            throw new IndexOutOfBoundsException("Invalid buffer boundaries " +
-                "[buf.length=" + buf.length + ", off=" + off + ", len=" + len + ']');
-
-        if (len == 0)
-            return 0; // Fully read done: read zero bytes correctly.
-
-        // Readable bytes in the file, starting from the specified position.
-        long readable = fileInfo.length() - pos;
-
-        if (readable <= 0)
-            return -1; // EOF.
-
-        long startTime = System.nanoTime();
-
-        if (readable < len)
-            len = (int)readable; // Truncate expected length to available.
-
-        assert len > 0;
-
-        byte[] block = blockFragmentizerSafe(pos / fileInfo.blockSize());
-
-        // Skip bytes to expected position.
-        int blockOff = (int)(pos % fileInfo.blockSize());
-
-        len = Math.min(len, block.length - blockOff);
-
-        U.arrayCopy(block, blockOff, buf, off, len);
-
-        bytes += len;
-        time += System.nanoTime() - startTime;
-
-        return len;
-    }
-
-    /**
-     * Method to safely retrieve file block. In case if file block is missing this method will check file map
-     * and update file info. This may be needed when file that we are reading is concurrently fragmented.
-     *
-     * @param blockIdx Block index to read.
-     * @return Block data.
-     * @throws IOException If read failed.
-     */
-    private byte[] blockFragmentizerSafe(long blockIdx) throws IOException {
+    @Override protected byte[] blockFragmentizerSafe(long blockIdx) throws IOException {
         try {
             try {
                 return block(blockIdx);
@@ -413,12 +90,12 @@ public class IgfsInputStreamImpl extends IgfsInputStream implements IgfsSecondar
 
                 // This failure may be caused by file being fragmented.
                 if (fileInfo.fileMap() != null && !fileInfo.fileMap().ranges().isEmpty()) {
-                    IgfsEntryInfo newInfo = igfsCtx.meta().info(fileInfo.id());
+                    IgfsEntryInfo newInfo = meta.info(fileInfo.id());
 
                     // File was deleted.
                     if (newInfo == null)
                         throw new IgfsPathNotFoundException("Failed to read file block (file was concurrently " +
-                                "deleted) [path=" + path + ", blockIdx=" + blockIdx + ']');
+                            "deleted) [path=" + path + ", blockIdx=" + blockIdx + ']');
 
                     fileInfo = newInfo;
 
@@ -440,112 +117,10 @@ public class IgfsInputStreamImpl extends IgfsInputStream implements IgfsSecondar
         }
     }
 
-    /**
-     * @param blockIdx Block index.
-     * @return File block data.
-     * @throws IOException If failed.
-     * @throws IgniteCheckedException If failed.
-     */
-    private byte[] block(long blockIdx) throws IOException, IgniteCheckedException {
-        assert blockIdx >= 0;
-
-        IgniteInternalFuture<byte[]> bytesFut = locCache.get(blockIdx);
-
-        if (bytesFut == null) {
-            if (closed)
-                throw new IOException("Stream is already closed: " + this);
-
-            seqReads = (prevBlockIdx != -1 && prevBlockIdx + 1 == blockIdx) ? ++seqReads : 0;
-
-            prevBlockIdx = blockIdx;
-
-            bytesFut = dataBlock(fileInfo, blockIdx);
-
-            assert bytesFut != null;
-
-            addLocalCacheFuture(blockIdx, bytesFut);
-        }
-
-        // Schedule the next block(s) prefetch.
-        if (prefetchBlocks > 0 && seqReads >= seqReadsBeforePrefetch - 1) {
-            for (int i = 1; i <= prefetchBlocks; i++) {
-                // Ensure that we do not prefetch over file size.
-                if (fileInfo.blockSize() * (i + blockIdx) >= fileInfo.length())
-                    break;
-                else if (locCache.get(blockIdx + i) == null)
-                    addLocalCacheFuture(blockIdx + i, dataBlock(fileInfo, blockIdx + i));
-            }
-        }
-
-        byte[] bytes = bytesFut.get();
-
-        if (bytes == null)
-            throw new IgfsCorruptedFileException("Failed to retrieve file's data block (corrupted file?) " +
-                "[path=" + path + ", blockIdx=" + blockIdx + ']');
-
-        int blockSize = fileInfo.blockSize();
-
-        if (blockIdx == fileInfo.blocksCount() - 1)
-            blockSize = (int)(fileInfo.length() % blockSize);
-
-        // If part of the file was reserved for writing, but was not actually written.
-        if (bytes.length < blockSize)
-            throw new IOException("Inconsistent file's data block (incorrectly written?)" +
-                " [path=" + path + ", blockIdx=" + blockIdx + ", blockSize=" + bytes.length +
-                ", expectedBlockSize=" + blockSize + ", fileBlockSize=" + fileInfo.blockSize() +
-                ", fileLen=" + fileInfo.length() + ']');
-
-        return bytes;
-    }
-
-    /**
-     * Add local cache future.
-     *
-     * @param idx Block index.
-     * @param fut Future.
-     */
-    private void addLocalCacheFuture(long idx, IgniteInternalFuture<byte[]> fut) {
-        assert Thread.holdsLock(this);
-
-        if (!locCache.containsKey(idx)) {
-            if (locCache.size() == maxLocCacheSize) {
-                final IgniteInternalFuture<byte[]> evictFut = locCache.remove(locCache.keySet().iterator().next());
-
-                if (!evictFut.isDone()) {
-                    pendingFuts.add(evictFut);
-
-                    evictFut.listen(new IgniteInClosure<IgniteInternalFuture<byte[]>>() {
-                        @Override
-                        public void apply(IgniteInternalFuture<byte[]> t) {
-                            pendingFuts.remove(evictFut);
-
-                            pendingFutsLock.lock();
-
-                            try {
-                                pendingFutsCond.signalAll();
-                            } finally {
-                                pendingFutsLock.unlock();
-                            }
-                        }
-                    });
-                }
-            }
-
-            locCache.put(idx, fut);
-        }
-    }
-
-    /**
-     * Get data block for specified block index.
-     *
-     * @param fileInfo File info.
-     * @param blockIdx Block index.
-     * @return Requested data block or {@code null} if nothing found.
-     * @throws IgniteCheckedException If failed.
-     */
-    @Nullable protected IgniteInternalFuture<byte[]> dataBlock(IgfsEntryInfo fileInfo, long blockIdx)
+    /** {@inheritDoc} */
+    @Override @Nullable protected IgniteInternalFuture<byte[]> dataBlock(long blockIdx)
         throws IgniteCheckedException {
-        return igfsCtx.data().dataBlock(fileInfo, path, blockIdx, secReader);
+        return data.dataBlock(fileInfo, path, blockIdx, secReader);
     }
 
     /** {@inheritDoc} */
