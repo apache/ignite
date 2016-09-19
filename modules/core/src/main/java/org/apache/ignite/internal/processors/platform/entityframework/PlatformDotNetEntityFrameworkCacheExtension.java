6eab821d5f21f77e664a1d84642bf9fd39a6f69e
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-09-19 12:58:02 +0300

    EF cache extension

---
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/platform/entityframework/PlatformDotNetEntityFrameworkCacheExtension.java
@@ -0,0 +1,43 @@
+package org.apache.ignite.internal.processors.platform.entityframework;
+
+import org.apache.ignite.IgniteCheckedException;
+import org.apache.ignite.internal.binary.BinaryRawReaderEx;
+import org.apache.ignite.internal.processors.platform.cache.PlatformCache;
+import org.apache.ignite.internal.processors.platform.cache.PlatformCacheExtension;
+import org.apache.ignite.internal.processors.platform.memory.PlatformMemory;
+
+import java.util.HashSet;
+
+/**
+ * EntityFramework cache extension.
+ */
+public class PlatformDotNetEntityFrameworkCacheExtension implements PlatformCacheExtension {
+    /** Extension ID. */
+    private static final int EXT_ID = 0;
+
+    /** Operation: increment entity set versions. */
+    private static final int OP_INCREMENT_VERSIONS = 1;
+
+    /** {@inheritDoc} */
+    @Override public int id() {
+        return EXT_ID;
+    }
+
+    /** {@inheritDoc} */
+    @SuppressWarnings("unchecked")
+    @Override public long processInOutStreamLong(PlatformCache target, int type, BinaryRawReaderEx reader,
+        PlatformMemory mem) throws IgniteCheckedException {
+        switch (type) {
+            case OP_INCREMENT_VERSIONS: {
+                HashSet keys = null;
+                target.rawCache().invokeAll(keys, new PlatformDotNetEntityFrameworkIncreaseVersionProcessor());
+
+                return target.writeResult(mem, null);
+            }
+
+            // TODO: Broadcast cleanup from here.
+        }
+
+        throw new IgniteCheckedException("Unsupported operation type: " + type);
+    }
+}
