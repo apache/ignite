5dbcfff931d4b54c6137e7509d15443060bd8bc9
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-09-20 20:44:44 +0300

    IGNITE-3291 .NET: IClusterGroup.ForDaemons: implement

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Ignite.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Ignite.cs
@@ -273,6 +273,12 @@ namespace Apache.Ignite.Core.Impl
         }
 
         /** <inheritdoc /> */
+        public IClusterGroup ForDaemons()
+        {
+            return _prj.ForDaemons();
+        }
+
+        /** <inheritdoc /> */
         public IClusterGroup ForHost(IClusterNode node)
         {
             IgniteArgumentCheck.NotNull(node, "node");
