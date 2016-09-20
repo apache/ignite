f8e35aa12051393a655b4a77a6dcec24c9acd0c1
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-09-20 18:49:30 +0300

    wip

--- modules/platforms/dotnet/Apache.Ignite.EntityFramework.Tests/EntityFrameworkCacheTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.EntityFramework.Tests/EntityFrameworkCacheTest.cs
@@ -28,7 +28,6 @@ namespace Apache.Ignite.EntityFramework.Tests
     using System.Data.Entity;
     using System.Data.Entity.Core.EntityClient;
     using System.Data.Entity.Infrastructure;
-    using System.Diagnostics;
     using System.IO;
     using System.Linq;
     using System.Threading;
