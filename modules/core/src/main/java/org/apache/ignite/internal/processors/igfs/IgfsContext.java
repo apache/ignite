c6b6329b66b93f981e61deb61d6d8d43918d3700
vozerov-gridgain <vozerov@gridgain.com>
2016-09-20 10:20:04 +0300

    Review.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/igfs/IgfsContext.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/igfs/IgfsContext.java
@@ -20,7 +20,6 @@ package org.apache.ignite.internal.processors.igfs;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.RejectedExecutionException;
 import org.apache.ignite.IgniteCheckedException;
