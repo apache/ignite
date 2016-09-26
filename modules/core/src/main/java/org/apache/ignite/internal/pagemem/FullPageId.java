1057af69b151ca3a8ba6accd900f3d9d7acff9b9
Alexey Goncharuk <alexey.goncharuk@gmail.com>
2016-09-26 18:29:28 +0300

    GG-11547 - Fixes for page ID.

--- modules/core/src/main/java/org/apache/ignite/internal/pagemem/FullPageId.java
+++ modules/core/src/main/java/org/apache/ignite/internal/pagemem/FullPageId.java
@@ -88,7 +88,7 @@ public class FullPageId {
      * @param cacheId Cache ID.
      */
     public FullPageId(long pageId, int cacheId) {
-        this.pageId = PageIdUtils.pageId(pageId);
+        this.pageId = pageId;
         this.cacheId = cacheId;
 
         effectivePageId = PageIdUtils.effectivePageId(pageId);
--- modules/core/src/test/java/org/apache/ignite/internal/pagemem/impl/PageMemoryNoLoadSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/pagemem/impl/PageMemoryNoLoadSelfTest.java
@@ -30,6 +30,7 @@ import org.apache.ignite.internal.mem.file.MappedFileMemoryProvider;
 import org.apache.ignite.internal.pagemem.FullPageId;
 import org.apache.ignite.internal.pagemem.Page;
 import org.apache.ignite.internal.pagemem.PageIdAllocator;
+import org.apache.ignite.internal.pagemem.PageIdUtils;
 import org.apache.ignite.internal.pagemem.PageMemory;
 import org.apache.ignite.internal.processors.cache.database.tree.io.PageIO;
 import org.apache.ignite.internal.util.typedef.internal.U;
@@ -163,6 +164,96 @@ public class PageMemoryNoLoadSelfTest extends GridCommonAbstractTest {
 
             for (int i = 0; i < pages; i++)
                 assertFalse(handles.add(allocatePage(mem)));
+        }
+        finally {
+            mem.stop();
+        }
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testPageIdRotation() throws Exception {
+        PageMemory mem = memory();
+
+        mem.start();
+
+        try {
+            int pages = 5;
+
+            Collection<FullPageId> old = new ArrayList<>();
+            Collection<FullPageId> updated = new ArrayList<>();
+
+            for (int i = 0; i < pages; i++)
+                old.add(allocatePage(mem));
+
+            // Check that initial pages are accessible.
+            for (FullPageId id : old) {
+                try (Page page = mem.page(id.cacheId(), id.pageId())) {
+                    ByteBuffer buf = page.getForWrite();
+
+                    assertNotNull(buf);
+
+                    try {
+                        long updId = PageIdUtils.rotatePageId(id.pageId());
+
+                        PageIO.setPageId(buf, updId);
+
+                        updated.add(new FullPageId(updId, id.cacheId()));
+                    }
+                    finally {
+                        page.releaseWrite(true);
+                    }
+                }
+            }
+
+            // Check that updated pages are inaccessible using old IDs.
+            for (FullPageId id : old) {
+                try (Page page = mem.page(id.cacheId(), id.pageId())) {
+                    ByteBuffer buf = page.getForWrite();
+
+                    if (buf != null) {
+                        page.releaseWrite(false);
+
+                        fail("Was able to acquire page write lock.");
+                    }
+
+                    buf = page.getForRead();
+
+                    if (buf != null) {
+                        page.releaseRead();
+
+                        fail("Was able to acquire page read lock.");
+                    }
+                }
+            }
+
+            // Check that updated pages are accessible using new IDs.
+            for (FullPageId id : updated) {
+                try (Page page = mem.page(id.cacheId(), id.pageId())) {
+                    ByteBuffer buf = page.getForWrite();
+
+                    assertNotNull(buf);
+
+                    try {
+                        assertEquals(id.pageId(), PageIO.getPageId(buf));
+                    }
+                    finally {
+                        page.releaseWrite(false);
+                    }
+
+                    buf = page.getForRead();
+
+                    assertNotNull(buf);
+
+                    try {
+                        assertEquals(id.pageId(), PageIO.getPageId(buf));
+                    }
+                    finally {
+                        page.releaseRead();
+                    }
+                }
+            }
         }
         finally {
             mem.stop();
