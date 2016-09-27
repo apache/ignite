cdf0bbedfbaddd3230c3b5ce1555275a93af0cc7
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-09-27 13:23:45 +0300

    wip tests

--- modules/platforms/dotnet/Apache.Ignite.log4net/IgniteLog4netLogger.cs
+++ modules/platforms/dotnet/Apache.Ignite.log4net/IgniteLog4netLogger.cs
@@ -17,10 +17,41 @@
 
 namespace Apache.Ignite.log4net
 {
+    using System;
+    using Apache.Ignite.Core.Log;
+
     /// <summary>
     /// Ignite log4net integration.
     /// </summary>
-    public class IgniteLog4NetLogger
+    public class IgniteLog4NetLogger : ILogger
     {
+        /// <summary>
+        /// Logs the specified message.
+        /// </summary>
+        /// <param name="level">The level.</param>
+        /// <param name="message">The message.</param>
+        /// <param name="args">The arguments to format <paramref name="message" />.
+        /// Can be null (formatting will not occur).</param>
+        /// <param name="formatProvider">The format provider. Can be null if <paramref name="args" /> is null.</param>
+        /// <param name="category">The logging category name.</param>
+        /// <param name="nativeErrorInfo">The native error information.</param>
+        /// <param name="ex">The exception. Can be null.</param>
+        public void Log(LogLevel level, string message, object[] args, IFormatProvider formatProvider, string category,
+            string nativeErrorInfo, Exception ex)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Determines whether the specified log level is enabled.
+        /// </summary>
+        /// <param name="level">The level.</param>
+        /// <returns>
+        /// Value indicating whether the specified log level is enabled
+        /// </returns>
+        public bool IsEnabled(LogLevel level)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
