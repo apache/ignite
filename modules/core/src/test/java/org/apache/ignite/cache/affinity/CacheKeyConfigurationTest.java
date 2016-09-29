3be5c5ea624f7a8786a670e13c69f0509a4682c2
sboikov <sboikov@gridgain.com>
2016-09-29 18:04:47 +0300

    ignite-2698 minor

--- modules/core/src/test/java/org/apache/ignite/cache/affinity/CacheKeyConfigurationTest.java
+++ modules/core/src/test/java/org/apache/ignite/cache/affinity/CacheKeyConfigurationTest.java
@@ -5,7 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,6 +17,8 @@
 
 package org.apache.ignite.cache.affinity;
 
+import java.io.Serializable;
+import java.util.concurrent.Callable;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.binary.BinaryObjectException;
 import org.apache.ignite.cache.CacheKeyConfiguration;
@@ -30,9 +34,6 @@ import org.apache.ignite.spi.discovery.tcp.ipfinder.TcpDiscoveryIpFinder;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 import org.apache.ignite.testframework.GridTestUtils;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
-
-import java.io.Serializable;
-import java.util.concurrent.Callable;
 
 /**
  * Tests on cache key configuration with different marshallers.
@@ -55,7 +56,7 @@ public class CacheKeyConfigurationTest extends GridCommonAbstractTest {
 
     /** Exception message of cache key configuration mismatch in case of non-binary marshaller. */
     private static final String NON_BINARY_MARSH_CONF_MISMATCH_ERROR =
-            "Local node's cache keys configuration is not equal to remote node's cache keys configuration";
+        "Local node's cache keys configuration is not equal to remote node's cache keys configuration";
 
     /** Exception message of cache key configuration mismatch in case of binary marshaller. */
     private static final String BINARY_MARSH_CONF_MISMATCH_ERROR = "Binary type has different affinity key fields";
@@ -68,11 +69,9 @@ public class CacheKeyConfigurationTest extends GridCommonAbstractTest {
 
     /** {@inheritDoc} */
     @Override protected IgniteConfiguration getConfiguration(String gridName) throws Exception {
-        IgniteConfiguration cfg = new IgniteConfiguration();
+        IgniteConfiguration cfg = super.getConfiguration(gridName);
 
-        TcpDiscoverySpi discoSpi = new TcpDiscoverySpi();
-        discoSpi.setIpFinder(IP_FINDER);
-        cfg.setDiscoverySpi(discoSpi);
+        ((TcpDiscoverySpi)cfg.getDiscoverySpi()).setIpFinder(IP_FINDER);
 
         CacheConfiguration cacheWoCacheKeyCfg = new CacheConfiguration();
         cacheWoCacheKeyCfg.setName(CACHE_WO_CACHE_KEY_NAME);
@@ -83,10 +82,13 @@ public class CacheKeyConfigurationTest extends GridCommonAbstractTest {
         CacheConfiguration cacheKeyAnnotatedFieldCfg = new CacheConfiguration();
         cacheKeyAnnotatedFieldCfg.setName(CACHE_WITH_ANNOTATED_KEY_FIELD);
 
-        CacheConfiguration cacheKeyAnnotatedMethodCfg = new CacheConfiguration();
-        cacheKeyAnnotatedMethodCfg.setName(CACHE_WITH_ANNOTATED_KEY_METHOD);
+        CacheConfiguration cacheKeyAnnotatedMtdCfg = new CacheConfiguration();
+        cacheKeyAnnotatedMtdCfg.setName(CACHE_WITH_ANNOTATED_KEY_METHOD);
 
-        cfg.setCacheConfiguration(cacheWoCacheKeyCfg, cacheWithCacheKeyCfg, cacheKeyAnnotatedFieldCfg, cacheKeyAnnotatedMethodCfg);
+        cfg.setCacheConfiguration(cacheWoCacheKeyCfg,
+            cacheWithCacheKeyCfg,
+            cacheKeyAnnotatedFieldCfg,
+            cacheKeyAnnotatedMtdCfg);
 
         cfg.setMarshaller(marshaller);
         cfg.setCacheKeyConfiguration(cacheKeyCfgs);
@@ -135,24 +137,24 @@ public class CacheKeyConfigurationTest extends GridCommonAbstractTest {
      *
      * @throws Exception If failed.
      * */
-    public void checkCacheKey() throws Exception {
+    private void checkCacheKey() throws Exception {
         try (Ignite g = startGrid(0)) {
             BaseAffinityKey key1 = new BaseAffinityKey(0, 51);
             AffinityKey key2 = new AffinityKey(0, 51, 100);
             AffinityKeyAnnotatedField key3 = new AffinityKeyAnnotatedField(3, 51, 101);
             AffinityKeyAnnotatedMethod key4 = new AffinityKeyAnnotatedMethod(3, 51, 101);
 
-            int partition1 = g.affinity(CACHE_WO_CACHE_KEY_NAME).partition(key1);
-            int partition2 = g.affinity(CACHE_WITH_CACHE_KEY_NAME).partition(key2);
-            int partition3 = g.affinity(CACHE_WITH_ANNOTATED_KEY_FIELD).partition(key3);
-            int partition4 = g.affinity(CACHE_WITH_ANNOTATED_KEY_METHOD).partition(key4);
+            int part1 = g.affinity(CACHE_WO_CACHE_KEY_NAME).partition(key1);
+            int part2 = g.affinity(CACHE_WITH_CACHE_KEY_NAME).partition(key2);
+            int part3 = g.affinity(CACHE_WITH_ANNOTATED_KEY_FIELD).partition(key3);
+            int part4 = g.affinity(CACHE_WITH_ANNOTATED_KEY_METHOD).partition(key4);
 
-            assertEquals(51, partition1);
-            assertEquals(100, partition2);
-            assertEquals(101, partition3);
+            assertEquals(51, part1);
+            assertEquals(100, part2);
+            assertEquals(101, part3);
 
             if (!(marshaller instanceof BinaryMarshaller))
-                assertEquals(101, partition4);
+                assertEquals(101, part4);
         }
     }
 
@@ -163,6 +165,7 @@ public class CacheKeyConfigurationTest extends GridCommonAbstractTest {
      * */
     public void testJdkMarshallerCacheKeyOverrides() throws Exception {
         marshaller = new JdkMarshaller();
+
         cacheKeyCfgs = new CacheKeyConfiguration[] {
                 new CacheKeyConfiguration(AffinityKeyAnnotatedField.class.getName(), "baseAffinityKey"),
                 new CacheKeyConfiguration(AffinityKeyAnnotatedMethod.class.getName(), "baseAffinityKey"),
@@ -178,6 +181,7 @@ public class CacheKeyConfigurationTest extends GridCommonAbstractTest {
      * */
     public void testOptimizedMarshallerCacheKeyOverrides() throws Exception {
         marshaller = new OptimizedMarshaller();
+
         cacheKeyCfgs = new CacheKeyConfiguration[] {
                 new CacheKeyConfiguration(AffinityKeyAnnotatedField.class.getName(), "baseAffinityKey"),
                 new CacheKeyConfiguration(AffinityKeyAnnotatedMethod.class.getName(), "baseAffinityKey"),
@@ -194,6 +198,7 @@ public class CacheKeyConfigurationTest extends GridCommonAbstractTest {
      * */
     public void testBinaryMarshallerCacheKeyOverrides() throws Exception {
         marshaller = new BinaryMarshaller();
+
         cacheKeyCfgs = new CacheKeyConfiguration[] {
                 new CacheKeyConfiguration(AffinityKeyAnnotatedField.class.getName(), "baseAffinityKey"),
                 new CacheKeyConfiguration(AffinityKeyAnnotatedMethod.class.getName(), "baseAffinityKey"),
@@ -207,16 +212,16 @@ public class CacheKeyConfigurationTest extends GridCommonAbstractTest {
      *
      * @throws Exception If failed.
      * */
-    public void checkCacheKeyOverridesAnnotations() throws Exception {
+    private void checkCacheKeyOverridesAnnotations() throws Exception {
         try (Ignite g = startGrid(0)) {
             AffinityKeyAnnotatedField key1 = new AffinityKeyAnnotatedField(3, 51, 101);
             AffinityKeyAnnotatedMethod key2 = new AffinityKeyAnnotatedMethod(3, 51, 101);
 
-            int partition3 = g.affinity(CACHE_WITH_ANNOTATED_KEY_FIELD).partition(key1);
-            int partition4 = g.affinity(CACHE_WITH_ANNOTATED_KEY_METHOD).partition(key2);
+            int part3 = g.affinity(CACHE_WITH_ANNOTATED_KEY_FIELD).partition(key1);
+            int part4 = g.affinity(CACHE_WITH_ANNOTATED_KEY_METHOD).partition(key2);
 
-            assertEquals(51, partition3);
-            assertEquals(51, partition4);
+            assertEquals(51, part3);
+            assertEquals(51, part4);
         }
     }
 
@@ -255,25 +260,27 @@ public class CacheKeyConfigurationTest extends GridCommonAbstractTest {
 
         try (Ignite g1 = startGrid(getTestGridName(0), cfg1)) {
             GridTestUtils.assertThrowsWithCause(new Callable<Object>() {
-                @Override
-                public Object call() throws Exception {
+                @Override public Object call() throws Exception {
                     IgniteConfiguration cfg = getConfiguration(getTestGridName(1));
+
                     cfg.setCacheKeyConfiguration(
                             new CacheKeyConfiguration(AffinityKeyAnnotatedField.class.getName(), "baseAffinityKey"));
+
                     try (Ignite g2 = startGrid(getTestGridName(1), cfg)) {
+                        // No-op.
                     }
                     return null;
                 }
             }, IgniteSpiException.class, NON_BINARY_MARSH_CONF_MISMATCH_ERROR);
 
             GridTestUtils.assertThrowsWithCause(new Callable<Object>() {
-                @Override
-                public Object call() throws Exception {
+                @Override public Object call() throws Exception {
                     IgniteConfiguration cfg = getConfiguration(getTestGridName(1));
                     cfg.setCacheKeyConfiguration(
                         new CacheKeyConfiguration(AffinityKeyAnnotatedField.class.getName(), "baseAffinityKey"),
                         new CacheKeyConfiguration(AffinityKeyAnnotatedMethod.class.getName(), "affinityKey"));
                     try (Ignite g2 = startGrid(getTestGridName(1), cfg)) {
+                        // No-op.
                     }
                     return null;
                 }
@@ -281,14 +288,14 @@ public class CacheKeyConfigurationTest extends GridCommonAbstractTest {
 
 
             GridTestUtils.assertThrowsWithCause(new Callable<Object>() {
-                @Override
-                public Object call() throws Exception {
+                @Override public Object call() throws Exception {
                     IgniteConfiguration cfg = getConfiguration(getTestGridName(1));
                     cfg.setCacheKeyConfiguration(
                             new CacheKeyConfiguration(AffinityKey.class.getName(), "baseAffinityKey"),
                             new CacheKeyConfiguration(AffinityKeyAnnotatedField.class.getName(), "baseAffinityKey"),
                             new CacheKeyConfiguration(AffinityKeyAnnotatedMethod.class.getName(), "baseAffinityKey"));
                     try (Ignite g2 = startGrid(getTestGridName(1), cfg)) {
+                        // No-op.
                     }
                     return null;
                 }
@@ -310,9 +317,9 @@ public class CacheKeyConfigurationTest extends GridCommonAbstractTest {
 
         try (final Ignite g1 = startGrid(getTestGridName(0), cfg1)) {
             GridTestUtils.assertThrowsWithCause(new Callable<Object>() {
-                @Override
-                public Object call() throws Exception {
+                @Override public Object call() throws Exception {
                     IgniteConfiguration cfg = getConfiguration(getTestGridName(1));
+
                     cfg.setCacheKeyConfiguration(new CacheKeyConfiguration(AffinityKey.class.getName(), "baseAffinityKey"));
 
                     try (Ignite g2 = startGrid(getTestGridName(1), cfg)) {
@@ -332,35 +339,30 @@ public class CacheKeyConfigurationTest extends GridCommonAbstractTest {
      * Affinity key base class.
      * */
     public static class BaseAffinityKey implements Serializable {
+        /** */
         private Integer objKey;
+
+        /** */
         private Integer baseAffinityKey;
 
+        /**
+         *
+         */
         public BaseAffinityKey() {
+            // No-op.
         }
 
+        /**
+         * @param objKey Key.
+         * @param baseAffinityKey Affinity key.
+         */
         public BaseAffinityKey(Integer objKey, Integer baseAffinityKey) {
             this.objKey = objKey;
             this.baseAffinityKey = baseAffinityKey;
         }
 
-        public Integer getObjKey() {
-            return objKey;
-        }
-
-        public void setObjKey(Integer objKey) {
-            this.objKey = objKey;
-        }
-
-        public Integer getBaseAffinityKey() {
-            return baseAffinityKey;
-        }
-
-        public void setBaseAffinityKey(Integer baseAffinityKey) {
-            this.baseAffinityKey = baseAffinityKey;
-        }
-
-        @Override
-        public boolean equals(Object o) {
+        /** {@inheritDoc} */
+        @Override public boolean equals(Object o) {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
 
@@ -371,11 +373,11 @@ public class CacheKeyConfigurationTest extends GridCommonAbstractTest {
 
         }
 
-        @Override
-        public int hashCode() {
-            int result = objKey != null ? objKey.hashCode() : 0;
-            result = 31 * result + (baseAffinityKey != null ? baseAffinityKey.hashCode() : 0);
-            return result;
+        /** {@inheritDoc} */
+        @Override public int hashCode() {
+            int res = objKey != null ? objKey.hashCode() : 0;
+            res = 31 * res + (baseAffinityKey != null ? baseAffinityKey.hashCode() : 0);
+            return res;
         }
     }
 
@@ -383,26 +385,28 @@ public class CacheKeyConfigurationTest extends GridCommonAbstractTest {
      * Affinity key class.
      * */
     public static class AffinityKey extends BaseAffinityKey {
+        /** */
         private Integer affinityKey;
 
+        /**
+         *
+         */
         public AffinityKey() {
+            // No-op.
         }
 
+        /**
+         * @param objKey Key.
+         * @param baseAffinityKey Base affinity key.
+         * @param affinityKey Affinity key.
+         */
         public AffinityKey(Integer objKey, Integer baseAffinityKey, Integer affinityKey) {
             super(objKey, baseAffinityKey);
             this.affinityKey = affinityKey;
         }
 
-        public Integer getAffinityKey() {
-            return affinityKey;
-        }
-
-        public void setAffinityKey(Integer affinityKey) {
-            this.affinityKey = affinityKey;
-        }
-
-        @Override
-        public boolean equals(Object o) {
+        /** {@inheritDoc} */
+        @Override public boolean equals(Object o) {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             if (!super.equals(o)) return false;
@@ -413,11 +417,11 @@ public class CacheKeyConfigurationTest extends GridCommonAbstractTest {
 
         }
 
-        @Override
-        public int hashCode() {
-            int result = super.hashCode();
-            result = 31 * result + (affinityKey != null ? affinityKey.hashCode() : 0);
-            return result;
+        /** {@inheritDoc} */
+        @Override public int hashCode() {
+            int res = super.hashCode();
+            res = 31 * res + (affinityKey != null ? affinityKey.hashCode() : 0);
+            return res;
         }
     }
 
@@ -425,27 +429,29 @@ public class CacheKeyConfigurationTest extends GridCommonAbstractTest {
      * Affinity key class with annotated {@link org.apache.ignite.cache.affinity.AffinityKeyMapped} field.
      * */
     public static class AffinityKeyAnnotatedField extends BaseAffinityKey {
+        /** */
         @AffinityKeyMapped
         private Integer affinityKey;
 
+        /**
+         *
+         */
         public AffinityKeyAnnotatedField() {
+            // No-op.
         }
 
+        /**
+         * @param objKey Key.
+         * @param baseAffinityKey Base affinity key.
+         * @param affinityKey Affinity key.
+         */
         public AffinityKeyAnnotatedField(Integer objKey, Integer baseAffinityKey, Integer affinityKey) {
             super(objKey, baseAffinityKey);
             this.affinityKey = affinityKey;
         }
 
-        public Integer getAffinityKey() {
-            return affinityKey;
-        }
-
-        public void setAffinityKey(Integer affinityKey) {
-            this.affinityKey = affinityKey;
-        }
-
-        @Override
-        public boolean equals(Object o) {
+        /** {@inheritDoc} */
+        @Override public boolean equals(Object o) {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             if (!super.equals(o)) return false;
@@ -456,11 +462,11 @@ public class CacheKeyConfigurationTest extends GridCommonAbstractTest {
 
         }
 
-        @Override
-        public int hashCode() {
-            int result = super.hashCode();
-            result = 31 * result + (affinityKey != null ? affinityKey.hashCode() : 0);
-            return result;
+        /** {@inheritDoc} */
+        @Override public int hashCode() {
+            int res = super.hashCode();
+            res = 31 * res + (affinityKey != null ? affinityKey.hashCode() : 0);
+            return res;
         }
     }
 
@@ -468,27 +474,43 @@ public class CacheKeyConfigurationTest extends GridCommonAbstractTest {
      * Affinity key class with annotated {@link org.apache.ignite.cache.affinity.AffinityKeyMapped} method.
      * */
     public static class AffinityKeyAnnotatedMethod extends BaseAffinityKey {
+        /** */
         private Integer affinityKey;
 
+        /**
+         *
+         */
         public AffinityKeyAnnotatedMethod() {
+            // No-op.
         }
 
+        /**
+         * @param objKey Key.
+         * @param baseAffinityKey Base affinity key.
+         * @param affinityKey Affinity key.
+         */
         public AffinityKeyAnnotatedMethod(Integer objKey, Integer baseAffinityKey, Integer affinityKey) {
             super(objKey, baseAffinityKey);
             this.affinityKey = affinityKey;
         }
 
+        /**
+         * @return Affinity key.
+         */
         @AffinityKeyMapped
         public Integer getAffinityKey() {
             return affinityKey;
         }
 
+        /**
+         * @param affinityKey Affinity key.
+         */
         public void setAffinityKey(Integer affinityKey) {
             this.affinityKey = affinityKey;
         }
 
-        @Override
-        public boolean equals(Object o) {
+        /** {@inheritDoc} */
+        @Override public boolean equals(Object o) {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             if (!super.equals(o)) return false;
@@ -499,11 +521,11 @@ public class CacheKeyConfigurationTest extends GridCommonAbstractTest {
 
         }
 
-        @Override
-        public int hashCode() {
-            int result = super.hashCode();
-            result = 31 * result + (affinityKey != null ? affinityKey.hashCode() : 0);
-            return result;
+        /** {@inheritDoc} */
+        @Override public int hashCode() {
+            int res = super.hashCode();
+            res = 31 * res + (affinityKey != null ? affinityKey.hashCode() : 0);
+            return res;
         }
     }
 }
