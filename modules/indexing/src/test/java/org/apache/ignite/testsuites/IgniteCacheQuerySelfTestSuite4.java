11b349e7314a32eefb9d4e16e79b48ca1d729990
Andrey V. Mashenkov <andrey.mashenkov@gmail.com>
2016-10-03 14:34:05 +0300

    Test added

--- modules/indexing/src/test/java/org/apache/ignite/testsuites/IgniteCacheQuerySelfTestSuite4.java
+++ modules/indexing/src/test/java/org/apache/ignite/testsuites/IgniteCacheQuerySelfTestSuite4.java
@@ -27,6 +27,7 @@ import org.apache.ignite.internal.processors.cache.query.continuous.CacheContinu
 import org.apache.ignite.internal.processors.cache.query.continuous.CacheContinuousQueryFailoverTxOffheapTieredTest;
 import org.apache.ignite.internal.processors.cache.query.continuous.CacheContinuousQueryFailoverTxReplicatedSelfTest;
 import org.apache.ignite.internal.processors.cache.query.continuous.CacheContinuousQueryFailoverTxSelfTest;
+import org.apache.ignite.loadtests.continuous.CacheContinuousQueryFailoverAtomicPrimaryWriteOrderLoadTest;
 
 /**
  * Test suite for cache queries.
@@ -50,6 +51,9 @@ public class IgniteCacheQuerySelfTestSuite4 extends TestSuite {
         suite.addTestSuite(CacheContinuousQueryAsyncFailoverAtomicPrimaryWriteOrderSelfTest.class);
         suite.addTestSuite(CacheContinuousQueryAsyncFailoverTxReplicatedSelfTest.class);
         suite.addTestSuite(CacheContinuousQueryAsyncFailoverTxSelfTest.class);
+
+        // Continuous queries failover load tests. (Long run)
+        suite.addTestSuite(CacheContinuousQueryFailoverAtomicPrimaryWriteOrderLoadTest.class);
 
         return suite;
     }
