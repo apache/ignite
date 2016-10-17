550181fccfc6e90b6f234c5ee835b67139ccd8c6
vozerov-gridgain <vozerov@gridgain.com>
2016-10-17 12:37:40 +0300

    WIP.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/atomic/MessageProblemReproducer.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/atomic/MessageProblemReproducer.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.internal.processors.cache.distributed.dht.atomic;
 
 import org.apache.ignite.Ignite;
@@ -12,12 +29,11 @@ import java.util.Collections;
 import java.util.UUID;
 
 /**
- * Created by vozerov on 10/17/2016.
+ * Message problem reproducer.
  */
 public class MessageProblemReproducer {
     /**
-     *
-     * @param args
+     * Entry point.
      */
     public static void main(String[] args) throws Exception {
         Ignite node1 = Ignition.start(config("1"));
@@ -31,7 +47,7 @@ public class MessageProblemReproducer {
             }
         });
 
-        node2.message().send("topic", new byte[70000]);
+        node2.message().send("topic", new byte[150000]);
 
         Thread.sleep(Long.MAX_VALUE);
     }
