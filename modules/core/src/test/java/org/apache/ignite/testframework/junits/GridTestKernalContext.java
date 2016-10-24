b9bb1f6bc02cb3054b1e9b9bfbd34b886db77624
vozerov-gridgain <vozerov@gridgain.com>
2016-10-24 11:50:51 +0300

    Merge commit '2ab094e0' into ignite-1.7.3.

--- modules/core/src/test/java/org/apache/ignite/testframework/junits/GridTestKernalContext.java
+++ modules/core/src/test/java/org/apache/ignite/testframework/junits/GridTestKernalContext.java
@@ -63,6 +63,7 @@ public class GridTestKernalContext extends GridKernalContextImpl {
                 null,
                 null,
                 null,
+                null,
                 U.allPluginProviders());
 
         GridTestUtils.setFieldValue(grid(), "cfg", config());
