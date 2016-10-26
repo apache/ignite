da3bce03ce63509c0fceca4859bb3fd07090fdd0
Alexey Goncharuk <alexey.goncharuk@gmail.com>
2016-10-26 13:58:29 +0300

    EA5 - Increased maximum index name length

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/database/MetadataStorage.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/database/MetadataStorage.java
@@ -40,7 +40,7 @@ import static org.apache.ignite.internal.pagemem.PageIdAllocator.FLAG_IDX;
  */
 public class MetadataStorage implements MetaStore {
     /** Max index name length (bytes num) */
-    public static final int MAX_IDX_NAME_LEN = 512;
+    public static final int MAX_IDX_NAME_LEN = 768;
 
     /** Bytes in byte. */
     private static final int BYTE_LEN = 1;
