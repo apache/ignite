034bb05a85f64a829a14b48a0166437e2118fe70
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-11-02 16:09:41 +0300

    IGNITE-4125 .NET: Tiered cache example

---
+++ modules/platforms/dotnet/examples/Apache.Ignite.Examples/Datagrid/MultiTieredCacheExample.cs
@@ -0,0 +1,107 @@
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Examples.Datagrid
+{
+    using System;
+    using System.Threading;
+    using Apache.Ignite.Core;
+    using Apache.Ignite.Core.Cache;
+    using Apache.Ignite.Core.Cache.Configuration;
+    using Apache.Ignite.Core.Cache.Eviction;
+    using Apache.Ignite.Core.Discovery.Tcp;
+
+    /// <summary>
+    /// This example demonstrates how multi-tiered Ignite cache stores data in various ways.
+    /// <para />
+    /// 1) Build the project Apache.Ignite.ExamplesDll (select it -> right-click -> Build).
+    ///    Apache.Ignite.ExamplesDll.dll must appear in %IGNITE_HOME%/platforms/dotnet/examples/Apache.Ignite.ExamplesDll/bin/${Platform]/${Configuration} folder.
+    /// 2) Set this class as startup object (Apache.Ignite.Examples project -> right-click -> Properties ->
+    ///     Application -> Startup object);
+    /// 3) Start example (F5 or Ctrl+F5).
+    /// <para />
+    /// This example can be run with standalone Apache Ignite.NET node:
+    /// 1) Run %IGNITE_HOME%/platforms/dotnet/bin/Apache.Ignite.exe:
+    /// Apache.Ignite.exe -configFileName=platforms\dotnet\examples\apache.ignite.examples\app.config -assembly=[path_to_Apache.Ignite.ExamplesDll.dll]
+    /// 2) Start example.
+    /// </summary>
+    public class MultiTieredCacheExample
+    {
+        /// <summary>Example cache name.</summary>
+        private const string CacheName = "dotnet_multi_tiered_example_cache";
+
+        [STAThread]
+        public static void Main()
+        {
+            using (var ignite = Ignition.StartFromApplicationConfiguration())
+            {
+                var cfg = new CacheConfiguration
+                {
+                    Name = CacheName,
+                    Backups = 1,
+                    EvictionPolicy = new LruEvictionPolicy
+                    {
+                        // Maximum number of entries that will be stored in Java heap. 
+                        MaxSize = 10
+                    },
+                    OffHeapMaxMemory = 1024*10 // 10 KB (maximum allowed off-heap space).
+                };
+
+                ICache<int, byte[]> cache = ignite.GetOrCreateCache<int, byte[]>(cfg);
+
+                // Sample data.
+                byte[] dataBytes = new byte[1024];
+
+                // Filling out cache and printing its metrics.
+                PrintCacheMetrics(cache);
+
+                for (int i = 0; i < 100; i++)
+                {
+                    cache.Put(i, dataBytes);
+
+                    if (i%10 == 0)
+                    {
+                        Console.WriteLine(">>> Cache entries created: {0}", i);
+
+                        PrintCacheMetrics(cache);
+                    }
+                }
+
+                Console.WriteLine(">>> Waiting for metrics final update...");
+
+                Thread.Sleep(((TcpDiscoverySpi) ignite.GetConfiguration().DiscoverySpi).HeartbeatFrequency);
+
+                PrintCacheMetrics(cache);
+
+                Console.WriteLine();
+                Console.WriteLine(">>> Example finished, press any key to exit ...");
+                Console.ReadKey();
+            }
+        }
+
+        /// <summary>
+        /// Prints the cache metrics.
+        /// </summary>
+        private static void PrintCacheMetrics(ICache<int, byte[]> cache)
+        {
+            var metrics = cache.GetMetrics();
+
+            Console.WriteLine("\n>>> Cache entries layout: [Java heap={0}, Off-Heap={1}, Swap={2}]\n",
+                metrics.Size, metrics.OffHeapEntriesCount, metrics.OverflowSize);
+        }
+    }
+}
