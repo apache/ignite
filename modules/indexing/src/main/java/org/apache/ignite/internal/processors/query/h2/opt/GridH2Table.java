678b4d26ae0812d8d61cdc5f4544b9a9cfce9957
Ilya Lantukh <ilantukh@gridgain.com>
2016-11-09 17:06:13 +0300

    Fixed iteration over indexes.

--- modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/opt/GridH2Table.java
+++ modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/opt/GridH2Table.java
@@ -631,8 +631,8 @@ public class GridH2Table extends TableBase {
 
                 if (old != null) {
                     // Remove row from all indexes.
-                    // Start from 2 because 0 - Scan (don't need to update), 1 - PK (already updated).
-                    for (int i = 2, len = idxs.size(); i < len; i++) {
+                    // Start from 3 because 0 - Scan (don't need to update), 1 - PK hash (already updated), 2 - PK (already updated).
+                    for (int i = 3, len = idxs.size(); i < len; i++) {
                         Row res = index(i).remove(old);
 
                         assert eq(pk, res, old) : "\n" + old + "\n" + res + "\n" + i + " -> " + index(i).getName();
