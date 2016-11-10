d5465c753327ca5b0b1b091ba992ba454c96b1ed
sboikov <sboikov@gridgain.com>
2016-11-10 18:26:07 +0300

    ignite-4154

--- modules/core/src/test/java/org/apache/ignite/testframework/junits/common/GridCommonAbstractTest.java
+++ modules/core/src/test/java/org/apache/ignite/testframework/junits/common/GridCommonAbstractTest.java
@@ -440,8 +440,21 @@ public abstract class GridCommonAbstractTest extends GridAbstractTest {
     protected void awaitPartitionMapExchange(boolean waitEvicts, boolean waitNode2PartUpdate) throws InterruptedException {
         long timeout = 30_000;
 
+        long startTime = -1;
+
+        Set<String> names = new HashSet<>();
+
         for (Ignite g : G.allGrids()) {
             IgniteKernal g0 = (IgniteKernal)g;
+
+            names.add(g0.configuration().getGridName());
+
+            if (startTime != -1) {
+                if (startTime != g0.context().discovery().gridStartTime())
+                    fail("Found nodes from different clusters [allNodes=" + names + ']');
+            }
+            else
+                startTime = g0.context().discovery().gridStartTime();
 
             for (IgniteCacheProxy<?, ?> c : g0.context().cache().jcaches()) {
                 CacheConfiguration cfg = c.context().config();
