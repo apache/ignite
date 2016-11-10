bac03cf6242828fbe88f58811b0029d95c6ae9e5
EdShangGG <eshangareev@gridgain.com>
2016-11-10 23:04:23 +0300

    GG-11676 Failed to reinitialize local partitions

--- modules/core/src/test/java/org/apache/ignite/testframework/junits/GridAbstractTest.java
+++ modules/core/src/test/java/org/apache/ignite/testframework/junits/GridAbstractTest.java
@@ -619,11 +619,16 @@ public abstract class GridAbstractTest extends TestCase {
             else
                 startGrid(i);
 
-        checkTopology(cnt);
+        if (checkTopology())
+            checkTopology(cnt);
 
         assert ignite != null;
 
         return ignite;
+    }
+
+    protected boolean checkTopology() {
+        return true;
     }
 
     /**
