b8a2b8ed81175887dd11d8f6b719c9f561d6a028
shtykh_roman <rshtykh@yahoo.com>
2016-11-16 15:41:15 +0700

    IGNITE-3066 Limited Redis protocol implementation. - Fixes #1212.
    Signed-off-by: Andrey Novikov <anovikov@apache.org>

--- modules/core/src/main/java/org/apache/ignite/internal/processors/rest/handlers/redis/exception/GridRedisGenericException.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/rest/handlers/redis/exception/GridRedisGenericException.java
@@ -15,24 +15,23 @@
  * limitations under the License.
  */
 
-package org.apache.ignite.internal.processors.rest.protocols.tcp;
+package org.apache.ignite.internal.processors.rest.handlers.redis.exception;
+
+import org.apache.ignite.IgniteCheckedException;
 
 /**
- * Type of message being parsed.
+ * Generic Redis protocol exception.
  */
-public enum GridClientPacketType {
-    /** Memcache protocol message. */
-    MEMCACHE,
+public class GridRedisGenericException extends IgniteCheckedException {
+    /** */
+    private static final long serialVersionUID = 0L;
 
-    /** Redis protocol message. */
-    REDIS,
-
-    /** Ignite handshake. */
-    IGNITE_HANDSHAKE,
-
-    /** Ignite handshake response. */
-    IGNITE_HANDSHAKE_RES,
-
-    /** Ignite message. */
-    IGNITE
+    /**
+     * Creates a generic exception with given error message.
+     *
+     * @param msg Error message.
+     */
+    public GridRedisGenericException(String msg) {
+        super(msg);
+    }
 }
