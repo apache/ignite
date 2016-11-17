218b699be0585c16a086bbeb6767d7c7a19e9ba0
tledkov-gridgain <tledkov@gridgain.com>
2016-11-17 10:34:52 +0500

    IGNITE-4239: add GridInternal annotaion for tasks instead of jobs.

---
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/compute/PublicThreadpoolStarvationTest.java
@@ -0,0 +1,166 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.processors.compute;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.ignite.IgniteCompute;
+import org.apache.ignite.IgniteDataStreamer;
+import org.apache.ignite.cache.CacheAtomicityMode;
+import org.apache.ignite.cache.CacheMode;
+import org.apache.ignite.cache.CachePeekMode;
+import org.apache.ignite.compute.ComputeTaskFuture;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.internal.IgniteEx;
+import org.apache.ignite.internal.binary.BinaryMarshaller;
+import org.apache.ignite.internal.processors.cache.GridCacheAbstractSelfTest;
+import org.apache.ignite.lang.IgniteRunnable;
+import org.apache.ignite.spi.communication.tcp.TcpCommunicationSpi;
+
+import static org.apache.ignite.cache.CacheAtomicityMode.ATOMIC;
+import static org.apache.ignite.cache.CacheMode.PARTITIONED;
+
+/**
+ * Base class of the tests to validate https://issues.apache.org/jira/browse/IGNITE-2310
+ */
+public class PublicThreadpoolStarvationTest extends GridCacheAbstractSelfTest {
+    /** Jobs count. */
+    private static final int JOBS_COUNT = 256;
+    /** Cache size. */
+    private static final int CACHE_SIZE = 32 * 1024;
+    /** Cache size. */
+    private static final String CACHE_NAME = "test";
+
+
+    /** {@inheritDoc} */
+    @Override protected long getTestTimeout() {
+        return 3600_000;
+    }
+
+    /** {@inheritDoc} */
+    @Override protected IgniteConfiguration getConfiguration(String gridName) throws Exception {
+        IgniteConfiguration cfg = super.getConfiguration(gridName);
+
+        ((TcpCommunicationSpi)cfg.getCommunicationSpi()).setSharedMemoryPort(-1);
+
+        cfg.setMarshaller(new BinaryMarshaller());
+
+        return cfg;
+    }
+
+    /** {@inheritDoc} */
+    @Override protected Class<?>[] indexedTypes() {
+        return new Class<?>[] {
+            Integer.class, String.class,
+        };
+    }
+
+    /** {@inheritDoc} */
+    @Override protected CacheAtomicityMode atomicityMode() {
+        return ATOMIC;
+    }
+
+    /** {@inheritDoc} */
+    @Override protected int gridCount() {
+        return 2;
+    }
+
+    /** {@inheritDoc} */
+    @Override protected CacheMode cacheMode() {
+        return PARTITIONED;
+    }
+
+    /** {@inheritDoc} */
+    @Override protected void beforeTestsStarted() throws Exception {
+        super.beforeTestsStarted();
+
+        info("Fill caches begin...");
+
+        fillCaches();
+
+        info("Caches are filled");
+    }
+
+    /** {@inheritDoc} */
+    @Override protected void afterTestsStopped() throws Exception {
+        grid(0).destroyCache("test");
+        super.afterTestsStopped();
+    }
+
+    /** {@inheritDoc} */
+    @Override protected void afterTest() throws Exception {
+        awaitPartitionMapExchange();
+
+        super.afterTest();
+    }
+
+    /** {@inheritDoc} */
+    @Override protected void beforeTest() throws Exception {
+        super.beforeTest();
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    private void fillCaches() throws Exception {
+        grid(0).createCache(CACHE_NAME);
+
+        try (
+            IgniteDataStreamer<Integer, String> streamer =
+                grid(0).dataStreamer(CACHE_NAME)) {
+
+            for (int i = 0; i < CACHE_SIZE; ++i)
+                streamer.addData(i, "Data " + i);
+        }
+
+        awaitPartitionMapExchange();
+    }
+
+    /**
+     *
+     * @throws Exception If failed.
+     */
+    public void testCacheSizeOnPublicThreadpoolStarvation() throws Exception {
+        IgniteCompute ic = grid(0).compute().withAsync();
+
+        List<ComputeTaskFuture> futs = new ArrayList<>();
+
+        for (int i = 0; i < JOBS_COUNT; ++i) {
+            ic.run(new IgniteRunnable() {
+                @Override public void run() {
+                    try {
+                        Thread.sleep(500);
+                    }
+                    catch (InterruptedException e) {
+                        // No-op.
+                    }
+                    grid(0).cache(CACHE_NAME).size(new CachePeekMode[0]);
+                }
+            });
+
+            futs.add(ic.future());
+        }
+
+        for (int i = 0; i < futs.size(); ++i) {
+            if (i % 10 == 0)
+                System.out.println("Wait for future: " + i);
+
+            futs.get(i).get();
+        }
+    }
+}
