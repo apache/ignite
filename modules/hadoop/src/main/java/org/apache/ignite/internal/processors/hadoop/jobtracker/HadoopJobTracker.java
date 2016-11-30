101afc2035567ee1ad1c0a20d0d11ed6dffd2fd4
devozerov <vozerov@gridgain.com>
2016-11-30 14:21:45 +0300

    Debug output.

--- modules/hadoop/src/main/java/org/apache/ignite/internal/processors/hadoop/jobtracker/HadoopJobTracker.java
+++ modules/hadoop/src/main/java/org/apache/ignite/internal/processors/hadoop/jobtracker/HadoopJobTracker.java
@@ -1331,6 +1331,8 @@ public class HadoopJobTracker extends HadoopComponent {
                         catch (IgniteCheckedException e) {
                             err = e;
                         }
+
+                        System.out.println(Thread.currentThread().getName() + ": Final flush finished: " + System.currentTimeMillis());
                     }
 
                     transform(jobId, new RemoveMappersProcessor(prev, taskInfo.inputSplit(), err));
