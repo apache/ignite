66bd614c1b936408411bffa455c0768c9e156766
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-12-01 12:59:20 +0300

    wip

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Cache/Query/CacheDmlQueriesTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Cache/Query/CacheDmlQueriesTest.cs
@@ -86,7 +86,7 @@ namespace Apache.Ignite.Core.Tests.Cache.Query
                                                "values (1, 2, 3, 'John'), (4, 5, 6, 'Mary')")).GetAll();
 
             Assert.AreEqual(1, res.Count);
-            Assert.AreEqual(2, res[0]);  // 2 affected rows
+            Assert.AreEqual(2, res[0][0]);  // 2 affected rows
 
             var foos = cache.OrderBy(x => x.Key).ToArray();
 
