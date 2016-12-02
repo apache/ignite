d8019a10a82145a027db33985689e873b05f0d4f
tledkov-gridgain <tledkov@gridgain.com>
2016-12-02 11:45:58 +0300

    IGNITE-4263: add debug

--- modules/hadoop/src/main/java/org/apache/ignite/internal/processors/hadoop/shuffle/mem/offheap/OffheapMemoryManager.java
+++ modules/hadoop/src/main/java/org/apache/ignite/internal/processors/hadoop/shuffle/mem/offheap/OffheapMemoryManager.java
@@ -57,16 +57,8 @@ public class OffheapMemoryManager extends MemoryManager {
     @Override public void close() {
         if (closed.compareAndSet(false, true)) {
 
-            int last = allPages.size() - 1;
-            int i = 0;
-
-            for (OffheapPage page : allPages) {
-                if (i == last)
-                    break;
-
+            for (OffheapPage page : allPages)
                 mem.release(page.ptr(), page.size());
-                i++;
-            }
 
             allPages.clear();
         }
@@ -89,23 +81,49 @@ public class OffheapMemoryManager extends MemoryManager {
         return ptr;
     }
 
+    /**
+     * @param ptr Pointer.
+     */
+    private void check(long ptr) {
+        for (OffheapPage page : allPages) {
+            if (ptr >= page.ptr() && ptr < page.ptr() + page.size())
+                return;
+        }
+
+        assert false : "Invalid ptr=" + ptr + ", pages=" + allPages.size();
+    }
+
     /** {@inheritDoc} */
     @Override public void copyMemory(long srcPtr, long destPtr, long len) {
+        check(srcPtr);
+        check(destPtr);
+        check(srcPtr + len);
+        check(destPtr + len);
+
         mem.copyMemory(srcPtr, destPtr, len);
     }
 
     /** {@inheritDoc} */
     @Override public void copyMemory(byte[] srcBuf, int srcOff, long destPtr, long len) {
+        check(destPtr);
+        check(destPtr + len);
+
         GridUnsafe.copyMemory(srcBuf, GridUnsafe.BYTE_ARR_OFF + srcOff, null, destPtr, len);
     }
 
     /** {@inheritDoc} */
     @Override public void copyMemory(long srcPtr, byte[] dstBuf, int dstOff, long len) {
+        check(srcPtr);
+        check(srcPtr + len);
+
         GridUnsafe.copyMemory(null, srcPtr, dstBuf, GridUnsafe.BYTE_ARR_OFF + dstOff, len);
     }
 
     /** {@inheritDoc} */
     @Override public Bytes bytes(long ptr, long len) {
+        check(ptr);
+        check(ptr + len);
+
         byte [] buf = new byte[(int)len];
 
         return new Bytes(mem.readBytes(ptr, buf, 0, (int)len), 0, (int)len);
@@ -113,86 +131,122 @@ public class OffheapMemoryManager extends MemoryManager {
 
     /** {@inheritDoc} */
     @Override public long readLongVolatile(long ptr) {
+        check(ptr);
+
         return mem.readLongVolatile(ptr);
     }
 
     /** {@inheritDoc} */
     @Override public void writeLongVolatile(long ptr, long v) {
+        check(ptr);
+
         mem.writeLongVolatile(ptr, v);
     }
 
     /** {@inheritDoc} */
     @Override public boolean casLong(long ptr, long exp, long v) {
+        check(ptr);
+
         return mem.casLong(ptr, exp, v);
     }
 
     /** {@inheritDoc} */
     @Override public long readLong(long ptr) {
+        check(ptr);
+
         return mem.readLong(ptr);
     }
 
     /** {@inheritDoc} */
     @Override public void writeLong(long ptr, long v) {
+        check(ptr);
+
         mem.writeLong(ptr, v);
     }
 
     /** {@inheritDoc} */
     @Override public int readInt(long ptr) {
+        check(ptr);
+
         return mem.readInt(ptr);
     }
 
     /** {@inheritDoc} */
     @Override public void writeInt(long ptr, int v) {
+        check(ptr);
+
         mem.writeInt(ptr, v);
     }
 
     /** {@inheritDoc} */
     @Override public float readFloat(long ptr) {
+        check(ptr);
+
         return mem.readFloat(ptr);
     }
 
     /** {@inheritDoc} */
     @Override public void writeFloat(long ptr, float v) {
+        check(ptr);
+
         mem.writeFloat(ptr, v);
     }
 
     /** {@inheritDoc} */
     @Override public double readDouble(long ptr) {
+        check(ptr);
+
         return mem.readDouble(ptr);
     }
 
     /** {@inheritDoc} */
     @Override public void writeDouble(long ptr, double v) {
+        check(ptr);
+
         mem.writeDouble(ptr, v);
     }
 
     /** {@inheritDoc} */
     @Override public short readShort(long ptr) {
+        check(ptr);
+
         return mem.readShort(ptr);
     }
 
     /** {@inheritDoc} */
     @Override public void writeShort(long ptr, short v) {
+        check(ptr);
+
         mem.writeShort(ptr, v);
     }
 
     /** {@inheritDoc} */
     @Override public byte readByte(long ptr) {
+        check(ptr);
+
         return mem.readByte(ptr);
     }
 
     /** {@inheritDoc} */
     @Override public void writeByte(long ptr, byte v) {
+        check(ptr);
+
         mem.writeByte(ptr, v);
     }
 
     /** {@inheritDoc} */
     @Override public void writeBytes(long ptr, byte[] arr, int off, int len) {
+        check(ptr);
+        check(ptr + len);
+
         mem.writeBytes(ptr, arr, off, len);
     }
 
     /** {@inheritDoc} */
     @Override public byte[] readBytes(long ptr, byte[] arr, int off, int len) {
+        check(ptr);
+        check(ptr + len);
+
         return mem.readBytes(ptr, arr, off, len);
     }
 }
