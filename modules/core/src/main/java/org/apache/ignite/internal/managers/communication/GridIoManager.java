f7057eb948cef92c794fd702df29ecfd0637af1a
Yakov Zhdanov <yzhdanov@gridgain.com>
2016-12-06 11:23:27 +0700

    minor

--- modules/core/src/main/java/org/apache/ignite/internal/managers/communication/GridIoManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/managers/communication/GridIoManager.java
@@ -806,8 +806,7 @@ public class GridIoManager extends GridManagerAdapter<CommunicationSpi<Serializa
 
         if (ctx.config().isUseStripedPool() &&
             plc == GridIoPolicy.SYSTEM_POOL &&
-            msg.partition() != Integer.MIN_VALUE &&
-            msg.message().directType() != 58 // TODO Query request should go to former pool.
+            msg.partition() != Integer.MIN_VALUE
             ) {
             ctx.getStripedExecutorService().execute(msg.partition(), c);
 
