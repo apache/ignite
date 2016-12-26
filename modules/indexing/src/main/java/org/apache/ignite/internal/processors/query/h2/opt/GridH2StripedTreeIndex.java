c77ae7bcdc8c1ee93f83ba03d564038010904682
AMRepo <andrey.mashenkov@gmail.com>
2016-12-26 19:19:27 +0300

    Minors

--- modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/opt/GridH2StripedTreeIndex.java
+++ modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/opt/GridH2StripedTreeIndex.java
@@ -830,7 +830,7 @@ public class GridH2StripedTreeIndex extends GridH2AbstractTreeIndex {
             else {
                 // Affinity key is not provided or is not the same in upper and lower bounds, we have to broadcast.
                 if (broadcastSegments == null)
-                    broadcastSegments = broadcastSegments(qctx, cctx); //TODO: replace with list of segments
+                    broadcastSegments = broadcastSegments(qctx, cctx);
 
                 segmentKeys = broadcastSegments;
             }
