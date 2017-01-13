4505066481d4fff601a14d344dc8059f5dbe73de
Ivan Rakov <ivan.glukos@gmail.com>
2017-01-14 00:32:08 +0300

    GG-11595: + serialVersionUid

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/CacheStoppedException.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/CacheStoppedException.java
@@ -6,6 +6,8 @@ import org.apache.ignite.IgniteCheckedException;
  * Used to report attempts of accessing stopped cache.
  */
 public class CacheStoppedException extends IgniteCheckedException {
+    private static final long serialVersionUID = 0L;
+
     public CacheStoppedException(String cacheName) {
         super("Failed to perform cache operation (cache is stopped): " + cacheName);
     }
