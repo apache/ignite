e8958390441bde0325d81379e2e49736c1a4b325
iveselovskiy <iveselovskiy@gridgain.com>
2017-01-16 13:45:35 +0300

    Refactoring to avoid Ignite interface of remote nodes. .

---
+++ modules/core/src/test/java/org/apache/ignite/testframework/junits/multijvm/IgniteNodeProxyBase.java
@@ -0,0 +1,7 @@
+package org.apache.ignite.testframework.junits.multijvm;
+
+/**
+ * Created by ivan on 13.01.17.
+ */
+public class IgniteNodeProxyBase {
+}
---
+++ modules/core/src/test/java/org/apache/ignite/testframework/junits/multijvm/NodeProcessParameters.java
@@ -0,0 +1,13 @@
+package org.apache.ignite.testframework.junits.multijvm;
+
+import java.io.File;
+import java.util.Map;
+
+/**
+*
+*/
+public class NodeProcessParameters {
+    private final Map<String, String> env;
+    private final File processDir;
+    private final File workDir;
+}
