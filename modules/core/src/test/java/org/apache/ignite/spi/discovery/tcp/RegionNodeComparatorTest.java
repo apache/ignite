f36f07da21ca51bf2585fb80b82239541a3a16d6
Alexander Menshikov <sharplermc@gmail.com>
2017-01-27 18:02:30 +0300

    Cleanup code in RegionNodeComparatorTest

--- modules/core/src/test/java/org/apache/ignite/spi/discovery/tcp/RegionNodeComparatorTest.java
+++ modules/core/src/test/java/org/apache/ignite/spi/discovery/tcp/RegionNodeComparatorTest.java
@@ -45,23 +45,22 @@ public class RegionNodeComparatorTest extends GridCommonAbstractTest {
         final Supplier<TcpDiscoveryNode> factory = new NodeFactory();
 
         ComparatorTester.Pair<TcpDiscoveryNode> pair = tester.testEquals(factory, comparator);
-        if (pair!=null)
+        if (pair != null)
             fail("RegionNodeComparator don't see equals in two same node " + pairToString(pair));
         pair = tester.testSymmetry(factory, comparator);
-        if (pair!=null)
+        if (pair != null)
             fail("RegionNodeComparator don't work symmetry in two node " + pairToString(pair));
     }
 
     /**
-     *
      * @param pair Pair of nodes.
      * @return Debug string.
      */
     private static String pairToString(ComparatorTester.Pair<TcpDiscoveryNode> pair) {
-        return "[firstNodeInternalOrder="+pair.first.internalOrder()+
-                ", firstNodeRegionId="+pair.first.getAttributes().get("CLUSTER_REGION_ID")+
-                ", secondNodeInternalOrder="+pair.second.internalOrder()+
-                ", secondNodeRegionId="+pair.second.getAttributes().get("CLUSTER_REGION_ID")+']';
+        return "[firstNodeInternalOrder=" + pair.first.internalOrder() +
+                ", firstNodeRegionId=" + pair.first.getAttributes().get("CLUSTER_REGION_ID") +
+                ", secondNodeInternalOrder=" + pair.second.internalOrder() +
+                ", secondNodeRegionId=" + pair.second.getAttributes().get("CLUSTER_REGION_ID") + ']';
     }
 
     /**
@@ -71,22 +70,22 @@ public class RegionNodeComparatorTest extends GridCommonAbstractTest {
      */
     public void testLogic() throws Exception {
         final int REGIONS = 13;
-        final int N = REGIONS*100;
+        final int N = REGIONS * 100;
         ArrayList<TcpDiscoveryNode> nodes = new ArrayList<>(N);
         final Comparator<TcpDiscoveryNode> comparator = new RegionNodeComparator();
 
         final NodeFactory factory = new NodeFactory();
 
         for (int i = 0; i < N; i++)
-            nodes.add(factory.get(i%REGIONS));
+            nodes.add(factory.get(i % REGIONS));
 
         ArrayList<TcpDiscoveryNode> sortedNodes = new ArrayList<>(nodes);
         sortedNodes.sort(comparator);
 
         int i = 0;
         for (int r = 1; r <= REGIONS; r++) {
-            for (int j = 0; j < N/REGIONS; j++) {
-                assertEquals(r==REGIONS?0:r, sortedNodes.get(i).internalOrder() % REGIONS);
+            for (int j = 0; j < N / REGIONS; j++) {
+                assertEquals(r == REGIONS ? 0 : r, sortedNodes.get(i).internalOrder() % REGIONS);
                 i++;
             }
         }
@@ -102,7 +101,6 @@ public class RegionNodeComparatorTest extends GridCommonAbstractTest {
         final Random random = new Random();
 
         /**
-         *
          * @return Node without id and CLUSTER_REGION_ID.
          */
         private TcpDiscoveryNode getRaw() {
@@ -123,7 +121,6 @@ public class RegionNodeComparatorTest extends GridCommonAbstractTest {
         }
 
         /**
-         *
          * @return Node with sequential id and random CLUSTER_REGION_ID.
          */
         @Override
@@ -136,7 +133,6 @@ public class RegionNodeComparatorTest extends GridCommonAbstractTest {
         }
 
         /**
-         *
          * @param region CLUSTER_REGION_ID.
          * @return Node with sequential id and specific CLUSTER_REGION_ID.
          */
@@ -154,7 +150,9 @@ public class RegionNodeComparatorTest extends GridCommonAbstractTest {
      * Class for testing comparators. In the future we may add new comparator, and then may use this class.
      */
     private static class ComparatorTester<T> {
-        /** The number of experiments */
+        /**
+         * The number of experiments
+         */
         final int N;
 
         /**
@@ -171,19 +169,19 @@ public class RegionNodeComparatorTest extends GridCommonAbstractTest {
          * @param comparator
          * @return Return null if okay, or pair of values which make error if any.
          */
-        Pair<T> testEquals(Supplier<T> factory, Comparator<T> comparator){
+        Pair<T> testEquals(Supplier<T> factory, Comparator<T> comparator) {
             HashMap<T, T> set = new HashMap<>();
 
-            for (int i = 0 ; i < N; i++) {
+            for (int i = 0; i < N; i++) {
                 T obj = factory.get();
                 T exist = set.putIfAbsent(obj, obj);
-                if (exist!=null) {
-                    if (comparator.compare(obj, exist)!=0)
+                if (exist != null) {
+                    if (comparator.compare(obj, exist) != 0)
                         return new Pair<>(obj, exist);
-                    if (comparator.compare(exist, obj)!=0)
+                    if (comparator.compare(exist, obj) != 0)
                         return new Pair<>(exist, obj);
                 }
-                if (comparator.compare(obj, obj)!=0)
+                if (comparator.compare(obj, obj) != 0)
                     return new Pair<>(obj, obj);
             }
 
@@ -192,14 +190,15 @@ public class RegionNodeComparatorTest extends GridCommonAbstractTest {
 
         /**
          * Test condition if x is greater than y then y is less than x.
+         *
          * @param factory
          * @param comparator
          * @return Return null if okay, or pair of values which make error if any.
          */
-        Pair<T> testSymmetry(Supplier<T> factory, Comparator<T> comparator){
+        Pair<T> testSymmetry(Supplier<T> factory, Comparator<T> comparator) {
             ArrayList<T> list = new ArrayList<>();
 
-            for (int i = 0 ; i < N; i++) {
+            for (int i = 0; i < N; i++) {
                 list.add(factory.get());
             }
 
@@ -207,7 +206,7 @@ public class RegionNodeComparatorTest extends GridCommonAbstractTest {
                 for (int j = 0; j < N; j++) {
                     int left = comparator.compare(list.get(i), list.get(j));
                     int right = comparator.compare(list.get(j), list.get(i));
-                    if (!((left<0 && right>0) || (left>0 && right<0) || (left==0 && right==0)))
+                    if (!((left < 0 && right > 0) || (left > 0 && right < 0) || (left == 0 && right == 0)))
                         return new Pair<>(list.get(i), list.get(j));
                 }
 
