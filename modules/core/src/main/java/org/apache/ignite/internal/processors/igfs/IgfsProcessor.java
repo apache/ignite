680f1b4545cd846baa6658fedf26edb6c1ebdc6b
devozerov <ppozerov@gmail.com>
2017-02-02 18:37:09 +0300

    Minors.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/igfs/IgfsProcessor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/igfs/IgfsProcessor.java
@@ -17,7 +17,6 @@
 
 package org.apache.ignite.internal.processors.igfs;
 
-import java.util.HashSet;
 import org.apache.ignite.IgniteCheckedException;
 import org.apache.ignite.IgniteFileSystem;
 import org.apache.ignite.cache.affinity.AffinityKeyMapper;
@@ -48,6 +47,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
