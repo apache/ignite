68743dfb62ff0089746c0c4e786fb84bb52b20b1
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-02-03 15:16:58 +0300

    IGNITE-4619 .NET: TransactionScope example

--- modules/platforms/dotnet/examples/Apache.Ignite.Examples/Datagrid/TransactionExample.cs
+++ modules/platforms/dotnet/examples/Apache.Ignite.Examples/Datagrid/TransactionExample.cs
@@ -18,7 +18,9 @@
 namespace Apache.Ignite.Examples.Datagrid
 {
     using System;
+    using System.Transactions;
     using Apache.Ignite.Core;
+    using Apache.Ignite.Core.Cache;
     using Apache.Ignite.Core.Cache.Configuration;
     using Apache.Ignite.Core.Transactions;
     using Apache.Ignite.ExamplesDll.Binary;
@@ -59,18 +61,7 @@ namespace Apache.Ignite.Examples.Datagrid
                     AtomicityMode = CacheAtomicityMode.Transactional
                 });
 
-                // Clean up caches on all nodes before run.
-                cache.Clear();
-
-                // Initialize.
-                cache.Put(1, new Account(1, 100));
-                cache.Put(2, new Account(2, 200));
-
-                Console.WriteLine();
-                Console.WriteLine(">>> Accounts before transfer: ");
-                Console.WriteLine(">>>     " + cache.Get(1));
-                Console.WriteLine(">>>     " + cache.Get(2));
-                Console.WriteLine();
+                InitAccounts(cache);
 
                 // Transfer money between accounts in a single transaction.
                 using (var tx = cache.Ignite.GetTransactions().TxStart(TransactionConcurrency.Pessimistic,
@@ -88,18 +79,63 @@ namespace Apache.Ignite.Examples.Datagrid
                     tx.Commit();
                 }
 
-                Console.WriteLine(">>> Transfer finished.");
+                DisplayAccounts(cache);
 
-                Console.WriteLine();
-                Console.WriteLine(">>> Accounts after transfer: ");
-                Console.WriteLine(">>>     " + cache.Get(1));
-                Console.WriteLine(">>>     " + cache.Get(2));
-                Console.WriteLine();
+                InitAccounts(cache);
+
+                // Do the same transaction with TransactionScope API.
+                using (var ts = new TransactionScope())
+                {
+                    Account acc1 = cache.Get(1);
+                    Account acc2 = cache.Get(2);
+
+                    acc1.Balance += 100;
+                    acc2.Balance -= 100;
+
+                    cache.Put(1, acc1);
+                    cache.Put(2, acc2);
+
+                    ts.Complete();
+                }
+
+                DisplayAccounts(cache);
             }
 
             Console.WriteLine();
             Console.WriteLine(">>> Example finished, press any key to exit ...");
             Console.ReadKey();
         }
+
+        private static void DisplayAccounts(ICache<int, Account> cache)
+        {
+            Console.WriteLine(">>> Transfer finished.");
+
+            Console.WriteLine();
+            Console.WriteLine(">>> Accounts after transfer: ");
+            Console.WriteLine(">>>     " + cache.Get(1));
+            Console.WriteLine(">>>     " + cache.Get(2));
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Initializes account balance.
+        /// </summary>
+        private static void InitAccounts(ICache<int, Account> cache)
+        {
+            // Clean up caches on all nodes before run.
+            cache.Clear();
+
+            // Initialize.
+            cache.Put(1, new Account(1, 100));
+            cache.Put(2, new Account(2, 200));
+
+            Console.WriteLine();
+            Console.WriteLine(">>> Accounts before transfer: ");
+            Console.WriteLine(">>>     " + cache.Get(1));
+            Console.WriteLine(">>>     " + cache.Get(2));
+            Console.WriteLine();
+        }
+
+
     }
 }
