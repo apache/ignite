af1cc0d81e36f4d7572324c7b3b234efbf6a8fd3
Yury Babak <ybabak@gridgain.com>
2017-02-07 15:53:23 +0300

    IGN-6530: added VectorArrayStorageTest

--- modules/core/src/test/java/org/apache/ignite/math/impls/VectorArrayStorageTest.java
+++ modules/core/src/test/java/org/apache/ignite/math/impls/VectorArrayStorageTest.java
@@ -1,27 +1,98 @@
 package org.apache.ignite.math.impls;
 
+import org.junit.Before;
 import org.junit.Test;
 
-/** */
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for {@link VectorArrayStorage}
+ */
 public class VectorArrayStorageTest {
-    /** */ @Test
-    public void get() throws Exception { // TODO write test
+    /** */
+    private static final int STORAGE_SIZE = 100;
 
+    /** */
+    private static final String WRONG_ATTRIBUTE_VALUE = "wrong attribute value";
+
+    /** */
+    private static final String NULL_DATA_ELEMENT = "null data element";
+
+    /** */
+    private static final String WRONG_DATA_ELEMENT = "wrong data element";
+
+    /** */
+    private static final double EXPECTED_DELTA = 0d;
+
+    /** */
+    private static final String NULL_DATA_STORAGE = "null data storage";
+
+    /** */
+    private static final String WRONG_DATA_SIZE = "wrong data size";
+
+    /** */
+    private static final String UNEXPECTED_DATA_VALUE = "unexpected data value";
+
+    /** */
+    private VectorArrayStorage testStorage;
+
+    /** */
+    @Before
+    public void setup() {
+        testStorage = new VectorArrayStorage(STORAGE_SIZE);
     }
 
-    /** */ @Test
-    public void set() throws Exception { // TODO write test
+    /** */
+    @Test
+    public void get() throws Exception {
+        fillStorage(STORAGE_SIZE);
 
+        for (int i = 0; i < STORAGE_SIZE; i++)
+            assertNotNull(NULL_DATA_ELEMENT, testStorage.get(i));
     }
 
-    /** */ @Test
-    public void isArrayBased() throws Exception { // TODO write test
+    /** */
+    @Test
+    public void set() throws Exception {
+        for (int i = 0; i < STORAGE_SIZE; i++) {
+            double random = Math.random();
 
+            testStorage.set(i, random);
+
+            assertEquals(WRONG_DATA_ELEMENT, testStorage.get(i), random, EXPECTED_DELTA);
+        }
     }
 
-    /** */ @Test
-    public void data() throws Exception { // TODO write test
+    /** */
+    @Test
+    public void isArrayBased() throws Exception {
+        assertTrue(WRONG_ATTRIBUTE_VALUE, testStorage.isArrayBased());
 
+        assertTrue(WRONG_ATTRIBUTE_VALUE, new VectorArrayStorage().isArrayBased());
+    }
+
+    /** */
+    @Test
+    public void data() throws Exception {
+        assertNotNull(NULL_DATA_STORAGE, testStorage.data());
+
+        assertEquals(WRONG_DATA_SIZE, testStorage.data().length, STORAGE_SIZE);
+
+        assertTrue(UNEXPECTED_DATA_VALUE, Arrays.equals(testStorage.data(), new double[STORAGE_SIZE]));
+
+        assertNull(UNEXPECTED_DATA_VALUE, new VectorArrayStorage().data());
+    }
+
+    /**
+     * fill storage by random doubles
+     *
+     * @param size storage size
+     */
+    private void fillStorage(int size) {
+        for (int i = 0; i < size; i++)
+            testStorage.set(i, Math.random());
     }
 
 }
