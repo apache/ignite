f0dcbbdecd10ee9afee3cb0c5a96bb1540d0c06a
Max Kozlov <dreamx.max@gmail.com>
2017-02-13 12:20:51 +0300

    Delete CLOCK mode in CacheAtomicWriteOrderMode.

--- modules/core/src/main/java/org/apache/ignite/cache/CacheAtomicWriteOrderMode.java
+++ modules/core/src/main/java/org/apache/ignite/cache/CacheAtomicWriteOrderMode.java
@@ -23,26 +23,10 @@ import org.jetbrains.annotations.Nullable;
  * Cache write ordering mode. This enumeration is taken into account only in {@link CacheAtomicityMode#ATOMIC}
  * atomicity mode. Write ordering mode determines which node assigns the write version, sender or the primary node.
  * <p>
- * For example, {@link #CLOCK} mode assigns write versions on a sender node which generally leads to better
- * performance in {@link CacheWriteSynchronizationMode#FULL_SYNC} synchronization mode, since in this case
- * sender can send write requests to primary and backups at the same time. Otherwise, if ordering mode is
- * {@link #PRIMARY}, it would only send request to primary node, which in turn will assign write version
+ * For example, {@link #PRIMARY} mode would only send request to primary node, which in turn will assign write version
  * and forward it to backups.
- * <p>
- * {@link #CLOCK} mode will be automatically configured only with {@link CacheWriteSynchronizationMode#FULL_SYNC}
- * write synchronization mode, as for other synchronization modes it does not render better performance.
  */
 public enum CacheAtomicWriteOrderMode {
-    /**
-     * In this mode, write versions are assigned on a sender node which generally leads to better
-     * performance in {@link CacheWriteSynchronizationMode#FULL_SYNC} synchronization mode, since in this case
-     * sender can send write requests to primary and backups at the same time.
-     * <p>
-     * This mode will be automatically configured only with {@link CacheWriteSynchronizationMode#FULL_SYNC}
-     * write synchronization mode, as for other synchronization modes it does not render better performance.
-     */
-    CLOCK,
-
     /**
      * Cache version is assigned only on primary node. This means that sender will only send write request
      * to primary node, which in turn will assign write version and forward it to backups.
