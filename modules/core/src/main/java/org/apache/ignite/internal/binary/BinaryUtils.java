353e4a3be8f328e2530daf6947aeb9f42ba50cc7
tledkov-gridgain <tledkov@gridgain.com>
2017-02-14 16:54:56 +0300

    IGNITE-4673: fix review issue

--- modules/core/src/main/java/org/apache/ignite/internal/binary/BinaryUtils.java
+++ modules/core/src/main/java/org/apache/ignite/internal/binary/BinaryUtils.java
@@ -1479,9 +1479,6 @@ public class BinaryUtils {
         throws BinaryObjectException {
         Class cls;
 
-        if (typeId == GridBinaryMarshaller.OBJECT_TYPE_ID)
-            return Object.class;
-
         if (typeId != GridBinaryMarshaller.UNREGISTERED_TYPE_ID)
             cls = ctx.descriptorForTypeId(true, typeId, ldr, false).describedClass();
         else {
@@ -1513,9 +1510,6 @@ public class BinaryUtils {
     public static Class resolveClass(BinaryContext ctx, int typeId, @Nullable String clsName,
         @Nullable ClassLoader ldr, boolean deserialize) {
         Class cls;
-
-        if (typeId == GridBinaryMarshaller.OBJECT_TYPE_ID)
-            return Object.class;
 
         if (typeId != GridBinaryMarshaller.UNREGISTERED_TYPE_ID)
             cls = ctx.descriptorForTypeId(true, typeId, ldr, deserialize).describedClass();
