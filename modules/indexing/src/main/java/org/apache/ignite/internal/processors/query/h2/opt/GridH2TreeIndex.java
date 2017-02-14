a6990c667c33786ff7dc172b334e9f62775681ce
Andrey V. Mashenkov <andrey.mashenkov@gmail.com>
2017-02-14 08:17:26 +0300

    Minor.

--- modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/opt/GridH2TreeIndex.java
+++ modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/opt/GridH2TreeIndex.java
@@ -137,7 +137,7 @@ public class GridH2TreeIndex extends GridH2IndexBase {
 
     /** {@inheritDoc} */
     protected final ConcurrentNavigableMap<GridSearchRowPointer, GridH2Row> treeForRead(int segment) {
-        assert segment == 0;
+        assert segment == -1;
 
         if (!isSnapshotEnabled())
             return tree;
