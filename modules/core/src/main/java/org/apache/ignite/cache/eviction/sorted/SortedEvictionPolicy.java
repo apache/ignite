19566fbc0a683e5a72cf91ee0f8de987c524c01d
voipp <aleksaero@gmail.com>
2017-02-15 11:54:48 +0300

    IGNITE-2552 EvictionPolicies refactored, logic changed

--- modules/core/src/main/java/org/apache/ignite/cache/eviction/sorted/SortedEvictionPolicy.java
+++ modules/core/src/main/java/org/apache/ignite/cache/eviction/sorted/SortedEvictionPolicy.java
@@ -29,8 +29,8 @@ import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
+import org.apache.ignite.cache.eviction.AbstractEvictionPolicy;
 import org.apache.ignite.cache.eviction.EvictableEntry;
-import org.apache.ignite.cache.eviction.EvictionPolicy;
 import org.apache.ignite.internal.util.GridConcurrentSkipListSet;
 import org.apache.ignite.internal.util.typedef.internal.A;
 import org.jetbrains.annotations.Nullable;
@@ -59,21 +59,9 @@ import static org.apache.ignite.configuration.CacheConfiguration.DFLT_CACHE_SIZE
  * <p>
  * User defined comparator should implement {@link Serializable} interface.
  */
-public class SortedEvictionPolicy<K, V> implements EvictionPolicy<K, V>, SortedEvictionPolicyMBean, Externalizable {
+public class SortedEvictionPolicy<K, V> extends AbstractEvictionPolicy<K, V> implements SortedEvictionPolicyMBean, Externalizable {
     /** */
     private static final long serialVersionUID = 0L;
-
-    /** Maximum size. */
-    private volatile int max = DFLT_CACHE_SIZE;
-
-    /** Batch size. */
-    private volatile int batchSize = 1;
-
-    /** Max memory size. */
-    private volatile long maxMemSize;
-
-    /** Memory size. */
-    private final LongAdder8 memSize = new LongAdder8();
 
     /** Comparator. */
     private Comparator<Holder<K, V>> comp;
@@ -118,11 +106,8 @@ public class SortedEvictionPolicy<K, V> implements EvictionPolicy<K, V>, SortedE
      * @param comp Entries comparator.
      */
     public SortedEvictionPolicy(int max, int batchSize, @Nullable Comparator<EvictableEntry<K, V>> comp) {
-        A.ensure(max >= 0, "max >= 0");
-        A.ensure(batchSize > 0, "batchSize > 0");
-
-        this.max = max;
-        this.batchSize = batchSize;
+        setMaxSize(max);
+        setBatchSize(batchSize);
         this.comp = comp == null ? new DefaultHolderComparator<K, V>() : new HolderComparator<>(comp);
         this.set = new GridConcurrentSkipListSetEx<>(this.comp);
     }
@@ -135,60 +120,6 @@ public class SortedEvictionPolicy<K, V> implements EvictionPolicy<K, V>, SortedE
     public SortedEvictionPolicy(@Nullable Comparator<EvictableEntry<K, V>> comp) {
         this.comp = comp == null ? new DefaultHolderComparator<K, V>() : new HolderComparator<>(comp);
         this.set = new GridConcurrentSkipListSetEx<>(this.comp);
-    }
-
-    /**
-     * Gets maximum allowed size of cache before entry will start getting evicted.
-     *
-     * @return Maximum allowed size of cache before entry will start getting evicted.
-     */
-    @Override public int getMaxSize() {
-        return max;
-    }
-
-    /**
-     * Sets maximum allowed size of cache before entry will start getting evicted.
-     *
-     * @param max Maximum allowed size of cache before entry will start getting evicted.
-     */
-    @Override public void setMaxSize(int max) {
-        A.ensure(max >= 0, "max >= 0");
-
-        this.max = max;
-    }
-
-    /** {@inheritDoc} */
-    @Override public int getBatchSize() {
-        return batchSize;
-    }
-
-    /** {@inheritDoc} */
-    @Override public void setBatchSize(int batchSize) {
-        A.ensure(batchSize > 0, "batchSize > 0");
-
-        this.batchSize = batchSize;
-    }
-
-    /** {@inheritDoc} */
-    @Override public int getCurrentSize() {
-        return set.sizex();
-    }
-
-    /** {@inheritDoc} */
-    @Override public long getMaxMemorySize() {
-        return maxMemSize;
-    }
-
-    /** {@inheritDoc} */
-    @Override public void setMaxMemorySize(long maxMemSize) {
-        A.ensure(maxMemSize >= 0, "maxMemSize >= 0");
-
-        this.maxMemSize = maxMemSize;
-    }
-
-    /** {@inheritDoc} */
-    @Override public long getCurrentMemorySize() {
-        return memSize.longValue();
     }
 
     /**
@@ -205,31 +136,11 @@ public class SortedEvictionPolicy<K, V> implements EvictionPolicy<K, V>, SortedE
         return Collections.unmodifiableCollection(cp);
     }
 
-    /** {@inheritDoc} */
-    @Override public void onEntryAccessed(boolean rmv, EvictableEntry<K, V> entry) {
-        if (!rmv) {
-            if (!entry.isCached())
-                return;
-
-            if (touch(entry))
-                shrink();
-        }
-        else {
-            Holder<K, V> holder = entry.removeMeta();
-
-            if (holder != null) {
-                removeHolder(holder);
-
-                memSize.add(-entry.size());
-            }
-        }
-    }
-
     /**
      * @param entry Entry to touch.
      * @return {@code True} if backed queue has been changed by this call.
      */
-    private boolean touch(EvictableEntry<K, V> entry) {
+    @Override protected boolean touch(EvictableEntry<K, V> entry) {
         Holder<K, V> holder = entry.meta();
 
         // Entry has not been added yet to backed queue.
@@ -245,12 +156,12 @@ public class SortedEvictionPolicy<K, V> implements EvictionPolicy<K, V>, SortedE
                 if (holder.order > 0) {
                     if (!entry.isCached()) {
                         // Was concurrently evicted, need to remove it from queue.
-                        removeHolder(holder);
+                        removeMeta(holder);
 
                         return false;
                     }
 
-                    memSize.add(entry.size());
+                    addToMemorySize(entry.size());
 
                     return true;
                 }
@@ -264,38 +175,9 @@ public class SortedEvictionPolicy<K, V> implements EvictionPolicy<K, V>, SortedE
         return false;
     }
 
-    /**
-     * Shrinks backed queue to maximum allowed size.
-     */
-    private void shrink() {
-        long maxMem = this.maxMemSize;
-
-        if (maxMem > 0) {
-            long startMemSize = memSize.longValue();
-
-            if (startMemSize >= maxMem)
-                for (long i = maxMem; i < startMemSize && memSize.longValue() > maxMem;) {
-                    int size = shrink0();
-
-                    if (size == -1)
-                        break;
-
-                    i += size;
-                }
-        }
-
-        int max = this.max;
-
-        if (max > 0) {
-            int startSize = set.sizex();
-
-            if (startSize >= max + (maxMem > 0 ? 1 : this.batchSize)) {
-                for (int i = max; i < startSize && set.sizex() > max; i++) {
-                    if (shrink0() == -1)
-                        break;
-                }
-            }
-        }
+    /** {@inheritDoc} */
+    @Override public int getCurrentSize() {
+        return set.size();
     }
 
     /**
@@ -303,7 +185,7 @@ public class SortedEvictionPolicy<K, V> implements EvictionPolicy<K, V>, SortedE
      *
      * @return number of bytes that was free. {@code -1} if queue is empty.
      */
-    private int shrink0() {
+    @Override protected int shrink0() {
         Holder<K, V> h = set.pollFirst();
 
         if (h == null)
@@ -313,10 +195,10 @@ public class SortedEvictionPolicy<K, V> implements EvictionPolicy<K, V>, SortedE
 
         EvictableEntry<K, V> entry = h.entry;
 
-        if (h.order > 0 && entry.removeMeta(h)) {
+        if (entry != null && h.order > 0 && entry.removeMeta(h)) {
             size = entry.size();
 
-            memSize.add(-size);
+            addToMemorySize(-size);
 
             if (!entry.evict())
                 touch(entry);
@@ -327,33 +209,31 @@ public class SortedEvictionPolicy<K, V> implements EvictionPolicy<K, V>, SortedE
 
     /** {@inheritDoc} */
     @Override public void writeExternal(ObjectOutput out) throws IOException {
-        out.writeInt(max);
-        out.writeInt(batchSize);
-        out.writeLong(maxMemSize);
+        super.writeExternal(out);
         out.writeObject(comp);
     }
 
     /** {@inheritDoc} */
     @SuppressWarnings("unchecked")
     @Override public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
-        max = in.readInt();
-        batchSize = in.readInt();
-        maxMemSize = in.readLong();
+        super.readExternal(in);
         comp = (Comparator<Holder<K, V>>)in.readObject();
     }
 
     /**
      * Removes holder from backed queue and marks holder as removed.
      *
-     * @param holder Holder.
+     * @param meta Holder.
      */
-    private void removeHolder(Holder<K, V> holder) {
+    @Override protected boolean removeMeta(Object meta) {
+        Holder<K, V> holder = (Holder<K, V>)meta;
+
         long order0 = holder.order;
 
         if (order0 > 0)
             holder.order = -order0;
 
-        set.remove(holder);
+        return set.remove(holder);
     }
 
     /**
