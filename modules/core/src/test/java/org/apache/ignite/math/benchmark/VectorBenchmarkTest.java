2dcb7f568116fed2eabd82ddb9dde2e9cbbce433
Oleg Ignatenko <oignatenko@gridgain.com>
2017-02-20 19:55:32 +0300

    IGN-6530 wip - benchmarks - added map methods -- verified with execution of benchmarks and report overview

--- modules/core/src/test/java/org/apache/ignite/math/benchmark/VectorBenchmarkTest.java
+++ modules/core/src/test/java/org/apache/ignite/math/benchmark/VectorBenchmarkTest.java
@@ -1,5 +1,6 @@
 package org.apache.ignite.math.benchmark;
 
+import java.util.function.BiConsumer;
 import java.util.function.Function;
 import org.apache.ignite.math.Vector;
 import org.apache.ignite.math.impls.DenseLocalOffHeapVector;
@@ -10,48 +11,52 @@ import static org.junit.Assert.*;
 
 /** */
 public class VectorBenchmarkTest {
-    // todo add benchmarks for map and fold methods (possibly a separate one),
-    //  and other methods in Vector and for other types of Vector and Matrix
+    // todo add benchmarks for other methods in Vector and for other types of Vector and Matrix
     /** */ @Test
     public void testDenseLocalOnHeapVector() throws Exception {
-        benchmark("DenseLocalOnHeapVector", DenseLocalOnHeapVector::new);
+        benchmark("DenseLocalOnHeapVector basic mix", DenseLocalOnHeapVector::new, this::basicMix);
+
+        benchmark("DenseLocalOnHeapVector map", DenseLocalOnHeapVector::new, this::mapMix);
     }
 
     /** */ @Test
     public void testDenseLocalOffHeapVector() throws Exception {
-        benchmark("DenseLocalOffHeapVector", DenseLocalOffHeapVector::new);
+        benchmark("DenseLocalOffHeapVector basic mix", DenseLocalOffHeapVector::new, this::basicMix);
+
+        benchmark("DenseLocalOffHeapVector map", DenseLocalOffHeapVector::new, this::mapMix);
     }
 
     /** */
-    private void benchmark(String namePrefix, Function<Integer, Vector> constructor) throws Exception {
+    private void benchmark(String namePrefix, Function<Integer, Vector> constructor,
+        BiConsumer<Integer, Function<Integer, Vector>> consumer) throws Exception {
         assertNotNull(namePrefix);
 
         new MathBenchmark(namePrefix + " small sizes").execute(() -> {
-            for (int size: new int[] {2, 3, 4, 5, 6, 7}) testMix(size, constructor);
+            for (int size: new int[] {2, 3, 4, 5, 6, 7}) consumer.accept(size, constructor);
         });
 
         new MathBenchmark(namePrefix + " sizes powers of 2").execute(() -> {
             for (int power: new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14})
-                testMix(1 << power, constructor);
+                consumer.accept(1 << power, constructor);
         });
 
         new MathBenchmark(namePrefix + " large sizes").execute(() -> {
             for (int power: new int[] {10, 12, 14, 16})
                 for (int delta : new int[] {-1, 0, 1})
-                    testMix((1 << power) + delta, constructor);
+                    consumer.accept((1 << power) + delta, constructor);
         });
 
         new MathBenchmark(namePrefix + " extra large sizes")
             .measurementTimes(10)
             .execute(() -> { // todo test powers 21, 22, 23 (power 24 killed my IDEA)
-            for (int power: new int[] {17, 18, 19, 20})
-                for (int delta : new int[] {-1, 0}) // IMPL NOTE delta +1 is not intended for use here
-                    testMix((1 << power) + delta, constructor);
-        });
+                for (int power : new int[] {17, 18, 19, 20})
+                    for (int delta : new int[] {-1, 0}) // IMPL NOTE delta +1 is not intended for use here
+                        consumer.accept((1 << power) + delta, constructor);
+            });
     }
 
     /** */
-    private void testMix(int size, Function<Integer, Vector> constructor) {
+    private void basicMix(int size, Function<Integer, Vector> constructor) {
         final Vector v1 = constructor.apply(size), v2 = constructor.apply(size);
 
         for (int idx = 0; idx < size; idx++) {
@@ -83,5 +88,24 @@ public class VectorBenchmarkTest {
         assertNotNull(v1.assign(v2));
 
         assertNotNull(v1.assign(1)); // IMPL NOTE this would better be last test for it sets all values the same
+    }
+
+    /** */
+    private void mapMix(int size, Function<Integer, Vector> constructor) {
+        final Vector v1 = constructor.apply(size), v2 = constructor.apply(size);
+
+        for (int idx = 0; idx < size; idx++) {
+            v1.set(idx, idx);
+
+            v2.set(idx, size - idx);
+        }
+
+        assertNotNull(v1.map((val) -> (val + 1)));
+
+        assertNotNull(v1.map(v2, (one, other) -> one + other / 2.0));
+
+        assertNotNull(v1.map((val, val1) -> (val + val1), 2.0));
+
+        // todo write code for foldMap
     }
 }
