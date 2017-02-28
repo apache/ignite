c555e059efd4a44d4d50ee9d1b9e877f734e5857
Ignite Teamcity <ignite@apache.org>
2017-02-28 17:46:29 +0300

    version changed to: 1.9.0

--- modules/platforms/dotnet/examples/Apache.Ignite.Examples/Properties/AssemblyInfo.cs
+++ modules/platforms/dotnet/examples/Apache.Ignite.Examples/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("41a0cb95-3435-4c78-b867-900b28e2c9ee")]
 
-[assembly: AssemblyVersion("2.0.0.16694")]
-[assembly: AssemblyFileVersion("2.0.0.16694")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
+[assembly: AssemblyVersion("1.9.0.18210")]
+[assembly: AssemblyFileVersion("1.9.0.18210")]
+[assembly: AssemblyInformationalVersion("1.9.0")]
