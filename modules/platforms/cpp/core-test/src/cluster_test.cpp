ced0e6f1ea590529abbf8cc1057c123f111a9fea
Igor Sapego <isapego@gridgain.com>
2017-03-01 22:38:43 +0300

    IGNITE-4617: Fix for tests

--- modules/platforms/cpp/core-test/src/cluster_test.cpp
+++ modules/platforms/cpp/core-test/src/cluster_test.cpp
@@ -22,6 +22,7 @@
 #include <boost/test/unit_test.hpp>
 
 #include <ignite/ignition.h>
+#include <ignite/test_utils.h>
 
 using namespace ignite;
 using namespace ignite::common::concurrent;
@@ -31,31 +32,17 @@ using namespace boost::unit_test;
 /*
  * Test setup fixture.
  */
-struct ClusterTestSuiteFixture {
+struct ClusterTestSuiteFixture
+{
+    Ignite grid;
+
     /*
      * Constructor.
      */
-    ClusterTestSuiteFixture()
+    ClusterTestSuiteFixture() :
+        grid(ignite_test::StartNode("cache-test.xml", "ClusterTest"))
     {
-        IgniteConfiguration cfg;
-
-        cfg.jvmOpts.push_back("-Xdebug");
-        cfg.jvmOpts.push_back("-Xnoagent");
-        cfg.jvmOpts.push_back("-Djava.compiler=NONE");
-        cfg.jvmOpts.push_back("-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005");
-        cfg.jvmOpts.push_back("-XX:+HeapDumpOnOutOfMemoryError");
-
-#ifdef IGNITE_TESTS_32
-        cfg.jvmInitMem = 256;
-        cfg.jvmMaxMem = 768;
-#else
-        cfg.jvmInitMem = 1024;
-        cfg.jvmMaxMem = 4096;
-#endif
-
-        cfg.springCfgPath.assign(getenv("IGNITE_NATIVE_TEST_CPP_CONFIG_PATH")).append("/cache-test.xml");
-
-        grid = Ignition::Start(cfg, "ClusterTest");
+        // No-op.
     }
 
     /*
@@ -64,10 +51,7 @@ struct ClusterTestSuiteFixture {
     ~ClusterTestSuiteFixture()
     {
         Ignition::StopAll(true);
-        grid = Ignite();
     }
-
-    Ignite grid;
 };
 
 BOOST_FIXTURE_TEST_SUITE(ClusterTestSuite, ClusterTestSuiteFixture)
