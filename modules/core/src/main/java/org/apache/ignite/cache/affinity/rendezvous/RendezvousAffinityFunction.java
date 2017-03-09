81ee1a81ffb41536ce2dec16a8443c2fe43ab77a
tledkov-gridgain <tledkov@gridgain.com>
2017-03-09 12:36:22 +0300

    IGNITE-3018: debug print

--- modules/core/src/main/java/org/apache/ignite/cache/affinity/rendezvous/RendezvousAffinityFunction.java
+++ modules/core/src/main/java/org/apache/ignite/cache/affinity/rendezvous/RendezvousAffinityFunction.java
@@ -14,6 +14,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.apache.ignite.cache.affinity.rendezvous;
 
@@ -442,9 +458,11 @@ public class RendezvousAffinityFunction implements AffinityFunction, Externaliza
 
             if (!exclNeighborsWarn) {
                 LT.warn(log, "Affinity function excludeNeighbors property is ignored " +
-                        "because topology has no enough nodes to assign backups.",
+                        "because topology has no enough nodes to assign backups. " +
+                        "Try to assign according with partition balance.",
                     "Affinity function excludeNeighbors property is ignored " +
-                        "because topology has no enough nodes to assign backups.");
+                        "because topology has no enough nodes to assign backups. " +
+                        "Try to assign according with partition balance.");
 
                 exclNeighborsWarn = true;
             }
@@ -548,7 +566,27 @@ public class RendezvousAffinityFunction implements AffinityFunction, Externaliza
             assignments.add(partAssignment);
         }
 
+        printAssignment(assignments);
+
         return assignments;
+    }
+
+    /**
+     * @param a ass.
+     */
+    private void printAssignment(List<List<ClusterNode>> a) {
+        int p = 0;
+        log.info("+++ ASS");
+        for (List<ClusterNode> pl : a) {
+            System.out.print(p + ": ");
+
+            for (ClusterNode n : pl)
+                System.out.print(n.id() + " ");
+
+            p++;
+
+            System.out.println("");
+        }
     }
 
     /**
