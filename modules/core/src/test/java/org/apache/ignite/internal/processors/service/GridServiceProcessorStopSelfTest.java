515937c91bbcc7fce3bf0a78c1f41f99dcbdbf9a
Andrey V. Mashenkov <andrey.mashenkov@gmail.com>
2017-03-14 15:45:27 +0300

    Minors

--- modules/core/src/test/java/org/apache/ignite/internal/processors/service/GridServiceProcessorStopSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/service/GridServiceProcessorStopSelfTest.java
@@ -17,59 +17,25 @@
 
 package org.apache.ignite.internal.processors.service;
 
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
-import org.apache.ignite.GridTestTask;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.IgniteException;
 import org.apache.ignite.IgniteServices;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.cache.CacheAtomicityMode;
-import org.apache.ignite.cache.CacheMode;
-import org.apache.ignite.cluster.ClusterNode;
-import org.apache.ignite.compute.ComputeJob;
-import org.apache.ignite.compute.ComputeJobResult;
-import org.apache.ignite.compute.ComputeJobResultPolicy;
-import org.apache.ignite.compute.ComputeTask;
 import org.apache.ignite.configuration.CacheConfiguration;
-import org.apache.ignite.events.DiscoveryEvent;
-import org.apache.ignite.events.EventType;
-import org.apache.ignite.internal.GridKernalContext;
 import org.apache.ignite.internal.IgniteEx;
 import org.apache.ignite.internal.IgniteInternalFuture;
-import org.apache.ignite.internal.client.GridClientPartitionAffinity;
-import org.apache.ignite.internal.events.DiscoveryCustomEvent;
-import org.apache.ignite.internal.processors.affinity.AffinityTopologyVersion;
-import org.apache.ignite.internal.processors.cache.CacheAffinityChangeMessage;
-import org.apache.ignite.internal.processors.continuous.StartRoutineAckDiscoveryMessage;
-import org.apache.ignite.internal.processors.continuous.StartRoutineDiscoveryMessage;
 import org.apache.ignite.internal.util.typedef.internal.U;
-import org.apache.ignite.lang.IgniteUuid;
-import org.apache.ignite.resources.IgniteInstanceResource;
 import org.apache.ignite.services.Service;
 import org.apache.ignite.services.ServiceContext;
-import org.apache.ignite.spi.discovery.tcp.internal.TcpDiscoveryNode;
-import org.apache.ignite.testframework.GridTestNode;
 import org.apache.ignite.testframework.GridTestUtils;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
-import org.jetbrains.annotations.Nullable;
-
-import static org.apache.ignite.events.EventType.EVT_CACHE_NODES_LEFT;
-import static org.apache.ignite.events.EventType.EVT_CACHE_REBALANCE_STARTED;
-import static org.apache.ignite.events.EventType.EVT_NODE_FAILED;
-import static org.apache.ignite.events.EventType.EVT_NODE_JOINED;
-import static org.apache.ignite.events.EventType.EVT_NODE_LEFT;
-import static org.apache.ignite.events.EventType.EVT_NODE_SEGMENTED;
 
 /**
  * Tests that {@link GridServiceProcessor} completes deploy/undeploy futures during node stop.
@@ -203,7 +169,7 @@ public class GridServiceProcessorStopSelfTest extends GridCommonAbstractTest {
      * Simple map service.
      */
     public interface TestService {
-        public void method() throws InterruptedException;
+        // No-op.
     }
 
     /**
@@ -212,10 +178,6 @@ public class GridServiceProcessorStopSelfTest extends GridCommonAbstractTest {
     public class TestServiceImpl implements Service, TestService {
         /** Serial version UID. */
         private static final long serialVersionUID = 0L;
-
-        /** */
-        @IgniteInstanceResource
-        Ignite ignite;
 
         /** {@inheritDoc} */
         @Override public void cancel(ServiceContext ctx) {
@@ -230,10 +192,6 @@ public class GridServiceProcessorStopSelfTest extends GridCommonAbstractTest {
         /** {@inheritDoc} */
         @Override public void execute(ServiceContext ctx) throws Exception {
             // No-op.
-        }
-
-        /** {@inheritDoc} */
-        @Override public void method() throws InterruptedException {
         }
     }
 }
