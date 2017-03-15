3c2e2455e2155854eefb96b8dd9f9da20cffed7d
Vyacheslav Daradur <daradurvs@gmail.com>
2017-03-15 10:38:45 +0300

    ignite-4211: unit-tests were removed (will be added within another task)

--- modules/spring/src/test/java/org/apache/ignite/testsuites/IgniteSpringTestSuite.java
+++ modules/spring/src/test/java/org/apache/ignite/testsuites/IgniteSpringTestSuite.java
@@ -21,7 +21,6 @@ import junit.framework.TestSuite;
 import org.apache.ignite.cache.spring.GridSpringCacheManagerSelfTest;
 import org.apache.ignite.cache.spring.SpringCacheManagerContextInjectionTest;
 import org.apache.ignite.internal.IgniteSpringBeanTest;
-import org.apache.ignite.cache.spring.SpringCacheTest;
 import org.apache.ignite.cache.store.jdbc.CacheJdbcBlobStoreFactorySelfTest;
 import org.apache.ignite.cache.store.jdbc.CacheJdbcPojoStoreFactorySelfTest;
 import org.apache.ignite.cache.store.jdbc.CachePojoStoreXmlSelfTest;
@@ -82,8 +81,6 @@ public class IgniteSpringTestSuite extends TestSuite {
 
         suite.addTestSuite(SpringCacheManagerContextInjectionTest.class);
         suite.addTestSuite(SpringTransactionManagerContextInjectionTest.class);
-
-        suite.addTestSuite(SpringCacheTest.class);
 
         return suite;
     }
