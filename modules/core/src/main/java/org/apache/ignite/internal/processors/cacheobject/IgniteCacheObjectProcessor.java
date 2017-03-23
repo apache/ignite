7daaf96a694e4264219f4a06a5b077f05a1d66f8
Ivan Rakov <ivan.glukos@gmail.com>
2017-03-23 20:46:08 +0300

    IGNITE-4534: removed unused methods from CacheObjectProcessor

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cacheobject/IgniteCacheObjectProcessor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cacheobject/IgniteCacheObjectProcessor.java
@@ -177,14 +177,6 @@ public interface IgniteCacheObjectProcessor extends GridProcessor {
     public CacheObject toCacheObject(CacheObjectContext ctx, ByteBuffer buf);
 
     /**
-     * @param ctx Cache context.
-     * @param buf Buffer to read from.
-     * @return Cache object.
-     */
-    @Deprecated // TODO IGNITE-4534: remove
-    public KeyCacheObject toKeyCacheObject(CacheObjectContext ctx, ByteBuffer buf) throws IgniteCheckedException;
-
-    /**
      * @param ctx Cache object context.
      * @param buf Buffer.
      * @param incompleteObj Incomplete cache object or {@code null} if it's a first read.
@@ -192,17 +184,6 @@ public interface IgniteCacheObjectProcessor extends GridProcessor {
      * @throws IgniteCheckedException If fail.
      */
     public IncompleteCacheObject toCacheObject(CacheObjectContext ctx, ByteBuffer buf,
-        @Nullable IncompleteCacheObject incompleteObj) throws IgniteCheckedException;
-
-    /**
-     * @param ctx Cache object context.
-     * @param buf Buffer.
-     * @param incompleteObj Incomplete cache object or {@code null} if it's a first read.
-     * @return Incomplete cache object.
-     * @throws IgniteCheckedException If fail.
-     */
-    @Deprecated // TODO IGNITE-4534: remove
-    public IncompleteCacheObject toKeyCacheObject(CacheObjectContext ctx, ByteBuffer buf,
         @Nullable IncompleteCacheObject incompleteObj) throws IgniteCheckedException;
 
     /**
