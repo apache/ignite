f70017cca40b0185a98dd5960f40a3f9806638a6
vopolski <2W3e4r5t>
2017-03-29 15:03:34 +0300

    IGNITE-4052 Changes code style.

--- modules/mesos/src/test/java/org/apache/ignite/mesos/IgniteFrameworkInfoTest.java
+++ modules/mesos/src/test/java/org/apache/ignite/mesos/IgniteFrameworkInfoTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.mesos;
 
 import junit.framework.TestCase;
@@ -5,39 +22,33 @@ import org.apache.mesos.Protos;
 import org.hamcrest.core.Is;
 import org.junit.Rule;
 import org.junit.contrib.java.lang.system.EnvironmentVariables;
-import org.omg.CORBA.Environment;
-
 import static org.junit.Assert.assertThat;
 
 /**
- * Created by sbt-opolskiy-va on 28.03.2017.
+ * FrameworkInfoTest
  */
 public class IgniteFrameworkInfoTest extends TestCase {
 
-    /**
-     * Framework name.
-     */
-    private final String IGNITE_FRAMEWORK_NAME = "Ignite";
+    /** Framework name.*/
+    private static final String IGNITE_FRAMEWORK_NAME = "Ignite";
 
-    /**
-     * Mesos user name in system environment.
-     */
-    private final String MESOS_USER_NAME = "MESOS_USER";
+    /** Mesos user name in system environment.*/
+    private static final String MESOS_USER_NAME = "MESOS_USER";
 
+    /** */
     private final String testName = "mesosusername";
 
-    /**
-     * Mesos user name in system environment.
-     */
-    private final String MESOS_ROLE = "MESOS_ROLE";
+    /** Mesos user name in system environment.*/
+    private static final String MESOS_ROLE = "MESOS_ROLE";
 
+    /** */
     private final String testRole = "mesosrole";
 
-    @Rule
-    public final EnvironmentVariables environmentVariables = new EnvironmentVariables();
+    /** */
+    @Rule public final EnvironmentVariables environmentVariables = new EnvironmentVariables();
 
-    @Override
-    public void setUp() throws Exception {
+    /** {@inheritDoc} */
+    @Override public void setUp() throws Exception {
         environmentVariables.set(MESOS_USER_NAME, testName);
         environmentVariables.set(MESOS_ROLE, testRole);
     }
@@ -49,12 +60,10 @@ public class IgniteFrameworkInfoTest extends TestCase {
 
         String name = System.getenv(MESOS_USER_NAME);
         String role = System.getenv(MESOS_ROLE);
-
         Protos.FrameworkInfo.Builder frameworkBuilder = Protos.FrameworkInfo.newBuilder()
             .setName(IGNITE_FRAMEWORK_NAME)
             .setUser(name != null ? name : "")
             .setRole(role != null ? role : "*");
-
 
         assertThat(testName, Is.is(name));
         assertThat(testRole, Is.is(role));
