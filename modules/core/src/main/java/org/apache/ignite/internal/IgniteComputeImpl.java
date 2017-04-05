2f5c21e1b26a43696cd3407c0d6c3ba2dab6d4ba
Andrey V. Mashenkov <andrey.mashenkov@gmail.com>
2017-04-05 18:24:39 +0300

    Minors

--- modules/core/src/main/java/org/apache/ignite/internal/IgniteComputeImpl.java
+++ modules/core/src/main/java/org/apache/ignite/internal/IgniteComputeImpl.java
@@ -50,6 +50,7 @@ import org.jetbrains.annotations.Nullable;
 import static org.apache.ignite.internal.GridClosureCallMode.BALANCE;
 import static org.apache.ignite.internal.GridClosureCallMode.BROADCAST;
 import static org.apache.ignite.internal.processors.task.GridTaskThreadContextKey.TC_NO_FAILOVER;
+import static org.apache.ignite.internal.processors.task.GridTaskThreadContextKey.TC_SUBGRID;
 import static org.apache.ignite.internal.processors.task.GridTaskThreadContextKey.TC_SUBGRID_PREDICATE;
 import static org.apache.ignite.internal.processors.task.GridTaskThreadContextKey.TC_SUBJ_ID;
 import static org.apache.ignite.internal.processors.task.GridTaskThreadContextKey.TC_TASK_NAME;
