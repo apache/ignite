b4e6e4a4abe0c3ca28ac58dccfc64c3682d69c36
Ilya Lantukh <ilantukh@gridgain.com>
2017-04-10 17:52:12 +0300

    ignite-4851 : Removed incorrect assertion for publicSize.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/GridDhtLocalPartition.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/GridDhtLocalPartition.java
@@ -984,7 +984,6 @@ public class GridDhtLocalPartition extends GridCacheConcurrentMapImpl implements
             long state = this.state.get();
 
             assert getPartState(state) != EVICTED;
-            assert getSize(state) > 0;
 
             if (this.state.compareAndSet(state, setSize(state, getSize(state) - 1)))
                 return;
