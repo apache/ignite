29404c850655cff2d4f4eb672e79742e809d8a29
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-04-12 10:30:17 +0300

    wip Java updates

--- modules/core/src/main/java/org/apache/ignite/internal/processors/platform/dotnet/PlatformDotNetConfigurationClosure.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/platform/dotnet/PlatformDotNetConfigurationClosure.java
@@ -153,7 +153,7 @@ public class PlatformDotNetConfigurationClosure extends PlatformAbstractConfigur
             BinaryNameMapper nameMapper = bCfg.getNameMapper();
 
             if (nameMapper == null) {
-                bCfg.setNameMapper(new BinaryBasicNameMapper(true));
+                bCfg.setNameMapper(new BinaryBasicNameMapper(false));
 
                 dotNetCfg.warnings(Collections.singleton("Binary name mapper is automatically set to " +
                     bCfg.getNameMapper()
