938b050c9b833a59f3572c479154848a80add4a7
tledkov-gridgain <tledkov@gridgain.com>
2017-04-14 15:55:18 +0300

    IGNITE-4699: minors

--- modules/spring/src/main/java/org/apache/ignite/IgniteSpringBean.java
+++ modules/spring/src/main/java/org/apache/ignite/IgniteSpringBean.java
@@ -34,7 +34,6 @@ import org.apache.ignite.internal.util.typedef.internal.S;
 import org.apache.ignite.lang.IgniteProductVersion;
 import org.apache.ignite.plugin.IgnitePlugin;
 import org.apache.ignite.plugin.PluginNotFoundException;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.DisposableBean;
