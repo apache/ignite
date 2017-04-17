816c735279de3335d02dc421476d3479c64cd54e
tledkov-gridgain <tledkov@gridgain.com>
2017-04-17 15:01:36 +0300

    IGNITE-4699: fix review issues

--- modules/core/src/main/java/org/apache/ignite/internal/processors/task/GridTaskWorker.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/task/GridTaskWorker.java
@@ -26,7 +26,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
-import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.AtomicReference;
