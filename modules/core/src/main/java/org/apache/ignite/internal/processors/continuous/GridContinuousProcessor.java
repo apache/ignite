e448a4d011c4137528f7915600fdf32cc5455128
Evgenii Zhuravlev <ezhuravlev@gridgain.com>
2017-04-18 12:14:07 +0300

    IGNITE-4939 Receive event before cache initialized fix

--- modules/core/src/main/java/org/apache/ignite/internal/processors/continuous/GridContinuousProcessor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/continuous/GridContinuousProcessor.java
@@ -323,10 +323,13 @@ public class GridContinuousProcessor extends GridProcessorAdapter {
 
         ctx.cacheObjects().onContinuousProcessorStarted(ctx);
 
-        ctx.service().onContinuousProcessorStarted(ctx);
-
         if (log.isDebugEnabled())
             log.debug("Continuous processor started.");
+    }
+
+    /** {@inheritDoc} */
+    @Override public void onKernalStart(boolean activeOnStart)  throws IgniteCheckedException {
+        ctx.service().onContinuousProcessorStarted(ctx);
     }
 
     /**
