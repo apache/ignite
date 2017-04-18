9d087c7899af4dc4df4e9a3c210bdae7694617ae
Evgenii Zhuravlev <ezhuravlev@gridgain.com>
2017-04-18 17:22:46 +0300

    IGNITE-4925 Fix IgniteCacheBinaryObjectsScanSelfTest.testScanNoClasses

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteCacheBinaryObjectsScanSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteCacheBinaryObjectsScanSelfTest.java
@@ -17,10 +17,12 @@
 
 package org.apache.ignite.internal.processors.cache;
 
+import java.util.ArrayList;
 import java.util.List;
 import javax.cache.Cache;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
+import org.apache.ignite.cache.affinity.rendezvous.RendezvousAffinityFunction;
 import org.apache.ignite.cache.query.ScanQuery;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
@@ -127,10 +129,12 @@ public class IgniteCacheBinaryObjectsScanSelfTest extends GridCommonAbstractTest
             assertEquals(PERSON_CLS_NAME, entry.getValue().getClass().getName());
         }
 
+        entries = new ArrayList<>();
 
-        entries = cache.query(new ScanQuery<>(9)).getAll();
+        for (int i = 0; i < RendezvousAffinityFunction.DFLT_PARTITION_COUNT; i++)
+            entries.addAll(cache.query(new ScanQuery<>(i)).getAll());
 
-        assertFalse(entries.isEmpty());
+        assertEquals(100, entries.size());
 
         for (Cache.Entry<Object, Object> entry : entries) {
             assertEquals(PERSON_KEY_CLS_NAME, entry.getKey().getClass().getName());
