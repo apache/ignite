621e1ecaab08f27a9f18140d6e330f5fc02bf67f
Sergey Chugunov <sergey.chugunov@gmail.com>
2017-04-21 18:00:34 +0300

    IGNITE-5025 Name of default MemoryPolicy was changed from null to 'default'

--- modules/core/src/test/java/org/apache/ignite/testframework/junits/GridTestKernalContext.java
+++ modules/core/src/test/java/org/apache/ignite/testframework/junits/GridTestKernalContext.java
@@ -39,7 +39,7 @@ public class GridTestKernalContext extends GridKernalContextImpl {
     /**
      * @param log Logger to use in context config.
      */
-    public GridTestKernalContext(IgniteLogger log) throws IgniteCheckedException {
+    public GridTestKernalContext(IgniteLogger log) {
         this(log, new IgniteConfiguration());
     }
 
@@ -47,7 +47,7 @@ public class GridTestKernalContext extends GridKernalContextImpl {
      * @param log Logger to use in context config.
      * @param cfg Configuration to use in Test
      */
-    public GridTestKernalContext(IgniteLogger log, IgniteConfiguration cfg) throws IgniteCheckedException {
+    public GridTestKernalContext(IgniteLogger log, IgniteConfiguration cfg) {
         super(new GridLoggerProxy(log, null, null, null),
                 new IgniteKernal(null),
                 cfg,
