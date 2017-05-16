5eea8adaf2f5d4d39e5a629dc3261988ab6669c0
Sergey Chugunov <sergey.chugunov@gmail.com>
2017-05-16 10:59:02 +0300

    apache headers are added, RAT checks pass

--- modules/pds/src/test/java/org/apache/ignite/grid/internal/processors/cache/database/CacheRebalancingWithPersistenceTxSelfTest.java
+++ modules/pds/src/test/java/org/apache/ignite/grid/internal/processors/cache/database/CacheRebalancingWithPersistenceTxSelfTest.java
@@ -1,12 +1,12 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
+ * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,26 +15,47 @@
  * limitations under the License.
  */
 
-package org.apache.ignite.cache.database;
+package org.apache.ignite.grid.internal.processors.cache.database;
 
 import org.apache.ignite.cache.CacheAtomicityMode;
 import org.apache.ignite.cache.CacheMode;
+import org.apache.ignite.cache.CacheRebalanceMode;
 import org.apache.ignite.cache.CacheWriteSynchronizationMode;
+import org.apache.ignite.cache.affinity.rendezvous.RendezvousAffinityFunction;
 import org.apache.ignite.configuration.CacheConfiguration;
 
 /**
  *
  */
-public class IgnitePersistentStoreAtomicCacheRebalancingTest extends IgnitePersistentStoreCacheRebalancingAbstractTest {
+public class CacheRebalancingWithPersistenceTxSelfTest extends CacheRebalancingWithPersistenceAbstractTest {
     /** {@inheritDoc} */
     @Override protected CacheConfiguration cacheConfiguration(String cacheName) {
         CacheConfiguration ccfg = new CacheConfiguration(cacheName);
 
-        ccfg.setAtomicityMode(CacheAtomicityMode.ATOMIC);
+        ccfg.setAtomicityMode(CacheAtomicityMode.TRANSACTIONAL);
         ccfg.setCacheMode(CacheMode.PARTITIONED);
+        ccfg.setRebalanceMode(CacheRebalanceMode.SYNC);
         ccfg.setBackups(1);
+        ccfg.setRebalanceDelay(10_000);
+        ccfg.setAffinity(new RendezvousAffinityFunction(false, 32));
         ccfg.setWriteSynchronizationMode(CacheWriteSynchronizationMode.FULL_SYNC);
 
         return ccfg;
+    }
+
+    /** {@inheritDoc} */
+    @Override protected void afterTest() throws Exception {
+        super.afterTest();
+
+        explicitTx = false;
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testTopologyChangesWithConstantLoadExplicitTx() throws Exception {
+        explicitTx = true;
+
+        testTopologyChangesWithConstantLoad();
     }
 }
