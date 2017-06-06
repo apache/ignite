982e519107596279250227a4471bc8263c8e193b
Igor Seliverstov <gvvinblade@gmail.com>
2017-06-06 16:12:22 +0300

    IGNITE-5196

--- modules/core/src/test/java/org/apache/ignite/internal/ClusterGroupSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/ClusterGroupSelfTest.java
@@ -19,7 +19,11 @@ package org.apache.ignite.internal;
 
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.Random;
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.cluster.ClusterGroup;
@@ -27,6 +31,7 @@ import org.apache.ignite.cluster.ClusterNode;
 import org.apache.ignite.internal.util.typedef.F;
 import org.apache.ignite.lang.IgnitePredicate;
 import org.apache.ignite.marshaller.Marshaller;
+import org.apache.ignite.testframework.GridTestUtils;
 import org.apache.ignite.testframework.junits.common.GridCommonTest;
 
 /**
@@ -269,6 +274,78 @@ public class ClusterGroupSelfTest extends ClusterGroupAbstractTest {
         assertEquals(2, srv.nodes().size());
         assertTrue(cli.nodes().contains(ignite(2).cluster().localNode()));
         assertTrue(cli.nodes().contains(ignite(3).cluster().localNode()));
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testForCacheNodesOnDynamicCacheCreateDestroy() throws Exception {
+        Random rnd = ThreadLocalRandom.current();
+
+        final AtomicReference<Exception> ex = new AtomicReference<>();
+
+        IgniteInternalFuture fut = runCacheCreateDestroyTask(ex);
+
+        while (!fut.isDone()) {
+            ignite.cluster().forCacheNodes("cache" + rnd.nextInt(16));
+        }
+
+        if (ex.get() != null)
+            throw ex.get();
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testForClientNodesOnDynamicCacheCreateDestroy() throws Exception {
+        Random rnd = ThreadLocalRandom.current();
+
+        final AtomicReference<Exception> ex = new AtomicReference<>();
+
+        IgniteInternalFuture fut = runCacheCreateDestroyTask(ex);
+
+        while (!fut.isDone()) {
+            ignite.cluster().forClientNodes("cache" + rnd.nextInt(16));
+        }
+
+        if (ex.get() != null)
+            throw ex.get();
+    }
+
+    /**
+     * @param exceptionHolder Exception holder.
+     * @return Task future.
+     */
+    private IgniteInternalFuture runCacheCreateDestroyTask(final AtomicReference<Exception> exceptionHolder) {
+        final long deadline = System.currentTimeMillis() + 5000;
+
+        final AtomicInteger cntr = new AtomicInteger();
+
+        return GridTestUtils.runMultiThreadedAsync(new Runnable() {
+            @Override public void run() {
+                int startIdx = cntr.getAndAdd(4);
+                int idx = 0;
+                boolean start = true;
+
+                while (System.currentTimeMillis() < deadline) {
+                    try {
+                        if (start)
+                            ignite.createCache("cache" + (startIdx + idx));
+                        else
+                            ignite.destroyCache("cache" + (startIdx + idx));
+
+                        if ((idx = (idx + 1) % 4) == 0)
+                            start = !start;
+                    }
+                    catch (Exception e) {
+                        if (exceptionHolder.get() != null || !exceptionHolder.compareAndSet(null, e))
+                            exceptionHolder.get().addSuppressed(e);
+
+                        break;
+                    }
+                }
+            }
+        }, 4, "cache-start-destroy");
     }
 
     /**
