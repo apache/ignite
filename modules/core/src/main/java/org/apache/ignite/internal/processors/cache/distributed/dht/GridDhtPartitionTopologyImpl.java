e1f4908805bca4d92bebfa8f197b7830efa17916
Pavel Kovalenko <jokserfn@gmail.com>
2017-06-15 14:52:04 +0300

    IGNITE-GG-12292 Remove assertion of checking FullMap sequences. Returned back updateLocal() method behavior.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/GridDhtPartitionTopologyImpl.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/GridDhtPartitionTopologyImpl.java
@@ -1846,8 +1846,8 @@ public class GridDhtPartitionTopologyImpl implements GridDhtPartitionTopology {
                     else
                         updateSeq = seq;
                 }
-                else // Sequentially update counter in case of any changes in partitions
-                    node2part.updateSequence(this.updateSeq.incrementAndGet());
+
+                node2part.updateSequence(updateSeq);
             }
         }
 
