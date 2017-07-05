eaaee978d37c5a66dff2d59515ddb70c40d5e31b
Yuci Gou <ygou@ebi.ac.uk>
2017-07-05 11:26:57 +0100

    IGNITE-5607: HttpSessionBindingListener is not supported for clustered web session

--- modules/web/src/main/java/org/apache/ignite/cache/websession/WebSessionV2.java
+++ modules/web/src/main/java/org/apache/ignite/cache/websession/WebSessionV2.java
@@ -168,6 +168,8 @@ class WebSessionV2 implements HttpSession {
         maxInactiveInterval = interval;
 
         maxInactiveIntervalChanged = true;
+        
+        genuineSes.setMaxInactiveInterval(interval);
     }
 
     /**
@@ -224,6 +226,8 @@ class WebSessionV2 implements HttpSession {
     /** {@inheritDoc} */
     @Override public void setAttribute(final String name, final Object val) {
         assertValid();
+        
+        genuineSes.setAttribute(name, val);
 
         if (val == null)
             removeAttribute(name);
