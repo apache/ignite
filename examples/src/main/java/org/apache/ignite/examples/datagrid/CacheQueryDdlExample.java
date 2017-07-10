8a41a6fb232c3f633dcbdc1da9e7bdba343b425a
devozerov <vozerov@gridgain.com>
2017-07-10 13:40:07 +0300

    Updated DDL example.

--- examples/src/main/java/org/apache/ignite/examples/datagrid/CacheQueryDdlExample.java
+++ examples/src/main/java/org/apache/ignite/examples/datagrid/CacheQueryDdlExample.java
@@ -50,8 +50,7 @@ public class CacheQueryDdlExample {
 
             // Create dummy cache to act as an entry point for SQL queries (new SQL API which do not require this
             // will appear in future versions, JDBC and ODBC drivers do not require it already).
-            CacheConfiguration<?, ?> cacheCfg = new CacheConfiguration<>(DUMMY_CACHE_NAME)
-                .setSqlSchema("PUBLIC");
+            CacheConfiguration<?, ?> cacheCfg = new CacheConfiguration<>(DUMMY_CACHE_NAME).setSqlSchema("PUBLIC");
 
             try (
                 IgniteCache<?, ?> cache = ignite.getOrCreateCache(cacheCfg)
