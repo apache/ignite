591923a305f533ddf8a9b91a1109677a4c93b2d8
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-07-27 22:00:17 +0300

    Suppress finalizer warning

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/PlatformJniTarget.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/PlatformJniTarget.cs
@@ -20,6 +20,7 @@ namespace Apache.Ignite.Core.Impl
     using System;
     using System.Collections.Generic;
     using System.Diagnostics;
+    using System.Diagnostics.CodeAnalysis;
     using System.IO;
     using System.Threading;
     using System.Threading.Tasks;
@@ -522,6 +523,8 @@ namespace Apache.Ignite.Core.Impl
         #endregion
 
         /** <inheritdoc /> */
+        [SuppressMessage("Microsoft.Usage", "CA1816:CallGCSuppressFinalizeCorrectly",
+            Justification = "There is no finalizer.")]
         public void Dispose()
         {
             if (_target != null)
