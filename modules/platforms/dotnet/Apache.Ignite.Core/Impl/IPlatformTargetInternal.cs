ae89c91725537f914e2566343268116fb58fc47a
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-07-27 13:48:11 +0300

    wip cleanup

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/IPlatformTargetInternal.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/IPlatformTargetInternal.cs
@@ -20,7 +20,6 @@ namespace Apache.Ignite.Core.Impl
     using System;
     using Apache.Ignite.Core.Impl.Binary;
     using Apache.Ignite.Core.Impl.Binary.IO;
-    using Apache.Ignite.Core.Impl.Unmanaged;
     using Apache.Ignite.Core.Interop;
 
     /// <summary>
