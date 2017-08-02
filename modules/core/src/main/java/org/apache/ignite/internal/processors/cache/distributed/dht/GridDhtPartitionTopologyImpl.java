b27768288ff9e33294741782d456a43eda5249ce
sboikov <sboikov@gridgain.com>
2017-08-02 17:25:31 +0300

    Call updateRebalanceVersion after evictions (was broken in c6fbe2d82a9f56f96c94551b09e85a12d192f32e);

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/GridDhtPartitionTopologyImpl.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/GridDhtPartitionTopologyImpl.java
@@ -623,10 +623,10 @@ public class GridDhtPartitionTopologyImpl implements GridDhtPartitionTopology {
 
             List<List<ClusterNode>> aff = grp.affinity().assignments(topVer);
 
-            updateRebalanceVersion(aff);
-
             if (node2part != null && node2part.valid())
                 changed |= checkEvictions(updateSeq, aff);
+
+            updateRebalanceVersion(aff);
 
             consistencyCheck();
         }
