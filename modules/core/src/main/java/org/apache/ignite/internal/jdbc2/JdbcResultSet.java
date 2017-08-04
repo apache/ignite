f1deae1224a71eebb2c7202814c8c069a777d426
devozerov <vozerov@gridgain.com>
2017-08-04 12:17:03 +0300

    Cosmetics.

--- modules/core/src/main/java/org/apache/ignite/internal/jdbc2/JdbcResultSet.java
+++ modules/core/src/main/java/org/apache/ignite/internal/jdbc2/JdbcResultSet.java
@@ -146,8 +146,8 @@ public class JdbcResultSet implements ResultSet {
             boolean loc = nodeId == null;
 
             // Connections from new clients send queries with new tasks, so we have to continue in the same manner
-            JdbcQueryTask qryTask = new JdbcQueryTask(loc ? ignite : null, conn.cacheName(), conn.schemaName(),
-                null, true, loc, null, fetchSize, uuid, conn.isLocalQuery(), conn.isCollocatedQuery(),
+            JdbcQueryTask qryTask = new JdbcQueryTask(loc ? ignite : null, conn.cacheName(), conn.schemaName(), null,
+                true, loc, null, fetchSize, uuid, conn.isLocalQuery(), conn.isCollocatedQuery(),
                 conn.isDistributedJoins());
 
             try {
