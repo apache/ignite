f7afb8eba6bea249f76376c0e66d7d0a230b36b6
dpavlov <dpavlov@gridgain.com>
2017-08-04 20:53:38 +0300

    IGNITE-5946: Ignite WebSessions: Flaky failure muted

--- modules/core/src/test/java/org/apache/ignite/spi/discovery/tcp/TcpClientDiscoverySpiSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/spi/discovery/tcp/TcpClientDiscoverySpiSelfTest.java
@@ -1328,7 +1328,7 @@ public class TcpClientDiscoverySpiSelfTest extends GridCommonAbstractTest {
      * @throws Exception If failed.
      */
     public void testReconnectAfterFailTopologyChanged() throws Exception {
-        fail("https://issues.apache.org/jira/browse/IGNITE-5841");
+        fail("https://issues.apache.org/jira/browse/IGNITE-5841"); // flaky test with success rate ~80%
         reconnectAfterFail(true);
     }
 
