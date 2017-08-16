210650360698a768bc40620f1edb5642a045307c
Alexander Menshikov <sharplermc@gmail.com>
2017-08-16 15:49:48 +0300

    Add jmh test for new reentrantlock

--- modules/benchmarks/src/main/java/org/apache/ignite/internal/benchmarks/jmh/cache/JmhCacheLocksBenchmark.java
+++ modules/benchmarks/src/main/java/org/apache/ignite/internal/benchmarks/jmh/cache/JmhCacheLocksBenchmark.java
@@ -17,20 +17,57 @@
 
 package org.apache.ignite.internal.benchmarks.jmh.cache;
 
+import java.util.UUID;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import javax.cache.processor.EntryProcessor;
+import javax.cache.processor.EntryProcessorException;
+import javax.cache.processor.MutableEntry;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.IgniteCheckedException;
 import org.apache.ignite.IgniteLock;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.binary.BinaryObjectException;
+import org.apache.ignite.binary.BinaryReader;
+import org.apache.ignite.binary.BinaryWriter;
+import org.apache.ignite.binary.Binarylizable;
 import org.apache.ignite.cache.CacheAtomicityMode;
+import org.apache.ignite.cache.CacheMode;
 import org.apache.ignite.cache.CacheWriteSynchronizationMode;
+import org.apache.ignite.cluster.ClusterGroup;
+import org.apache.ignite.configuration.CacheConfiguration;
+import org.apache.ignite.events.Event;
 import org.apache.ignite.internal.benchmarks.jmh.runner.JmhIdeBenchmarkRunner;
+import org.apache.ignite.internal.managers.eventstorage.GridLocalEventListener;
+import org.apache.ignite.internal.processors.cache.GridCacheContext;
+import org.apache.ignite.internal.processors.datastructures.GridCacheInternalKey;
+import org.apache.ignite.internal.processors.datastructures.GridCacheInternalKeyImpl;
+import org.apache.ignite.internal.processors.datastructures.GridCacheLockImpl2;
+import org.apache.ignite.internal.processors.datastructures.GridCacheLockState2;
+import org.apache.ignite.lang.IgniteBiInClosure;
+import org.apache.ignite.lang.IgniteBiPredicate;
+import org.apache.ignite.lang.IgniteFuture;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import static org.apache.ignite.internal.managers.communication.GridIoPolicy.SYSTEM_POOL;
 
 /**
  * IgniteCache.lock() vs Ignite.reentrantLock().
@@ -48,40 +85,82 @@ public class JmhCacheLocksBenchmark extends JmhCacheAbstractBenchmark {
     /** Parameter for Ignite.reentrantLock(). */
     private static final boolean fair = false;
 
+    /** Number of nodes. */
+    final static int MAX_NODES = 6;
+
+    /** */
+    final static Ignite[] nodes = new Ignite[MAX_NODES];
+
+    /** The counter for getting a node. */
+    final static AtomicInteger countForThread = new AtomicInteger(0);
+
     /** IgniteCache.lock() with a fixed lock key. */
-    private Lock cacheLock;
+    @State(Scope.Benchmark)
+    public static class CacheLockState {
+        /** */
+        public final Lock cacheLock;
 
-    /** Ignite.reentrantLock() with a fixed lock key. */
-    private IgniteLock igniteLock;
-
-    /** New Ignite.reentrantLock() with a fixed lock key. */
-    private IgniteLock igniteLock2;
+        /** */
+        public CacheLockState() {
+            cacheLock = nodes[countForThread.getAndIncrement() % MAX_NODES]
+                .cache(DEFAULT_CACHE_NAME).lock(lockKey);
+        }
+    }
 
     /**
      * Test IgniteCache.lock() with fixed key and no-op inside.
      */
     @Benchmark
-    public void cacheLock() {
-        cacheLock.lock();
-        cacheLock.unlock();
+    public void cacheLock(final CacheLockState lockState) {
+        lockState.cacheLock.lock();
+        lockState.cacheLock.unlock();
+    }
+
+    /** Ignite.reentrantLock() with a fixed lock key. */
+    @State(Scope.Benchmark)
+    public static class IgniteLockState {
+        /** */
+        public final IgniteLock igniteLock;
+
+        /** */
+        public IgniteLockState() {
+            igniteLock = nodes[countForThread.getAndIncrement() % MAX_NODES]
+                .reentrantLock(lockKey, failoverSafe, fair, true);
+        }
     }
 
     /**
      * Test Ignite.reentrantLock() with fixed key and no-op inside.
      */
     @Benchmark
-    public void igniteLock() {
-        igniteLock.lock();
-        igniteLock.unlock();
+    public void igniteLock(final IgniteLockState lockState) {
+        lockState.igniteLock.lock();
+        lockState.igniteLock.unlock();
+    }
+
+    /**
+     * State for new Ignite.reentrantLock().
+     */
+    @State(Scope.Thread)
+    public static class IgniteLockState2 {
+        /** */
+        public final IgniteLock igniteLock;
+
+        /** */
+        public IgniteLockState2() {
+            final int k = countForThread.getAndIncrement() % MAX_NODES;
+            igniteLock = nodes[k]
+                .reentrantLock(lockKey+"2", true);
+        }
     }
 
     /**
      * Test new Ignite.reentrantLock() with fixed key and no-op inside.
      */
     @Benchmark
-    public void igniteLock2() {
-        igniteLock2.lock();
-        igniteLock2.unlock();
+    public void igniteLock2(final IgniteLockState2 lockState) {
+        lockState.igniteLock.lock();
+        lockState.igniteLock.unlock();
     }
 
     /**
@@ -89,11 +168,10 @@ public class JmhCacheLocksBenchmark extends JmhCacheAbstractBenchmark {
      */
     @Setup(Level.Trial)
     public void createLock() {
-        cacheLock = cache.lock(lockKey);
+        nodes[0] = node;
 
-        igniteLock = node.reentrantLock(lockKey, failoverSafe, fair, true);
-
-        igniteLock2 = node.reentrantLock(lockKey+"2", true);
+        for (int i = 1; i < MAX_NODES; i++)
+            nodes[i] = Ignition.start(configuration("node"+(i+147)));
     }
 
     /**
@@ -104,7 +182,7 @@ public class JmhCacheLocksBenchmark extends JmhCacheAbstractBenchmark {
      */
     public static void main(String[] args) throws Exception {
         final String simpleClsName = JmhCacheLocksBenchmark.class.getSimpleName();
-        final int threads = 4;
+        final int threads = MAX_NODES;
         final boolean client = true;
         final CacheAtomicityMode atomicityMode = CacheAtomicityMode.TRANSACTIONAL;
         final CacheWriteSynchronizationMode writeSyncMode = CacheWriteSynchronizationMode.FULL_SYNC;
@@ -119,6 +197,8 @@ public class JmhCacheLocksBenchmark extends JmhCacheAbstractBenchmark {
             .threads(threads)
             .include(simpleClsName)
             .output(output + ".jmh.log")
+            .timeUnit(TimeUnit.MICROSECONDS)
+            .mode(Mode.AverageTime)
             .jvmArgs(
                 "-Xms1g",
                 "-Xmx1g",
