219046330fae7b874dc7105c25ea05d2824fb4da
Ilya Lantukh <ilantukh@gridgain.com>
2017-08-18 11:37:01 +0300

    MetaStore backward compatibility fix.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/database/MetadataStorage.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/database/MetadataStorage.java
@@ -39,7 +39,7 @@ import org.apache.ignite.internal.util.typedef.internal.U;
  */
 public class MetadataStorage implements MetaStore {
     /** Max index name length (bytes num) */
-    public static final int MAX_IDX_NAME_LEN = 255;
+    public static final int MAX_IDX_NAME_LEN = 768;
 
     /** Bytes in byte. */
     private static final int BYTE_LEN = 1;
