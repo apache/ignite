4de9de484dc595e816f8024b18c2988d5bd03983
Evgeny Stanilovskiy <estanilovskiy@gridgain.com>
2017-08-18 15:23:42 +0300

    IGNITE-5869 client hangs infinitely while binary configuration differs from server

--- modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/ServerImpl.java
+++ modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/ServerImpl.java
@@ -3516,7 +3516,7 @@ class ServerImpl extends TcpDiscoveryImpl {
 
                                 try {
                                     trySendMessageDirectly(node,
-                                        new TcpDiscoveryCheckFailedMessage(locNodeId, err.sendMessage()));
+                                        new TcpDiscoveryCheckFailedMessage(err.nodeId(), err.sendMessage()));
                                 }
                                 catch (IgniteSpiException e) {
                                     if (log.isDebugEnabled())
@@ -6047,6 +6047,16 @@ class ServerImpl extends TcpDiscoveryImpl {
                                 }
                                 else {
                                     ignored = true;
+
+                                    ClientMessageWorker worker = clientMsgWorkers.get(msg.creatorNodeId());
+
+                                    if (worker != null) {
+                                        msg.verify(getLocalNodeId());
+                                        worker.addMessage(msg);
+                                    }
+                                    else if (log.isDebugEnabled())
+                                        log.debug("Failed to find client message worker " +
+                                                "[clientNode=" + msg.creatorNodeId() + ']');
 
                                     state = spiState;
                                 }
