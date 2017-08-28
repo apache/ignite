5c0874281e1daecf72dbe7f3eb62221515e5d566
Alexander Paschenko <alexander.a.paschenko@gmail.com>
2017-08-28 20:18:29 +0300

    IGNITE-5620 Began sorting out errors.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/query/IgniteSQLException.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/query/IgniteSQLException.java
@@ -22,6 +22,27 @@ import org.apache.ignite.IgniteException;
 import org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode;
 import org.jetbrains.annotations.Nullable;
 
+import static org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode.CACHE_NOT_FOUND;
+import static org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode.COLUMN_ALREADY_EXISTS;
+import static org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode.COLUMN_NOT_FOUND;
+import static org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode.CONCURRENT_UPDATE;
+import static org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode.DUPLICATE_KEY;
+import static org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode.ENTRY_PROCESSING;
+import static org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode.INDEX_ALREADY_EXISTS;
+import static org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode.INDEX_NOT_FOUND;
+import static org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode.KEY_UPDATE;
+import static org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode.NULL_KEY;
+import static org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode.NULL_TABLE_DESCRIPTOR;
+import static org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode.NULL_VALUE;
+import static org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode.PARSING;
+import static org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode.STMT_TYPE_MISMATCH;
+import static org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode.TABLE_ALREADY_EXISTS;
+import static org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode.TABLE_DROP_FAILED;
+import static org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode.TABLE_NOT_FOUND;
+import static org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode.UNEXPECTED_ELEMENT_TYPE;
+import static org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode.UNEXPECTED_OPERATION;
+import static org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode.UNSUPPORTED_OPERATION;
+
 /**
  * Specific exception bearing information about query processing errors for more detailed
  * errors in JDBC driver.
@@ -92,5 +113,62 @@ public class IgniteSQLException extends IgniteException {
      */
     public SQLException toJdbcException() {
         return new SQLException(getMessage(), sqlState, statusCode, this);
+    }
+
+    /**
+     *
+     * @param statusCode
+     * @return
+     * @see <a href="http://en.wikibooks.org/wiki/Structured_Query_Language/SQLSTATE">Wikipedia: SQLSTATE spec.</a>
+     */
+    private static String codeToSqlState(int statusCode) {
+        switch (statusCode) {
+            case DUPLICATE_KEY:
+                return "23000"; // Generic value for "integrity constraint violation" 23 class.
+
+            case NULL_KEY:
+            case NULL_VALUE:
+                return "22004"; // "Null value not allowed".
+
+            case UNSUPPORTED_OPERATION:
+                return "0A000"; // Generic value for "feature not supported" 0A class.
+
+            case CONCURRENT_UPDATE:
+                return "40000"; // Generic value for "tx rollback" 0A class.
+
+            case PARSING:
+                return "42000"; // Generic value for "syntax error or access rule violation" 42 class.
+
+            // 42 - class for "syntax error or access rule violation" + error specific part.
+            case TABLE_NOT_FOUND:
+            case INDEX_ALREADY_EXISTS:
+            case STMT_TYPE_MISMATCH:
+            case INDEX_NOT_FOUND:
+            case TABLE_ALREADY_EXISTS:
+            case COLUMN_NOT_FOUND:
+            case COLUMN_ALREADY_EXISTS:
+                return "42" + String.valueOf(statusCode).substring(1);
+
+            case UNEXPECTED_OPERATION:
+            case UNEXPECTED_ELEMENT_TYPE:
+            case KEY_UPDATE:
+                return "421" + String.valueOf(statusCode).substring(2);
+
+            // Internal errors - XX class.
+            case ENTRY_PROCESSING:
+                return "XX001";
+
+            case TABLE_DROP_FAILED:
+                return "XX002";
+
+            case NULL_TABLE_DESCRIPTOR:
+                return "XX003";
+
+            case CACHE_NOT_FOUND:
+                return "XX004";
+
+            default:
+                return "50000";  // Generic value for custom "50" class.
+        }
     }
 }
