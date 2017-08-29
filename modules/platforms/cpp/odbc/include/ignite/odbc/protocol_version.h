292113621bc259d271553137adf8e447c51dee41
Igor Sapego <isapego@gridgain.com>
2017-08-29 19:06:43 +0300

    IGNITE-6119: Added 'lazy' flag to configuration

--- modules/platforms/cpp/odbc/include/ignite/odbc/protocol_version.h
+++ modules/platforms/cpp/odbc/include/ignite/odbc/protocol_version.h
@@ -33,6 +33,7 @@ namespace ignite
         public:
             /** Current protocol version. */
             static const ProtocolVersion VERSION_2_1_0;
+            static const ProtocolVersion VERSION_2_3_0;
 
             typedef std::set<ProtocolVersion> VersionSet;
 
