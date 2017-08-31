337622723977ddb74f92a310b69658cac158bb77
Alexey Kukushkin <AlexeyKukushkin@yahoo.com>
2017-08-31 17:44:11 +0300

    Fix for IGNITE-5966

---
+++ modules/core/src/main/java/org/apache/ignite/internal/DuplicateTypeIdException.java
@@ -0,0 +1,59 @@
+package org.apache.ignite.internal;
+
+import org.apache.ignite.IgniteCheckedException;
+
+/**
+ * The exception indicates a duplicate type ID was encountered.
+ */
+public class DuplicateTypeIdException extends IgniteCheckedException {
+    /** Serialization version */
+    private static final long serialVersionUID = 0L;
+
+    /** Platform ID */
+    private final byte platformId;
+
+    /** Type ID */
+    private final int typeId;
+
+    /** Name of already registered class */
+    private final String oldClsName;
+
+    /** Name of new class being registered */
+    private final String newClsName;
+
+    /**
+     * Initializes new instance of {@link DuplicateTypeIdException} class.
+     *
+     * @param platformId Platform ID
+     * @param typeId Type ID
+     * @param oldClsName Name of already registered class
+     * @param newClsName Name of new class being registered
+     */
+    DuplicateTypeIdException(byte platformId, int typeId, String oldClsName, String newClsName) {
+        this.platformId = platformId;
+        this.typeId = typeId;
+        this.oldClsName = oldClsName;
+        this.newClsName = newClsName;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override public String getMessage() {
+        return "Duplicate ID [platformId="
+            + platformId
+            + ", typeId="
+            + typeId
+            + ", oldCls="
+            + oldClsName
+            + ", newCls="
+            + newClsName + "]";
+    }
+
+    /**
+     * @return Name of already registered class
+     */
+    public String getRegisteredClassName() {
+        return oldClsName;
+    }
+}
