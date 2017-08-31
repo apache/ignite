e1cbe2b7358691e5f082b457723285c61d36c15b
tledkov-gridgain <tledkov@gridgain.com>
2017-08-31 17:03:44 +0300

    IGNITE-5425 JDBC thin driver: don't throw unsupported exception on ClientInfo methods. This closes #2184. This closes #2525.

--- modules/core/src/main/java/org/apache/ignite/internal/jdbc/thin/JdbcThinDatabaseMetadata.java
+++ modules/core/src/main/java/org/apache/ignite/internal/jdbc/thin/JdbcThinDatabaseMetadata.java
@@ -1490,7 +1490,6 @@ public class JdbcThinDatabaseMetadata implements DatabaseMetaData {
 
     /** {@inheritDoc} */
     @Override public ResultSet getClientInfoProperties() throws SQLException {
-        // TODO: IGNITE-5425.
         return new JdbcThinResultSet(Collections.<List<Object>>emptyList(), Arrays.asList(
             new JdbcColumnMeta(null, null, "NAME", String.class),
             new JdbcColumnMeta(null, null, "MAX_LEN", Integer.class),
