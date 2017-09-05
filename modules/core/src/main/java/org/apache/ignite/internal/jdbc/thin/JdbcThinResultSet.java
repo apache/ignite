990cf694fad0d5c7bb29dab8573fdb27b85f9e2c
tledkov-gridgain <tledkov@gridgain.com>
2017-09-05 13:22:14 +0300

    IGNITE-5344: minor

--- modules/core/src/main/java/org/apache/ignite/internal/jdbc/thin/JdbcThinResultSet.java
+++ modules/core/src/main/java/org/apache/ignite/internal/jdbc/thin/JdbcThinResultSet.java
@@ -1740,7 +1740,7 @@ public class JdbcThinResultSet implements ResultSet {
     /**
      * @param closeStmt Close statement on this result set close.
      */
-    public void closeStmt(boolean closeStmt) {
+    void closeStmt(boolean closeStmt) {
         this.closeStmt = closeStmt;
     }
 
