141b0a1160105c5fe0637583d8bf960b72191d82
Vitaliy Biryukov <BiryukovVitaliy92@gmail.com>
2017-09-08 16:31:55 +0300

    IGNITE-5865: Refactoring. javadoc.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/transactions/TxOptimisticDeadlockDetectionTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/transactions/TxOptimisticDeadlockDetectionTest.java
@@ -421,7 +421,7 @@ public class TxOptimisticDeadlockDetectionTest extends GridCommonAbstractTest {
     }
 
     /**
-     * This method added in order to not Change the behavior of this test after refactoring.
+     * This method added in order to not change the behavior of this test after refactoring.
      * @param key Key.
      * @param i I.
      * @return Incremented key.
