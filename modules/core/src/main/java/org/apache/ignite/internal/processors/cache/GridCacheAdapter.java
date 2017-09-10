2c3dbfa95d25ca837624cc5f69c612134a65e39f
Nikolay Izhikov <NIzhikov.dev@gmail.com>
2017-09-10 23:36:02 +0300

    IGNITE-6307: Remove new local entries from onHeap map in case of Exception inside getAll

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheAdapter.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheAdapter.java
@@ -1863,6 +1863,8 @@ public abstract class GridCacheAdapter<K, V> implements IgniteInternalCache<K, V
         if (tx == null || tx.implicit()) {
             Map<KeyCacheObject, EntryGetResult> misses = null;
 
+            Set<GridCacheEntryEx> newLocalEntries = null;
+
             final AffinityTopologyVersion topVer = tx == null ?
                 (canRemap ?
                     ctx.affinity().affinityTopologyVersion() : ctx.shared().exchange().readyAffinityVersion()) :
@@ -1937,6 +1939,8 @@ public abstract class GridCacheAdapter<K, V> implements IgniteInternalCache<K, V
                             }
 
                             if (!skipEntry) {
+                                boolean isNewLocalEntry = this.map.getEntry(ctx, key) == null;
+
                                 entry = entryEx(key);
 
                                 if (entry == null) {
@@ -1944,6 +1948,13 @@ public abstract class GridCacheAdapter<K, V> implements IgniteInternalCache<K, V
                                         ctx.cache().metrics0().onRead(false);
 
                                     break;
+                                }
+
+                                if (isNewLocalEntry) {
+                                    if (newLocalEntries == null)
+                                        newLocalEntries = new HashSet<>();
+
+                                    newLocalEntries.add(entry);
                                 }
 
                                 if (storeEnabled) {
@@ -2138,6 +2149,11 @@ public abstract class GridCacheAdapter<K, V> implements IgniteInternalCache<K, V
                 if (misses != null) {
                     for (KeyCacheObject key0 : misses.keySet())
                         ctx.evicts().touch(peekEx(key0), topVer);
+                }
+
+                if (newLocalEntries != null) {
+                    for (GridCacheEntryEx entry : newLocalEntries)
+                        removeEntry(entry);
                 }
 
                 return new GridFinishedFuture<>(e);
