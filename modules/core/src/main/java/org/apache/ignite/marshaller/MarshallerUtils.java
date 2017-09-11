fdb998256c03fbaba00dde0e66b6e5bd7838098b
Dmitri Samoilov <camoiloc@gmail.com>
2017-09-11 15:37:22 +0100

    jdkMarshaller(String nodeName) didn't set nodeName
    Node name was being set on a new instance of JdkMarshaller, not the one being returned

--- modules/core/src/main/java/org/apache/ignite/marshaller/MarshallerUtils.java
+++ modules/core/src/main/java/org/apache/ignite/marshaller/MarshallerUtils.java
@@ -48,7 +48,7 @@ public class MarshallerUtils {
     public static JdkMarshaller jdkMarshaller(@Nullable String nodeName) {
         JdkMarshaller marsh = new JdkMarshaller();
 
-        setNodeName(new JdkMarshaller(), nodeName);
+        setNodeName(marsh, nodeName);
 
         return marsh;
     }
