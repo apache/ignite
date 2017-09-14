ae164e88221f10f18f7be6800f5eda0a764d2a64
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-09-14 19:24:18 +0300

    File copy script in progress

---
+++ modules/platforms/dotnet/build-nuget.ps1
@@ -0,0 +1,18 @@
+﻿# TODO: Integrate this script into build.ps1 with -asmDirs
+
+# 1. Copy .NET binaries to corresponding bin\Release folders
+$bins = "c:\w\ignite-2.2\ignite.dotnet.bin"
+
+ls $bins | % `
+{
+    if (($_.Extension -eq ".exe") -or ($_.Extension -eq ".dll")) {
+        $projName = [System.IO.Path]::GetFileNameWithoutExtension($_.Name);
+
+        if ($projName.StartsWith("Apache.Ignite")) {
+            $target = "$projName\bin\Release"
+            mkdir -Force $target
+
+            xcopy /s /y $_.FullName $target
+        }
+    }
+}
