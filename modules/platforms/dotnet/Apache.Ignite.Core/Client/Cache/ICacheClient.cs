22002141a1d594556f1b72fa0dd139ab0a1414aa
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-09-18 12:57:26 +0300

    IGNITE-6404 Fix failures in .NET (NPE in ExceptionUtils, inspections)

--- modules/platforms/dotnet/Apache.Ignite.Core/Client/Cache/ICacheClient.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Client/Cache/ICacheClient.cs
@@ -22,6 +22,7 @@ namespace Apache.Ignite.Core.Client.Cache
     /// <summary>
     /// Client cache API. See <see cref="IIgniteClient.GetCache{K, V}"/>.
     /// </summary>
+    // ReSharper disable once TypeParameterCanBeVariant (ICache shoul not be variant, more methods will be added)
     public interface ICacheClient<TK, TV>
     {
         /// <summary>
