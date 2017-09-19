4ee9f9478840908a5525c83f173e0c273ac4a493
Vyacheslav Daradur <daradurvs@gmail.com>
2017-09-19 11:50:40 +0300

    wip

--- modules/compatibility/src/test/java/org/apache/ignite/compatibility/testframework/junits/CompatibilityTestIgniteNodeRunner.java
+++ modules/compatibility/src/test/java/org/apache/ignite/compatibility/testframework/junits/CompatibilityTestIgniteNodeRunner.java
@@ -125,8 +125,6 @@ public class CompatibilityTestIgniteNodeRunner extends IgniteNodeRunner {
 
         cfg.setLocalEventListeners(lsnrs);
 
-        cfg.setDiscoverySpi().
-
         return rmtNodeStartedLatch;
     }
 
