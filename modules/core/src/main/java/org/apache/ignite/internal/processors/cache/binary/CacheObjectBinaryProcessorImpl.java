39ee8af17f5cbe2db83b5ce127ac6e85c9fb4bf7
Andrey V. Mashenkov <andrey.mashenkov@gmail.com>
2017-09-26 17:38:21 +0300

    Backport for IGNITE-2779: BinaryMarshaller caches must be cleaned during client reconnect.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/binary/CacheObjectBinaryProcessorImpl.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/binary/CacheObjectBinaryProcessorImpl.java
@@ -155,9 +155,11 @@ public class CacheObjectBinaryProcessorImpl extends IgniteCacheObjectProcessorIm
     @GridToStringExclude
     private IgniteBinary binaries;
 
-    /** Listener removes all registred binary schemas after the local client reconnected. */
+    /** Listener removes all registred binary schemas and user type descriptors after the local client reconnected. */
     private final GridLocalEventListener clientDisconLsnr = new GridLocalEventListener() {
         @Override public void onEvent(Event evt) {
+            binaryContext().unregisterUserTypeDescriptors();
+
             binaryContext().unregisterBinarySchemas();
         }
     };
