56d173378de373fe65a26193c5b150b73f0ffeae
vk <kaligularnd@gmail.com>
2017-09-26 22:15:04 +0300

    IGNITE-6346 Fix: distributed set does not work in REPLICATED mode
    Changes:
    1. Fixed bug
    2. Added tests

---
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/datastructures/partitioned/GridCachePartitionedAtomicSetClientTest.java
@@ -0,0 +1,16 @@
+package org.apache.ignite.internal.processors.cache.datastructures.partitioned;
+
+import org.apache.ignite.cache.CacheAtomicityMode;
+import org.apache.ignite.cache.CacheMode;
+import org.apache.ignite.internal.processors.cache.datastructures.GridCacheSetClientAbstractTest;
+
+public class GridCachePartitionedAtomicSetClientTest extends GridCacheSetClientAbstractTest {
+    
+    @Override protected CacheMode collectionCacheMode() {
+        return CacheMode.PARTITIONED;
+    }
+
+    @Override protected CacheAtomicityMode collectionCacheAtomicityMode() {
+        return CacheAtomicityMode.ATOMIC;
+    }
+}
