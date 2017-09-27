d2adf03895efe6a5061cc1b6f362acd5dfc386a4
Alexander Paschenko <alexander.a.paschenko@gmail.com>
2017-09-27 14:51:17 +0300

    IGNITE-6242 SQL: Added ability to specify cache, key type and value type names for CREATE TABLE. This closes #2740.

--- modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/sql/GridSqlCreateTable.java
+++ modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/sql/GridSqlCreateTable.java
@@ -38,6 +38,15 @@ public class GridSqlCreateTable extends GridSqlStatement {
     /** Cache name upon which new cache configuration for this table must be based. */
     private String templateName;
 
+    /** Name of new cache associated with this table. */
+    private String cacheName;
+
+    /** Name of cache key type. */
+    private String keyTypeName;
+
+    /** Name of cache value type. */
+    private String valTypeName;
+
     /** Group to put new cache into. */
     private String cacheGrp;
 
@@ -77,6 +86,48 @@ public class GridSqlCreateTable extends GridSqlStatement {
      */
     public void templateName(String templateName) {
         this.templateName = templateName;
+    }
+
+    /**
+     * @return Name of new cache associated with this table.
+     */
+    public String cacheName() {
+        return cacheName;
+    }
+
+    /**
+     * @param cacheName Name of new cache associated with this table.
+     */
+    public void cacheName(String cacheName) {
+        this.cacheName = cacheName;
+    }
+
+    /**
+     * @return Name of cache key type.
+     */
+    public String keyTypeName() {
+        return keyTypeName;
+    }
+
+    /**
+     * @param keyTypeName Name of cache key type.
+     */
+    public void keyTypeName(String keyTypeName) {
+        this.keyTypeName = keyTypeName;
+    }
+
+    /**
+     * @return Name of cache value type.
+     */
+    public String valueTypeName() {
+        return valTypeName;
+    }
+
+    /**
+     * @param valTypeName Name of cache value type.
+     */
+    public void valueTypeName(String valTypeName) {
+        this.valTypeName = valTypeName;
     }
 
     /**
