29887d516c2fed17dae965571b6cbbaab5240af3
NSAmelchev <nsamelchev@gmail.com>
2017-10-04 13:47:20 +0300

    add braces

--- modules/core/src/main/java/org/apache/ignite/internal/binary/BinaryClassDescriptor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/binary/BinaryClassDescriptor.java
@@ -905,10 +905,11 @@ public class BinaryClassDescriptor {
 
                     short checksum = reader.readShort();
 
-                    if (checksum != this.checksum)
+                    if (checksum != this.checksum) {
                         throw new ClassNotFoundException("Binary stream class checksum mismatch " +
                             "(is same version of marshalled class present on all nodes?) " +
                             "[expected=" + this.checksum + ", actual=" + checksum + ", cls=" + cls + ']');
+                    }
 
                     ((Externalizable)res).readExternal(reader);
 
