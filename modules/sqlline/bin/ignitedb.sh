2d3726cf6a33a054724aa70b2d535b0e4b26ecb5
oleg-ostanin <oostanin@gridgain.com>
2017-10-06 14:23:29 +0300

    IGNITE-6561 edited script

--- modules/sqlline/bin/ignitedb.sh
+++ modules/sqlline/bin/ignitedb.sh
@@ -20,6 +20,104 @@
 # Ignite database launcher.
 #
 
+self_name=$(basename $0)
+
+function print_help()
+{
+    echo "Script for Apache Ignite database starting."
+    echo "Usage: $self_name options..."
+    echo
+}
+
+# Defaults:
+HOST="127.0.0.1"
+PORT="10800"
+SCHEMA=""
+distributedJoins="false"
+enforceJoinOrder="false"
+collocated="false"
+replicatedOnly="false"
+autoCloseServerCursor="false"
+socketSendBuffer="0"
+socketReceiveBuffer="0"
+tcpNoDelay="true"
+lazy="false"
+
+for i in "$@"
+do
+   case $i in
+        # Print help
+        -h|--help)
+            print_help
+        ;;
+        # Host to connect. By default 127.0.0.1.
+        -h=*|--host=*)
+            HOST="${i#*=}"
+            shift # get value after "="
+        ;;
+        # Port to connect. By default 10800.
+        -p=*|--port=*)
+            PORT="${i#*=}"
+            shift # get value after "="
+        ;;
+        # Schema.
+        -s=*|--schema=*)
+            SCHEMA="/${i#*=}"
+            shift # get value after "="
+        ;;
+        # Distributed joins flag.
+        -dj=*|--distributedJoins=*)
+            distributedJoins="${i#*=}"
+            shift # get value after "="
+        ;;
+        # Enforce join order flag.
+        -ej=*|--enforceJoinOrder=*)
+            enforceJoinOrder="${i#*=}"
+            shift # get value after "="
+        ;;
+        # Collocated flag.
+        -c=*|--collocated=*)
+            collocated="${i#*=}"
+            shift # get value after "="
+        ;;
+        # Replicated only flag.
+        -r=*|--replicatedOnly=*)
+            replicatedOnly="${i#*=}"
+            shift # get value after "="
+        ;;
+        # Auto close server cursor flag.
+        -ac=*|--autoCloseServerCursor=*)
+            autoCloseServerCursor="${i#*=}"
+            shift # get value after "="
+        ;;
+        # Socket send buffer.
+        -ssb=*|--socketSendBuffer=*)
+            socketSendBuffer="${i#*=}"
+            shift # get value after "="
+        ;;
+        # Socket receive buffer.
+        -srb=*|--socketReceiveBuffer=*)
+            socketReceiveBuffer="${i#*=}"
+            shift # get value after "="
+        ;;
+        # TCP no delay flag.
+        -tnd=*|--tcpNoDelay=*)
+            SYNC_MODE="${i#*=}"
+            shift # get value after "="
+        ;;
+        # Lazy flag.
+        -l=*|--lazy=*)
+            lazy="${i#*=}"
+            shift # get value after "="
+        ;;
+
+        *)
+        ;;
+    esac
+done
+
+params="jdbc:ignite:thin://${HOST}:${PORT}${SCHEMA}?distributedJoins=${distributedJoins}&enforceJoinOrder=${enforceJoinOrder}&collocated=${collocated}&replicatedOnly=${replicatedOnly}&autoCloseServerCursor=${autoCloseServerCursor}&socketSendBuffer=${socketSendBuffer}&socketReceiveBuffer=${socketReceiveBuffer}&tcpNoDelay=${tcpNoDelay}&lazy=${lazy}"
+
 #
 # Import common functions.
 #
@@ -36,11 +134,6 @@ SCRIPTS_HOME="${IGNITE_HOME_TMP}/bin"
 source "${SCRIPTS_HOME}"/include/functions.sh
 
 #
-# Discover path to Java executable and check it's version.
-#
-checkJava
-
-#
 # Discover IGNITE_HOME environment variable.
 #
 setIgniteHome
@@ -55,13 +148,4 @@ CP="${IGNITE_LIBS}"
 
 CP=${CP}:${IGNITE_HOME_TMP}/bin/include/sqlline/*
 
-if [ $# -eq 0 ]
-then
-
-    java -cp ${CP} sqlline.SqlLine -d org.apache.ignite.IgniteJdbcThinDriver --color=true --verbose=true --showWarnings=true --showNestedErrs=true -u jdbc:ignite:thin://127.0.0.1/
-
-else
-
-    java -cp ${CP} sqlline.SqlLine ${@}
-
-fi
+java -cp ${CP} sqlline.SqlLine -d org.apache.ignite.IgniteJdbcThinDriver --color=true --verbose=true --showWarnings=true --showNestedErrs=true -u ${params}
