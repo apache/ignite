4c3d78168ba8e8f5d6bcb83b50032604c681d6f2
skalashnikov <skalashnikov@gridgain.com>
2017-10-11 17:28:14 +0300

    Revert "IGNITE-6024: applied review comment"
    This reverts commit 94ce00d90bb7536d28842df1c40617d778f36dfb.

--- modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/dml/UpdatePlan.java
+++ modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/dml/UpdatePlan.java
@@ -72,7 +72,7 @@ public final class UpdatePlan {
     public final boolean isReplicatedOnly;
 
     /** Cache identifiers. */
-    public final List<Integer> cacheIds;
+    public List<Integer> cacheIds;
 
     /** */
     private UpdatePlan(UpdateMode mode, GridH2Table tbl, String[] colNames, int[] colTypes, KeyValueSupplier keySupplier,
