9f005b897fc797a27abfa19e19ac2ce01370a3b3
Ilya Nozhkin <nozhkin.ii@gmail.com>
2017-10-18 00:04:41 +0300

    implemented the local version of FCM

--- modules/ml/src/main/java/org/apache/ignite/ml/clustering/BaseFuzzyCMeansClusterer.java
+++ modules/ml/src/main/java/org/apache/ignite/ml/clustering/BaseFuzzyCMeansClusterer.java
@@ -2,11 +2,22 @@ package org.apache.ignite.ml.clustering;
 
 import org.apache.ignite.ml.math.DistanceMeasure;
 import org.apache.ignite.ml.math.Matrix;
+import org.apache.ignite.ml.math.Vector;
 
 public abstract class BaseFuzzyCMeansClusterer<T extends Matrix> implements Clusterer<T, FuzzyCMeansModel> {
-    private DistanceMeasure measure;
+    protected DistanceMeasure measure;
 
-    private double exponentialWeight;
+    protected double exponentialWeight;
+    protected double maxCentersDelta;
 
     public abstract FuzzyCMeansModel cluster(T points, int k);
+
+    /**
+     * Calculates the distance between two vectors. * with the configured {@link DistanceMeasure}.
+     *
+     * @return the distance between the two clusterables
+     */
+    protected double distance(final Vector v1, final Vector v2) {
+        return measure.compute(v1, v2);
+    }
 }
--- modules/ml/src/main/java/org/apache/ignite/ml/clustering/FuzzyCMeansLocalClusterer.java
+++ modules/ml/src/main/java/org/apache/ignite/ml/clustering/FuzzyCMeansLocalClusterer.java
@@ -1,4 +1,145 @@
 package org.apache.ignite.ml.clustering;
 
-public class FuzzyCMeansLocalClusterer {
+import org.apache.ignite.ml.math.Matrix;
+import org.apache.ignite.ml.math.Vector;
+import org.apache.ignite.ml.math.exceptions.ConvergenceException;
+import org.apache.ignite.ml.math.exceptions.MathIllegalArgumentException;
+import org.apache.ignite.ml.math.impls.matrix.DenseLocalOnHeapMatrix;
+import org.apache.ignite.ml.math.impls.vector.DenseLocalOnHeapVector;
+
+import java.util.List;
+import java.util.Random;
+
+public class FuzzyCMeansLocalClusterer extends BaseFuzzyCMeansClusterer<DenseLocalOnHeapMatrix> implements
+        WeightedClusterer<DenseLocalOnHeapMatrix, FuzzyCMeansModel> {
+
+    private int maxIterations;
+
+    private Random rand;
+
+    @Override
+    public FuzzyCMeansModel cluster(DenseLocalOnHeapMatrix points, int k) {
+        return null;
+    }
+
+    private Vector getWeightsVector(List<Double> weights) {
+        return null;
+    }
+
+    private Matrix initializeCenters(Matrix centers, Matrix points, int k, Vector weights) {
+        //int dimensions = points.columnSize();
+        int numPoints = points.rowSize();
+
+        Vector firstCenter = points.viewRow(rand.nextInt(numPoints));
+        centers.setRow(0, firstCenter.getStorage().data());
+
+        Vector costs = points.foldRows(vector -> distance(vector, firstCenter));
+        costs = costs.times(weights);
+
+        double sum = costs.sum();
+
+        for (int i = 1; i < k; i++) {
+            double probe = rand.nextDouble() * sum;
+            double counter = 0;
+            int id = 0;
+
+            for (int j = 0; j < numPoints; j++) {
+                counter += costs.getX(i);
+                if (counter >= probe) {
+                    id = j;
+                    break;
+                }
+            }
+
+            centers.setRow(i, points.viewRow(id).getStorage().data());
+            sum -= costs.get(id);
+            costs.set(id, 0.0);
+        }
+
+        return centers;
+    }
+
+    private void calculateDistances(Matrix distances, Matrix points, Matrix centers) {
+        int numPoints = points.rowSize();
+        int numCenters = centers.rowSize();
+
+        for (int i = 0; i < numCenters; i++) {
+            for (int j = 0; j < numPoints; j++) {
+                distances.set(i, j, distance(centers.viewRow(i), points.viewRow(j)));
+            }
+        }
+    }
+
+    private void calculateMembership(Matrix membership, Matrix distances, Vector weights) {
+        int numPoints = distances.columnSize();
+        int numCenters = distances.rowSize();
+        double fuzzyMembershipCoefficient = 2 / (exponentialWeight - 1);
+
+        for (int i = 0; i < numCenters; i++) {
+            for (int j = 0; j < numPoints; j++) {
+                double invertedFuzzyWeight = 0;
+                for (int k = 0; k < numCenters; k++) {
+                    invertedFuzzyWeight += Math.pow(distances.get(i, j) / distances.get(k, j),
+                                                    fuzzyMembershipCoefficient);
+                }
+                double weight = 1.0 / invertedFuzzyWeight * weights.getX(j);
+                membership.setX(i, j, Math.pow(weight, exponentialWeight));
+            }
+        }
+    }
+
+    private Matrix calculateNewCenters(Matrix points, Matrix membership) {
+        Vector membershipSums = membership.foldRows(row -> row.sum());
+        Matrix newCenters = membership.times(points);
+
+        int numCenters = newCenters.rowSize();
+        for (int i = 0; i < numCenters; i++) {
+            newCenters.viewRow(i).divide(membershipSums.getX(i));
+        }
+
+        return newCenters;
+    }
+
+    private boolean isFinished(Matrix centers, Matrix newCenters) {
+        int numCenters = centers.rowSize();
+
+        for (int i = 0; i < numCenters; i++) {
+            if (distance(centers.viewRow(i), newCenters.viewRow(i)) > maxCentersDelta) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public FuzzyCMeansModel cluster(DenseLocalOnHeapMatrix points, int k, List<Double> weights) throws MathIllegalArgumentException, ConvergenceException {
+        Matrix centers = new DenseLocalOnHeapMatrix(k, points.columnSize());
+        Matrix distances = new DenseLocalOnHeapMatrix(k, points.rowSize());
+        Matrix membership = new DenseLocalOnHeapMatrix(k, points.rowSize());
+        Vector weightsVector = new DenseLocalOnHeapVector(weights.size());
+        for (int i = 0; i < weights.size(); i++) {
+            weightsVector.setX(i, weights.get(i));
+        }
+
+        initializeCenters(centers, points, k, weightsVector);
+
+        int iteration = 0;
+        boolean finished = false;
+        while (iteration < maxIterations && !finished) {
+            calculateDistances(distances, points, centers);
+            calculateMembership(membership, distances, weightsVector);
+            Matrix newCenters = calculateNewCenters(points, membership);
+
+            finished = isFinished(centers, newCenters);
+            centers = newCenters;
+            iteration++;
+        }
+
+        Vector[] centersArray = new Vector[k];
+        for (int i = 0; i < k; i++) {
+            centersArray[i] = centers.getRow(i);
+        }
+        return new FuzzyCMeansModel(centersArray, measure);
+    }
 }
