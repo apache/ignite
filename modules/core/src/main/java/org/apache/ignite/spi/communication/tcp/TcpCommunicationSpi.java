001a5fb1130d893677ba5dc03797808e1ac90a41
Ilya Kasnacheev <ilya.kasnacheev@gmail.com>
2017-10-23 12:50:15 +0300

    IGNITE-6071 White list of exceptions to suppress in createTcpClient.

--- modules/core/src/main/java/org/apache/ignite/spi/communication/tcp/TcpCommunicationSpi.java
+++ modules/core/src/main/java/org/apache/ignite/spi/communication/tcp/TcpCommunicationSpi.java
@@ -3325,7 +3325,8 @@ public class TcpCommunicationSpi extends IgniteSpiAdapter implements Communicati
                 }
             }
 
-            if (X.hasCause(errs, ConnectException.class, HandshakeException.class))
+            if (!X.hasCause(errs, SocketTimeoutException.class, HandshakeTimeoutException.class,
+                IgniteSpiOperationTimeoutException.class))
                 throw errs;
         }
 
