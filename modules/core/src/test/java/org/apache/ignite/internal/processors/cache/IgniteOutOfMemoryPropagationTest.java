a83a80ec53611d48b73d09557521056b5bf8dced
mcherkasov <mcherkasov@gridgain.com>
2017-10-24 17:10:27 +0300

    Added OOM propagation to streamer.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteOutOfMemoryPropagationTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteOutOfMemoryPropagationTest.java
@@ -20,6 +20,7 @@ package org.apache.ignite.internal.processors.cache;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.ignite.IgniteCache;
+import org.apache.ignite.IgniteDataStreamer;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.cache.CacheAtomicityMode;
 import org.apache.ignite.cache.CacheMode;
@@ -37,7 +38,7 @@ import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 /**
  *
  */
-public class IgniteOutOfMemoryPropagationTest  extends GridCommonAbstractTest {
+public class IgniteOutOfMemoryPropagationTest extends GridCommonAbstractTest {
 
     public static final int NODES = 3;
     public static final String DEFAULT_CACHE_NAME = "default";
@@ -59,22 +60,30 @@ public class IgniteOutOfMemoryPropagationTest  extends GridCommonAbstractTest {
         return 10 * 60 * 1000;
     }
 
+    public void testPutOOMPropagation() throws Exception {
+        testOOMPropagation(false);
+    }
+
+    public void testStreamerOOMPropagation() throws Exception {
+       testOOMPropagation(true);
+    }
+
     /**
      *
      */
-    public void testOOMPropagation() throws Exception {
+    private void testOOMPropagation(boolean useStreamer) throws Exception {
         Throwable t = null;
         for (CacheAtomicityMode atomicityMode : CacheAtomicityMode.values()) {
             for (CacheMode cacheMode : CacheMode.values()) {
                 for (CacheWriteSynchronizationMode writeSyncMode : CacheWriteSynchronizationMode.values()) {
                     for (int backupsCount = 0; backupsCount < 1; backupsCount++) {
-                        if( writeSyncMode == CacheWriteSynchronizationMode.FULL_ASYNC
+                        if (writeSyncMode == CacheWriteSynchronizationMode.FULL_ASYNC
                             || cacheMode == CacheMode.REPLICATED)
                             continue;
 
                         initGrid(atomicityMode, cacheMode, writeSyncMode, backupsCount);
                         try {
-                            forceOOM();
+                            forceOOM(useStreamer);
                         }
                         catch (Throwable t0) {
                             t = t0;
@@ -92,6 +101,7 @@ public class IgniteOutOfMemoryPropagationTest  extends GridCommonAbstractTest {
 
     /**
      * Ignite grid of 3 server nodes with passed parameters.
+     *
      * @param atomicityMode atomicity mode
      * @param mode cache mode
      * @param writeSyncMode cache write synchronization mode
@@ -102,7 +112,7 @@ public class IgniteOutOfMemoryPropagationTest  extends GridCommonAbstractTest {
         CacheWriteSynchronizationMode writeSyncMode, int backupsCount) throws Exception {
 
         System.out.println("Starting grid: CacheAtomicityMode." + atomicityMode +
-        " CacheMode." + mode + " CacheWriteSynchronizationMode." + writeSyncMode + " backupsCount = " + backupsCount);
+            " CacheMode." + mode + " CacheWriteSynchronizationMode." + writeSyncMode + " backupsCount = " + backupsCount);
         this.atomicityMode = atomicityMode;
         this.mode = mode;
         this.backupdsCount = backupsCount;
@@ -117,22 +127,31 @@ public class IgniteOutOfMemoryPropagationTest  extends GridCommonAbstractTest {
         client = startGrid(NODES + 1);
     }
 
-    public void forceOOM() throws Exception {
+    public void forceOOM(boolean useStreamer) throws Exception {
         final IgniteCache<Object, Object> cache = client.cache(DEFAULT_CACHE_NAME);
+
+        IgniteDataStreamer<String, String> streamer = client.dataStreamer(DEFAULT_CACHE_NAME);
+
         Map<String, String> map = new HashMap<>();
 
         for (int i = 0; i < Integer.MAX_VALUE; i++) {
             map.put("k" + i, "v" + i);
 
             if (map.size() > 1_000) {
-                cache.putAll(map);
+                if (useStreamer)
+                    streamer.addData(map);
+                else
+                    cache.putAll(map);
 
                 map.clear();
             }
         }
 
         if (map.size() > 0)
-            cache.putAll(map);
+            if (useStreamer)
+                streamer.addData(map);
+            else
+                cache.putAll(map);
 
     }
 
@@ -147,7 +166,6 @@ public class IgniteOutOfMemoryPropagationTest  extends GridCommonAbstractTest {
                 * 1024 * 1024));
 
         cfg.setDataStorageConfiguration(memCfg);
-
 
         CacheConfiguration<Object, Object> baseCfg = new CacheConfiguration<>(DEFAULT_CACHE_NAME);
         baseCfg.setAtomicityMode(this.atomicityMode);
