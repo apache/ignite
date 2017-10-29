6c4c77c262f9818142819f8b40a4855a62aabd4f
Artem Malykh <amalykh@gridgain.com>
2017-10-29 15:42:43 +0300

    IGNITE-6783: First steps[3].

--- modules/ml/src/main/java/org/apache/ignite/ml/trainers/group/GroupTrainer.java
+++ modules/ml/src/main/java/org/apache/ignite/ml/trainers/group/GroupTrainer.java
@@ -18,38 +18,52 @@
 package org.apache.ignite.ml.trainers.group;
 
 import java.util.Collection;
+import java.util.UUID;
+import org.apache.ignite.IgniteCache;
 import org.apache.ignite.IgniteCluster;
 import org.apache.ignite.lang.IgniteBiTuple;
 import org.apache.ignite.lang.IgnitePredicate;
 import org.apache.ignite.ml.Model;
-import org.apache.ignite.ml.math.functions.IgniteBiFunction;
+import org.apache.ignite.ml.math.functions.IgniteBiConsumer;
+import org.apache.ignite.ml.math.functions.IgniteConsumer;
 import org.apache.ignite.ml.math.functions.IgniteFunction;
+import org.apache.ignite.ml.math.functions.IgniteUncurriedBiFunction;
 import org.apache.ignite.ml.trainers.Trainer;
 
-public abstract class GroupTrainer<I, O, R, C, M extends Model, T> implements Trainer<M, T> {
-    IgniteCluster cluster;
-    IgniteBiFunction<Integer, T, O> init;
+public abstract class GroupTrainer<I, O, R, LC, RC, M extends Model, T> implements Trainer<M, T> {
+    IgniteUncurriedBiFunction<Integer, T, O> init;
     IgniteFunction<I, O> worker;
-    IgniteFunction<I, IgniteBiTuple<C, R>> handler;
+    IgniteFunction<I, IgniteBiTuple<LC, R>> handler;
     IgnitePredicate<Collection<O>> stopper;
     IgniteFunction<Integer, R> result;
     IgniteFunction<Collection<R>, M> modelProducer;
+    IgniteCache<GroupTrainerCacheKey, RC> cache;
+    int nodeLocalEntitiesCount;
 
-    public GroupTrainer(IgniteCluster cluster, IgniteBiFunction<Integer, T, O> init, IgniteFunction<I, O> worker,
-        IgniteFunction<I, IgniteBiTuple<C, R>> handler,
+    public GroupTrainer(IgniteUncurriedBiFunction<Integer, T, O> init, IgniteFunction<I, O> worker,
+        IgniteFunction<I, IgniteBiTuple<LC, R>> handler,
         IgnitePredicate<Collection<O>> stopper,
         IgniteFunction<Integer, R> result,
-        IgniteFunction<Collection<R>, M> modelProducer) {
-        this.cluster = cluster;
+        IgniteFunction<Collection<R>, M> modelProducer,
+        IgniteCache<GroupTrainerCacheKey, RC> cache,
+        int nodeLocEntitiesCnt) {
         this.init = init;
         this.worker = worker;
         this.handler = handler;
         this.stopper = stopper;
         this.result = result;
         this.modelProducer = modelProducer;
+        this.cache = cache;
+        this.nodeLocalEntitiesCount = nodeLocEntitiesCnt;
     }
 
     @Override public M train(T data) {
+        UUID trainingUUID = UUID.randomUUID();
+
+        initGlobalContext(data, trainingUUID);
+
         return null;
     }
+
+    protected abstract void initGlobalContext(T data, UUID trainingUUID);
 }
