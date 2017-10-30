96772985a0a0137ee403a4ea17e58f3ac833c526
devozerov <vozerov@gridgain.com>
2017-10-30 12:38:42 +0300

    Started work on CREATE TABLE.

---
+++ modules/core/src/main/java/org/apache/ignite/internal/sql/command/SqlColumn.java
@@ -0,0 +1,12 @@
+package org.apache.ignite.internal.sql.command;
+
+/**
+ * SQL column.
+ */
+public class SqlColumn {
+    /** Column name. */
+    private final String name;
+
+    /** Column type. */
+    private final int typ;
+}
