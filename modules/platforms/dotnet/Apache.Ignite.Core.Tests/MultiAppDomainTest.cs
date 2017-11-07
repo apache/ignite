4b5d0dd79143e69f043c441738e35b2a459510b3
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-11-07 17:13:43 +0300

    MultiAppDomainTest added

---
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/MultiAppDomainTest.cs
@@ -0,0 +1,123 @@
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Core.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests Ignite behavior in multi-AppDomain scenario.
+    /// Such a scenario occurs within IIS, for example, or within application plugins.
+    /// </summary>
+    public class MultiAppDomainTest
+    {
+        /** */
+        public const string CacheName = "cache";
+
+        /** */
+        private readonly List<AppDomain> _domains = new List<AppDomain>();
+
+        /// <summary>
+        /// Tests the IIS behavior:
+        /// when application is restarted, new AppDomain is started while old one is still running.
+        /// </summary>
+        [Test]
+        public void TestIisBehavior()
+        {
+            var ignite = Ignition.Start(GetConfig());
+            ignite.CreateCache<int, int>(CacheName).Put(1, 1);
+
+            // Check same domain.
+            new DomainRunner().RunTest();
+
+            var type = typeof(DomainRunner);
+            Assert.IsNotNull(type.FullName);
+
+            // Start and stop domains.
+            for (var i = 0; i < 20; i++)
+            {
+                var domain = AppDomain.CreateDomain("TestIisBehavior-" + i);
+                _domains.Add(domain);
+
+                var runner = (DomainRunner) domain.CreateInstanceAndUnwrap(type.Assembly.FullName, type.FullName);
+                runner.RunTest();
+
+                if (i > 3)
+                {
+                    var oldDomain = _domains[i - 3];
+                    _domains[i - 3] = null;
+
+                    AppDomain.Unload(oldDomain);
+                }
+            }
+
+            // TODO: Verify topology, verify console output.
+        }
+
+        /// <summary>
+        /// Tears down the test.
+        /// </summary>
+        [TearDown]
+        public void TearDown()
+        {
+            Ignition.StopAll(true);
+
+            foreach (var appDomain in _domains)
+            {
+                if (appDomain != null)
+                {
+                    AppDomain.Unload(appDomain);
+                }
+            }
+
+            _domains.Clear();
+        }
+
+        /// <summary>
+        /// Gets the configuration.
+        /// </summary>
+        private static IgniteConfiguration GetConfig()
+        {
+            return new IgniteConfiguration(TestUtils.GetTestConfiguration())
+            {
+                AutoGenerateIgniteInstanceName = true
+            };
+        }
+
+        /// <summary>
+        /// Class to instantiate in another domain.
+        /// </summary>
+        private class DomainRunner : MarshalByRefObject
+        {
+            /// <summary>
+            /// Runs the test.
+            /// </summary>
+            public void RunTest()
+            {
+                var cfg = GetConfig();
+
+                // No need to stop: this will happen on domain unload.
+                var ignite = Ignition.Start(cfg);
+                
+                var cache = ignite.GetCache<int, int>(CacheName);
+                Assert.AreEqual(1, cache[1]);
+            }
+        }
+    }
+}
