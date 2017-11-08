e738b6d4730a7be20f4d1dabe0afcac0e9ef2586
Pereslegin Pavel <xtern@users.noreply.github.com>
2017-11-08 23:21:08 +0300

    IGNITE-6844 Added top ver and nodes count to cluster mx bean

---
+++ modules/core/src/main/java/org/apache/ignite/internal/cluster/IgniteClusterMXBeanImpl.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.cluster;
+
+import org.apache.ignite.internal.managers.discovery.GridDiscoveryManager;
+import org.apache.ignite.mxbean.IgniteClusterMXBean;
+
+/**
+ * Management bean for monitoring cluster state.
+ */
+public class IgniteClusterMXBeanImpl implements IgniteClusterMXBean {
+    /** Grid discovery manager. */
+    private final GridDiscoveryManager discoMgr;
+
+    /**
+     * @param discoMgr {@link GridDiscoveryManager Grid discovery manager}.
+     */
+    public IgniteClusterMXBeanImpl(GridDiscoveryManager discoMgr) {
+        this.discoMgr = discoMgr;
+    }
+
+    /** {@inheritDoc} */
+    @Override public int getTotalServerNodes() {
+        return discoMgr.discoCache().serverNodes().size();
+    }
+
+    /** {@inheritDoc} */
+    @Override public long getTopologyVersion() {
+        return discoMgr.topologyVersion();
+    }
+}
+
