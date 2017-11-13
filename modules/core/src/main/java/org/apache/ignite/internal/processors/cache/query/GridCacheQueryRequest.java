2c78c77508f97df28eda4a0e5815d85e0dded1a4
nikolay_tikhonov <ntikhonov@gridgain.com>
2017-11-13 13:46:30 +0300

    Fixed .NET tests and javadoc.
    Signed-off-by: nikolay_tikhonov <ntikhonov@gridgain.com>

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/query/GridCacheQueryRequest.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/query/GridCacheQueryRequest.java
@@ -483,10 +483,10 @@ public class GridCacheQueryRequest extends GridCacheMessage implements GridCache
     }
 
     /**
-     * This method needed for scan query. {@link #GridCacheQueryRequest#partition()} method related with IO policy and cannot be used
+     * This method needed for scan query. {@link #partition()} method related with IO policy and cannot be used
      * for this role. Otherwise Query will be processed in striped pool.
      *
-     * @return partition.
+     * @return partition ID.
      */
     public int partitionId() {
         return part;
