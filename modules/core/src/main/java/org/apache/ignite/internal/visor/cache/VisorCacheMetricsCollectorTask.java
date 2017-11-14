a78e61f766a1c242de38ce4b98d63b38b3bea6a7
vsisko <vsisko@gridgain.com>
2017-11-14 09:44:18 +0700

    IGNITE-6863 Visor CMD: Fixed check that cache available on node. (cherry picked from commit e489e37)

--- modules/core/src/main/java/org/apache/ignite/internal/visor/cache/VisorCacheMetricsCollectorTask.java
+++ modules/core/src/main/java/org/apache/ignite/internal/visor/cache/VisorCacheMetricsCollectorTask.java
@@ -22,6 +22,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import org.apache.ignite.compute.ComputeJobResult;
+import org.apache.ignite.internal.processors.cache.GridCacheContext;
 import org.apache.ignite.internal.processors.cache.GridCacheProcessor;
 import org.apache.ignite.internal.processors.cache.IgniteCacheProxy;
 import org.apache.ignite.internal.processors.task.GridInternal;
@@ -109,7 +110,9 @@ public class VisorCacheMetricsCollectorTask extends VisorMultiNodeTask<VisorCach
             boolean allCaches = cacheNames.isEmpty();
 
             for (IgniteCacheProxy ca : caches) {
-                if (ca.context().started()) {
+                GridCacheContext ctx = ca.context();
+
+                if (ctx.started() && (ctx.affinityNode() || ctx.isNear())) {
                     String cacheName = ca.getName();
 
                     VisorCacheMetrics cm = new VisorCacheMetrics(ignite, cacheName);
