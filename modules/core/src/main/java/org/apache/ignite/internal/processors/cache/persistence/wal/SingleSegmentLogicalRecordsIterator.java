e29e370bc344e5c0ab4f962f62c0351937c56458
Ivan Rakov <ivan.glukos@gmail.com>
2017-11-16 16:21:54 +0300

    IGNITE-5938 Javadoc

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/SingleSegmentLogicalRecordsIterator.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/SingleSegmentLogicalRecordsIterator.java
@@ -22,6 +22,7 @@ import java.util.Set;
 import org.apache.ignite.IgniteCheckedException;
 import org.apache.ignite.IgniteLogger;
 import org.apache.ignite.internal.pagemem.wal.WALPointer;
+import org.apache.ignite.internal.pagemem.wal.record.MarshalledRecord;
 import org.apache.ignite.internal.pagemem.wal.record.WALRecord;
 import org.apache.ignite.internal.processors.cache.GridCacheSharedContext;
 import org.apache.ignite.internal.processors.cache.persistence.file.FileIOFactory;
@@ -34,6 +35,7 @@ import org.jetbrains.annotations.Nullable;
 
 /**
  * Iterates over logical records of one WAL segment from archive. Used for WAL archive compression.
+ * Doesn't deserialize actual record data, returns {@link MarshalledRecord} instances instead.
  */
 public class SingleSegmentLogicalRecordsIterator extends AbstractWalRecordsIterator {
     /** Serial version uid. */
