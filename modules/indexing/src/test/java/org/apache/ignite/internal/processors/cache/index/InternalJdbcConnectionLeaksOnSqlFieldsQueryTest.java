6e88cbd5a6271713f61168bc8656994fe14f1819
tledkov-gridgain <tledkov@gridgain.com>
2017-12-05 12:47:28 +0300

    IGNITE-7095: cosmetic

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/index/InternalJdbcConnectionLeaksOnSqlFieldsQueryTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/index/InternalJdbcConnectionLeaksOnSqlFieldsQueryTest.java
@@ -18,11 +18,9 @@
 package org.apache.ignite.internal.processors.cache.index;
 
 import java.sql.Connection;
-import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import org.apache.ignite.IgniteCache;
-import org.apache.ignite.cache.QueryEntity;
 import org.apache.ignite.cache.query.QueryCursor;
 import org.apache.ignite.cache.query.SqlFieldsQuery;
 import org.apache.ignite.configuration.CacheConfiguration;
