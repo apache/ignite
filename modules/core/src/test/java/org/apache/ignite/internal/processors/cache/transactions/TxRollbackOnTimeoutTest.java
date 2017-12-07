7c6542553f041339f712bbde1541aa54c9418ed9
Aleksei Scherbakov <alexey.scherbakoff@gmail.com>
2017-12-07 20:10:24 +0300

    IGNITE-6827 wip.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/transactions/TxRollbackOnTimeoutTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/transactions/TxRollbackOnTimeoutTest.java
@@ -279,11 +279,12 @@ public class TxRollbackOnTimeoutTest extends GridCommonAbstractTest {
 
         deadlockUnblockedOnTimeout(ignite(0), ignite(0));
 
-        Ignite client = startClient();
+        // TODO Detection for client is unreliable, see https://issues.apache.org/jira/browse/IGNITE-6405
+        // Ignite client = startClient();
 
-        deadlockUnblockedOnTimeout(ignite(0), client);
+        // deadlockUnblockedOnTimeout(ignite(0), client);
 
-        deadlockUnblockedOnTimeout(client, ignite(0));
+        // deadlockUnblockedOnTimeout(client, ignite(0));
     }
 
     /**
