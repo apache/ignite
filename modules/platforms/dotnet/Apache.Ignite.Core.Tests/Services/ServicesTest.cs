4b6d6ac4d9483169633738389ac52bf4f28591c0
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-12-21 17:58:33 +0300

    cleanup

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Services/ServicesTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Services/ServicesTest.cs
@@ -72,10 +72,7 @@ namespace Apache.Ignite.Core.Tests.Services
         public void SetUp()
         {
             StartGrids();
-
-#if !NETCOREAPP2_0
             EventsTestHelper.ListenResult = true;
-#endif
         }
 
         /// <summary>
@@ -99,9 +96,7 @@ namespace Apache.Ignite.Core.Tests.Services
             }
             finally
             {
-#if !NETCOREAPP2_0
                 EventsTestHelper.AssertFailures();
-#endif
 
                 if (TestContext.CurrentContext.Test.Name.StartsWith("TestEventTypes"))
                     StopGrids(); // clean events for other tests
@@ -900,21 +895,21 @@ namespace Apache.Ignite.Core.Tests.Services
         /// </summary>
         private IgniteConfiguration GetConfiguration(string springConfigUrl)
         {
-#if !NETCOREAPP2_0
             if (!CompactFooter)
                 springConfigUrl = ComputeApiTestFullFooter.ReplaceFooterSetting(springConfigUrl);
-#endif
 
-            return new IgniteConfiguration(TestUtils.GetTestConfiguration())
+            return new IgniteConfiguration
             {
                 SpringConfigUrl = springConfigUrl,
+                JvmClasspath = TestUtils.CreateTestClasspath(),
+                JvmOptions = TestUtils.TestJavaOptions(),
                 BinaryConfiguration = new BinaryConfiguration(
                     typeof (TestIgniteServiceBinarizable),
                     typeof (TestIgniteServiceBinarizableErr),
                     typeof (PlatformComputeBinarizable),
                     typeof (BinarizableObject))
                 {
-                    NameMapper = new BinaryBasicNameMapper { IsSimpleName = true }
+                    NameMapper = BinaryBasicNameMapper.SimpleNameInstance
                 }
             };
         }
@@ -999,7 +994,7 @@ namespace Apache.Ignite.Core.Tests.Services
             int Method(int arg);
         }
 
-#pragma warning disable 649
+        #pragma warning disable 649
 
         /// <summary>
         /// Test serializable service.
