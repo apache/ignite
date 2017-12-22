282103eb5050796f92fb07ce1eb898d52da5bc1d
Alexey Goncharuk <alexey.goncharuk@gmail.com>
2017-12-22 15:37:16 +0300

    Fixing checkpoint lock acquisition
    (cherry picked from commit 144c666)
    (cherry picked from commit b00be67)
    (cherry picked from commit 003f033)
    (cherry picked from commit 956092f)
    (cherry picked from commit db4fa5b)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/GridDhtTxPrepareFuture.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/GridDhtTxPrepareFuture.java
@@ -347,6 +347,8 @@ public final class GridDhtTxPrepareFuture extends GridCacheCompoundFuture<Ignite
 
             ExpiryPolicy expiry = cacheCtx.expiryForTxEntry(txEntry);
 
+            cctx.database().checkpointReadLock();
+
             try {
                 if ((txEntry.op() == CREATE || txEntry.op() == UPDATE) &&
                     txEntry.conflictExpireTime() == CU.EXPIRE_TIME_CALCULATE) {
@@ -508,6 +510,9 @@ public final class GridDhtTxPrepareFuture extends GridCacheCompoundFuture<Ignite
             }
             catch (GridCacheEntryRemovedException e) {
                 assert false : "Got entry removed exception while holding transactional lock on entry [e=" + e + ", cached=" + cached + ']';
+            }
+            finally {
+                cctx.database().checkpointReadUnlock();
             }
         }
     }
