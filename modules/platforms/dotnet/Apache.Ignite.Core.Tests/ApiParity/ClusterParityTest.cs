8f93cb4f6d697a47ba247644191d09039f7b9c1c
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-12-25 15:42:55 +0300

    .NET: Suppress autoactivation API parity failures with link to IGNITE-7301

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/ApiParity/ClusterParityTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/ApiParity/ClusterParityTest.cs
@@ -38,7 +38,11 @@
         /** Members that are missing on .NET side and should be added in future. */
         private static readonly string[] MissingMembers =
         {
-            "enableStatistics"  // IGNITE-7276
+            "enableStatistics",  // IGNITE-7276
+            
+            // IGNITE-7301
+            "active",
+            "setBaselineTopology"
         };
 
         /// <summary>
