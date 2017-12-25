7c0fe0474c24d8ae61f6be94f6f31ef9e6813db6
gg-shq <kshirokov@gridgain.com>
2017-12-25 15:44:58 +0300

    IGNITE-6861: Fixed javadoc in SqlParserCreateTableSelfTest.

--- modules/core/src/test/java/org/apache/ignite/internal/sql/SqlParserCreateTableSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/sql/SqlParserCreateTableSelfTest.java
@@ -55,27 +55,22 @@ import static org.apache.ignite.internal.sql.SqlKeyword.WRITE_SYNCHRONIZATION_MO
 @SuppressWarnings({"UnusedReturnValue", "ThrowableNotThrown"})
 public class SqlParserCreateTableSelfTest extends SqlParserAbstractSelfTest {
 
-    /** FIXME */
-    private static final String PARTITIONED_TMPL_NAME = CacheMode.PARTITIONED.name();
-    /** FIXME */
-    private static final String REPLICATED_TMPL_NAME = CacheMode.REPLICATED.name();
-
-    /** FIXME */
+    /** Correct primary key name. */
     private static final String PK_NAME = "pk";
-    /** FIXME */
+    /** Incorrect primary key name (incorrect identifier). */
     private static final String WRONG_PK_NAME = "wrong-pk";
 
-    /** FIXME */
+    /** List of good identifiers. */
     private static final String[] GOOD_IDS = new String[] { "test" };
-    /** FIXME */
+    /** List of incorrect identifiers. */
     private static final String[] BAD_IDS = new String[] { "1badId", "`badId" };
 
-    /** FIXME */
+    /** List of good string values. */
     private static final String GOOD_STR = "test";
 
-    /** FIXME */
+    /** Parameter tests. */
     private static final List<TestParamDef<?>> PARAM_TESTS = new LinkedList<>();
-    /** FIXME */
+    /** Default parameter values (when not specified in SQL command). */
     private static final List<TestParamDef.DefValPair<?>> DEFAULT_PARAM_VALS;
 
     static {
@@ -83,8 +78,8 @@ public class SqlParserCreateTableSelfTest extends SqlParserAbstractSelfTest {
             Arrays.asList(
                 new TestParamDef.MissingValue<String>(null),
                 new TestParamDef.ValidValue<String>(DEFAULT, null),
-                new TestParamDef.ValidIdentityValue<>(PARTITIONED_TMPL_NAME),
-                new TestParamDef.ValidIdentityValue<>(REPLICATED_TMPL_NAME)
+                new TestParamDef.ValidIdentityValue<>(CacheMode.PARTITIONED.name()),
+                new TestParamDef.ValidIdentityValue<>(CacheMode.REPLICATED.name())
         )));
 
         PARAM_TESTS.add(new TestParamDef<>(BACKUPS, "backups", Integer.class,
@@ -152,7 +147,13 @@ public class SqlParserCreateTableSelfTest extends SqlParserAbstractSelfTest {
         DEFAULT_PARAM_VALS = createDefaultParamVals(PARAM_TESTS);
     }
 
-    /** FIXME */
+    /**
+     * Creates {@link SqlParserCreateTableSelfTest#DEFAULT_PARAM_VALS} list by taking defaults from
+     * {@link SqlParserCreateTableSelfTest#PARAM_TESTS}.
+     *
+     * @param paramTests Tests for parameters.
+     * @return List with default parameter values.
+     */
     @SuppressWarnings("unchecked")
     private static List<TestParamDef.DefValPair<?>> createDefaultParamVals(List<TestParamDef<?>> paramTests) {
         List<TestParamDef.DefValPair<?>> defParamVals = new LinkedList<>();
@@ -179,7 +180,7 @@ public class SqlParserCreateTableSelfTest extends SqlParserAbstractSelfTest {
     }
 
     /**
-     * Tests for CREATE TABLE command.
+     * Tests for base CREATE TABLE command.
      *
      * @throws Exception If failed.
      */
@@ -217,7 +218,11 @@ public class SqlParserCreateTableSelfTest extends SqlParserAbstractSelfTest {
         }
     }
 
-    /** FIXME */
+    /**
+     * Tests for column definition section in CREATE TABLE command.
+     *
+     * @throws AssertionError If failed.
+     */
     public void testColumnNames() {
         try (H2FallbackTempDisabler disabler = new H2FallbackTempDisabler(true)) {
             assertParseError(null,
@@ -238,7 +243,27 @@ public class SqlParserCreateTableSelfTest extends SqlParserAbstractSelfTest {
         }
     }
 
-    /** FIXME */
+    /**
+     * Tests for parameters of CREATE TABLE command. Each parameter is tested with a list of options.
+     *
+     * @throws AssertionError If failed.
+     */
+    public void testParams() throws Exception {
+
+        try (H2FallbackTempDisabler disabler = new H2FallbackTempDisabler(true)) {
+
+            String baseCmd = "CREATE TABLE tbl (" + PK_NAME + " INT PRIMARY KEY, b VARCHAR)";
+
+            for (TestParamDef testParamDef : PARAM_TESTS)
+                testParameter(null, baseCmd, testParamDef, DEFAULT_PARAM_VALS);
+        }
+    }
+
+    /**
+     * Tests for wrapping-related parameters for CREATE TABLE command.
+     *
+     * @throws AssertionError If failed.
+     */
     public void testWrapParams() {
 
         try (H2FallbackTempDisabler disabler = new H2FallbackTempDisabler(true)) {
@@ -264,233 +289,4 @@ public class SqlParserCreateTableSelfTest extends SqlParserAbstractSelfTest {
                 "Key and value type names should be different for CREATE TABLE: KvType");
         }
     }
-
-    /** FIXME */
-    public void testParams() throws Exception {
-
-        try (H2FallbackTempDisabler disabler = new H2FallbackTempDisabler(true)) {
-
-            String baseCmd = "CREATE TABLE tbl (" + PK_NAME + " INT PRIMARY KEY, b VARCHAR)";
-
-            for (TestParamDef testParamDef : PARAM_TESTS)
-                testParameter(null, baseCmd, testParamDef, DEFAULT_PARAM_VALS);
-        }
-
-
-//        // Base.
-//        parseValidate(null, "CREATE INDEX idx ON tbl(a)", null, "TBL", "IDX", DEFAULT_PROPS, "A", false);
-//        parseValidate(null, "CREATE INDEX idx ON tbl(a ASC)", null, "TBL", "IDX", DEFAULT_PROPS, "A", false);
-//        parseValidate(null, "CREATE INDEX idx ON tbl(a DESC)", null, "TBL", "IDX", DEFAULT_PROPS, "A", true);
-//
-//        // Case (in)sensitivity.
-//        parseValidate(null, "CREATE INDEX IDX ON TBL(COL)", null, "TBL", "IDX", DEFAULT_PROPS, "COL", false);
-//        parseValidate(null, "CREATE INDEX iDx ON tBl(cOl)", null, "TBL", "IDX", DEFAULT_PROPS, "COL", false);
-//
-//        parseValidate(null, "CREATE INDEX \"idx\" ON tbl(col)", null, "TBL", "idx", DEFAULT_PROPS, "COL", false);
-//        parseValidate(null, "CREATE INDEX \"iDx\" ON tbl(col)", null, "TBL", "iDx", DEFAULT_PROPS, "COL", false);
-//
-//        parseValidate(null, "CREATE INDEX idx ON \"tbl\"(col)", null, "tbl", "IDX", DEFAULT_PROPS, "COL", false);
-//        parseValidate(null, "CREATE INDEX idx ON \"tBl\"(col)", null, "tBl", "IDX", DEFAULT_PROPS, "COL", false);
-//
-//        parseValidate(null, "CREATE INDEX idx ON tbl(\"col\")", null, "TBL", "IDX", DEFAULT_PROPS, "col", false);
-//        parseValidate(null, "CREATE INDEX idx ON tbl(\"cOl\")", null, "TBL", "IDX", DEFAULT_PROPS, "cOl", false);
-//
-//        parseValidate(null, "CREATE INDEX idx ON tbl(\"cOl\" ASC)", null, "TBL", "IDX", DEFAULT_PROPS, "cOl", false);
-//        parseValidate(null, "CREATE INDEX idx ON tbl(\"cOl\" DESC)", null, "TBL", "IDX", DEFAULT_PROPS, "cOl", true);
-//
-//        // Columns.
-//        parseValidate(null, "CREATE INDEX idx ON tbl(a, b)", null, "TBL", "IDX", DEFAULT_PROPS, "A", false, "B", false);
-//
-//        parseValidate(null, "CREATE INDEX idx ON tbl(a ASC, b)", null, "TBL", "IDX", DEFAULT_PROPS, "A", false, "B", false);
-//        parseValidate(null, "CREATE INDEX idx ON tbl(a, b ASC)", null, "TBL", "IDX", DEFAULT_PROPS, "A", false, "B", false);
-//        parseValidate(null, "CREATE INDEX idx ON tbl(a ASC, b ASC)", null, "TBL", "IDX", DEFAULT_PROPS, "A", false, "B", false);
-//
-//        parseValidate(null, "CREATE INDEX idx ON tbl(a DESC, b)", null, "TBL", "IDX", DEFAULT_PROPS, "A", true, "B", false);
-//        parseValidate(null, "CREATE INDEX idx ON tbl(a, b DESC)", null, "TBL", "IDX", DEFAULT_PROPS, "A", false, "B", true);
-//        parseValidate(null, "CREATE INDEX idx ON tbl(a DESC, b DESC)", null, "TBL", "IDX", DEFAULT_PROPS, "A", true, "B", true);
-//
-//        parseValidate(null, "CREATE INDEX idx ON tbl(a ASC, b DESC)", null, "TBL", "IDX", DEFAULT_PROPS, "A", false, "B", true);
-//        parseValidate(null, "CREATE INDEX idx ON tbl(a DESC, b ASC)", null, "TBL", "IDX", DEFAULT_PROPS, "A", true, "B", false);
-//
-//        parseValidate(null, "CREATE INDEX idx ON tbl(a, b, c)", null, "TBL", "IDX", DEFAULT_PROPS, "A", false, "B", false, "C", false);
-//        parseValidate(null, "CREATE INDEX idx ON tbl(a DESC, b, c)", null, "TBL", "IDX", DEFAULT_PROPS, "A", true, "B", false, "C", false);
-//        parseValidate(null, "CREATE INDEX idx ON tbl(a, b DESC, c)", null, "TBL", "IDX", DEFAULT_PROPS, "A", false, "B", true, "C", false);
-//        parseValidate(null, "CREATE INDEX idx ON tbl(a, b, c DESC)", null, "TBL", "IDX", DEFAULT_PROPS, "A", false, "B", false, "C", true);
-//
-//        // Negative cases.
-//        assertParseError(null, "CREATE INDEX idx ON tbl()", "Unexpected token");
-//        assertParseError(null, "CREATE INDEX idx ON tbl(a, a)", "Column already defined: A");
-//        assertParseError(null, "CREATE INDEX idx ON tbl(a, b, a)", "Column already defined: A");
-//        assertParseError(null, "CREATE INDEX idx ON tbl(b, a, a)", "Column already defined: A");
-//
-//        // Tests with schema.
-//        parseValidate(null, "CREATE INDEX idx ON schema.tbl(a)", "SCHEMA", "TBL", "IDX", DEFAULT_PROPS, "A", false);
-//        parseValidate(null, "CREATE INDEX idx ON \"schema\".tbl(a)", "schema", "TBL", "IDX", DEFAULT_PROPS, "A", false);
-//        parseValidate(null, "CREATE INDEX idx ON \"sChema\".tbl(a)", "sChema", "TBL", "IDX", DEFAULT_PROPS, "A", false);
-//
-//        parseValidate("SCHEMA", "CREATE INDEX idx ON tbl(a)", "SCHEMA", "TBL", "IDX", DEFAULT_PROPS, "A", false);
-//        parseValidate("schema", "CREATE INDEX idx ON tbl(a)", "schema", "TBL", "IDX", DEFAULT_PROPS, "A", false);
-//        parseValidate("sChema", "CREATE INDEX idx ON tbl(a)", "sChema", "TBL", "IDX", DEFAULT_PROPS, "A", false);
-//
-//        // No index name.
-//        parseValidate(null, "CREATE INDEX ON tbl(a)", null, "TBL", null, DEFAULT_PROPS, "A", false);
-//        parseValidate(null, "CREATE INDEX ON schema.tbl(a)", "SCHEMA", "TBL", null, DEFAULT_PROPS, "A", false);
-//
-//        // NOT EXISTS
-//        SqlCreateIndexCommand cmd;
-//
-//        cmd = parseValidate(null, "CREATE INDEX idx ON schema.tbl(a)", "SCHEMA", "TBL", "IDX", DEFAULT_PROPS, "A", false);
-//        assertFalse(cmd.ifNotExists());
-//
-//        cmd = parseValidate(null, "CREATE INDEX IF NOT EXISTS idx ON schema.tbl(a)", "SCHEMA", "TBL", "IDX", DEFAULT_PROPS, "A", false);
-//        assertTrue(cmd.ifNotExists());
-//
-//        assertParseError(null, "CREATE INDEX IF idx ON tbl(a)", "Unexpected token: \"IDX\"");
-//        assertParseError(null, "CREATE INDEX IF NOT idx ON tbl(a)", "Unexpected token: \"IDX\"");
-//        assertParseError(null, "CREATE INDEX IF EXISTS idx ON tbl(a)", "Unexpected token: \"EXISTS\"");
-//        assertParseError(null, "CREATE INDEX NOT EXISTS idx ON tbl(a)", "Unexpected token: \"NOT\"");
-//
-//        // SPATIAL
-//        cmd = parseValidate(null, "CREATE INDEX idx ON schema.tbl(a)", "SCHEMA", "TBL", "IDX", DEFAULT_PROPS, "A", false);
-//        assertFalse(cmd.spatial());
-//
-//        cmd = parseValidate(null, "CREATE SPATIAL INDEX idx ON schema.tbl(a)", "SCHEMA", "TBL", "IDX", DEFAULT_PROPS, "A", false);
-//        assertTrue(cmd.spatial());
-//
-//        // UNIQUE
-//        assertParseError(null, "CREATE UNIQUE INDEX idx ON tbl(a)", "Unsupported keyword: \"UNIQUE\"");
-//
-//        // HASH
-//        assertParseError(null, "CREATE HASH INDEX idx ON tbl(a)", "Unsupported keyword: \"HASH\"");
-//
-//        // PRIMARY KEY
-//        assertParseError(null, "CREATE PRIMARY KEY INDEX idx ON tbl(a)", "Unsupported keyword: \"PRIMARY\"");
-//
-//        // PARALLEL
-//        parseValidate(null, "CREATE INDEX idx ON tbl(a DESC) PARALLEL 1", null, "TBL", "IDX", getProps(1, null), "A", true);
-//        parseValidate(null, "CREATE INDEX idx ON tbl(a DESC) PARALLEL  3", null, "TBL", "IDX", getProps(3, null), "A", true);
-//        parseValidate(null, "CREATE INDEX idx ON tbl(a DESC)   PARALLEL  7", null, "TBL", "IDX", getProps(7, null), "A", true);
-//        parseValidate(null, "CREATE INDEX idx ON tbl(a DESC)   PARALLEL  0", null, "TBL", "IDX", getProps(0, null), "A", true);
-//        assertParseError(null, "CREATE INDEX idx ON tbl(a DESC) PARALLEL ", "Failed to parse SQL statement \"CREATE INDEX idx ON tbl(a DESC) PARALLEL [*]\"");
-//        assertParseError(null, "CREATE INDEX idx ON tbl(a DESC) PARALLEL abc", "Unexpected token: \"ABC\"");
-//        assertParseError(null, "CREATE INDEX idx ON tbl(a DESC) PARALLEL -2", "Failed to parse SQL statement \"CREATE INDEX idx ON tbl(a DESC) PARALLEL -[*]2\": Illegal PARALLEL value. Should be positive: -2");
-//
-//        // INLINE_SIZE option
-//        assertParseError(null, "CREATE INDEX ON tbl(a) INLINE_SIZE",
-//            "Unexpected end of command (expected: \"[integer]\")");
-//
-//        assertParseError(null, "CREATE INDEX ON tbl(a) INLINE_SIZE HASH",
-//            "Unexpected token: \"HASH\" (expected: \"[integer]\")");
-//
-//        assertParseError(null, "CREATE INDEX ON tbl(a) INLINE_SIZE elegua",
-//            "Unexpected token: \"ELEGUA\" (expected: \"[integer]\")");
-//
-//        assertParseError(null, "CREATE INDEX ON tbl(a) INLINE_SIZE -9223372036854775808",
-//            "Unexpected token: \"9223372036854775808\" (expected: \"[integer]\")");
-//
-//        assertParseError(null, "CREATE INDEX ON tbl(a) INLINE_SIZE " + Integer.MIN_VALUE,
-//            "Illegal INLINE_SIZE value. Should be positive: " + Integer.MIN_VALUE);
-//
-//        assertParseError(null, "CREATE INDEX ON tbl(a) INLINE_SIZE -1", "Failed to parse SQL statement \"CREATE INDEX ON tbl(a) INLINE_SIZE -[*]1\": Illegal INLINE_SIZE value. Should be positive: -1");
-//
-//        parseValidate(null, "CREATE INDEX idx ON schema.tbl(a) INLINE_SIZE 0", "SCHEMA", "TBL", "IDX", getProps(null, 0), "A", false);
-//        parseValidate(null, "CREATE INDEX idx ON schema.tbl(a) INLINE_SIZE 1", "SCHEMA", "TBL", "IDX", getProps(null, 1), "A", false);
-//        parseValidate(null, "CREATE INDEX idx ON schema.tbl(a) INLINE_SIZE " + Integer.MAX_VALUE,
-//            "SCHEMA", "TBL", "IDX", getProps(null, Integer.MAX_VALUE), "A", false);
-//
-//        // Both parallel and inline size
-//        parseValidate(null, "CREATE INDEX idx ON schema.tbl(a) INLINE_SIZE 5 PARALLEL 7", "SCHEMA", "TBL", "IDX", getProps(7, 5), "A", false);
-//        parseValidate(null, "CREATE INDEX idx ON schema.tbl(a) PARALLEL 3 INLINE_SIZE 9 ", "SCHEMA", "TBL", "IDX", getProps(3, 9), "A", false);
-//
-//        assertParseError(null, "CREATE INDEX idx ON schema.tbl(a) PARALLEL 3 INLINE_SIZE 9 PARALLEL 2", "Failed to parse SQL statement \"CREATE INDEX idx ON schema.tbl(a) PARALLEL 3 INLINE_SIZE [*]9 PARALLEL 2\": Only one PARALLEL clause may be specified.");
-//        assertParseError(null, "CREATE INDEX idx ON schema.tbl(a) PARALLEL 3 INLINE_SIZE 9 abc ", "Failed to parse SQL statement \"CREATE INDEX idx ON schema.tbl(a) PARALLEL 3 INLINE_SIZE 9 [*]abc \": Unexpected token: \"ABC\"");
-//        assertParseError(null, "CREATE INDEX idx ON schema.tbl(a) PARALLEL  INLINE_SIZE 9 abc ", "Failed to parse SQL statement \"CREATE INDEX idx ON schema.tbl(a) PARALLEL  [*]INLINE_SIZE 9 abc \": Unexpected token: \"INLINE_SIZE\" (expected: \"[integer]\")");
-//        assertParseError(null, "CREATE INDEX idx ON schema.tbl(a) PARALLEL 3 INLINE_SIZE abc ", "Failed to parse SQL statement \"CREATE INDEX idx ON schema.tbl(a) PARALLEL 3 INLINE_SIZE [*]abc \": Unexpected token: \"ABC\" (expected: \"[integer]\")");
-    }
-
-
-
-    /**
-     * Parse and validate SQL script.
-     *
-     * @param schema Schema.
-     * @param sql SQL.
-     * @param expSchemaName Expected schema name.
-     * @param expTblName Expected table name.
-     * @param props Expected properties.
-     * @param expColDefs Expected column definitions.
-     * @return Command.
-     */
-    private static SqlCreateTableCommand parseValidate(String schema, String sql, String expSchemaName,
-        String expTblName, Map<String, Object> props, Object... expColDefs) {
-
-        SqlCreateTableCommand cmd = (SqlCreateTableCommand)new SqlParser(schema, sql).nextCommand();
-
-        validate(cmd, expSchemaName, expTblName, props, expColDefs);
-
-        return cmd;
-    }
-
-    /**
-     * Validate create index command.
-     *
-     * @param cmd Command.
-     * @param expSchemaName Expected schema name.
-     * @param expTblName Expected table name.
-     * @param props Expected properties.
-     * @param expColDefs Expected column definitions.
-     */
-    private static void validate(SqlCreateTableCommand cmd, String expSchemaName, String expTblName,
-        Map<String, Object> props, Object... expColDefs) {
-
-//        assertEquals(expSchemaName, cmd.schemaName());
-//        assertEquals(expTblName, cmd.tableName());
-//
-//        Map<String, Object> cmpProps = getProps(cmd.parallel(), cmd.inlineSize());
-//
-//        assertEquals(cmpProps, props);
-//
-//        if (F.isEmpty(expColDefs) || expColDefs.length % 2 == 1)
-//            throw new IllegalArgumentException("Column definitions must be even.");
-//
-//        Map<String, SqlColumn> cols = cmd.columns();
-//
-//        assertEquals(expColDefs.length / 2, cols.size());
-//
-//        Iterator<SqlColumn> colIter = cols.values().iterator();
-//
-//        for (int i = 0; i < expColDefs.length;) {
-//            SqlIndexColumn col = colIter.next();
-//
-//            String expColName = (String)expColDefs[i++];
-//            Boolean expDesc = (Boolean) expColDefs[i++];
-//
-//            assertEquals(expColName, col.name());
-//            assertEquals(expDesc, (Boolean)col.descending());
-//        }
-    }
-
-    /**
-     * Returns map with command properties.
-     *
-     * @param parallel Parallel property value. <code>Null</code> for a default value.
-     * @param inlineSize Inline size property value. <code>Null</code> for a default value.
-     * @return Command properties.
-     */
-    private static Map<String, Object> getProps(Integer parallel, Integer inlineSize) {
-//        if (parallel == null)
-//            parallel = 0;
-//
-//        if (inlineSize == null)
-//            inlineSize = QueryIndex.DFLT_INLINE_SIZE;
-//
-        Map<String, Object> props = new HashMap<>();
-//
-//        props.put(PARALLEL, parallel);
-//        props.put(INLINE_SIZE, inlineSize);
-//
-        return Collections.unmodifiableMap(props);
-    }
-
-
 }
