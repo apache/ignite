2cb9d1528cc061ef6c119ffe7d4b5b0e1b67d360
dmitrievanthony <dmitrievanthony@gmail.com>
2018-01-09 13:06:23 +0300

    IGNITE-7332 Use javassist to generate test classes in runtime.

--- examples/src/test/ml/org/apache/ignite/ml/testsuites/IgniteExamplesMLTestSuite.java
+++ examples/src/test/ml/org/apache/ignite/ml/testsuites/IgniteExamplesMLTestSuite.java
@@ -17,33 +17,20 @@
 
 package org.apache.ignite.ml.testsuites;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtNewMethod;
+import javassist.NotFoundException;
 import junit.framework.TestSuite;
-import org.apache.ignite.ml.examples.CacheMatrixExampleSelfTest;
-import org.apache.ignite.ml.examples.CacheVectorExampleSelfTest;
-import org.apache.ignite.ml.examples.CholeskyDecompositionExampleSelfTest;
-import org.apache.ignite.ml.examples.DistributedLinearRegressionExampleWithQRTrainerSelfTest;
-import org.apache.ignite.ml.examples.DistributedLinearRegressionExampleWithSGDTrainerTest;
-import org.apache.ignite.ml.examples.EigenDecompositionExampleSelfTest;
-import org.apache.ignite.ml.examples.FuzzyCMeansExampleSelfTest;
-import org.apache.ignite.ml.examples.KMeansDistributedClustererExampleSelfTest;
-import org.apache.ignite.ml.examples.KMeansLocalClustererExampleSelfTest;
-import org.apache.ignite.ml.examples.KNNClassificationExampleSelfTest;
-import org.apache.ignite.ml.examples.KNNRegressionExampleSelfTest;
-import org.apache.ignite.ml.examples.LUDecompositionExampleSelfTest;
-import org.apache.ignite.ml.examples.MNISTExampleSelfTest;
-import org.apache.ignite.ml.examples.MatrixCustomStorageExampleSelfTest;
-import org.apache.ignite.ml.examples.MatrixExampleSelfTest;
-import org.apache.ignite.ml.examples.OffHeapMatrixExampleSelfTest;
-import org.apache.ignite.ml.examples.OffHeapVectorExampleSelfTest;
-import org.apache.ignite.ml.examples.QRDecompositionExampleSelfTest;
-import org.apache.ignite.ml.examples.SingularValueDecompositionExampleSelfTest;
-import org.apache.ignite.ml.examples.SparseDistributedMatrixExampleSelfTest;
-import org.apache.ignite.ml.examples.SparseMatrixExampleSelfTest;
-import org.apache.ignite.ml.examples.SparseVectorExampleSelfTest;
-import org.apache.ignite.ml.examples.TracerExampleSelfTest;
-import org.apache.ignite.ml.examples.VectorCustomStorageExampleSelfTest;
-import org.apache.ignite.ml.examples.VectorExampleSelfTest;
 import org.apache.ignite.testframework.GridTestUtils;
+import org.apache.ignite.testframework.junits.common.GridAbstractExamplesTest;
 
 import static org.apache.ignite.IgniteSystemProperties.IGNITE_OVERRIDE_MCAST_GRP;
 
@@ -57,38 +44,95 @@ public class IgniteExamplesMLTestSuite extends TestSuite {
      * @return Suite.
      * @throws Exception If failed.
      */
-    public static TestSuite suite() {
+    public static TestSuite suite() throws Exception {
         System.setProperty(IGNITE_OVERRIDE_MCAST_GRP,
             GridTestUtils.getNextMulticastGroup(IgniteExamplesMLTestSuite.class));
 
         TestSuite suite = new TestSuite("Ignite ML Examples Test Suite");
 
-        suite.addTest(new TestSuite(CacheMatrixExampleSelfTest.class));
-        suite.addTest(new TestSuite(CacheVectorExampleSelfTest.class));
-        suite.addTest(new TestSuite(CholeskyDecompositionExampleSelfTest.class));
-        suite.addTest(new TestSuite(DistributedLinearRegressionExampleWithQRTrainerSelfTest.class));
-        suite.addTest(new TestSuite(DistributedLinearRegressionExampleWithSGDTrainerTest.class));
-        suite.addTest(new TestSuite(EigenDecompositionExampleSelfTest.class));
-        suite.addTest(new TestSuite(FuzzyCMeansExampleSelfTest.class));
-        suite.addTest(new TestSuite(KMeansDistributedClustererExampleSelfTest.class));
-        suite.addTest(new TestSuite(KMeansLocalClustererExampleSelfTest.class));
-        suite.addTest(new TestSuite(KNNClassificationExampleSelfTest.class));
-        suite.addTest(new TestSuite(KNNRegressionExampleSelfTest.class));
-        suite.addTest(new TestSuite(LUDecompositionExampleSelfTest.class));
-        suite.addTest(new TestSuite(MatrixCustomStorageExampleSelfTest.class));
-        suite.addTest(new TestSuite(MatrixExampleSelfTest.class));
-        suite.addTest(new TestSuite(MNISTExampleSelfTest.class));
-        suite.addTest(new TestSuite(OffHeapMatrixExampleSelfTest.class));
-        suite.addTest(new TestSuite(OffHeapVectorExampleSelfTest.class));
-        suite.addTest(new TestSuite(QRDecompositionExampleSelfTest.class));
-        suite.addTest(new TestSuite(SingularValueDecompositionExampleSelfTest.class));
-        suite.addTest(new TestSuite(SparseDistributedMatrixExampleSelfTest.class));
-        suite.addTest(new TestSuite(SparseMatrixExampleSelfTest.class));
-        suite.addTest(new TestSuite(SparseVectorExampleSelfTest.class));
-        suite.addTest(new TestSuite(TracerExampleSelfTest.class));
-        suite.addTest(new TestSuite(VectorCustomStorageExampleSelfTest.class));
-        suite.addTest(new TestSuite(VectorExampleSelfTest.class));
+        for (Class clazz : getClasses("org.apache.ignite.examples.ml", ".*Example$"))
+            suite.addTest(new TestSuite(makeTestClass(clazz, "org.apache.ignite.ml.examples")));
 
         return suite;
+    }
+
+    /**
+     * Creates test class for given example.
+     *
+     * @param exampleCls Class of the example to be tested
+     * @param basePkgForTests Base package to create test classes in
+     * @return Test class
+     * @throws NotFoundException if class not found
+     * @throws CannotCompileException if test class cannot be compiled
+     */
+    private static Class makeTestClass(Class<?> exampleCls, String basePkgForTests)
+        throws NotFoundException, CannotCompileException {
+        ClassPool cp = ClassPool.getDefault();
+
+        CtClass cl = cp.makeClass(basePkgForTests + "." + exampleCls.getSimpleName() + "SelfName");
+
+        cl.setSuperclass(cp.getCtClass(GridAbstractExamplesTest.class.getName()));
+
+        cl.addMethod(CtNewMethod.make("public void testExample() { "
+            + exampleCls.getCanonicalName()
+            + ".main("
+            + GridAbstractExamplesTest.class.getName()
+            + ".EMPTY_ARGS); }", cl));
+
+        return cl.toClass();
+    }
+
+    /**
+     * Scans all classes accessible from the context class loader which belong to the given package and subpackages.
+     *
+     * @param pkgName The base package
+     * @return The classes
+     * @throws ClassNotFoundException if some classes not found
+     * @throws IOException if some resources unavailable
+     */
+    private static List<Class> getClasses(String pkgName, String clsNamePtrn) throws ClassNotFoundException, IOException {
+        String path = pkgName.replace('.', '/');
+
+        Enumeration<URL> resources = Thread.currentThread()
+            .getContextClassLoader()
+            .getResources(path);
+
+        List<File> dirs = new ArrayList<>();
+        while (resources.hasMoreElements())
+            dirs.add(new File(resources.nextElement().getFile()));
+
+        List<Class> classes = new ArrayList<>();
+        for (File directory : dirs)
+            classes.addAll(findClasses(directory, pkgName, clsNamePtrn));
+
+        return classes;
+    }
+
+    /**
+     * Recursive method used to find all classes in a given directory and subdirs.
+     *
+     * @param dir The base directory
+     * @param pkgName The package name for classes found inside the base directory
+     * @return The classes
+     * @throws ClassNotFoundException if class not found
+     */
+    private static List<Class> findClasses(File dir, String pkgName, String clsNamePtrn) throws ClassNotFoundException {
+        List<Class> classes = new ArrayList<>();
+        if (!dir.exists())
+            return classes;
+
+        File[] files = dir.listFiles();
+        if (files != null)
+            for (File file : files) {
+                if (file.isDirectory())
+                    classes.addAll(findClasses(file, pkgName + "." + file.getName(), clsNamePtrn));
+                else if (file.getName().endsWith(".class")) {
+                    String clsName = pkgName + '.' + file.getName().substring(0, file.getName().length() - 6);
+                    if (clsName.matches(clsNamePtrn))
+                        classes.add(Class.forName(clsName));
+                }
+            }
+
+        return classes;
     }
 }
