41a720709490ad24c2ad347a1839e369b140ece9
rkondakov <rkondakov@gridgain.com>
2018-01-10 16:38:18 +0300

    Added tests for JDBC and Java query API.

--- modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/IgniteH2Indexing.java
+++ modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/IgniteH2Indexing.java
@@ -1509,7 +1509,8 @@ public class IgniteH2Indexing implements GridQueryIndexing {
                                 cachesCreated = true;
                             }
                             else
-                                throw new IgniteSQLException(e.getMessage(), IgniteQueryErrorCode.PARSING, e);
+                                throw new IgniteSQLException("Failed to parse query. " + e.getMessage(),
+                                    IgniteQueryErrorCode.PARSING, e);
                         }
                     }
 
