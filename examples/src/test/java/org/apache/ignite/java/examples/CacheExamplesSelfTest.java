5a887372b0fd0d5a4992e2cc467131952af79e24
Ivanov Petr <pivanov@gridgain.com>
2018-01-10 16:48:55 +0300

    IGNITE-7203 Java 8 by default  * fixed https://reviews.ignite.apache.org/ignite/review/IGNT-CR-450 review issues IGNITE-7203 Java 8 by default  * further improved AOP test behaviour by moving javaagent call into pom.xml .NET: Fix WindowsServiceTest - pass JVM path to the service .NET: Another attempt to fix WindowsServiceTest, adding logger, no idea what the problem is .NET: Another attempt to fix WindowsServiceTest .NET: Another attempt to fix WindowsServiceTest .NET: Another attempt to fix WindowsServiceTest IGNITE-7203 Java 8 by default  * replaced aspectj dependency with newest one for attempt to fix AOP tests Attempty to fix AOP failure. .NET: Fix TestStopFromJava .NET: Fix ExecutableTest IGNITE-7203 Java 8 by default IGNITE-7216 Refactor examples project  * fixed notes from review IGNITE-7203 Java 8 by default IGNITE-7216 Refactor examples project  * refactored examples module and ml part of other modules: merged java8 and java packages IGNITE-7203 Java 8 by default  * got rid of java-1.8 related profiles  * remove ml profile and included ml build into main reactor  * added master properties to maven-compiler-plugin for whole project to be compiled only by java-1.8  * refactored met properties + gathered them in parent module

---
+++ examples/src/test/java/org/apache/ignite/java/examples/CacheExamplesSelfTest.java
@@ -0,0 +1,155 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.java.examples;
+
+import org.apache.ignite.examples.java.datagrid.CacheAffinityExample;
+import org.apache.ignite.examples.java.datagrid.CacheEntryProcessorExample;
+import org.apache.ignite.examples.java.datagrid.CacheApiExample;
+import org.apache.ignite.testframework.junits.common.GridAbstractExamplesTest;
+
+//import org.apache.ignite.examples.java.datagrid.starschema.*;
+//import org.apache.ignite.examples.java.datagrid.store.dummy.*;
+//import org.apache.ignite.examples.java.datastructures.*;
+
+/**
+ * Cache examples self test.
+ */
+public class CacheExamplesSelfTest extends GridAbstractExamplesTest {
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCacheAffinityExample() throws Exception {
+        CacheAffinityExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCacheEntryProcessorExample() throws Exception {
+        CacheEntryProcessorExample.main(EMPTY_ARGS);
+    }
+
+//    TODO: IGNITE-711 next example(s) should be implemented for java 8
+//    or testing method(s) should be removed if example(s) does not applicable for java 8.
+//    /**
+//     * @throws Exception If failed.
+//     */
+//    public void testCacheAtomicLongExample() throws Exception {
+//        IgniteAtomicLongExample.main(EMPTY_ARGS);
+//    }
+//
+//    /**
+//     * @throws Exception If failed.
+//     */
+//    public void testCacheAtomicReferenceExample() throws Exception {
+//        IgniteAtomicReferenceExample.main(EMPTY_ARGS);
+//    }
+//
+//    /**
+//     * @throws Exception If failed.
+//     */
+//    public void testCacheAtomicSequenceExample() throws Exception {
+//        IgniteAtomicSequenceExample.main(EMPTY_ARGS);
+//    }
+//
+//    /**
+//     * @throws Exception If failed.
+//     */
+//    public void testCacheAtomicStampedExample() throws Exception {
+//        IgniteAtomicStampedExample.main(EMPTY_ARGS);
+//    }
+//
+//    /**
+//     * @throws Exception If failed.
+//     */
+//    public void testCacheCountDownLatchExample() throws Exception {
+//        IgniteCountDownLatchExample.main(EMPTY_ARGS);
+//    }
+//
+//    /**
+//     * @throws Exception If failed.
+//     */
+//    public void testCacheQueueExample() throws Exception {
+//        IgniteQueueExample.main(EMPTY_ARGS);
+//    }
+//
+//    /**
+//     * @throws Exception If failed.
+//     */
+//    public void testCacheSetExample() throws Exception {
+//        IgniteSetExample.main(EMPTY_ARGS);
+//    }
+//
+//    /**
+//     * @throws Exception If failed.
+//     */
+//    public void testCacheDummyStoreExample() throws Exception {
+//        CacheDummyStoreExample.main(EMPTY_ARGS);
+//    }
+//
+//    /**
+//     * @throws Exception If failed.
+//     */
+//    public void testCacheQueryExample() throws Exception {
+//        CacheQueryExample.main(EMPTY_ARGS);
+//    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCacheApiExample() throws Exception {
+        CacheApiExample.main(EMPTY_ARGS);
+    }
+
+//    TODO: IGNITE-711 next example(s) should be implemented for java 8
+//    or testing method(s) should be removed if example(s) does not applicable for java 8.
+//    /**
+//     * @throws Exception If failed.
+//     */
+//    public void testCacheTransactionExample() throws Exception {
+//        CacheTransactionExample.main(EMPTY_ARGS);
+//    }
+//
+//    /**
+//     * @throws Exception If failed.
+//     */
+//    public void testCacheDataStreamerExample() throws Exception {
+//        CacheDataStreamerExample.main(EMPTY_ARGS);
+//    }
+//
+//    /**
+//     * @throws Exception If failed.
+//     */
+//    public void testCachePutGetExample() throws Exception {
+//        CachePutGetExample.main(EMPTY_ARGS);
+//    }
+//
+//    /**
+//     * @throws Exception If failed.
+//     */
+//    public void testSnowflakeSchemaExample() throws Exception {
+//        CacheStarSchemaExample.main(EMPTY_ARGS);
+//    }
+//
+//    /**
+//     * @throws Exception If failed.
+//     */
+//    public void testCacheContinuousQueryExample() throws Exception {
+//        CacheContinuousQueryExample.main(EMPTY_ARGS);
+//    }
+}
