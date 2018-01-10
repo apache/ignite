2585ddd052b8cf27c11e5346f0d91abae19529ab
Pavel Tupitsyn <ptupitsyn@apache.org>
2018-01-10 18:08:40 +0300

    wip

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/ApiParity/ClusterParityTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/ApiParity/ClusterParityTest.cs
@@ -38,10 +38,7 @@
         /** Members that are missing on .NET side and should be added in future. */
         private static readonly string[] MissingMembers =
         {
-            "enableStatistics",  // IGNITE-7276
-            
-            // IGNITE-7301
-            "setBaselineTopology"
+            "enableStatistics"  // IGNITE-7276
         };
 
         /// <summary>
