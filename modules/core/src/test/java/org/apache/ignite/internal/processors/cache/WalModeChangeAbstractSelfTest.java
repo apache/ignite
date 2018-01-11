86e5114193ea3f9ef8e05b235761897638eba839
devozerov <vozerov@gridgain.com>
2018-01-11 11:52:17 +0300

    Extracted base methods.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/WalModeChangeAbstractSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/WalModeChangeAbstractSelfTest.java
@@ -20,27 +20,10 @@ package org.apache.ignite.internal.processors.cache;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCheckedException;
 import org.apache.ignite.IgniteException;
-import org.apache.ignite.Ignition;
 import org.apache.ignite.cache.CacheAtomicityMode;
 import org.apache.ignite.cache.CacheMode;
-import org.apache.ignite.cluster.ClusterNode;
-import org.apache.ignite.configuration.CacheConfiguration;
-import org.apache.ignite.configuration.DataRegionConfiguration;
-import org.apache.ignite.configuration.DataStorageConfiguration;
-import org.apache.ignite.configuration.IgniteConfiguration;
-import org.apache.ignite.internal.util.lang.GridAbsPredicate;
 import org.apache.ignite.internal.util.lang.IgniteInClosureX;
-import org.apache.ignite.internal.util.typedef.F;
-import org.apache.ignite.internal.util.typedef.internal.U;
-import org.apache.ignite.lang.IgnitePredicate;
-import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
-import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
-import org.apache.ignite.testframework.GridTestUtils;
-import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 
-import java.sql.SQLException;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.concurrent.Callable;
 
 import static org.apache.ignite.cache.CacheAtomicityMode.ATOMIC;
@@ -48,13 +31,12 @@ import static org.apache.ignite.cache.CacheAtomicityMode.TRANSACTIONAL;
 import static org.apache.ignite.cache.CacheMode.LOCAL;
 import static org.apache.ignite.cache.CacheMode.PARTITIONED;
 import static org.apache.ignite.cache.CacheMode.REPLICATED;
-import static org.apache.ignite.internal.processors.cache.persistence.file.FilePageStoreManager.DFLT_STORE_DIR;
 
 /**
  * Test dynamic WAL mode change.
  */
 
-public abstract class WalModeChangeAbstractSelfTest extends GridCommonAbstractTest {
+public abstract class WalModeChangeAbstractSelfTest extends WalModeChangeCommonAbstractSelfTest {
 
     // TODO: Test concurrent cache destroy (one thread send messages, another thread creates/destroys cache)
 
@@ -68,41 +50,8 @@ public abstract class WalModeChangeAbstractSelfTest extends GridCommonAbstractTe
 
     // TODO: Test with concurrent cache operations.
 
-    /** Shared IP finder. */
-    private static final TcpDiscoveryVmIpFinder IP_FINDER = new TcpDiscoveryVmIpFinder(true);
-
-    /** Node filter. */
-    private static final IgnitePredicate<ClusterNode> FILTER = new CacheNodeFilter();
-
-    /** Cache name. */
-    private static final String CACHE_NAME = "cache";
-
-    /** Cache name 2. */
-    private static final String CACHE_NAME_2 = "cache_2";
-
-    /** Volatile data region. */
-    private static final String REGION_VOLATILE = "volatile";
-
-    /** Server 1. */
-    private static final String SRV_1 = "srv_1";
-
-    /** Server 2. */
-    private static final String SRV_2 = "srv_2";
-
-    /** Server 3. */
-    private static final String SRV_3 = "srv_3";
-
-    /** Client. */
-    private static final String CLI = "cli";
-
-    /** Node attribute for filtering. */
-    private static final String FILTER_ATTR = "FILTER";
-
     /** Whether coordinator node should be filtered out. */
     private final boolean filterOnCrd;
-
-    /** Whether this is JDBC test. */
-    private final boolean jdbc;
 
     /**
      * Constructor.
@@ -111,8 +60,9 @@ public abstract class WalModeChangeAbstractSelfTest extends GridCommonAbstractTe
      * @param jdbc Whether this is JDBC test.
      */
     protected WalModeChangeAbstractSelfTest(boolean filterOnCrd, boolean jdbc) {
+        super(jdbc);
+
         this.filterOnCrd = filterOnCrd;
-        this.jdbc = jdbc;
     }
 
     /** {@inheritDoc} */
@@ -334,235 +284,5 @@ public abstract class WalModeChangeAbstractSelfTest extends GridCommonAbstractTe
                 }
             }
         });
-    }
-
-    /**
-     * Create cache.
-     *
-     * @param node Node.
-     * @param ccfg Cache configuration.
-     */
-    @SuppressWarnings("unchecked")
-    protected void createCache(Ignite node, CacheConfiguration ccfg) {
-        node.createCache(ccfg);
-    }
-
-    /**
-     * Destroy cache.
-     *
-     * @param node Node.
-     * @param cacheName Cache name.
-     */
-    protected void destroyCache(Ignite node, String cacheName) {
-        node.destroyCache(cacheName);
-    }
-
-    /**
-     * Enable WAL.
-     *
-     * @param node Node.
-     * @param cacheName Cache name.
-     * @return Result.
-     */
-    protected boolean walEnable(Ignite node, String cacheName) {
-        return node.cluster().walEnable(cacheName);
-    }
-
-    /**
-     * Disable WAL.
-     *
-     * @param node Node.
-     * @param cacheName Cache name.
-     * @return Result.
-     */
-    protected boolean walDisable(Ignite node, String cacheName) {
-        return node.cluster().walDisable(cacheName);
-    }
-
-    /**
-     * Enable WAL.
-     *
-     * @param node Node.
-     * @param cacheName Cache name.
-     * @param expRes Expected result.
-     */
-    private void assertWalEnable(Ignite node, String cacheName, boolean expRes) {
-        boolean res = walEnable(node, cacheName);
-
-        if (!jdbc)
-            assertEquals(expRes, res);
-    }
-
-    /**
-     * Disable WAL.
-     *
-     * @param node Node.
-     * @param cacheName Cache name.
-     * @param expRes Expected result.
-     */
-    private void assertWalDisable(Ignite node, String cacheName, boolean expRes) {
-        boolean res = walDisable(node, cacheName);
-
-        if (!jdbc)
-            assertEquals(expRes, res);
-    }
-
-    /**
-     * Assert WAL state on all nodes.
-     *
-     * @param cacheName Cache name.
-     * @param expState Expected state.
-     * @throws IgniteCheckedException If fialed.
-     */
-    private void assertForAllNodes(String cacheName, boolean expState) throws IgniteCheckedException {
-        info("");
-
-        for (final Ignite node : Ignition.allGrids()) {
-            info(">>> Checking WAL state on node: " + node.name());
-
-            assert GridTestUtils.waitForCondition(new GridAbsPredicate() {
-                @Override public boolean apply() {
-                    return node.cluster().isWalEnabled(cacheName) == expState;
-                }
-            }, 1000L);
-        }
-    }
-
-    /**
-     * Ensure exception is thrown.
-     *
-     * @param cmd Command.
-     * @param errCls Expected error class.
-     * @param errMsg Expected error message.
-     */
-    private void assertThrows(Callable<Void> cmd, Class errCls, String errMsg) {
-        try {
-            cmd.call();
-
-            fail("Exception is not thrown");
-        }
-        catch (Exception e) {
-            if (jdbc) {
-                e = (Exception) e.getCause();
-
-                assert e instanceof SQLException : e.getClass().getName();
-            }
-            else
-                assert F.eq(errCls, e.getClass());
-
-            if (errMsg != null) {
-                assert e.getMessage() != null;
-                assert e.getMessage().startsWith(errMsg) : e.getMessage();
-            }
-        }
-    }
-
-    /**
-     * Execute certain logic for all nodes.
-     *
-     * @param task Task.
-     * @throws Exception If failed.
-     */
-    private void forAllNodes(IgniteInClosureX<Ignite> task) throws Exception {
-        for (Ignite node : Ignition.allGrids()) {
-            try {
-                info("");
-                info(">>> Executing test on node: " + node.name());
-
-                task.applyx(node);
-            }
-            finally {
-                for (Ignite node0 : Ignition.allGrids()) {
-                    Collection<String> cacheNames = node0.cacheNames();
-
-                    for (String cacheName : cacheNames)
-                        destroyCache(node0, cacheName);
-                }
-            }
-        }
-    }
-
-    /**
-     * Create node configuration.
-     *
-     * @param name Name.
-     * @param cli Client flag.
-     * @param filter Whether node should be filtered out.
-     * @return Node configuration.
-     */
-    private IgniteConfiguration config(String name, boolean cli, boolean filter) {
-        IgniteConfiguration cfg = new IgniteConfiguration();
-
-        cfg.setIgniteInstanceName(name);
-        cfg.setClientMode(cli);
-        cfg.setLocalHost("127.0.0.1");
-
-        cfg.setDiscoverySpi(new TcpDiscoverySpi().setIpFinder(IP_FINDER));
-
-        DataRegionConfiguration regionCfg = new DataRegionConfiguration().setPersistenceEnabled(true);
-
-        DataRegionConfiguration volatileRegionCfg = new DataRegionConfiguration().setName(REGION_VOLATILE)
-            .setPersistenceEnabled(false);
-
-        DataStorageConfiguration storageCfg = new DataStorageConfiguration();
-
-        storageCfg.setDefaultDataRegionConfiguration(regionCfg);
-        storageCfg.setDataRegionConfigurations(volatileRegionCfg);
-
-        cfg.setDataStorageConfiguration(storageCfg);
-
-        if (filter)
-            cfg.setUserAttributes(Collections.singletonMap(FILTER_ATTR, true));
-
-        return cfg;
-    }
-
-    /**
-     * Create common cache configuration (default name, transactional).
-     *
-     * @param mode Mode.
-     * @return Cache configuration.
-     */
-    private CacheConfiguration cacheConfig(CacheMode mode) {
-        return cacheConfig(CACHE_NAME, mode, TRANSACTIONAL);
-    }
-
-    /**
-     * Create cache configuration.
-     *
-     * @param name Name.
-     * @param mode Mode.
-     * @param atomicityMode Atomicity mode.
-     * @return Cache configuration.
-     */
-    private CacheConfiguration cacheConfig(String name, CacheMode mode, CacheAtomicityMode atomicityMode) {
-        CacheConfiguration ccfg = new CacheConfiguration();
-
-        ccfg.setName(name);
-        ccfg.setCacheMode(mode);
-        ccfg.setAtomicityMode(atomicityMode);
-
-        ccfg.setNodeFilter(FILTER);
-
-        return ccfg;
-    }
-
-    /**
-     * @throws IgniteCheckedException If failed.
-     */
-    private void deleteWorkFiles() throws IgniteCheckedException {
-        deleteRecursively(U.resolveWorkDirectory(U.defaultWorkDirectory(), DFLT_STORE_DIR, false));
-    }
-
-    /**
-     * Cache node filter.
-     */
-    private static class CacheNodeFilter implements IgnitePredicate<ClusterNode> {
-        /** {@inheritDoc} */
-        @Override public boolean apply(ClusterNode node) {
-            Object filterAttr = node.attribute(FILTER_ATTR);
-
-            return filterAttr == null;
-        }
     }
 }
