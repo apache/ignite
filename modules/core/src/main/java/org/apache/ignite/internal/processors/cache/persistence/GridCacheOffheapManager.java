a52acd9589fc98929c859bf9b337f55279a71343
dpavlov <dpavlov@gridgain.com>
2018-01-11 16:43:40 +0300

    IGNITE-7380: Test detecting non reasonable update of PagePartitionCounters in case all counters were not modified

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/GridCacheOffheapManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/GridCacheOffheapManager.java
@@ -66,7 +66,6 @@ import org.apache.ignite.internal.processors.cache.tree.CacheDataRowStore;
 import org.apache.ignite.internal.processors.cache.tree.CacheDataTree;
 import org.apache.ignite.internal.processors.cache.tree.PendingEntriesTree;
 import org.apache.ignite.internal.processors.cache.version.GridCacheVersion;
-import org.apache.ignite.internal.util.GridUnsafe;
 import org.apache.ignite.internal.util.lang.GridCursor;
 import org.apache.ignite.internal.util.tostring.GridToStringInclude;
 import org.apache.ignite.internal.util.typedef.T2;
@@ -370,7 +369,7 @@ public class GridCacheOffheapManager extends IgniteCacheOffheapManagerImpl imple
                 try {
                     PagePartitionCountersIO cntrsIO = PageIO.getPageIO(curAddr);
 
-                    if (cntrsIO.readCacheSizes(curAddr, cacheSizes))
+                    if (PagePartitionCountersIO.readCacheSizes(curAddr, cacheSizes))
                         break;
 
                     nextId = cntrsIO.getNextCountersPageId(curAddr);
@@ -401,9 +400,9 @@ public class GridCacheOffheapManager extends IgniteCacheOffheapManagerImpl imple
      */
     private static long writeSharedGroupCacheSizes(PageMemory pageMem, int grpId,
         long cntrsPageId, int partId, Map<Integer, Long> sizes) throws IgniteCheckedException {
-        byte[] data = serializeCacheSizes(sizes);
+        byte[] data = PagePartitionCountersIO.serializeCacheSizes(sizes);
 
-        int items = data.length / 12;
+        int items = data.length / PagePartitionCountersIO.ITEM_SIZE;
         boolean init = cntrsPageId == 0;
 
         if (init && !sizes.isEmpty())
@@ -504,23 +503,6 @@ public class GridCacheOffheapManager extends IgniteCacheOffheapManagerImpl imple
         finally {
             pageMem.releasePage(grpId, metaPageId, metaPage);
         }
-    }
-
-    /**
-     * @param cacheSizes Cache sizes: cache Id in shared group mapped to its size. Not null.
-     * @return Serialized cache sizes or 0-byte length array if map was empty.
-     */
-    private static byte[] serializeCacheSizes(Map<Integer, Long> cacheSizes) {
-        // Item size = 4 bytes (cache ID) + 8 bytes (cache size) = 12 bytes
-        byte[] data = new byte[cacheSizes.size() * 12];
-        long off = GridUnsafe.BYTE_ARR_OFF;
-
-        for (Map.Entry<Integer, Long> entry : cacheSizes.entrySet()) {
-            GridUnsafe.putInt(data, off, entry.getKey()); off += 4;
-            GridUnsafe.putLong(data, off, entry.getValue()); off += 8;
-        }
-
-        return data;
     }
 
     /**
