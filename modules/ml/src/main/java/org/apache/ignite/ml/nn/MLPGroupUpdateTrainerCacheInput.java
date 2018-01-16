6eccf23009c1e0a03638255555e8c93a77667126
artemmalykh <amalykh@gridgain.com>
2018-01-16 13:29:04 +0300

    IGNITE-7350: Distributed MLP cleanup/refactoring
    this closes #3368

--- modules/ml/src/main/java/org/apache/ignite/ml/nn/MLPGroupUpdateTrainerCacheInput.java
+++ modules/ml/src/main/java/org/apache/ignite/ml/nn/MLPGroupUpdateTrainerCacheInput.java
@@ -19,6 +19,7 @@ package org.apache.ignite.ml.nn;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.apache.ignite.Ignite;
@@ -57,6 +58,32 @@ public class MLPGroupUpdateTrainerCacheInput extends AbstractMLPGroupUpdateTrain
     private final MultilayerPerceptron mlp;
 
     /**
+     * Random number generator.
+     */
+    private final Random rand;
+
+    /**
+     * Construct instance of this class with given parameters.
+     *
+     * @param arch Architecture of multilayer perceptron.
+     * @param init Initializer of multilayer perceptron.
+     * @param networksCnt Count of networks to be trained in parallel by {@link MLPGroupUpdateTrainer}.
+     * @param cache Cache with labeled vectors.
+     * @param batchSize Size of batch to return on each training iteration.
+     * @param rand RNG.
+     */
+    public MLPGroupUpdateTrainerCacheInput(MLPArchitecture arch, MLPInitializer init,
+        int networksCnt, IgniteCache<Integer, LabeledVector<Vector, Vector>> cache,
+        int batchSize, Random rand) {
+        super(networksCnt);
+
+        this.batchSize = batchSize;
+        this.cache = cache;
+        this.mlp = new MultilayerPerceptron(arch, init);
+        this.rand = rand;
+    }
+
+    /**
      * Construct instance of this class with given parameters.
      *
      * @param arch Architecture of multilayer perceptron.
@@ -68,11 +95,7 @@ public class MLPGroupUpdateTrainerCacheInput extends AbstractMLPGroupUpdateTrain
     public MLPGroupUpdateTrainerCacheInput(MLPArchitecture arch, MLPInitializer init,
         int networksCnt, IgniteCache<Integer, LabeledVector<Vector, Vector>> cache,
         int batchSize) {
-        super(networksCnt);
-
-        this.batchSize = batchSize;
-        this.cache = cache;
-        this.mlp = new MultilayerPerceptron(arch, init);
+        this(arch, init, networksCnt, cache, batchSize, new Random());
     }
 
     /**
@@ -93,6 +116,7 @@ public class MLPGroupUpdateTrainerCacheInput extends AbstractMLPGroupUpdateTrain
     @Override public IgniteSupplier<IgniteBiTuple<Matrix, Matrix>> batchSupplier() {
         String cName = cache.getName();
         int bs = batchSize;
+        Random r = rand; // IMPL NOTE this is intended to make below lambda more lightweight.
 
         return () -> {
             Ignite ignite = Ignition.localIgnite();
@@ -105,7 +129,7 @@ public class MLPGroupUpdateTrainerCacheInput extends AbstractMLPGroupUpdateTrain
 
             int locKeysCnt = keys.size();
 
-            int[] selected = Utils.selectKDistinct(locKeysCnt, Math.min(bs, locKeysCnt));
+            int[] selected = Utils.selectKDistinct(locKeysCnt, Math.min(bs, locKeysCnt), r);
 
             // Get dimensions of vectors in cache. We suppose that every feature vector has
             // same dimension d 1 and every label has the same dimension d2.
