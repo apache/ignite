0d7f393cf9ce1c8796b42ec6b21aec08b0fefbb1
Stanislav Lukyanov <stanlukyanov@gmail.com>
2018-01-19 14:44:31 +0300

    IGNITE-7284: Introduce DEV_ONLY marker to IgniteLogger. Fixes #3346.
    Signed-off-by: Valentin Kulichenko <valentin.lulichenko@gmail.com>
    
    (cherry picked from commit edc5a5f)

--- modules/core/src/main/java/org/apache/ignite/internal/binary/BinaryClassDescriptor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/binary/BinaryClassDescriptor.java
@@ -191,7 +191,7 @@ public class BinaryClassDescriptor {
 
         if (useOptMarshaller && userType && !U.isIgnite(cls) && !U.isJdk(cls) &&
             !GridQueryProcessor.isGeometryClass(cls)) {
-            U.warn(ctx.log(), "Class \"" + cls.getName() + "\" cannot be serialized using " +
+            U.warnDevOnly(ctx.log(), "Class \"" + cls.getName() + "\" cannot be serialized using " +
                 BinaryMarshaller.class.getSimpleName() + " because it either implements Externalizable interface " +
                 "or have writeObject/readObject methods. " + OptimizedMarshaller.class.getSimpleName() + " will be " +
                 "used instead and class instances will be deserialized on the server. Please ensure that all nodes " +
