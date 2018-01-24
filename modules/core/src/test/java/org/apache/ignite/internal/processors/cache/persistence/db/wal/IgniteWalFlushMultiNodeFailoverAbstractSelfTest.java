26060d62bd511f63ba782cf6e5abddff8ffdfa2c
ilantukh <ilantukh@gridgain.com>
2018-01-24 16:26:38 +0300

    gg-13223 : Unmuted test.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/db/wal/IgniteWalFlushMultiNodeFailoverAbstractSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/db/wal/IgniteWalFlushMultiNodeFailoverAbstractSelfTest.java
@@ -74,8 +74,6 @@ public abstract class IgniteWalFlushMultiNodeFailoverAbstractSelfTest extends Gr
 
     /** {@inheritDoc} */
     @Override protected void beforeTest() throws Exception {
-        fail("https://issues.apache.org/jira/browse/IGNITE-7365");
-
         stopAllGrids();
 
         deleteWorkFiles();
