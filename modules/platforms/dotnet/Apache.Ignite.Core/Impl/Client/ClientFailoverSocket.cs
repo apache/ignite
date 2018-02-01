405fe398dbdd7179edde56fe7bcf4c859f9f97a9
Pavel Tupitsyn <ptupitsyn@apache.org>
2018-02-01 15:18:23 +0300

    Added ClientFailoverSocket

---
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Client/ClientFailoverSocket.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Core.Impl.Client
+{
+    using System;
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+    using Apache.Ignite.Core.Client;
+    using Apache.Ignite.Core.Impl.Binary.IO;
+
+    /// <summary>
+    /// Socket wrapper with failover functionality: reconnects on failure.
+    /// </summary>
+    internal class ClientFailoverSocket : IClientSocket
+    {
+        /** Underlying socket. */
+        private volatile ClientSocket _socket;
+
+        /** Config. */
+        private readonly IgniteClientConfiguration _config;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClientFailoverSocket"/> class.
+        /// </summary>
+        /// <param name="config">The configuration.</param>
+        public ClientFailoverSocket(IgniteClientConfiguration config)
+        {
+            Debug.Assert(config != null);
+            
+            _config = config;
+            _socket = new ClientSocket(config);
+
+            // TODO: Subscribe to failure, reconnect.
+        }
+
+        /** <inheritdoc /> */
+        public T DoOutInOp<T>(ClientOp opId, Action<IBinaryStream> writeAction, Func<IBinaryStream, T> readFunc,
+            Func<ClientStatusCode, string, T> errorFunc = null)
+        {
+            return _socket.DoOutInOp(opId, writeAction, readFunc, errorFunc);
+        }
+
+        /** <inheritdoc /> */
+        public Task<T> DoOutInOpAsync<T>(ClientOp opId, Action<IBinaryStream> writeAction, Func<IBinaryStream, T> readFunc, Func<ClientStatusCode, string, T> errorFunc = null)
+        {
+            return _socket.DoOutInOpAsync(opId, writeAction, readFunc, errorFunc);
+        }
+
+        /** <inheritdoc /> */
+        public void Dispose()
+        {
+            // TODO: set some flag, or set socket to null?
+            _socket.Dispose();
+        }
+    }
+}
