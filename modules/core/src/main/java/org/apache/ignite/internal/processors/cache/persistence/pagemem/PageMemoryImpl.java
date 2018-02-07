0dee11bef395c41a65f781d093004a462b0b9714
dpavlov <dpavlov@gridgain.com>
2018-02-07 21:14:49 +0300

    manual merge fix

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/pagemem/PageMemoryImpl.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/pagemem/PageMemoryImpl.java
@@ -461,9 +461,6 @@ public class PageMemoryImpl implements PageMemoryEx {
         if (isThrottlingEnabled())
             writeThrottle.onMarkDirty(false);
 
-        if (isThrottlingEnabled())
-            writeThrottle.onMarkDirty(false);
-
         long pageId = storeMgr.allocatePage(cacheId, partId, flags);
 
         assert PageIdUtils.pageIndex(pageId) > 0; //it's crucial for tracking pages (zero page is super one)
