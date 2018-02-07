9d86c8cfbf317272d0df70518f1dee5c15751474
gg-shq <kshirokov@gridgain.com>
2018-02-07 18:58:48 +0300

    IGNITE-7535: Removed BATCH_SIZE tests (will be added in IGNITE-7605)

--- modules/core/src/test/java/org/apache/ignite/internal/sql/SqlParserBulkLoadSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/sql/SqlParserBulkLoadSelfTest.java
@@ -115,32 +115,5 @@ public class SqlParserBulkLoadSelfTest extends SqlParserAbstractSelfTest {
         assertParseError(null,
             "copy from \"any.file\" into Person (_key, age, firstName, lastName) format csv charset \"8^)\"",
             "Unknown charset name: '8^)'");
-
-        // BATCH_SIZE
-
-        assertParseError(null,
-            "copy from \"any.file\" into Person (_key, age, firstName, lastName) format csv" +
-                " batch_size " + Integer.MIN_VALUE,
-            "Batch size should be within [1..");
-
-        assertParseError(null,
-            "copy from \"any.file\" into Person (_key, age, firstName, lastName) format csv" +
-                " batch_size 0",
-            "Batch size should be within [1..");
-
-        assertParseError(null,
-            "copy from \"any.file\" into Person (_key, age, firstName, lastName) format csv" +
-                " batch_size " + Integer.MAX_VALUE,
-            "Batch size should be within [1..2147483135]");
-
-        assertParseError(null,
-            "copy from \"any.file\" into Person (_key, age, firstName, lastName) format csv" +
-                " batch_size",
-            "Unexpected end of command (expected: \"[integer]\")");
-
-        assertParseError(null,
-            "copy from \"any.file\" into Person (_key, age, firstName, lastName) format csv" +
-                " batch_size postmark",
-            "Unexpected token: \"POSTMARK\" (expected: \"[integer]\")");
     }
 }
