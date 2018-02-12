dd22e281352c6bdbec90b2b3370e9f0822873c71
dpavlov <dpavlov@gridgain.com>
2018-02-12 20:27:28 +0300

    IGNITE-7202: long running tests fix

---
+++ modules/direct-io/src/test/java/org/apache/ignite/testsuites/IgnitePdsEvictionTest2.java
@@ -0,0 +1,7 @@
+package org.apache.ignite.testsuites;
+
+import org.apache.ignite.internal.processors.cache.persistence.db.file.IgnitePdsEvictionTest;
+
+//todo
+public class IgnitePdsEvictionTest2 extends IgnitePdsEvictionTest {
+}
