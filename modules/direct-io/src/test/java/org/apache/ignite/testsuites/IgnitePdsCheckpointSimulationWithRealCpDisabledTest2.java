f850e0fd051ddaf368439d8f0f99148c545ea33b
dpavlov <dpavlov@gridgain.com>
2018-02-12 20:44:48 +0300

    IGNITE-7202: long running tests fix

---
+++ modules/direct-io/src/test/java/org/apache/ignite/testsuites/IgnitePdsCheckpointSimulationWithRealCpDisabledTest2.java
@@ -0,0 +1,11 @@
+package org.apache.ignite.testsuites;
+
+import org.apache.ignite.internal.processors.cache.persistence.db.file.IgnitePdsCheckpointSimulationWithRealCpDisabledTest;
+
+//todo remove
+public class IgnitePdsCheckpointSimulationWithRealCpDisabledTest2 extends IgnitePdsCheckpointSimulationWithRealCpDisabledTest {
+
+    @Override public void testDirtyFlag() throws Exception {
+        super.testDirtyFlag();
+    }
+}
--- modules/direct-io/src/test/java/org/apache/ignite/testsuites/IgnitePdsEvictionTest2.java
+++ modules/direct-io/src/test/java/org/apache/ignite/testsuites/IgnitePdsEvictionTest2.java
@@ -2,6 +2,6 @@ package org.apache.ignite.testsuites;
 
 import org.apache.ignite.internal.processors.cache.persistence.db.file.IgnitePdsEvictionTest;
 
-//todo
+//todo remove
 public class IgnitePdsEvictionTest2 extends IgnitePdsEvictionTest {
 }
