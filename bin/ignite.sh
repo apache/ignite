683eb38e71bf7d11ad6ac34bc8d39e95f406156e
Alexandr Kuramshin <ein.nsk.ru@gmail.com>
2018-02-12 11:39:55 +0300

    IGNITE-6521 Update JVM options (default and recommendations) for better performance - Fixes #2858.
    Signed-off-by: Alexey Goncharuk <alexey.goncharuk@gmail.com>
    
    (cherry picked from commit 53e24e1)

--- bin/ignite.sh
+++ bin/ignite.sh
@@ -100,8 +100,7 @@ fi
 #
 # Uncomment the following GC settings if you see spikes in your throughput due to Garbage Collection.
 #
-# JVM_OPTS="$JVM_OPTS -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+UseTLAB -XX:NewSize=128m -XX:MaxNewSize=128m"
-# JVM_OPTS="$JVM_OPTS -XX:MaxTenuringThreshold=0 -XX:SurvivorRatio=1024 -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=60"
+# JVM_OPTS="$JVM_OPTS -XX:+UseG1GC"
 
 #
 # Uncomment if you get StackOverflowError.
