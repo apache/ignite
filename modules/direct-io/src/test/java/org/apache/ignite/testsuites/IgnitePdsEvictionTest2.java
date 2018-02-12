e3d3a8246dfdb34003e01eacce2fa00547b61401
dpavlov <dpavlov@gridgain.com>
2018-02-12 21:19:26 +0300

    IGNITE-7202: long running tests locating problem with direct IO

--- modules/direct-io/src/test/java/org/apache/ignite/testsuites/IgnitePdsEvictionTest2.java
+++ modules/direct-io/src/test/java/org/apache/ignite/testsuites/IgnitePdsEvictionTest2.java
@@ -4,4 +4,12 @@ import org.apache.ignite.internal.processors.cache.persistence.db.file.IgnitePds
 
 //todo remove
 public class IgnitePdsEvictionTest2 extends IgnitePdsEvictionTest {
+
+    @Override protected long getTestTimeout() {
+        return 15 * 60 * 1000;
+    }
+
+    @Override public void testPageEviction() throws Exception {
+        super.testPageEviction();
+    }
 }
