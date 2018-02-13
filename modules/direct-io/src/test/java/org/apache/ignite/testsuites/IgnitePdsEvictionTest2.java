8312c518127b7f32a667eaaca37fd6155bd1363d
dpavlov <dpavlov@gridgain.com>
2018-02-13 19:36:18 +0300

    IGNITE-7686: PDS Direct IO failure: IgnitePdsEvictionTest.testPageEviction

--- modules/direct-io/src/test/java/org/apache/ignite/testsuites/IgnitePdsEvictionTest2.java
+++ modules/direct-io/src/test/java/org/apache/ignite/testsuites/IgnitePdsEvictionTest2.java
@@ -5,12 +5,14 @@ import org.apache.ignite.internal.processors.cache.persistence.db.file.IgnitePds
 //todo remove
 public class IgnitePdsEvictionTest2 extends IgnitePdsEvictionTest {
 
+    /** {@inheritDoc} */
     @Override protected long getTestTimeout() {
         return 15 * 60 * 1000;
     }
 
+    /** {@inheritDoc} */
     protected int getPagesNum() {
-        return 1000;
+        return 1024;
     }
 
     @Override public void testPageEviction() throws Exception {
