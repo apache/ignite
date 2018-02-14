28c69f4a89967952b93dee568c76bc3f01c6b857
Nikolay Izhikov <nizhikov@apache.org>
2018-02-14 07:40:20 +0300

    IGNITE-7652: Code review fixes.

--- examples/src/main/java/org/apache/ignite/examples/datagrid/CacheContinuousQueryWithTransformerExample.java
+++ examples/src/main/java/org/apache/ignite/examples/datagrid/CacheContinuousQueryWithTransformerExample.java
@@ -17,8 +17,7 @@
 
 package org.apache.ignite.examples.datagrid;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.Map;
 import javax.cache.Cache;
@@ -32,8 +31,8 @@ import org.apache.ignite.cache.query.ContinuousQueryWithTransformer;
 import org.apache.ignite.cache.query.QueryCursor;
 import org.apache.ignite.examples.ExampleNodeStartup;
 import org.apache.ignite.examples.model.Address;
-import org.apache.ignite.examples.model.Employee;
-import org.apache.ignite.examples.model.EmployeeKey;
+import org.apache.ignite.examples.model.Organization;
+import org.apache.ignite.examples.model.OrganizationType;
 import org.apache.ignite.lang.IgniteClosure;
 
 /**
@@ -72,17 +71,17 @@ public class CacheContinuousQueryWithTransformerExample {
             System.out.println(">>> Cache continuous query with transformer example started.");
 
             // Auto-close cache at the end of the example.
-            try (IgniteCache<EmployeeKey, Employee> cache = ignite.getOrCreateCache(CACHE_NAME)) {
+            try (IgniteCache<Integer, Organization> cache = ignite.getOrCreateCache(CACHE_NAME)) {
                 // Create new continuous query with transformer.
-                ContinuousQueryWithTransformer<EmployeeKey, Employee, String> qry =
+                ContinuousQueryWithTransformer<Integer, Organization, String> qry =
                     new ContinuousQueryWithTransformer<>();
 
                 //Factory to create transformers.
-                Factory<IgniteClosure<CacheEntryEvent<? extends EmployeeKey, ? extends Employee>, String>> factory =
+                Factory<IgniteClosure<CacheEntryEvent<? extends Integer, ? extends Organization>, String>> factory =
                     FactoryBuilder.factoryOf(
                         //Return one field of complex object.
                         //Only this field will be sent over to a local node over the network.
-                        (IgniteClosure<CacheEntryEvent<? extends EmployeeKey, ? extends Employee>, String>)
+                        (IgniteClosure<CacheEntryEvent<? extends Integer, ? extends Organization>, String>)
                             event -> event.getValue().name());
 
                 qry.setRemoteTransformerFactory(factory);
@@ -90,11 +89,11 @@ public class CacheContinuousQueryWithTransformerExample {
                 //Listener that will receive transformed data.
                 qry.setLocalListener(names -> {
                     for (String name : names)
-                        System.out.println("New employee name: " + name);
+                        System.out.println("New organization name: " + name);
                 });
 
                 // Execute query.
-                try (QueryCursor<Cache.Entry<EmployeeKey, Employee>> cur = cache.query(qry)) {
+                try (QueryCursor<Cache.Entry<Integer, Organization>> cur = cache.query(qry)) {
                     populateCache(cache);
 
                     // Wait for a while while callback is notified about remaining puts.
@@ -111,60 +110,43 @@ public class CacheContinuousQueryWithTransformerExample {
     /**
      * Populates cache with data.
      *
-     * @param cache Employee cache.
+     * @param cache Organization cache.
      */
     @SuppressWarnings("TypeMayBeWeakened")
-    private static void populateCache(IgniteCache<EmployeeKey, Employee> cache) {
-        Map<EmployeeKey, Employee> data = new HashMap<>();
+    private static void populateCache(IgniteCache<Integer, Organization> cache) {
+        Map<Integer, Organization> data = new HashMap<>();
 
-        data.put(new EmployeeKey(1, 1), new Employee(
-            "James Wilson",
-            12500,
-            new Address("1096 Eddy Street, San Francisco, CA", 94109),
-            Arrays.asList("Human Resources", "Customer Service")
-        ));
+        data.put(1, new Organization(
+            "Microsoft", // Name.
+            new Address("1096 Eddy Street, San Francisco, CA", 94109), // Address.
+            OrganizationType.PRIVATE, // Type.
+            new Timestamp(System.currentTimeMillis()))); // Last update time.
 
-        data.put(new EmployeeKey(2, 1), new Employee(
-            "Daniel Adams",
-            11000,
-            new Address("184 Fidler Drive, San Antonio, TX", 78130),
-            Arrays.asList("Development", "QA")
-        ));
+        data.put(2, new Organization(
+            "Red Cross", // Name.
+            new Address("184 Fidler Drive, San Antonio, TX", 78205), // Address.
+            OrganizationType.NON_PROFIT, // Type.
+            new Timestamp(System.currentTimeMillis()))); // Last update time.
 
-        data.put(new EmployeeKey(3, 1), new Employee(
-            "Cristian Moss",
-            12500,
-            new Address("667 Jerry Dove Drive, Florence, SC", 29501),
-            Collections.singletonList("Logistics")
-        ));
+        data.put(3, new Organization(
+            "Apple", // Name.
+            new Address("1 Infinite Loop, Cupertino, CA", 95014), // Address.
+            OrganizationType.PRIVATE, // Type.
+            new Timestamp(System.currentTimeMillis()))); // Last update time.
 
-        data.put(new EmployeeKey(4, 2), new Employee(
-            "Allison Mathis",
-            25300,
-            new Address("2702 Freedom Lane, San Francisco, CA", 94109),
-            Collections.singletonList("Development")
-        ));
+        data.put(4, new Organization(
+            "IBM", // Name.
+            new Address("1 New Orchard Road Armonk, New York", 10504), // Address.
+            OrganizationType.PRIVATE, // Type.
+            new Timestamp(System.currentTimeMillis()))); // Last update time.
 
-        data.put(new EmployeeKey(5, 2), new Employee(
-            "Breana Robbin",
-            6500,
-            new Address("3960 Sundown Lane, Austin, TX", 78130),
-            Collections.singletonList("Sales")
-        ));
+        data.put(5, new Organization(
+            "NASA Armstrong Flight Research Center", // Name.
+            new Address("4800 Lilly Ave, Edwards, CA", 793523), // Address.
+            OrganizationType.NON_PROFIT, // Type.
+            new Timestamp(System.currentTimeMillis()))); // Last update time.
 
-        data.put(new EmployeeKey(6, 2), new Employee(
-            "Philip Horsley",
-            19800,
-            new Address("2803 Elsie Drive, Sioux Falls, SD", 57104),
-            Collections.singletonList("Sales")
-        ));
 
-        data.put(new EmployeeKey(7, 2), new Employee(
-            "Brian Peters",
-            10600,
-            new Address("1407 Pearlman Avenue, Boston, MA", 12110),
-            Arrays.asList("Development", "QA")
-        ));
 
         cache.putAll(data);
     }
