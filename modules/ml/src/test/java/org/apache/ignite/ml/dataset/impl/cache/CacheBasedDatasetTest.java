fe681cbcba214d741835670af2146a4cbdf7b944
Pavel Kovalenko <jokserfn@gmail.com>
2018-02-14 11:34:14 +0300

    IGNITE-6113 Compilation fixed.

--- modules/ml/src/test/java/org/apache/ignite/ml/dataset/impl/cache/CacheBasedDatasetTest.java
+++ modules/ml/src/test/java/org/apache/ignite/ml/dataset/impl/cache/CacheBasedDatasetTest.java
@@ -291,7 +291,6 @@ public class CacheBasedDatasetTest extends GridCommonAbstractTest {
                             .append(String.format(" %7s  |", partition.state()))
                             .append(String.format(" %7s  |", partition.reload()))
                             .append(String.format(" %13s  |", partition.reservations()))
-                            .append(String.format(" %18s  |", partition.shouldBeRenting()))
                             .append(String.format(" %8s  |", partition.primary(instanceState.topVer)))
                             .append(String.format(" %16d  |", partition.dataStore().fullSize()))
                             .append("\n");
