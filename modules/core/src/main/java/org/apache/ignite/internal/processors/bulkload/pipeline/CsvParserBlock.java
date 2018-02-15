68aaadba2aa0be2b368201a427108e48fc23bd84
gg-shq <kshirokov@gridgain.com>
2018-02-15 16:03:17 +0300

    IGNITE-7681: Performance test removed (and attached to the IGNITE-7681 as a patch file). Fixed license, javadoc and naming in CSV parser.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/bulkload/pipeline/CsvParserBlock.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/bulkload/pipeline/CsvParserBlock.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.internal.processors.bulkload.pipeline;
 
 import java.util.ArrayList;
@@ -6,8 +23,11 @@ import org.apache.ignite.IgniteCheckedException;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Parses CSV file by processing both lines and fields. Unifinished fields and lines
+ * Speed-optimized CSV file parser, which processes both lines and fields. Unifinished fields and lines
  * are kept between invocations of {@link #accept(char[], boolean)}.
+ * <p>
+ * Please note that speed of parsing was of higher priority than using "proper" OOP design patterns.
+ * Regular expressions aren't used here for the same reason.
  */
 public class CsvParserBlock extends PipelineBlock<char[], List<Object>> {
     /** Leftover characters from the previous invocation of {@link #accept(char[], boolean)}. */
@@ -33,7 +53,7 @@ public class CsvParserBlock extends PipelineBlock<char[], List<Object>> {
         for (int i = 0; i < leftover.length(); i++) {
             switch (leftover.charAt(i)) {
                 case ',':
-                    fields.add(substrWithoutQuotes(leftover, lastPos, i));
+                    fields.add(unquotedSubstring(leftover, lastPos, i));
 
                     lastPos = i + 1;
 
@@ -41,7 +61,7 @@ public class CsvParserBlock extends PipelineBlock<char[], List<Object>> {
 
                 case '\r':
                 case '\n':
-                    fields.add(substrWithoutQuotes(leftover, lastPos, i));
+                    fields.add(unquotedSubstring(leftover, lastPos, i));
 
                     nextBlock.accept(new ArrayList<>(fields), false);
 
@@ -64,7 +84,7 @@ public class CsvParserBlock extends PipelineBlock<char[], List<Object>> {
             leftover.delete(0, lastPos);
 
         if (isLastPortion && leftover.length() > 0) {
-            fields.add(substrWithoutQuotes(leftover, 0, leftover.length()));
+            fields.add(unquotedSubstring(leftover, 0, leftover.length()));
 
             leftover.setLength(0);
 
@@ -75,14 +95,14 @@ public class CsvParserBlock extends PipelineBlock<char[], List<Object>> {
     }
 
     /**
-     * Takes substring from the {@code str}, omitting quotes if they are present.
+     * Extracts substring from the {@code str}, omitting quotes if they are found exactly at substring boundaries.
      *
      * @param str The string to take substring from.
      * @param from The beginning index.
      * @param to The end index (last character position + 1).
      * @return The substring without quotes.
      */
-    @NotNull private String substrWithoutQuotes(@NotNull StringBuilder str, int from, int to) {
+    @NotNull private String unquotedSubstring(@NotNull StringBuilder str, int from, int to) {
         if ((to - from) >= 2 && str.charAt(from) == '"' && str.charAt(to - 1) == '"')
             return str.substring(from + 1, to - 1);
 
