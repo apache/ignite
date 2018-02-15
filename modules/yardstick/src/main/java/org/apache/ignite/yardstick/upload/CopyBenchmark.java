e1a91f9547c82ac622a2392e4b352de82d062d67
Pavel Kuznetsov <palmihal@gmail.com>
2018-02-15 17:31:58 +0300

    IGNITE-7253: Don't place csv file in tmpfs; Added comments

--- modules/yardstick/src/main/java/org/apache/ignite/yardstick/upload/CopyBenchmark.java
+++ modules/yardstick/src/main/java/org/apache/ignite/yardstick/upload/CopyBenchmark.java
@@ -18,35 +18,64 @@
 package org.apache.ignite.yardstick.upload;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.PrintStream;
 import java.sql.PreparedStatement;
 import java.util.Map;
+import org.yardstickframework.BenchmarkUtils;
 
+/**
+ * Single shot benchmark for the COPY FROM... sql query.
+ * Measures total time of load number of rows from csv file using thin driver
+ */
 public class CopyBenchmark extends AbstractUploadBenchmark {
-
+    /** csv file for warmup */
     private String warmupCsv;
+
+    /** csv file for benchmared action */
     private String realCsv;
 
+    /** {@inheritDoc} */
     @Override protected void init() {
         super.init();
+
         warmupCsv = generateWarmupCsv();
         realCsv = generateRealCsv();
     }
 
+    /** Generate csv file for copy operation being benchmarked. */
     private String generateRealCsv() {
         String prefix = "data-" + INSERT_SIZE;
+
         return generate(prefix, INSERT_SIZE);
     }
 
+    /** Generate csv file for copy operation being performed during warmup. */
     private String generateWarmupCsv() {
         return generate("warmup", WARMUP_ROWS_CNT);
     }
 
+    /**
+     * Generate csv file with specified prefix and containing specified number
+     * of records (lines).
+     * <br/>
+     * Each record contain incremental (starting 1) id and set of random values.
+     * <br/>
+     * File is created in current directory and is deleted on JVM exits.
+     *
+     * @param filePrefix prefix for csv to be generated.
+     * @param records how many csv lines generate.
+     *
+     * @return absolute path for generated csv file.
+     */
     private String generate(String filePrefix, long records){
         try {
-            File f = File.createTempFile(filePrefix, ".csv");
-            // TODO: delete on exit?
+            File workDir = new File(System.getProperty("user.dir"));
+
+            File f = File.createTempFile(filePrefix, ".csv", workDir);
+
+            f.deleteOnExit();
+
+            BenchmarkUtils.println("Generating file: " + f.getAbsolutePath());
 
             try (PrintStream out = new PrintStream(f)) {
                 for (long id = 1; id <= records; id++) {
@@ -55,13 +84,16 @@ public class CopyBenchmark extends AbstractUploadBenchmark {
                 }
             }
 
+            BenchmarkUtils.println("File generated (will be removed on exit)");
+
             return f.getAbsolutePath();
         }
-        catch (IOException ex){
+        catch (Exception ex){
             throw new RuntimeException("Couldn't generate CSV file, terminating", ex);
         }
     }
 
+    /** {@inheritDoc} */
     @Override protected void warmup() throws Exception {
         try (PreparedStatement fromCsv = conn.get().prepareStatement(queries.copyFrom(warmupCsv))) {
             fromCsv.executeUpdate();
@@ -72,6 +104,7 @@ public class CopyBenchmark extends AbstractUploadBenchmark {
         }
     }
 
+    /** {@inheritDoc} */
     @Override public boolean test(Map<Object, Object> ctx) throws Exception {
         try (PreparedStatement fromCsv = conn.get().prepareStatement(queries.copyFrom(realCsv))) {
             fromCsv.executeUpdate();
