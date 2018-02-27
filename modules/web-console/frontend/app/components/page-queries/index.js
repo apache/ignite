9ad3a058b8415a0f6ff24bb3b46d13afe1a09b8e
alexdel <verbalab@yandex.ru>
2018-02-27 16:30:33 +0700

    WC-157. Web Console. Cleaning slots.

--- modules/web-console/frontend/app/components/page-queries/index.js
+++ modules/web-console/frontend/app/components/page-queries/index.js
@@ -49,6 +49,7 @@ export default angular.module('ignite-console.sql', [
             $postLink() {
                 this.$timeout(() => {
                     this.$transclude((clone) => {
+                        this.pageQueries[this.slotName].empty();
                         clone.appendTo(this.pageQueries[this.slotName]);
                     });
                 });
