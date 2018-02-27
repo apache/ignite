b3544e263b9c22917fdf039441102668e5627cad
Aleksey Plekhanov <plehanov.alex@gmail.com>
2018-02-27 18:35:43 +0300

    IGNITE-6868 Concurrency performance fix

--- modules/core/src/main/java/org/apache/ignite/spi/communication/tcp/TcpCommunicationMetricsListener.java
+++ modules/core/src/main/java/org/apache/ignite/spi/communication/tcp/TcpCommunicationMetricsListener.java
@@ -17,13 +17,15 @@
 
 package org.apache.ignite.spi.communication.tcp;
 
+import java.util.Collection;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.LongAdder;
 import org.apache.ignite.internal.managers.communication.GridIoMessage;
+import org.apache.ignite.internal.util.GridConcurrentHashSet;
 import org.apache.ignite.internal.util.nio.GridNioMetricsListener;
 import org.apache.ignite.internal.util.typedef.F;
 import org.apache.ignite.internal.util.typedef.internal.U;
@@ -52,17 +54,88 @@ public class TcpCommunicationMetricsListener implements GridNioMetricsListener{
         }
     };
 
-    /** Received messages count grouped by message type. */
-    private final ConcurrentMap<String, LongAdder> rcvdMsgsCntByType = new ConcurrentHashMap<>();
+    /** Counter factory. */
+    private static final Callable<MessageCounter> MSGS_CNT_FACTORY = new Callable<MessageCounter>() {
+        @Override public MessageCounter call() {
+            return new MessageCounter();
+        }
+    };
 
-    /** Received messages count grouped by sender. */
-    private final ConcurrentMap<UUID, LongAdder> rcvdMsgsCntByNode = new ConcurrentHashMap<>();
+    /**
+     * Received/sent message counter.
+     */
+    private static class MessageCounter {
+        /** Received messages count. */
+        volatile long rcvdMsgs;
 
-    /** Sent messages count grouped by message type. */
-    private final ConcurrentMap<String, LongAdder> sentMsgsCntByType = new ConcurrentHashMap<>();
+        /** Sent messages count. */
+        volatile long sentMsgs;
 
-    /** Sent messages count grouped by receiver. */
-    private final ConcurrentMap<UUID, LongAdder> sentMsgsCntByNode = new ConcurrentHashMap<>();
+        /**
+         * Increment received messages count.
+         */
+        public void incrementRcvdCount() {
+            rcvdMsgs++;
+        }
+
+        /**
+         * Increment sent messages count.
+         */
+        public void incrementSentCount() {
+            sentMsgs++;
+        }
+
+        /**
+         * @return Received messages count.
+         */
+        public long rcvdMsgs() {
+            return rcvdMsgs;
+        }
+
+        /**
+         * @return Sent messages count.
+         */
+        public long sentMsgs() {
+            return sentMsgs;
+        }
+    }
+
+    /**
+     * Thread local counters map.
+     * @param <T> grouping key class.
+     */
+    private class ThreadLocalCountersMap<T> extends ThreadLocal<Map<T, MessageCounter>> {
+        /** Counter maps for all threads. */
+        private final Collection<Map<T, MessageCounter>> cntrAggregator;
+
+        /**
+         * @param aggregator Counter maps for all threads.
+         */
+        private ThreadLocalCountersMap(Collection<Map<T, MessageCounter>> aggregator) {
+            cntrAggregator = aggregator;
+        }
+
+        /** {@inheritDoc} */
+        @Override protected Map<T, MessageCounter> initialValue() {
+            Map<T, MessageCounter> cntrMap = new ConcurrentHashMap<>();
+
+            cntrAggregator.add(cntrMap);
+
+            return cntrMap;
+        }
+    }
+
+    /** Message counters by node for all threads. */
+    Set<Map<UUID, MessageCounter>> msgsCntrByNodeAll = new GridConcurrentHashSet<>();
+
+    /** Message counter by node for local thread. */
+    ThreadLocal<Map<UUID, MessageCounter>> msgsCntrByNode = new ThreadLocalCountersMap<>(msgsCntrByNodeAll);
+
+    /** Message counters by type for all threads. */
+    Set<Map<String, MessageCounter>> msgsCntrByTypeAll = new GridConcurrentHashSet<>();
+
+    /** Message counter by type for local thread. */
+    ThreadLocal<Map<String, MessageCounter>> msgsCntrByType = new ThreadLocalCountersMap<>(msgsCntrByTypeAll);
 
     /** {@inheritDoc} */
     @Override public void onBytesSent(int bytesCnt) {
@@ -89,11 +162,14 @@ public class TcpCommunicationMetricsListener implements GridNioMetricsListener{
         if (msg instanceof GridIoMessage)
             msg = ((GridIoMessage)msg).message();
 
-        LongAdder cntByType = F.addIfAbsent(sentMsgsCntByType, msg.getClass().getSimpleName(), LONG_ADDER_FACTORY);
-        LongAdder cntByNode = F.addIfAbsent(sentMsgsCntByNode, nodeId, LONG_ADDER_FACTORY);
+        Map<String, MessageCounter> cntrMapByType = msgsCntrByType.get();
+        Map<UUID, MessageCounter> cntrMapByNode = msgsCntrByNode.get();
 
-        cntByType.increment();
-        cntByNode.increment();
+        MessageCounter cntrByType = F.addIfAbsent(cntrMapByType, msg.getClass().getSimpleName(), MSGS_CNT_FACTORY);
+        MessageCounter cntrByNode = F.addIfAbsent(cntrMapByNode, nodeId, MSGS_CNT_FACTORY);
+
+        cntrByType.incrementSentCount();
+        cntrByNode.incrementSentCount();
     }
 
     /**
@@ -111,11 +187,14 @@ public class TcpCommunicationMetricsListener implements GridNioMetricsListener{
         if (msg instanceof GridIoMessage)
             msg = ((GridIoMessage)msg).message();
 
-        LongAdder cntByType = F.addIfAbsent(rcvdMsgsCntByType, msg.getClass().getSimpleName(), LONG_ADDER_FACTORY);
-        LongAdder cntByNode = F.addIfAbsent(rcvdMsgsCntByNode, nodeId, LONG_ADDER_FACTORY);
+        Map<String, MessageCounter> cntrMapByType = msgsCntrByType.get();
+        Map<UUID, MessageCounter> cntrMapByNode = msgsCntrByNode.get();
 
-        cntByType.increment();
-        cntByNode.increment();
+        MessageCounter cntrByType = F.addIfAbsent(cntrMapByType, msg.getClass().getSimpleName(), MSGS_CNT_FACTORY);
+        MessageCounter cntrByNode = F.addIfAbsent(cntrMapByNode, nodeId, MSGS_CNT_FACTORY);
+
+        cntrByType.incrementRcvdCount();
+        cntrByNode.incrementRcvdCount();
     }
 
     /**
