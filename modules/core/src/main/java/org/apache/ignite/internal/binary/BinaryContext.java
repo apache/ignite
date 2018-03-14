d00e8501b414d73a96dc7c967f33ed54fec463c4
Nikolay Izhikov <nizhikov@apache.org>
2018-03-14 22:33:47 +0300

    IGNITE-7756: IgniteUuid added to predefined types - Fixes #3550.

--- modules/core/src/main/java/org/apache/ignite/internal/binary/BinaryContext.java
+++ modules/core/src/main/java/org/apache/ignite/internal/binary/BinaryContext.java
@@ -333,6 +333,7 @@ public class BinaryContext {
         registerPredefinedType(GridMapEntry.class, 60);
         registerPredefinedType(IgniteBiTuple.class, 61);
         registerPredefinedType(T2.class, 62);
+        registerPredefinedType(IgniteUuid.class, 63);
 
         registerPredefinedType(PlatformJavaObjectFactoryProxy.class,
             GridBinaryMarshaller.PLATFORM_JAVA_OBJECT_FACTORY_PROXY);
