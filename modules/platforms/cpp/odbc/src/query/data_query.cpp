85161c080ce5412bd42cd3a49332e4107cba9573
Igor Sapego <isapego@gridgain.com>
2018-03-26 12:01:28 +0300

    IGNITE-4193: Added protocol-level changes

--- modules/platforms/cpp/odbc/src/query/data_query.cpp
+++ modules/platforms/cpp/odbc/src/query/data_query.cpp
@@ -210,7 +210,7 @@ namespace ignite
             {
                 const std::string& schema = connection.GetSchema();
 
-                QueryExecuteRequest req(schema, sql, params, timeout);
+                QueryExecuteRequest req(schema, sql, params, timeout, connection.IsAutoCommit());
                 QueryExecuteResponse rsp;
 
                 try
