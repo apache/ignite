73565faee2a3fba5303a6d6dc1eab262fdfa4bbb
Andrey V. Mashenkov <andrey.mashenkov@gmail.com>
2018-04-04 19:13:23 +0300

    Tests added.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/db/IgnitePdsWithTtlTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/db/IgnitePdsWithTtlTest.java
@@ -20,92 +20,102 @@ package org.apache.ignite.internal.processors.cache.persistence.db;
 import com.google.common.base.Strings;
 import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
-import javax.cache.expiry.AccessedExpiryPolicy;
+import javax.cache.expiry.CreatedExpiryPolicy;
 import javax.cache.expiry.Duration;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.Ignition;
+import org.apache.ignite.cache.CacheRebalanceMode;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.DataRegionConfiguration;
 import org.apache.ignite.configuration.DataStorageConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.configuration.WALMode;
 import org.apache.ignite.internal.IgniteEx;
+import org.apache.ignite.internal.IgniteInterruptedCheckedException;
 import org.apache.ignite.internal.processors.cache.IgniteCacheProxy;
+import org.apache.ignite.internal.util.typedef.PA;
+import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 import org.apache.ignite.testframework.GridTestUtils;
-import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 
 /**
  * Test TTL worker with persistence enabled
  */
-public class IgnitePdsWithTtlTest extends GridCommonAbstractTest {
-/** */
+public class IgnitePdsWithTtlTest extends PdsAbstractTest {
+    /** */
     public static final String CACHE = "expirableCache";
 
     /** */
-    public static final int DURATION_SEC = 10;
+    private static final int EXPIRATION_TIMEOUT = 10;
 
     /** */
     public static final int ENTRIES = 7000;
 
-    /**
-     * copy of GridCacheSharedTtlCleanupManager.CLEANUP_WORKER_SLEEP_INTERVAL
-     */
-    private static final long CLEANUP_WORKER_SLEEP_INTERVAL = 500;
+    /** */
+    private static final TcpDiscoveryVmIpFinder FINDER = new TcpDiscoveryVmIpFinder(true);
 
     /** {@inheritDoc} */
     @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
         final IgniteConfiguration cfg = super.getConfiguration(igniteInstanceName);
+
+        TcpDiscoverySpi disco = new TcpDiscoverySpi();
+        disco.setIpFinder(FINDER);
+
+        cfg.setDiscoverySpi(disco);
+
         final CacheConfiguration ccfg = new CacheConfiguration();
         ccfg.setName(CACHE);
-        ccfg.setExpiryPolicyFactory(AccessedExpiryPolicy.factoryOf(new Duration(TimeUnit.SECONDS, DURATION_SEC)));
+        ccfg.setExpiryPolicyFactory(CreatedExpiryPolicy.factoryOf(new Duration(TimeUnit.SECONDS, EXPIRATION_TIMEOUT)));
         ccfg.setEagerTtl(true);
         ccfg.setGroupName("group1");
 
-        final DataStorageConfiguration storCfg = new DataStorageConfiguration();
-        final DataRegionConfiguration regCfg = new DataRegionConfiguration();
-        regCfg.setPersistenceEnabled(true);
-        storCfg.setDefaultDataRegionConfiguration(regCfg);
-        cfg.setDataStorageConfiguration(storCfg);
+        ccfg.setRebalanceMode(CacheRebalanceMode.SYNC);
+        cfg.setDataStorageConfiguration(
+            new DataStorageConfiguration()
+                .setDefaultDataRegionConfiguration(
+                    new DataRegionConfiguration()
+                        .setMaxSize(200L * 1024 * 1024)
+                        .setPersistenceEnabled(true)
+                ).setWalMode(WALMode.DEFAULT));
 
         cfg.setCacheConfiguration(ccfg);
         return cfg;
-    }
-
-    /** {@inheritDoc} */
-    @Override protected void beforeTest() throws Exception {
-        super.beforeTest();
-        GridTestUtils.deleteDbFiles();
-    }
-
-    /** {@inheritDoc} */
-    @Override protected void afterTest() throws Exception {
-        super.afterTest();
-        stopAllGrids();
     }
 
     /**
      * @throws Exception if failed.
      */
     public void testTtlIsApplied() throws Exception {
-        final IgniteEx ignite = startGrid(0);
-
-        ignite.active(true);
-
-        for (int i = 0; i < 1; i++)
-            loadAndWaitForCleanup(ignite, i*ENTRIES, false);
-
-        stopAllGrids();
+        loadAndWaitForCleanup(false);
     }
 
     /**
      * @throws Exception if failed.
      */
     public void testTtlIsAppliedAfterRestart() throws Exception {
-        final IgniteEx ignite = startGrid(0);
+        loadAndWaitForCleanup(true);
+    }
 
-        ignite.active(true);
+    /**
+     * @throws Exception if failed.
+     */
+    private void loadAndWaitForCleanup(boolean restartGrid) throws Exception {
+        IgniteEx srv = startGrid(0);
+        srv.active(true);
 
-        for (int i = 0; i < 1; i++)
-            loadAndWaitForCleanup(ignite, i*ENTRIES, true);
+        fillCache(srv.cache(CACHE));
+
+        if (restartGrid) {
+            Ignition.stop(srv.name(), false);
+            srv = startGrid(0);
+            srv.active(true);
+        }
+
+        final IgniteCache<Integer, String> cache = srv.cache(CACHE);
+
+        pringStatistics((IgniteCacheProxy)cache, "After restart from LFS");
+
+        waitAndCheckExpired(cache);
 
         stopAllGrids();
     }
@@ -113,96 +123,56 @@ public class IgnitePdsWithTtlTest extends GridCommonAbstractTest {
     /**
      * @throws Exception if failed.
      */
-    private void loadAndWaitForCleanup(IgniteEx ignite, final int start, boolean restartGrid) throws Exception {
-        {
-            final IgniteCache<Integer, String> cache = ignite.cache(CACHE);
-
-            putEntriesBatch(cache, start);
-
-            assertEquals(ENTRIES, cache.size());
-
-            System.out.println("After cache puts {{");
-            ((IgniteCacheProxy)cache).context().printMemoryStats();
-            System.out.println("}} ");
-        }
-        final long ms = System.currentTimeMillis();
-        {
-            if(restartGrid) {
-                Ignition.stop(ignite.name(), false);
-                ignite = startGrid(0);
-                ignite.active(true);
-            }
-
-            final IgniteCache<Integer, String> cache = ignite.cache(CACHE);
-            System.out.println("After restart from LFS {{");
-            ((IgniteCacheProxy)cache).context().printMemoryStats();
-            System.out.println("}} After restart from LFS");
-
-            if (restartGrid)
-                for (int i = 0; i < 50; i++)
-                    cache.get(i); // touch entries
-
-            final long timeout = DURATION_SEC * 1000 + CLEANUP_WORKER_SLEEP_INTERVAL;
-            Thread.sleep(timeout);
-
-            System.out.println("After sleep {{");
-            ((IgniteCacheProxy)cache).context().printMemoryStats();
-            System.out.println("}} After sleep");
-
-            assertEquals(0, cache.size());
-        }
-    }
-
-    //it is possible test will be passing if SQL indexes are enabled
-    /**
-     * @throws Exception if failed.
-     */
     public void testRebalancingWithTtlExpirable() throws Exception {
-        final IgniteEx srv1 = startGrid(0);
+        IgniteEx srv = startGrid(0);
+        srv.active(true);
 
-        srv1.active(true);
-
-        {
-            final IgniteCache<Integer, String> cache = srv1.cache(CACHE);
-
-            putEntriesBatch(cache, 0);
-
-            assertEquals(ENTRIES, cache.size());
-
-            System.out.println("After cache puts {{");
-            ((IgniteCacheProxy)cache).context().printMemoryStats();
-            System.out.println("}} ");
-        }
-
+        fillCache(srv.cache(CACHE));
 
         //causes rebalancing start
-        final IgniteEx srv2 = startGrid(1);
-        {
-            final IgniteCache<Integer, String> cacheOn2 = srv2.cache(CACHE);
-            System.out.println("After rebalancing start  {{");
-            ((IgniteCacheProxy)cacheOn2).context().printMemoryStats();
-            System.out.println("}} After rebalancing start");
+        srv = startGrid(1);
 
+        final IgniteCache<Integer, String> cache = srv.cache(CACHE);
 
-            final long timeout = DURATION_SEC * 1000 + CLEANUP_WORKER_SLEEP_INTERVAL;
-            Thread.sleep(timeout);
+        pringStatistics((IgniteCacheProxy)cache, "After rebalancing start");
 
-
-            System.out.println("After timeout {{");
-            ((IgniteCacheProxy)cacheOn2).context().printMemoryStats();
-            System.out.println("}} After timeout");
-
-            assertEquals(0, cacheOn2.size());
-        }
+        waitAndCheckExpired(cache);
 
         stopAllGrids();
     }
 
     /** */
-    private void putEntriesBatch(IgniteCache<Integer, String> cache, final int startKey) {
+    protected void fillCache(IgniteCache<Integer, String> cache) {
         cache.putAll(new TreeMap<Integer, String>() {{
-            for (int i = startKey; i < startKey + ENTRIES; i++)
+            for (int i = 0; i < ENTRIES; i++)
                 put(i, Strings.repeat("Some value " + i, 125));
         }});
+
+        //Touch entries.
+        for (int i = 0; i < ENTRIES; i++)
+            cache.get(i); // touch entries
+
+        pringStatistics((IgniteCacheProxy)cache, "After cache puts");
+    }
+
+    /** */
+    protected void waitAndCheckExpired(IgniteCache<Integer, String> cache) throws IgniteInterruptedCheckedException {
+        GridTestUtils.waitForCondition(new PA() {
+            @Override public boolean apply() {
+                return cache.size() == 0;
+            }
+        }, TimeUnit.SECONDS.toMillis(EXPIRATION_TIMEOUT + 1));
+
+        pringStatistics((IgniteCacheProxy)cache, "After timeout");
+
+        for (int i = 0; i < ENTRIES; i++)
+            assertNull(cache.get(i));
+    }
+
+    /** */
+    private void pringStatistics(IgniteCacheProxy cache, String msg) {
+        System.out.println(msg + " {{");
+        cache.context().printMemoryStats();
+        System.out.println("}} " + msg);
     }
 }
