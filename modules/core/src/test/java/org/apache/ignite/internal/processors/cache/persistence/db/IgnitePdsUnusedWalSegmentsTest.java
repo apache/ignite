0b94a1cdfc119577fe150a3d9094c897f9031537
Ivan Daschinskiy <ivandasch@gmail.com>
2018-04-05 19:34:28 +0300

    IGNITE-7912 Reworking test, part 2.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/db/IgnitePdsUnusedWalSegmentsTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/db/IgnitePdsUnusedWalSegmentsTest.java
@@ -17,11 +17,6 @@
 
 package org.apache.ignite.internal.processors.cache.persistence.db;
 
-import java.io.File;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.cache.affinity.rendezvous.RendezvousAffinityFunction;
 import org.apache.ignite.configuration.CacheConfiguration;
@@ -33,19 +28,13 @@ import org.apache.ignite.internal.IgniteEx;
 import org.apache.ignite.internal.pagemem.wal.IgniteWriteAheadLogManager;
 import org.apache.ignite.internal.processors.cache.persistence.GridCacheDatabaseSharedManager;
 import org.apache.ignite.internal.processors.cache.persistence.wal.FileWALPointer;
-import org.apache.ignite.internal.processors.cache.persistence.wal.FileWriteAheadLogManager;
-import org.apache.ignite.internal.visor.VisorTaskArgument;
-import org.apache.ignite.internal.visor.misc.VisorWalTask;
-import org.apache.ignite.internal.visor.misc.VisorWalTaskArg;
-import org.apache.ignite.internal.visor.misc.VisorWalTaskOperation;
-import org.apache.ignite.internal.visor.misc.VisorWalTaskResult;
 import org.apache.ignite.testframework.GridTestUtils;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 
 import static org.apache.ignite.IgniteSystemProperties.IGNITE_PDS_MAX_CHECKPOINT_MEMORY_HISTORY_SIZE;
 
 /**
- * Test correctness of WAL Visor Task and correctness of delete.
+ * Test correctness of truncating unused WAL segments.
  */
 public class IgnitePdsUnusedWalSegmentsTest extends GridCommonAbstractTest {
     /** {@inheritDoc} */
@@ -88,119 +77,13 @@ public class IgnitePdsUnusedWalSegmentsTest extends GridCommonAbstractTest {
     }
 
     /**
-     * Tests correctness of {@link VisorWalTaskOperation}.
+     * Tests that range reserved method return correct number of reserved WAL segments.
      *
      * @throws Exception if failed.
      */
-    public void testCorrectnessOfDeletionTaskSegments() throws Exception {
-        try {
-            IgniteEx ig0 = (IgniteEx) startGrids(4);
-
-            ig0.cluster().active(true);
-
-            IgniteCache<Object, Object> cache = ig0.cache(DEFAULT_CACHE_NAME);
-
-            for (int k = 0; k < 10_000; k++)
-                cache.put(k, new byte[1024]);
-
-            forceCheckpoint();
-
-            for (int k = 0; k < 1_000; k++)
-                cache.put(k, new byte[1024]);
-
-            forceCheckpoint();
-
-            VisorWalTaskResult printRes = ig0.compute().execute(VisorWalTask.class,
-                    new VisorTaskArgument<>(ig0.cluster().node().id(),
-                            new VisorWalTaskArg(VisorWalTaskOperation.PRINT_UNUSED_WAL_SEGMENTS), false));
-
-            assertEquals("Check that print task finished without exceptions", printRes.results().size(), 4);
-
-            List<File> walArchives = new ArrayList<>();
-
-            printRes.results().entrySet().forEach(e -> {
-                log.warning("Node " + e.getKey());
-                log.warning("Num of segments " + e.getValue().size());
-            });
-
-            for(int i = 0; i < 4; i++){
-                IgniteEx ig = grid(i);
-                log.warning("node:" + ig.cluster().node().consistentId().toString());
-                GridCacheDatabaseSharedManager dbMgr = (GridCacheDatabaseSharedManager) ig.context().cache().context()
-                        .database();
-                log.warning("min reserved index: " + dbMgr.reservedWalSegmentIndex());
-
-                File walArchiveDir = GridTestUtils.getFieldValue(ig.context().cache().context().wal(),"walArchiveDir");
-
-                File[] wals = walArchiveDir.listFiles(FileWriteAheadLogManager.WAL_SEGMENT_FILE_FILTER);
-
-
-
-                if (wals != null) {
-                    for (File wal: wals)
-                        log.warning(wal.getAbsolutePath());
-
-                }
-
-            }
-
-            for (Collection<String> pathsPerNode : printRes.results().values()) {
-                assertTrue("Check that all nodes has unused WAL segments", pathsPerNode.size() > 0);
-
-                for (String path : pathsPerNode)
-                    walArchives.add(Paths.get(path).toFile());
-            }
-
-            VisorWalTaskResult delRes = ig0.compute().execute(VisorWalTask.class,
-                    new VisorTaskArgument<>(ig0.cluster().node().id(),
-                            new VisorWalTaskArg(VisorWalTaskOperation.DELETE_UNUSED_WAL_SEGMENTS), false));
-
-            assertEquals("Check that delete task finished with no exceptions", delRes.results().size(), 4);
-
-            List<File> walDeletedArchives = new ArrayList<>();
-
-            for (Collection<String> pathsPerNode : delRes.results().values()) {
-                assertTrue("Check that unused WAL segments deleted from all nodes",
-                        pathsPerNode.size() > 0);
-
-                for (String path : pathsPerNode)
-                    walDeletedArchives.add(Paths.get(path).toFile());
-            }
-
-            for (File f : walDeletedArchives)
-                assertTrue("Checking existing of deleted WAL archived segments: " + f.getAbsolutePath(), !f.exists());
-
-            for (File f : walArchives)
-                assertTrue("Checking existing of WAL archived segments from print task after delete: " + f.getAbsolutePath(),
-                        !f.exists());
-        }
-        finally {
-            stopAllGrids();
-        }
-    }
-
-    /**
-     * Tests that reserved WAL segment wasn't affected by {@link VisorWalTask}.
-     *
-     * @throws Exception if failed.
-     */
-    public void testReservedWalSegmentNotAffected() throws Exception {
-        try {
-            IgniteEx ig0 = (IgniteEx) startGrids(4);
-
-            ig0.cluster().active(true);
-
-            IgniteCache<Object, Object> cache = ig0.cache(DEFAULT_CACHE_NAME);
-
-            for (int k = 0; k < 10_000; k++)
-                cache.put(k, new byte[1024]);
-
-            forceCheckpoint();
-
-            for (int k = 0; k < 1_000; k++)
-                cache.put(k, new byte[1024]);
-
-            forceCheckpoint();
+    public void testWalManagerRangeReservation() throws Exception {
+        try{
+            IgniteEx ig0 = prepareGrid(4);
 
             GridCacheDatabaseSharedManager dbMgr = (GridCacheDatabaseSharedManager) ig0.context().cache().context()
                     .database();
@@ -209,31 +92,83 @@ public class IgnitePdsUnusedWalSegmentsTest extends GridCommonAbstractTest {
 
             long resIdx = getReservedWalSegmentIndex(dbMgr);
 
+            assertTrue("Expected that at least resIdx greater than 0, real is " + resIdx, resIdx > 0);
+
+            assertTrue("Expected that dbMbr returns valid resIdx", dbMgr.reservedWalSegmentIndex() == resIdx);
+
             // Reserve previous WAL segment.
             wal.reserve(new FileWALPointer(resIdx - 1, 0, 0));
 
-            VisorWalTaskResult printRes = ig0.compute().execute(VisorWalTask.class,
-                    new VisorTaskArgument<>(ig0.cluster().node().id(),
-                            new VisorWalTaskArg(VisorWalTaskOperation.PRINT_UNUSED_WAL_SEGMENTS), false));
+            int resCnt = wal.reserved(new FileWALPointer(resIdx - 1, 0, 0), new FileWALPointer(resIdx, 0, 0));
 
-            int fileCnt2 = printRes.results().get(ig0.cluster().node().consistentId().toString()).size();
-
-            // Check that manually reserved WAL segment wasn't printed.
-            assertTrue(fileCnt2 == resIdx - 1 );
-
-            VisorWalTaskResult delRes = ig0.compute().execute(VisorWalTask.class,
-                    new VisorTaskArgument<>(ig0.cluster().node().id(),
-                            new VisorWalTaskArg(VisorWalTaskOperation.PRINT_UNUSED_WAL_SEGMENTS), false));
-
-            fileCnt2 = delRes.results().get(ig0.cluster().node().consistentId().toString()).size();
-
-            // Check that manually reserved WAL segment wasn't deleted.
-            assertTrue(fileCnt2 == resIdx - 1 );
+            assertTrue("Expected resCnt is 2, real is " + resCnt, resCnt == 2);
         }
         finally {
             stopAllGrids();
         }
     }
+
+    /**
+     * Tests that grid cache manager correctly truncates unused WAL segments;
+     *
+     * @throws Exception if failed.
+     */
+    public void testUnusedWalTruncate() throws Exception {
+        try{
+            IgniteEx ig0 = prepareGrid(4);
+
+            GridCacheDatabaseSharedManager dbMgr = (GridCacheDatabaseSharedManager) ig0.context().cache().context()
+                    .database();
+
+            IgniteWriteAheadLogManager wal = ig0.context().cache().context().wal();
+
+            long resIdx = getReservedWalSegmentIndex(dbMgr);
+
+            assertTrue("Expected that at least resIdx greater than 0, real is " + resIdx, resIdx > 0);
+
+            assertTrue("Expected that dbMbr returns valid resIdx", dbMgr.reservedWalSegmentIndex() == resIdx);
+
+            // Reserve previous WAL segment.
+            wal.reserve(new FileWALPointer(resIdx - 1, 0, 0));
+
+            int numDel = dbMgr.walSegmentsTruncate();
+
+            int expNumDel = (int)resIdx - 1;
+
+            assertTrue("Expected del segments is " + expNumDel + ", real is " + numDel, expNumDel == numDel);
+        }
+        finally {
+            stopAllGrids();
+        }
+    }
+
+    /**
+     * Starts grid and populates test data.
+     *
+     * @param cnt Grid count.
+     * @return First started grid.
+     * @throws Exception
+     */
+    private IgniteEx prepareGrid(int cnt) throws Exception {
+        IgniteEx ig0 = (IgniteEx)startGrids(cnt);
+
+        ig0.cluster().active(true);
+
+        IgniteCache<Object, Object> cache = ig0.cache(DEFAULT_CACHE_NAME);
+
+        for (int k = 0; k < 10_000; k++)
+            cache.put(k, new byte[1024]);
+
+        forceCheckpoint();
+
+        for (int k = 0; k < 1_000; k++)
+            cache.put(k, new byte[1024]);
+
+        forceCheckpoint();
+
+        return ig0;
+    }
+
 
     /**
      * Get index of reserved WAL segment by checkpointer.
