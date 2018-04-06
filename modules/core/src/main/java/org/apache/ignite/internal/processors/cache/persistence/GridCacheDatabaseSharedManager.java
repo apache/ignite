40b9c8e227783f8d90fff5f2db4688e63be3dd37
Pavel Sapezhko <pavel.sapezhko@synesis.ru>
2018-04-06 17:36:23 +0300

    IGNITE-8167: Fix inconsistent last record pointer in case of recovery from corrupted WAL

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/GridCacheDatabaseSharedManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/GridCacheDatabaseSharedManager.java
@@ -1958,7 +1958,7 @@ public class GridCacheDatabaseSharedManager extends IgniteCacheDatabaseSharedMan
 
         long start = U.currentTimeMillis();
         int applied = 0;
-        WALPointer lastRead = null;
+        WALPointer lastRead = status.endPtr == CheckpointStatus.NULL_PTR ? null : status.endPtr;
 
         Collection<Integer> ignoreGrps = storeOnly ? Collections.emptySet() : initiallyWalDisabledGrps;
 
