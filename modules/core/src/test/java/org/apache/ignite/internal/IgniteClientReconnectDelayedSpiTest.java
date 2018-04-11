74dda105be0a8e57c22d5eaa72e2464b959ee1a4
Maxim Muzafarov <maxmuzaf@gmail.com>
2018-04-11 17:47:47 +0300

    IGNITE-7791: add apache license and javadocs

--- modules/core/src/test/java/org/apache/ignite/internal/IgniteClientReconnectDelayedSpiTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/IgniteClientReconnectDelayedSpiTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.internal;
 
 import java.util.Map;
@@ -24,11 +41,10 @@ import org.apache.ignite.testframework.GridTestUtils;
 
 import static org.apache.ignite.cache.CacheAtomicityMode.TRANSACTIONAL;
 
+/**
+ * Test cases with {@link TestCommunicationDelayedSpi} for capturing messages and resending at moment we need it.
+ */
 public class IgniteClientReconnectDelayedSpiTest extends IgniteClientReconnectAbstractTest {
-
-    /** */
-    private static final int SRV_CNT = 3;
-
     /** */
     private static final String PRECONFIGURED_CACHE = "preconfigured-cache";
 
@@ -50,7 +66,7 @@ public class IgniteClientReconnectDelayedSpiTest extends IgniteClientReconnectAb
 
     /** {@inheritDoc} */
     @Override protected int serverCount() {
-        return SRV_CNT;
+        return 3;
     }
 
     /**
@@ -103,7 +119,7 @@ public class IgniteClientReconnectDelayedSpiTest extends IgniteClientReconnectAb
 
     /**
      * Capturing {@link GridDhtPartitionsSingleMessage} at moment when they occurs for resending them at the moment
-     * when we need it by test cases.
+     * when we need it by our scenario.
      */
     private static class TestCommunicationDelayedSpi extends TcpCommunicationSpi {
         /** {@inheritDoc} */
