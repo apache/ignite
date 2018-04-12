9c4f9f8c3188ba0052304e5a6fc43e4041e4126d
Denis Mekhanikov <dmekhanikov@gmail.com>
2018-04-12 18:54:41 +0300

    ignite-8205 add test with deployment on client node

--- modules/core/src/test/java/org/apache/ignite/internal/processors/service/ServiceDeploymentOnActivationTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/service/ServiceDeploymentOnActivationTest.java
@@ -20,19 +20,30 @@ package org.apache.ignite.internal.processors.service;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import org.apache.ignite.Ignite;
+import org.apache.ignite.cluster.ClusterNode;
 import org.apache.ignite.configuration.DataRegionConfiguration;
 import org.apache.ignite.configuration.DataStorageConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.configuration.WALMode;
+import org.apache.ignite.lang.IgnitePredicate;
+import org.apache.ignite.services.ServiceConfiguration;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 
 /**
  * Check, that services are redeployed, when cluster is deactivated and activated back again.
  */
 public class ServiceDeploymentOnActivationTest extends GridCommonAbstractTest {
+    /** */
+    private static final String SERVICE_NAME = "test-service";
+
+    /** */
+    private boolean client = false;
+
     /** {@inheritDoc} */
     @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
         IgniteConfiguration cfg = super.getConfiguration(igniteInstanceName);
+
+        cfg.setClientMode(client);
 
         cfg.setDataStorageConfiguration(
             new DataStorageConfiguration().setDefaultDataRegionConfiguration(
@@ -46,7 +57,14 @@ public class ServiceDeploymentOnActivationTest extends GridCommonAbstractTest {
 
     /** {@inheritDoc} */
     @Override protected void beforeTest() throws Exception {
+        client = false;
+
         cleanPersistenceDir();
+    }
+
+    /** {@inheritDoc} */
+    @Override protected void afterTest() throws Exception {
+        stopAllGrids();
     }
 
     /**
@@ -54,18 +72,41 @@ public class ServiceDeploymentOnActivationTest extends GridCommonAbstractTest {
      */
     public void testServiceDeploymentOnActivation() throws Exception {
         Ignite ignite = startGrid(1);
+        checkDeploy(ignite, null);
+    }
 
+    /**
+     * @throws Exception if failed.
+     */
+    public void testServiceDeploymentOnClientOnActivation() throws Exception {
+        Ignite ignite = startGrid(1);
+
+        client = true;
+        startGrid(2);
+
+        checkDeploy(ignite, ignite.cluster().forClients().predicate());
+    }
+
+    /**
+     * @param ignite Ignite.
+     * @param nodeFilter Node filter.
+     */
+    private void checkDeploy(Ignite ignite, IgnitePredicate<ClusterNode> nodeFilter) throws InterruptedException {
         ignite.cluster().active(true);
-
-        String svcName = "test-service";
 
         CountDownLatch exeLatch = new CountDownLatch(1);
         CountDownLatch cancelLatch = new CountDownLatch(1);
 
-        DummyService.exeLatch(svcName, exeLatch);
-        DummyService.cancelLatch(svcName, cancelLatch);
+        DummyService.exeLatch(SERVICE_NAME, exeLatch);
+        DummyService.cancelLatch(SERVICE_NAME, cancelLatch);
 
-        ignite.services().deployNodeSingleton(svcName, new DummyService());
+        ServiceConfiguration srvcCfg = new ServiceConfiguration();
+        srvcCfg.setName(SERVICE_NAME);
+        srvcCfg.setMaxPerNodeCount(1);
+        srvcCfg.setNodeFilter(nodeFilter);
+        srvcCfg.setService(new DummyService());
+
+        ignite.services().deploy(srvcCfg);
 
         assertTrue(exeLatch.await(10, TimeUnit.SECONDS));
 
@@ -75,7 +116,7 @@ public class ServiceDeploymentOnActivationTest extends GridCommonAbstractTest {
 
         exeLatch = new CountDownLatch(1);
 
-        DummyService.exeLatch(svcName, exeLatch);
+        DummyService.exeLatch(SERVICE_NAME, exeLatch);
 
         ignite.cluster().active(true);
 
