1e7272cc9cf0003729a02650a3d66cddc86c2fa0
Aleksey Plekhanov <plehanov.alex@gmail.com>
2018-04-18 13:01:36 +0300

    IGNITE-8191 Quiet and info

--- modules/core/src/main/java/org/apache/ignite/internal/IgniteKernal.java
+++ modules/core/src/main/java/org/apache/ignite/internal/IgniteKernal.java
@@ -1988,11 +1988,6 @@ public class IgniteKernal implements IgniteEx, IgniteMXBean, Externalizable {
             U.quiet(false, "");
             U.quiet(false, "Ignite node started OK (id=" + U.id8(locNode.id()) +
                 (F.isEmpty(igniteInstanceName) ? "" : ", instance name=" + igniteInstanceName) + ')');
-
-            if (!cluster().active()) {
-                U.quiet(false, ">>> Ignite cluster is not active (limited functionality available). "
-                    + "Use control.(sh|bat) script or IgniteCluster interface to activate.");
-            }
         }
 
         if (log.isInfoEnabled()) {
@@ -2025,6 +2020,11 @@ public class IgniteKernal implements IgniteEx, IgniteMXBean, Externalizable {
                     ">>> Local ports: " + sb + NL;
 
             log.info(str);
+        }
+
+        if (!cluster().active()) {
+            U.quietAndInfo(log, ">>> Ignite cluster is not active (limited functionality available). "
+                + "Use control.(sh|bat) script or IgniteCluster interface to activate.");
         }
     }
 
