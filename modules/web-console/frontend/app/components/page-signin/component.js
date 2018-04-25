2f8df26367bf81058aefa0bdd6c5b234d849250c
Ilya Borisov <klaster1@gmail.com>
2018-04-25 16:13:19 +0700

    IGNITE-8370 Web Console: Split "Sign In" page to three separate pages.

--- modules/web-console/frontend/app/components/page-signin/component.js
+++ modules/web-console/frontend/app/components/page-signin/component.js
@@ -15,24 +15,12 @@
  * limitations under the License.
  */
 
-import {dropTestDB, resolveUrl, insertTestUser} from 'envtools';
-import {createRegularUser} from '../../roles';
-import {userMenu} from '../../components/userMenu';
-import {pageSignin} from '../../page-models/pageSignin1';
+import template from './template.pug';
+import controller from './controller';
+import './style.scss';
 
-const user = createRegularUser();
-
-fixture('Logout')
-    .before(async() => {
-        await dropTestDB();
-        await insertTestUser();
-    })
-    .after(async() => {
-        await dropTestDB();
-    });
-
-test('Successful logout', async(t) => {
-    await t.useRole(user).navigateTo(resolveUrl('/settings/profile'));
-    await userMenu.clickOption('Log out');
-    await t.expect(pageSignin.selector.exists).ok('Goes to sign in page after logout');
-});
+/** @type {ng.IComponentOptions} */
+export default {
+    controller,
+    template
+};
