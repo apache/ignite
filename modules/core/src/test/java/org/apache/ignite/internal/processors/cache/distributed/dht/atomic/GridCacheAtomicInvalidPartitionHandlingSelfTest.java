505dea3fabf4fe42addb794ca7526dfe86127994
denis.garus <d.garus@isimplelab.com>
2018-05-03 10:36:03 +0300

    IGNITE-5979 temp

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/dht/atomic/GridCacheAtomicInvalidPartitionHandlingSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/dht/atomic/GridCacheAtomicInvalidPartitionHandlingSelfTest.java
@@ -17,18 +17,21 @@
 
 package org.apache.ignite.internal.processors.cache.distributed.dht.atomic;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.UUID;
 import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.IgniteDataStreamer;
+import org.apache.ignite.cache.CacheAtomicityMode;
 import org.apache.ignite.cache.CachePartialUpdateException;
 import org.apache.ignite.cache.CacheWriteSynchronizationMode;
 import org.apache.ignite.cache.affinity.Affinity;
@@ -42,8 +45,8 @@ import org.apache.ignite.internal.TestDelayingCommunicationSpi;
 import org.apache.ignite.internal.managers.communication.GridIoMessage;
 import org.apache.ignite.internal.processors.cache.GridCacheAdapter;
 import org.apache.ignite.internal.processors.cache.GridCacheEntryEx;
+import org.apache.ignite.internal.processors.cache.GridCacheEntryRemovedException;
 import org.apache.ignite.internal.processors.cache.distributed.dht.GridDhtInvalidPartitionException;
-import org.apache.ignite.internal.processors.cache.version.GridCacheVersion;
 import org.apache.ignite.internal.util.lang.GridAbsPredicate;
 import org.apache.ignite.internal.util.typedef.internal.CU;
 import org.apache.ignite.internal.util.typedef.internal.U;
@@ -57,8 +60,6 @@ import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import static org.apache.ignite.cache.CacheMode.PARTITIONED;
 import static org.apache.ignite.cache.CacheRebalanceMode.SYNC;
 import static org.apache.ignite.cache.CacheWriteSynchronizationMode.FULL_ASYNC;
-import static org.apache.ignite.cache.CacheWriteSynchronizationMode.FULL_SYNC;
-import static org.apache.ignite.cache.CacheWriteSynchronizationMode.PRIMARY_SYNC;
 
 /**
  * Test GridDhtInvalidPartitionException handling in ATOMIC cache during restarts.
@@ -67,6 +68,7 @@ import static org.apache.ignite.cache.CacheWriteSynchronizationMode.PRIMARY_SYNC
 public class GridCacheAtomicInvalidPartitionHandlingSelfTest extends GridCommonAbstractTest {
     /** IP finder. */
     private static final TcpDiscoveryIpFinder IP_FINDER = new TcpDiscoveryVmIpFinder(true);
+    private static final int BACKUPS = 2;
 
     /** Delay flag. */
     private static volatile boolean delay;
@@ -96,13 +98,17 @@ public class GridCacheAtomicInvalidPartitionHandlingSelfTest extends GridCommonA
         return cfg;
     }
 
+    @Override protected long getTestTimeout() {
+        return 30 * 60 * 1000;
+    }
+
     /** {@inheritDoc} */
     protected CacheConfiguration cacheConfiguration() {
         CacheConfiguration ccfg = new CacheConfiguration(DEFAULT_CACHE_NAME);
 
         ccfg.setCacheMode(PARTITIONED);
 
-        ccfg.setBackups(1);
+        ccfg.setBackups(BACKUPS);
         ccfg.setWriteSynchronizationMode(writeSync);
 
         ccfg.setRebalanceMode(SYNC);
@@ -122,6 +128,10 @@ public class GridCacheAtomicInvalidPartitionHandlingSelfTest extends GridCommonA
         stopAllGrids();
     }
 
+    @Override protected void afterTestsStopped() throws Exception {
+        super.afterTestsStopped();
+    }
+
     /**
      * @return {@code True} if test updates from client node.
      */
@@ -132,9 +142,9 @@ public class GridCacheAtomicInvalidPartitionHandlingSelfTest extends GridCommonA
     /**
      * @throws Exception If failed.
      */
-    public void testPrimaryFullSync() throws Exception {
+    /*public void testPrimaryFullSync() throws Exception {
         checkRestarts(FULL_SYNC);
-    }
+    }*/
 
     /**
      * @throws Exception If failed.
@@ -143,11 +153,201 @@ public class GridCacheAtomicInvalidPartitionHandlingSelfTest extends GridCommonA
         checkRestarts(PRIMARY_SYNC);
     }*/
 
+    /*public void testMyTest() throws Exception {
+        this.writeSync = FULL_ASYNC;
+        IgniteEx ignite = startGrid(3);
+        IgniteCache<String, Integer> cache = ignite.cache(DEFAULT_CACHE_NAME);
+        cache.put("@@@MY_KEY@@@", 505);
+        Thread.sleep(2 * 1000);
+        cache.put("@@@MY_KEY@@@", 2005);
+        Thread.sleep(2 * 1000);
+
+        //grid(0).localNode().
+
+        assertEquals(Integer.valueOf(2005), cache.get("@@@MY_KEY@@@"));
+    }*/
+
     /**
      * @throws Exception If failed.
      */
-    public void testPrimaryFullAsync() throws Exception {
+    public void testPrimaryFullAsync0() throws Exception {
         checkRestartsForAsyncMode();
+    }
+
+    private void checkRestartsForAsyncMode() throws Exception {
+        StringBuilder sb = new StringBuilder();
+
+        this.writeSync = FULL_ASYNC;
+
+        final int gridCnt = 6;
+
+        startGrids(gridCnt);
+
+        awaitPartitionMapExchange();
+
+        assertEquals(testClientNode(), (boolean)grid(0).configuration().isClientMode());
+
+        final int range = 30_000;
+
+        populateCache(grid(0), range);
+        //checkWriteValuesSingleThread(gridCnt, range);
+
+        System.err.println("FINISHED PUTS");
+
+        delay = true;
+
+        final IgniteCache<Object, Object> cache = grid(0).cache(DEFAULT_CACHE_NAME);
+
+        final AtomicBoolean done = new AtomicBoolean();
+        final AtomicInteger threadIdx = new AtomicInteger(0);
+        // Start put threads.
+        IgniteInternalFuture<?> fut = multithreadedAsync(new Callable<Object>() {
+            @Override public Object call() throws Exception {
+                Random rnd = new Random();
+                Integer thIdx = threadIdx.incrementAndGet() * 1000;
+                while (!done.get()) {
+                    try {
+                        int cnt = rnd.nextInt(5);
+
+                        if (cnt < 2) {
+                            int key = rnd.nextInt(range);
+                            //int val = rnd.nextInt();
+                            cache.put(key, 111);
+                        }
+                        else {
+                            Map<Integer, Integer> upd = new TreeMap<>();
+
+                            for (int i = 0; i < cnt; i++) {
+                                int key = rnd.nextInt(range);
+                                upd.put(key, 222);
+                            }
+
+                            cache.putAll(upd);
+                        }
+                    }
+                    catch (CachePartialUpdateException ignored) {
+                        // No-op.
+                    }
+                }
+
+                return null;
+            }
+        }, 4, "putAll-thread");
+
+        Random rnd = new Random();
+
+        // Restart random nodes.
+        /*sb.append(" RESTART").append("\n");
+        for (int r = 0; r < 20; r++) {
+            int idx0 = rnd.nextInt(gridCnt - 1) + 1;
+            IgniteEx ignite = grid(idx0);
+            sb.append(r)
+                .append(":\t")
+                .append(idx0)
+                .append("\t")
+                .append(ignite.localNode().id());
+
+            stopGrid(idx0);
+
+            U.sleep(200);
+
+            ignite = startGrid(idx0);
+            sb.append("\t").append("after")
+                .append("\t").append(ignite.localNode().id())
+                .append("\t").append(ignite.localNode().consistentId())
+                .append("\n");
+        }
+        sb.append("\n");*/
+
+        int[] restartOrder = new int[]{2, 4, 1, 1, 2, 1, 4, 3, 4, 1, 2, 1, 1, 5, 4, 4, 2, 5, 3, 4};
+        for (int idx0 : restartOrder) {
+            stopGrid(idx0);
+
+            U.sleep(200);
+
+            startGrid(idx0);
+        }
+
+//        U.sleep(20 * 200 + 2 * 1000);
+
+        done.set(true);
+
+        awaitPartitionMapExchange(false, false, null);
+
+        fut.get();
+
+//            List<DebugData> debugList = new LinkedList<>();
+        for (int k = 0; k < range; k++) {
+            boolean wasErr;
+            int cnt = 0;
+            do {
+                wasErr = false;
+                Collection<ClusterNode> affNodes = affinity(cache).mapKeyToPrimaryAndBackups(k);
+//                debugList.clear();
+                final List<GridCacheAdapter> internalCaches = new ArrayList<>();
+                for (int i = 0; i < gridCnt; i++) {
+                    ClusterNode locNode = grid(i).localNode();
+                    if (affNodes.contains(locNode))
+                        internalCaches.add(((IgniteKernal)grid(i)).internalCache(DEFAULT_CACHE_NAME));
+                }
+                try {
+                    assertEquals(BACKUPS + 1, internalCaches.size());
+
+                    List<GridCacheEntryEx> entries = new ArrayList<>();
+
+                    for (GridCacheAdapter adapter : internalCaches) {
+                        GridCacheEntryEx entry = null;
+                        try {
+                            entry = adapter.entryEx(k);
+                            entry.unswap();
+                        }
+                        catch (GridDhtInvalidPartitionException ignored) {
+                            //no-op
+                        }
+
+                        assertNotNull("Failed to find entry on node for key [locNode=" + adapter.context().localNodeId() +
+                            ", key=" + k + ']', entry);
+
+                        assertTrue("Partition is invalid [entry=" + entry + ']', entry.partitionValid());
+
+                        entries.add(entry);
+                    }
+
+                    GridCacheEntryEx first = entries.get(0);
+
+                    assertNotNull(first.version());
+
+                    for (int i = 1; i < entries.size(); i++)
+                        assertEntry(first, entries.get(i));
+                }
+                catch (AssertionError e) {
+                    if (cnt < 10) {
+                        cnt++;
+                        wasErr = true;
+                        System.out.println("Fail [attempt=" + cnt + ", err=" + e.getMessage() + ']');
+                        U.sleep(10 * 1000);
+                    }
+                    else {
+                        System.out.println(sb.toString());
+                        throw e;
+                    }
+                }
+
+            }
+            while (wasErr);
+        }
+    }
+
+    private void assertEntry(GridCacheEntryEx exp, GridCacheEntryEx act) throws GridCacheEntryRemovedException {
+        assertEquals("Failed to check value for key [key=" + exp.key() +
+                ", node=" + exp.context().localNodeId() + ", ver=" + exp.version() +
+                ", actNode=" + act.context().localNodeId() + ", actVer" + act.version() + ']',
+            CU.<Integer>value(exp.rawGet(), exp.context(), false),
+            CU.<Integer>value(act.rawGet(), act.context(), false));
+
+        assertEquals("Failed to check version for key [key=" + exp.key() + ", firstNode=" +
+                exp.context().localNodeId() + ", secondNode=" + act.context().localNodeId() + ']',
+            exp.version(), act.version());
     }
 
     private Set<Integer> populateCache(IgniteEx ignite, int range) {
@@ -166,7 +366,7 @@ public class GridCacheAtomicInvalidPartitionHandlingSelfTest extends GridCommonA
         return set;
     }
 
-    private void checkWriteValuesSingleThread(int gridCnt, int range) throws Exception{
+    private void checkWriteValuesSingleThread(int gridCnt, int range) throws Exception {
         IgniteEx ignite = grid(0);
 
         final Set<Integer> keys = populateCache(ignite, range);
@@ -209,166 +409,11 @@ public class GridCacheAtomicInvalidPartitionHandlingSelfTest extends GridCommonA
         assertTrue(keys.isEmpty());
     }
 
-    private void checkRestartsForAsyncMode() throws Exception {
-        this.writeSync = FULL_ASYNC;
-
-        final int gridCnt = 6;
-
-        startGrids(gridCnt);
-
-        awaitPartitionMapExchange();
-
-        try {
-            assertEquals(testClientNode(), (boolean)grid(0).configuration().isClientMode());
-
-            final int range = 100_000;
-
-            checkWriteValuesSingleThread(gridCnt, range);
-
-            System.err.println("FINISHED PUTS");
-
-            delay = true;
-
-            final IgniteCache<Object, Object> cache = grid(0).cache(DEFAULT_CACHE_NAME);
-
-            final AtomicBoolean done = new AtomicBoolean();
-            // Start put threads.
-            IgniteInternalFuture<?> fut = multithreadedAsync(new Callable<Object>() {
-                @Override public Object call() throws Exception {
-                    Random rnd = new Random();
-
-                    while (!done.get()) {
-                        try {
-                            int cnt = rnd.nextInt(5);
-
-                            if (cnt < 2) {
-                                int key = rnd.nextInt(range);
-
-                                int val = rnd.nextInt();
-
-                                cache.put(key, val);
-                            }
-                            else {
-                                Map<Integer, Integer> upd = new TreeMap<>();
-
-                                for (int i = 0; i < cnt; i++)
-                                    upd.put(rnd.nextInt(range), rnd.nextInt());
-
-                                cache.putAll(upd);
-                            }
-                        }
-                        catch (CachePartialUpdateException ignored) {
-                            // No-op.
-                        }
-                    }
-
-                    return null;
-                }
-            }, 4, "putAll-thread");
-
-            Random rnd = new Random();
-
-            // Restart random nodes.
-            for (int r = 0; r < 20; r++) {
-                int idx0 = rnd.nextInt(gridCnt - 1) + 1;
-
-                stopGrid(idx0);
-
-                U.sleep(200);
-
-                startGrid(idx0);
-            }
-
-            done.set(true);
-
-            fut.get();
-
-            awaitPartitionMapExchange();
-
-            for (int k = 0; k < range; k++) {
-                Collection<ClusterNode> affNodes = affinity(cache).mapKeyToPrimaryAndBackups(k);
-
-                // Test is valid with at least one backup.
-                assert affNodes.size() >= 2;
-
-                Object val = null;
-                GridCacheVersion ver = null;
-                UUID nodeId = null;
-
-                for (int i = 0; i < gridCnt; i++) {
-                    ClusterNode locNode = grid(i).localNode();
-
-                    GridCacheAdapter<Object, Object> c = ((IgniteKernal)grid(i)).internalCache(DEFAULT_CACHE_NAME);
-
-                    GridCacheEntryEx entry = null;
-
-                    try {
-                        entry = c.entryEx(k);
-
-                        entry.unswap();
-                    }
-                    catch (GridDhtInvalidPartitionException ignored) {
-                        // Skip key.
-                    }
-
-                    for (int r = 0; r < 10; r++) {
-                        try {
-                            if (affNodes.contains(locNode)) {
-                                assert c.affinity().isPrimaryOrBackup(locNode, k);
-
-                                boolean primary = c.affinity().isPrimary(locNode, k);
-
-                                assertNotNull("Failed to find entry on node for key [locNode=" + locNode.id() +
-                                    ", key=" + k + ']', entry);
-
-                                if (val == null) {
-                                    assertNull(ver);
-
-                                    val = CU.value(entry.rawGet(), entry.context(), false);
-                                    ver = entry.version();
-                                    nodeId = locNode.id();
-                                }
-                                else {
-                                    assertNotNull(ver);
-
-                                    assertEquals("Failed to check value for key [key=" + k + ", node=" +
-                                            locNode.id() + ", primary=" + primary + ", recNodeId=" + nodeId + ']',
-                                        val, CU.value(entry.rawGet(), entry.context(), false));
-
-                                    assertEquals("Failed to check version for key [key=" + k + ", node=" +
-                                            locNode.id() + ", primary=" + primary + ", recNodeId=" + nodeId + ']',
-                                        ver, entry.version());
-                                }
-                            }
-                            else
-                                assertTrue("Invalid entry: " + entry, entry == null || !entry.partitionValid());
-                        }
-                        catch (AssertionError e) {
-                            if (r == 9) {
-                                info("Failed to verify cache contents: " + e.getMessage());
-
-                                throw e;
-                            }
-
-                            info("Failed to verify cache contents, will retry: " + e.getMessage());
-
-                            // Give some time to finish async updates.
-                            U.sleep(1000);
-                        }
-                    }
-                }
-            }
-        }
-        finally {
-            stopAllGrids();
-        }
-    }
-
     /**
      * @param writeSync Write synchronization mode to check.
      * @throws Exception If failed.
      */
-    private void checkRestarts(CacheWriteSynchronizationMode writeSync)
+    /*private void checkRestarts(CacheWriteSynchronizationMode writeSync)
         throws Exception {
         this.writeSync = writeSync;
 
@@ -506,7 +551,7 @@ public class GridCacheAtomicInvalidPartitionHandlingSelfTest extends GridCommonA
         finally {
             stopAllGrids();
         }
-    }
+    }*/
 
     /**
      *
@@ -518,4 +563,33 @@ public class GridCacheAtomicInvalidPartitionHandlingSelfTest extends GridCommonA
                 (msg instanceof GridDhtAtomicAbstractUpdateRequest));
         }
     }
+
+    class DebugData {
+
+        private int order;
+        private ClusterNode node;
+        private GridCacheEntryEx entry;
+
+        public DebugData(int order, ClusterNode node, GridCacheEntryEx entry) {
+            this.order = order;
+            this.node = node;
+            this.entry = entry;
+        }
+
+        @Override public String toString() {
+            try {
+                return "DebugData{" +
+                    "order=" + order +
+                    ", nodeId=" + node.id() +
+                    ", key=" + entry.key() +
+                    ", ver=" + entry.version() +
+                    ", value=" + CU.value(entry.rawGet(), entry.context(), false) +
+                    '}';
+            }
+            catch (Exception e) {
+                return e.getMessage();
+            }
+        }
+    }
+
 }
