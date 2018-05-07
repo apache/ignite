a9260f0a46f427b39a300d22913024dc1ea1f62b
ekaterina-nbl <ekaterina.vergizova@nobitlost.com>
2018-05-07 20:09:06 +0300

    examples

--- modules/platforms/nodejs/examples/CachePutGetExample.js
+++ modules/platforms/nodejs/examples/CachePutGetExample.js
@@ -58,24 +58,34 @@ class Person {
 //   -- ScanQuery
 class CachePutGetExample {
 
+    constructor() {
+        this._personCache = null;
+        this._personObjectType = null;
+        this._binaryObjectCache = null; 
+    }
+
     async start() {
         const igniteClient = new IgniteClient(this.onStateChanged.bind(this));
         try {
             await igniteClient.connect(new IgniteClientConfiguration(ENDPOINT));
 
-            const cache = (await igniteClient.getOrCreateCache(CACHE_NAME)).
-                setKeyType(ObjectType.PRIMITIVE_TYPE.INTEGER);
-
-            const personComplexObjectType = new ComplexObjectType(new Person(), PERSON_TYPE_NAME).
+            this._personObjectType = new ComplexObjectType(new Person(), PERSON_TYPE_NAME).
                 setFieldType('id', ObjectType.PRIMITIVE_TYPE.INTEGER);
 
-            await this.putComplexObjects(cache, personComplexObjectType);
-            await this.putAllBinaryObjects(cache, personComplexObjectType);
+            this._personCache = (await igniteClient.getOrCreateCache(CACHE_NAME)).
+                setKeyType(ObjectType.PRIMITIVE_TYPE.INTEGER).
+                setValueType(this._personObjectType);
 
-            await this.getAllComplexObjects(cache, personComplexObjectType);
-            await this.getBinaryObjects(cache);
+            this._binaryObjectCache = igniteClient.getCache(CACHE_NAME).
+                setKeyType(ObjectType.PRIMITIVE_TYPE.INTEGER);
 
-            await this.scanQuery(cache, personComplexObjectType);
+            await this.putComplexObjects();
+            await this.putAllBinaryObjects();
+
+            await this.getAllComplexObjects();
+            await this.getBinaryObjects();
+
+            await this.scanQuery();
 
             await igniteClient.destroyCache(CACHE_NAME);
         }
@@ -87,23 +97,20 @@ class CachePutGetExample {
         }
     }
 
-    async putComplexObjects(cache, personComplexObjectType) {
-        cache.setValueType(personComplexObjectType);
-
+    async putComplexObjects() {
         const person1 = new Person('John', 'Doe', 1000);
         const person2 = new Person('Jane', 'Roe', 2000);
 
         // put multiple values in parallel
         await Promise.all([
-            await cache.put(person1.id, person1),
-            await cache.put(person2.id, person2)
+            await this._personCache.put(person1.id, person1),
+            await this._personCache.put(person2.id, person2)
         ]);
 
         console.log('Complex Objects put successfully');
     }
 
-    async putAllBinaryObjects(cache, personComplexObjectType) {
-        cache.setValueType(null);
+    async putAllBinaryObjects() {
         // create binary object from scratch
         const personBinaryObject1 = new BinaryObject(PERSON_TYPE_NAME).
             setField('id', Person.generateId(), ObjectType.PRIMITIVE_TYPE.INTEGER).
@@ -113,9 +120,9 @@ class CachePutGetExample {
 
         // create binary object from complex object
         const personBinaryObject2 = await BinaryObject.fromObject(
-            new Person('Richard', 'Miles', 800), personComplexObjectType);
+            new Person('Richard', 'Miles', 800), this._personObjectType);
 
-        await cache.putAll([
+        await this._binaryObjectCache.putAll([
             new CacheEntry(await personBinaryObject1.getField('id'), personBinaryObject1),
             new CacheEntry(await personBinaryObject2.getField('id'), personBinaryObject2)
         ]);
@@ -123,18 +130,16 @@ class CachePutGetExample {
         console.log('Binary Objects put successfully using putAll()');
     }
 
-    async getAllComplexObjects(cache, personComplexObjectType) {
-        cache.setValueType(personComplexObjectType);
-        const persons = await cache.getAll([2, 3]);
+    async getAllComplexObjects() {
+        const persons = await this._personCache.getAll([2, 3]);
         console.log('Complex Objects getAll:');
         for (let person of persons) {
             this.printPersonObject(person.getValue());
         }
     }
     
-    async getBinaryObjects(cache) {
-        cache.setValueType(null);
-        const personBinaryObject = await cache.get(3);
+    async getBinaryObjects() {
+        const personBinaryObject = await this._binaryObjectCache.get(3);
         console.log('Binary Object get:');
         console.log('  ' + personBinaryObject.getTypeName());
         let fieldValue;
@@ -144,9 +149,8 @@ class CachePutGetExample {
         }
     }
 
-    async scanQuery(cache, personComplexObjectType) {
-        cache.setValueType(personComplexObjectType);
-        const cursor = await cache.query(new ScanQuery());
+    async scanQuery() {
+        const cursor = await this._personCache.query(new ScanQuery());
         console.log('Scan query results:');
         for (let cacheEntry of await cursor.getAll()) {
             this.printPersonObject(cacheEntry.getValue());
