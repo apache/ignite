585f6566e8409a8669e71cf91475ee9af7c7186a
Ivan Rakov <ivan.glukos@gmail.com>
2018-05-08 15:49:59 +0300

    IGNITE-8429 Unexpected error during incorrect WAL segment decompression, causes node termination + synchronized block
    (cherry picked from commit 99a07ed)
    
    (cherry picked from commit 7037022)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/FileWriteAheadLogManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/FileWriteAheadLogManager.java
@@ -2085,7 +2085,9 @@ public class FileWriteAheadLogManager extends GridCacheSharedManagerAdapter impl
                         IgniteCheckedException e = new IgniteCheckedException("Error during WAL segment " +
                             "decompression [segmentIdx=" + segmentToDecompress + "]", t);
 
-                        decompressionFutures.remove(segmentToDecompress).onDone(e);
+                        synchronized (this) {
+                            decompressionFutures.remove(segmentToDecompress).onDone(e);
+                        }
                     }
                 }
             }
