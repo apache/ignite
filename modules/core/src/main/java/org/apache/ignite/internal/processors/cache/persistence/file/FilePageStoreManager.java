2fcda060a98867269aa92ed1c00bd9fd93520782
denis.garus <d.garus@isimplelab.com>
2018-05-10 16:35:45 +0300

    IGNITE-8162 Handle ClassNotFoundException during deserialization of persisted cache configuration
    Signed-off-by: Anton Vinogradov <av@apache.org>

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/file/FilePageStoreManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/file/FilePageStoreManager.java
@@ -692,6 +692,15 @@ public class FilePageStoreManager extends GridCacheSharedManagerAdapter implemen
             throw new IgniteCheckedException("Failed to read cache configuration from disk for cache: " +
                 conf.getAbsolutePath(), e);
         }
+        catch (IgniteCheckedException e) {
+            if (e.hasCause(ClassNotFoundException.class))
+                throw new IgniteCheckedException("An error occurred during cache configuration loading from file [file=" +
+                    conf.getAbsolutePath() + "]. You may want to remove the configuration file; cache will be running " +
+                    "after next node start if static Ignite Configuration contains correct configuration of this cache. " +
+                    "If it was started dynamically, you may need to start it again (all data will be present).", e);
+            else
+                throw e;
+        }
     }
 
     /** {@inheritDoc} */
