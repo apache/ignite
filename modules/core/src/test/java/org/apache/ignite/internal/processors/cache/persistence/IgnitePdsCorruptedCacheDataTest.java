d14d79eae49922442d90f7b820fd85e6692c635a
sbt-garus-dg <garus.d.g@gmail.com>
2018-05-10 11:46:03 +0300

    IGNITE-8162 fix comment

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/IgnitePdsCorruptedCacheDataTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/IgnitePdsCorruptedCacheDataTest.java
@@ -17,27 +17,39 @@
 
 package org.apache.ignite.internal.processors.cache.persistence;
 
+import java.io.File;
 import javax.cache.configuration.Factory;
+import org.apache.ignite.IgniteCache;
 import org.apache.ignite.IgniteCheckedException;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.DataRegionConfiguration;
 import org.apache.ignite.configuration.DataStorageConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.internal.IgniteEx;
+import org.apache.ignite.internal.processors.cache.GridCacheSharedContext;
+import org.apache.ignite.internal.processors.cache.persistence.file.FilePageStoreManager;
 import org.apache.ignite.internal.util.typedef.internal.U;
 import org.apache.ignite.logger.NullLogger;
 import org.apache.ignite.testframework.GridTestUtils;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 
+import static org.apache.ignite.internal.processors.cache.persistence.file.FilePageStoreManager.CACHE_DATA_FILENAME;
+import static org.apache.ignite.internal.processors.cache.persistence.file.FilePageStoreManager.CACHE_DIR_PREFIX;
+
 /**
  *
  */
 public class IgnitePdsCorruptedCacheDataTest extends GridCommonAbstractTest {
+    /** Test cache name. */
+    private static final String TEST_CACHE = "test_cache";
     /** Start grid with known cache factory. */
-    private boolean withFactory = true;
+    private boolean withFactory;
 
     /** {@inheritDoc} */
     @Override protected void beforeTest() throws Exception {
         cleanPersistenceDir();
+
+        withFactory = true;
 
         super.beforeTest();
     }
@@ -75,7 +87,7 @@ public class IgnitePdsCorruptedCacheDataTest extends GridCommonAbstractTest {
      */
     @SuppressWarnings("unchecked")
     private CacheConfiguration getCacheConfiguration() throws Exception {
-        CacheConfiguration cacheCfg = new CacheConfiguration("test_cache");
+        CacheConfiguration cacheCfg = new CacheConfiguration(TEST_CACHE);
 
         if (withFactory) {
             Factory storeFactory = (Factory)getExternalClassLoader()
@@ -92,9 +104,14 @@ public class IgnitePdsCorruptedCacheDataTest extends GridCommonAbstractTest {
      * @throws Exception if failed.
      */
     public void testFilePageStoreManagerShouldThrowExceptionWhenFactoryClassCannotBeLoaded() throws Exception {
-        startGrid()
-            .cluster()
+        IgniteEx ignite = (IgniteEx)startGrid();
+
+        ignite.cluster()
             .active(true);
+
+        IgniteCache<Integer, String> cache = ignite.cache(TEST_CACHE);
+
+        cache.put(1, "test value");
 
         stopGrid();
 
@@ -106,5 +123,40 @@ public class IgnitePdsCorruptedCacheDataTest extends GridCommonAbstractTest {
             IgniteCheckedException.class,
             "An error occurred during cache configuration loading from file"
         );
+
+        GridCacheSharedContext sharedCtx = ignite.context().cache().context();
+        FilePageStoreManager pageStore = (FilePageStoreManager)sharedCtx.pageStore();
+
+        assertNotNull(pageStore);
+
+        assertTrue("Cache data file wasn't deleted", deleteCacheDataFile(pageStore.workDir()));
+
+        ignite = (IgniteEx)startGrid();
+
+        ignite.cluster()
+            .active(true);
+
+        cache = ignite.cache(TEST_CACHE);
+
+        assertEquals("test value", cache.get(1));
+    }
+
+    /**
+     * Delete cache data file.
+     *
+     * @param workDir Cache work directory.
+     * @return true if file was deleted.
+     */
+    private boolean deleteCacheDataFile(File workDir) {
+        File[] files = workDir.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory() && file.getName().equals(CACHE_DIR_PREFIX + TEST_CACHE))
+                    return U.delete(new File(file, CACHE_DATA_FILENAME));
+            }
+        }
+
+        return false;
     }
 }
