a92ef21f824022a3cf6b66a8ec3ef2c5baf7b8a9
Dmitriy Sorokin <sbt.sorokin.dvl@gmail.com>
2018-05-14 19:23:40 +0300

    IGNITE-4958: PageEvictionPagesRecyclingAndReusingTest test timeout changed to 10 min (5 min is not enough for TC in Tx modes).

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/eviction/paged/PageEvictionPagesRecyclingAndReusingTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/eviction/paged/PageEvictionPagesRecyclingAndReusingTest.java
@@ -33,6 +33,9 @@ import org.apache.ignite.internal.processors.cache.persistence.tree.reuse.ReuseL
  *
  */
 public class PageEvictionPagesRecyclingAndReusingTest extends PageEvictionAbstractTest {
+    /** Test timeout. */
+    private static final long TEST_TIMEOUT = 10 * 60 * 1000;
+
     /** Number of small entries. */
     private static final int SMALL_ENTRIES = ENTRIES * 10;
 
@@ -44,6 +47,11 @@ public class PageEvictionPagesRecyclingAndReusingTest extends PageEvictionAbstra
     /** {@inheritDoc} */
     @Override protected void afterTest() throws Exception {
         stopAllGrids();
+    }
+
+    /** {@inheritDoc} */
+    @Override protected long getTestTimeout() {
+        return TEST_TIMEOUT;
     }
 
     /**
