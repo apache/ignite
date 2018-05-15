1b6f21564f89966a4dd992e86c0314fbca5c88a6
NSAmelchev <nsamelchev@gmail.com>
2018-05-15 18:18:40 +0300

    Fix timeout

--- modules/core/src/test/java/org/apache/ignite/spi/communication/tcp/IgniteTcpCommunicationRecoveryAckClosureSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/spi/communication/tcp/IgniteTcpCommunicationRecoveryAckClosureSelfTest.java
@@ -301,7 +301,7 @@ public class IgniteTcpCommunicationRecoveryAckClosureSelfTest<T extends Communic
         final GridNioServer srv1 = U.field(spi1, "nioSrvr");
 
         // For prevent session close on write timeout.
-        srv1.writeTimeout(60_000);
+        srv1.writeTimeout(10_000);
 
         final AtomicInteger ackMsgs = new AtomicInteger(0);
 
