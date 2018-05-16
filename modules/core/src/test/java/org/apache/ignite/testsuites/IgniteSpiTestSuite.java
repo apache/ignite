be4a6f92fcf5d3e5216ae3931147e6facb167bf1
NSAmelchev <nsamelchev@gmail.com>
2018-05-16 09:34:58 +0300

    Revert "For TC test"
    This reverts commit b9c296f985ea63c76135d3cdccfe1cc38c51297b.

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteSpiTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteSpiTestSuite.java
@@ -31,37 +31,37 @@ public class IgniteSpiTestSuite extends TestSuite {
      */
     public static TestSuite suite() throws Exception {
         TestSuite suite = new TestSuite("Ignite SPIs Test Suite");
-//
-//        // Failover.
-//        suite.addTest(IgniteSpiFailoverSelfTestSuite.suite());
-//
-//        // Collision.
-//        suite.addTest(IgniteSpiCollisionSelfTestSuite.suite());
-//
-//        // Event storage.
-//        suite.addTest(IgniteSpiEventStorageSelfTestSuite.suite());
-//
-//        // Load Balancing.
-//        suite.addTest(IgniteSpiLoadBalancingSelfTestSuite.suite());
-//
-//        // Checkpoints.
-//        suite.addTest(IgniteSpiCheckpointSelfTestSuite.suite());
-//
-//        // Deployment
-//        suite.addTest(IgniteSpiDeploymentSelfTestSuite.suite());
-//
-//        // Discovery.
-//        suite.addTest(IgniteSpiDiscoverySelfTestSuite.suite());
+
+        // Failover.
+        suite.addTest(IgniteSpiFailoverSelfTestSuite.suite());
+
+        // Collision.
+        suite.addTest(IgniteSpiCollisionSelfTestSuite.suite());
+
+        // Event storage.
+        suite.addTest(IgniteSpiEventStorageSelfTestSuite.suite());
+
+        // Load Balancing.
+        suite.addTest(IgniteSpiLoadBalancingSelfTestSuite.suite());
+
+        // Checkpoints.
+        suite.addTest(IgniteSpiCheckpointSelfTestSuite.suite());
+
+        // Deployment
+        suite.addTest(IgniteSpiDeploymentSelfTestSuite.suite());
+
+        // Discovery.
+        suite.addTest(IgniteSpiDiscoverySelfTestSuite.suite());
 
         // Communication.
         suite.addTest(IgniteSpiCommunicationSelfTestSuite.suite());
 
-       /* // All other tests.
+        // All other tests.
         suite.addTestSuite(GridNoopManagerSelfTest.class);
 
         // Local Message Listener tests.
         suite.addTestSuite(GridManagerLocalMessageListenerSelfTest.class);
-*/
+
         return suite;
     }
 }
