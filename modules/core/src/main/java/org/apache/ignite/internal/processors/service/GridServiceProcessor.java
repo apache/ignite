651073a6d9ad4fa4d0cd1bff2e9903192cab4981
mcherkasov <mcherkasov@gridgain.com>
2018-05-17 16:27:54 +0300

    GG-13876 Merge service re-assignments during cluster startup

--- modules/core/src/main/java/org/apache/ignite/internal/processors/service/GridServiceProcessor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/service/GridServiceProcessor.java
@@ -111,9 +111,12 @@ import org.jetbrains.annotations.Nullable;
 import org.jsr166.ConcurrentHashMap8;
 
 import static org.apache.ignite.IgniteSystemProperties.IGNITE_SERVICES_COMPATIBILITY_MODE;
+import static org.apache.ignite.IgniteSystemProperties.IGNITE_SERVICE_REASSIGN_DELAY;
+import static org.apache.ignite.IgniteSystemProperties.getInteger;
 import static org.apache.ignite.IgniteSystemProperties.getString;
 import static org.apache.ignite.configuration.DeploymentMode.ISOLATED;
 import static org.apache.ignite.configuration.DeploymentMode.PRIVATE;
+import static org.apache.ignite.events.EventType.EVT_NODE_JOINED;
 import static org.apache.ignite.internal.IgniteNodeAttributes.ATTR_SERVICES_COMPATIBILITY_MODE;
 import static org.apache.ignite.internal.processors.cache.GridCacheUtils.UTILITY_CACHE_NAME;
 import static org.apache.ignite.transactions.TransactionConcurrency.PESSIMISTIC;
@@ -128,6 +131,9 @@ public class GridServiceProcessor extends GridProcessorAdapter {
     /** */
     public static final IgniteProductVersion LAZY_SERVICES_CFG_SINCE = IgniteProductVersion.fromString("1.5.22");
 
+    /** */
+    private static final int REASSIGN_DELAY = getInteger(IGNITE_SERVICE_REASSIGN_DELAY, 30_000);
+
     /** Versions that only compatible with each other, and from 1.5.33. */
     private static final Set<IgniteProductVersion> SERVICE_TOP_CALLABLE_VER1;
 
@@ -139,7 +145,7 @@ public class GridServiceProcessor extends GridProcessorAdapter {
 
     /** */
     private static final int[] EVTS = {
-        EventType.EVT_NODE_JOINED,
+        EVT_NODE_JOINED,
         EventType.EVT_NODE_LEFT,
         EventType.EVT_NODE_FAILED,
         DiscoveryCustomEvent.EVT_DISCOVERY_CUSTOM_EVT
@@ -598,12 +604,12 @@ public class GridServiceProcessor extends GridProcessorAdapter {
     public IgniteInternalFuture<?> deployAll(ClusterGroup prj, Collection<ServiceConfiguration> cfgs) {
         if (prj == null)
             // Deploy to servers by default if no projection specified.
-            return deployAll(cfgs,  ctx.cluster().get().forServers().predicate());
+            return deployAll(cfgs, ctx.cluster().get().forServers().predicate());
         else if (prj.predicate() == F.<ClusterNode>alwaysTrue())
-            return deployAll(cfgs,  null);
+            return deployAll(cfgs, null);
         else
             // Deploy to predicate nodes by default.
-            return deployAll(cfgs,  prj.predicate());
+            return deployAll(cfgs, prj.predicate());
     }
 
     /**
@@ -1936,8 +1942,9 @@ public class GridServiceProcessor extends GridProcessorAdapter {
 
                 currTopVer = topVer;
 
-                depExe.execute(new DepRunnable() {
-                    @Override public void run0() {
+                final DepRunnable depRunnable = new DepRunnable() {
+                    @Override
+                    public void run0() {
                         // In case the cache instance isn't tracked by DiscoveryManager anymore.
                         discoCache.updateAlives(ctx.discovery());
 
@@ -2031,7 +2038,17 @@ public class GridServiceProcessor extends GridProcessorAdapter {
                             }
                         }
                     }
-                });
+                };
+
+                if (REASSIGN_DELAY <= 0 || evt.type() != EVT_NODE_JOINED)
+                    depExe.execute(depRunnable);
+                else
+                    ctx.timeout().schedule(new Runnable() {
+                        @Override
+                        public void run() {
+                            depExe.execute(depRunnable);
+                        }
+                    }, REASSIGN_DELAY, -1);
             }
             finally {
                 busyLock.leaveBusy();
