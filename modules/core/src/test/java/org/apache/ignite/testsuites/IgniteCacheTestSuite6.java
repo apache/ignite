372d3026218fcb36b6b84751cf6384e7d5cd513b
Ivan Daschinskiy <ivandasch@gmail.com>
2018-05-28 15:47:05 +0300

    IGNITE-8624: Add reproducer to IGNITE-8624 issue. After fix it passes.

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheTestSuite6.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheTestSuite6.java
@@ -42,17 +42,7 @@ import org.apache.ignite.internal.processors.cache.distributed.IgniteOptimisticT
 import org.apache.ignite.internal.processors.cache.distributed.IgniteOptimisticTxSuspendResumeTest;
 import org.apache.ignite.internal.processors.cache.distributed.IgnitePessimisticTxSuspendResumeTest;
 import org.apache.ignite.internal.processors.cache.persistence.IgnitePdsCacheAssignmentNodeRestartsTest;
-import org.apache.ignite.internal.processors.cache.transactions.TxLabelTest;
-import org.apache.ignite.internal.processors.cache.transactions.TxMultiCacheAsyncOpsTest;
-import org.apache.ignite.internal.processors.cache.transactions.TxOptimisticPrepareOnUnstableTopologyTest;
-import org.apache.ignite.internal.processors.cache.transactions.TxRollbackAsyncWithPersistenceTest;
-import org.apache.ignite.internal.processors.cache.transactions.TxOptimisticOnPartitionExchangeTest;
-import org.apache.ignite.internal.processors.cache.transactions.TxRollbackOnTimeoutNearCacheTest;
-import org.apache.ignite.internal.processors.cache.transactions.TxRollbackOnTimeoutNoDeadlockDetectionTest;
-import org.apache.ignite.internal.processors.cache.transactions.TxRollbackOnTimeoutTest;
-import org.apache.ignite.internal.processors.cache.transactions.TxRollbackAsyncNearCacheTest;
-import org.apache.ignite.internal.processors.cache.transactions.TxRollbackAsyncTest;
-import org.apache.ignite.internal.processors.cache.transactions.TxRollbackOnTopologyChangeTest;
+import org.apache.ignite.internal.processors.cache.transactions.*;
 
 /**
  * Test suite.
@@ -88,6 +78,8 @@ public class IgniteCacheTestSuite6 extends TestSuite {
         suite.addTestSuite(TxLabelTest.class);
 
         suite.addTestSuite(TxMultiCacheAsyncOpsTest.class);
+
+        suite.addTestSuite(TxOnCachesStartTest.class);
 
         suite.addTestSuite(IgnitePdsCacheAssignmentNodeRestartsTest.class);
 
