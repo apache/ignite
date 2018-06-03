825de709a9491a0528c9a1a503915fa27c33e615
Jock Tanner <tanner.of.kha@gmail.com>
2018-06-03 23:50:55 +1000

    IGNITE-7782 Start working with requests.

---
+++ modules/platforms/python/ignite_requests/__init__.py
@@ -0,0 +1,67 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import ctypes
+import socket
+
+from.op_codes import OP_HANDSHAKE
+from constants import *
+
+
+class HandshakeRequest(ctypes.LittleEndianStructure):
+    _pack_ = 1
+    _fields_ = [
+        ('length', ctypes.c_int),
+        ('hs_code', ctypes.c_byte),
+        ('version_major', ctypes.c_short),
+        ('version_minor', ctypes.c_short),
+        ('version_patch', ctypes.c_short),
+        ('client_code', ctypes.c_byte),
+    ]
+
+    def __init__(self):
+        super().__init__()
+        self.length = 8
+        self.hs_code = OP_HANDSHAKE
+        self.version_major = PROTOCOL_VERSION_MAJOR
+        self.version_minor = PROTOCOL_VERSION_MINOR
+        self.version_patch = PROTOCOL_VERSION_PATCH
+        self.client_code = 2
+
+
+def read_response(conn: socket.socket):
+    buffer = conn.recv(4)
+    length = int.from_bytes(buffer, byteorder='little')
+    buffer += conn.recv(length)
+    hs_code = int.from_bytes(buffer[4:5], byteorder='little')
+    fields = [
+        ('length', ctypes.c_int),
+        ('hs_code', ctypes.c_byte),
+    ]
+    if hs_code == 0:
+        fields += [
+            ('version_major', ctypes.c_short),
+            ('version_minor', ctypes.c_short),
+            ('version_patch', ctypes.c_short),
+        ]
+    response_class = type(
+        'HandshakeResponse',
+        (ctypes.LittleEndianStructure,),
+        {
+            '_pack_': 1,
+            '_fields_': fields,
+        },
+    )
+    return response_class.from_buffer_copy(buffer)
---
+++ modules/platforms/python/test/test_handshake.py
@@ -0,0 +1,37 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import socket
+
+from ignite_requests import HandshakeRequest, read_response
+
+
+def test_handshake():
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    conn.connect(('localhost', 10900))
+    hs_request = HandshakeRequest()
+    conn.send(hs_request)
+    hs_response = read_response(conn)
+    assert hs_response.hs_code != 0
+
+    # intentionally pass wrong protocol version
+    conn.close()
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    conn.connect(('localhost', 10900))
+    hs_request = HandshakeRequest()
+    hs_request.version_major = 10
+    conn.send(hs_request)
+    hs_response = read_response(conn)
+    assert hs_response.hs_code == 0
