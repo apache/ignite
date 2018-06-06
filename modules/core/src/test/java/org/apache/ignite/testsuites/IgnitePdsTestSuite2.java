f330951aa4fd6f13c2f69c890e02c6620a374293
Dmitriy Govorukhin <dmitriy.govorukhin@gmail.com>
2018-06-06 17:20:18 +0300

    IGNITE-8685 Fixed switch segment record size - Fixes #4130.
    Signed-off-by: Alexey Goncharuk <alexey.goncharuk@gmail.com>
    
    (cherry picked from commit 2a048bd)

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgnitePdsTestSuite2.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgnitePdsTestSuite2.java
@@ -46,6 +46,7 @@ import org.apache.ignite.internal.processors.cache.persistence.db.wal.IgniteWalF
 import org.apache.ignite.internal.processors.cache.persistence.db.wal.IgniteWalFlushFailoverTest;
 import org.apache.ignite.internal.processors.cache.persistence.db.wal.IgniteWalFlushLogOnlySelfTest;
 import org.apache.ignite.internal.processors.cache.persistence.db.wal.IgniteWalHistoryReservationsTest;
+import org.apache.ignite.internal.processors.cache.persistence.db.wal.IgniteWalIteratorSwitchSegmentTest;
 import org.apache.ignite.internal.processors.cache.persistence.db.wal.IgniteWalSerializerVersionTest;
 import org.apache.ignite.internal.processors.cache.persistence.db.wal.WalCompactionTest;
 import org.apache.ignite.internal.processors.cache.persistence.db.wal.crc.IgniteDataIntegrityTests;
