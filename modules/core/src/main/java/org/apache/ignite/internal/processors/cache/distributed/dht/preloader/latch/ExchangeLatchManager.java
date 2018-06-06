6956097ceedd2e3e9edcb627d97e49afb7f0a26d
Pavel Kovalenko <jokserfn@gmail.com>
2018-06-06 15:24:39 +0300

    IGNITE-8482 Skip 2-phase partition release wait in case of activation or dynamic caches start - Fixes #4078.
    Signed-off-by: Alexey Goncharuk <alexey.goncharuk@gmail.com>
    
    (cherry picked from commit 7c565d2)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/latch/ExchangeLatchManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/latch/ExchangeLatchManager.java
@@ -452,11 +452,12 @@ public class ExchangeLatchManager {
                             io.sendToGridTopic(node, GridTopic.TOPIC_EXCHANGE, new LatchAckMessage(id, topVer, true), GridIoPolicy.SYSTEM_POOL);
 
                             if (log.isDebugEnabled())
-                                log.debug("Final ack is ackSent [latch=" + latchId() + ", to=" + node.id() + "]");
+                                log.debug("Final ack has sent [latch=" + latchId() + ", to=" + node.id() + "]");
                         }
-                    } catch (IgniteCheckedException e) {
+                    }
+                    catch (IgniteCheckedException e) {
                         if (log.isDebugEnabled())
-                            log.debug("Unable to send final ack [latch=" + latchId() + ", to=" + node.id() + "]");
+                            log.debug("Failed to send final ack [latch=" + latchId() + ", to=" + node.id() + "]: " + e.getMessage());
                     }
                 }
             });
@@ -500,7 +501,7 @@ public class ExchangeLatchManager {
             int remaining = permits.decrementAndGet();
 
             if (log.isDebugEnabled())
-                log.debug("Count down + [latch=" + latchId() + ", remaining=" + remaining + "]");
+                log.debug("Count down [latch=" + latchId() + ", remaining=" + remaining + "]");
 
             if (remaining == 0)
                 complete();
@@ -563,7 +564,7 @@ public class ExchangeLatchManager {
          */
         private void newCoordinator(ClusterNode coordinator) {
             if (log.isDebugEnabled())
-                log.debug("Coordinator is changed [latch=" + latchId() + ", crd=" + coordinator.id() + "]");
+                log.debug("Coordinator is changed [latch=" + latchId() + ", newCrd=" + coordinator.id() + "]");
 
             synchronized (this) {
                 this.coordinator = coordinator;
@@ -585,11 +586,12 @@ public class ExchangeLatchManager {
                 io.sendToGridTopic(coordinator, GridTopic.TOPIC_EXCHANGE, new LatchAckMessage(id, topVer, false), GridIoPolicy.SYSTEM_POOL);
 
                 if (log.isDebugEnabled())
-                    log.debug("Ack is ackSent + [latch=" + latchId() + ", to=" + coordinator.id() + "]");
-            } catch (IgniteCheckedException e) {
+                    log.debug("Ack has sent [latch=" + latchId() + ", to=" + coordinator.id() + "]");
+            }
+            catch (IgniteCheckedException e) {
                 // Coordinator is unreachable. On coodinator node left discovery event ack will be resent.
                 if (log.isDebugEnabled())
-                    log.debug("Unable to send ack [latch=" + latchId() + ", to=" + coordinator.id() + "]: " + e.getMessage());
+                    log.debug("Failed to send ack [latch=" + latchId() + ", to=" + coordinator.id() + "]: " + e.getMessage());
             }
         }
 
