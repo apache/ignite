d13a7bef47a05e1fc2e2aafd0479916f728285cd
a-polyakov <polyakov.alexandr.alexandrovich@gmail.com>
2018-06-18 19:11:32 +0300

    IGNITE-8601 Add to control.sh utility information about transaction start time - Fixes #4070.
    (cherry picked from commit 4776a1a)

--- modules/core/src/main/java/org/apache/ignite/internal/visor/tx/VisorTxInfo.java
+++ modules/core/src/main/java/org/apache/ignite/internal/visor/tx/VisorTxInfo.java
@@ -20,8 +20,11 @@ package org.apache.ignite.internal.visor.tx;
 import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
-import java.io.Serializable;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Collection;
+import java.util.TimeZone;
 import java.util.UUID;
 import org.apache.ignite.internal.util.typedef.internal.S;
 import org.apache.ignite.internal.util.typedef.internal.U;
@@ -38,7 +41,15 @@ public class VisorTxInfo extends VisorDataTransferObject {
     private static final long serialVersionUID = 0L;
 
     /** */
+    private static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+
+    /** */
     private IgniteUuid xid;
+
+    /**
+     * Transaction start time.
+     */
+    private long startTime;
 
     /** */
     private long duration;
@@ -73,6 +84,7 @@ public class VisorTxInfo extends VisorDataTransferObject {
 
     /**
      * @param xid Xid.
+     * @param startTime Start time of transaction.
      * @param duration Duration.
      * @param isolation Isolation.
      * @param concurrency Concurrency.
@@ -82,10 +94,11 @@ public class VisorTxInfo extends VisorDataTransferObject {
      * @param state State.
      * @param size Size.
      */
-    public VisorTxInfo(IgniteUuid xid, long duration, TransactionIsolation isolation,
+    public VisorTxInfo(IgniteUuid xid, long startTime, long duration, TransactionIsolation isolation,
         TransactionConcurrency concurrency, long timeout, String lb, Collection<UUID> primaryNodes,
         TransactionState state, int size) {
         this.xid = xid;
+        this.startTime = startTime;
         this.duration = duration;
         this.isolation = isolation;
         this.concurrency = concurrency;
@@ -99,6 +112,16 @@ public class VisorTxInfo extends VisorDataTransferObject {
     /** */
     public IgniteUuid getXid() {
         return xid;
+    }
+
+    /** */
+    public long getStartTime() {
+        return startTime;
+    }
+
+    /** */
+    public String getFormattedStartTime() {
+        return dateTimeFormatter.format(LocalDateTime.ofInstant(Instant.ofEpochMilli(startTime), TimeZone.getDefault().toZoneId()));
     }
 
     /** */
@@ -142,6 +165,11 @@ public class VisorTxInfo extends VisorDataTransferObject {
     }
 
     /** {@inheritDoc} */
+    @Override public byte getProtocolVersion() {
+        return V2;
+    }
+
+    /** {@inheritDoc} */
     @Override protected void writeExternalData(ObjectOutput out) throws IOException {
         U.writeGridUuid(out, xid);
         out.writeLong(duration);
@@ -152,10 +180,12 @@ public class VisorTxInfo extends VisorDataTransferObject {
         U.writeCollection(out, primaryNodes);
         U.writeEnum(out, state);
         out.writeInt(size);
+        out.writeLong(startTime);
     }
 
     /** {@inheritDoc} */
-    @Override protected void readExternalData(byte protoVer, ObjectInput in) throws IOException, ClassNotFoundException {
+    @Override protected void readExternalData(byte protoVer,
+        ObjectInput in) throws IOException, ClassNotFoundException {
         xid = U.readGridUuid(in);
         duration = in.readLong();
         isolation = TransactionIsolation.fromOrdinal(in.readByte());
@@ -165,6 +195,7 @@ public class VisorTxInfo extends VisorDataTransferObject {
         primaryNodes = U.readCollection(in);
         state = TransactionState.fromOrdinal(in.readByte());
         size = in.readInt();
+        startTime = protoVer >= V2 ? in.readLong() : 0L;
     }
 
     /** {@inheritDoc} */
