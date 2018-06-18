2cb2cf9c1053a28f636b94936a685344fa29e1b5
Pavel Kovalenko <jokserfn@gmail.com>
2018-06-18 18:55:56 +0300

    IGNITE-8768 WIP

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/GridDhtPartitionsEvictor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/GridDhtPartitionsEvictor.java
@@ -19,6 +19,8 @@ package org.apache.ignite.internal.processors.cache.distributed.dht;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 import org.apache.ignite.IgniteLogger;
+import org.apache.ignite.IgniteSystemProperties;
+import org.apache.ignite.internal.IgniteInternalFuture;
 import org.apache.ignite.internal.processors.cache.CacheGroupContext;
 import org.apache.ignite.internal.processors.cache.GridCacheSharedContext;
 import org.apache.ignite.internal.util.typedef.internal.GPC;
@@ -29,8 +31,11 @@ import org.apache.ignite.internal.util.typedef.internal.U;
  * Class that serves asynchronous partition eviction process.
  */
 public class GridDhtPartitionsEvictor {
-    /** Show eviction progress frequency in ms. */
-    private static final int SHOW_EVICTION_PROGRESS_FREQ_MS = 2 * 60 * 1000; // 2 Minutes.
+    /** Default eviction progress show frequency. */
+    private static final int DEFAULT_SHOW_EVICTION_PROGRESS_FREQ_MS = 2 * 60 * 1000; // 2 Minutes.
+
+    /** Eviction progress frequency property name. */
+    private static final String SHOW_EVICTION_PROGRESS_FREQ = "SHOW_EVICTION_PROGRESS_FREQ";
 
     /** */
     private final GridCacheSharedContext<?, ?> ctx;
@@ -47,6 +52,13 @@ public class GridDhtPartitionsEvictor {
     /** Flag indicates that eviction process is running at the moment, false in other case. */
     private final AtomicBoolean evictionRunning = new AtomicBoolean();
 
+    /** Future for currently running eviction. */
+    private volatile IgniteInternalFuture<Boolean> evictionFuture;
+
+    /** Eviction progress frequency in ms. */
+    private final long evictionProgressFreqMs = IgniteSystemProperties.getLong(SHOW_EVICTION_PROGRESS_FREQ,
+        DEFAULT_SHOW_EVICTION_PROGRESS_FREQ_MS);
+
     /**
      * Constructor.
      *
@@ -61,6 +73,74 @@ public class GridDhtPartitionsEvictor {
         this.log = ctx.logger(getClass());
     }
 
+    private final GPC<Boolean> evictionTask = () -> {
+        boolean locked = true;
+
+        long nextShowProgressTime = U.currentTimeMillis() + evictionProgressFreqMs;
+
+        while (locked || !evictionQueue.isEmpty()) {
+            if (!locked && !evictionRunning.compareAndSet(false, true))
+                return false;
+
+            try {
+                for (GridDhtLocalPartition part : evictionQueue.values()) {
+                    // Show progress of currently evicting partitions.
+                    if (U.currentTimeMillis() >= nextShowProgressTime) {
+                        if (log.isInfoEnabled())
+                            log.info("Eviction in progress [grp=" + grp.cacheOrGroupName()
+                                + ", remainingCnt=" + evictionQueue.size() + "]");
+
+                        nextShowProgressTime = U.currentTimeMillis() + evictionProgressFreqMs;
+                    }
+
+                    try {
+                        boolean success = part.tryClear();
+
+                        if (success) {
+                            evictionQueue.remove(part.id());
+
+                            if (part.state() == GridDhtPartitionState.EVICTED && part.markForDestroy())
+                                part.destroy();
+                        }
+                    }
+                    catch (Throwable ex) {
+                        if (ctx.kernalContext().isStopping()) {
+                            LT.warn(log, ex, "Partition eviction failed (current node is stopping).",
+                                false,
+                                true);
+
+                            evictionQueue.clear();
+
+                            return true;
+                        }
+                        else
+                            LT.error(log, ex, "Partition eviction failed, this can cause grid hang.");
+                    }
+                }
+            }
+            finally {
+                if (!evictionQueue.isEmpty()) {
+                    if (ctx.kernalContext().isStopping()) {
+                        evictionQueue.clear();
+
+                        locked = false;
+                    }
+                    else
+                        locked = true;
+                }
+                else {
+                    boolean res = evictionRunning.compareAndSet(true, false);
+
+                    assert res;
+
+                    locked = false;
+                }
+            }
+        }
+
+        return true;
+    };
+
     /**
      * Adds partition to eviction queue and starts eviction process.
      *
@@ -70,75 +150,20 @@ public class GridDhtPartitionsEvictor {
         evictionQueue.putIfAbsent(part.id(), part);
 
         if (evictionRunning.compareAndSet(false, true)) {
-            ctx.kernalContext().closure().callLocalSafe(new GPC<Boolean>() {
+            evictionFuture = ctx.kernalContext().closure().callLocalSafe(new GPC<Boolean>() {
                 @Override public Boolean call() {
-                    boolean locked = true;
 
-                    long nextShowProgressTime = U.currentTimeMillis() + SHOW_EVICTION_PROGRESS_FREQ_MS;
-
-                    while (locked || !evictionQueue.isEmpty()) {
-                        if (!locked && !evictionRunning.compareAndSet(false, true))
-                            return false;
-
-                        try {
-                            for (GridDhtLocalPartition part : evictionQueue.values()) {
-                                // Show progress of currently evicting partitions.
-                                if (U.currentTimeMillis() >= nextShowProgressTime) {
-                                    if (log.isInfoEnabled())
-                                        log.info("Eviction in progress [grp=" + grp.cacheOrGroupName()
-                                                + ", remainingCnt=" + evictionQueue.size() + "]");
-
-                                    nextShowProgressTime = U.currentTimeMillis() + SHOW_EVICTION_PROGRESS_FREQ_MS;
-                                }
-
-                                try {
-                                    boolean success = part.tryClear();
-
-                                    if (success) {
-                                        evictionQueue.remove(part.id());
-
-                                        if (part.state() == GridDhtPartitionState.EVICTED && part.markForDestroy())
-                                            part.destroy();
-                                    }
-                                }
-                                catch (Throwable ex) {
-                                    if (ctx.kernalContext().isStopping()) {
-                                        LT.warn(log, ex, "Partition eviction failed (current node is stopping).",
-                                                false,
-                                                true);
-
-                                        evictionQueue.clear();
-
-                                        return true;
-                                    }
-                                    else
-                                        LT.error(log, ex, "Partition eviction failed, this can cause grid hang.");
-                                }
-                            }
-                        }
-                        finally {
-                            if (!evictionQueue.isEmpty()) {
-                                if (ctx.kernalContext().isStopping()) {
-                                    evictionQueue.clear();
-
-                                    locked = false;
-                                }
-                                else
-                                    locked = true;
-                            }
-                            else {
-                                boolean res = evictionRunning.compareAndSet(true, false);
-
-                                assert res;
-
-                                locked = false;
-                            }
-                        }
-                    }
-
-                    return true;
                 }
             }, /*system pool*/ true);
         }
     }
+
+    /**
+     *
+     */
+    public void stop() {
+
+    }
+
+
 }
