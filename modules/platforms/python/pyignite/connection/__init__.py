1ad9a720bd9043f2c08c715eef6a54fc8b684152
Dmitry Melnichuk <dmitry.melnichuk@nobitlost.com>
2018-07-02 11:42:31 +1000

    IGNITE-7782 Add parameters to SSL connection. Update autogenerated part of the docs.

--- modules/platforms/python/pyignite/connection/__init__.py
+++ modules/platforms/python/pyignite/connection/__init__.py
@@ -33,19 +33,55 @@ class Connection:
 
     socket = None
 
-    def __init__(self, use_ssl=True):
+    def __init__(
+        self, use_ssl: bool=None, ssl_version=None, ssl_ciphers=None,
+        ssl_cert_requires=ssl.CERT_NONE,
+        ssl_keyfile: str=None, ssl_certfile: str=None,
+    ):
+        """
+        Initialize connection.
+
+        For the use of the last two parameters see
+        https://docs.python.org/3/library/ssl.html#ssl-certificates.
+
+        :param use_ssl: (optional) set to True if Ignite server uses SSL
+         on its binary connector. Defaults to not use SSL,
+        :param ssl_version: (optional) SSL version constant from standard
+         `ssl` module. Defaults to TLS v1.1, as in Ignite 2.5,
+        :param ssl_ciphers: (optional) ciphers to use. If not provided,
+         `ssl` default ciphers are used,
+        :param ssl_cert_requires: (optional) determines how the remote side
+         certificate is treated:
+         * `ssl.CERT_NONE` − remote certificate is ignored (default),
+         * `ssl.CERT_OPTIONAL` − remote certificate will be validated,
+           if provided,
+         * `ssl.CERT_REQUIRED` − valid remote certificate is required,
+        :param ssl_keyfile: (optional) a path to SSL key file to identify
+         local party,
+        :param ssl_certfile: (optional) a path to ssl certificate file
+         to identify local party.
+        """
         _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         if use_ssl:
             _socket = ssl.wrap_socket(
                 _socket,
-                ssl_version=SSL_VERSION,
-                ciphers=SSL_CIPHERS
+                ssl_version=ssl_version or SSL_DEFAULT_VERSION,
+                ciphers=ssl_ciphers or SSL_DEFAULT_CIPHERS,
+                cert_reqs=ssl_cert_requires,
+                keyfile=ssl_keyfile,
+                certfile=ssl_certfile,
             )
         self.socket = _socket
 
     read_response = read_response
 
-    def connect(self, host, port):
+    def connect(self, host: str, port: int):
+        """
+        Connect to the server.
+
+        :param host: Ignite server host,
+        :param port: Ignite server port.
+        """
         if self.socket is not None:
             self.socket.connect((host, port))
 
@@ -68,7 +104,13 @@ class Connection:
                 )
             )
 
-    def send(self, data, flags=None):
+    def send(self, data: bytes, flags=None):
+        """
+        Send data down the socket.
+
+        :param data: bytes to send,
+        :param flags: (optional) OS-specific flags.
+        """
         kwargs = {}
         if flags is not None:
             kwargs['flags'] = flags
@@ -82,7 +124,14 @@ class Connection:
                 raise SocketError('Socket connection broken.')
             total_bytes_sent += bytes_sent
 
-    def recv(self, buffersize, flags=None):
+    def recv(self, buffersize, flags=None) -> bytes:
+        """
+        Receive data from socket.
+
+        :param buffersize: bytes to receive,
+        :param flags: (optional) OS-specific flags,
+        :return: data received.
+        """
         kwargs = {}
         if flags is not None:
             kwargs['flags'] = flags
@@ -100,11 +149,19 @@ class Connection:
         return b''.join(chunks)
 
     def close(self):
+        """
+        Mark socket closed. This is recommended but not required, since
+        sockets are automatically closed when they are garbage-collected.
+        """
         self.socket.close()
         self.socket = None
 
 
 class PrefetchConnection(Connection):
+    """
+    Use this socket wrapper, when you wish to “put back” some data you just
+    receive from the socket.
+    """
     prefetch = None
     conn = None
 
