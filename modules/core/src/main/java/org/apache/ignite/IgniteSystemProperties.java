ee33c63963eaad2547d57d1bff3cb55572bbafa2
Andrey V. Mashenkov <andrey.mashenkov@gmail.com>
2018-07-03 19:19:47 +0300

    IGNITE-8681 Using ExpiryPolicy with persistence causes significant slowdown - Fixes #4285.
    Signed-off-by: Ivan Rakov <irakov@apache.org>
    
    (cherry picked from commit a06c47f)

--- modules/core/src/main/java/org/apache/ignite/IgniteSystemProperties.java
+++ modules/core/src/main/java/org/apache/ignite/IgniteSystemProperties.java
@@ -912,6 +912,13 @@ public final class IgniteSystemProperties {
     public static final String IGNITE_DUMP_THREADS_ON_FAILURE = "IGNITE_DUMP_THREADS_ON_FAILURE";
 
     /**
+     * Throttling timeout in millis which avoid excessive PendingTree access on unwind if there is nothing to clean yet.
+     *
+     * Default is 500 ms.
+     */
+    public static final String IGNITE_UNWIND_THROTTLING_TIMEOUT = "IGNITE_UNWIND_THROTTLING_TIMEOUT";
+
+    /**
      * Enforces singleton.
      */
     private IgniteSystemProperties() {
