6a14a55019f3c87d2980852e21072adb6b432ea1
Andrey Gura <agura@apache.org>
2018-07-09 18:22:21 +0300

    POM versions are updated to 2.7.0-SNAPSHOT

--- modules/platforms/dotnet/Apache.Ignite.AspNet/Properties/AssemblyInfo.cs
+++ modules/platforms/dotnet/Apache.Ignite.AspNet/Properties/AssemblyInfo.cs
@@ -33,9 +33,9 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("13ea96fc-cc83-4164-a7c0-4f30ed797460")]
 
-[assembly: AssemblyVersion("2.6.0.29110")]
-[assembly: AssemblyFileVersion("2.6.0.29110")]
-[assembly: AssemblyInformationalVersion("2.6.0")]
+[assembly: AssemblyVersion("2.7.0.30114")]
+[assembly: AssemblyFileVersion("2.7.0.30114")]
+[assembly: AssemblyInformationalVersion("2.7.0")]
 
 [assembly: CLSCompliant(true)]
 
