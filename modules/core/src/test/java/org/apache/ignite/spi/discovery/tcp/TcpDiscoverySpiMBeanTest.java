8dab486b89eed9091a611fe6ef8a5466ca56e140
Alexey Goncharuk <alexey.goncharuk@gmail.com>
2018-07-18 13:08:42 +0300

    IGNITE-9029 Fixed TcpDiscoverySpiMBeanTest

--- modules/core/src/test/java/org/apache/ignite/spi/discovery/tcp/TcpDiscoverySpiMBeanTest.java
+++ modules/core/src/test/java/org/apache/ignite/spi/discovery/tcp/TcpDiscoverySpiMBeanTest.java
@@ -20,6 +20,7 @@ package org.apache.ignite.spi.discovery.tcp;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.internal.IgniteEx;
 import org.apache.ignite.internal.util.typedef.internal.U;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 import org.apache.ignite.testframework.GridStringLogger;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import javax.management.JMX;
@@ -34,10 +35,14 @@ public class TcpDiscoverySpiMBeanTest extends GridCommonAbstractTest {
     /** */
     private GridStringLogger strLog = new GridStringLogger();
 
+    /** */
+    private static final TcpDiscoveryVmIpFinder IP_FINDER = new TcpDiscoveryVmIpFinder(true);
+
     /** {@inheritDoc} */
     @Override protected IgniteConfiguration getConfiguration(final String igniteInstanceName) throws Exception {
         IgniteConfiguration cfg = super.getConfiguration(igniteInstanceName);
         TcpDiscoverySpi tcpSpi = new TcpDiscoverySpi();
+        tcpSpi.setIpFinder(IP_FINDER);
         cfg.setDiscoverySpi(tcpSpi);
         cfg.setGridLogger(strLog);
 
