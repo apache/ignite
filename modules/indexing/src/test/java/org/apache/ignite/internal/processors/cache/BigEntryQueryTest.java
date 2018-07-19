c257356b8d1f2a197fe34e66da165f7fe4c7d8e6
Alexey Goncharuk <alexey.goncharuk@gmail.com>
2018-07-19 13:21:32 +0300

    IGNITE-8900 Style fixes

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/BigEntryQueryTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/BigEntryQueryTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.internal.processors.cache;
 
 import java.io.Serializable;
@@ -23,14 +40,21 @@ import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.events.EventType;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 
+/**
+ * This is a specific test for IGNITE-8900.
+ */
 public class BigEntryQueryTest extends GridCommonAbstractTest {
-
+    /** */
     public static final String CACHE = "cache";
 
+    /** {@inheritDoc} */
     @Override protected long getTestTimeout() {
         return 60_000;
     }
 
+    /**
+     * @throws Exception if failed.
+     */
     public void testBigEntriesSelect() throws Exception {
         startGrids(2);
 
@@ -86,6 +110,7 @@ public class BigEntryQueryTest extends GridCommonAbstractTest {
         }
     }
 
+    /** {@inheritDoc} */
     @Override protected IgniteConfiguration getConfiguration() throws Exception {
         IgniteConfiguration cfg = super.getConfiguration();
 
@@ -104,12 +129,20 @@ public class BigEntryQueryTest extends GridCommonAbstractTest {
      * Class containing value to be placed into the cache.
      */
     public static class Value implements Serializable {
+        /** */
         final byte[] data;
 
+        /**
+         * @param data Data.
+         */
         public Value(final byte[] data) {
             this.data = data;
         }
 
+        /**
+         * @param bytes Bytes.
+         * @return Value.
+         */
         public static Value of(final byte[] bytes) {
             return new Value(bytes);
         }
