e1c3cc9d30baeaff4e653751775ab39a347b753b
Pavel Kovalenko <jokserfn@gmail.com>
2018-07-24 17:48:57 +0300

    IGNITE-8791 Fixed missed update counter in WAL data record for backup transaction - Fixes #4264.
    Signed-off-by: Alexey Goncharuk <alexey.goncharuk@gmail.com>
    
    (cherry picked from commit 13e2a31)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/serializer/RecordDataV1Serializer.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/serializer/RecordDataV1Serializer.java
@@ -1481,6 +1481,10 @@ public class RecordDataV1Serializer implements RecordDataSerializer {
             CacheObjectContext coCtx = cacheCtx.cacheObjectContext();
 
             KeyCacheObject key = co.toKeyCacheObject(coCtx, keyType, keyBytes);
+
+            if (key.partition() == -1)
+                key.partition(partId);
+
             CacheObject val = valBytes != null ? co.toCacheObject(coCtx, valType, valBytes) : null;
 
             return new DataEntry(
