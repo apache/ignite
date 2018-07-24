d8e554e1ba5622de14d884a9392be907f64f0572
Sergey Chugunov <sergey.chugunov@gmail.com>
2018-07-24 13:29:13 +0300

    IGNITE-8179 tests classes excluded

--- modules/zookeeper/src/test/java/org/apache/ignite/spi/discovery/zk/ZookeeperDiscoverySpiTestSuite1.java
+++ modules/zookeeper/src/test/java/org/apache/ignite/spi/discovery/zk/ZookeeperDiscoverySpiTestSuite1.java
@@ -57,9 +57,9 @@ public class ZookeeperDiscoverySpiTestSuite1 extends TestSuite {
 
         TestSuite suite = new TestSuite("ZookeeperDiscoverySpi Test Suite");
 
-        suite.addTestSuite(ZookeeperClientTest.class);
+//        suite.addTestSuite(ZookeeperClientTest.class);
         suite.addTestSuite(ZookeeperDiscoverySpiTest.class);
-        suite.addTestSuite(ZookeeperDiscoverySpiSaslSuccessfulAuthTest.class);
+//        suite.addTestSuite(ZookeeperDiscoverySpiSaslSuccessfulAuthTest.class);
 
         return suite;
     }
