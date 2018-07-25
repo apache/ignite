f1256094cd1a52a8506516e658eebff8b9745f61
Garus Denis <garus.d.g@gmail.com>
2018-07-25 12:36:41 +0300

    IGNITE-8705 CacheMBStatisticsBeanTest.testClear failed
    Signed-off-by: Anton Vinogradov <av@apache.org>
    
    (cherry picked from commit 0553210)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GatewayProtectedCacheProxy.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GatewayProtectedCacheProxy.java
@@ -1486,6 +1486,18 @@ public class GatewayProtectedCacheProxy<K, V> extends AsyncSupportAdapter<Ignite
         }
     }
 
+    /** {@inheritDoc} */
+    @Override public void clearStatistics() {
+        CacheOperationGate opGate = onEnter();
+
+        try {
+            delegate.clearStatistics();
+        }
+        finally {
+            onLeave(opGate);
+        }
+    }
+
     /**
      * Safely get CacheGateway.
      *
