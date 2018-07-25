4e4c5eb8af2fc756c898c7ad76b57f024097b44e
Anton Dmitriev <dmitrievanthony@gmail.com>
2018-07-25 18:28:28 +0300

    IGNITE-9055 Add required licence headers.

--- modules/tensorflow/src/main/resources/ignite-tf.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
-java -Xms4G -Xmx4G -DIGNITE_QUIET=false -cp "$SCRIPT_PATH/lib/*" org.apache.ignite.tensorflow.submitter.JobSubmitter "$@"
