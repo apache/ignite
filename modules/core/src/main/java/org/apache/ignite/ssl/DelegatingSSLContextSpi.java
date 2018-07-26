5c0c10d57f3e10b5ad734d172c90070cc73152ff
mcherkasov <mcherkasov@gridgain.com>
2018-07-26 20:03:49 +0300

    Fixed code style.

--- modules/core/src/main/java/org/apache/ignite/ssl/DelegatingSSLContextSpi.java
+++ modules/core/src/main/java/org/apache/ignite/ssl/DelegatingSSLContextSpi.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.ssl;
 
 import java.security.KeyManagementException;
@@ -12,69 +29,75 @@ import javax.net.ssl.SSLSessionContext;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 
+/** */
 class DelegatingSSLContextSpi extends SSLContextSpi {
 
+    /** */
     private final SSLContext delegate;
 
+    /** */
     private final SSLParameters parameters;
 
-    DelegatingSSLContextSpi(SSLContext delegate,
-        SSLParameters parameters) {
+    /** */
+    DelegatingSSLContextSpi(SSLContext delegate, SSLParameters parameters) {
         this.delegate = delegate;
         this.parameters = parameters;
     }
 
-    @Override
-    protected void engineInit(KeyManager[] keyManagers,
-        TrustManager[] trustManagers, SecureRandom secureRandom)
-        throws KeyManagementException {
+    /** {@inheritDoc} */
+    @Override protected void engineInit(KeyManager[] keyManagers, TrustManager[] trustManagers,
+        SecureRandom secureRandom) throws KeyManagementException {
         delegate.init(keyManagers, trustManagers, secureRandom);
     }
 
-    @Override
-    protected SSLSocketFactory engineGetSocketFactory() {
+    /** {@inheritDoc} */
+    @Override protected SSLSocketFactory engineGetSocketFactory() {
         return new SSLSocketFactoryWrapper(delegate.getSocketFactory(), parameters);
     }
 
-    @Override
-    protected SSLServerSocketFactory engineGetServerSocketFactory() {
+    /** {@inheritDoc} */
+    @Override protected SSLServerSocketFactory engineGetServerSocketFactory() {
         return new SSLServerSocketFactoryWrapper(delegate.getServerSocketFactory(),
             parameters);
     }
 
-    @Override
-    protected SSLEngine engineCreateSSLEngine() {
+    /** {@inheritDoc} */
+    @Override protected SSLEngine engineCreateSSLEngine() {
         final SSLEngine engine = delegate.createSSLEngine();
+
         if (parameters != null)
             engine.setSSLParameters(parameters);
+
         return engine;
     }
 
-    @Override
-    protected SSLEngine engineCreateSSLEngine(String s, int i) {
+    /** {@inheritDoc} */
+    @Override protected SSLEngine engineCreateSSLEngine(String s, int i) {
         final SSLEngine engine = delegate.createSSLEngine();
+
         if (parameters != null)
             engine.setSSLParameters(parameters);
+
         return engine;
     }
 
-    @Override
-    protected SSLSessionContext engineGetServerSessionContext() {
+    /** {@inheritDoc} */
+    @Override protected SSLSessionContext engineGetServerSessionContext() {
         return delegate.getServerSessionContext();
     }
 
-    @Override
-    protected SSLSessionContext engineGetClientSessionContext() {
+    /** {@inheritDoc} */
+    @Override protected SSLSessionContext engineGetClientSessionContext() {
         return delegate.getClientSessionContext();
     }
 
-    @Override
-    protected SSLParameters engineGetDefaultSSLParameters() {
+    /** {@inheritDoc} */
+    @Override protected SSLParameters engineGetDefaultSSLParameters() {
         return delegate.getDefaultSSLParameters();
     }
 
-    @Override
-    protected SSLParameters engineGetSupportedSSLParameters() {
+    /** {@inheritDoc} */
+    @Override protected SSLParameters engineGetSupportedSSLParameters() {
         return delegate.getSupportedSSLParameters();
     }
 }
