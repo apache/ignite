e610137e5ec2740cd4d5c0fddd277302767f6cce
Dmitriy Shabalin <dmitriyff@gmail.com>
2018-07-27 16:38:54 +0700

    WC-585 Wip

--- modules/web-console/frontend/app/components/form-field/index.js
+++ modules/web-console/frontend/app/components/form-field/index.js
@@ -20,7 +20,11 @@ import './style.scss';
 import {directive as showValidationError} from './showValidationError.directive';
 import {directive as copyInputValue} from './copyInputValueButton.directive';
 
+import { default as formFieldSize } from './components/form-field-size';
+
 export default angular
-    .module('ignite-console.form-field', [])
+    .module('ignite-console.form-field', [
+        formFieldSize.name
+    ])
     .directive('ngModel', showValidationError)
     .directive('copyInputValueButton', copyInputValue);
