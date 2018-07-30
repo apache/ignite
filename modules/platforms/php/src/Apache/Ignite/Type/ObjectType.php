594778a28ab8ec13fbdb4cbc7907b7909497a3f5
alexey-nbl <alexey.kosenchuk@nobitlost.com>
2018-07-30 19:33:37 +0300

    Update ObjectType.php

--- modules/platforms/php/src/Apache/Ignite/Type/ObjectType.php
+++ modules/platforms/php/src/Apache/Ignite/Type/ObjectType.php
@@ -21,16 +21,16 @@ namespace Apache\Ignite\Type;
 /** 
  * Base class representing a type of Ignite object.
  * 
- * The class has no public constructor. Only subclasses may be instantiated.
+ * The class is abstract and has no public constructor. Only subclasses may be instantiated.
  *
  * There are two groups of Ignite object types:
  *
- * - Primitive (simple) types. To fully describe such a type it is enough to specify
- * Ignite type code @ref PrimitiveTypeCodes only.
+ * - Primitive (simple) types. To fully describe such a type:
+ * it is enough to specify Ignite type code @ref PrimitiveTypeCodes only.
  *
- * - Non-primitive (composite) types. To fully describe such a type
- * Ignite type code @ref CompositeTypeCodes with additional information should be specified.
- * Eg. a kind of map or a kind of collection.
+ * - Non-primitive (composite) types. To fully describe such a type:
+ * Ignite type code @ref CompositeTypeCodes with additional information
+ * (eg. a kind of map or a kind of collection) should be specified.
  *
  * This class helps the Ignite client to make a mapping between PHP types
  * and types used by Ignite.
@@ -41,43 +41,74 @@ namespace Apache\Ignite\Type;
  *
  * ----------------------------------------------------------------------------
  *
- * DEFAULT MAPPING FROM PHP type TO Ignite type code.
+ * COMMENTS TO ALL TABLES:
  *
- * This mapping is used when an application does not explicitly specify an Ignite type
- * for a field and is writing data to that field.
+ * PHP type
+ * ----------------
+ * It is a PHP primitive or a PHP object: http://php.net/manual/en/language.types.intro.php
+ * Plus additional types:
+ *   - Ds/Set, Ds/Map - ???
+ *   - Date, Time, Timestamp, ???, BinaryObject - are PHP classes introduced by the Ignite client.
  *
- * <pre>
- * | PHP type                  | Ignite type code      |
- * | ------------------------- | ----------------------|
- * | boolean                   | BOOLEAN               |
- * | integer                   | INTEGER               |
- * | float                     | DOUBLE                |
- * | string                    | STRING                |
- * | Date                      | DATE                  |
- * | Time                      | TIME                  |
- * | Timestamp                 | TIMESTAMP             |
- * | array of boolean          | BOOLEAN_ARRAY / MAP   |
- * | array of integer          | INTEGER_ARRAY / MAP   |
- * | array of float            | DOUBLE_ARRAY / MAP    |
- * | array of string           | STRING_ARRAY / MAP    |
- * | array of Date             | DATE_ARRAY / MAP      |
- * | array of Time             | TIME_ARRAY / MAP      |
- * | array of Timestamp        | TIMESTAMP_ARRAY / MAP |
- * | Ds/Set                    | COLLECTION (HASH_SET) |
- * | Ds/Map                    | MAP (HASH_MAP)        |
- * </pre>
- *
- * Type of an array content is determined by the type of the first element of the array.
- * Empty array has no default mapping.
- *
- * All other PHP types have no default mapping.
+ * Ignite type code
+ * ----------------
+ * It is a type code of Ignite primitive type (@ref PrimitiveTypeCodes)
+ * or Ignite composite type (@ref CompositeTypeCodes).
  *
  * ----------------------------------------------------------------------------
  *
- * DEFAULT MAPPING FROM Ignite type code TO PHP type.
+ * DEFAULT MAPPING FROM PHP type TO Ignite type code:
  *
  * This mapping is used when an application does not explicitly specify an Ignite type
- * for a field and is reading data from that field.
+ * for a field and it is writing data to that field.
+ *
+ * <pre>
+ * | PHP type                      | Ignite type code      |
+ * | ----------------------------- | ----------------------|
+ * | boolean                       | BOOLEAN               |
+ * | integer                       | INTEGER               |
+ * | float                         | DOUBLE                |
+ * | string                        | STRING                |
+ * | Date                          | DATE                  |
+ * | Time                          | TIME                  |
+ * | Timestamp                     | TIMESTAMP             |
+ * | ???                           | ENUM                  |
+ * | ???                           | DECIMAL               |
+ * | BinaryObject ???              | COMPLEX_OBJECT        |
+ * | any other PHP Class ???       | COMPLEX_OBJECT        |
+ * | associative array of          |                       |
+ * |    any PHP supported type ??? | MAP (HASH_MAP)        |
+ * | indexed array of boolean      | BOOLEAN_ARRAY         |
+ * | indexed array of integer      | INTEGER_ARRAY         |
+ * | indexed array of float        | DOUBLE_ARRAY          |
+ * | indexed array of string       | STRING_ARRAY          |
+ * | indexed array of Date         | DATE_ARRAY            |
+ * | indexed array of Time         | TIME_ARRAY            |
+ * | indexed array of Timestamp    | TIMESTAMP_ARRAY       |
+ * | indexed array of ???          | ENUM_ARRAY            |
+ * | indexed array of ???          | DECIMAL_ARRAY         |
+ * | indexed array of BinaryObject | OBJECT_ARRAY          |
+ * | indexed array of              |                       |
+ * |       any other PHP Class ??? | OBJECT_ARRAY          |
+ * | Ds/Set                        | COLLECTION (HASH_SET) |
+ * | Ds/Map                        | MAP (HASH_MAP)        |
+ * </pre>
+ *
+ * All other PHP types have no default mapping.
+ *
+ * PHP Class - ???
+ * PHP supported type - is any PHP type mentioned in the table.
+ *
+ * Associative and indexed PHP arrays: http://php.net/manual/en/language.types.array.php
+ * Type of an array's value is determined by the value's type in the first element of the array.
+ * Empty array has no default mapping.
+ *
+ * ----------------------------------------------------------------------------
+ *
+ * DEFAULT MAPPING FROM Ignite type code TO PHP type:
+ *
+ * This mapping is used when an application does not explicitly specify an Ignite type
+ * for a field and it is reading data from that field.
  *
  * <pre>
  * | Ignite type code             | PHP type                              |
@@ -88,24 +119,32 @@ namespace Apache\Ignite\Type;
  * | LONG                         | integer/float                         |
  * | FLOAT                        | float                                 |
  * | DOUBLE                       | float                                 |
+ * | DECIMAL                      | ???                                   |
  * | BOOLEAN                      | boolean                               |
  * | STRING                       | string                                |
  * | CHAR                         | string (one character)                |
+ * | UUID                         | ???                                   |
  * | DATE                         | Date                                  |
  * | TIME                         | Time                                  |
  * | TIMESTAMP                    | Timestamp                             |
+ * | ENUM                         | ???                                   |
+ * | COMPLEX_OBJECT               | BinaryObject ???                      |
  * | BYTE_ARRAY                   | array of integer                      |
  * | SHORT_ARRAY                  | array of integer                      |
  * | INTEGER_ARRAY                | array of integer                      |
- * | LONG_ARRAY                   | array of integer/float                |
+ * | LONG_ARRAY                   | array of integer/float ???            |
  * | FLOAT_ARRAY                  | array of float                        |
  * | DOUBLE_ARRAY                 | array of float                        |
+ * | DECIMAL_ARRAY                | ???                                   |
  * | BOOLEAN_ARRAY                | array of boolean                      |
  * | STRING_ARRAY                 | array of string                       |
  * | CHAR_ARRAY                   | array of string (one character)       |
+ * | UUID_ARRAY                   | ???                                   |
  * | DATE_ARRAY                   | array of Date                         |
  * | TIME_ARRAY                   | array of Time                         |
  * | TIMESTAMP_ARRAY              | array of Timestamp                    |
+ * | ENUM_ARRAY                   | ???                                   |
+ * | OBJECT_ARRAY                 | ???                                   |
  * | COLLECTION (USER_COL)        | array                                 |
  * | COLLECTION (ARR_LIST)        | array                                 |
  * | COLLECTION (LINKED_LIST)     | array                                 |
@@ -120,14 +159,52 @@ namespace Apache\Ignite\Type;
  *
  * ----------------------------------------------------------------------------
  *
- * COMMENTS TO ALL TABLES
+ * RETURNED PHP types WHEN READING DATA OF THE SPECIFIED Ignite type code:
  *
- * PHP type - is a PHP primitive or a PHP object
- * (http://php.net/manual/en/language.types.intro.php)
+ * When an application explicitly specifies an Ignite type for a field
+ * and it is reading data from that field, the following PHP types
+ * are returned for every concrete Ignite type code -
+ * SEE THE PREVIOUS TABLE with the following additional comments:
  *
- * Ignite type code - is the type code of an Ignite primitive type (@ref PrimitiveTypeCodes)
- * or an Ignite composite type (@ref CompositeTypeCodes).
+ * TO UPDATE ???
  *
+ * - for COMPLEX_OBJECT the Ignite Client returns a JavaScript Object
+ * which is defined by the specified {@link ComplexObjectType}.
+ *
+ * - the returned Map for MAP is defined by the specified {@link MapObjectType}.
+ *
+ * - the returned Set or Array for COLLECTION is defined by the specified {@link CollectionObjectType}.
+ *
+ * - the returned Array for OBJECT_ARRAY is defined by the specified {@link ObjectArrayType}.
+ *
+ * - NULL cannot be specified as a type of a field but JavaScript null may be returned
+ * as a value of a field.
+ *
+ * ----------------------------------------------------------------------------
+ *
+ * ALLOWED PHP types WHEN WRITING DATA OF THE SPECIFIED Ignite type code:
+ *
+ * When an application explicitly specifies an Ignite type for a field
+ * and it is writing data to that field, the following PHP types
+ * are allowed for every concrete Ignite type code -
+ * SEE THE PREVIOUS TABLE with the following additional comments:
+ *
+ * TO UPDATE ???
+ *
+ * - for COMPLEX_OBJECT the Ignite Client allows a JavaScript Object
+ * which is defined by the specified {@link ComplexObjectType}.
+ *
+ * - the allowed Map for MAP is defined by the specified {@link MapObjectType}.
+ *
+ * - the allowed Set or Array for COLLECTION is defined by the specified {@link CollectionObjectType}.
+ *
+ * - the allowed Array for OBJECT_ARRAY is defined by the specified {@link ObjectArrayType}.
+ *
+ * - NULL cannot be specified as a type of a field but JavaScript null is allowed
+ * as value of a field (but not as a key/value in a cache) or as a value of Array/Set/Map element
+ * for all Ignite types, except BYTE, SHORT, INTEGER, LONG, FLOAT, DOUBLE, CHAR, BOOLEAN.
+ *
+ * - for all *_ARRAY Ignite types an empty JavaScript Array is allowed.
  * ----------------------------------------------------------------------------
  * 
  */
@@ -333,6 +410,7 @@ abstract class ObjectType
     private $typeCode;
     
     /**
+     * The class is abstract and has no public constructor. Only subclasses may be instantiated.
      * 
      * @param int $typeCode
      */
@@ -342,8 +420,9 @@ abstract class ObjectType
     }
     
     /**
+     * Gets Ignite type code of this Ignite object type.
      * 
-     * @return int
+     * @return int Ignite type code
      */
     public function getTypeCode(): int
     {
