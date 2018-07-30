0930b09fd5f3a6b1fe652200db12e72d98181005
Maxim Muzafarov <maxmuzaf@gmail.com>
2018-07-30 21:37:18 +0300

    IGNITE-7165: review changes 5

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPreloader.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPreloader.java
@@ -433,12 +433,12 @@ public class GridDhtPreloader extends GridCachePreloaderAdapter {
     /** {@inheritDoc} */
     @Override public Runnable addAssignments(
         GridDhtPreloaderAssignments assignments,
-        boolean forcePreload,
+        boolean forceRebalance,
         long rebalanceId,
         Runnable next,
         @Nullable GridCompoundFuture<Boolean, Boolean> forcedRebFut
     ) {
-        return demander.addAssignments(assignments, forcePreload, rebalanceId, next, forcedRebFut);
+        return demander.addAssignments(assignments, forceRebalance, rebalanceId, next, forcedRebFut);
     }
 
     /**
