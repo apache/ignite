68e8137ced1ff8a2d1abbae8c8ac351b132079a3
Alexander Kalinin <verbalab@yandex.ru>
2018-07-31 10:14:10 +0700

    IGNITE-9103 Web Console: Fixed select menu component with items that contains html.

--- modules/web-console/frontend/app/components/bs-select-menu/strip.filter.js
+++ modules/web-console/frontend/app/components/bs-select-menu/strip.filter.js
@@ -15,14 +15,8 @@
  * limitations under the License.
  */
 
-import angular from 'angular';
-
-import directive from './directive';
-import {default as transcludeToBody} from './transcludeToBody.directive';
-import stripFilter from './strip.filter';
-
-export default angular
-    .module('ignite-console.bs-select-menu', [])
-    .directive('bssmTranscludeToBody', transcludeToBody)
-    .directive('bsSelectMenu', directive)
-    .filter('bsSelectStrip', stripFilter);
+export default function() {
+    return function(val) {
+        return val.replace(/(<\/?\w+>)/igm, '');
+    };
+}
