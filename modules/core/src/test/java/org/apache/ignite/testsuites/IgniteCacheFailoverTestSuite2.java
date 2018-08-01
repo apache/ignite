7e63db291698d4feca1ad5c3b9120a2c49490e83
sboikov <sboikov@apache.org>
2018-08-01 09:33:26 +0300

    ignite-9142

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheFailoverTestSuite2.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheFailoverTestSuite2.java
@@ -44,25 +44,10 @@ public class IgniteCacheFailoverTestSuite2 {
     public static TestSuite suite() throws Exception {
         TestSuite suite = new TestSuite("Cache Failover Test Suite2");
 
-        suite.addTestSuite(GridCachePartitionedTxSalvageSelfTest.class);
-        suite.addTestSuite(CacheGetFromJobTest.class);
-
-        suite.addTestSuite(GridCacheAtomicFailoverSelfTest.class);
-        suite.addTestSuite(GridCacheAtomicReplicatedFailoverSelfTest.class);
-
-        suite.addTestSuite(GridCachePartitionedFailoverSelfTest.class);
-        suite.addTestSuite(GridCacheColocatedFailoverSelfTest.class);
-        suite.addTestSuite(GridCacheReplicatedFailoverSelfTest.class);
-
-        suite.addTestSuite(IgniteCacheCrossCacheTxFailoverTest.class);
-
-        suite.addTestSuite(CacheAsyncOperationsFailoverAtomicTest.class);
-        suite.addTestSuite(CacheAsyncOperationsFailoverTxTest.class);
-
-        suite.addTestSuite(CachePutAllFailoverAtomicTest.class);
-        suite.addTestSuite(CachePutAllFailoverTxTest.class);
-        suite.addTestSuite(IgniteChangingBaselineDownCachePutAllFailoverTest.class);
-        suite.addTestSuite(IgniteChangingBaselineUpCachePutAllFailoverTest.class);
+        for (int i = 0; i < 10; i++) {
+            suite.addTestSuite(CacheAsyncOperationsFailoverAtomicTest.class);
+            suite.addTestSuite(CacheAsyncOperationsFailoverTxTest.class);
+        }
 
         return suite;
     }
