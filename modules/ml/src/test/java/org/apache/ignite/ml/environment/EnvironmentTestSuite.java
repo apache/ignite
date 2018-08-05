36b6a6bd6aca99ff7af6826dbf8188a77b8b5287
Oleg Ignatenko <oignatenko@gridgain.com>
2018-08-05 17:45:10 +0300

    IGNITE-9146 Analyse and improve code coverage in ML module - corrected typo in javadoc -- verified with diffs overview

--- modules/ml/src/test/java/org/apache/ignite/ml/environment/EnvironmentTestSuite.java
+++ modules/ml/src/test/java/org/apache/ignite/ml/environment/EnvironmentTestSuite.java
@@ -25,6 +25,7 @@ import org.junit.runners.Suite;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+    LearningEnvironmentBuilderTest.class,
     LearningEnvironmentTest.class
 })
 public class EnvironmentTestSuite {
