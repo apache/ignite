514fab956bdbe08bc2c2198587f16b2067de3c81
Ivan Daschinskiy <ivandasch@gmail.com>
2018-08-07 18:14:06 +0300

    IGNITE-9149: Test extended.

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/index/H2DynamicColumnsAbstractBasicSelfTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/index/H2DynamicColumnsAbstractBasicSelfTest.java
@@ -21,6 +21,7 @@ import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 
 import org.apache.ignite.IgniteCache;
@@ -293,23 +294,34 @@ public abstract class H2DynamicColumnsAbstractBasicSelfTest extends DynamicColum
         CacheConfiguration<Integer, Object> ccfg = defaultCacheConfiguration().setName("GuidTest")
                 .setIndexedTypes(Integer.class, GuidTest.class);
 
+        Random rnd = new Random();
+
         IgniteCache<Integer, Object> cache = ignite(nodeIndex()).getOrCreateCache(ccfg);
 
         run(cache, "ALTER TABLE \"GuidTest\".GuidTest ADD COLUMN GUID UUID");
+        run(cache, "ALTER TABLE \"GuidTest\".GuidTest ADD COLUMN DATA BINARY(128)");
 
         doSleep(500);
 
-        QueryField c = c("GUID", Object.class.getName());
+        QueryField c1 = c("GUID", Object.class.getName());
 
-        checkTableState("GuidTest", "GUIDTEST", c);
+        QueryField c2 = c("DATA", byte[].class.getName());
+
+        checkTableState("GuidTest", "GUIDTEST", c1, c2);
 
         UUID guid1 = UUID.randomUUID();
         UUID guid2 = UUID.randomUUID();
 
-        run(cache, "INSERT INTO \"GuidTest\".GuidTest (_key, id, guid) values " +
-                "(1, 1, '" + guid1.toString() + "')");
+        // Populate random data for BINARY field.
+        byte[] data1 = new byte[128];
+        rnd.nextBytes(data1);
+        byte[] data2 = new byte[128];
+        rnd.nextBytes(data2);
 
-        cache.put(2, new GuidTest(2, guid2));
+        run(cache, "INSERT INTO \"GuidTest\".GuidTest (_key, id, guid, data) values " +
+                "(1, 1, ?, ?)", guid1.toString(), data1);
+
+        cache.put(2, new GuidTest(2, guid2, data2));
 
         List<List<?>> res = run(cache, "select _key, id, guid from \"GuidTest\".GuidTest order by id");
 
@@ -326,8 +338,8 @@ public abstract class H2DynamicColumnsAbstractBasicSelfTest extends DynamicColum
             BinaryObject val1 = (BinaryObject)cache.withKeepBinary().get(1);
             BinaryObject val2 = (BinaryObject)cache.withKeepBinary().get(2);
 
-            assertEquals(guid1, (UUID)val1.field("guid"));
-            assertEquals(guid2, (UUID)val2.field("guid"));
+            assertEquals(guid1, val1.field("guid"));
+            assertEquals(guid2, val2.field("guid"));
         }
 
         cache.destroy();
@@ -832,13 +844,18 @@ public abstract class H2DynamicColumnsAbstractBasicSelfTest extends DynamicColum
         /** */
         private UUID guid;
 
+        /** */
+        private byte[] data;
+
         /**
-         * @param id Id.
+         * @param id   Id.
          * @param guid Guid.
+         * @param data Data.
          */
-        public GuidTest(int id, UUID guid) {
+        public GuidTest(int id, UUID guid, byte[] data) {
             this.id = id;
             this.guid = guid;
+            this.data = data;
         }
 
         /**
@@ -853,6 +870,13 @@ public abstract class H2DynamicColumnsAbstractBasicSelfTest extends DynamicColum
          */
         public UUID guid() {
             return guid;
+        }
+
+        /**
+         * @return Data.
+         */
+        public byte[] data() {
+            return data;
         }
     }
 }
