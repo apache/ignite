39b0b9f7e6194e6babe96e8cf429b7bbea48e5f3
Ivan Daschinskiy <ivandasch@gmail.com>
2018-08-07 18:32:00 +0300

    IGNITE-9183: Code review reaction.

--- modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/ddl/DdlStatementsProcessor.java
+++ modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/ddl/DdlStatementsProcessor.java
@@ -26,7 +26,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
-
 import org.apache.ignite.IgniteCheckedException;
 import org.apache.ignite.IgniteCluster;
 import org.apache.ignite.cache.QueryEntity;
