b7de9734a8c3710da828d2991136f98f80ebe349
Oleg Ignatenko <oignatenko@gridgain.com>
2018-08-08 16:13:30 +0300

    IGNITE-9146: Analyse and improve code coverage in ML module
    this closes #4489

--- modules/ml/src/test/java/org/apache/ignite/ml/environment/EnvironmentTestSuite.java
+++ modules/ml/src/test/java/org/apache/ignite/ml/environment/EnvironmentTestSuite.java
@@ -15,31 +15,18 @@
  * limitations under the License.
  */
 
-package org.apache.ignite.ml.math.exceptions;
+package org.apache.ignite.ml.environment;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
 /**
- * This class is based on the corresponding class from Apache Common Math lib.
- * Base class for arithmetic exceptions.
+ * Test suite for all tests located in org.apache.ignite.ml.trees package.
  */
-public class MathArithmeticException extends MathRuntimeException {
-    /** Serializable version Id. */
-    private static final long serialVersionUID = -6024911025449780478L;
-
-    /**
-     * Default constructor.
-     */
-    public MathArithmeticException() {
-        this("Arithmetic exception.");
-    }
-
-    /**
-     * Constructor with a specific message.
-     *
-     * @param format Message pattern providing the specific context of the error.
-     * @param args Arguments.
-     */
-    public MathArithmeticException(String format, Object... args) {
-        super(format, args);
-    }
-
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    LearningEnvironmentBuilderTest.class,
+    LearningEnvironmentTest.class
+})
+public class EnvironmentTestSuite {
 }
