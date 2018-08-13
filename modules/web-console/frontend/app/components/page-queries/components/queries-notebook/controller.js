28db35f8b826fcc84d9db1408b85c2f2819f39b3
Dmitriy Shabalin <dmitriyff@gmail.com>
2018-08-13 17:51:32 +0700

    WC-585 Fix labels

--- modules/web-console/frontend/app/components/page-queries/components/queries-notebook/controller.js
+++ modules/web-console/frontend/app/components/page-queries/components/queries-notebook/controller.js
@@ -285,17 +285,17 @@ export class NotebookCtrl {
 
         $scope.pageSizes = [50, 100, 200, 400, 800, 1000];
         $scope.pageSizesOptions = [{
-            value: 50, label: 50
+            value: 50, label: '50'
         }, {
-            value: 100, label: 100
+            value: 100, label: '100'
         }, {
-            value: 200, label: 200
+            value: 200, label: '200'
         }, {
-            value: 400, label: 400
+            value: 400, label: '400'
         }, {
-            value: 800, label: 800
+            value: 800, label: '800'
         }, {
-            value: 1000, label: 1000
+            value: 1000, label: '1000'
         }];
         $scope.maxPages = [
             {label: 'Unlimited', value: 0},
