c3fc9964a28bd20770997ffe01ceb381f76f4169
Aleksei Scherbakov <alexey.scherbakoff@gmail.com>
2018-08-21 12:01:19 +0300

    IGNITE-7339 Fixed partition eviction after restore from storage - Fixes #4138.
    Signed-off-by: Alexey Goncharuk <alexey.goncharuk@gmail.com>
    (cherry picked from commit 570ee960a337695272cf6a18453f1b68a8d9182e)

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheTestSuite7.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheTestSuite7.java
@@ -33,6 +33,7 @@ import org.apache.ignite.internal.processors.cache.WalModeChangeAdvancedSelfTest
 import org.apache.ignite.internal.processors.cache.WalModeChangeCoordinatorNotAffinityNodeSelfTest;
 import org.apache.ignite.internal.processors.cache.WalModeChangeSelfTest;
 import org.apache.ignite.internal.processors.cache.distributed.Cache64kPartitionsTest;
+import org.apache.ignite.internal.processors.cache.distributed.CacheRentingStateRepairTest;
 import org.apache.ignite.internal.processors.cache.distributed.CacheDataLossOnPartitionMoveTest;
 import org.apache.ignite.internal.processors.cache.distributed.dht.IgniteCacheStartWithLoadTest;
 import org.apache.ignite.internal.processors.cache.distributed.rebalancing.GridCacheRebalancingPartitionCountersTest;
@@ -94,6 +95,8 @@ public class IgniteCacheTestSuite7 extends TestSuite {
         suite.addTestSuite(IgniteDynamicCacheStartFailWithPersistenceTest.class);
 
         suite.addTestSuite(TxWithSmallTimeoutAndContentionOneKeyTest.class);
+
+        suite.addTestSuite(CacheRentingStateRepairTest.class);
 
         suite.addTestSuite(TransactionIntegrityWithPrimaryIndexCorruptionTest.class);
         suite.addTestSuite(CacheDataLossOnPartitionMoveTest.class);
