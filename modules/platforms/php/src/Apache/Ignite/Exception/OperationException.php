a285653d57311f74cdeca9e4bb172fc9c41f187c
alexey-nbl <alexey.kosenchuk@nobitlost.com>
2018-08-21 18:18:58 +0300

    Update OperationException.php

--- modules/platforms/php/src/Apache/Ignite/Exception/OperationException.php
+++ modules/platforms/php/src/Apache/Ignite/Exception/OperationException.php
@@ -19,7 +19,7 @@
 namespace Apache\Ignite\Exception;
 
 /**
- * Class representing an exception thrown when Ignite server returns error for the requested operation.
+ * Ignite server returns error for the requested operation.
  */
 class OperationException extends ClientException
 {
