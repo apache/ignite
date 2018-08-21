a12b8daaec037247620df79b9ec90b6e2023f783
Denis Mekhanikov <dmekhanikov@gmail.com>
2018-08-21 19:27:52 +0300

    Calculate multicast usage statistics per suite

--- modules/core/src/test/java/org/apache/ignite/MulticastSeeker.java
+++ modules/core/src/test/java/org/apache/ignite/MulticastSeeker.java
@@ -3,7 +3,14 @@ package org.apache.ignite;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import junit.framework.Test;
+import junit.framework.TestSuite;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.spi.discovery.DiscoverySpi;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
@@ -12,24 +19,75 @@ import org.apache.ignite.testframework.junits.GridAbstractTest;
 import org.reflections.Reflections;
 
 public class MulticastSeeker {
+    private final Reflections reflections = new Reflections("org.apache.ignite");
+
     public static void main(String[] args) {
-        Reflections reflections = new Reflections("org.apache.ignite");
+        new MulticastSeeker().doMain();
+    }
 
-        Set<Class<? extends GridAbstractTest>> testClasses = reflections.getSubTypesOf(GridAbstractTest.class);
+    private void doMain() {
+        Set<String> multiTestNames = collectMulticastTests();
 
-        for (Class<? extends GridAbstractTest> cls : testClasses) {
-            if (!Modifier.isAbstract(cls.getModifiers()) && isMulticast(cls))
-                System.out.println(cls.getName());
+        List<Class<? extends TestSuite>> suites =
+            new ArrayList<>(reflections.getSubTypesOf(TestSuite.class));
+
+        suites.sort(Comparator.comparing(Class::getName));
+
+        for (Class<? extends TestSuite> suiteCls : suites) {
+            List<String> suiteMultiTests = new ArrayList<>();
+
+            try {
+                TestSuite suite = invokeMethod(suiteCls, null, "suite");
+
+                for (int i = 0; i < suite.testCount(); i++) {
+                    Test test = suite.testAt(i);
+
+                    if (test instanceof TestSuite) {
+                        TestSuite childSuite = (TestSuite)test;
+
+                        if (multiTestNames.contains(childSuite.getName()))
+                            suiteMultiTests.add(childSuite.getName());
+                    }
+                    else
+                        System.err.println(suite.getName() + "[" + i + "] is not a TestSuite");
+                }
+
+                Collections.sort(suiteMultiTests);
+
+                System.out.println(suiteCls.getName() + ": " + suiteMultiTests.size() + " tests");
+
+                for (String testName : suiteMultiTests)
+                    System.out.println("\t" + testName);
+
+                System.out.println();
+            }
+            catch (ReflectiveOperationException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    private static boolean isMulticast(Class<? extends GridAbstractTest> cls) {
+    private Set<String> collectMulticastTests() {
+        Set<Class<? extends GridAbstractTest>> testClasses = reflections.getSubTypesOf(GridAbstractTest.class);
+
+        Set<String> names = new HashSet<>();
+
+        for (Class<? extends GridAbstractTest> cls : testClasses) {
+            if (!Modifier.isAbstract(cls.getModifiers()) && isMulticast(cls))
+                names.add(cls.getName());
+        }
+
+        return names;
+    }
+
+    private boolean isMulticast(Class<? extends GridAbstractTest> cls) {
         try {
             Constructor<? extends GridAbstractTest> ctor = cls.getConstructor();
 
             GridAbstractTest testInstance = ctor.newInstance();
 
-            IgniteConfiguration igniteCfg = invokeMethod(testInstance, "getConfiguration", "server");
+            IgniteConfiguration igniteCfg =
+                invokeMethod(cls, testInstance, "getConfiguration", "server");
 
             DiscoverySpi disco = igniteCfg.getDiscoverySpi();
             if (disco instanceof TcpDiscoverySpi)
@@ -38,14 +96,11 @@ public class MulticastSeeker {
                 return false;
         }
         catch (Exception e) {
-            // e.printStackTrace();
             return false;
         }
     }
 
-    private static <T> T invokeMethod(Object instance, String mtdName, Object... params) throws ReflectiveOperationException {
-        Class<?> cls = instance.getClass();
-
+    private <T> T invokeMethod(Class<?> cls, Object instance, String mtdName, Object... params) throws ReflectiveOperationException {
         Class<?>[] paramClasses = getParamClasses(params);
 
         Method getCfgMtd = findMethod(cls, mtdName, paramClasses);
@@ -55,7 +110,7 @@ public class MulticastSeeker {
         return (T)getCfgMtd.invoke(instance, params);
     }
 
-    private static Class<?>[] getParamClasses(Object[] params) {
+    private Class<?>[] getParamClasses(Object[] params) {
         Class<?>[] classes = new Class[params.length];
 
         for (int i = 0; i < params.length; i++)
@@ -64,7 +119,7 @@ public class MulticastSeeker {
         return classes;
     }
 
-    private static Method findMethod(Class<?> cls, String mtdName, Class<?>... params) throws NoSuchMethodException {
+    private Method findMethod(Class<?> cls, String mtdName, Class<?>... params) throws NoSuchMethodException {
         Class<?> curCls = cls;
 
         while (curCls != null) {
