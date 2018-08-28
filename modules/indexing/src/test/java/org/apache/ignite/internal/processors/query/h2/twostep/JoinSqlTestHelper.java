87d200abd3defc7b85b9041870733f50fcda6285
SGrimstad <sgrimstad@gridgain.com>
2018-08-28 14:08:47 +0300

    IGNITE-9141 Fixes according to review

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/h2/twostep/JoinSqlTestHelper.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/h2/twostep/JoinSqlTestHelper.java
@@ -26,7 +26,6 @@ import org.apache.ignite.cache.query.annotations.QuerySqlField;
 public class JoinSqlTestHelper {
     /** */
     private static final int ORG_COUNT = 100;
-
     /** */
     private static final int PERSON_PER_ORG_COUNT = 10;
 
