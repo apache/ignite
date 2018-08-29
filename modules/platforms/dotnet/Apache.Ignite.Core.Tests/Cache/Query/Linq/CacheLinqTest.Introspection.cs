fe7adfbc66b76d320a35e3e8bb43c23a79f6ba59
Pavel Tupitsyn <ptupitsyn@apache.org>
2018-08-29 15:27:44 +0300

    Fixing tests

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Cache/Query/Linq/CacheLinqTest.Introspection.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Cache/Query/Linq/CacheLinqTest.Introspection.cs
@@ -89,7 +89,7 @@ namespace Apache.Ignite.Core.Tests.Cache.Query.Linq
                   "Local=True, PageSize=999, EnableDistributedJoins=False, EnforceJoinOrder=True, " +
                   "Timeout=00:00:02.5000000, ReplicatedOnly=True, Colocated=True, Schema=, Lazy=True]]"
                 : "CacheQueryable [CacheName=person_org, TableName=Person, Query=SqlFieldsQuery " +
-                  "[Sql=select _T0._KEY, _T0._VAL from person_org_Schema.Person as _T0 where " +
+                  "[Sql=select _T0._KEY, _T0._VAL from PERSON_ORG_SCHEMA.Person as _T0 where " +
                   "(_T0._KEY > ?), Arguments=[10], " +
                   "Local=True, PageSize=999, EnableDistributedJoins=False, EnforceJoinOrder=True, " +
                   "Timeout=00:00:02.5000000, ReplicatedOnly=True, Colocated=True, Schema=, Lazy=True]]", str);
@@ -105,7 +105,7 @@ namespace Apache.Ignite.Core.Tests.Cache.Query.Linq
             fq = fieldsQuery.GetFieldsQuery();
             Assert.AreEqual(GetSqlEscapeAll()
                     ? "select _T0.\"Name\" from \"person_org\".\"Person\" as _T0"
-                    : "select _T0.NAME from person_org_Schema.Person as _T0",
+                    : "select _T0.NAME from PERSON_ORG_SCHEMA.Person as _T0",
                 fq.Sql);
 
             Assert.IsFalse(fq.Local);
