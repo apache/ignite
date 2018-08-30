6a5b599f9bef1d959f659868391ad060a364c031
Alexey Goncharuk <alexey.goncharuk@gmail.com>
2018-08-30 11:35:53 +0300

    IGNITE-9326 Fixed deferred serialization error handling for EntryProcessor result - Fixes #4588.
    Signed-off-by: Alexey Goncharuk <alexey.goncharuk@gmail.com>
    
    (cherry picked from commit 50d5ab9)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/atomic/GridDhtAtomicCache.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/atomic/GridDhtAtomicCache.java
@@ -1756,6 +1756,10 @@ public class GridDhtAtomicCache<K, V> extends GridDhtCacheAdapter<K, V> {
                             top.readUnlock();
                         }
 
+                        // This call will convert entry processor invocation results to cache object instances.
+                        // Must be done outside topology read lock to avoid deadlocks.
+                        res.returnValue().marshalResult(ctx);
+
                         break;
                     }
                     catch (UnregisteredClassException ex) {
