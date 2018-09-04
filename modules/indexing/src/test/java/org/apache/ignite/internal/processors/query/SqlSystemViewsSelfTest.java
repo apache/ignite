ea0150a491689bfe804bdeb0a608e73d88921f30
Aleksey Plekhanov <plehanov.alex@gmail.com>
2018-09-04 16:46:44 +0300

    IGNITE-9366 Review fix

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/SqlSystemViewsSelfTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/SqlSystemViewsSelfTest.java
@@ -21,19 +21,27 @@ import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+import java.util.TimeZone;
 import java.util.UUID;
 import java.util.concurrent.Callable;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.cache.QueryEntity;
 import org.apache.ignite.cache.query.SqlFieldsQuery;
+import org.apache.ignite.cluster.ClusterMetrics;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.DataRegionConfiguration;
 import org.apache.ignite.configuration.DataStorageConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.internal.ClusterMetricsSnapshot;
 import org.apache.ignite.internal.IgniteNodeAttributes;
 import org.apache.ignite.internal.processors.cache.query.IgniteQueryErrorCode;
+import org.apache.ignite.internal.util.typedef.F;
+import org.apache.ignite.internal.util.typedef.G;
 import org.apache.ignite.internal.util.typedef.X;
+import org.apache.ignite.lang.IgniteFuture;
+import org.apache.ignite.lang.IgniteRunnable;
 import org.apache.ignite.testframework.GridTestUtils;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 
@@ -41,6 +49,9 @@ import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
  * Tests for ignite SQL system views.
  */
 public class SqlSystemViewsSelfTest extends GridCommonAbstractTest {
+    /** Metrics check attempts. */
+    private static final int METRICS_CHECK_ATTEMPTS = 10;
+
     /** {@inheritDoc} */
     @Override protected void beforeTest() throws Exception {
         super.beforeTest();
@@ -183,9 +194,12 @@ public class SqlSystemViewsSelfTest extends GridCommonAbstractTest {
      * @throws Exception If failed.
      */
     public void testNodesViews() throws Exception {
-        Ignite ignite1 = startGrid(getTestIgniteInstanceName(), getConfiguration());
-        Ignite ignite2 = startGrid(getTestIgniteInstanceName(1), getConfiguration().setClientMode(true));
-        Ignite ignite3 = startGrid(getTestIgniteInstanceName(2), getConfiguration().setDaemon(true));
+        Ignite igniteSrv = startGrid(getTestIgniteInstanceName(), getConfiguration().setMetricsUpdateFrequency(500L));
+        Ignite igniteCli = startGrid(getTestIgniteInstanceName(1), getConfiguration().setMetricsUpdateFrequency(500L)
+            .setClientMode(true));
+        startGrid(getTestIgniteInstanceName(2), getConfiguration().setMetricsUpdateFrequency(500L).setDaemon(true));
+
+        UUID nodeId0 = igniteSrv.cluster().localNode().id();
 
         awaitPartitionMapExchange();
 
@@ -203,7 +217,7 @@ public class SqlSystemViewsSelfTest extends GridCommonAbstractTest {
 
         assertEquals(1, resSrv.size());
 
-        assertEquals(ignite1.cluster().localNode().id(), resSrv.get(0).get(0));
+        assertEquals(nodeId0, resSrv.get(0).get(0));
 
         assertEquals(1, resSrv.get(0).get(1));
 
@@ -212,7 +226,7 @@ public class SqlSystemViewsSelfTest extends GridCommonAbstractTest {
 
         assertEquals(1, resCli.size());
 
-        assertEquals(ignite2.cluster().localNode().id(), resCli.get(0).get(0));
+        assertEquals(nodeId(1), resCli.get(0).get(0));
 
         assertEquals(2, resCli.get(0).get(1));
 
@@ -221,7 +235,7 @@ public class SqlSystemViewsSelfTest extends GridCommonAbstractTest {
 
         assertEquals(1, resDaemon.size());
 
-        assertEquals(ignite3.cluster().localNode().id(), resDaemon.get(0).get(0));
+        assertEquals(nodeId(2), resDaemon.get(0).get(0));
 
         assertEquals(3, resDaemon.get(0).get(1));
 
@@ -229,32 +243,30 @@ public class SqlSystemViewsSelfTest extends GridCommonAbstractTest {
         assertEquals(0, execSql("SELECT ID FROM IGNITE.NODES WHERE ID = '-'").size());
 
         assertEquals(1, execSql("SELECT ID FROM IGNITE.NODES WHERE ID = ?",
-            ignite1.cluster().localNode().id()).size());
+            nodeId0).size());
 
         assertEquals(1, execSql("SELECT ID FROM IGNITE.NODES WHERE ID = ?",
-            ignite3.cluster().localNode().id()).size());
+            nodeId(2)).size());
 
         // Check index on ID column with disjunction.
         assertEquals(3, execSql("SELECT ID FROM IGNITE.NODES WHERE ID = ? " +
-            "OR node_order=1 OR node_order=2 OR node_order=3", ignite1.cluster().localNode().id()).size());
+            "OR node_order=1 OR node_order=2 OR node_order=3", nodeId0).size());
 
         // Check quick-count.
         assertEquals(3L, execSql("SELECT COUNT(*) FROM IGNITE.NODES").get(0).get(0));
 
         // Check joins
-        assertEquals(ignite1.cluster().localNode().id(), execSql("SELECT N1.ID FROM IGNITE.NODES N1 JOIN " +
+        assertEquals(nodeId0, execSql("SELECT N1.ID FROM IGNITE.NODES N1 JOIN " +
             "IGNITE.NODES N2 ON N1.NODE_ORDER = N2.NODE_ORDER JOIN IGNITE.NODES N3 ON N2.ID = N3.ID " +
             "WHERE N3.NODE_ORDER = 1")
             .get(0).get(0));
 
         // Check sub-query
-        assertEquals(ignite1.cluster().localNode().id(), execSql("SELECT N1.ID FROM IGNITE.NODES N1 " +
+        assertEquals(nodeId0, execSql("SELECT N1.ID FROM IGNITE.NODES N1 " +
             "WHERE NOT EXISTS (SELECT 1 FROM IGNITE.NODES N2 WHERE N2.ID = N1.ID AND N2.NODE_ORDER <> 1)")
             .get(0).get(0));
 
         // Check node attributes view
-        UUID cliNodeId = ignite2.cluster().localNode().id();
-
         String cliAttrName = IgniteNodeAttributes.ATTR_CLIENT_MODE;
 
         assertColumnTypes(execSql("SELECT NODE_ID, NAME, VALUE FROM IGNITE.NODE_ATTRIBUTES").get(0),
@@ -269,7 +281,7 @@ public class SqlSystemViewsSelfTest extends GridCommonAbstractTest {
 
         assertEquals(1,
             execSql("SELECT NODE_ID FROM IGNITE.NODE_ATTRIBUTES WHERE NODE_ID = ? AND NAME = ? AND VALUE = 'true'",
-                cliNodeId, cliAttrName).size());
+                nodeId(1), cliAttrName).size());
 
         assertEquals(0,
             execSql("SELECT NODE_ID FROM IGNITE.NODE_ATTRIBUTES WHERE NODE_ID = '-' AND NAME = ?",
@@ -277,10 +289,10 @@ public class SqlSystemViewsSelfTest extends GridCommonAbstractTest {
 
         assertEquals(0,
             execSql("SELECT NODE_ID FROM IGNITE.NODE_ATTRIBUTES WHERE NODE_ID = ? AND NAME = '-'",
-                cliNodeId).size());
+                nodeId(1)).size());
 
         // Check node metrics view.
-        List<List<?>> resMetrics = execSql("SELECT NODE_ID, LAST_UPDATE_TIME, " +
+        String sqlAllMetrics = "SELECT NODE_ID, LAST_UPDATE_TIME, " +
             "MAX_ACTIVE_JOBS, CUR_ACTIVE_JOBS, AVG_ACTIVE_JOBS, " +
             "MAX_WAITING_JOBS, CUR_WAITING_JOBS, AVG_WAITING_JOBS, " +
             "MAX_REJECTED_JOBS, CUR_REJECTED_JOBS, AVG_REJECTED_JOBS, TOTAL_REJECTED_JOBS, " +
@@ -295,7 +307,9 @@ public class SqlSystemViewsSelfTest extends GridCommonAbstractTest {
             "UPTIME, JVM_START_TIME, NODE_START_TIME, LAST_DATA_VERSION, " +
             "CUR_THREAD_COUNT, MAX_THREAD_COUNT, TOTAL_THREAD_COUNT, CUR_DAEMON_THREAD_COUNT, " +
             "SENT_MESSAGES_COUNT, SENT_BYTES_COUNT, RECEIVED_MESSAGES_COUNT, RECEIVED_BYTES_COUNT, " +
-            "OUTBOUND_MESSAGES_QUEUE FROM IGNITE.NODE_METRICS");
+            "OUTBOUND_MESSAGES_QUEUE FROM IGNITE.NODE_METRICS";
+
+        List<List<?>> resMetrics = execSql(sqlAllMetrics);
 
         assertColumnTypes(resMetrics.get(0), UUID.class, Timestamp.class,
             Integer.class, Integer.class, Float.class, // Active jobs.
@@ -322,7 +336,7 @@ public class SqlSystemViewsSelfTest extends GridCommonAbstractTest {
 
         // Check index on NODE_ID column.
         assertEquals(1, execSql("SELECT LAST_UPDATE_TIME FROM IGNITE.NODE_METRICS WHERE NODE_ID = ?",
-            cliNodeId).size());
+            nodeId(1)).size());
 
         // Check malformed value for indexed column.
         assertEquals(0, execSql("SELECT LAST_UPDATE_TIME FROM IGNITE.NODE_METRICS WHERE NODE_ID = ?",
@@ -330,6 +344,114 @@ public class SqlSystemViewsSelfTest extends GridCommonAbstractTest {
 
         // Check quick-count.
         assertEquals(3L, execSql("SELECT COUNT(*) FROM IGNITE.NODE_METRICS").get(0).get(0));
+
+        // Check metric values.
+
+        // Broadcast jobs to server and client nodes to get non zero metric values.
+        for (int i = 0; i < 100; i++) {
+            IgniteFuture<Void > fut = igniteSrv.compute(igniteSrv.cluster().forNodeId(nodeId0, nodeId(1)))
+                .broadcastAsync(
+                    new IgniteRunnable() {
+                        @Override public void run() {
+                            Random rnd = new Random();
+
+                            try {
+                                doSleep(rnd.nextInt(100));
+                            }
+                            catch (Throwable ignore) {
+                                // No-op.
+                            }
+                        }
+                    });
+
+            if (i % 10 == 0)
+                fut.cancel();
+        }
+
+        doSleep(igniteSrv.configuration().getMetricsUpdateFrequency() * 3L);
+
+        for (Ignite grid : G.allGrids()) {
+            UUID nodeId = grid.cluster().localNode().id();
+
+            // Metrics for node must be collected from another node to avoid race and get consistent metrics snapshot.
+            Ignite ignite = F.eq(nodeId, nodeId0) ? igniteCli : igniteSrv;
+
+            for (int i = 0; i < METRICS_CHECK_ATTEMPTS; i++) {
+                ClusterMetrics metrics = ignite.cluster().node(nodeId).metrics();
+
+                assertTrue(metrics instanceof ClusterMetricsSnapshot);
+
+                resMetrics = execSql(ignite, sqlAllMetrics + " WHERE NODE_ID = ?", nodeId);
+
+                log.info("Check metrics for node " + grid.name() + ", attempt " + (i + 1));
+
+                if (metrics.getLastUpdateTime() == ((Timestamp)resMetrics.get(0).get(1)).getTime()) {
+                    assertEquals(metrics.getMaximumActiveJobs(), resMetrics.get(0).get(2));
+                    assertEquals(metrics.getCurrentActiveJobs(), resMetrics.get(0).get(3));
+                    assertEquals(metrics.getAverageActiveJobs(), resMetrics.get(0).get(4));
+                    assertEquals(metrics.getMaximumWaitingJobs(), resMetrics.get(0).get(5));
+                    assertEquals(metrics.getCurrentWaitingJobs(), resMetrics.get(0).get(6));
+                    assertEquals(metrics.getAverageWaitingJobs(), resMetrics.get(0).get(7));
+                    assertEquals(metrics.getMaximumRejectedJobs(), resMetrics.get(0).get(8));
+                    assertEquals(metrics.getCurrentRejectedJobs(), resMetrics.get(0).get(9));
+                    assertEquals(metrics.getAverageRejectedJobs(), resMetrics.get(0).get(10));
+                    assertEquals(metrics.getTotalRejectedJobs(), resMetrics.get(0).get(11));
+                    assertEquals(metrics.getMaximumCancelledJobs(), resMetrics.get(0).get(12));
+                    assertEquals(metrics.getCurrentCancelledJobs(), resMetrics.get(0).get(13));
+                    assertEquals(metrics.getAverageCancelledJobs(), resMetrics.get(0).get(14));
+                    assertEquals(metrics.getTotalCancelledJobs(), resMetrics.get(0).get(15));
+                    assertEquals(metrics.getMaximumJobWaitTime(), convertToMilliseconds(resMetrics.get(0).get(16)));
+                    assertEquals(metrics.getCurrentJobWaitTime(), convertToMilliseconds(resMetrics.get(0).get(17)));
+                    assertEquals((long)metrics.getAverageJobWaitTime(), convertToMilliseconds(resMetrics.get(0).get(18)));
+                    assertEquals(metrics.getMaximumJobExecuteTime(), convertToMilliseconds(resMetrics.get(0).get(19)));
+                    assertEquals(metrics.getCurrentJobExecuteTime(), convertToMilliseconds(resMetrics.get(0).get(20)));
+                    assertEquals((long)metrics.getAverageJobExecuteTime(), convertToMilliseconds(resMetrics.get(0).get(21)));
+                    assertEquals(metrics.getTotalJobsExecutionTime(), convertToMilliseconds(resMetrics.get(0).get(22)));
+                    assertEquals(metrics.getTotalExecutedJobs(), resMetrics.get(0).get(23));
+                    assertEquals(metrics.getTotalExecutedTasks(), resMetrics.get(0).get(24));
+                    assertEquals(metrics.getTotalBusyTime(), convertToMilliseconds(resMetrics.get(0).get(25)));
+                    assertEquals(metrics.getTotalIdleTime(), convertToMilliseconds(resMetrics.get(0).get(26)));
+                    assertEquals(metrics.getCurrentIdleTime(), convertToMilliseconds(resMetrics.get(0).get(27)));
+                    assertEquals(metrics.getBusyTimePercentage(), resMetrics.get(0).get(28));
+                    assertEquals(metrics.getIdleTimePercentage(), resMetrics.get(0).get(29));
+                    assertEquals(metrics.getTotalCpus(), resMetrics.get(0).get(30));
+                    assertEquals(metrics.getCurrentCpuLoad(), resMetrics.get(0).get(31));
+                    assertEquals(metrics.getAverageCpuLoad(), resMetrics.get(0).get(32));
+                    assertEquals(metrics.getCurrentGcCpuLoad(), resMetrics.get(0).get(33));
+                    assertEquals(metrics.getHeapMemoryInitialized(), resMetrics.get(0).get(34));
+                    assertEquals(metrics.getHeapMemoryUsed(), resMetrics.get(0).get(35));
+                    assertEquals(metrics.getHeapMemoryCommitted(), resMetrics.get(0).get(36));
+                    assertEquals(metrics.getHeapMemoryMaximum(), resMetrics.get(0).get(37));
+                    assertEquals(metrics.getHeapMemoryTotal(), resMetrics.get(0).get(38));
+                    assertEquals(metrics.getNonHeapMemoryInitialized(), resMetrics.get(0).get(39));
+                    assertEquals(metrics.getNonHeapMemoryUsed(), resMetrics.get(0).get(40));
+                    assertEquals(metrics.getNonHeapMemoryCommitted(), resMetrics.get(0).get(41));
+                    assertEquals(metrics.getNonHeapMemoryMaximum(), resMetrics.get(0).get(42));
+                    assertEquals(metrics.getNonHeapMemoryTotal(), resMetrics.get(0).get(43));
+                    assertEquals(metrics.getUpTime(), convertToMilliseconds(resMetrics.get(0).get(44)));
+                    assertEquals(metrics.getStartTime(), ((Timestamp)resMetrics.get(0).get(45)).getTime());
+                    assertEquals(metrics.getNodeStartTime(), ((Timestamp)resMetrics.get(0).get(46)).getTime());
+                    assertEquals(metrics.getLastDataVersion(), resMetrics.get(0).get(47));
+                    assertEquals(metrics.getCurrentThreadCount(), resMetrics.get(0).get(48));
+                    assertEquals(metrics.getMaximumThreadCount(), resMetrics.get(0).get(49));
+                    assertEquals(metrics.getTotalStartedThreadCount(), resMetrics.get(0).get(50));
+                    assertEquals(metrics.getCurrentDaemonThreadCount(), resMetrics.get(0).get(51));
+                    assertEquals(metrics.getSentMessagesCount(), resMetrics.get(0).get(52));
+                    assertEquals(metrics.getSentBytesCount(), resMetrics.get(0).get(53));
+                    assertEquals(metrics.getReceivedMessagesCount(), resMetrics.get(0).get(54));
+                    assertEquals(metrics.getReceivedBytesCount(), resMetrics.get(0).get(55));
+                    assertEquals(metrics.getOutboundMessagesQueueSize(), resMetrics.get(0).get(56));
+
+                    break;
+                }
+                else {
+                    log.info("Metrics was updated in background, will retry check");
+
+                    if (i == METRICS_CHECK_ATTEMPTS - 1)
+                        fail("Failed to check metrics, attempts limit reached (" + METRICS_CHECK_ATTEMPTS + ')');
+                }
+            }
+        }
     }
 
     /**
@@ -389,5 +511,19 @@ public class SqlSystemViewsSelfTest extends GridCommonAbstractTest {
         cfg.setConsistentId(consistentId);
 
         return cfg;
+    }
+
+    /**
+     * Convert Time to milliseconds.
+     *
+     * Note: Returned Time values from SQL it's milliseconds since January 1, 1970, 00:00:00 GMT. To get right interval
+     * in milliseconds this value must be adjusted to current time zone.
+     *
+     * @param sqlTime Time value returned from SQL.
+     */
+    private long convertToMilliseconds(Object sqlTime) {
+        Time time0 = (Time)sqlTime;
+
+        return time0.getTime() + TimeZone.getDefault().getOffset(time0.getTime());
     }
 }
