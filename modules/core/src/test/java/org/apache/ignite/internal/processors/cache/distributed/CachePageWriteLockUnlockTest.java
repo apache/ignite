6750579ad6b0c1f1788d3b9fb60a50d516e64b4e
Aleksei Scherbakov <alexey.scherbakoff@gmail.com>
2018-09-07 19:49:43 +0300

    IGNITE-9445 wip.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/CachePageWriteLockUnlockTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/CachePageWriteLockUnlockTest.java
@@ -17,6 +17,10 @@
 
 package org.apache.ignite.internal.processors.cache.distributed;
 
+import java.util.Iterator;
+import javax.cache.Cache;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCheckedException;
 import org.apache.ignite.cache.affinity.rendezvous.RendezvousAffinityFunction;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.DataRegionConfiguration;
@@ -24,23 +28,31 @@ import org.apache.ignite.configuration.DataStorageConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.configuration.WALMode;
 import org.apache.ignite.internal.IgniteEx;
+import org.apache.ignite.internal.IgniteKernal;
+import org.apache.ignite.internal.pagemem.store.IgnitePageStoreManager;
+import org.apache.ignite.internal.pagemem.store.PageStore;
+import org.apache.ignite.internal.processors.cache.CacheGroupContext;
+import org.apache.ignite.internal.processors.cache.distributed.dht.GridDhtCacheAdapter;
+import org.apache.ignite.internal.processors.cache.distributed.dht.GridDhtLocalPartition;
 import org.apache.ignite.internal.processors.cache.persistence.file.FilePageStoreManager;
-import org.apache.ignite.internal.util.typedef.internal.U;
+import org.apache.ignite.internal.processors.cache.persistence.pagemem.PageMemoryEx;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
+
+import static org.apache.ignite.internal.processors.cache.distributed.dht.GridDhtPartitionState.OWNING;
 
 /**
  *
  */
 public class CachePageWriteLockUnlockTest extends GridCommonAbstractTest {
+    /** */
+    public static final int PARTITION = 0;
+
     /** {@inheritDoc} */
     @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
         IgniteConfiguration cfg = super.getConfiguration(igniteInstanceName);
 
-        CacheConfiguration ccfg = new CacheConfiguration(DEFAULT_CACHE_NAME);
-
-        ccfg.setAffinity(new RendezvousAffinityFunction(false, 32));
-
-        cfg.setCacheConfiguration(ccfg);
+        cfg.setCacheConfiguration(new CacheConfiguration(DEFAULT_CACHE_NAME).
+            setAffinity(new RendezvousAffinityFunction(false, 32)));
 
         cfg.setActiveOnStart(false);
 
@@ -58,32 +70,19 @@ public class CachePageWriteLockUnlockTest extends GridCommonAbstractTest {
     }
 
     /**
-     * exclude metastorage: org.apache.ignite.internal.processors.cache.persistence.metastorage.MetaStorage#METASTORAGE_CACHE_NAME
      *
-     * org.apache.ignite.internal.processors.cache.persistence.freelist.AbstractFreeList.RemoveRowHandler
-     *
-     * org.apache.ignite.internal.processors.cache.persistence.DataStructure#recyclePage(long, long, long, java.lang.Boolean)
-     *
-     * org.apache.ignite.internal.pagemem.PageIdUtils#rotatePageId(long)
-     *
-     * org.apache.ignite.internal.pagemem.wal.record.delta.RecycleRecord - restore new pageId on recovery.
      */
-    public void testUnlock() throws Exception {
+    public void testPreloadPartition() throws Exception {
         try {
             IgniteEx grid0 = startGrid(0);
 
             grid0.cluster().active(true);
 
-            grid0.cache(DEFAULT_CACHE_NAME).put(0, 0);
+            int total = 512;
 
-            grid0.cache(DEFAULT_CACHE_NAME).remove(0);
+            putData(grid0, total, PARTITION);
 
-            grid0.cache(DEFAULT_CACHE_NAME).put(0, 0);
-
-            // Second iteration ensures what page is taken from reuse bucket.
-            grid0.cache(DEFAULT_CACHE_NAME).remove(0);
-
-            grid0.cache(DEFAULT_CACHE_NAME).put(0, 0);
+            grid0.cache(DEFAULT_CACHE_NAME).removeAll();
 
             forceCheckpoint();
 
@@ -93,12 +92,95 @@ public class CachePageWriteLockUnlockTest extends GridCommonAbstractTest {
 
             grid0.cluster().active(true);
 
-            grid0.cache(DEFAULT_CACHE_NAME).put(0, 0);
+            putData(grid0, total, PARTITION); // Will use pages from reuse pool.
 
-            grid0.cache(DEFAULT_CACHE_NAME).put(0, 0);
+            forceCheckpoint();
+
+            stopGrid(0);
+
+            grid0 = startGrid(0);
+
+            preloadPartition(grid0, DEFAULT_CACHE_NAME, PARTITION);
+
+            Iterator<Cache.Entry<Object, Object>> it = grid0.cache(DEFAULT_CACHE_NAME).iterator();
+
+            int c0 = 0;
+
+            while (it.hasNext()) {
+                Cache.Entry<Object, Object> entry = it.next();
+
+                c0++;
+            }
+
+            assertEquals(total, c0);
         }
         finally {
             stopAllGrids();
+        }
+    }
+
+    /**
+     * @param grid Grid.
+     * @param total Total.
+     * @param part Partition.
+     */
+    private void putData(Ignite grid, int total, int part) {
+        int c = 0, k = 0;
+
+        while(c < total) {
+            if (grid(0).affinity(DEFAULT_CACHE_NAME).partition(k) == part) {
+                grid.cache(DEFAULT_CACHE_NAME).put(k, k);
+
+                c++;
+            }
+
+            k++;
+        }
+    }
+
+    /**
+     * Preload partition fast by iterating on all pages in disk order.
+     *
+     * @param grid Grid.
+     * @param cacheName Cache name.
+     * @param p P.
+     */
+    private void preloadPartition(Ignite grid, String cacheName, int p) throws IgniteCheckedException {
+        GridDhtCacheAdapter<Object, Object> dht = ((IgniteKernal)grid).internalCache(cacheName).context().dht();
+
+        GridDhtLocalPartition part = dht.topology().localPartition(p);
+
+        assertNotNull(part);
+
+        assertTrue(part.state() == OWNING);
+
+        CacheGroupContext grpCtx = dht.context().group();
+
+        if (part.state() != OWNING)
+            return;
+
+        IgnitePageStoreManager pageStoreMgr = grpCtx.shared().pageStore();
+
+        if (pageStoreMgr instanceof FilePageStoreManager) {
+            FilePageStoreManager filePageStoreMgr = (FilePageStoreManager)pageStoreMgr;
+
+            PageStore pageStore = filePageStoreMgr.getStore(grpCtx.groupId(), part.id());
+
+            PageMemoryEx pageMemory = (PageMemoryEx)grpCtx.dataRegion().pageMemory();
+
+            long pageId = pageMemory.partitionMetaPageId(grpCtx.groupId(), part.id());
+
+            for (int pageNo = 0; pageNo < pageStore.pages(); pageId++, pageNo++) {
+                long pagePointer = -1;
+
+                try {
+                    pagePointer = pageMemory.acquirePage(grpCtx.groupId(), pageId);
+                }
+                finally {
+                    if (pagePointer != -1)
+                        pageMemory.releasePage(grpCtx.groupId(), pageId, pagePointer);
+                }
+            }
         }
     }
 
