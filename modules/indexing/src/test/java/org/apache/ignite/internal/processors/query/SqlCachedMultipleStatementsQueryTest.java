e70158b6c7e6535ceb434927a834be7a8cb1876f
tledkov-gridgain <tledkov@gridgain.com>
2018-09-10 11:51:33 +0300

    IGNITE-9342: Fix two-step queries cache for multiple statements query

---
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/SqlCachedMultipleStatementsQueryTest.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.processors.query;
+
+import java.util.List;
+import org.apache.ignite.cache.query.FieldsQueryCursor;
+import org.apache.ignite.cache.query.SqlFieldsQuery;
+import org.apache.ignite.internal.IgniteEx;
+import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
+
+/**
+ * Tests for schemas.
+ */
+public class SqlCachedMultipleStatementsQueryTest extends GridCommonAbstractTest {
+    /** Node. */
+    private IgniteEx node;
+
+    /** {@inheritDoc} */
+    @Override protected void beforeTest() throws Exception {
+        node = (IgniteEx)startGrid();
+    }
+
+    /** {@inheritDoc} */
+    @Override protected void afterTest() throws Exception {
+        stopAllGrids();
+    }
+
+    /**
+     */
+    public void test() {
+        List<FieldsQueryCursor<List<?>>> curs = sql("SELECT 1; SELECT 2");
+
+        assertEquals(2, curs.size());
+        assertEquals(1, curs.get(0).getAll().get(0).get(0));
+        assertEquals(2, curs.get(1).getAll().get(0).get(0));
+
+        curs = sql("SELECT 1; SELECT 2");
+
+        assertEquals(2, curs.size());
+        assertEquals(1, curs.get(0).getAll().get(0).get(0));
+        assertEquals(2, curs.get(1).getAll().get(0).get(0));
+    }
+
+    /**
+     * @param sql SQL query.
+     * @return Results.
+     */
+    private List<FieldsQueryCursor<List<?>>> sql(String sql) {
+        GridQueryProcessor qryProc = node.context().query();
+
+        SqlFieldsQuery qry = new SqlFieldsQuery(sql).setSchema("PUBLIC");
+
+        return qryProc.querySqlFields(qry, true, false);
+    }
+}
