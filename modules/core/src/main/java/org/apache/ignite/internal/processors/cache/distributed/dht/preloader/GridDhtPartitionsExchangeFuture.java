2f72215cf2c28067ce6487f2b1b03e1fc0575ddd
ibessonov <bessonov.ip@gmail.com>
2018-09-12 15:03:36 +0300

    IGNITE-7618 Minor problem in deactivation detection in GridDhtPartitionsExchangeFuture

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPartitionsExchangeFuture.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPartitionsExchangeFuture.java
@@ -345,7 +345,8 @@ public class GridDhtPartitionsExchangeFuture extends GridDhtTopologyFutureAdapte
         this.exchActions = exchActions;
         this.affChangeMsg = affChangeMsg;
         this.validator = new GridDhtPartitionsStateValidator(cctx);
-        this.clusterIsActive = exchActions == null || !exchActions.deactivate();
+        if (exchActions != null && exchActions.deactivate())
+            this.clusterIsActive = false;
 
         log = cctx.logger(getClass());
         exchLog = cctx.logger(EXCHANGE_LOG);
