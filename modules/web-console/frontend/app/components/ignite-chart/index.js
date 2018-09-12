ffb52ebb087d8e7d934ff69e2000c37624a8ec40
Alexander Kalinin <verbalab@yandex.ru>
2018-09-12 15:30:51 +0700

    IGNITE-9510 Web Console: Added vertical cursor to ignite-chart component, fix tooltip positioning.

--- modules/web-console/frontend/app/components/ignite-chart/index.js
+++ modules/web-console/frontend/app/components/ignite-chart/index.js
@@ -32,6 +32,7 @@ export default angular
             chartHistory: '<',
             chartTitle: '<',
             chartColors: '<',
+            chartHeaderText: '<',
             maxPointsNumber: '<',
             refreshRate: '<'
         }
