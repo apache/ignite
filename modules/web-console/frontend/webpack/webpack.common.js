8f5cde1df8dd5dedff75b84cffb0342941707697
Ilya Borisov <Klaster1@gmail.com>
2018-09-13 16:07:04 +0700

    IGNITE-9552 Disable eslint loader for .ts files.

--- modules/web-console/frontend/webpack/webpack.common.js
+++ modules/web-console/frontend/webpack/webpack.common.js
@@ -80,7 +80,7 @@ const config = {
             },
             { test: /\.worker\.js$/, use: { loader: 'worker-loader' } },
             {
-                test: /\.(js|ts)$/,
+                test: /\.(js)$/,
                 enforce: 'pre',
                 exclude: [/node_modules/],
                 use: [{
