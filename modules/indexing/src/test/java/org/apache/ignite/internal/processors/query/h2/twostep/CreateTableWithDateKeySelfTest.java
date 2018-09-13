40109877f7f57bb3a465b146d140f63899acaa34
SGrimstad <sgrimstad@gridgain.com>
2018-09-13 16:32:43 +0300

    IGNITE-8552 fixed test according to review

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/h2/twostep/CreateTableWithDateKeySelfTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/h2/twostep/CreateTableWithDateKeySelfTest.java
@@ -1,10 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.internal.processors.query.h2.twostep;
 
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.List;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.cache.query.FieldsQueryCursor;
 import org.apache.ignite.cache.query.SqlFieldsQuery;
+import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 
 /** */
@@ -16,11 +41,63 @@ public class CreateTableWithDateKeySelfTest extends GridCommonAbstractTest {
     private static Ignite ignite;
 
     /** */
-    public void testPassTableWithDateKeyCreation(){
-        IgniteCache<Object, Object> cache = ignite.getOrCreateCache(DEFAULT_CACHE_NAME+"1");
+    private static IgniteCache<?, ?> initCache;
 
-        try (FieldsQueryCursor<List<?>> cur = cache.query(new SqlFieldsQuery(
-            "CREATE TABLE Tab1(id DATE primary key, dateField DATE)"))) {
+    /** */
+    public void testPassTableWithDateKeyCreation() {
+        final String creationQry = "CREATE TABLE %s (id DATE primary key, dateField DATE) " +
+            "WITH \"cache_name=%s, WRAP_VALUE=false\"";
+
+        final Date key = new Date();
+
+        final Date val = Date.from(Instant.now().minus(1, ChronoUnit.DAYS));
+
+        checkInsertUpdateDelete(creationQry, "Tab1", key, val);
+    }
+
+    /** */
+    public void testPassTableWithTimeKeyCreation() {
+        final String creationQry = "CREATE TABLE %s (id TIME primary key, dateField TIME) " +
+            "WITH \"cache_name=%s, WRAP_VALUE=false\"";
+
+        final Time key = Time.valueOf(LocalTime.now());
+
+        final Time val = Time.valueOf(LocalTime.now());
+
+        checkInsertUpdateDelete(creationQry, "Tab2", key, val);
+    }
+
+    /** */
+    public void testPassTableWithTimeStampKeyCreation() {
+        final String creationQry = "CREATE TABLE %s (id TIMESTAMP primary key, dateField TIMESTAMP) " +
+            "WITH \"cache_name=%s, WRAP_VALUE=false\"";
+
+        final Timestamp key = Timestamp.valueOf(LocalDateTime.now());
+
+        final Timestamp val = Timestamp.valueOf(LocalDateTime.now());
+
+        checkInsertUpdateDelete(creationQry, "Tab3", key, val);
+    }
+
+    /**
+     * Common testing logic
+     *
+     * @param creationQry Create table query.
+     * @param tableName Table name.
+     * @param key Sample key.
+     * @param val Sample value.
+     * @param <K> Type of key.
+     * @param <V> Type of value.
+     */
+    private <K, V> void checkInsertUpdateDelete(
+        final String creationQry,
+        final String tableName,
+        final K key,
+        final V val) {
+        final String cacheName = "TABLE_CACHE_" + tableName;
+
+        try (FieldsQueryCursor<List<?>> cur = initCache.query(
+            new SqlFieldsQuery(String.format(creationQry, tableName, cacheName)))) {
 
             assertNotNull(cur);
 
@@ -30,14 +107,11 @@ public class CreateTableWithDateKeySelfTest extends GridCommonAbstractTest {
 
             assertEquals(0L, rows.get(0).get(0));
         }
-    }
 
-    /** */
-    public void testPassTableWithTimeKeyCreation(){
-        IgniteCache<Object, Object> cache = ignite.getOrCreateCache(DEFAULT_CACHE_NAME+"2");
+        IgniteCache<K, V> cache = ignite.getOrCreateCache(cacheName);
 
         try (FieldsQueryCursor<List<?>> cur = cache.query(new SqlFieldsQuery(
-            "CREATE TABLE Tab2(id TIME primary key, dateField TIME)"))) {
+            "INSERT INTO " + tableName + " VALUES(?, ?)").setArgs(key, val))) {
 
             assertNotNull(cur);
 
@@ -45,30 +119,75 @@ public class CreateTableWithDateKeySelfTest extends GridCommonAbstractTest {
 
             assertEquals(1, rows.size());
 
-            assertEquals(0L, rows.get(0).get(0));
+            assertEquals(1L, rows.get(0).get(0));
+        }
+
+        assertSelection(tableName, cache, key, val);
+
+        assertEquals(val, cache.get(key));
+
+        cache.remove(key);
+
+        assertAbsence(tableName, cache, key);
+
+        assertFalse(cache.containsKey(key));
+
+        cache.put(key, val);
+
+        assertEquals(val, cache.get(key));
+
+        assertSelection(tableName, cache, key, val);
+
+        try (FieldsQueryCursor<List<?>> cur = cache.query(new SqlFieldsQuery(
+            "DELETE FROM " + tableName + " WHERE id=?").setArgs(key))) {
+
+            assertNotNull(cur);
+
+            List<List<?>> rows = cur.getAll();
+
+            assertEquals(1, rows.size());
+
+            assertEquals(1L, rows.get(0).get(0));
+        }
+
+        assertAbsence(tableName, cache, key);
+
+        assertFalse(cache.containsKey(key));
+    }
+
+    private <K, V> void assertAbsence(final String tableName, final IgniteCache<K, V> cache, final K key) {
+        try (FieldsQueryCursor<List<?>> cur = cache.query(new SqlFieldsQuery(
+            "select * from " + tableName + " where id=?").setArgs(key))) {
+            assertNotNull(cur);
+
+            List<List<?>> rows = cur.getAll();
+
+            assertEquals(0, rows.size());
         }
     }
 
-    /** */
-    public void testPassTableWithTimeStampKeyCreation(){
-        IgniteCache<Object, Object> cache = ignite.getOrCreateCache(DEFAULT_CACHE_NAME+"3");
-
+    private <K, V> void assertSelection(final String tableName, final IgniteCache<K, V> cache, final K key,
+        final V val) {
         try (FieldsQueryCursor<List<?>> cur = cache.query(new SqlFieldsQuery(
-            "CREATE TABLE Tab3(id TIMESTAMP primary key, dateField TIMESTAMP)"))) {
-
+            "select * from " + tableName + " where id=?").setArgs(key))) {
             assertNotNull(cur);
 
             List<List<?>> rows = cur.getAll();
 
             assertEquals(1, rows.size());
 
-            assertEquals(0L, rows.get(0).get(0));
+            assertEquals(key, rows.get(0).get(0));
+
+            assertEquals(val, rows.get(0).get(1));
         }
     }
 
     /** {@inheritDoc} */
     @Override protected void beforeTestsStarted() throws Exception {
         ignite = startGridsMultiThreaded(NODES_COUNT, false);
+        initCache = ignite.getOrCreateCache(new CacheConfiguration<>(DEFAULT_CACHE_NAME)
+            .setSqlSchema("PUBLIC")
+        );
     }
 
     /** {@inheritDoc} */
