d97622927fbee35c4420511246593a8396692aea
Pavel Kovalenko <jokserfn@gmail.com>
2018-09-18 16:43:38 +0300

    IGNITE-9492 Reworked condition exchangeInProgress.
    (cherry picked from commit 095f944)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCachePartitionExchangeManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCachePartitionExchangeManager.java
@@ -2147,6 +2147,9 @@ public class GridCachePartitionExchangeManager<K, V> extends GridCacheSharedMana
      * @return {@code True} If there is any exchange future in progress.
      */
     private boolean exchangeInProgress() {
+        if (exchWorker.hasPendingServerExchange())
+            return true;
+
         GridDhtPartitionsExchangeFuture current = lastTopologyFuture();
 
         if (current == null)
@@ -2154,8 +2157,12 @@ public class GridCachePartitionExchangeManager<K, V> extends GridCacheSharedMana
 
         GridDhtTopologyFuture finished = lastFinishedFut.get();
 
-        if (finished == null || finished.result().compareTo(current.initialVersion()) < 0)
-            return true;
+        if (finished == null || finished.result().compareTo(current.initialVersion()) < 0) {
+            ClusterNode triggeredBy = current.firstEvent().eventNode();
+
+            if (!triggeredBy.isClient())
+                return true;
+       }
 
         return false;
     }
