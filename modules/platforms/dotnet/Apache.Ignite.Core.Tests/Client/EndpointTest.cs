4af741b5d18ce3f4ec3c7507b07f3762bd894471
Pavel Tupitsyn <ptupitsyn@apache.org>
2018-09-23 19:50:58 +0300

    Adding EndpointTest

---
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Client/EndpointTest.cs
@@ -0,0 +1,17 @@
+namespace Apache.Ignite.Core.Tests.Client
+{
+    using Apache.Ignite.Core.Impl.Client;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests for <see cref="Endpoint"/> class.
+    /// </summary>
+    public class EndpointTest
+    {
+        [Test]
+        public void GetEndpoints_InvalidConfigFormat_ThrowsIgniteClientException()
+        {
+
+        }
+    }
+}
