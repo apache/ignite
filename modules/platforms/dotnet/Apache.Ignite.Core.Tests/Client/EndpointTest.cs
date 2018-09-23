943ef2369d2c5dc32eeb67feb1dc4cb171645805
Pavel Tupitsyn <ptupitsyn@apache.org>
2018-09-23 19:58:35 +0300

    Adding Endpoint parsing tests

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Client/EndpointTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Client/EndpointTest.cs
@@ -1,5 +1,7 @@
 namespace Apache.Ignite.Core.Tests.Client
 {
+    using System.Linq;
+    using Apache.Ignite.Core.Client;
     using Apache.Ignite.Core.Impl.Client;
     using NUnit.Framework;
 
@@ -11,7 +13,24 @@ namespace Apache.Ignite.Core.Tests.Client
         [Test]
         public void GetEndpoints_InvalidConfigFormat_ThrowsIgniteClientException()
         {
+            var cfg = new IgniteClientConfiguration("");
+        }
 
+        [Test]
+        public void GetEndpoints_ParsesPortsAndRanges()
+        {
+            var ipWithDefaultPort = Endpoint.GetEndpoints(new IgniteClientConfiguration("1.2.3.4")).Single();
+            Assert.AreEqual("1.2.3.4", ipWithDefaultPort.Host);
+            Assert.AreEqual(IgniteClientConfiguration.DefaultPort, ipWithDefaultPort.Port);
+
+            var ipWithCustomPort = Endpoint.GetEndpoints(new IgniteClientConfiguration("1.2.3.4:678")).Single();
+            Assert.AreEqual("1.2.3.4", ipWithCustomPort.Host);
+            Assert.AreEqual(678, ipWithCustomPort.Port);
+
+            var ipWithPortRange = Endpoint.GetEndpoints(new IgniteClientConfiguration("1.2.3.4:678..680")).Single();
+            Assert.AreEqual("1.2.3.4", ipWithPortRange.Host);
+            Assert.AreEqual(678, ipWithPortRange.Port);
+            Assert.AreEqual(2, ipWithPortRange.PortRange);
         }
     }
 }
