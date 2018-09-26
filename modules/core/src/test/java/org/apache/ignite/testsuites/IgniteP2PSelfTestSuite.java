984043ae0706140970a0d386a5342877d154ac3a
Sergey Antonov <antonovsergey93@gmail.com>
2018-09-26 16:55:06 +0300

    IGNITE-9381 Clear class cache on p2p undeploy - Fixes #4784.
    Signed-off-by: Alexey Goncharuk <alexey.goncharuk@gmail.com>
    (cherry picked from commit d40dcb3a8f25a5bb098662fa7270b69eb3cc3ab5)

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteP2PSelfTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteP2PSelfTestSuite.java
@@ -35,6 +35,7 @@ import org.apache.ignite.p2p.GridP2PRemoteClassLoadersSelfTest;
 import org.apache.ignite.p2p.GridP2PSameClassLoaderSelfTest;
 import org.apache.ignite.p2p.GridP2PTimeoutSelfTest;
 import org.apache.ignite.p2p.GridP2PUndeploySelfTest;
+import org.apache.ignite.p2p.P2PScanQueryUndeployTest;
 import org.apache.ignite.p2p.P2PStreamingClassLoaderTest;
 import org.apache.ignite.p2p.SharedDeploymentTest;
 import org.apache.ignite.testframework.GridTestUtils;
@@ -76,6 +77,7 @@ public class IgniteP2PSelfTestSuite extends TestSuite {
         suite.addTest(new TestSuite(DeploymentClassLoaderCallableTest.class));
         suite.addTest(new TestSuite(P2PStreamingClassLoaderTest.class));
         suite.addTest(new TestSuite(SharedDeploymentTest.class));
+        suite.addTest(new TestSuite(P2PScanQueryUndeployTest.class));
         GridTestUtils.addTestIfNeeded(suite, GridDeploymentMessageCountSelfTest.class, ignoredTests);
 
         return suite;
