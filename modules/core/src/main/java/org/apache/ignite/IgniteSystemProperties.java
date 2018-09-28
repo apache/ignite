74f239ff29b6329f70639ef53e509b7018d60f90
Ivan Daschinskiy <ivandasch@gmail.com>
2018-09-28 18:03:45 +0300

    IGNITE-9693 Scale up wal compression workers to increase performance - Fixes #4831.
    Signed-off-by: Ivan Rakov <irakov@apache.org>
    (cherry picked from commit 036bd074d8bfd25a2c4c463a60dde00604d11b9d)

--- modules/core/src/main/java/org/apache/ignite/IgniteSystemProperties.java
+++ modules/core/src/main/java/org/apache/ignite/IgniteSystemProperties.java
@@ -879,6 +879,12 @@ public final class IgniteSystemProperties {
      */
     public static final String IGNITE_LOADED_PAGES_BACKWARD_SHIFT_MAP = "IGNITE_LOADED_PAGES_BACKWARD_SHIFT_MAP";
 
+
+    /**
+     * Count of WAL compressor worker threads. Default value is 4.
+     */
+    public static final String IGNITE_WAL_COMPRESSOR_WORKER_THREAD_CNT = "IGNITE_WAL_COMPRESSOR_WORKER_THREAD_CNT";
+
     /**
      * Whenever read load balancing is enabled, that means 'get' requests will be distributed between primary and backup
      * nodes if it is possible and {@link CacheConfiguration#readFromBackup} is {@code true}.
