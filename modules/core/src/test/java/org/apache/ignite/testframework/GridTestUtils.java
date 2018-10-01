041db4bdd98570aafd46a1308a77cf73f73b6ba9
Yury Gerzhedovich <ygerzhedovich@gridgain.com>
2018-10-01 14:25:22 +0300

    ignite-8580: minor fix

--- modules/core/src/test/java/org/apache/ignite/testframework/GridTestUtils.java
+++ modules/core/src/test/java/org/apache/ignite/testframework/GridTestUtils.java
@@ -43,7 +43,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Properties;
 import java.util.Queue;
 import java.util.Random;
 import java.util.Set;
