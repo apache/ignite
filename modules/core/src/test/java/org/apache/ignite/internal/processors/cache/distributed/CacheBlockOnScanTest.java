5708547a5a1803e34d60312aaf09e53b9e2bc872
ibessonov <bessonov.ip@gmail.com>
2018-10-02 14:50:27 +0300

    IGNITE-9694 Test prototype

---
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/CacheBlockOnScanTest.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.processors.cache.distributed;
+
+import java.util.Objects;
+import java.util.Random;
+import org.apache.ignite.cache.query.ScanQuery;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ *
+ */
+public class CacheBlockOnScanTest extends CacheBlockOnReadAbstractTest {
+
+    /** {@inheritDoc} */
+    @Override @NotNull protected CacheReadBackgroundOperation<?, ?> getReadOperation() {
+        return new IntCacheReadBackgroundOperation() {
+            /** Random. */
+            private Random random = new Random();
+
+            /** {@inheritDoc} */
+            @Override public void doRead() {
+                int idx = random.nextInt(entriesCount());
+
+                cache().query(new ScanQuery<>((k, v) -> Objects.equals(k, idx))).getAll();
+            }
+        };
+    }
+}
