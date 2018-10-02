66793724cefa9ec18cf1ae6ebcd927b8f64935bf
Andrey Kuznetsov <stkuzma@gmail.com>
2018-10-02 12:51:38 +0300

    IGNITE-9744 One more compelled cancel() on worker termination.

--- modules/core/src/main/java/org/apache/ignite/internal/util/nio/GridNioServer.java
+++ modules/core/src/main/java/org/apache/ignite/internal/util/nio/GridNioServer.java
@@ -2909,6 +2909,9 @@ public class GridNioServer<T> {
                     lsnr.onFailure(CRITICAL_ERROR, err);
                 else if (err != null)
                     lsnr.onFailure(SYSTEM_WORKER_TERMINATION, err);
+                else
+                    // In case of closed == true, prevent general-case termination handling.
+                    cancel();
             }
         }
 
