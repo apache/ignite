24c5a178b8aaee5bfb43ea343f454c1ecf1c4b30
tledkov-gridgain <tledkov@gridgain.com>
2018-10-03 13:09:17 +0300

    IGNITE-9727: Fixed ignite.sh and ignite.bat scripts to support Java 9+. This closes #4871.

--- bin/include/functions.sh
+++ bin/include/functions.sh
@@ -84,11 +84,6 @@ checkJava() {
         echo "Please point JAVA_HOME variable to installation of JDK 1.8 or JDK 9."
         echo "You can also download latest JDK at http://java.com/download"
         exit 1
-    elif [ $version -gt 9 ]; then
-        echo "$0, WARNING:"
-        echo "The $version version of JAVA installed in JAVA_HOME=$JAVA_HOME was not tested with Apache Ignite."
-        echo "Run it on your own risk or point JAVA_HOME variable to installation of JDK 1.8 or JDK 9."
-        echo "You can also download JDK at http://java.com/download"
     fi
 }
 
