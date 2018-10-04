a43a575a2d852e0632d920972e216ec0744ea9d9
Anton Dmitriev <dmitrievanthony@gmail.com>
2018-10-04 17:03:51 +0300

    IGNITE-9788: ML: IgniteDataset op is not loaded in TensorFlow worker
    this closes #4912

--- modules/tensorflow/src/main/java/org/apache/ignite/tensorflow/cluster/tfrunning/TensorFlowServerScriptFormatter.java
+++ modules/tensorflow/src/main/java/org/apache/ignite/tensorflow/cluster/tfrunning/TensorFlowServerScriptFormatter.java
@@ -66,7 +66,8 @@ public class TensorFlowServerScriptFormatter {
         builder.append("\n");
 
         builder.append("import tensorflow as tf").append('\n');
-        builder.append("fto_import_contrib_ops = tf.contrib.resampler").append("\n");
+        builder.append("to_import_contrib_ops = tf.contrib.resampler").append("\n");
+        builder.append("from tensorflow.contrib.ignite import IgniteDataset").append("\n");
         builder.append("import tensorflow.contrib.igfs.python.ops.igfs_ops").append("\n");
 
         builder.append("print('job:%s task:%d' % ('")
