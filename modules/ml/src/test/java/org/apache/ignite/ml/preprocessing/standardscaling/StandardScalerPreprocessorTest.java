a9af7030f3792628fd0cf0af70d80d89f65cb055
dehasi <rgaleyev@gmail.com>
2018-10-09 06:54:04 +0200

    IGNITE-9284: Add more data to test

--- modules/ml/src/test/java/org/apache/ignite/ml/preprocessing/standardscaling/StandardScalerPreprocessorTest.java
+++ modules/ml/src/test/java/org/apache/ignite/ml/preprocessing/standardscaling/StandardScalerPreprocessorTest.java
@@ -13,26 +13,26 @@ public class StandardScalerPreprocessorTest {
 
     @Test
     public void apply_() {
-        double[][] inputData = new double[][]{
-            {2., 4., 1.},
-            {1., 8., 22.},
-            {4., 10., 100.},
-            {0., 22., 300.}
+        double[][] inputData = new double[][] {
+            {0, 2., 4., .1},
+            {0, 1., -18., 2.2},
+            {1, 4., 10., -.1},
+            {1, 0., 22., 1.3}
         };
-        double[] means= new double[]{1,2,3};
-        double[] variances = new double[]{1,2,3};
+        double[] means = new double[] {0.5, 1.75, 4.5, 0.875};
+        double[] variances = new double[] {0.25, 2.1875, 210.75, 0.871875};
 
         StandardScalerPreprocessor<Integer, Vector> preprocessor = new StandardScalerPreprocessor<>(
             means,
             variances,
-            (k,v)-> v
+            (k, v) -> v
         );
 
-        double[][] expectedData = new double[][]{
-            {2. / 4, (4. - 4.) / 18.,  0.},
-            {1. / 4, (8. - 4.) / 18.,  (22. - 1.) / 299.},
-            {1.,     (10. - 4.) / 18., (100. - 1.) / 299.},
-            {0.,     (22. - 4.) / 18., (300. - 1.) / 299.}
+        double[][] expectedData = new double[][] {
+            {-1., 0.16903085, -0.03444183, -0.82999331},
+            {-1., -0.50709255, -1.54988233, 1.41902081},
+            {1., 1.52127766, 0.37886012, -1.04418513},
+            {1., -1.18321596, 1.20546403, 0.45515762}
         };
 
         for (int i = 0; i < inputData.length; i++)
