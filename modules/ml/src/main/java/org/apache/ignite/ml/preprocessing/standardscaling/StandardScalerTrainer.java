98570cf0461198873a4b9722eaa167042e59fa2e
dehasi <rgaleyev@gmail.com>
2018-10-10 17:46:54 +0200

    IGNITE-9284: Count sigma with mean

--- modules/ml/src/main/java/org/apache/ignite/ml/preprocessing/standardscaling/StandardScalerTrainer.java
+++ modules/ml/src/main/java/org/apache/ignite/ml/preprocessing/standardscaling/StandardScalerTrainer.java
@@ -17,7 +17,6 @@
 
 package org.apache.ignite.ml.preprocessing.standardscaling;
 
-import java.util.Arrays;
 import org.apache.ignite.ml.dataset.Dataset;
 import org.apache.ignite.ml.dataset.DatasetBuilder;
 import org.apache.ignite.ml.dataset.UpstreamEntry;
@@ -37,43 +36,56 @@ public class StandardScalerTrainer<K, V> implements PreprocessingTrainer<K, V, V
     @Override public StandardScalerPreprocessor<K, V> fit(DatasetBuilder<K, V> datasetBuilder,
         IgniteBiFunction<K, V, Vector> basePreprocessor) {
 
-        MeanHelper meanHelper = calculateMeans(datasetBuilder, basePreprocessor);
-        VarianceHelper varianceHelper = calculateVariance(datasetBuilder, basePreprocessor, meanHelper);
-        double[] sigma = new double[meanHelper.means.length];
-        for (int i = 0; i < varianceHelper.vars.length; i++) {
-            sigma[i] = Math.sqrt(varianceHelper.vars[i]);
+        SumHelper sumHelper = computeSum(datasetBuilder, basePreprocessor);
+
+        int n = sumHelper.sum.length;
+        long count = sumHelper.count;
+        double[] mean = new double[n];
+        double[] sigma = new double[n];
+
+        // Var = (SumSq − (Sum * Sum) / N) / N
+        for (int i = 0; i < n; i++) {
+            mean[i] = sumHelper.sum[i] / count;
+            double variace = (sumHelper.squaredSum[i] - sumHelper.sum[i] * sumHelper.sum[i] / count) / count;
+            sigma[i] = Math.sqrt(variace);
         }
-        return new StandardScalerPreprocessor<>(meanHelper.means, sigma, basePreprocessor);
+        return new StandardScalerPreprocessor<>(mean, sigma, basePreprocessor);
     }
 
-    private MeanHelper calculateMeans(DatasetBuilder<K, V> datasetBuilder,
+    private SumHelper computeSum(DatasetBuilder<K, V> datasetBuilder,
         IgniteBiFunction<K, V, Vector> basePreprocessor) {
-        try (Dataset<EmptyContext, MeanHelper> dataset = datasetBuilder.build(
+        try (Dataset<EmptyContext, SumHelper> dataset = datasetBuilder.build(
             (upstream, upstreamSize) -> new EmptyContext(),
             (upstream, upstreamSize, ctx) -> {
                 double[] sum = null;
+                double[] squaredSum = null;
                 long count = 0;
+
                 while (upstream.hasNext()) {
                     UpstreamEntry<K, V> entity = upstream.next();
                     Vector row = basePreprocessor.apply(entity.getKey(), entity.getValue());
 
                     if (sum == null) {
                         sum = new double[row.size()];
-                        Arrays.fill(sum, .0);
+                        squaredSum = new double[row.size()];
                     }
-                    else
+                    else {
                         assert sum.length == row.size() : "Base preprocessor must return exactly " + sum.length
                             + " features";
+                    }
 
                     ++count;
                     for (int i = 0; i < row.size(); i++) {
-                        sum[i] += row.get(i);
+                        double x = row.get(i);
+                        sum[i] += x;
+                        squaredSum[i] += x * x;
                     }
                 }
-                return new MeanHelper(sum, count);
+                return new SumHelper(sum, squaredSum, count);
             }
         )) {
-            MeanHelper helper = dataset.compute(data -> data,
+
+            return dataset.compute(data -> data,
                 (a, b) -> {
                     if (a == null)
                         return b;
@@ -82,81 +94,27 @@ public class StandardScalerTrainer<K, V> implements PreprocessingTrainer<K, V, V
 
                     return a.merge(b);
                 });
-
-            for (int i = 0; i < helper.means.length; i++) {
-                helper.means[i] /= helper.count;
-            }
-
-            return helper;
         }
         catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    VarianceHelper calculateVariance(DatasetBuilder<K, V> datasetBuilder,
-        IgniteBiFunction<K, V, Vector> basePreprocessor, MeanHelper meansHelper) {
-        double[] means = meansHelper.means;
-        try (Dataset<EmptyContext, VarianceHelper> dataset = datasetBuilder.build(
-            (upstream, upstreamSize) -> new EmptyContext(),
-            (upstream, upstreamSize, ctx) -> {
-                double[] squaredDiff = null;
-                while (upstream.hasNext()) {
-                    UpstreamEntry<K, V> entity = upstream.next();
-                    Vector row = basePreprocessor.apply(entity.getKey(), entity.getValue());
-
-                    if (squaredDiff == null) {
-                        squaredDiff = new double[row.size()];
-                        Arrays.fill(squaredDiff, .0);
-                    }
-                    else
-                        assert squaredDiff.length == row.size() : "Base preprocessor must return exactly " + squaredDiff.length
-                            + " features";
-
-                    for (int i = 0; i < row.size(); i++) {
-                        squaredDiff[i] += (means[i] - row.get(i)) * (means[i] - row.get(i));
-                    }
-                }
-                return new VarianceHelper(squaredDiff);
-            }
-        )) {
-            VarianceHelper helper = dataset.compute(data -> data,
-                (a, b) -> {
-                    if (a == null)
-                        return b;
-                    if (b == null)
-                        return a;
-
-                    return a.merge(b);
-                });
-
-            for (int i = 0; i < helper.vars.length; i++) {
-                helper.vars[i] /= meansHelper.count;
-            }
-
-            return helper;
-        }
-        catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static class MeanHelper implements AutoCloseable {
-        double[] means;
+    private static class SumHelper implements AutoCloseable {
+        double[] sum;
+        double[] squaredSum;
         long count;
 
-        public MeanHelper(double[] means, long count) {
-            this.means = means;
+        public SumHelper(double[] sum, double[] squaredSum, long count) {
+            this.sum = sum;
+            this.squaredSum = squaredSum;
             this.count = count;
         }
 
-        public double[] getMeans() {
-            return means;
-        }
-
-        MeanHelper merge(MeanHelper that) {
-            for (int i = 0; i < means.length; i++) {
-                means[i] += that.means[i];
+        SumHelper merge(SumHelper that) {
+            for (int i = 0; i < sum.length; i++) {
+                sum[i] += that.sum[i];
+                squaredSum[i] += that.squaredSum[i];
             }
             count += that.count;
             return this;
@@ -168,27 +126,4 @@ public class StandardScalerTrainer<K, V> implements PreprocessingTrainer<K, V, V
         }
     }
 
-    private static class VarianceHelper implements AutoCloseable {
-        double[] vars;
-
-        public VarianceHelper(double[] vars) {
-            this.vars = vars;
-        }
-
-        public double[] getMeans() {
-            return vars;
-        }
-
-        VarianceHelper merge(VarianceHelper that) {
-            for (int i = 0; i < vars.length; i++) {
-                vars[i] += that.vars[i];
-            }
-            return this;
-        }
-
-        /** */
-        @Override public void close() {
-            // Do nothing, GC will clean up.
-        }
-    }
 }
