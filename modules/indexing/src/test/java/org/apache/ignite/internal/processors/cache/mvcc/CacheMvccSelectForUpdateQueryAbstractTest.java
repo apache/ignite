585617b6b35b7e037a247df899f0fdecfde44f74
devozerov <vozerov@gridgain.com>
2018-10-10 16:52:23 +0300

    IGNITE-9724: MVCC: impoved reliability of CacheMvccSelectForUpdateQueryAbstractTest.

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/mvcc/CacheMvccSelectForUpdateQueryAbstractTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/mvcc/CacheMvccSelectForUpdateQueryAbstractTest.java
@@ -96,8 +96,6 @@ public abstract class CacheMvccSelectForUpdateQueryAbstractTest extends CacheMvc
      *
      */
     public void testSelectForUpdateDistributed() throws Exception {
-        fail("https://issues.apache.org/jira/browse/IGNITE-9724");
-
         doTestSelectForUpdateDistributed("Person", false);
     }
 
@@ -162,6 +160,8 @@ public abstract class CacheMvccSelectForUpdateQueryAbstractTest extends CacheMvc
      * @throws Exception If failed.
      */
     void doTestSelectForUpdateDistributed(String cacheName, boolean outsideTx) throws Exception {
+        awaitPartitionMapExchange();
+
         Ignite node = grid(0);
 
         IgniteCache<Integer, ?> cache = node.cache(cacheName);
