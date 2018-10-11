9d16c92d047e21de6c3bae20415659e2999d109a
dehasi <rgaleyev@gmail.com>
2018-10-11 21:35:40 +0200

    IGNITE-9282: Add java docs

--- modules/ml/src/main/java/org/apache/ignite/ml/naivebayes/gaussian/GaussianNaiveBayesSumsHolder.java
+++ modules/ml/src/main/java/org/apache/ignite/ml/naivebayes/gaussian/GaussianNaiveBayesSumsHolder.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.ignite.ml.naivebayes.gaussian;
 
 import java.io.Serializable;
@@ -5,7 +21,7 @@ import java.util.HashMap;
 import java.util.Map;
 import org.apache.ignite.ml.math.util.MapUtil;
 
-/** /** Service class is used to calculate means and vaiances */
+/** Service class is used to calculate means and vaiances */
 class GaussianNaiveBayesSumsHolder implements Serializable {
     /** Serial version uid. */
     private static final long serialVersionUID = 1L;
@@ -16,7 +32,7 @@ class GaussianNaiveBayesSumsHolder implements Serializable {
     /** Rows count for each label */
     Map<Double, Integer> featureCountersPerLbl = new HashMap<>();
 
-    /** Merge current */
+    /** Merge to current */
     GaussianNaiveBayesSumsHolder merge(GaussianNaiveBayesSumsHolder other) {
         featureSumsPerLbl = MapUtil.mergeMaps(featureSumsPerLbl, other.featureSumsPerLbl, this::sum, HashMap::new);
         featureSquaredSumsPerLbl = MapUtil.mergeMaps(featureSquaredSumsPerLbl, other.featureSquaredSumsPerLbl, this::sum, HashMap::new);
