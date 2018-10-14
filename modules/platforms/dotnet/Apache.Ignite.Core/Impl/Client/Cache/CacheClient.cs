5862fd3e47e4f846951dab9e5f1e3ab8cba42117
Pavel Tupitsyn <ptupitsyn@apache.org>
2018-10-14 22:59:21 +0300

    TODOs

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Client/Cache/CacheClient.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Client/Cache/CacheClient.cs
@@ -96,6 +96,8 @@ namespace Apache.Ignite.Core.Impl.Client.Cache
         {
             IgniteArgumentCheck.NotNull(key, "key");
 
+            // TODO: What is a good way to abstract Affinity-based operations?
+            // DoOutInOpAffinity?
             return DoOutInOp(ClientOp.CacheGet, w => w.WriteObject(key), UnmarshalNotNull<TV>);
         }
 
@@ -700,7 +702,7 @@ namespace Apache.Ignite.Core.Impl.Client.Cache
             QueryBase.WriteQueryArgs(writer, qry.Arguments);
 
             // .NET client does not discern between different statements for now.
-            // We cound have ExecuteNonQuery method, which uses StatementType.Update, for example.
+            // We could have ExecuteNonQuery method, which uses StatementType.Update, for example.
             writer.WriteByte((byte)StatementType.Any);
 
             writer.WriteBoolean(qry.EnableDistributedJoins);
