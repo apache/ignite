063efb33c2fcaabf20610d78ca91952c8501f843
rkondakov <kondakov87@mail.ru>
2018-10-15 17:42:38 +0300

    IGNITE-7953: MVCC: Continuous queries support. This closes #4767.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/transactions/IgniteTxLocalAdapter.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/transactions/IgniteTxLocalAdapter.java
@@ -983,6 +983,8 @@ public abstract class IgniteTxLocalAdapter extends IgniteTxAdapter implements Ig
                 // Apply cache sizes only for primary nodes. Update counters were applied on prepare state.
                 applyTxSizes();
 
+                cctx.mvccCaching().onTxFinished(this, true);
+
                 if (ptr != null && !cctx.tm().logTxRecords())
                     cctx.wal().flush(ptr, false);
             }
@@ -994,8 +996,6 @@ public abstract class IgniteTxLocalAdapter extends IgniteTxAdapter implements Ig
             }
             finally {
                 cctx.database().checkpointReadUnlock();
-
-                notifyDrManager(state() == COMMITTING && err == null);
 
                 cctx.tm().resetContext();
             }
@@ -1092,6 +1092,8 @@ public abstract class IgniteTxLocalAdapter extends IgniteTxAdapter implements Ig
                 assert !needsCompletedVersions || committedVers != null : "Missing committed versions for transaction: " + this;
                 assert !needsCompletedVersions || rolledbackVers != null : "Missing rolledback versions for transaction: " + this;
             }
+
+            cctx.mvccCaching().onTxFinished(this, commit);
         }
     }
 
@@ -1130,8 +1132,6 @@ public abstract class IgniteTxLocalAdapter extends IgniteTxAdapter implements Ig
     @Override public void userRollback(boolean clearThreadMap) throws IgniteCheckedException {
         TransactionState state = state();
 
-        notifyDrManager(false);
-
         if (state != ROLLING_BACK && state != ROLLED_BACK) {
             setRollbackOnly();
 
@@ -1148,6 +1148,8 @@ public abstract class IgniteTxLocalAdapter extends IgniteTxAdapter implements Ig
 
         if (DONE_FLAG_UPD.compareAndSet(this, 0, 1)) {
             cctx.tm().rollbackTx(this, clearThreadMap, forceSkipCompletedVers);
+
+            cctx.mvccCaching().onTxFinished(this, false);
 
             if (!internal()) {
                 Collection<CacheStoreManager> stores = txState.stores(cctx);
