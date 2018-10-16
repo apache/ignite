784c57333c54c541a05fce0f9aa9e996efc0adbd
ascherbakoff <alexey.scherbakoff@gmail.com>
2018-10-16 21:13:17 +0300

    Merge branch 'master' of https://github.com/apache/ignite into ignite-9830
    # Conflicts:
    #	modules/core/src/main/java/org/apache/ignite/IgniteSystemProperties.java

--- modules/core/src/main/java/org/apache/ignite/IgniteSystemProperties.java
+++ modules/core/src/main/java/org/apache/ignite/IgniteSystemProperties.java
@@ -1019,12 +1019,6 @@ public final class IgniteSystemProperties {
     public static final String IGNITE_REBALANCE_THROTTLE_OVERRIDE = "IGNITE_REBALANCE_THROTTLE_OVERRIDE";
 
     /**
-     * System property to override {@link CacheConfiguration#rebalanceThrottle} configuration property for all caches.
-     * {@code 0} by default, which means that override is disabled.
-     */
-    public static final String IGNITE_REBALANCE_THROTTLE_OVERRIDE = "IGNITE_REBALANCE_THROTTLE_OVERRIDE";
-
-    /**
      * Enforces singleton.
      */
     private IgniteSystemProperties() {
