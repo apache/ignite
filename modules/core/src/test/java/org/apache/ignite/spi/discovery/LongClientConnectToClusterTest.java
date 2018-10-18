781468e8265eb9c4e12ea1147af3e8911576db7f
vd-pyatkov <vpyatkov@gridgain.com>
2018-10-18 14:52:21 +0300

    Change test.

--- modules/core/src/test/java/org/apache/ignite/spi/discovery/LongClientConnectToClusterTest.java
+++ modules/core/src/test/java/org/apache/ignite/spi/discovery/LongClientConnectToClusterTest.java
@@ -20,25 +20,37 @@ package org.apache.ignite.spi.discovery;
 import org.apache.ignite.IgniteCheckedException;
 import org.apache.ignite.cluster.ClusterNode;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.events.EventType;
 import org.apache.ignite.internal.IgniteEx;
+import org.apache.ignite.internal.util.future.IgniteFinishedFutureImpl;
+import org.apache.ignite.lang.IgniteFuture;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 import org.apache.ignite.spi.discovery.tcp.messages.TcpDiscoveryAbstractMessage;
 import org.apache.ignite.spi.discovery.tcp.messages.TcpDiscoveryNodeAddFinishedMessage;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
+import org.jetbrains.annotations.Nullable;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Map;
 
 /**
  * Test client connects to two nodes cluster during time more than the {@link org.apache.ignite.configuration.IgniteConfiguration#clientFailureDetectionTimeout}.
  */
 public class LongClientConnectToClusterTest extends GridCommonAbstractTest {
+    /** Client metrics update count. */
+    private static int clientMetricsUpdateCnt;
+
     /** {@inheritDoc} */
     @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
         TcpDiscoverySpi discoSpi = getTestIgniteInstanceName(0).equals(igniteInstanceName)
             ? new DelayedTcpDiscoverySpi()
+            : getTestIgniteInstanceName(1).equals(igniteInstanceName)
+            ? new UpdateMetricsInterceptorTcpDiscoverySpi()
             : new TcpDiscoverySpi();
 
         return super.getConfiguration(igniteInstanceName)
@@ -75,22 +87,80 @@ public class LongClientConnectToClusterTest extends GridCommonAbstractTest {
 
     /**
      * Test method.
+     *
      * @throws Exception If failed.
      */
     public void testClientConnectToCluster() throws Exception {
+        clientMetricsUpdateCnt = 0;
+
         IgniteEx client = startGrid("client");
+
+        assertTrue(clientMetricsUpdateCnt > 0);
 
         assertTrue(client.localNode().isClient());
 
         assertEquals(client.cluster().nodes().size(), 3);
     }
 
+    /** Discovery SPI which intercept TcpDiscoveryClientMetricsUpdateMessage. */
+    private static class UpdateMetricsInterceptorTcpDiscoverySpi extends TcpDiscoverySpi {
+        /** */
+        private class DiscoverySpiListenerWrapper implements DiscoverySpiListener {
+            /** */
+            private DiscoverySpiListener delegate;
+
+            /**
+             * @param delegate Delegate.
+             */
+            private DiscoverySpiListenerWrapper(DiscoverySpiListener delegate) {
+                this.delegate = delegate;
+            }
+
+            /** {@inheritDoc} */
+            @Override public IgniteFuture<?> onDiscovery(
+                int type,
+                long topVer,
+                ClusterNode node,
+                Collection<ClusterNode> topSnapshot,
+                @Nullable Map<Long, Collection<ClusterNode>> topHist,
+                @Nullable DiscoverySpiCustomMessage spiCustomMsg
+            ) {
+                if (EventType.EVT_NODE_METRICS_UPDATED == type) {
+                    log.info("Metrics update message catched from node " + node);
+
+                    if (node.isClient())
+                        clientMetricsUpdateCnt++;
+                }
+
+                if (delegate != null)
+                    return delegate.onDiscovery(type, topVer, node, topSnapshot, topHist, spiCustomMsg);
+
+                return new IgniteFinishedFutureImpl<>();
+            }
+
+            /** {@inheritDoc} */
+            @Override public void onLocalNodeInitialized(ClusterNode locNode) {
+                // No-op.
+            }
+        }
+
+        /** {@inheritDoc} */
+        @Override public void setListener(@Nullable DiscoverySpiListener lsnr) {
+            super.setListener(new DiscoverySpiListenerWrapper(lsnr));
+        }
+    }
+
     /** Discovery SPI delayed TcpDiscoveryNodeAddFinishedMessage. */
     private static class DelayedTcpDiscoverySpi extends TcpDiscoverySpi {
 
-        /** {@inheritDoc} */
-        @Override protected void writeToSocket(ClusterNode node, Socket sock, OutputStream out, TcpDiscoveryAbstractMessage msg,
+        @Override protected <T> T readMessage(Socket sock, @Nullable InputStream in,
             long timeout) throws IOException, IgniteCheckedException {
+            return super.readMessage(sock, in, timeout);
+        }
+
+        /** {@inheritDoc} */
+        @Override protected void writeToSocket(ClusterNode node, Socket sock, OutputStream out,
+            TcpDiscoveryAbstractMessage msg, long timeout) throws IOException, IgniteCheckedException {
             if (msg instanceof TcpDiscoveryNodeAddFinishedMessage && msg.topologyVersion() == 3) {
                 log.info("Catched discovery message: " + msg);
 
