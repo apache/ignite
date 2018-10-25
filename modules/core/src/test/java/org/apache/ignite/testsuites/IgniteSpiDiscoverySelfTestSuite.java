c9877442c7727de9d75b38e0d66c8d00ea6bce04
Dmitrii Ryabov <SomeFireOne@gmail.com>
2018-10-25 18:19:54 +0300

    9986 flakiness

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteSpiDiscoverySelfTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteSpiDiscoverySelfTestSuite.java
@@ -77,71 +77,11 @@ public class IgniteSpiDiscoverySelfTestSuite extends TestSuite {
 
         TestSuite suite = new TestSuite("Ignite Discovery SPI Test Suite");
 
-        // Tcp.
-        suite.addTest(new TestSuite(TcpDiscoveryVmIpFinderSelfTest.class));
-        suite.addTest(new TestSuite(TcpDiscoverySharedFsIpFinderSelfTest.class));
-        suite.addTest(new TestSuite(TcpDiscoveryJdbcIpFinderSelfTest.class));
-        suite.addTest(new TestSuite(TcpDiscoveryMulticastIpFinderSelfTest.class));
-
         suite.addTest(new TestSuite(TcpDiscoverySelfTest.class));
-        suite.addTest(new TestSuite(TcpDiscoverySpiSelfTest.class));
-        //suite.addTest(new TestSuite(TcpDiscoverySpiRandomStartStopTest.class));
-        //suite.addTest(new TestSuite(TcpDiscoverySpiSslSelfTest.class));
-        //suite.addTest(new TestSuite(TcpDiscoverySpiWildcardSelfTest.class));
-        suite.addTest(new TestSuite(TcpDiscoverySpiFailureTimeoutSelfTest.class));
-        suite.addTest(new TestSuite(TcpDiscoverySpiMBeanTest.class));
-        suite.addTest(new TestSuite(TcpDiscoverySpiStartStopSelfTest.class));
-        suite.addTest(new TestSuite(TcpDiscoverySpiConfigSelfTest.class));
-        suite.addTest(new TestSuite(TcpDiscoveryMarshallerCheckSelfTest.class));
-        suite.addTest(new TestSuite(TcpDiscoverySnapshotHistoryTest.class));
-
-        suite.addTest(new TestSuite(GridTcpSpiForwardingSelfTest.class));
-
-        suite.addTest(new TestSuite(TcpClientDiscoverySpiSelfTest.class));
-        suite.addTest(new TestSuite(LongClientConnectToClusterTest.class));
-        suite.addTest(new TestSuite(TcpClientDiscoveryMarshallerCheckSelfTest.class));
-        suite.addTest(new TestSuite(TcpClientDiscoverySpiMulticastTest.class));
-        suite.addTest(new TestSuite(TcpClientDiscoverySpiFailureTimeoutSelfTest.class));
-        suite.addTest(new TestSuite(TcpClientDiscoveryUnresolvedHostTest.class));
-
-        suite.addTest(new TestSuite(TcpDiscoveryNodeConsistentIdSelfTest.class));
-        suite.addTest(new TestSuite(TcpDiscoveryNodeConfigConsistentIdSelfTest.class));
-
-        suite.addTest(new TestSuite(TcpDiscoveryRestartTest.class));
-        suite.addTest(new TestSuite(TcpDiscoveryMultiThreadedTest.class));
-        //suite.addTest(new TestSuite(TcpDiscoveryConcurrentStartTest.class));
-
-        suite.addTest(new TestSuite(TcpDiscoverySegmentationPolicyTest.class));
-
-        suite.addTest(new TestSuite(TcpDiscoveryNodeAttributesUpdateOnReconnectTest.class));
-        suite.addTest(new TestSuite(AuthenticationRestartTest.class));
-
-        suite.addTest(new TestSuite(TcpDiscoveryWithWrongServerTest.class));
-
-        suite.addTest(new TestSuite(TcpDiscoverySpiReconnectDelayTest.class));
-
-        suite.addTest(new TestSuite(IgniteDiscoveryMassiveNodeFailTest.class));
-
-        // Client connect.
-        suite.addTest(new TestSuite(IgniteClientConnectTest.class));
-        suite.addTest(new TestSuite(IgniteClientReconnectMassiveShutdownTest.class));
-        suite.addTest(new TestSuite(TcpDiscoveryClientSuspensionSelfTest.class));
 
         // SSL.
         suite.addTest(new TestSuite(TcpDiscoverySslSelfTest.class));
         suite.addTest(new TestSuite(TcpDiscoverySslTrustedSelfTest.class));
-        suite.addTest(new TestSuite(TcpDiscoverySslSecuredUnsecuredTest.class));
-        suite.addTest(new TestSuite(TcpDiscoverySslTrustedUntrustedTest.class));
-        suite.addTest(new TestSuite(TcpDiscoverySslParametersTest.class));
-
-        // Disco cache reuse.
-        suite.addTest(new TestSuite(IgniteDiscoveryCacheReuseSelfTest.class));
-
-        suite.addTest(new TestSuite(DiscoveryUnmarshalVulnerabilityTest.class));
-
-        suite.addTest(new TestSuite(FilterDataForClientNodeDiscoveryTest.class));
-
-        suite.addTest(new TestSuite(TcpDiscoveryPendingMessageDeliveryTest.class));
 
         return suite;
     }
--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteSpiTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteSpiTestSuite.java
@@ -33,37 +33,8 @@ public class IgniteSpiTestSuite extends TestSuite {
     public static TestSuite suite() throws Exception {
         TestSuite suite = new TestSuite("Ignite SPIs Test Suite");
 
-        // Failover.
-        suite.addTest(IgniteSpiFailoverSelfTestSuite.suite());
-
-        // Collision.
-        suite.addTest(IgniteSpiCollisionSelfTestSuite.suite());
-
-        // Event storage.
-        suite.addTest(IgniteSpiEventStorageSelfTestSuite.suite());
-
-        // Load Balancing.
-        suite.addTest(IgniteSpiLoadBalancingSelfTestSuite.suite());
-
-        // Checkpoints.
-        suite.addTest(IgniteSpiCheckpointSelfTestSuite.suite());
-
-        // Deployment
-        suite.addTest(IgniteSpiDeploymentSelfTestSuite.suite());
-
-        // Discovery.
-        suite.addTest(IgniteSpiDiscoverySelfTestSuite.suite());
-
-        // Communication.
-        suite.addTest(IgniteSpiCommunicationSelfTestSuite.suite());
-
-        // All other tests.
-        suite.addTestSuite(GridNoopManagerSelfTest.class);
-
-        // Local Message Listener tests.
-        suite.addTestSuite(GridManagerLocalMessageListenerSelfTest.class);
-
-        suite.addTestSuite(KeystoreEncryptionSpiSelfTest.class);
+        for (int i = 0; i < 100; i++)
+            suite.addTest(IgniteSpiDiscoverySelfTestSuite.suite());
 
         return suite;
     }
