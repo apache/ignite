b1f7383c7cece19c36d9c9fcf0ec7370caa0a2c7
Sergi Vladykin <sergi.vladykin@gmail.com>
2018-10-26 14:52:46 +0300

    Javadocs and license

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/CacheCompressionManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/CacheCompressionManager.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.internal.processors.cache.persistence;
 
 import java.nio.ByteBuffer;
@@ -11,6 +28,9 @@ import org.apache.ignite.internal.processors.compress.CompressionProcessor;
 import static org.apache.ignite.internal.processors.compress.CompressionProcessor.checkCompressionLevelBounds;
 import static org.apache.ignite.internal.processors.compress.CompressionProcessor.getDefaultCompressionLevel;
 
+/**
+ * Cache compression manager.
+ */
 public class CacheCompressionManager extends GridCacheManagerAdapter {
     /** */
     private PageCompression pageCompression;
@@ -21,7 +41,7 @@ public class CacheCompressionManager extends GridCacheManagerAdapter {
     /** */
     private CompressionProcessor compressProc;
 
-    /** */
+    /** {@inheritDoc} */
     @Override protected void start0() throws IgniteCheckedException {
         DataRegionConfiguration cfg = cctx.dataRegion().config();
 
