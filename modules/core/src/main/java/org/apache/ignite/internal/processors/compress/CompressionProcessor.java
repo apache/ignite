aa3f0c748cd6cc7bed2d44e1cb6975fb85ed7d7d
Sergi Vladykin <sergi.vladykin@gmail.com>
2018-10-28 16:22:12 +0300

    minor

--- modules/core/src/main/java/org/apache/ignite/internal/processors/compress/CompressionProcessor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/compress/CompressionProcessor.java
@@ -32,16 +32,34 @@ import org.apache.ignite.internal.processors.cache.persistence.tree.io.PageIO;
  */
 public class CompressionProcessor extends GridProcessorAdapter {
     /** */
-    public static final byte UNCOMPRESSED_PAGE = 0;
+    public static final int LZ4_MIN_LEVEL = 0;
 
     /** */
-    public static final byte COMPACTED_PAGE = 1;
+    public static final int LZ4_MAX_LEVEL = 17;
 
     /** */
-    public static final byte ZSTD_COMPRESSED_PAGE = 2;
+    public static final int LZ4_DEFAULT_LEVEL = 0;
 
     /** */
-    public static final byte LZ4_COMPRESSED_PAGE = 3;
+    public static final int ZSTD_MIN_LEVEL = -131072;
+
+    /** */
+    public static final int ZSTD_MAX_LEVEL = 22;
+
+    /** */
+    public static final int ZSTD_DEFAULT_LEVEL = 3;
+
+    /** */
+    protected static final byte UNCOMPRESSED_PAGE = 0;
+
+    /** */
+    protected static final byte COMPACTED_PAGE = 1;
+
+    /** */
+    protected static final byte ZSTD_COMPRESSED_PAGE = 2;
+
+    /** */
+    protected static final byte LZ4_COMPRESSED_PAGE = 3;
 
     /**
      * @param ctx Kernal context.
@@ -57,10 +75,10 @@ public class CompressionProcessor extends GridProcessorAdapter {
     public static int getDefaultCompressionLevel(PageCompression compression) {
         switch (compression) {
             case ZSTD:
-                return 3;
+                return ZSTD_DEFAULT_LEVEL;
 
             case LZ4:
-                return 0;
+                return LZ4_DEFAULT_LEVEL;
 
             case SKIP_GARBAGE:
                 return 0;
@@ -77,14 +95,11 @@ public class CompressionProcessor extends GridProcessorAdapter {
     public static int checkCompressionLevelBounds(int compressLevel, PageCompression compression) {
         switch (compression) {
             case ZSTD:
-                // TODO Use Zstd.minCompressionLevel() and Zstd.maxCompressionLevel() after zstd jar upgrade.
-                if (compressLevel < -131072 || compressLevel > 22)
-                    throw new IllegalArgumentException("Compression level for ZSTD must be between -131072 and 22." );
+                checkCompressionLevelBounds(compressLevel, ZSTD_MIN_LEVEL, ZSTD_MAX_LEVEL);
                 break;
 
             case LZ4:
-                if (compressLevel < 0 || compressLevel > 17)
-                    throw new IllegalArgumentException("Compression level for LZ4 must be between 0 and 17." );
+                checkCompressionLevelBounds(compressLevel, LZ4_MIN_LEVEL, LZ4_MAX_LEVEL);
                 break;
 
             case SKIP_GARBAGE:
@@ -93,8 +108,19 @@ public class CompressionProcessor extends GridProcessorAdapter {
             default:
                 throw new IllegalArgumentException("Compression: " + compression);
         }
-
         return compressLevel;
+    }
+
+    /**
+     * @param compressLevel Compression level.
+     * @param min Min level.
+     * @param max Max level.
+     */
+    private static void checkCompressionLevelBounds(int compressLevel, int min, int max) {
+        if (compressLevel < min  || compressLevel > max) {
+            throw new IllegalArgumentException("Compression level for LZ4 must be between " + min +
+                " and " + max + ".");
+        }
     }
 
     /**
