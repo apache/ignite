a5510150fd0ecb2a0b7d7b3837c0b77ed6b95daa
Sergey Antonov <antonovsergey93@gmail.com>
2018-11-02 23:44:39 +0300

    IGNITE-10134 Fix bug in reducing result on multinode execution.

--- modules/core/src/main/java/org/apache/ignite/internal/visor/cache/VisorCacheConfigurationCollectorTask.java
+++ modules/core/src/main/java/org/apache/ignite/internal/visor/cache/VisorCacheConfigurationCollectorTask.java
@@ -17,16 +17,21 @@
 
 package org.apache.ignite.internal.visor.cache;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import org.apache.ignite.IgniteException;
+import org.apache.ignite.compute.ComputeJobResult;
 import org.apache.ignite.internal.processors.task.GridInternal;
-import org.apache.ignite.internal.visor.VisorOneNodeTask;
+import org.apache.ignite.internal.visor.VisorMultiNodeTask;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Task that collect cache metrics from all nodes.
  */
 @GridInternal
 public class VisorCacheConfigurationCollectorTask
-    extends VisorOneNodeTask<VisorCacheConfigurationCollectorTaskArg, Map<String, VisorCacheConfiguration>> {
+    extends VisorMultiNodeTask<VisorCacheConfigurationCollectorTaskArg, Map<String, VisorCacheConfiguration>, Map<String, VisorCacheConfiguration>> {
     /** */
     private static final long serialVersionUID = 0L;
 
@@ -34,4 +39,24 @@ public class VisorCacheConfigurationCollectorTask
     @Override protected VisorCacheConfigurationCollectorJob job(VisorCacheConfigurationCollectorTaskArg arg) {
         return new VisorCacheConfigurationCollectorJob(arg, debug);
     }
+
+
+    /** {@inheritDoc} */
+    @Override protected @Nullable Map<String, VisorCacheConfiguration> reduce0(
+        List<ComputeJobResult> results
+    ) throws IgniteException {
+        if (results == null)
+            return null;
+
+        if(results.size() == 1)
+            return results.get(0).getData();
+
+        Map<String, VisorCacheConfiguration> map = new HashMap<>();
+
+        for (ComputeJobResult res : results)
+            map.putAll(res.getData());
+
+        return map;
+    }
+
 }
