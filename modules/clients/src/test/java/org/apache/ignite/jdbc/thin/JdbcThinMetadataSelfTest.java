1e7b3f1947da236b071465c661525ffbe40af06b
Pavel Kuznetsov <palmihal@gmail.com>
2018-11-06 17:39:11 +0300

    ignite-9989: Updated tests to work with many tables.
    Backported test for primary keys metadata from metadata test of jdbc
    thin driver. This change made IGNITE-10118 highlighted.

--- modules/clients/src/test/java/org/apache/ignite/jdbc/thin/JdbcThinMetadataSelfTest.java
+++ modules/clients/src/test/java/org/apache/ignite/jdbc/thin/JdbcThinMetadataSelfTest.java
@@ -566,8 +566,7 @@ public class JdbcThinMetadataSelfTest extends JdbcThinAbstractSelfTest {
                     '.' + rs.getString("COLUMN_NAME"));
             }
 
-            assert expectedPks.equals(actualPks) : "expectedPks=" + expectedPks +
-                ", actualPks" + actualPks;
+            assertEquals("Metadata contains unexpected primary keys info.", expectedPks, actualPks);
         }
     }
 
