edb496b9489615852d2c48960d79b4d8325e375a
ipavlukhin <vololo100@gmail.com>
2018-11-07 16:28:22 +0300

    adopt existing peek tests and add them to MVCC suite

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheMvccTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheMvccTestSuite.java
@@ -18,6 +18,9 @@
 package org.apache.ignite.testsuites;
 
 import junit.framework.TestSuite;
+import org.apache.ignite.cache.CacheAtomicityMode;
+import org.apache.ignite.internal.processors.cache.IgniteCacheTxPeekModesTest;
+import org.apache.ignite.internal.processors.cache.IgniteCacheTxReplicatedPeekModesTest;
 import org.apache.ignite.internal.processors.cache.mvcc.CacheMvccClusterRestartTest;
 import org.apache.ignite.internal.processors.cache.mvcc.CacheMvccConfigurationValidationTest;
 import org.apache.ignite.internal.processors.cache.mvcc.CacheMvccIteratorWithConcurrentTransactionTest;
@@ -34,6 +37,8 @@ import org.apache.ignite.internal.processors.cache.mvcc.CacheMvccTransactionsTes
 import org.apache.ignite.internal.processors.cache.mvcc.CacheMvccVacuumTest;
 import org.apache.ignite.internal.processors.cache.mvcc.MvccCachePeekTest;
 import org.apache.ignite.internal.processors.datastreamer.DataStreamProcessorMvccSelfTest;
+
+import static org.apache.ignite.cache.CacheAtomicityMode.TRANSACTIONAL_SNAPSHOT;
 
 /**
  *
@@ -58,6 +63,9 @@ public class IgniteCacheMvccTestSuite extends TestSuite {
 
         suite.addTestSuite(MvccCachePeekTest.class);
 
+        suite.addTestSuite(MvccIgniteCacheTxPeekModesTest.class);
+        suite.addTestSuite(MvccIgniteCacheTxReplicatedPeekModesTest.class);
+
         // Concurrent ops tests.
         suite.addTestSuite(CacheMvccIteratorWithConcurrentTransactionTest.class);
         suite.addTestSuite(CacheMvccLocalEntriesWithConcurrentTransactionTest.class);
@@ -71,5 +79,35 @@ public class IgniteCacheMvccTestSuite extends TestSuite {
         suite.addTestSuite(CacheMvccProcessorLazyStartTest.class);
 
         return suite;
+    }
+
+    /** */
+    public static class MvccIgniteCacheTxPeekModesTest extends IgniteCacheTxPeekModesTest {
+        /** {@inheritDoc} */
+        @Override protected CacheAtomicityMode atomicityMode() {
+            return TRANSACTIONAL_SNAPSHOT;
+        }
+
+        /** {@inheritDoc} */
+        @Override public void testLocalEntries() throws Exception {
+            fail("https://issues.apache.org/jira/browse/IGNITE-10167");
+
+            super.testLocalEntries();
+        }
+    }
+
+    /** */
+    public static class MvccIgniteCacheTxReplicatedPeekModesTest extends IgniteCacheTxReplicatedPeekModesTest {
+        /** {@inheritDoc} */
+        @Override protected CacheAtomicityMode atomicityMode() {
+            return TRANSACTIONAL_SNAPSHOT;
+        }
+
+        /** {@inheritDoc} */
+        @Override public void testLocalEntries() throws Exception {
+            fail("https://issues.apache.org/jira/browse/IGNITE-10167");
+
+            super.testLocalEntries();
+        }
     }
 }
