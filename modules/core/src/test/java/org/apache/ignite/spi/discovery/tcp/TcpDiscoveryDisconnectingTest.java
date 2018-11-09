1ee6261596589723062e7acaa43d22bbf0e856ed
Pavel Voronkin <pvoronkin@gridgain.com>
2018-11-09 15:00:03 +0300

    IGNITE-10417 Fixed test.

--- modules/core/src/test/java/org/apache/ignite/spi/discovery/tcp/TcpDiscoveryDisconnectingTest.java
+++ modules/core/src/test/java/org/apache/ignite/spi/discovery/tcp/TcpDiscoveryDisconnectingTest.java
@@ -19,6 +19,8 @@ package org.apache.ignite.spi.discovery.tcp;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.function.Consumer;
+import org.apache.ignite.Ignite;
 import org.apache.ignite.cluster.ClusterNode;
 import org.apache.ignite.internal.IgniteEx;
 import org.apache.ignite.internal.managers.discovery.CustomEventListener;
@@ -27,9 +29,12 @@ import org.apache.ignite.internal.managers.discovery.DiscoCache;
 import org.apache.ignite.internal.managers.discovery.DiscoveryCustomMessage;
 import org.apache.ignite.internal.managers.discovery.GridDiscoveryManager;
 import org.apache.ignite.internal.processors.affinity.AffinityTopologyVersion;
+import org.apache.ignite.internal.util.lang.GridAbsPredicate;
+import org.apache.ignite.internal.util.typedef.G;
 import org.apache.ignite.lang.IgniteUuid;
 import org.apache.ignite.spi.discovery.tcp.internal.TcpDiscoverySpiState;
 import org.apache.ignite.spi.discovery.tcp.messages.TcpDiscoveryEnsureDelivery;
+import org.apache.ignite.testframework.GridTestUtils;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.jetbrains.annotations.Nullable;
 import org.mockito.internal.util.reflection.Whitebox;
@@ -48,13 +53,10 @@ public class TcpDiscoveryDisconnectingTest extends GridCommonAbstractTest {
      * @throws Exception If failed.
      */
     public void testCustomMessageWhileSpiIsStopping() throws Exception {
+
         List<DummyCustomDiscoveryMessage> messages = new CopyOnWriteArrayList<>();
 
-        IgniteEx coord = startGrid("coordinator");
-
-        IgniteEx node1 = startGrid("node1");
-
-        IgniteEx node2 = startGrid("node2");
+        startGrids(3);
 
         CustomEventListener<DummyCustomDiscoveryMessage> listener = new CustomEventListener<DummyCustomDiscoveryMessage>() {
             @Override public void onCustomEvent(
@@ -65,15 +67,20 @@ public class TcpDiscoveryDisconnectingTest extends GridCommonAbstractTest {
             }
         };
 
-        coord.context().discovery().setCustomEventListener(DummyCustomDiscoveryMessage.class, listener);
-        node1.context().discovery().setCustomEventListener(DummyCustomDiscoveryMessage.class, listener);
-        node2.context().discovery().setCustomEventListener(DummyCustomDiscoveryMessage.class, listener);
+        G.allGrids().forEach(new Consumer<Ignite>() {
+            @Override public void accept(Ignite ignite) {
+                ((IgniteEx)ignite)
+                    .context()
+                    .discovery()
+                    .setCustomEventListener(DummyCustomDiscoveryMessage.class, listener);
+            }
+        });
 
-        coord.cluster().active(true);
+        grid(0).cluster().active(true);
 
-        TcpDiscoverySpi coordDisco = (TcpDiscoverySpi)coord.configuration().getDiscoverySpi();
+        TcpDiscoverySpi coordDisco = (TcpDiscoverySpi)grid(0).configuration().getDiscoverySpi();
 
-        TcpDiscoverySpi nodeDisco = (TcpDiscoverySpi)node1.configuration().getDiscoverySpi();
+        TcpDiscoverySpi nodeDisco = (TcpDiscoverySpi)grid(1).configuration().getDiscoverySpi();
 
         TcpDiscoveryImpl impl = (TcpDiscoveryImpl)Whitebox.getInternalState(nodeDisco, "impl");
         Whitebox.setInternalState(impl, "spiState", TcpDiscoverySpiState.DISCONNECTING);
@@ -81,9 +88,13 @@ public class TcpDiscoveryDisconnectingTest extends GridCommonAbstractTest {
         // Custom message on a singleton cluster shouldn't break consistency of PendingMessages.
         sendDummyCustomMessage(coordDisco, IgniteUuid.randomUuid());
 
-        stopAllGrids();
+        GridTestUtils.waitForCondition(new GridAbsPredicate() {
+            @Override public boolean apply() {
+                return messages.size() ==  3;
+            }
+        }, 10000);
 
-        assertEquals(3, messages.size());
+        stopAllGrids();
     }
 
     /**
