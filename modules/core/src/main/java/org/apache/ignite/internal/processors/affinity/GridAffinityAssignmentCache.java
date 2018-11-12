91c037edb8818a1434be59ad2a04d1944a80f8d6
Sergey Antonov <antonovsergey93@gmail.com>
2018-11-12 20:38:15 +0300

    GG-14435 condition was changed in assertion.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/affinity/GridAffinityAssignmentCache.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/affinity/GridAffinityAssignmentCache.java
@@ -694,7 +694,7 @@ public class GridAffinityAssignmentCache {
         else
             awaitTopologyVersion(topVer);
 
-        assert topVer.topologyVersion() >= 0 : topVer;
+        assert !ctx.cluster().get().active() || topVer.topologyVersion() >= 0 : topVer;
 
         AffinityAssignment cache = head.get();
 
