eb7ddf5e3884c5ae468c5a1e822b8a17496398fd
Nikolay Izhikov <nizhikov@apache.org>
2018-11-19 15:05:38 +0300

    IGNITE-9996: Final fix

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/serializer/RecordDataV1Serializer.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/serializer/RecordDataV1Serializer.java
@@ -147,6 +147,9 @@ public class RecordDataV1Serializer implements RecordDataSerializer {
     private final GridEncryptionManager encMgr;
 
     /** */
+    private final boolean encryptionDisabled;
+
+    /** */
     private static final byte ENCRYPTED = 1;
 
     /** */
@@ -162,6 +165,8 @@ public class RecordDataV1Serializer implements RecordDataSerializer {
         this.pageSize = cctx.database().pageSize();
         this.encSpi = cctx.gridConfig().getEncryptionSpi();
         this.encMgr = cctx.kernalContext().encryption();
+
+        encryptionDisabled = encSpi instanceof NoopEncryptionSpi;
 
         //This happen on offline WAL iteration(we don't have encryption keys available).
         if (encSpi != null)
@@ -228,7 +233,7 @@ public class RecordDataV1Serializer implements RecordDataSerializer {
      * @return {@code True} if this record should be encrypted.
      */
     private boolean needEncryption(WALRecord rec) {
-        if (encSpi instanceof NoopEncryptionSpi)
+        if (encryptionDisabled)
             return false;
 
         if (!(rec instanceof WalRecordCacheGroupAware))
@@ -242,7 +247,7 @@ public class RecordDataV1Serializer implements RecordDataSerializer {
      * @return {@code True} if this record should be encrypted.
      */
     private boolean needEncryption(int grpId) {
-        if (encSpi instanceof NoopEncryptionSpi)
+        if (encryptionDisabled)
             return false;
 
         return encMgr.groupKey(grpId) != null;
@@ -1923,6 +1928,9 @@ public class RecordDataV1Serializer implements RecordDataSerializer {
      * @return Real record type.
      */
     RecordType recordType(WALRecord rec) {
+        if (encryptionDisabled)
+            return rec.type();
+
         if (needEncryption(rec))
             return ENCRYPTED_RECORD;
 
@@ -1937,6 +1945,9 @@ public class RecordDataV1Serializer implements RecordDataSerializer {
      * @return {@code True} if this data record should be encrypted.
      */
     boolean isDataRecordEncrypted(DataRecord rec) {
+        if (encryptionDisabled)
+            return false;
+
         for (DataEntry e : rec.writeEntries()) {
             if(needEncryption(cctx.cacheContext(e.cacheId()).groupId()))
                 return true;
@@ -2014,7 +2025,7 @@ public class RecordDataV1Serializer implements RecordDataSerializer {
         for (DataEntry entry : dataRec.writeEntries()) {
             int clSz = entrySize(entry);
 
-            if (needEncryption(cctx.cacheContext(entry.cacheId()).groupId()))
+            if (!encryptionDisabled && needEncryption(cctx.cacheContext(entry.cacheId()).groupId()))
                 sz += encSpi.encryptedSize(clSz) + 1 /* encrypted flag */ + 4 /* groupId */ + 4 /* data size */;
             else {
                 sz += clSz;
