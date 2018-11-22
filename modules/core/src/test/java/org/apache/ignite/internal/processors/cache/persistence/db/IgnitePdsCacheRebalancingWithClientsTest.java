79a16fa66a22fa36af751f50d24d8cee752dc75a
Sergey Chugunov <sergey.chugunov@gmail.com>
2018-11-22 18:37:11 +0300

    IGNITE-10374 test for client joining during full rebalancing

---
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/db/IgnitePdsCacheRebalancingWithClientsTest.java
@@ -0,0 +1,173 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.ignite.internal.processors.cache.persistence.db;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.IgniteSystemProperties;
+import org.apache.ignite.cache.CacheAtomicityMode;
+import org.apache.ignite.cache.CacheMode;
+import org.apache.ignite.cache.affinity.rendezvous.RendezvousAffinityFunction;
+import org.apache.ignite.cluster.ClusterNode;
+import org.apache.ignite.configuration.CacheConfiguration;
+import org.apache.ignite.configuration.DataRegionConfiguration;
+import org.apache.ignite.configuration.DataStorageConfiguration;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.configuration.WALMode;
+import org.apache.ignite.internal.IgniteEx;
+import org.apache.ignite.internal.TestRecordingCommunicationSpi;
+import org.apache.ignite.internal.processors.cache.distributed.dht.preloader.GridDhtPartitionDemandMessage;
+import org.apache.ignite.internal.util.typedef.internal.U;
+import org.apache.ignite.lang.IgniteBiPredicate;
+import org.apache.ignite.plugin.extensions.communication.Message;
+import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
+
+import static org.apache.ignite.internal.processors.cache.persistence.file.FilePageStoreManager.DFLT_STORE_DIR;
+
+/**
+ * A test class for any tests that check rebalancing process in presence of client nodes and
+ * IGNITE_DISABLE_WAL_DURING_REBALANCING optimization enabled.
+ */
+public class IgnitePdsCacheRebalancingWithClientsTest extends GridCommonAbstractTest {
+    /** Block message predicate to set to Communication SPI in node configuration. */
+    private IgniteBiPredicate<ClusterNode, Message> blockMessagePredicate;
+
+    /** {@inheritDoc} */
+    @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
+        IgniteConfiguration cfg = super.getConfiguration(igniteInstanceName);
+
+        CacheConfiguration ccfg1 = new CacheConfiguration("cache1")
+            .setAtomicityMode(CacheAtomicityMode.ATOMIC)
+            .setCacheMode(CacheMode.REPLICATED)
+            .setAffinity(new RendezvousAffinityFunction(false, 8));
+
+        CacheConfiguration ccfg2 = new CacheConfiguration("cache2")
+            .setBackups(1)
+            .setAtomicityMode(CacheAtomicityMode.TRANSACTIONAL)
+            .setCacheMode(CacheMode.PARTITIONED)
+            .setAffinity(new RendezvousAffinityFunction(false, 16));
+
+        CacheConfiguration ccfg3 = new CacheConfiguration("cache3")
+            .setBackups(1)
+            .setAtomicityMode(CacheAtomicityMode.ATOMIC)
+            .setCacheMode(CacheMode.PARTITIONED)
+            .setAffinity(new RendezvousAffinityFunction(false, 32));
+
+        cfg.setCacheConfiguration(ccfg1, ccfg2, ccfg3);
+
+        if ("client".equals(igniteInstanceName))
+            cfg.setClientMode(true);
+        else {
+            DataStorageConfiguration dsCfg = new DataStorageConfiguration()
+                .setConcurrencyLevel(Runtime.getRuntime().availableProcessors() * 4)
+                .setWalMode(WALMode.LOG_ONLY)
+                .setDefaultDataRegionConfiguration(
+                    new DataRegionConfiguration()
+                        .setPersistenceEnabled(true)
+                        .setMaxSize(256 * 1024 * 1024));
+
+            cfg.setDataStorageConfiguration(dsCfg);
+        }
+
+        TestRecordingCommunicationSpi commSpi = new TestRecordingCommunicationSpi();
+        commSpi.blockMessages(blockMessagePredicate);
+
+        cfg.setCommunicationSpi(commSpi);
+
+
+        return cfg;
+    }
+
+    /**
+     *
+     */
+    public void testClientJoinsDuringRebalancing() throws Exception {
+        Ignite ig0 = startGrids(2);
+
+        ig0.active(true);
+
+        for (int i = 0; i < 3; i++)
+            fillCache(ig0.getOrCreateCache("cache" + i));
+
+        String ig1Name = "node01-" + grid(1).localNode().consistentId();
+
+        stopGrid(1);
+
+        cleanPersistenceFiles(ig1Name);
+
+        int groupId = ((IgniteEx) ig0).cachex("cache3").context().groupId();
+
+        blockMessagePredicate = (node, msg) -> {
+            if (msg instanceof GridDhtPartitionDemandMessage)
+                return ((GridDhtPartitionDemandMessage) msg).groupId() == groupId;
+
+            return false;
+        };
+
+        IgniteEx ig1 = startGrid(1);
+
+        startGrid("client");
+
+        stopGrid("client");
+
+        TestRecordingCommunicationSpi commSpi = (TestRecordingCommunicationSpi) ig1
+            .configuration().getCommunicationSpi();
+
+        commSpi.stopBlock();
+
+        Thread.sleep(30_000);
+    }
+
+    private void cleanPersistenceFiles(String igName) throws Exception {
+        String ig1DbPath = Paths.get(DFLT_STORE_DIR, igName).toString();
+
+        File igDbDir = U.resolveWorkDirectory(U.defaultWorkDirectory(), ig1DbPath, false);
+
+        U.delete(igDbDir);
+        Files.createDirectory(igDbDir.toPath());
+
+        String ig1DbWalPath = Paths.get(DFLT_STORE_DIR, "wal", igName).toString();
+
+        U.delete(U.resolveWorkDirectory(U.defaultWorkDirectory(), ig1DbWalPath, false));
+    }
+
+    private void fillCache(IgniteCache cache) {
+        for (int i = 0; i < 2_000; i++)
+            cache.put(i, "value_" + i);
+    }
+
+    /** {@inheritDoc} */
+    @Override protected void beforeTest() throws Exception {
+        super.beforeTest();
+
+        cleanPersistenceDir();
+
+        System.setProperty(IgniteSystemProperties.IGNITE_DISABLE_WAL_DURING_REBALANCING, "true");
+    }
+
+    /** {@inheritDoc} */
+    @Override protected void afterTest() throws Exception {
+        stopAllGrids();
+
+        cleanPersistenceDir();
+
+        System.clearProperty(IgniteSystemProperties.IGNITE_DISABLE_WAL_DURING_REBALANCING);
+    }
+}
