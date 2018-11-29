d699ebcc32b4137f269cc41a7095b4614366b10c
Roman Guseinov <gromcase@gmail.com>
2018-11-29 16:40:46 +0300

    IGNITE-10354 Failing client node due to not receiving metrics updates - Fixes #5485.
    Signed-off-by: Ilya Kasnacheev <ilya.kasnacheev@gmail.com>
    
    (cherry picked from commit d4496c7)

--- modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/ServerImpl.java
+++ modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/ServerImpl.java
@@ -5263,6 +5263,9 @@ class ServerImpl extends TcpDiscoveryImpl {
                             if (clientNodeIds.contains(clientNode.id()))
                                 clientNode.clientAliveTime(spi.clientFailureDetectionTimeout());
                             else {
+                                if (clientNode.clientAliveTime() == 0L)
+                                    clientNode.clientAliveTime(spi.clientFailureDetectionTimeout());
+
                                 boolean aliveCheck = clientNode.isClientAlive();
 
                                 if (!aliveCheck && isLocalNodeCoordinator()) {
