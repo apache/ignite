eeeba1cc2f9d9dc02065c19c7c1079008bf73e00
Oleg Ignatenko <oignatenko@gridgain.com>
2018-12-03 03:26:13 +0300

    IGNITE-10175 migrate core module tests from Junit 3 to 4 - wip - migrating -- verified with diffs overview

--- modules/core/src/test/java/org/apache/ignite/client/SslParametersTest.java
+++ modules/core/src/test/java/org/apache/ignite/client/SslParametersTest.java
@@ -27,10 +27,14 @@ import org.apache.ignite.ssl.SslContextFactory;
 import org.apache.ignite.testframework.GridTestUtils;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.jetbrains.annotations.NotNull;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 /**
  * Tests cases when node connects to cluster with different set of cipher suites.
  */
+@RunWith(JUnit4.class)
 public class SslParametersTest extends GridCommonAbstractTest {
 
     public static final String TEST_CACHE_NAME = "TEST";
