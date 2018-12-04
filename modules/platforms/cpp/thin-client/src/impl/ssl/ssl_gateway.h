b920442b2e3a68376bf48de4bcc98c98183ae2f5
Igor Sapego <isapego@apache.org>
2018-12-04 20:25:34 +0300

    IGNITE-8626: Added OpenSSL 1.1 support for Windows

--- modules/platforms/cpp/thin-client/src/impl/ssl/ssl_gateway.h
+++ modules/platforms/cpp/thin-client/src/impl/ssl/ssl_gateway.h
@@ -18,8 +18,13 @@
 #ifndef _IGNITE_IMPL_THIN_SSL_SSL_LIBRARY
 #define _IGNITE_IMPL_THIN_SSL_SSL_LIBRARY
 
+#include <openssl/ssl.h>
+#include <openssl/conf.h>
+#include <openssl/err.h>
+
 #include <ignite/common/concurrent.h>
 #include <ignite/common/dynamic_load_os.h>
+
 
 namespace ignite
 {
@@ -34,6 +39,7 @@ namespace ignite
                  */
                 struct SslFunctions
                 {
+                    void *fpSSLeay_version;
                     void *fpSSL_CTX_new;
                     void *fpSSL_CTX_free;
                     void *fpSSL_CTX_set_verify;
@@ -65,6 +71,12 @@ namespace ignite
                     void *fpBIO_ctrl;
                     void *fpERR_get_error;
                     void *fpERR_error_string_n;
+                    void *fpERR_print_errors_fp;
+
+                    void *fpOpenSSL_version;
+                    void *fpSSL_CTX_set_options;
+                    void *fpOPENSSL_init_ssl;
+                    void *fpTLS_client_method;
                 };
 
                 /**
@@ -104,6 +116,88 @@ namespace ignite
                         return inited;
                     }
 
+                    char* SSLeay_version_(int type);
+
+                    int OPENSSL_init_ssl_(uint64_t opts, const OPENSSL_INIT_SETTINGS *settings);
+
+                    long SSL_CTX_set_options_(SSL_CTX* ctx, long options);
+
+                    long SSL_CTX_ctrl_(SSL_CTX* ctx, int cmd, long larg, void* parg);
+
+                    SSL_CTX* SSL_CTX_new_(const SSL_METHOD* meth);
+
+                    void SSL_CTX_free_(SSL_CTX* ctx);
+
+                    void SSL_CTX_set_verify_(SSL_CTX* ctx, int mode, int (*callback)(int, X509_STORE_CTX*));
+
+                    void SSL_CTX_set_verify_depth_(SSL_CTX* ctx, int depth);
+
+                    int SSL_CTX_load_verify_locations_(SSL_CTX* ctx, const char* cAfile, const char* cApath);
+
+                    int SSL_CTX_use_certificate_chain_file_(SSL_CTX* ctx, const char* file);
+
+                    int SSL_CTX_use_RSAPrivateKey_file_(SSL_CTX* ctx, const char* file, int type);
+
+                    int SSL_CTX_set_cipher_list_(SSL_CTX* ctx, const char* str);
+
+                    long SSL_get_verify_result_(const SSL* s);
+
+                    int SSL_library_init_();
+
+                    void SSL_load_error_strings_();
+
+                    X509* SSL_get_peer_certificate_(const SSL* s);
+
+                    long SSL_ctrl_(SSL* s, int cmd, long larg, void* parg);
+
+                    long SSL_set_tlsext_host_name_(SSL* s, const char* name);
+
+                    void SSL_set_connect_state_(SSL* s);
+
+                    int SSL_connect_(SSL* s);
+
+                    int SSL_get_error_(const SSL* s, int ret);
+
+                    int SSL_want_(const SSL* s);
+
+                    int SSL_write_(SSL* s, const void* buf, int num);
+
+                    int SSL_read_(SSL* s, void* buf, int num);
+
+                    int SSL_pending_(const SSL* ssl);
+
+                    int SSL_get_fd_(const SSL* ssl);
+
+                    void SSL_free_(SSL* ssl);
+
+                    const SSL_METHOD* SSLv23_client_method_();
+
+                    const SSL_METHOD* TLS_client_method_();
+
+                    void OPENSSL_config_(const char* configName);
+
+                    void X509_free_(X509* a);
+
+                    BIO* BIO_new_ssl_connect_(SSL_CTX* ctx);
+
+                    void BIO_free_all(BIO* a);
+
+                    long BIO_ctrl(BIO* bp, int cmd, long larg, void* parg);
+
+                    long BIO_get_fd_(BIO* bp, int* fd);
+
+                    long BIO_get_ssl_(BIO* bp, SSL** ssl);
+
+                    long BIO_set_nbio_(BIO* bp, long n);
+
+                    long BIO_set_conn_hostname_(BIO* bp, const char* name);
+
+                    unsigned long ERR_get_error_();
+
+                    void ERR_error_string_n_(unsigned long e, char* buf, size_t len);
+
+                    void ERR_print_errors_fp_(FILE *fd);
+
                 private:
                     /**
                      * Constructor.
@@ -114,6 +208,11 @@ namespace ignite
                      * Destructor.
                      */
                     ~SslGateway();
+
+                    /**
+                     * Unload all SSL symbols.
+                     */
+                    void UnloadAll();
 
                     /**
                      * Load SSL library.
@@ -128,9 +227,27 @@ namespace ignite
                     void LoadSslLibraries();
 
                     /**
-                     * Load SSL method.
+                     * Load mandatory SSL methods.
+                     *
+                     * @throw IgniteError if can not load one of the functions.
+                     */
+                    void LoadMandatoryMethods();
+
+                    /**
+                     * Try load SSL method.
+                     *
                      * @param name Name.
                      * @return Method pointer.
+                     */
+                    void* TryLoadSslMethod(const char* name);
+
+                    /**
+                     * Load SSL method.
+                     *
+                     * @param name Name.
+                     * @return Method pointer.
+                     *
+                     * @throw IgniteError if the method is not present.
                      */
                     void* LoadSslMethod(const char* name);
 
@@ -145,6 +262,9 @@ namespace ignite
 
                     /** ssleay32 module. */
                     common::dynamic::Module ssleay32;
+
+                    /** libcrypto module. */
+                    common::dynamic::Module libcrypto;
 
                     /** libssl module. */
                     common::dynamic::Module libssl;
