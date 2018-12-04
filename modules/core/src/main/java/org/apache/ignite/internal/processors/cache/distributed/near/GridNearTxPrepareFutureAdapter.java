125420e55c6a223a40e78e20d32b86263d8b204e
Ivan Rakov <irakov@apache.org>
2018-12-04 14:01:38 +0300

    1PC is enabled again

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/near/GridNearTxPrepareFutureAdapter.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/near/GridNearTxPrepareFutureAdapter.java
@@ -185,8 +185,8 @@ public abstract class GridNearTxPrepareFutureAdapter extends
 
             Collection<UUID> backups = entry.getValue();
 
-//            if (backups.size() <= 1)
-//                tx.onePhaseCommit(true);
+            if (backups.size() <= 1)
+                tx.onePhaseCommit(true);
         }
     }
 
