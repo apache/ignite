1ebdf5ae5b59a8780003db8a6deac0e2d259bab9
tledkov <tledkov@gridgain.com>
2018-12-06 18:41:50 +0300

    IGNITE-10580: add test to reproduce

---
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/SqlLocalQueryConnectionAndStatementTest.java
@@ -0,0 +1,110 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.ignite.internal.processors.query;
+
+import java.util.Iterator;
+import java.util.List;
+import org.apache.ignite.cache.query.FieldsQueryCursor;
+import org.apache.ignite.cache.query.SqlFieldsQuery;
+import org.apache.ignite.configuration.ClientConnectorConfiguration;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.internal.util.typedef.internal.U;
+import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
+
+/**
+ * Test for statement reuse.
+ */
+public class SqlLocalQueryConnectionAndStatementTest extends GridCommonAbstractTest {
+    /** Url. */
+    final String URL = "jdbc:ignite:thin://localhost";
+
+    /** Test duration. */
+    final long TEST_DUR = 10_000;
+
+    /** {@inheritDoc} */
+    public void beforeTest() throws Exception {
+        super.beforeTest();
+
+        startGrids(1);
+    }
+
+    /** {@inheritDoc} */
+    @Override protected void afterTest() throws Exception {
+        stopAllGrids();
+
+        super.afterTest();
+    }
+
+    /** {@inheritDoc} */
+    @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
+        return super.getConfiguration(igniteInstanceName)
+            .setClientConnectorConfiguration(new ClientConnectorConfiguration()
+                .setThreadPoolSize(1));
+    }
+
+    /**
+     */
+    public void testReplicated() {
+        sql("CREATE TABLE repl_tbl (id LONG PRIMARY KEY, val LONG) WITH \"template=replicated\"").getAll();
+
+        for (int i = 0; i < 10; i++)
+            sql("insert into repl_tbl(id,val) VALUES(" + i + "," + i + ")").getAll();
+
+        Iterator<List<?>> it0 = sql("SELECT * FROM repl_tbl").iterator();
+
+        it0.next();
+
+        sql("SELECT * FROM repl_tbl").getAll();
+
+        it0.next();
+    }
+
+    /**
+     */
+    public void testLocalQuery() {
+        sql("CREATE TABLE tbl (id LONG PRIMARY KEY, val LONG)").getAll();
+
+        for (int i = 0; i < 10; i++)
+            sql("insert into tbl(id,val) VALUES(" + i + "," + i + ")").getAll();
+
+        Iterator<List<?>> it0 = sql(new SqlFieldsQuery("SELECT * FROM tbl").setLocal(true)).iterator();
+
+        it0.next();
+
+        sql(new SqlFieldsQuery("SELECT * FROM tbl").setLocal(true)).getAll();
+
+        it0.next();
+    }
+
+    /**
+     * @param sql SQL query.
+     * @return Results.
+     */
+    private FieldsQueryCursor<List<?>> sql(String sql) {
+        return sql(new SqlFieldsQuery(sql));
+    }
+
+    /**
+     * @param qry SQL query.
+     * @return Results.
+     */
+    private FieldsQueryCursor<List<?>> sql(SqlFieldsQuery qry) {
+        GridQueryProcessor qryProc = grid(0).context().query();
+
+        return qryProc.querySqlFields(qry, true);
+    }
+}
