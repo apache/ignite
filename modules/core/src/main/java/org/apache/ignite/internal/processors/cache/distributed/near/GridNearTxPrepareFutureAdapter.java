0b904406859e2851ec484022044255d32804e3bd
Aleksei Scherbakov <alexey.scherbakoff@gmail.com>
2018-12-10 18:25:15 +0300

    IGNITE-10078 wip.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/near/GridNearTxPrepareFutureAdapter.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/near/GridNearTxPrepareFutureAdapter.java
@@ -178,16 +178,16 @@ public abstract class GridNearTxPrepareFutureAdapter extends
         if (tx.storeWriteThrough() || tx.txState().mvccEnabled()) // TODO IGNITE-3479 (onePhase + mvcc)
             return;
 
-        if (txNodes.size() == 1) {
-            Map.Entry<UUID, Collection<UUID>> entry = txNodes.entrySet().iterator().next();
-
-            assert entry != null;
-
-            Collection<UUID> backups = entry.getValue();
-
-            if (backups.size() <= 1)
-                tx.onePhaseCommit(true);
-        }
+//        if (txNodes.size() == 1) {
+//            Map.Entry<UUID, Collection<UUID>> entry = txNodes.entrySet().iterator().next();
+//
+//            assert entry != null;
+//
+//            Collection<UUID> backups = entry.getValue();
+//
+//            if (backups.size() <= 1)
+//                tx.onePhaseCommit(true);
+//        }
     }
 
     /**
