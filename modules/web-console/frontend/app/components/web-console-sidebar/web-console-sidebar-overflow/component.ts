cf8ce9f3e738c46538075cfd2b20973a92e995b8
Ilya Borisov <Klaster1@gmail.com>
2018-12-10 12:53:26 +0700

    IGNITE-10239 Handle navigation items overflow.

--- modules/web-console/frontend/app/components/web-console-sidebar/web-console-sidebar-overflow/component.ts
+++ modules/web-console/frontend/app/components/web-console-sidebar/web-console-sidebar-overflow/component.ts
@@ -15,12 +15,12 @@
  * limitations under the License.
  */
 
+import controller from './controller';
 import template from './template.pug';
-import {component as overflow} from './web-console-sidebar-overflow/component';
-import {component as nav} from './web-console-sidebar-navigation/component';
-import {component as sidebar} from './component';
+import './style.scss';
 
-export default angular.module('sidebar', [])
-    .component('webConsoleSidebarOverflow', overflow)
-    .component('webConsoleSidebarNavigation', nav)
-    .component('webConsoleSidebar', sidebar);
+export const component: ng.IComponentOptions =  {
+    controller,
+    transclude: true,
+    template
+};
