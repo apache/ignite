f6a23d7f88076fdec7f7ff27ca22154dff816beb
Anton Kalashnikov <kaa.dev@yandex.ru>
2018-12-11 15:39:42 +0300

    IGNITE-10622 Fixed missed discovery event in the case of the next node failure - Fixes #5628.
    Signed-off-by: Alexey Goncharuk <alexey.goncharuk@gmail.com>

--- modules/core/src/test/java/org/apache/ignite/spi/discovery/tcp/TcpDiscoveryPendingMessageDeliveryTest.java
+++ modules/core/src/test/java/org/apache/ignite/spi/discovery/tcp/TcpDiscoveryPendingMessageDeliveryTest.java
@@ -24,6 +24,7 @@ import java.util.Set;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCheckedException;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.internal.IgniteEx;
 import org.apache.ignite.internal.managers.discovery.CustomMessageWrapper;
 import org.apache.ignite.internal.managers.discovery.DiscoCache;
 import org.apache.ignite.internal.managers.discovery.DiscoveryCustomMessage;
@@ -76,6 +77,8 @@ public class TcpDiscoveryPendingMessageDeliveryTest extends GridCommonAbstractTe
             disco = new DyingDiscoverySpi();
         else if (igniteInstanceName.startsWith("listener"))
             disco = new ListeningDiscoverySpi();
+        else if (igniteInstanceName.startsWith("receiver"))
+            disco = new DyingThreadDiscoverySpi();
         else
             disco = new TcpDiscoverySpi();
 
@@ -191,11 +194,63 @@ public class TcpDiscoveryPendingMessageDeliveryTest extends GridCommonAbstractTe
     }
 
     /**
+     * @throws Exception If failed.
+     */
+    public void testDeliveryAllFailedMessagesInCorrectOrder() throws Exception {
+        IgniteEx coord = startGrid("coordinator");
+        TcpDiscoverySpi coordDisco = (TcpDiscoverySpi)coord.configuration().getDiscoverySpi();
+
+        Set<TcpDiscoveryAbstractMessage> sentEnsuredMsgs = new GridConcurrentHashSet<>();
+        coordDisco.addSendMessageListener(msg -> {
+            if (coordDisco.ensured(msg))
+                sentEnsuredMsgs.add(msg);
+        });
+
+        //Node which receive message but will not send it further around the ring.
+        IgniteEx receiver = startGrid("receiver");
+
+        //Node which will be failed first.
+        IgniteEx dummy = startGrid("dummy");
+
+        //Node which should received all fail message in any way.
+        startGrid("listener");
+
+        sentEnsuredMsgs.clear();
+        receivedEnsuredMsgs.clear();
+
+        blockMsgs = true;
+
+        log.info("Sending fail node messages");
+
+        coord.context().discovery().failNode(dummy.localNode().id(), "Dummy node failed");
+        coord.context().discovery().failNode(receiver.localNode().id(), "Receiver node failed");
+
+        boolean delivered = GridTestUtils.waitForCondition(() -> {
+            log.info("Waiting for messages delivery");
+
+            return receivedEnsuredMsgs.equals(sentEnsuredMsgs);
+        }, 5000);
+
+        assertTrue("Sent: " + sentEnsuredMsgs + "; received: " + receivedEnsuredMsgs, delivered);
+    }
+
+    /**
      * @param disco Discovery SPI.
      * @param id Message id.
      */
     private void sendDummyCustomMessage(TcpDiscoverySpi disco, IgniteUuid id) {
         disco.sendCustomEvent(new CustomMessageWrapper(new DummyCustomDiscoveryMessage(id)));
+    }
+
+    /**
+     * Discovery SPI, that makes a thread to die when {@code blockMsgs} is set to {@code true}.
+     */
+    private class DyingThreadDiscoverySpi extends TcpDiscoverySpi {
+        /** {@inheritDoc} */
+        @Override protected void startMessageProcess(TcpDiscoveryAbstractMessage msg) {
+            if (blockMsgs)
+                throw new RuntimeException("Thread is dying");
+        }
     }
 
     /**
