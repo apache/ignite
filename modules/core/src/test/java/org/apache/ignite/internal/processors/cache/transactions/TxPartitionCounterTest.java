8948804cebb8ff409ef4d700f0d52d589cb4bf16
Aleksei Scherbakov <alexey.scherbakoff@gmail.com>
2018-12-11 18:53:41 +0300

    IGNITE-10078 wip.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/transactions/TxPartitionCounterTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/transactions/TxPartitionCounterTest.java
@@ -25,10 +25,8 @@ public class TxPartitionCounterTest extends TxSinglePartitionAbstractTest {
 
                         // Order prepares.
                         if (futs.size() == 3) {
-                            for (Integer integer : txMap.keySet()) {
-                                GridFutureAdapter<?> fut = futs.get(txMap.get(integer));
-                                fut.onDone();
-                            }
+                            GridFutureAdapter<?> fut = futs.get(txMap.get(2));
+                            fut.onDone();
                         }
                     }
                 });
@@ -39,37 +37,44 @@ public class TxPartitionCounterTest extends TxSinglePartitionAbstractTest {
             @Override public boolean onAfterPrimaryPrepare(IgniteEx node, IgniteInternalTx tx,
                 GridFutureAdapter<?> proceedFut) {
 
-//                switch (futs.size()) {
-//                    case 3:
-//                        assertEquals(txMap.get(2), tx.nearXidVersion().asGridUuid());
-//                        futs.remove(txMap.get(2));
-//                        futs.get(txMap.get(1)).onDone();
-//
-//                        break;
-//                    case 2:
-//                        assertEquals(txMap.get(1), tx.nearXidVersion().asGridUuid());
-//                        futs.remove(txMap.get(1));
-//                        futs.get(txMap.get(0)).onDone();
-//
-//                        break;
-//                    case 1:
-//                        assertEquals(txMap.get(0), tx.nearXidVersion().asGridUuid());
-//                        futs.remove(txMap.get(0));
-//
-//                        break;
-//                }
+                GridTestUtils.runAsync(new Runnable() {
+                    @Override public void run() {
+                        switch (futs.size()) {
+                            case 3:
+                                assertEquals(txMap.get(2), tx.nearXidVersion().asGridUuid());
+                                futs.remove(txMap.get(2));
+                                futs.get(txMap.get(1)).onDone();
+
+                                break;
+                            case 2:
+                                assertEquals(txMap.get(1), tx.nearXidVersion().asGridUuid());
+                                futs.remove(txMap.get(1));
+                                futs.get(txMap.get(0)).onDone();
+
+                                break;
+                            case 1:
+                                assertEquals(txMap.get(0), tx.nearXidVersion().asGridUuid());
+                                futs.remove(txMap.get(0));
+
+                                break;
+                        }
+                    }
+                });
 
                 return false;
             }
 
-            @Override public boolean onBeforePrimaryFinish(IgniteEx n, IgniteInternalTx tx, GridFutureAdapter<?> proceedFut) {
+            @Override public boolean onBeforePrimaryFinish(IgniteEx n, IgniteInternalTx tx, GridFutureAdapter<?>
+                proceedFut) {
                 return false;
             }
 
             @Override public void onTxStart(Transaction tx, int idx) {
                 txMap.put(idx, tx.xid());
             }
-        }, 5, 7, 3);
+        }
+
+            , 5, 7, 3);
 
         int size = grid("client").cache(DEFAULT_CACHE_NAME).size();
 
