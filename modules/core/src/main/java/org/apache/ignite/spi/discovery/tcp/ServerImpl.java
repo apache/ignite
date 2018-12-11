5cef3be4325eb0a5869f8ed26f22130782a0ce08
Anton Kalashnikov <kaa.dev@yandex.ru>
2018-12-11 15:39:42 +0300

    IGNITE-10622 Fixed missed discovery event in the case of the next node failure - Fixes #5628.
    Signed-off-by: Alexey Goncharuk <alexey.goncharuk@gmail.com>

--- modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/ServerImpl.java
+++ modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/ServerImpl.java
@@ -3227,7 +3227,7 @@ class ServerImpl extends TcpDiscoveryImpl {
 
                             assert !forceSndPending || msg instanceof TcpDiscoveryNodeLeftMessage;
 
-                            if (failure || forceSndPending) {
+                            if (failure || forceSndPending || newNextNode) {
                                 if (log.isDebugEnabled())
                                     log.debug("Pending messages will be sent [failure=" + failure +
                                         ", newNextNode=" + newNextNode +
