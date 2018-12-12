fe9719f7e16d35ffcfd448d2622c39e46bd01503
Aleksei Scherbakov <alexey.scherbakoff@gmail.com>
2018-12-12 14:49:00 +0300

    IGNITE-10078 wip.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/transactions/TxPartitionCounterTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/transactions/TxPartitionCounterTest.java
@@ -1,5 +1,7 @@
 package org.apache.ignite.internal.processors.cache.transactions;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
@@ -7,35 +9,42 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.atomic.AtomicInteger;
+import org.apache.ignite.IgniteCheckedException;
+import org.apache.ignite.IgniteException;
 import org.apache.ignite.internal.IgniteEx;
+import org.apache.ignite.internal.IgniteInternalFuture;
 import org.apache.ignite.internal.processors.cache.PartitionUpdateCounter;
 import org.apache.ignite.internal.processors.cache.distributed.dht.topology.GridDhtInvalidPartitionException;
 import org.apache.ignite.internal.processors.cache.distributed.dht.topology.GridDhtLocalPartition;
 import org.apache.ignite.internal.util.future.GridFutureAdapter;
+import org.apache.ignite.lang.IgniteInClosure;
 import org.apache.ignite.lang.IgniteUuid;
 import org.apache.ignite.testframework.GridTestUtils;
 import org.apache.ignite.transactions.Transaction;
 import org.jetbrains.annotations.Nullable;
 
-import static org.apache.ignite.testframework.GridTestUtils.runAsync;
-
 /**
  */
 public class TxPartitionCounterTest extends TxSinglePartitionAbstractTest {
+    /** Futures tracking map. */
+    private Queue<IgniteInternalFuture<?>> taskFuts = new ConcurrentLinkedQueue<>();
+
     public void test() throws Exception {
         Map<Integer, IgniteUuid> txMap = new ConcurrentHashMap<>();
 
         Map<IgniteUuid, GridFutureAdapter<?>> futs = new ConcurrentHashMap<>();
 
+        int[] prepOrd = new int[] {1, 2, 0};
+        int[] commitOrd = new int[] {2, 1, 0};
+        int[] sizes = new int[] {5, 7, 3};
+
         Queue<Integer> prepOrder = new ConcurrentLinkedQueue<>();
-        prepOrder.add(1);
-        prepOrder.add(2);
-        prepOrder.add(0);
+        for (int i = 0; i < prepOrd.length; i++)
+            prepOrder.add(prepOrd[i]);
 
         Queue<Integer> commitOrder = new ConcurrentLinkedQueue<>();
-        commitOrder.add(2);
-        commitOrder.add(1);
-        commitOrder.add(0);
+        for (int i = 0; i < commitOrd.length; i++)
+            commitOrder.add(commitOrd[i]);
 
         int partId = 0;
         int backups = 2;
@@ -63,13 +72,21 @@ public class TxPartitionCounterTest extends TxSinglePartitionAbstractTest {
 
                 runAsync(new Runnable() {
                     @Override public void run() {
+                        if (prepOrder.isEmpty())
+                            return;
+
                         futs.remove(txMap.get(prepOrder.poll())).onDone();
 
                         if (prepOrder.isEmpty()) {
                             GridDhtLocalPartition part = internalCache(0).context().topology().localPartition(0);
                             PartitionUpdateCounter cntr = part.dataStore().partUpdateCounter();
 
-                            System.out.println();
+                            int i = 0;
+                            for (PartitionUpdateCounter.Item item : cntr.holes()) {
+                                assertEquals(sizes[prepOrd[i]], item.delta());
+
+                                i++;
+                            }
                         }
                     }
                 });
@@ -96,6 +113,9 @@ public class TxPartitionCounterTest extends TxSinglePartitionAbstractTest {
             @Override public boolean onAfterPrimaryFinish(IgniteEx n, IgniteUuid ver, GridFutureAdapter<?> proceedFut) {
                 runAsync(new Runnable() {
                     @Override public void run() {
+                        if (commitOrder.isEmpty())
+                            return;
+
                         futs.remove(txMap.get(commitOrder.poll())).onDone();
                     }
                 });
@@ -106,7 +126,7 @@ public class TxPartitionCounterTest extends TxSinglePartitionAbstractTest {
             @Override public void onTxStart(Transaction tx, int idx) {
                 txMap.put(idx, tx.xid());
             }
-        }, 5, 7, 3);
+        }, sizes);
 
         int size = grid("client").cache(DEFAULT_CACHE_NAME).size();
 
@@ -115,6 +135,17 @@ public class TxPartitionCounterTest extends TxSinglePartitionAbstractTest {
         @Nullable GridDhtLocalPartition part = internalCache(0).context().topology().localPartition(0);
         PartitionUpdateCounter cntr = part.dataStore().partUpdateCounter();
 
-        System.out.println();
+        // Check futures are executed without errors.
+        for (IgniteInternalFuture<?> fut : taskFuts)
+            fut.get();
+    }
+
+    /**
+     * @param r Runnable.
+     */
+    public void runAsync(Runnable r) {
+        IgniteInternalFuture fut = GridTestUtils.runAsync(r);
+
+        taskFuts.add(fut);
     }
 }
