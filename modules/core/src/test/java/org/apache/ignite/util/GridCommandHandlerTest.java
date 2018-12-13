6b395874cfa8f923e0227da0138798dcbbc9a4bc
vd-pyatkov <vpyatkov@gridgain.com>
2018-12-13 14:30:45 +0300

    IGNITE-10446 control.sh --cache idle_verify fail with NPE when node left grid (resolve)

--- modules/core/src/test/java/org/apache/ignite/util/GridCommandHandlerTest.java
+++ modules/core/src/test/java/org/apache/ignite/util/GridCommandHandlerTest.java
@@ -19,6 +19,7 @@ package org.apache.ignite.util;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
