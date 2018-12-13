37da807b4908d27464ac156fb5b82f41d88bf801
Rodion4ik <rodion.vlasov@mail.ru>
2018-12-13 19:01:26 +0300

    StopAllGrids(true) in TearDown
    StopAllGrids(true) in TearDown

--- modules/core/src/test/java/org/apache/ignite/testframework/junits/GridAbstractTest.java
+++ modules/core/src/test/java/org/apache/ignite/testframework/junits/GridAbstractTest.java
@@ -1836,7 +1836,7 @@ public abstract class GridAbstractTest extends TestCase {
                 }
 
                 if (isSafeTopology()) {
-                    stopAllGrids(cancelJobsAfterTestsStopped());
+                    stopAllGrids();
 
                     if (stopGridErr) {
                         err = new RuntimeException("Not all Ignite instances has been stopped. " +
