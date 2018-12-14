b5a8ac8becc4225dae2e0fee5c1607c61a656677
rkondakov <kondakov87@mail.ru>
2018-12-14 14:08:55 +0300

    IGNITE-10464: MVCC: Create "PDS 4" test suite for MVCC mode. This closes #5627.
    (cherry picked from commit c7233155368000cd482cd6f206f4dbecf26fd7ec)

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteClusterActivateDeactivateTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteClusterActivateDeactivateTest.java
@@ -358,7 +358,7 @@ public class IgniteClusterActivateDeactivateTest extends GridCommonAbstractTest 
     @Test
     public void testJoinWhileActivate1_Server() throws Exception {
         if (MvccFeatureChecker.forcedMvcc())
-            fail("https://issues.apache.org/jira/browse/IGNITE-10583");
+            fail("https://issues.apache.org/jira/browse/IGNITE-10421");
 
         joinWhileActivate1(false, false);
     }
@@ -369,7 +369,7 @@ public class IgniteClusterActivateDeactivateTest extends GridCommonAbstractTest 
     @Test
     public void testJoinWhileActivate1_WithCache_Server() throws Exception {
         if (MvccFeatureChecker.forcedMvcc())
-            fail("https://issues.apache.org/jira/browse/IGNITE-10583");
+            fail("https://issues.apache.org/jira/browse/IGNITE-10421");
 
         joinWhileActivate1(false, true);
     }
