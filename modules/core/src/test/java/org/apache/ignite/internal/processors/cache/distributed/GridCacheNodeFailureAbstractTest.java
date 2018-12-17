ce36787848a366efa78f8975095895264b8c0a0a
Oleg Ignatenko <oignatenko@gridgain.com>
2018-12-17 21:07:14 +0300

    IGNITE-10177 Junit 3 cleanup - wip - cleanup -- verified with diffs overview and rebuild

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/GridCacheNodeFailureAbstractTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/GridCacheNodeFailureAbstractTest.java
@@ -42,7 +42,6 @@ import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.apache.ignite.transactions.Transaction;
 import org.apache.ignite.transactions.TransactionConcurrency;
 import org.apache.ignite.transactions.TransactionIsolation;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -82,7 +81,6 @@ public abstract class GridCacheNodeFailureAbstractTest extends GridCommonAbstrac
     private static final List<Ignite> IGNITEs = new ArrayList<>();
 
     /** {@inheritDoc} */
-    @Before
     @Override public void setUp() throws Exception {
         MvccFeatureChecker.failIfNotSupported(MvccFeatureChecker.Feature.ENTRY_LOCK);
 
