69bc9e735b1c6f1612d6109fd80b6333ce8e4741
rkondakov <kondakov87@mail.ru>
2018-12-19 14:25:42 +0300

    IGNITE-9774: Test remuted with a proper ticket. This closes #5687.
    (cherry picked from commit ca8a80b)

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/mvcc/CacheMvccTransactionsTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/mvcc/CacheMvccTransactionsTest.java
@@ -1577,6 +1577,7 @@ public class CacheMvccTransactionsTest extends CacheMvccAbstractTest {
      */
     @Ignore("https://issues.apache.org/jira/browse/IGNITE-9774")
     @Test
+    @Ignore("https://issues.apache.org/jira/browse/IGNITE-9949")
     public void testPutAllGetAll_ClientServer_Backups1_Restart_Scan() throws Exception {
         putAllGetAll(RestartMode.RESTART_RND_SRV, 4, 2, 1, 64, null, SCAN, PUT);
     }
