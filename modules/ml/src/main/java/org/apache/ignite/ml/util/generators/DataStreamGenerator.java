376e702f85292e33f35eb76fa92544cf26f786f2
Alexey Platonov <aplatonovv@gmail.com>
2018-12-21 15:23:12 +0300

    javadocs WIP

--- modules/ml/src/main/java/org/apache/ignite/ml/util/generators/DataStreamGenerator.java
+++ modules/ml/src/main/java/org/apache/ignite/ml/util/generators/DataStreamGenerator.java
@@ -23,25 +23,44 @@ import java.util.stream.Stream;
 import org.apache.ignite.lang.IgniteBiPredicate;
 import org.apache.ignite.ml.dataset.DatasetBuilder;
 import org.apache.ignite.ml.dataset.UpstreamTransformerBuilder;
-import org.apache.ignite.ml.dataset.impl.local.LocalDatasetBuilder;
 import org.apache.ignite.ml.math.functions.IgniteFunction;
 import org.apache.ignite.ml.math.primitives.vector.Vector;
 import org.apache.ignite.ml.structures.DatasetRow;
 import org.apache.ignite.ml.structures.LabeledVector;
 import org.apache.ignite.ml.util.generators.primitives.scalar.RandomProducer;
 
+/**
+ * Provides general interface for generation of pseudorandom vectors in according to shape defined
+ * by logic of specific data stream generator.
+ */
 public interface DataStreamGenerator {
-    Stream<LabeledVector<Vector, Double>> labeled();
+    /**
+     * @return stream of {@link LabeledVector} in according to dataset shape.
+     */
+    public Stream<LabeledVector<Vector, Double>> labeled();
 
-    default Stream<Vector> unlabeled() {
+    /**
+     * @return stream of unlabeled {@link Vector} in according to dataset shape.
+     */
+    public default Stream<Vector> unlabeled() {
         return labeled().map(DatasetRow::features);
     }
 
-    default Stream<LabeledVector<Vector, Double>> labeled(IgniteFunction<Vector, Double> classifier) {
+    /**
+     * @param classifier user defined classifier for vectors stream.
+     * @return stream of {@link LabeledVector} in according to dataset shape and user's classifier.
+     */
+    public default Stream<LabeledVector<Vector, Double>> labeled(IgniteFunction<Vector, Double> classifier) {
         return labeled().map(DatasetRow::features).map(v -> new LabeledVector<>(v, classifier.apply(v)));
     }
 
-    default DataStreamGenerator mapVectors(IgniteFunction<Vector, Vector> f) {
+    /**
+     * Apply user defined mapper to vectors stream without labels hiding.
+     *
+     * @param f mapper of vectors of data stream.
+     * @return stream of mapped vectors.
+     */
+    public default DataStreamGenerator mapVectors(IgniteFunction<Vector, Vector> f) {
         DataStreamGenerator orig = this;
         return new DataStreamGenerator() {
             @Override public Stream<LabeledVector<Vector, Double>> labeled() {
@@ -50,47 +69,63 @@ public interface DataStreamGenerator {
         };
     }
 
-    default DataStreamGenerator blur(RandomProducer rnd) {
+    /**
+     * @param rnd generator of pseudorandom scalars modifying vector components with label saving.
+     * @return stream of blurred vectors with same labels.
+     */
+    public default DataStreamGenerator blur(RandomProducer rnd) {
         return mapVectors(rnd::noizify);
     }
 
-    default Map<Vector, Double> asMap(int datasetSize) {
+    /**
+     * Convert first N values from stream to map.
+     *
+     * @param datasetSize dataset size.
+     * @return map of vectors and labels.
+     */
+    public default Map<Vector, Double> asMap(int datasetSize) {
         return labeled().limit(datasetSize)
             .collect(Collectors.toMap(DatasetRow::features, LabeledVector::label));
     }
 
-    default DatasetBuilder<Vector, Double> asDatasetBuilder(int datasetSize, int partitions) {
+    /**
+     * Convert first N values from stream to {@link DatasetBuilder}.
+     *
+     * @param datasetSize dataset size.
+     * @param partitions partitions count.
+     * @return dataset builder.
+     */
+    public default DatasetBuilder<Vector, Double> asDatasetBuilder(int datasetSize, int partitions) {
         return new DatasetBuilderAdapter(this, datasetSize, partitions);
     }
 
-    default DatasetBuilder<Vector, Double> asDatasetBuilder(int datasetSize, IgniteBiPredicate<Vector, Double> filter,
-        int partitions, UpstreamTransformerBuilder<Vector, Double> upstreamTransformerBuilder) {
-
-        return new DatasetBuilderAdapter(this, datasetSize, filter, partitions, upstreamTransformerBuilder);
-    }
-
-    default DatasetBuilder<Vector, Double> asDatasetBuilder(int datasetSize, IgniteBiPredicate<Vector, Double> filter,
+    /**
+     * Convert first N values from stream to {@link DatasetBuilder}.
+     *
+     * @param datasetSize dataset size.
+     * @param filter data filter.
+     * @param partitions partitions count.
+     * @return dataset builder.
+     */
+    public default DatasetBuilder<Vector, Double> asDatasetBuilder(int datasetSize, IgniteBiPredicate<Vector, Double> filter,
         int partitions) {
 
         return new DatasetBuilderAdapter(this, datasetSize, filter, partitions);
     }
 
-    public static class DatasetBuilderAdapter extends LocalDatasetBuilder<Vector, Double> {
-        public DatasetBuilderAdapter(DataStreamGenerator generator, int datasetSize, int partitions) {
-            super(generator.asMap(datasetSize), partitions);
-        }
+    /**
+     * Convert first N values from stream to {@link DatasetBuilder}.
+     *
+     * @param datasetSize dataset size.
+     * @param filter data filter.
+     * @param partitions partitions count.
+     * @param upstreamTransformerBuilder upstream transformer builder.
+     * @return dataset builder.
+     */
+    public default DatasetBuilder<Vector, Double> asDatasetBuilder(int datasetSize, IgniteBiPredicate<Vector, Double> filter,
+        int partitions, UpstreamTransformerBuilder<Vector, Double> upstreamTransformerBuilder) {
 
-        public DatasetBuilderAdapter(DataStreamGenerator generator, int datasetSize,
-            IgniteBiPredicate<Vector, Double> filter, int partitions,
-            UpstreamTransformerBuilder<Vector, Double> upstreamTransformerBuilder) {
-
-            super(generator.asMap(datasetSize), filter, partitions, upstreamTransformerBuilder);
-        }
-
-        public DatasetBuilderAdapter(DataStreamGenerator generator, int datasetSize,
-            IgniteBiPredicate<Vector, Double> filter, int partitions) {
-
-            super(generator.asMap(datasetSize), filter, partitions);
-        }
+        return new DatasetBuilderAdapter(this, datasetSize, filter, partitions, upstreamTransformerBuilder);
     }
+
 }
