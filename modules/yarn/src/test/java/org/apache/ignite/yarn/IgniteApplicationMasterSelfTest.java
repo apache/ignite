699f0331907d5eea32d72cca25fda5bfc8849416
Oleg Ignatenko <oignatenko@gridgain.com>
2018-12-22 15:53:01 +0300

    IGNITE-10739 get rid of using JUnit 3 API in IgniteConfigVariationsAbstractTes - formatting corrected per self-review -- verified with diffs overview and rebuild

--- modules/yarn/src/test/java/org/apache/ignite/yarn/IgniteApplicationMasterSelfTest.java
+++ modules/yarn/src/test/java/org/apache/ignite/yarn/IgniteApplicationMasterSelfTest.java
@@ -145,7 +145,7 @@ public class IgniteApplicationMasterSelfTest {
      * @throws Exception If failed.
      */
     @Test
-     public void testMemoryOverHeadPreventAllocation() throws Exception {
+    public void testMemoryOverHeadPreventAllocation() throws Exception {
         rmMock.availableRes(new MockResource(1024, 2));
         appMaster.setRmClient(rmMock);
         appMaster.setNmClient(new NMMock());
