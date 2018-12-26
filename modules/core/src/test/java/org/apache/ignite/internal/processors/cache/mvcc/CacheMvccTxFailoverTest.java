3d23b45bf27a9bb3a96568540169394962e16ded
Oleg Ignatenko <oignatenko@gridgain.com>
2018-12-26 08:48:33 +0300

    GG-14582 Backport JUnit 4 to 2.7 master - fix for errors discovered by TC bot

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/mvcc/CacheMvccTxFailoverTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/mvcc/CacheMvccTxFailoverTest.java
@@ -40,6 +40,7 @@ import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.apache.ignite.transactions.Transaction;
 import org.apache.ignite.transactions.TransactionConcurrency;
 import org.apache.ignite.transactions.TransactionIsolation;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -218,10 +219,9 @@ public class CacheMvccTxFailoverTest extends GridCommonAbstractTest {
     /**
      * @throws Exception If fails.
      */
+    @Ignore("https://issues.apache.org/jira/browse/IGNITE-10219")
     @Test
     public void testLostRollbackOnBackup() throws Exception {
-        fail("https://issues.apache.org/jira/browse/IGNITE-10219");
-
         IgniteEx node = startGrid(0);
 
         startGrid(1);
