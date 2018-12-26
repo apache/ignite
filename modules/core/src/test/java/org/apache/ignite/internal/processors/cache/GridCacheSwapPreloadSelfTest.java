a79eac2d8ba9f50991e5a859451e8eacc9ae6f49
Oleg Ignatenko <oignatenko@gridgain.com>
2018-12-27 02:20:18 +0300

    GG-14582 Backport JUnit 4 to 2.7 master - fix for errors discovered by TC bot

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/GridCacheSwapPreloadSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/GridCacheSwapPreloadSelfTest.java
@@ -36,6 +36,7 @@ import org.apache.ignite.spi.discovery.tcp.ipfinder.TcpDiscoveryIpFinder;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.jetbrains.annotations.Nullable;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -148,6 +149,7 @@ public class GridCacheSwapPreloadSelfTest extends GridCommonAbstractTest {
     /**
      * @throws Exception If failed.
      */
+    @Ignore("https://issues.apache.org/jira/browse/IGNITE-614")
     @Test
     public void testSwapReplicatedMultithreaded() throws Exception {
         cacheMode = REPLICATED;
@@ -156,6 +158,7 @@ public class GridCacheSwapPreloadSelfTest extends GridCommonAbstractTest {
     }
 
     /** @throws Exception If failed. */
+    @Ignore("https://issues.apache.org/jira/browse/IGNITE-614")
     @Test
     public void testSwapPartitionedMultithreaded() throws Exception {
         cacheMode = PARTITIONED;
