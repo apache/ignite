647becf3be5225a1fc287267ef82d10c0be3ca24
Oleg Ignatenko <oignatenko@gridgain.com>
2018-12-27 02:24:27 +0300

    GG-14582 Backport JUnit 4 to 2.7 master - fix for errors discovered by TC bot

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/query/continuous/CacheContinuousQueryRandomOperationsTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/query/continuous/CacheContinuousQueryRandomOperationsTest.java
@@ -76,6 +76,7 @@ import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.apache.ignite.transactions.Transaction;
 import org.apache.ignite.transactions.TransactionConcurrency;
 import org.apache.ignite.transactions.TransactionIsolation;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -505,10 +506,9 @@ public class CacheContinuousQueryRandomOperationsTest extends GridCommonAbstract
     /**
      * @throws Exception If failed.
      */
+    @Ignore("https://issues.apache.org/jira/browse/IGNITE-8582")
     @Test
     public void testDoubleRemoveMvccTxWithStore() throws Exception {
-        fail("https://issues.apache.org/jira/browse/IGNITE-8582");
-
         CacheConfiguration<Object, Object> ccfg = cacheConfiguration(PARTITIONED,
             1,
             TRANSACTIONAL_SNAPSHOT,
@@ -533,10 +533,9 @@ public class CacheContinuousQueryRandomOperationsTest extends GridCommonAbstract
     /**
      * @throws Exception If failed.
      */
+    @Ignore("https://issues.apache.org/jira/browse/IGNITE-8582")
     @Test
     public void testDoubleRemoveReplicatedMvccTxWithStore() throws Exception {
-        fail("https://issues.apache.org/jira/browse/IGNITE-8582");
-
         CacheConfiguration<Object, Object> ccfg = cacheConfiguration(REPLICATED,
             0,
             TRANSACTIONAL_SNAPSHOT,
