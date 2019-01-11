55803b8334f321094d7ee5fe1d7cf30f062726dc
NSAmelchev <nsamelchev@gmail.com>
2019-01-11 16:16:53 +0300

    For TC muss run

--- modules/zookeeper/src/test/java/org/apache/ignite/spi/discovery/zk/ZookeeperDiscoverySpiTestSuite1.java
+++ modules/zookeeper/src/test/java/org/apache/ignite/spi/discovery/zk/ZookeeperDiscoverySpiTestSuite1.java
@@ -62,10 +62,12 @@ public class ZookeeperDiscoverySpiTestSuite1 {
 
         TestSuite suite = new TestSuite("ZookeeperDiscoverySpi Test Suite");
 
-        suite.addTest(new JUnit4TestAdapter(ZookeeperClientTest.class));
+//        suite.addTest(new JUnit4TestAdapter(ZookeeperClientTest.class));
+
+        for (int i = 0; i < 30; i++)
         suite.addTest(new JUnit4TestAdapter(ZookeeperDiscoverySpiTest.class));
-        suite.addTest(new JUnit4TestAdapter(ZookeeperDiscoverySpiSaslFailedAuthTest.class));
-        suite.addTest(new JUnit4TestAdapter(ZookeeperDiscoverySpiSaslSuccessfulAuthTest.class));
+//        suite.addTest(new JUnit4TestAdapter(ZookeeperDiscoverySpiSaslFailedAuthTest.class));
+//        suite.addTest(new JUnit4TestAdapter(ZookeeperDiscoverySpiSaslSuccessfulAuthTest.class));
 
         return suite;
     }
