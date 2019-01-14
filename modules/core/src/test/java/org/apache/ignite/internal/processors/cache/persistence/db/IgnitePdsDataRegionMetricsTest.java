6ae419a6993c22b22740f9891dd2ef88f84c3bbb
Andrey V. Mashenkov <andrey.mashenkov@gmail.com>
2019-01-14 14:49:56 +0300

    IGNITE-10591: MVCC: Incorrect data region metrics. This closes #5690.
    (cherry picked from commit ee551ce9679b2d2ddbbf04684644df0c3ce21481)

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/db/IgnitePdsDataRegionMetricsTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/db/IgnitePdsDataRegionMetricsTest.java
@@ -57,6 +57,7 @@ import org.junit.runners.JUnit4;
 import static java.nio.file.Files.newDirectoryStream;
 import static org.apache.ignite.configuration.DataStorageConfiguration.DFLT_DATA_REG_DEFAULT_NAME;
 import static org.apache.ignite.internal.processors.cache.GridCacheUtils.UTILITY_CACHE_NAME;
+import static org.apache.ignite.internal.processors.cache.mvcc.txlog.TxLog.TX_LOG_CACHE_NAME;
 import static org.apache.ignite.internal.processors.cache.persistence.file.FilePageStoreManager.META_STORAGE_NAME;
 import static org.apache.ignite.internal.processors.cache.persistence.metastorage.MetaStorage.METASTORAGE_CACHE_ID;
 import static org.apache.ignite.internal.processors.cache.persistence.metastorage.MetaStorage.METASTORAGE_CACHE_NAME;
@@ -165,7 +166,7 @@ public class IgnitePdsDataRegionMetricsTest extends GridCommonAbstractTest {
 
                 cache.putAll(map);
 
-                forceCheckpoint();
+                forceCheckpoint(node);
 
                 checkMetricsConsistency(node);
             }
@@ -319,6 +320,7 @@ public class IgnitePdsDataRegionMetricsTest extends GridCommonAbstractTest {
     private void checkMetricsConsistency(final IgniteEx node) throws Exception {
         checkMetricsConsistency(node, DEFAULT_CACHE_NAME);
         checkMetricsConsistency(node, UTILITY_CACHE_NAME);
+        checkMetricsConsistency(node, TX_LOG_CACHE_NAME);
         checkMetricsConsistency(node, METASTORAGE_CACHE_NAME);
     }
 
@@ -329,8 +331,10 @@ public class IgnitePdsDataRegionMetricsTest extends GridCommonAbstractTest {
         assert pageStoreMgr != null : "Persistence is not enabled";
 
         boolean metaStore = METASTORAGE_CACHE_NAME.equals(cacheName);
+        boolean txLog = TX_LOG_CACHE_NAME.equals(cacheName);
 
         File cacheWorkDir = metaStore ? new File(pageStoreMgr.workDir(), META_STORAGE_NAME) :
+            txLog ? new File(pageStoreMgr.workDir(), TX_LOG_CACHE_NAME) :
             pageStoreMgr.cacheWorkDir(node.cachex(cacheName).configuration());
 
         long totalPersistenceSize = 0;
@@ -351,6 +355,7 @@ public class IgnitePdsDataRegionMetricsTest extends GridCommonAbstractTest {
         GridCacheSharedContext cctx = node.context().cache().context();
 
         String regionName = metaStore ? GridCacheDatabaseSharedManager.METASTORE_DATA_REGION_NAME :
+            txLog ? TX_LOG_CACHE_NAME :
             cctx.cacheContext(CU.cacheId(cacheName)).group().dataRegion().config().getName();
 
         long totalAllocatedPagesFromMetrics = cctx.database().memoryMetrics(regionName).getTotalAllocatedPages();
