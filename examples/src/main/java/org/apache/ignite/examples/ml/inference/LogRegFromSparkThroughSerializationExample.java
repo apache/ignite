27d28005c8aa7d6a72c0cba4a15e665f39e4c06b
Zinoviev Alexey <zaleslaw.sin@gmail.com>
2019-01-15 19:09:43 +0300

    IGNITE-10866: Fixed output

--- examples/src/main/java/org/apache/ignite/examples/ml/inference/LogRegFromSparkThroughSerializationExample.java
+++ examples/src/main/java/org/apache/ignite/examples/ml/inference/LogRegFromSparkThroughSerializationExample.java
@@ -54,7 +54,7 @@ public class LogRegFromSparkThroughSerializationExample {
     /** Run example. */
     public static void main(String[] args) throws FileNotFoundException {
         System.out.println();
-        System.out.println(">>> Logistic regression model loaded from PMML over partitioned dataset usage example started.");
+        System.out.println(">>> Logistic regression model loaded from Spark through serialization over partitioned dataset usage example started.");
         // Start ignite grid.
         try (Ignite ignite = Ignition.start("examples/config/example-ignite.xml")) {
             System.out.println(">>> Ignite grid started.");
