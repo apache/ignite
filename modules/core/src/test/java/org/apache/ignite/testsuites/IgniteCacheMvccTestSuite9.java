954abec2288364b28e465cf29dfe3d3341ecf007
Andrey V. Mashenkov <andrey.mashenkov@gmail.com>
2019-01-16 12:20:24 +0300

    IGNITE-10518: Mute test. This closes #5832.
    (cherry picked from commit fa4ebf5dcd623474f6b699008143d92f5932c159)

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheMvccTestSuite9.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheMvccTestSuite9.java
@@ -24,6 +24,7 @@ import org.apache.ignite.IgniteSystemProperties;
 import org.apache.ignite.internal.processors.cache.IgniteCacheGetCustomCollectionsSelfTest;
 import org.apache.ignite.internal.processors.cache.IgniteCacheLoadRebalanceEvictionSelfTest;
 import org.apache.ignite.internal.processors.cache.distributed.CacheAtomicPrimarySyncBackPressureTest;
+import org.apache.ignite.internal.processors.cache.distributed.IgniteTxCachePrimarySyncTest;
 import org.apache.ignite.internal.processors.cache.distributed.IgniteTxConcurrentRemoveObjectsTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.AllTests;
@@ -43,6 +44,9 @@ public class IgniteCacheMvccTestSuite9 {
 
         // Skip classes that already contains Mvcc tests
         ignoredTests.add(IgniteTxConcurrentRemoveObjectsTest.class);
+
+        // Non supported modes.
+        ignoredTests.add(IgniteTxCachePrimarySyncTest.class);
 
         // Atomic caches.
         ignoredTests.add(CacheAtomicPrimarySyncBackPressureTest.class);
