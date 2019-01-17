da730ad042b9d2450c73735854551d283a3198a4
Oleg Ignatenko <oignatenko@gridgain.com>
2019-01-17 16:19:03 +0300

    IGNITE-10927 Relieve JUnit3TestLegacySupport from inheriting deprecated junit.framework.Assert - wip -- verified with diffs overview and rebuild

--- modules/core/src/test/java/org/apache/ignite/testframework/junits/JUnit3TestLegacyAssert.java
+++ modules/core/src/test/java/org/apache/ignite/testframework/junits/JUnit3TestLegacyAssert.java
@@ -28,106 +28,106 @@ import org.junit.Assert;
 class JUnit3TestLegacyAssert {
     /** See class javadocs. */
     protected static void assertTrue(String msg, boolean cond) {
-        // todo keep and deledate
+        Assert.assertTrue(msg, cond);
     }
 
     /** See class javadocs. */
     protected static void assertTrue(boolean cond) {
-        // todo keep and deledate
+        Assert.assertTrue(cond);
     }
 
     /** See class javadocs. */
     protected static void assertFalse(String msg, boolean cond) {
-        // todo keep and deledate
+        Assert.assertFalse(msg, cond);
     }
 
     /** See class javadocs. */
     protected static void assertFalse(boolean cond) {
-        // todo keep and deledate
+        Assert.assertFalse(cond);
     }
 
     /** See class javadocs. */
     protected static void assertEquals(String msg, Object exp, Object actual) {
-        // todo keep and deledate
+        Assert.assertEquals(msg, exp, actual);
     }
 
     /** See class javadocs. */
     protected static void assertEquals(Object exp, Object actual) {
-        // todo keep and deledate
+        Assert.assertEquals(exp, actual);
     }
 
     /** See class javadocs. */
     protected static void assertEquals(String msg, String exp, String actual) {
-        // todo keep and deledate
+        Assert.assertEquals(msg, exp, actual);
     }
 
     /** See class javadocs. */
     protected static void assertEquals(String exp, String actual) {
-        // todo keep and deledate
+        Assert.assertEquals(exp, actual);
     }
 
     /** See class javadocs. */
     protected static void assertEquals(String msg, long exp, long actual) {
-        // todo keep and deledate
+        Assert.assertEquals(msg, exp, actual);
     }
 
     /** See class javadocs. */
     protected static void assertEquals(long exp, long actual) {
-        // todo keep and deledate
+        Assert.assertEquals(exp, actual);
     }
 
     /** See class javadocs. */
     protected static void assertEquals(boolean exp, boolean actual) {
-        // todo keep and deledate
+        Assert.assertEquals(exp, actual);
     }
 
     /** See class javadocs. */
     protected static void assertEquals(String msg, int exp, int actual) {
-        // todo keep and deledate
+        Assert.assertEquals(msg, exp, actual);
     }
 
     /** See class javadocs. */
     protected static void assertEquals(int exp, int actual) {
-        // todo keep and deledate
+        Assert.assertEquals(exp, actual);
     }
 
     /** See class javadocs. */
     protected static void assertNotNull(Object obj) {
-        // todo keep and deledate
+        Assert.assertNotNull(obj);
     }
 
     /** See class javadocs. */
     protected static void assertNotNull(String msg, Object obj) {
-        // todo keep and deledate
+        Assert.assertNotNull(msg, obj);
     }
 
     /** See class javadocs. */
     protected static void assertNull(Object obj) {
-        // todo keep and deledate
+        Assert.assertNull(obj);
     }
 
     /** See class javadocs. */
     protected static void assertNull(String msg, Object obj) {
-        // todo keep and deledate
+        Assert.assertNull(msg, obj);
     }
 
     /** See class javadocs. */
     protected static void fail(String msg) {
-        // todo keep and deledate
+        Assert.fail(msg);
     }
 
     /** See class javadocs. */
     protected static void fail() {
-        // todo keep and deledate
+        Assert.fail();
     }
 
     /** See class javadocs. */
     protected static void assertSame(Object exp, Object actual) {
-        // todo keep and deledate
+        //   todo keep and deledate
     }
 
     /** See class javadocs. */
     protected static void assertNotSame(Object exp, Object actual) {
-        // todo keep and deledate
+        //   todo keep and deledate
     }
 }
