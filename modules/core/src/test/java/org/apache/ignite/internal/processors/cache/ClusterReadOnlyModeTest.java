2fafb38fa782fd573b525379c198b9dfc6dbaab6
Alexey Stelmak <spiderru5597@gmail.com>
2019-01-17 19:06:10 +0300

    tdr-171 Create new exception class for read-only mode

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/ClusterReadOnlyModeTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/ClusterReadOnlyModeTest.java
@@ -82,7 +82,7 @@ public class ClusterReadOnlyModeTest extends ClusterReadOnlyModeAbstractTest {
                             fail("Put must fail for cache " + cacheName);
                         }
                         catch (Exception e) {
-                            // No-op.
+                            checkThatRootCauseIsReadOnly(e);
                         }
 
                         // All removes must fail.
@@ -92,7 +92,7 @@ public class ClusterReadOnlyModeTest extends ClusterReadOnlyModeAbstractTest {
                             fail("Remove must fail for cache " + cacheName);
                         }
                         catch (Exception e) {
-                            // No-op.
+                            checkThatRootCauseIsReadOnly(e);
                         }
                     }
                     else {
@@ -113,7 +113,7 @@ public class ClusterReadOnlyModeTest extends ClusterReadOnlyModeAbstractTest {
 
         for (Ignite ignite : G.allGrids()) {
             for (String cacheName : CACHE_NAMES) {
-                boolean failed = false;
+                Throwable failed = null;
 
                 try (IgniteDataStreamer<Integer, Integer> streamer = ignite.dataStreamer(cacheName)) {
                     for (int i = 0; i < 10; i++) {
@@ -123,11 +123,13 @@ public class ClusterReadOnlyModeTest extends ClusterReadOnlyModeAbstractTest {
                     }
                 }
                 catch (CacheException ignored) {
-                    failed = true;
+                    failed = ignored;
                 }
 
-                if (failed != readOnly)
+                if ((failed == null) == readOnly)
                     fail("Streaming to " + cacheName + " must " + (readOnly ? "fail" : "succeed"));
+
+                checkThatRootCauseIsReadOnly(failed);
             }
         }
     }
