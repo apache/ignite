faf4f88936537c960b0b806a207d2efbd318025a
Yuriy Gerzhedovich <ygerzhedovich@gridgain.com>
2019-01-23 13:17:19 +0300

    IGNITE-10754: SQL: Query history tracking. This closes #5805.

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/IgniteConfigurationSerializerTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/IgniteConfigurationSerializerTest.cs
@@ -102,6 +102,7 @@ namespace Apache.Ignite.Core.Tests
 
             Assert.AreEqual(10000, cfg.MvccVacuumFrequency);
             Assert.AreEqual(4, cfg.MvccVacuumThreadCount);
+            Assert.AreEqual(123, cfg.SqlQueryHistorySize);
 
             Assert.IsNotNull(cfg.SqlSchemas);
             Assert.AreEqual(2, cfg.SqlSchemas.Count);
@@ -1020,11 +1021,12 @@ namespace Apache.Ignite.Core.Tests
                     }
                 },
                 SslContextFactory = new SslContextFactory(),
-                FailureHandler = new StopNodeOrHaltFailureHandler()
+                FailureHandler = new StopNodeOrHaltFailureHandler
                 {
                     TryStop = false,
                     Timeout = TimeSpan.FromSeconds(10)
-                }
+                },
+                SqlQueryHistorySize = 345
             };
         }
 
