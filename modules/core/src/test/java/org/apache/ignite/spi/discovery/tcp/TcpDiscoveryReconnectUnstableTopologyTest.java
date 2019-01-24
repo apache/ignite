79a1730d60cd7e3700b7cb10231d425d1731c6ee
Aleksei Scherbakov <alexey.scherbakoff@gmail.com>
2019-01-24 13:12:12 +0300

    IGNITE-10933 Remove juni4 after merge.

--- modules/core/src/test/java/org/apache/ignite/spi/discovery/tcp/TcpDiscoveryReconnectUnstableTopologyTest.java
+++ modules/core/src/test/java/org/apache/ignite/spi/discovery/tcp/TcpDiscoveryReconnectUnstableTopologyTest.java
@@ -57,7 +57,6 @@ import org.junit.runners.JUnit4;
  *
  * Pass condition: new node successfully joins topology.
  */
-@RunWith(JUnit4.class)
 public class TcpDiscoveryReconnectUnstableTopologyTest extends GridCommonAbstractTest {
     /** */
     private static final TcpDiscoveryVmIpFinder ipFinder = new TcpDiscoveryVmIpFinder(true);
@@ -88,7 +87,6 @@ public class TcpDiscoveryReconnectUnstableTopologyTest extends GridCommonAbstrac
     /**
      * @throws Exception If failed.
      */
-    @Test
     public void testReconnectUnstableTopology() throws Exception {
         try {
             List<IgniteEx> nodes = new ArrayList<>();
