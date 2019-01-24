786404cee591737b7b031d1b0722922773e9d475
mcherkasov <mcherkasov@gridgain.com>
2019-01-24 15:45:01 +0300

    Test added directly to basic suite and suite for plugin api is removed.

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteBasicTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteBasicTestSuite.java
@@ -83,6 +83,7 @@ import org.apache.ignite.messaging.GridMessagingNoPeerClassLoadingSelfTest;
 import org.apache.ignite.messaging.GridMessagingSelfTest;
 import org.apache.ignite.messaging.IgniteMessagingSendAsyncTest;
 import org.apache.ignite.messaging.IgniteMessagingWithClientTest;
+import org.apache.ignite.plugin.PluginNodeValidationTest;
 import org.apache.ignite.plugin.security.SecurityPermissionSetBuilderTest;
 import org.apache.ignite.spi.GridSpiLocalHostInjectionTest;
 import org.apache.ignite.startup.properties.NotStringSystemPropertyTest;
@@ -209,7 +210,7 @@ import org.junit.runners.Suite;
 
     CacheLocalGetSerializationTest.class,
 
-    IgnitePluginApiTestSuite.class,
+    PluginNodeValidationTest.class,
 })
 public class IgniteBasicTestSuite {
 }
