c5b32df0b96307c60350c66012cc57b855004a46
dmitrievanthony <dmitrievanthony@gmail.com>
2019-01-28 12:10:08 +0300

    IGNITE-11071: Move SQL example into org.apache.ignite.ml.sql package.

--- examples/src/main/java/org/apache/ignite/examples/ml/sql/DecisionTreeClassificationTrainerSQLTableExample.java
+++ examples/src/main/java/org/apache/ignite/examples/ml/sql/DecisionTreeClassificationTrainerSQLTableExample.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.ignite.examples.ml.tree;
+package org.apache.ignite.examples.ml.sql;
 
 import java.util.List;
 import org.apache.ignite.Ignite;
