05c4a0ece70594b552b2506cf222604af32dd627
Oleg Ignatenko <oignatenko@gridgain.com>
2019-01-28 10:47:32 +0300

    IGNITE-10179 use @BeforeClass and @AfterClass instead of isFirstTest() and isLastTest() - fixes for errors discovered by TC bot -- verified with diffs overview, rebuild and execution of unit tests

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/SqlLocalQueryConnectionAndStatementTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/SqlLocalQueryConnectionAndStatementTest.java
@@ -31,7 +31,7 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class SqlLocalQueryConnectionAndStatementTest extends AbstractIndexingCommonTest {
     /** {@inheritDoc} */
-    public void beforeTestsStarted() throws Exception {
+    @Override public void beforeTestsStarted() throws Exception {
         super.beforeTestsStarted();
 
         startGrids(1);
