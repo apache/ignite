17657e940c6393277cd19dc12da61babdea9a51d
Oleg Ignatenko <oignatenko@gridgain.com>
2019-01-29 20:00:02 +0300

    IGNITE-10758 remove from tests scaffolding annotations - manual merge correction -- verified with diffs overview and rebuild

--- modules/core/src/test/java/org/apache/ignite/internal/IgniteVersionUtilsSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/IgniteVersionUtilsSelfTest.java
@@ -20,12 +20,9 @@ package org.apache.ignite.internal;
 import java.util.Calendar;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
 /**
  */
-@RunWith(JUnit4.class)
 public class IgniteVersionUtilsSelfTest extends GridCommonAbstractTest {
     /**
      * @throws Exception If failed.
