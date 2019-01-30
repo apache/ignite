639ee7cb54bc6a7954c8a1a2dd1f46ff8b04e2b6
Rodion4ik <rodion.vlasov@mail.ru>
2019-01-30 11:44:47 +0300

    Remove variable

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/near/GridNearTxLocal.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/near/GridNearTxLocal.java
@@ -320,7 +320,7 @@ public class GridNearTxLocal extends GridDhtTxLocalAdapter implements GridTimeou
 
     /** {@inheritDoc} */
     @Override protected void clearPrepareFuture(GridDhtTxPrepareFuture fut) {
-        PREP_FUT_UPD.compareAndSet(this, fut, null);
+       //No-op.
     }
 
     /**
