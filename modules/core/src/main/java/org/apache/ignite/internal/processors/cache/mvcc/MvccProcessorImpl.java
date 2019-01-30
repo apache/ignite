056aa71435ec14e3ac43ac98265c2e72441e0373
ipavlukhin <vololo100@gmail.com>
2019-01-30 16:28:25 +0300

    during tx recovery voting on MVCC coordinator await votes only from nodes supporting MVCC

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/mvcc/MvccProcessorImpl.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/mvcc/MvccProcessorImpl.java
@@ -452,8 +452,10 @@ public class MvccProcessorImpl extends GridProcessorAdapter implements MvccProce
                 RecoveryBallotBox ballotBox = recoveryBallotBoxes
                     .computeIfAbsent(nodeId, uuid -> new RecoveryBallotBox());
 
-                ballotBox
-                    .voters(evt.topologyNodes().stream().map(ClusterNode::id).collect(Collectors.toList()));
+                ballotBox.voters(evt.topologyNodes().stream()
+                    .filter(this::supportsMvcc)
+                    .map(ClusterNode::id)
+                    .collect(Collectors.toList()));
 
                 tryFinishRecoveryVoting(nodeId, ballotBox);
             }
