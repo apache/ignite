e8ac1232a45fae95b37b7447846fbc8f1e8f7d1d
Oleg Ignatenko <oignatenko@gridgain.com>
2019-02-01 21:09:56 +0300

    IGNITE-11123 change fail to assume in MvccFeatureChecker // backport to 2.7

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/mvcc/CacheMvccPartitionedSqlCoordinatorFailoverTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/mvcc/CacheMvccPartitionedSqlCoordinatorFailoverTest.java
@@ -18,6 +18,7 @@
 package org.apache.ignite.internal.processors.cache.mvcc;
 
 import org.apache.ignite.cache.CacheMode;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
