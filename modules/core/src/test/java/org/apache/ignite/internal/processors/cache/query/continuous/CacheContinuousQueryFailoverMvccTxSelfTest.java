192832c53803e0e934fe6fbb1450f425dd1f2215
ipavlukhin <vololo100@gmail.com>
2019-02-04 12:13:06 +0300

    IGNITE-11148: PartitionCountersNeighborcastFuture blocks partition map exchange. This closes #5998.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/query/continuous/CacheContinuousQueryFailoverMvccTxSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/query/continuous/CacheContinuousQueryFailoverMvccTxSelfTest.java
@@ -44,11 +44,4 @@ public class CacheContinuousQueryFailoverMvccTxSelfTest extends CacheContinuousQ
     @Override public void testBackupQueueEvict() throws Exception {
         // No-op.
     }
-
-    /** {@inheritDoc} */
-    @Ignore("https://issues.apache.org/jira/browse/IGNITE-11148")
-    @Test
-    @Override public void testMultiThreadedFailover() throws Exception {
-        // No-op.
-    }
 }
