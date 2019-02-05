abc14188676bf300642a2414873f490db3fd4c74
devozerov <vozerov@gridgain.com>
2019-02-05 11:42:12 +0300

    Cleanup.

--- modules/core/src/main/java/org/apache/ignite/internal/sql/optimizer/affinity/PartitionTableModel.java
+++ modules/core/src/main/java/org/apache/ignite/internal/sql/optimizer/affinity/PartitionTableModel.java
@@ -17,10 +17,6 @@
 
 package org.apache.ignite.internal.sql.optimizer.affinity;
 
-import org.apache.ignite.internal.sql.optimizer.affinity.PartitionJoinCondition;
-import org.apache.ignite.internal.sql.optimizer.affinity.PartitionJoinGroup;
-import org.apache.ignite.internal.sql.optimizer.affinity.PartitionTable;
-import org.apache.ignite.internal.sql.optimizer.affinity.PartitionTableAffinityDescriptor;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
