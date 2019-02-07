c66baf6ef0b3ccbd3df76b44acfe364491521f84
Rodion4ik <rodion.vlasov@mail.ru>
2019-02-07 22:30:51 +0300

    CacheMvccAbstractFeatureTest as current master

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/mvcc/CacheMvccAbstractFeatureTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/mvcc/CacheMvccAbstractFeatureTest.java
@@ -58,7 +58,7 @@ public abstract class CacheMvccAbstractFeatureTest extends CacheMvccAbstractTest
     private static final String CACHE_NAME = "Person";
 
     /** */
-    private Ignite node;
+    private static Ignite node;
 
     /** {@inheritDoc} */
     @Override protected CacheMode cacheMode() {
