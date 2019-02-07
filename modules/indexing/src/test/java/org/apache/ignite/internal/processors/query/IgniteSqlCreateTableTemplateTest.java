2c6d8e0527119fd719db2767f7895098128524c5
mpudov <mpudov@gridgain.com>
2019-02-07 13:27:04 +0300

    IGNITE-11219 CREATE TABLE with template doesn't work with data inserted from KV API

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/IgniteSqlCreateTableTemplateTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/IgniteSqlCreateTableTemplateTest.java
@@ -20,51 +20,126 @@ package org.apache.ignite.internal.processors.query;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.binary.BinaryObjectBuilder;
+import org.apache.ignite.cache.affinity.AffinityKeyMapper;
 import org.apache.ignite.cache.query.SqlFieldsQuery;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
-import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
+import org.apache.ignite.internal.processors.cache.index.AbstractIndexingCommonTest;
 import org.junit.Test;
+
+import java.io.Serializable;
 
 /**
  * Ensures that SQL queries work for tables created dynamically based on a template.
  */
-public class IgniteSqlCreateTableTemplateTest extends GridCommonAbstractTest {
+public class IgniteSqlCreateTableTemplateTest extends AbstractIndexingCommonTest {
 
     /** {@inheritDoc} */
     @Override
-    protected IgniteConfiguration getConfiguration() throws Exception {
-        IgniteConfiguration configuration = super.getConfiguration();
-        CacheConfiguration cacheConfiguration = new CacheConfiguration();
-        cacheConfiguration.setName("TEST_TEMPLATE*");
-        configuration.setCacheConfiguration(cacheConfiguration);
+    public IgniteConfiguration getConfiguration(String name) throws Exception {
+        IgniteConfiguration configuration = new IgniteConfiguration();
+        configuration.setIgniteInstanceName(name);
+
+        CacheConfiguration defaultCacheConfiguration = new CacheConfiguration();
+        defaultCacheConfiguration.setName("DEFAULT_TEMPLATE*");
+
+        CacheConfiguration customCacheConfiguration = new CacheConfiguration();
+        customCacheConfiguration.setName("CUSTOM_TEMPLATE*");
+        MockAffinityKeyMapper customAffinityMapper = new MockAffinityKeyMapper();
+        customCacheConfiguration.setAffinityMapper(customAffinityMapper);
+
+        configuration.setCacheConfiguration(defaultCacheConfiguration, customCacheConfiguration);
+
         return configuration;
     }
 
+    /** {@inheritDoc} */
+    @Override
+    protected void beforeTestsStarted() throws Exception {
+        startGrid();
+        super.beforeTestsStarted();
+    }
 
     /**
      * Tests select statement works on a table with BinaryObject as a primary key.
      */
     @Test
     public void testSelectForTableWithDataInsertedWithKeyValueAPI() throws Exception {
-        Ignite ignite = startGrid(getConfiguration());
+        Ignite ignite = grid();
         IgniteCache cache = ignite.getOrCreateCache("test");
 
-        cache.query(new SqlFieldsQuery(
-                "CREATE TABLE IF NOT EXISTS TEST(\n" +
-                "  TEST_ID                INT        NOT NULL,\n" +
-                "  TEST_FIELD             VARCHAR2(100),\n" +
-                "  PRIMARY KEY (TEST_ID)\n" +
-                ") with \"TEMPLATE=TEST_TEMPLATE,KEY_TYPE=TEST_KEY ,CACHE_NAME=TEST_CACHE , VALUE_TYPE=TEST_VALUE,ATOMICITY=TRANSACTIONAL\";").setSchema("PUBLIC"));
+        createTable(cache, "PERSON", "DEFAULT_TEMPLATE");
 
-        BinaryObjectBuilder keyBuilder = ignite.binary().builder("TEST_KEY");
-        keyBuilder.setField("TEST_ID", 1);
+        createTable(cache, "ORGANIZATION", "DEFAULT_TEMPLATE");
 
-        BinaryObjectBuilder valueBuilder = ignite.binary().builder("TEST_VALUE");
-        valueBuilder.setField("TEST_FIELD", "test");
+        BinaryObjectBuilder keyBuilder = ignite.binary().builder("PERSON_KEY");
+        keyBuilder.setField("ID", 1);
+        keyBuilder.setField("AFF_PERSON", 2);
 
-        ignite.cache("TEST_CACHE").withKeepBinary().put(keyBuilder.build(), valueBuilder.build());
+        BinaryObjectBuilder valueBuilder = ignite.binary().builder("PERSON_VALUE");
+        valueBuilder.setField("NAME", "test");
 
-        assertEquals(1, ignite.cache("TEST_CACHE").query(new SqlFieldsQuery("Select TEST_FIELD from TEST where TEST_ID = 1")).getAll().size());
+        ignite.cache("PERSON_CACHE").withKeepBinary().put(keyBuilder.build(), valueBuilder.build());
+
+        keyBuilder = ignite.binary().builder("ORGANIZATION_KEY");
+        keyBuilder.setField("ID", 1);
+        keyBuilder.setField("AFF_ORGANIZATION", 2);
+
+        valueBuilder = ignite.binary().builder("ORGANIZATION_VALUE");
+        valueBuilder.setField("NAME", "test");
+
+        ignite.cache("ORGANIZATION_CACHE").withKeepBinary().put(keyBuilder.build(), valueBuilder.build());
+
+        assertEquals(1, ignite.cache("PERSON_CACHE").query(new SqlFieldsQuery("Select NAME from PERSON where ID = 1")).getAll().size());
+        assertEquals(1, ignite.cache("PERSON_CACHE").query(new SqlFieldsQuery("Select NAME from PERSON where AFF_PERSON = 2")).getAll().size());
+        assertEquals(1, ignite.cache("ORGANIZATION_CACHE").query(new SqlFieldsQuery("Select NAME from ORGANIZATION where AFF_ORGANIZATION = 2")).getAll().size());
+    }
+
+    /**
+     * Creates table based on a template
+     * @param cache
+     */
+    private void createTable(IgniteCache cache, String tableName, String template) {
+        String sql = String.format(
+                "CREATE TABLE IF NOT EXISTS %1$s(\n" +
+                "  ID                INT        NOT NULL,\n" +
+                "  AFF_%1$s        INT        NOT NULL,\n" +
+                "  NAME              VARCHAR2(100),\n" +
+                "  PRIMARY KEY (ID, AFF_%1$s)\n" +
+                ") with \"TEMPLATE=%2$s,KEY_TYPE=%1$s_KEY, AFFINITY_KEY=AFF_%1$s, CACHE_NAME=%1$s_CACHE, VALUE_TYPE=%1$s_VALUE, ATOMICITY=TRANSACTIONAL\";", tableName, template);
+        cache.query(new SqlFieldsQuery(sql).setSchema("PUBLIC"));
+    }
+
+    /**
+     * When template has custom affinity mapper
+     * then cache created via CREATE TABLE command should have the same affinity mapper.
+     * @throws Exception
+     */
+    @Test
+    public void testCustomAffinityKeyMapperIsNotOverwritten() throws Exception {
+        Ignite ignite = grid();
+        IgniteCache cache = ignite.getOrCreateCache("test");
+
+        createTable(cache, "CUSTOM", "CUSTOM_TEMPLATE");
+
+        assertTrue(ignite.getOrCreateCache("CUSTOM_CACHE").getConfiguration(CacheConfiguration.class).getAffinityMapper() instanceof MockAffinityKeyMapper);
+    }
+
+    /**
+     * Mock affinity mapper implementation.
+     */
+    private static class MockAffinityKeyMapper implements AffinityKeyMapper, Serializable {
+
+        /** {@inheritDoc} */
+        @Override
+        public Object affinityKey(Object key) {
+            return null;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public void reset() {
+            // no-op
+        }
     }
 }
