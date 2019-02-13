c5ffa8919edb1736e03740d89dc61b6cbf934d38
Rodion4ik <rodion.vlasov@mail.ru>
2019-02-13 13:39:48 +0300

    IGNITE-8558 Provide an opportunity to stop grids and cancel tasks after execution all tests - Fixes #5477.
    Signed-off-by: Ivan Rakov <irakov@apache.org>

--- modules/core/src/test/java/org/apache/ignite/testframework/junits/GridAbstractTest.java
+++ modules/core/src/test/java/org/apache/ignite/testframework/junits/GridAbstractTest.java
@@ -1778,7 +1778,7 @@ public abstract class GridAbstractTest extends JUnit3TestLegacySupport {
         }
 
         if (isSafeTopology()) {
-            stopAllGrids(false);
+            stopAllGrids(true);
 
             if (stopGridErr) {
                 err = new RuntimeException("Not all Ignite instances has been stopped. " +
