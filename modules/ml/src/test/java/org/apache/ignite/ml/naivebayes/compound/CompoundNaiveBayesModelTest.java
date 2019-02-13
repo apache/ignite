835492e1fc032c26eae89d21e61cd61e8d1b9807
dehasi <rgaleyev@gmail.com>
2019-02-13 22:54:28 +0100

    IGNITE-9978: Fix formatting

--- modules/ml/src/test/java/org/apache/ignite/ml/naivebayes/compound/CompoundNaiveBayesModelTest.java
+++ modules/ml/src/test/java/org/apache/ignite/ml/naivebayes/compound/CompoundNaiveBayesModelTest.java
@@ -22,8 +22,9 @@ import org.apache.ignite.ml.math.primitives.vector.VectorUtils;
 import org.apache.ignite.ml.naivebayes.discrete.DiscreteNaiveBayesModel;
 import org.apache.ignite.ml.naivebayes.discrete.DiscreteNaiveBayesSumsHolder;
 import org.apache.ignite.ml.naivebayes.gaussian.GaussianNaiveBayesModel;
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /** Tests for {@link CompoundNaiveBayesModel} */
 public class CompoundNaiveBayesModelTest {
@@ -32,6 +33,9 @@ public class CompoundNaiveBayesModelTest {
     public void testPredictOnlyGaus() {
         double first = 1;
         double second = 2;
+        double[] labels = {first, second};
+        double[] classProbabilities = new double[] {.5, .5};
+
         double[][] means = new double[][] {
             {5.855, 176.25, 11.25},
             {5.4175, 132.5, 7.5},
@@ -40,37 +44,36 @@ public class CompoundNaiveBayesModelTest {
             {3.5033E-2, 1.2292E2, 9.1667E-1},
             {9.7225E-2, 5.5833E2, 1.6667},
         };
-        double[] probabilities = new double[] {.5, .5};
-        double[] labels = {first, second};
         GaussianNaiveBayesModel gaussianModel =
-            new GaussianNaiveBayesModel(means, variances, probabilities, labels, null);
+            new GaussianNaiveBayesModel(means, variances, classProbabilities, labels, null);
+
         Vector observation = VectorUtils.of(6, 130, 8);
 
         CompoundNaiveBayesModel model = CompoundNaiveBayesModel.builder()
-            .wirhClassProbabilities(probabilities)
+            .wirhClassProbabilities(classProbabilities)
             .withLabels(labels)
             .withGaussianModel(gaussianModel)
             .withGaussianModelRange(0, observation.size())
             .build();
 
-        Assert.assertEquals(second, model.predict(observation), 0.0001);
-
+        assertEquals(second, model.predict(observation), 0.0001);
     }
 
     @Test /** */
     public void testPredictOnlyDiscrete() {
         double first = 1;
         double second = 2;
+        double[] labels = {first, second};
+        double[] classProbabilities = new double[] {6. / 13, 7. / 13};
+
         double[][][] probabilities = new double[][][] {
             {{.5, .5}, {.2, .3, .5}, {2. / 3., 1. / 3.}, {.4, .1, .5}, {.5, .5}},
             {{0, 1}, {1. / 7, 2. / 7, 4. / 7}, {4. / 7, 3. / 7}, {2. / 7, 3. / 7, 2. / 7}, {4. / 7, 3. / 7,}}
         };
-
-        double[] classProbabilities = new double[] {6. / 13, 7. / 13};
         double[][] thresholds = new double[][] {{.5}, {.2, .7}, {.5}, {.5, 1.5}, {.5}};
-        double[] labels = {first, second};
         DiscreteNaiveBayesModel discreteModel =
             new DiscreteNaiveBayesModel(probabilities, classProbabilities, labels, thresholds, new DiscreteNaiveBayesSumsHolder());
+
         Vector observation = VectorUtils.of(2, 0, 1, 2, 0);
 
         CompoundNaiveBayesModel model = CompoundNaiveBayesModel.builder()
@@ -80,7 +83,7 @@ public class CompoundNaiveBayesModelTest {
             .withDiscreteModelRange(0, observation.size())
             .build();
 
-        Assert.assertEquals(second, model.predict(observation), 0.0001);
+        assertEquals(second, model.predict(observation), 0.0001);
     }
 
     @Test /** */
