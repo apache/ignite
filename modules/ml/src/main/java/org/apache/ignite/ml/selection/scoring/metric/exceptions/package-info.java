4af65876a83d21d010728876375b73cd229424b0
zaleslaw <zaleslaw.sin@gmail.com>
2019-02-14 19:08:03 +0300

    IGNITE-10902: Fixed compilation errors

--- modules/ml/src/main/java/org/apache/ignite/ml/selection/scoring/metric/exceptions/package-info.java
+++ modules/ml/src/main/java/org/apache/ignite/ml/selection/scoring/metric/exceptions/package-info.java
@@ -15,25 +15,8 @@
  * limitations under the License.
  */
 
-package org.apache.ignite.ml.selection.scoring.metric;
-
-import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.Map;
-
 /**
- * Common interface to present metric values for different ML tasks.
+ * <!-- Package description. -->
+ * Root package for exceptions.
  */
-public interface MetricValues {
-    /** Returns the pair of metric name and metric value. */
-    public default Map<String, Double> toMap() {
-        Map<String, Double> metricValues = new HashMap<>();
-        for (Field field : getClass().getDeclaredFields())
-            try {
-                metricValues.put(field.getName(), field.getDouble(this));
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        return metricValues;
-    }
-}
+package org.apache.ignite.ml.selection.scoring.metric.exceptions;
