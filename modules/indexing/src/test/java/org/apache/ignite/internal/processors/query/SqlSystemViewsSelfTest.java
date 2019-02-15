0d7ecb505a9963cdbec3b8bcb9bf4569c4917eb8
devozerov <vozerov@gridgain.com>
2019-02-15 12:27:20 +0300

    IGNITE-11177: SQL: Fixed time overflow for IGNITE.NODE_METRICS view. This closes #6044.

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/SqlSystemViewsSelfTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/SqlSystemViewsSelfTest.java
@@ -23,7 +23,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-import java.util.TimeZone;
 import java.util.UUID;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
