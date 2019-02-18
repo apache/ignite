d717be49947a48b5bca2e7e44293a6be5e39189e
Sergey Antonov <antonovsergey93@gmail.com>
2019-02-18 20:04:14 +0300

    IGNITE-10928 Avoid to start wal compressor in recovery mode.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/FileWriteAheadLogManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/FileWriteAheadLogManager.java
@@ -493,7 +493,7 @@ public class FileWriteAheadLogManager extends GridCacheSharedManagerAdapter impl
             archiver.restart();
         }
 
-        if (dsCfg.isWalCompactionEnabled()) {
+        if (dsCfg.isWalCompactionEnabled() && !cctx.kernalContext().recoveryMode()) {
             assert compressor != null : "Compressor should be initialized.";
 
             compressor.restart();
