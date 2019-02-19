722757b409dfdaea530e846d9f887687c08f173d
Ivan Rakov <irakov@apache.org>
2019-02-19 19:24:06 +0300

    IGNITE-10873 CorruptedTreeException during simultaneous cache put operations

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/persistence/db/IndexingMultithreadedLoadContinuousRestartTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/persistence/db/IndexingMultithreadedLoadContinuousRestartTest.java
@@ -143,6 +143,8 @@ public class IndexingMultithreadedLoadContinuousRestartTest extends GridCommonAb
 
     /**
      * Tests that continuous non-graceful node stop under load doesn't break SQL indexes.
+     *
+     * @throws Exception If failed.
      */
     @Test
     public void test() throws Exception {
