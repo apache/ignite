0649184cac5a08c248ebefb9a79cca9c821de260
Ivan Rakov <irakov@apache.org>
2019-02-21 11:43:59 +0300

    IGNITE-10873 CorruptedTreeException during simultaneous cache put operations - Fixes #6128.
    Signed-off-by: Ivan Rakov <irakov@apache.org>
    (cherry picked from commit 81e5bee1f7ece600ba6c8e5a4bb1dcf792adcb89)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/query/GridQueryProcessor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/query/GridQueryProcessor.java
@@ -1825,6 +1825,7 @@ public class GridQueryProcessor extends GridProcessorAdapter {
         assert cctx != null;
         assert newRow != null;
         assert prevRowAvailable || prevRow == null;
+        // No need to acquire busy lock here - operation is protected by GridCacheQueryManager.busyLock
 
         KeyCacheObject key = newRow.key();
 
@@ -1834,40 +1835,32 @@ public class GridQueryProcessor extends GridProcessorAdapter {
         if (idx == null)
             return;
 
-        if (!busyLock.enterBusy())
-            throw new NodeStoppingException("Operation has been cancelled (node is stopping).");
+        String cacheName = cctx.name();
 
-        try {
-            String cacheName = cctx.name();
+        CacheObjectContext coctx = cctx.cacheObjectContext();
 
-            CacheObjectContext coctx = cctx.cacheObjectContext();
+        QueryTypeDescriptorImpl desc = typeByValue(cacheName, coctx, key, newRow.value(), true);
 
-            QueryTypeDescriptorImpl desc = typeByValue(cacheName, coctx, key, newRow.value(), true);
+        if (prevRowAvailable && prevRow != null) {
+            QueryTypeDescriptorImpl prevValDesc = typeByValue(cacheName,
+                coctx,
+                key,
+                prevRow.value(),
+                false);
 
-            if (prevRowAvailable && prevRow != null) {
-                QueryTypeDescriptorImpl prevValDesc = typeByValue(cacheName,
-                    coctx,
-                    key,
-                    prevRow.value(),
-                    false);
+            if (prevValDesc != desc) {
+                if (prevValDesc != null)
+                    idx.remove(cctx, prevValDesc, prevRow);
 
-                if (prevValDesc != desc) {
-                    if (prevValDesc != null)
-                        idx.remove(cctx, prevValDesc, prevRow);
-
-                    // Row has already been removed from another table indexes
-                    prevRow = null;
-                }
+                // Row has already been removed from another table indexes
+                prevRow = null;
             }
+        }
 
             if (desc == null)
                 return;
 
-            idx.store(cctx, desc, newRow, prevRow, prevRowAvailable);
-        }
-        finally {
-            busyLock.leaveBusy();
-        }
+        idx.store(cctx, desc, newRow, prevRow, prevRowAvailable);
     }
 
     /**
@@ -2463,32 +2456,24 @@ public class GridQueryProcessor extends GridProcessorAdapter {
      */
     public void remove(GridCacheContext cctx, CacheDataRow val)
         throws IgniteCheckedException {
-        assert val != null;
+        // No need to acquire busy lock here - operation is protected by GridCacheQueryManager.busyLock
 
         if (log.isDebugEnabled())
-            log.debug("Remove [cacheName=" + cctx.name() + ", key=" + val.key()+ ", val=" + val.value() + "]");
+            log.debug("Remove [cacheName=" + cctx.name() + ", key=" + val.key() + ", val=" + val.value() + "]");
 
         if (idx == null)
             return;
 
-        if (!busyLock.enterBusy())
-            throw new IllegalStateException("Failed to remove from index (grid is stopping).");
+        QueryTypeDescriptorImpl desc = typeByValue(cctx.name(),
+            cctx.cacheObjectContext(),
+            val.key(),
+            val.value(),
+            false);
 
-        try {
-            QueryTypeDescriptorImpl desc = typeByValue(cctx.name(),
-                cctx.cacheObjectContext(),
-                val.key(),
-                val.value(),
-                false);
+        if (desc == null)
+            return;
 
-            if (desc == null)
-                return;
-
-            idx.remove(cctx, desc, val);
-        }
-        finally {
-            busyLock.leaveBusy();
-        }
+        idx.remove(cctx, desc, val);
     }
 
     /**
