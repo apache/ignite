ed68b1164b845878e1bdd3b16f2d45f436f4c1b6
antkr <antkr.dev@gmail.com>
2019-03-01 14:52:24 +0300

    Revert "Java 11 .bat fixed"
    This reverts commit 8a7a415

--- examples/src/main/java/org/apache/ignite/examples/datagrid/CacheDataStreamerExample.java
+++ examples/src/main/java/org/apache/ignite/examples/datagrid/CacheDataStreamerExample.java
@@ -17,12 +17,8 @@
 
 package org.apache.ignite.examples.datagrid;
 
-import java.util.HashMap;
-import java.util.Map;
-import javax.cache.CacheException;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
-import org.apache.ignite.IgniteClientDisconnectedException;
 import org.apache.ignite.IgniteDataStreamer;
 import org.apache.ignite.IgniteException;
 import org.apache.ignite.Ignition;
@@ -46,7 +42,7 @@ public class CacheDataStreamerExample {
     private static final String CACHE_NAME = CacheDataStreamerExample.class.getSimpleName();
 
     /** Number of entries to load. */
-    private static final int ENTRY_COUNT = 5000000;
+    private static final int ENTRY_COUNT = 500000;
 
     /** Heap size required to run this example. */
     public static final int MIN_MEMORY = 512 * 1024 * 1024;
@@ -60,61 +56,36 @@ public class CacheDataStreamerExample {
     public static void main(String[] args) throws IgniteException {
         ExamplesUtils.checkMinMemory(MIN_MEMORY);
 
-        Ignition.setClientMode(true);
-        Map<Integer, Integer> map = new HashMap<>();
-        //map.entrySet().parallelStream().forEach();
-
         try (Ignite ignite = Ignition.start("examples/config/example-ignite.xml")) {
             System.out.println();
             System.out.println(">>> Cache data streamer example started.");
 
-            boolean cancelled = false;
-            while (!cancelled) {
             // Auto-close cache at the end of the example.
             try (IgniteCache<Integer, String> cache = ignite.getOrCreateCache(CACHE_NAME)) {
                 long start = System.currentTimeMillis();
 
+                try (IgniteDataStreamer<Integer, String> stmr = ignite.dataStreamer(CACHE_NAME)) {
+                    // Configure loader.
+                    stmr.perNodeBufferSize(1024);
+                    stmr.perNodeParallelOperations(8);
 
-                    try (IgniteDataStreamer<Integer, String> stmr = ignite.dataStreamer(CACHE_NAME)) {
-                        // Configure loader.
-                        stmr.perNodeBufferSize(1024);
-                        stmr.perNodeParallelOperations(8);
+                    for (int i = 0; i < ENTRY_COUNT; i++) {
+                        stmr.addData(i, Integer.toString(i));
 
-                        for (int i = 0; i < ENTRY_COUNT; i++) {
-                            stmr.addData(i, Integer.toString(i));
-
-                            //Thread.sleep(1000)
-                            // Print out progress while loading cache.
-                            if (i > 0 && i % 10000 == 0)
-                                System.out.println("Loaded " + i + " keys.");
-
-                            if ( i + 1 == ENTRY_COUNT)
-                                cancelled = true;
-
-                            stmr.flush();
-                        }
+                        // Print out progress while loading cache.
+                        if (i > 0 && i % 10000 == 0)
+                            System.out.println("Loaded " + i + " keys.");
                     }
-                    catch (CacheException e) {
-                        if (e.getCause() instanceof IgniteClientDisconnectedException) {
-                            IgniteClientDisconnectedException exc = (IgniteClientDisconnectedException)e.getCause();
-                            exc.reconnectFuture().get();
-                        }
-                        else
-                            throw e;
-                    }
-
+                }
 
                 long end = System.currentTimeMillis();
 
                 System.out.println(">>> Loaded " + ENTRY_COUNT + " keys in " + (end - start) + "ms.");
             }
-
+            finally {
+                // Distributed cache could be removed from cluster only by #destroyCache() call.
+                ignite.destroyCache(CACHE_NAME);
             }
-
-        }
-        finally {
-            // Distributed cache could be removed from cluster only by #destroyCache() call.
-            //ignite.destroyCache(CACHE_NAME);
         }
     }
 }
