183539df7370939794da84d81aaf23831e32e94a
dehasi <rgaleyev@gmail.com>
2019-03-08 23:06:40 +0100

    IGNITE-9978: Implement trainer

--- modules/ml/src/main/java/org/apache/ignite/ml/naivebayes/compound/CompoundNaiveBayesTrainer.java
+++ modules/ml/src/main/java/org/apache/ignite/ml/naivebayes/compound/CompoundNaiveBayesTrainer.java
@@ -22,7 +22,9 @@ import org.apache.ignite.ml.dataset.DatasetBuilder;
 import org.apache.ignite.ml.environment.LearningEnvironmentBuilder;
 import org.apache.ignite.ml.math.functions.IgniteBiFunction;
 import org.apache.ignite.ml.math.primitives.vector.Vector;
+import org.apache.ignite.ml.naivebayes.discrete.DiscreteNaiveBayesModel;
 import org.apache.ignite.ml.naivebayes.discrete.DiscreteNaiveBayesTrainer;
+import org.apache.ignite.ml.naivebayes.gaussian.GaussianNaiveBayesModel;
 import org.apache.ignite.ml.naivebayes.gaussian.GaussianNaiveBayesTrainer;
 import org.apache.ignite.ml.trainers.FeatureLabelExtractor;
 import org.apache.ignite.ml.trainers.SingleLabelDatasetTrainer;
@@ -30,6 +32,10 @@ import org.apache.ignite.ml.trainers.SingleLabelDatasetTrainer;
 /** Created by Ravil on 04/02/2019. */
 public class CompoundNaiveBayesTrainer extends SingleLabelDatasetTrainer<CompoundNaiveBayesModel> {
 
+    /** Prior probabilities of each class */
+    private double[] clsProbabilities;
+    /** Labels. */
+    private double[] labels;
     GaussianNaiveBayesTrainer gaussianNaiveBayesTrainer;
     private Predicate<Integer> gaussianSkipFeature;
     DiscreteNaiveBayesTrainer discreteNaiveBayesTrainer;
@@ -59,12 +65,18 @@ public class CompoundNaiveBayesTrainer extends SingleLabelDatasetTrainer<Compoun
 
     @Override protected <K, V> CompoundNaiveBayesModel updateModel(CompoundNaiveBayesModel mdl,
         DatasetBuilder<K, V> datasetBuilder, FeatureLabelExtractor<K, V, Double> extractor) {
-        if (mdl != null) {
             gaussianNaiveBayesTrainer.setSkipFeature(gaussianSkipFeature);
-            gaussianNaiveBayesTrainer.fit(datasetBuilder, extractor);
+            GaussianNaiveBayesModel gaussianNaiveBayesModel = gaussianNaiveBayesTrainer.fit(datasetBuilder, extractor);
             discreteNaiveBayesTrainer.setSkipFeature(discreteSkipFeature);
-            discreteNaiveBayesTrainer.fit(datasetBuilder, extractor);
-        }
-        return null;
+            DiscreteNaiveBayesModel discreteNaiveBayesModel = discreteNaiveBayesTrainer.fit(datasetBuilder, extractor);
+
+            return CompoundNaiveBayesModel.builder()
+                .withLabels(labels)
+                .wirhPriorProbabilities(clsProbabilities)
+                .withGaussianModel(gaussianNaiveBayesModel)
+                .withDiscreteSkipFuture(gaussianSkipFeature)
+                .withDiscreteModel(discreteNaiveBayesModel)
+                .withDiscreteSkipFuture(discreteSkipFeature)
+                .build();
     }
 }
