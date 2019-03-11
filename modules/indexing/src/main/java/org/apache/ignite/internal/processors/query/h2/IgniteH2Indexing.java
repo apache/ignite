0d1d71fea573f479170a50bd48f83ecaab395b2e
devozerov <vozerov@gridgain.com>
2019-03-11 12:13:01 +0300

    IGNITE-11227: SQL: Added missing security checks which were incorrectly removed in IGNITE-11227.

--- modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/IgniteH2Indexing.java
+++ modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/IgniteH2Indexing.java
@@ -456,9 +456,6 @@ public class IgniteH2Indexing implements GridQueryIndexing {
                 throw new IgniteSQLException("SELECT FOR UPDATE query requires transactional " +
                     "cache with MVCC enabled.", IgniteQueryErrorCode.UNSUPPORTED_OPERATION);
 
-            if (ctx.security().enabled())
-                checkSecurity(select.cacheIds());
-
             GridNearTxSelectForUpdateFuture sfuFut = null;
 
             int opTimeout = qryParams.timeout();
@@ -1426,6 +1423,9 @@ public class IgniteH2Indexing implements GridQueryIndexing {
         MvccQueryTracker mvccTracker,
         GridQueryCancel cancel
     ) throws IgniteCheckedException {
+        if (ctx.security().enabled())
+            checkSecurity(select.cacheIds());
+
         boolean autoStartTx = mvccEnabled(ctx) && !qryParams.autoCommit() && tx(ctx) == null;
 
         Iterable<List<?>> iter;
