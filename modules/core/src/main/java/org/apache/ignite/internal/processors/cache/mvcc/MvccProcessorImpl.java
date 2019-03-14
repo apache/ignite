065b6143f5b81374f091bb9cba503a9b725304bd
Andrey V. Mashenkov <andrey.mashenkov@gmail.com>
2019-03-14 12:21:42 +0300

    IGNITE-11215: Minor.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/mvcc/MvccProcessorImpl.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/mvcc/MvccProcessorImpl.java
@@ -2145,6 +2145,11 @@ public class MvccProcessorImpl extends GridProcessorAdapter implements MvccProce
 
                     throw e; // Cancelled.
                 }
+                catch (NodeStoppingException e) {
+                    task.onDone(e);
+
+                    return;
+                }
                 catch (Throwable e) {
                     task.onDone(e);
 
