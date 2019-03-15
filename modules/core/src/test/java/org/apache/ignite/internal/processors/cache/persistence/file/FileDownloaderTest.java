a825af5eabdfc379d98fe0022ff2265529189ce4
Alexey Goncharuk <alexey.goncharuk@gmail.com>
2019-03-15 17:04:03 +0300

    IGNITE-11546 Fixed FileDownloader early close - Fixes #6271.
    Signed-off-by: Alexey Goncharuk <alexey.goncharuk@gmail.com>

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/file/FileDownloaderTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/file/FileDownloaderTest.java
@@ -24,7 +24,6 @@ import java.net.InetSocketAddress;
 import java.nio.channels.SocketChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.concurrent.CountDownLatch;
 import org.apache.ignite.internal.util.future.GridFutureAdapter;
 import org.apache.ignite.internal.util.typedef.internal.U;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
@@ -85,7 +84,7 @@ public class FileDownloaderTest extends GridCommonAbstractTest {
 
         GridFutureAdapter<Long> finishFut = new GridFutureAdapter<>();
 
-        FileUploader uploader = new FileUploader(UPLOADER_PATH);
+        FileUploader uploader = new FileUploader(UPLOADER_PATH, log);
 
         SocketChannel sc = null;
 
@@ -96,13 +95,7 @@ public class FileDownloaderTest extends GridCommonAbstractTest {
             U.warn(log, "Fail connect to " + address, e);
         }
 
-        CountDownLatch downLatch = new CountDownLatch(1);
-
-        runAsync(() -> {
-            downloader.download(finishFut);
-
-            downLatch.countDown();
-        });
+        runAsync(downloader::download);
 
         SocketChannel finalSc = sc;
 
@@ -110,9 +103,9 @@ public class FileDownloaderTest extends GridCommonAbstractTest {
 
         finishFut.get();
 
-        downloader.download(finishFut.get(), null);
+        downloader.onResult(finishFut.get(), null);
 
-        downLatch.await();
+        downloader.finishFuture().get();
 
         assertTrue(DOWNLOADER_PATH.toFile().exists());
 
