be0840dc983646236907efbd5f06c0cdc630d374
Dmitriy Pavlov <dpavlov@apache.org>
2019-03-22 19:16:35 +0300

    IGNITE-11600: Fix launch scripts for Java 12 (#6326)
    (cherry picked from commit 47a49cd08d52cb96b7872b291057005abcc0789b)

--- modules/sqlline/bin/sqlline.bat
+++ modules/sqlline/bin/sqlline.bat
@@ -127,7 +127,7 @@ if %MAJOR_JAVA_VER% GEQ 9 if %MAJOR_JAVA_VER% LSS 11 (
     %JVM_OPTS%
 )
 
-if %MAJOR_JAVA_VER% == 11 (
+if %MAJOR_JAVA_VER% GEQ 11 (
     set JVM_OPTS= ^
     --add-exports=java.base/jdk.internal.misc=ALL-UNNAMED ^
     --add-exports=java.base/sun.nio.ch=ALL-UNNAMED ^
