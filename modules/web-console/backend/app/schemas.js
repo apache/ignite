01c45aad5b19c29e810eb6373d53019d1f7d220d
Vasiliy Sisko <vsisko@gridgain.com>
2019-03-29 13:17:02 +0700

    IGNITE-11259 Web Console: Added missing "enumValues" to binary configuration.

--- modules/web-console/backend/app/schemas.js
+++ modules/web-console/backend/app/schemas.js
@@ -590,7 +590,8 @@ module.exports.factory = function(mongoose) {
                 idMapper: String,
                 nameMapper: String,
                 serializer: String,
-                enum: Boolean
+                enum: Boolean,
+                enumValues: [String]
             }],
             compactFooter: Boolean
         },
