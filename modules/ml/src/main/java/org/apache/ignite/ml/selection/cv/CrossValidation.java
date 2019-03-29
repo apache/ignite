7ddf776bf4f6320284ed7f2c883c37751e5d302d
Zinoviev Alexey <zaleslaw.sin@gmail.com>
2019-03-29 14:26:03 +0300

    IGNITE-9497: [ML] Add Pipeline support to Cross-Validation process
    This closes #6226

--- modules/ml/src/main/java/org/apache/ignite/ml/selection/cv/CrossValidation.java
+++ modules/ml/src/main/java/org/apache/ignite/ml/selection/cv/CrossValidation.java
@@ -17,6 +17,14 @@
 
 package org.apache.ignite.ml.selection.cv;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.lang.IgniteBiPredicate;
@@ -27,6 +35,8 @@ import org.apache.ignite.ml.dataset.impl.cache.CacheBasedDatasetBuilder;
 import org.apache.ignite.ml.dataset.impl.local.LocalDatasetBuilder;
 import org.apache.ignite.ml.math.functions.IgniteBiFunction;
 import org.apache.ignite.ml.math.primitives.vector.Vector;
+import org.apache.ignite.ml.pipeline.Pipeline;
+import org.apache.ignite.ml.pipeline.PipelineMdl;
 import org.apache.ignite.ml.selection.paramgrid.ParamGrid;
 import org.apache.ignite.ml.selection.paramgrid.ParameterSetGenerator;
 import org.apache.ignite.ml.selection.scoring.cursor.CacheBasedLabelPairCursor;
@@ -36,15 +46,6 @@ import org.apache.ignite.ml.selection.scoring.metric.Metric;
 import org.apache.ignite.ml.selection.split.mapper.SHA256UniformMapper;
 import org.apache.ignite.ml.selection.split.mapper.UniformMapper;
 import org.apache.ignite.ml.trainers.DatasetTrainer;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.function.BiFunction;
-import java.util.function.Function;
 
 /**
  * Cross validation score calculator. Cross validation is an approach that allows to avoid overfitting that is made the
@@ -113,14 +114,14 @@ public class CrossValidation<M extends IgniteModel<Vector, L>, L, K, V> {
      * @param filter           Base {@code upstream} data filter.
      * @param featureExtractor Feature extractor.
      * @param lbExtractor      Label extractor.
-     * @param cv               Number of folds.
+     * @param amountOfFolds    Amount of folds.
      * @param paramGrid        Parameter grid.
      * @return Array of scores of the estimator for each run of the cross validation.
      */
     public CrossValidationResult score(DatasetTrainer<M, L> trainer, Metric<L> scoreCalculator, Ignite ignite,
-        IgniteCache<K, V> upstreamCache, IgniteBiPredicate<K, V> filter,
-        IgniteBiFunction<K, V, Vector> featureExtractor, IgniteBiFunction<K, V, L> lbExtractor, int cv,
-        ParamGrid paramGrid) {
+                                       IgniteCache<K, V> upstreamCache, IgniteBiPredicate<K, V> filter,
+                                       IgniteBiFunction<K, V, Vector> featureExtractor, IgniteBiFunction<K, V, L> lbExtractor, int amountOfFolds,
+                                       ParamGrid paramGrid) {
 
         List<Double[]> paramSets = new ParameterSetGenerator(paramGrid.getParamValuesByParamIdx()).generate();
 
@@ -159,7 +160,7 @@ public class CrossValidation<M extends IgniteModel<Vector, L>, L, K, V> {
             }
 
             double[] locScores = score(trainer, scoreCalculator, ignite, upstreamCache, filter, featureExtractor, lbExtractor,
-                new SHA256UniformMapper<>(), cv);
+                new SHA256UniformMapper<>(), amountOfFolds);
 
             cvRes.addScores(locScores, paramMap);
 
@@ -308,9 +309,9 @@ public class CrossValidation<M extends IgniteModel<Vector, L>, L, K, V> {
      */
     private double[] score(DatasetTrainer<M, L> trainer, Function<IgniteBiPredicate<K, V>,
         DatasetBuilder<K, V>> datasetBuilderSupplier,
-        BiFunction<IgniteBiPredicate<K, V>, M, LabelPairCursor<L>> testDataIterSupplier,
-        IgniteBiFunction<K, V, Vector> featureExtractor, IgniteBiFunction<K, V, L> lbExtractor,
-        Metric<L> scoreCalculator, UniformMapper<K, V> mapper, int cv) {
+                           BiFunction<IgniteBiPredicate<K, V>, M, LabelPairCursor<L>> testDataIterSupplier,
+                           IgniteBiFunction<K, V, Vector> featureExtractor, IgniteBiFunction<K, V, L> lbExtractor,
+                           Metric<L> scoreCalculator, UniformMapper<K, V> mapper, int cv) {
 
         double[] scores = new double[cv];
 
@@ -328,6 +329,149 @@ public class CrossValidation<M extends IgniteModel<Vector, L>, L, K, V> {
             M mdl = trainer.fit(datasetBuilder, FeatureLabelExtractorWrapper.wrap(featureExtractor, lbExtractor)); //TODO: IGNITE-11580
 
             try (LabelPairCursor<L> cursor = testDataIterSupplier.apply(trainSetFilter, mdl)) {
+                scores[i] = scoreCalculator.score(cursor.iterator());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return scores;
+    }
+
+
+    /**
+     * Computes cross-validated metrics with a passed parameter grid.
+     *
+     * The real cross-validation training will be called each time for each parameter set.
+     *
+     * @param pipeline        Pipeline of stages.
+     * @param scoreCalculator Base score calculator.
+     * @param ignite          Ignite instance.
+     * @param upstreamCache   Ignite cache with {@code upstream} data.
+     * @param filter          Base {@code upstream} data filter.
+     * @param lbExtractor     Label extractor.
+     * @param amountOfFolds   Amount of folds.
+     * @param paramGrid       Parameter grid.
+     * @return Array of scores of the estimator for each run of the cross validation.
+     */
+    public CrossValidationResult score(Pipeline<K, V, Vector> pipeline,
+                                       Metric<L> scoreCalculator,
+                                       Ignite ignite,
+                                       IgniteCache<K, V> upstreamCache,
+                                       IgniteBiPredicate<K, V> filter,
+                                       IgniteBiFunction<K, V, L> lbExtractor,
+                                       int amountOfFolds,
+                                       ParamGrid paramGrid) {
+
+        List<Double[]> paramSets = new ParameterSetGenerator(paramGrid.getParamValuesByParamIdx()).generate();
+
+        CrossValidationResult cvRes = new CrossValidationResult();
+
+        DatasetTrainer trainer = pipeline.getTrainer();
+
+        paramSets.forEach(paramSet -> {
+            Map<String, Double> paramMap = new HashMap<>();
+
+
+            for (int paramIdx = 0; paramIdx < paramSet.length; paramIdx++) {
+                String paramName = paramGrid.getParamNameByIndex(paramIdx);
+                Double paramVal = paramSet[paramIdx];
+
+                paramMap.put(paramName, paramVal);
+
+                try {
+                    final String mtdName = "with" +
+                        paramName.substring(0, 1).toUpperCase() +
+                        paramName.substring(1);
+
+                    Method trainerSetter = null;
+
+                    // We should iterate along all methods due to we have no info about signature and passed types.
+                    for (Method method : trainer.getClass().getDeclaredMethods()) {
+                        if (method.getName().equals(mtdName))
+                            trainerSetter = method;
+                    }
+
+                    if (trainerSetter != null)
+                        trainerSetter.invoke(trainer, paramVal);
+                    else
+                        throw new NoSuchMethodException(mtdName);
+
+                } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            double[] locScores = scorePipeline(
+                pipeline,
+                predicate -> new CacheBasedDatasetBuilder<>(
+                    ignite,
+                    upstreamCache,
+                    (k, v) -> filter.apply(k, v) && predicate.apply(k, v)
+                ),
+                (predicate, mdl) -> new CacheBasedLabelPairCursor<>(
+                    upstreamCache,
+                    (k, v) -> filter.apply(k, v) && !predicate.apply(k, v),
+                    ((PipelineMdl<K, V>) mdl).getFeatureExtractor(),
+                    lbExtractor,
+                    mdl
+                ),
+                scoreCalculator,
+                new SHA256UniformMapper<>(),
+                amountOfFolds
+            );
+
+
+            cvRes.addScores(locScores, paramMap);
+
+            final double locAvgScore = Arrays.stream(locScores).average().orElse(Double.MIN_VALUE);
+
+            if (locAvgScore > cvRes.getBestAvgScore()) {
+                cvRes.setBestScore(locScores);
+                cvRes.setBestHyperParams(paramMap);
+                System.out.println(paramMap.toString());
+            }
+        });
+
+        return cvRes;
+
+    }
+
+    /**
+     * Computes cross-validated metrics.
+     *
+     * @param pipeline               Pipeline of stages.
+     * @param datasetBuilderSupplier Dataset builder supplier.
+     * @param testDataIterSupplier   Test data iterator supplier.
+     * @param scoreCalculator        Base score calculator.
+     * @param mapper                 Mapper used to map a key-value pair to a point on the segment (0, 1).
+     * @param cv                     Number of folds.
+     * @return Array of scores of the estimator for each run of the cross validation.
+     */
+    private double[] scorePipeline(Pipeline<K, V, Vector> pipeline,
+                                   Function<IgniteBiPredicate<K, V>, DatasetBuilder<K, V>> datasetBuilderSupplier,
+                                   BiFunction<IgniteBiPredicate<K, V>, M, LabelPairCursor<L>> testDataIterSupplier,
+                                   Metric<L> scoreCalculator,
+                                   UniformMapper<K, V> mapper,
+                                   int cv
+    ) {
+
+        double[] scores = new double[cv];
+
+        double foldSize = 1.0 / cv;
+        for (int i = 0; i < cv; i++) {
+            double from = foldSize * i;
+            double to = foldSize * (i + 1);
+
+            IgniteBiPredicate<K, V> trainSetFilter = (k, v) -> {
+                double pnt = mapper.map(k, v);
+                return pnt < from || pnt > to;
+            };
+
+            DatasetBuilder<K, V> datasetBuilder = datasetBuilderSupplier.apply(trainSetFilter);
+            PipelineMdl<K, V> mdl = pipeline.fit(datasetBuilder);
+
+            try (LabelPairCursor<L> cursor = testDataIterSupplier.apply(trainSetFilter, (M) mdl)) {
                 scores[i] = scoreCalculator.score(cursor.iterator());
             } catch (Exception e) {
                 throw new RuntimeException(e);
