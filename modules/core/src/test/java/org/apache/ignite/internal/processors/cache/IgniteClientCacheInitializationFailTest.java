9a9c817f8fb663e9bbfaab76d3d08058d30597d1
EdShangGG <eshangareev@gridgain.com>
2019-03-29 18:23:55 +0300

    IGNITE-11606 Fix index update after index.bin manual removal - Fixes #6325.
    Signed-off-by: Alexey Goncharuk <alexey.goncharuk@gmail.com>

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteClientCacheInitializationFailTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteClientCacheInitializationFailTest.java
@@ -381,6 +381,11 @@ public class IgniteClientCacheInitializationFailTest extends GridCommonAbstractT
         }
 
         /** {@inheritDoc} */
+        @Override public void markAsRebuildNeeded(GridCacheContext cctx) {
+            // No-op.
+        }
+
+        /** {@inheritDoc} */
         @Override public IndexingQueryFilter backupFilter(AffinityTopologyVersion topVer, int[] parts) {
             return null;
         }
