eb5b555dbad0188069d8e50d947c14fd211a9bfa
dehasi <rgaleyev@gmail.com>
2019-03-30 22:38:52 +0100

    IGNITE-9978: Create a test dataset

--- modules/ml/src/test/java/org/apache/ignite/ml/naivebayes/compound/CompoundNaiveBayesModelTest.java
+++ modules/ml/src/test/java/org/apache/ignite/ml/naivebayes/compound/CompoundNaiveBayesModelTest.java
@@ -17,6 +17,8 @@
 
 package org.apache.ignite.ml.naivebayes.compound;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.ignite.ml.math.primitives.vector.Vector;
 import org.apache.ignite.ml.math.primitives.vector.VectorUtils;
 import org.apache.ignite.ml.naivebayes.discrete.DiscreteNaiveBayesModel;
@@ -28,6 +30,34 @@ import static org.junit.Assert.assertEquals;
 
 /** Tests for {@link CompoundNaiveBayesModel} */
 public class CompoundNaiveBayesModelTest {
+    /** Precision in test checks. */
+    private static final double PRECISION = 1e-2;
+
+    /** */
+    private static final double LABEL_1 = 1.;
+
+    /** */
+    private static final double LABEL_2 = 2.;
+
+    private static final Map<Integer, double[]> data = new HashMap<>();
+
+    static {
+        data.put(0, new double[] {6, 180, 12, 0, 0, 1, 1, 1, LABEL_1});
+        data.put(1, new double[] {5.92, 190, 11, 1, 0, 1, 1, 0, LABEL_1});
+        data.put(2, new double[] {5.58, 170, 12, 1, 1, 0, 0, 1, LABEL_1});
+        data.put(3, new double[] {5.92, 165, 10, 1, 1, 0, 0, 0, LABEL_1});
+//        data.put(4, new double[] {0, 1, 0, 0, 1, LABEL_1});
+//        data.put(5, new double[] {0, 0, 0, 1, 0, LABEL_1});
+
+        data.put(6, new double[] {5, 100, 6, 1, 0, 0, 1, 1, LABEL_2});
+        data.put(7, new double[] {5.5, 150, 8, 1, 1, 0, 0, 1, LABEL_2});
+        data.put(8, new double[] {5.42, 130, 7, 1, 1, 1, 1, 0, LABEL_2});
+        data.put(9, new double[] {5.75, 150, 9, 1, 1, 0, 1, 0, LABEL_2});
+//        data.put(10, new double[] {1, 1, 0, 1, 1, LABEL_2});
+//        data.put(11, new double[] {1, 0, 1, 1, 0, LABEL_2});
+//        data.put(12, new double[] {1, 0, 1, 0, 0, LABEL_2});
+
+    }
 
     @Test /** */
     public void testPredictOnlyGaus() {
@@ -86,6 +116,17 @@ public class CompoundNaiveBayesModelTest {
 
     @Test /** */
     public void testPredictGausAndDiscrete() {
+        double first = 1;
+        double second = 2;
+        double[][][] probabilities = new double[][][] {
+            {{.5, .5}, {.2, .3, .5}, {2. / 3., 1. / 3.}, {.4, .1, .5}, {.5, .5}},
+            {{0, 1}, {1. / 7, 2. / 7, 4. / 7}, {4. / 7, 3. / 7}, {2. / 7, 3. / 7, 2. / 7}, {4. / 7, 3. / 7,}}
+        };
+
+        double[] classProbabilities = new double[] {6. / 13, 7. / 13};
+        double[][] thresholds = new double[][] {{.5}, {.2, .7}, {.5}, {.5, 1.5}, {.5}};
+        DiscreteNaiveBayesModel mdl = new DiscreteNaiveBayesModel(probabilities, classProbabilities, new double[] {first, second}, thresholds, new DiscreteNaiveBayesSumsHolder());
+
 
     }
 }
--- modules/ml/src/test/java/org/apache/ignite/ml/naivebayes/compound/CompoundNaiveBayesTrainerTest.java
+++ modules/ml/src/test/java/org/apache/ignite/ml/naivebayes/compound/CompoundNaiveBayesTrainerTest.java
@@ -1,5 +1,40 @@
 package org.apache.ignite.ml.naivebayes.compound;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.ignite.ml.common.TrainerTest;
+
 /** Test for {@link CompoundNaiveBayesTrainer} */
-public class CompoundNaiveBayesTrainerTest {
+public class CompoundNaiveBayesTrainerTest  extends TrainerTest {
+
+    /** Precision in test checks. */
+    private static final double PRECISION = 1e-2;
+
+    /** */
+    private static final double LABEL_1 = 1.;
+
+    /** */
+    private static final double LABEL_2 = 2.;
+
+    private static final Map<Integer, double[]> data = new HashMap<>();
+
+    static {
+        data.put(0, new double[] {6, 180, 12, 0, 0, 1, 1, 1, LABEL_1});
+        data.put(1, new double[] {5.92, 190, 11, 1, 0, 1, 1, 0, LABEL_1});
+        data.put(2, new double[] {5.58, 170, 12, 1, 1, 0, 0, 1, LABEL_1});
+        data.put(3, new double[] {5.92, 165, 10, 1, 1, 0, 0, 0, LABEL_1});
+//        data.put(4, new double[] {0, 1, 0, 0, 1, LABEL_1});
+//        data.put(5, new double[] {0, 0, 0, 1, 0, LABEL_1});
+
+        data.put(4, new double[] {5, 100, 6, 1, 0, 0, 1, 1, LABEL_2});
+        data.put(5, new double[] {5.5, 150, 8, 1, 1, 0, 0, 1, LABEL_2});
+        data.put(6, new double[] {5.42, 130, 7, 1, 1, 1, 1, 0, LABEL_2});
+        data.put(7, new double[] {5.75, 150, 9, 1, 1, 0, 1, 0, LABEL_2});
+//        data.put(10, new double[] {1, 1, 0, 1, 1, LABEL_2});
+//        data.put(11, new double[] {1, 0, 1, 1, 0, LABEL_2});
+//        data.put(12, new double[] {1, 0, 1, 0, 0, LABEL_2});
+
+    }
+
+
 }
