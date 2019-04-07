7ebe42433846bf8ef61f7f64d338943d7e8dad28
Pavel Tupitsyn <ptupitsyn@apache.org>
2019-04-07 10:49:10 +0300

    IGNITE-11525 .NET: Deprecate SqlQuery API (#6415)
    * Mark APIs as Obsolete
    * Remove from examples

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Client/Cache/SqlQueryTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Client/Cache/SqlQueryTest.cs
@@ -34,6 +34,7 @@ namespace Apache.Ignite.Core.Tests.Client.Cache
         [Test]
         public void TestSqlQuery()
         {
+#pragma warning disable 618
             var cache = GetClientCache<Person>();
 
             // All items.
@@ -63,6 +64,7 @@ namespace Apache.Ignite.Core.Tests.Client.Cache
             qry.Sql = "abc";
             qry.QueryType = null;
             Assert.Throws<ArgumentNullException>(() => cache.Query(qry));
+#pragma warning restore 618
         }
 
         /// <summary>
@@ -71,6 +73,7 @@ namespace Apache.Ignite.Core.Tests.Client.Cache
         [Test]
         public void TestSqlQueryDistributedJoins()
         {
+#pragma warning disable 618
             var cache = GetClientCache<Person>();
 
             // Non-distributed join returns incomplete results.
@@ -83,6 +86,7 @@ namespace Apache.Ignite.Core.Tests.Client.Cache
             // Distributed join fixes the problem.
             qry.EnableDistributedJoins = true;
             Assert.AreEqual(Count, cache.Query(qry).Count());
+#pragma warning restore 618
         }
 
         /// <summary>
