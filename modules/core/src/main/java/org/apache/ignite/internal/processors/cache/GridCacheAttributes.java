17e89b5ab1858d8cee686a5bb519157c4119b674
Pavel Kovalenko <jokserfn@gmail.com>
2019-04-10 17:44:17 +0300

    IGNITE-1903 Fixed caches validation.
    Signed-off-by: Pavel Kovalenko <jokserfn@gmail.com>

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheAttributes.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheAttributes.java
@@ -167,6 +167,9 @@ public class GridCacheAttributes implements Serializable {
      * @return Eviction filter class name.
      */
     public String evictionFilterClassName() {
+        if (enrichment != null)
+            return enrichment.getFieldClassName("evictFilter");
+
         return className(ccfg.getEvictionFilter());
     }
 
@@ -184,6 +187,9 @@ public class GridCacheAttributes implements Serializable {
      * @return Eviction policy factory class name.
      */
     public String evictionPolicyFactoryClassName() {
+        if (enrichment != null)
+            return enrichment.getFieldClassName("evictPlcFactory");
+
         return className(ccfg.getEvictionPolicyFactory());
     }
 
@@ -205,7 +211,15 @@ public class GridCacheAttributes implements Serializable {
      * @return Near eviction policy factory class name.
      */
     public String nearEvictionPolicyFactoryClassName() {
-        return className(ccfg.getEvictionPolicyFactory());
+        NearCacheConfiguration nearCfg = ccfg.getNearConfiguration();
+
+        if (nearCfg == null)
+            return null;
+
+        if (enrichment != null && enrichment.nearCacheConfigurationEnrichment() != null)
+            return enrichment.nearCacheConfigurationEnrichment().getFieldClassName("nearEvictPlcFactory");
+
+        return className(nearCfg.getNearEvictionPolicyFactory());
     }
 
     /**
