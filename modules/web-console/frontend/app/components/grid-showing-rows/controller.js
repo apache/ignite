bf1aed798e1f48734212ecb5cc901a743bffe5a3
Ilya Borisov <klaster1@gmail.com>
2019-04-11 10:22:42 +0700

    IGNITE-9981 Web Console: Implemented advanced footer for grid.
    (cherry picked from commit 0d7070dbde1b354f5f14ae3f55f87c9c16be3ea7)

--- modules/web-console/frontend/app/components/grid-showing-rows/controller.js
+++ modules/web-console/frontend/app/components/grid-showing-rows/controller.js
@@ -18,12 +18,14 @@
 import _ from 'lodash';
 
 export default class {
-    static $inject = ['$scope', 'uiGridConstants'];
+    static $inject = ['$scope', 'IgniteCopyToClipboard', 'uiGridExporterService', 'uiGridExporterConstants', 'IgniteMessages', 'CSV'];
 
-    constructor($scope, uiGridConstants) {
-        Object.assign(this, {$scope, uiGridConstants});
+    constructor($scope, IgniteCopyToClipboard, uiGridExporterService, uiGridExporterConstants, IgniteMessages, CSV) {
+        Object.assign(this, {$scope, IgniteCopyToClipboard, uiGridExporterService, uiGridExporterConstants, IgniteMessages, CSV});
 
         this.count = 0;
+        this.visible = 0;
+        this.selected = 0;
     }
 
     $onChanges(changes) {
@@ -33,15 +35,61 @@ export default class {
             this.gridApi.core.on.rowsVisibleChanged(this.$scope, () => {
                 this.applyValues();
             });
+            this.gridApi.selection.on.rowSelectionChanged(this.$scope, () => this.updateSelectedCount());
+            this.gridApi.selection.on.rowSelectionChangedBatch(this.$scope, () => this.updateSelectedCount());
         }
+    }
+
+    updateSelectedCount() {
+        this.selected = this.gridApi.selection.getSelectedCount();
     }
 
     applyValues() {
         if (!this.gridApi.grid.rows.length) {
             this.count = 0;
+            this.visible = 0;
+            this.selected = 0;
             return;
         }
 
-        this.count = _.sumBy(this.gridApi.grid.rows, (row) => Number(row.visible));
+        this.count = this.gridApi.grid.rows.length;
+        this.visible = _.sumBy(this.gridApi.grid.rows, (row) => Number(row.visible));
+        this.updateSelectedCount();
+    }
+
+    copyToClipBoard() {
+        if (this.count === 0 || !this.gridApi) {
+            this.IgniteMessages.showError('No data to be copied');
+            return;
+        }
+
+        const data = [];
+        const grid = this.gridApi.grid;
+        grid.options.exporterSuppressColumns = [];
+        const exportColumnHeaders = this.uiGridExporterService.getColumnHeaders(grid, this.uiGridExporterConstants.VISIBLE);
+
+        grid.rows.forEach((row) => {
+            if (!row.visible)
+                return;
+
+            const values = [];
+
+            exportColumnHeaders.forEach((exportCol) => {
+                const col = grid.columns.find(({ field }) => field === exportCol.name);
+
+                if (!col || !col.visible || col.colDef.exporterSuppressExport === true)
+                    return;
+
+                const value = grid.getCellValue(row, col);
+
+                values.push({ value });
+            });
+
+            data.push(values);
+        });
+
+        const csvContent = this.uiGridExporterService.formatAsCsv(exportColumnHeaders, data, this.CSV.getSeparator());
+
+        this.IgniteCopyToClipboard.copy(csvContent);
     }
 }
