296f85607f0deae530f2ea6b7bc1caddf805d5f9
denis-chudov <dchudov@gridgain.com>
2019-04-11 17:20:50 +0300

    IGNITE-11392 Improve LRT diagnostic messages
    Signed-off-by: Ivan Rakov <irakov@apache.org>

--- modules/core/src/main/java/org/apache/ignite/IgniteSystemProperties.java
+++ modules/core/src/main/java/org/apache/ignite/IgniteSystemProperties.java
@@ -1171,6 +1171,13 @@ public final class IgniteSystemProperties {
     public static final String IGNITE_SQL_SYSTEM_SCHEMA_NAME_IGNITE = "IGNITE_SQL_SYSTEM_SCHEMA_NAME_IGNITE";
 
     /**
+     * Shows if dump requests from local node to near node are allowed, when long running transaction
+     * is found. If allowed, the compute request to near node will be made to get thread dump of transaction
+     * owner thread.
+     */
+    public static final String IGNITE_TX_OWNER_DUMP_REQUESTS_ALLOWED = "IGNITE_TX_OWNER_DUMP_REQUESTS_ALLOWED";
+
+    /**
      * Enforces singleton.
      */
     private IgniteSystemProperties() {
