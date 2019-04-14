d7686fee1b8285b89cacbd561c53721679568984
Alexey Scherbakov <alexey.scherbakoff@gmail.com>
2019-04-14 21:54:32 +0300

    IGNITE-10078 Disabled debugging counter for TC run.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/IgniteCacheOffheapManagerImpl.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/IgniteCacheOffheapManagerImpl.java
@@ -1439,8 +1439,8 @@ public class IgniteCacheOffheapManagerImpl implements IgniteCacheOffheapManager 
             this.name = name;
             this.rowStore = rowStore;
             this.dataTree = dataTree;
-            pCntr = new PartitionUpdateCounterDebugWrapper(grp, partId, 0);
-            //pCntr = new PartitionUpdateCounterImpl();
+            //pCntr = new PartitionUpdateCounterDebugWrapper(grp, partId, 0);
+            pCntr = new PartitionUpdateCounterImpl();
         }
 
         /**
