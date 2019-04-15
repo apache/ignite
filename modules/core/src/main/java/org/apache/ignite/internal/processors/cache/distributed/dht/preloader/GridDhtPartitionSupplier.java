a6da91a3f192b650c9485e14ccc1890bc75ea48e
Ilya Kasnacheev <ilya.kasnacheev@gmail.com>
2019-04-15 18:06:23 +0300

    IGNITE-11745 Fairly choose nodes for partition demanding during rebalance.
    Also adds EVT_CACHE_REBALANCE_PART_SUPPLIED and EVT_CACHE_REBALANCE_PART_MISSED events.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPartitionSupplier.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPartitionSupplier.java
@@ -54,6 +54,8 @@ import org.apache.ignite.internal.util.typedef.internal.U;
 import org.apache.ignite.lang.IgnitePredicate;
 import org.apache.ignite.spi.IgniteSpiException;
 
+import static org.apache.ignite.events.EventType.EVT_CACHE_REBALANCE_PART_MISSED;
+import static org.apache.ignite.events.EventType.EVT_CACHE_REBALANCE_PART_SUPPLIED;
 import static org.apache.ignite.internal.processors.cache.distributed.dht.topology.GridDhtPartitionState.OWNING;
 
 /**
@@ -365,6 +367,9 @@ class GridDhtPartitionSupplier {
 
                     remainingParts.remove(part);
 
+                    if (grp.eventRecordable(EVT_CACHE_REBALANCE_PART_MISSED))
+                        grp.addRebalanceMissEvent(part);
+
                     if (log.isDebugEnabled())
                         log.debug("Requested partition is marked as missing ["
                             + supplyRoutineInfo(topicId, nodeId, demandMsg) + ", p=" + part + "]");
@@ -392,6 +397,9 @@ class GridDhtPartitionSupplier {
                     supplyMsg.last(part, loc.updateCounter());
 
                     remainingParts.remove(part);
+
+                    if (grp.eventRecordable(EVT_CACHE_REBALANCE_PART_SUPPLIED))
+                        grp.addRebalanceSupplyEvent(part);
                 }
             }
 
@@ -409,11 +417,17 @@ class GridDhtPartitionSupplier {
                     supplyMsg.last(p, loc.updateCounter());
 
                     remainingIter.remove();
+
+                    if (grp.eventRecordable(EVT_CACHE_REBALANCE_PART_SUPPLIED))
+                        grp.addRebalanceSupplyEvent(p);
                 }
                 else if (iter.isPartitionMissing(p)) {
                     supplyMsg.missed(p);
 
                     remainingIter.remove();
+
+                    if (grp.eventRecordable(EVT_CACHE_REBALANCE_PART_MISSED))
+                        grp.addRebalanceMissEvent(p);
                 }
             }
 
