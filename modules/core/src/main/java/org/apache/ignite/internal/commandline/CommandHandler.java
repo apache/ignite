a7e4dac10eee628979af2731e4f5f65999cb092f
Ilya Kasnacheev <ilya.kasnacheev@gmail.com>
2019-04-17 16:19:27 +0300

    IGNITE-11753 Improve error message on first connection attempt to secured cluster.

--- modules/core/src/main/java/org/apache/ignite/internal/commandline/CommandHandler.java
+++ modules/core/src/main/java/org/apache/ignite/internal/commandline/CommandHandler.java
@@ -53,6 +53,7 @@ import org.apache.ignite.internal.client.GridClientFactory;
 import org.apache.ignite.internal.client.GridClientHandshakeException;
 import org.apache.ignite.internal.client.GridClientNode;
 import org.apache.ignite.internal.client.GridServerUnreachableException;
+import org.apache.ignite.internal.client.impl.GridClientImpl;
 import org.apache.ignite.internal.client.impl.connection.GridClientConnectionResetException;
 import org.apache.ignite.internal.client.ssl.GridSslBasicContextFactory;
 import org.apache.ignite.internal.commandline.argument.CommandArgUtils;
@@ -2797,6 +2798,8 @@ public class CommandHandler {
 
             boolean tryConnectAgain = true;
 
+            boolean suppliedAuth = !F.isEmpty(args.getUserName()) && !F.isEmpty(args.getPassword());
+
             int tryConnectMaxCount = 3;
 
             while (tryConnectAgain) {
@@ -2857,6 +2860,9 @@ public class CommandHandler {
                 }
 
                 try (GridClient client = GridClientFactory.start(clientCfg)) {
+                    if (!client.connected())
+                        client.throwLastError();
+
                     switch (args.command()) {
                         case ACTIVATE:
                             activate(client);
@@ -2896,12 +2902,15 @@ public class CommandHandler {
                 }
                 catch (Throwable e) {
                     if (tryConnectMaxCount > 0 && isAuthError(e)) {
-                        log("Authentication error, try connection again.");
+                        log(suppliedAuth ?
+                            "Authentication error, please try again." : "This cluster requires authentication.");
 
                         if (F.isEmpty(args.getUserName()))
                             args.setUserName(requestDataFromConsole("user: "));
 
                         args.setPassword(new String(requestPasswordFromConsole("password: ")));
+
+                        suppliedAuth = true;
 
                         tryConnectAgain = true;
 
