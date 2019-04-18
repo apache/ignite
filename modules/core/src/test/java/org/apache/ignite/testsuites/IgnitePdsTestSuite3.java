19dc2c0f7c247bec685b811f3ad23583c8a27116
Pavel Kovalenko <jokserfn@gmail.com>
2019-04-18 18:49:30 +0300

    IGNITE-11743 Keep cache descriptor available until cache is stopped - Fixes #6470.
    Signed-off-by: Pavel Kovalenko <jokserfn@gmail.com>
    
    (cherry picked from commit ee7f56f)
    Signed-off-by: Pavel Kovalenko <jokserfn@gmail.com>

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgnitePdsTestSuite3.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgnitePdsTestSuite3.java
@@ -21,6 +21,7 @@ import java.util.Collection;
 import junit.framework.TestSuite;
 import org.apache.ignite.internal.processors.cache.persistence.IgnitePdsContinuousRestartTest;
 import org.apache.ignite.internal.processors.cache.persistence.IgnitePdsContinuousRestartTestWithExpiryPolicy;
+import org.apache.ignite.internal.processors.cache.persistence.IgnitePdsNodeJoinWithCachesStopping;
 import org.apache.ignite.testframework.GridTestUtils;
 import org.junit.runner.RunWith;
 import org.junit.runners.AllTests;
@@ -59,5 +60,6 @@ public class IgnitePdsTestSuite3 extends TestSuite {
         // Rebalancing test
         GridTestUtils.addTestIfNeeded(suite, IgnitePdsContinuousRestartTest.class, ignoredTests);
         GridTestUtils.addTestIfNeeded(suite, IgnitePdsContinuousRestartTestWithExpiryPolicy.class, ignoredTests);
+        GridTestUtils.addTestIfNeeded(suite, IgnitePdsNodeJoinWithCachesStopping.class, ignoredTests);
     }
 }
