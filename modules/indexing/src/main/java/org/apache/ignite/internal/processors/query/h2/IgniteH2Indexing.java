9b00ae5363c53c525f60b33c3dc514faf842da08
tledkov <tledkov@gridgain.com>
2019-04-18 14:40:30 +0300

    IGNITE-11470 minors

--- modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/IgniteH2Indexing.java
+++ modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/IgniteH2Indexing.java
@@ -1774,7 +1774,7 @@ public class IgniteH2Indexing implements GridQueryIndexing {
         String colNamePtrn) {
         Collection<ColumnInformation> infos = new ArrayList<>();
 
-        //Gather information about tables
+        // Gather information about tables.
         schemaMgr.dataTables().stream()
             .filter(t -> matches(t.getSchema().getName(), schemaNamePtrn))
             .filter(t -> matches(t.getName(), tblNamePtrn))
