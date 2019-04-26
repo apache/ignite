63fc5cb45e583453b5f1491b47e9ca564afee0bf
mcherkasov <mcherkasov@gridgain.com>
2019-04-26 18:37:54 +0300

    GG-17424 Disable by default bash pipefail/errexit/nounset options from Ignite startup scripts
    (cherry picked from commit f871f10)
    
    (cherry picked from commit e66ff5d)

--- bin/ignite.sh
+++ bin/ignite.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
-set -o nounset
-set -o errexit
-set -o pipefail
-set -o errtrace
-set -o functrace
+if [ ! -z "${IGNITE_SCRIPT_STRICT_MODE:-}" ]
+then
+    set -o nounset
+    set -o errexit
+    set -o pipefail
+    set -o errtrace
+    set -o functrace
+fi
+
 #
 # Copyright 2019 GridGain Systems, Inc. and Contributors.
-# 
+#
 # Licensed under the GridGain Community Edition License (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
