c34d75724588509dfd7b5c5dbd2fbc44ddf5b140
Ryzhov Serhey <s.vi.ryzhov@gmail.com>
2019-04-29 13:24:56 +0300

    other

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/query/continuous/CacheContinuesQueryRemoteFilter.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/query/continuous/CacheContinuesQueryRemoteFilter.java
@@ -1,42 +1,52 @@
 package org.apache.ignite.internal.processors.cache.query.continuous;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import javax.cache.configuration.Factory;
+import javax.cache.event.CacheEntryEvent;
+import javax.cache.event.CacheEntryEventFilter;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.cache.CacheMode;
+import org.apache.ignite.cache.affinity.Affinity;
 import org.apache.ignite.cache.query.ContinuousQuery;
+import org.apache.ignite.cluster.ClusterNode;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.resources.IgniteInstanceResource;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.apache.log4j.BasicConfigurator;
 import org.junit.Test;
 
 public class CacheContinuesQueryRemoteFilter extends GridCommonAbstractTest {
-    /** */
+    /**
+     *
+     */
     protected static final String CLIENT = "_client";
 
-    /** */
+    /**
+     *
+     */
     protected static final String SERVER = "_server";
 
-
-
     private static final int DATA_AMOUNT = 10;
-    private static final long TIMEOUT = 30_000L;
+    private static final long TIMEOUT = 10_000L;
+
+    public static AtomicInteger counter = new AtomicInteger(0);
 
     @Test
     public void test() throws Exception {
         BasicConfigurator.configure();
 
-        Ignite grid1 = startGrid(1+SERVER);
-        Ignite grid2 =startGrid(2+SERVER);
+        Ignite grid1 = startGrid(1 + SERVER);
+        Ignite grid2 = startGrid(2 + SERVER);
 
-
-
-        AtomicInteger counter = new AtomicInteger(0);
         AtomicInteger cacheSize = new AtomicInteger(0);
 
-        try(Ignite client=startGrid("1" + CLIENT)) {
+        IgniteCache<Object, Object> cacheCounter = grid1.getOrCreateCache(new CacheConfiguration<Object, Object>()
+            .setName("counter")
+            .setCacheMode(CacheMode.REPLICATED));
 
+        try (Ignite client = startGrid("1" + CLIENT)) {
             IgniteCache<String, String> cache = client.getOrCreateCache(new CacheConfiguration<String, String>()
                 .setName("myCache")
                 .setCacheMode(CacheMode.REPLICATED));
@@ -46,15 +56,10 @@ public class CacheContinuesQueryRemoteFilter extends GridCommonAbstractTest {
 
 //            qry.setRemoteFilterFactory(() -> event -> {
 //                counter.incrementAndGet();
-//                System.out.println("RemoteFilter=" + event.getKey() + " " + event.getEventType());
+//                System.out.println("RemoteFilter=" + event.getKey() + " " + event.getEventType() + " counter " + counter.get());
 //                return true;
 //            });
-
-        qry.setRemoteFilter(evt -> {
-            counter.incrementAndGet();
-            System.out.println("RemoteFilter: type{" + evt.getEventType() + "}, key{" + evt.getKey() + "}, value{" + evt.getValue() + "}");
-            return true;
-        });
+            qry.setRemoteFilterFactory(new RemoteFactory<String, String>());
 
             qry.setLocalListener(events -> {
                 events.forEach(event -> {
@@ -62,21 +67,35 @@ public class CacheContinuesQueryRemoteFilter extends GridCommonAbstractTest {
                 });
             });
 
+            startGrid(3 + SERVER);
+
             cache.query(qry);
+            for (int i = 0; i < DATA_AMOUNT; i++)
+                cache.put("k" + i, "v" + i);
+            cache.forEach(a -> cacheSize.incrementAndGet());
+        }
+
+        assertTrue("Counter:" + counter.get(), (DATA_AMOUNT) == counter.get());
+
+        counter.set(0);
+
+        Ignite grid4 = startGrid(4 + SERVER);
+
+        try (Ignite client = startGrid("1" + CLIENT)) {
+            IgniteCache cache = client.getOrCreateCache(new CacheConfiguration<String, String>()
+                .setName("myCache")
+                .setCacheMode(CacheMode.REPLICATED));
 
             for (int i = 0; i < DATA_AMOUNT; i++)
                 cache.put("k" + i, "v" + i);
-
-            cache.forEach(a->cacheSize.incrementAndGet());
-
         }
 
         Thread.sleep(TIMEOUT);
 
+        assertTrue("CacheSize:" + cacheSize.get(), DATA_AMOUNT == cacheSize.get());
 
-        assertTrue("CacheSize:"+cacheSize.get(), DATA_AMOUNT==cacheSize.get());
+        assertTrue("Counter:" + counter.get(), DATA_AMOUNT == counter.get());
 
-        assertTrue("Counter:"+counter.get(),(DATA_AMOUNT*2)==counter.get());
 
     }
 
@@ -89,3 +108,45 @@ public class CacheContinuesQueryRemoteFilter extends GridCommonAbstractTest {
     }
 }
 
+class RemoteFactory<T,E> implements Factory<CacheEntryEventFilter<T, E>> {
+
+    @IgniteInstanceResource Ignite ignite;
+
+    private ClusterNode node;
+
+    public RemoteFactory(ClusterNode node) {
+        this.node = node;
+    }
+
+    public RemoteFactory() {
+    }
+
+    @Override
+    public CacheEntryEventFilter<T, E> create() {
+        return new CacheEntryEventFilter<T, E>() {
+            @IgniteInstanceResource
+            private Ignite ignite;
+
+            private ClusterNode node;
+
+            @Override
+            public boolean evaluate(CacheEntryEvent<? extends T, ? extends E> evt) {
+                if (nodesPrimaryForThisKey(evt)){
+                    CacheContinuesQueryRemoteFilter.counter.incrementAndGet();
+                    return true;
+                }else
+
+                return false;
+            }
+
+            boolean nodesPrimaryForThisKey(CacheEntryEvent evt) {
+                Affinity aff = ignite.affinity(evt.getSource().getName());
+                node = ignite.cluster().localNode();
+                ClusterNode primary = aff.mapKeyToNode(evt.getKey());
+                return primary.id().equals(node.id());
+            }
+
+
+        };
+    }
+}
