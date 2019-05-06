c4783d360324a1dd7ba8b84f61a5d46e4b0c07e1
tledkov <tledkov@gridgain.com>
2019-05-06 15:31:43 +0300

    GG-17326 Backport [IGNITE-11604] SQL: Fixed problem with DROP COLUMN which was not able remove a column properly in some cases

--- modules/core/src/main/java/org/apache/ignite/internal/processors/query/QueryUtils.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/query/QueryUtils.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2019 GridGain Systems, Inc. and Contributors.
- * 
+ *
  * Licensed under the GridGain Community Edition License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -1494,6 +1494,35 @@ public class QueryUtils {
         }
 
         return new SQLException(e.getMessage(), sqlState, code, e);
+    }
+
+    /**
+     * Get field name by alias.
+     *
+     * @param entity Query entity.
+     * @param alias Filed's alias.
+     * @return Field name.
+     */
+    public static String fieldNameByAlias(QueryEntity entity, String alias) {
+        if (!F.isEmpty(entity.getAliases())) {
+            for (Map.Entry<String, String> aliasEntry : entity.getAliases().entrySet()) {
+                if (F.eq(aliasEntry.getValue(), alias))
+                    return aliasEntry.getKey();
+            }
+        }
+
+        return alias;
+    }
+
+    /**
+     * Remove field by alias.
+     *
+     * @param entity Query entity.
+     * @param alias Filed's alias.
+     * @return {@code true} if the field is removed. Otherwise returns {@code false}.
+     */
+    public static boolean removeField(QueryEntity entity, String alias) {
+        return entity.getFields().remove(fieldNameByAlias(entity, alias)) != null;
     }
 
     /**
