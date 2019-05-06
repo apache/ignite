f72889747d2c7bfdaea332701ac13ad27ae4f4f4
Pavel Kuznetsov <palmihal@gmail.com>
2019-05-06 20:52:32 +0300

    ignite-10690: Fixed Ignite unsupported sql types.
    tests wip.

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/IgniteCacheSqlQueryUnsupportedTypeSelfTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/IgniteCacheSqlQueryUnsupportedTypeSelfTest.java
@@ -17,11 +17,15 @@
 
 package org.apache.ignite.internal.processors.cache;
 
+import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 import org.apache.ignite.IgniteCache;
+import org.apache.ignite.binary.BinaryObject;
 import org.apache.ignite.cache.QueryEntity;
 import org.apache.ignite.cache.QueryIndex;
 import org.apache.ignite.cache.query.SqlFieldsQuery;
@@ -32,10 +36,16 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Checks that types that types that is not (yet) supported by Ignite (even though those are supported by H2) can be
- * inserted into the database and are mapped to JAVA_OBJECT.
+ * Checks that both types can be inserted and queried:
+ * 1) not (yet) supported by Ignite (even though they are supported by H2) : Instant,
+ * 2) native sql types : Long, Integer, String...
  */
 public class IgniteCacheSqlQueryUnsupportedTypeSelfTest extends GridCommonAbstractTest {
+    /**
+     * Name of the value type. Fake. We use binary objects to put to the cache.
+     */
+    private final String TYPE_NAME = "SomeType";
+
     /**
      * Starts cluster with one node.
      */
@@ -48,62 +58,108 @@ public class IgniteCacheSqlQueryUnsupportedTypeSelfTest extends GridCommonAbstra
      * Stops the cluster.
      */
     @After
-    public void tearOff () {
+    public void tearOff() {
         stopAllGrids();
     }
 
     /**
-     * Execute sql query.
+     * Create cache configuration containing table that has field with the specified type.
      *
-     * @param sql query.
-     * @param args positional args.
-     * @return Fetched result of the query.
+     * @param testedFldCls type of the "testFld" column/field.
+     * @return cache configuration.
      */
-    private List<List<?>> execute(String sql, Object... args) {
-        return grid(0).cache("CACHE").query(new SqlFieldsQuery(sql).setSchema("PUBLIC").setArgs(args)).getAll();
-    }
-
-    private CacheConfiguration instantCacheConfiguration() {
+    private CacheConfiguration cacheConfigForType(Class testedFldCls) {
         return new CacheConfiguration()
-            .setName("CACHE")
+            .setName("CACHE_" + testedFldCls.getSimpleName())
             .setQueryEntities(Collections.singleton(
-                new QueryEntity(Integer.class.getName(), Person.class.getName())
+                new QueryEntity(Integer.class.getName(), TYPE_NAME)
                     .addQueryField("id", Integer.class.getName(), null)
-                    .addQueryField("time", Instant.class.getName(), null)
+                    .addQueryField("testFld", testedFldCls.getName(), null)
                     .setTableName("PERSON")
-                .setIndexes(Arrays.asList(
-                    new QueryIndex("id", true),
-                    new QueryIndex("time", true)
-                ))
+                    .setIndexes(Arrays.asList(
+                        new QueryIndex("id", true),
+                        new QueryIndex("testFld", true)
+                    ))
             ));
     }
 
     /**
-     * Check that unsupported by IgniteSQL type is correctly inserted.
+     * Check that both natively supported and unsupported by IgniteSQL types are correctly inserted and queried.
      */
     @Test
-    public void testUnsupportedSqlType(){
-        try (IgniteCache<Integer, Person> person = grid(0).createCache(instantCacheConfiguration())) {
-            person.put(1, new Person(1));
-            person.put(2, new Person(2));
+    public void testUnsupportedSqlType() {
+        Object[] testedValues = {
+            // Types that maps on sql types natively:
+            "String",
+            42,
+            7L,
+            true,
+            1.2d,
+            (byte)25,
+            (short)54,
+            //Character is unsupported
+            3.4f,
+            "garbage".getBytes(),
+            UUID.randomUUID(),
+            new BigDecimal("1.450"),
+            new java.sql.Date(System.currentTimeMillis()),
+            new java.sql.Time(System.currentTimeMillis()),
+            new java.sql.Timestamp(System.currentTimeMillis()),
+            new java.util.Date(),
+            // No tests for the geometry
+            java.time.LocalDate.now(),
+            java.time.LocalTime.now(),
+            java.time.LocalDateTime.now(),
 
-            List<List<?>> res = execute("SELECT * FROM CACHE.PERSON WHERE time = (select time from cache.person where id = 1)");
+            // Non native sql types (mapped to JAVA_OBJECT):
+            Instant.now(),
+            new Object(),
+            new HashMap<>()
+        };
 
-            assertEquals(res.get(0).get(0), 1);
-        }
+        for (Object val : testedValues)
+            testType(val, val.getClass());
+
+        testType(null, Void.class);
+        testType(null, Void.TYPE);
     }
 
     /**
-     * Pojo with the unsupported type.
+     * Check that specified value can be inserted into column of specified type.
+     * @param val value to be inserted.
+     * @param testedType type of the column.
      */
-    public static class Person {
-        int id;
+    private void testType(Object val, Class testedType) {
+        try (IgniteCache<Integer, BinaryObject> cache = grid(0).createCache(cacheConfigForType(testedType)).withKeepBinary()) {
+            cache.put(1, createVal(1, val));
 
-        Instant time;
+            List<List<?>> res = cache.query(new SqlFieldsQuery("SELECT * FROM PERSON WHERE testFld = (select testFld from person where id = 1)")).getAll();
 
-        Person(int id) {
-            this.id = id;
-            time = Instant.now();
+            assertEquals(res.get(0).get(0), 1);
+            assertEquals(res.get(0).get(1), val);
         }
+        catch (Exception e) {
+            throw new AssertionError("Couldn't validate field of type " + testedType +
+                " with the sample value " + val, e);
+        }
+    }
+
+    @Test
+    public void test() {
+        createVal(1, 42);
+    }
+
+    /**
+     * Create binary object with field of tested type. This object matches table, created by {@linkplain #cacheConfigForType(Class)}.
+     *
+     * @param id id field value.
+     * @param testedFldVal value of the tested field.
+     * @return binary object to put into the cache.
+     */
+    private BinaryObject createVal(int id, Object testedFldVal) {
+        return grid(0).binary().builder(TYPE_NAME)
+            .setField("id", id)
+            .setField("testFld", testedFldVal, ((Class<? super Object>)testedFldVal.getClass()))
+            .build();
     }
 }
