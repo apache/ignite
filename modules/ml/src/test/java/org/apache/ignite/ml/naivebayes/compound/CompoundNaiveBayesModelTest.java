cc5d08d85c10a035f7661b76d2876fe482b1efd2
Ravil Galeyev <dehasi@yandex.ru>
2019-05-07 22:34:08 +0200

    IGNITE-9978: Refactoring

--- modules/ml/src/test/java/org/apache/ignite/ml/naivebayes/compound/CompoundNaiveBayesModelTest.java
+++ modules/ml/src/test/java/org/apache/ignite/ml/naivebayes/compound/CompoundNaiveBayesModelTest.java
@@ -41,6 +41,8 @@ public class CompoundNaiveBayesModelTest {
     /** The second label*/
     private static final double LABEL_2 = 2.;
 
+    private static final double[] labels = {LABEL_1, LABEL_2};
+
     /** */
     private static final Map<Integer, double[]> data = new HashMap<>();
 
@@ -49,24 +51,15 @@ public class CompoundNaiveBayesModelTest {
         data.put(1, new double[] {5.92, 190, 11, 1, 0, 1, 1, 0, LABEL_1});
         data.put(2, new double[] {5.58, 170, 12, 1, 1, 0, 0, 1, LABEL_1});
         data.put(3, new double[] {5.92, 165, 10, 1, 1, 0, 0, 0, LABEL_1});
-//        data.put(4, new double[] {0, 1, 0, 0, 1, LABEL_1});
-//        data.put(5, new double[] {0, 0, 0, 1, 0, LABEL_1});
 
-        data.put(6, new double[] {5, 100, 6, 1, 0, 0, 1, 1, LABEL_2});
-        data.put(7, new double[] {5.5, 150, 8, 1, 1, 0, 0, 1, LABEL_2});
-        data.put(8, new double[] {5.42, 130, 7, 1, 1, 1, 1, 0, LABEL_2});
-        data.put(9, new double[] {5.75, 150, 9, 1, 1, 0, 1, 0, LABEL_2});
-//        data.put(10, new double[] {1, 1, 0, 1, 1, LABEL_2});
-//        data.put(11, new double[] {1, 0, 1, 1, 0, LABEL_2});
-//        data.put(12, new double[] {1, 0, 1, 0, 0, LABEL_2});
-
+        data.put(4, new double[] {5, 100, 6, 1, 0, 0, 1, 1, LABEL_2});
+        data.put(5, new double[] {5.5, 150, 8, 1, 1, 0, 0, 1, LABEL_2});
+        data.put(6, new double[] {5.42, 130, 7, 1, 1, 1, 1, 0, LABEL_2});
+        data.put(7, new double[] {5.75, 150, 9, 1, 1, 0, 1, 0, LABEL_2});
     }
 
     @Test /** */
     public void testPredictOnlyGaus() {
-        double first = 1;
-        double second = 2;
-        double[] labels = {first, second};
         double[] classProbabilities = new double[] {.5, .5};
 
         double[][] means = new double[][] {
@@ -87,12 +80,11 @@ public class CompoundNaiveBayesModelTest {
             .withLabels(labels)
             .withGaussianModel(gaussianModel);
 
-        assertEquals(second, model.predict(observation), PRECISION);
+        assertEquals(LABEL_2, model.predict(observation), PRECISION);
     }
 
     @Test /** */
     public void testPredictOnlyDiscrete() {
-        double[] labels = {LABEL_1, LABEL_2};
         double[] classProbabilities = new double[] {6. / 13, 7. / 13};
 
         double[][][] probabilities = new double[][][] {
@@ -120,7 +112,6 @@ public class CompoundNaiveBayesModelTest {
             {{0, 1}, {1. / 7, 2. / 7, 4. / 7}, {4. / 7, 3. / 7}, {2. / 7, 3. / 7, 2. / 7}, {4. / 7, 3. / 7,}}
         };
         double[] classProbabilities = new double[] {.5, .5};
-        double[] labels = {LABEL_1, LABEL_2};
 
         double[][] thresholds = new double[][] { {.5}, {.5}, {.5}, {.5}, {.5}};
         DiscreteNaiveBayesModel discreteModel = new DiscreteNaiveBayesModel(probabilities, classProbabilities, labels, thresholds, null);
