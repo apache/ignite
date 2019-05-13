e046236376f10460bb4284f4a4717a034f3520e3
Pavel Kuznetsov <palmihal@gmail.com>
2019-05-13 21:23:17 +0300

    Revert "ignite-10690: WIP"
    This reverts commit 406383b9

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/IgniteCacheSqlQueryUnsupportedTypeSelfTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/IgniteCacheSqlQueryUnsupportedTypeSelfTest.java
@@ -36,10 +36,16 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Checks that both types can be inserted and queried: 1) not (yet) supported by Ignite, even though they are supported
- * by H2 (Instant for example) 2) native sql types (Long, Integer, String...)
+ * Checks that both types can be inserted and queried:
+ * 1) not (yet) supported by Ignite (even though they are supported by H2) : Instant,
+ * 2) native sql types : Long, Integer, String...
  */
 public class IgniteCacheSqlQueryUnsupportedTypeSelfTest extends GridCommonAbstractTest {
+    /**
+     * Name of the value type. Fake. We use binary objects to put to the cache.
+     */
+    private final String TYPE_NAME = "SomeType";
+
     /**
      * Starts cluster with one node.
      */
@@ -64,9 +70,9 @@ public class IgniteCacheSqlQueryUnsupportedTypeSelfTest extends GridCommonAbstra
      */
     private CacheConfiguration cacheConfigForType(Class testedFldCls) {
         return new CacheConfiguration()
-            .setName("CACHE_" + valueName(testedFldCls))
+            .setName("CACHE_" + testedFldCls.getSimpleName())
             .setQueryEntities(Collections.singleton(
-                new QueryEntity(Integer.class.getName(), valueName(testedFldCls))
+                new QueryEntity(Integer.class.getName(), TYPE_NAME)
                     .addQueryField("id", Integer.class.getName(), null)
                     .addQueryField("testFld", testedFldCls.getName(), null)
                     .setTableName("PERSON")
@@ -78,20 +84,10 @@ public class IgniteCacheSqlQueryUnsupportedTypeSelfTest extends GridCommonAbstra
     }
 
     /**
-     * Computes type name of the cache value. It is useful to have this name based on the tested type name.
-     *
-     * @param testedFldCls type of the field, currently is tested.
-     * @return type name for the cache value.
-     */
-    private static String valueName(Class testedFldCls) {
-        return testedFldCls.getName().replace('.', '_');
-    }
-
-    /**
      * Check that both natively supported and unsupported by IgniteSQL types are correctly inserted and queried.
      */
     @Test
-    public void testAllSqlTypes() {
+    public void testUnsupportedSqlType() {
         Object[] testedValues = {
             // Types that maps on sql types natively:
             "String",
@@ -106,8 +102,8 @@ public class IgniteCacheSqlQueryUnsupportedTypeSelfTest extends GridCommonAbstra
             "garbage".getBytes(),
             UUID.randomUUID(),
             new BigDecimal("1.450"),
-            java.sql.Date.valueOf("2019-05-05"),
-            java.sql.Time.valueOf("15:45:10"),
+            new java.sql.Date(System.currentTimeMillis()),
+            new java.sql.Time(System.currentTimeMillis()),
             new java.sql.Timestamp(System.currentTimeMillis()),
             new java.util.Date(),
             // No tests for the geometry
@@ -118,51 +114,52 @@ public class IgniteCacheSqlQueryUnsupportedTypeSelfTest extends GridCommonAbstra
             // Non native sql types (mapped to JAVA_OBJECT):
             Instant.now(),
             new Object(),
-            new HashMap<>(),
-            new Object[]{1,2,3}
+            new HashMap<>()
         };
 
         for (Object val : testedValues)
             testType(val, val.getClass());
 
-//        testType(null, Void.class);
-//        testType(null, Void.TYPE);
+        testType(null, Void.class);
+        testType(null, Void.TYPE);
     }
 
     /**
      * Check that specified value can be inserted into column of specified type.
-     *
      * @param val value to be inserted.
      * @param testedType type of the column.
      */
     private void testType(Object val, Class testedType) {
         try (IgniteCache<Integer, BinaryObject> cache = grid(0).createCache(cacheConfigForType(testedType)).withKeepBinary()) {
-            cache.put(1, createVal(1, val, testedType));
+            cache.put(1, createVal(1, val));
 
-            List<List<?>> res = cache.query(new SqlFieldsQuery("SELECT * FROM PERSON WHERE testFld = ?").setArgs(val)).getAll();
+            List<List<?>> res = cache.query(new SqlFieldsQuery("SELECT * FROM PERSON WHERE testFld = (select testFld from person where id = 1)")).getAll();
 
-            assertEquals("Expected only one result", 1, res.size());
+            assertEquals(res.get(0).get(0), 1);
+            assertEquals(res.get(0).get(1), val);
         }
-        catch (Throwable e) {
-            throw new AssertionError("Couldn't validate field of type " + testedType.getTypeName() +
+        catch (Exception e) {
+            throw new AssertionError("Couldn't validate field of type " + testedType +
                 " with the sample value " + val, e);
         }
     }
 
+    @Test
+    public void test() {
+        createVal(1, 42);
+    }
+
     /**
-     * Create binary object with field of tested type. This object matches table, created by {@linkplain
-     * #cacheConfigForType(Class)}. Name of the binary type is based on the tested field type.
+     * Create binary object with field of tested type. This object matches table, created by {@linkplain #cacheConfigForType(Class)}.
      *
      * @param id id field value.
      * @param testedFldVal value of the tested field.
      * @return binary object to put into the cache.
      */
-    private BinaryObject createVal(int id, Object testedFldVal, Class fldType) {
-        String binObjName = valueName(fldType);
-
-        return grid(0).binary().builder(binObjName)
+    private BinaryObject createVal(int id, Object testedFldVal) {
+        return grid(0).binary().builder(TYPE_NAME)
             .setField("id", id)
-            .setField("testFld", testedFldVal, fldType)
+            .setField("testFld", testedFldVal, ((Class<? super Object>)testedFldVal.getClass()))
             .build();
     }
 }
