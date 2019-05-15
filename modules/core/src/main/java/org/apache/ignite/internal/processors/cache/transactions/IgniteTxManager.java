7ecad3511996b845921ff84d9ecdd6c4204f53c4
Andrey Kalinin <prize2step@gmail.com>
2019-05-15 09:45:36 +0300

    GG-17472 PVS: V6004 The 'then' statement is equivalent to the 'else' statement. IgniteTxManager (#22)
    (cherry picked from commit 54041afdfc8c6fb514f40e191371b10ec22afc2a)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/transactions/IgniteTxManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/transactions/IgniteTxManager.java
@@ -2226,12 +2226,8 @@ public class IgniteTxManager extends GridCacheSharedManagerAdapter {
                     if (nearFut != null) {
                         Set<IgniteTxKey> nearRequestedKeys = nearFut.requestedKeys();
 
-                        if (nearRequestedKeys != null) {
-                            if (requestedKeys == null)
-                                requestedKeys = nearRequestedKeys;
-                            else
-                                requestedKeys = nearRequestedKeys;
-                        }
+                        if (nearRequestedKeys != null)
+                            requestedKeys = nearRequestedKeys;
                     }
                 }
                 else {
