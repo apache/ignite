effda413530dd1028d2a734fabf6cb6570e0e5be
Ravil Galeyev <dehasi@yandex.ru>
2019-05-15 21:31:08 +0200

    IGNITE-9978: Extract data

--- modules/ml/src/test/java/org/apache/ignite/ml/naivebayes/compound/CompoundNaiveBayesTrainerTest.java
+++ modules/ml/src/test/java/org/apache/ignite/ml/naivebayes/compound/CompoundNaiveBayesTrainerTest.java
@@ -7,14 +7,12 @@ import org.apache.ignite.ml.naivebayes.discrete.DiscreteNaiveBayesModel;
 import org.apache.ignite.ml.naivebayes.discrete.DiscreteNaiveBayesTrainer;
 import org.apache.ignite.ml.naivebayes.gaussian.GaussianNaiveBayesModel;
 import org.apache.ignite.ml.naivebayes.gaussian.GaussianNaiveBayesTrainer;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
+import static org.apache.ignite.ml.naivebayes.compound.Data.*;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
@@ -23,29 +21,6 @@ public class CompoundNaiveBayesTrainerTest extends TrainerTest {
 
     /** Precision in test checks. */
     private static final double PRECISION = 1e-2;
-
-    /** The first label */
-    private static final double LABEL_1 = 1.;
-
-    /** The second label */
-    private static final double LABEL_2 = 2.;
-
-    private static final Map<Integer, double[]> data = new HashMap<>();
-
-    /** */
-    private static final double[][] BINARIZED_DATA_THRESHOLDS = new double[][] {{.5}, {.5}, {.5}, {.5}, {.5}};
-
-    static {
-        data.put(0, new double[]{6, 180, 12, 0, 0, 1, 1, 1, LABEL_1});
-        data.put(1, new double[]{5.92, 190, 11, 1, 0, 1, 1, 0, LABEL_1});
-        data.put(2, new double[]{5.58, 170, 12, 1, 1, 0, 0, 1, LABEL_1});
-        data.put(3, new double[]{5.92, 165, 10, 1, 1, 0, 0, 0, LABEL_1});
-
-        data.put(6, new double[]{5, 100, 6, 1, 0, 0, 1, 1, LABEL_2});
-        data.put(7, new double[]{5.5, 150, 8, 1, 1, 0, 0, 1, LABEL_2});
-        data.put(8, new double[]{5.42, 130, 7, 1, 1, 1, 1, 0, LABEL_2});
-        data.put(9, new double[]{5.75, 150, 9, 1, 1, 0, 1, 0, LABEL_2});
-    }
 
     /** */
     private CompoundNaiveBayesTrainer trainer;
@@ -58,7 +33,7 @@ public class CompoundNaiveBayesTrainerTest extends TrainerTest {
                 .setClsProbabilities(new double[]{.5, .5})
                 .setGaussianNaiveBayesTrainer(new GaussianNaiveBayesTrainer().setSkipFeature(f -> f > 2))
                 .setDiscreteNaiveBayesTrainer(new DiscreteNaiveBayesTrainer()
-                        .setBucketThresholds(BINARIZED_DATA_THRESHOLDS)
+                        .setBucketThresholds(binarizedDataThresholds)
                         .withEquiprobableClasses()
                         .setSkipFeature(f -> f <= 2));
     }
