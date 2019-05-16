7a0d17427918229dba1960e4871d5fe61c2b6191
Andrey Kalinin <prize2step@gmail.com>
2019-05-16 11:54:58 +0300

    GG-18682 Should to filtered indexes by cache name instead of validate all caches in group. (#29)
    (cherry picked from commit 69b56e71e7409b477f7be300fe15ee79d7af8541)

--- modules/indexing/src/main/java/org/apache/ignite/internal/visor/verify/ValidateIndexesClosure.java
+++ modules/indexing/src/main/java/org/apache/ignite/internal/visor/verify/ValidateIndexesClosure.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2019 GridGain Systems, Inc. and Contributors.
- * 
+ *
  * Licensed under the GridGain Community Edition License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -220,20 +220,22 @@ public class ValidateIndexesClosure implements IgniteCallable<VisorValidateIndex
             IgniteH2Indexing indexing = (IgniteH2Indexing)qry.getIndexing();
 
             for (GridCacheContext ctx : grpCtx.caches()) {
-                Collection<GridQueryTypeDescriptor> types = qry.types(ctx.name());
+                if (cacheNames.contains(ctx.name())) {
+                    Collection<GridQueryTypeDescriptor> types = qry.types(ctx.name());
 
-                if (!F.isEmpty(types)) {
-                    for (GridQueryTypeDescriptor type : types) {
-                        GridH2Table gridH2Tbl = indexing.dataTable(ctx.name(), type.tableName());
+                    if (!F.isEmpty(types)) {
+                        for (GridQueryTypeDescriptor type : types) {
+                            GridH2Table gridH2Tbl = indexing.dataTable(ctx.name(), type.tableName());
 
-                        if (gridH2Tbl == null)
-                            continue;
+                            if (gridH2Tbl == null)
+                                continue;
 
-                        ArrayList<Index> indexes = gridH2Tbl.getIndexes();
+                            ArrayList<Index> indexes = gridH2Tbl.getIndexes();
 
-                        for (Index idx : indexes)
-                            if (idx instanceof H2TreeIndex)
-                                idxArgs.add(new T2<>(ctx, idx));
+                            for (Index idx : indexes)
+                                if (idx instanceof H2TreeIndex)
+                                    idxArgs.add(new T2<>(ctx, idx));
+                        }
                     }
                 }
             }
