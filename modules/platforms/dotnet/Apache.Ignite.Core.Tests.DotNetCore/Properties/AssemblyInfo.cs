538fd91c0fa603ecf251306094501ab77ef285b4
Alexandr Shapkin <ashapkin@gridgain.com>
2019-05-17 13:24:43 +0300

    GG-18794 Can't run example .Net with .Net docker node

---
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests.DotNetCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Apache.Ignite.Core.Tests.DotNetCore.Properties")]
+[assembly: AssemblyDescription("Apache Ignite.NET Core Tests")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Apache Software Foundation")]
+[assembly: AssemblyProduct("Apache Ignite.NET")]
+[assembly: AssemblyCopyright("Copyright 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+#if NETCOREAPP2_0
+//[assembly: AssemblyKeyFile("Apache.Ignite.Core.Tests.DotNetCore.snk")]
+#endif
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("27f92be8-0fde-4996-8d8d-0453d77e1484")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
