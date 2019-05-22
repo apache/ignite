39e1a521392821aea1a9dbcba3754c7424e94030
Sergey Chugunov <sergey.chugunov@gmail.com>
2019-05-22 15:18:15 +0300

    GG-18877 additional heartbeat to prevent FailureProcessor from treating tcp-comm-worker as blocked
    Signed-off-by: Dmitriy Govorukhin <dmitriy.govorukhin@gmail.com>
    
    (cherry-picked from commit #8905c3f)

--- modules/core/src/main/java/org/apache/ignite/spi/communication/tcp/TcpCommunicationSpi.java
+++ modules/core/src/main/java/org/apache/ignite/spi/communication/tcp/TcpCommunicationSpi.java
@@ -3542,6 +3542,11 @@ public class TcpCommunicationSpi extends IgniteSpiAdapter implements Communicati
                         break;
                     }
                 }
+
+                CommunicationWorker commWorker0 = commWorker;
+
+                if (commWorker0 != null && commWorker0.runner() == Thread.currentThread())
+                    commWorker0.updateHeartbeat();
             }
 
             if (client != null)
