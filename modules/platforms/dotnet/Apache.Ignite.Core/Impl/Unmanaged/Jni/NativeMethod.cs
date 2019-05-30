63675999f7900138970adaf360b3c016b7b2b067
Alexandr Shapkin <ashapkin@gridgain.com>
2019-05-30 17:04:19 +0300

    GG-19147 Resharper inspections got broken after update
    (cherry picked from commit bfd97007c584f57496db87aa9007796712a4f605)

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Unmanaged/Jni/NativeMethod.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Unmanaged/Jni/NativeMethod.cs
@@ -42,6 +42,7 @@ namespace Apache.Ignite.Core.Impl.Unmanaged.Jni
         /// <summary>
         /// Function pointer (from <see cref="Marshal.GetFunctionPointerForDelegate"/>).
         /// </summary>
+        // ReSharper disable once NotAccessedField.Global
         public IntPtr FuncPtr;
     }
 }
