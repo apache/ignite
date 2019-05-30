519dfd3142f6e7667ec33c53000da57744054eec
ibessonov <bessonov.ip@gmail.com>
2019-05-30 10:06:11 +0300

    GG-12665 Use UTC timezone in build date.

--- modules/core/src/main/java/org/apache/ignite/internal/IgniteVersionUtils.java
+++ modules/core/src/main/java/org/apache/ignite/internal/IgniteVersionUtils.java
@@ -18,6 +18,7 @@ package org.apache.ignite.internal;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 import org.apache.ignite.lang.IgniteProductVersion;
 
 /**
@@ -29,6 +30,9 @@ public class IgniteVersionUtils {
 
     /** Ignite version. */
     public static final IgniteProductVersion VER;
+
+    /** UTC build date formatter. */
+    public static final SimpleDateFormat BUILD_TSTAMP_DATE_FORMATTER;
 
     /** Formatted build date. */
     public static final String BUILD_TSTAMP_STR;
@@ -66,7 +70,11 @@ public class IgniteVersionUtils {
         BUILD_TSTAMP = !BUILD_TSTAMP_FROM_PROPERTY.isEmpty() && Long.parseLong(BUILD_TSTAMP_FROM_PROPERTY) != 0
             ? Long.parseLong(BUILD_TSTAMP_FROM_PROPERTY) : System.currentTimeMillis() / 1000;
 
-        BUILD_TSTAMP_STR = new SimpleDateFormat("yyyyMMdd").format(new Date(BUILD_TSTAMP * 1000));
+        BUILD_TSTAMP_DATE_FORMATTER = new SimpleDateFormat("yyyyMMdd");
+
+        BUILD_TSTAMP_DATE_FORMATTER.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+        BUILD_TSTAMP_STR = BUILD_TSTAMP_DATE_FORMATTER.format(new Date(BUILD_TSTAMP * 1000));
 
         COPYRIGHT = BUILD_TSTAMP_STR.substring(0, 4) + " Copyright(C) Apache Software Foundation";
 
