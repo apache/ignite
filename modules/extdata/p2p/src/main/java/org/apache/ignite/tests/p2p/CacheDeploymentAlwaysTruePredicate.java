a39d5ef608fed98e3d6a4522e85e655b71d5d57c
Nikolay Izhikov <nizhikov@apache.org>
2019-06-02 15:37:02 +0300

    IGNITE-11872: Reverting package empty lines

--- modules/extdata/p2p/src/main/java/org/apache/ignite/tests/p2p/CacheDeploymentAlwaysTruePredicate.java
+++ modules/extdata/p2p/src/main/java/org/apache/ignite/tests/p2p/CacheDeploymentAlwaysTruePredicate.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.ignite.tests.p2p;
 
 import org.apache.ignite.lang.IgniteBiPredicate;
