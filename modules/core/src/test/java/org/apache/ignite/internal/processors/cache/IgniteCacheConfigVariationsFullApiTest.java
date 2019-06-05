009297c5d1fc973d5dbb2dcc30acb88398c21216
Fedotov <vanen31@gmail.com>
2019-06-05 17:48:39 +0300

    remove ignored annotations

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteCacheConfigVariationsFullApiTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteCacheConfigVariationsFullApiTest.java
@@ -6007,7 +6007,6 @@ public class IgniteCacheConfigVariationsFullApiTest extends IgniteCacheConfigVar
     /**
      * @throws Exception If failed.
      */
-    @Ignore("https://issues.apache.org/jira/browse/IGNITE-11850")
     @Test
     public void testGetOutTx() throws Exception {
         checkGetOutTx(false, false);
@@ -6016,7 +6015,6 @@ public class IgniteCacheConfigVariationsFullApiTest extends IgniteCacheConfigVar
     /**
      * @throws Exception If failed.
      */
-    @Ignore("https://issues.apache.org/jira/browse/IGNITE-11850")
     @Test
     public void testGetOutTxAsyncOld() throws Exception {
         checkGetOutTx(true, true);
@@ -6025,7 +6023,6 @@ public class IgniteCacheConfigVariationsFullApiTest extends IgniteCacheConfigVar
     /**
      * @throws Exception If failed.
      */
-    @Ignore("https://issues.apache.org/jira/browse/IGNITE-11850")
     @Test
     public void testGetOutTxAsync() throws Exception {
         checkGetOutTx(true, false);
