26bd56b42c7b87dc331ae04943a1de1e3d455a78
Vasiliy Sisko <vsisko@gridgain.com>
2019-06-05 18:20:34 +0700

    GG-18767 Web Console: Fixed web agent launch script. #36

--- modules/web-console/web-agent/bin/ignite-web-agent.bat
+++ modules/web-console/web-agent/bin/ignite-web-agent.bat
@@ -65,7 +65,7 @@ for /f "tokens=* USEBACKQ" %%f in (`%cmd% -version 2^>^&1`) do (
 for /f "tokens=1-3  delims= " %%a in ("%var%") do set JAVA_VER_STR=%%c
 set JAVA_VER_STR=%JAVA_VER_STR:"=%
 
-for /f "tokens=1,2 delims=." %%a in ("%JAVA_VER_STR%.x") do set MAJOR_JAVA_VER=%%a & set MINOR_JAVA_VER=%%b
+for /f "tokens=1,2 delims=." %%a in ("%JAVA_VER_STR%.x") do set MAJOR_JAVA_VER=%%a& set MINOR_JAVA_VER=%%b
 if %MAJOR_JAVA_VER% == 1 set MAJOR_JAVA_VER=%MINOR_JAVA_VER%
 
 if %MAJOR_JAVA_VER% LSS 8 (
@@ -96,7 +96,7 @@ set JVM_OPTS=%JVM_OPTS% -Djava.net.useSystemProxies=true -Djdk.http.auth.tunneli
 ::
 :: Final JVM_OPTS for Java 9+ compatibility
 ::
-if "%MAJOR_JAVA_VER%" == "8" (
+if %MAJOR_JAVA_VER% == 8 (
     set JVM_OPTS= ^
     -XX:+AggressiveOpts ^
     %JVM_OPTS%
@@ -116,7 +116,7 @@ if %MAJOR_JAVA_VER% GEQ 9 if %MAJOR_JAVA_VER% LSS 11 (
     %JVM_OPTS%
 )
 
-if "%MAJOR_JAVA_VER%" GEQ "11" (
+if %MAJOR_JAVA_VER% GEQ 11 (
     set JVM_OPTS= ^
     --add-exports=java.base/jdk.internal.misc=ALL-UNNAMED ^
     --add-exports=java.base/sun.nio.ch=ALL-UNNAMED ^
