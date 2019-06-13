4aaf9a157632239843da96ee9793eccad8a1a61f
ibessonov <bessonov.ip@gmail.com>
2019-06-13 18:33:23 +0300

    GG-19196 Avoided confusing message 'Cant find snapshot metadata'.
    (cherry picked from commit e75433a65fb2d84b6cad24156f2c9ca9c0e2a864)

--- modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/twostep/GridMapQueryExecutor.java
+++ modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/twostep/GridMapQueryExecutor.java
@@ -26,7 +26,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
