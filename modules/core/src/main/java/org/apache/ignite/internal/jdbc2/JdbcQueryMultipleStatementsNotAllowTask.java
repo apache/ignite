9b43857381df72124d050cd4df09ba6c2c383a1d
tledkov <tledkov@gridgain.com>
2019-06-13 18:35:46 +0300

    GG-19413: [8.8-next]-[GG-19386] JDBC v2: adds 'schema' to URL parameters, makes 'cache' parameter is optional
    (cherry picked from commit fec4695f57e11d4e1b451481c87d6c5e726b9148)

---
+++ modules/core/src/main/java/org/apache/ignite/internal/jdbc2/JdbcQueryMultipleStatementsNotAllowTask.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2019 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.jdbc2;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteJdbcDriver;
+
+/**
+ * Task for SQL queries execution through {@link IgniteJdbcDriver}.
+ * The query can contains several SQL statements.
+ */
+class JdbcQueryMultipleStatementsNotAllowTask extends JdbcQueryMultipleStatementsTask {
+    /** Serial version uid. */
+    private static final long serialVersionUID = 0L;
+
+    /**
+     * @param ignite Ignite.
+     * @param schemaName Schema name.
+     * @param sql Sql query.
+     * @param isQry Operation type flag - query or not - to enforce query type check.
+     * @param loc Local execution flag.
+     * @param args Args.
+     * @param fetchSize Fetch size.
+     * @param locQry Local query flag.
+     * @param collocatedQry Collocated query flag.
+     * @param distributedJoins Distributed joins flag.
+     * @param enforceJoinOrder Enforce joins order falg.
+     * @param lazy Lazy query execution flag.
+     */
+    public JdbcQueryMultipleStatementsNotAllowTask(Ignite ignite, String schemaName, String sql, Boolean isQry, boolean loc,
+        Object[] args, int fetchSize, boolean locQry, boolean collocatedQry, boolean distributedJoins,
+        boolean enforceJoinOrder, boolean lazy) {
+        super(ignite, schemaName, sql, isQry, loc, args, fetchSize, locQry, collocatedQry, distributedJoins,
+            enforceJoinOrder, lazy);
+    }
+
+    /** {@inheritDoc} */
+    @Override protected boolean allowMultipleStatements() {
+        return false;
+    }
+}
