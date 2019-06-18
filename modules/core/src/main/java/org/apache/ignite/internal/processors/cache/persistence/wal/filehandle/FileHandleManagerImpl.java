54d30f09a2278149e410843928f719df1883716c
ibessonov <bessonov.ip@gmail.com>
2019-06-18 09:47:39 +0300

    GG-19520 Store error in WalWriter.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/filehandle/FileHandleManagerImpl.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/filehandle/FileHandleManagerImpl.java
@@ -385,6 +385,8 @@ public class FileHandleManagerImpl implements FileHandleManager {
                 err = t;
             }
             finally {
+                this.err = err;
+
                 unparkWaiters(Long.MAX_VALUE);
 
                 if (err == null && !isCancelled)
@@ -551,6 +553,8 @@ public class FileHandleManagerImpl implements FileHandleManager {
                 assert hdl.written == hdl.fileIO.position();
             }
             catch (IOException e) {
+                err = e;
+
                 StorageException se = new StorageException("Failed to write buffer.", e);
 
                 cctx.kernalContext().failure().process(new FailureContext(CRITICAL_ERROR, se));
