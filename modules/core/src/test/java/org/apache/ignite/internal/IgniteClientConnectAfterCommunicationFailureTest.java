7a0b79bf219a8363219cbadb7700248bed0028e0
Dmitriy Govorukhin <dmitriy.govorukhin@gmail.com>
2019-06-19 11:48:23 +0300

    IGNITE-11934 Bugs & tests fixes - Fixes #6619
    Co-authored-by: Vladislav Pyatkov <vldpyatkov@gmail.com>
    Co-authored-by: Ivan Bessonov <bessonov.ip@gmail.com>
    Co-authored-by: Anton Kalashnikov <kaa.dev@yandex.ru>
    Co-authored-by: Dmitriy Govorukhin <dmitriy.govorukhin@gmail.com>
    Co-authored-by: Igor Seliverstov <gvvinblade@gmail.com>

--- modules/core/src/test/java/org/apache/ignite/internal/IgniteClientConnectAfterCommunicationFailureTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/IgniteClientConnectAfterCommunicationFailureTest.java
@@ -18,15 +18,13 @@
 package org.apache.ignite.internal;
 
 import java.util.Arrays;
-import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCheckedException;
 import org.apache.ignite.cluster.ClusterNode;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.internal.util.nio.GridCommunicationClient;
 import org.apache.ignite.internal.util.typedef.internal.U;
-import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
-import org.apache.ignite.spi.discovery.tcp.ipfinder.multicast.TcpDiscoveryMulticastIpFinder;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.junit.Test;
 
@@ -34,7 +32,6 @@ import org.junit.Test;
  * Tests client to be able restore connection to cluster on subsequent attempts after communication problems.
  */
 public class IgniteClientConnectAfterCommunicationFailureTest extends GridCommonAbstractTest {
-
     /** {@inheritDoc} */
     @Override protected void afterTest() throws Exception {
         stopAllGrids();
@@ -42,18 +39,10 @@ public class IgniteClientConnectAfterCommunicationFailureTest extends GridCommon
 
     /** {@inheritDoc} */
     @Override protected IgniteConfiguration getConfiguration(String gridName) throws Exception {
-        IgniteConfiguration cfg = super.getConfiguration(gridName);
-
-        ((TcpDiscoverySpi)cfg.getDiscoverySpi()).setIpFinder(new TcpDiscoveryMulticastIpFinder());
-
-        cfg.setNetworkTimeout(500);
-        cfg.setCommunicationSpi(new TcpCommunicationSpi(gridName.contains("block")));
-
-        if (gridName.contains("client")) {
-            cfg.setClientMode(true);
-        }
-
-        return cfg;
+        return super.getConfiguration(gridName)
+            .setNetworkTimeout(500)
+            .setCommunicationSpi(new TcpCommunicationSpi(gridName.contains("block")))
+            .setClientMode(gridName.contains("client"));
     }
 
     /**
@@ -126,38 +115,28 @@ public class IgniteClientConnectAfterCommunicationFailureTest extends GridCommon
         /**
          * Whether this instance should actually block.
          */
-        private final boolean isBlocking;
-
-        /**
-         * Local node ID that is prevented from creating connections.
-         */
-        private volatile UUID blockedNodeId = null;
+        private final AtomicBoolean isBlocking;
 
         /**
          *
          * @param isBlocking Whether this instance should actually block.
          */
         public TcpCommunicationSpi(boolean isBlocking) {
-            this.isBlocking = isBlocking;
+            this.isBlocking = new AtomicBoolean(isBlocking);
         }
 
         /** {@inheritDoc} */
         @Override protected GridCommunicationClient createTcpClient(ClusterNode node, int connIdx)
             throws IgniteCheckedException {
-            if (blockHandshakeOnce(getLocalNode().id())) {
+            if (blockHandshakeOnce())
                 throw new IgniteCheckedException("Node is blocked");
-            }
 
             return super.createTcpClient(node, connIdx);
         }
 
         /** Check if this connection is blocked. */
-        private boolean blockHandshakeOnce(UUID nodeId) {
-            if (isBlocking && (blockedNodeId == null || blockedNodeId.equals(nodeId))) {
-                blockedNodeId = nodeId;
-                return true;
-            }
-            return false;
+        private boolean blockHandshakeOnce() {
+            return isBlocking.compareAndSet(true, false);
         }
     }
 }
