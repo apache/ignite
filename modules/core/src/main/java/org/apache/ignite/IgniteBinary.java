6ae0977413d5bbd0d8bda1635090fa96ffe462b0
Andrey V. Mashenkov <andrey.mashenkov@gmail.com>
2019-06-21 14:35:59 +0300

    GG-19517: Fix javadoc.

--- modules/core/src/main/java/org/apache/ignite/IgniteBinary.java
+++ modules/core/src/main/java/org/apache/ignite/IgniteBinary.java
@@ -317,7 +317,7 @@ public interface IgniteBinary {
      * Converts provided object to instance of {@link org.apache.ignite.binary.BinaryObject}.
      *
      * @param obj Object to convert.
-     * @return Converted object.
+     * @return Converted object or {@null} if obj is null.
      * @throws org.apache.ignite.binary.BinaryObjectException In case of error.
      */
     public <T> T toBinary(@Nullable Object obj) throws BinaryObjectException;
