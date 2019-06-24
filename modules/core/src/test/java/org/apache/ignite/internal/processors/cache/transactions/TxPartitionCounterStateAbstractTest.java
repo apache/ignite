36e32aa30638f862975119540861d5b5a41d31ca
Aleksei Scherbakov <alexey.scherbakoff@gmail.com>
2019-06-24 18:52:55 +0300

    IGNITE-GG-18754 Minor test fix.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/transactions/TxPartitionCounterStateAbstractTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/transactions/TxPartitionCounterStateAbstractTest.java
@@ -116,7 +116,6 @@ public abstract class TxPartitionCounterStateAbstractTest extends GridCommonAbst
     /** {@inheritDoc} */
     @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
         IgniteConfiguration cfg = super.getConfiguration(igniteInstanceName);
-        cfg.setFailureDetectionTimeout(10000000000L);
 
         cfg.setConsistentId("node" + igniteInstanceName);
         cfg.setFailureHandler(new StopNodeFailureHandler());
