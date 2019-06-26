f077b39abfe1a07b87dbc20306b92358d47a6522
Igor Sapego <isapego@apache.org>
2019-06-26 18:40:31 +0300

    Revert "NBL-9: C++ implemented missing methods for ClusterGroup"
    This reverts commit 09bc0727

--- modules/platforms/cpp/core-test/src/cluster_test.cpp
+++ modules/platforms/cpp/core-test/src/cluster_test.cpp
@@ -31,25 +31,16 @@ using namespace boost::unit_test;
  */
 struct ClusterTestSuiteFixture
 {
-    Ignite server1;
-    Ignite server2;
-    Ignite server3;
-    Ignite client;
+    Ignite node;
 
     /*
      * Constructor.
      */
     ClusterTestSuiteFixture() :
 #ifdef IGNITE_TESTS_32
-        server1(ignite_test::StartNode("compute-server0-32.xml", "ClusterTestServer0")),
-        server2(ignite_test::StartNode("compute-server1-32.xml", "ClusterTestServer1")),
-        server3(ignite_test::StartNode("compute-server1-32.xml", "ClusterTestServer2")),
-        client(ignite_test::StartNode("compute-client-32.xml", "ClusterTestClient"))
+        node(ignite_test::StartNode("cache-test-32.xml", "ClusterTest"))
 #else
-        server1(ignite_test::StartNode("compute-server0.xml", "ClusterTestServer0")),
-        server2(ignite_test::StartNode("compute-server1.xml", "ClusterTestServer1")),
-        server3(ignite_test::StartNode("compute-server1.xml", "ClusterTestServer2")),
-        client(ignite_test::StartNode("compute-client.xml", "ClusterTestClient"))
+        node(ignite_test::StartNode("cache-test.xml", "ClusterTest"))
 #endif
     {
         // No-op.
@@ -60,7 +51,6 @@ struct ClusterTestSuiteFixture
      */
     ~ClusterTestSuiteFixture()
     {
-        Ignition::Stop(client.GetName(), true);
         Ignition::StopAll(true);
     }
 };
@@ -98,262 +88,70 @@ BOOST_FIXTURE_TEST_SUITE(ClusterTestSuite, ClusterTestSuiteFixture)
 
 BOOST_AUTO_TEST_CASE(IgniteGetCluster)
 {
-    IgniteCluster cluster = server1.GetCluster();
+    IgniteCluster cluster = node.GetCluster();
 
     BOOST_REQUIRE(cluster.IsActive());
+}
+
+BOOST_AUTO_TEST_CASE(IgniteGetNodes)
+{
+    IgniteCluster cluster = node.GetCluster();
+
+    BOOST_REQUIRE(cluster.IsActive());
+
+    ClusterGroup group = cluster.AsClusterGroup();
+
+    std::vector<ClusterNode> nodes = group.GetNodes();
+
+    BOOST_REQUIRE(nodes.size() == 1);
 }
 
 BOOST_AUTO_TEST_CASE(IgniteForAttribute)
 {
-    IgniteCluster cluster = server1.GetCluster();
+    IgniteCluster cluster = node.GetCluster();
 
     BOOST_REQUIRE(cluster.IsActive());
 
-    ClusterGroup group1 = cluster.AsClusterGroup().ForAttribute("TestAttribute", "Value0");
-    ClusterGroup group2 = cluster.AsClusterGroup().ForAttribute("TestAttribute", "Value1");
-    ClusterGroup group3 = cluster.AsClusterGroup().ForAttribute("NotExistAttribute", "Value0");
-    ClusterGroup group4 = cluster.AsClusterGroup().ForAttribute("TestAttribute", "NotExistValue");
+    ClusterGroup group1 = cluster.AsClusterGroup().ForAttribute("TestAttribute", "Value");
+    ClusterGroup group2 = cluster.AsClusterGroup().ForAttribute("NotExistAttribute", "Value");
 
     BOOST_REQUIRE(group1.GetNodes().size() == 1);
-    BOOST_REQUIRE(group2.GetNodes().size() == 2);
-    BOOST_REQUIRE(group3.GetNodes().size() == 0);
-    BOOST_REQUIRE(group4.GetNodes().size() == 0);
-}
-
-BOOST_AUTO_TEST_CASE(IgniteForCacheNodes)
-{
-    IgniteCluster cluster = server1.GetCluster();
-
-    BOOST_REQUIRE(cluster.IsActive());
-
-    ClusterGroup group1 = cluster.AsClusterGroup().ForCacheNodes("CacheName0");
-    ClusterGroup group2 = cluster.AsClusterGroup().ForCacheNodes("CacheName1");
-    ClusterGroup group3 = cluster.AsClusterGroup().ForCacheNodes("CacheName2");
-    ClusterGroup group4 = cluster.AsClusterGroup().ForCacheNodes("InvalidCacheName");
-
-    BOOST_REQUIRE(group1.GetNodes().size() == 3);
-    BOOST_REQUIRE(group2.GetNodes().size() == 3);
-    BOOST_REQUIRE(group3.GetNodes().size() == 4);
-    BOOST_REQUIRE(group4.GetNodes().size() == 0);
-}
-
-BOOST_AUTO_TEST_CASE(IgniteForClientNodes)
-{
-    IgniteCluster cluster = server1.GetCluster();
-
-    BOOST_REQUIRE(cluster.IsActive());
-
-    ClusterGroup group1 = cluster.AsClusterGroup().ForClientNodes("CacheName0");
-    ClusterGroup group2 = cluster.AsClusterGroup().ForClientNodes("CacheName2");
-    ClusterGroup group3 = cluster.AsClusterGroup().ForClientNodes("InvalidCacheName");
-
-    BOOST_REQUIRE(group1.GetNodes().size() == 0);
-    BOOST_REQUIRE(group2.GetNodes().size() == 1);
-    BOOST_REQUIRE(group3.GetNodes().size() == 0);
-}
-
-BOOST_AUTO_TEST_CASE(IgniteForDaemons)
-{
-    IgniteCluster cluster = server1.GetCluster();
-
-    BOOST_REQUIRE(cluster.IsActive());
-
-    ClusterGroup group = cluster.AsClusterGroup().ForDaemons();
-
-    std::vector<ClusterNode> nodes = group.GetNodes();
-
-    BOOST_REQUIRE(nodes.size() == 0);
+    BOOST_REQUIRE(group2.GetNodes().size() == 0);
 }
 
 BOOST_AUTO_TEST_CASE(IgniteForDataNodes)
 {
-    IgniteCluster cluster = server1.GetCluster();
+    IgniteCluster cluster = node.GetCluster();
 
     BOOST_REQUIRE(cluster.IsActive());
 
-    ClusterGroup group1 = cluster.AsClusterGroup().ForDataNodes("CacheName0");
-    ClusterGroup group2 = cluster.AsClusterGroup().ForDataNodes("CacheName1");
-    ClusterGroup group3 = cluster.AsClusterGroup().ForDataNodes("CacheName2");
-    ClusterGroup group4 = cluster.AsClusterGroup().ForDataNodes("InvalidCacheName");
-
-    BOOST_REQUIRE(group1.GetNodes().size() == 3);
-    BOOST_REQUIRE(group2.GetNodes().size() == 3);
-    BOOST_REQUIRE(group3.GetNodes().size() == 3);
-    BOOST_REQUIRE(group4.GetNodes().size() == 0);
-}
-
-BOOST_AUTO_TEST_CASE(IgniteForHost)
-{
-    IgniteCluster cluster = server1.GetCluster();
-
-    BOOST_REQUIRE(cluster.IsActive());
-
-    ClusterGroup group1 = cluster.AsClusterGroup();
-    ClusterGroup group2 = cluster.AsClusterGroup().ForHost(group1.GetNodes().front());
-
-    BOOST_REQUIRE(group1.GetNodes().size() == 4);
-    BOOST_REQUIRE(group2.GetNodes().size() == 4);
-}
-
-BOOST_AUTO_TEST_CASE(IgniteForNode)
-{
-    IgniteCluster cluster = server1.GetCluster();
-
-    BOOST_REQUIRE(cluster.IsActive());
-
-    ClusterGroup group1 = cluster.AsClusterGroup();
-    ClusterGroup group2 = cluster.AsClusterGroup().ForNode(group1.GetNodes().front());
-
-    BOOST_REQUIRE(group1.GetNodes().size() == 4);
-    BOOST_REQUIRE(group2.GetNodes().size() == 1);
-}
-
-BOOST_AUTO_TEST_CASE(IgniteForNodeId)
-{
-    IgniteCluster cluster = server1.GetCluster();
-
-    BOOST_REQUIRE(cluster.IsActive());
-
-    ClusterGroup group1 = cluster.AsClusterGroup();
-    ClusterGroup group2 = cluster.AsClusterGroup().ForNodeId(group1.GetNodes().front().GetId());
-    ClusterGroup group3 = cluster.AsClusterGroup().ForNodeId(Guid(100, 500));
-
-    BOOST_REQUIRE(group1.GetNodes().size() == 4);
-    BOOST_REQUIRE(group2.GetNodes().size() == 1);
-    BOOST_REQUIRE(group3.GetNodes().size() == 0);
-}
-
-BOOST_AUTO_TEST_CASE(IgniteForNodeIds)
-{
-    IgniteCluster cluster = server1.GetCluster();
-
-    BOOST_REQUIRE(cluster.IsActive());
-
-    ClusterGroup group1 = cluster.AsClusterGroup();
-
-    std::vector<ClusterNode> nodes = group1.GetNodes();
-    std::vector<Guid> ids;
-    ids.push_back(nodes.at(0).GetId());
-    ids.push_back(nodes.at(1).GetId());
-
-    ClusterGroup group2 = cluster.AsClusterGroup().ForNodeIds(ids);
-
-    BOOST_REQUIRE(group1.GetNodes().size() == 4);
-    BOOST_REQUIRE(group2.GetNodes().size() == 2);
-}
-
-BOOST_AUTO_TEST_CASE(IgniteForNodes)
-{
-    IgniteCluster cluster = server1.GetCluster();
-
-    BOOST_REQUIRE(cluster.IsActive());
-
-    ClusterGroup group1 = cluster.AsClusterGroup();
-    ClusterGroup group2 = cluster.AsClusterGroup().ForServers();
-    ClusterGroup group3 = cluster.AsClusterGroup().ForNodes(group2.GetNodes());
-
-    BOOST_REQUIRE(group1.GetNodes().size() == 4);
-    BOOST_REQUIRE(group2.GetNodes().size() == 3);
-    BOOST_REQUIRE(group3.GetNodes().size() == 3);
-}
-
-BOOST_AUTO_TEST_CASE(IgniteForOldest)
-{
-    IgniteCluster cluster = server1.GetCluster();
-
-    BOOST_REQUIRE(cluster.IsActive());
-
-    ClusterGroup group1 = cluster.AsClusterGroup().ForOldest();
+    ClusterGroup group1 = cluster.AsClusterGroup().ForDataNodes("cache1");
+    ClusterGroup group2 = cluster.AsClusterGroup().ForDataNodes("notExist");
 
     BOOST_REQUIRE(group1.GetNodes().size() == 1);
-}
-
-BOOST_AUTO_TEST_CASE(IgniteForRandom)
-{
-    IgniteCluster cluster = server1.GetCluster();
-
-    BOOST_REQUIRE(cluster.IsActive());
-
-    ClusterGroup group1 = cluster.AsClusterGroup().ForRandom();
-
-    BOOST_REQUIRE(group1.GetNodes().size() == 1);
-}
-
-BOOST_AUTO_TEST_CASE(IgniteForRemotes)
-{
-    IgniteCluster cluster = server1.GetCluster();
-
-    BOOST_REQUIRE(cluster.IsActive());
-
-    ClusterGroup group1 = cluster.AsClusterGroup().ForRemotes();
-
-    BOOST_REQUIRE(group1.GetNodes().size() == 3);
+    BOOST_REQUIRE(group2.GetNodes().size() == 0);
 }
 
 BOOST_AUTO_TEST_CASE(IgniteForServers)
 {
-    IgniteCluster cluster = server1.GetCluster();
+    IgniteCluster cluster = node.GetCluster();
 
     BOOST_REQUIRE(cluster.IsActive());
 
-    ClusterGroup group1 = cluster.AsClusterGroup().ForServers();
-    ClusterGroup group2 = cluster.AsClusterGroup().ForServers().ForAttribute("TestAttribute", "Value0");
-    ClusterGroup group3 = cluster.AsClusterGroup().ForServers().ForAttribute("TestAttribute", "Value1");
-
-    BOOST_REQUIRE(group1.GetNodes().size() == 3);
-    BOOST_REQUIRE(group2.GetNodes().size() == 1);
-    BOOST_REQUIRE(group3.GetNodes().size() == 2);
-}
-
-BOOST_AUTO_TEST_CASE(IgniteForYoungest)
-{
-    IgniteCluster cluster = server1.GetCluster();
-
-    BOOST_REQUIRE(cluster.IsActive());
-
-    ClusterGroup group = cluster.AsClusterGroup().ForYoungest();
+    ClusterGroup group = cluster.AsClusterGroup().ForServers();
 
     BOOST_REQUIRE(group.GetNodes().size() == 1);
 }
 
 BOOST_AUTO_TEST_CASE(IgniteForCpp)
 {
-    IgniteCluster cluster = server1.GetCluster();
+    IgniteCluster cluster = node.GetCluster();
 
     BOOST_REQUIRE(cluster.IsActive());
 
     ClusterGroup group = cluster.AsClusterGroup().ForCpp();
 
-    BOOST_REQUIRE(group.GetNodes().size() == 4);
-}
-
-BOOST_AUTO_TEST_CASE(IgniteGetNode)
-{
-    IgniteCluster cluster = server1.GetCluster();
-
-    BOOST_REQUIRE(cluster.IsActive());
-
-    ClusterGroup group = cluster.AsClusterGroup();
-
-    ClusterNode node1 = group.GetNode();
-    ClusterNode node2 = group.GetNodes().at(0);
-    ClusterNode node3 = group.GetNode(node1.GetId());
-
-    BOOST_REQUIRE(node1.GetId() == node2.GetId());
-    BOOST_REQUIRE(node2.GetId() == node3.GetId());
-}
-
-BOOST_AUTO_TEST_CASE(IgniteGetNodes)
-{
-    IgniteCluster cluster = server1.GetCluster();
-
-    BOOST_REQUIRE(cluster.IsActive());
-
-    ClusterGroup group = cluster.AsClusterGroup();
-
-    std::vector<ClusterNode> nodes = group.GetNodes();
-
-    BOOST_REQUIRE(nodes.size() == 4);
+    BOOST_REQUIRE(group.GetNodes().size() == 1);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
