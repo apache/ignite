90907b5eabae01398a00ddbca0b023c72b06dcc9
Vasiliy Sisko <vsisko@gridgain.com>
2019-06-27 13:37:22 +0300

    GG-20249 Extract message from exception cause stack trace. (#196)

--- modules/web-console/frontend/app/services/ErrorParser.service.ts
+++ modules/web-console/frontend/app/services/ErrorParser.service.ts
@@ -17,6 +17,9 @@
 import isEmpty from 'lodash/isEmpty';
 import {nonEmpty} from 'app/utils/lodashMixins';
 
+const CAUSE_STR = 'Caused by: ';
+const ERR_START_STR = ' err=';
+
 export default class {
     static $inject = ['JavaTypes'];
 
@@ -27,7 +30,7 @@ export default class {
         this.JavaTypes = JavaTypes;
     }
 
-    extractMessage(err, prefix) {
+    parse(err, prefix) {
         prefix = prefix || '';
 
         if (err) {
@@ -42,7 +45,7 @@ export default class {
                 if (traceIndex > 0)
                     msg = msg.substring(0, traceIndex);
 
-                const lastIdx = msg.lastIndexOf(' err=');
+                const lastIdx = msg.lastIndexOf(ERR_START_STR);
                 let msgEndIdx = msg.indexOf(']', lastIdx);
 
                 if (lastIdx > 0 && msgEndIdx > 0) {
@@ -58,20 +61,41 @@ export default class {
                     }
                 }
 
-                return prefix + (lastIdx >= 0 ? msg.substring(lastIdx + 5, msgEndIdx > 0 ? msgEndIdx : traceIndex) : msg);
+                const causes = [];
+
+                let causeIdx = err.message.indexOf(CAUSE_STR);
+
+                while (causeIdx >= 0) {
+                    // Find next ": " in cause message to skip exception class name.
+                    const msgStart = err.message.indexOf(': ', causeIdx + CAUSE_STR.length) + 2;
+                    const causeEndLine = err.message.indexOf('\n', msgStart);
+                    const msgEnd = err.message.indexOf('[', msgStart);
+                    const cause = err.message.substring(msgStart, msgEnd >= 0 && msgEnd < causeEndLine ? msgEnd : causeEndLine);
+
+                    causes.unshift(cause);
+
+                    causeIdx = err.message.indexOf(CAUSE_STR, causeIdx + CAUSE_STR.length);
+                }
+
+                return new ErrorParseResult(
+                    prefix + (lastIdx >= 0
+                        ? msg.substring(lastIdx + ERR_START_STR.length, msgEndIdx > 0 ? msgEndIdx : traceIndex)
+                        : msg),
+                    causes
+                );
             }
 
             if (nonEmpty(err.className)) {
                 if (isEmpty(prefix))
                     prefix = 'Internal cluster error: ';
 
-                return prefix + err.className;
+                return new ErrorParseResult(prefix + err.className);
             }
 
-            return prefix + err;
+            return new ErrorParseResult(prefix + err);
         }
 
-        return prefix + 'Internal error.';
+        return new ErrorParseResult(prefix + 'Internal error.');
     }
 
     extractFullMessage(err) {
@@ -84,5 +108,25 @@ export default class {
             msg = msg.substring(0, traceIndex);
 
         return clsName + (msg);
+    }
+}
+
+/**
+ * Information with error parsing result.
+ */
+export class ErrorParseResult {
+    /** String with parsed error message. */
+    message: String;
+
+    /** List of error causes in reverse order. */
+    causes: String[];
+
+    /**
+     * @param {String} message String with parsed error message.
+     * @param {Array.<String>} causes List of error causes in reverse order.
+     */
+    constructor(message: String, causes = []) {
+        this.message = message;
+        this.causes = causes;
     }
 }
