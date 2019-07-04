cc4fef6e20f810b3888056218b8d8aa3486251e3
Andrey Kalinin <prize2step@gmail.com>
2019-07-04 13:32:34 +0300

    IGNITE-9934 Improve logging on partition map exchange - Fixes #6295.
    Signed-off-by: Dmitriy Govorukhin <dmitriy.govorukhin@gmail.com>

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPartitionsExchangeFuture.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPartitionsExchangeFuture.java
@@ -2901,6 +2901,7 @@ public class GridDhtPartitionsExchangeFuture extends GridDhtTopologyFutureAdapte
                         if (log.isInfoEnabled()) {
                             log.info("Coordinator received single message [ver=" + initialVersion() +
                                 ", node=" + nodeId +
+                                (allReceived ? "" : ", remainingNodes=" + remaining.size()) +
                                 ", allReceived=" + allReceived + ']');
                         }
                     }
