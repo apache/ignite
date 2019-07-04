72e1dde23ca736fbd9d45588a16c7a6b1ee876a4
ibelyakov <ibelyakov@gridgain.com>
2019-07-04 17:09:31 +0300

    GG-20806 Backport GG-20798 Fix failing tests.
    (cherry picked from commit b823cc6757b6c09b11e0658a6daf398eb2030941)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPartitionDemander.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPartitionDemander.java
@@ -1082,7 +1082,7 @@ public class GridDhtPartitionDemander {
             this.log = null;
             this.rebalanceId = -1;
             this.routines = 0;
-            this.cancelLock = null;
+            this.cancelLock = new ReentrantReadWriteLock();
         }
 
         /**
