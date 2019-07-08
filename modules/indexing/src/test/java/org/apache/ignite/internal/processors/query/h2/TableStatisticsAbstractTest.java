3ff9628a9303a0efbc50c5aad6590ff843b606fc
rkondakov <kondakov87@mail.ru>
2019-07-08 14:33:10 +0300

    Reverted test isolation.

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/h2/TableStatisticsAbstractTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/h2/TableStatisticsAbstractTest.java
@@ -61,7 +61,7 @@ public abstract class TableStatisticsAbstractTest extends GridCommonAbstractTest
 
         Collections.reverse(dirOrdTbls);
 
-        log.error("debug: dirOrdTbls=" + dirOrdTbls + ", directOrder=" + directOrder);
+        log.warning("debug: dirOrdTbls=" + dirOrdTbls + ", directOrder=" + directOrder);
 
         String reversedOrder = replaceTablePlaceholders(sql, (String[]) dirOrdTbls.toArray());
 
