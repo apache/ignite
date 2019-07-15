428f84fe9dfe16ddc08c31fbc37e30f0259080fa
Andrey V. Mashenkov <andrey.mashenkov@gmail.com>
2019-07-15 12:29:01 +0300

    GG-21157: Fix imports.

--- modules/h2/src/main/java/org/h2/index/HashJoinIndex.java
+++ modules/h2/src/main/java/org/h2/index/HashJoinIndex.java
@@ -22,7 +22,6 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import org.apache.ignite.internal.processors.query.h2.H2MemoryTracker;
 import org.h2.command.dml.AllColumnsForPlan;
