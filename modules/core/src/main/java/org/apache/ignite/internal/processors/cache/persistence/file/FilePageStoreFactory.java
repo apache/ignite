gridgain/gridgain

GG-17345 Reduce heap occupation by o.a.i.i.processors.cache.persistence.file.FilePageStore instances

modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/file/FilePageStoreFactory.java
@@ -17,20 +17,40 @@
 package org.apache.ignite.internal.processors.cache.persistence.file;
 
 import java.io.File;
+import java.nio.file.Path;
 import org.apache.ignite.IgniteCheckedException;
 import org.apache.ignite.internal.pagemem.PageIdAllocator;
 import org.apache.ignite.internal.pagemem.store.PageStore;
 import org.apache.ignite.internal.processors.cache.persistence.AllocatedPageTracker;
+import org.apache.ignite.lang.IgniteOutClosure;
 
 /**
  *
  */
 public interface FilePageStoreFactory {
     /**
-     * Creates instance of FilePageStore based on given file.
+     * Creates instance of PageStore based on given file.
      *
      * @param type Data type, can be {@link PageIdAllocator#FLAG_IDX} or {@link PageIdAllocator#FLAG_DATA}.
      * @param file File Page store file.
+     * @param allocatedTracker metrics updater.
+     * @return page store
+     * @throws IgniteCheckedException if failed.
      */
-    PageStore createPageStore(byte type, File file, AllocatedPageTracker allocatedTracker) throws IgniteCheckedException;
+    default PageStore createPageStore(byte type, File file, AllocatedPageTracker allocatedTracker)
+        throws IgniteCheckedException {
+        return createPageStore(type, file::toPath, allocatedTracker);
+    }
+
+    /**
+     * Creates instance of PageStore based on file path provider.
+     *
+     * @param type Data type, can be {@link PageIdAllocator#FLAG_IDX} or {@link PageIdAllocator#FLAG_DATA}
+     * @param pathProvider File Page store path provider.
+     * @param allocatedTracker metrics updater
+     * @return page store
+     * @throws IgniteCheckedException if failed
+     */
+    PageStore createPageStore(byte type, IgniteOutClosure<Path> pathProvider, AllocatedPageTracker allocatedTracker)
+        throws IgniteCheckedException;
 }

apache/ignite

(cherry picked from commit 806b1b6d861dfa76980ba39970bb111ecaa15789)

modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/file/FilePageStoreFactory.java
@@ -18,19 +18,39 @@
 package org.apache.ignite.internal.processors.cache.persistence.file;
 
 import java.io.File;
+import java.nio.file.Path;
 import org.apache.ignite.IgniteCheckedException;
 import org.apache.ignite.internal.pagemem.PageIdAllocator;
 import org.apache.ignite.internal.processors.cache.persistence.AllocatedPageTracker;
+import org.apache.ignite.lang.IgniteOutClosure;
 
 /**
  *
  */
 public interface FilePageStoreFactory {
     /**
-     * Creates instance of FilePageStore based on given file.
+     * Creates instance of PageStore based on given file.
      *
      * @param type Data type, can be {@link PageIdAllocator#FLAG_IDX} or {@link PageIdAllocator#FLAG_DATA}.
      * @param file File Page store file.
+     * @param allocatedTracker metrics updater.
+     * @return page store
+     * @throws IgniteCheckedException if failed.
      */
-    public FilePageStore createPageStore(byte type, File file, AllocatedPageTracker allocatedTracker) throws IgniteCheckedException;
+    default FilePageStore createPageStore(byte type, File file, AllocatedPageTracker allocatedTracker)
+        throws IgniteCheckedException {
+        return createPageStore(type, file::toPath, allocatedTracker);
+    }
+
+    /**
+     * Creates instance of PageStore based on file path provider.
+     *
+     * @param type Data type, can be {@link PageIdAllocator#FLAG_IDX} or {@link PageIdAllocator#FLAG_DATA}
+     * @param pathProvider File Page store path provider.
+     * @param allocatedTracker metrics updater
+     * @return page store
+     * @throws IgniteCheckedException if failed
+     */
+    FilePageStore createPageStore(byte type, IgniteOutClosure<Path> pathProvider, AllocatedPageTracker allocatedTracker)
+        throws IgniteCheckedException;
 }