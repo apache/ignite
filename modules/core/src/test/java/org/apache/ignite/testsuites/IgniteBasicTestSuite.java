2ca3f151bc7ad42f004a626b3d7fb994d4b3a574
pavlukhin <vololo100@gmail.com>
2019-07-16 17:27:24 +0300

    GG-19412 Fail Continuous Query registration and do not fail node if remote filter class is missing (#177)
    (cherry picked from commit a28138f9d784dcd9efce025c822f0fa5887f82ea)

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteBasicTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteBasicTestSuite.java
@@ -44,6 +44,7 @@ import org.apache.ignite.internal.IgniteSlowClientDetectionSelfTest;
 import org.apache.ignite.internal.MarshallerContextLockingSelfTest;
 import org.apache.ignite.internal.TransactionsMXBeanImplTest;
 import org.apache.ignite.internal.managers.IgniteDiagnosticMessagesTest;
+import org.apache.ignite.internal.managers.discovery.IncompleteDeserializationExceptionTest;
 import org.apache.ignite.internal.pagemem.wal.record.WALRecordTest;
 import org.apache.ignite.internal.processors.DeadLockOnNodeLeftExchangeTest;
 import org.apache.ignite.internal.processors.affinity.GridAffinityAssignmentV2Test;
@@ -237,6 +238,8 @@ public class IgniteBasicTestSuite extends TestSuite {
         suite.addTestSuite(MessageOrderLogListenerTest.class);
 
         suite.addTestSuite(ClassPathContentLoggingTest.class);
+
+        suite.addTestSuite(IncompleteDeserializationExceptionTest.class);
 
         return suite;
     }
