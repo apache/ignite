1b8a61544e5a327c00ffac8de0b331b85a786b85
ipavlukhin <vololo100@gmail.com>
2019-07-19 22:33:50 +0300

    IGNITE-12000 Await evictions before stopping nodes in IgniteSqlQueryMinMaxTest to make test stable - Fixes #6708.
    (cherry picked from commit 3a6904cdc48f0e6f07ea939bd7bbf31652937216)

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/h2/IgniteSqlQueryMinMaxTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/h2/IgniteSqlQueryMinMaxTest.java
@@ -46,6 +46,8 @@ public class IgniteSqlQueryMinMaxTest extends AbstractIndexingCommonTest {
     @Override protected void afterTest() throws Exception {
         super.afterTest();
 
+        awaitPartitionMapExchange(true, false, null);
+
         stopAllGrids();
     }
 
