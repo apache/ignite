f6e27ebca7a48e01da428cd32efdb22035dc3cad
Vasiliy Sisko <vsisko@gridgain.com>
2019-07-25 16:42:03 +0300

    GG-18760 Configuration tests moved to separate module.  (#301)

--- modules/web-console/web-console-configuration-tests/src/test/java/org/apache/ignite/console/testsuites/WebConsoleConfigurationTestsTestSuite.java
+++ modules/web-console/web-console-configuration-tests/src/test/java/org/apache/ignite/console/testsuites/WebConsoleConfigurationTestsTestSuite.java
@@ -16,14 +16,7 @@
 
 package org.apache.ignite.console.testsuites;
 
-import org.apache.ignite.console.listener.NotificationEventListenerTest;
-import org.apache.ignite.console.repositories.AccountsRepositoryTest;
-import org.apache.ignite.console.db.TableSelfTest;
-import org.apache.ignite.console.services.AccountServiceTest;
-import org.apache.ignite.console.services.ActivitiesServiceTest;
-import org.apache.ignite.console.services.AdminServiceTest;
-import org.apache.ignite.console.services.NotificationServiceTest;
-import org.apache.ignite.console.web.security.PasswordEncoderTest;
+import org.apache.ignite.console.configuration.WebConsoleConfigurationSelfTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -32,14 +25,7 @@ import org.junit.runners.Suite;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    AccountServiceTest.class,
-    PasswordEncoderTest.class,
-    TableSelfTest.class,
-    ActivitiesServiceTest.class,
-    AdminServiceTest.class,
-    NotificationEventListenerTest.class,
-    AccountsRepositoryTest.class,
-    NotificationServiceTest.class
+    WebConsoleConfigurationSelfTest.class
 })
-public class WebConsoleTestSuite {
+public class WebConsoleConfigurationTestsTestSuite {
 }
