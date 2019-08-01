039600aa3638ac6f0a66670c13cc5ae09cd3c7ae
zstan <stanilovsky@gmail.com>
2019-08-01 16:14:50 +0300

    GG-22286 [IGNITE-10654] Print warn message in case of index creating with already existing fields.
    Signed-off-by: Andrey Gura <agura@apache.org>
     cherry picked from commit c41466f480aca524f7a9da2891d1988c0e82bc43)

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/index/BasicIndexTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/index/BasicIndexTest.java
@@ -29,8 +29,10 @@ import java.util.Objects;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import org.apache.ignite.IgniteCache;
+import org.apache.ignite.IgniteCheckedException;
 import org.apache.ignite.cache.QueryEntity;
 import org.apache.ignite.cache.QueryIndex;
+import org.apache.ignite.cache.affinity.rendezvous.RendezvousAffinityFunction;
 import org.apache.ignite.cache.query.SqlFieldsQuery;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.DataRegionConfiguration;
@@ -40,6 +42,7 @@ import org.apache.ignite.internal.IgniteEx;
 import org.apache.ignite.internal.processors.cache.IgniteInternalCache;
 import org.apache.ignite.internal.processors.cache.persistence.file.FilePageStoreManager;
 import org.apache.ignite.internal.processors.query.GridQueryProcessor;
+import org.apache.ignite.internal.processors.query.QueryUtils;
 import org.apache.ignite.internal.util.typedef.G;
 import org.apache.ignite.internal.util.typedef.internal.S;
 import org.apache.ignite.internal.util.typedef.internal.U;
@@ -48,6 +51,10 @@ import org.apache.ignite.spi.discovery.tcp.ipfinder.TcpDiscoveryIpFinder;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 import org.apache.ignite.testframework.ListeningTestLogger;
 import org.apache.ignite.testframework.LogListener;
+import org.jetbrains.annotations.Nullable;
+import org.junit.Test;
+
+import static org.apache.ignite.internal.processors.query.h2.opt.GridH2PrimaryScanIndex.SCAN_INDEX_NAME_SUFFIX;
 
 import static org.apache.ignite.internal.processors.query.h2.database.H2Tree.IGNITE_THROTTLE_INLINE_SIZE_CALCULATION;
 import static org.apache.ignite.internal.processors.query.h2.opt.GridH2PrimaryScanIndex.SCAN_INDEX_NAME_SUFFIX;
@@ -56,6 +63,15 @@ import static org.apache.ignite.internal.processors.query.h2.opt.GridH2PrimarySc
  * A set of basic tests for caches with indexes.
  */
 public class BasicIndexTest extends AbstractIndexingCommonTest {
+    /** Default client name. */
+    private static final String CLIENT_NAME = "client";
+
+    /** {@code True} If index need to be created throught static config. */
+    private static boolean createIdx = true;
+
+    /** {@code True} If cache nedd to be created throught static config. */
+    private static boolean createStaticCache = true;
+
     /** Default table name. */
     private static final String TEST_TBL_NAME = "PUBLIC.TEST_TABLE";
 
@@ -77,9 +93,10 @@ public class BasicIndexTest extends AbstractIndexingCommonTest {
     /** Server listening logger. */
     private ListeningTestLogger srvLog;
 
-    /**
-     * {@inheritDoc}
-     */
+    /** Client listening logger. */
+    private ListeningTestLogger clientLog;
+
+    /** {@inheritDoc} */
     @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
         assertNotNull(inlineSize);
 
@@ -94,6 +111,17 @@ public class BasicIndexTest extends AbstractIndexingCommonTest {
             new TcpDiscoverySpi().setIpFinder(IP_FINDER)
         );
 
+        if (igniteInstanceName.startsWith(CLIENT_NAME)) {
+            igniteCfg.setClientMode(true);
+
+            if (clientLog != null)
+                igniteCfg.setGridLogger(clientLog);
+        }
+        else {
+            if (srvLog != null)
+                igniteCfg.setGridLogger(srvLog);
+        }
+
         LinkedHashMap<String, String> fields = new LinkedHashMap<>();
         fields.put("keyStr", String.class.getName());
         fields.put("keyLong", Long.class.getName());
@@ -102,7 +130,11 @@ public class BasicIndexTest extends AbstractIndexingCommonTest {
         fields.put("valLong", Long.class.getName());
         fields.put("valPojo", Pojo.class.getName());
 
+        if (!createIdx)
+            indexes = Collections.emptyList();
+
         CacheConfiguration<Key, Val> ccfg = new CacheConfiguration<Key, Val>(DEFAULT_CACHE_NAME)
+            .setAffinity(new RendezvousAffinityFunction(false, 32))
             .setQueryEntities(Collections.singleton(
                 new QueryEntity()
                     .setKeyType(Key.class.getName())
@@ -110,15 +142,17 @@ public class BasicIndexTest extends AbstractIndexingCommonTest {
                     .setFields(fields)
                     .setKeyFields(new HashSet<>(Arrays.asList("keyStr", "keyLong", "keyPojo")))
                     .setIndexes(indexes)
+                    .setAliases(Collections.singletonMap(QueryUtils.KEY_FIELD_NAME, "pk_id"))
             ))
             .setSqlIndexMaxInlineSize(inlineSize);
 
-        igniteCfg.setCacheConfiguration(ccfg);
+        if (createStaticCache)
+            igniteCfg.setCacheConfiguration(ccfg);
 
         if (isPersistenceEnabled) {
             igniteCfg.setDataStorageConfiguration(new DataStorageConfiguration()
                 .setDefaultDataRegionConfiguration(
-                    new DataRegionConfiguration().setPersistenceEnabled(true)
+                    new DataRegionConfiguration().setPersistenceEnabled(true).setMaxSize(10 * 1024 * 1024)
                 )
             );
         }
@@ -147,6 +181,8 @@ public class BasicIndexTest extends AbstractIndexingCommonTest {
         stopAllGrids();
 
         cleanPersistenceDir();
+
+        srvLog = clientLog = null;
 
         super.afterTest();
     }
@@ -594,6 +630,366 @@ public class BasicIndexTest extends AbstractIndexingCommonTest {
 
             stopAllGrids();
         }
+    }
+
+    /**
+     * Tests mixed dynamic and static caches with indexes creation.
+     *
+     * @throws Exception If failed.
+     */
+    public void testDynamicIdxOnStaticCacheWithIdxWithoutPersistence() throws Exception {
+        runDynamicIdxOnStaticCacheWithIdx(false);
+    }
+
+    /**
+     * Tests mixed dynamic and static caches with indexes creation.
+     *
+     * @throws Exception If failed.
+     */
+    public void testDynamicIdxOnStaticCacheWithIdxWithPersistence() throws Exception {
+        runDynamicIdxOnStaticCacheWithIdx(true);
+    }
+
+    /** */
+    private void runDynamicIdxOnStaticCacheWithIdx(boolean persistEnabled) throws Exception {
+        isPersistenceEnabled = persistEnabled;
+
+        inlineSize = 10;
+
+        createIdx = false;
+
+        indexes = Collections.singletonList(new QueryIndex("valStr"));
+
+        IgniteEx ig0 = startGrid(0);
+
+        createIdx = true;
+
+        startGrid(1);
+
+        if (persistEnabled)
+            ig0.cluster().active(true);
+
+        IgniteCache<Key, Val> cache = grid(0).cache(DEFAULT_CACHE_NAME);
+
+        populateCache();
+
+        String plan = cache.query(new SqlFieldsQuery("explain select * from Val where valStr between 0 and ?")
+            .setArgs(100)).getAll().get(0).get(0).toString();
+
+        assertTrue(plan, plan.contains(SCAN_INDEX_NAME_SUFFIX));
+
+        stopAllGrids();
+
+        if (persistEnabled)
+            cleanPersistenceDir();
+
+        createStaticCache = false;
+
+        ig0 = startGrid(0);
+
+        if (persistEnabled)
+            ig0.cluster().active(true);
+
+        ig0.getOrCreateCache(DEFAULT_CACHE_NAME);
+
+        populateCache();
+
+        createStaticCache = true;
+
+        try {
+            startGrid(1);
+
+            fail("Exception wasn't thrown");
+        }
+        catch (IgniteCheckedException e) {
+            // no op.
+        }
+    }
+
+    /**
+     * Tests dynamic indexes creation with equal fields.
+     *
+     * @throws Exception If failed.
+     */
+    public void testEqualFieldsDynamicIndexesWithoutPersistence() throws Exception {
+        runEqualFieldsDynamicIndexes(false);
+    }
+
+    /**
+     * Tests dynamic indexes creation with equal fields.
+     *
+     * @throws Exception If failed.
+     */
+    public void testEqualFieldsDynamicIndexesWithPersistence() throws Exception {
+        runEqualFieldsDynamicIndexes(true);
+    }
+
+    /** */
+    private void runEqualFieldsDynamicIndexes(boolean persistEnabled) throws Exception {
+        isPersistenceEnabled = persistEnabled;
+
+        indexes = Collections.singletonList(new QueryIndex("valStr"));
+
+        inlineSize = 10;
+
+        srvLog = new ListeningTestLogger(false, log);
+
+        clientLog = new ListeningTestLogger(false, log);
+
+        String msg1 = "Index with the given set or subset of columns already exists";
+
+        LogListener lsnr = LogListener.matches(msg1).andMatches(Pattern.compile(".*newIndexName=idx[0-9]")).build();
+
+        LogListener staticCachesLsnr = LogListener.matches(msg1).build();
+
+        srvLog.registerListener(staticCachesLsnr);
+
+        IgniteEx ig0 = startGrid(0);
+
+        if (persistEnabled)
+            ig0.cluster().active(true);
+
+        IgniteCache<Key, Val> cache = grid(0).cache(DEFAULT_CACHE_NAME);
+
+        populateCache();
+
+        cache.query(new SqlFieldsQuery("create index \"idx0\" on Val(valStr)"));
+
+        assertTrue(staticCachesLsnr.check());
+
+        srvLog.unregisterListener(staticCachesLsnr);
+
+        srvLog.registerListener(lsnr);
+
+        cache.query(new SqlFieldsQuery("create index \"idx1\" on Val(valStr, valLong)"));
+
+        cache.query(new SqlFieldsQuery("create index \"idx2\" on Val(valStr desc, valLong)"));
+
+        assertFalse(lsnr.check());
+
+        cache.query(new SqlFieldsQuery("create index \"idx3\" on Val(valStr, valLong)"));
+
+        cache.query(new SqlFieldsQuery("create index \"idx4\" on Val(valLong)"));
+
+        assertTrue(lsnr.check());
+
+        srvLog.unregisterListener(lsnr);
+
+        IgniteEx client = (IgniteEx)startGrid(CLIENT_NAME);
+
+        cache = client.cache(DEFAULT_CACHE_NAME);
+
+        LogListener lsnrIdx5 = LogListener.matches(msg1).andMatches("idx5").build();
+
+        srvLog.registerListener(lsnrIdx5);
+
+        cache.query(new SqlFieldsQuery("create index \"idx5\" on Val(valStr desc, valLong)"));
+
+        assertTrue(lsnrIdx5.check());
+
+        LogListener lsnrIdx7 = LogListener.matches(msg1).andMatches("idx7").build();
+
+        srvLog.registerListener(lsnrIdx7);
+
+        cache.query(new SqlFieldsQuery("create index \"idx6\" on Val(valLong)"));
+
+        cache.query(new SqlFieldsQuery("create index \"idx7\" on Val(keyStr, keyLong, keyPojo, valLong)"));
+
+        assertFalse(lsnrIdx7.check());
+    }
+
+    /** */
+    private boolean checkIdxUsed(GridQueryProcessor qryProc, @Nullable String idxName, String tblName, String... reqFlds) {
+        String sql = "explain select * from " + tblName + " where ";
+
+        for (int i = 0; i < reqFlds.length; ++i)
+            sql += reqFlds[i] + " > 0 " + ((i < reqFlds.length - 1) ? " and " : "");
+
+        String plan = qryProc.querySqlFields(new SqlFieldsQuery(sql), true)
+            .getAll().get(0).get(0).toString().toUpperCase();
+
+        return idxName != null ? (!plan.contains(SCAN_INDEX_NAME_SUFFIX) && plan.contains(idxName.toUpperCase())) : !plan.contains(SCAN_INDEX_NAME_SUFFIX);
+    }
+
+    /** */
+    private boolean checkIdxAlreadyExistLog(GridQueryProcessor qryProc, String idxName, String tblName, String... reqFlds) {
+        String msg0 = "Index with the given set or subset of columns already exists";
+
+        String sql = "create index \"" + idxName + "\" on " + tblName + "(";
+
+        for (int i = 0; i < reqFlds.length; ++i)
+            sql += reqFlds[i] + ((i < reqFlds.length - 1) ? ", " : ")");
+
+        LogListener lsnrIdx = LogListener.matches(msg0).andMatches(idxName).build();
+
+        srvLog.registerListener(lsnrIdx);
+
+        qryProc.querySqlFields(new SqlFieldsQuery(sql), true).getAll();
+
+        return lsnrIdx.check();
+    }
+
+    /**
+     *  Checks index usage for full coverage.
+     */
+    public void testAllTableFieldsCoveredByIdx() throws Exception {
+        inlineSize = 10;
+
+        srvLog = new ListeningTestLogger(false, log);
+
+        IgniteEx ig0 = startGrid(0);
+
+        GridQueryProcessor qryProc = ig0.context().query();
+
+        populateTable(qryProc, TEST_TBL_NAME, 2, "FIRST_NAME", "LAST_NAME",
+            "ADDRESS", "LANG");
+
+        assertFalse(checkIdxUsed(qryProc, null, TEST_TBL_NAME, "LANG"));
+
+        assertFalse(checkIdxUsed(qryProc, null, TEST_TBL_NAME, "LAST_NAME"));
+
+        String sqlIdx2 = String.format("create index \"idx2\" on %s(LANG, ADDRESS)", TEST_TBL_NAME);
+
+        qryProc.querySqlFields(new SqlFieldsQuery(sqlIdx2), true).getAll();
+
+        assertTrue(checkIdxUsed(qryProc, "idx2", TEST_TBL_NAME, "FIRST_NAME",
+            "LAST_NAME", "LANG", "ADDRESS"));
+
+        assertTrue(checkIdxUsed(qryProc, null, TEST_TBL_NAME, "FIRST_NAME",
+            "LAST_NAME", "ADDRESS", "LANG"));
+        // first idx fields not belongs to request fields.
+        assertFalse(checkIdxUsed(qryProc, "idx2", TEST_TBL_NAME, "ADDRESS", "LAST_NAME"));
+    }
+
+    /**
+     *  Checks index usage for partial coverage.
+     *  Last field not participate in any index.
+     */
+    public void testPartialTableFieldsCoveredByIdx() throws Exception {
+        inlineSize = 10;
+
+        String msg0 = "Index with the given set or subset of columns already exists";
+
+        srvLog = new ListeningTestLogger(false, log);
+
+        IgniteEx ig0 = startGrid(0);
+
+        GridQueryProcessor qryProc = ig0.context().query();
+
+        String cacheName = QueryUtils.createTableCacheName("PUBLIC", "TEST_TABLE");
+
+        populateTable(qryProc, TEST_TBL_NAME, 2, "FIRST_NAME", "LAST_NAME",
+            "ADDRESS", "LANG", "GENDER");
+
+        IgniteCache<Object, Object> jcache = ig0.cache(cacheName);
+
+        assertFalse(checkIdxUsed(qryProc, null, TEST_TBL_NAME, "LANG"));
+
+        assertFalse(checkIdxUsed(qryProc, null, TEST_TBL_NAME, "LAST_NAME"));
+
+        String sqlIdx2 = String.format("create index \"idx2\" on %s(LANG, ADDRESS)", TEST_TBL_NAME);
+
+        qryProc.querySqlFields(new SqlFieldsQuery(sqlIdx2), true).getAll();
+        // _key_PK used.
+        assertTrue(checkIdxUsed(qryProc, "idx2", TEST_TBL_NAME, "FIRST_NAME",
+            "LAST_NAME", "LANG", "ADDRESS"));
+        // _key_PK used.
+        assertTrue(checkIdxUsed(qryProc, null, TEST_TBL_NAME, "FIRST_NAME",
+            "LAST_NAME", "LANG", "ADDRESS"));
+
+        assertTrue(checkIdxUsed(qryProc, null, TEST_TBL_NAME, "FIRST_NAME",
+            "LAST_NAME", "ADDRESS", "LANG"));
+        // first idx fields not belongs to request fields.
+        assertFalse(checkIdxUsed(qryProc, "idx2", TEST_TBL_NAME, "ADDRESS",
+            "LAST_NAME"));
+
+        assertFalse(checkIdxAlreadyExistLog(
+            qryProc, "idx3", TEST_TBL_NAME, "ADDRESS", "LANG"));
+    }
+
+    /**
+     * Tests different fields sequence in indexes.
+     */
+    public void testCreateIdxWithDifferentIdxFldsSeq() throws Exception {
+        inlineSize = 10;
+
+        srvLog = new ListeningTestLogger(false, log);
+
+        IgniteEx ig0 = startGrid(0);
+
+        IgniteEx client = (IgniteEx)startGrid(CLIENT_NAME);
+
+        GridQueryProcessor qryProc = ig0.context().query();
+
+        populateTable(qryProc, TEST_TBL_NAME, 1, "c1", "c2", "c3", "c4", "c5");
+
+        assertFalse(checkIdxAlreadyExistLog(
+            qryProc, "idx1", TEST_TBL_NAME, "c1", "c2", "c3", "c4", "c5"));
+
+        assertFalse(checkIdxAlreadyExistLog(
+            qryProc, "idx2", TEST_TBL_NAME, "c1", "c3", "c4", "c5"));
+
+        assertTrue(checkIdxAlreadyExistLog(
+            qryProc, "idx3", TEST_TBL_NAME, "c1", "c2"));
+
+        assertTrue(checkIdxAlreadyExistLog(
+            qryProc, "idx4", TEST_TBL_NAME, "c1", "c3"));
+
+        assertFalse(checkIdxAlreadyExistLog(
+            qryProc, "idx5", TEST_TBL_NAME, "c1", "c4", "c5"));
+
+        GridQueryProcessor qryProcCl = client.context().query();
+
+        assertTrue(checkIdxAlreadyExistLog(
+            qryProcCl, "idx6", TEST_TBL_NAME, "c1", "c2"));
+    }
+
+    /**
+     * Tests different fields sequence in indexes.
+     * Last field not participate in any index.
+     */
+    public void testIndexWithDifferentFldsReqPartialFldsInIdx() throws Exception {
+        inlineSize = 10;
+
+        IgniteEx ig0 = startGrid(0);
+
+        GridQueryProcessor qryProc = ig0.context().query();
+
+        populateTable(qryProc, TEST_TBL_NAME, 2, "FIRST_NAME", "LAST_NAME",
+            "ADDRESS", "LANG", "GENDER");
+
+        String sqlIdx1 = String.format("create index \"idx1\" on %s(LANG, LAST_NAME, ADDRESS, FIRST_NAME)", TEST_TBL_NAME);
+
+        qryProc.querySqlFields(new SqlFieldsQuery(sqlIdx1), true).getAll();
+
+        assertTrue(checkIdxUsed(qryProc, null, TEST_TBL_NAME, "FIRST_NAME", "LAST_NAME", "LANG"));
+
+        assertFalse(checkIdxUsed(qryProc, null, TEST_TBL_NAME,  "LAST_NAME", "ADDRESS"));
+    }
+
+    /**
+     * Tests different fields sequence in indexes.
+     * All fields covered by indexes.
+     */
+    public void testIndexWithDifferentFldsReqAllFldsInIdx() throws Exception {
+        inlineSize = 10;
+
+        IgniteEx ig0 = startGrid(0);
+
+        GridQueryProcessor qryProc = ig0.context().query();
+
+        populateTable(qryProc, TEST_TBL_NAME, 2, "FIRST_NAME", "LAST_NAME",
+            "ADDRESS", "LANG");
+
+        String sqlIdx1 = String.format("create index \"idx1\" on %s(LANG, LAST_NAME, ADDRESS, FIRST_NAME)", TEST_TBL_NAME);
+
+        qryProc.querySqlFields(new SqlFieldsQuery(sqlIdx1), true).getAll();
+
+        assertTrue(checkIdxUsed(qryProc, null, TEST_TBL_NAME, "FIRST_NAME", "LAST_NAME", "LANG"));
+
+        assertTrue(checkIdxUsed(qryProc, null, TEST_TBL_NAME, "FIRST_NAME", "LAST_NAME", "ADDRESS"));
+
+        assertTrue(checkIdxUsed(qryProc, null, TEST_TBL_NAME,  "LAST_NAME", "ADDRESS"));
     }
 
     /** */
