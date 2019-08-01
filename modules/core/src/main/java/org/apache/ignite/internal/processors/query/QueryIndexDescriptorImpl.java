8d6242550581cee2bb22630c7cadbf028764f636
zstan <stanilovsky@gmail.com>
2019-08-01 16:14:50 +0300

    GG-22329 [IGNITE-10654] Print warn message in case of index creating with already existing fields.
    Signed-off-by: Andrey Gura <agura@apache.org>
    (cherry picked from commit c41466f480aca524f7a9da2891d1988c0e82bc43)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/query/QueryIndexDescriptorImpl.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/query/QueryIndexDescriptorImpl.java
@@ -126,7 +126,7 @@ public class QueryIndexDescriptorImpl implements GridQueryIndexDescriptor {
 
         if (descending) {
             if (descendings == null)
-                descendings  = new HashSet<>();
+                descendings = new HashSet<>();
 
             descendings.add(field);
         }
