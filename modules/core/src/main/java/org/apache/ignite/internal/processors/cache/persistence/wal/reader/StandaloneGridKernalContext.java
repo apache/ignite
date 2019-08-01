eb09afedb0fd8728a594d5c36e2015d853495b7e
Fedotov <vanen31@gmail.com>
2019-08-01 11:46:16 +0300

    fix GridManagerStopSelfTest

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/reader/StandaloneGridKernalContext.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/reader/StandaloneGridKernalContext.java
@@ -200,7 +200,6 @@ public class StandaloneGridKernalContext implements GridKernalContext {
 
         cfg.setDiscoverySpi(new StandaloneNoopDiscoverySpi());
         cfg.setCommunicationSpi(new StandaloneNoopCommunicationSpi());
-        cfg.setMetricExporterSpi(new NoopMetricExporterSpi());
 
         final Marshaller marshaller = new BinaryMarshaller();
         cfg.setMarshaller(marshaller);
