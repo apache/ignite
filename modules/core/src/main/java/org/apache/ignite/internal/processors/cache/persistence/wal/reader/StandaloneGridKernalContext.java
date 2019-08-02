4efe78b3b709505d49e8b73ae59a42d2eed07b30
Ivan Fedotov <vanen31@gmail.com>
2019-08-02 13:05:06 +0300

    IGNITE-11922: Migration of ClusterMetricsMXBeanImpl to the new metric framework (#6648)
    Metrics migration

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/reader/StandaloneGridKernalContext.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/reader/StandaloneGridKernalContext.java
@@ -156,6 +156,7 @@ public class StandaloneGridKernalContext implements GridKernalContext {
 
         this.marshallerCtx = new MarshallerContextImpl(null, null);
         this.cfg = prepareIgniteConfiguration();
+        this.metricMgr = new GridMetricManager(this);
 
         // Fake folder provided to perform processor startup on empty folder.
         if (binaryMetadataFileStoreDir == null)
@@ -167,8 +168,6 @@ public class StandaloneGridKernalContext implements GridKernalContext {
             marshallerCtx.setMarshallerMappingFileStoreDir(marshallerMappingFileStoreDir);
             marshallerCtx.onMarshallerProcessorStarted(this, null);
         }
-
-        metricMgr = new GridMetricManager(this);
     }
 
     /**
