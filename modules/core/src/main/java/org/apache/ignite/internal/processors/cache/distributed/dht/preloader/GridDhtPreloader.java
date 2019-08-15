1796cae97cadda02fbbd55d4edff15dcec3c36e5
ktkalenko <ktkalenko@gridgain.com>
2019-08-15 10:19:23 +0300

    Revert "Merge branch 'gg-21268' into 8.8-master"
    This reverts commit 3688f93addc7384ac5c7788931db6ce941074c15, reversing
    changes made to 8bcf1d0e342f8329de4455271a7dcdcb479f2096.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPreloader.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPreloader.java
@@ -632,13 +632,4 @@ public class GridDhtPreloader extends GridCachePreloaderAdapter {
     @Override public void dumpDebugInfo() {
         // No-op
     }
-
-    /**
-     * Return demander.
-     *
-     * @return Demander.
-     * */
-    public GridDhtPartitionDemander demander() {
-        return demander;
-    }
 }
