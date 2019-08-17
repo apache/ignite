9e6f62795c9df16967bcee94aef9e85b39c64a15
Anton Kalashnikov <kaa.dev@yandex.ru>
2019-08-17 09:38:29 +0300

    GG-22946 [GG-22144]  Fixed sometimes address already in use for FakeServer
    Signed-off-by: Dmitriy Govorukhin <dmitriy.govorukhin@gmail.com>
    
    (cherry picked from commit 98af58a)
    Signed-off-by: Dmitriy Govorukhin <dmitriy.govorukhin@gmail.com>

--- modules/core/src/test/java/org/apache/ignite/spi/communication/tcp/TcpCommunicationSpiFaultyClientTest.java
+++ modules/core/src/test/java/org/apache/ignite/spi/communication/tcp/TcpCommunicationSpiFaultyClientTest.java
@@ -17,6 +17,7 @@
 package org.apache.ignite.spi.communication.tcp;
 
 import java.io.IOException;
+import java.net.BindException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.util.Collections;
@@ -58,6 +59,9 @@ public class TcpCommunicationSpiFaultyClientTest extends GridCommonAbstractTest 
             return block && node.order() == 3;
         }
     };
+
+    /** Server port for {@link FakeServer}. */
+    private static int serverPort = 47200;
 
     /** Client mode. */
     private static boolean clientMode;
@@ -106,7 +110,27 @@ public class TcpCommunicationSpiFaultyClientTest extends GridCommonAbstractTest 
     @Override protected void beforeTestsStarted() throws Exception {
         super.beforeTestsStarted();
 
+        serverPort = takeFreePort();
+
         System.setProperty(IgniteSystemProperties.IGNITE_ENABLE_FORCIBLE_NODE_KILL, "true");
+    }
+
+    /**
+     * @throws IOException If failed.
+     */
+    private static int takeFreePort() throws IOException {
+        int freePort = serverPort;
+
+        while(true) {
+            try {
+                U.closeQuiet(startServerSocket(freePort));
+
+                return freePort;
+            }
+            catch (BindException ignore) { //If address already in use (Bind failed) t trying to choose another one.
+                freePort++;
+            }
+        }
     }
 
     /** {@inheritDoc} */
@@ -283,6 +307,13 @@ public class TcpCommunicationSpiFaultyClientTest extends GridCommonAbstractTest 
     }
 
     /**
+     * @throws IOException If failed.
+     */
+    private static ServerSocket startServerSocket(int port) throws IOException {
+        return new ServerSocket(port, 50, InetAddress.getByName("127.0.0.1"));
+    }
+
+    /**
      * Server that emulates connection troubles.
      */
     private static class FakeServer implements Runnable {
@@ -296,7 +327,7 @@ public class TcpCommunicationSpiFaultyClientTest extends GridCommonAbstractTest 
          * Default constructor.
          */
         FakeServer() throws IOException {
-            srv = new ServerSocket(47200, 50, InetAddress.getByName("127.0.0.1"));
+            srv = startServerSocket(serverPort);
         }
 
         /**
@@ -335,7 +366,7 @@ public class TcpCommunicationSpiFaultyClientTest extends GridCommonAbstractTest 
                 Map<String, Object> attrs = new HashMap<>(node.attributes());
 
                 attrs.put(createAttributeName(ATTR_ADDRS), Collections.singleton("127.0.0.1"));
-                attrs.put(createAttributeName(ATTR_PORT), 47200);
+                attrs.put(createAttributeName(ATTR_PORT), serverPort);
                 attrs.put(createAttributeName(ATTR_EXT_ADDRS), Collections.emptyList());
                 attrs.put(createAttributeName(ATTR_HOST_NAMES), Collections.emptyList());
 
