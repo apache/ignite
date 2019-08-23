c4cc0ca055b83228d21c8d11aee5be5cd3587256
Anton Kalashnikov <kaa.dev@yandex.ru>
2019-08-23 16:16:32 +0300

    GG-22973 Scheduled checkpoint moving to progress under write lock
    (cherry picked from commit 6bac9c8)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/IgniteCacheDatabaseSharedManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/IgniteCacheDatabaseSharedManager.java
@@ -16,6 +16,7 @@
 
 package org.apache.ignite.internal.processors.cache.persistence;
 
+import javax.management.InstanceNotFoundException;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -25,7 +26,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import javax.management.InstanceNotFoundException;
 import org.apache.ignite.DataRegionMetrics;
 import org.apache.ignite.DataRegionMetricsProvider;
 import org.apache.ignite.DataStorageMetrics;
@@ -74,6 +74,7 @@ import org.apache.ignite.internal.util.typedef.internal.CU;
 import org.apache.ignite.internal.util.typedef.internal.LT;
 import org.apache.ignite.internal.util.typedef.internal.U;
 import org.apache.ignite.lang.IgniteBiTuple;
+import org.apache.ignite.lang.IgniteInClosure;
 import org.apache.ignite.lang.IgniteOutClosure;
 import org.apache.ignite.mxbean.DataRegionMetricsMXBean;
 import org.jetbrains.annotations.Nullable;
@@ -895,6 +896,20 @@ public class IgniteCacheDatabaseSharedManager extends GridCacheSharedManagerAdap
      * @throws IgniteCheckedException If failed.
      */
     public void waitForCheckpoint(String reason) throws IgniteCheckedException {
+        waitForCheckpoint(reason, null);
+    }
+
+    /**
+     * Waits until current state is checkpointed and execution listeners after finish.
+     *
+     * @param reason Reason for checkpoint wakeup if it would be required.
+     * @param lsnr Listeners which should be called in checkpoint thread after current checkpoint finished.
+     * @throws IgniteCheckedException If failed.
+     */
+    public <R> void waitForCheckpoint(
+        String reason,
+        IgniteInClosure<? super IgniteInternalFuture<R>> lsnr
+    ) throws IgniteCheckedException {
         // No-op
     }
 
