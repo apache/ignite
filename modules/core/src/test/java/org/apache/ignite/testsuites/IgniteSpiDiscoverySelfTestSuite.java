86c4126f9e8864049c6e15f8cca960534f287b9a
Denis Mekhanikov <dmekhanikov@gmail.com>
2019-08-23 13:23:11 +0300

    GG-22696 [IGNITE-10808] Fixed Discovery message queue may build up with TcpDiscoveryMetricsUpdateMessage - Fixes #5771

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteSpiDiscoverySelfTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteSpiDiscoverySelfTestSuite.java
@@ -28,6 +28,7 @@ import org.apache.ignite.spi.discovery.tcp.IgniteClientConnectSslTest;
 import org.apache.ignite.spi.discovery.tcp.IgniteClientConnectTest;
 import org.apache.ignite.spi.discovery.tcp.IgniteClientReconnectMassiveShutdownSslTest;
 import org.apache.ignite.spi.discovery.tcp.IgniteClientReconnectMassiveShutdownTest;
+import org.apache.ignite.spi.discovery.tcp.IgniteMetricsOverflowTest;
 import org.apache.ignite.spi.discovery.tcp.TcpClientDiscoveryMarshallerCheckSelfTest;
 import org.apache.ignite.spi.discovery.tcp.TcpClientDiscoverySpiCoordinatorChangeTest;
 import org.apache.ignite.spi.discovery.tcp.TcpClientDiscoverySpiFailureTimeoutSelfTest;
@@ -156,7 +157,9 @@ import static org.apache.ignite.IgniteSystemProperties.IGNITE_OVERRIDE_MCAST_GRP
 
     TcpDiscoveryPendingMessageDeliveryTest.class,
 
-    TcpDiscoveryReconnectUnstableTopologyTest.class
+    TcpDiscoveryReconnectUnstableTopologyTest.class,
+
+    IgniteMetricsOverflowTest.class
 })
 public class IgniteSpiDiscoverySelfTestSuite {
     /** */
