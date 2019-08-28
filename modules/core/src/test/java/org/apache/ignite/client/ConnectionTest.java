846ffae94491255dcccff310c751d23f9574a2ec
Igor Sapego <isapego@apache.org>
2019-08-28 14:44:01 +0300

    GG-23022: Fixed exception on message parsing failure
    (cherry picked from commit 4ff32922050d91d3a223364bf5b9ed17f061ac12)

--- modules/core/src/test/java/org/apache/ignite/client/ConnectionTest.java
+++ modules/core/src/test/java/org/apache/ignite/client/ConnectionTest.java
@@ -60,6 +60,15 @@ public class ConnectionTest {
         testConnection("127.0.0.1:47500", "127.0.0.1:10801", Config.SERVER);
     }
 
+    /** */
+    @Test(expected = org.apache.ignite.client.ClientConnectionException.class)
+    public void testInvalidBigHandshakeMessage() throws Exception {
+        char[] data = new char[1024 * 1024 * 128];
+        String userName = new String(data);
+
+        testConnectionWithUsername(userName, Config.SERVER);
+    }
+
     /**
      * @param addrs Addresses to connect.
      */
@@ -67,6 +76,17 @@ public class ConnectionTest {
         try (LocalIgniteCluster cluster = LocalIgniteCluster.start(1);
              IgniteClient client = Ignition.startClient(new ClientConfiguration()
                      .setAddresses(addrs))) {
+        }
+    }
+    /**
+     * @param userName User name.
+     * @param addrs Addresses to connect.
+     */
+    private void testConnectionWithUsername(String userName, String... addrs) throws Exception {
+        try (LocalIgniteCluster cluster = LocalIgniteCluster.start(1);
+             IgniteClient client = Ignition.startClient(new ClientConfiguration()
+                 .setAddresses(addrs)
+                 .setUserName(userName))) {
         }
     }
 }
