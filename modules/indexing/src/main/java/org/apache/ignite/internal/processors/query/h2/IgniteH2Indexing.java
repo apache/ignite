21a77c712eebcae469fd1ac35c078b5131f0bb38
Igor Belyakov <igor.belyakov.s@gmail.com>
2019-08-28 18:51:24 +0300

    Revert "GG-23239 Fix full indexes rebuild after each restart cluster."
    This reverts commit 317a35fe54d514add8cd86aae288db1c6e408525.

--- modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/IgniteH2Indexing.java
+++ modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/IgniteH2Indexing.java
@@ -2135,7 +2135,10 @@ public class IgniteH2Indexing implements GridQueryIndexing {
 
         SchemaIndexCacheVisitorClosure clo;
 
-        if (!pageStore.hasIndexStore(cctx.groupId())) {
+        boolean idxStoreMissing = !pageStore.hasIndexStore(cctx.groupId());
+
+        if (idxStoreMissing ||
+            IgniteSystemProperties.getBoolean(IgniteSystemProperties.FORCE_FULL_INDEX_REBUILD, true)) {
             // If there are no index store, rebuild all indexes.
             clo = new IndexRebuildFullClosure(cctx.queries());
         }
@@ -2149,12 +2152,8 @@ public class IgniteH2Indexing implements GridQueryIndexing {
                 tblDesc.table().collectIndexesForPartialRebuild(clo0);
             }
 
-            if (clo0.hasIndexes()) {
-                if (IgniteSystemProperties.getBoolean(IgniteSystemProperties.FORCE_FULL_INDEX_REBUILD, true))
-                    clo = new IndexRebuildFullClosure(cctx.queries());
-                else
-                    clo = clo0;
-            }
+            if (clo0.hasIndexes())
+                clo = clo0;
             else
                 return null;
         }
