fe04723aadc466498feda880a4cb698cbdcc6621
Dmitriy Govorukhin <dmitriy.govorukhin@gmail.com>
2019-09-05 10:15:21 +0300

    GG-23632 [GG-23463] Fixed WAL writer may close file IO with unflushed changes when MMAP is disabled

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/FileWriteAheadLogManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/FileWriteAheadLogManager.java
@@ -131,6 +131,7 @@ import org.apache.ignite.thread.IgniteThread;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import static java.lang.Long.MAX_VALUE;
 import static java.nio.file.StandardOpenOption.CREATE;
 import static java.nio.file.StandardOpenOption.READ;
 import static java.nio.file.StandardOpenOption.WRITE;
@@ -2757,9 +2758,10 @@ public class FileWriteAheadLogManager extends GridCacheSharedManagerAdapter impl
                 return;
             }
 
-            assert ptr.index() == getSegmentId();
+            assert ptr.index() == getSegmentId() : "Pointer segment idx is not equals to current write segment idx. " +
+                "ptr=" + ptr + " segmetntId=" + getSegmentId();
 
-            walWriter.flushBuffer(ptr.fileOffset());
+            walWriter.flushBuffer(ptr.fileOffset() + ptr.length());
         }
 
         /**
@@ -3419,7 +3421,7 @@ public class FileWriteAheadLogManager extends GridCacheSharedManagerAdapter impl
                             }
                         }
                         else {
-                            unparkWaiters(Long.MAX_VALUE);
+                            unparkWaiters(MAX_VALUE);
 
                             return;
                         }
@@ -3450,7 +3452,7 @@ public class FileWriteAheadLogManager extends GridCacheSharedManagerAdapter impl
 
                             err = e;
 
-                            unparkWaiters(Long.MAX_VALUE);
+                            unparkWaiters(MAX_VALUE);
 
                             return;
                         }
@@ -3484,7 +3486,9 @@ public class FileWriteAheadLogManager extends GridCacheSharedManagerAdapter impl
                         finally {
                             seg.release();
 
-                            long p = pos <= UNCONDITIONAL_FLUSH || err != null ? Long.MAX_VALUE : currentHandle().written;
+                            boolean unparkAll = (pos == UNCONDITIONAL_FLUSH || pos == FILE_CLOSE) || err != null;
+
+                            long p = unparkAll ? MAX_VALUE : currentHandle().written;
 
                             unparkWaiters(p);
                         }
@@ -3497,7 +3501,7 @@ public class FileWriteAheadLogManager extends GridCacheSharedManagerAdapter impl
             finally {
                 this.err = err;
 
-                unparkWaiters(Long.MAX_VALUE);
+                unparkWaiters(MAX_VALUE);
 
                 if (err == null && !isCancelled)
                     err = new IllegalStateException("Worker " + name() + " is terminated unexpectedly");
