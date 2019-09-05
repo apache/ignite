c24972d733c5901ccc9657afae5af92f1dcf81b0
ktkalenko <ktkalenko@gridgain.com>
2019-09-05 16:14:18 +0300

    I fixed it according to the commentary of the reviewer.

--- modules/core/src/main/java/org/apache/ignite/internal/visor/verify/VisorIdleVerifyTaskArg.java
+++ modules/core/src/main/java/org/apache/ignite/internal/visor/verify/VisorIdleVerifyTaskArg.java
@@ -1,12 +1,12 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
+ * contributor license agreements. See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * the License. You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -24,7 +24,6 @@ import java.util.Set;
 import org.apache.ignite.internal.util.typedef.internal.S;
 import org.apache.ignite.internal.util.typedef.internal.U;
 import org.apache.ignite.internal.visor.VisorDataTransferObject;
-import org.jetbrains.annotations.NotNull;
 
 import static java.util.Objects.nonNull;
 import static org.apache.ignite.internal.visor.verify.CacheFilterEnum.DEFAULT;
@@ -202,7 +201,7 @@ public class VisorIdleVerifyTaskArg extends VisorDataTransferObject {
     /**
      * @return Kind fo cache.
      */
-    @NotNull public CacheFilterEnum cacheFilterEnum() {
+    public CacheFilterEnum cacheFilterEnum() {
         return cacheFilterEnum;
     }
 
