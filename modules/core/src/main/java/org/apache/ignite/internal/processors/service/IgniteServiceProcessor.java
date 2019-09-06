95a64aa3699ff27e1fb9dcbaee7d3d44296171e0
Nikolay Izhikov <nizhikov@apache.org>
2019-09-06 12:40:23 +0300

    IGNITE-12145: Monitoring list engine.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/service/IgniteServiceProcessor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/service/IgniteServiceProcessor.java
@@ -61,6 +61,10 @@ import org.apache.ignite.internal.processors.cache.DynamicCacheChangeRequest;
 import org.apache.ignite.internal.processors.cluster.ChangeGlobalStateMessage;
 import org.apache.ignite.internal.processors.cluster.DiscoveryDataClusterState;
 import org.apache.ignite.internal.processors.cluster.IgniteChangeGlobalStateSupport;
+import org.apache.ignite.internal.processors.metric.GridMetricManager;
+import org.apache.ignite.spi.metric.ReadOnlyMonitoringListRegistry;
+import org.apache.ignite.spi.metric.list.MonitoringList;
+import org.apache.ignite.spi.metric.list.view.ServiceView;
 import org.apache.ignite.internal.util.future.GridCompoundFuture;
 import org.apache.ignite.internal.util.future.GridFinishedFuture;
 import org.apache.ignite.internal.util.future.GridFutureAdapter;
@@ -90,6 +94,8 @@ import static org.apache.ignite.configuration.DeploymentMode.ISOLATED;
 import static org.apache.ignite.configuration.DeploymentMode.PRIVATE;
 import static org.apache.ignite.events.EventType.EVT_NODE_JOINED;
 import static org.apache.ignite.internal.GridComponent.DiscoveryDataExchangeType.SERVICE_PROC;
+import static org.apache.ignite.internal.processors.metric.GridMetricManager.SVCS_MON_LIST;
+import static org.apache.ignite.internal.processors.metric.GridMetricManager.SVCS_MON_LIST_DESC;
 
 /**
  * Ignite service processor.
@@ -123,6 +129,14 @@ public class IgniteServiceProcessor extends ServiceProcessorAdapter implements I
      * @see ServiceDeploymentManager
      */
     private final ConcurrentMap<IgniteUuid, ServiceInfo> registeredServices = new ConcurrentHashMap<>();
+
+    /**
+     * Services monitoring list.
+     *
+     * @see ReadOnlyMonitoringListRegistry
+     * @see GridMetricManager
+     */
+    private final MonitoringList<IgniteUuid, ServiceView> svcsMonList;
 
     /**
      * Collection of services information that were processed by deployment worker. <b>It is updated from deployment
@@ -185,6 +199,8 @@ public class IgniteServiceProcessor extends ServiceProcessorAdapter implements I
      */
     public IgniteServiceProcessor(GridKernalContext ctx) {
         super(ctx);
+
+        svcsMonList = ctx.metric().list(SVCS_MON_LIST, SVCS_MON_LIST_DESC, ServiceView.class);
     }
 
     /** {@inheritDoc} */
@@ -264,6 +280,7 @@ public class IgniteServiceProcessor extends ServiceProcessorAdapter implements I
         cancelDeployedServices();
 
         registeredServices.clear();
+        svcsMonList.clear();
 
         // If user requests sent to network but not received back to handle in deployment manager.
         Stream.concat(depFuts.values().stream(), undepFuts.values().stream()).forEach(fut -> {
@@ -333,8 +350,10 @@ public class IgniteServiceProcessor extends ServiceProcessorAdapter implements I
 
         ServiceProcessorCommonDiscoveryData clusterData = (ServiceProcessorCommonDiscoveryData)data.commonData();
 
-        for (ServiceInfo desc : clusterData.registeredServices())
+        for (ServiceInfo desc : clusterData.registeredServices()) {
             registeredServices.put(desc.serviceId(), desc);
+            svcsMonList.add(desc);
+        }
     }
 
     /** {@inheritDoc} */
@@ -368,6 +387,7 @@ public class IgniteServiceProcessor extends ServiceProcessorAdapter implements I
 
             if (oldDesc == null) {
                 registeredServices.put(desc.serviceId(), desc);
+                svcsMonList.add(desc);
 
                 continue;
             }
@@ -1495,7 +1515,10 @@ public class IgniteServiceProcessor extends ServiceProcessorAdapter implements I
             // First node start, method onGridDataReceived(DiscoveryDataBag.GridDiscoveryData) has not been called.
             ArrayList<ServiceInfo> staticServicesInfo = staticallyConfiguredServices(false);
 
-            staticServicesInfo.forEach(desc -> registeredServices.put(desc.serviceId(), desc));
+            staticServicesInfo.forEach(desc -> {
+                registeredServices.put(desc.serviceId(), desc);
+                svcsMonList.add(desc);
+            });
         }
 
         ServiceDeploymentActions depActions = null;
@@ -1598,6 +1621,7 @@ public class IgniteServiceProcessor extends ServiceProcessorAdapter implements I
                             ServiceInfo desc = new ServiceInfo(snd.id(), reqSrvcId, cfg);
 
                             registeredServices.put(reqSrvcId, desc);
+                            svcsMonList.add(desc);
 
                             toDeploy.put(reqSrvcId, desc);
                         }
@@ -1631,6 +1655,7 @@ public class IgniteServiceProcessor extends ServiceProcessorAdapter implements I
             }
             else if (req instanceof ServiceUndeploymentRequest) {
                 ServiceInfo rmv = registeredServices.remove(reqSrvcId);
+                svcsMonList.remove(reqSrvcId);
 
                 assert oldDesc == rmv : "Concurrent map modification.";
 
@@ -1681,6 +1706,8 @@ public class IgniteServiceProcessor extends ServiceProcessorAdapter implements I
 
                     if (desc.cacheName().equals(chReq.cacheName())) {
                         toUndeploy.put(desc.serviceId(), desc);
+
+                        svcsMonList.remove(desc.serviceId());
 
                         return true;
                     }
