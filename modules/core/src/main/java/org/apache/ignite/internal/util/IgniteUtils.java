6e78ccc47c0328063c1f7148dadc6d28c5d45005
Nikolay Izhikov <nizhikov@apache.org>
2019-09-09 16:21:42 +0300

    IGNITE-12145: Code style fixes.

--- modules/core/src/main/java/org/apache/ignite/internal/util/IgniteUtils.java
+++ modules/core/src/main/java/org/apache/ignite/internal/util/IgniteUtils.java
@@ -114,7 +114,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.Objects;
 import java.util.Random;
 import java.util.ServiceLoader;
 import java.util.Set;
