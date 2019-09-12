27c44f7738171e86e378b30529f22b532df159fa
Aleksei Scherbakov <alexey.scherbakoff@gmail.com>
2019-09-12 18:23:03 +0300

    GG-23781 Prevent pessimistic tx lock request processing over incomplete topology.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/colocated/GridDhtColocatedLockFuture.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/colocated/GridDhtColocatedLockFuture.java
@@ -1704,10 +1704,6 @@ public final class GridDhtColocatedLockFuture extends GridCacheCompoundIdentityF
 
                 if (res.compatibleRemapVersion()) {
                     if (tx != null) {
-                        // Versions are compatible.
-                        cctx.shared().exchange().
-                            lastAffinityChangedTopologyVersion(res.clientRemapVersion(), tx.topologyVersionSnapshot());
-
                         tx.onRemap(res.clientRemapVersion(), false);
 
                         // Use remapped version for all subsequent mappings.
