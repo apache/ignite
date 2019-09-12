82fdf53df3160b0303bf84b3acb79b897d697a66
denis-chudov <dchudov@gridgain.com>
2019-09-12 11:59:55 +0300

    GG-23835 Fix negative time in Transaction time dump

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/near/GridNearTxLocal.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/near/GridNearTxLocal.java
@@ -3917,14 +3917,14 @@ public class GridNearTxLocal extends GridDhtTxLocalAdapter implements GridTimeou
         if (state == COMMITTED || state == ROLLED_BACK) {
             leaveSystemSection();
 
-            //if commitOrRollbackTime != 0 it means that we already have written metrics and dumped it in log at least once
+            // If commitOrRollbackTime != 0 it means that we already have written metrics and dumped it in log at least once.
             if (!commitOrRollbackTime.compareAndSet(0, System.nanoTime() - commitOrRollbackStartTime.get()))
                 return res;
 
             long systemTimeMillis = U.nanosToMillis(this.systemTime.get());
             long totalTimeMillis = System.currentTimeMillis() - startTime();
 
-            //in some cases totalTimeMillis can be less than systemTimeMillis, as they are calculated with different precision
+            // In some cases totalTimeMillis can be less than systemTimeMillis, as they are calculated with different precision.
             long userTimeMillis = Math.max(totalTimeMillis - systemTimeMillis, 0);
 
             cctx.txMetrics().onNearTxComplete(systemTimeMillis, userTimeMillis);
@@ -4016,7 +4016,7 @@ public class GridNearTxLocal extends GridDhtTxLocalAdapter implements GridTimeou
     public IgniteInternalFuture<?> prepareNearTxLocal() {
         enterSystemSection();
 
-        //we assume that prepare start time should be set only once for the transaction
+        // We assume that prepare start time should be set only once for the transaction.
         prepareStartTime.compareAndSet(0, System.nanoTime());
 
         GridNearTxPrepareFutureAdapter fut = (GridNearTxPrepareFutureAdapter)prepFut;
@@ -4117,7 +4117,7 @@ public class GridNearTxLocal extends GridDhtTxLocalAdapter implements GridTimeou
 
             prepareFut.listen(new CI1<IgniteInternalFuture<?>>() {
                 @Override public void apply(IgniteInternalFuture<?> f) {
-                    //these values should not be changed after set once
+                    // These values should not be changed after set once.
                     prepareTime.compareAndSet(0, System.nanoTime() - prepareStartTime.get());
 
                     commitOrRollbackStartTime.compareAndSet(0, System.nanoTime());
@@ -4185,6 +4185,9 @@ public class GridNearTxLocal extends GridDhtTxLocalAdapter implements GridTimeou
             log.debug("Rolling back near tx: " + this);
 
         enterSystemSection();
+
+        // This value should not be changed after set once.
+        commitOrRollbackStartTime.compareAndSet(0, System.nanoTime());
 
         if (!onTimeout && trackTimeout)
             removeTimeoutHandler();
@@ -5146,8 +5149,8 @@ public class GridNearTxLocal extends GridDhtTxLocalAdapter implements GridTimeou
      * Enters the section when system time for this transaction is counted.
      */
     public void enterSystemSection() {
-        //setting systemStartTime only if it equals 0, otherwise it means that we are already in system section
-        //and sould do nothing.
+        // Setting systemStartTime only if it equals 0, otherwise it means that we are already in system section
+        // and should do nothing.
         systemStartTime.compareAndSet(0, System.nanoTime());
     }
 
