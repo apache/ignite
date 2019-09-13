3bede2fd26b9767ee5186ea67a4185a8fdf450db
ktkalenko <ktkalenko@gridgain.com>
2019-09-13 11:24:14 +0300

    GG-23810 Error in case of IGNITE_WRITE_REBALANCE_STATISTICS=true.
    (cherry picked from commit ec4579a5972389aaf3b4598d206b18fef2e7e741)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPartitionDemander.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPartitionDemander.java
@@ -82,8 +82,8 @@ import static java.lang.System.currentTimeMillis;
 import static java.util.Collections.singletonList;
 import static java.util.Collections.singletonMap;
 import static java.util.Objects.nonNull;
-import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toSet;
 import static org.apache.ignite.events.EventType.EVT_CACHE_REBALANCE_OBJECT_LOADED;
 import static org.apache.ignite.events.EventType.EVT_CACHE_REBALANCE_PART_LOADED;
 import static org.apache.ignite.events.EventType.EVT_CACHE_REBALANCE_STARTED;
@@ -1561,13 +1561,13 @@ public class GridDhtPartitionDemander {
      *
      * @return List demanders.
      * */
-    private List<GridDhtPartitionDemander> demanders(){
+    private Set<GridDhtPartitionDemander> demanders(){
         return ctx.cacheContexts().stream()
             .map(GridCacheContext::preloader)
             .filter(GridDhtPreloader.class::isInstance)
             .map(GridDhtPreloader.class::cast)
             .map(GridDhtPreloader::demander)
-            .collect(toList());
+            .collect(toSet());
     }
 
     /**
@@ -1610,10 +1610,10 @@ public class GridDhtPartitionDemander {
                 return;
         }
 
-        List<GridDhtPartitionDemander> demanders = demanders();
+        Set<GridDhtPartitionDemander> demanders = demanders();
 
-        Map<CacheGroupContext, Collection<RebalanceFuture>> rebFutrs =
-            demanders.stream().collect(toMap(demander -> demander.grp, demander -> demander.lastStatFutures));
+        Map<CacheGroupContext, Collection<RebalanceFuture>> rebFutrs = demanders.stream()
+            .collect(toMap(demander -> demander.grp, demander -> demander.lastStatFutures));
 
         try {
             log.info(rebalanceStatistics(true, rebFutrs));
