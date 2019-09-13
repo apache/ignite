81ac2a6c87f59fe60819be8d6e037606747d181c
tledkov <tledkov@gridgain.com>
2019-09-13 22:14:39 +0300

    GG-23828 use non interruptibly lock at the GridH2Table for cache operations

--- modules/indexing/src/test/java/org/apache/ignite/testsuites/IgniteCacheQuerySelfTestSuite2.java
+++ modules/indexing/src/test/java/org/apache/ignite/testsuites/IgniteCacheQuerySelfTestSuite2.java
@@ -50,6 +50,9 @@ import org.apache.ignite.internal.processors.query.IgniteCacheGroupsCompareQuery
 import org.apache.ignite.internal.processors.query.IgniteCacheGroupsSqlDistributedJoinSelfTest;
 import org.apache.ignite.internal.processors.query.IgniteCacheGroupsSqlSegmentedIndexMultiNodeSelfTest;
 import org.apache.ignite.internal.processors.query.IgniteCacheGroupsSqlSegmentedIndexSelfTest;
+import org.apache.ignite.internal.processors.query.SqlIndexConsistencyAfterInterruptAtomicCacheOperationTest;
+import org.apache.ignite.internal.processors.query.SqlIndexConsistencyAfterInterruptTxCacheOperationTest;
+import org.apache.ignite.internal.processors.query.SqlTwoCachesInGroupWithSameEntryTest;
 import org.apache.ignite.internal.processors.query.h2.twostep.CacheQueryMemoryLeakTest;
 import org.apache.ignite.internal.processors.query.h2.twostep.DisappearedCacheCauseRetryMessageSelfTest;
 import org.apache.ignite.internal.processors.query.h2.twostep.DisappearedCacheWasNotFoundMessageSelfTest;
@@ -68,6 +71,10 @@ public class IgniteCacheQuerySelfTestSuite2 extends TestSuite {
      */
     public static TestSuite suite() throws Exception {
         TestSuite suite = new IgniteTestSuite("Ignite Cache Queries Test Suite 2");
+
+        suite.addTestSuite(SqlIndexConsistencyAfterInterruptAtomicCacheOperationTest.class);
+        suite.addTestSuite(SqlIndexConsistencyAfterInterruptTxCacheOperationTest.class);
+        suite.addTestSuite(SqlTwoCachesInGroupWithSameEntryTest.class);
 
         // Dynamic index create/drop tests.
         suite.addTestSuite(DynamicIndexPartitionedAtomicConcurrentSelfTest.class);
