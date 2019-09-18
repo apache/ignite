104096fd916e4a234c1488b22bdff7554d51b4ca
Alexey Platonov <40424278+avplatonov@users.noreply.github.com>
2019-09-18 10:58:54 +0300

    IGNITE-12155: [ML] Implementation of distributed estimator (#6857)

--- examples/src/main/java/org/apache/ignite/examples/ml/tree/FraudDetectionExample.java
+++ examples/src/main/java/org/apache/ignite/examples/ml/tree/FraudDetectionExample.java
@@ -28,7 +28,6 @@ import org.apache.ignite.ml.environment.LearningEnvironmentBuilder;
 import org.apache.ignite.ml.math.primitives.vector.Vector;
 import org.apache.ignite.ml.regressions.logistic.LogisticRegressionSGDTrainer;
 import org.apache.ignite.ml.selection.scoring.evaluator.Evaluator;
-import org.apache.ignite.ml.selection.scoring.metric.classification.BinaryClassificationMetricValues;
 import org.apache.ignite.ml.selection.split.TrainTestDatasetSplitter;
 import org.apache.ignite.ml.selection.split.TrainTestSplit;
 import org.apache.ignite.ml.trainers.DatasetTrainer;
@@ -38,14 +37,13 @@ import org.apache.ignite.ml.util.SandboxMLCache;
 
 /**
  * Example of using classification algorithms for fraud detection problem.
- *
- * Description of models can be found in:
- *      https://en.wikipedia.org/wiki/Logistic_regression and
- *      https://en.wikipedia.org/wiki/Decision_tree_learning .
- * Original dataset can be downloaded from: https://www.kaggle.com/mlg-ulb/creditcardfraud/ .
- * Copy of dataset are stored in:  modules/ml/src/main/resources/datasets/fraud_detection.csv .
- * Score for clusterizer estimation: accuracy, recall, precision, f1-score .
- * Description of entropy can be found in: https://en.wikipedia.org/wiki/Evaluation_of_binary_classifiers .
+ * <p>
+ * Description of models can be found in: https://en.wikipedia.org/wiki/Logistic_regression and
+ * https://en.wikipedia.org/wiki/Decision_tree_learning . Original dataset can be downloaded from:
+ * https://www.kaggle.com/mlg-ulb/creditcardfraud/ . Copy of dataset are stored in:
+ * modules/ml/src/main/resources/datasets/fraud_detection.csv . Score for clusterizer estimation: accuracy, recall,
+ * precision, f1-score . Description of entropy can be found in: https://en.wikipedia.org/wiki/Evaluation_of_binary_classifiers
+ * .
  */
 public class FraudDetectionExample {
     public static void main(String[] args) throws IOException {
@@ -78,10 +76,12 @@ public class FraudDetectionExample {
                         .withEnvironmentBuilder(LearningEnvironmentBuilder.defaultBuilder().withRNGSeed(0)),
                     vectorizer, split
                 );
-            } finally {
+            }
+            finally {
                 dataCache.destroy();
             }
-        } finally {
+        }
+        finally {
             System.out.flush();
         }
     }
@@ -89,11 +89,11 @@ public class FraudDetectionExample {
     /**
      * Train model and estimate it.
      *
-     * @param ignite Ignite
-     * @param dataCache Data set cache.
-     * @param trainer Trainer.
+     * @param ignite     Ignite
+     * @param dataCache  Data set cache.
+     * @param trainer    Trainer.
      * @param vectorizer Upstream vectorizer.
-     * @param splitter Train test splitter.
+     * @param splitter   Train test splitter.
      */
     private static void trainAndEstimateModel(Ignite ignite,
         IgniteCache<Integer, Vector> dataCache,
@@ -107,20 +107,6 @@ public class FraudDetectionExample {
         );
 
         System.out.println(">>> Perform scoring.");
-        BinaryClassificationMetricValues metricValues = Evaluator.evaluate(
-            dataCache,
-            splitter.getTestFilter(),
-            mdl,
-            vectorizer
-        );
-
-        System.out.println(String.format(">> Model accuracy: %.2f", metricValues.accuracy()));
-        System.out.println(String.format(">> Model precision: %.2f", metricValues.precision()));
-        System.out.println(String.format(">> Model recall: %.2f", metricValues.recall()));
-        System.out.println(String.format(">> Model f1-score: %.2f", metricValues.f1Score()));
-        System.out.println(">> Confusion matrix:");
-        System.out.println(">>                    fraud (ans) | not fraud (ans)");
-        System.out.println(String.format(">> fraud (pred)     | %1$11.2f | %2$15.2f ", metricValues.tp(), metricValues.fp()));
-        System.out.println(String.format(">> not fraud (pred) | %1$11.2f | %2$15.2f ", metricValues.fn(), metricValues.tn()));
+        System.out.println(Evaluator.evaluateBinaryClassification(dataCache, splitter.getTestFilter(), mdl, vectorizer));
     }
 }
