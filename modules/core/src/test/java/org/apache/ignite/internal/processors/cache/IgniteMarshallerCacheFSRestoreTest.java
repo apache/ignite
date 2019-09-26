3db5e0734b48a86d9b7d1b43dad1dfd3503288d6
mstepachev <maksim.stepachev@gmail.com>
2019-09-26 17:37:10 +0300

    GG-24363 PartitionsEvictManagerAbstractTest breaks inspections
    (cherry picked from commit ee92768a978a4185afab47fa716bc943de0bbbe5)

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteMarshallerCacheFSRestoreTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteMarshallerCacheFSRestoreTest.java
@@ -103,7 +103,14 @@ public class IgniteMarshallerCacheFSRestoreTest extends GridCommonAbstractTest {
     @Override protected void afterTest() throws Exception {
         cleanUpWorkDir();
 
+        cleanPersistenceDir();
+
         stopAllGrids();
+    }
+
+    /** {@inheritDoc} */
+    @Override protected void beforeTest() throws Exception {
+        cleanPersistenceDir();
     }
 
     /**
