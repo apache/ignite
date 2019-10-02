12117641ab8aaea14e9569c665fec34ca6b6690a
Pavel Pereslegin <xxtern@gmail.com>
2019-10-02 11:18:58 +0300

    IGNITE-12181 Skip WAL rebalancing case for in-memory cache.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/IgnitePdsAtomicCacheHistoricalRebalancingTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/IgnitePdsAtomicCacheHistoricalRebalancingTest.java
@@ -20,6 +20,8 @@ package org.apache.ignite.internal.processors.cache.persistence;
 import org.apache.ignite.IgniteSystemProperties;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.internal.processors.cache.persistence.db.wal.IgniteWalRebalanceTest;
+import org.junit.Ignore;
+import org.junit.Test;
 
 /**
  *
@@ -60,5 +62,12 @@ public class IgnitePdsAtomicCacheHistoricalRebalancingTest extends IgnitePdsAtom
 
         if (!walRebalanceInvoked)
             throw new AssertionError("WAL rebalance hasn't been invoked.");
+    }
+
+    /** {@inheritDoc} */
+    @Test
+    @Ignore
+    @Override public void testTopologyChangesWithConstantLoadOnInMemoryCache() {
+        // No-op.
     }
 }
