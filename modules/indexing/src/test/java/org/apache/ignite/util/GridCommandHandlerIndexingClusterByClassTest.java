4f738c5d492c5291e50fcd615e9839749cc1e1c2
ktkalenko <ktkalenko@gridgain.com>
2019-10-03 12:09:17 +0300

    GG-24353 Extend test coverage for GG-20185
    (cherry picked from commit df3ab6f1b09236a5999be97550fb3ce8e1e3340e)
    (cherry picked from commit a0c72e4c7129acd09bff11608c2a860954f886d5)

--- modules/indexing/src/test/java/org/apache/ignite/util/GridCommandHandlerIndexingClusterByClassTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/util/GridCommandHandlerIndexingClusterByClassTest.java
@@ -109,6 +109,24 @@ public class GridCommandHandlerIndexingClusterByClassTest extends GridCommandHan
     }
 
     /**
+     * Checks that missing lines were detected in CacheDataTree with the output
+     * of cache group name and id.
+     */
+    public void testBrokenCacheDataTreeShouldFailValidationWithCacheGroupInfo() {
+        breakCacheDataTree(crd, CACHE_NAME, 1);
+
+        injectTestSystemOut();
+
+        assertEquals(EXIT_CODE_OK, execute("--cache", "validate_indexes", CACHE_NAME));
+
+        assertContains(
+            log,
+            testOut.toString(),
+            "[cacheGroup=group1, cacheGroupId=-1237460590, cache=persons-cache-vi, cacheId=-528791027, idx=_key_PK]"
+        );
+    }
+
+    /**
      * Tests that missing rows in H2 indexes are detected.
      */
     public void testBrokenSqlIndexShouldFailValidation() throws Exception {
