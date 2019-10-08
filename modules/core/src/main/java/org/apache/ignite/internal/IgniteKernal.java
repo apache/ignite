ee2e421212feaddf8d63f2d0b461d2eff4f63103
Mikhail <pmgheap.sbt@gmail.com>
2019-10-08 12:21:44 +0300

    IGNITE-12268 Adds ability to set up custom REST processor.

--- modules/core/src/main/java/org/apache/ignite/internal/IgniteKernal.java
+++ modules/core/src/main/java/org/apache/ignite/internal/IgniteKernal.java
@@ -173,6 +173,7 @@ import org.apache.ignite.internal.processors.query.GridQueryProcessor;
 import org.apache.ignite.internal.processors.resource.GridResourceProcessor;
 import org.apache.ignite.internal.processors.resource.GridSpringResourceContext;
 import org.apache.ignite.internal.processors.rest.GridRestProcessor;
+import org.apache.ignite.internal.processors.rest.IgniteRestProcessor;
 import org.apache.ignite.internal.processors.security.GridSecurityProcessor;
 import org.apache.ignite.internal.processors.security.IgniteSecurity;
 import org.apache.ignite.internal.processors.security.IgniteSecurityProcessor;
@@ -1215,7 +1216,7 @@ public class IgniteKernal implements IgniteEx, IgniteMXBean, Externalizable {
                 startProcessor(new GridJobProcessor(ctx));
                 startProcessor(new GridTaskProcessor(ctx));
                 startProcessor((GridProcessor)SCHEDULE.createOptional(ctx));
-                startProcessor(new GridRestProcessor(ctx));
+                startProcessor(createComponent(IgniteRestProcessor.class, ctx));
                 startProcessor(new DataStreamProcessor(ctx));
                 startProcessor((GridProcessor)IGFS.create(ctx, F.isEmpty(cfg.getFileSystemConfiguration())));
                 startProcessor(new GridContinuousProcessor(ctx));
@@ -4394,6 +4395,9 @@ public class IgniteKernal implements IgniteEx, IgniteMXBean, Externalizable {
 
         if(cls.equals(GridSecurityProcessor.class))
             return null;
+
+        if (cls.equals(IgniteRestProcessor.class))
+            return (T)new GridRestProcessor(ctx);
 
         Class<T> implCls = null;
 
