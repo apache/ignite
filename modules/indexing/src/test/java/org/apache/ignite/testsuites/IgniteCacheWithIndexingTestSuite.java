703c28ce262016d6ab960defbdadbf8b0cd05dab
denis-chudov <dchudov@gridgain.com>
2019-10-09 16:58:11 +0300

    GG-23809 Failing test RebuildIndexLogMessageTest on ignite-2.5-master

--- modules/indexing/src/test/java/org/apache/ignite/testsuites/IgniteCacheWithIndexingTestSuite.java
+++ modules/indexing/src/test/java/org/apache/ignite/testsuites/IgniteCacheWithIndexingTestSuite.java
@@ -37,7 +37,6 @@ import org.apache.ignite.internal.processors.cache.IgniteCacheGroupsSqlTest;
 import org.apache.ignite.internal.processors.cache.IgniteCacheStarvationOnRebalanceTest;
 import org.apache.ignite.internal.processors.cache.IgniteClientReconnectQueriesTest;
 import org.apache.ignite.internal.processors.cache.index.H2TreeCorruptedTreeExceptionTest;
-import org.apache.ignite.internal.processors.cache.persistence.RebuildIndexLogMessageTest;
 import org.apache.ignite.internal.processors.cache.ttl.CacheTtlAtomicLocalSelfTest;
 import org.apache.ignite.internal.processors.cache.ttl.CacheTtlAtomicPartitionedSelfTest;
 import org.apache.ignite.internal.processors.cache.ttl.CacheTtlReadOnlyModeSelfTest;
@@ -94,8 +93,6 @@ public class IgniteCacheWithIndexingTestSuite extends TestSuite {
         suite.addTestSuite(BinaryTypeRegistrationTest.class);
 
         suite.addTestSuite(ClusterReadOnlyModeSqlTest.class);
-
-        suite.addTestSuite(RebuildIndexLogMessageTest.class);
 
         suite.addTestSuite(CacheTtlReadOnlyModeSelfTest.class);
 
