ed6eb7e6440b45998b5b5c2df094d7711bc3a7b7
Anton Vinogradov <av@apache.org>
2019-11-05 09:24:35 +0300

    IGNITE-9913 Wip
    Signed-off-by: Anton Vinogradov <av@apache.org>

--- modules/core/src/main/java/org/apache/ignite/internal/processors/affinity/GridAffinityAssignmentCache.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/affinity/GridAffinityAssignmentCache.java
@@ -24,7 +24,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentMap;
