bdb225c90248da6a8cd500e1e84d1e0412d53511
Aleksandr Shapkin <ashapkin@gridgain.com>
2019-11-08 18:54:02 +0300

    IGNITE-9033 .NET Thin Client: add cache expiry policies
    - Add `CacheClientConfiguration.ExpiryPolicyFactory`
    - Add `ICacheClient.WithExpiryPolicy`

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Client/ClientSocket.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Client/ClientSocket.cs
@@ -53,8 +53,16 @@ namespace Apache.Ignite.Core.Impl.Client
         /** Version 1.4.0. */
         public static readonly ClientProtocolVersion Ver140 = new ClientProtocolVersion(1, 4, 0);
 
+        /** Version 1.5.0. */
+        // This version is reserved for IEP-34 Thin client: transactions support
+        // ReSharper disable once UnusedMember.Global
+        public static readonly ClientProtocolVersion Ver150 = new ClientProtocolVersion(1, 5, 0);
+
+        /** Version 1.6.0. */
+        public static readonly ClientProtocolVersion Ver160 = new ClientProtocolVersion(1, 6, 0);
+
         /** Current version. */
-        public static readonly ClientProtocolVersion CurrentProtocolVersion = Ver140;
+        public static readonly ClientProtocolVersion CurrentProtocolVersion = Ver160;
 
         /** Handshake opcode. */
         private const byte OpHandshake = 1;
