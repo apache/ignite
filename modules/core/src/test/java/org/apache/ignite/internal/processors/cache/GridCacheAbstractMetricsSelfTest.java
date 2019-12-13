f6e0c73b3f2340a791a0c34fa1c72c51d0dc3a74
NSAmelchev <nsamelchev@gmail.com>
2019-12-13 21:22:18 +0300

    Add commitTime, rollbackTime metrics

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/GridCacheAbstractMetricsSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/GridCacheAbstractMetricsSelfTest.java
@@ -1460,6 +1460,48 @@ public abstract class GridCacheAbstractMetricsSelfTest extends GridCacheAbstract
         assertEquals(1, Arrays.stream(m.value()).filter(v -> v == 1).count());
     }
 
+    /** */
+    @Test
+    public void testCommitTime() {
+        IgniteCache<String, Integer> cache = grid(0).cache(DEFAULT_CACHE_NAME);
+
+        if (cache.getConfiguration(CacheConfiguration.class).getAtomicityMode() == CacheAtomicityMode.ATOMIC)
+            return;
+
+        HistogramMetric m = metric("CommitTime");
+
+        assertTrue(Arrays.toString(m.value()), Arrays.stream(m.value()).allMatch(v -> v == 0));
+
+        try (Transaction tx = grid(0).transactions().txStart()) {
+            cache.put(String.valueOf(1), 1);
+
+            tx.commit();
+        }
+
+        assertEquals(1, Arrays.stream(m.value()).filter(v -> v == 1).count());
+    }
+
+    /** */
+    @Test
+    public void testRollbackTime() {
+        IgniteCache<String, Integer> cache = grid(0).cache(DEFAULT_CACHE_NAME);
+
+        if (cache.getConfiguration(CacheConfiguration.class).getAtomicityMode() == CacheAtomicityMode.ATOMIC)
+            return;
+
+        HistogramMetric m = metric("RollbackTime");
+
+        assertTrue(Arrays.stream(m.value()).allMatch(v -> v == 0));
+
+        try (Transaction tx = grid(0).transactions().txStart()) {
+            cache.put(String.valueOf(1), 1);
+
+            tx.rollback();
+        }
+
+        assertEquals(1, Arrays.stream(m.value()).filter(v -> v == 1).count());
+    }
+
     /**
      * @param name Metric name to find.
      * @return Metric.
