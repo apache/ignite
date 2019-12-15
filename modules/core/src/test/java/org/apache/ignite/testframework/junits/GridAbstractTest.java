174c323304478b6ba5c0ddb786af3e00fc440d90
ipavlukhin <vololo100@gmail.com>
2019-12-15 18:50:47 +0300

    Try to remove config "optimization" in GridAbstractTest

--- modules/core/src/test/java/org/apache/ignite/testframework/junits/GridAbstractTest.java
+++ modules/core/src/test/java/org/apache/ignite/testframework/junits/GridAbstractTest.java
@@ -980,7 +980,7 @@ public abstract class GridAbstractTest extends JUnitAssertAware {
                     }
                 }
 
-                Ignite node = IgnitionEx.start(optimize(cfg), ctx);
+                Ignite node = IgnitionEx.start(cfg, ctx);
 
                 IgniteConfiguration nodeCfg = node.configuration();
 
