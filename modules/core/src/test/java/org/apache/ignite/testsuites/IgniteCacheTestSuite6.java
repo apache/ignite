98df34178f6fbcb610ea2d594f3699860d8b755b
Aleksei Scherbakov <alexey.scherbakoff@gmail.com>
2019-12-16 11:56:04 +0300

    GG-26204 Fixed tx recovery protocol behavior to prevent a race with concurrent rollback.
    (cherry picked from commit ae6708979131513480002c1cdf99d339839da584)

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheTestSuite6.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheTestSuite6.java
@@ -45,6 +45,7 @@ import org.apache.ignite.internal.processors.cache.transactions.TxOnCachesStartT
 import org.apache.ignite.internal.processors.cache.transactions.TxOnCachesStopTest;
 import org.apache.ignite.internal.processors.cache.transactions.TxOptimisticOnPartitionExchangeTest;
 import org.apache.ignite.internal.processors.cache.transactions.TxOptimisticPrepareOnUnstableTopologyTest;
+import org.apache.ignite.internal.processors.cache.transactions.TxRecoveryWithConcurrentRollbackTest;
 import org.apache.ignite.internal.processors.cache.transactions.TxRollbackAsyncNearCacheTest;
 import org.apache.ignite.internal.processors.cache.transactions.TxRollbackAsyncTest;
 import org.apache.ignite.internal.processors.cache.transactions.TxRollbackOnMapOnInvalidTopologyTest;
@@ -129,7 +130,9 @@ public class IgniteCacheTestSuite6 extends TestSuite {
         //suite.addTestSuite(IgniteCacheClientMultiNodeUpdateTopologyLockTest.class);
 
         suite.addTestSuite(TxRollbackOnMapOnInvalidTopologyTest.class);
-        
+
+        suite.addTestSuite(TxRecoveryWithConcurrentRollbackTest.class);
+
         return suite;
     }
 }
