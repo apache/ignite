006b91acc4ef63b3299c114f958a1d5fef20caf2
Sergey Antonov <antonovsergey93@gmail.com>
2020-01-09 16:14:17 +0300

    IGNITE-12225 Add enum for cluster state - Fixes #7194.
    Signed-off-by: Ivan Rakov <irakov@apache.org>

--- modules/indexing/src/test/java/org/apache/ignite/testsuites/IgniteCacheWithIndexingTestSuite.java
+++ modules/indexing/src/test/java/org/apache/ignite/testsuites/IgniteCacheWithIndexingTestSuite.java
@@ -30,6 +30,7 @@ import org.apache.ignite.internal.processors.cache.CacheQueryFilterExpiredTest;
 import org.apache.ignite.internal.processors.cache.CacheRandomOperationsMultithreadedTest;
 import org.apache.ignite.internal.processors.cache.CacheRegisterMetadataLocallyTest;
 import org.apache.ignite.internal.processors.cache.ClientReconnectAfterClusterRestartTest;
+import org.apache.ignite.internal.processors.cache.ClusterReadOnlyModeDoesNotBreakSqlSelectTest;
 import org.apache.ignite.internal.processors.cache.ClusterReadOnlyModeSqlTest;
 import org.apache.ignite.internal.processors.cache.GridCacheOffHeapSelfTest;
 import org.apache.ignite.internal.processors.cache.GridCacheOffheapIndexEntryEvictTest;
@@ -96,6 +97,8 @@ import org.junit.runners.Suite;
     BinaryTypeRegistrationTest.class,
 
     ClusterReadOnlyModeSqlTest.class,
+
+    ClusterReadOnlyModeDoesNotBreakSqlSelectTest.class,
 
     CacheGroupMetricsWithIndexTest.class,
     CacheGroupMetricsWithIndexBuildFailTest.class,
