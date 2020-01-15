2f49fc899e0dbbb31ecd8bf9b3c4fa54641d20bd
d.garus <garus.d.g@gmail.com>
2020-01-15 10:44:47 +0300

    IGNITE-12342 fix comment

--- modules/core/src/test/java/org/apache/ignite/internal/processors/security/AbstractRemoteSecurityContextCheckTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/security/AbstractRemoteSecurityContextCheckTest.java
@@ -256,13 +256,6 @@ public abstract class AbstractRemoteSecurityContextCheckTest extends AbstractSec
         }
 
         /** */
-        private Verifier expectSubjId(UUID expSecSubjId) {
-            this.expSecSubjId = expSecSubjId;
-
-            return this;
-        }
-
-        /** */
         public Verifier initiator(IgniteEx initiator) {
             expSecSubjId = secSubjectId(initiator);
 
