bde6381a9cfc8238a7eaf6ddb2cce39f2b76801c
gkatzioura <gkatzioura@gmail.com>
2020-01-19 14:29:46 +0000

    Changed terraform script for elb deployment

--- modules/aws/deployment/elbdeployment/output.tf
+++
@@ -1,4 +0,0 @@
-output "subnet_names" {
-  value = [for s in data.aws_subnet.subnet_values: s.id]
-}
-
--- modules/aws/deployment/elbdeployment/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  version = "~> 2.0"
-  region  = "eu-west-1"
-}
-
-terraform {
-  required_version = "~>0.12.0"
-}
