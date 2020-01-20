f4a42b7d4d70d55ae8b391cc91cf52e874f32c0a
Alexey Goncharuk <alexey.goncharuk@gmail.com>
2020-01-20 16:35:47 +0300

    IGNITE-12463 Fix failed compilation after concurrent merge

--- modules/compatibility/src/test/java/org/apache/ignite/compatibility/persistence/MetaStorageCompatibilityTest.java
+++ modules/compatibility/src/test/java/org/apache/ignite/compatibility/persistence/MetaStorageCompatibilityTest.java
@@ -29,6 +29,7 @@ import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.internal.IgniteEx;
 import org.apache.ignite.internal.IgnitionEx;
 import org.apache.ignite.internal.processors.cache.GridCacheAbstractFullApiSelfTest;
+import org.apache.ignite.internal.processors.cache.persistence.CheckpointState;
 import org.apache.ignite.internal.processors.cache.persistence.GridCacheDatabaseSharedManager;
 import org.apache.ignite.internal.processors.cache.persistence.file.FileIO;
 import org.apache.ignite.internal.processors.cache.persistence.file.FileIODecorator;
@@ -264,7 +265,7 @@ public class MetaStorageCompatibilityTest extends IgnitePersistenceCompatibility
                 ignite.active(true);
 
                 ((IgniteEx)ignite).context().cache().context().database()
-                    .forceCheckpoint("force test checkpoint").finishFuture().get();
+                    .forceCheckpoint("force test checkpoint").futureFor(CheckpointState.FINISHED).get();
 
                 ((GridCacheDatabaseSharedManager)(((IgniteEx)ignite).context().cache().context().database()))
                     .enableCheckpoints(false);
