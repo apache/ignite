f7c954c2739351dd31a96721545237068eb9e696
Kirill Tkalenko <tkalkirill@yandex.ru>
2020-01-22 12:24:25 +0300

    IGNITE-12497 PartitionsEvictManager should log all partitions which will be evicted - Fixes #7202.
    Signed-off-by: Ivan Rakov <irakov@apache.org>

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteBasicTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteBasicTestSuite.java
@@ -19,6 +19,7 @@ package org.apache.ignite.testsuites;
 
 import org.apache.ignite.ClassPathContentLoggingTest;
 import org.apache.ignite.GridSuppressedExceptionSelfTest;
+import org.apache.ignite.internal.processors.cache.distributed.dht.topology.EvictPartitionInLogTest;
 import org.apache.ignite.internal.processors.failure.FailureProcessorLoggingTest;
 import org.apache.ignite.failure.FailureHandlerTriggeredTest;
 import org.apache.ignite.failure.OomFailureHandlerTest;
@@ -187,6 +188,7 @@ import org.junit.runners.Suite;
 
     PartitionsEvictionTaskFailureHandlerTest.class,
     DropCacheContextDuringEvictionTest.class,
+    EvictPartitionInLogTest.class,
 
     IgniteExceptionInNioWorkerSelfTest.class,
     IgniteLocalNodeMapBeforeStartTest.class,
