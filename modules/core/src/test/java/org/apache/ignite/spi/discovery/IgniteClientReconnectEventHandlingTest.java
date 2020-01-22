2f04f0d1e7f04e7ed3aa4554f68aa0c726aae3a5
Andrei Aleksandrov <aealexsandrov@gmail.com>
2020-01-22 15:50:50 +0300

    Copyrights 2019 -> 2020

--- modules/core/src/test/java/org/apache/ignite/spi/discovery/IgniteClientReconnectEventHandlingTest.java
+++ modules/core/src/test/java/org/apache/ignite/spi/discovery/IgniteClientReconnectEventHandlingTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 GridGain Systems, Inc. and Contributors.
+ * Copyright 2020 GridGain Systems, Inc. and Contributors.
  *
  * Licensed under the GridGain Community Edition License (the "License");
  * you may not use this file except in compliance with the License.
