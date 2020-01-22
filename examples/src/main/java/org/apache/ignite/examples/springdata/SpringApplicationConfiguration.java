2385cbb5d9cddef156c5f5ea5e404a6914692e3f
Maxim Muzafarov <mmuzaf@apache.org>
2020-01-22 16:11:01 +0300

    IGNITE-8641: Spring data example now uses its own xml config (#7280)

--- examples/src/main/java/org/apache/ignite/examples/springdata/SpringApplicationConfiguration.java
+++ examples/src/main/java/org/apache/ignite/examples/springdata/SpringApplicationConfiguration.java
@@ -19,9 +19,7 @@ package org.apache.ignite.examples.springdata;
 
 import org.apache.ignite.Ignite;
 import org.apache.ignite.Ignition;
-import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
-import org.apache.ignite.examples.model.Person;
 import org.apache.ignite.springdata20.repository.IgniteRepository;
 import org.apache.ignite.springdata20.repository.config.EnableIgniteRepositories;
 import org.apache.ignite.springdata20.repository.support.IgniteRepositoryFactoryBean;
@@ -41,29 +39,13 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @EnableIgniteRepositories
-public class SpringAppCfg {
+public class SpringApplicationConfiguration {
     /**
      * Creating Apache Ignite instance bean. A bean will be passed to {@link IgniteRepositoryFactoryBean} to initialize
      * all Ignite based Spring Data repositories and connect to a cluster.
      */
     @Bean
     public Ignite igniteInstance() {
-        IgniteConfiguration cfg = new IgniteConfiguration();
-
-        // Setting some custom name for the node.
-        cfg.setIgniteInstanceName("springDataNode");
-
-        // Enabling peer-class loading feature.
-        cfg.setPeerClassLoadingEnabled(true);
-
-        // Defining and creating a new cache to be used by Ignite Spring Data repository.
-        CacheConfiguration ccfg = new CacheConfiguration("PersonCache");
-
-        // Setting SQL schema for the cache.
-        ccfg.setIndexedTypes(Long.class, Person.class);
-
-        cfg.setCacheConfiguration(ccfg);
-
-        return Ignition.start(cfg);
+        return Ignition.start("examples/config/spring/example-spring-data.xml");
     }
 }
