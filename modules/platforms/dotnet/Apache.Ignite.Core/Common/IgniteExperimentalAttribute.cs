ecfaf582ad549e52d9639de38cf9ad3c5d9315c9
Alexey Goncharuk <alexey.goncharuk@gmail.com>
2020-01-23 15:39:02 +0300

    IGNITE-12559 Add @IgniteExperimental annotation

--- modules/platforms/dotnet/Apache.Ignite.Core/Common/IgniteExperimentalAttribute.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Common/IgniteExperimentalAttribute.cs
@@ -29,7 +29,7 @@ namespace Apache.Ignite.Core.Common
     /// (both minor and maintenance).
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class IgniteExperimentalAttribute : Attribute
+    public sealed class IgniteExperimentalAttribute : Attribute
     {
         // No-op.
     }
