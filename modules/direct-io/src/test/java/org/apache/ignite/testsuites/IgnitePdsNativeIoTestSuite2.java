93af1dcc703bfc8c6abb465cd52bc1ce56f6f843
Nikolay Izhikov <nizhikov@apache.org>
2020-01-28 15:09:30 +0300

    IGNITE-12580: Failing test repeated run

--- modules/direct-io/src/test/java/org/apache/ignite/testsuites/IgnitePdsNativeIoTestSuite2.java
+++ modules/direct-io/src/test/java/org/apache/ignite/testsuites/IgnitePdsNativeIoTestSuite2.java
@@ -19,7 +19,6 @@ package org.apache.ignite.testsuites;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.ignite.internal.processors.cache.persistence.DiskPageCompressionIntegrationDirectIOTest;
-import org.apache.ignite.internal.processors.cache.persistence.IgniteNativeIoLocalWalModeChangeDuringRebalancingSelfTest;
 import org.apache.ignite.internal.processors.cache.persistence.IgniteNativeIoPdsRecoveryAfterFileCorruptionTest;
 import org.apache.ignite.internal.processors.cache.persistence.db.wal.IgniteNativeIoWalFlushFsyncSelfTest;
 import org.apache.ignite.testframework.junits.DynamicSuite;
@@ -43,8 +42,6 @@ public class IgnitePdsNativeIoTestSuite2 {
 
         //Integrity test with reduced count of pages.
         suite.add(IgniteNativeIoPdsRecoveryAfterFileCorruptionTest.class);
-
-        suite.add(IgniteNativeIoLocalWalModeChangeDuringRebalancingSelfTest.class);
 
         suite.add(IgniteNativeIoWalFlushFsyncSelfTest.class);
 
