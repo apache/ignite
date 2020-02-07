06df297db9f9008141df04b42f49989740379931
Anton Kalashnikov <kaa.dev@yandex.ru>
2020-02-07 19:23:46 +0300

    IGNITE-12631 Incorrect rewriting wal record type in marshalled mode during iteration - Fixes #7371.
    Signed-off-by: Ivan Rakov <irakov@apache.org>

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/serializer/RecordV1Serializer.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/serializer/RecordV1Serializer.java
@@ -386,10 +386,13 @@ public class RecordV1Serializer implements RecordSerializer {
                 size = in0.io().size();
             }
             catch (IOException ignore) {
-                // No-op. It just for information. Fail calculate file size.
+                // It just for information. Fail calculate file size.
+                e.addSuppressed(ignore);
             }
 
-            throw new IgniteCheckedException("Failed to read WAL record at position: " + startPos + " size: " + size, e);
+            throw new IgniteCheckedException(
+                "Failed to read WAL record at position: " + startPos + ", size: " + size + ", expectedPtr: " + expPtr, e
+            );
         }
     }
 
