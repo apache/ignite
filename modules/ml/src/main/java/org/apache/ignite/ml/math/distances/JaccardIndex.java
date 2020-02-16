bafb8dc21ac5dc20ff627eb6fb0474c409a54f96
Ravil Galeyev <dehasi@yandex.ru>
2020-02-16 19:20:05 +0100

    IGNITE-12383 Rename set

--- modules/ml/src/main/java/org/apache/ignite/ml/math/distances/JaccardIndex.java
+++ modules/ml/src/main/java/org/apache/ignite/ml/math/distances/JaccardIndex.java
@@ -27,13 +27,13 @@ import org.apache.ignite.ml.math.primitives.vector.Vector;
 public class JaccardIndex implements DistanceMeasure {
     @Override
     public double compute(Vector a, Vector b) throws CardinalityException {
-        Set<Double> aSet = new HashSet<>();
+        Set<Double> uniqueValues = new HashSet<>();
         for (int i = 0; i < a.size(); i++)
-            aSet.add(a.get(i));
+            uniqueValues.add(a.get(i));
 
         double intersect = 0;
         for (int i = 0; i < b.size(); i++)
-            if (aSet.contains(b.get(i)))
+            if (uniqueValues.contains(b.get(i)))
                 ++intersect;
 
         return intersect / (a.size() + b.size() - intersect);
