e6b7973d0c22117dbe693d0dc791522274379eeb
Alexey Goncharuk <alexey.goncharuk@gmail.com>
2020-02-17 08:53:07 +0300

    IGNITE-12650 Mark MVCC with @IgniteExperimental - Fixes #7419.
    Signed-off-by: Alexey Goncharuk <alexey.goncharuk@gmail.com>

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/CacheExchangeMergeTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/CacheExchangeMergeTest.java
@@ -1292,9 +1292,8 @@ public class CacheExchangeMergeTest extends GridCommonAbstractTest {
     }
 
     /**
-     * @throws Exception If failed.
      */
-    private void checkAffinity() throws Exception {
+    private void checkAffinity() {
         List<Ignite> nodes = G.allGrids();
 
         ClusterNode crdNode = null;
@@ -1306,7 +1305,7 @@ public class CacheExchangeMergeTest extends GridCommonAbstractTest {
                 crdNode = locNode;
         }
 
-        AffinityTopologyVersion topVer = ((IgniteKernal)grid(crdNode)).
+        AffinityTopologyVersion topVer = ((IgniteEx)grid(crdNode)).
             context().cache().context().exchange().readyAffinityVersion();
 
         Map<String, List<List<ClusterNode>>> affMap = new HashMap<>();
@@ -1314,7 +1313,7 @@ public class CacheExchangeMergeTest extends GridCommonAbstractTest {
         for (Ignite node : nodes) {
             IgniteKernal node0 = (IgniteKernal)node;
 
-            for (IgniteInternalCache cache : node0.context().cache().caches()) {
+            for (IgniteInternalCache<?, ?> cache : node0.context().cache().caches()) {
                 List<List<ClusterNode>> aff = affMap.get(cache.name());
                 List<List<ClusterNode>> aff0 = cache.context().affinity().assignments(topVer);
 
