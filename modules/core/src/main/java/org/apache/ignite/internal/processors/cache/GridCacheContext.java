2306bec0419816853a8a14ec390bcaa5019e2341
ktkalenko <ktkalenko@gridgain.com>
2020-02-18 17:39:26 +0300

    GG-27598 Fix javadoc warn test

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheContext.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheContext.java
@@ -2341,7 +2341,9 @@ public class GridCacheContext<K, V> implements Externalizable {
     }
 
     /**
-     * Returns future that assigned to last performing {@link GridDistributedCacheAdapter.GlobalRemoveAllJob}
+     * Returns future that assigned to last performing {@code GlobalRemoveAllJob}.
+     *
+     * @return Future that assigned to last performing {@code GlobalRemoveAllJob}.
      */
     public AtomicReference<IgniteInternalFuture<Boolean>> lastRemoveAllJobFut() {
         return lastRmvAllJobFut;
