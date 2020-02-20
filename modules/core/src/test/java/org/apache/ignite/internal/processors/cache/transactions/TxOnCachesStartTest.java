414ee9a205cd309f6a30da701cda1e0143ca23cb
zstan <stanilovsky@gmail.com>
2020-02-20 09:39:12 +0300

    GG-27704 TxOnCachesStartTest#testTransactionCloseOnCachesStartAndStop test muted.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/transactions/TxOnCachesStartTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/transactions/TxOnCachesStartTest.java
@@ -74,6 +74,8 @@ public class TxOnCachesStartTest extends GridCommonAbstractTest {
      * @throws Exception If failed.
      */
     public void testTransactionCloseOnCachesStartAndStop() throws Exception {
+        fail("https://ggsystems.atlassian.net/browse/GG-27704");
+        
         Ignite srv =  startGrids(5);
 
         IgniteEx client1 = startGrid(getConfiguration("client-1").setClientMode(true));
