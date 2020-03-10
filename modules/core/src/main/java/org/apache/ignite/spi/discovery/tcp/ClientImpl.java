gridgain/gridgain

GG-27825 SoLinger does not set for reader-sockets in discovery

modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/ClientImpl.java
@@ -1148,8 +1148,9 @@ class ClientImpl extends TcpDiscoveryImpl {
                 try {
                     InputStream in = sockStream.stream();
 
-                    sock.setKeepAlive(true);
-                    sock.setTcpNoDelay(true);
+                    assert sock.getKeepAlive() && sock.getTcpNoDelay() : "Socket didn't configure properly:" +
+                        " KeepAlive " + sock.getKeepAlive() +
+                        " TcpNoDelay " + sock.getTcpNoDelay();
 
                     while (!isInterrupted()) {
                         TcpDiscoveryAbstractMessage msg;
@@ -1578,8 +1579,9 @@ class ClientImpl extends TcpDiscoveryImpl {
 
                         InputStream in = sockStream.stream();
 
-                        sock.setKeepAlive(true);
-                        sock.setTcpNoDelay(true);
+                        assert sock.getKeepAlive() && sock.getTcpNoDelay() : "Socket didn't configure properly:" +
+                            " KeepAlive " + sock.getKeepAlive() +
+                            " TcpNoDelay " + sock.getTcpNoDelay();
 
                         List<TcpDiscoveryAbstractMessage> msgs = null;
 

apache/ignite

(cherry picked from commit cbe082be24923c53f2734ab9bf327c36e1917050)

modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/ClientImpl.java
@@ -1116,8 +1116,9 @@ class ClientImpl extends TcpDiscoveryImpl {
                 try {
                     InputStream in = sockStream.stream();
 
-                    sock.setKeepAlive(true);
-                    sock.setTcpNoDelay(true);
+                    assert sock.getKeepAlive() && sock.getTcpNoDelay() : "Socket wasn't configured properly:" +
+                        " KeepAlive " + sock.getKeepAlive() +
+                        " TcpNoDelay " + sock.getTcpNoDelay();
 
                     while (!isInterrupted()) {
                         TcpDiscoveryAbstractMessage msg;
@@ -1546,8 +1547,9 @@ class ClientImpl extends TcpDiscoveryImpl {
 
                         InputStream in = sockStream.stream();
 
-                        sock.setKeepAlive(true);
-                        sock.setTcpNoDelay(true);
+                        assert sock.getKeepAlive() && sock.getTcpNoDelay() : "Socket wasn't configured properly:" +
+                            " KeepAlive " + sock.getKeepAlive() +
+                            " TcpNoDelay " + sock.getTcpNoDelay();
 
                         List<TcpDiscoveryAbstractMessage> msgs = null;
 