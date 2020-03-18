d38944d948b739c2962df0a60d45abb787528e38
Nikolay <nizhikov@apache.org>
2020-03-18 13:31:38 +0300

    IGNITE-12768: MetricRegistryMBean doesn't show histogram values in case when histogram name contains underscore character (#7542)

--- modules/core/src/main/java/org/apache/ignite/spi/metric/jmx/MetricRegistryMBean.java
+++ modules/core/src/main/java/org/apache/ignite/spi/metric/jmx/MetricRegistryMBean.java
@@ -22,7 +22,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 import javax.management.MBeanAttributeInfo;
 import javax.management.MBeanInfo;
 import org.apache.ignite.internal.processors.metric.impl.MetricUtils;
@@ -38,6 +37,7 @@ import org.apache.ignite.spi.metric.ReadOnlyMetricManager;
 import org.apache.ignite.spi.metric.ReadOnlyMetricRegistry;
 
 import static java.util.Arrays.binarySearch;
+import static org.apache.ignite.internal.processors.metric.impl.MetricUtils.HISTOGRAM_NAME_DIVIDER;
 import static org.apache.ignite.internal.processors.metric.impl.MetricUtils.INF;
 import static org.apache.ignite.internal.processors.metric.impl.MetricUtils.histogramBucketNames;
 
@@ -155,12 +155,25 @@ public class MetricRegistryMBean extends ReadOnlyDynamicMBean {
      * @see MetricUtils#histogramBucketNames(HistogramMetric, Map)
      */
     public static Long searchHistogram(String name, ReadOnlyMetricRegistry mreg) {
-        Scanner sc = new Scanner(name).useDelimiter("_");
+        int highBoundIdx;
 
-        if (!sc.hasNext())
+        boolean isInf = name.endsWith(INF);
+
+        if (isInf)
+            highBoundIdx = name.length() - 4;
+        else {
+            highBoundIdx = name.lastIndexOf(HISTOGRAM_NAME_DIVIDER);
+
+            if (highBoundIdx == -1)
+                return null;
+        }
+
+        int lowBoundIdx = name.lastIndexOf(HISTOGRAM_NAME_DIVIDER, highBoundIdx - 1);
+
+        if (lowBoundIdx == -1)
             return null;
 
-        Metric m = mreg.findMetric(sc.next());
+        Metric m = mreg.findMetric(name.substring(0, lowBoundIdx));
 
         if (!(m instanceof HistogramMetric))
             return null;
@@ -170,20 +183,30 @@ public class MetricRegistryMBean extends ReadOnlyDynamicMBean {
         long[] bounds = h.bounds();
         long[] values = h.value();
 
-        if (!sc.hasNextLong())
+        long lowBound;
+
+        try {
+            lowBound = Long.parseLong(name.substring(lowBoundIdx + 1, highBoundIdx));
+        }
+        catch (NumberFormatException e) {
             return null;
+        }
 
-        long lowBound = sc.nextLong();
-
-        //If `highBound` not presented this can be last interval `[max]_inf`.
-        if (!sc.hasNextLong()) {
-            if (sc.hasNext() && INF.equals(sc.next()) && bounds[bounds.length - 1] == lowBound)
+        if (isInf) {
+            if (bounds[bounds.length - 1] == lowBound)
                 return values[values.length - 1];
 
             return null;
         }
 
-        long highBound = sc.nextLong();
+        long highBound;
+
+        try {
+            highBound = Long.parseLong(name.substring(highBoundIdx + 1));
+        }
+        catch (NumberFormatException e) {
+            return null;
+        }
 
         int idx = binarySearch(bounds, highBound);
 
