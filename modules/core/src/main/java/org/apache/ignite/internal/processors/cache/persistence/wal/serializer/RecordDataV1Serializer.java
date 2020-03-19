e25176d02bb82e5dd87358e05c50310311fc10bf
ibessonov <bessonov.ip@gmail.com>
2020-03-19 09:40:46 +0300

    IGNITE-12789 Delta record handling moved to V2 serializer.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/serializer/RecordDataV1Serializer.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/serializer/RecordDataV1Serializer.java
@@ -90,7 +90,6 @@ import org.apache.ignite.internal.pagemem.wal.record.delta.RotatedIdPartRecord;
 import org.apache.ignite.internal.pagemem.wal.record.delta.SplitExistingPageRecord;
 import org.apache.ignite.internal.pagemem.wal.record.delta.SplitForwardPageRecord;
 import org.apache.ignite.internal.pagemem.wal.record.delta.TrackingPageDeltaRecord;
-import org.apache.ignite.internal.pagemem.wal.record.delta.TrackingPageRepairDeltaRecord;
 import org.apache.ignite.internal.processors.cache.CacheObject;
 import org.apache.ignite.internal.processors.cache.CacheObjectContext;
 import org.apache.ignite.internal.processors.cache.DynamicCacheDescriptor;
@@ -512,9 +511,6 @@ public class RecordDataV1Serializer implements RecordDataSerializer {
 
             case TRACKING_PAGE_DELTA:
                 return 4 + 8 + 8 + 8 + 8;
-
-            case TRACKING_PAGE_REPAIR_DELTA:
-                return 4 + 8;
 
             case META_PAGE_UPDATE_LAST_SUCCESSFUL_SNAPSHOT_ID:
                 return 4 + 8 + 8 + 8;
@@ -1106,14 +1102,6 @@ public class RecordDataV1Serializer implements RecordDataSerializer {
                 long lastSuccessfulSnapshotId0 = in.readLong();
 
                 res = new TrackingPageDeltaRecord(cacheId, pageId, pageIdToMark, nextSnapshotId0, lastSuccessfulSnapshotId0);
-
-                break;
-
-            case TRACKING_PAGE_REPAIR_DELTA:
-                cacheId = in.readInt();
-                pageId = in.readLong();
-
-                res = new TrackingPageRepairDeltaRecord(cacheId, pageId);
 
                 break;
 
@@ -1728,14 +1716,6 @@ public class RecordDataV1Serializer implements RecordDataSerializer {
                 buf.putLong(tpDelta.pageIdToMark());
                 buf.putLong(tpDelta.nextSnapshotTag());
                 buf.putLong(tpDelta.lastSuccessfulSnapshotTag());
-
-                break;
-
-            case TRACKING_PAGE_REPAIR_DELTA:
-                TrackingPageRepairDeltaRecord tprDelta = (TrackingPageRepairDeltaRecord)rec;
-
-                buf.putInt(tprDelta.groupId());
-                buf.putLong(tprDelta.pageId());
 
                 break;
 
