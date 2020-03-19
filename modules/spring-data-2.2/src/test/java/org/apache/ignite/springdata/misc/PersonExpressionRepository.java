167bbd08f3ab136c74f3b42ffa999e5100f394b3
Sergey Chernolyas <sergey.chernolyas@gmail.com>
2020-03-19 13:07:16 +0300

    IGNITE-12799 Fixed wrong SpEL expression.
    Signed-off-by: Slava Koptilin <slava.koptilin@gmail.com>
    (cherry picked from commit 3928bb3a13ed8cedd09344cc5554179c04dec6cd)

--- modules/spring-data-2.2/src/test/java/org/apache/ignite/springdata/misc/PersonExpressionRepository.java
+++ modules/spring-data-2.2/src/test/java/org/apache/ignite/springdata/misc/PersonExpressionRepository.java
@@ -23,6 +23,6 @@ import org.apache.ignite.springdata22.repository.config.RepositoryConfig;
 /**
  *
  */
-@RepositoryConfig(cacheName = "@cacheNames.personCacheName")
+@RepositoryConfig(cacheName = "#{cacheNames.personCacheName}")
 public interface PersonExpressionRepository extends IgniteRepository<Person, Integer> {
 }
