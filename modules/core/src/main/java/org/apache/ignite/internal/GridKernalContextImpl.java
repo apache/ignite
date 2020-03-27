d80f1557aad9f8a2101085cb26537aef6b9302fc
Sunny Chan <github@sunnychan.hk>
2020-03-27 18:19:15 +0800

    Make thread non-static

--- modules/core/src/main/java/org/apache/ignite/internal/GridKernalContextImpl.java
+++ modules/core/src/main/java/org/apache/ignite/internal/GridKernalContextImpl.java
@@ -1047,7 +1047,7 @@ public class GridKernalContextImpl implements GridKernalContext, Externalizable 
             comp.printMemoryStats();
     }
 
-    private static final boolean IS_IGNITE_DAEMON_SYSPROP_SET = IgniteSystemProperties.getBoolean(IGNITE_DAEMON);
+    private final boolean IS_IGNITE_DAEMON_SYSPROP_SET = IgniteSystemProperties.getBoolean(IGNITE_DAEMON);
 
     /** {@inheritDoc} */
     @Override public boolean isDaemon() {
