7d55653abc029c18d45a1fba9ab5a9f778f7c80d
Vladislav Pyatkov <vldpyatkov@gmail.com>
2020-04-02 13:34:08 +0300

    IGNITE-12818 SoLinger should be used for all sockets in discovery - Fixes #7557.
    Signed-off-by: Alexey Goncharuk <alexey.goncharuk@gmail.com>

--- modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/TcpDiscoverySpi.java
+++ modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/TcpDiscoverySpi.java
@@ -1572,9 +1572,7 @@ public class TcpDiscoverySpi extends IgniteSpiAdapter implements IgniteDiscovery
 
             sock.bind(new InetSocketAddress(locHost, 0));
 
-            sock.setTcpNoDelay(true);
-
-            sock.setSoLinger(getSoLinger() >= 0, getSoLinger());
+            configureSocketOptions(sock);
 
             return sock;
         } catch (IOException e) {
@@ -1628,6 +1626,20 @@ public class TcpDiscoverySpi extends IgniteSpiAdapter implements IgniteDiscovery
             if (!cancelled)
                 throw new SocketTimeoutException("Write timed out (socket was concurrently closed).");
         }
+    }
+
+    /**
+     * Configures socket options.
+     *
+     * @param sock Socket.
+     * @throws SocketException If failed.
+     */
+    void configureSocketOptions(Socket sock) throws SocketException {
+        sock.setTcpNoDelay(true);
+
+        sock.setSoLinger(getSoLinger() >= 0, getSoLinger());
+
+        sock.setKeepAlive(true);
     }
 
     /**
