1b51981c434c5fabe56996f7e3ed7f3cb7bacfe9
Sergey Chugunov <schugunov@gridgain.com>
2020-04-17 11:37:10 +0300

    IGNITE-8343 Skip IPv6 address if node is started in 'prefer IPv4' mode.
    Cherry-picked from ca40049107a

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteSpiCommunicationSelfTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteSpiCommunicationSelfTestSuite.java
@@ -44,6 +44,7 @@ import org.apache.ignite.spi.communication.tcp.TcpCommunicationSpiFaultyClientSs
 import org.apache.ignite.spi.communication.tcp.TcpCommunicationSpiFaultyClientTest;
 import org.apache.ignite.spi.communication.tcp.TcpCommunicationSpiFreezingClientTest;
 import org.apache.ignite.spi.communication.tcp.TcpCommunicationSpiHalfOpenedConnectionTest;
+import org.apache.ignite.spi.communication.tcp.TcpCommunicationSpiMultiJvmTest;
 import org.apache.ignite.spi.communication.tcp.TcpCommunicationSpiSkipMessageSendTest;
 import org.apache.ignite.spi.communication.tcp.TcpCommunicationStatisticsTest;
 import org.junit.runner.RunWith;
@@ -97,6 +98,7 @@ import org.junit.runners.Suite;
     IgniteTcpCommunicationHandshakeWaitSslTest.class,
     IgniteTcpCommunicationConnectOnInitTest.class,
 
+    TcpCommunicationSpiMultiJvmTest.class,
     //GridCacheDhtLockBackupSelfTest.class,
 })
 public class IgniteSpiCommunicationSelfTestSuite {
