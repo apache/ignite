b8af0b332d9cfa69f8a056dcdf629080c9738390
Maxim Muzafarov <maxmuzaf@gmail.com>
2020-04-25 01:50:26 +0300

    IGNITE-12393: support whitespace rule 11

--- modules/core/src/main/java/org/apache/ignite/internal/jdbc/thin/JdbcThinConnection.java
+++ modules/core/src/main/java/org/apache/ignite/internal/jdbc/thin/JdbcThinConnection.java
@@ -1520,10 +1520,10 @@ public class JdbcThinConnection implements Connection {
 
         long lsb = 0;
 
-        for (int i=0; i<8; i++)
+        for (int i = 0; i < 8; i++)
             msb = (msb << 8) | (randomBytes[i] & 0xff);
 
-        for (int i=8; i<16; i++)
+        for (int i = 8; i < 16; i++)
             lsb = (lsb << 8) | (randomBytes[i] & 0xff);
 
         UUID randomUUID =  new UUID(msb, lsb);
