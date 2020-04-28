9fe2a55b59480b276b33aae8502b872ea1fb0c4c
Maxim Muzafarov <maxmuzaf@gmail.com>
2020-04-28 20:40:54 +0300

    IGNITE-11073: creation of cluster snapshots

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/pagemem/PageMemoryImplNoLoadTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/pagemem/PageMemoryImplNoLoadTest.java
@@ -100,6 +100,7 @@ public class PageMemoryImplNoLoadTest extends PageMemoryNoLoadSelfTest {
             null,
             null,
             null,
+            null,
             null
         );
 
