dcf4e47c03c63c3838503bb10d4bb1491daa375d
Ilya Kasnacheev <ilya.kasnacheev@gmail.com>
2020-05-01 14:16:28 +0300

    Introduce a failing test scenario.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteCachePutKeyAttachedBinaryObjectTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteCachePutKeyAttachedBinaryObjectTest.java
@@ -17,8 +17,12 @@
 
 package org.apache.ignite.internal.processors.cache;
 
+import javax.cache.Cache.Entry;
 import org.apache.ignite.IgniteCache;
+import org.apache.ignite.binary.BinaryBasicNameMapper;
+import org.apache.ignite.binary.BinaryObject;
 import org.apache.ignite.binary.BinaryObjectBuilder;
+import org.apache.ignite.configuration.BinaryConfiguration;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.failure.StopNodeFailureHandler;
@@ -37,12 +41,14 @@ public class IgniteCachePutKeyAttachedBinaryObjectTest extends GridCommonAbstrac
     @Override protected IgniteConfiguration getConfiguration(String gridName) throws Exception {
         return super.getConfiguration(gridName)
             .setFailureHandler(new StopNodeFailureHandler())
-            .setCacheConfiguration(new CacheConfiguration<>(CACHE_NAME));
+            .setCacheConfiguration(new CacheConfiguration<>(CACHE_NAME))
+            // For brevity, not needed to reproduce the issue.
+            .setBinaryConfiguration(new BinaryConfiguration().setNameMapper(new BinaryBasicNameMapper(true)));
     }
 
     /** {@inheritDoc} */
-    @Override protected void afterTestsStopped() throws Exception {
-        super.afterTestsStopped();
+    @Override protected void afterTest() throws Exception {
+        super.afterTest();
 
         stopAllGrids();
     }
@@ -76,23 +82,99 @@ public class IgniteCachePutKeyAttachedBinaryObjectTest extends GridCommonAbstrac
     }
 
     /**
-     *
+     * @throws Exception If failed.
      */
+    @Test
+    public void testKeyRefersToSubkeyInValue() throws Exception {
+        startGrid(0);
+
+        IgniteCache<CompositeKey, CompositeValue> cache = grid(0).cache(CACHE_NAME);
+
+        SubKey subKey = new SubKey(10);
+        CompositeKey compositeKey = new CompositeKey(20, subKey);
+        CompositeValue compositeVal = new CompositeValue("foo", subKey, compositeKey);
+
+        cache.put(compositeKey, compositeVal);
+
+        IgniteCache<Object, Object> binCache = cache.withKeepBinary();
+
+        BinaryObject binObj = (BinaryObject)binCache.get(compositeKey);
+
+        binCache.put(binObj.field("key"), binObj.toBuilder().setField("val", "bar").build());
+
+        // For debugging purpose only, may be removed after test is fixed.
+        for (Entry e : binCache)
+            log.info("Entry: " + e);
+
+        assertEquals("bar", cache.get(compositeKey).val());
+
+        assertEquals(1, cache.size());
+    }
+
+    /** */
     public static class AttachedKey {
         /** */
         public int id;
 
-        /** **/
+        /** */
         public AttachedKey(int id) {
             this.id = id;
         }
     }
 
-    /**
-     *
-     */
+    /** */
     public static class HolderKey {
         /** */
         public AttachedKey key;
+    }
+
+    /** */
+    public static class SubKey {
+        /** */
+        private int subId;
+
+        /** */
+        public SubKey(int subId) {
+            this.subId = subId;
+        }
+    }
+
+    public static class CompositeKey {
+        /** */
+        private int id;
+
+        /** */
+        private SubKey subKey;
+
+        /** */
+        public CompositeKey(int id, SubKey subKey) {
+            this.id = id;
+            this.subKey = subKey;
+        }
+    }
+
+
+    /** */
+    public static class CompositeValue {
+        /** */
+        private String val;
+
+        /** */
+        private SubKey subKey;
+
+        /** */
+        private CompositeKey key;
+
+        /** */
+        public CompositeValue(String val, SubKey subKey, CompositeKey key) {
+            this.val = val;
+            this.subKey = subKey;
+            this.key = key;
+        }
+
+        /** */
+        public String val() {
+            return val;
+        }
     }
 }
