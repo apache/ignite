9d96ac0ac4b5b66f65f2a700264fe20734c202c5
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-05-03 18:20:29 +0300

    Add CLUSTER_GROUP_GET_NODES_ENDPOINTS feature flag

--- modules/core/src/main/java/org/apache/ignite/internal/processors/platform/client/ClientBitmaskFeature.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/platform/client/ClientBitmaskFeature.java
@@ -28,7 +28,10 @@ public enum ClientBitmaskFeature implements ThinProtocolFeature {
     USER_ATTRIBUTES(0),
 
     /** Compute tasks (execute by task name). */
-    EXECUTE_TASK_BY_NAME(1);
+    EXECUTE_TASK_BY_NAME(1),
+
+    /** Client discovery. */
+    CLUSTER_GROUP_GET_NODES_ENDPOINTS(2);
 
     /** */
     private static final EnumSet<ClientBitmaskFeature> ALL_FEATURES_AS_ENUM_SET =
