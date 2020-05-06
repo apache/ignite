ff2a2f0c20432fcd7476eebbb14857a043daa74a
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-05-06 18:52:10 +0300

    Adding tests

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Client/ClientFeaturesTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Client/ClientFeaturesTest.cs
@@ -18,6 +18,7 @@
 namespace Apache.Ignite.Core.Tests.Client
 {
     using System;
+    using System.Linq;
     using Apache.Ignite.Core.Impl.Client;
     using NUnit.Framework;
 
@@ -56,6 +57,19 @@ namespace Apache.Ignite.Core.Tests.Client
             Assert.AreEqual(ClientSocket.Ver150, ClientFeatures.GetMinVersion(ClientOp.ClusterChangeState));
             Assert.AreEqual(ClientSocket.Ver150, ClientFeatures.GetMinVersion(ClientOp.ClusterChangeWalState));
             Assert.AreEqual(ClientSocket.Ver150, ClientFeatures.GetMinVersion(ClientOp.ClusterGetWalState));
+        }
+
+        /// <summary>
+        /// Tests <see cref="ClientFeatures.AllFeatures"/>.
+        /// </summary>
+        [Test]
+        public void TestAllFeatures()
+        {
+            var expected = Enum.GetValues(typeof(ClientBitmaskFeature))
+                .Cast<int>()
+                .Aggregate(0, (a, b) => a | (1 << b));
+            
+            Assert.AreEqual(expected, ClientFeatures.AllFeatures.Single());
         }
     }
 }
