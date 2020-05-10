ba248757acaa99e1b8590998aac29d618026c8f4
Mikhail Kostyuckovich <m.kostyuckovich@irdeto.com>
2020-05-10 13:39:01 +0200

    IGNITE-12976: added tests for nulls last/nulls first sql syntax in spring-data integration

---
+++ modules/spring-data-2.0/src/test/java/org/apache/ignite/springdata20/repository/query/IgniteQueryGeneratorTest.java
@@ -0,0 +1,31 @@
+package org.apache.ignite.springdata20.repository.query;
+
+import org.junit.Test;
+import org.springframework.data.domain.Sort;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class IgniteQueryGeneratorTest {
+    @Test
+    public void testAddSortingNullsFirst() {
+        assertThat(
+                IgniteQueryGenerator.addSorting(
+                        new StringBuilder("SELECT * FROM someTable"),
+                        Sort.by(new Sort.Order(Sort.Direction.ASC, "someColumn").nullsFirst())
+                ).toString(),
+                is("SELECT * FROM someTable ORDER BY someColumn ASC NULLS FIRST")
+        );
+    }
+
+    @Test
+    public void testAddSortingNullsLast() {
+        assertThat(
+                IgniteQueryGenerator.addSorting(
+                        new StringBuilder("SELECT * FROM someTable"),
+                        Sort.by(new Sort.Order(Sort.Direction.ASC, "someColumn").nullsLast())
+                ).toString(),
+                is("SELECT * FROM someTable ORDER BY someColumn ASC NULLS LAST")
+        );
+    }
+}
