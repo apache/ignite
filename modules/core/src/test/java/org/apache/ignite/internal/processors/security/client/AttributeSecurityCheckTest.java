64fcfdff6bc69cf57d4b2f1715c303e6144028b6
olegostanin <oleg.alex.ostanin@gmail.com>
2020-05-13 14:53:21 +0300

    IGNITE-12891 fix comment typo

--- modules/core/src/test/java/org/apache/ignite/internal/processors/security/client/AttributeSecurityCheckTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/security/client/AttributeSecurityCheckTest.java
@@ -34,7 +34,7 @@ import org.junit.runners.JUnit4;
 import static org.apache.ignite.plugin.security.SecurityPermissionSetBuilder.ALLOW_ALL;
 
 /**
- * Checks user attributes presents in CridClient messages.
+ * Checks user attributes presents in GridClient messages.
  */
 @RunWith(JUnit4.class)
 public class AttributeSecurityCheckTest extends CommonSecurityCheckTest {
