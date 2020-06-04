4c138953ba317b0b1836eaef30ae0f436bc611bd
Igor Seliverstov <gvvinblade@gmail.com>
2020-06-04 21:35:27 +0300

    small fixes

--- modules/calcite/src/main/java/org/apache/ignite/internal/processors/query/calcite/rule/logical/LogicalOrToUnionRule.java
+++ modules/calcite/src/main/java/org/apache/ignite/internal/processors/query/calcite/rule/logical/LogicalOrToUnionRule.java
@@ -17,22 +17,19 @@
 
 package org.apache.ignite.internal.processors.query.calcite.rule.logical;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 import org.apache.calcite.plan.RelOptRule;
 import org.apache.calcite.plan.RelOptRuleCall;
 import org.apache.calcite.plan.RelOptUtil;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.core.RelFactories;
 import org.apache.calcite.rel.logical.LogicalFilter;
-import org.apache.calcite.rel.logical.LogicalUnion;
 import org.apache.calcite.rex.RexBuilder;
-import org.apache.calcite.rex.RexCall;
 import org.apache.calcite.rex.RexNode;
 import org.apache.calcite.rex.RexUtil;
 import org.apache.calcite.sql.SqlKind;
+import org.apache.calcite.tools.RelBuilder;
 
 /**
  *
@@ -53,35 +50,23 @@ public class LogicalOrToUnionRule extends RelOptRule {
     /** {@inheritDoc} */
     @Override public void onMatch(RelOptRuleCall call) {
         final LogicalFilter rel = call.rel(0);
-        final RexBuilder rexBuilder = rel.getCluster().getRexBuilder();
-
+        RexBuilder rexBuilder = rel.getCluster().getRexBuilder();
         RexNode dnf = RexUtil.toDnf(rexBuilder, rel.getCondition());
 
         if (!dnf.isA(SqlKind.OR))
             return;
 
-        List<RexNode> operands = ((RexCall)dnf).getOperands();
+        List<RexNode> operands = RelOptUtil.disjunctions(dnf);
+        RelBuilder builder = relBuilderFactory.create(rel.getCluster(), null);
+        for (RexNode operand : operands) {
+            List<RexNode> others = operands.stream()
+                .filter(op -> op != operand)
+                .collect(Collectors.toList());
 
-        if (operands.size() != 2)
-            return; // TODO: allow more than 2 operands.
+            builder.push(rel.getInput()).filter(RexUtil.andNot(rexBuilder, operand, others));
+        }
 
-        RelNode input = rel.getInput(0);
-
-        LogicalUnion union1 =
-            LogicalUnion.create(Arrays.asList(
-                LogicalFilter.create(input, operands.get(0)),
-                LogicalFilter.create(input, RexUtil.andNot(rexBuilder, operands.get(1), operands.get(0)))), true);
-
-        LogicalUnion union2 =
-            LogicalUnion.create(Arrays.asList(
-                LogicalFilter.create(input, operands.get(1)),
-                LogicalFilter.create(input, RexUtil.andNot(rexBuilder, operands.get(0), operands.get(1)))), true);
-
-        Map<RelNode, RelNode> equivMap = new HashMap<>();
-
-        equivMap.put(RelOptUtil.createCastRel(union1, rel.getRowType(), false), rel);
-        equivMap.put(RelOptUtil.createCastRel(union2, rel.getRowType(), false), rel);
-
-        call.transformTo(rel, equivMap);
+        RelNode union = builder.union(true).build();
+        call.transformTo(union);
     }
 }
