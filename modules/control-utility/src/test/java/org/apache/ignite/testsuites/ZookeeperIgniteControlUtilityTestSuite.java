f4e2b8a398d576ae4bebe1d6fd9528bbbd6f3ce1
ktkalenko <ktkalenko@gridgain.com>
2020-06-09 16:52:26 +0300

    IGNITE-13123 WIP copy suite zookeeper -> control-utility

---
+++ modules/control-utility/src/test/java/org/apache/ignite/testsuites/ZookeeperIgniteControlUtilityTestSuite.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.testsuites;
+
+import org.apache.ignite.spi.discovery.zk.ZookeeperDiscoverySpiTestConfigurator;
+import org.apache.ignite.util.GridCommandHandlerClusterByClassTest;
+import org.apache.ignite.util.GridCommandHandlerTest;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Regular Ignite tests executed with {@link org.apache.ignite.spi.discovery.zk.ZookeeperDiscoverySpi}.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    GridCommandHandlerTest.class,
+    GridCommandHandlerClusterByClassTest.class
+})
+public class ZookeeperIgniteControlUtilityTestSuite {
+    /**
+     * @throws Exception Thrown in case of the failure.
+     */
+    @BeforeClass
+    public static void init() throws Exception {
+        ZookeeperDiscoverySpiTestConfigurator.initTestSuite();
+    }
+}
