22af51e366acb4721122fec4249fac215234dd42
Semyon Danilov <samvimes@yandex.ru>
2020-06-16 15:32:08 +0300

    IGNITE-12994 Binary metadata and marshaller directories moved to PDS directory. Fixes #7792
    Signed-off-by: Slava Koptilin <slava.koptilin@gmail.com>

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteBinaryObjectsTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteBinaryObjectsTestSuite.java
@@ -56,6 +56,7 @@ import org.apache.ignite.internal.binary.streams.BinaryAbstractOutputStreamTest;
 import org.apache.ignite.internal.binary.streams.BinaryHeapStreamByteOrderSelfTest;
 import org.apache.ignite.internal.binary.streams.BinaryOffheapStreamByteOrderSelfTest;
 import org.apache.ignite.internal.processors.cache.binary.BinaryAtomicCacheLocalEntriesSelfTest;
+import org.apache.ignite.internal.processors.cache.binary.BinaryMetadataMoveLegacyFolderTest;
 import org.apache.ignite.internal.processors.cache.binary.BinaryMetadataRegistrationCacheApiTest;
 import org.apache.ignite.internal.processors.cache.binary.BinaryMetadataRegistrationCacheStoreTest;
 import org.apache.ignite.internal.processors.cache.binary.BinaryMetadataRegistrationEntryProcessorTest;
@@ -160,7 +161,9 @@ import org.junit.runners.Suite;
     BinaryAbstractOutputStreamTest.class,
     BinaryOffheapStreamByteOrderSelfTest.class,
 
-    GridCacheBinaryObjectUserClassloaderSelfTest.class
+    GridCacheBinaryObjectUserClassloaderSelfTest.class,
+
+    BinaryMetadataMoveLegacyFolderTest.class
 })
 public class IgniteBinaryObjectsTestSuite {
 }
