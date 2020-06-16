32b3ebcf242ecf35fdd5fc9fb1cd1eddec847c0a
Ryzhov Sergei <s.vi.ryzhov@gmail.com>
2020-06-16 14:12:46 +0300

    IGNITE-13153: fix code style

--- modules/core/src/test/java/org/apache/ignite/util/CommandHandlerUserAttributesTest.java
+++ modules/core/src/test/java/org/apache/ignite/util/CommandHandlerUserAttributesTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.util;
 
 import java.io.ByteArrayInputStream;
@@ -22,8 +39,8 @@ import static org.apache.ignite.ssl.SslContextFactory.DFLT_SSL_PROTOCOL;
 import static org.junit.Assert.assertNotNull;
 
 /**
- * Checks userAttributes in GridClientConfiguration contains X509Certificate
- * when CommandHandler used with SSL parameters.
+ * Checks userAttributes in GridClientConfiguration contains X509Certificate when CommandHandler used with SSL
+ * parameters.
  */
 public class CommandHandlerUserAttributesTest {
     /** */
@@ -34,11 +51,10 @@ public class CommandHandlerUserAttributesTest {
         char[] chars = "123456".toCharArray();
 
         ConnectionAndSslParameters params = new ConnectionAndSslParameters(CommandList.STATE.command(),
-            DFLT_HOST, DFLT_PORT, "test","test",
-            DFLT_PING_INTERVAL, DFLT_PING_TIMEOUT,false, false,DFLT_SSL_PROTOCOL,"",
-            SslContextFactory.DFLT_KEY_ALGORITHM, "src/test/resources/client.jks",chars,
-            SslContextFactory.DFLT_STORE_TYPE,"src/test/resources/trust.jks", chars,
-            SslContextFactory.DFLT_STORE_TYPE);
+            DFLT_HOST, DFLT_PORT, "test", "test", DFLT_PING_INTERVAL, DFLT_PING_TIMEOUT,
+            false, false, DFLT_SSL_PROTOCOL, "", SslContextFactory.DFLT_KEY_ALGORITHM,
+            "src/test/resources/client.jks", chars, SslContextFactory.DFLT_STORE_TYPE,
+            "src/test/resources/trust.jks", chars, SslContextFactory.DFLT_STORE_TYPE);
 
         GridClientConfiguration clientCfg = Whitebox.invokeMethod(cmdHndlr, "getClientConfiguration", params);
 
@@ -47,7 +63,9 @@ public class CommandHandlerUserAttributesTest {
         assertNotNull(fromPEM(strPem));
     }
 
-    /** */
+    /**
+     *
+     */
     private static X509Certificate fromPEM(String strPem) throws CertificateException {
         byte[] decode = Base64.getDecoder().decode(strPem.getBytes());
 
