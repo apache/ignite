0e7b670b9f144eb114b9f746fbba988363497e47
张俊锋 <zhangjunfeng@viewhigh.com>
2020-06-19 09:13:19 +0800

    6.19

---
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/rest/IgniteRestProcessor.java
@@ -0,0 +1,11 @@
+package org.apache.ignite.internal.processors.rest;
+import org.apache.ignite.internal.processors.GridProcessor;
+
+/**
+ * REST processor marker interface.
+ *
+ * @see GridRestProcessor
+ */
+public interface IgniteRestProcessor extends GridProcessor {
+    // No-op.
+}
