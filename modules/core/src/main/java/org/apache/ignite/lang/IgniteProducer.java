86f63f2d5fe2e557c5640939281eccf3a3cb0d7d
Semyon Danilov <samvimes@yandex.ru>
2020-07-08 11:01:45 +0300

    IGNITE-14347: Fix Node Failure on Receiving Data of Unknown Class via Distributed Metastorage
    Original author of this patch is Sam Vimes

---
+++ modules/core/src/main/java/org/apache/ignite/lang/IgniteProducer.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2020 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.lang;
+
+import org.apache.ignite.IgniteCheckedException;
+
+/**
+ * Defines a producer which can throw IgniteCheckedException.
+ *
+ * @param <T> Type of producible value.
+ */
+@FunctionalInterface
+public interface IgniteProducer<T> {
+    /**
+     * Produce value.
+     */
+    public T produce() throws IgniteCheckedException;
+
+}
