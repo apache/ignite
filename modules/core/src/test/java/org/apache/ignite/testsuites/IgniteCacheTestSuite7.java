f6ef6e483dd572428a8b2fc715fae9886729425b
vd_pyatkov <vldpyatkov@gmail.com>
2020-07-13 14:35:47 +0300

    IGNITE-13191 Public-facing API for "waiting for backups on shutdown" - Fixes #7970
    Signed-off-by: Ivan Rakov <ivan.glukos@gmail.com>

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheTestSuite7.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheTestSuite7.java
@@ -42,6 +42,8 @@ import org.apache.ignite.internal.processors.cache.distributed.CacheDataLossOnPa
 import org.apache.ignite.internal.processors.cache.distributed.CachePartitionLossWithPersistenceTest;
 import org.apache.ignite.internal.processors.cache.distributed.CachePartitionLostAfterSupplierHasLeftTest;
 import org.apache.ignite.internal.processors.cache.distributed.CacheRentingStateRepairTest;
+import org.apache.ignite.internal.processors.cache.distributed.dht.GracefulShutdownTest;
+import org.apache.ignite.internal.processors.cache.distributed.dht.GridCacheDhtPreloadWaitForBackupsWithPersistenceTest;
 import org.apache.ignite.internal.processors.cache.distributed.dht.GridCacheScheduleResendPartitionsAfterEvictionTest;
 import org.apache.ignite.internal.processors.cache.distributed.dht.IgniteCacheStartWithLoadTest;
 import org.apache.ignite.internal.processors.cache.distributed.rebalancing.GridCacheRebalancingPartitionCountersTest;
@@ -138,6 +140,8 @@ public class IgniteCacheTestSuite7 {
         GridTestUtils.addTestIfNeeded(suite, AtomicPartitionCounterStateConsistencyTest.class, ignoredTests);
         GridTestUtils.addTestIfNeeded(suite, AtomicPartitionCounterStateConsistencyHistoryRebalanceTest.class, ignoredTests);
 
+        GridTestUtils.addTestIfNeeded(suite, GridCacheDhtPreloadWaitForBackupsWithPersistenceTest.class, ignoredTests);
+        GridTestUtils.addTestIfNeeded(suite, GracefulShutdownTest.class, ignoredTests);
         GridTestUtils.addTestIfNeeded(suite, TxWithKeyContentionSelfTest.class, ignoredTests);
 
         return suite;
