d40e977cd4f5863b811923980a0d0cfb964ac382
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-07-22 11:01:50 +0300

    Fix LINQ method visitor

--- modules/platforms/dotnet/Apache.Ignite.Linq/Impl/MethodVisitor.cs
+++ modules/platforms/dotnet/Apache.Ignite.Linq/Impl/MethodVisitor.cs
@@ -67,9 +67,11 @@ namespace Apache.Ignite.Linq.Impl
             GetParameterizedTrimMethod("Trim", "trim"),
             GetParameterizedTrimMethod("TrimStart", "ltrim"),
             GetParameterizedTrimMethod("TrimEnd", "rtrim"),
+#if NETCOREAPP
             GetCharTrimMethod("Trim", "trim"),
             GetCharTrimMethod("TrimStart", "ltrim"),
             GetCharTrimMethod("TrimEnd", "rtrim"),
+#endif
             GetStringMethod("Replace", "replace", typeof(string), typeof(string)),
             GetStringMethod("PadLeft", "lpad", typeof (int)),
             GetStringMethod("PadLeft", "lpad", typeof (int), typeof (char)),
@@ -375,8 +377,9 @@ namespace Apache.Ignite.Linq.Impl
                 (e, v) => VisitParameterizedTrimFunc(e, v, sqlName));
         }
 
+#if NETCOREAPP
         /// <summary>
-        /// Gets string parameterized Trim(TrimStart, TrimEnd) method.
+        /// Gets string parameterized Trim(TrimStart, TrimEnd) method that takes a single char.
         /// </summary>
         private static KeyValuePair<MethodInfo, VisitMethodDelegate> GetCharTrimMethod(string name,
             string sqlName)
@@ -384,7 +387,8 @@ namespace Apache.Ignite.Linq.Impl
             return GetMethod(typeof(string), name, new[] {typeof(char)},
                 (e, v) => VisitParameterizedTrimFunc(e, v, sqlName));
         }
-
+#endif
+        
         /// <summary>
         /// Gets the math method.
         /// </summary>
