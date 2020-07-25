gridgain/gridgain

GG-26007 Improvement of human-readable format of WAL records.

modules/dev-utils/src/main/java/org/apache/ignite/development/utils/IgniteWalConverter.java
@@ -16,152 +16,187 @@
 
 package org.apache.ignite.development.utils;
 
-import java.io.File;
+import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.List;
+import org.apache.ignite.IgniteSystemProperties;
 import org.apache.ignite.internal.pagemem.wal.WALIterator;
 import org.apache.ignite.internal.pagemem.wal.WALPointer;
 import org.apache.ignite.internal.pagemem.wal.record.DataEntry;
 import org.apache.ignite.internal.pagemem.wal.record.DataRecord;
 import org.apache.ignite.internal.pagemem.wal.record.MetastoreDataRecord;
+import org.apache.ignite.internal.pagemem.wal.record.TimeStampRecord;
 import org.apache.ignite.internal.pagemem.wal.record.WALRecord;
 import org.apache.ignite.internal.processors.cache.persistence.tree.io.PageIO;
-import org.apache.ignite.internal.processors.cache.persistence.wal.FileWriteAheadLogManager;
+import org.apache.ignite.internal.processors.cache.persistence.wal.FileDescriptor;
 import org.apache.ignite.internal.processors.cache.persistence.wal.reader.IgniteWalIteratorFactory;
+import org.apache.ignite.internal.processors.cache.persistence.wal.serializer.RecordV1Serializer;
 import org.apache.ignite.internal.processors.query.h2.database.io.H2ExtrasInnerIO;
 import org.apache.ignite.internal.processors.query.h2.database.io.H2ExtrasLeafIO;
 import org.apache.ignite.internal.processors.query.h2.database.io.H2InnerIO;
 import org.apache.ignite.internal.processors.query.h2.database.io.H2LeafIO;
 import org.apache.ignite.internal.processors.query.h2.database.io.H2MvccInnerIO;
 import org.apache.ignite.internal.processors.query.h2.database.io.H2MvccLeafIO;
+import org.apache.ignite.internal.util.IgniteUtils;
+import org.apache.ignite.internal.util.typedef.F;
 import org.apache.ignite.lang.IgniteBiTuple;
-import org.apache.ignite.logger.NullLogger;
-import org.jetbrains.annotations.Nullable;
-
-import static java.util.stream.Collectors.toList;
-import static org.apache.ignite.IgniteSystemProperties.IGNITE_TO_STRING_INCLUDE_SENSITIVE;
-import static org.apache.ignite.IgniteSystemProperties.getBoolean;
-import static org.apache.ignite.IgniteSystemProperties.getEnum;
-import static org.apache.ignite.development.utils.ProcessSensitiveData.HIDE;
-import static org.apache.ignite.development.utils.ProcessSensitiveData.SHOW;
-
+ 
 /**
  * Print WAL log data in human-readable form.
  */
 public class IgniteWalConverter {
     /**
-     * System property for printing {@link WALRecord}. By default, {@code false}.
-     */
-    static final String PRINT_RECORDS = "PRINT_RECORDS";
-
-    /**
-     * System property for printing {@link WalStat}. By default, {@code true}.
-     */
-    static final String PRINT_STAT = "PRINT_STAT";
-
-    /**
-     * System property for setting {@link ProcessSensitiveData strategy} of output sensitive data.
-     * By default, {@link ProcessSensitiveData#SHOW}.
-     */
-    static final String SENSITIVE_DATA = "SENSITIVE_DATA";
-
-    /**
      * @param args Args.
      * @throws Exception If failed.
      */
-    public static void main(String[] args) throws Exception {
-        if (args.length < 2)
-            throw new IllegalArgumentException("\nYou need to provide:\n" +
-                    "\t1. Size of pages, which was selected for file store (1024, 2048, 4096, etc).\n" +
-                    "\t2. Path to dir with wal files.\n" +
-                    "\t3. (Optional) Path to dir with archive wal files.");
+    public static void main(String[] args) {
+        final IgniteWalConverterArguments parameters = IgniteWalConverterArguments.parse(System.out, args);
 
+        if (parameters != null)
+            convert(System.out, parameters);
+    }
+
+    /**
+     * Write to out WAL log data in human-readable form.
+     *
+     * @param out        Receiver of result.
+     * @param params Parameters.
+     */
+    public static void convert(final PrintStream out, final IgniteWalConverterArguments params) {
         PageIO.registerH2(H2InnerIO.VERSIONS, H2LeafIO.VERSIONS, H2MvccInnerIO.VERSIONS, H2MvccLeafIO.VERSIONS);
         H2ExtrasInnerIO.register();
         H2ExtrasLeafIO.register();
 
-        boolean printRecords = getBoolean(PRINT_RECORDS, false); //TODO read them from argumetns
-        boolean printStat = getBoolean(PRINT_STAT, true); //TODO read them from argumetns
-        ProcessSensitiveData sensitiveData = getEnum(SENSITIVE_DATA, SHOW); //TODO read them from argumetns
+        System.setProperty(IgniteSystemProperties.IGNITE_TO_STRING_INCLUDE_SENSITIVE,
+            Boolean.toString(params.getProcessSensitiveData() == ProcessSensitiveData.HIDE));
 
-        if (printRecords && HIDE == sensitiveData)
-            System.setProperty(IGNITE_TO_STRING_INCLUDE_SENSITIVE, Boolean.FALSE.toString());
+        System.setProperty(IgniteSystemProperties.IGNITE_PDS_SKIP_CRC, Boolean.toString(params.isSkipCrc()));
+        RecordV1Serializer.skipCrc = params.isSkipCrc();
 
-        final IgniteWalIteratorFactory factory = new IgniteWalIteratorFactory(new NullLogger());
+        System.setProperty(IgniteSystemProperties.IGNITE_TO_STRING_MAX_LENGTH, String.valueOf(Integer.MAX_VALUE));
 
-        final File walWorkDirWithConsistentId = new File(args[1]);
+        final WalStat stat = params.isPrintStat() ? new WalStat() : null;
 
-        final File[] workFiles = walWorkDirWithConsistentId.listFiles(FileWriteAheadLogManager.WAL_SEGMENT_FILE_FILTER);
+        IgniteWalIteratorFactory.IteratorParametersBuilder iteratorParametersBuilder = new IgniteWalIteratorFactory.IteratorParametersBuilder()
+            .pageSize(params.getPageSize())
+            .binaryMetadataFileStoreDir(params.getBinaryMetadataFileStoreDir())
+            .marshallerMappingFileStoreDir(params.getMarshallerMappingFileStoreDir())
+            .keepBinary(params.isKeepBinary());
 
-        if (workFiles == null)
-            throw new IllegalArgumentException("No .wal files in dir: " + args[1]);
+        if (params.getWalDir() != null)
+            iteratorParametersBuilder.filesOrDirs(params.getWalDir());
 
-        @Nullable final WalStat stat = printStat ? new WalStat() : null;
+        if (params.getWalArchiveDir() != null)
+            iteratorParametersBuilder.filesOrDirs(params.getWalArchiveDir());
 
-        IgniteWalIteratorFactory.IteratorParametersBuilder iteratorParametersBuilder =
-                new IgniteWalIteratorFactory.IteratorParametersBuilder().filesOrDirs(workFiles)
-                    .pageSize(Integer.parseInt(args[0]));
+        final IgniteWalIteratorFactory factory = new IgniteWalIteratorFactory();
+
+        boolean printAlways = F.isEmpty(params.getRecordTypes());
 
         try (WALIterator stIt = factory.iterator(iteratorParametersBuilder)) {
+            String currentWalPath = null;
+
             while (stIt.hasNextX()) {
+                final String currentRecordWalPath = getCurrentWalFilePath(stIt);
+
+                if (currentWalPath == null || !currentWalPath.equals(currentRecordWalPath)) {
+                    out.println("File: " + currentRecordWalPath);
+
+                    currentWalPath = currentRecordWalPath;
+                }
+
                 IgniteBiTuple<WALPointer, WALRecord> next = stIt.nextX();
 
                 final WALPointer pointer = next.get1();
+
                 final WALRecord record = next.get2();
 
                 if (stat != null)
                     stat.registerRecord(record, pointer, true);
 
-                if (printRecords)
-                    System.out.println("[W] " + toString(record, sensitiveData));
-            }
-        }
+                if (printAlways || params.getRecordTypes().contains(record.type())) {
+                    boolean print = true;
 
-        if (args.length >= 3) {
-            final File walArchiveDirWithConsistentId = new File(args[2]);
+                    if (record instanceof TimeStampRecord)
+                        print = withinTimeRange((TimeStampRecord) record, params.getFromTime(), params.getToTime());
 
-            try (WALIterator stIt = factory.iterator(walArchiveDirWithConsistentId)) {
-                while (stIt.hasNextX()) {
-                    IgniteBiTuple<WALPointer, WALRecord> next = stIt.nextX();
+                    final String recordStr = toString(record, params.getProcessSensitiveData());
 
-                    final WALPointer pointer = next.get1();
-                    final WALRecord record = next.get2();
-
-                    if (stat != null)
-                        stat.registerRecord(record, pointer, false);
-
-                    if (printRecords)
-                        System.out.println("[A] " + toString(record, sensitiveData));
+                    if (print && (F.isEmpty(params.getRecordContainsText()) || recordStr.contains(params.getRecordContainsText())))
+                        out.println(recordStr);
                 }
             }
         }
-
-        System.err.flush();
+        catch (Exception e) {
+            e.printStackTrace(out);
+        }
 
         if (stat != null)
-            System.out.println("Statistic collected:\n" + stat.toString());
+            out.println("Statistic collected:\n" + stat.toString());
+    }
+
+    /**
+     * Checks if provided TimeStampRecord is within time range.
+     *
+     * @param rec Record.
+     * @param fromTime Lower bound for timestamp.
+     * @param toTime Upper bound for timestamp;
+     * @return {@code True} if timestamp is within range.
+     */
+    private static boolean withinTimeRange(TimeStampRecord rec, Long fromTime, Long toTime) {
+        if (fromTime != null && rec.timestamp() < fromTime)
+            return false;
+
+        if (toTime != null && rec.timestamp() > toTime)
+            return false;
+
+        return true;
+    }
+
+    /**
+     * Get current wal file path, used in {@code WALIterator}
+     *
+     * @param it WALIterator.
+     * @return Current wal file path.
+     */
+    private static String getCurrentWalFilePath(WALIterator it) {
+        String result = null;
+
+        try {
+            final Integer curIdx = IgniteUtils.field(it, "curIdx");
+
+            final List<FileDescriptor> walFileDescriptors = IgniteUtils.field(it, "walFileDescriptors");
+
+            if (curIdx != null && walFileDescriptors != null && !walFileDescriptors.isEmpty())
+                result = walFileDescriptors.get(curIdx).getAbsolutePath();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 
     /**
      * Converting {@link WALRecord} to a string with sensitive data.
      *
-     * @param walRecord Instance of {@link WALRecord}.
+     * @param walRecord     Instance of {@link WALRecord}.
      * @param sensitiveData Strategy for processing of sensitive data.
      * @return String representation of {@link WALRecord}.
      */
     private static String toString(WALRecord walRecord, ProcessSensitiveData sensitiveData) {
-        if (SHOW == sensitiveData || HIDE == sensitiveData)
-            return walRecord.toString();
+        if (walRecord instanceof DataRecord) {
+            final DataRecord dataRecord = (DataRecord)walRecord;
 
-        if (MetastoreDataRecord.class.isInstance(walRecord))
-            walRecord = new MetastoreDataRecordWrapper((MetastoreDataRecord)walRecord, sensitiveData);
-        else if (DataRecord.class.isInstance(walRecord)) {
-            DataRecord dataRecord = (DataRecord)walRecord;
+            final List<DataEntry> entryWrappers = new ArrayList<>(dataRecord.writeEntries().size());
 
-            List<DataEntry> entryWrappers = dataRecord.writeEntries().stream()
-                .map(dataEntry -> new DataEntryWrapper(dataEntry, sensitiveData)).collect(toList());
+            for (DataEntry dataEntry : dataRecord.writeEntries())
+                entryWrappers.add(new DataEntryWrapper(dataEntry, sensitiveData));
 
             dataRecord.setWriteEntries(entryWrappers);
         }
+        else if (walRecord instanceof MetastoreDataRecord)
+            walRecord = new MetastoreDataRecordWrapper((MetastoreDataRecord)walRecord, sensitiveData);
 
         return walRecord.toString();
     }

apache/ignite

(cherry picked from commit e63718d435d0072e3a03fa57902e37c807d9bf01)

modules/dev-utils/src/main/java/org/apache/ignite/development/utils/IgniteWalConverter.java
@@ -32,6 +32,8 @@ import org.apache.ignite.internal.processors.query.h2.database.io.H2ExtrasInnerI
 import org.apache.ignite.internal.processors.query.h2.database.io.H2ExtrasLeafIO;
 import org.apache.ignite.internal.processors.query.h2.database.io.H2InnerIO;
 import org.apache.ignite.internal.processors.query.h2.database.io.H2LeafIO;
+import org.apache.ignite.internal.processors.query.h2.database.io.H2MvccInnerIO;
+import org.apache.ignite.internal.processors.query.h2.database.io.H2MvccLeafIO;
 import org.apache.ignite.lang.IgniteBiTuple;
 import org.apache.ignite.logger.NullLogger;
 import org.jetbrains.annotations.Nullable;
@@ -48,118 +50,157 @@ import static org.apache.ignite.development.utils.ProcessSensitiveData.SHOW;
  */
 public class IgniteWalConverter {
     /**
-     * System property for printing {@link WALRecord}. By default, {@code false}.
-     */
-    static final String PRINT_RECORDS = "PRINT_RECORDS";
-
-    /**
-     * System property for printing {@link WalStat}. By default, {@code true}.
-     */
-    static final String PRINT_STAT = "PRINT_STAT";
-
-    /**
-     * System property for setting {@link ProcessSensitiveData strategy} of output sensitive data.
-     * By default, {@link ProcessSensitiveData#SHOW}.
-     */
-    static final String SENSITIVE_DATA = "SENSITIVE_DATA";
-
-    /**
      * @param args Args.
+     * @throws Exception If failed.
      */
-    public static void main(String[] args) throws Exception {
-        if (args.length < 2)
-            throw new IllegalArgumentException("\nYou need to provide:\n" +
-                    "\t1. Size of pages, which was selected for file store (1024, 2048, 4096, etc).\n" +
-                    "\t2. Path to dir with wal files.\n" +
-                    "\t3. (Optional) Path to dir with archive wal files.");
+    public static void main(String[] args) {
+        final IgniteWalConverterArguments parameters = IgniteWalConverterArguments.parse(System.out, args);
 
-        PageIO.registerH2(H2InnerIO.VERSIONS, H2LeafIO.VERSIONS);
+        if (parameters != null)
+            convert(System.out, parameters);
+    }
+
+    /**
+     * Write to out WAL log data in human-readable form.
+     *
+     * @param out        Receiver of result.
+     * @param params Parameters.
+     */
+    public static void convert(final PrintStream out, final IgniteWalConverterArguments params) {
+        PageIO.registerH2(H2InnerIO.VERSIONS, H2LeafIO.VERSIONS, H2MvccInnerIO.VERSIONS, H2MvccLeafIO.VERSIONS);
         H2ExtrasInnerIO.register();
         H2ExtrasLeafIO.register();
 
-        boolean printRecords = getBoolean(PRINT_RECORDS, false); //TODO read them from argumetns
-        boolean printStat = getBoolean(PRINT_STAT, true); //TODO read them from argumetns
-        ProcessSensitiveData sensitiveData = getEnum(SENSITIVE_DATA, SHOW); //TODO read them from argumetns
+        System.setProperty(IgniteSystemProperties.IGNITE_TO_STRING_INCLUDE_SENSITIVE,
+            Boolean.toString(params.getProcessSensitiveData() == ProcessSensitiveData.HIDE));
 
-        if (printRecords && HIDE == sensitiveData)
-            System.setProperty(IGNITE_TO_STRING_INCLUDE_SENSITIVE, Boolean.FALSE.toString());
+        System.setProperty(IgniteSystemProperties.IGNITE_PDS_SKIP_CRC, Boolean.toString(params.isSkipCrc()));
+        RecordV1Serializer.skipCrc = params.isSkipCrc();
 
-        final IgniteWalIteratorFactory factory = new IgniteWalIteratorFactory(new NullLogger());
+        System.setProperty(IgniteSystemProperties.IGNITE_TO_STRING_MAX_LENGTH, String.valueOf(Integer.MAX_VALUE));
 
-        final File walWorkDirWithConsistentId = new File(args[1]);
+        final WalStat stat = params.isPrintStat() ? new WalStat() : null;
 
-        final File[] workFiles = walWorkDirWithConsistentId.listFiles(FileWriteAheadLogManager.WAL_SEGMENT_FILE_FILTER);
+        IgniteWalIteratorFactory.IteratorParametersBuilder iteratorParametersBuilder = new IgniteWalIteratorFactory.IteratorParametersBuilder()
+            .pageSize(params.getPageSize())
+            .binaryMetadataFileStoreDir(params.getBinaryMetadataFileStoreDir())
+            .marshallerMappingFileStoreDir(params.getMarshallerMappingFileStoreDir())
+            .keepBinary(params.isKeepBinary());
 
-        if (workFiles == null)
-            throw new IllegalArgumentException("No .wal files in dir: " + args[1]);
+        if (params.getWalDir() != null)
+            iteratorParametersBuilder.filesOrDirs(params.getWalDir());
 
-        @Nullable final WalStat stat = printStat ? new WalStat() : null;
+        if (params.getWalArchiveDir() != null)
+            iteratorParametersBuilder.filesOrDirs(params.getWalArchiveDir());
 
-        IgniteWalIteratorFactory.IteratorParametersBuilder iteratorParametersBuilder =
-                new IgniteWalIteratorFactory.IteratorParametersBuilder().filesOrDirs(workFiles)
-                    .pageSize(Integer.parseInt(args[0]));
+        final IgniteWalIteratorFactory factory = new IgniteWalIteratorFactory();
+
+        boolean printAlways = F.isEmpty(params.getRecordTypes());
 
         try (WALIterator stIt = factory.iterator(iteratorParametersBuilder)) {
+            String currentWalPath = null;
+
             while (stIt.hasNextX()) {
+                final String currentRecordWalPath = getCurrentWalFilePath(stIt);
+
+                if (currentWalPath == null || !currentWalPath.equals(currentRecordWalPath)) {
+                    out.println("File: " + currentRecordWalPath);
+
+                    currentWalPath = currentRecordWalPath;
+                }
+
                 IgniteBiTuple<WALPointer, WALRecord> next = stIt.nextX();
 
                 final WALPointer pointer = next.get1();
+
                 final WALRecord record = next.get2();
 
                 if (stat != null)
                     stat.registerRecord(record, pointer, true);
 
-                if (printRecords)
-                    System.out.println("[W] " + toString(record, sensitiveData));
-            }
-        }
+                if (printAlways || params.getRecordTypes().contains(record.type())) {
+                    boolean print = true;
 
-        if (args.length >= 3) {
-            final File walArchiveDirWithConsistentId = new File(args[2]);
+                    if (record instanceof TimeStampRecord)
+                        print = withinTimeRange((TimeStampRecord) record, params.getFromTime(), params.getToTime());
 
-            try (WALIterator stIt = factory.iterator(walArchiveDirWithConsistentId)) {
-                while (stIt.hasNextX()) {
-                    IgniteBiTuple<WALPointer, WALRecord> next = stIt.nextX();
+                    final String recordStr = toString(record, params.getProcessSensitiveData());
 
-                    final WALPointer pointer = next.get1();
-                    final WALRecord record = next.get2();
-
-                    if (stat != null)
-                        stat.registerRecord(record, pointer, false);
-
-                    if (printRecords)
-                        System.out.println("[A] " + toString(record, sensitiveData));
+                    if (print && (F.isEmpty(params.getRecordContainsText()) || recordStr.contains(params.getRecordContainsText())))
+                        out.println(recordStr);
                 }
             }
         }
-
-        System.err.flush();
+        catch (Exception e) {
+            e.printStackTrace(out);
+        }
 
         if (stat != null)
-            System.out.println("Statistic collected:\n" + stat.toString());
+            out.println("Statistic collected:\n" + stat.toString());
+    }
+
+    /**
+     * Checks if provided TimeStampRecord is within time range.
+     *
+     * @param rec Record.
+     * @param fromTime Lower bound for timestamp.
+     * @param toTime Upper bound for timestamp;
+     * @return {@code True} if timestamp is within range.
+     */
+    private static boolean withinTimeRange(TimeStampRecord rec, Long fromTime, Long toTime) {
+        if (fromTime != null && rec.timestamp() < fromTime)
+            return false;
+
+        if (toTime != null && rec.timestamp() > toTime)
+            return false;
+
+        return true;
+    }
+
+    /**
+     * Get current wal file path, used in {@code WALIterator}
+     *
+     * @param it WALIterator.
+     * @return Current wal file path.
+     */
+    private static String getCurrentWalFilePath(WALIterator it) {
+        String result = null;
+
+        try {
+            final Integer curIdx = IgniteUtils.field(it, "curIdx");
+
+            final List<FileDescriptor> walFileDescriptors = IgniteUtils.field(it, "walFileDescriptors");
+
+            if (curIdx != null && walFileDescriptors != null && !walFileDescriptors.isEmpty())
+                result = walFileDescriptors.get(curIdx).getAbsolutePath();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 
     /**
      * Converting {@link WALRecord} to a string with sensitive data.
      *
-     * @param walRecord Instance of {@link WALRecord}.
+     * @param walRecord     Instance of {@link WALRecord}.
      * @param sensitiveData Strategy for processing of sensitive data.
      * @return String representation of {@link WALRecord}.
      */
     private static String toString(WALRecord walRecord, ProcessSensitiveData sensitiveData) {
-        if (SHOW == sensitiveData || HIDE == sensitiveData)
-            return walRecord.toString();
+        if (walRecord instanceof DataRecord) {
+            final DataRecord dataRecord = (DataRecord)walRecord;
 
-        if (MetastoreDataRecord.class.isInstance(walRecord))
-            walRecord = new MetastoreDataRecordWrapper((MetastoreDataRecord)walRecord, sensitiveData);
-        else if (DataRecord.class.isInstance(walRecord)) {
-            DataRecord dataRecord = (DataRecord)walRecord;
+            final List<DataEntry> entryWrappers = new ArrayList<>(dataRecord.writeEntries().size());
 
-            List<DataEntry> entryWrappers = dataRecord.writeEntries().stream()
-                .map(dataEntry -> new DataEntryWrapper(dataEntry, sensitiveData)).collect(toList());
+            for (DataEntry dataEntry : dataRecord.writeEntries())
+                entryWrappers.add(new DataEntryWrapper(dataEntry, sensitiveData));
 
             dataRecord.setWriteEntries(entryWrappers);
         }
+        else if (walRecord instanceof MetastoreDataRecord)
+            walRecord = new MetastoreDataRecordWrapper((MetastoreDataRecord)walRecord, sensitiveData);
 
         return walRecord.toString();
     }