e16b2b27690405332160a55183bc46cb88f54aca
Aleksey Plekhanov <plehanov.alex@gmail.com>
2020-07-28 11:14:02 +0500

    IGNITE-13300 Fix Ignite sandbox vulnerability (user code can be executed in privileged proxy) - Fixes #8084.
    Signed-off-by: Aleksey Plekhanov <plehanov.alex@gmail.com>

--- modules/core/src/test/java/org/apache/ignite/testsuites/SecurityTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/SecurityTestSuite.java
@@ -57,6 +57,7 @@ import org.apache.ignite.internal.processors.security.sandbox.EventsSandboxTest;
 import org.apache.ignite.internal.processors.security.sandbox.IgniteOperationsInsideSandboxTest;
 import org.apache.ignite.internal.processors.security.sandbox.IgnitionComponentProxyTest;
 import org.apache.ignite.internal.processors.security.sandbox.MessagingSandboxTest;
+import org.apache.ignite.internal.processors.security.sandbox.PrivilegedProxyTest;
 import org.apache.ignite.internal.processors.security.sandbox.SecuritySubjectPermissionsTest;
 import org.apache.ignite.ssl.MultipleSSLContextsTest;
 import org.junit.runner.RunWith;
@@ -110,6 +111,7 @@ import org.junit.runners.Suite;
     ContinuousQuerySandboxTest.class,
     ContinuousQueryWithTransformerSandboxTest.class,
     EventsSandboxTest.class,
+    PrivilegedProxyTest.class,
 
     IgniteSecurityProcessorTest.class,
     MultipleSSLContextsTest.class
