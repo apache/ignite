da22909747a9205b7097b2ab0b2b802a7b986a4f
Ivan Daschinskiy <ivandasch@gmail.com>
2020-07-28 16:06:36 +0300

    IGNITE-13291 Add path validation to Ignite codebase. Remove unnecessary dependency to Apache Curator. (#8080)

--- modules/zookeeper/src/main/java/org/apache/ignite/spi/discovery/zk/internal/ZkIgnitePaths.java
+++ modules/zookeeper/src/main/java/org/apache/ignite/spi/discovery/zk/internal/ZkIgnitePaths.java
@@ -22,7 +22,7 @@ import java.util.UUID;
 /**
  *
  */
-class ZkIgnitePaths {
+public class ZkIgnitePaths {
     /** */
     static final String PATH_SEPARATOR = "/";
 
@@ -303,5 +303,66 @@ class ZkIgnitePaths {
         String flagsStr = path.substring(startIdx, startIdx + 2);
 
         return (byte)(Integer.parseInt(flagsStr, 16) - 128);
+    }
+
+    /**
+     * Validate the provided znode path string.
+     *
+     * @param path znode path string.
+     * @return The given path if it was valid, for fluent chaining.
+     * @throws IllegalArgumentException if the path is invalid/
+     */
+    public static String validatePath(String path) throws IllegalArgumentException {
+        if (path == null)
+            throw new IllegalArgumentException("Path cannot be null");
+
+        if (path.isEmpty())
+            throw new IllegalArgumentException("Path length must be > 0");
+
+        if (path.charAt(0) != '/')
+            throw new IllegalArgumentException("Path must start with / character");
+
+        if (path.length() == 1)
+            return path;
+
+        if (path.charAt(path.length() - 1) == '/')
+            throw new IllegalArgumentException("Path must not end with / character");
+
+        String reason = null;
+        char prev = '/';
+        char chars[] = path.toCharArray();
+        char c;
+
+        for (int i = 1; i < chars.length; prev = chars[i], i++) {
+            c = chars[i];
+
+            if (c == 0) {
+                reason = "null character not allowed @" + i;
+
+                break;
+            }
+            else if (c == '/' && prev == '/') {
+                reason = "empty node name specified @" + i;
+
+                break;
+            }
+            else if (c == '.' && (i + 1 == chars.length || chars[i + 1] == '/')
+                && ((chars[i - 2] == '/' && prev == '.') || chars[i - 1] == '/')) {
+                reason = "relative paths not allowed @" + i;
+
+                break;
+            }
+            else if (c > '\u0000' && c < '\u001f' || c > '\u007f' && c < '\u009f' || c > '\ud800' && c < '\uf8ff'
+                || c > '\ufff0' && c < '\uffff') {
+                reason = "invalid charater @" + i;
+
+                break;
+            }
+        }
+
+        if (reason != null)
+            throw new IllegalArgumentException("Invalid path string \"" + path + "\" caused by " + reason);
+
+        return path;
     }
 }
