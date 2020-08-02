838ea488c0c6994e19c52e42a07febdbd98a02ba
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-08-02 14:18:55 +0300

    Fix install-package.ps1

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests.NuGet/install-package.ps1
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests.NuGet/install-package.ps1
@@ -10,15 +10,15 @@ rmdir packages -Force -Recurse
 (Get-Content Apache.Ignite.Core.Tests.NuGet.csproj) `
     -replace 'packages\\Apache.Ignite(.*?)\.\d.*?\\', ('packages\Apache.Ignite$1.' + "$ver\") `
     | Out-File Apache.Ignite.Core.Tests.NuGet.csproj  -Encoding utf8
-	
+
 # Detect NuGet
 $ng = "nuget"
-if ((Get-Command $ng -ErrorAction SilentlyContinue) -eq $null) { 
-    $ng = ".\nuget.exe"
+if ((Get-Command $ng -ErrorAction SilentlyContinue) -eq $null) {
+    $ng = "$PSScriptRoot\..\nuget.exe"
 
     if (-not (Test-Path $ng)) {
         echo "Downloading NuGet..."
-        (New-Object System.Net.WebClient).DownloadFile("https://dist.nuget.org/win-x86-commandline/v3.3.0/nuget.exe", "nuget.exe");    
+        (New-Object System.Net.WebClient).DownloadFile("https://dist.nuget.org/win-x86-commandline/v5.3.1/nuget.exe", $ng)
     }
 }
 
