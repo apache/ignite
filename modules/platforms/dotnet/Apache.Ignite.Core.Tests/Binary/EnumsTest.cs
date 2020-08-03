c448c957f9684d49a2aad6bbc78898a85f2d6d05
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-08-03 09:45:25 +0300

    IGNITE-13293 .NET: Fix enum serialization performance
    * Don't send enum metadata more than once (main part of this fix - see `BinaryWriter` changes)
    * Cache name-value map per enum type
    * Add benchmark
    
    As a result, enum field serialization is on par with int field:
    
    ```
    |                Method |     Mean |    Error |   StdDev | Ratio | RatioSD |
    |---------------------- |---------:|---------:|---------:|------:|--------:|
    |          PutPrimitive | 45.47 us | 0.907 us | 1.330 us |  1.00 |    0.00 |
    |     PutPrimitiveAsync | 66.28 us | 1.317 us | 1.617 us |  1.45 |    0.05 |
    |  PutClassWithIntField | 68.34 us | 1.275 us | 1.130 us |  1.49 |    0.05 |
    | PutClassWithEnumField | 71.13 us | 1.419 us | 2.734 us |  1.57 |    0.08 |
    ```
    
    (cherry picked from commit e18016b79f928e1e7adb7ed185982a099f2093a2)

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Binary/EnumsTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Binary/EnumsTest.cs
@@ -18,6 +18,7 @@
 namespace Apache.Ignite.Core.Tests.Binary
 {
     using System;
+    using System.Linq;
     using System.Runtime.Serialization;
     using Apache.Ignite.Core.Binary;
     using Apache.Ignite.Core.Impl;
@@ -84,6 +85,11 @@ namespace Apache.Ignite.Core.Tests.Binary
                 {
                     Assert.AreEqual(string.Format("{0} [typeId={1}, enumValue={2}, enumValueName={3}]",
                         typeof(T).FullName, binRes.GetBinaryType().TypeId, binRes.EnumValue, val), binRes.ToString());
+
+                    var expectedEnumNames = Enum.GetValues(typeof(T)).OfType<T>().Select(x => x.ToString()).ToList();
+                    var actualEnumNames = binRes.GetBinaryType().GetEnumValues().Select(v => v.EnumName).ToList();
+                    
+                    CollectionAssert.AreEquivalent(expectedEnumNames, actualEnumNames);
                 }
                 else
                 {
