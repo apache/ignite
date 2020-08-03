8617b9bbe1cded549a925fc4af4b1f46d08e506d
Mikhail Petrov <pmgheap.sbt@gmail.com>
2020-08-03 12:24:00 +0300

    IGNITE-13310 Adds tracing of SQL queries.

--- modules/control-utility/src/test/java/org/apache/ignite/util/GridCommandHandlerTest.java
+++ modules/control-utility/src/test/java/org/apache/ignite/util/GridCommandHandlerTest.java
@@ -92,7 +92,6 @@ import org.apache.ignite.internal.processors.cache.version.GridCacheVersion;
 import org.apache.ignite.internal.processors.cluster.GridClusterStateProcessor;
 import org.apache.ignite.internal.util.lang.GridAbsPredicate;
 import org.apache.ignite.internal.util.typedef.G;
-import org.apache.ignite.internal.util.typedef.T2;
 import org.apache.ignite.internal.util.typedef.X;
 import org.apache.ignite.internal.util.typedef.internal.U;
 import org.apache.ignite.internal.visor.cache.VisorFindAndDeleteGarbageInPersistenceTaskResult;
@@ -1270,9 +1269,8 @@ public class GridCommandHandlerTest extends GridCommandHandlerClusterPerMethodAb
         primSpi.waitForBlocked(clients.length);
 
         // Unblock only finish messages from clients from 2 to 4.
-        primSpi.stopBlock(true, new IgnitePredicate<T2<ClusterNode, GridIoMessage>>() {
-            @Override public boolean apply(T2<ClusterNode, GridIoMessage> objects) {
-                GridIoMessage iom = objects.get2();
+        primSpi.stopBlock(true, blockedMsg -> {
+                GridIoMessage iom = blockedMsg.ioMessage();
 
                 Message m = iom.message();
 
@@ -1284,7 +1282,7 @@ public class GridCommandHandlerTest extends GridCommandHandlerClusterPerMethodAb
 
                 return true;
             }
-        });
+        );
 
         // Wait until queue is stable
         for (Ignite ignite : G.allGrids()) {
