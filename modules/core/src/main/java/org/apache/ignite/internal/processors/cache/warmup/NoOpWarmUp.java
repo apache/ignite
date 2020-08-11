4338b08e4c312004a535d8acc2c9d8d9f53f5920
ktkalenko <ktkalenko@gridgain.com>
2020-08-11 12:18:48 +0300

    IGNITE-13345 WIP add intefaces, configuration, noop impl

---
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/warmup/NoOpWarmUp.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.processors.cache.warmup;
+
+import org.apache.ignite.IgniteCheckedException;
+import org.apache.ignite.internal.GridKernalContext;
+import org.apache.ignite.internal.processors.cache.persistence.DataRegion;
+
+/**
+ * Noop warming up strategy.
+ */
+public class NoOpWarmUp implements WarmUpStrategy<NoOpWarmUpConfiguration> {
+    /** {@inheritDoc} */
+    @Override public Class<NoOpWarmUpConfiguration> configClass() {
+        return NoOpWarmUpConfiguration.class;
+    }
+
+    /** {@inheritDoc} */
+    @Override public void warmUp(
+        GridKernalContext kernalCtx,
+        NoOpWarmUpConfiguration cfg,
+        DataRegion region
+    ) throws IgniteCheckedException {
+        // No-op.
+    }
+
+    /** {@inheritDoc} */
+    @Override public void close() throws IgniteCheckedException {
+        // No-op.
+    }
+}
