6b9c7f42781a1fd1b6b8a5b753e268027389a441
abudnikov <abudnikov@gridgain.com>
2020-08-19 13:18:15 +0300

    update tracing page

--- docs/_docs/code-snippets/java/src/main/java/org/apache/ignite/snippets/Tracing.java
+++ docs/_docs/code-snippets/java/src/main/java/org/apache/ignite/snippets/Tracing.java
@@ -1,9 +1,11 @@
 package org.apache.ignite.snippets;
 
 import org.apache.ignite.Ignite;
-import org.apache.ignite.IgniteSystemProperties;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.spi.tracing.Scope;
+import org.apache.ignite.spi.tracing.TracingConfigurationCoordinates;
+import org.apache.ignite.spi.tracing.TracingConfigurationParameters;
 import org.junit.jupiter.api.Test;
 
 import io.opencensus.exporter.trace.zipkin.ZipkinExporterConfiguration;
@@ -24,11 +26,23 @@ public class Tracing {
         ignite.close();
     }
 
+    void enableSampling() {
+        //tag::enable-sampling[]
+        Ignite ignite = Ignition.start();
+
+        ignite.tracingConfiguration().set(
+                new TracingConfigurationCoordinates.Builder(Scope.TX).build(),
+                new TracingConfigurationParameters.Builder().withSamplingRate(0.5).build());
+
+        //end::enable-sampling[]
+    }
+
     @Test
     void exportToZipkin() {
         //tag::export-to-zipkin[]
-        ZipkinTraceExporter.createAndRegister(ZipkinExporterConfiguration.builder()
-                .setV2Url("http://localhost:9411/api/v2/spans").setServiceName("ignite-cluster").build());
+        ZipkinTraceExporter.createAndRegister(
+                ZipkinExporterConfiguration.builder().setV2Url("http://localhost:9411/api/v2/spans")
+                        .setServiceName("ignite-cluster").build());
 
         IgniteConfiguration cfg = new IgniteConfiguration();
 
