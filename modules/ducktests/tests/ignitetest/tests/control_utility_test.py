de6802b836ed653ba8427daca13d15f1e1276130
Ivan Daschinskiy <ivandasch@gmail.com>
2020-08-29 16:20:10 +0300

    Fixes data regions config.

--- modules/ducktests/tests/ignitetest/tests/control_utility_test.py
+++ modules/ducktests/tests/ignitetest/tests/control_utility_test.py
@@ -199,7 +199,8 @@ class BaselineTests(IgniteTest):
             cluster_state="INACTIVE",
             version=IgniteVersion(version),
             data_storage=DataStorageConfiguration(
-                default=DataRegionConfiguration(persistent=True)
+                default=DataRegionConfiguration(name='persistent', persistent=True),
+                regions=[DataRegionConfiguration(name='in-memory', persistent=False, max_size=100 * 1024 * 1024)]
             )
         )
 
