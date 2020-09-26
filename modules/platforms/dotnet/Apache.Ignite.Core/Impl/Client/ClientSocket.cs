84e6301f9836cd819328ee6aa95f8bbf5c9d4356
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-09-26 20:54:48 +0300

    Use owning stream to simplify socket shutdown

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Client/ClientSocket.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Client/ClientSocket.cs
@@ -868,7 +868,7 @@ namespace Apache.Ignite.Core.Impl.Client
         /// </summary>
         private static Stream GetSocketStream(Socket socket, IgniteClientConfiguration cfg, string host)
         {
-            var stream = new NetworkStream(socket)
+            var stream = new NetworkStream(socket, true)
             {
                 WriteTimeout = (int) cfg.SocketTimeout.TotalMilliseconds
             };
@@ -991,8 +991,10 @@ namespace Apache.Ignite.Core.Impl.Client
 
                 _exception = _exception ?? new ObjectDisposedException(typeof(ClientSocket).FullName);
                 EndRequestsWithError();
-                _stream.Dispose();
-                _socket.Dispose();
+                
+                // This will call Socket.Shutdown and Socket.Close.
+                _stream.Close();
+                
                 _listenerEvent.Set();
                 _listenerEvent.Dispose();
 
