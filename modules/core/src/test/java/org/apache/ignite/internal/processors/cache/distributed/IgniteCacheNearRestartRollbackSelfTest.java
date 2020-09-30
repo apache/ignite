7b4aadba1c45097bec3bf3a5bfe08546fc4128e2
Alexey Scherbakov <alexey.scherbakoff@gmail.com>
2020-09-30 08:59:43 +0300

    IGNITE-13441 Codestyle.
    Signed-off-by: Alexey Scherbakov <alexey.scherbakoff@gmail.com>

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/IgniteCacheNearRestartRollbackSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/IgniteCacheNearRestartRollbackSelfTest.java
@@ -46,7 +46,6 @@ import org.apache.ignite.testframework.GridTestUtils;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.apache.ignite.transactions.Transaction;
 import org.apache.ignite.transactions.TransactionRollbackException;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.apache.ignite.transactions.TransactionConcurrency.PESSIMISTIC;
