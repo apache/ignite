f9fa2a68fb273f3e92d2ed087169478c7b9a24c2
MixSwir <mix-swir@yandex.ru>
2020-10-05 16:56:30 +0300

    comments, doc

--- modules/ducktests/src/main/java/org/apache/ignite/internal/ducktest/tests/start_stop_client/SingleClientNode.java
+++ modules/ducktests/src/main/java/org/apache/ignite/internal/ducktest/tests/start_stop_client/SingleClientNode.java
@@ -1,22 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.internal.ducktest.tests.start_stop_client;
 
+import java.util.UUID;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.cache.CacheAtomicityMode;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.internal.ducktest.utils.IgniteAwareApplication;
 
-import java.util.UUID;
-
+/**
+ *
+ */
 public class SingleClientNode  extends IgniteAwareApplication {
 
+    /* params**/
     private IgniteCache<String, String> cache;
     private String cacheName;
     private String reportName;
     private long pacing = 0;
 
-    @Override
-    protected void run(JsonNode jsonNode) throws Exception {
+    /** {@inheritDoc} */
+    @Override protected void run(JsonNode jsonNode) throws Exception {
         cacheName = jsonNode.get("cacheName").asText();
         reportName = jsonNode.get("reportName").asText();
         pacing = jsonNode.get("pacing").asLong();
@@ -33,6 +53,7 @@ public class SingleClientNode  extends IgniteAwareApplication {
         markFinished();
     }
 
+    /* cache config**/
     private CacheConfiguration prepareCacheConfiguration(String cacheName){
         CacheConfiguration cfg = new CacheConfiguration();
         cfg.setBackups(2);
@@ -41,6 +62,7 @@ public class SingleClientNode  extends IgniteAwareApplication {
         return cfg;
     }
 
+    /* single cache operation**/
     private long cacheOperation() throws InterruptedException {
         String key = UUID.randomUUID().toString();
         String value = UUID.randomUUID().toString();
