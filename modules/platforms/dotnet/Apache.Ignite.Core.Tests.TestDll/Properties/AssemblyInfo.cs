70742da8e9f9e614bef1212718bf9216027ec355
Aleksey Plekhanov <plehanov.alex@gmail.com>
2020-10-15 09:56:23 +0300

    Ignite 2.9 Version update

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests.TestDll/Properties/AssemblyInfo.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests.TestDll/Properties/AssemblyInfo.cs
@@ -45,6 +45,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.9.0.42453")]
-[assembly: AssemblyFileVersion("2.9.0.42453")]
+[assembly: AssemblyVersion("2.9.0.50002")]
+[assembly: AssemblyFileVersion("2.9.0.50002")]
 [assembly: AssemblyInformationalVersion("2.9.0")]
