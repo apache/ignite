0abf6fafe77ec447f2bfea1d7583a2b85b2ab192
ktkalenko <ktkalenko@gridgain.com>
2020-10-30 09:51:55 +0300

    IGNITE-13613 API to get full WAL size and implementation to track WAL segments rollover and compression processes - Fixes #8388.
    Signed-off-by: Sergey Chugunov <sergey.chugunov@gmail.com>

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/pagemem/NoOpWALManager.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/pagemem/NoOpWALManager.java
@@ -188,4 +188,14 @@ public class NoOpWALManager implements IgniteWriteAheadLogManager {
     @Override public long maxArchivedSegmentToDelete() {
         return -1;
     }
+
+    /** {@inheritDoc} */
+    @Override public long segmentSize(long idx) {
+        return -1;
+    }
+
+    /** {@inheritDoc} */
+    @Override public WALPointer lastWritePointer() {
+        return null;
+    }
 }
