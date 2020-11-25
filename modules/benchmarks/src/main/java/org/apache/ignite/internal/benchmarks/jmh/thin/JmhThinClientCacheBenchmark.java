c7c8ee1c6db3bcc328038d77190d3324425d64ef
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-11-25 17:20:22 +0300

    wip thin client benchmarks - investigate hangs

--- modules/benchmarks/src/main/java/org/apache/ignite/internal/benchmarks/jmh/thin/JmhThinClientCacheBenchmark.java
+++ modules/benchmarks/src/main/java/org/apache/ignite/internal/benchmarks/jmh/thin/JmhThinClientCacheBenchmark.java
@@ -1,7 +1,11 @@
 package org.apache.ignite.internal.benchmarks.jmh.thin;
 
+import java.util.ArrayList;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.ThreadLocalRandom;
 
+import org.apache.commons.math3.stat.inference.TestUtils;
 import org.apache.ignite.internal.benchmarks.jmh.runner.JmhIdeBenchmarkRunner;
 import org.openjdk.jmh.annotations.Benchmark;
 
@@ -9,7 +13,7 @@ public class JmhThinClientCacheBenchmark extends JmhThinClientAbstractBenchmark 
     /**
      * Cache put benchmark.
      */
-    @Benchmark
+    // @Benchmark
     public void put() {
         int key = ThreadLocalRandom.current().nextInt(CNT);
 
@@ -23,7 +27,10 @@ public class JmhThinClientCacheBenchmark extends JmhThinClientAbstractBenchmark 
     public Object get() {
         int key = ThreadLocalRandom.current().nextInt(CNT);
 
-        return cache.get(key);
+        System.out.println(key);
+        byte[] res = cache.get(key);
+        System.out.println(key + "-DONE");
+        return res;
     }
 
     /**
@@ -33,20 +40,39 @@ public class JmhThinClientCacheBenchmark extends JmhThinClientAbstractBenchmark 
      * @throws Exception If failed.
      */
     public static void main(String[] args) throws Exception {
-//        JmhThinClientCacheBenchmark b = new JmhThinClientCacheBenchmark();
-//        b.setup();
-//        b.put();
-//        b.get();
-//        b.tearDown();
-
         // TODO: Benchmark hangs when GridNioServer.selectorCount > 1
-        JmhIdeBenchmarkRunner.create()
-                .forks(1)
-                .threads(4)
-                .warmupIterations(5)
-                .measurementIterations(10)
-                .benchmarks(JmhThinClientCacheBenchmark.class.getSimpleName())
-                .jvmArguments("-Xms4g", "-Xmx4g")
-                .run();
+
+
+        JmhThinClientCacheBenchmark b = new JmhThinClientCacheBenchmark();
+        b.setup();
+        b.put();
+        b.get();
+
+        ArrayList<ForkJoinTask> tasks = new ArrayList<>();
+        for (int i =0; i < 4; i++) {
+            ForkJoinTask<?> task = ForkJoinPool.commonPool().submit(() -> {
+                for (int j = 0; j < 10000; j++) {
+                    b.get();
+                    b.put();
+                }
+            });
+
+            tasks.add(task);
+        }
+
+        for (ForkJoinTask t: tasks) {
+            t.join();
+        }
+
+        b.tearDown();
+
+//        JmhIdeBenchmarkRunner.create()
+//                .forks(1)
+//                .threads(4)
+//                .warmupIterations(5)
+//                .measurementIterations(10)
+//                .benchmarks(JmhThinClientCacheBenchmark.class.getSimpleName())
+//                .jvmArguments("-Xms4g", "-Xmx4g")
+//                .run();
     }
 }
