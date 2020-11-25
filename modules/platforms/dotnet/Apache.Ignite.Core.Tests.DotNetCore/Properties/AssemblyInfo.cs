a23806ae4fdf1c5deaa7340db01838b6d089b7de
ymolochkov <ynmolochkov@sberbank.ru>
2020-11-25 15:07:15 +0300

    Ignite 2.9.1 version update

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests.DotNetCore/Properties/AssemblyInfo.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests.DotNetCore/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.9.0.42453")]
-[assembly: AssemblyFileVersion("2.9.0.42453")]
+[assembly: AssemblyVersion("2.9.1.51042")]
+[assembly: AssemblyFileVersion("2.9.1.51042")]
--- modules/platforms/dotnet/Apache.Ignite.EntityFramework/Properties/AssemblyInfo.cs
+++ modules/platforms/dotnet/Apache.Ignite.EntityFramework/Properties/AssemblyInfo.cs
@@ -32,9 +32,9 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("c558518a-c1a0-4224-aaa9-a8688474b4dc")]
 
-[assembly: AssemblyVersion("2.9.0.42453")]
-[assembly: AssemblyFileVersion("2.9.0.42453")]
-[assembly: AssemblyInformationalVersion("2.9.0")]
+[assembly: AssemblyVersion("2.9.1.51042")]
+[assembly: AssemblyFileVersion("2.9.1.51042")]
+[assembly: AssemblyInformationalVersion("2.9.1")]
 
 [assembly: CLSCompliant(true)]
 
