be7cf0b0b5c3992c327eab24b56b43b617b07abc
Konstantin Sirotkin <konstantins304@gmail.com>
2020-12-02 11:54:56 +0300

    IGNITE-13572 Don't skip filtering for caches with zero backups - Fixes #8439.
    Signed-off-by: Ilya Kasnacheev <ilya.kasnacheev@gmail.com>
    
    (cherry picked from commit 236bbb22bc3339d3d4b59b3bc3a56c2a0dcfdd98)

--- modules/indexing/src/test/java/org/apache/ignite/testsuites/IgniteBinaryCacheQueryTestSuite.java
+++ modules/indexing/src/test/java/org/apache/ignite/testsuites/IgniteBinaryCacheQueryTestSuite.java
@@ -168,6 +168,7 @@ import org.apache.ignite.internal.processors.cache.index.LongIndexNameTest;
 import org.apache.ignite.internal.processors.cache.index.OptimizedMarshallerIndexNameTest;
 import org.apache.ignite.internal.processors.cache.index.QueryEntityValidationSelfTest;
 import org.apache.ignite.internal.processors.cache.index.SchemaExchangeSelfTest;
+import org.apache.ignite.internal.processors.cache.index.SqlPartitionEvictionTest;
 import org.apache.ignite.internal.processors.cache.index.SqlTransactionCommandsWithMvccDisabledSelfTest;
 import org.apache.ignite.internal.processors.cache.index.StopNodeOnRebuildIndexFailureTest;
 import org.apache.ignite.internal.processors.cache.local.IgniteCacheLocalAtomicQuerySelfTest;
@@ -576,6 +577,9 @@ import org.junit.runners.Suite;
 
     // Partition loss.
     IndexingCachePartitionLossPolicySelfTest.class,
+
+    // Partitions eviction
+    SqlPartitionEvictionTest.class,
 
     // GROUP_CONCAT
     IgniteSqlGroupConcatCollocatedTest.class,
