ffb64d9e18049b9b9fdd6c0e20ef81663a78dc05
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-12-08 17:30:39 +0300

    Serializer test done

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/IgniteConfigurationSerializerTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/IgniteConfigurationSerializerTest.cs
@@ -186,6 +186,10 @@ namespace Apache.Ignite.Core.Tests
             Assert.AreEqual(99, af.Partitions);
             Assert.IsTrue(af.ExcludeNeighbors);
 
+            var afBf = af.AffinityBackupFilter as ClusterNodeAttributeAffinityBackupFilter;
+            Assert.IsNotNull(afBf);
+            Assert.AreEqual(new[] {"foo1", "bar2"}, afBf.AttributeNames);
+
             var platformCacheConfiguration = cacheCfg.PlatformCacheConfiguration;
             Assert.AreEqual("int", platformCacheConfiguration.KeyTypeName);
             Assert.AreEqual("string", platformCacheConfiguration.ValueTypeName);
