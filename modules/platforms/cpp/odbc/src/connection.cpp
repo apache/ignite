27062ee6b1c5d934d25b27089177b920d4c70417
Igor Sapego <isapego@apache.org>
2020-12-09 18:12:16 +0300

    IGNITE-13801: Fix Ab Initio related ODBC issues
    This closes #8528

--- modules/platforms/cpp/odbc/src/connection.cpp
+++ modules/platforms/cpp/odbc/src/connection.cpp
@@ -119,7 +119,7 @@ namespace ignite
             {
                 std::string dsn = config.GetDsn();
 
-                ReadDsnConfiguration(dsn.c_str(), config);
+                ReadDsnConfiguration(dsn.c_str(), config, &GetDiagnosticRecords());
             }
 
             return InternalEstablish(config);
