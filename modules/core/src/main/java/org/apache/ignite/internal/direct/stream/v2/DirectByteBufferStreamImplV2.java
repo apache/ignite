542385ca614be0316f0b34097e1383ae1b47b161
Ilya Kazakov <kazakov.ilya@gmail.com>
2020-12-17 17:13:26 +0800

    IGNITE-13856 linear performance for DirectByteBufferStreamImplV2.writeString

--- modules/core/src/main/java/org/apache/ignite/internal/direct/stream/v2/DirectByteBufferStreamImplV2.java
+++ modules/core/src/main/java/org/apache/ignite/internal/direct/stream/v2/DirectByteBufferStreamImplV2.java
@@ -28,7 +28,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.RandomAccess;
 import java.util.UUID;
-
 import org.apache.ignite.IgniteException;
 import org.apache.ignite.internal.direct.stream.DirectByteBufferStream;
 import org.apache.ignite.internal.processors.affinity.AffinityTopologyVersion;
