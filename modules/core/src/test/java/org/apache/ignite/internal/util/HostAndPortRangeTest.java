58054e3f7ea8a2b2259ef4cb2eb04e416aa81c17
Varvara Kozhukhova <varvara_kozhukhova@epam.com>
2020-12-18 13:25:18 +0300

    Added license and javadocs

--- modules/core/src/test/java/org/apache/ignite/internal/util/HostAndPortRangeTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/util/HostAndPortRangeTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.internal.util;
 
 import org.apache.ignite.IgniteCheckedException;
@@ -7,6 +24,10 @@ import static org.junit.Assert.assertEquals;
 
 public class HostAndPortRangeTest {
 
+    /**
+     * tests correct input address with IPv4 host and port range.
+     * @throws IgniteCheckedException on incorrect host/port
+     */
     @Test
     public void testParseIPv4WithPortRange() throws IgniteCheckedException {
         String addrStr = "127.0.0.1:8080..8090";
@@ -18,6 +39,10 @@ public class HostAndPortRangeTest {
         assertEquals(expected, actual);
     }
 
+    /**
+     * tests correct input address with IPv4 host and single port.
+     * @throws IgniteCheckedException on incorrect host/port
+     */
     @Test
     public void testParseIPv4WithSinglePort() throws IgniteCheckedException {
         String addrStr = "127.0.0.1:8080";
@@ -29,6 +54,10 @@ public class HostAndPortRangeTest {
         assertEquals(expected, actual);
     }
 
+    /**
+     * ests correct input address with IPv4 host and no port.
+     * @throws IgniteCheckedException on incorrect host/port
+     */
     @Test
     public void testParseIPv4NoPort() throws IgniteCheckedException {
         String addrStr = "127.0.0.1";
@@ -40,6 +69,10 @@ public class HostAndPortRangeTest {
         assertEquals(expected, actual);
     }
 
+    /**
+     * tests correct input address with IPv6 host and port range.
+     * @throws IgniteCheckedException on incorrect host/port
+     */
     @Test
     public void testParseIPv6WithPortRange() throws IgniteCheckedException {
         String addrStr = "[::1]:8080..8090";
@@ -51,6 +84,10 @@ public class HostAndPortRangeTest {
         assertEquals(expected, actual);
     }
 
+    /**
+     * tests correct input address with IPv6 host and single port.
+     * @throws IgniteCheckedException on incorrect host/port
+     */
     @Test
     public void testParseIPv6WithSinglePort() throws IgniteCheckedException {
         String addrStr = "[3ffe:2a00:100:7031::]:8080";
@@ -62,6 +99,10 @@ public class HostAndPortRangeTest {
         assertEquals(expected, actual);
     }
 
+    /**
+     * tests correct input address with IPv6 host and no port.
+     * @throws IgniteCheckedException on incorrect host/port
+     */
     @Test
     public void testParseIPv6NoPort() throws IgniteCheckedException {
         String addrStr = "[::FFFF:129.144.52.38]";
@@ -73,6 +114,10 @@ public class HostAndPortRangeTest {
         assertEquals(expected, actual);
     }
 
+    /**
+     * tests incorrect input address with IPv6 host (no brackets) and port.
+     * @throws IgniteCheckedException on incorrect host/port
+     */
     @Test(expected = IgniteCheckedException.class)
     public void testParseIPv6IncorrectHost() throws IgniteCheckedException {
         String addrStr = "3ffe:2a00:100:7031:::8080";
@@ -83,6 +128,10 @@ public class HostAndPortRangeTest {
 
     }
 
+    /**
+     * tests empty host and port.
+     * @throws IgniteCheckedException on incorrect host/port
+     */
     @Test(expected = IgniteCheckedException.class)
     public void testParseNoHost() throws IgniteCheckedException {
         String addrStr = ":8080";
@@ -92,6 +141,10 @@ public class HostAndPortRangeTest {
         HostAndPortRange actual = HostAndPortRange.parse(addrStr, dfltPortFrom, dfltPortTo, errMsgPrefix);
     }
 
+    /**
+     * tests empty address string.
+     * @throws IgniteCheckedException on incorrect host/port
+     */
     @Test(expected = IgniteCheckedException.class)
     public void testParseNoAddress() throws IgniteCheckedException {
         String addrStr = "";
