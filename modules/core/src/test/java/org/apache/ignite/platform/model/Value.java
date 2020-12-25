6d02e32e7f049e4f78f7abd37f4ff91d77f738c2
Nikolay <nizhikov@apache.org>
2020-12-25 22:52:13 +0300

    IGNITE-13734 .NET: Register service return type on method invocation (#8602)

--- modules/core/src/test/java/org/apache/ignite/platform/model/Value.java
+++ modules/core/src/test/java/org/apache/ignite/platform/model/Value.java
@@ -15,20 +15,37 @@
  * limitations under the License.
  */
 
-package org.apache.ignite.platform;
+package org.apache.ignite.platform.model;
+
+import java.util.Objects;
 
 /** Test value object. */
-public class Department {
+public class Value {
     /** */
-    private String name;
+    private String val;
 
     /** */
-    public String getName() {
-        return name;
+    public Value(String val) {
+        this.val = val;
     }
 
     /** */
-    public void setName(String name) {
-        this.name = name;
+    public String getVal() {
+        return val;
+    }
+
+    /** {@inheritDoc} */
+    @Override public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Value value = (Value)o;
+        return Objects.equals(val, value.val);
+    }
+
+    /** {@inheritDoc} */
+    @Override public int hashCode() {
+        return Objects.hash(val);
     }
 }
