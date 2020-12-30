2a489ae6eb71d295ad22bd5ec9979bf72d2ae608
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-12-30 19:04:16 +0300

    wip TODOs

--- modules/platforms/dotnet/Apache.Ignite.Linq/Impl/AliasDictionary.cs
+++ modules/platforms/dotnet/Apache.Ignite.Linq/Impl/AliasDictionary.cs
@@ -203,6 +203,7 @@ namespace Apache.Ignite.Linq.Impl
                                 propExpr.Member.Name == memberHint.Member.Name &&
                                 propExpr.Type == memberHint.Type)
                             {
+                                // TODO: QuerySqlField.Name is ignored for some reason on this code path
                                 return GetQuerySource(propExpr);
                             }
                         }
