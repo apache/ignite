ecb345f9655919c1589c41bc32372658532f6457
Igor Sapego <isapego@apache.org>
2020-12-31 08:04:31 +0300

    IGNITE-9109: SQL API for C++ thin
    This closes #8628

--- modules/platforms/cpp/binary/include/ignite/impl/interop/interop_memory.h
+++ modules/platforms/cpp/binary/include/ignite/impl/interop/interop_memory.h
@@ -234,7 +234,7 @@ namespace ignite
                 /**
                  * Reallocate memory.
                  *
-                 * @param cap Desired capactiy.
+                 * @param cap Desired capacity.
                  */
                 virtual void Reallocate(int32_t cap) = 0;
             protected:
@@ -260,7 +260,7 @@ namespace ignite
                  *
                  * @param memPtr Memory pointer.
                  */
-                explicit InteropUnpooledMemory(int8_t* memPtr);
+                explicit InteropUnpooledMemory(int8_t* memPtr = 0);
 
                 /**
                  * Destructor.
@@ -268,7 +268,21 @@ namespace ignite
                 ~InteropUnpooledMemory();
 
                 virtual void Reallocate(int32_t cap);
+
+                /**
+                 * Try get owning copy.
+                 *
+                 * @param mem Memory instance to transfer ownership to.
+                 * @return True on success
+                 */
+                bool TryGetOwnership(InteropUnpooledMemory& mem);
+
             private:
+                /**
+                 * Release all resources.
+                 */
+                void CleanUp();
+
                 /** Whether this instance is owner of memory chunk. */
                 bool owning; 
 
