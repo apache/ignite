b5068b804a0315f9337ce891d829a0b5d61e749f
pvinokurov <vinokurov.pasha@gmail.com>
2021-01-07 14:55:14 +0300

    Checkstyle cleanup

--- modules/core/src/main/java/org/apache/ignite/internal/processors/rest/handlers/cache/GridCacheCommandHandler.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/rest/handlers/cache/GridCacheCommandHandler.java
@@ -1124,7 +1124,7 @@ public class GridCacheCommandHandler extends GridRestCommandHandlerAdapter {
 
         /** {@inheritDoc} */
         @Override public Collection<GridCacheSqlMetadata> execute() {
-            try{
+            try {
                if (future == null) {
                     if (!ignite.cluster().active())
                         return Collections.emptyList();
