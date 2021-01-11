fe621dab91eaee74c69fa362969cf4c3b0139173
Slava Koptilin <slava.koptilin@gmail.com>
2021-01-11 18:16:08 +0300

    IGNITE-13926 Fixed javadoc

--- modules/core/src/main/java/org/apache/ignite/Ignite.java
+++ modules/core/src/main/java/org/apache/ignite/Ignite.java
@@ -360,9 +360,14 @@ public interface Ignite extends AutoCloseable {
         throws CacheException;
 
     /**
-     * Stops cache and clean up its data.
+     * Destroys a cache with the given name and cleans data that was written to the cache. The call will
+     * deallocate all resources associated with the given cache on all nodes in the cluster. There is no way
+     * to undo the action and recover destroyed data.
      * <p>
-     * If a cache with the specified name does not exist in the cluster, the operation has no effect.
+     * All existing instances of {@link IgniteCache} will be invalidated, subsequent calls to the API
+     * will throw exceptions.
+     * <p>
+     * If a cache with the specified name does not exist in the grid, the operation has no effect.
      *
      * @param cacheName Cache name to destroy.
      * @throws CacheException If error occurs.
@@ -370,12 +375,17 @@ public interface Ignite extends AutoCloseable {
     public void destroyCache(String cacheName) throws CacheException;
 
     /**
-     * Stops caches and clean up their data.
+     * Destroys caches with the given names and cleans data that was written to the caches. The call will
+     * deallocate all resources associated with the given caches on all nodes in the cluster. There is no way
+     * to undo the action and recover destroyed data.
+     * <p>
+     * All existing instances of {@link IgniteCache} will be invalidated, subsequent calls to the API
+     * will throw exceptions.
      * <p>
      * If the specified collection contains {@code null} or an empty value,
-     * this method will throw {@link IllegalArgumentException} and the caches will not be stopped.
+     * this method will throw {@link IllegalArgumentException} and the caches will not be destroyed.
      * <p>
-     * If a cache with the specified name does not exist in the cluster, the specified value will be skipped.
+     * If a cache with the specified name does not exist in the grid, the specified value will be skipped.
      *
      * @param cacheNames Collection of cache names to destroy.
      * @throws CacheException If error occurs.
