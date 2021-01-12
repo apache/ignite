41492bededbfb5a91812b39facfe33cc61041027
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-01-12 11:50:20 +0300

    Add

---
+++ modules/platforms/dotnet/examples2/ServerNode/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Apache.Ignite.Core;
+
+namespace ServerNode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Reference shared assembly in this project.
+            using (Ignition.Start())
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
