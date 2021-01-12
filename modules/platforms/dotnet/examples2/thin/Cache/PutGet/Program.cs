c6e09b98d28b13a934294e17e961afb3b2832618
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-01-12 11:41:39 +0300

    Adding examples

---
+++ modules/platforms/dotnet/examples2/thin/Cache/PutGet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Apache.Ignite.Core;
+using Apache.Ignite.Core.Client;
+using Apache.Ignite.Core.Client.Cache;
+
+namespace PutGet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cfg = new IgniteClientConfiguration("127.0.0.1");
+
+            using (IIgniteClient ignite = Ignition.StartClient(cfg))
+            {
+                ICacheClient<int, string> cache = ignite.GetOrCreateCache<int, string>("my-cache");
+
+                cache.Put(1, "Hello World!");
+
+                Console.WriteLine(">>> " + cache.Get(1));
+            }
+        }
+    }
+}
