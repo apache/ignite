ccabc0ae87d194d2e7a46ad3d503fa80ead9a3bb
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-01-12 13:18:01 +0300

    Adding Thick examples

---
+++ modules/platforms/dotnet/examples2/Thick/Cache/PutGet/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Apache.Ignite.Core;
+using Apache.Ignite.Core.Client;
+using Apache.Ignite.Core.Client.Cache;
+
+namespace IgniteExamples.Thin.PutGet
+{
+    /// <summary>
+    /// Put/Get example.
+    /// <para />
+    /// This example requires an Ignite server node. You can start the node in any of the following ways:
+    /// * docker run -p 10800:10800 apacheignite/ignite
+    /// * dotnet run -p ServerNode.csproj
+    /// * ignite.sh/ignite.bat from the distribution
+    /// </summary>
+    public class Program
+    {
+        public static void Main()
+        {
+            var cfg = new IgniteClientConfiguration("127.0.0.1");
+
+            using (IIgniteClient ignite = Ignition.StartClient(cfg))
+            {
+                ICacheClient<int, string> cache = ignite.GetOrCreateCache<int, string>("PutGetString");
+
+                cache.Put(1, "Hello World!");
+
+                Console.WriteLine(">>> " + cache.Get(1));
+            }
+        }
+    }
+}
---
+++ modules/platforms/dotnet/examples2/Thick/Cache/ScanQuery/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using Apache.Ignite.Core;
+using Apache.Ignite.Core.Cache.Query;
+using Apache.Ignite.Core.Client;
+using Apache.Ignite.Core.Client.Cache;
+using IgniteExamples.Shared;
+using IgniteExamples.Shared.Models;
+using IgniteExamples.Shared.ScanQuery;
+
+namespace IgniteExamples.Thin.ScanQuery
+{
+    /// <summary>
+    /// ScanQuery example.
+    /// <para />
+    /// This example requires an active Ignite server node with <see cref="EmployeeFilter"/> type loaded.
+    /// Start ServerNode project one or more times before running this example.
+    /// </summary>
+    public class Program
+    {
+        public static void Main()
+        {
+            var cfg = new IgniteClientConfiguration("127.0.0.1");
+
+            using (IIgniteClient ignite = Ignition.StartClient(cfg))
+            {
+                ICacheClient<int, Employee> cache = ignite.GetOrCreateCache<int, Employee>("ThinScanQuery");
+
+                cache[1] = SampleData.GetEmployees().First();
+
+                var query = new ScanQuery<int, Employee>
+                {
+                    Filter = new EmployeeFilter
+                    {
+                        EmployeeName = "Wilson"
+                    }
+                };
+
+                foreach (var cacheEntry in cache.Query(query))
+                {
+                    Console.WriteLine(cacheEntry);
+                }
+            }
+        }
+    }
+}
