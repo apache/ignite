8047344b6a28803239adc42af82ba1b38239a683
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-01-14 18:49:46 +0300

    wip examples

--- modules/platforms/dotnet/examples2/Thick/Cache/PutGet/Program.cs
+++ modules/platforms/dotnet/examples2/Thick/Cache/PutGet/Program.cs
@@ -1,6 +1,24 @@
-﻿using System;
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
 using Apache.Ignite.Core;
 using Apache.Ignite.Core.Cache;
+using IgniteExamples.Shared;
 
 namespace IgniteExamples.Thick.PutGet
 {
@@ -9,9 +27,11 @@ namespace IgniteExamples.Thick.PutGet
     /// </summary>
     public class Program
     {
+        private const string CacheName = "dotnet_cache_put_get";
+
         public static void Main()
         {
-            using (IIgnite ignite = Ignition.Start())
+            using (IIgnite ignite = Ignition.Start(Utils.GetServerNodeConfiguration()))
             {
                 ICache<int, string> cache = ignite.GetOrCreateCache<int, string>("PutGetString");
 
