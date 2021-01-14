bf9e1b74de3c125c977b3bd8d41aaea02ea59f44
tledkov <tledkov@gridgain.com>
2021-01-14 14:30:18 +0300

    fix runtime idx push

--- modules/calcite/src/main/java/org/apache/ignite/internal/processors/query/calcite/exec/RuntimeTreeIndex.java
+++ modules/calcite/src/main/java/org/apache/ignite/internal/processors/query/calcite/exec/RuntimeTreeIndex.java
@@ -73,10 +73,14 @@ public class RuntimeTreeIndex<Row> implements GridIndex<Row>, AutoCloseable {
      * Add row to index.
      */
     public void push(Row r) {
-        List<Row> eqRows = rows.putIfAbsent(r, new ArrayList<>(Collections.singletonList(r)));
+        List<Row> newEqRows = new ArrayList<>();
+
+        List<Row> eqRows = rows.putIfAbsent(r, newEqRows);
 
         if (eqRows != null)
             eqRows.add(r);
+        else
+            newEqRows.add(r);
     }
 
     /** */
