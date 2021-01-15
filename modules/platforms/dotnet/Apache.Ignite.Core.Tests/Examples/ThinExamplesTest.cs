775d68e4d4a3e83705a2fd0a4640790ef0586327
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-01-15 14:25:01 +0300

    Fix thin tests

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Examples/ThinExamplesTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Examples/ThinExamplesTest.cs
@@ -13,6 +13,24 @@ namespace Apache.Ignite.Core.Tests.Examples
         private static readonly Example[] ThinExamples = Example.AllExamples.Where(e => e.IsThin).ToArray();
 
         /// <summary>
+        /// Sets up the fixture.
+        /// </summary>
+        [TestFixtureSetUp]
+        public void FixtureSetUp()
+        {
+            Ignition.Start(TestUtils.GetTestConfiguration());
+        }
+
+        /// <summary>
+        /// Tears down the fixture.
+        /// </summary>
+        [TestFixtureTearDown]
+        public void FixtureTearDown()
+        {
+            Ignition.StopAll(true);
+        }
+
+        /// <summary>
         /// Tests the thin client example
         /// </summary>
         [Test, TestCaseSource(nameof(ThinExamples))]
