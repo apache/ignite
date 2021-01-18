8421f35fb34b4899f996808f9f8bf2e99884a904
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-01-18 22:52:10 +0300

    store

--- modules/platforms/dotnet/examples/Thick/Cache/Store/Program.cs
+++ modules/platforms/dotnet/examples/Thick/Cache/Store/Program.cs
@@ -18,24 +18,87 @@
 namespace IgniteExamples.Thick.Cache.Store
 {
     using System;
+    using System.Collections.Generic;
     using Apache.Ignite.Core;
+    using Apache.Ignite.Core.Cache.Configuration;
     using IgniteExamples.Shared;
+    using IgniteExamples.Shared.Cache;
+    using IgniteExamples.Shared.Models;
 
     /// <summary>
-    /// TODO
+    /// This example demonstrates a custom cache store usage.
     /// </summary>
     public class Program
     {
+        /// <summary>Cache name.</summary>
+        private const string CacheName = "dotnet_cache_with_store";
+
+        /// <summary>
+        /// Runs the example.
+        /// </summary>
+        [STAThread]
         public static void Main()
         {
-            using (IIgnite ignite = Ignition.Start(Utils.GetServerNodeConfiguration()))
+            using (var ignite = Ignition.Start(Utils.GetServerNodeConfiguration()))
             {
                 Console.WriteLine();
-                Console.WriteLine(">>> Example started.");
+                Console.WriteLine(">>> Cache store example started.");
 
-                // TODO
+                var cache = ignite.GetOrCreateCache<int, Employee>(new CacheConfiguration
+                {
+                    Name = CacheName,
+                    ReadThrough = true,
+                    WriteThrough = true,
+                    KeepBinaryInStore = false,  // Cache store works with deserialized data.
+                    CacheStoreFactory = new EmployeeStoreFactory()
+                });
+
+                // Clean up caches on all nodes before run.
+                cache.Clear();
 
                 Console.WriteLine();
+                Console.WriteLine(">>> Cleared values from cache.");
+                Console.WriteLine(">>> Current cache size: " + cache.GetSize());
+
+                // Load entries from store which pass provided filter.
+                cache.LoadCache(new EmployeeStorePredicate());
+
+                Console.WriteLine();
+                Console.WriteLine(">>> Loaded entry from store through ICache.LoadCache().");
+                Console.WriteLine(">>> Current cache size: " + cache.GetSize());
+
+                // Load entry from store calling ICache.Get() method.
+                Employee emp = cache.Get(2);
+
+                Console.WriteLine();
+                Console.WriteLine(">>> Loaded entry from store through ICache.Get(): " + emp);
+                Console.WriteLine(">>> Current cache size: " + cache.GetSize());
+
+                // Put an entry to the cache
+                cache.Put(3, new Employee(
+                    "James Wilson",
+                    12500,
+                    new Address("1096 Eddy Street, San Francisco, CA", 94109),
+                    new List<string> { "Human Resources", "Customer Service" }
+                    ));
+
+                Console.WriteLine();
+                Console.WriteLine(">>> Put entry to cache. ");
+                Console.WriteLine(">>> Current cache size: " + cache.GetSize());
+
+                // Clear values again.
+                cache.Clear();
+
+                Console.WriteLine();
+                Console.WriteLine(">>> Cleared values from cache again.");
+                Console.WriteLine(">>> Current cache size: " + cache.GetSize());
+
+                // Read values from cache after clear.
+                Console.WriteLine();
+                Console.WriteLine(">>> Read values after clear:");
+
+                for (int i = 1; i <= 3; i++)
+                    Console.WriteLine(">>>     Key=" + i + ", value=" + cache.Get(i));
             }
 
             Console.WriteLine();
