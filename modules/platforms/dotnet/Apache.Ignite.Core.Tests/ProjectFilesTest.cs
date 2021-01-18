11a62346588b0cd9d04c5c22ec822ed11cbe208e
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-01-18 12:51:51 +0300

    Fixing ProjectFilesTest

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/ProjectFilesTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/ProjectFilesTest.cs
@@ -37,7 +37,10 @@ namespace Apache.Ignite.Core.Tests
         {
             var projFiles = TestUtils.GetDotNetSourceDir()
                 .GetFiles("*.csproj", SearchOption.AllDirectories)
-                .Where(x => !x.FullName.ToLower().Contains("dotnetcore") && !x.FullName.Contains("Benchmark"))
+                .Where(x => !x.FullName.ToLower().Contains("dotnetcore") && 
+                            !x.FullName.Contains("Benchmark") &&
+                            !x.FullName.Contains("templates") &&
+                            !x.FullName.Contains("examples"))
                 .ToArray();
 
             Assert.GreaterOrEqual(projFiles.Length, 7);
@@ -144,7 +147,8 @@ namespace Apache.Ignite.Core.Tests
             return TestUtils.GetDotNetSourceDir().GetFiles("*.csproj", SearchOption.AllDirectories)
                 .Where(x => x.Name != "Apache.Ignite.csproj" &&
                             !x.Name.Contains("Test") &&
-                            !x.Name.Contains("Example") &&
+                            !x.FullName.Contains("examples") &&
+                            !x.FullName.Contains("templates") &&
                             !x.Name.Contains("DotNetCore") &&
                             !x.Name.Contains("Benchmark"));
         }
@@ -164,8 +168,10 @@ namespace Apache.Ignite.Core.Tests
         [Test]
         public void TestSlnToolsVersion()
         {
-            var slnFiles = TestUtils.GetDotNetSourceDir().GetFiles("*.sln", SearchOption.AllDirectories)
-                .Where(x => !x.Name.Contains("DotNetCore")).ToArray();
+            var slnFiles = TestUtils.GetDotNetSourceDir()
+                .GetFiles("*.sln", SearchOption.AllDirectories)
+                .Where(x => !x.Name.Contains("DotNetCore") && !x.Name.Contains("Examples"))
+                .ToArray();
 
             Assert.GreaterOrEqual(slnFiles.Length, 2);
             CheckFiles(slnFiles, x => !x.Contains("# Visual Studio 2010") ||
