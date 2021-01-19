a02d0a2e9b11809d4c9a911439bef08e13e73f4e
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-01-19 16:04:45 +0300

    Add ThickExamplesExternalNodeTest

---
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Examples/ThickExamplesExternalNodeTest.cs
@@ -0,0 +1,48 @@
+namespace Apache.Ignite.Core.Tests.Examples
+{
+    using System.Linq;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests thick examples.
+    /// </summary>
+    [Category(TestUtils.CategoryExamples)]
+    public class ThickExamplesExternalNodeTest
+    {
+        /** */
+        private static readonly Example[] ThickExamples = Example.AllExamples
+            .Where(e => !e.IsThin && !e.IsClient && e.RequiresExternalNode)
+            .ToArray();
+
+        /// <summary>
+        /// Sets up the fixture.
+        /// </summary>
+        [TestFixtureSetUp]
+        public void FixtureSetUp()
+        {
+            Ignition.Start(TestUtils.GetTestConfiguration(name: "server"));
+        }
+
+        /// <summary>
+        /// Tears down the fixture.
+        /// </summary>
+        [TestFixtureTearDown]
+        public void FixtureTearDown()
+        {
+            Ignition.StopAll(true);
+        }
+
+        /// <summary>
+        /// Tests thick mode example.
+        /// </summary>
+        [Test, TestCaseSource(nameof(ThickExamples))]
+        public void TestThickExampleWithExternalNode(Example example)
+        {
+            // TODO: Verify required output ("example started", "example finished").
+            Assert.IsFalse(example.IsThin);
+            Assert.IsTrue(example.RequiresExternalNode);
+
+            example.Run();
+        }
+    }
+}
