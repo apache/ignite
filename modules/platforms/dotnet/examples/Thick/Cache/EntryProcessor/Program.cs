9b6aa64b7fb5baa49a7e536bf5aab33c9b952200
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-01-22 08:32:33 +0300

    IGNITE-13979 .NET: Modernize examples
    * Target .NET Core 2.1
    * Every example resides in a separate project for better CLI and IDE experience
    * NuGet-based: `examples` folder can be downloaded independently and everything just works
    * Easy standalone node startup with `ServerNode` project
    * More Thin Client examples
    * Deliver examples as a project template with `dotnet new`: Apache.Ignite.Examples NuGet package added
    * Nicer README with screenshots

---
+++ modules/platforms/dotnet/examples/Thick/Cache/EntryProcessor/Program.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Examples.Thick.Cache.EntryProcessor
+{
+    using System;
+    using System.Linq;
+    using Apache.Ignite.Core;
+    using Apache.Ignite.Core.Cache;
+    using Apache.Ignite.Examples.Shared;
+    using Apache.Ignite.Examples.Shared.Cache;
+
+    /// <summary>
+    /// This example demonstrates the affinity collocation of a closure with data
+    /// by creating and modifying cache entries via an EntryProcessor.
+    /// </summary>
+    public static class Program
+    {
+        private const string CacheName = "dotnet_cache_entry_processor";
+
+        private const int EntryCount = 20;
+
+        public static void Main()
+        {
+            using (IIgnite ignite = Ignition.Start(Utils.GetServerNodeConfiguration()))
+            {
+                Console.WriteLine();
+                Console.WriteLine(">>> Cache EntryProcessor example started.");
+
+                ICache<int, int> cache = ignite.GetOrCreateCache<int, int>(CacheName);
+                cache.Clear();
+
+                // Populate cache with Invoke.
+                int[] keys = Enumerable.Range(1, EntryCount).ToArray();
+
+                foreach (var key in keys)
+                    cache.Invoke(key, new CachePutEntryProcessor(), 10);
+
+                PrintCacheEntries(cache);
+
+                // Increment entries by 5 with InvokeAll.
+                cache.InvokeAll(keys, new CacheIncrementEntryProcessor(), 5);
+
+                PrintCacheEntries(cache);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(">>> Example finished, press any key to exit ...");
+            Console.ReadKey();
+        }
+
+        private static void PrintCacheEntries(ICache<int, int> cache)
+        {
+            Console.WriteLine("\n>>> Entries in cache:");
+
+            foreach (var entry in cache)
+                Console.WriteLine(entry);
+        }
+    }
+}
