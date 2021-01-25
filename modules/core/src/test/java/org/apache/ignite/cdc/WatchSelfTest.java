cc2dea44c09f6220848d4a921d1b9f5dd17976b3
Nikolay Izhikov <nizhikov@apache.org>
2021-01-26 00:14:40 +0300

    IGNITE-13596: Tests fixed.

--- modules/core/src/test/java/org/apache/ignite/cdc/WatchSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/cdc/WatchSelfTest.java
@@ -59,20 +59,17 @@ public class WatchSelfTest extends GridCommonAbstractTest {
     /** {@inheritDoc} */
     @Override protected void afterTest() throws Exception {
         Files.walkFileTree(root, new SimpleFileVisitor<Path>() {
-            @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            @Override public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
                 Files.delete(file);
                 return FileVisitResult.CONTINUE;
             }
 
-            @Override
-            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+            @Override public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
                 Files.delete(file);
                 return FileVisitResult.CONTINUE;
             }
 
-            @Override
-            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            @Override public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
                 if (exc != null) {
                     throw exc;
                 }
