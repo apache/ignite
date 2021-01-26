5089f0c8c3b4a572c705f4636e72a8a89fb660fa
Ivan Dashchinskiy <ivandasch@gmail.com>
2021-01-26 12:17:15 +0300

    Return ability to specify custom image to run tests

--- modules/ducktests/tests/docker/run_tests.sh
+++ modules/ducktests/tests/docker/run_tests.sh
@@ -129,14 +129,19 @@ while [[ $# -ge 1 ]]; do
         -n|--num-nodes) IGNITE_NUM_CONTAINERS="$2"; shift 2;;
         -j|--max-parallel) MAX_PARALLEL="$2"; shift 2;;
         --jdk) JDK_VERSION="$2"; shift 2;;
+        --image) IMAGE_NAME="$2"; shift 2;;
         -f|--force) FORCE=$1; shift;;
         *) break;;
     esac
 done
 
-
-IMAGE_NAME="$IMAGE_PREFIX-$JDK_VERSION"
-"$SCRIPT_DIR"/ducker-ignite build -j "openjdk:$JDK_VERSION" $IMAGE_NAME || die "ducker-ignite build failed"
+if [ -z "$IMAGE_NAME" ]; then
+    IMAGE_NAME="$IMAGE_PREFIX-$JDK_VERSION"
+    "$SCRIPT_DIR"/ducker-ignite build -j "openjdk:$JDK_VERSION" $IMAGE_NAME || die "ducker-ignite build failed"
+else
+    echo "[WARN] Used non-default image $IMAGE_NAME. Be sure you use actual version of the image. " \
+         "Otherwise build it with 'ducker-ignite build' command"
+fi
 
 if [ -z "$FORCE" ]; then
     # If docker image changed then restart cluster (down here and up within next step)
