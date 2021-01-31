ab89f6e4dd2e094cf57c7fe43a9db8cef7a85ccc
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-01-31 10:23:57 +0300

    Fix multiple generic args support

--- modules/core/src/main/java/org/apache/ignite/binary/BinaryBasicNameMapper.java
+++ modules/core/src/main/java/org/apache/ignite/binary/BinaryBasicNameMapper.java
@@ -91,16 +91,13 @@ public class BinaryBasicNameMapper implements BinaryNameMapper {
         // Example: Foo.Bar`1[[Baz.Qux`1[[System.String],[System.Int32]]]]
         int genericIdx = clsName.indexOf("[[");
 
-        if (genericIdx > 0) {
-            int genericPartIdx = clsName.indexOf("],[", genericIdx);
+        if (genericIdx > 0)
+            clsName = clsName.substring(0, genericIdx + 2) + simpleName(clsName.substring(genericIdx + 2));
 
-            if (genericPartIdx > genericIdx)
-                clsName = clsName.substring(0, genericIdx + 2) +
-                        simpleName(clsName.substring(genericIdx + 2, genericPartIdx + 3)) +
-                        simpleName(clsName.substring(genericPartIdx + 3));
-            else
-                clsName = clsName.substring(0, genericIdx + 2) + simpleName(clsName.substring(genericIdx + 2));
-        }
+        genericIdx = clsName.indexOf("],[", genericIdx);
+
+        if (genericIdx > 0)
+            clsName = clsName.substring(0, genericIdx + 3) + simpleName(clsName.substring(genericIdx + 3));
 
         int idx = clsName.lastIndexOf('$');
 
