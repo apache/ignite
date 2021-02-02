af9e05705bc944ad4005a4e9bff6647d33d20325
Nikolay Izhikov <nizhikov@apache.org>
2021-02-02 13:00:32 +0300

    IGNITE-14071 .NET: RegisterSameJavaType mode enabled for messaging
    (cherry picked from commit 4f7579d7275a3fc9d82601395ac51c4878268205)

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Services/IJavaService.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Services/IJavaService.cs
@@ -217,6 +217,15 @@ namespace Apache.Ignite.Core.Tests.Services
         void sleep(long delayMs);
 
         /** */
+        void startReceiveMessage();
+
+        /** */
+        bool testMessagesReceived();
+
+        /** */
+        void testSendMessage();
+
+        /** */
         object contextAttribute(string name);
     }
 
