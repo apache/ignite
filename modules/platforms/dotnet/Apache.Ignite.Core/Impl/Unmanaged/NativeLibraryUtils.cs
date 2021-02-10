d59317cd1cc1326f2c1f234762ec72d672e50b9b
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-02-10 19:21:39 +0300

    wip NativeLibraryUtils

---
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Unmanaged/NativeLibraryUtils.cs
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Core.Impl.Unmanaged
+{
+    using System;
+    using System.Reflection;
+    using System.Runtime.InteropServices;
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static class NativeLibraryUtils
+    {
+        public static void SetDllImportResolvers()
+        {
+            // Init custom resolver for .NET 5+ single-file apps.
+            // TODO: Do it with Reflection
+            // Type.GetType("System.Runtime.InteropServices.NativeLibrary")
+            // NativeLibrary.SetDllImportResolver(typeof(NativeLibraryUtils).Assembly,
+            //     (string libraryName, Assembly assembly, DllImportSearchPath searchPath) =>
+            //         Resolve(libraryName, assembly, searchPath));
+        }
+
+        private static IntPtr Resolve(string libraryName, Assembly assembly, DllImportSearchPath searchPath)
+        {
+            return libraryName == "libcoreclr.so"
+                ? (IntPtr) (-1)  // Self-referencing binary.
+                : IntPtr.Zero;   // Skip.
+        }
+    }
+}
