1b01aeb84d7c49baa248affa9d8a1c445317b5c3
Ижиков Николай Владимирович <NVIzhikov.SBT@sberbank.ru>
2021-02-17 19:35:18 +0300

    IGN-2681 .NET: Enable RegisterSameJavaType for Cache API
    Merge in OSPT/ignite from IGN-2681 to master
    
    Squashed commit of the following:
    
    commit 52c96c308aca0e2baeb7a1c476d990a215d69810
    Author: Nikolay Izhikov <nizhikov@apache.org>
    Date:   Wed Feb 17 18:42:57 2021 +0300
    
        IGN-2681: Enable RegisterSameJavaType mode for a cache operations.
    
    (cherry picked from commit 330ef81ed2e678d51928d903f861fd0170a1cafd)

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Services/PlatformTestService.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Services/PlatformTestService.cs
@@ -642,6 +642,23 @@ namespace Apache.Ignite.Core.Tests.Services
             throw new NotImplementedException();
         }
 
+        public void putValsForCache()
+        {
+            _ignite.GetOrCreateCache<int, V9>("V9").Put(1, new V9 {Name = "1"});
+
+            var v10 = _ignite.GetOrCreateCache<int, V10>("V10");
+
+            v10.Put(1, new V10 {Name = "1"});
+            v10.Put(2, new V10 {Name = "2"});
+
+            _ignite.GetOrCreateCache<int, V11>("V11").Put(1, new V11 {Name = "1"});
+
+            var v12 = _ignite.GetOrCreateCache<int, V12>("V12");
+
+            v12.Put(1, new V12 {Name = "1"});
+            v12.Put(2, new V12 {Name = "2"});
+        }
+
         /** <inheritDoc /> */
         public object testRoundtrip(object x)
         {
