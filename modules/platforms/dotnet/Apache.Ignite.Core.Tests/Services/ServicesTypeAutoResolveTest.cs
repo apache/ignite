af32d44988775a2763a5a386ea7fd1095ff12c17
Ижиков Николай Владимирович <NVIzhikov.SBT@sberbank.ru>
2021-02-17 19:35:18 +0300

    IGN-2681 .NET: Enable RegisterSameJavaType for Cache API
    Merge in OSPT/ignite from IGN-2681 to master
    
    Squashed commit of the following:
    
    commit 52c96c308aca0e2baeb7a1c476d990a215d69810
    Author: Nikolay Izhikov <nizhikov@apache.org>
    Date:   Wed Feb 17 18:42:57 2021 +0300
    
        IGN-2681: Enable RegisterSameJavaType mode for a cache operations.
    
    (cherry picked from commit 330ef81ed2e678d51928d903f861fd0170a1cafd)

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Services/ServicesTypeAutoResolveTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Services/ServicesTypeAutoResolveTest.cs
@@ -284,6 +284,36 @@ namespace Apache.Ignite.Core.Tests.Services
 
             if (_useBinaryArray)
                 Assert.AreEqual(typeof(User[]), users2.GetType());
+
+            svc.putValsForCache();
+
+            Assert.AreEqual("1", _grid1.GetCache<int, V9>("V9").Get(1).Name);
+
+            var v10 = _grid1.GetCache<int, V10>("V10").GetAll(new List<int> {1, 2});
+
+            Assert.AreEqual(2, v10.Count);
+
+            foreach (var entry in v10)
+            {
+                if (entry.Key == 1)
+                    Assert.AreEqual("1", entry.Value.Name);
+                else
+                    Assert.AreEqual("2", entry.Value.Name);
+            }
+
+            Assert.AreEqual("1", _grid1.GetCache<int, V11>("V11").GetAsync(1).Result.Name);
+
+            var v12 = _grid1.GetCache<int, V12>("V12").GetAllAsync(new List<int> {1, 2}).Result;
+
+            Assert.AreEqual(2, v12.Count);
+
+            foreach (var entry in v12)
+            {
+                if (entry.Key == 1)
+                    Assert.AreEqual("1", entry.Value.Name);
+                else
+                    Assert.AreEqual("2", entry.Value.Name);
+            }
         }
 
         /// <summary>
