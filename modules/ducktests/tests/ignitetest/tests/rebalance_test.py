0dd03568e266b4b8e8bf0ec80f04e22ba20c76f5
Dmitriy Sorokin <sbt.sorokin.dvl@gmail.com>
2021-03-02 12:06:00 +0300

    IGNITE-14228 Ducktape test of rebalance for in-memory mode

--- modules/ducktests/tests/ignitetest/tests/rebalance_test.py
+++ modules/ducktests/tests/ignitetest/tests/rebalance_test.py
@@ -16,8 +16,6 @@
 """
 Module contains rebalance tests.
 """
-from enum import IntEnum
-
 from ducktape.mark import defaults
 
 from ignitetest.services.ignite import IgniteService
@@ -25,27 +23,8 @@ from ignitetest.services.ignite_app import IgniteApplicationService
 from ignitetest.services.utils.ignite_configuration import IgniteConfiguration
 from ignitetest.services.utils.ignite_configuration.discovery import from_ignite_cluster
 from ignitetest.utils import cluster, ignite_versions
-from ignitetest.utils.enum import constructible
 from ignitetest.utils.ignite_test import IgniteTest
 from ignitetest.utils.version import IgniteVersion, DEV_BRANCH, LATEST
-
-
-@constructible
-class Mode(IntEnum):
-    """
-    Rebalance mode.
-    """
-    IN_MEMORY = 0
-    PERSISTENT = 1
-
-
-@constructible
-class TriggerEvent(IntEnum):
-    """
-    Rebalance trigger event.
-    """
-    NODE_JOIN = 0
-    NODE_LEFT = 1
 
 
 # pylint: disable=W0223
@@ -57,19 +36,19 @@ class RebalanceTest(IgniteTest):
     PRELOAD_TIMEOUT = 60
     REBALANCE_TIMEOUT = 60
 
+    # pylint: disable=too-many-arguments
     @cluster(num_nodes=NUM_NODES)
     @ignite_versions(str(DEV_BRANCH), str(LATEST))
-    @defaults(trigger_event=[TriggerEvent.NODE_JOIN, TriggerEvent.NODE_LEFT],
-              mode=[Mode.IN_MEMORY], cache_count=[1], entry_count=[10000], entry_size=[1000],
+    @defaults(cache_count=[1], entry_count=[10000], entry_size=[1000],
               rebalance_thread_pool_size=[2], rebalance_batch_size=[512 * 1024], rebalance_throttle=[0])
-    def test_rebalance(self, ignite_version, trigger_event,
-                       mode, cache_count, entry_count, entry_size,
-                       rebalance_thread_pool_size, rebalance_batch_size, rebalance_throttle):
+    def test_rebalance_on_node_join(self, ignite_version,
+                                    cache_count, entry_count, entry_size,
+                                    rebalance_thread_pool_size, rebalance_batch_size, rebalance_throttle):
         """
         Test performs rebalance test which consists of following steps:
             * Start cluster.
             * Put data to it via IgniteClientApp.
-            * Triggering a rebalance event (node enter or leave) and awaits for rebalance to finish.
+            * Triggering a rebalance event (node join) and awaits for rebalance to finish.
         """
         node_config = IgniteConfiguration(
             version=IgniteVersion(ignite_version),
@@ -77,45 +56,92 @@ class RebalanceTest(IgniteTest):
             rebalance_batch_size=rebalance_batch_size,
             rebalance_throttle=rebalance_throttle)
 
-        node_count = len(self.test_context.cluster) - (2 if trigger_event is TriggerEvent.NODE_JOIN else 1)
-
-        ignites = IgniteService(self.test_context, config=node_config, num_nodes=node_count)
+        ignites = IgniteService(self.test_context, config=node_config, num_nodes=len(self.test_context.cluster) - 2)
         ignites.start()
 
-        # This client just put some data to the cache.
+        self.preload_data(node_config._replace(client_mode=True, discovery_spi=from_ignite_cluster(ignites)),
+                          cache_count, entry_count, entry_size)
+
+        ignite = IgniteService(self.test_context, node_config._replace(discovery_spi=from_ignite_cluster(ignites)),
+                               num_nodes=1)
+        ignite.start()
+
+        self.await_rebalance_start(ignite)
+
+        start = self.monotonic()
+
+        self.await_rebalance_complete(ignite)
+
+        return {"Rebalanced in (sec)": self.monotonic() - start}
+
+    # pylint: disable=too-many-arguments
+    @cluster(num_nodes=NUM_NODES)
+    @ignite_versions(str(DEV_BRANCH), str(LATEST))
+    @defaults(cache_count=[1], entry_count=[10000], entry_size=[1000],
+              rebalance_thread_pool_size=[2], rebalance_batch_size=[512 * 1024], rebalance_throttle=[0])
+    def test_rebalance_on_node_left(self, ignite_version,
+                                    cache_count, entry_count, entry_size,
+                                    rebalance_thread_pool_size, rebalance_batch_size, rebalance_throttle):
+        """
+        Test performs rebalance test which consists of following steps:
+            * Start cluster.
+            * Put data to it via IgniteClientApp.
+            * Triggering a rebalance event (node left) and awaits for rebalance to finish.
+        """
+        node_config = IgniteConfiguration(
+            version=IgniteVersion(ignite_version),
+            rebalance_thread_pool_size=rebalance_thread_pool_size,
+            rebalance_batch_size=rebalance_batch_size,
+            rebalance_throttle=rebalance_throttle)
+
+        ignites = IgniteService(self.test_context, config=node_config, num_nodes=len(self.test_context.cluster) - 1)
+        ignites.start()
+
+        self.preload_data(node_config._replace(client_mode=True, discovery_spi=from_ignite_cluster(ignites)),
+                          cache_count, entry_count, entry_size)
+
+        ignites.stop_node(ignites.nodes[len(ignites.nodes) - 1])
+
+        self.await_rebalance_start(ignites)
+
+        start = self.monotonic()
+
+        self.await_rebalance_complete(ignites)
+
+        return {"Rebalanced in (sec)": self.monotonic() - start}
+
+    def preload_data(self, config, cache_count, entry_count, entry_size):
+        """
+        Puts entry_count of key-value pairs of entry_size bytes to cache_count caches.
+        """
         IgniteApplicationService(
             self.test_context,
-            config=node_config._replace(client_mode=True, discovery_spi=from_ignite_cluster(ignites)),
+            config=config,
             java_class_name="org.apache.ignite.internal.ducktest.tests.rebalance_test.DataGenerationApplication",
             params={"cacheCount": cache_count, "entryCount": entry_count, "entrySize": entry_size},
             startup_timeout_sec=self.PRELOAD_TIMEOUT
         ).run()
 
-        if trigger_event is TriggerEvent.NODE_JOIN:
-            ignite = IgniteService(self.test_context, node_config._replace(discovery_spi=from_ignite_cluster(ignites)),
-                                   num_nodes=1)
-            ignite.start()
-            lookup_node = ignite.nodes[0]
-        else:
-            ignites.stop_node(ignites.nodes[node_count - 1])
-            lookup_node = ignites.nodes[0]
-
-        ignites.await_event_on_node(
+    @staticmethod
+    def await_rebalance_start(ignite):
+        """
+        Awaits rebalance starting on some test-cache
+        """
+        ignite.await_event_on_node(
             "Starting rebalance routine \\[test-cache-",
-            lookup_node,
+            ignite.nodes[0],
             timeout_sec=RebalanceTest.REBALANCE_TIMEOUT,
             from_the_beginning=True,
             backoff_sec=1)
 
-        start = self.monotonic()
-
-        ignites.await_event_on_node(
+    @staticmethod
+    def await_rebalance_complete(ignite):
+        """
+        Awaits rebalance complete on some test-cache
+        """
+        ignite.await_event_on_node(
             "Completed rebalance future: RebalanceFuture \\[state=STARTED, grp=CacheGroupContext \\[grp=test-cache-",
-            lookup_node,
+            ignite.nodes[0],
             timeout_sec=RebalanceTest.REBALANCE_TIMEOUT,
             from_the_beginning=True,
             backoff_sec=1)
-
-        data = {"Rebalanced in (sec)": self.monotonic() - start}
-
-        return data
