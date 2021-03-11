d49122f680dcecb026429c8fe32f9e6dbb1ba7d9
Yury Gerzhedovich <ygerzhedovich@gridgain.com>
2021-03-11 10:45:23 +0300

    IGNITE-14275: fixes after review

--- modules/calcite/src/test/java/org/apache/ignite/internal/processors/query/calcite/planner/AggregatePlannerTest.java
+++ modules/calcite/src/test/java/org/apache/ignite/internal/processors/query/calcite/planner/AggregatePlannerTest.java
@@ -28,6 +28,7 @@ import org.apache.calcite.rel.RelCollations;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
 import org.apache.calcite.sql.SqlExplainLevel;
 import org.apache.calcite.sql.fun.SqlAvgAggFunction;
+import org.apache.calcite.sql.fun.SqlCountAggFunction;
 import org.apache.calcite.util.ImmutableIntList;
 import org.apache.ignite.internal.processors.query.calcite.metadata.ColocationGroup;
 import org.apache.ignite.internal.processors.query.calcite.prepare.PlanningContext;
@@ -51,6 +52,7 @@ import org.apache.ignite.internal.processors.query.calcite.type.IgniteTypeSystem
 import org.apache.ignite.internal.util.typedef.F;
 import org.apache.ignite.testframework.GridTestUtils;
 import org.hamcrest.core.IsInstanceOf;
+import org.jetbrains.annotations.NotNull;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -78,22 +80,7 @@ public class AggregatePlannerTest extends AbstractPlannerTest {
      */
     @Test
     public void singleWithoutIndex() throws Exception {
-        IgniteTypeFactory f = new IgniteTypeFactory(IgniteTypeSystem.INSTANCE);
-
-        TestTable tbl = new TestTable(
-            new RelDataTypeFactory.Builder(f)
-                .add("ID", f.createJavaType(Integer.class))
-                .add("VAL0", f.createJavaType(Integer.class))
-                .add("VAL1", f.createJavaType(Integer.class))
-                .add("GRP0", f.createJavaType(Integer.class))
-                .add("GRP1", f.createJavaType(Integer.class))
-                .build()) {
-
-            @Override public IgniteDistribution distribution() {
-                return IgniteDistributions.broadcast();
-            }
-        }
-            .addIndex(RelCollations.of(ImmutableIntList.of(1, 2)), "val0_val1");
+        TestTable tbl = createBroadcastTable().addIndex(RelCollations.of(ImmutableIntList.of(1, 2)), "val0_val1");
 
         IgniteSchema publicSchema = new IgniteSchema("PUBLIC");
 
@@ -129,20 +116,7 @@ public class AggregatePlannerTest extends AbstractPlannerTest {
     public void singleWithIndex() throws Exception {
         IgniteTypeFactory f = new IgniteTypeFactory(IgniteTypeSystem.INSTANCE);
 
-        TestTable tbl = new TestTable(
-            new RelDataTypeFactory.Builder(f)
-                .add("ID", f.createJavaType(Integer.class))
-                .add("VAL0", f.createJavaType(Integer.class))
-                .add("VAL1", f.createJavaType(Integer.class))
-                .add("GRP0", f.createJavaType(Integer.class))
-                .add("GRP1", f.createJavaType(Integer.class))
-                .build()) {
-
-            @Override public IgniteDistribution distribution() {
-                return IgniteDistributions.broadcast();
-            }
-        }
-            .addIndex(RelCollations.of(ImmutableIntList.of(3, 4)), "grp0_grp1");
+        TestTable tbl = createBroadcastTable().addIndex(RelCollations.of(ImmutableIntList.of(3, 4)), "grp0_grp1");
 
         IgniteSchema publicSchema = new IgniteSchema("PUBLIC");
 
@@ -176,31 +150,7 @@ public class AggregatePlannerTest extends AbstractPlannerTest {
      */
     @Test
     public void mapReduceGroupBy() throws Exception {
-        IgniteTypeFactory f = new IgniteTypeFactory(IgniteTypeSystem.INSTANCE);
-
-        TestTable tbl = new TestTable(
-            new RelDataTypeFactory.Builder(f)
-                .add("ID", f.createJavaType(Integer.class))
-                .add("VAL0", f.createJavaType(Integer.class))
-                .add("VAL1", f.createJavaType(Integer.class))
-                .add("GRP0", f.createJavaType(Integer.class))
-                .add("GRP1", f.createJavaType(Integer.class))
-                .build()) {
-
-            @Override public ColocationGroup colocationGroup(PlanningContext ctx) {
-                return ColocationGroup.forAssignments(Arrays.asList(
-                    select(nodes, 0, 1),
-                    select(nodes, 1, 2),
-                    select(nodes, 2, 0),
-                    select(nodes, 0, 1),
-                    select(nodes, 1, 2)
-                ));
-            }
-
-            @Override public IgniteDistribution distribution() {
-                return IgniteDistributions.affinity(0, "test", "hash");
-            }
-        };
+        TestTable tbl = createAffinityTable();
 
         IgniteSchema publicSchema = new IgniteSchema("PUBLIC");
 
@@ -245,30 +195,7 @@ public class AggregatePlannerTest extends AbstractPlannerTest {
     public void mapReduceDistinctWithIndex() throws Exception {
         IgniteTypeFactory f = new IgniteTypeFactory(IgniteTypeSystem.INSTANCE);
 
-        TestTable tbl = new TestTable(
-            new RelDataTypeFactory.Builder(f)
-                .add("ID", f.createJavaType(Integer.class))
-                .add("VAL0", f.createJavaType(Integer.class))
-                .add("VAL1", f.createJavaType(Integer.class))
-                .add("GRP0", f.createJavaType(Integer.class))
-                .add("GRP1", f.createJavaType(Integer.class))
-                .build()) {
-
-            @Override public ColocationGroup colocationGroup(PlanningContext ctx) {
-                return ColocationGroup.forAssignments(Arrays.asList(
-                    select(nodes, 0, 1),
-                    select(nodes, 1, 2),
-                    select(nodes, 2, 0),
-                    select(nodes, 0, 1),
-                    select(nodes, 1, 2)
-                ));
-            }
-
-            @Override public IgniteDistribution distribution() {
-                return IgniteDistributions.affinity(0, "test", "hash");
-            }
-        }
-            .addIndex(RelCollations.of(ImmutableIntList.of(1, 2)), "val0_val1");
+        TestTable tbl = createAffinityTable().addIndex(RelCollations.of(ImmutableIntList.of(1, 2)), "val0_val1");
 
         IgniteSchema publicSchema = new IgniteSchema("PUBLIC");
 
@@ -302,15 +229,97 @@ public class AggregatePlannerTest extends AbstractPlannerTest {
             assertNotNull(findFirstNode(phys, byClass(IgniteIndexScan.class)));
     }
 
-    /** */
+    /**
+     *
+     */
     @Test
-    public void notApplicableForSortAggregate() {
-        if (algo == AggregateAlgorithm.HASH)
+    public void notApplicableForSortAggregate() throws Exception {
+        if (algo == AggregateAlgorithm.SORT)
             return;
 
+        TestTable tbl = createAffinityTable().addIndex(RelCollations.of(ImmutableIntList.of(1, 2)), "val0_val1");
+
+        IgniteSchema publicSchema = new IgniteSchema("PUBLIC");
+
+        publicSchema.addTable("TEST", tbl);
+
+        String sqlMin = "SELECT MIN(val0) FROM test";
+
+        GridTestUtils.assertThrows(log,
+            () -> physicalPlan(
+                sqlMin,
+                publicSchema,
+                "HashAggregateConverterRule"
+            ),
+            RelOptPlanner.CannotPlanException.class,
+            "There are not enough rules to produce a node with desired properties"
+        );
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void noGroupByAggregate() throws Exception {
+        if (algo == AggregateAlgorithm.SORT)
+            return;
+
+        TestTable tbl = createAffinityTable().addIndex(RelCollations.of(ImmutableIntList.of(1, 2)), "val0_val1");
+
+        IgniteSchema publicSchema = new IgniteSchema("PUBLIC");
+
+        publicSchema.addTable("TEST", tbl);
+
+        String sqlCount = "SELECT COUNT(*) FROM test";
+
+        IgniteRel phys = physicalPlan(
+            sqlCount,
+            publicSchema,
+            algo.ruleToDisable
+        );
+
+        checkSplitAndSerialization(phys, publicSchema);
+
+        IgniteAggregate mapAgg = findFirstNode(phys, byClass(algo.map));
+        IgniteReduceAggregateBase rdcAgg = findFirstNode(phys, byClass(algo.reduce));
+
+        assertNotNull("Invalid plan\n" + RelOptUtil.toString(phys), rdcAgg);
+        assertNotNull("Invalid plan\n" + RelOptUtil.toString(phys), mapAgg);
+
+        Assert.assertThat(
+            "Invalid plan\n" + RelOptUtil.toString(phys),
+            F.first(rdcAgg.aggregateCalls()).getAggregation(),
+            IsInstanceOf.instanceOf(SqlCountAggFunction.class));
+
+        Assert.assertThat(
+            "Invalid plan\n" + RelOptUtil.toString(phys),
+            F.first(mapAgg.getAggCallList()).getAggregation(),
+            IsInstanceOf.instanceOf(SqlCountAggFunction.class));
+    }
+
+    @NotNull private TestTable createBroadcastTable() {
         IgniteTypeFactory f = new IgniteTypeFactory(IgniteTypeSystem.INSTANCE);
 
         TestTable tbl = new TestTable(
+            new RelDataTypeFactory.Builder(f)
+                .add("ID", f.createJavaType(Integer.class))
+                .add("VAL0", f.createJavaType(Integer.class))
+                .add("VAL1", f.createJavaType(Integer.class))
+                .add("GRP0", f.createJavaType(Integer.class))
+                .add("GRP1", f.createJavaType(Integer.class))
+                .build()) {
+
+            @Override public IgniteDistribution distribution() {
+                return IgniteDistributions.broadcast();
+            }
+        };
+        return tbl;
+    }
+
+    @NotNull private AbstractPlannerTest.TestTable createAffinityTable() {
+        IgniteTypeFactory f = new IgniteTypeFactory(IgniteTypeSystem.INSTANCE);
+
+        return new TestTable(
             new RelDataTypeFactory.Builder(f)
                 .add("ID", f.createJavaType(Integer.class))
                 .add("VAL0", f.createJavaType(Integer.class))
@@ -332,24 +341,7 @@ public class AggregatePlannerTest extends AbstractPlannerTest {
             @Override public IgniteDistribution distribution() {
                 return IgniteDistributions.affinity(0, "test", "hash");
             }
-        }
-            .addIndex(RelCollations.of(ImmutableIntList.of(1, 2)), "val0_val1");
-
-        IgniteSchema publicSchema = new IgniteSchema("PUBLIC");
-
-        publicSchema.addTable("TEST", tbl);
-
-        String sql = "SELECT MIN(val0) FROM test";
-
-        GridTestUtils.assertThrows(log,
-            () -> physicalPlan(
-                sql,
-                publicSchema,
-                "HashAggregateConverterRule"
-            ),
-            RelOptPlanner.CannotPlanException.class,
-            "There are not enough rules to produce a node with desired properties"
-        );
+        };
     }
 
     /** */
