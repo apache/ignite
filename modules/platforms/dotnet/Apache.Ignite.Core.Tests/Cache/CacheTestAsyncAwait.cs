be6112f157a7cafc740447f1570c537908db8e1c
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-03-11 14:49:44 +0300

    Add .NET tests CacheTestAsyncAwait

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Cache/CacheTestAsyncAwait.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Cache/CacheTestAsyncAwait.cs
@@ -15,29 +15,30 @@
  * limitations under the License.
  */
 
-namespace Apache.Ignite.Core.Tests.Client.Cache
+namespace Apache.Ignite.Core.Tests.Cache
 {
     using System.Threading.Tasks;
-    using Apache.Ignite.Core.Tests.Client;
     using NUnit.Framework;
 
     /// <summary>
-    /// Tests thin cache operations with async/await.
+    /// Tests thick cache operations with async/await.
     /// </summary>
-    public class CacheTestAsyncAwait : ClientTestBase
+    public class CacheTestAsyncAwait : TestBase
     {
         /// <summary>
         /// Tests that async continuations are executed on a ThreadPool thread, not on response handler thread.
         /// </summary>
         [Test]
-        public async Task TestAsyncAwaitContinuationIsExecutedOnThreadPool()
+        public async Task TestAsyncAwaitContinuationIsExecutedWithConfiguredExecutor()
         {
-            var cache = GetClientCache<int>();
-            await cache.PutAsync(1, 1).ConfigureAwait(false);
+            var cache = Ignite.GetOrCreateCache<int, int>(TestUtils.TestName);
 
-            // This causes deadlock if async continuation is executed on response handler thread.
-            cache.PutAsync(2, 2).Wait();
-            Assert.AreEqual(2, cache.Get(2));
+            // This causes deadlock if async continuation is executed on the striped thread.
+            await cache.PutAsync(1, 1);
+            await cache.PutAsync(1, 2);
+
+            Assert.AreEqual(2, cache.Get(1));
         }
+
     }
 }
