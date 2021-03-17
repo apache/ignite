d8cad34cbb37b57106807f9f24f9986b437bba72
Шишков Илья Валерьевич <Shishkov.I.Val@sberbank.ru>
2021-03-17 14:30:44 +0300

    IGN-2851 Add thin client compatibility for 4.281.3 and 4.290 (both server and client)
    Merge in OSPT/ignite from IGN-2851 to master
    
    Squashed commit of the following:
    
    commit e09af126a5438b4e2f0237739e96d0dae1c8318a
    Author: Shishkov Ilya <shishkovilja@yandex.ru>
    Date:   Tue Mar 16 15:23:18 2021 +0300
    
        IGN-2581 Fix potential NPE during protocol versions comparison
    
    commit c562149112c96946287de7ab3265313503f836a5
    Author: Shishkov Ilya <shishkovilja@yandex.ru>
    Date:   Tue Mar 16 15:13:06 2021 +0300
    
        IGN-2581 Make ProtocolContext#ise281Compatible non-static
    
    commit 71def878633380ae1da663154521116147be1cda
    Author: Shishkov Ilya <shishkovilja@yandex.ru>
    Date:   Tue Mar 16 14:26:11 2021 +0300
    
        IGN-2581 Make ProtocolContext#ise281Compatible non-static
    
    commit f23ef32089f53e6c05c1204a34bea2c1e35f9efc
    Author: Shishkov Ilya <shishkovilja@yandex.ru>
    Date:   Mon Mar 15 17:50:07 2021 +0300
    
        IGN-2581 Add ThinClientPermissionCheckSecurityWith281CompatibilityTest to SecurityTestSuite
    
    commit 72de08b29680cdfaf0d8d6bc6a9bbf7eedcfc099
    Author: Shishkov Ilya <shishkovilja@yandex.ru>
    Date:   Mon Mar 15 15:20:44 2021 +0300
    
        IGN-2851 Fix code-style
    
    commit 128e98796278eb3924234c6486b4a63d96f484eb
    Author: Shishkov Ilya <shishkovilja@yandex.ru>
    Date:   Mon Mar 15 13:58:25 2021 +0300
    
        IGN-2851 Add client compatibility flag to IgniteSystemProperties, add extra test
    
    commit ae3fc31dda92fea456d35c06830c2f55a6bb7006
    Author: Shishkov Ilya <shishkovilja@yandex.ru>
    Date:   Mon Mar 15 12:09:48 2021 +0300
    
        IGN-2851 Replace ClientProtocolContext#setIse281Compatible() to constructor parameter
    
    commit 2bdc44bb9cec9a74f928573be8b1df9cbf29c1d7
    Author: Shishkov Ilya <shishkovilja@yandex.ru>
    Date:   Sat Mar 13 11:45:25 2021 +0300
    
        IGN-2851 Fix backward compatibility of 4.290 client with 4.281.3 server
    
    commit ab7d5158cb9531b8d9b424c4e50a5972124e2036
    Author: Shishkov Ilya <shishkovilja@yandex.ru>
    Date:   Fri Mar 12 10:14:08 2021 +0300
    
        IGN-2851 Fix backward compatibility of 4.290 server with 4.281.3 thin client

--- modules/core/src/test/java/org/apache/ignite/testsuites/SecurityTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/SecurityTestSuite.java
@@ -34,6 +34,7 @@ import org.apache.ignite.internal.processors.security.client.AdditionalSecurityC
 import org.apache.ignite.internal.processors.security.client.AttributeSecurityCheckTest;
 import org.apache.ignite.internal.processors.security.client.IgniteClientContainSubjectAddressTest;
 import org.apache.ignite.internal.processors.security.client.ThinClientPermissionCheckSecurityTest;
+import org.apache.ignite.internal.processors.security.client.ThinClientPermissionCheckSecurityWith281CompatibilityTest;
 import org.apache.ignite.internal.processors.security.client.ThinClientPermissionCheckTest;
 import org.apache.ignite.internal.processors.security.client.ThinClientSecurityContextOnRemoteNodeTest;
 import org.apache.ignite.internal.processors.security.client.ThinClientSslPermissionCheckTest;
@@ -84,6 +85,7 @@ import org.junit.runners.Suite;
     ComputePermissionCheckTest.class,
     ThinClientPermissionCheckTest.class,
     ThinClientPermissionCheckSecurityTest.class,
+    ThinClientPermissionCheckSecurityWith281CompatibilityTest.class,
     ContinuousQueryPermissionCheckTest.class,
     IgniteClientContainSubjectAddressTest.class,
     SnapshotPermissionCheckTest.class,
