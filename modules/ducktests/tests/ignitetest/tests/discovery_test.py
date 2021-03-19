109254520eb80d3061ae06b7c91b281acb5af6ff
Vladimir Steshin <vladsz83@gmail.com>
2021-03-19 15:20:35 +0300

    research

--- modules/ducktests/tests/ignitetest/tests/discovery_test.py
+++ modules/ducktests/tests/ignitetest/tests/discovery_test.py
@@ -92,8 +92,8 @@ class DiscoveryTest(IgniteTest):
 
     @cluster(num_nodes=MAX_CONTAINERS)
     @ignite_versions(str(DEV_BRANCH), str(LATEST))
-    @matrix(nodes_to_kill=[2], disable_conn_recovery=[True],
-            net_part=[IgniteService.NetPart.INPUT, IgniteService.NetPart.ALL],
+    @matrix(nodes_to_kill=[2], disable_conn_recovery=[False, True],
+            net_part=[IgniteService.NetPart.ALL, IgniteService.NetPart.INPUT],
             load_type=[ClusterLoad.ATOMIC, ClusterLoad.TRANSACTIONAL])
     def test_nodes_fail_not_sequential_tcp(self, ignite_version, nodes_to_kill, load_type, disable_conn_recovery: bool,
                                            net_part: IgniteService.NetPart):
@@ -109,7 +109,7 @@ class DiscoveryTest(IgniteTest):
     @cluster(num_nodes=MAX_CONTAINERS)
     @ignite_versions(str(DEV_BRANCH), str(LATEST))
     @matrix(load_type=[ClusterLoad.ATOMIC, ClusterLoad.TRANSACTIONAL],
-            net_part=[IgniteService.NetPart.INPUT, IgniteService.NetPart.ALL], disable_conn_recovery=[True])
+            net_part=[IgniteService.NetPart.ALL, IgniteService.NetPart.INPUT], disable_conn_recovery=[False, True])
     def test_2_nodes_fail_sequential_tcp(self, ignite_version, load_type, disable_conn_recovery: bool,
                                          net_part: IgniteService.NetPart):
         """
