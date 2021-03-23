baaf0b62f955bcba78e3aec5322defaeae22f852
Maksim Timonin <timonin.maxim@gmail.com>
2021-03-23 12:20:34 +0300

    Reduce count of rows to run faster on TC.

--- modules/compatibility/src/test/java/org/apache/ignite/compatibility/persistence/IndexTypesCompatibilityTest.java
+++ modules/compatibility/src/test/java/org/apache/ignite/compatibility/persistence/IndexTypesCompatibilityTest.java
@@ -52,7 +52,7 @@ public class IndexTypesCompatibilityTest extends IndexAbstractCompatibilityTest 
     private static final String TEST_CACHE_NAME = IndexTypesCompatibilityTest.class.getSimpleName();
 
     /** */
-    private static final int ROWS_CNT = 300;
+    private static final int ROWS_CNT = 100;
 
     /** */
     private static final String TABLE_PREFIX = "TABLE_";
