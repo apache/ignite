f2ccf1daa6532fff6386dacf74abf1af2719449f
Dmitriy Sorokin <sbt.sorokin.dvl@gmail.com>
2021-03-29 11:26:51 +0300

    IGNITE-14391 Multi-node cache data preloading

--- modules/ducktests/src/main/java/org/apache/ignite/internal/ducktest/tests/rebalance/DataGenerationApplication.java
+++ modules/ducktests/src/main/java/org/apache/ignite/internal/ducktest/tests/rebalance/DataGenerationApplication.java
@@ -67,10 +67,8 @@ public class DataGenerationApplication extends IgniteAwareApplication {
             for (int i = from; i < to; i++) {
                 stmr.addData(i, new DataModel(entrySize));
 
-                if ((i - from + 1) % logEach == 0) {
-                    if (log.isDebugEnabled())
-                        log.debug("Streamed " + (i - from + 1) + " entries into " + cacheName);
-                }
+                if ((i - from + 1) % logEach == 0 && log.isDebugEnabled())
+                    log.debug("Streamed " + (i - from + 1) + " entries into " + cacheName);
 
                 if (i % flushEach == 0)
                     stmr.flush();
