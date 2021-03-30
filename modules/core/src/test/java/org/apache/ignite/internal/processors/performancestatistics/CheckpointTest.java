342457576ca6f3cc5b11ad2ac7474a674c9276e4
Ryzhov Sergei <s.vi.ryzhov@gmail.com>
2021-03-30 20:42:02 +0300

    IGNITE-14385: update checkpoint test

--- modules/core/src/test/java/org/apache/ignite/internal/processors/performancestatistics/CheckpointTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/performancestatistics/CheckpointTest.java
@@ -17,8 +17,6 @@
 
 package org.apache.ignite.internal.processors.performancestatistics;
 
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import org.apache.ignite.cluster.ClusterState;
@@ -28,10 +26,12 @@ import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.internal.IgniteEx;
 import org.apache.ignite.internal.processors.metric.MetricRegistry;
 import org.apache.ignite.internal.processors.metric.impl.AtomicLongMetric;
-import org.apache.ignite.spi.metric.jmx.JmxMetricExporterSpi;
+import org.apache.ignite.testframework.ListeningTestLogger;
+import org.apache.ignite.testframework.LogListener;
 import org.junit.Test;
 
 import static org.apache.ignite.internal.processors.cache.persistence.DataStorageMetricsImpl.DATASTORAGE_METRIC_PREFIX;
+import static org.apache.ignite.testframework.GridTestUtils.waitForCondition;
 
 /**
  * Tests checkpoint performance statistics.
@@ -40,15 +40,18 @@ public class CheckpointTest extends AbstractPerformanceStatisticsTest {
     /** Ignite. */
     private static IgniteEx srv;
 
+    /** Listener test logger. */
+    private static ListeningTestLogger listeningLog;
+
     /** {@inheritDoc} */
     @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
         IgniteConfiguration cfg = super.getConfiguration(igniteInstanceName);
 
         cfg.setCacheConfiguration(defaultCacheConfiguration());
-        cfg.setMetricExporterSpi(new JmxMetricExporterSpi());
+        cfg.setGridLogger(listeningLog);
         cfg.setDataStorageConfiguration(new DataStorageConfiguration()
             .setMetricsEnabled(true)
-            .setCheckpointFrequency(10 * 1000)
+            .setCheckpointFrequency(1000)
             .setDefaultDataRegionConfiguration(new DataRegionConfiguration().setPersistenceEnabled(true)));
 
         return cfg;
@@ -56,6 +59,8 @@ public class CheckpointTest extends AbstractPerformanceStatisticsTest {
 
     /** {@inheritDoc} */
     @Override protected void beforeTestsStarted() throws Exception {
+        listeningLog = new ListeningTestLogger(log);
+
         cleanPersistenceDir();
 
         srv = startGrid();
@@ -70,69 +75,42 @@ public class CheckpointTest extends AbstractPerformanceStatisticsTest {
 
         startCollectStatistics();
 
-        srv.cluster().state(ClusterState.INACTIVE);
+        LogListener lsnr = LogListener.matches("Checkpoint finished")
+            .build();
 
-        AtomicBoolean checked = new AtomicBoolean(false);
-
-        stopCollectStatisticsAndRead(new TestHandler() {
-            @Override public void checkpoint(long beforeLockDuration, long lockWaitDuration, long listenersExecDuration,
-                long markDuration, long lockHoldDuration, long pagesWriteDuration, long fsyncDuration,
-                long walCpRecordFsyncDuration, long writeCheckpointEntryDuration, long splitAndSortCpPagesDuration,
-                long totalDuration, long checkpointStartTime, int pagesSize, int dataPagesWritten, int cowPagesWritten) {
-                checked.set(true);
-            }
-        });
-
-        assertTrue(checked.get());
-    }
-    /** @throws Exception If failed. */
-    @Test
-    public void testCheckpoint2() throws Exception {
-        cleanPerformanceStatisticsDir();
-
-        startCollectStatistics();
+        listeningLog.registerListener(lsnr);
 
         MetricRegistry mreg = srv.context().metric().registry(DATASTORAGE_METRIC_PREFIX);
 
         AtomicLongMetric lastCpBeforeLockDuration = mreg.findMetric("LastCheckpointBeforeLockDuration");
         AtomicLongMetric lastCpLockWaitDuration = mreg.findMetric("LastCheckpointLockWaitDuration");
-        AtomicLongMetric lastCpListenersExecuteDuration = mreg.findMetric("LastCheckpointListenersExecuteDuration");
-
+        AtomicLongMetric lastCpListenersExecDuration = mreg.findMetric("LastCheckpointListenersExecuteDuration");
         AtomicLongMetric lastCpMarcDuration = mreg.findMetric("LastCheckpointMarkDuration");
         AtomicLongMetric lastCpLockHoldDuration = mreg.findMetric("LastCheckpointLockHoldDuration");
         AtomicLongMetric lastCpPagesWriteDuration = mreg.findMetric("LastCheckpointPagesWriteDuration");
         AtomicLongMetric lastCpFsyncDuration = mreg.findMetric("LastCheckpointFsyncDuration");
-
         AtomicLongMetric lastCpWalRecordFsyncDuration = mreg.findMetric("LastCheckpointWalRecordFsyncDuration");
         AtomicLongMetric lastCpWriteEntryDuration = mreg.findMetric("LastCheckpointWriteEntryDuration");
         AtomicLongMetric lastCpSplitAndSortPagesDuration =
             mreg.findMetric("LastCheckpointSplitAndSortPagesDuration");
-
         AtomicLongMetric lastCpDuration = mreg.findMetric("LastCheckpointDuration");
         AtomicLongMetric lastCpStart = mreg.findMetric("LastCheckpointStart");
         AtomicLongMetric lastCpTotalPages = mreg.findMetric("LastCheckpointTotalPagesNumber");
         AtomicLongMetric lastCpDataPages = mreg.findMetric("LastCheckpointDataPagesNumber");
         AtomicLongMetric lastCpCOWPages = mreg.findMetric("LastCheckpointCopiedOnWritePagesNumber");
 
-//        srv.cluster().state(ClusterState.INACTIVE);
-
-        TimeUnit.SECONDS.sleep(15);
-
-        AtomicBoolean checked = new AtomicBoolean(false);
+        assertTrue(waitForCondition(lsnr::check, TIMEOUT));
 
         AtomicLong expLastCpBeforeLockDuration = new AtomicLong();
         AtomicLong expLastCpLockWaitDuration = new AtomicLong();
-        AtomicLong expLastCpListenersExecuteDuration = new AtomicLong();
-
+        AtomicLong expLastCpListenersExecDuration = new AtomicLong();
         AtomicLong expLastCpMarkDuration = new AtomicLong();
         AtomicLong expLastCpLockHoldDuration = new AtomicLong();
         AtomicLong expLastCpPagesWriteDuration = new AtomicLong();
         AtomicLong expLastCpFsyncDuration = new AtomicLong();
-
         AtomicLong expLastCpWalRecordFsyncDuration = new AtomicLong();
         AtomicLong expLastCpWriteEntryDuration = new AtomicLong();
         AtomicLong expLastCpSplitAndSortPagesDuration = new AtomicLong();
-
         AtomicLong expLastTotalDuration = new AtomicLong();
         AtomicLong expLastcCheckpointStartTime = new AtomicLong();
         AtomicInteger expPagesSize = new AtomicInteger();
@@ -143,45 +121,36 @@ public class CheckpointTest extends AbstractPerformanceStatisticsTest {
             @Override public void checkpoint(long beforeLockDuration, long lockWaitDuration, long listenersExecDuration,
                 long markDuration, long lockHoldDuration, long pagesWriteDuration, long fsyncDuration,
                 long walCpRecordFsyncDuration, long writeCheckpointEntryDuration, long splitAndSortCpPagesDuration,
-                long totalDuration, long checkpointStartTime, int pagesSize, int dataPagesWritten, int cowPagesWritten) {
+                long totalDuration, long checkpointStartTime, int pagesSize, int dataPagesWritten, int cowPagesWritten)
+            {
                 expLastCpBeforeLockDuration.set(beforeLockDuration);
                 expLastCpLockWaitDuration.set(lockWaitDuration);
-                expLastCpListenersExecuteDuration.set(listenersExecDuration);
-
+                expLastCpListenersExecDuration.set(listenersExecDuration);
                 expLastCpMarkDuration.set(markDuration);
                 expLastCpLockHoldDuration.set(lockHoldDuration);
                 expLastCpPagesWriteDuration.set(pagesWriteDuration);
                 expLastCpFsyncDuration.set(fsyncDuration);
-
                 expLastCpWalRecordFsyncDuration.set(walCpRecordFsyncDuration);
                 expLastCpWriteEntryDuration.set(writeCheckpointEntryDuration);
                 expLastCpSplitAndSortPagesDuration.set(splitAndSortCpPagesDuration);
-
                 expLastTotalDuration.set(totalDuration);
                 expLastcCheckpointStartTime.set(checkpointStartTime);
                 expPagesSize.set(pagesSize);
                 expDataPagesWritten.set(dataPagesWritten);
                 expCOWPagesWritten.set(cowPagesWritten);
-
-                checked.set(true);
             }
         });
 
-        assertTrue(checked.get());
-
         assertEquals(expLastCpBeforeLockDuration.get(), lastCpBeforeLockDuration.value());
         assertEquals(expLastCpLockWaitDuration.get(), lastCpLockWaitDuration.value());
-        assertEquals(expLastCpListenersExecuteDuration.get(), lastCpListenersExecuteDuration.value());
-
+        assertEquals(expLastCpListenersExecDuration.get(), lastCpListenersExecDuration.value());
         assertEquals(expLastCpMarkDuration.get(), lastCpMarcDuration.value());
         assertEquals(expLastCpLockHoldDuration.get(), lastCpLockHoldDuration.value());
         assertEquals(expLastCpPagesWriteDuration.get(), lastCpPagesWriteDuration.value());
         assertEquals(expLastCpFsyncDuration.get(), lastCpFsyncDuration.value());
-
         assertEquals(expLastCpWalRecordFsyncDuration.get(), lastCpWalRecordFsyncDuration.value());
         assertEquals(expLastCpWriteEntryDuration.get(), lastCpWriteEntryDuration.value());
         assertEquals(expLastCpSplitAndSortPagesDuration.get(), lastCpSplitAndSortPagesDuration.value());
-
         assertEquals(expLastTotalDuration.get(), lastCpDuration.value());
         assertEquals(expLastcCheckpointStartTime.get(), lastCpStart.value());
         assertEquals(expPagesSize.get(), lastCpTotalPages.value());
