5bee999c6f14e3ad996aaa9365d378f33e9c1195
Aleksey Plekhanov <plehanov.alex@gmail.com>
2021-04-08 13:31:08 +0300

    IGNITE-14402 Java thin client: Continuous queries support - Fixes #8960.
    Signed-off-by: Aleksey Plekhanov <plehanov.alex@gmail.com>

--- modules/core/src/test/java/org/apache/ignite/internal/client/thin/ComputeTaskTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/client/thin/ComputeTaskTest.java
@@ -35,8 +35,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
-
-import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteException;
 import org.apache.ignite.client.ClientCache;
 import org.apache.ignite.client.ClientClusterGroup;
@@ -51,11 +49,8 @@ import org.apache.ignite.compute.ComputeTaskName;
 import org.apache.ignite.configuration.ClientConnectorConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.configuration.ThinClientConfiguration;
-import org.apache.ignite.internal.processors.odbc.ClientListenerProcessor;
 import org.apache.ignite.internal.util.typedef.F;
-import org.apache.ignite.internal.util.typedef.G;
 import org.apache.ignite.internal.util.typedef.T2;
-import org.apache.ignite.mxbean.ClientProcessorMXBean;
 import org.apache.ignite.testframework.GridTestUtils;
 import org.jetbrains.annotations.Nullable;
 import org.junit.Test;
@@ -644,18 +639,6 @@ public class ComputeTaskTest extends AbstractThinClientTest {
             res.add(nodeId(i));
 
         return res;
-    }
-
-    /**
-     *
-     */
-    private void dropAllThinClientConnections() {
-        for (Ignite ignite : G.allGrids()) {
-            ClientProcessorMXBean mxBean = getMxBean(ignite.name(), "Clients",
-                ClientListenerProcessor.class, ClientProcessorMXBean.class);
-
-            mxBean.dropAllConnections();
-        }
     }
 
     /**
