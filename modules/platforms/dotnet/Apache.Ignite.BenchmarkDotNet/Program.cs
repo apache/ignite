0860f1b160f5e16ccad37475211471f501b56322
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-05-31 10:37:57 +0300

    Updating benchmark results

--- modules/platforms/dotnet/Apache.Ignite.BenchmarkDotNet/Program.cs
+++ modules/platforms/dotnet/Apache.Ignite.BenchmarkDotNet/Program.cs
@@ -17,7 +17,7 @@
 
 namespace Apache.Ignite.BenchmarkDotNet
 {
-    using Apache.Ignite.BenchmarkDotNet.ThinClient;
+    using Apache.Ignite.BenchmarkDotNet.DataStreamer;
     using global::BenchmarkDotNet.Running;
 
     /// <summary>
@@ -30,7 +30,7 @@ namespace Apache.Ignite.BenchmarkDotNet
         /// </summary>
         public static void Main()
         {
-            BenchmarkRunner.Run<ThinClientDataStreamerBenchmarkMultithreaded>();
+            BenchmarkRunner.Run<DataStreamerBenchmark>();
         }
     }
 }
