5de1baec25cbc830679042f91ce873f033e01acf
Vladislav Pyatkov <vldpyatkov@gmail.com>
2021-07-01 17:27:58 +0300

    IGNITE-15033 AssertionError: Unexpected rebalance on rebalanced cluster. Fixes #9211
    Signed-off-by: Slava Koptilin <slava.koptilin@gmail.com>

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/GridCacheOffheapManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/GridCacheOffheapManager.java
@@ -397,7 +397,7 @@ public class GridCacheOffheapManager extends IgniteCacheOffheapManagerImpl imple
     ) throws IgniteCheckedException {
         RowStore rowStore0 = store.rowStore();
 
-        if (rowStore0 != null) {
+        if (rowStore0 != null && (partitionStatesRestored || grp.isLocal())) {
             ((CacheFreeList)rowStore0.freeList()).saveMetadata(grp.statisticsHolderData());
 
             PartitionMetaStorage<SimpleDataRow> partStore = store.partStorage();
