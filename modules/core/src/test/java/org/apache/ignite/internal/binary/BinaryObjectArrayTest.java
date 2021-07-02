04c8ca58b89fe2ed0226e0b45e93a5831ece4df3
Anton Vinogradov <av@apache.org>
2021-07-02 11:00:29 +0300

    WIP

--- modules/core/src/test/java/org/apache/ignite/internal/binary/BinaryObjectArrayTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/binary/BinaryObjectArrayTest.java
@@ -27,38 +27,121 @@ import org.junit.Test;
  */
 public class BinaryObjectArrayTest extends GridCommonAbstractTest {
     /**
+     *
+     */
+    @Override protected void afterTest() throws Exception {
+        super.afterTest();
+
+        stopAllGrids();
+    }
+
+    /**
      * Checking we able to get TestClass[], not just Object[].
      */
     @Test
-    public void testArray() throws Exception {
-        Ignite ign = startGrid();
+    public void testArrayObject() throws Exception {
+        Ignite ignite = startGrid();
 
-        IgniteCache<Integer, TestClass[]> cache = ign.createCache("cache");
+        IgniteCache<Integer, TestClass[]> cache = ignite.createCache("cache");
 
         int key = 1;
 
-        cache.put(key, new TestClass[] {new TestClass(42), new TestClass(43)});
+        cache.put(key, generate());
 
-        TestClass[] arr = cache.get(key);
+        check(cache.get(key));
+    }
 
-        assertEquals(TestClass[].class, arr.getClass());
+    /**
+     * Checking we able to get TestClass[], not just Object[].
+     */
+    @Test
+    public void testArrayField() throws Exception {
+        Ignite ignite = startGrid();
 
-        for (TestClass obj : arr)
-            assertTrue(obj.field == 42 || obj.field == 43);
+        IgniteCache<Integer, TestClassHolder> cache = ignite.createCache("cache");
+
+        int key = 1;
+
+        cache.put(key, new TestClassHolder(generate()));
+
+        check(cache.get(key).arr);
     }
 
     /**
      *
      */
-    private static class TestClass{
+    private TestClass[] generate() {
+        return new TestClass[] {
+            new TestClass42(),
+            new TestClass43(),
+            new TestClass(44) {
+            }};
+    }
+
+    /**
+     * @param arr Array.
+     */
+    private void check(TestClass[] arr) {
+        assertEquals(TestClass[].class, arr.getClass());
+        assertEquals(TestClass42.class, arr[0].getClass());
+        assertEquals(TestClass43.class, arr[1].getClass());
+
+        assertEquals(new Integer(42), arr[0].field);
+        assertEquals(new Integer(43), arr[1].field);
+        assertEquals(new Integer(44), arr[2].field);
+    }
+
+    /**
+     *
+     */
+    private abstract static class TestClass {
         /** */
         private final Integer field;
 
         /**
          * @param field Field.
          */
-        public TestClass(Integer field) {
+        protected TestClass(Integer field) {
             this.field = field;
+        }
+    }
+
+    /**
+     *
+     */
+    private static class TestClass42 extends TestClass {
+        /**
+         *
+         */
+        public TestClass42() {
+            super(42);
+        }
+    }
+
+    /**
+     *
+     */
+    private static class TestClass43 extends TestClass {
+        /**
+         *
+         */
+        public TestClass43() {
+            super(43);
+        }
+    }
+
+    /**
+     *
+     */
+    private static class TestClassHolder {
+        /** */
+        private final TestClass[] arr;
+
+        /**
+         * @param arr Array.
+         */
+        public TestClassHolder(TestClass[] arr) {
+            this.arr = arr;
         }
     }
 }
