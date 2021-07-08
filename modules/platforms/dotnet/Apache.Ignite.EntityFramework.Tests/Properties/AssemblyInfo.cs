6870efbea9422dad0bd5f1163e2e8dbbe201b1ce
Гидаспов Алексей <asgidaspov@sberbank.ru>
2021-07-08 18:19:14 +0500

    IGNITE-15061 Update version in ignite-2.11 release branch - Fixes #9231.
    Signed-off-by: Aleksey Plekhanov <plehanov.alex@gmail.com>

--- modules/platforms/dotnet/Apache.Ignite.EntityFramework.Tests/Properties/AssemblyInfo.cs
+++ modules/platforms/dotnet/Apache.Ignite.EntityFramework.Tests/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("cda5700e-78f3-4a9e-a9b0-704cbe94651c")]
 
-[assembly: AssemblyVersion("2.11.0.51807")]
-[assembly: AssemblyFileVersion("2.11.0.51807")]
+[assembly: AssemblyVersion("2.11.0.56346")]
+[assembly: AssemblyFileVersion("2.11.0.56346")]
 [assembly: AssemblyInformationalVersion("2.11.0")]
 
 [assembly: CLSCompliant(true)]
