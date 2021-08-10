7d68bbe8a7ba4368884952508f2cfb98ee5f9cbd
Igor Sapego <isapego@apache.org>
2021-08-10 11:52:52 +0300

    IGNITE-12177 Code style fix

--- modules/core/src/test/java/org/apache/ignite/platform/PlatformComputeNodeNameTask.java
+++ modules/core/src/test/java/org/apache/ignite/platform/PlatformComputeNodeNameTask.java
@@ -17,6 +17,9 @@
 
 package org.apache.ignite.platform;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.cluster.ClusterNode;
 import org.apache.ignite.compute.ComputeJob;
@@ -27,10 +30,6 @@ import org.apache.ignite.internal.util.typedef.F;
 import org.apache.ignite.resources.IgniteInstanceResource;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Test task returning Node name.
