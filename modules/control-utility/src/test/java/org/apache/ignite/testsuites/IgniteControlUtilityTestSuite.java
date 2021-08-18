b181e6fde7f5b8711f8ccb2939425a79be292761
Anton Vinogradov <av@apache.org>
2021-08-18 17:55:59 +0300

    WIP

--- modules/control-utility/src/test/java/org/apache/ignite/testsuites/IgniteControlUtilityTestSuite.java
+++ modules/control-utility/src/test/java/org/apache/ignite/testsuites/IgniteControlUtilityTestSuite.java
@@ -26,6 +26,7 @@ import org.apache.ignite.util.GridCommandHandlerBrokenIndexTest;
 import org.apache.ignite.util.GridCommandHandlerCheckIndexesInlineSizeTest;
 import org.apache.ignite.util.GridCommandHandlerClusterByClassTest;
 import org.apache.ignite.util.GridCommandHandlerClusterByClassWithSSLTest;
+import org.apache.ignite.util.GridCommandHandlerConsistencyTest;
 import org.apache.ignite.util.GridCommandHandlerDefragmentationTest;
 import org.apache.ignite.util.GridCommandHandlerIndexForceRebuildTest;
 import org.apache.ignite.util.GridCommandHandlerIndexListTest;
@@ -88,6 +89,8 @@ import org.junit.runners.Suite;
     GridCommandHandlerPropertiesTest.class,
 
     GridCommandHandlerDefragmentationTest.class,
+
+    GridCommandHandlerConsistencyTest.class,
 
     SystemViewCommandTest.class,
     MetricCommandTest.class,
