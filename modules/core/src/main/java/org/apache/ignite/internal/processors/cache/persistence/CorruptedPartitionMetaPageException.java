eebda74d003ad3471a576bc04f151edd70ed1801
Denis Chudov <moonglloom@gmail.com>
2021-09-06 14:55:56 +0300

    IGNITE-15227 Better diagnostic information for PDS corruption scenarios. Fixes #9292
    Signed-off-by: Sergey Chugunov <sergey.chugunov@gmail.com>

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/CorruptedPartitionMetaPageException.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/CorruptedPartitionMetaPageException.java
@@ -14,47 +14,34 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.apache.ignite.internal.processors.cache.persistence;
 
-package org.apache.ignite.internal.processors.cache.persistence.freelist;
-
-import java.util.Collection;
-import org.apache.ignite.internal.processors.cache.persistence.AbstractCorruptedPersistenceException;
 import org.apache.ignite.internal.util.typedef.T2;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * Exception to distinguish {@link AbstractFreeList} broken invariants.
+ * Exception to distinguish partition meta page broken invariants.
  */
-public class CorruptedFreeListException extends AbstractCorruptedPersistenceException {
+public class CorruptedPartitionMetaPageException extends AbstractCorruptedPersistenceException {
     /** */
     private static final long serialVersionUID = 0L;
 
     /**
-     * @param msg Message.
+     * @param msg   Message.
      * @param cause Cause.
      * @param grpId Group id.
-     * @param pageIds Ids of pages that are possibly corrupted.
+     * @param pages Ids of pages that might be corrupted.
      */
-    public CorruptedFreeListException(String msg, @Nullable Throwable cause, int grpId, Collection<Long> pageIds) {
-        this(msg, cause, grpId, pageIds.stream().mapToLong(Long::longValue).toArray());
+    protected CorruptedPartitionMetaPageException(String msg, @Nullable Throwable cause, int grpId, long... pages) {
+        this(msg, cause, toPagesArray(grpId, pages));
     }
 
     /**
-     * @param msg Message.
-     * @param cause Cause.
-     * @param grpId Group id.
-     * @param pageIds Ids of pages that are possibly corrupted.
-     */
-    public CorruptedFreeListException(String msg, @Nullable Throwable cause, int grpId, long... pageIds) {
-        this(msg, cause, toPagesArray(grpId, pageIds));
-    }
-
-    /**
-     * @param msg Message.
+     * @param msg   Message.
      * @param cause Cause.
      * @param pages (groupId, pageId) pairs for pages that might be corrupted.
      */
-    public CorruptedFreeListException(String msg,
+    protected CorruptedPartitionMetaPageException(String msg,
         @Nullable Throwable cause,
         T2<Integer, Long>[] pages
     ) {
