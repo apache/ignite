8f3f07d372a084f90ed6dfbeebd76086c6a14877
Maxim Muzafarov <maxmuzaf@gmail.com>
2021-09-11 20:15:14 +0300

    Update 2.11.0-SNAPSHOT version to 2.11.0 prior to release

--- modules/platforms/dotnet/Apache.Ignite.Benchmarks/Properties/AssemblyInfo.cs
+++ modules/platforms/dotnet/Apache.Ignite.Benchmarks/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("8fae8395-7e91-411a-a78f-44d6d3fed0fc")]
 
-[assembly: AssemblyVersion("2.11.0.51807")]
-[assembly: AssemblyFileVersion("2.11.0.51807")]
+[assembly: AssemblyVersion("2.11.0.57956")]
+[assembly: AssemblyFileVersion("2.11.0.57956")]
 [assembly: AssemblyInformationalVersion("2.11.0")]
--- modules/platforms/dotnet/Apache.Ignite.Core.Tests.TestDll/Properties/AssemblyInfo.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests.TestDll/Properties/AssemblyInfo.cs
@@ -45,6 +45,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.11.0.51807")]
-[assembly: AssemblyFileVersion("2.11.0.51807")]
+[assembly: AssemblyVersion("2.11.0.57956")]
+[assembly: AssemblyFileVersion("2.11.0.57956")]
 [assembly: AssemblyInformationalVersion("2.11.0")]
