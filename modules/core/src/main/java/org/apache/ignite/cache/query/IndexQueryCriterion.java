8dc093dfcec4b6c59f9a484d018f96c053cd39f5
Maksim Timonin <timonin.maxim@gmail.com>
2021-09-17 14:00:25 +0300

    IGNITE-14699 Add IndexQuery API. (#9118)

--- modules/core/src/main/java/org/apache/ignite/cache/query/IndexQueryCriterion.java
+++ modules/core/src/main/java/org/apache/ignite/cache/query/IndexQueryCriterion.java
@@ -1,12 +1,12 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
+ * contributor license agreements. See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * the License. You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,22 +15,16 @@
  * limitations under the License.
  */
 
-package org.apache.ignite.internal.cache.query.index;
+package org.apache.ignite.cache.query;
 
-import java.util.List;
-import org.apache.ignite.internal.cache.query.index.sorted.IndexKeyDefinition;
+import java.io.Serializable;
 
 /**
- * Basic interface for index description required to create or destroy index.
+ * Basic interface for {@link IndexQuery} criterion.
  */
-public interface IndexDefinition {
+public interface IndexQueryCriterion extends Serializable {
     /**
-     * @return Index name.
+     * @return Index field name.
      */
-    public IndexName idxName();
-
-    /**
-     * @return List of index key definitions.
-     */
-    public List<IndexKeyDefinition> indexKeyDefinitions();
+    public String field();
 }
