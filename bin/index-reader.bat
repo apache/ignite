89c84fa86c4994f0773d8a7fb1bfc372ce92ebea
Freeman Fang <freeman.fang@gmail.com>
2021-09-28 16:24:46 -0400

    [IGNITE-15635]start ignite with JDK17

--- bin/index-reader.bat
+++ bin/index-reader.bat
@@ -179,7 +179,7 @@ if %MAJOR_JAVA_VER% GEQ 9 if %MAJOR_JAVA_VER% LSS 11 (
     %CONTROL_JVM_OPTS%
 )
 
-if %MAJOR_JAVA_VER% GEQ 11 (
+if %MAJOR_JAVA_VER% GEQ 11 if %MAJOR_JAVA_VER% LSS 17 (
     set CONTROL_JVM_OPTS= ^
     --add-exports=java.base/jdk.internal.misc=ALL-UNNAMED ^
     --add-exports=java.base/sun.nio.ch=ALL-UNNAMED ^
@@ -188,6 +188,20 @@ if %MAJOR_JAVA_VER% GEQ 11 (
     --add-exports=java.base/sun.reflect.generics.reflectiveObjects=ALL-UNNAMED ^
     --add-opens=jdk.management/com.sun.management.internal=ALL-UNNAMED ^
     --illegal-access=permit ^
+    %CONTROL_JVM_OPTS%
+)
+
+if %MAJOR_JAVA_VER% GEQ 17 (
+    set CONTROL_JVM_OPTS= ^
+    --add-exports=java.base/jdk.internal.misc=ALL-UNNAMED ^
+    --add-exports=java.base/sun.nio.ch=ALL-UNNAMED ^
+    --add-exports=java.management/com.sun.jmx.mbeanserver=ALL-UNNAMED ^
+    --add-exports=jdk.internal.jvmstat/sun.jvmstat.monitor=ALL-UNNAMED ^
+    --add-exports=java.base/sun.reflect.generics.reflectiveObjects=ALL-UNNAMED ^
+    --add-opens=jdk.management/com.sun.management.internal=ALL-UNNAMED ^
+    --add-opens=java.base/java.nio=ALL-UNNAMED ^
+    --add-opens=java.base/java.util=ALL-UNNAMED ^
+    --add-opens=java.base/java.lang=ALL-UNNAMED ^
     %CONTROL_JVM_OPTS%
 )
 
