3379d082e515453df7f440269496c18275841953
Maksim Timonin <timonin.maxim@gmail.com>
2021-10-03 20:23:45 +0300

    Remove pylint from tox

--- modules/ducktests/tests/ignitetest/utils/version.py
+++ modules/ducktests/tests/ignitetest/utils/version.py
@@ -61,7 +61,6 @@ class IgniteVersion(LooseVersion):
         if isinstance(other, str):
             other = IgniteVersion(other)
 
-        # pylint: disable=W0511
         # todo solve comparability issues and uncomment the following
         # if self.project != other.project:
         #     raise Exception("Incomperable versons v1=%s, v2=%s because of different projects" % (self, other))
