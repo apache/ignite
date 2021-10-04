b9fa7a16548d0df9532b92b6117d6932f2532273
ksirotkin <jkonsir@gmail.com>
2021-10-04 17:26:12 +0300

    IGNITE-13810 append expire policy info into log of started cache

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/expiry/ExpiryPolicyInfoLoggingTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/expiry/ExpiryPolicyInfoLoggingTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.internal.processors.cache.expiry;
 
 import java.util.concurrent.TimeUnit;
@@ -21,18 +38,25 @@ import org.junit.Test;
  * Test for checking logging of expiry policy info per cache.
  */
 public class ExpiryPolicyInfoLoggingTest extends GridCommonAbstractTest {
+
     /** */
     private static final String CACHE_1_NAME = "cache1";
+
     /** */
     private static final String CACHE_2_NAME = "cache2";
+
     /** */
     private static final String STARTED_CACHE_MSG = "Started cache [name=%s";
+
     /** */
     private static final String STARTED_CACHE_IN_RECOVERY_MODE_MSG = "Started cache in recovery mode [name=%s,";
+
     /** */
     private static final String EXPRITY_POLICY_MSG = "expiryPolicy=[type=%s, isEagerTtl=%s]";
+
     /** */
     private boolean persistenceEnabled;
+
     /** */
     private final ListeningTestLogger log = new ListeningTestLogger(GridAbstractTest.log);
 
