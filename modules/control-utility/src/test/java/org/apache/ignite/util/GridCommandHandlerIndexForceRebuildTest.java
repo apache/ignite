9a9c0464468350f4a96e735d824ab3589ef59ae9
NSAmelchev <nsamelchev@gmail.com>
2021-10-06 12:33:16 +0300

    +dumpStack

--- modules/control-utility/src/test/java/org/apache/ignite/util/GridCommandHandlerIndexForceRebuildTest.java
+++ modules/control-utility/src/test/java/org/apache/ignite/util/GridCommandHandlerIndexForceRebuildTest.java
@@ -413,7 +413,7 @@ public class GridCommandHandlerIndexForceRebuildTest extends GridCommandHandlerA
                 ThreadLocalRandom r = ThreadLocalRandom.current();
 
                 while (!stopLoad.get())
-                    n.cache(cacheName1).put(r.nextInt(1000), new Person(r.nextInt(), valueOf(r.nextLong())));
+                    n.cache(cacheName1).put(r.nextInt(), new Person(r.nextInt(), valueOf(r.nextLong())));
             });
 
             assertTrue(waitForCondition(() -> n.cache(cacheName1).size() > cacheSize, getTestTimeout()));
@@ -735,8 +735,13 @@ public class GridCommandHandlerIndexForceRebuildTest extends GridCommandHandlerA
                 if (fut != null) {
                     fut.onDone();
 
-                    assertTrue("Failed to wait for indexes rebuild unblocking",
-                        GridTestUtils.waitForCondition(() -> !blockRebuildIdx.containsKey(cctx.name()), 60_000));
+                    try {
+                        assertTrue("Failed to wait for indexes rebuild unblocking",
+                            GridTestUtils.waitForCondition(() -> !blockRebuildIdx.containsKey(cctx.name()), 60_000));
+                    }
+                    finally {
+                        U.dumpStack("@@@");
+                    }
                 }
             }
             catch (IgniteInterruptedCheckedException e) {
