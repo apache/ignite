e32479f678f2bd5de04ce9f1087c4e32c16b338e
Vladimir Steshin <vladsz83@gmail.com>
2021-10-08 07:56:02 +0300

    fix

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/snapshot/AbstractSnapshotSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/snapshot/AbstractSnapshotSelfTest.java
@@ -113,7 +113,7 @@ public abstract class AbstractSnapshotSelfTest extends GridCommonAbstractTest {
     protected static final int CACHE_KEYS_RANGE = 1024;
 
     /** Timeout in milliseconds to await for snapshot operation being completed. */
-    protected static final long TIMEOUT = 15_000;
+    protected static final long TIMEOUT = 30_000;
 
     /** List of collected snapshot test events. */
     protected final List<Integer> locEvts = new CopyOnWriteArrayList<>();
