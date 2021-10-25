a47652a25efa97e852f4b5e791ccee4e0811d384
Timotheus Preisinger <30527148+timotheuspreisinger@users.noreply.github.com>
2021-10-25 21:20:59 +0200

    Fix for IGNITE-15820
    Fix .NET Thin Client streaming problem with binary data being not available through SQL: IGNITE-15820 (https://issues.apache.org/jira/browse/IGNITE-15820)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/platform/client/streamer/ClientDataStreamerReader.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/platform/client/streamer/ClientDataStreamerReader.java
@@ -66,6 +66,11 @@ class ClientDataStreamerReader {
         if (obj == null)
             return null;
 
+        // if the object already is an instance of CacheObject, there is no need to wrap it
+		if (obj instanceof CacheObject) {
+			return (T) obj;
+		}
+
         int pos1 = in.position();
 
         in.position(pos0);
