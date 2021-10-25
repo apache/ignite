9f5afd14d6c185db201987869aa55fb2da89d625
Ilya Kazakov <kazakov.ilya@gmail.com>
2021-10-25 13:20:04 +0800

    IGNITE-15807 checkstyle

--- modules/core/src/main/java/org/apache/ignite/internal/client/thin/ReliableChannel.java
+++ modules/core/src/main/java/org/apache/ignite/internal/client/thin/ReliableChannel.java
@@ -25,7 +25,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
+//import java.util.Random;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
@@ -687,7 +687,7 @@ final class ReliableChannel implements AutoCloseable {
         }
 
         if (dfltChannelIdx == -1)
-            dfltChannelIdx = 0;//new Random().nextInt(reinitHolders.size());
+            dfltChannelIdx = 0; //new Random().nextInt(reinitHolders.size());
 
         curChannelsGuard.writeLock().lock();
 
