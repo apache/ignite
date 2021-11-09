11517d9a04d8cc8aaf67567c13f8925cf0033c98
Pavel Pereslegin <xxtern@gmail.com>
2021-11-09 18:13:38 +0300

    IGNITE-15801 Annotation-based injection of ServiceContext. (#9542)

--- modules/core/src/main/java/org/apache/ignite/services/ServiceContext.java
+++ modules/core/src/main/java/org/apache/ignite/services/ServiceContext.java
@@ -19,12 +19,14 @@ package org.apache.ignite.services;
 
 import java.io.Serializable;
 import java.util.UUID;
+import org.apache.ignite.resources.ServiceContextResource;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * Service execution context. Execution context is provided into {@link Service#execute(ServiceContext)}
- * and {@link Service#cancel(ServiceContext)} methods and contains information about specific service
- * execution.
+ * Service execution context. This context is provided using {@link ServiceContextResource} annotation and contains
+ * information about specific service execution.
+ *
+ * @see ServiceContextResource
  */
 public interface ServiceContext extends Serializable {
     /**
