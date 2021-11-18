f0e672ae0896fdc93286f90f6326b68f132ff762
Maksim Timonin <timonin.maxim@gmail.com>
2021-11-18 17:23:07 +0300

    Add test for double too

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/IgniteSqlQueryDecimalArgumentsWithTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/IgniteSqlQueryDecimalArgumentsWithTest.java
@@ -22,26 +22,57 @@ import java.util.List;
 import java.util.regex.Pattern;
 import org.apache.ignite.cache.query.SqlFieldsQuery;
 import org.apache.ignite.internal.IgniteEx;
+import org.apache.ignite.internal.cache.query.index.IndexName;
+import org.apache.ignite.internal.cache.query.index.sorted.inline.InlineIndex;
+import org.apache.ignite.internal.processors.cache.GridCacheContext;
+import org.apache.ignite.internal.util.typedef.F;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-/** */
+/** Checks whether correctly compare non-inlined decimal search key with inlined types. */
+@RunWith(Parameterized.class)
 public class IgniteSqlQueryDecimalArgumentsWithTest extends GridCommonAbstractTest {
     /** */
-    private static final String IDX_NAME = "FLAG_IDX";
+    private static final String IDX_NAME = "FLD_IDX";
+
+    /** */
+    private static final String TABLE_NAME = "FLD_TABLE";
+
+    /** */
+    private static final String TABLE_CACHE_NAME = "FLD_TABLE_CACHE";
 
     /** */
     private IgniteEx node;
+
+    /** */
+    @Parameterized.Parameter()
+    public String idxFldName;
+
+    /** */
+    @Parameterized.Parameter(1)
+    public String idxFldType;
+
+    /** */
+    @Parameterized.Parameters(name = "fld={0} type={1}")
+    public static List<Object[]> params() {
+        return F.asList(
+            new Object[] {"intFld", "int"},
+            new Object[] {"dblFld", "double"}
+        );
+    }
 
     /** {@inheritDoc} */
     @Override protected void beforeTest() throws Exception {
         node = startGrid();
 
-        query("create table test_table(id varchar primary key, flag int);");
+        query("create table " + TABLE_NAME + "(id varchar primary key, " + idxFldName + " " + idxFldType + ")" +
+            " WITH \"CACHE_NAME=" + TABLE_CACHE_NAME + "\";");
 
-        query("create index " + IDX_NAME + " on test_table(flag);");
+        query("create index " + IDX_NAME + " on " + TABLE_NAME + "(" + idxFldName + ");");
 
-        query("insert into test_table(id, flag) values('a1', 1);");
+        query("insert into " + TABLE_NAME + "(id, " + idxFldName + ") values('a1', 1);");
     }
 
     /** {@inheritDoc} */
@@ -51,44 +82,44 @@ public class IgniteSqlQueryDecimalArgumentsWithTest extends GridCommonAbstractTe
 
     /** Math calculations produces decimal values for searching index tree. */
     @Test
-    public void testDecimalCalculatedFieldComparedCorrectly() {
-        String checkSql = "select * from test_table where flag < 100 * 0.5;";
-
-        assertIndexUsed(checkSql);
-
-        List<List<?>> result = query(checkSql);
-
-        assertEquals(1, result.size());
-        assertEquals(2, result.get(0).size());
-        assertEquals(1, result.get(0).get(1));
+    public void testDecimalCalculatedField() {
+        check("select * from " + TABLE_NAME + " where " + idxFldName + " < 100 * 0.5;");
     }
 
-    /** Math calculations produces decimal values for searching index tree. */
+    /** */
     @Test
-    public void testExplicitDecimalField() {
-        String checkSql = "select * from test_table where flag < ?;";
-        BigDecimal arg = new BigDecimal("50.0");
-
-        assertIndexUsed(checkSql, arg);
-
-        List<List<?>> result = query(checkSql, arg);
-
-        assertEquals(1, result.size());
-        assertEquals(2, result.get(0).size());
-        assertEquals(1, result.get(0).get(1));
+    public void testExplicitDecimalArgument() {
+        check("select * from " + TABLE_NAME + " where " + idxFldName + " < ?;", new BigDecimal("50.0"));
     }
 
-    /** Assert if index is not used for searching. */
-    private void assertIndexUsed(String sql, Object... args) {
+    /** */
+    private void check(String sql, Object... arg) {
+        assertTrue(idxInlineSize() > 0);
+
         String explain = "explain " + sql;
 
-        String res = query(explain, args).get(0).get(0).toString();
+        String res = query(explain, arg).get(0).get(0).toString();
 
-        assertTrue(Pattern.compile(IDX_NAME + "[^\\s]+: FLAG <").matcher(res).find());
+        // Assert if index is not used for searching.
+        assertTrue(Pattern.compile(IDX_NAME + "[^\\s]+: " + idxFldName.toUpperCase() + " <").matcher(res).find());
+
+        List<List<?>> result = query(sql, arg);
+
+        assertEquals(1, result.size());
     }
 
     /** */
     private List<List<?>> query(String qry, Object... args) {
         return node.context().query().querySqlFields(new SqlFieldsQuery(qry).setArgs(args), false).getAll();
+    }
+
+    /** */
+    private int idxInlineSize() {
+        GridCacheContext<?, ?> cctx = node.cachex(TABLE_CACHE_NAME).context();
+
+        InlineIndex idx = (InlineIndex)node.context().indexProcessor().index(
+            new IndexName(cctx.name(), "PUBLIC", TABLE_NAME, IDX_NAME));
+
+        return idx.inlineSize();
     }
 }
