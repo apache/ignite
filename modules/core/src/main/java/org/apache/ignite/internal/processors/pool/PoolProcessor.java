30f8daa1f4b8527ed61d6226e0d2e2a3b7318778
Pavel Pereslegin <xxtern@gmail.com>
2021-12-07 13:44:07 +0300

    IGNITE-16070 Use OOME handler.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/pool/PoolProcessor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/pool/PoolProcessor.java
@@ -533,7 +533,7 @@ public class PoolProcessor extends GridProcessorAdapter {
                 DFLT_THREAD_KEEP_ALIVE_TIME,
                 new LinkedBlockingQueue<>(),
                 GridIoPolicy.UNDEFINED,
-                excHnd);
+                oomeHnd);
 
             reencryptExecSvc.allowCoreThreadTimeOut(true);
         }
