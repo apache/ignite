1c487746776a534616f4d27dfb47178287dd3840
vladimirermakov <vladermakov07@gmail.com>
2021-12-27 19:34:26 +0300

    Extended test. Removed part of new code.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/query/GridQueryProcessor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/query/GridQueryProcessor.java
@@ -61,7 +61,6 @@ import org.apache.ignite.internal.GridKernalContext;
 import org.apache.ignite.internal.IgniteInternalFuture;
 import org.apache.ignite.internal.NodeStoppingException;
 import org.apache.ignite.internal.binary.BinaryMetadata;
-import org.apache.ignite.internal.binary.BinaryObjectImpl;
 import org.apache.ignite.internal.cache.query.index.IndexProcessor;
 import org.apache.ignite.internal.cache.query.index.IndexQueryProcessor;
 import org.apache.ignite.internal.cache.query.index.IndexQueryResult;
@@ -2619,15 +2618,7 @@ public class GridQueryProcessor extends GridProcessorAdapter {
 
         if (binaryVal) {
             int typeId = ctx.cacheObjects().typeId(val);
-            //In the case of using a simple name mapper, the typeId may be different, and we
-            // won't be able to find it in the types map.
-            // Therefore, we additionally calculate typeId based on typeName from metadata.
-            if (val instanceof BinaryObjectImpl) {
-                BinaryType metadata = ctx.cacheObjects().metadata(typeId);
-    
-                if (metadata != null)
-                    typeId = ((BinaryObjectImpl)val).context().typeId(metadata.typeName());
-            }
+
             id = new QueryTypeIdKey(cacheName, typeId);
         }
         else {
