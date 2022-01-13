apache/ignite-3

IGNITE-16281 Fix deadlock

modules/client-common/src/main/java/org/apache/ignite/client/proto/query/JdbcQueryEventHandler.java
@@ -17,6 +17,7 @@
 
 package org.apache.ignite.client.proto.query;
 
+import java.util.concurrent.CompletableFuture;
 import org.apache.ignite.client.proto.query.event.BatchExecuteRequest;
 import org.apache.ignite.client.proto.query.event.BatchExecuteResult;
 import org.apache.ignite.client.proto.query.event.JdbcMetaColumnsRequest;
@@ -43,71 +44,71 @@ public interface JdbcQueryEventHandler {
      * {@link QueryExecuteRequest} command handler.
      *
      * @param req Execute query request.
-     * @return Result.
+     * @return Result future.
      */
-    QueryExecuteResult query(QueryExecuteRequest req);
+    CompletableFuture<QueryExecuteResult> query(QueryExecuteRequest req);
 
     /**
      * {@link QueryFetchRequest} command handler.
      *
      * @param req Fetch query request.
-     * @return Result.
+     * @return Result future.
      */
-    QueryFetchResult fetch(QueryFetchRequest req);
+    CompletableFuture<QueryFetchResult> fetch(QueryFetchRequest req);
 
     /**
      * {@link BatchExecuteRequest} command handler.
      *
      * @param req Batch query request.
-     * @return Result.
+     * @return Result future.
      */
-    BatchExecuteResult batch(BatchExecuteRequest req);
+    CompletableFuture<BatchExecuteResult> batch(BatchExecuteRequest req);
 
     /**
      * {@link QueryCloseRequest} command handler.
      *
      * @param req Close query request.
-     * @return Result.
+     * @return Result future.
      */
-    QueryCloseResult close(QueryCloseRequest req);
+    CompletableFuture<QueryCloseResult> close(QueryCloseRequest req);
 
     /**
      * {@link JdbcMetaTablesRequest} command handler.
      *
      * @param req Jdbc tables metadata request.
-     * @return Result.
+     * @return Result future.
      */
-    JdbcMetaTablesResult tablesMeta(JdbcMetaTablesRequest req);
+    CompletableFuture<JdbcMetaTablesResult> tablesMeta(JdbcMetaTablesRequest req);
 
     /**
      * {@link JdbcMetaColumnsRequest} command handler.
      *
      * @param req Jdbc columns metadata request.
-     * @return Result.
+     * @return Result future.
      */
-    JdbcMetaColumnsResult columnsMeta(JdbcMetaColumnsRequest req);
+    CompletableFuture<JdbcMetaColumnsResult> columnsMeta(JdbcMetaColumnsRequest req);
 
     /**
      * {@link JdbcMetaSchemasRequest} command handler.
      *
      * @param req Jdbc schemas metadata request.
-     * @return Result.
+     * @return Result future.
      */
-    JdbcMetaSchemasResult schemasMeta(JdbcMetaSchemasRequest req);
+    CompletableFuture<JdbcMetaSchemasResult> schemasMeta(JdbcMetaSchemasRequest req);
 
     /**
      * {@link JdbcMetaPrimaryKeysRequest} command handler.
      *
      * @param req Jdbc primary keys metadata request.
-     * @return Result.
+     * @return Result future.
      */
-    JdbcMetaPrimaryKeysResult primaryKeysMeta(JdbcMetaPrimaryKeysRequest req);
+    CompletableFuture<JdbcMetaPrimaryKeysResult> primaryKeysMeta(JdbcMetaPrimaryKeysRequest req);
 
     /**
      * {@link JdbcQueryMetadataRequest} command handler.
      *
      * @param req Jdbc query metadata request.
-     * @return Result.
+     * @return Result future.
      */
-    JdbcMetaColumnsResult queryMetadata(JdbcQueryMetadataRequest req);
+    CompletableFuture<JdbcMetaColumnsResult> queryMetadata(JdbcQueryMetadataRequest req);
 }

modules/client-handler/src/main/java/org/apache/ignite/client/handler/requests/sql/ClientSqlCloseRequest.java
@@ -20,7 +20,6 @@ package org.apache.ignite.client.handler.requests.sql;
 import java.util.concurrent.CompletableFuture;
 import org.apache.ignite.client.proto.query.JdbcQueryEventHandler;
 import org.apache.ignite.client.proto.query.event.QueryCloseRequest;
-import org.apache.ignite.client.proto.query.event.QueryCloseResult;
 import org.apache.ignite.internal.client.proto.ClientMessagePacker;
 import org.apache.ignite.internal.client.proto.ClientMessageUnpacker;
 
@@ -34,7 +33,7 @@ public class ClientSqlCloseRequest {
      * @param in      Client message unpacker.
      * @param out     Client message packer.
      * @param handler Query event handler.
-     * @return null value indicates synchronous operation.
+     * @return Operation future.
      */
     public static CompletableFuture<Void> process(
             ClientMessageUnpacker in,
@@ -45,10 +44,6 @@ public class ClientSqlCloseRequest {
 
         req.readBinary(in);
 
-        QueryCloseResult res = handler.close(req);
-
-        res.writeBinary(out);
-
-        return null;
+        return handler.close(req).thenAccept(res -> res.writeBinary(out));
     }
 }

modules/client-handler/src/main/java/org/apache/ignite/client/handler/requests/sql/ClientSqlExecuteBatchRequest.java
@@ -20,7 +20,6 @@ package org.apache.ignite.client.handler.requests.sql;
 import java.util.concurrent.CompletableFuture;
 import org.apache.ignite.client.proto.query.JdbcQueryEventHandler;
 import org.apache.ignite.client.proto.query.event.BatchExecuteRequest;
-import org.apache.ignite.client.proto.query.event.BatchExecuteResult;
 import org.apache.ignite.internal.client.proto.ClientMessagePacker;
 import org.apache.ignite.internal.client.proto.ClientMessageUnpacker;
 
@@ -34,7 +33,7 @@ public class ClientSqlExecuteBatchRequest {
      * @param in      Client message unpacker.
      * @param out     Client message packer.
      * @param handler Query event handler.
-     * @return null value indicates synchronous operation.
+     * @return Operation future.
      */
     public static CompletableFuture<Void> process(
             ClientMessageUnpacker in,
@@ -45,10 +44,6 @@ public class ClientSqlExecuteBatchRequest {
 
         req.readBinary(in);
 
-        BatchExecuteResult res = handler.batch(req);
-
-        res.writeBinary(out);
-
-        return null;
+        return handler.batch(req).thenAccept(res -> res.writeBinary(out));
     }
 }

modules/client-handler/src/main/java/org/apache/ignite/client/handler/requests/sql/ClientSqlExecuteRequest.java
@@ -20,7 +20,6 @@ package org.apache.ignite.client.handler.requests.sql;
 import java.util.concurrent.CompletableFuture;
 import org.apache.ignite.client.proto.query.JdbcQueryEventHandler;
 import org.apache.ignite.client.proto.query.event.QueryExecuteRequest;
-import org.apache.ignite.client.proto.query.event.QueryExecuteResult;
 import org.apache.ignite.internal.client.proto.ClientMessagePacker;
 import org.apache.ignite.internal.client.proto.ClientMessageUnpacker;
 
@@ -34,7 +33,7 @@ public class ClientSqlExecuteRequest {
      * @param in      Client message unpacker.
      * @param out     Client message packer.
      * @param handler Query event handler.
-     * @return null value indicates synchronous operation.
+     * @return Operation future.
      */
     public static CompletableFuture<Void> execute(
             ClientMessageUnpacker in,
@@ -45,10 +44,6 @@ public class ClientSqlExecuteRequest {
 
         req.readBinary(in);
 
-        QueryExecuteResult res = handler.query(req);
-
-        res.writeBinary(out);
-
-        return null;
+        return handler.query(req).thenAccept(res -> res.writeBinary(out));
     }
 }

modules/client/src/main/java/org/apache/ignite/internal/client/query/JdbcClientQueryEventHandler.java
@@ -17,6 +17,7 @@
 
 package org.apache.ignite.internal.client.query;
 
+import java.util.concurrent.CompletableFuture;
 import org.apache.ignite.client.proto.query.JdbcQueryEventHandler;
 import org.apache.ignite.client.proto.query.event.BatchExecuteRequest;
 import org.apache.ignite.client.proto.query.event.BatchExecuteResult;
@@ -56,91 +57,73 @@ public class JdbcClientQueryEventHandler implements JdbcQueryEventHandler {
 
     /** {@inheritDoc} */
     @Override
-    public QueryExecuteResult query(QueryExecuteRequest req) {
+    public CompletableFuture<QueryExecuteResult> query(QueryExecuteRequest req) {
         QueryExecuteResult res = new QueryExecuteResult();
 
-        client.sendRequest(ClientOp.SQL_EXEC, req, res);
-
-        return res;
+        return client.sendRequest(ClientOp.SQL_EXEC, req, res);
     }
 
     /** {@inheritDoc} */
     @Override
-    public QueryFetchResult fetch(QueryFetchRequest req) {
+    public CompletableFuture<QueryFetchResult> fetch(QueryFetchRequest req) {
         QueryFetchResult res = new QueryFetchResult();
 
-        client.sendRequest(ClientOp.SQL_NEXT, req, res);
-
-        return res;
+        return client.sendRequest(ClientOp.SQL_NEXT, req, res);
     }
 
     /** {@inheritDoc} */
     @Override
-    public BatchExecuteResult batch(BatchExecuteRequest req) {
+    public CompletableFuture<BatchExecuteResult> batch(BatchExecuteRequest req) {
         BatchExecuteResult res = new BatchExecuteResult();
 
-        client.sendRequest(ClientOp.SQL_EXEC_BATCH, req, res);
-
-        return res;
+        return client.sendRequest(ClientOp.SQL_EXEC_BATCH, req, res);
     }
 
     /** {@inheritDoc} */
     @Override
-    public QueryCloseResult close(QueryCloseRequest req) {
+    public CompletableFuture<QueryCloseResult> close(QueryCloseRequest req) {
         QueryCloseResult res = new QueryCloseResult();
 
-        client.sendRequest(ClientOp.SQL_CURSOR_CLOSE, req, res);
-
-        return res;
+        return client.sendRequest(ClientOp.SQL_CURSOR_CLOSE, req, res);
     }
 
     /** {@inheritDoc} */
     @Override
-    public JdbcMetaTablesResult tablesMeta(JdbcMetaTablesRequest req) {
+    public CompletableFuture<JdbcMetaTablesResult> tablesMeta(JdbcMetaTablesRequest req) {
         JdbcMetaTablesResult res = new JdbcMetaTablesResult();
 
-        client.sendRequest(ClientOp.SQL_TABLE_META, req, res);
-
-        return res;
+        return client.sendRequest(ClientOp.SQL_TABLE_META, req, res);
     }
 
     /** {@inheritDoc} */
     @Override
-    public JdbcMetaColumnsResult columnsMeta(JdbcMetaColumnsRequest req) {
+    public CompletableFuture<JdbcMetaColumnsResult> columnsMeta(JdbcMetaColumnsRequest req) {
         JdbcMetaColumnsResult res = new JdbcMetaColumnsResult();
 
-        client.sendRequest(ClientOp.SQL_COLUMN_META, req, res);
-
-        return res;
+        return client.sendRequest(ClientOp.SQL_COLUMN_META, req, res);
     }
 
     /** {@inheritDoc} */
     @Override
-    public JdbcMetaSchemasResult schemasMeta(JdbcMetaSchemasRequest req) {
+    public CompletableFuture<JdbcMetaSchemasResult> schemasMeta(JdbcMetaSchemasRequest req) {
         JdbcMetaSchemasResult res = new JdbcMetaSchemasResult();
 
-        client.sendRequest(ClientOp.SQL_SCHEMAS_META, req, res);
-
-        return res;
+        return client.sendRequest(ClientOp.SQL_SCHEMAS_META, req, res);
     }
 
     /** {@inheritDoc} */
     @Override
-    public JdbcMetaPrimaryKeysResult primaryKeysMeta(JdbcMetaPrimaryKeysRequest req) {
+    public CompletableFuture<JdbcMetaPrimaryKeysResult> primaryKeysMeta(JdbcMetaPrimaryKeysRequest req) {
         JdbcMetaPrimaryKeysResult res = new JdbcMetaPrimaryKeysResult();
 
-        client.sendRequest(ClientOp.SQL_PK_META, req, res);
-
-        return res;
+        return client.sendRequest(ClientOp.SQL_PK_META, req, res);
     }
 
     /** {@inheritDoc} */
     @Override
-    public JdbcMetaColumnsResult queryMetadata(JdbcQueryMetadataRequest req) {
+    public CompletableFuture<JdbcMetaColumnsResult> queryMetadata(JdbcQueryMetadataRequest req) {
         JdbcMetaColumnsResult res = new JdbcMetaColumnsResult();
 
-        client.sendRequest(ClientOp.SQL_QUERY_META, req, res);
-
-        return res;
+        return client.sendRequest(ClientOp.SQL_QUERY_META, req, res);
     }
 }

modules/client/src/main/java/org/apache/ignite/internal/jdbc/JdbcResultSet.java
@@ -207,7 +207,7 @@ public class JdbcResultSet implements ResultSet {
         ensureNotClosed();
 
         if ((rowsIter == null || !rowsIter.hasNext()) && !finished) {
-            QueryFetchResult res = qryHandler.fetch(new QueryFetchRequest(cursorId, fetchSize));
+            QueryFetchResult res = qryHandler.fetch(new QueryFetchRequest(cursorId, fetchSize)).join();
 
             if (!res.hasResults()) {
                 throw IgniteQueryErrorCode.createJdbcSqlException(res.err(), res.status());
@@ -259,7 +259,7 @@ public class JdbcResultSet implements ResultSet {
 
         try {
             if (stmt != null && (!finished || (isQuery && !autoClose))) {
-                QueryCloseResult res = qryHandler.close(new QueryCloseRequest(cursorId));
+                QueryCloseResult res = qryHandler.close(new QueryCloseRequest(cursorId)).join();
 
                 if (!res.hasResults()) {
                     throw IgniteQueryErrorCode.createJdbcSqlException(res.err(), res.status());
@@ -2186,7 +2186,7 @@ public class JdbcResultSet implements ResultSet {
         }
 
         if (!metaInit) {
-            JdbcMetaColumnsResult res = qryHandler.queryMetadata(new JdbcQueryMetadataRequest(cursorId));
+            JdbcMetaColumnsResult res = qryHandler.queryMetadata(new JdbcQueryMetadataRequest(cursorId)).join();
 
             meta = res.meta();
 

modules/client/src/main/java/org/apache/ignite/internal/jdbc/JdbcStatement.java
@@ -130,7 +130,7 @@ public class JdbcStatement implements Statement {
         QueryExecuteRequest req = new QueryExecuteRequest(schema, pageSize, maxRows, sql,
                 args == null ? ArrayUtils.OBJECT_EMPTY_ARRAY : args.toArray());
 
-        QueryExecuteResult res = conn.handler().query(req);
+        QueryExecuteResult res = conn.handler().query(req).join();
 
         if (!res.hasResults()) {
             throw IgniteQueryErrorCode.createJdbcSqlException(res.err(), res.status());
@@ -547,7 +547,7 @@ public class JdbcStatement implements Statement {
         BatchExecuteRequest req = new BatchExecuteRequest(conn.getSchema(), batch, conn.getAutoCommit());
 
         try {
-            BatchExecuteResult res = conn.handler().batch(req);
+            BatchExecuteResult res = conn.handler().batch(req).join();
 
             if (!res.hasResults()) {
                 throw new BatchUpdateException(res.err(),

apache/ignite

modules/client-common/src/main/java/org/apache/ignite/client/proto/query/JdbcQueryEventHandler.java
@@ -17,6 +17,7 @@
 
 package org.apache.ignite.client.proto.query;
 
+import java.util.concurrent.CompletableFuture;
 import org.apache.ignite.client.proto.query.event.BatchExecuteRequest;
 import org.apache.ignite.client.proto.query.event.BatchExecuteResult;
 import org.apache.ignite.client.proto.query.event.JdbcMetaColumnsRequest;
@@ -43,71 +44,71 @@ public interface JdbcQueryEventHandler {
      * {@link QueryExecuteRequest} command handler.
      *
      * @param req Execute query request.
-     * @return Result.
+     * @return Result future.
      */
-    QueryExecuteResult query(QueryExecuteRequest req);
+    CompletableFuture<QueryExecuteResult> queryAsync(QueryExecuteRequest req);
 
     /**
      * {@link QueryFetchRequest} command handler.
      *
      * @param req Fetch query request.
-     * @return Result.
+     * @return Result future.
      */
-    QueryFetchResult fetch(QueryFetchRequest req);
+    CompletableFuture<QueryFetchResult> fetchAsync(QueryFetchRequest req);
 
     /**
      * {@link BatchExecuteRequest} command handler.
      *
      * @param req Batch query request.
-     * @return Result.
+     * @return Result future.
      */
-    BatchExecuteResult batch(BatchExecuteRequest req);
+    CompletableFuture<BatchExecuteResult> batchAsync(BatchExecuteRequest req);
 
     /**
      * {@link QueryCloseRequest} command handler.
      *
      * @param req Close query request.
-     * @return Result.
+     * @return Result future.
      */
-    QueryCloseResult close(QueryCloseRequest req);
+    CompletableFuture<QueryCloseResult> closeAsync(QueryCloseRequest req);
 
     /**
      * {@link JdbcMetaTablesRequest} command handler.
      *
      * @param req Jdbc tables metadata request.
-     * @return Result.
+     * @return Result future.
      */
-    JdbcMetaTablesResult tablesMeta(JdbcMetaTablesRequest req);
+    CompletableFuture<JdbcMetaTablesResult> tablesMetaAsync(JdbcMetaTablesRequest req);
 
     /**
      * {@link JdbcMetaColumnsRequest} command handler.
      *
      * @param req Jdbc columns metadata request.
-     * @return Result.
+     * @return Result future.
      */
-    JdbcMetaColumnsResult columnsMeta(JdbcMetaColumnsRequest req);
+    CompletableFuture<JdbcMetaColumnsResult> columnsMetaAsync(JdbcMetaColumnsRequest req);
 
     /**
      * {@link JdbcMetaSchemasRequest} command handler.
      *
      * @param req Jdbc schemas metadata request.
-     * @return Result.
+     * @return Result future.
      */
-    JdbcMetaSchemasResult schemasMeta(JdbcMetaSchemasRequest req);
+    CompletableFuture<JdbcMetaSchemasResult> schemasMetaAsync(JdbcMetaSchemasRequest req);
 
     /**
      * {@link JdbcMetaPrimaryKeysRequest} command handler.
      *
      * @param req Jdbc primary keys metadata request.
-     * @return Result.
+     * @return Result future.
      */
-    JdbcMetaPrimaryKeysResult primaryKeysMeta(JdbcMetaPrimaryKeysRequest req);
+    CompletableFuture<JdbcMetaPrimaryKeysResult> primaryKeysMetaAsync(JdbcMetaPrimaryKeysRequest req);
 
     /**
      * {@link JdbcQueryMetadataRequest} command handler.
      *
      * @param req Jdbc query metadata request.
-     * @return Result.
+     * @return Result future.
      */
-    JdbcMetaColumnsResult queryMetadata(JdbcQueryMetadataRequest req);
+    CompletableFuture<JdbcMetaColumnsResult> queryMetadataAsync(JdbcQueryMetadataRequest req);
 }

modules/client-handler/src/main/java/org/apache/ignite/client/handler/requests/sql/ClientSqlCloseRequest.java
@@ -20,7 +20,6 @@ package org.apache.ignite.client.handler.requests.sql;
 import java.util.concurrent.CompletableFuture;
 import org.apache.ignite.client.proto.query.JdbcQueryEventHandler;
 import org.apache.ignite.client.proto.query.event.QueryCloseRequest;
-import org.apache.ignite.client.proto.query.event.QueryCloseResult;
 import org.apache.ignite.internal.client.proto.ClientMessagePacker;
 import org.apache.ignite.internal.client.proto.ClientMessageUnpacker;
 
@@ -34,7 +33,7 @@ public class ClientSqlCloseRequest {
      * @param in      Client message unpacker.
      * @param out     Client message packer.
      * @param handler Query event handler.
-     * @return null value indicates synchronous operation.
+     * @return Operation future.
      */
     public static CompletableFuture<Void> process(
             ClientMessageUnpacker in,
@@ -45,10 +44,6 @@ public class ClientSqlCloseRequest {
 
         req.readBinary(in);
 
-        QueryCloseResult res = handler.close(req);
-
-        res.writeBinary(out);
-
-        return null;
+        return handler.closeAsync(req).thenAccept(res -> res.writeBinary(out));
     }
 }

modules/client-handler/src/main/java/org/apache/ignite/client/handler/requests/sql/ClientSqlExecuteBatchRequest.java
@@ -20,7 +20,6 @@ package org.apache.ignite.client.handler.requests.sql;
 import java.util.concurrent.CompletableFuture;
 import org.apache.ignite.client.proto.query.JdbcQueryEventHandler;
 import org.apache.ignite.client.proto.query.event.BatchExecuteRequest;
-import org.apache.ignite.client.proto.query.event.BatchExecuteResult;
 import org.apache.ignite.internal.client.proto.ClientMessagePacker;
 import org.apache.ignite.internal.client.proto.ClientMessageUnpacker;
 
@@ -34,7 +33,7 @@ public class ClientSqlExecuteBatchRequest {
      * @param in      Client message unpacker.
      * @param out     Client message packer.
      * @param handler Query event handler.
-     * @return null value indicates synchronous operation.
+     * @return Operation future.
      */
     public static CompletableFuture<Void> process(
             ClientMessageUnpacker in,
@@ -45,10 +44,6 @@ public class ClientSqlExecuteBatchRequest {
 
         req.readBinary(in);
 
-        BatchExecuteResult res = handler.batch(req);
-
-        res.writeBinary(out);
-
-        return null;
+        return handler.batchAsync(req).thenAccept(res -> res.writeBinary(out));
     }
 }

modules/client-handler/src/main/java/org/apache/ignite/client/handler/requests/sql/ClientSqlExecuteRequest.java
@@ -20,7 +20,6 @@ package org.apache.ignite.client.handler.requests.sql;
 import java.util.concurrent.CompletableFuture;
 import org.apache.ignite.client.proto.query.JdbcQueryEventHandler;
 import org.apache.ignite.client.proto.query.event.QueryExecuteRequest;
-import org.apache.ignite.client.proto.query.event.QueryExecuteResult;
 import org.apache.ignite.internal.client.proto.ClientMessagePacker;
 import org.apache.ignite.internal.client.proto.ClientMessageUnpacker;
 
@@ -34,7 +33,7 @@ public class ClientSqlExecuteRequest {
      * @param in      Client message unpacker.
      * @param out     Client message packer.
      * @param handler Query event handler.
-     * @return null value indicates synchronous operation.
+     * @return Operation future.
      */
     public static CompletableFuture<Void> execute(
             ClientMessageUnpacker in,
@@ -45,10 +44,6 @@ public class ClientSqlExecuteRequest {
 
         req.readBinary(in);
 
-        QueryExecuteResult res = handler.query(req);
-
-        res.writeBinary(out);
-
-        return null;
+        return handler.queryAsync(req).thenAccept(res -> res.writeBinary(out));
     }
 }

modules/client/src/main/java/org/apache/ignite/internal/client/query/JdbcClientQueryEventHandler.java
@@ -17,6 +17,7 @@
 
 package org.apache.ignite.internal.client.query;
 
+import java.util.concurrent.CompletableFuture;
 import org.apache.ignite.client.proto.query.JdbcQueryEventHandler;
 import org.apache.ignite.client.proto.query.event.BatchExecuteRequest;
 import org.apache.ignite.client.proto.query.event.BatchExecuteResult;
@@ -56,91 +57,73 @@ public class JdbcClientQueryEventHandler implements JdbcQueryEventHandler {
 
     /** {@inheritDoc} */
     @Override
-    public QueryExecuteResult query(QueryExecuteRequest req) {
+    public CompletableFuture<QueryExecuteResult> queryAsync(QueryExecuteRequest req) {
         QueryExecuteResult res = new QueryExecuteResult();
 
-        client.sendRequest(ClientOp.SQL_EXEC, req, res);
-
-        return res;
+        return client.sendRequestAsync(ClientOp.SQL_EXEC, req, res);
     }
 
     /** {@inheritDoc} */
     @Override
-    public QueryFetchResult fetch(QueryFetchRequest req) {
+    public CompletableFuture<QueryFetchResult> fetchAsync(QueryFetchRequest req) {
         QueryFetchResult res = new QueryFetchResult();
 
-        client.sendRequest(ClientOp.SQL_NEXT, req, res);
-
-        return res;
+        return client.sendRequestAsync(ClientOp.SQL_NEXT, req, res);
     }
 
     /** {@inheritDoc} */
     @Override
-    public BatchExecuteResult batch(BatchExecuteRequest req) {
+    public CompletableFuture<BatchExecuteResult> batchAsync(BatchExecuteRequest req) {
         BatchExecuteResult res = new BatchExecuteResult();
 
-        client.sendRequest(ClientOp.SQL_EXEC_BATCH, req, res);
-
-        return res;
+        return client.sendRequestAsync(ClientOp.SQL_EXEC_BATCH, req, res);
     }
 
     /** {@inheritDoc} */
     @Override
-    public QueryCloseResult close(QueryCloseRequest req) {
+    public CompletableFuture<QueryCloseResult> closeAsync(QueryCloseRequest req) {
         QueryCloseResult res = new QueryCloseResult();
 
-        client.sendRequest(ClientOp.SQL_CURSOR_CLOSE, req, res);
-
-        return res;
+        return client.sendRequestAsync(ClientOp.SQL_CURSOR_CLOSE, req, res);
     }
 
     /** {@inheritDoc} */
     @Override
-    public JdbcMetaTablesResult tablesMeta(JdbcMetaTablesRequest req) {
+    public CompletableFuture<JdbcMetaTablesResult> tablesMetaAsync(JdbcMetaTablesRequest req) {
         JdbcMetaTablesResult res = new JdbcMetaTablesResult();
 
-        client.sendRequest(ClientOp.SQL_TABLE_META, req, res);
-
-        return res;
+        return client.sendRequestAsync(ClientOp.SQL_TABLE_META, req, res);
     }
 
     /** {@inheritDoc} */
     @Override
-    public JdbcMetaColumnsResult columnsMeta(JdbcMetaColumnsRequest req) {
+    public CompletableFuture<JdbcMetaColumnsResult> columnsMetaAsync(JdbcMetaColumnsRequest req) {
         JdbcMetaColumnsResult res = new JdbcMetaColumnsResult();
 
-        client.sendRequest(ClientOp.SQL_COLUMN_META, req, res);
-
-        return res;
+        return client.sendRequestAsync(ClientOp.SQL_COLUMN_META, req, res);
     }
 
     /** {@inheritDoc} */
     @Override
-    public JdbcMetaSchemasResult schemasMeta(JdbcMetaSchemasRequest req) {
+    public CompletableFuture<JdbcMetaSchemasResult> schemasMetaAsync(JdbcMetaSchemasRequest req) {
         JdbcMetaSchemasResult res = new JdbcMetaSchemasResult();
 
-        client.sendRequest(ClientOp.SQL_SCHEMAS_META, req, res);
-
-        return res;
+        return client.sendRequestAsync(ClientOp.SQL_SCHEMAS_META, req, res);
     }
 
     /** {@inheritDoc} */
     @Override
-    public JdbcMetaPrimaryKeysResult primaryKeysMeta(JdbcMetaPrimaryKeysRequest req) {
+    public CompletableFuture<JdbcMetaPrimaryKeysResult> primaryKeysMetaAsync(JdbcMetaPrimaryKeysRequest req) {
         JdbcMetaPrimaryKeysResult res = new JdbcMetaPrimaryKeysResult();
 
-        client.sendRequest(ClientOp.SQL_PK_META, req, res);
-
-        return res;
+        return client.sendRequestAsync(ClientOp.SQL_PK_META, req, res);
     }
 
     /** {@inheritDoc} */
     @Override
-    public JdbcMetaColumnsResult queryMetadata(JdbcQueryMetadataRequest req) {
+    public CompletableFuture<JdbcMetaColumnsResult> queryMetadataAsync(JdbcQueryMetadataRequest req) {
         JdbcMetaColumnsResult res = new JdbcMetaColumnsResult();
 
-        client.sendRequest(ClientOp.SQL_QUERY_META, req, res);
-
-        return res;
+        return client.sendRequestAsync(ClientOp.SQL_QUERY_META, req, res);
     }
 }

modules/client/src/main/java/org/apache/ignite/internal/jdbc/JdbcResultSet.java
@@ -207,7 +207,7 @@ public class JdbcResultSet implements ResultSet {
         ensureNotClosed();
 
         if ((rowsIter == null || !rowsIter.hasNext()) && !finished) {
-            QueryFetchResult res = qryHandler.fetch(new QueryFetchRequest(cursorId, fetchSize));
+            QueryFetchResult res = qryHandler.fetchAsync(new QueryFetchRequest(cursorId, fetchSize)).join();
 
             if (!res.hasResults()) {
                 throw IgniteQueryErrorCode.createJdbcSqlException(res.err(), res.status());
@@ -259,7 +259,7 @@ public class JdbcResultSet implements ResultSet {
 
         try {
             if (stmt != null && (!finished || (isQuery && !autoClose))) {
-                QueryCloseResult res = qryHandler.close(new QueryCloseRequest(cursorId));
+                QueryCloseResult res = qryHandler.closeAsync(new QueryCloseRequest(cursorId)).join();
 
                 if (!res.hasResults()) {
                     throw IgniteQueryErrorCode.createJdbcSqlException(res.err(), res.status());
@@ -2186,7 +2186,7 @@ public class JdbcResultSet implements ResultSet {
         }
 
         if (!metaInit) {
-            JdbcMetaColumnsResult res = qryHandler.queryMetadata(new JdbcQueryMetadataRequest(cursorId));
+            JdbcMetaColumnsResult res = qryHandler.queryMetadataAsync(new JdbcQueryMetadataRequest(cursorId)).join();
 
             meta = res.meta();
 

modules/client/src/main/java/org/apache/ignite/internal/jdbc/JdbcStatement.java
@@ -130,7 +130,7 @@ public class JdbcStatement implements Statement {
         QueryExecuteRequest req = new QueryExecuteRequest(schema, pageSize, maxRows, sql,
                 args == null ? ArrayUtils.OBJECT_EMPTY_ARRAY : args.toArray());
 
-        QueryExecuteResult res = conn.handler().query(req);
+        QueryExecuteResult res = conn.handler().queryAsync(req).join();
 
         if (!res.hasResults()) {
             throw IgniteQueryErrorCode.createJdbcSqlException(res.err(), res.status());
@@ -547,7 +547,7 @@ public class JdbcStatement implements Statement {
         BatchExecuteRequest req = new BatchExecuteRequest(conn.getSchema(), batch, conn.getAutoCommit());
 
         try {
-            BatchExecuteResult res = conn.handler().batch(req);
+            BatchExecuteResult res = conn.handler().batchAsync(req).join();
 
             if (!res.hasResults()) {
                 throw new BatchUpdateException(res.err(),