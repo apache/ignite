gridgain/incubator-ignite

IGNITE-15733 Eventually failure of baseline registration

modules/core/src/main/java/org/apache/ignite/internal/managers/discovery/GridDiscoveryManager.java
@@ -669,6 +669,8 @@ public class GridDiscoveryManager extends GridManagerAdapter<DiscoverySpi> {
                     else if (customMsg instanceof ChangeGlobalStateFinishMessage) {
                         ctx.state().onStateFinishMessage((ChangeGlobalStateFinishMessage)customMsg);
 
+                        ctx.cluster().onLocalJoin();
+
                         Snapshot snapshot = topSnap.get();
 
                         // Topology version does not change, but need create DiscoCache with new state.
@@ -821,8 +823,6 @@ public class GridDiscoveryManager extends GridManagerAdapter<DiscoverySpi> {
                             ctx.service().onLocalJoin(discoEvt, discoCache);
 
                             ctx.encryption().onLocalJoin();
-
-                            ctx.cluster().onLocalJoin();
                         }
                     }
 

apache/ignite

modules/core/src/main/java/org/apache/ignite/internal/managers/discovery/GridDiscoveryManager.java
@@ -663,6 +663,8 @@ public class GridDiscoveryManager extends GridManagerAdapter<DiscoverySpi> {
                             new AffinityTopologyVersion(topVer, minorTopVer),
                             (ChangeGlobalStateMessage)customMsg,
                             discoCache());
+
+                        ctx.cluster().onChangeState();
                     }
                     else if (customMsg instanceof ChangeGlobalStateFinishMessage) {
                         ctx.state().onStateFinishMessage((ChangeGlobalStateFinishMessage)customMsg);