apache/ignite-3

IGNITE-15945 Class inheritance and effectively final fields

modules/network/src/main/java/org/apache/ignite/internal/network/serialization/marshal/BuiltInContainerMarshallers.java
@@ -18,13 +18,14 @@
 package org.apache.ignite.internal.network.serialization.marshal;
 
 import static java.util.Collections.singletonList;
+import static org.apache.ignite.internal.network.serialization.marshal.ProtocolMarshalling.writeLength;
 
 import java.io.DataInput;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.BitSet;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -35,6 +36,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.IntFunction;
 import org.apache.ignite.internal.network.serialization.ClassDescriptor;
+import org.apache.ignite.internal.network.serialization.Classes;
 
 /**
  * Utility to (un)marshal built-in collections and maps.
@@ -65,25 +67,67 @@ class BuiltInContainerMarshallers {
     /**
      * Used to write elements.
      */
-    private final ValueWriter<?> elementWriter;
+    private final TypedValueWriter typedWriter;
+    private final ValueWriter<Object> untypedWriter;
 
-    BuiltInContainerMarshallers(ValueWriter<?> elementWriter) {
-        this.elementWriter = elementWriter;
+    private final TypedValueReader typedReader;
+
+    BuiltInContainerMarshallers(TypedValueWriter typedWriter, TypedValueReader typedReader) {
+        this.typedWriter = typedWriter;
+        untypedWriter = (obj, out, ctx) -> typedWriter.write(obj, null, out, ctx);
+        this.typedReader = typedReader;
     }
 
     void writeGenericRefArray(Object[] array, ClassDescriptor arrayDescriptor, DataOutputStream output, MarshallingContext context)
             throws IOException, MarshalException {
-        output.writeUTF(array.getClass().getComponentType().getName());
-        writeCollection(Arrays.asList(array), arrayDescriptor, output, context);
+        Class<?> componentType = array.getClass().getComponentType();
+
+        BuiltInMarshalling.writeClass(componentType, output);
+        writeLength(array.length, output);
+
+        if (array.length > 0 && Classes.isValueTypeKnownUpfront(componentType)) {
+            BitSet nullsBitSet = new BitSet(array.length);
+
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] == null) {
+                    nullsBitSet.set(i);
+                }
+            }
+
+            BuiltInMarshalling.writeBitSet(nullsBitSet, output);
+        }
+
+        for (Object object : array) {
+            typedWriter.write(object, componentType, output, context);
+        }
+
+        context.addUsedDescriptor(arrayDescriptor);
     }
 
     <T> T[] preInstantiateGenericRefArray(DataInput input, UnmarshallingContext context) throws IOException, UnmarshalException {
         return BuiltInMarshalling.preInstantiateGenericRefArray(input, context);
     }
 
-    <T> void fillGenericRefArray(DataInputStream input, T[] array, ValueReader<T> elementReader, UnmarshallingContext context)
+    @SuppressWarnings("unchecked")
+    <T> void fillGenericRefArray(DataInputStream input, T[] array, UnmarshallingContext context)
             throws IOException, UnmarshalException {
-        BuiltInMarshalling.fillGenericRefArrayFrom(input, array, elementReader, context);
+        if (array.length == 0) {
+            return;
+        }
+
+        if (Classes.isValueTypeKnownUpfront(array.getClass().getComponentType())) {
+            BitSet nullsBitSet = BuiltInMarshalling.readBitSet(input);
+
+            for (int i = 0; i < array.length; i++) {
+                if (!nullsBitSet.get(i)) {
+                    array[i] = (T) typedReader.read(input, array.getClass().getComponentType(), context);
+                }
+            }
+        } else {
+            for (int i = 0; i < array.length; i++) {
+                array[i] = (T) typedReader.read(input, array.getClass().getComponentType(), context);
+            }
+        }
     }
 
     void writeBuiltInCollection(Collection<?> object, ClassDescriptor descriptor, DataOutputStream output, MarshallingContext context)
@@ -117,12 +161,12 @@ class BuiltInContainerMarshallers {
     ) throws IOException, MarshalException {
         context.addUsedDescriptor(collectionDescriptor);
 
-        BuiltInMarshalling.writeCollection(collection, output, valueWriter(), context);
+        BuiltInMarshalling.writeCollection(collection, output, untypedWriter(), context);
     }
 
     @SuppressWarnings("unchecked")
-    private <T> ValueWriter<T> valueWriter() {
-        return (ValueWriter<T>) elementWriter;
+    private <T> ValueWriter<T> untypedWriter() {
+        return (ValueWriter<T>) untypedWriter;
     }
 
     private void writeSingletonList(List<?> list, ClassDescriptor listDescriptor, DataOutputStream output, MarshallingContext context)
@@ -133,7 +177,7 @@ class BuiltInContainerMarshallers {
 
         context.addUsedDescriptor(listDescriptor);
 
-        valueWriter().write(element, output, context);
+        typedWriter.write(element, null, output, context);
     }
 
     @SuppressWarnings("unchecked")
@@ -198,7 +242,7 @@ class BuiltInContainerMarshallers {
 
         context.addUsedDescriptor(mapDescriptor);
 
-        BuiltInMarshalling.writeMap(map, output, valueWriter(), valueWriter(), context);
+        BuiltInMarshalling.writeMap(map, output, untypedWriter(), untypedWriter(), context);
     }
 
     private boolean supportsAsBuiltInMap(ClassDescriptor mapDescriptor) {

modules/network/src/test/java/org/apache/ignite/internal/network/serialization/ClassesTest.java
@@ -17,6 +17,8 @@
 
 package org.apache.ignite.internal.network.serialization;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -25,6 +27,8 @@ import java.io.ObjectInput;
 import java.io.ObjectOutput;
 import java.io.Serializable;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ClassesTest {
     @Test
@@ -103,6 +107,101 @@ class ClassesTest {
         assertTrue(Classes.isExternalizable(EmptyExternalizable.class));
     }
 
+    @Test
+    void isRuntimeEnumReturnsFalseForNonEnum() {
+        assertFalse(Classes.isRuntimeEnum(Object.class));
+    }
+
+    @Test
+    void isRuntimeEnumReturnsTrueForSimpleEnums() {
+        assertTrue(Classes.isRuntimeEnum(SimpleEnum.class));
+    }
+
+    @Test
+    void isRuntimeEnumReturnsTrueForAnonClassesForMembersOfEnum() {
+        assertTrue(Classes.isRuntimeEnum(EnumWithAnonClassMember.class));
+    }
+
+    @Test
+    void isRuntimeEnumReturnsFalseForAbstractEnumClass() {
+        assertFalse(Classes.isRuntimeEnum(Enum.class));
+    }
+
+    @Test
+    void enumClassAsInSourceCodeReturnsTheClassItselfForSimpleEnums() {
+        assertThat(Classes.enumClassAsInSourceCode(SimpleEnum.MEMBER.getClass()), is(SimpleEnum.class));
+    }
+
+    @Test
+    void enumClassAsInSourceCodeReturnsTheClassItselfForAnonymousEnumSubclasses() {
+        assertThat(Classes.enumClassAsInSourceCode(EnumWithAnonClassMember.MEMBER.getClass()), is(EnumWithAnonClassMember.class));
+    }
+
+    @Test
+    void isValueTypeKnownUpfrontReturnsTrueForFinalNonArrayClasses() {
+        assertTrue(Classes.isValueTypeKnownUpfront(FinalClass.class));
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {byte.class, short.class, int.class, long.class, float.class, double.class, char.class, boolean.class})
+    void isValueTypeKnownUpfrontReturnsTrueForPrimitiveClasses(Class<?> primitiveClass) {
+        assertTrue(Classes.isValueTypeKnownUpfront(primitiveClass));
+    }
+
+    @Test
+    void isValueTypeKnownUpfrontReturnsFalseForNonFinalNonArrayClasses() {
+        assertFalse(Classes.isValueTypeKnownUpfront(NonFinalClass.class));
+    }
+
+    @Test
+    void isValueTypeKnownUpfrontReturnsTrueForSimpleEnumClasses() {
+        assertTrue(Classes.isValueTypeKnownUpfront(SimpleEnum.class));
+    }
+
+    @Test
+    void isValueTypeKnownUpfrontReturnsTrueForEnumClassesWithAnonMembers() {
+        assertTrue(Classes.isValueTypeKnownUpfront(EnumWithAnonClassMember.class));
+    }
+
+    @Test
+    void isValueTypeKnownUpfrontReturnsFalseForAbstractEnum() {
+        assertFalse(Classes.isValueTypeKnownUpfront(Enum.class));
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {
+            byte[].class, short[].class, int[].class, long[].class,
+            float[].class, double[].class, char[].class, boolean[].class
+    })
+    void isValueTypeKnownUpfrontReturnsTrueForPrimitiveArrayClasses(Class<?> primitiveArrayClass) {
+        assertTrue(Classes.isValueTypeKnownUpfront(primitiveArrayClass));
+    }
+
+    @Test
+    void isValueTypeKnownUpfrontReturnsTrueForArraysOfFinalClasses() {
+        assertTrue(Classes.isValueTypeKnownUpfront(FinalClass[].class));
+    }
+
+    @Test
+    void isValueTypeKnownUpfrontReturnsFalseForArraysOfNonFinalClasses() {
+        assertFalse(Classes.isValueTypeKnownUpfront(NonFinalClass[].class));
+    }
+
+    @Test
+    void isValueTypeKnownUpfrontReturnsTrueForArraysOfSimpleEnumClasses() {
+        assertTrue(Classes.isValueTypeKnownUpfront(SimpleEnum[].class));
+    }
+
+    @Test
+    void isValueTypeKnownUpfrontReturnsTrueForArraysOfEnumClassesWithAnonMembers() {
+        assertTrue(Classes.isValueTypeKnownUpfront(EnumWithAnonClassMember[].class));
+    }
+
+    @Test
+    void isValueTypeKnownUpfrontReturnsFalseForArraysOfAbstractEnum() {
+        assertFalse(Classes.isValueTypeKnownUpfront(Enum[].class));
+    }
+
     private static class EmptySerializable implements Serializable {
     }
 
@@ -122,5 +221,20 @@ class ClassesTest {
         public void readExternal(ObjectInput in) {
             // no-op
         }
+    }
+
+    private enum SimpleEnum {
+        MEMBER
+    }
+
+    private enum EnumWithAnonClassMember {
+        MEMBER {
+        }
+    }
+
+    private static final class FinalClass {
+    }
+
+    private static class NonFinalClass {
     }
 }

apache/ignite

modules/network/src/main/java/org/apache/ignite/internal/network/serialization/marshal/BuiltInContainerMarshallers.java
@@ -18,13 +18,14 @@
 package org.apache.ignite.internal.network.serialization.marshal;
 
 import static java.util.Collections.singletonList;
+import static org.apache.ignite.internal.network.serialization.marshal.ProtocolMarshalling.writeLength;
 
 import java.io.DataInput;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.BitSet;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -35,6 +36,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.IntFunction;
 import org.apache.ignite.internal.network.serialization.ClassDescriptor;
+import org.apache.ignite.internal.network.serialization.Classes;
 
 /**
  * Utility to (un)marshal built-in collections and maps.
@@ -65,25 +67,67 @@ class BuiltInContainerMarshallers {
     /**
      * Used to write elements.
      */
-    private final ValueWriter<?> elementWriter;
+    private final TypedValueWriter typedWriter;
+    private final ValueWriter<Object> untypedWriter;
 
-    BuiltInContainerMarshallers(ValueWriter<?> elementWriter) {
-        this.elementWriter = elementWriter;
+    private final TypedValueReader typedReader;
+
+    BuiltInContainerMarshallers(TypedValueWriter typedWriter, TypedValueReader typedReader) {
+        this.typedWriter = typedWriter;
+        untypedWriter = (obj, out, ctx) -> typedWriter.write(obj, null, out, ctx);
+        this.typedReader = typedReader;
     }
 
     void writeGenericRefArray(Object[] array, ClassDescriptor arrayDescriptor, DataOutputStream output, MarshallingContext context)
             throws IOException, MarshalException {
-        output.writeUTF(array.getClass().getComponentType().getName());
-        writeCollection(Arrays.asList(array), arrayDescriptor, output, context);
+        Class<?> componentType = array.getClass().getComponentType();
+
+        BuiltInMarshalling.writeClass(componentType, output);
+        writeLength(array.length, output);
+
+        if (array.length > 0 && Classes.isRuntimeTypeKnownUpfront(componentType)) {
+            BitSet nullsBitSet = new BitSet(array.length);
+
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] == null) {
+                    nullsBitSet.set(i);
+                }
+            }
+
+            BuiltInMarshalling.writeBitSet(nullsBitSet, output);
+        }
+
+        for (Object object : array) {
+            typedWriter.write(object, componentType, output, context);
+        }
+
+        context.addUsedDescriptor(arrayDescriptor);
     }
 
     <T> T[] preInstantiateGenericRefArray(DataInput input, UnmarshallingContext context) throws IOException, UnmarshalException {
         return BuiltInMarshalling.preInstantiateGenericRefArray(input, context);
     }
 
-    <T> void fillGenericRefArray(DataInputStream input, T[] array, ValueReader<T> elementReader, UnmarshallingContext context)
+    @SuppressWarnings("unchecked")
+    <T> void fillGenericRefArray(DataInputStream input, T[] array, UnmarshallingContext context)
             throws IOException, UnmarshalException {
-        BuiltInMarshalling.fillGenericRefArrayFrom(input, array, elementReader, context);
+        if (array.length == 0) {
+            return;
+        }
+
+        if (Classes.isRuntimeTypeKnownUpfront(array.getClass().getComponentType())) {
+            BitSet nullsBitSet = BuiltInMarshalling.readBitSet(input);
+
+            for (int i = 0; i < array.length; i++) {
+                if (!nullsBitSet.get(i)) {
+                    array[i] = (T) typedReader.read(input, array.getClass().getComponentType(), context);
+                }
+            }
+        } else {
+            for (int i = 0; i < array.length; i++) {
+                array[i] = (T) typedReader.read(input, array.getClass().getComponentType(), context);
+            }
+        }
     }
 
     void writeBuiltInCollection(Collection<?> object, ClassDescriptor descriptor, DataOutputStream output, MarshallingContext context)
@@ -117,12 +161,12 @@ class BuiltInContainerMarshallers {
     ) throws IOException, MarshalException {
         context.addUsedDescriptor(collectionDescriptor);
 
-        BuiltInMarshalling.writeCollection(collection, output, valueWriter(), context);
+        BuiltInMarshalling.writeCollection(collection, output, untypedWriter(), context);
     }
 
     @SuppressWarnings("unchecked")
-    private <T> ValueWriter<T> valueWriter() {
-        return (ValueWriter<T>) elementWriter;
+    private <T> ValueWriter<T> untypedWriter() {
+        return (ValueWriter<T>) untypedWriter;
     }
 
     private void writeSingletonList(List<?> list, ClassDescriptor listDescriptor, DataOutputStream output, MarshallingContext context)
@@ -133,7 +177,7 @@ class BuiltInContainerMarshallers {
 
         context.addUsedDescriptor(listDescriptor);
 
-        valueWriter().write(element, output, context);
+        typedWriter.write(element, null, output, context);
     }
 
     @SuppressWarnings("unchecked")
@@ -198,7 +242,7 @@ class BuiltInContainerMarshallers {
 
         context.addUsedDescriptor(mapDescriptor);
 
-        BuiltInMarshalling.writeMap(map, output, valueWriter(), valueWriter(), context);
+        BuiltInMarshalling.writeMap(map, output, untypedWriter(), untypedWriter(), context);
     }
 
     private boolean supportsAsBuiltInMap(ClassDescriptor mapDescriptor) {

modules/network/src/test/java/org/apache/ignite/internal/network/serialization/ClassesTest.java
@@ -17,6 +17,8 @@
 
 package org.apache.ignite.internal.network.serialization;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -25,6 +27,8 @@ import java.io.ObjectInput;
 import java.io.ObjectOutput;
 import java.io.Serializable;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ClassesTest {
     @Test
@@ -103,6 +107,101 @@ class ClassesTest {
         assertTrue(Classes.isExternalizable(EmptyExternalizable.class));
     }
 
+    @Test
+    void isRuntimeEnumReturnsFalseForNonEnum() {
+        assertFalse(Classes.isRuntimeEnum(Object.class));
+    }
+
+    @Test
+    void isRuntimeEnumReturnsTrueForSimpleEnums() {
+        assertTrue(Classes.isRuntimeEnum(SimpleEnum.class));
+    }
+
+    @Test
+    void isRuntimeEnumReturnsTrueForAnonClassesForMembersOfEnum() {
+        assertTrue(Classes.isRuntimeEnum(EnumWithAnonClassMember.class));
+    }
+
+    @Test
+    void isRuntimeEnumReturnsFalseForAbstractEnumClass() {
+        assertFalse(Classes.isRuntimeEnum(Enum.class));
+    }
+
+    @Test
+    void enumClassAsInSourceCodeReturnsTheClassItselfForSimpleEnums() {
+        assertThat(Classes.enumClassAsInSourceCode(SimpleEnum.MEMBER.getClass()), is(SimpleEnum.class));
+    }
+
+    @Test
+    void enumClassAsInSourceCodeReturnsTheClassItselfForAnonymousEnumSubclasses() {
+        assertThat(Classes.enumClassAsInSourceCode(EnumWithAnonClassMember.MEMBER.getClass()), is(EnumWithAnonClassMember.class));
+    }
+
+    @Test
+    void isValueTypeKnownUpfrontReturnsTrueForFinalNonArrayClasses() {
+        assertTrue(Classes.isRuntimeTypeKnownUpfront(FinalClass.class));
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {byte.class, short.class, int.class, long.class, float.class, double.class, char.class, boolean.class})
+    void isValueTypeKnownUpfrontReturnsTrueForPrimitiveClasses(Class<?> primitiveClass) {
+        assertTrue(Classes.isRuntimeTypeKnownUpfront(primitiveClass));
+    }
+
+    @Test
+    void isValueTypeKnownUpfrontReturnsFalseForNonFinalNonArrayClasses() {
+        assertFalse(Classes.isRuntimeTypeKnownUpfront(NonFinalClass.class));
+    }
+
+    @Test
+    void isValueTypeKnownUpfrontReturnsTrueForSimpleEnumClasses() {
+        assertTrue(Classes.isRuntimeTypeKnownUpfront(SimpleEnum.class));
+    }
+
+    @Test
+    void isValueTypeKnownUpfrontReturnsTrueForEnumClassesWithAnonMembers() {
+        assertTrue(Classes.isRuntimeTypeKnownUpfront(EnumWithAnonClassMember.class));
+    }
+
+    @Test
+    void isValueTypeKnownUpfrontReturnsFalseForAbstractEnum() {
+        assertFalse(Classes.isRuntimeTypeKnownUpfront(Enum.class));
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {
+            byte[].class, short[].class, int[].class, long[].class,
+            float[].class, double[].class, char[].class, boolean[].class
+    })
+    void isValueTypeKnownUpfrontReturnsTrueForPrimitiveArrayClasses(Class<?> primitiveArrayClass) {
+        assertTrue(Classes.isRuntimeTypeKnownUpfront(primitiveArrayClass));
+    }
+
+    @Test
+    void isValueTypeKnownUpfrontReturnsTrueForArraysOfFinalClasses() {
+        assertTrue(Classes.isRuntimeTypeKnownUpfront(FinalClass[].class));
+    }
+
+    @Test
+    void isValueTypeKnownUpfrontReturnsFalseForArraysOfNonFinalClasses() {
+        assertFalse(Classes.isRuntimeTypeKnownUpfront(NonFinalClass[].class));
+    }
+
+    @Test
+    void isValueTypeKnownUpfrontReturnsTrueForArraysOfSimpleEnumClasses() {
+        assertTrue(Classes.isRuntimeTypeKnownUpfront(SimpleEnum[].class));
+    }
+
+    @Test
+    void isValueTypeKnownUpfrontReturnsTrueForArraysOfEnumClassesWithAnonMembers() {
+        assertTrue(Classes.isRuntimeTypeKnownUpfront(EnumWithAnonClassMember[].class));
+    }
+
+    @Test
+    void isValueTypeKnownUpfrontReturnsFalseForArraysOfAbstractEnum() {
+        assertFalse(Classes.isRuntimeTypeKnownUpfront(Enum[].class));
+    }
+
     private static class EmptySerializable implements Serializable {
     }
 
@@ -122,5 +221,20 @@ class ClassesTest {
         public void readExternal(ObjectInput in) {
             // no-op
         }
+    }
+
+    private enum SimpleEnum {
+        MEMBER
+    }
+
+    private enum EnumWithAnonClassMember {
+        MEMBER {
+        }
+    }
+
+    private static final class FinalClass {
+    }
+
+    private static class NonFinalClass {
     }
 }