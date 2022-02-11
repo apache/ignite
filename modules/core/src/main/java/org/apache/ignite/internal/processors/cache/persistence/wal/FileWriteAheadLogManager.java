08214ee3680011e8d75809fa4e6b37a920474c26
Nikolay <nizhikov@apache.org>
2022-02-11 21:53:25 +0300

    IGNITE-16523 Rollover by timeout fixed (#9815)
    (cherry picked from commit 6ca3e2d3ab1ee6bef8588d202fd1e38df3a00244)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/FileWriteAheadLogManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/FileWriteAheadLogManager.java
@@ -796,6 +796,9 @@ public class FileWriteAheadLogManager extends GridCacheSharedManagerAdapter impl
         assert timeoutRolloverMux != null;
 
         synchronized (timeoutRolloverMux) {
+            if (timeoutRollover != null)
+                return;
+
             long nextEndTime = walForceArchiveTimeout > 0
                 ? nextTimeout(lastRolloverMs.get(), walForceArchiveTimeout)
                 : nextTimeout(lastRecordLoggedMs.get(), walAutoArchiveAfterInactivity);
@@ -806,7 +809,7 @@ public class FileWriteAheadLogManager extends GridCacheSharedManagerAdapter impl
 
     /** */
     private long nextTimeout(long lastEvt, long timeout) {
-        return lastEvt <= 0 ? U.currentTimeMillis() : lastEvt + timeout;
+        return (lastEvt <= 0 ? U.currentTimeMillis() : lastEvt) + timeout;
     }
 
     /** {@inheritDoc} */
@@ -3514,11 +3517,11 @@ public class FileWriteAheadLogManager extends GridCacheSharedManagerAdapter impl
             assert timeoutRolloverMux != null;
 
             synchronized (timeoutRolloverMux) {
+                timeoutRollover = null;
+
                 if (!cancel) {
-                    if (log.isDebugEnabled()) {
-                        log.debug("Checking if WAL rollover required (" +
-                            new Time(U.currentTimeMillis()).toString() + ")");
-                    }
+                    if (log.isDebugEnabled())
+                        log.debug("Checking if WAL rollover required (" + new Time(U.currentTimeMillis()) + ")");
 
                     checkWalRolloverRequired();
 
@@ -3557,6 +3560,8 @@ public class FileWriteAheadLogManager extends GridCacheSharedManagerAdapter impl
                     timeoutRollover.cancel();
 
                     cctx.time().removeTimeoutObject(timeoutRollover);
+
+                    this.timeoutRollover = null;
                 }
             }
         }
