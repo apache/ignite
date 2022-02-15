c0899872f3fa9b5fc180537bd2bcc0a7473ec7c6
Ryzhov Sergei <s.vi.ryzhov@gmail.com>
2022-02-15 10:12:57 +0300

    IGNITE-13389: fix test ClientSecurityPermissionsTest.cs

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Client/ClientSecurityPermissionsTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Client/ClientSecurityPermissionsTest.cs
@@ -86,7 +86,7 @@ namespace Apache.Ignite.Core.Tests.Client
                 var clientEx = (IgniteClientException)ex.GetBaseException();
 
                 Assert.AreEqual(ClientStatusCode.SecurityViolation, clientEx.StatusCode);
-                Assert.AreEqual("Client is not authorized to perform this operation", clientEx.Message);
+                Assert.IsTrue(clientEx.Message.StartsWith("Client is not authorized to perform this operation"));
             }
         }
 
