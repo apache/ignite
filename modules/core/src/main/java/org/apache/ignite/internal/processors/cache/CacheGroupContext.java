95ad019446f4de6021b23c2ca00c843d1c94eaee
Semyon Danilov <samvimes@yandex.ru>
2022-02-15 12:23:45 +0300

    IGNITE-16662 Fix NPE in PersistenceTask

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/CacheGroupContext.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/CacheGroupContext.java
@@ -1267,7 +1267,7 @@ public class CacheGroupContext {
     }
 
     /**
-     * @param enabled Enabled flag..
+     * @param enabled Enabled flag.
      */
     private void persistGlobalWalState(boolean enabled) {
         shared().database().walEnabled(grpId, enabled, false);
