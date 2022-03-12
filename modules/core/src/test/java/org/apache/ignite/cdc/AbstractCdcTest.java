5555e8b5ee36006325eb151f90669fa7a44aee7c
Nikita Amelchev <nsamelchev@gmail.com>
2022-03-12 12:27:34 +0300

    IGNITE-14829 Save DataEntry index inside CDC state (#9828)

--- modules/core/src/test/java/org/apache/ignite/cdc/AbstractCdcTest.java
+++ modules/core/src/test/java/org/apache/ignite/cdc/AbstractCdcTest.java
@@ -42,6 +42,7 @@ import org.apache.ignite.internal.processors.metric.MetricRegistry;
 import org.apache.ignite.internal.util.lang.GridAbsPredicate;
 import org.apache.ignite.internal.util.typedef.CI3;
 import org.apache.ignite.internal.util.typedef.F;
+import org.apache.ignite.internal.util.typedef.T2;
 import org.apache.ignite.lang.IgniteBiTuple;
 import org.apache.ignite.spi.metric.LongMetric;
 import org.apache.ignite.spi.metric.MetricExporterSpi;
@@ -86,6 +87,15 @@ public abstract class AbstractCdcTest extends GridCommonAbstractTest {
         super.beforeTest();
     }
 
+    /** {@inheritDoc} */
+    @Override protected void afterTestsStopped() throws Exception {
+        super.afterTestsStopped();
+
+        stopAllGrids();
+
+        cleanPersistenceDir();
+    }
+
     /** */
     protected CdcMain createCdc(CdcConsumer cnsmr, IgniteConfiguration cfg) {
         return createCdc(cnsmr, cfg, null);
@@ -107,8 +117,8 @@ public abstract class AbstractCdcTest extends GridCommonAbstractTest {
         return new CdcMain(cfg, null, cdcCfg) {
             @Override protected CdcConsumerState createState(Path stateDir) {
                 return new CdcConsumerState(stateDir) {
-                    @Override public void save(WALPointer ptr) throws IOException {
-                        super.save(ptr);
+                    @Override public void save(T2<WALPointer, Integer> state) throws IOException {
+                        super.save(state);
 
                         if (!F.isEmpty(conditions)) {
                             for (GridAbsPredicate p : conditions) {
