3ab14049d534687d5b066e2c70d5c5699dd71f38
vladsz83 <vladsz83@gmail.com>
2022-03-16 11:34:35 +0300

    minority

--- modules/calcite/src/test/java/org/apache/ignite/internal/processors/query/calcite/DataTypesTest.java
+++ modules/calcite/src/test/java/org/apache/ignite/internal/processors/query/calcite/DataTypesTest.java
@@ -101,7 +101,7 @@ public class DataTypesTest extends AbstractBasicIntegrationTest {
                 .returns(2, uuid2.toString(), uuid2)
                 .check();
 
-            // TODO: https://issues.apache.org/jira/browse/IGNITE-16693 Incorrect processing nulls values in merge join.
+            //TODO: https://issues.apache.org/jira/browse/IGNITE-16693 Incorrect processing nulls values in merge join.
             assertQuery("SELECT /*+ DISABLE_RULE('MergeJoinConverter') */ t1.* from t t1, (select * from t) t2 " +
                 "where t1.uid = t2.uid")
                 .returns(1, "fd10556e-fc27-4a99-b5e4-89b8344cb3ce", uuid1)
