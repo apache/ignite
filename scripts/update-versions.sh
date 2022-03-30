73fe1cfd37996fc2cafd2eb4e3f26f3111b9fd04
Maxim Muzafarov <maxmuzaf@gmail.com>
2022-03-30 17:10:36 +0300

    IGNITE-16592 Fix review comments

--- scripts/update-versions.sh
+++ scripts/update-versions.sh
@@ -35,7 +35,7 @@ sed -i '' -e "s/<revision>.*<\/revision>/<revision>$1<\/revision>/" ./parent/pom
 cd modules/apache-license-gen
 mvn versions:set -DnewVersion=$1 -Pall-java,all-scala,all-other -DgenerateBackupPoms=false -DgroupId=* -DartifactId=* -DoldVersion=* -DprocessDependencies=false
 
-cd ../shared
+cd ../checkstyle
 mvn versions:set -DnewVersion=$1 -Pall-java,all-scala,all-other -DgenerateBackupPoms=false -DgroupId=* -DartifactId=* -DoldVersion=* -DprocessDependencies=false
 
 cd ../../
