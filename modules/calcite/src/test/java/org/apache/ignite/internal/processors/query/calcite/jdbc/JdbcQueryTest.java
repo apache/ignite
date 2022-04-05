82a08e8b2344d50d8dfc41ce9e9249d1f27ef622
Vladimir Steshin <vladsz83@gmail.com>
2022-04-05 13:33:46 +0300

    fix

--- modules/calcite/src/test/java/org/apache/ignite/internal/processors/query/calcite/jdbc/JdbcQueryTest.java
+++ modules/calcite/src/test/java/org/apache/ignite/internal/processors/query/calcite/jdbc/JdbcQueryTest.java
@@ -20,6 +20,7 @@ package org.apache.ignite.internal.processors.query.calcite.jdbc;
 import java.sql.BatchUpdateException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
