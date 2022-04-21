3b753b4a358d8d18b0ca2754e829c0db4670077a
Mikhail Petrov <pmgheap.sbt@gmail.com>
2022-04-21 15:49:36 +0300

    IGNITE-16806 Fixes minor issues.

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/IgniteQueryConvertibleTypesValidationTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/IgniteQueryConvertibleTypesValidationTest.java
@@ -30,12 +30,16 @@ import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
+import org.apache.ignite.cache.QueryEntity;
 import org.apache.ignite.cache.query.SqlFieldsQuery;
+import org.apache.ignite.configuration.CacheConfiguration;
+import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import static java.util.Collections.singletonList;
 import static org.h2.util.LocalDateTimeUtils.localDateToDateValue;
 import static org.h2.util.LocalDateTimeUtils.localTimeToTimeValue;
 
@@ -59,46 +63,49 @@ public class IgniteQueryConvertibleTypesValidationTest extends GridCommonAbstrac
     public Function<Object, Object> sqlTypeConverter;
 
     /** */
-    @Parameterized.Parameters(name = "isValidationEnabled={0}, sqlType={1}, testObjCls={2}")
+    @Parameterized.Parameter(4)
+    public boolean isDdl;
+
+    /** */
+    @Parameterized.Parameters(name = "isValidationEnabled={0}, sqlType={1}, testObjCls={2}, isDdl={4}")
     public static Collection<Object[]> parameters() {
         Collection<Object[]> params = new ArrayList<>();
 
-        for (boolean v : Arrays.asList(true, false)) {
-            params.add(new Object[] {v, "TIMESTAMP", LocalDateTime.class, f(d -> Timestamp.valueOf((LocalDateTime)d))});
-            params.add(new Object[] {v, "TIMESTAMP", Date.class, f(d -> new Timestamp(((Date)d).getTime()))});
-            params.add(new Object[] {v, "TIMESTAMP", java.sql.Date.class, f(d -> new Timestamp(((Date)d).getTime()))});
-            params.add(new Object[] {v, "DATE", LocalDate.class, f(d -> localDateToDateValue(d).getDate())});
-            params.add(new Object[] {v, "TIME", LocalTime.class, f(d -> localTimeToTimeValue(d).getTime())});
+        for (boolean isV : Arrays.asList(true, false)) {
+            for (boolean isDdl : Arrays.asList(true, false)) {
+                params.add(new Object[] {isV, "TIMESTAMP", LocalDateTime.class, f(d -> Timestamp.valueOf((LocalDateTime)d)), isDdl});
+                params.add(new Object[] {isV, "TIMESTAMP", Date.class, f(d -> new Timestamp(((Date)d).getTime())), isDdl});
+                params.add(new Object[] {isV, "TIMESTAMP", java.sql.Date.class, f(d -> new Timestamp(((Date)d).getTime())), isDdl});
+                params.add(new Object[] {isV, "DATE", LocalDate.class, f(d -> localDateToDateValue(d).getDate()), isDdl});
+                params.add(new Object[] {isV, "TIME", LocalTime.class, f(d -> localTimeToTimeValue(d).getTime()), isDdl});
+            }
         }
 
         return params;
     }
 
     /** {@inheritDoc} */
-    @Override protected void beforeTestsStarted() throws Exception {
-        super.beforeTestsStarted();
+    @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
+        IgniteConfiguration cfg = super.getConfiguration(igniteInstanceName);
 
-        startGrid(0);
+        cfg.getSqlConfiguration().setValidationEnabled(true);
+
+        return cfg;
     }
 
     /** {@inheritDoc} */
     @Override protected void beforeTest() throws Exception {
         super.beforeTest();
 
-        execute("DROP TABLE IF EXISTS DATA;");
-
-        grid(0).destroyCache(DEFAULT_CACHE_NAME);
-
-        grid(0).binary().types().forEach(type -> grid(0).context().cacheObjects().removeType(type.typeId()));
+        stopAllGrids();
     }
 
     /** */
     @Test
     public void testIgniteQueryConvertibleTypesValidation() throws Exception {
-        execute("CREATE TABLE DATA (id INT PRIMARY KEY, data " + sqlType + ") WITH" +
-            " \"KEY_TYPE=java.lang.Integer" +
-            ", VALUE_TYPE=org.apache.ignite.internal.processors.query.IgniteQueryConvertibleTypesValidationTest$Data" +
-            ", CACHE_NAME=default\"");
+        startGrid(0);
+
+        createTable();
 
         execute("CREATE INDEX DATA_IDX ON DATA(data DESC);");
 
@@ -118,8 +125,31 @@ public class IgniteQueryConvertibleTypesValidationTest extends GridCommonAbstrac
     }
 
     /** */
-    private static <T, R> Function<Object, Object> f(Function<T, R> f) {
-        return (Function<Object, Object>)f;
+    private void createTable() {
+        if (isDdl) {
+            execute("CREATE TABLE DATA (id INT PRIMARY KEY, data " + sqlType + ") WITH" +
+                " \"KEY_TYPE=java.lang.Integer" +
+                ", VALUE_TYPE=org.apache.ignite.internal.processors.query.IgniteQueryConvertibleTypesValidationTest$Data" +
+                ", CACHE_NAME=default\"");
+        }
+        else {
+            QueryEntity projEntity = new QueryEntity();
+            projEntity.setKeyType(Integer.class.getName());
+            projEntity.setValueType(Data.class.getName());
+            projEntity.addQueryField("id", Integer.class.getName(), null);
+            projEntity.addQueryField("data", toClassName(sqlType), null);
+
+            projEntity.setTableName("DATA");
+
+            grid(0).createCache(new CacheConfiguration<>(DEFAULT_CACHE_NAME)
+                .setQueryEntities(singletonList(projEntity))
+                .setSqlSchema("PUBLIC"));
+        }
+    }
+
+    /** */
+    private List<List<?>> execute(String qry, Object... args) {
+        return grid(0).context().query().querySqlFields(new SqlFieldsQuery(qry).setArgs(args), false).getAll();
     }
 
     /** */
@@ -141,8 +171,18 @@ public class IgniteQueryConvertibleTypesValidationTest extends GridCommonAbstrac
     }
 
     /** */
-    private List<List<?>> execute(String qry, Object... args) {
-        return grid(0).context().query().querySqlFields(new SqlFieldsQuery(qry).setArgs(args), false).getAll();
+    private static String toClassName(String sqlType) {
+        switch (sqlType) {
+            case "TIMESTAMP" : return java.sql.Timestamp.class.getName();
+            case "TIME" : return java.sql.Time.class.getName();
+            case "DATE" : return java.sql.Date.class.getName();
+            default: throw new IllegalStateException();
+        }
+    }
+
+    /** */
+    private static <T, R> Function<Object, Object> f(Function<T, R> f) {
+        return (Function<Object, Object>)f;
     }
 
     /** */
