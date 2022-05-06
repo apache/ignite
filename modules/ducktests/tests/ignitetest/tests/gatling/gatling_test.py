36bcb5caa8e714791d744d2ce675a76d7a211dd2
Sergey Korotkov <serge.korotkov@gmail.com>
2022-05-06 19:09:38 +0700

    IGN-gatling first draft

---
+++ modules/ducktests/tests/ignitetest/tests/gatling/gatling_test.py
@@ -0,0 +1,62 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This module contains gatling tests
+"""
+
+from ignitetest.services.ignite import IgniteService
+from ignitetest.services.ignite_app import IgniteApplicationService
+from ignitetest.services.utils.ignite_configuration import IgniteConfiguration, IgniteThinClientConfiguration
+from ignitetest.utils import ignite_versions, cluster
+from ignitetest.utils.ignite_test import IgniteTest
+from ignitetest.utils.version import DEV_BRANCH, IgniteVersion
+from ignitetest.services.utils.ssl.client_connector_configuration import ClientConnectorConfiguration
+from ignitetest.services.utils.ignite_configuration.cache import CacheConfiguration
+
+
+class GatlingTest(IgniteTest):
+    """
+    Tests services implementations
+    """
+    JAVA_CLIENT_CLASS_NAME = "org.apache.ignite.internal.ducktest.tests.gatling.GatlingRunnerApplication"
+
+    @cluster(num_nodes=2)
+    @ignite_versions(str(DEV_BRANCH))
+    def test_ignite_app_start_stop(self, ignite_version):
+        """
+        Test that IgniteService and IgniteApplicationService correctly start and stop
+        """
+        server_config = IgniteConfiguration(version=IgniteVersion(ignite_version),
+                                            caches=[CacheConfiguration(name='TEST-CACHE')],
+                                            client_connector_configuration=ClientConnectorConfiguration())
+
+        ignite = IgniteService(self.test_context, server_config, 1)
+
+        addresses = ignite.nodes[0].account.hostname + ":" + str(server_config.client_connector_configuration.port)
+
+        thin_clients = IgniteApplicationService(self.test_context,
+                                                IgniteThinClientConfiguration(
+                                                    addresses=addresses,
+                                                    version=IgniteVersion(ignite_version)),
+                                                java_class_name=self.JAVA_CLIENT_CLASS_NAME,
+                                                num_nodes=1)
+
+        thin_clients.params = {
+            "simulation": "org.apache.ignite.gatling.simulation.BasicSimulation"
+        }
+        ignite.start()
+        thin_clients.run()
+        ignite.stop()
