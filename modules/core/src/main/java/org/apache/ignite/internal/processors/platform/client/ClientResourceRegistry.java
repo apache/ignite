a95700f072ddac697bb72fe6cf8cde78c9a85cbd
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-05-11 14:45:21 +0300

    Revert resource handling changes

--- modules/core/src/main/java/org/apache/ignite/internal/processors/platform/client/ClientResourceRegistry.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/platform/client/ClientResourceRegistry.java
@@ -28,9 +28,6 @@ public class ClientResourceRegistry {
     /** Handles. */
     private final Map<Long, Object> res = new ConcurrentHashMap<>();
 
-    /** Reverse map. */
-    private final Map<Object, Long> resReverse = new ConcurrentHashMap<>();
-
     /** ID generator. */
     private final AtomicLong idGen = new AtomicLong();
 
@@ -42,14 +39,6 @@ public class ClientResourceRegistry {
      */
     public long put(Object obj) {
         long id = idGen.incrementAndGet();
-
-        res.put(id, obj);
-
-        return id;
-    }
-
-    public long getOrCreate(Object obj) {
-        long id = resReverse.computeIfAbsent(obj, o -> idGen.incrementAndGet());
 
         res.put(id, obj);
 
@@ -89,7 +78,6 @@ public class ClientResourceRegistry {
                 "Failed to find resource with id: " + hnd
             );
 
-        resReverse.remove(obj);
         closeIfNeeded(obj);
     }
 
