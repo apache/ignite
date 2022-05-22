461cf4b4f31cde6cd61995bf07ffde42cbbacd3b
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-05-22 20:13:35 +0300

    wip AtomicLongClient

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Client/DataStructures/AtomicLongClient.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Client/DataStructures/AtomicLongClient.cs
@@ -18,14 +18,24 @@
 namespace Apache.Ignite.Core.Impl.Client.DataStructures
 {
     using Apache.Ignite.Core.Client.DataStructures;
+    using Apache.Ignite.Core.Impl.Binary;
 
     /// <summary>
     /// Thin client atomic long.
     /// </summary>
-    internal sealed class AtomicLongClient : IAtomicLongClient 
+    internal sealed class AtomicLongClient : IAtomicLongClient
     {
         /** */
+        private const string DefaultDataStructuresCacheGroupName = "default-ds-group";
+
+        /** */
+        private const string AtomicsCacheName = "ignite-sys-atomic-cache";
+
+        /** */
         private readonly ClientFailoverSocket _socket;
+
+        /** */
+        private readonly int _cacheId;
 
         /// <summary>
         /// Initializes a new instance of <see cref="AtomicLongClient"/> class.
@@ -36,48 +46,80 @@ namespace Apache.Ignite.Core.Impl.Client.DataStructures
         {
             _socket = socket;
             Name = name;
+
+            var cacheName = AtomicsCacheName + "@" + DefaultDataStructuresCacheGroupName;
+            _cacheId = BinaryUtils.GetCacheId(cacheName);
         }
 
+        /** <inheritDoc /> */
         public string Name { get; }
 
+        /** <inheritDoc /> */
         public long Read()
         {
-            throw new System.NotImplementedException();
+            return _socket.DoOutInOpAffinity(
+                ClientOp.AtomicLongValueGet,
+                ctx => WriteName(ctx),
+                r => r.Reader.ReadLong(),
+                _cacheId,
+                AffinityKey);
         }
 
+        /** <inheritDoc /> */
         public long Increment()
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritDoc /> */
         public long Add(long value)
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritDoc /> */
         public long Decrement()
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritDoc /> */
         public long Exchange(long value)
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritDoc /> */
         public long CompareExchange(long value, long comparand)
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritDoc /> */
         public bool IsClosed()
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritDoc /> */
         public void Close()
         {
             throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the affinity key.
+        /// GridCacheInternalKeyImpl uses name as AffinityKeyMapped.
+        /// </summary>
+        private string AffinityKey => Name;
+
+        /// <summary>
+        /// Writes the name of this data structure.
+        /// </summary>
+        private void WriteName(ClientRequestContext ctx)
+        {
+            ctx.Writer.WriteString(Name);
+            ctx.Writer.WriteString(null); // Group name.
         }
     }
 }
