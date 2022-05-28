be089a7be1aa20f5f9598683ffc98ffba56dbaaa
NSAmelchev <nsamelchev@gmail.com>
2022-05-28 13:39:19 +0300

    Fix tests

--- modules/control-utility/src/test/java/org/apache/ignite/util/GridCommandHandlerClusterByClassAbstractTest.java
+++ modules/control-utility/src/test/java/org/apache/ignite/util/GridCommandHandlerClusterByClassAbstractTest.java
@@ -25,8 +25,6 @@ import org.apache.ignite.internal.IgniteEx;
 
 import static java.util.stream.Collectors.toSet;
 import static java.util.stream.Stream.of;
-import static org.apache.ignite.testframework.GridTestUtils.DFLT_TEST_TIMEOUT;
-import static org.apache.ignite.testframework.GridTestUtils.waitForCondition;
 
 /**
  * It is recommended to extends from this class in case of creating a cluster
@@ -77,9 +75,7 @@ public abstract class GridCommandHandlerClusterByClassAbstractTest extends GridC
         Set<String> rmvCacheNames = new HashSet<>(crd.cacheNames());
         rmvCacheNames.removeAll(cfgCacheNames);
 
-        crd.destroyCaches(rmvCacheNames);
-
-        waitForCondition(() -> client.cacheNames().stream().noneMatch(rmvCacheNames::contains), DFLT_TEST_TIMEOUT);
+        client.destroyCaches(rmvCacheNames);
 
         cfgCacheNames.stream()
             .map(crd::cache)
