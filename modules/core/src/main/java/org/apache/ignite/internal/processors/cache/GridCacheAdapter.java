27a50c6483f7b097227695938545a611d7fbbe02
Anton Vinogradov <av@apache.org>
2022-05-30 17:03:29 +0300

    WIP

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheAdapter.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheAdapter.java
@@ -5223,7 +5223,7 @@ public abstract class GridCacheAdapter<K, V> implements IgniteInternalCache<K, V
                     // No updates since consistency violation has been found.
                     ((primVal != null &&
                         (primVal instanceof Object[] && entryVal instanceof Object[] ?
-                            Arrays.equals((Object[])primVal, (Object[])entryVal) :
+                            Arrays.deepEquals((Object[])primVal, (Object[])entryVal) :
                             primVal.equals(entryVal))) &&
                         (primVer.equals(((CacheInvokeEntry<Object, Object>)entry).entry().version())))) {
 
