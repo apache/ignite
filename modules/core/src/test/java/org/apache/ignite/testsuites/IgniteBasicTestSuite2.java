d75dda1b68908db1f14d4687c909b27b68924569
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2022-06-10 14:28:39 +0400

    IGNITE-17152 Improve logging levels for situations when dealing with a client node

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteBasicTestSuite2.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteBasicTestSuite2.java
@@ -90,6 +90,7 @@ import org.apache.ignite.plugin.PluginNodeValidationTest;
 import org.apache.ignite.plugin.security.SecurityPermissionSetBuilderTest;
 import org.apache.ignite.spi.checkpoint.noop.NoopCheckpointSpiLoggingTest;
 import org.apache.ignite.startup.properties.NotStringSystemPropertyTest;
+import org.apache.ignite.testframework.MemorizingAppenderTest;
 import org.apache.ignite.testframework.MessageOrderLogListenerTest;
 import org.apache.ignite.testframework.test.ConfigVariationsExecutionTest;
 import org.apache.ignite.testframework.test.ConfigVariationsTestSuiteBuilderTest;
@@ -171,6 +172,8 @@ import org.junit.runners.Suite;
     GridLongRunningInitNewCrdFutureDiagnosticsTest.class,
 
     MessageOrderLogListenerTest.class,
+
+    MemorizingAppenderTest.class,
 
     CacheLocalGetSerializationTest.class,
 
