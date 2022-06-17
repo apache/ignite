c5dddff1ad501a875dc7182b8dbab36fbf19c7df
Maxim Muzafarov <maxmuzaf@gmail.com>
2022-06-17 16:32:40 +0300

    IGN-5165 Update Ignite version and groupId to the Ignite SE style
    (cherry picked from commit 23af1521ade4f339ae96fb1f34db330ff35029fe)

--- modules/ducktests/tests/ignitetest/__init__.py
+++ modules/ducktests/tests/ignitetest/__init__.py
@@ -19,4 +19,4 @@
 # due to python version naming restrictions, which are enforced by python packaging tools
 # (see  https://www.python.org/dev/peps/pep-0440/)
 #
-__version__ = '2.14.0'
+__version__ = '14.0.0-SNAPSHOT'
