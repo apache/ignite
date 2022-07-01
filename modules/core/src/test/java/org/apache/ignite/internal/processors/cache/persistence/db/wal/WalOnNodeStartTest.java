c25ce73c30a9def5a2d306f153e123ef3e174438
Aleksey Plekhanov <plehanov.alex@gmail.com>
2022-07-01 18:16:38 +0500

    IGNITE-16872 Fix 'Invalid cross-device link error' when extending WAL segments size

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/db/wal/WalOnNodeStartTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/db/wal/WalOnNodeStartTest.java
@@ -18,16 +18,21 @@
 package org.apache.ignite.internal.processors.cache.persistence.db.wal;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.file.OpenOption;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.cluster.ClusterState;
 import org.apache.ignite.configuration.DataRegionConfiguration;
 import org.apache.ignite.configuration.DataStorageConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.configuration.WALMode;
 import org.apache.ignite.internal.IgniteEx;
 import org.apache.ignite.internal.pagemem.wal.WALIterator;
 import org.apache.ignite.internal.pagemem.wal.record.PageSnapshot;
 import org.apache.ignite.internal.pagemem.wal.record.WALRecord;
+import org.apache.ignite.internal.processors.cache.persistence.file.FileIO;
+import org.apache.ignite.internal.processors.cache.persistence.file.RandomAccessFileIOFactory;
 import org.apache.ignite.internal.processors.cache.persistence.tree.io.PageIO;
 import org.apache.ignite.internal.processors.cache.persistence.wal.WALPointer;
 import org.apache.ignite.internal.processors.cache.persistence.wal.reader.IgniteWalIteratorFactory;
@@ -36,6 +41,7 @@ import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.junit.Test;
 
 import static org.apache.ignite.internal.processors.cache.persistence.wal.FileWriteAheadLogManager.WAL_SEGMENT_FILE_FILTER;
+import static org.apache.ignite.internal.processors.cache.persistence.wal.FileWriteAheadLogManager.WAL_TEMP_NAME_PATTERN;
 import static org.apache.ignite.testframework.GridTestUtils.getFieldValue;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.not;
@@ -139,12 +145,14 @@ public class WalOnNodeStartTest extends GridCommonAbstractTest {
 
         IgniteConfiguration cfg = getConfiguration();
         cfg.getDataStorageConfiguration().setWalSegmentSize(walSize);
+        cfg.getDataStorageConfiguration().setWalMode(WALMode.FSYNC);
 
         IgniteEx ignite = startGrid(cfg);
 
         ignite.cluster().state(ClusterState.ACTIVE);
 
         File walWorkDir = getFieldValue(walMgr(ignite), "walWorkDir");
+        File walArchiveDir = getFieldValue(walMgr(ignite), "walArchiveDir");
 
         stopAllGrids();
 
@@ -160,6 +168,8 @@ public class WalOnNodeStartTest extends GridCommonAbstractTest {
 
         cfg = getConfiguration();
         cfg.getDataStorageConfiguration().setWalSegmentSize(walSize);
+        cfg.getDataStorageConfiguration().setWalMode(WALMode.FSYNC);
+        cfg.getDataStorageConfiguration().setFileIOFactory(new WorkDirCheckingFileIOFactory(walWorkDir, walArchiveDir));
 
         startGrid(cfg);
 
@@ -171,5 +181,29 @@ public class WalOnNodeStartTest extends GridCommonAbstractTest {
 
         for (File wal : wals)
             assertEquals(walSize, wal.length());
+    }
+
+    /** */
+    private static class WorkDirCheckingFileIOFactory extends RandomAccessFileIOFactory {
+        /** */
+        private final File walWorkDir;
+
+        /** */
+        private final File walArchiveDir;
+
+        /** */
+        private WorkDirCheckingFileIOFactory(File walWorkDir, File walArchiveDir) {
+            this.walWorkDir = walWorkDir;
+            this.walArchiveDir = walArchiveDir;
+        }
+
+        /** {@inheritDoc} */
+        @Override public FileIO create(File file, OpenOption... modes) throws IOException {
+            // Ensure that reformated temp files created in the same dir as original WAL segments.
+            if (WAL_TEMP_NAME_PATTERN.matcher(file.getName()).matches() && !walArchiveDir.equals(file.getParentFile()))
+                assertEquals(walWorkDir, file.getParentFile());
+
+            return super.create(file, modes);
+        }
     }
 }
