d2b22047c3cca2702752c85ddb7f79b49351c6ad
Sergey Korotkov <serge.korotkov@gmail.com>
2022-07-01 20:45:58 +0700

    IGNITE-16979 some fixes

--- modules/gatling-plugin/src/test/scala/org/apache/ignite/gatling/compile/IgniteCompileTest.scala
+++ modules/gatling-plugin/src/test/scala/org/apache/ignite/gatling/compile/IgniteCompileTest.scala
@@ -225,8 +225,7 @@ class IgniteCompileTest extends Simulation {
       val cache = client.cache[Int, Int]("test-cache").toOption.get
       cache.get(session("key").as[Int])(value => print(value), _ => ())
       client.close()(
-        _ =>
-          client.txStart()({ _ => }, { _ => }),
+        _ => client.txStart()({ _ => }, { _ => }),
         _ => ()
       )
       session
