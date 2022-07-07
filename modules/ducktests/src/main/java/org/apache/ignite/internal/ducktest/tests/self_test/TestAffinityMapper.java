0c5c7adb59a581354c451355bcdcfa88ebc060d9
Sergey Korotkov <serge.korotkov@gmail.com>
2022-07-07 12:19:55 +0700

    IGNITE-17322 Add test

--- modules/ducktests/src/main/java/org/apache/ignite/internal/ducktest/tests/self_test/TestAffinityMapper.java
+++ modules/ducktests/src/main/java/org/apache/ignite/internal/ducktest/tests/self_test/TestAffinityMapper.java
@@ -1,2 +1,17 @@
-package org.apache.ignite.internal.ducktest.tests.self_test;public class TestAffinityMapper {
+package org.apache.ignite.internal.ducktest.tests.self_test;
+
+import org.apache.ignite.cache.affinity.AffinityKeyMapper;
+
+/**
+ * Affinity mapper used to test configuration of the custom affinity mapper in the server node config XML.
+ */
+public class TestAffinityMapper implements AffinityKeyMapper {
+    /** {@inheritDoc} */
+    @Override public Object affinityKey(Object key) {
+        return key;
+    }
+
+    /** {@inheritDoc} */
+    @Override public void reset() {
+    }
 }
