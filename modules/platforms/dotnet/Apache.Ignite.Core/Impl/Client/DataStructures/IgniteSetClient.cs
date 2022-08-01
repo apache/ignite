7c0342509db8f0c277a3d0a09089881122ebf42a
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-08-01 14:46:40 +0300

    wip IgniteSetClient

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Client/DataStructures/IgniteSetClient.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Client/DataStructures/IgniteSetClient.cs
@@ -27,102 +27,133 @@ namespace Apache.Ignite.Core.Impl.Client.DataStructures
     /// <typeparam name="T">Element type.</typeparam>
     internal sealed class IgniteSetClient<T> : IIgniteSetClient<T>
     {
+        /** <inheritdoc /> */
         public IEnumerator<T> GetEnumerator()
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritdoc /> */
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
         }
 
+        /** <inheritdoc /> */
         void ICollection<T>.Add(T item)
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritdoc /> */
         public void ExceptWith(IEnumerable<T> other)
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritdoc /> */
         public void IntersectWith(IEnumerable<T> other)
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritdoc /> */
         public bool IsProperSubsetOf(IEnumerable<T> other)
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritdoc /> */
         public bool IsProperSupersetOf(IEnumerable<T> other)
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritdoc /> */
         public bool IsSubsetOf(IEnumerable<T> other)
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritdoc /> */
         public bool IsSupersetOf(IEnumerable<T> other)
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritdoc /> */
         public bool Overlaps(IEnumerable<T> other)
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritdoc /> */
         public bool SetEquals(IEnumerable<T> other)
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritdoc /> */
         public void SymmetricExceptWith(IEnumerable<T> other)
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritdoc /> */
         public void UnionWith(IEnumerable<T> other)
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritdoc /> */
         bool ISet<T>.Add(T item)
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritdoc /> */
         public void Clear()
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritdoc /> */
         public bool Contains(T item)
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritdoc /> */
         public void CopyTo(T[] array, int arrayIndex)
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritdoc /> */
         public bool Remove(T item)
         {
             throw new System.NotImplementedException();
         }
 
+        /** <inheritdoc /> */
         public int Count { get; }
+
+        /** <inheritdoc /> */
         public bool IsReadOnly { get; }
+
+        /** <inheritdoc /> */
         public string Name { get; }
+
+        /** <inheritdoc /> */
         public bool Colocated { get; }
+
+        /** <inheritdoc /> */
         public int PageSize { get; set; }
+
+        /** <inheritdoc /> */
         public bool IsClosed { get; }
+
+        /** <inheritdoc /> */
         public void Close()
         {
             throw new System.NotImplementedException();
