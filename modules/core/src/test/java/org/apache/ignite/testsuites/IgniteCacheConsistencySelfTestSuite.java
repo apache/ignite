57301dd9cb3c0a796a471bfd36b663536d0ac0d7
Maksim Timonin <timonin.maxim@gmail.com>
2022-08-01 19:21:06 +0300

    Add missed test

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheConsistencySelfTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheConsistencySelfTestSuite.java
@@ -28,6 +28,7 @@ import org.apache.ignite.internal.processors.cache.consistency.persistence.PdsAt
 import org.apache.ignite.internal.processors.cache.consistency.persistence.PdsExplicitTransactionalReadRepairTest;
 import org.apache.ignite.internal.processors.cache.consistency.persistence.PdsImplicitTransactionalReadRepairTest;
 import org.apache.ignite.internal.processors.cache.consistentcut.ConcurrentTxsConsistentCutTest;
+import org.apache.ignite.internal.processors.cache.consistentcut.ConsistentCutActivationTest;
 import org.apache.ignite.internal.processors.cache.consistentcut.ConsistentCutNoBackupMessagesBlockingTest;
 import org.apache.ignite.internal.processors.cache.consistentcut.ConsistentCutNoBackupWALBlockingTest;
 import org.apache.ignite.internal.processors.cache.consistentcut.ConsistentCutSingleBackupMessagesBlockingTest;
@@ -61,6 +62,7 @@ import org.junit.runners.Suite;
     SingleBackupImplicitTransactionalReadRepairTest.class,
 
     // Consistent Cut
+    ConsistentCutActivationTest.class,
     ConsistentCutNoBackupMessagesBlockingTest.class,
     ConsistentCutSingleBackupMessagesBlockingTest.class,
     ConsistentCutTwoBackupMessagesBlockingTest.class,
