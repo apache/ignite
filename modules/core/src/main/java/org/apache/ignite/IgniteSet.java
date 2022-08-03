d7c307a6dd1b0f1874b507cf231347672ec871bb
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-08-03 10:09:49 +0300

    IGNITE-17206 .NET: Thin client: Add IgniteSet (#10177)
    * Implement `IgniteSet.withKeepBinary` to support non-Java user classes.
    * Add `IIgniteSetClient`, implement all standard `ISet` methods except `IsSubsetOf`, `IsProperSubsetOf`, `Overlaps`, `SymmetricExceptWith` (do not map to Java API).

--- modules/core/src/main/java/org/apache/ignite/IgniteSet.java
+++ modules/core/src/main/java/org/apache/ignite/IgniteSet.java
@@ -130,4 +130,12 @@ public interface IgniteSet<T> extends Set<T>, Closeable {
      * @throws IgniteException If job failed.
      */
     public <R> R affinityCall(IgniteCallable<R> job) throws IgniteException;
+
+    /**
+     * Returns Ignite set that operates on binary objects without deserialization.
+     *
+     * @param <T1> Type of binary objects.
+     * @return New set instance for binary objects.
+     */
+    public <T1> IgniteSet<T1> withKeepBinary();
 }
