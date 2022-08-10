641dc34799b1704e474fbfb03c528a6e4300c891
Slava Koptilin <slava.koptilin@gmail.com>
2022-08-10 16:44:29 +0300

    GG-35222 Fixed checkstyle

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/CacheAffinityChangeMessage.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/CacheAffinityChangeMessage.java
@@ -166,6 +166,7 @@ public class CacheAffinityChangeMessage implements DiscoveryCustomMessage {
         return true;
     }
 
+    /** {@inheritDoc} */
     @Override public boolean stopProcess() {
         return stopProc;
     }
