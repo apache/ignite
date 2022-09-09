a47b929d0172cb0c276ad9e908fa5bd7f2b24828
liyujue <liyujue@ignite-service.cn>
2022-09-09 11:34:23 +0300

    IGNITE-17658 add-opens=java.base/java.lang.invoke for peer class loading - Fixes #10242.
    Signed-off-by: Ilya Kaznacheev <ilya.kaznacheev@chronicle.software>

--- bin/include/jvmdefaults.sh
+++ bin/include/jvmdefaults.sh
@@ -65,6 +65,7 @@ getJavaSpecificOpts() {
           --add-opens=java.base/java.nio=ALL-UNNAMED \
           --add-opens=java.base/java.util=ALL-UNNAMED \
           --add-opens=java.base/java.lang=ALL-UNNAMED \
+          --add-opens=java.base/java.lang.invoke=ALL-UNNAMED \
           ${current_value}"
   fi
 
