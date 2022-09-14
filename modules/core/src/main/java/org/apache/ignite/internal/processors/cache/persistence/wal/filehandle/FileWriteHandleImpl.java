80398bc8a97f5108499bbdf658875d1d296e9855
Nikita Amelchev <nsamelchev@gmail.com>
2022-09-14 17:38:32 +0300

    IGNITE-17675 Fixed file resources leak on cluster deactivation/stop in case non full sync WAL mode. (#10250)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/filehandle/FileWriteHandleImpl.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/filehandle/FileWriteHandleImpl.java
@@ -507,7 +507,10 @@ class FileWriteHandleImpl extends AbstractFileHandle implements FileWriteHandle 
                     lastFsyncPos = written;
                 }
 
-                walWriter.close();
+                if (mmap)
+                    U.closeQuiet(fileIO);
+                else
+                    walWriter.close();
 
                 if (!mmap && !rollOver)
                     buf.free();
