232a97608870529eec671d704c6c280457373ff9
Aleksey Plekhanov <plehanov.alex@gmail.com>
2022-09-27 14:58:17 +0300

    IGNITE-17764 Fix performance drop on index find operation - Fixes #10269.
    Signed-off-by: Aleksey Plekhanov <plehanov.alex@gmail.com>
    (cherry picked from commit 3b0b5ed9a41fd9f3249f8c42bb59ba0e3ceb55d1)

--- modules/core/src/main/java/org/apache/ignite/internal/cache/query/index/sorted/keys/ShortIndexKey.java
+++ modules/core/src/main/java/org/apache/ignite/internal/cache/query/index/sorted/keys/ShortIndexKey.java
@@ -82,22 +82,11 @@ public class ShortIndexKey extends NumericIndexKey {
 
     /** {@inheritDoc} */
     @Override public int compare(IndexKey o) {
-        return o.type() == type() ? Integer.compare(key, ((ShortIndexKey)o).key) : -((NumericIndexKey)o).compareTo(key);
+        return -((NumericIndexKey)o).compareTo(key);
     }
 
     /** {@inheritDoc} */
     @Override public String toString() {
         return String.valueOf(key);
     }
-
-//    /** {@inheritDoc} */
-//    @Override public int hashCode() {
-//        return key;
-//    }
-//
-//    /** {@inheritDoc} */
-//    @Override public boolean equals(Object o) {
-//        return o instanceof ShortIndexKey && key == ((ShortIndexKey)o).key;
-//    }
-
 }
