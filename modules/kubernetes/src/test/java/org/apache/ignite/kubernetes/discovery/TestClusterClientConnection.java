0fcd7e111c82b4164cbeebc09cf1acbb90152bbd
Simon Ochsenreither <simon@ochsenreither.de>
2022-09-30 16:42:31 +0200

    IGNITE-11461 Add `Automatic-Module-Name` to ignite-core and ignite-kubernetes ...
    ... and fix package interference between those modules.

--- modules/kubernetes/src/test/java/org/apache/ignite/kubernetes/discovery/TestClusterClientConnection.java
+++ modules/kubernetes/src/test/java/org/apache/ignite/kubernetes/discovery/TestClusterClientConnection.java
@@ -20,7 +20,7 @@ package org.apache.ignite.kubernetes.discovery;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.client.ClientCache;
 import org.apache.ignite.client.IgniteClient;
-import org.apache.ignite.client.ThinClientKubernetesAddressFinder;
+import org.apache.ignite.kubernetes.client.ThinClientKubernetesAddressFinder;
 import org.apache.ignite.configuration.ClientConfiguration;
 import org.apache.ignite.configuration.ClientConnectorConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
