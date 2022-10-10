1d9807ba331af0ffbc94608b8036dac16547ae53
Nikolay Izhikov <nizhikov@apache.org>
2022-10-10 14:16:04 +0300

    IGNITE-17852 Fix incorrect CRC for compression

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/file/FilePageStore.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/file/FilePageStore.java
@@ -734,8 +734,8 @@ public class FilePageStore implements PageStore {
                     }
 
                     // Check whether crc was calculated somewhere above the stack if it is forcibly skipped.
-                    assert skipCrc || PageIO.getCrc(pageBuf) != 0 || calcCrc32(pageBuf, getCrcSize(pageId, pageBuf)) == 0
-                        : "CRC hasn't been calculated, crc=0";
+                    assert skipCrc || PageIO.getCrc(pageBuf) != 0 || calcCrc32(pageBuf, getCrcSize(pageId, pageBuf)) == 0 :
+                        "CRC hasn't been calculated, crc=0";
 
                     assert pageBuf.position() == 0 : pageBuf.position();
 
