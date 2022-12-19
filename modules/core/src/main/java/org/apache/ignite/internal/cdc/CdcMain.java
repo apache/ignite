ae44a81e2ae641e913f1766a3095380a0fd3e485
Nikolay <nizhikov@apache.org>
2022-12-19 14:36:28 +0300

    IGNITE-18404 Stop CDC consumer in shutdown hook (#10450)

--- modules/core/src/main/java/org/apache/ignite/internal/cdc/CdcMain.java
+++ modules/core/src/main/java/org/apache/ignite/internal/cdc/CdcMain.java
@@ -220,6 +220,9 @@ public class CdcMain implements Runnable {
     private Map<Integer, Long> cachesState;
 
     /** Stopped flag. */
+    private volatile boolean started;
+
+    /** Stopped flag. */
     private volatile boolean stopped;
 
     /** Already processed segments. */
@@ -314,19 +317,21 @@ public class CdcMain implements Runnable {
 
                 consumer.start(mreg, kctx.metric().registry(metricName("cdc", "consumer")));
 
+                started = true;
+
                 try {
                     consumeWalSegmentsUntilStopped();
                 }
                 finally {
-                    consumer.stop();
-
-                    if (log.isInfoEnabled())
-                        log.info("Ignite Change Data Capture Application stopped.");
+                    stop();
                 }
             }
             finally {
                 for (GridComponent comp : kctx)
                     comp.stop(false);
+
+                if (log.isInfoEnabled())
+                    log.info("Ignite Change Data Capture Application stopped.");
             }
         }
     }
@@ -811,10 +816,15 @@ public class CdcMain implements Runnable {
     /** Stops the application. */
     public void stop() {
         synchronized (this) {
+            if (stopped || !started)
+                return;
+
             if (log.isInfoEnabled())
                 log.info("Stopping Change Data Capture service instance");
 
             stopped = true;
+
+            consumer.stop();
         }
     }
 
