5ebbea2429208fab5372985c30e79760f74fab67
Anton Vinogradov <av@apache.org>
2023-01-17 20:40:41 +0300

    WIP

--- modules/compress/src/test/java/org/apache/ignite/testsuites/IgnitePdsCompressionTestSuite2.java
+++ modules/compress/src/test/java/org/apache/ignite/testsuites/IgnitePdsCompressionTestSuite2.java
@@ -34,8 +34,6 @@ public class IgnitePdsCompressionTestSuite2 {
     public static List<Class<?>> suite() {
         List<Class<?>> suite = new ArrayList<>();
 
-        suite.addAll(IgniteCompressionTestSuite.suite());
-
         enableCompressionByDefault();
         IgnitePdsTestSuite2.addRealPageStoreTests(suite, null);
 
