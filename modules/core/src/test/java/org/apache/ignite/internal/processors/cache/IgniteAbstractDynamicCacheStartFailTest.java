7aca5e9edd7961abd3861664a59c4d23a4e4088e
Vladimir Steshin <vladsz83@gmail.com>
2023-01-18 13:21:00 +0300

    jdoc fix

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteAbstractDynamicCacheStartFailTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/IgniteAbstractDynamicCacheStartFailTest.java
@@ -897,13 +897,13 @@ public abstract class IgniteAbstractDynamicCacheStartFailTest extends GridCacheA
         }
     }
 
-    /** 'MXBean'-named interface to register mx bean at dynamic cache creation to simulate failur of cache creation. */
+    /** 'MXBean'-named interface to register mx bean at dynamic cache creation to simulate failure. */
     public interface UUIDMXBean {
 
     }
 
     /**
-     * Empty filter used to register mxbean at dynamic cache creation to simalate failure of cache creation.
+     * Empty filter used to register mxbean at dynamic cache creation to simalate failure.
      */
     public static class AnyNodeMXFilter implements IgnitePredicate<ClusterNode>, UUIDMXBean {
         /** {@inheritDoc} */
