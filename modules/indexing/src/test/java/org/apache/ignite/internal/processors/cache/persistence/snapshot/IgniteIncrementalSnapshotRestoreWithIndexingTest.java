73ed037dad064af073ccce012c7f41d25cf4cf68
Maksim Timonin <timonin.maxim@gmail.com>
2023-02-01 15:41:38 +0300

    Fix checkstyle

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/persistence/snapshot/IgniteIncrementalSnapshotRestoreWithIndexingTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/persistence/snapshot/IgniteIncrementalSnapshotRestoreWithIndexingTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.internal.processors.cache.persistence.snapshot;
 
 import java.util.HashMap;
@@ -171,18 +188,28 @@ public class IgniteIncrementalSnapshotRestoreWithIndexingTest extends AbstractIn
         return id;
     }
 
-    @Override
-    protected int nodes() {
+    /** {@inheritDoc} */
+    @Override protected int nodes() {
         return 3;
     }
 
-    @Override
-    protected int backups() {
+    /** {@inheritDoc} */
+    @Override protected int backups() {
         return 2;
     }
 
     /** */
     private enum Operation {
-        INSERT, INSERT_MULTIPLE, UPDATE, DELETE
+        /** */
+        INSERT,
+
+        /** */
+        INSERT_MULTIPLE,
+
+        /** */
+        UPDATE,
+
+        /** */
+        DELETE
     }
 }
