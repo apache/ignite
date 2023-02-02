76dc17d64a7e0394d659258ee107918f8c9b2026
Mikhail Petrov <pmgheap.sbt@gmail.com>
2023-02-02 15:26:44 +0300

    IGNITE-17554 Fixes review issues.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/odbc/ClientListenerNioListener.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/odbc/ClientListenerNioListener.java
@@ -63,7 +63,7 @@ public class ClientListenerNioListener extends GridNioServerListenerAdapter<Clie
     public static final byte THIN_CLIENT = 2;
 
     /** Client types. */
-    public static final byte[] CLI_TYPES = {THIN_CLIENT, JDBC_CLIENT, ODBC_CLIENT};
+    public static final byte[] CLI_TYPES = {ODBC_CLIENT, JDBC_CLIENT, THIN_CLIENT};
 
     /** Connection handshake timeout task. */
     public static final int CONN_CTX_HANDSHAKE_TIMEOUT_TASK = GridNioSessionMetaKey.nextUniqueKey();
