034547aed5331d0e0b3f7bf27a87939f8df6eef6
Anton Vinogradov <av@apache.org>
2023-02-08 18:32:43 +0300

    WIP

---
+++ modules/indexing/src/test/java/org/apache/ignite/cache/query/IndexQueryCacheKeyValueTransformedFieldsTest.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.cache.query;
+
+import java.nio.ByteBuffer;
+import java.util.concurrent.ThreadLocalRandom;
+import org.apache.ignite.IgniteCheckedException;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.spi.transform.CacheObjectTransformerAdapter;
+
+/** Test checks that indexing works (including inlining) with enabled cache objects transformer. */
+public class IndexQueryCacheKeyValueTransformedFieldsTest extends IndexQueryCacheKeyValueFieldsTest {
+    /** {@inheritDoc} */
+    @Override protected IgniteConfiguration getConfiguration(String instanceName) throws Exception {
+        return super.getConfiguration(instanceName).setCacheObjectTransformer(new RandomShiftCacheObjectTransformer());
+    }
+
+    /**
+     * Transforms each object with a random shift.
+     */
+    protected static final class RandomShiftCacheObjectTransformer extends CacheObjectTransformerAdapter {
+        /** {@inheritDoc} */
+        @Override protected ByteBuffer transform(ByteBuffer original) throws IgniteCheckedException {
+            ByteBuffer transformed = byteBuffer(original.remaining() + 4);
+
+            int shift = ThreadLocalRandom.current().nextInt();
+
+            transformed.putInt(shift);
+
+            while (original.hasRemaining())
+                transformed.put((byte)(original.get() + shift));
+
+            transformed.flip();
+
+            return transformed;
+        }
+
+        /** {@inheritDoc} */
+        @Override protected ByteBuffer restore(ByteBuffer transformed) {
+            ByteBuffer restored = byteBuffer(transformed.remaining() - 4);
+
+            int shift = transformed.getInt();
+
+            while (transformed.hasRemaining())
+                restored.put((byte)(transformed.get() - shift));
+
+            restored.flip();
+
+            return restored;
+        }
+    }
+}
