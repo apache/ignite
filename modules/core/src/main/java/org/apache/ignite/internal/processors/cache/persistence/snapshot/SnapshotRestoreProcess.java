b5ebfc2918f3099b376c361d9f99a240333ca24a
Maksim Timonin <timoninmaxim@apache.org>
2023-02-13 13:54:29 +0300

    IGNITE-18764 Restore marshaller data from snapshot (#10527)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/snapshot/SnapshotRestoreProcess.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/snapshot/SnapshotRestoreProcess.java
@@ -56,13 +56,17 @@ import org.apache.ignite.events.EventType;
 import org.apache.ignite.internal.GridKernalContext;
 import org.apache.ignite.internal.IgniteFeatures;
 import org.apache.ignite.internal.IgniteInternalFuture;
+import org.apache.ignite.internal.IgniteInterruptedCheckedException;
 import org.apache.ignite.internal.NodeStoppingException;
+import org.apache.ignite.internal.binary.BinaryContext;
+import org.apache.ignite.internal.binary.BinaryUtils;
 import org.apache.ignite.internal.cluster.ClusterTopologyCheckedException;
 import org.apache.ignite.internal.managers.discovery.DiscoCache;
 import org.apache.ignite.internal.processors.affinity.GridAffinityAssignmentCache;
 import org.apache.ignite.internal.processors.cache.GridCacheSharedContext;
 import org.apache.ignite.internal.processors.cache.GridLocalConfigManager;
 import org.apache.ignite.internal.processors.cache.StoredCacheData;
+import org.apache.ignite.internal.processors.cache.binary.CacheObjectBinaryProcessorImpl;
 import org.apache.ignite.internal.processors.cache.persistence.file.FilePageStoreManager;
 import org.apache.ignite.internal.processors.cache.persistence.snapshot.IgniteSnapshotManager.ClusterSnapshotFuture;
 import org.apache.ignite.internal.processors.cluster.DiscoveryDataClusterState;
@@ -82,6 +86,7 @@ import org.jetbrains.annotations.Nullable;
 
 import static java.util.Optional.ofNullable;
 import static org.apache.ignite.internal.IgniteFeatures.SNAPSHOT_RESTORE_CACHE_GROUP;
+import static org.apache.ignite.internal.MarshallerContextImpl.mappingFileStoreWorkDir;
 import static org.apache.ignite.internal.pagemem.PageIdAllocator.INDEX_PARTITION;
 import static org.apache.ignite.internal.processors.cache.binary.CacheObjectBinaryProcessorImpl.binaryWorkDir;
 import static org.apache.ignite.internal.processors.cache.persistence.file.FilePageStoreManager.CACHE_GRP_DIR_PREFIX;
@@ -870,6 +875,10 @@ public class SnapshotRestoreProcess {
                             File binDir = binaryWorkDir(snpDir.getAbsolutePath(), meta.folderName());
 
                             ctx.cacheObjects().updateMetadata(binDir, opCtx0.stopChecker);
+
+                            File marshallerDir = mappingFileStoreWorkDir(snpDir.getAbsolutePath());
+
+                            restoreMappings(marshallerDir, opCtx0.stopChecker);
                         }
                         catch (Throwable t) {
                             log.error("Unable to perform metadata update operation for the cache groups restore process", t);
@@ -1078,6 +1087,31 @@ public class SnapshotRestoreProcess {
         }
 
         return retFut;
+    }
+
+    /** Restore registered mappings for user classes. */
+    private void restoreMappings(File marshallerDir, BooleanSupplier stopChecker) throws IgniteCheckedException {
+        File[] mappings = marshallerDir.listFiles(BinaryUtils::notTmpFile);
+
+        if (mappings == null)
+            throw new IgniteException("Failed to list marshaller directory [dir=" + marshallerDir + ']');
+
+        for (File map: mappings) {
+            if (stopChecker.getAsBoolean())
+                return;
+
+            if (Thread.interrupted())
+                throw new IgniteInterruptedCheckedException("Thread has been interrupted.");
+
+            String fileName = map.getName();
+
+            int typeId = BinaryUtils.mappedTypeId(fileName);
+            byte platformId = BinaryUtils.mappedFilePlatformId(fileName);
+
+            BinaryContext binCtx = ((CacheObjectBinaryProcessorImpl)ctx.cacheObjects()).binaryContext();
+
+            binCtx.registerUserClassName(typeId, BinaryUtils.readMapping(map), false, false, platformId);
+        }
     }
 
     /**
