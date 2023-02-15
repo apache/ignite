48fc730389cda2f1615ba2643ffbd2389ce8f632
Vladimir Steshin <vladsz83@gmail.com>
2023-02-15 16:29:07 +0300

    second impl

--- modules/core/src/test/java/org/apache/ignite/internal/processors/security/cluster/ClusterStateSecurityTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/security/cluster/ClusterStateSecurityTest.java
@@ -22,14 +22,15 @@ import org.apache.ignite.cluster.ClusterState;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.internal.processors.security.AbstractSecurityTest;
 import org.apache.ignite.internal.processors.security.impl.TestSecurityPluginProvider;
+import org.apache.ignite.internal.util.typedef.F;
 import org.apache.ignite.plugin.security.SecurityPermission;
 import org.junit.Test;
 
+import static java.lang.Boolean.*;
+import static org.apache.ignite.cluster.ClusterState.*;
 import static org.apache.ignite.plugin.security.SecurityPermission.ADMIN_CLUSTER_STATE_ACTIVE;
-import static org.apache.ignite.plugin.security.SecurityPermission.CACHE_CREATE;
+import static org.apache.ignite.plugin.security.SecurityPermission.ADMIN_CLUSTER_STATE_INACTIVE;
 import static org.apache.ignite.plugin.security.SecurityPermission.JOIN_AS_SERVER;
-import static org.apache.ignite.plugin.security.SecurityPermission.TASK_CANCEL;
-import static org.apache.ignite.plugin.security.SecurityPermission.TASK_EXECUTE;
 import static org.apache.ignite.plugin.security.SecurityPermissionSetBuilder.create;
 import static org.apache.ignite.testframework.GridTestUtils.assertThrows;
 import static org.junit.Assert.assertNotEquals;
@@ -41,6 +42,12 @@ public class ClusterStateSecurityTest extends AbstractSecurityTest {
     /** */
     private boolean isClient;
 
+    /** */
+    private ClusterState startState = INACTIVE;
+
+    /** */
+    private SecurityPermission[] adminPerms;
+
     /** {@inheritDoc} */
     @Override protected void afterTest() throws Exception {
         super.afterTest();
@@ -48,88 +55,130 @@ public class ClusterStateSecurityTest extends AbstractSecurityTest {
         stopAllGrids();
     }
 
-    /** */
-    @Test
-    public void testActivationAllowed() throws Exception {
-        testChangeClusterState(configuration(0, ADMIN_CLUSTER_STATE_ACTIVE), ClusterState.ACTIVE, true);
-    }
+    /** {@inheritDoc} */
+    @Override protected IgniteConfiguration getConfiguration(String instanceName) throws Exception {
+        IgniteConfiguration cfg = super.getConfiguration(instanceName);
 
-    /** */
-    @Test
-    public void testActivationReadOnlyAllowed() throws Exception {
-        testChangeClusterState(configuration(0, ADMIN_CLUSTER_STATE_ACTIVE), ClusterState.ACTIVE_READ_ONLY, true);
-    }
-
-    /** */
-    @Test
-    public void testActivationNotAllowed() throws Exception {
-        testChangeClusterState(null, ClusterState.ACTIVE, false);
-    }
-
-    /** */
-    @Test
-    public void testActivationReadOnlyNotAllowed() throws Exception {
-        testChangeClusterState(null, ClusterState.ACTIVE_READ_ONLY, false);
-    }
-
-    /** */
-    private void testChangeClusterState(IgniteConfiguration cfg, ClusterState state, boolean allowed) throws Exception {
-        if (cfg == null)
-            cfg = configuration(0);
-
-        Ignite ig = startGrid(cfg);
-
-        ClusterState stateBefore = ig.cluster().state();
-
-        if (allowed) {
-            ig.cluster().state(state);
-
-            assertNotEquals(stateBefore, ig.cluster().state());
-            assertEquals(state, ig.cluster().state());
-        }
-        else {
-            assertThrows(
-                null,
-                () -> {
-                    ig.cluster().state(state);
-
-                    return null;
-                },
-                org.apache.ignite.plugin.security.SecurityException.class,
-                "Authorization failed [perm=ADMIN_CLUSTER_STATE_ACTIVE"
-            );
-
-            assertEquals(stateBefore, ig.cluster().state());
-            assertNotEquals(state, ig.cluster().state());
-        }
-    }
-
-    /** @return Ignite node configuration. */
-    private IgniteConfiguration configuration(int idx, SecurityPermission... perms) throws Exception {
-        String name = getTestIgniteInstanceName(idx);
-
-        IgniteConfiguration cfg = getConfiguration(
-            name,
-            new TestSecurityPluginProvider(
-                name,
-                "",
-                create()
-                    .defaultAllowAll(false)
-                    .appendSystemPermissions(JOIN_AS_SERVER)
-                    .appendCachePermissions(DEFAULT_CACHE_NAME, CACHE_CREATE)
-                    .appendTaskPermissions(
-                        "org.apache.ignite.internal.processors.affinity.GridAffinityUtils$AffinityJob",
-                        TASK_EXECUTE, TASK_CANCEL)
-                    .appendSystemPermissions(perms)
-                    .build(),
-                null,
-                false
-            )
-        );
+        cfg.setPluginProviders(new TestSecurityPluginProvider(
+            instanceName,
+            "",
+            create()
+                .defaultAllowAll(false)
+                .appendSystemPermissions(JOIN_AS_SERVER)
+                .appendSystemPermissions(adminPerms)
+                .build(),
+            null,
+            false
+        ));
 
         cfg.setClientMode(isClient);
-        cfg.setClusterStateOnStart(ClusterState.INACTIVE);
+        cfg.setClusterStateOnStart(startState);
 
         return cfg;
+    }
+
+    /** */
+    @Test
+    public void testActivationDeactivationAllowed() throws Exception {
+        adminPerms = F.asArray(ADMIN_CLUSTER_STATE_ACTIVE, ADMIN_CLUSTER_STATE_INACTIVE);
+
+        testChangeClusterState(
+            F.asArray(ACTIVE, INACTIVE, ACTIVE_READ_ONLY, INACTIVE),
+            F.asArray(TRUE, TRUE, TRUE, TRUE)
+        );
+    }
+
+    /** */
+    @Test
+    public void testActivationAllowedDeactivationNotAllowed() throws Exception {
+        adminPerms = F.asArray(ADMIN_CLUSTER_STATE_ACTIVE);
+
+        testChangeClusterState(F.asArray(ACTIVE, INACTIVE), F.asArray(TRUE, FALSE));
+    }
+
+    /** */
+    @Test
+    public void testActivationReadOnlyAllowedDeactivationNotAllowed() throws Exception {
+        adminPerms = F.asArray(ADMIN_CLUSTER_STATE_ACTIVE);
+
+        testChangeClusterState(F.asArray(ACTIVE_READ_ONLY, INACTIVE), F.asArray(TRUE, FALSE));
+    }
+
+    /** */
+    @Test
+    public void testDeactivationAllowedActivationNotAllowed() throws Exception {
+        adminPerms = F.asArray(ADMIN_CLUSTER_STATE_INACTIVE);
+
+        startState = ACTIVE;
+
+        testChangeClusterState(F.asArray(INACTIVE, ACTIVE, ACTIVE_READ_ONLY), F.asArray(TRUE, FALSE, FALSE));
+    }
+
+    /** */
+    @Test
+    public void testActiveIsAllowedAtStartByDefault() throws Exception {
+        testAllowedAtStart(ACTIVE);
+    }
+
+    /** */
+    @Test
+    public void testInactiveReadOnlyIsAllowedAtStartByDefault() throws Exception {
+        testAllowedAtStart(ACTIVE_READ_ONLY);
+    }
+
+    /** */
+    @Test
+    public void testInactiveIsAllowedAtStartByDefault() throws Exception {
+        testAllowedAtStart(INACTIVE);
+    }
+
+    /** */
+    private void testChangeClusterState(ClusterState[] states, Boolean[] allowed) throws Exception {
+        assert states != null && allowed != null && states.length == allowed.length && states.length > 0;
+
+        Ignite ig = startGrids(3);
+
+        for(int i=0; i<states.length; ++i){
+            assert allowed[i] != null;
+
+            ClusterState stateBefore = ig.cluster().state();
+            ClusterState stateTo = states[i];
+
+            assert !stateBefore.equals(stateTo);
+
+            if (allowed[i]) {
+                ig.cluster().state(stateTo);
+
+                assertNotEquals(stateBefore, ig.cluster().state());
+                assertEquals(stateTo, ig.cluster().state());
+            }
+            else {
+                assertThrows(
+                    null,
+                    () -> {
+                        ig.cluster().state(stateTo);
+
+                        return null;
+                    },
+                    org.apache.ignite.plugin.security.SecurityException.class,
+                    "Authorization failed [perm=" + (stateTo == INACTIVE ? ADMIN_CLUSTER_STATE_INACTIVE :
+                        ADMIN_CLUSTER_STATE_ACTIVE)
+                );
+
+                assertEquals(stateBefore, ig.cluster().state());
+                assertNotEquals(stateTo, ig.cluster().state());
+            }
+        }
+    }
+
+    /** */
+    private void testAllowedAtStart(ClusterState startState) throws Exception {
+        this.startState = startState;
+
+        adminPerms = F.asArray(startState == INACTIVE ? ADMIN_CLUSTER_STATE_ACTIVE : ADMIN_CLUSTER_STATE_INACTIVE);
+
+        Ignite ig = startGrids(3);
+
+        assertEquals(startState, ig.cluster().state());
     }
 }
