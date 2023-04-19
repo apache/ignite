3b7e3c25311169aa930559aaef5e6593c826a306
Aleksey Plekhanov <plehanov.alex@gmail.com>
2023-04-19 16:32:13 +0300

    IGNITE-17194 Fix update version script

--- scripts/update-versions.sh
+++ scripts/update-versions.sh
@@ -43,4 +43,6 @@ sed "${SED_OPTION[@]}" -e "s/<revision>\(.*\)</<revision>${1}</g" ./parent/pom.x
 
 echo Updating sub-modules versions to "$1" and resouces during the build
 
-mvn install -P all-java,all-scala,platforms,skip-docs -DskipTests
+#mvn install -pl modules/tools
+mvn validate -P update-versions
+#mvn install -P update-versions,skip-docs -DskipTests
