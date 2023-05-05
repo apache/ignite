cdd06955299aebd1abdf36a366f797031ae821c0
Anton Vinogradov <av@apache.org>
2023-05-05 13:49:44 +0300

    WIP

--- modules/control-utility/src/test/java/org/apache/ignite/util/GridCommandHandlerAbstractTest.java
+++ modules/control-utility/src/test/java/org/apache/ignite/util/GridCommandHandlerAbstractTest.java
@@ -407,7 +407,7 @@ public abstract class GridCommandHandlerAbstractTest extends GridCommonAbstractT
     }
 
     /** Custom SSL factory. */
-    protected Factory<SSLContext> sslFactory(){
+    protected Factory<SSLContext> sslFactory() {
         return GridTestUtils.sslFactory();
     }
 
