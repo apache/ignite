c4b6105cb4fb4e08f91e06427f9181c4f7b477e8
Anton Vinogradov <av@apache.org>
2023-05-19 14:12:16 +0300

    Revert "WIP"
    This reverts commit c7daf719b9d93e3815e2930655f56f0c6545b20a.

--- modules/core/src/main/java/org/apache/ignite/internal/util/GridIntList.java
+++ modules/core/src/main/java/org/apache/ignite/internal/util/GridIntList.java
@@ -45,7 +45,7 @@ public class GridIntList implements Message, Externalizable {
     private int[] arr;
 
     /** */
-    private volatile int idx;
+    private int idx;
 
     /**
      *
@@ -171,9 +171,7 @@ public class GridIntList implements Message, Externalizable {
         else if (arr.length == idx)
             arr = Arrays.copyOf(arr, arr.length << 1);
 
-        arr[idx] = x;
-
-        idx++;
+        arr[idx++] = x;
     }
 
     /**
