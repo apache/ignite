b907f6cb1660dd2288adcc9900df21c0f89e36ca
nizhikov <nizhikov@apache.org>
2023-05-31 19:31:06 +0300

    IGNITE-15629 Code cleanup

--- modules/core/src/main/java/org/apache/ignite/internal/visor/verify/VisorValidateIndexesTask.java
+++ modules/core/src/main/java/org/apache/ignite/internal/visor/verify/VisorValidateIndexesTask.java
@@ -32,7 +32,6 @@ import org.apache.ignite.cluster.ClusterNode;
 import org.apache.ignite.compute.ComputeJobResult;
 import org.apache.ignite.internal.management.cache.CacheValidateIndexesCommandArg;
 import org.apache.ignite.internal.processors.task.GridInternal;
-import org.apache.ignite.internal.util.typedef.internal.A;
 import org.apache.ignite.internal.util.typedef.internal.S;
 import org.apache.ignite.internal.visor.VisorJob;
 import org.apache.ignite.internal.visor.VisorMultiNodeTask;
@@ -114,9 +113,7 @@ public class VisorValidateIndexesTask extends VisorMultiNodeTask<CacheValidateIn
         }
 
         /** {@inheritDoc} */
-        @Override protected VisorValidateIndexesJobResult run(@Nullable CacheValidateIndexesCommandArg arg) throws IgniteException {
-            A.notNull(arg, "arg");
-
+        @Override protected VisorValidateIndexesJobResult run(CacheValidateIndexesCommandArg arg) throws IgniteException {
             try {
                 ValidateIndexesClosure clo = new ValidateIndexesClosure(
                     this::isCancelled,
