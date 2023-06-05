303a43553ebb39554b2030dc9b3551cc20fc1009
Sergey Korotkov <serge.korotkov@gmail.com>
2023-06-05 15:03:41 +0700

    IGNITE-19645 Suppress an illegal reflective access runtime warning for JDK11

--- bin/include/jvmdefaults.bat
+++ bin/include/jvmdefaults.bat
@@ -50,7 +50,10 @@ if %java_version% GEQ 11 if %java_version% LSS 15 (
     --add-exports=java.management/com.sun.jmx.mbeanserver=ALL-UNNAMED ^
     --add-exports=jdk.internal.jvmstat/sun.jvmstat.monitor=ALL-UNNAMED ^
     --add-exports=java.base/sun.reflect.generics.reflectiveObjects=ALL-UNNAMED ^
+    --add-opens=java.base/java.io=ALL-UNNAMED ^
     --add-opens=java.base/java.nio=ALL-UNNAMED ^
+    --add-opens=java.base/java.util=ALL-UNNAMED ^
+    --add-opens=java.base/java.lang=ALL-UNNAMED ^
     --add-opens=java.base/sun.nio.ch=ALL-UNNAMED ^
     --add-opens=jdk.management/com.sun.management.internal=ALL-UNNAMED ^
     --illegal-access=permit ^
