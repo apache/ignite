fab756035accde0779c512fd681f9497801f0acf
NSAmelchev <nsamelchev@gmail.com>
2023-06-29 16:33:17 +0300

    wip

--- modules/core/src/main/java/org/apache/ignite/internal/visor/cdc/VisorCdcDeleteLostSegmentsTask.java
+++ modules/core/src/main/java/org/apache/ignite/internal/visor/cdc/VisorCdcDeleteLostSegmentsTask.java
@@ -89,10 +89,10 @@ public class VisorCdcDeleteLostSegmentsTask extends VisorMultiNodeTask<CdcDelete
         protected IgniteLogger log;
 
         /** */
-        private FileWriteAheadLogManager wal;
+        private transient FileWriteAheadLogManager wal;
 
         /** */
-        private File walCdcDir;
+        private transient File walCdcDir;
 
         /**
          * Create job with specified argument.
