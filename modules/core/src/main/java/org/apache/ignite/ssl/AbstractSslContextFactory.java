10f0fea9246608538208559933fe5cf3aa4f7a4d
Mikhail Petrov <pmgheap.sbt@gmail.com>
2023-07-14 15:04:42 +0300

    IGNITE-19975 Fixes review issues.

--- modules/core/src/main/java/org/apache/ignite/ssl/AbstractSslContextFactory.java
+++ modules/core/src/main/java/org/apache/ignite/ssl/AbstractSslContextFactory.java
@@ -32,7 +32,7 @@ import org.apache.ignite.internal.util.typedef.internal.A;
 /**
  * Represents abstract implementation of SSL Context Factory that caches the result of the first successful
  * attempt to create an {@link SSLContext} and always returns it as a result of further invocations of the
- * {@link SslContextFactory#create()}} method.
+ * {@link AbstractSslContextFactory#create()}} method.
  */
 public abstract class AbstractSslContextFactory implements Factory<SSLContext> {
     /** */
