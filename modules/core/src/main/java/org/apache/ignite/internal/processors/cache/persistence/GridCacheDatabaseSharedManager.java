071b2cee039ca3992262eb0bbe137f183f4b1a23
Vladimir Steshin <vladsz83@gmail.com>
2023-08-02 16:06:22 +0300

    review fix. + TODO

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/GridCacheDatabaseSharedManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/GridCacheDatabaseSharedManager.java
@@ -1203,8 +1203,8 @@ public class GridCacheDatabaseSharedManager extends IgniteCacheDatabaseSharedMan
             chpBufSize = cacheSize;
         }
 
-        // Should be actual checkpoint lock checker, related to the checkpointer for this data region. Like
-        // LightweightCheckpointManager#checkpointer for the defragmentation.
+        // TODO IGNITE-20138 : Should be actual checkpoint lock checker, related to the checkpointer for this data
+        // region. Like LightweightCheckpointManager#checkpointer for the defragmentation.
         CheckpointLockStateChecker chpLockChecker = this;
 
         PageMemoryImpl pageMem = new PageMemoryImpl(
@@ -1224,7 +1224,7 @@ public class GridCacheDatabaseSharedManager extends IgniteCacheDatabaseSharedMan
                 // Write page to disk.
                 pageMgr.write(fullId.groupId(), fullId.pageId(), pageBuf, tag, true);
 
-                // Should be actual checkpointer, related to this data rageion. Like
+                // TODO IGNITE-20138 : Should be actual checkpointer, related to this data rageion. Like
                 // LightweightCheckpointManager#checkpointer for the defragmentation.
                 getCheckpointer().currentProgress().updateEvictedPages(1);
             },
