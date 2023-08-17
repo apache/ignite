2ba290e701a3c12d7979a38bca8294f9aefdbe80
Maksim Timonin <timoninmaxim@apache.org>
2023-08-17 11:17:41 +0300

    IGNITE-20228 Fix connectable nodes list for control.sh (#10898)

--- modules/core/src/main/java/org/apache/ignite/internal/management/api/CommandUtils.java
+++ modules/core/src/main/java/org/apache/ignite/internal/management/api/CommandUtils.java
@@ -707,7 +707,7 @@ public class CommandUtils {
                 .collect(Collectors.toList());
 
             if (!connectable.isEmpty())
-                compute = compute.projection(nodes);
+                compute = compute.projection(connectable);
 
             return compute.execute(
                 taskCls.getName(),
