22afe2660fe4e01cc5a6b4e26bd25a0e8e8c0fe7
Sergey Korotkov <serge.korotkov@gmail.com>
2023-10-04 15:14:52 +0700

    IGNITE-20470 [ducktests] Add simple test for the cache dump

---
+++ modules/ducktests/tests/ignitetest/tests/dump_test.py
@@ -0,0 +1,99 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Module contains cache dump tests.
+"""
+import os
+import sys
+from ducktape.mark import defaults
+
+from ignitetest.services.ignite import IgniteService
+from ignitetest.services.utils.control_utility import ControlUtility
+from ignitetest.services.utils.dump_utility import DumpUtility
+from ignitetest.services.utils.ignite_configuration import IgniteConfiguration, DataStorageConfiguration
+from ignitetest.services.utils.ignite_configuration.data_storage import DataRegionConfiguration
+from ignitetest.services.utils.ignite_configuration.discovery import from_ignite_cluster
+from ignitetest.tests.util import DataGenerationParams, preload_data
+from ignitetest.utils import cluster, ignite_versions
+from ignitetest.utils.ignite_test import IgniteTest
+from ignitetest.utils.version import IgniteVersion, DEV_BRANCH
+
+
+class DumpTest(IgniteTest):
+    """
+    Test cache dump.
+    """
+    DUMP_NAME = "test_dump"
+    CACHE_NAME = "test-cache"
+
+    @cluster(num_nodes=5)
+    @ignite_versions(str(DEV_BRANCH))
+    @defaults(nodes=[1, 3], backups=[1], cache_count=[1], entry_count=[50_000], entry_size=[1024], preloaders=[1])
+    def dump_test(self, ignite_version, nodes, backups, cache_count, entry_count, entry_size, preloaders):
+        """
+        Basic dump test.
+        """
+        data_gen_params = DataGenerationParams(backups=backups, cache_count=cache_count, entry_count=entry_count,
+                                               entry_size=entry_size, preloaders=preloaders)
+
+        ignite_config = IgniteConfiguration(
+            version=IgniteVersion(ignite_version),
+            data_storage=DataStorageConfiguration(
+                checkpoint_frequency=5000,
+                default=DataRegionConfiguration(
+                    persistence_enabled=True,
+                    max_size=data_gen_params.data_region_max_size
+                )
+            ),
+            metric_exporters={'org.apache.ignite.spi.metric.jmx.JmxMetricExporterSpi'}
+        )
+
+        ignite = IgniteService(self.test_context, ignite_config, num_nodes=nodes)
+        ignite.start()
+
+        control_utility = ControlUtility(ignite)
+        control_utility.activate()
+
+        preload_data(
+            self.test_context,
+            ignite.config._replace(client_mode=True, discovery_spi=from_ignite_cluster(ignite)),
+            data_gen_params=data_gen_params)
+
+        dump_utility = DumpUtility(self.test_context, ignite)
+
+        dump_utility.create(self.DUMP_NAME)
+
+        def dump_files_total_size_on_node(node):
+            dump_dir = os.path.join(ignite.snapshots_dir, self.DUMP_NAME, "db",
+                                    ignite.consistent_dir(node.account.externally_routable_ip))
+
+            return int(node.account.ssh_output(
+                "find " + dump_dir + " -name '*.dump' -exec du -a --bytes {} \\; "
+                                     "| awk '{  total += $1 }; END { print total }'"
+            ).decode(sys.getdefaultencoding()).splitlines()[0])
+
+        dump_files_total_sizes = {
+            ignite.consistent_dir(node.account.externally_routable_ip): dump_files_total_size_on_node(node)
+            for node in ignite.nodes
+        }
+
+        assert all([size > 0 for size in dump_files_total_sizes.values()]), "dumps are not empty on all nodes"
+
+        ignite.stop()
+
+        return {
+            "dump_sizes": dump_files_total_sizes
+        }
