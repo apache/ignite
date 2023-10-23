1874d0f3396ab4dbd85b6aa2a9c9cf150bc84923
Ilya Shishkov <shishkovilja@gmail.com>
2023-10-23 22:56:38 +0300

    IGNITE-10516 Code-review fixes: add test for multiline query

--- modules/core/src/test/java/org/apache/ignite/internal/processors/query/schema/IndexWithSameNameTestBase.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/query/schema/IndexWithSameNameTestBase.java
@@ -18,14 +18,19 @@
 package org.apache.ignite.internal.processors.query.schema;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.stream.Collectors;
 import org.apache.ignite.Ignite;
+import org.apache.ignite.cache.query.FieldsQueryCursor;
 import org.apache.ignite.cache.query.SqlFieldsQuery;
 import org.apache.ignite.cluster.ClusterState;
 import org.apache.ignite.configuration.DataRegionConfiguration;
@@ -33,6 +38,7 @@ import org.apache.ignite.configuration.DataStorageConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.configuration.QueryEngineConfiguration;
 import org.apache.ignite.configuration.SqlConfiguration;
+import org.apache.ignite.internal.IgniteEx;
 import org.apache.ignite.internal.TestRecordingCommunicationSpi;
 import org.apache.ignite.internal.managers.discovery.CustomMessageWrapper;
 import org.apache.ignite.internal.managers.discovery.DiscoveryCustomMessage;
@@ -40,6 +46,8 @@ import org.apache.ignite.internal.processors.query.GridQueryProcessor;
 import org.apache.ignite.internal.processors.query.schema.management.IndexDescriptor;
 import org.apache.ignite.internal.processors.query.schema.message.SchemaFinishDiscoveryMessage;
 import org.apache.ignite.internal.processors.query.schema.message.SchemaOperationStatusMessage;
+import org.apache.ignite.internal.processors.query.schema.operation.SchemaIndexCreateOperation;
+import org.apache.ignite.internal.util.lang.GridTuple3;
 import org.apache.ignite.internal.util.typedef.internal.U;
 import org.apache.ignite.spi.discovery.DiscoverySpiCustomMessage;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
@@ -48,12 +56,15 @@ import org.apache.ignite.spi.discovery.tcp.messages.TcpDiscoveryCustomEventMessa
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.junit.Test;
 
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.apache.ignite.internal.TestRecordingCommunicationSpi.spi;
 import static org.apache.ignite.internal.processors.query.QueryUtils.KEY_FIELD_NAME;
 import static org.apache.ignite.internal.processors.query.schema.IndexWithSameNameTestBase.SchemaFinishListeningTcpDiscoverySpi.discoSpi;
 import static org.apache.ignite.internal.util.lang.GridFunc.asSet;
+import static org.apache.ignite.internal.util.lang.GridFunc.t;
 
 /** */
+@SuppressWarnings("deprecation")
 public abstract class IndexWithSameNameTestBase extends GridCommonAbstractTest {
     /** Test index. */
     public static final String TEST_INDEX = "TEST_IDX";
@@ -64,14 +75,31 @@ public abstract class IndexWithSameNameTestBase extends GridCommonAbstractTest {
     /** All nodes count, including non-baseline nodes. */
     public static final int NODES_COUNT = BASELINE_SIZE + 2;
 
+    /** Client node index. */
+    public static final int CLIENT_INDEX = NODES_COUNT - 1;
+
+    /** Correct index fields. */
+    public static final Set<String> CORRECT_FIELDS = asSet("K1", "V1");
+
+    /** Fields of duplicate index. */
+    public static final Set<String> DUPLICATE_FIELDS = asSet("K2", "V2");
+
     /** Schema finish latch. */
     public static CountDownLatch schemaFinishLatch;
+
+    /** Index create operation identifiers. */
+    public static Map<Set<String>, UUID> idxOps = new ConcurrentHashMap<>();
+
+    /** Schema status latch. */
+    private CountDownLatch schemaStatusLatch;
 
     /** {@inheritDoc} */
     @Override protected void beforeTest() throws Exception {
         super.beforeTest();
 
         cleanPersistenceDir();
+
+        idxOps.clear();
     }
 
     /** {@inheritDoc} */
@@ -91,7 +119,16 @@ public abstract class IndexWithSameNameTestBase extends GridCommonAbstractTest {
         discoSpi.setIpFinder(((TcpDiscoverySpi)cfg.getDiscoverySpi()).getIpFinder());
 
         TestRecordingCommunicationSpi commSpi = new TestRecordingCommunicationSpi();
-        commSpi.record(SchemaOperationStatusMessage.class);
+
+        commSpi.record((n, msg) -> {
+            if (msg instanceof SchemaOperationStatusMessage) {
+                schemaStatusLatch.countDown();
+
+                return true;
+            }
+
+            return false;
+        });
 
         return cfg.setDiscoverySpi(discoSpi)
             .setCommunicationSpi(commSpi)
@@ -102,91 +139,148 @@ public abstract class IndexWithSameNameTestBase extends GridCommonAbstractTest {
                     .setPersistenceEnabled(true)));
     }
 
-    /**
-     *
-     */
+    /** */
     protected abstract QueryEngineConfiguration getEngineConfiguration();
 
-    /**
-     *
-     */
+    /** */
     @Test
-    @SuppressWarnings("deprecation")
-    public void testWithRestart() throws Exception {
-        startGrids(BASELINE_SIZE)
-            .cluster()
-            .state(ClusterState.ACTIVE);
+    public void testSeparateRequests() throws Exception {
+        doTestWithRestart(qryProc -> {
+            checkIndexCreate(qryProc, t("TABLE1", CORRECT_FIELDS, /* nop */ false));
+            checkIndexCreate(qryProc, t("TABLE2", DUPLICATE_FIELDS, /* nop */ true));
+        });
+    }
+
+    /** */
+    @Test
+    public void testMultiLineRequest() throws Exception {
+        doTestWithRestart(qryProc -> checkIndexCreate(
+            qryProc,
+            t("TABLE1", CORRECT_FIELDS, /* nop */ false),
+            t("TABLE2", DUPLICATE_FIELDS, /* nop */ true)));
+    }
+
+    /** */
+    public void doTestWithRestart(ConsumerX<GridQueryProcessor> idxCreateAction) throws Exception {
+        // Ensure coordinator.
+        IgniteEx crd = startGrid(0);
+
+        startGridsMultiThreaded(1, BASELINE_SIZE - 1);
+
+        waitForTopology(BASELINE_SIZE);
+
+        crd.cluster().state(ClusterState.ACTIVE);
+        awaitPartitionMapExchange();
 
         // Non-baseline nodes.
         startGrid(BASELINE_SIZE);
-        startClientGrid(NODES_COUNT - 1);
+        startClientGrid(CLIENT_INDEX);
 
-        GridQueryProcessor qryProc = grid(0).context().query();
+        waitForTopology(NODES_COUNT);
+        assertEquals(BASELINE_SIZE, crd.cluster().currentBaselineTopology().size());
 
-        qryProc.querySqlFields(new SqlFieldsQuery("CREATE TABLE T1 (k1 INT PRIMARY KEY, v1 INT)"), true).getAll();
-        qryProc.querySqlFields(new SqlFieldsQuery("CREATE TABLE T2 (k2 INT PRIMARY KEY, v2 INT)"), true).getAll();
+        GridQueryProcessor qryProc = crd.context().query();
 
-        Set<String> correctFields = asSet("K1", "V1");
-        Set<String> duplicateFields = asSet("K2", "V2");
+        qryProc.querySqlFields(new SqlFieldsQuery("CREATE TABLE TABLE1 (K1 INT PRIMARY KEY, V1 INT)"), true).getAll();
+        qryProc.querySqlFields(new SqlFieldsQuery("CREATE TABLE TABLE2 (K2 INT PRIMARY KEY, V2 INT)"), true).getAll();
 
-        // This index will be created.
-        checkIndexCreate(qryProc, "T1", correctFields, false);
+        idxCreateAction.accept(qryProc);
 
-        // This index must not be created. Query must be no-op.
-        checkIndexCreate(qryProc, "T2", duplicateFields, true);
+        assertSingleIndex(NODES_COUNT);
 
-        assertSingleIndex(correctFields);
-
-        grid(0).cluster().state(ClusterState.INACTIVE);
+        crd.cluster().state(ClusterState.INACTIVE);
         awaitPartitionMapExchange();
         stopAllGrids();
 
         startGrids(BASELINE_SIZE);
 
-        assertSingleIndex(correctFields);
+        assertSingleIndex(BASELINE_SIZE);
     }
 
     /**
      * @param qryProc Query processor.
-     * @param tblName Table name.
-     * @param idxFields Index fields.
-     * @param expNop Expected no-op flag.
+     * @param idxParams Expected index parameters.
      */
-    private void checkIndexCreate(GridQueryProcessor qryProc, String tblName, Set<String> idxFields, boolean expNop)
+    private void checkIndexCreate(GridQueryProcessor qryProc, GridTuple3<String, Set<String>, Boolean>... idxParams)
         throws InterruptedException {
+        List<GridTuple3<String, Set<String>, Boolean>> idxParams0 = Arrays.asList(idxParams);
+
+        // Multi-statement index create.
+        String idxCreateSql = idxParams0.stream()
+            .map(t -> String.format("CREATE INDEX IF NOT EXISTS %s ON %s (%s)", TEST_INDEX,
+                t.get1(),
+                String.join(",", t.get2())))
+            .collect(Collectors.joining("\n;"));
+
         // SchemaFinishDiscoveryMessage is processed twice on coordinator.
-        schemaFinishLatch = new CountDownLatch(NODES_COUNT + 1);
+        schemaFinishLatch = new CountDownLatch((NODES_COUNT + 1) * idxParams0.size());
 
-        String createIdxSql = String.format("CREATE INDEX IF NOT EXISTS %s ON %s (%s)", TEST_INDEX, tblName,
-            String.join(",", idxFields));
+        // SchemaOperationStatusMessage is not sent by coordinator and client.
+        schemaStatusLatch = new CountDownLatch((NODES_COUNT - 2) * idxParams0.size());
 
-        qryProc.querySqlFields(new SqlFieldsQuery(createIdxSql), true).getAll();
+        qryProc.querySqlFields(new SqlFieldsQuery(idxCreateSql), true, false)
+            .forEach(FieldsQueryCursor::getAll);
 
-        schemaFinishLatch.await();
+        schemaStatusLatch.await(getTestTimeout(), MILLISECONDS);
+        schemaFinishLatch.await(getTestTimeout(), MILLISECONDS);
 
-        checkNoOpMessages(expNop);
+        checkNoOpMessages(idxParams0);
     }
 
     /**
-     * @param expNop Expected no-op flag.
+     * @param idxParams Expected index parameters.
      */
-    private void checkNoOpMessages(boolean expNop) {
+    private void checkNoOpMessages(List<GridTuple3<String, Set<String>, Boolean>> idxParams) {
         for (int i = 0; i < NODES_COUNT; i++) {
-            assertTrue(spi(grid(i)).recordedMessages(false)
-                .stream()
-                .allMatch(msg -> ((SchemaOperationStatusMessage)msg).nop() == expNop));
+            List<Object> commMsgs = spi(grid(i)).recordedMessages(false);
 
-            assertTrue(discoSpi(grid(i)).recordedMessages()
-                .stream()
-                .allMatch(msg -> msg.nop() == expNop));
+            boolean crdOrClient = i == 0 || i == CLIENT_INDEX;
+
+            String commMsgErr = "Unexpected SchemaOperationStatusMessage count on node: igniteIndex=" + i +
+                ", commMsgsCnt=" + commMsgs.size();
+
+            int sqlCnt = idxParams.size();
+
+            // SchemaOperationStatusMessage is not sent by coordinator and client.
+            assertEquals(commMsgErr, crdOrClient ? 0 : sqlCnt, commMsgs.size());
+
+            List<SchemaFinishDiscoveryMessage> discoMsgs = discoSpi(grid(i)).recordedMessages();
+
+            String discoMsgErr = "Unexpected SchemaFinishDiscoveryMessage count on node: igniteIndex=" + i +
+                ", discoMsgsCnt=" + discoMsgs.size();
+
+            // SchemaFinishDiscoveryMessage is processed twice on coordinator.
+            assertEquals(discoMsgErr, i == 0 ? 2 * sqlCnt : sqlCnt, discoMsgs.size());
+
+            for (GridTuple3<String, Set<String>, Boolean> param : idxParams) {
+                Set<String> expIdxFields = param.get2();
+                boolean expNop = param.get3();
+
+                UUID opId = idxOps.get(expIdxFields);
+
+                String commNopErr = String.format(
+                    "Unexpected no-op flag in communication messages: opId=%s, igniteIndex=%d, expNop=%b, recordedMsgs=%s",
+                    opId, i, expNop, commMsgs);
+
+                assertTrue(commNopErr, commMsgs.stream()
+                    .map(msg -> ((SchemaOperationStatusMessage)msg))
+                    .filter(msg -> opId.equals(msg.operationId()))
+                    .allMatch(msg -> msg.nop() == expNop));
+
+                String discoNopErr = String.format(
+                    "Unexpected no-op flag in discovery messages: opId=%s, igniteIndex=%d, expNop=%b, recordedMsgs=%s",
+                    opId, i, expNop, discoMsgs);
+
+                assertTrue(discoNopErr, discoMsgs.stream()
+                    .filter(msg -> opId.equals(msg.operation().id()))
+                    .allMatch(msg -> msg.nop() == expNop));
+            }
         }
     }
 
-    /**
-     * @param expIdxFields Expected index fields.
-     */
-    private void assertSingleIndex(Collection<String> expIdxFields) {
-        for (int i = 0; i < BASELINE_SIZE; i++) {
+    /** */
+    private void assertSingleIndex(int nodesCnt) {
+        for (int i = 0; i < nodesCnt; i++) {
             Collection<IndexDescriptor> indexes = grid(i).context()
                 .query()
                 .schemaManager()
@@ -205,13 +299,11 @@ public abstract class IndexWithSameNameTestBase extends GridCommonAbstractTest {
                 .filter(f -> !KEY_FIELD_NAME.equalsIgnoreCase(f))
                 .collect(Collectors.toSet());
 
-            assertEqualsCollectionsIgnoringOrder(expIdxFields, actualFields);
+            assertEqualsCollectionsIgnoringOrder(CORRECT_FIELDS, actualFields);
         }
     }
 
-    /**
-     *
-     */
+    /** */
     public static class SchemaFinishListeningTcpDiscoverySpi extends TcpDiscoverySpi {
         /** Filtered messages. */
         private final List<SchemaFinishDiscoveryMessage> recordedMsgs = new CopyOnWriteArrayList<>();
@@ -226,7 +318,13 @@ public abstract class IndexWithSameNameTestBase extends GridCommonAbstractTest {
                     DiscoveryCustomMessage discoCustomMsg = ((CustomMessageWrapper)spiCustomMsg).delegate();
 
                     if (discoCustomMsg instanceof SchemaFinishDiscoveryMessage) {
-                        recordedMsgs.add((SchemaFinishDiscoveryMessage)discoCustomMsg);
+                        SchemaFinishDiscoveryMessage finishMsg = (SchemaFinishDiscoveryMessage)discoCustomMsg;
+
+                        SchemaIndexCreateOperation op = (SchemaIndexCreateOperation)finishMsg.operation();
+
+                        idxOps.putIfAbsent(new HashSet<>(op.index().getFieldNames()), op.id());
+
+                        recordedMsgs.add(finishMsg);
 
                         schemaFinishLatch.countDown();
                     }
@@ -239,16 +337,12 @@ public abstract class IndexWithSameNameTestBase extends GridCommonAbstractTest {
             }
         }
 
-        /**
-         * @param ignite Ignite.
-         */
+        /** */
         public static SchemaFinishListeningTcpDiscoverySpi discoSpi(Ignite ignite) {
             return (SchemaFinishListeningTcpDiscoverySpi)ignite.configuration().getDiscoverySpi();
         }
 
-        /**
-         *
-         */
+        /** */
         public List<SchemaFinishDiscoveryMessage> recordedMessages() {
             List<SchemaFinishDiscoveryMessage> recordedMsgs0 = new ArrayList<>(recordedMsgs);
 
@@ -256,5 +350,11 @@ public abstract class IndexWithSameNameTestBase extends GridCommonAbstractTest {
 
             return recordedMsgs0;
         }
+    }
+
+    /** */
+    private interface ConsumerX<T> {
+        /** */
+        void accept(T t) throws Exception;
     }
 }
