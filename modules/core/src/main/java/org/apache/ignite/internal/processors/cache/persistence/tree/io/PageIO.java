77dbca7bc531346e152b8fce63dd1eb0433a715b
Ilya Shishkov <shishkovilja@gmail.com>
2023-12-25 16:56:47 +0300

    IGNITE-21130 Remove all others, except for TxState

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/tree/io/PageIO.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/tree/io/PageIO.java
@@ -27,8 +27,6 @@ import org.apache.ignite.internal.pagemem.PageIdUtils;
 import org.apache.ignite.internal.pagemem.PageMemory;
 import org.apache.ignite.internal.pagemem.PageUtils;
 import org.apache.ignite.internal.pagemem.wal.IgniteWriteAheadLogManager;
-import org.apache.ignite.internal.processors.cache.mvcc.txlog.TxLogInnerIO;
-import org.apache.ignite.internal.processors.cache.mvcc.txlog.TxLogLeafIO;
 import org.apache.ignite.internal.processors.cache.persistence.IndexStorageImpl;
 import org.apache.ignite.internal.processors.cache.persistence.defragmentation.LinkMap;
 import org.apache.ignite.internal.processors.cache.persistence.freelist.io.PagesListMetaIO;
@@ -777,12 +775,6 @@ public abstract class PageIO {
                     break;
 
                 return (Q)h2MvccLeafIOs.forVersion(ver);
-
-            case T_TX_LOG_INNER:
-                return (Q)TxLogInnerIO.VERSIONS.forVersion(ver);
-
-            case T_TX_LOG_LEAF:
-                return (Q)TxLogLeafIO.VERSIONS.forVersion(ver);
 
             case T_DATA_REF_INNER:
                 return (Q)DataInnerIO.VERSIONS.forVersion(ver);
