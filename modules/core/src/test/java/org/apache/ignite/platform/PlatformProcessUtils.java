4e2778b5a5b4c2b322626db4780436dd7e74b08f
Vladimir Steshin <vladsz83@gmail.com>
2023-12-31 12:41:27 +0300

    + .net test fix

--- modules/core/src/test/java/org/apache/ignite/platform/PlatformProcessUtils.java
+++ modules/core/src/test/java/org/apache/ignite/platform/PlatformProcessUtils.java
@@ -27,12 +27,16 @@ import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import org.apache.ignite.internal.util.typedef.internal.U;
 
 /**
  * Platform process utils for tests.
  */
 @SuppressWarnings("unused") // Called from Platform.
 public class PlatformProcessUtils {
+    /** Seconds to wait until the process stops. */
+    private static final long STOP_WAIT_SECONDS = 15;
+
     /** */
     private static volatile Process process;
 
@@ -127,13 +131,28 @@ public class PlatformProcessUtils {
     }
 
     /**
-     * Kills the process previously started with {@link #startProcess}.
+     * Kills the process previously started with {@link #startProcess}. Waits for {@link #STOP_WAIT_SECONDS} until
+     * the process stops.
      */
     public static void destroyProcess() throws Exception {
         if (process == null)
             throw new Exception("Process has not been started");
 
+        System.out.println("PlatformProcessUtils >> stopping the process...");
+
+        long time = System.nanoTime();
+
         process.destroyForcibly();
+
+        boolean stopped = process.waitFor(STOP_WAIT_SECONDS, TimeUnit.SECONDS);
+
+        time = U.nanosToMillis(System.nanoTime() - time);
+
         process = null;
+
+        if (!stopped)
+            throw new IllegalStateException("The process hasn't stopped within " + STOP_WAIT_SECONDS + " seconds.");
+
+        System.out.println("PlatformProcessUtils >> the process has stopped within " + time + " milliseconds.");
     }
 }
