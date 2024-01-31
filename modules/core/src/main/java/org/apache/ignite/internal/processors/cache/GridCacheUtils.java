71717031f5700bb8bfd441376f32af8b3c9c97ce
Mikhail Petrov <32207922+petrov-mg@users.noreply.github.com>
2024-01-31 19:25:37 +0300

    IGNITE-21399 Fixed logging of reduced cache entries for distributed long running operations. (#11210)
    (cherry picked from commit 597302da972c1e0de0b6d183215074c9c711f72e)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheUtils.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheUtils.java
@@ -98,6 +98,7 @@ import org.apache.ignite.internal.util.typedef.T2;
 import org.apache.ignite.internal.util.typedef.X;
 import org.apache.ignite.internal.util.typedef.internal.A;
 import org.apache.ignite.internal.util.typedef.internal.CU;
+import org.apache.ignite.internal.util.typedef.internal.S;
 import org.apache.ignite.internal.util.typedef.internal.U;
 import org.apache.ignite.lang.IgniteBiInClosure;
 import org.apache.ignite.lang.IgniteClosure;
@@ -643,7 +644,7 @@ public class GridCacheUtils {
 
             /** {@inheritDoc} */
             @Override public String toString() {
-                return "Map reducer: " + ret;
+                return S.toString("Map Reducer", "reducedEntries", ret);
             }
         };
     }
@@ -676,7 +677,7 @@ public class GridCacheUtils {
 
             /** {@inheritDoc} */
             @Override public synchronized String toString() {
-                return "Collection reducer: " + ret;
+                return S.toString("Collection Reducer", "reducedElements", ret);
             }
         };
     }
--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheTestSuite13.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheTestSuite13.java
@@ -35,6 +35,7 @@ import org.apache.ignite.internal.metric.SystemViewClusterActivationTest;
 import org.apache.ignite.internal.metric.SystemViewComputeJobTest;
 import org.apache.ignite.internal.metric.SystemViewSelfTest;
 import org.apache.ignite.internal.processors.cache.CacheClearAsyncDeadlockTest;
+import org.apache.ignite.internal.processors.cache.CacheDistributedGetLongRunningFutureDumpTest;
 import org.apache.ignite.internal.processors.cache.GridCacheDataTypesCoverageTest;
 import org.apache.ignite.internal.processors.cache.GridCacheLongRunningTransactionDiagnosticsTest;
 import org.apache.ignite.internal.processors.cache.GridCacheVersionGenerationWithCacheStorageTest;
@@ -100,6 +101,7 @@ public class IgniteCacheTestSuite13 {
         GridTestUtils.addTestIfNeeded(suite, CacheClearAsyncDeadlockTest.class, ignoredTests);
 
         GridTestUtils.addTestIfNeeded(suite, TxAsyncOpsSemaphorePermitsExceededTest.class, ignoredTests);
+        GridTestUtils.addTestIfNeeded(suite, CacheDistributedGetLongRunningFutureDumpTest.class, ignoredTests);
 
         return suite;
     }
