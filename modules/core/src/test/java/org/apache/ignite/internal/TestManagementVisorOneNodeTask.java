16b2edd8392a6787f316d748f1ca3a6b0d176b98
Nikita Amelchev <nsamelchev@gmail.com>
2024-02-07 13:01:13 +0300

    IGNITE-21110 Added the management event to all management commands (#11121)

--- modules/core/src/test/java/org/apache/ignite/internal/TestManagementVisorOneNodeTask.java
+++ modules/core/src/test/java/org/apache/ignite/internal/TestManagementVisorOneNodeTask.java
@@ -19,7 +19,6 @@ package org.apache.ignite.internal;
 
 import java.util.List;
 import org.apache.ignite.compute.ComputeJobResult;
-import org.apache.ignite.internal.processors.task.GridVisorManagementTask;
 import org.apache.ignite.internal.visor.VisorJob;
 import org.apache.ignite.internal.visor.VisorOneNodeTask;
 import org.apache.ignite.internal.visor.VisorTaskArgument;
@@ -28,7 +27,6 @@ import org.jetbrains.annotations.Nullable;
 /**
  *
  */
-@GridVisorManagementTask
 public class TestManagementVisorOneNodeTask extends VisorOneNodeTask<VisorTaskArgument, Object> {
     /** */
     private static final long serialVersionUID = 0L;
