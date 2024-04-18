383d735312f9db356b9b999b14bb56049f80cfbf
Julia Bakulina <julia.bakuli@yandex.ru>
2024-04-18 22:03:16 +0300

    IGNITE-21888 Cleanup MvccUtils

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/mvcc/MvccUtils.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/mvcc/MvccUtils.java
@@ -17,180 +17,18 @@
 
 package org.apache.ignite.internal.processors.cache.mvcc;
 
-import org.apache.ignite.configuration.TransactionConfiguration;
 import org.apache.ignite.internal.GridKernalContext;
-import org.apache.ignite.internal.processors.cache.CacheGroupContext;
-import org.apache.ignite.internal.processors.cache.GridCacheContext;
 import org.apache.ignite.internal.processors.cache.distributed.near.GridNearTxLocal;
-import org.apache.ignite.internal.processors.cache.mvcc.txlog.TxState;
 import org.apache.ignite.internal.processors.cache.transactions.IgniteInternalTx;
 import org.apache.ignite.internal.processors.cache.transactions.IgniteTxManager;
 import org.apache.ignite.internal.processors.cache.version.GridCacheVersion;
-import org.apache.ignite.internal.transactions.IgniteTxAlreadyCompletedCheckedException;
-import org.apache.ignite.internal.transactions.IgniteTxUnexpectedStateCheckedException;
-import org.apache.ignite.internal.util.typedef.internal.CU;
-import org.apache.ignite.transactions.TransactionState;
 import org.apache.ignite.transactions.TransactionUnsupportedConcurrencyException;
 import org.jetbrains.annotations.Nullable;
-
-import static org.apache.ignite.cache.CacheWriteSynchronizationMode.FULL_SYNC;
-import static org.apache.ignite.transactions.TransactionConcurrency.PESSIMISTIC;
-import static org.apache.ignite.transactions.TransactionIsolation.REPEATABLE_READ;
 
 /**
  * Utils for MVCC.
  */
 public class MvccUtils {
-    /** Mask for operation counter bits. (Excludes hints and flags) */
-    public static final int MVCC_OP_COUNTER_MASK = ~(Integer.MIN_VALUE >> 2);
-
-    /** */
-    public static final long MVCC_CRD_COUNTER_NA = 0L;
-
-    /** */
-    public static final long MVCC_COUNTER_NA = 0L;
-
-    /** */
-    public static final int MVCC_OP_COUNTER_NA = 0;
-
-    /**
-     *
-     */
-    private MvccUtils() {
-    }
-
-    /**
-     * @param cctx Cache context.
-     * @param mvccCrd Mvcc coordinator version.
-     * @param mvccCntr Mvcc counter.
-     * @param mvccOpCntr Mvcc operation counter.
-     * @return TxState
-     * @see TxState
-     */
-    public static byte state(GridCacheContext cctx, long mvccCrd, long mvccCntr, int mvccOpCntr) {
-        return TxState.NA;
-    }
-
-    /**
-     * @param grp Cache group context.
-     * @param mvccCrd Mvcc coordinator version.
-     * @param mvccCntr Mvcc counter.
-     * @param mvccOpCntr Mvcc operation counter.
-     * @return TxState
-     * @see TxState
-     */
-    public static byte state(CacheGroupContext grp, long mvccCrd, long mvccCntr, int mvccOpCntr) {
-        return TxState.NA;
-    }
-
-    /**
-     *
-     * @param grp Cache group context.
-     * @param state State.
-     * @param crd Mvcc coordinator counter.
-     * @param cntr Mvcc counter.
-     * @param opCntr Mvcc operation counter.
-     * @return State exception.
-     */
-    public static IgniteTxUnexpectedStateCheckedException unexpectedStateException(
-        CacheGroupContext grp, byte state, long crd, long cntr,
-        int opCntr) {
-        return unexpectedStateException(grp.shared().kernalContext(), state, crd, cntr, opCntr, null);
-    }
-
-    /**
-     * @param cctx Cache context.
-     * @param state State.
-     * @param crd Mvcc coordinator counter.
-     * @param cntr Mvcc counter.
-     * @param opCntr Mvcc operation counter.
-     * @param snapshot Mvcc snapshot
-     * @return State exception.
-     */
-    public static IgniteTxUnexpectedStateCheckedException unexpectedStateException(
-        GridCacheContext cctx, byte state, long crd, long cntr,
-        int opCntr, MvccSnapshot snapshot) {
-        return unexpectedStateException(cctx.kernalContext(), state, crd, cntr, opCntr, snapshot);
-    }
-
-    /** */
-    private static IgniteTxUnexpectedStateCheckedException unexpectedStateException(GridKernalContext ctx, byte state, long crd, long cntr,
-        int opCntr, MvccSnapshot snapshot) {
-        String msg = "Unexpected state: [state=" + state + ", rowVer=" + crd + ":" + cntr + ":" + opCntr;
-
-        msg += ", localNodeId=" + ctx.localNodeId() + "]";
-
-        return new IgniteTxUnexpectedStateCheckedException(msg);
-    }
-
-    /**
-     * Compares to pairs of coordinator/counter versions. See {@link Comparable}.
-     *
-     * @param mvccCrdLeft First coordinator version.
-     * @param mvccCntrLeft First counter version.
-     * @param mvccCrdRight Second coordinator version.
-     * @param mvccCntrRight Second counter version.
-     * @return Comparison result, see {@link Comparable}.
-     */
-    public static int compare(long mvccCrdLeft, long mvccCntrLeft, long mvccCrdRight, long mvccCntrRight) {
-        return compare(mvccCrdLeft, mvccCntrLeft, 0, mvccCrdRight, mvccCntrRight, 0);
-    }
-
-    /**
-     * Compares to pairs of coordinator/counter versions. See {@link Comparable}.
-     *
-     * @param mvccCrdLeft First coordinator version.
-     * @param mvccCntrLeft First counter version.
-     * @param mvccOpCntrLeft First operation counter.
-     * @param mvccCrdRight Second coordinator version.
-     * @param mvccCntrRight Second counter version.
-     * @param mvccOpCntrRight Second operation counter.
-     * @return Comparison result, see {@link Comparable}.
-     */
-    public static int compare(long mvccCrdLeft, long mvccCntrLeft, int mvccOpCntrLeft, long mvccCrdRight,
-        long mvccCntrRight, int mvccOpCntrRight) {
-        int cmp;
-
-        if ((cmp = Long.compare(mvccCrdLeft, mvccCrdRight)) != 0
-            || (cmp = Long.compare(mvccCntrLeft, mvccCntrRight)) != 0
-            || (cmp = Integer.compare(mvccOpCntrLeft & MVCC_OP_COUNTER_MASK, mvccOpCntrRight & MVCC_OP_COUNTER_MASK)) != 0)
-            return cmp;
-
-        return 0;
-    }
-
-    /**
-     * @param crdVer Mvcc coordinator version.
-     * @param cntr Counter.
-     * @param opCntr Operation counter.
-     * @return Always {@code true}.
-     */
-    public static boolean mvccVersionIsValid(long crdVer, long cntr, int opCntr) {
-        return mvccVersionIsValid(crdVer, cntr) && (opCntr & MVCC_OP_COUNTER_MASK) != MVCC_OP_COUNTER_NA;
-    }
-
-    /**
-     * @param crdVer Mvcc coordinator version.
-     * @param cntr Counter.
-     * @return {@code True} if version is valid.
-     */
-    public static boolean mvccVersionIsValid(long crdVer, long cntr) {
-        return crdVer > MVCC_CRD_COUNTER_NA && cntr > MVCC_COUNTER_NA;
-    }
-
-    /**
-     * Checks transaction state.
-     * @param tx Transaction.
-     * @return Checked transaction.
-     */
-    public static GridNearTxLocal checkActive(GridNearTxLocal tx) throws IgniteTxAlreadyCompletedCheckedException {
-        if (tx != null && tx.state() != TransactionState.ACTIVE)
-            throw new IgniteTxAlreadyCompletedCheckedException("Transaction is already completed.");
-
-        return tx;
-    }
-
-
     /**
      * @param ctx Grid kernal context.
      * @return Currently started user transaction, or {@code null} if none started.
@@ -220,55 +58,6 @@ public class MvccUtils {
                 throw new TransactionUnsupportedConcurrencyException("Only pessimistic transactions are supported when MVCC is enabled.");
             }
         }
-
-        return tx;
-    }
-
-    /**
-     * @param ctx Grid kernal context.
-     * @param timeout Transaction timeout.
-     * @return Newly started SQL transaction.
-     */
-    public static GridNearTxLocal txStart(GridKernalContext ctx, long timeout) {
-        return txStart(ctx, null, timeout);
-    }
-
-    /**
-     * @param cctx Cache context.
-     * @param timeout Transaction timeout.
-     * @return Newly started SQL transaction.
-     */
-    public static GridNearTxLocal txStart(GridCacheContext cctx, long timeout) {
-        return txStart(cctx.kernalContext(), cctx, timeout);
-    }
-
-    /**
-     * @param ctx Grid kernal context.
-     * @param cctx Cache context.
-     * @param timeout Transaction timeout.
-     * @return Newly started SQL transaction.
-     */
-    private static GridNearTxLocal txStart(GridKernalContext ctx, @Nullable GridCacheContext cctx, long timeout) {
-        if (timeout == 0) {
-            TransactionConfiguration tcfg = CU.transactionConfiguration(cctx, ctx.config());
-
-            if (tcfg != null)
-                timeout = tcfg.getDefaultTxTimeout();
-        }
-
-        GridNearTxLocal tx = ctx.cache().context().tm().newTx(
-            false,
-            false,
-            cctx != null && cctx.systemTx() ? cctx : null,
-            PESSIMISTIC,
-            REPEATABLE_READ,
-            timeout,
-            cctx == null || !cctx.skipStore(),
-            0,
-            null
-        );
-
-        tx.syncMode(FULL_SYNC);
 
         return tx;
     }
