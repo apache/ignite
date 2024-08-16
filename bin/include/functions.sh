3b8efdfea7591b49288f143f380ec94a86a64ccc
Maksim Timonin <timonin.maxim@gmail.com>
2024-08-16 16:01:23 +0300

    IGNITE-23017 Remove jdk8 from build scripts

--- bin/include/functions.sh
+++ bin/include/functions.sh
@@ -66,7 +66,7 @@ checkJava() {
         if [ $RETCODE -ne 0 ]; then
             echo $0", ERROR:"
             echo "JAVA_HOME environment variable is not found."
-            echo "Please point JAVA_HOME variable to location of JDK 1.8 or later."
+            echo "Please point JAVA_HOME variable to location of JDK 11 or later."
             echo "You can also download latest JDK at http://java.com/download"
 
             exit 1
@@ -82,10 +82,10 @@ checkJava() {
     #
     javaMajorVersion "$JAVA"
 
-    if [ $version -lt 8 ]; then
+    if [ $version -lt 11 ]; then
         echo "$0, ERROR:"
         echo "The $version version of JAVA installed in JAVA_HOME=$JAVA_HOME is incompatible."
-        echo "Please point JAVA_HOME variable to installation of JDK 1.8 or later."
+        echo "Please point JAVA_HOME variable to installation of JDK 11 or later."
         echo "You can also download latest JDK at http://java.com/download"
         exit 1
     fi
