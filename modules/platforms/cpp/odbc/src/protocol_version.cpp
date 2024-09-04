e8053bb1c58e0e84af8339c8979ba329e9e6dc7f
Maksim Davydov <MMDavydov@sberbank.ru>
2024-09-04 12:19:33 +0300

    IGNITE-21927 * Protocol Version Typo fix

--- modules/platforms/cpp/odbc/src/protocol_version.cpp
+++ modules/platforms/cpp/odbc/src/protocol_version.cpp
@@ -34,7 +34,7 @@ namespace ignite
         const ProtocolVersion ProtocolVersion::VERSION_2_7_0(2, 7, 0);
         const ProtocolVersion ProtocolVersion::VERSION_2_8_0(2, 8, 0);
         const ProtocolVersion ProtocolVersion::VERSION_2_13_0(2, 13, 0);
-        const ProtocolVersion ProtocolVersion::VERSION_2_13_0(2, 17, 0);
+        const ProtocolVersion ProtocolVersion::VERSION_2_17_0(2, 17, 0);
 
         ProtocolVersion::VersionSet::value_type supportedArray[] = {
             ProtocolVersion::VERSION_2_1_0,
