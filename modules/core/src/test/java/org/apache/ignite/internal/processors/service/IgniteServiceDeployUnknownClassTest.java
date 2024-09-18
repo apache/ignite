7e152e940c7662fb016aa330aba57eda19a86aeb
Nikolay Izhikov <nizhikov@apache.org>
2024-09-18 11:36:22 +0300

    IGNITE-23202 Fix server node fail when nodeFilter class unknown

--- modules/core/src/test/java/org/apache/ignite/internal/processors/service/IgniteServiceDeployUnknownClassTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/service/IgniteServiceDeployUnknownClassTest.java
@@ -40,9 +40,6 @@ public class IgniteServiceDeployUnknownClassTest extends GridCommonAbstractTest 
     /** */
     public static final String NODE_FILTER_CLS_NAME = "org.apache.ignite.tests.p2p.ExcludeNodeFilter";
 
-    /** */
-    private static ClassLoader extClsLdr;
-
     /** {@inheritDoc} */
     @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
         IgniteConfiguration cfg = super.getConfiguration(igniteInstanceName);
@@ -53,42 +50,29 @@ public class IgniteServiceDeployUnknownClassTest extends GridCommonAbstractTest 
         return cfg;
     }
 
-    /** {@inheritDoc} */
-    @Override protected void beforeTest() throws Exception {
-        super.beforeTest();
-
-        extClsLdr = getExternalClassLoader();
-    }
-
-    /** {@inheritDoc} */
-    @Override protected void afterTest() throws Exception {
-        stopAllGrids();
-
-        super.afterTest();
-
-        extClsLdr = null;
-    }
-
     /** @throws Exception If failed. */
     @Test
     public void testFailWhenClassNotFound() throws Exception {
-        IgniteEx srv = startGrid(getConfiguration("server"));
-        IgniteEx cli = startClientGrid(1);
+        ClassLoader extClsLdr = getExternalClassLoader();
 
-        ServiceConfiguration svcCfg = new ServiceConfiguration()
-            .setName("TestDeploymentService")
-            .setService(((Class<Service>)extClsLdr.loadClass(NOOP_SERVICE_CLS_NAME)).getDeclaredConstructor().newInstance())
-            .setNodeFilter(((Class<IgnitePredicate<ClusterNode>>)extClsLdr.loadClass(NODE_FILTER_CLS_NAME))
-                .getConstructor(UUID.class)
-                .newInstance(cli.configuration().getNodeId()))
-            .setTotalCount(1);
+        try (IgniteEx srv = startGrid(getConfiguration("server"));
+             IgniteEx cli = startClientGrid(1)) {
 
-        assertThrowsWithCause(() -> cli.services().deploy(svcCfg), ServiceDeploymentException.class);
+            ServiceConfiguration svcCfg = new ServiceConfiguration()
+                .setName("TestDeploymentService")
+                .setService(((Class<Service>)extClsLdr.loadClass(NOOP_SERVICE_CLS_NAME)).getDeclaredConstructor().newInstance())
+                .setNodeFilter(((Class<IgnitePredicate<ClusterNode>>)extClsLdr.loadClass(NODE_FILTER_CLS_NAME))
+                    .getConstructor(UUID.class)
+                    .newInstance(cli.configuration().getNodeId()))
+                .setTotalCount(1);
 
-        // TODO: https://issues.apache.org/jira/browse/IGNITE-23226
-        // assertTrue(cli.services().serviceDescriptors().isEmpty());
+            assertThrowsWithCause(() -> cli.services().deploy(svcCfg), ServiceDeploymentException.class);
 
-        // Check node alive.
-        srv.createCache(DEFAULT_CACHE_NAME).put(1, 1);
+            // TODO: https://issues.apache.org/jira/browse/IGNITE-23226
+            // assertTrue(cli.services().serviceDescriptors().isEmpty());
+
+            // Check node alive.
+            srv.createCache(DEFAULT_CACHE_NAME).put(1, 1);
+        }
     }
 }
