38893d7657a7d3ff482aa62ea1cd424ecded6168
popov_aleksandr <a.popovprodby@gmail.com>
2024-10-17 14:01:58 +0300

    review fixes

--- modules/core/src/main/java/org/apache/ignite/internal/managers/systemview/walker/SnapshotViewWalker.java
+++ modules/core/src/main/java/org/apache/ignite/internal/managers/systemview/walker/SnapshotViewWalker.java
@@ -36,7 +36,7 @@ public class SnapshotViewWalker implements SystemViewRowAttributeWalker<Snapshot
         v.accept(4, "snapshotRecordSegment", Long.class);
         v.accept(5, "incrementIndex", Integer.class);
         v.accept(6, "type", String.class);
-        v.accept(7, "creationTime", Long.class);
+        v.accept(7, "snapshotTime", Long.class);
     }
 
     /** {@inheritDoc} */
@@ -48,7 +48,7 @@ public class SnapshotViewWalker implements SystemViewRowAttributeWalker<Snapshot
         v.accept(4, "snapshotRecordSegment", Long.class, row.snapshotRecordSegment());
         v.accept(5, "incrementIndex", Integer.class, row.incrementIndex());
         v.accept(6, "type", String.class, row.type());
-        v.accept(7, "creationTime", Long.class, row.creationTime());
+        v.accept(7, "snapshotTime", Long.class, row.creationTime());
     }
 
     /** {@inheritDoc} */
