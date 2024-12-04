8d8ed110bb167a9805b1b09d5ed9644b603b5266
Sergey Korotkov <serge.korotkov@gmail.com>
2024-12-04 17:46:03 +0700

    IGNITE-23815 TEMP remove tests from suite PDS5

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgnitePdsTestSuite5.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgnitePdsTestSuite5.java
@@ -74,58 +74,58 @@ public class IgnitePdsTestSuite5 {
     public static List<Class<?>> suite(Collection<Class> ignoredTests) {
         List<Class<?>> suite = new ArrayList<>();
 
-        // Basic PageMemory tests.
-        GridTestUtils.addTestIfNeeded(suite, PageMemoryNoLoadSelfTest.class, ignoredTests);
-        GridTestUtils.addTestIfNeeded(suite, PageMemoryImplNoLoadTest.class, ignoredTests);
-        GridTestUtils.addTestIfNeeded(suite, PageMemoryNoStoreLeakTest.class, ignoredTests);
-        GridTestUtils.addTestIfNeeded(suite, PageMemoryLazyAllocationTest.class, ignoredTests);
-        GridTestUtils.addTestIfNeeded(suite, PageMemoryLazyAllocationWithPDSTest.class, ignoredTests);
-        GridTestUtils.addTestIfNeeded(suite, IndexStoragePageMemoryImplTest.class, ignoredTests);
-        GridTestUtils.addTestIfNeeded(suite, PageMemoryImplTest.class, ignoredTests);
-        GridTestUtils.addTestIfNeeded(suite, PageIdDistributionTest.class, ignoredTests);
-        GridTestUtils.addTestIfNeeded(suite, TrackingPageIOTest.class, ignoredTests);
-        GridTestUtils.addTestIfNeeded(suite, PageIOFreeSizeTest.class, ignoredTests);
-
-        // BTree tests with store page memory.
-        GridTestUtils.addTestIfNeeded(suite, BPlusTreePageMemoryImplTest.class, ignoredTests);
-        GridTestUtils.addTestIfNeeded(suite, BPlusTreeReuseListPageMemoryImplTest.class, ignoredTests);
-
-        GridTestUtils.addTestIfNeeded(suite, SegmentedRingByteBufferTest.class, ignoredTests);
-
-        // Write throttling
-        GridTestUtils.addTestIfNeeded(suite, PagesWriteThrottleSmokeTest.class, ignoredTests);
-        GridTestUtils.addTestIfNeeded(suite, SpeedBasedThrottleBreakdownTest.class, ignoredTests);
-
-        // Discovery data handling on node join and old cluster abnormal shutdown
-        GridTestUtils.addTestIfNeeded(suite, IgnitePdsDiscoDataHandlingInNewClusterTest.class, ignoredTests);
-
-        // Metrics
-        GridTestUtils.addTestIfNeeded(suite, FillFactorMetricTest.class, ignoredTests);
-        GridTestUtils.addTestIfNeeded(suite, UsedPagesMetricTest.class, ignoredTests);
-        GridTestUtils.addTestIfNeeded(suite, UsedPagesMetricTestPersistence.class, ignoredTests);
-
-        // WAL delta consistency
-        GridTestUtils.addTestIfNeeded(suite, CpTriggeredWalDeltaConsistencyTest.class, ignoredTests);
-        GridTestUtils.addTestIfNeeded(suite, ExplicitWalDeltaConsistencyTest.class, ignoredTests);
-        GridTestUtils.addTestIfNeeded(suite, SysPropWalDeltaConsistencyTest.class, ignoredTests);
-
-        // Binary meta tests.
-        GridTestUtils.addTestIfNeeded(suite, IgnitePdsCacheObjectBinaryProcessorOnDiscoveryTest.class, ignoredTests);
-
-        GridTestUtils.addTestIfNeeded(suite, SegmentAwareTest.class, ignoredTests);
-
-        GridTestUtils.addTestIfNeeded(suite, WalEnableDisableWithNodeShutdownTest.class, ignoredTests);
-        GridTestUtils.addTestIfNeeded(suite, WalEnableDisableWithRestartsTest.class, ignoredTests);
-
-        GridTestUtils.addTestIfNeeded(suite, WalArchiveConsistencyTest.class, ignoredTests);
-
-        GridTestUtils.addTestIfNeeded(suite, RestorePartitionStateTest.class, ignoredTests);
-
-        GridTestUtils.addTestIfNeeded(suite, FileWriteAheadLogManagerSelfTest.class, ignoredTests);
-        GridTestUtils.addTestIfNeeded(suite, IgniteCacheDatabaseSharedManagerSelfTest.class, ignoredTests);
-
-        GridTestUtils.addTestIfNeeded(suite, WalCompactionNotificationsTest.class, ignoredTests);
-        GridTestUtils.addTestIfNeeded(suite, WalPageRecordCompactionTest.class, ignoredTests);
+//        // Basic PageMemory tests.
+//        GridTestUtils.addTestIfNeeded(suite, PageMemoryNoLoadSelfTest.class, ignoredTests);
+//        GridTestUtils.addTestIfNeeded(suite, PageMemoryImplNoLoadTest.class, ignoredTests);
+//        GridTestUtils.addTestIfNeeded(suite, PageMemoryNoStoreLeakTest.class, ignoredTests);
+//        GridTestUtils.addTestIfNeeded(suite, PageMemoryLazyAllocationTest.class, ignoredTests);
+//        GridTestUtils.addTestIfNeeded(suite, PageMemoryLazyAllocationWithPDSTest.class, ignoredTests);
+//        GridTestUtils.addTestIfNeeded(suite, IndexStoragePageMemoryImplTest.class, ignoredTests);
+//        GridTestUtils.addTestIfNeeded(suite, PageMemoryImplTest.class, ignoredTests);
+//        GridTestUtils.addTestIfNeeded(suite, PageIdDistributionTest.class, ignoredTests);
+//        GridTestUtils.addTestIfNeeded(suite, TrackingPageIOTest.class, ignoredTests);
+//        GridTestUtils.addTestIfNeeded(suite, PageIOFreeSizeTest.class, ignoredTests);
+//
+//        // BTree tests with store page memory.
+//        GridTestUtils.addTestIfNeeded(suite, BPlusTreePageMemoryImplTest.class, ignoredTests);
+//        GridTestUtils.addTestIfNeeded(suite, BPlusTreeReuseListPageMemoryImplTest.class, ignoredTests);
+//
+//        GridTestUtils.addTestIfNeeded(suite, SegmentedRingByteBufferTest.class, ignoredTests);
+//
+//        // Write throttling
+//        GridTestUtils.addTestIfNeeded(suite, PagesWriteThrottleSmokeTest.class, ignoredTests);
+//        GridTestUtils.addTestIfNeeded(suite, SpeedBasedThrottleBreakdownTest.class, ignoredTests);
+//
+//        // Discovery data handling on node join and old cluster abnormal shutdown
+//        GridTestUtils.addTestIfNeeded(suite, IgnitePdsDiscoDataHandlingInNewClusterTest.class, ignoredTests);
+//
+//        // Metrics
+//        GridTestUtils.addTestIfNeeded(suite, FillFactorMetricTest.class, ignoredTests);
+//        GridTestUtils.addTestIfNeeded(suite, UsedPagesMetricTest.class, ignoredTests);
+//        GridTestUtils.addTestIfNeeded(suite, UsedPagesMetricTestPersistence.class, ignoredTests);
+//
+//        // WAL delta consistency
+//        GridTestUtils.addTestIfNeeded(suite, CpTriggeredWalDeltaConsistencyTest.class, ignoredTests);
+//        GridTestUtils.addTestIfNeeded(suite, ExplicitWalDeltaConsistencyTest.class, ignoredTests);
+//        GridTestUtils.addTestIfNeeded(suite, SysPropWalDeltaConsistencyTest.class, ignoredTests);
+//
+//        // Binary meta tests.
+//        GridTestUtils.addTestIfNeeded(suite, IgnitePdsCacheObjectBinaryProcessorOnDiscoveryTest.class, ignoredTests);
+//
+//        GridTestUtils.addTestIfNeeded(suite, SegmentAwareTest.class, ignoredTests);
+//
+//        GridTestUtils.addTestIfNeeded(suite, WalEnableDisableWithNodeShutdownTest.class, ignoredTests);
+//        GridTestUtils.addTestIfNeeded(suite, WalEnableDisableWithRestartsTest.class, ignoredTests);
+//
+//        GridTestUtils.addTestIfNeeded(suite, WalArchiveConsistencyTest.class, ignoredTests);
+//
+//        GridTestUtils.addTestIfNeeded(suite, RestorePartitionStateTest.class, ignoredTests);
+//
+//        GridTestUtils.addTestIfNeeded(suite, FileWriteAheadLogManagerSelfTest.class, ignoredTests);
+//        GridTestUtils.addTestIfNeeded(suite, IgniteCacheDatabaseSharedManagerSelfTest.class, ignoredTests);
+//
+//        GridTestUtils.addTestIfNeeded(suite, WalCompactionNotificationsTest.class, ignoredTests);
+//        GridTestUtils.addTestIfNeeded(suite, WalPageRecordCompactionTest.class, ignoredTests);
 
         GridTestUtils.addTestIfNeeded(suite, WalRotatedIdPartRecordTest.class, ignoredTests);
 
