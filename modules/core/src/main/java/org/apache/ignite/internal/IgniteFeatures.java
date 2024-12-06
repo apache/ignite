becfb3050bb93bd568db7e0b5a202ce083311ccf
Nikolay <nizhikov@apache.org>
2024-12-06 12:51:48 +0300

    IGNITE-23840 Remove IgniteFeatures#DIFFERENT_REBALANCE_POOL_SIZE and related code (#11698)

--- modules/core/src/main/java/org/apache/ignite/internal/IgniteFeatures.java
+++ modules/core/src/main/java/org/apache/ignite/internal/IgniteFeatures.java
@@ -44,9 +44,6 @@ public enum IgniteFeatures {
     /** Data paket compression. */
     DATA_PACKET_COMPRESSION(3),
 
-    /** Support of different rebalance size for nodes.  */
-    DIFFERENT_REBALANCE_POOL_SIZE(4),
-
     /** Support of splitted cache configurations to avoid broken deserialization on non-affinity nodes. */
     SPLITTED_CACHE_CONFIGURATIONS(5),
 
