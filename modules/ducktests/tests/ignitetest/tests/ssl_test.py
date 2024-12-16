0b8d40cb9e33ae147ef4b96d9de5df2f2f810d40
Nikita Amelchev <namelchev@apache.org>
2024-12-16 22:03:09 +0300

    IGNITE-24001 Fixed ducktests

--- modules/ducktests/tests/ignitetest/tests/ssl_test.py
+++ modules/ducktests/tests/ignitetest/tests/ssl_test.py
@@ -28,6 +28,9 @@ from ignitetest.utils import ignite_versions, cluster
 from ignitetest.utils.ignite_test import IgniteTest
 from ignitetest.utils.version import IgniteVersion, DEV_BRANCH, LATEST
 
+from tests.ignitetest.services.utils.ssl.client_connector_configuration import ClientConnectorConfiguration
+from tests.ignitetest.utils.version import V_2_16_0
+
 
 class SslTest(IgniteTest):
     """
@@ -47,7 +50,11 @@ class SslTest(IgniteTest):
 
         server_configuration = IgniteConfiguration(
             version=IgniteVersion(ignite_version), ssl_params=server_ssl,
-            connector_configuration=ConnectorConfiguration(ssl_enabled=True, ssl_params=server_ssl))
+            client_connector_configuration=ClientConnectorConfiguration(ssl_enabled=True, ssl_params=server_ssl))
+
+        if ignite_version <= V_2_16_0:
+            server_configuration._replace(
+                connector_configuration=ConnectorConfiguration(ssl_enabled=True, ssl_params=server_ssl))
 
         ignite = IgniteService(self.test_context, server_configuration, num_nodes=2,
                                startup_timeout_sec=180)
@@ -55,7 +62,8 @@ class SslTest(IgniteTest):
         client_configuration = server_configuration._replace(
             client_mode=True,
             ssl_params=SslParams(shared_root, key_store_jks=DEFAULT_CLIENT_KEYSTORE),
-            connector_configuration=None)
+            connector_configuration=None,
+            client_connector_configuration=None)
 
         app = IgniteApplicationService(
             self.test_context,
