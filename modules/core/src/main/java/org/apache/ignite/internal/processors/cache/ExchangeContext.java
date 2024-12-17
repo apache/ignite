08ebc23f4eb2fa29001b900fd9a501747c2f7ddd
Nikolay <nizhikov@apache.org>
2024-12-17 12:43:31 +0300

    IGNITE-23855 Remove IgniteFeatures#PME_FREE_SWITCH and related code (#11706)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/ExchangeContext.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/ExchangeContext.java
@@ -29,8 +29,6 @@ import org.apache.ignite.internal.util.typedef.internal.S;
 import org.jetbrains.annotations.Nullable;
 
 import static org.apache.ignite.IgniteSystemProperties.getBoolean;
-import static org.apache.ignite.internal.IgniteFeatures.PME_FREE_SWITCH;
-import static org.apache.ignite.internal.IgniteFeatures.allNodesSupports;
 import static org.apache.ignite.internal.events.DiscoveryCustomEvent.EVT_DISCOVERY_CUSTOM_EVT;
 import static org.apache.ignite.internal.processors.cache.GridCachePartitionExchangeManager.exchangeProtocolVersion;
 import static org.apache.ignite.internal.processors.cache.persistence.snapshot.IgniteSnapshotManager.isSnapshotOperation;
@@ -85,17 +83,10 @@ public class ExchangeContext {
 
         int protocolVer = exchangeProtocolVersion(fut.firstEventCache().minimumNodeVersion());
 
-        boolean allNodesSupportsPmeFreeSwitch = allNodesSupports(fut.firstEventCache().allNodes(), PME_FREE_SWITCH);
-
-        if (!allNodesSupportsPmeFreeSwitch)
-            log.warning("Current topology does not support the PME-free switch. Please check all nodes support" +
-                " this feature and it was not explicitly disabled by IGNITE_PME_FREE_SWITCH_DISABLED JVM option.");
-
         boolean pmeFreeAvailable = (fut.wasRebalanced() && fut.isBaselineNodeFailed()) || isSnapshotOperation(fut.firstEvent());
 
         if (!compatibilityNode &&
-            pmeFreeAvailable &&
-            allNodesSupportsPmeFreeSwitch) {
+            pmeFreeAvailable) {
             exchangeFreeSwitch = true;
             merge = false;
         }
