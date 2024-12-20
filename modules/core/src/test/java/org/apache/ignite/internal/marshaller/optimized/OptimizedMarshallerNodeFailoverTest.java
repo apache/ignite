b3927d1ab68a5cc9e9610f367fe78b8fe9b5a613
Nikolay Izhikov <nizhikov@apache.org>
2024-12-20 12:18:06 +0300

    IGNITE-15083 WIP

--- modules/core/src/test/java/org/apache/ignite/internal/marshaller/optimized/OptimizedMarshallerNodeFailoverTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/marshaller/optimized/OptimizedMarshallerNodeFailoverTest.java
@@ -50,10 +50,7 @@ public class OptimizedMarshallerNodeFailoverTest extends GridCommonAbstractTest 
     @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
         IgniteConfiguration cfg = super.getConfiguration(igniteInstanceName);
 
-        //cfg.setMarshaller(new OptimizedMarshaller());
-
         cfg.setWorkDirectory(workDir);
-
 
         if (cache) {
             CacheConfiguration ccfg = new CacheConfiguration(DEFAULT_CACHE_NAME);
