873db564aef280030271c2cca1625420a3890821
Nikolay Izhikov <nizhikov@apache.org>
2025-02-03 13:02:08 +0300

    IGNITE-24130 Code review fixes

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/filename/NodeFileTree.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/filename/NodeFileTree.java
@@ -25,7 +25,7 @@ import org.apache.ignite.internal.util.typedef.internal.U;
 
 /**
  * Provides access to Ignite node file tree.
- * Note, that base path can be different for each usage:
+ * Note, that root path can be different for each usage:
  * <ul>
  *     <li>Ignite node.</li>
  *     <li>Snapshot files.</li>
