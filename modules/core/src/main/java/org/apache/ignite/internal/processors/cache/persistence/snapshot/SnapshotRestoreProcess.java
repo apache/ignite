ebd86f1eab55ae20b7b042e6aa3bb825e484e9fe
Nikolay Izhikov <nizhikov@apache.org>
2025-02-25 12:19:45 +0300

    IGNITE-24130 WIP

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/snapshot/SnapshotRestoreProcess.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/snapshot/SnapshotRestoreProcess.java
@@ -758,7 +758,16 @@ public class SnapshotRestoreProcess {
                 if (!F.isEmpty(req.groups()) && !req.groups().contains(grpName))
                     continue;
 
-                File cacheDir = ft.cacheStorage(snpCacheDir.getName());
+                Map<String, StoredCacheData> ccfgs = new HashMap<>();
+
+                locCfgMgr.readCacheGroupCaches(snpCacheDir, ccfgs);
+
+                if (F.isEmpty(ccfgs))
+                    continue;
+
+                cfgsByName.putAll(ccfgs);
+
+                File cacheDir = ft.cacheStorage(F.first(ccfgs.values()).config());
 
                 if (cacheDir.exists()) {
                     if (!cacheDir.isDirectory()) {
@@ -784,8 +793,6 @@ public class SnapshotRestoreProcess {
                     throw new IgniteCheckedException("Unable to restore cache group, temp directory already exists " +
                         "[group=" + grpName + ", dir=" + tmpCacheDir + ']');
                 }
-
-                locCfgMgr.readCacheGroupCaches(snpCacheDir, cfgsByName);
             }
         }
 
