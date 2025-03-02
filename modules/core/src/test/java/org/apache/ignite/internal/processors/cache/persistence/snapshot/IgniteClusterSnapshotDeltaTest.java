ca0957e4820cab68592dc9d56752476ab7f34282
Nikolay Izhikov <nizhikov@apache.org>
2025-03-02 15:06:36 +0300

    IGNITE-24130 WIP

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/snapshot/IgniteClusterSnapshotDeltaTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/persistence/snapshot/IgniteClusterSnapshotDeltaTest.java
@@ -122,7 +122,7 @@ public class IgniteClusterSnapshotDeltaTest extends AbstractSnapshotSelfTest {
         mgr.localSnapshotSenderFactory(sft -> new DelegateSnapshotSender(log,
             mgr.snapshotExecutorService(), old.apply(sft)) {
             @Override public void sendPart0(File from, File to, @Nullable String drName, GroupPartitionId pair, Long length) {
-                if (cacheDir.equals(to.getName()))
+                if (cacheDir.equals(to.getParentFile().getName()))
                     partStart.countDown();
 
                 try {
