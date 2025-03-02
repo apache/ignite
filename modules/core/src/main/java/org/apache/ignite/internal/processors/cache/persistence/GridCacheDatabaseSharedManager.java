837f5e34f96908c267542f12b466758a74f1664d
Nikolay Izhikov <nizhikov@apache.org>
2025-03-02 15:13:44 +0300

    IGNITE-24617 Revert unnecessary changes

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/GridCacheDatabaseSharedManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/GridCacheDatabaseSharedManager.java
@@ -173,7 +173,6 @@ import static org.apache.ignite.IgniteSystemProperties.getBoolean;
 import static org.apache.ignite.IgniteSystemProperties.getInteger;
 import static org.apache.ignite.internal.cluster.DistributedConfigurationUtils.makeUpdateListener;
 import static org.apache.ignite.internal.cluster.DistributedConfigurationUtils.setDefaultValue;
-import static org.apache.ignite.internal.pagemem.PageIdAllocator.INDEX_PARTITION;
 import static org.apache.ignite.internal.pagemem.PageIdUtils.partId;
 import static org.apache.ignite.internal.pagemem.wal.record.WALRecord.RecordType.CHECKPOINT_RECORD;
 import static org.apache.ignite.internal.pagemem.wal.record.WALRecord.RecordType.MASTER_KEY_CHANGE_RECORD;
@@ -717,7 +716,7 @@ public class GridCacheDatabaseSharedManager extends IgniteCacheDatabaseSharedMan
                 for (int partId = 0; partId < partitions; partId++)
                     memEx.invalidate(grpDesc.groupId(), partId);
 
-                memEx.invalidate(grpDesc.groupId(), INDEX_PARTITION);
+                memEx.invalidate(grpDesc.groupId(), PageIdAllocator.INDEX_PARTITION);
             }
 
             if (grpDesc.config().isEncryptionEnabled())
@@ -2476,7 +2475,7 @@ public class GridCacheDatabaseSharedManager extends IgniteCacheDatabaseSharedMan
      * @param partId Partition id.
      */
     private boolean skipRemovedIndexUpdates(int grpId, int partId) {
-        return (partId == INDEX_PARTITION) && !storeMgr.hasIndexStore(grpId);
+        return (partId == PageIdAllocator.INDEX_PARTITION) && !storeMgr.hasIndexStore(grpId);
     }
 
     /**
@@ -2688,7 +2687,7 @@ public class GridCacheDatabaseSharedManager extends IgniteCacheDatabaseSharedMan
                             exec.submit(() -> {
                                 GridCacheContext cacheCtx = cctx.cacheContext(cacheId);
 
-                                if (skipRemovedIndexUpdates(cacheCtx.groupId(), INDEX_PARTITION))
+                                if (skipRemovedIndexUpdates(cacheCtx.groupId(), PageIdAllocator.INDEX_PARTITION))
                                     cctx.kernalContext().query().markAsRebuildNeeded(cacheCtx, true);
 
                                 try {
