7779a063534e3b117a4dc86c6be08172cc479348
Vladimir Steshin <vladsz83@gmail.com>
2025-03-11 14:04:44 +0300

    minor

--- modules/calcite/src/test/java/org/apache/ignite/internal/processors/query/calcite/integration/IndexRebuildIntegrationTest.java
+++ modules/calcite/src/test/java/org/apache/ignite/internal/processors/query/calcite/integration/IndexRebuildIntegrationTest.java
@@ -96,7 +96,7 @@ public class IndexRebuildIntegrationTest extends AbstractBasicIntegrationTest {
 
         executeSql("CREATE TABLE tbl2 (id INT PRIMARY KEY, val VARCHAR)");
 
-        // IgniteJoinsOrderOptimizationRule prefers bigget rel on the left
+        // IgniteJoinsOrderOptimizationRule prefers bigger rel on the left.
         for (int i = 0; i < 1000; i++)
             executeSql("INSERT INTO tbl2 VALUES (?, ?)", i, "val" + i);
     }
