978897e125e1f85ddb5c35865262892028fe4700
Maksim Davydov <70368398+maksaska@users.noreply.github.com>
2025-03-24 11:32:15 +0300

    IGNITE-24260 Remove IgniteDataTransferObject protoVer fields (#11910)

--- modules/core/src/main/java/org/apache/ignite/internal/management/diagnostic/ConnectivityResult.java
+++ modules/core/src/main/java/org/apache/ignite/internal/management/diagnostic/ConnectivityResult.java
@@ -55,7 +55,7 @@ public class ConnectivityResult extends IgniteDataTransferObject {
     }
 
     /** {@inheritDoc} */
-    @Override protected void readExternalData(byte protoVer, ObjectInput in) throws IOException, ClassNotFoundException {
+    @Override protected void readExternalData(ObjectInput in) throws IOException, ClassNotFoundException {
         nodeStatuses = U.readMap(in);
     }
 
--- modules/core/src/main/java/org/apache/ignite/internal/management/meta/MetaRemoveCommandArg.java
+++ modules/core/src/main/java/org/apache/ignite/internal/management/meta/MetaRemoveCommandArg.java
@@ -46,8 +46,8 @@ public class MetaRemoveCommandArg extends MetaDetailsCommandArg {
     }
 
     /** {@inheritDoc} */
-    @Override protected void readExternalData(byte protoVer, ObjectInput in) throws IOException, ClassNotFoundException {
-        super.readExternalData(protoVer, in);
+    @Override protected void readExternalData(ObjectInput in) throws IOException, ClassNotFoundException {
+        super.readExternalData(in);
 
         out = U.readString(in);
     }
