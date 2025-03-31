4fd787e8914ffd736a6e4cb76c89d7720097f380
Nikolay Kuznetsov <nikolai.kuznetsov.nk@gmail.com>
2025-03-31 13:52:05 +0700

    IGNITE-24883 #refactor

--- modules/core/src/main/java/org/apache/ignite/internal/management/api/JmxCommandsProvider.java
+++
@@ -1,9 +0,0 @@
-package org.apache.ignite.internal.management.api;
-
-import java.util.Collection;
-
-/** External commands provider plugin to register in JMX */
-public interface JmxCommandsProvider {
-    /** Gets all supported by this provider commands. */
-    Collection<Command<?, ?>> commands();
-}
