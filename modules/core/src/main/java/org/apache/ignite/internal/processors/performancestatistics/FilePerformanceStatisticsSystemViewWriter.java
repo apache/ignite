642d2ef1f105f64f9f77ca5690a56604a0461311
chesnokoff <chesnokoff239@gmail.com>
2025-04-03 16:07:42 +0300

    IGNITE-24286 Edit filterable views

--- modules/core/src/main/java/org/apache/ignite/internal/processors/performancestatistics/FilePerformanceStatisticsSystemViewWriter.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/performancestatistics/FilePerformanceStatisticsSystemViewWriter.java
@@ -83,9 +83,7 @@ public class FilePerformanceStatisticsSystemViewWriter extends AbstractFilePerfo
             "metrics",
             "caches",
             "sql.queries",
-            "nodes",
-            "statisticsPartitionData",
-            "partitionStates");
+            "nodes");
         sysViewPredicate = view -> !ignoredViews.contains(view.name());
     }
 
