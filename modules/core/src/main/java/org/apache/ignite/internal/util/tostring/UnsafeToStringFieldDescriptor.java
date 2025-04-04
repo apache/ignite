ee73c118c8c0e14b294fbf1c06a2be2113d22f71
Nikolay Izhikov <nizhikov@apache.org>
2025-04-04 12:52:46 +0300

    IGNITE-25012 Extract GridToStringBuilder Unsafe code to interface

---
+++ modules/core/src/main/java/org/apache/ignite/internal/util/tostring/UnsafeToStringFieldDescriptor.java
@@ -0,0 +1,83 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.util.tostring;
+
+import java.lang.reflect.Field;
+import org.apache.ignite.internal.util.GridUnsafe;
+
+/**
+ * Simple field descriptor containing field name and its order in the class descriptor.
+ */
+class UnsafeToStringFieldDescriptor extends GridToStringFieldDescriptor{
+    /**
+     * @param field Field;
+     */
+    UnsafeToStringFieldDescriptor(Field field) {
+        super(field);
+    }
+
+    /** {@inheritDoc} */
+    @Override long offset(Field field) {
+        return GridUnsafe.objectFieldOffset(field);
+    }
+
+    /** {@inheritDoc} */
+    @Override public Object objectValue(Object obj) {
+        return GridUnsafe.getObjectField(obj, offset());
+    }
+
+    /** {@inheritDoc} */
+    @Override public byte byteValue(Object obj) {
+        return GridUnsafe.getByteField(obj, offset());
+    }
+
+    /** {@inheritDoc} */
+    @Override public boolean booleanValue(Object obj) {
+        return GridUnsafe.getBooleanField(obj, offset());
+    }
+
+    /** {@inheritDoc} */
+    @Override public char charValue(Object obj) {
+        return GridUnsafe.getCharField(obj, offset());
+    }
+
+    /** {@inheritDoc} */
+    @Override public short shortValue(Object obj) {
+        return GridUnsafe.getShortField(obj, offset());
+    }
+
+    /** {@inheritDoc} */
+    @Override public int intField(Object obj) {
+        return GridUnsafe.getIntField(obj, offset());
+    }
+
+    /** {@inheritDoc} */
+    @Override public float floatField(Object obj) {
+        return GridUnsafe.getFloatField(obj, offset());
+    }
+
+    /** {@inheritDoc} */
+    @Override public long longField(Object obj) {
+        return GridUnsafe.getLongField(obj, offset());
+    }
+
+    /** {@inheritDoc} */
+    @Override public double doubleField(Object obj) {
+        return GridUnsafe.getDoubleField(obj, offset());
+    }
+}
