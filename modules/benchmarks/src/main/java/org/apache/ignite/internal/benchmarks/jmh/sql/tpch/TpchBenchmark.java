e2ac99e313f8de1436c2d32c7dd1d8527b30d4ce
Vladimir Steshin <vladsz83@gmail.com>
2025-04-14 19:28:47 +0300

    fix bench

--- modules/benchmarks/src/main/java/org/apache/ignite/internal/benchmarks/jmh/sql/tpch/TpchBenchmark.java
+++ modules/benchmarks/src/main/java/org/apache/ignite/internal/benchmarks/jmh/sql/tpch/TpchBenchmark.java
@@ -109,37 +109,36 @@ public class TpchBenchmark {
     private String scale;
 
     /** Query engine. */
-    @Param({"CALCITE", "H2"})
+    @Param({"H2"})
     private String engine;
 
     /**
      * Query id.
      * <p>
-     * The commented queries do not currently work with Calcite even for scale=0.01.
      * The 11, 13, 15 can not be parsed with H2.
      */
     @Param({
         "1",
-//        "2", // TODO: https://issues.apache.org/jira/browse/IGNITE-24731
+        "2",
         "3",
         "4",
-//        "5", // TODO: https://issues.apache.org/jira/browse/IGNITE-24741
+        "5",
         "6",
         "7",
-//        "8", // TODO: https://issues.apache.org/jira/browse/IGNITE-24746
-//        "9", // TODO: https://issues.apache.org/jira/browse/IGNITE-24752
+        "8",
+        "9",
         "10",
-        "11",
+        //"11",
         "12",
-        "13",
+        //"13",
         "14",
-        "15",
-//        "16", // TODO: https://issues.apache.org/jira/browse/IGNITE-24753
+        //"15",
+        "16",
 //        "17", // TODO: https://issues.apache.org/jira/browse/IGNITE-24754
         "18",
-//        "19", // TODO: https://issues.apache.org/jira/browse/IGNITE-24756
-//        "20", // TODO: https://issues.apache.org/jira/browse/IGNITE-24730
-//        "21", // TODO: https://issues.apache.org/jira/browse/IGNITE-24757
+        "19",
+        "20",
+        "21",
         "22"
     })
     private String queryId;
