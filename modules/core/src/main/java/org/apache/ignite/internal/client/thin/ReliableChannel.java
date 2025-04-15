711730e1e906df80fefefb215fc81fe6d042e318
Popov Aleksandr <a.popovprodby@gmail.com>
2025-04-15 17:16:21 +0300

    fix

--- modules/core/src/main/java/org/apache/ignite/internal/client/thin/ReliableChannel.java
+++ modules/core/src/main/java/org/apache/ignite/internal/client/thin/ReliableChannel.java
@@ -418,47 +418,16 @@ final class ReliableChannel implements AutoCloseable {
                 CompletableFuture<T> fut = new CompletableFuture<>();
                 List<ClientConnectionException> failures = new ArrayList<>();
 
-                try {
-                    ClientChannelHolder hld = nodeChannels.get(affNodeId);
-                    if (hld != null) {
-                        ClientChannel channel = hld.getOrCreateChannel();
+                Object result = applyOnNodeChannel(
+                    affNodeId,
+                    channel -> applyOnClientChannelAsync(channel, op, payloadWriter, payloadReader),
+                    failures
+                );
 
-                        channel.serviceAsync(op, payloadWriter, payloadReader)
-                            .whenComplete((res, err) -> {
-                                if (err == null) {
-                                    fut.complete(res);
+                // TODO: retry the channel.
 
-                                    return;
-                                }
-
-                                failures.add((ClientConnectionException)err);
-
-                                onChannelFailure(hld, channel, err, failures);
-
-                                if (shouldRetry(op, 0, (ClientConnectionException)err)) {
-                                    // Retry with the same channel after reconnection
-                                    try {
-                                        ClientChannel newChannel = hld.getOrCreateChannel();
-
-                                        newChannel.serviceAsync(op, payloadWriter, payloadReader)
-                                            .whenComplete((retryRes, retryErr) -> {
-                                                if (retryErr == null)
-                                                    fut.complete(retryRes);
-                                                else
-                                                    handleServiceAsync(fut, op, payloadWriter, payloadReader, failures);
-                                            });
-                                    } catch (Throwable t) {
-                                        handleServiceAsync(fut, op, payloadWriter, payloadReader, failures);
-                                    }
-                                } else
-                                    handleServiceAsync(fut, op, payloadWriter, payloadReader, failures);
-                            });
-
-                        return new IgniteClientFutureImpl<>(fut);
-                    }
-                } catch (ClientConnectionException e) {
-                    failures.add(e);
-                }
+                if (result != null)
+                    return new IgniteClientFutureImpl<>(fut);
             }
         }
 
