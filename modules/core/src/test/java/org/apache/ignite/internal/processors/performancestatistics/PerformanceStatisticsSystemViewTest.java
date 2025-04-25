c6611d1a8e08e3351d965dc4d7595d52ddc545d7
chesnokoff <chesnokoff239@gmail.com>
2025-04-25 08:31:13 +0300

    IGNITE-24286 Rollback PerformanceStatisticsSystemViewTest

--- modules/core/src/test/java/org/apache/ignite/internal/processors/performancestatistics/PerformanceStatisticsSystemViewTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/performancestatistics/PerformanceStatisticsSystemViewTest.java
@@ -23,9 +23,6 @@ import java.util.Set;
 import java.util.UUID;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.internal.IgniteEx;
-import org.apache.ignite.internal.managers.systemview.walker.AtomicReferenceViewWalker;
-import org.apache.ignite.internal.util.lang.gridfunc.PredicateCollectionView;
-import org.apache.ignite.spi.systemview.view.datastructures.AtomicReferenceView;
 import org.apache.ignite.testframework.ListeningTestLogger;
 import org.apache.ignite.testframework.LogListener;
 import org.junit.Test;
@@ -65,28 +62,15 @@ public class PerformanceStatisticsSystemViewTest extends AbstractPerformanceStat
         listeningLog.registerListener(lsnr);
 
         try (IgniteEx igniteEx = startGrid(0)) {
+            startCollectStatistics();
+
             Set<String> viewsExpected = new HashSet<>();
             igniteEx.context().systemView().forEach(view -> {
                 if (view.size() > 0 && !IGNORED_VIEWS.contains(view.name()))
                     viewsExpected.add(view.name());
             });
 
-            String invalidViewName = "invalidView";
-            igniteEx.context().systemView().registerView(
-                invalidViewName,
-                invalidViewName,
-                new AtomicReferenceViewWalker(),
-                new PredicateCollectionView<>(null),
-                AtomicReferenceView::new
-            );
-
-            LogListener warningLsnr = LogListener.matches("Unable to write system view: " + invalidViewName + ".").build();
-            listeningLog.registerListener(warningLsnr);
-
-            startCollectStatistics();
-
             assertTrue("Performance statistics writer did not finish.", waitForCondition(lsnr::check, TIMEOUT));
-            assertTrue("Performance statistics writer did not catch exception.", waitForCondition(warningLsnr::check, TIMEOUT));
 
             Set<String> viewsActual = new HashSet<>();
             stopCollectStatisticsAndRead(new TestHandler() {
