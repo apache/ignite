ba10faee455b1d2143f15b156028f9be981f1c51
Vladislav Novikov <vladnovoren@gmail.com>
2025-05-29 13:48:21 +0300

    IGNITE-25518 Fix

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/snapshot/IgniteSnapshotManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/snapshot/IgniteSnapshotManager.java
@@ -2190,7 +2190,7 @@ public class IgniteSnapshotManager extends GridCacheSharedManagerAdapter
         A.notNullOrEmpty(name, "Snapshot name cannot be null or empty.");
         A.ensure(U.alphanumericUnderscore(name), "Snapshot name must satisfy the following name pattern: a-zA-Z0-9_");
         A.ensure(grpNames == null || !grpNames.isEmpty(), "List of cache group names cannot be empty.");
-        A.ensure(grpNames.size() == new HashSet<>().size(), "Cache group names must be unique");
+        A.ensure(grpNames.size() == new HashSet<>(grpNames).size(), "Cache group names must be unique");
 
         cctx.kernalContext().security().authorize(ADMIN_SNAPSHOT);
 
