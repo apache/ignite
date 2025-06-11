bbfd57fcd8fcb4d5a8a57e88b2952675c92f65f6
Mikhail Petrov <pmgheap.sbt@gmail.com>
2025-06-11 12:30:23 +0300

    IGNITE-25609 wip

--- modules/core/src/main/java/org/apache/ignite/client/ClientAuthorizationException.java
+++ modules/core/src/main/java/org/apache/ignite/client/ClientAuthorizationException.java
@@ -24,29 +24,9 @@ public class ClientAuthorizationException extends ClientException {
     /** Serial version uid. */
     private static final long serialVersionUID = 0L;
 
-    /** Message. */
-    private static final String MSG = "User is not authorized to perform this operation";
-
-    /**
-     * Default constructor.
-     */
-    public ClientAuthorizationException() {
-        this(MSG);
-    }
-
     /** @param msg the detail message. */
     public ClientAuthorizationException(String msg) {
         super(msg);
-    }
-
-    /**
-     * Constructs a new exception with the specified cause and a detail
-     * message of <tt>(cause==null ? null : cause.toString())</tt>.
-     *
-     * @param cause the cause.
-     */
-    public ClientAuthorizationException(Throwable cause) {
-        super(MSG, cause);
     }
 
     /**
