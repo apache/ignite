ad244f2a97751abd9456d78a5ee18e416efaf8e5
Denis Mitin <okreda1@gmail.com>
2025-06-26 13:00:31 +0300

    - added custom application specs - redid the test_std_sql_operators test - CalciteTestingApplication is now running a module test

--- modules/ducktests/tests/ignitetest/tests/calcite_test.py
+++ modules/ducktests/tests/ignitetest/tests/calcite_test.py
@@ -16,48 +16,33 @@
 """
 This module contains tests for calcite engine
 """
-from ignitetest.services.ignite import IgniteService
-from ignitetest.services.ignite_app import IgniteApplicationService
-from ignitetest.services.utils.ignite_configuration import IgniteConfiguration, IgniteThinJdbcConfiguration
-from ignitetest.services.utils.ssl.client_connector_configuration import ClientConnectorConfiguration
-from ignitetest.utils import ignite_versions, cluster
+from ignitetest.services.ignite_app import IgniteCustomApplicationService
+from ignitetest.services.utils.ignite_configuration import CustomApplicationConfiguration
+from ignitetest.utils import cluster, ignite_versions
 from ignitetest.utils.ignite_test import IgniteTest
-from ignitetest.utils.version import DEV_BRANCH, IgniteVersion, LATEST
-from ignitetest.utils.bean import Bean
+from ignitetest.utils.version import DEV_BRANCH, IgniteVersion
 
 
 class CalciteTest(IgniteTest):
     """
     Calcite engine tests
     """
-    @cluster(num_nodes=2)
-    @ignite_versions(str(DEV_BRANCH), str(LATEST))
-    def test_std_sql_operators_via_thin_jdbc(self, ignite_version):
+    @cluster(num_nodes=1)
+    @ignite_versions(str(DEV_BRANCH))
+    def test_std_sql_operators(self, ignite_version):
         """
-        This test validates the proper functioning of SQL operators through the thin JDBC client on the Calcite engine.
+        This test validates that classpath contains all the required libraries that used by calcite engine.
         """
-        config = IgniteConfiguration(
-            version=IgniteVersion(ignite_version),
-            client_connector_configuration=ClientConnectorConfiguration(),
-            sql_configuration=Bean("org.apache.ignite.configuration.SqlConfiguration", query_engines_configuration=[
-                    Bean("org.apache.ignite.calcite.CalciteQueryEngineConfiguration", default=True)]))
 
-        ignite = IgniteService(self.test_context, config, num_nodes=1, modules=["calcite"])
+        modules = ["calcite", "core"]
 
-        ignite.start()
-
-        address = ignite.nodes[0].account.hostname + ":" + str(config.client_connector_configuration.port)
-
-        app = IgniteApplicationService(
+        app = IgniteCustomApplicationService(
             self.test_context,
-            IgniteThinJdbcConfiguration(
-                addresses=[address]
-            ),
-            java_class_name="org.apache.ignite.internal.ducktest.tests.calcite.CalciteTestingApplication",
+            config=CustomApplicationConfiguration(version=IgniteVersion(ignite_version)),
+            modules=modules,
+            main_java_class="org.apache.ignite.internal.ducktest.tests.calcite.CalciteTestingApplication",
             num_nodes=1)
 
         app.start()
 
         app.await_stopped()
-
-        ignite.stop()
