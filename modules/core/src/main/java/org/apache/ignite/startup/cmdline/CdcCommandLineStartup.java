1aef1dd5d174250066f390e9684bb29ed2ee1742
Maksim Davydov <davydov.mm17@physics.msu.ru>
2025-07-11 17:01:45 +0300

    IGNITE-25871 Fix CdcCommandLineStartup interruption guarantee

--- modules/core/src/main/java/org/apache/ignite/startup/cmdline/CdcCommandLineStartup.java
+++ modules/core/src/main/java/org/apache/ignite/startup/cmdline/CdcCommandLineStartup.java
@@ -99,6 +99,12 @@ public class CdcCommandLineStartup {
 
                 appThread.interrupt();
 
+                try {
+                    appThread.join();
+                } catch (InterruptedException ie) {
+                    // No-op.
+                }
+
                 Thread.currentThread().interrupt();
             }
         }
