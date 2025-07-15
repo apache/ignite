cdd61912b98265d5fb90c14b3bbbc4d39bb183b5
Sergey Chugunov <sergey.chugunov@gmail.com>
2025-07-15 17:08:16 +0300

    IGNITE-25641 Refactor test

--- modules/core/src/test/java/org/apache/ignite/cache/store/CacheStoreWithIgniteTxFailureTest.java
+++ modules/core/src/test/java/org/apache/ignite/cache/store/CacheStoreWithIgniteTxFailureTest.java
@@ -17,10 +17,10 @@
 
 package org.apache.ignite.cache.store;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.IntFunction;
 import javax.cache.Cache;
 import org.apache.ignite.Ignite;
@@ -28,21 +28,24 @@ import org.apache.ignite.IgniteCache;
 import org.apache.ignite.IgniteException;
 import org.apache.ignite.cache.CacheInterceptor;
 import org.apache.ignite.cache.CacheInterceptorAdapter;
-import org.apache.ignite.cache.CacheMode;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.NearCacheConfiguration;
 import org.apache.ignite.failure.FailureHandler;
 import org.apache.ignite.failure.StopNodeFailureHandler;
 import org.apache.ignite.internal.IgniteEx;
 import org.apache.ignite.internal.processors.cache.GridCacheAbstractSelfTest;
+import org.apache.ignite.internal.util.typedef.G;
 import org.apache.ignite.transactions.Transaction;
 import org.jetbrains.annotations.Nullable;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 /**
  * Tests to check scenarios with system failures during transaction commit. Internal system failures are modelled with
  * {@link CacheInterceptor} custom implementation throwing an exception during final commit phase.
  */
+@RunWith(Parameterized.class)
 public class CacheStoreWithIgniteTxFailureTest extends GridCacheAbstractSelfTest {
     /** */
     private static final int GRID_COUNT = 3;
@@ -51,19 +54,59 @@ public class CacheStoreWithIgniteTxFailureTest extends GridCacheAbstractSelfTest
     private static final int KEYS_NUMBER = 50;
 
     /** */
-    private static final int FAILING_NODE_IDX = 1;
+    private static final int FAULTY_NODE_IDX = 1;
 
     /** */
     private static final IntFunction<Integer> KEY_UPDATE_FUNCTION = key -> key + KEYS_NUMBER * 3;
 
     /** */
+    private enum FaultyNodeType {
+        /** */
+        PRIMARY,
+        /** */
+        BACKUP
+    }
+
+    /** */
+    private enum FaultyNodeRole {
+        /** */
+        REGULAR,
+        /** */
+        TX_COORDINATOR
+    }
+
+    /** */
+    @Parameterized.Parameter
+    public FaultyNodeType faultyNodeType;
+
+    /** */
+    @Parameterized.Parameter(1)
+    public FaultyNodeRole faultyNodeRole;
+
+    /** */
+    @Parameterized.Parameter(2)
+    public boolean withFaulireHandler;
+
+    /** */
     private volatile FailureHandler failureHandler;
 
     /** */
-    private volatile CacheMode cacheMode;
+    @Parameterized.Parameters(name = "faultyNodeType={0}, faultyNodeRole={1}, withFaulireHandler={2}")
+    public static List<Object[]> parameters() {
+        List<Object[]> params = new ArrayList<>();
 
-    /** */
-    private volatile boolean txCoordinator;
+        params.add(new Object[] {FaultyNodeType.PRIMARY, FaultyNodeRole.REGULAR, true});
+        params.add(new Object[] {FaultyNodeType.PRIMARY, FaultyNodeRole.REGULAR, false});
+        params.add(new Object[] {FaultyNodeType.BACKUP, FaultyNodeRole.REGULAR, true});
+        params.add(new Object[] {FaultyNodeType.BACKUP, FaultyNodeRole.REGULAR, false});
+
+        params.add(new Object[] {FaultyNodeType.PRIMARY, FaultyNodeRole.TX_COORDINATOR, false});
+        params.add(new Object[] {FaultyNodeType.BACKUP, FaultyNodeRole.TX_COORDINATOR, false});
+        params.add(new Object[] {FaultyNodeType.PRIMARY, FaultyNodeRole.TX_COORDINATOR, true});
+        params.add(new Object[] {FaultyNodeType.BACKUP, FaultyNodeRole.TX_COORDINATOR, true});
+
+        return params;
+    }
 
     /** {@inheritDoc} */
     @Override protected void beforeTestsStarted() throws Exception {
@@ -81,8 +124,6 @@ public class CacheStoreWithIgniteTxFailureTest extends GridCacheAbstractSelfTest
 
         storeStgy.resetStore();
         failureHandler = null;
-        cacheMode = null;
-        txCoordinator = false;
     }
 
     /** {@inheritDoc} */
@@ -101,11 +142,6 @@ public class CacheStoreWithIgniteTxFailureTest extends GridCacheAbstractSelfTest
     }
 
     /** {@inheritDoc} */
-    @Override protected CacheMode cacheMode() {
-        return cacheMode != null ? cacheMode : super.cacheMode();
-    }
-
-    /** {@inheritDoc} */
     @Override protected NearCacheConfiguration nearConfiguration() {
         return null;
     }
@@ -114,240 +150,69 @@ public class CacheStoreWithIgniteTxFailureTest extends GridCacheAbstractSelfTest
     @Override protected CacheConfiguration cacheConfiguration(String igniteInstanceName) throws Exception {
         CacheConfiguration ccfg = super.cacheConfiguration(igniteInstanceName);
 
-        ccfg.setInterceptor(new FailingCacheInterceptor(igniteInstanceName, FAILING_NODE_IDX, txCoordinator));
+        ccfg.setInterceptor(new FaultyNodeInterceptor(igniteInstanceName, faultyNodeRole));
 
         return ccfg;
     }
 
     /**
-     * @throws Exception If failed.
+     *
      */
     @Test
-    public void testExceptionOnBackupAfterCommitToStoreWithFailureHandler() throws Exception {
-        failureHandler = new StopNodeFailureHandler();
+    public void testSystemExceptionAfterCacheStoreCommit() throws Exception {
+        failureHandler = withFaulireHandler ? new StopNodeFailureHandler() : null;
 
-        IgniteEx ig = startGrids(gridCount());
-        IgniteCache<Integer, Integer> cache = ig.cache(DEFAULT_CACHE_NAME);
-
-        fillCache(cache, KEYS_NUMBER);
-
-        List<Integer> keysOnBackup = findKeys(grid(FAILING_NODE_IDX).localNode(), cache, 5, 0, 1);
-
-        updateKeysInTx(startClientGrid(), keysOnBackup, KEY_UPDATE_FUNCTION);
-
-        waitForTopology(3);
-
-        checkKeysOnUnaffectedNodes(keysOnBackup);
+        doTest();
     }
 
     /**
      * @throws Exception If failed.
      */
-    @Test
-    public void testExceptionOnBackupAfterCommitToStore() throws Exception {
+    private void doTest() throws Exception {
         IgniteEx ig = startGrids(gridCount());
         IgniteCache<Integer, Integer> cache = ig.cache(DEFAULT_CACHE_NAME);
 
         fillCache(cache, KEYS_NUMBER);
 
-        List<Integer> keysOnBackup = findKeys(grid(FAILING_NODE_IDX).localNode(), cache, 5, 0, 1);
+        int keysType = faultyNodeType == FaultyNodeType.PRIMARY ? 0 : 1;
 
-        updateKeysInTx(startClientGrid(), keysOnBackup, KEY_UPDATE_FUNCTION);
+        List<Integer> keysOnFaultyNode = findKeys(grid(FAULTY_NODE_IDX).localNode(), cache, 5, 0, keysType);
 
-        checkKeysOnAffectedNode(keysOnBackup);
-        checkKeysOnUnaffectedNodes(keysOnBackup);
-    }
+        IgniteEx txCoordinator = faultyNodeRole == FaultyNodeRole.TX_COORDINATOR ? grid(FAULTY_NODE_IDX) : startClientGrid();
+        if (faultyNodeType == FaultyNodeType.PRIMARY)
+            updateKeysInTxWithExceptionCatching(txCoordinator, keysOnFaultyNode);
+        else
+            updateKeysInTx(txCoordinator, keysOnFaultyNode);
 
-    /**
-     * @throws Exception If failed.
-     */
-    @Test
-    public void testExceptionOnBackupAfterCommitToStoreOnReplicatedCache() throws Exception {
-        cacheMode = CacheMode.REPLICATED;
+        if (withFaulireHandler) { // FH doesn't fail tx coordinator
+            if (faultyNodeRole == FaultyNodeRole.TX_COORDINATOR) {
+                if (faultyNodeType == FaultyNodeType.BACKUP) {
+                    waitForTopology(2); // two servers - tx coordinator hosting backup partition fails
+                }
+                else {
+                    waitForTopology(3); // three servers - tx coordinator hosting primary partition doesn't fail
 
-        IgniteEx ig = startGrids(gridCount());
-        IgniteCache<Integer, Integer> cache = ig.cache(DEFAULT_CACHE_NAME);
-
-        fillCache(cache, KEYS_NUMBER);
-
-        List<Integer> keysOnBackup = findKeys(grid(FAILING_NODE_IDX).localNode(), cache, 5, 0, 1);
-
-        updateKeysInTx(startClientGrid(), keysOnBackup, KEY_UPDATE_FUNCTION);
-
-        checkKeysOnAffectedNode(keysOnBackup);
-        checkKeysOnUnaffectedNodes(keysOnBackup);
-    }
-
-    /**
-     * @throws Exception If failed.
-     */
-    @Test
-    public void testExceptionOnBackupAfterCommitToStoreOnReplicatedCacheWithFailureHandler() throws Exception {
-        cacheMode = CacheMode.REPLICATED;
-        failureHandler = new StopNodeFailureHandler();
-
-        IgniteEx ig = startGrids(gridCount());
-        IgniteCache<Integer, Integer> cache = ig.cache(DEFAULT_CACHE_NAME);
-
-        fillCache(cache, KEYS_NUMBER);
-
-        List<Integer> keysOnBackup = findKeys(grid(FAILING_NODE_IDX).localNode(), cache, 5, 0, 1);
-
-        updateKeysInTx(startClientGrid(), keysOnBackup, KEY_UPDATE_FUNCTION);
-
-        waitForTopology(3);
-
-        checkKeysOnUnaffectedNodes(keysOnBackup);
-    }
-
-    /**
-     * @throws Exception If failed.
-     */
-    @Test
-    public void testExceptionOnPrimaryAfterCommitToStore() throws Exception {
-        IgniteEx ig = startGrids(gridCount());
-        IgniteCache<Integer, Integer> cache = ig.cache(DEFAULT_CACHE_NAME);
-
-        fillCache(cache, KEYS_NUMBER);
-
-        List<Integer> keysOnPrimary = findKeys(grid(FAILING_NODE_IDX).localNode(), cache, 5, 0, 0);
-
-        IgniteEx clientNode = startClientGrid();
-
-        try {
-            updateKeysInTx(clientNode, keysOnPrimary, KEY_UPDATE_FUNCTION);
-        }
-        catch (Exception ignored) {
-            // Ignored
-        }
-
-        checkKeysOnAffectedNode(keysOnPrimary);
-        checkKeysOnUnaffectedNodes(keysOnPrimary);
-    }
-
-    /**
-     * @throws Exception If failed.
-     */
-    @Test
-    public void testExceptionOnPrimaryAfterCommitToStoreWithFailureHandler() throws Exception {
-        failureHandler = new StopNodeFailureHandler();
-
-        IgniteEx ig = startGrids(gridCount());
-        IgniteCache<Integer, Integer> cache = ig.cache(DEFAULT_CACHE_NAME);
-
-        fillCache(cache, KEYS_NUMBER);
-
-        List<Integer> keysOnPrimary = findKeys(grid(FAILING_NODE_IDX).localNode(), cache, 5, 0, 0);
-
-        try {
-            updateKeysInTx(startClientGrid(), keysOnPrimary, KEY_UPDATE_FUNCTION);
-        }
-        catch (Exception ignored) {
-            // Ignored
-        }
-
-        waitForTopology(3);
-        checkKeysOnUnaffectedNodes(keysOnPrimary);
-    }
-
-    /**
-     * @throws Exception If failed.
-     */
-    @Test
-    public void testExceptionOnPrimaryTxCoordinatorAfterCommitToStore() throws Exception {
-        txCoordinator = true;
-
-        IgniteEx ig = startGrids(gridCount());
-        IgniteCache<Integer, Integer> cache = ig.cache(DEFAULT_CACHE_NAME);
-
-        fillCache(cache, KEYS_NUMBER);
-
-        List<Integer> keysOnPrimary = findKeys(grid(FAILING_NODE_IDX).localNode(), cache, 5, 0, 0);
-
-        try {
-            updateKeysInTx(grid(FAILING_NODE_IDX), keysOnPrimary, KEY_UPDATE_FUNCTION);
-        }
-        catch (Exception ignored) {
-            // Ignored
-        }
-
-        checkKeysOnAffectedNode(keysOnPrimary); // This should pass as values should be thrown away when tx commit fails
-        checkKeysOnUnaffectedNodes(keysOnPrimary);
-    }
-
-    /**
-     * @throws Exception If failed.
-     */
-    @Test
-    public void testExceptionOnBackupTxCoordinatorAfterCommitToStoreWithFailureHandler() throws Exception {
-        failureHandler = new StopNodeFailureHandler();
-        txCoordinator = true;
-
-        IgniteEx ig = startGrids(gridCount());
-        IgniteCache<Integer, Integer> cache = ig.cache(DEFAULT_CACHE_NAME);
-
-        fillCache(cache, KEYS_NUMBER);
-
-        List<Integer> keysOnBackup = findKeys(grid(FAILING_NODE_IDX).localNode(), cache, 5, 0, 1);
-
-        try {
-            updateKeysInTx(grid(FAILING_NODE_IDX), keysOnBackup, KEY_UPDATE_FUNCTION);
-        }
-        catch (Exception ignored) {
-            // Ignored
-        }
-
-        waitForTopology(2); // this should pass as tx coordinator node should fail after error on tx commit
-        checkKeysOnUnaffectedNodes(keysOnBackup);
-    }
-
-    /**
-     * @throws Exception If failed.
-     */
-    @Test
-    public void testExceptionOnPrimaryTxCoordinatorAfterCommitToStoreWithFailureHandler() throws Exception {
-        txCoordinator = true;
-        failureHandler = new StopNodeFailureHandler();
-
-        IgniteEx ig = startGrids(gridCount());
-        IgniteCache<Integer, Integer> cache = ig.cache(DEFAULT_CACHE_NAME);
-
-        fillCache(cache, KEYS_NUMBER);
-
-        List<Integer> keysOnPrimary = findKeys(grid(FAILING_NODE_IDX).localNode(), cache, 5, 0, 0);
-
-        try {
-            updateKeysInTx(grid(FAILING_NODE_IDX), keysOnPrimary, KEY_UPDATE_FUNCTION);
-        }
-        catch (Exception ignored) {
-            // Ignored
-        }
-
-        checkKeysOnAffectedNode(keysOnPrimary);
-        checkKeysOnUnaffectedNodes(keysOnPrimary);
-    }
-
-    /** */
-    private void checkKeysOnAffectedNode(List<Integer> keysToCheck) {
-        IgniteCache<Object, Object> cache = grid(FAILING_NODE_IDX).cache(DEFAULT_CACHE_NAME);
-
-        for (Integer key : keysToCheck) {
-            assertEquals(storeStgy.getFromStore(key), cache.get(key));
-        }
-    }
-
-    /** */
-    private void checkKeysOnUnaffectedNodes(List<Integer> keysToCheck) {
-        for (int i = 0; i < gridCount(); i++) {
-            if (i != FAILING_NODE_IDX) {
-                IgniteCache<Object, Object> cache = grid(i).cache(DEFAULT_CACHE_NAME);
-
-                for (Integer key : keysToCheck) {
-                    Object val = cache.get(key);
-                    assertEquals(storeStgy.getFromStore(key), val);
+                    // check on affected node should pass as the node is alive and cache entries are cleaned up from the cache
+                    checkKeysOnAffectedNode(keysOnFaultyNode);
                 }
             }
+            else {
+                // two servers and a client node which is a tx coordinator
+                waitForTopology(3);
+
+                assertTrue("Client node should survive test scenario",
+                    G.allGrids()
+                        .stream()
+                        .filter(ignite -> ((IgniteEx)ignite).context().clientNode())
+                        .count() == 1);
+            }
+
         }
+        else {
+            checkKeysOnAffectedNode(keysOnFaultyNode);
+        }
+
+        checkKeysOnUnaffectedNodes(keysOnFaultyNode);
     }
 
     /** */
@@ -358,12 +223,37 @@ public class CacheStoreWithIgniteTxFailureTest extends GridCacheAbstractSelfTest
     }
 
     /** */
-    private void updateKeysInTx(Ignite ig, List<Integer> keys, IntFunction<Integer> keyUpdateFunction) {
+    private void checkKeysOnAffectedNode(List<Integer> keysToCheck) {
+        IgniteCache<Object, Object> cache = grid(FAULTY_NODE_IDX).cache(DEFAULT_CACHE_NAME);
+
+        for (Integer key : keysToCheck) {
+            assertEquals(storeStgy.getFromStore(key), cache.get(key));
+        }
+    }
+
+    /** */
+    private void checkKeysOnUnaffectedNodes(List<Integer> keysToCheck) {
+        for (int i = 0; i < gridCount(); i++) {
+            if (i != FAULTY_NODE_IDX) {
+                IgniteEx ig = grid(i);
+
+                IgniteCache<Object, Object> cache = ig.cache(DEFAULT_CACHE_NAME);
+
+                for (Integer key : keysToCheck) {
+                    Object val = cache.get(key);
+                    assertEquals("Wrong key observed on node " + i + "; nodeName " + ig.name(), storeStgy.getFromStore(key), val);
+                }
+            }
+        }
+    }
+
+    /** */
+    private void updateKeysInTx(Ignite ig, List<Integer> keys) {
         IgniteCache<Object, Object> cache = ig.cache(DEFAULT_CACHE_NAME);
 
         try (Transaction tx = ig.transactions().txStart()) {
             for (Integer key : keys) {
-                cache.put(key, keyUpdateFunction.apply(key));
+                cache.put(key, KEY_UPDATE_FUNCTION.apply(key));
             }
 
             tx.commit();
@@ -371,28 +261,36 @@ public class CacheStoreWithIgniteTxFailureTest extends GridCacheAbstractSelfTest
     }
 
     /** */
-    private static class FailingCacheInterceptor extends CacheInterceptorAdapter<Integer, Integer> {
+    private void updateKeysInTxWithExceptionCatching(Ignite ig, List<Integer> keys) {
+        try {
+            updateKeysInTx(ig, keys);
+        }
+        catch (Exception ignored) {
+            // No-op
+        }
+    }
+
+    /** */
+    private static class FaultyNodeInterceptor extends CacheInterceptorAdapter<Integer, Integer> {
+        /** */
+        private static final String FAULTY_NODE_SUFFIX = "Test" + FAULTY_NODE_IDX;
+
         /** */
         private final String instanceName;
 
         /** */
-        private final String targetNodeSuffix;
+        private final FaultyNodeRole faultyNodeRole;
 
         /** */
-        private final boolean txCoordinator;
-
-        /** */
-        private final Map<Integer, AtomicInteger> map = new ConcurrentHashMap<>();
+        private final Map<Integer, Boolean> map = new ConcurrentHashMap<>();
 
         /**
          * @param instanceName Ignite node instance name.
-         * @param nodeIdx Ignite node index.
-         * @param txCoordinator Flag if node is tx coordinator.
+         * @param faultyNodeRole Flag if node is tx coordinator.
          */
-        private FailingCacheInterceptor(String instanceName, int nodeIdx, boolean txCoordinator) {
+        private FaultyNodeInterceptor(String instanceName, FaultyNodeRole faultyNodeRole) {
             this.instanceName = instanceName;
-            targetNodeSuffix = "Test" + nodeIdx;
-            this.txCoordinator = txCoordinator;
+            this.faultyNodeRole = faultyNodeRole;
         }
 
         /** {@inheritDoc} */
@@ -400,14 +298,11 @@ public class CacheStoreWithIgniteTxFailureTest extends GridCacheAbstractSelfTest
             if (newVal < 2 * KEYS_NUMBER)
                 return newVal;
 
-            if (instanceName.contains(targetNodeSuffix)) {
-                if (txCoordinator) {
+            if (instanceName.contains(FAULTY_NODE_SUFFIX)) {
+                if (faultyNodeRole == FaultyNodeRole.TX_COORDINATOR) {
                     if (!map.containsKey(newVal))
-                        map.put(newVal, new AtomicInteger(0));
+                        map.put(newVal, true);
                     else
-                        map.get(newVal).incrementAndGet();
-
-                    if (map.get(newVal).get() == 1)
                         throw new IgniteException("IgniteException from onBeforePut on tx coordinator: " + instanceName);
                 }
                 else
