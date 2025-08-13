ab3f0985a5b22c65220e84537dbff27293aa1f91
Maksim Davydov <davydov.mm17@physics.msu.ru>
2025-08-13 12:07:41 +0300

    IGNITE-25963 Use MessageSerializer for GridCacheMessage

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheMessage.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheMessage.java
@@ -665,7 +665,8 @@ public abstract class GridCacheMessage implements Message {
     }
 
     /** {@inheritDoc} */
-    @Override public boolean writeTo(ByteBuffer buf, MessageWriter writer) { // TODO: Safe to remove only after all inheritors have migrated to the new ser/der scheme (IGNITE-25490).
+    @Override public boolean writeTo(ByteBuffer buf, MessageWriter writer) {
+        // TODO: Safe to remove only after all inheritors have migrated to the new ser/der scheme (IGNITE-25490).
         writer.setBuffer(buf);
 
         if (!writer.isHeaderWritten()) {
@@ -700,7 +701,8 @@ public abstract class GridCacheMessage implements Message {
     }
 
     /** {@inheritDoc} */
-    @Override public boolean readFrom(ByteBuffer buf, MessageReader reader) { // TODO: Safe to remove only after all inheritors have migrated to the new ser/der scheme (IGNITE-25490).
+    @Override public boolean readFrom(ByteBuffer buf, MessageReader reader) {
+        // TODO: Safe to remove only after all inheritors have migrated to the new ser/der scheme (IGNITE-25490).
         reader.setBuffer(buf);
 
         switch (reader.state()) {
