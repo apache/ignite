ebf10053a610743fbc1202baddfff392362764c4
Aleksandr Nikolaev <nao-it@ya.ru>
2025-08-15 19:20:24 +0300

    [MINOR] fix rebase

--- modules/compatibility/src/test/java/org/apache/ignite/compatibility/testframework/junits/IgniteCompatibilityAbstractTest.java
+++ modules/compatibility/src/test/java/org/apache/ignite/compatibility/testframework/junits/IgniteCompatibilityAbstractTest.java
@@ -288,7 +288,7 @@ public abstract class IgniteCompatibilityAbstractTest extends GridCommonAbstract
         excluded.add("indexing");
 
         // Exclude all modules necessary to compile the current version of core module to avoid conflicts with previous versions.
-        excluded.addAll(Set.of("binary", "codegen2", "commons", "unsafe"));
+        excluded.addAll(Set.of("binary", "commons", "unsafe"));
 
         return excluded;
     }
