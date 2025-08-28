45a0096f98adb5640e0e7166a8f9a46564c7e311
Maksim Davydov <davydov.mm17@physics.msu.ru>
2025-08-28 14:46:34 +0300

    IGNITE-25968 Use MessageSerializer for FragmentDescription

--- modules/codegen2/src/main/java/org/apache/ignite/internal/MessageSerializerGenerator.java
+++ modules/codegen2/src/main/java/org/apache/ignite/internal/MessageSerializerGenerator.java
@@ -29,7 +29,6 @@ import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
