999caccac43cf0f3578463b42e0b7dc084a37fc7
Maksim Timonin <timonin.maxim@gmail.com>
2025-09-03 22:36:47 +0300

    IGNITE-24379 Add github-ci check for calcite deps in release

---
+++ scripts/check-calcite-dependencies.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# Checks wheter SQL Calcite operators can be executed and no missed dependencies.
+#
+
+M2_HOME=~/.m2/repository
+
+JUNIT=$M2_HOME/junit/junit/4.13.2/junit-4.13.2.jar
+HAMCREST=$M2_HOME/org/hamcrest/hamcrest/2.2/hamcrest-2.2.jar
+
+export IGNITE_HOME=$PWD/target/release-package-apache-ignite
+
+USER_LIBS=""
+
+for lib in ignite-log4j2 ignite-calcite; do
+    USER_LIBS="$USER_LIBS:$IGNITE_HOME/libs/optional/$lib/*"
+done
+
+# Add *-tests.jar to classpath to include the test to run.
+for testlib in core calcite; do
+    USER_LIBS="$USER_LIBS:$PWD/modules/$testlib/target/*"
+done
+
+MAIN_CLASS=org.junit.runner.JUnitCore \
+  USER_LIBS=$USER_LIBS:$JUNIT:$HAMCREST \
+  $IGNITE_HOME/bin/ignite.sh org.apache.ignite.internal.processors.query.calcite.integration.StdSqlOperatorsTest
