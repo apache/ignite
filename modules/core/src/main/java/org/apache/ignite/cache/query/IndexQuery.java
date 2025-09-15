98745f54a7f949a63b47f3efe9aff235b84cc5d3
Коротков Михаил Максимович <Korotkov.M.M@sbertech.ru>
2025-09-15 14:17:04 +0300

    IGNITE-26266: Mark IndexQuery#getCriteria with @Nullable, move the annotation to the method line

--- modules/core/src/main/java/org/apache/ignite/cache/query/IndexQuery.java
+++ modules/core/src/main/java/org/apache/ignite/cache/query/IndexQuery.java
@@ -138,8 +138,7 @@ public final class IndexQuery<K, V> extends Query<Cache.Entry<K, V>> {
      *
      * @return List of criteria for this index query.
      */
-    @Nullable
-    public List<IndexQueryCriterion> getCriteria() {
+    @Nullable public List<IndexQueryCriterion> getCriteria() {
         return criteria;
     }
 
