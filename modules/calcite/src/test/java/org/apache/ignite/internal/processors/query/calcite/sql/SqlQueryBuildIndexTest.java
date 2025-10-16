ecb3157cfd84ac64b041a563289aba9abc84b7dc
Vsevolod Kovalev <mcashesha@mail.ru>
2025-10-16 17:18:26 +0700

    Add test for building index via SQL

--- modules/calcite/src/test/java/org/apache/ignite/internal/processors/query/calcite/sql/SqlQueryBuildIndexTest.java
+++ modules/calcite/src/test/java/org/apache/ignite/internal/processors/query/calcite/sql/SqlQueryBuildIndexTest.java
@@ -1,30 +1,15 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.apache.ignite.cache.query;
+package org.apache.ignite.internal.processors.query.calcite.sql;
 
 import java.util.LinkedHashMap;
 import java.util.concurrent.CountDownLatch;
+
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.IgniteDataStreamer;
 import org.apache.ignite.IgniteException;
 import org.apache.ignite.cache.CacheAtomicityMode;
 import org.apache.ignite.cache.QueryEntity;
+import org.apache.ignite.cache.query.SqlFieldsQuery;
 import org.apache.ignite.cluster.ClusterState;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.DataRegionConfiguration;
@@ -40,11 +25,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-import static org.apache.ignite.cache.query.IndexQueryCriteriaBuilder.between;
-
 /** */
 @RunWith(Parameterized.class)
-public class IndexQueryBuildIndexTest extends GridCommonAbstractTest {
+public class SqlQueryBuildIndexTest extends GridCommonAbstractTest {
     /** */
     private static final String CACHE = "TEST_CACHE";
 
@@ -67,22 +50,27 @@ public class IndexQueryBuildIndexTest extends GridCommonAbstractTest {
     /** */
     @Parameterized.Parameters(name = "qryNode={0}")
     public static Object[] parameters() {
-        return new Object[] { "CRD", "CLN" };
+        return new Object[] {"CRD", "CLN"};
     }
 
-    /** {@inheritDoc} */
+    /**
+     * {@inheritDoc}
+     */
     @Override protected void beforeTest() throws Exception {
         cleanPersistenceDir();
     }
 
-    /** {@inheritDoc} */
+    /**
+     * {@inheritDoc}
+     */
     @Override protected void afterTest() throws Exception {
         stopAllGrids();
-
         cleanPersistenceDir();
     }
 
-    /** {@inheritDoc} */
+    /**
+     * {@inheritDoc}
+     */
     @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
         IgniteConfiguration cfg = super.getConfiguration(igniteInstanceName);
 
@@ -90,9 +78,7 @@ public class IndexQueryBuildIndexTest extends GridCommonAbstractTest {
             .setTableName(TBL)
             .setKeyFieldName("id")
             .setValueFieldName("fld")
-            .setFields(new LinkedHashMap<>(
-                F.asMap("id", Long.class.getName(), "fld", Integer.class.getName()))
-            );
+            .setFields(new LinkedHashMap<>(F.asMap("id", Long.class.getName(), "fld", Integer.class.getName())));
 
         CacheConfiguration<Long, Integer> ccfg = new CacheConfiguration<Long, Integer>()
             .setName(CACHE)
@@ -101,8 +87,9 @@ public class IndexQueryBuildIndexTest extends GridCommonAbstractTest {
             .setBackups(1);
 
         cfg.setDataStorageConfiguration(
-            new DataStorageConfiguration().setDefaultDataRegionConfiguration(
-                new DataRegionConfiguration().setPersistenceEnabled(persistenceEnabled)));
+            new DataStorageConfiguration()
+                .setDefaultDataRegionConfiguration(new DataRegionConfiguration().setPersistenceEnabled(persistenceEnabled))
+        );
 
         cfg.setCacheConfiguration(ccfg);
 
@@ -124,11 +111,11 @@ public class IndexQueryBuildIndexTest extends GridCommonAbstractTest {
 
         insertData();
 
-        CountDownLatch idxBuild = new CountDownLatch(1);
+        CountDownLatch idxBuildGate = new CountDownLatch(1);
 
         grid(0).context().pools().buildIndexExecutorService().execute(() -> {
             try {
-                idxBuild.await();
+                idxBuildGate.await();
             }
             catch (InterruptedException e) {
                 throw new RuntimeException(e);
@@ -136,45 +123,42 @@ public class IndexQueryBuildIndexTest extends GridCommonAbstractTest {
         });
 
         multithreadedAsync(() -> {
-            SqlFieldsQuery idxCreate = new SqlFieldsQuery("create index " + IDX + " on " + TBL + "(fld)");
-
-            cache.query(idxCreate).getAll();
+            SqlFieldsQuery ddl = new SqlFieldsQuery("CREATE INDEX " + IDX + " ON " + TBL + "(fld)");
+            cache.query(ddl).getAll();
         }, 1);
 
         IndexProcessor ip = grid(0).context().indexProcessor();
 
-        IndexName name = new IndexName(
-            cache.getName(), CACHE, TBL.toUpperCase(), IDX
-        );
+        IndexName name = new IndexName(cache.getName(), CACHE, TBL.toUpperCase(), IDX);
 
         boolean seenBuilding = GridTestUtils.waitForCondition(() -> {
             Index idx = ip.index(name);
-
             return idx != null && idx.rebuildInProgress();
         }, 10_000);
 
         assertTrue("Index must exist and be in rebuild state", seenBuilding);
 
-        IndexQuery<Long, Integer> qry = new IndexQuery<Long, Integer>(Integer.class)
-            .setCriteria(between("fld", 0, CNT));
+        String sql = "SELECT id FROM " + TBL + " USE INDEX(" + IDX + ") " +
+            "WHERE fld BETWEEN ? AND ? ORDER BY id";
+
+        SqlFieldsQuery q = new SqlFieldsQuery(sql).setArgs(0, CNT - 1);
 
         GridTestUtils.assertThrows(null, () -> {
-            cache.query(qry).getAll();
-        }, IgniteException.class, "Failed to run IndexQuery due to index rebuild is in progress");
+            cache.query(q).getAll();
+        }, IgniteException.class, "Failed to parse query. Index \"" + IDX + "\" not found; SQL statement");
 
-        idxBuild.countDown();
+        idxBuildGate.countDown();
 
         crd.cache(CACHE).indexReadyFuture().get();
 
         boolean done = GridTestUtils.waitForCondition(() -> {
             Index idx = ip.index(name);
-
             return idx != null && !idx.rebuildInProgress();
         }, 20_000);
 
         assertTrue("Build must finish", done);
 
-        assertEquals(CNT, cache.query(qry).getAll().size());
+        assertEquals(CNT, cache.query(new SqlFieldsQuery(sql).setArgs(0, CNT - 1)).getAll().size());
     }
 
     /** */
@@ -188,7 +172,6 @@ public class IndexQueryBuildIndexTest extends GridCommonAbstractTest {
     /** */
     private IgniteCache<Long, Integer> cache() throws Exception {
         Ignite n = "CRD".equals(qryNode) ? grid(0) : startClientGrid();
-
         return n.cache(CACHE);
     }
 
