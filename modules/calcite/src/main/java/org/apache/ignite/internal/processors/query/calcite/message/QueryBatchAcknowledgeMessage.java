9db77efdf5de69ef4dabc7ce9bdd7c6f492dafa2
Denis <116185428+DenisPolo@users.noreply.github.com>
2025-10-23 20:04:47 +1000

    Apply suggestion from @shishkovilja
    Co-authored-by: Ilya Shishkov <shishkovilja@gmail.com>

--- modules/calcite/src/main/java/org/apache/ignite/internal/processors/query/calcite/message/QueryBatchAcknowledgeMessage.java
+++ modules/calcite/src/main/java/org/apache/ignite/internal/processors/query/calcite/message/QueryBatchAcknowledgeMessage.java
@@ -61,7 +61,7 @@ public class QueryBatchAcknowledgeMessage implements ExecutionContextAware {
     /**
      * @param qryId New query ID.
      */
-    public void queryId(UUID queryId) {
+    public void queryId(UUID qryId) {
         this.queryId = queryId;
     }
 
