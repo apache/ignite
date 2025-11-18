b90acd501ffd1d95fbdebeb2b90c604b4f0ed8e9
chesnokoff <chesnokoff239@gmail.com>
2025-11-18 14:37:56 +0300

    IGNITE-26966 Change order of perms

--- modules/core/src/main/java/org/apache/ignite/plugin/security/SecurityPermission.java
+++ modules/core/src/main/java/org/apache/ignite/plugin/security/SecurityPermission.java
@@ -58,9 +58,6 @@ public enum SecurityPermission {
     /** Visor admin operations permissions. */
     ADMIN_OPS,
 
-    /** Permission to perform rolling upgrade. */
-    ADMIN_ROLLING_UPGRADE,
-
     /** System processes kill permission. */
     ADMIN_KILL,
 
@@ -120,7 +117,10 @@ public enum SecurityPermission {
     SQL_VIEW_CREATE,
 
     /** Permission to execute DROP VIEW command. */
-    SQL_VIEW_DROP;
+    SQL_VIEW_DROP,
+
+    /** Permission to perform rolling upgrade. */
+    ADMIN_ROLLING_UPGRADE;
 
     /** Enumerated values. */
     private static final SecurityPermission[] VALS = values();
