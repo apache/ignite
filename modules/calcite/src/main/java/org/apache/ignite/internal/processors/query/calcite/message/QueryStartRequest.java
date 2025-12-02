126c702d94e0c35f7323af189768fb834cc1cc58
Vladimir Steshin <vladsz83@gmail.com>
2025-12-02 12:22:10 +0300

    impl

--- modules/calcite/src/main/java/org/apache/ignite/internal/processors/query/calcite/message/QueryStartRequest.java
+++ modules/calcite/src/main/java/org/apache/ignite/internal/processors/query/calcite/message/QueryStartRequest.java
@@ -22,9 +22,7 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.UUID;
 import org.apache.ignite.IgniteCheckedException;
-import org.apache.ignite.internal.GridDirectCollection;
-import org.apache.ignite.internal.GridDirectMap;
-import org.apache.ignite.internal.GridDirectTransient;
+import org.apache.ignite.internal.Order;
 import org.apache.ignite.internal.processors.affinity.AffinityTopologyVersion;
 import org.apache.ignite.internal.processors.cache.GridCacheSharedContext;
 import org.apache.ignite.internal.processors.query.calcite.metadata.FragmentDescription;
@@ -39,43 +37,51 @@ import org.jetbrains.annotations.Nullable;
  */
 public class QueryStartRequest implements MarshalableMessage, ExecutionContextAware {
     /** */
+    @Order(0)
     private String schema;
 
     /** */
+    @Order(value = 1, method = "queryId")
     private UUID qryId;
 
     /** */
+    @Order(value = 2, method = "originatingQueryId")
     private long originatingQryId;
 
     /** */
+    @Order(value = 3, method = "topologyVersion")
     private AffinityTopologyVersion ver;
 
     /** */
+    @Order(value = 4, method = "fragmentDescription")
     private FragmentDescription fragmentDesc;
 
     /** */
+    @Order(value = 5)
     private String root;
 
     /** Total count of fragments in query for this node. */
+    @Order(value = 6, method = "totalFragmentsCount")
     private int totalFragmentsCnt;
 
     /** */
-    @GridDirectTransient
-    private Object[] params;
+    private @Nullable Object[] params;
 
     /** */
-    private byte[] paramsBytes;
+    @Order(value = 7, method = "parametersBytes")
+    private @Nullable byte[] paramsBytes;
 
     /** */
+    @Order(value = 8)
     private long timeout;
 
     /** */
-    @GridDirectCollection(QueryTxEntry.class)
+    @Order(value = 9, method = "queryTransactionEntries")
     private @Nullable Collection<QueryTxEntry> qryTxEntries;
 
     /** */
-    @GridDirectMap(keyType = String.class, valueType = String.class)
-    private Map<String, String> appAttrs;
+    @Order(value = 10, method = "applicationAttributes")
+    private @Nullable Map<String, String> appAttrs;
 
     /** */
     @SuppressWarnings("AssignmentOrReturnOfFieldWithMutableType")
@@ -87,10 +93,10 @@ public class QueryStartRequest implements MarshalableMessage, ExecutionContextAw
         AffinityTopologyVersion ver,
         FragmentDescription fragmentDesc,
         int totalFragmentsCnt,
-        Object[] params,
+        @Nullable Object[] params,
         @Nullable byte[] paramsBytes,
         long timeout,
-        Collection<QueryTxEntry> qryTxEntries,
+        @Nullable Collection<QueryTxEntry> qryTxEntries,
         @Nullable Map<String, String> appAttrs
     ) {
         this.qryId = qryId;
@@ -117,16 +123,41 @@ public class QueryStartRequest implements MarshalableMessage, ExecutionContextAw
         return schema;
     }
 
+    /** */
+    public void schema(String schema) {
+        this.schema = schema;
+    }
+
     /** {@inheritDoc} */
     @Override public UUID queryId() {
         return qryId;
     }
 
+    /** */
+    public void queryId(UUID qryId) {
+        this.qryId = qryId;
+    }
+
     /**
      * @return Registered local query ID on originating node.
      */
-    public long originatingQryId() {
+    public long originatingQueryId() {
         return originatingQryId;
+    }
+
+    /** */
+    public void originatingQueryId(long originatingQryId) {
+        this.originatingQryId = originatingQryId;
+    }
+
+    /** */
+    public @Nullable byte[] parametersBytes() {
+        return paramsBytes;
+    }
+
+    /** */
+    public void parametersBytes(@Nullable byte[] paramsBytes) {
+        this.paramsBytes = paramsBytes;
     }
 
     /** {@inheritDoc} */
@@ -141,11 +172,21 @@ public class QueryStartRequest implements MarshalableMessage, ExecutionContextAw
         return fragmentDesc;
     }
 
+    /** */
+    public void fragmentDescription(FragmentDescription fragmentDesc) {
+        this.fragmentDesc = fragmentDesc;
+    }
+
     /**
      * @return Topology version.
      */
     public AffinityTopologyVersion topologyVersion() {
         return ver;
+    }
+
+    /** */
+    public void topologyVersion(AffinityTopologyVersion ver) {
+        this.ver = ver;
     }
 
     /**
@@ -155,6 +196,11 @@ public class QueryStartRequest implements MarshalableMessage, ExecutionContextAw
         return root;
     }
 
+    /** */
+    public void root(String root) {
+        this.root = root;
+    }
+
     /**
      * @return Total count of fragments in query for this node.
      */
@@ -162,11 +208,16 @@ public class QueryStartRequest implements MarshalableMessage, ExecutionContextAw
         return totalFragmentsCnt;
     }
 
+    /** */
+    public void totalFragmentsCount(int totalFragmentsCnt) {
+        this.totalFragmentsCnt = totalFragmentsCnt;
+    }
+
     /**
      * @return Query parameters.
      */
     @SuppressWarnings("AssignmentOrReturnOfFieldWithMutableType")
-    public Object[] parameters() {
+    public @Nullable Object[] parameters() {
         return params;
     }
 
@@ -185,6 +236,16 @@ public class QueryStartRequest implements MarshalableMessage, ExecutionContextAw
         return timeout;
     }
 
+    /** */
+    public void timeout(long timeout) {
+        this.timeout = timeout;
+    }
+
+    /** */
+    public void parametersBytes(long timeout) {
+        this.timeout = timeout;
+    }
+
     /**
      * @return Transaction entries to mixin on query processing.
      */
@@ -193,8 +254,18 @@ public class QueryStartRequest implements MarshalableMessage, ExecutionContextAw
     }
 
     /** */
-    public Map<String, String> appAttrs() {
+    public void queryTransactionEntries(@Nullable Collection<QueryTxEntry> qryTxEntries) {
+        this.qryTxEntries = qryTxEntries;
+    }
+
+    /** */
+    public @Nullable Map<String, String> applicationAttributes() {
         return appAttrs;
+    }
+
+    /** */
+    public void applicationAttributes(@Nullable Map<String, String> appAttrs) {
+        this.appAttrs = appAttrs;
     }
 
     /** {@inheritDoc} */
