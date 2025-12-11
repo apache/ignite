bbead82c1e74d7c1f3848c5717c8d1760f09b921
zstan <zstan@apache.com>
2025-12-11 19:24:41 +0300

    fix

--- modules/core/src/test/java/org/apache/ignite/internal/client/thin/ReliableChannelDuplicationTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/client/thin/ReliableChannelDuplicationTest.java
@@ -17,23 +17,20 @@
 
 package org.apache.ignite.internal.client.thin;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import java.net.InetSocketAddress;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import org.junit.Assume;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
 import static java.util.stream.IntStream.range;
 
 /**
  * Tests for duplication in channels' list.
  */
-@RunWith(Parameterized.class)
 public class ReliableChannelDuplicationTest extends ThinClientAbstractPartitionAwarenessTest {
     /** {@inheritDoc} */
     @Override protected void afterTest() throws Exception {
@@ -42,24 +39,12 @@ public class ReliableChannelDuplicationTest extends ThinClientAbstractPartitionA
         stopAllGrids();
     }
 
-    /** Grid count. */
-    @Parameterized.Parameter(0)
-    public int gridCnt;
-
-    /**  */
-    @Parameterized.Parameters(name = "gridCount = {0}")
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][] {
-            { 1 },
-            { 3 }
-        });
-    }
-
     /**
      * Test after cluster restart the number of channels remains equal to the number of nodes.
      */
-    @Test
-    public void testDuplicationOnClusterRestart() throws Exception {
+    @ParameterizedTest(name = "gridCount = {0}")
+    @ValueSource(ints = {1, 3})
+    public void testDuplicationOnClusterRestart(int gridCnt) throws Exception {
         startGrids(gridCnt);
 
         initClient(getClientConfiguration(range(0, gridCnt).toArray()), range(0, gridCnt).toArray());
@@ -78,9 +63,7 @@ public class ReliableChannelDuplicationTest extends ThinClientAbstractPartitionA
      */
     @Test
     public void testStopSingleNodeDuringOperation() throws Exception {
-        Assume.assumeFalse(gridCnt == 1);
-
-        testChannelDuplication(1, 0);
+        testChannelDuplication(3, 1, 0);
     }
 
     /**
@@ -88,9 +71,7 @@ public class ReliableChannelDuplicationTest extends ThinClientAbstractPartitionA
      */
     @Test
     public void testStopAndRestartNode() throws Exception {
-        Assume.assumeFalse(gridCnt == 1);
-
-        testChannelDuplication(1, 1);
+        testChannelDuplication(3, 1, 1);
     }
 
     /**
@@ -98,9 +79,7 @@ public class ReliableChannelDuplicationTest extends ThinClientAbstractPartitionA
      */
     @Test
     public void testStopMultipleNodesDuringOperation() throws Exception {
-        Assume.assumeFalse(gridCnt < 3);
-
-        testChannelDuplication(2, 2);
+        testChannelDuplication(3, 2, 2);
     }
 
     /**
@@ -131,10 +110,11 @@ public class ReliableChannelDuplicationTest extends ThinClientAbstractPartitionA
     /**
      * Tests that no duplicate channel holders are created during node restarts and topology changes.
      *
+     * @param gridCnt Grids to start.
      * @param gridsStop int Grids to stop.
      * @param gridsRestart int Grids to restart after stop.
      */
-    private void testChannelDuplication(int gridsStop, int gridsRestart) throws Exception {
+    private void testChannelDuplication(int gridCnt, int gridsStop, int gridsRestart) throws Exception {
         startGrids(gridCnt);
 
         initClient(getClientConfiguration(range(0, gridCnt).toArray()), range(0, gridCnt).toArray());
