483a631a2fec5e63b186d0231cc25ad742d87375
Nikolay Izhikov <nizhikov@apache.org>
2025-12-17 11:22:41 +0300

    IGNITE-27372 Fix snapshot restoration when indexPath equal to storagePath

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgnitePdsTestSuite8.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgnitePdsTestSuite8.java
@@ -50,8 +50,8 @@ import org.apache.ignite.internal.processors.cache.persistence.diagnostic.pagelo
 import org.apache.ignite.internal.processors.cache.persistence.diagnostic.pagelocktracker.stack.OffHeapLockStackTest;
 import org.apache.ignite.internal.processors.cache.persistence.filename.CacheConfigStoragePathTest;
 import org.apache.ignite.internal.processors.cache.persistence.filename.CustomCacheStorageConfigurationSelfTest;
-import org.apache.ignite.internal.processors.cache.persistence.filename.SnapshotRestoreIndexPathTest;
 import org.apache.ignite.internal.processors.cache.persistence.filename.SnapshotCreationNonDefaultStoragePathTest;
+import org.apache.ignite.internal.processors.cache.persistence.filename.SnapshotRestoreIndexPathTest;
 import org.apache.ignite.internal.processors.cache.warmup.LoadAllWarmUpStrategySelfTest;
 import org.apache.ignite.internal.processors.cache.warmup.WarmUpSelfTest;
 import org.apache.ignite.testframework.GridTestUtils;
