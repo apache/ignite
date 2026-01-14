c956627e28827eb166882e0ea06f138bc56cf99b
Vladimir Steshin <vladsz83@gmail.com>
2026-01-14 21:00:03 +0300

    + loadApp

---
+++ modules/ducktests/src/main/java/org/apache/ignite/internal/ducktest/tests/flex/FlexLoadApplication.java
@@ -0,0 +1,165 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.ducktest.tests.flex;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.apache.ignite.internal.ducktest.utils.IgniteAwareApplication;
+
+/** */
+public class FlexLoadApplication extends IgniteAwareApplication {
+    /** */
+    private static final int THREADS = 10;
+
+    /** */
+    private static final int START_TIME_WAIT_SEC = 20;
+
+    /** */
+    private static final int PRELOAD_TIME_SEC = 10;
+
+    /** {@inheritDoc} */
+    @Override public void run(JsonNode jsonNode) throws Exception {
+        createTable();
+
+        final ForkJoinPool executor = new ForkJoinPool(THREADS);
+        final CountDownLatch initLatch = new CountDownLatch(THREADS);
+        final AtomicLong counter = new AtomicLong();
+        final AtomicBoolean preloaded = new AtomicBoolean();
+
+        log.info("TEST | Load pool parallelism=" + executor.getParallelism());
+
+        for (int i = 0; i < THREADS; ++i) {
+            executor.submit(() -> {
+                final Random rnd = new Random();
+                boolean init = false;
+
+                while (active()) {
+                    try (Connection conn = thinJdbcDataSource.getConnection()) {
+                        PreparedStatement ps = conn.prepareStatement("INSERT INTO SCS_DM_DOCUMENTS values(?,?,?)");
+
+                        while (active()) {
+                            long id = counter.incrementAndGet();
+
+                            ps.setLong(1, id);
+                            ps.setString(2, UUID.randomUUID().toString());
+                            ps.setBigDecimal(3, BigDecimal.valueOf(rnd.nextDouble()));
+
+                            int res = ps.executeUpdate();
+
+                            if (res != 1)
+                                throw new IllegalStateException("Failed to insert a row. The results is not 1.");
+
+                            if (!init) {
+                                init = true;
+
+                                initLatch.countDown();
+                            }
+                        }
+                    }
+                    catch (Throwable th) {
+                        if (!preloaded.get()) {
+                            log.error("TEST | Failed to preload. Marking as broken.", th);
+
+                            markBroken(th);
+
+                            synchronized (this) {
+                                notifyAll();
+                            }
+                        }
+                        else
+                            log.info("TEST | Failed to load. Recreating connection. Err: " + th.getMessage());
+                    }
+                }
+            });
+        }
+
+        if (!active())
+            return;
+
+        if (!initLatch.await(START_TIME_WAIT_SEC, TimeUnit.SECONDS)) {
+            Exception th = new IllegalStateException("Failed to start loading.");
+
+            markBroken(th);
+
+            throw th;
+        }
+
+        log.info("TEST | Started " + THREADS + " loading threads. Preloading...");
+
+        synchronized (this) {
+            wait(PRELOAD_TIME_SEC * 1000);
+        }
+
+        preloaded.set(true);
+
+        log.info("TEST | Preloaded. Inserted about " + counter.get() + " records. Continue loading...");
+
+        markInitialized();
+
+        while (active()) {
+            synchronized (this) {
+                wait(150);
+            }
+        }
+
+        log.info("TEST | Finished. Stopping...");
+
+        markFinished();
+    }
+
+    /** */
+    private void createTable() throws Exception {
+        try (Connection conn = thinJdbcDataSource.getConnection()) {
+            conn.createStatement().execute("CREATE TABLE SCS_DM_DOCUMENTS(" +
+                "id INT, strVal VARCHAR, decVal DECIMAL, PRIMARY KEY(id)" +
+                ") WITH \"cache_name=TBG_SCS_DM_DOCUMENTS\""
+            );
+
+            try {
+                ResultSet rs = conn.prepareStatement("SELECT count(1) FROM SCS_DM_DOCUMENTS").executeQuery();
+
+                rs.next();
+
+                int cnt = rs.getInt(1);
+
+                if (cnt == 0)
+                    log.info("TEST | Created table 'SCS_DM_DOCUMENTS'.");
+                else
+                    throw new IllegalStateException("Unexpected empty table count: " + cnt);
+            }
+            catch (Exception t) {
+                t = new IllegalStateException("Failed to create table SCS_DM_DOCUMENTS.", t);
+
+                markBroken(t);
+
+                throw t;
+            }
+        }
+
+    }
+}
--- modules/ducktests/tests/ignitetest/tests/flex/flex_test.py
+++ modules/ducktests/tests/ignitetest/tests/flex/flex_test.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 """
-Module contains discovery tests.
+Module contains Flex tests.
 """
 
 import os
@@ -25,35 +25,106 @@ from time import monotonic
 from typing import NamedTuple
 
 from ignitetest.services.ignite import IgniteService
+from ignitetest.services.ignite_app import IgniteApplicationService
 from ignitetest.services.utils.ignite_aware import node_failed_event_pattern
-from ignitetest.services.utils.ignite_configuration import IgniteConfiguration
+from ignitetest.services.utils.ignite_configuration import IgniteConfiguration, DataStorageConfiguration, \
+    IgniteThinJdbcConfiguration
 from ignitetest.services.utils.ignite_configuration.cache import CacheConfiguration
+from ignitetest.services.utils.ignite_configuration.data_storage import DataRegionConfiguration
+from ignitetest.services.utils.ssl.client_connector_configuration import ClientConnectorConfiguration
 from ignitetest.utils import cluster
 from ignitetest.utils.bean import Bean
 from ignitetest.utils.ignite_test import IgniteTest
 from ignitetest.utils.version import LATEST_2_17, IgniteVersion
 
-
 class FlexTest(IgniteTest):
     SERVERS = 3
     SERVER_IDX_TO_DROP = 1
     IGNITE_VERSION = LATEST_2_17
-    FAILURE_DETECTION_TIMEOUT_SEC = 7
 
     @cluster(num_nodes=SERVERS + 1)
     def flex_test(self):
+        servers, ignite_config = self.launch_cluster()
+
+        load_app = self.start_load_app(servers, ignite_config.client_connector_configuration.port)
+
+        self.kill_node(servers)
+
+        self.logger.info("TEST | Stopping the load application ...")
+
+        load_app.stop()
+        load_app.await_stopped()
+
+        self.logger.info("TEST | The load application has stopped.")
+
+        servers.stop()
+
+    def kill_node(self, servers):
+        failedNode = servers.nodes[self.SERVER_IDX_TO_DROP]
+        failedNodeId = servers.node_id(failedNode)
+
+        self.logger.info(f"TEST | 'kill -9' node {self.SERVER_IDX_TO_DROP} with id {failedNodeId} ...")
+
+        servers.stop_node(failedNode, force_stop=True)
+
+        self.logger.info("TEST | Awaiting for the node-failed-event...")
+        servers.await_event(node_failed_event_pattern(failedNodeId), 30, from_the_beginning=True,
+                            nodes=[servers.nodes[0], servers.nodes[2]])
+
+        self.logger.info("TEST | Awaiting for the new cluster state...")
+        servers.await_event(f"servers={self.SERVERS - 1}, clients=0, state=ACTIVE, CPUs=", 30, from_the_beginning=True,
+                            nodes=[servers.nodes[0], servers.nodes[2]])
+
+        self.logger.info("TEST | The cluster has detected the node failure.")
+
+    def start_load_app(self, servers, jdbcPort):
+        jdbcPort = str(jdbcPort)
+
+        addrs = [servers.nodes[0].account.hostname + ":" + jdbcPort,
+                 servers.nodes[1].account.hostname + ":" + jdbcPort,
+                 servers.nodes[2].account.hostname + ":" + jdbcPort]
+
+        app = IgniteApplicationService(
+            self.test_context,
+            IgniteThinJdbcConfiguration(
+                version=self.IGNITE_VERSION,
+                addresses=addrs
+            ),
+            java_class_name="org.apache.ignite.internal.ducktest.tests.flex.FlexLoadApplication",
+            num_nodes=1
+        )
+
+        self.logger.info("TEST | Starting the loading application...")
+
+        app.start()
+
+        self.logger.info("TEST | Waiting for the load application initialization...")
+
+        app.await_started()
+
+        self.logger.info("TEST | The load application has initialized.")
+
+        return app
+
+    def launch_cluster(self):
         cacheAffinity = Bean("org.apache.ignite.cache.affinity.rendezvous.RendezvousAffinityFunction", partitions=512)
 
         ignite_config = IgniteConfiguration(
             version=self.IGNITE_VERSION,
             metrics_log_frequency = 0,
-            failure_detection_timeout=self.FAILURE_DETECTION_TIMEOUT_SEC * 1000,
             caches=[CacheConfiguration(
                 name='TBG_SCS_DM_DOCUMENTS',
                 atomicity_mode='TRANSACTIONAL',
                 affinity = cacheAffinity,
                 cache_mode = 'REPLICATED'
-            )]
+            )],
+            data_storage=DataStorageConfiguration(
+                default=DataRegionConfiguration(
+                    initial_size = 2048 * 1024 * 1024,
+                    max_size = 8192 * 1024 * 1024
+                )
+            ),
+            client_connector_configuration=ClientConnectorConfiguration()
         )
 
         servers, start_servers_sec = start_servers(self.test_context, self.SERVERS, ignite_config)
@@ -61,24 +132,7 @@ class FlexTest(IgniteTest):
         servers.await_event(f"servers={self.SERVERS}, clients=0, state=ACTIVE, CPUs=", 30, from_the_beginning=True,
                             nodes=servers.nodes)
 
-        failedNode = servers.nodes[self.SERVER_IDX_TO_DROP]
-        failedNodeId = servers.node_id(failedNode)
-
-        self.logger.info(f"'kill -9' node {self.SERVER_IDX_TO_DROP} with id {failedNodeId} ...")
-
-        servers.stop_node(failedNode, force_stop=True)
-
-        self.logger.info("Awaiting for the node-failed-event ...")
-        servers.await_event(node_failed_event_pattern(failedNodeId), self.FAILURE_DETECTION_TIMEOUT_SEC * 3,
-                            from_the_beginning=True, nodes=[servers.nodes[0], servers.nodes[2]])
-
-        self.logger.info("Awaiting for the new cluster state ...")
-        servers.await_event(f"servers={self.SERVERS - 1}, clients=0, state=ACTIVE, CPUs=", self.FAILURE_DETECTION_TIMEOUT_SEC * 3,
-                            from_the_beginning=True, nodes=[servers.nodes[0], servers.nodes[2]])
-
-        self.logger.info("The cluster has detected the node failure.")
-
-        servers.stop()
+        return servers, ignite_config
 
 def start_servers(test_context, num_nodes, ignite_config, modules=None):
     """
