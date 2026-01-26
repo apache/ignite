c3e373bb4e74ca28fcd36a1dffda0f19b8b9c050
chesnokoff <chesnokoff239@gmail.com>
2026-01-26 10:49:43 +0300

    IGNITE-27554 Add compatibility test

---
+++ modules/compatibility/src/test/java/org/apache/ignite/compatibility/spi/discovery/TcpDiscoveryOldClientSerModeCompatibilityTest.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.compatibility.spi.discovery;
+
+import org.apache.ignite.compatibility.IgniteReleasedVersion;
+import org.apache.ignite.compatibility.testframework.junits.IgniteCompatibilityAbstractTest;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.testframework.GridTestUtils;
+import org.apache.ignite.testframework.ListeningTestLogger;
+import org.apache.ignite.testframework.LogListener;
+import org.junit.Test;
+
+
+/**
+ * Compatibility test that ensures previous-version client fails to connect to the current server
+ * and the server reports the missing serMode byte.
+ */
+public class TcpDiscoveryOldClientSerModeCompatibilityTest extends IgniteCompatibilityAbstractTest {
+    /** */
+    private static final String SER_MODE_MSG = "serMode byte is expected";
+
+    /** */
+    private static final IgniteReleasedVersion OLD_VERSION = IgniteReleasedVersion.VER_2_17_0;
+
+    /** */
+    private ListeningTestLogger listeningLog;
+
+    @Override protected void afterTest() throws Exception {
+        super.afterTest();
+
+        stopAllGrids();
+    }
+
+    /** {@inheritDoc} */
+    @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
+        IgniteConfiguration cfg = super.getConfiguration(igniteInstanceName);
+
+        if (listeningLog != null)
+            cfg.setGridLogger(listeningLog);
+
+        return cfg;
+    }
+
+    /** */
+    @Test
+    public void testOldClientRejectedOnSerMode() throws Exception {
+        setLoggerDebugLevel();
+
+        listeningLog = new ListeningTestLogger(log);
+
+        LogListener serModeListener = LogListener.matches(SER_MODE_MSG).build();
+
+        listeningLog.registerListener(serModeListener);
+
+        startGrid(0);
+
+        GridTestUtils.assertThrows(
+            log,
+            () -> startGrid("old-client", OLD_VERSION.toString(), null),
+            AssertionError.class,
+            null
+        );
+
+        assertTrue("Expected serMode error in server log.", serModeListener.check(getTestTimeout()));
+    }
+}
