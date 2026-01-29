6595d93e39627fe20d771f8510316c7fd7693a20
Nikolay <nizhikov@apache.org>
2026-01-29 16:51:34 +0300

    Update modules/core/src/main/java/org/apache/ignite/internal/dto/IgniteDataTransferObjectSerializer.java
    Co-authored-by: Sergey Chugunov <sergey.chugunov@gmail.com>

--- modules/core/src/main/java/org/apache/ignite/internal/dto/IgniteDataTransferObjectSerializer.java
+++ modules/core/src/main/java/org/apache/ignite/internal/dto/IgniteDataTransferObjectSerializer.java
@@ -36,7 +36,7 @@ public interface IgniteDataTransferObjectSerializer<T> {
     /**
      *
      * @param instance
-     * @param in
+     * @param in Input stream to read object from.
      * @return
      * @throws IOException If read operation failed.
      * @throws ClassNotFoundException
