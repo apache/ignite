141bbff8aa66638b9826b55a6564ba1d717df50e
Nikolay <nizhikov@apache.org>
2026-02-04 14:56:47 +0300

    IGNITE-27733 Generate serdes code for BaselineNode (#12689)

--- modules/codegen2/src/main/java/org/apache/ignite/internal/idto/IgniteDataTransferObjectProcessor.java
+++ modules/codegen2/src/main/java/org/apache/ignite/internal/idto/IgniteDataTransferObjectProcessor.java
@@ -72,7 +72,6 @@ public class IgniteDataTransferObjectProcessor extends AbstractProcessor {
 
     /** Currently unsupported classes. */
     private static final Set<String> UNSUPPORTED = Set.of(
-        "org.apache.ignite.internal.management.baseline.BaselineNode",
         "org.apache.ignite.internal.processors.cache.CacheMetricsSnapshot",
         "org.apache.ignite.internal.commandline.cache.check_indexes_inline_size.CheckIndexInlineSizesResult",
         "org.apache.ignite.internal.management.cache.ContentionJobResult",
