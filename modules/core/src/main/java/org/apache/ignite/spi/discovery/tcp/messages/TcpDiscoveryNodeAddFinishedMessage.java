af409e6edc6be6874a2aa54ad0f2b894ad7c1af1
Dmitry Werner <grimekillah@gmail.com>
2026-02-05 14:00:29 +0500

    up direct type

--- modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/messages/TcpDiscoveryNodeAddFinishedMessage.java
+++ modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/messages/TcpDiscoveryNodeAddFinishedMessage.java
@@ -174,6 +174,6 @@ public class TcpDiscoveryNodeAddFinishedMessage extends TcpDiscoveryAbstractTrac
 
     /** {@inheritDoc} */
     @Override public short directType() {
-        return 18;
+        return 19;
     }
 }
