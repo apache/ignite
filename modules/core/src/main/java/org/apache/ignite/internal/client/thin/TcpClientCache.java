5ea0888dcd6f93d1de5c21e5ae604b437b0fbcce
Aleksey Plekhanov <plehanov.alex@gmail.com>
2026-02-13 12:18:54 +0300

    IGNITE-27816 Java Thin: Add ClientCache.sizeLong method - Fixes #12725.
    Signed-off-by: Aleksey Plekhanov <plehanov.alex@gmail.com>

--- modules/core/src/main/java/org/apache/ignite/internal/client/thin/TcpClientCache.java
+++ modules/core/src/main/java/org/apache/ignite/internal/client/thin/TcpClientCache.java
@@ -313,23 +313,35 @@ public class TcpClientCache<K, V> implements ClientCache<K, V> {
     @Override public int size(CachePeekMode... peekModes) throws ClientException {
         return ch.service(
             ClientOperation.CACHE_GET_SIZE,
-            req -> {
-                writeCacheInfo(req);
-                ClientUtils.collection(peekModes, req.out(), (out, m) -> out.writeByte((byte)m.ordinal()));
-            },
-            res -> (int)res.in().readLong()
+            req -> writePeekModes(peekModes, req),
+            this::readCacheSizeInt
         );
     }
 
     /** {@inheritDoc} */
     @Override public IgniteClientFuture<Integer> sizeAsync(CachePeekMode... peekModes) throws ClientException {
         return ch.serviceAsync(
-                ClientOperation.CACHE_GET_SIZE,
-                req -> {
-                    writeCacheInfo(req);
-                    ClientUtils.collection(peekModes, req.out(), (out, m) -> out.writeByte((byte)m.ordinal()));
-                },
-                res -> (int)res.in().readLong()
+            ClientOperation.CACHE_GET_SIZE,
+            req -> writePeekModes(peekModes, req),
+            this::readCacheSizeInt
+        );
+    }
+
+    /** {@inheritDoc} */
+    @Override public long sizeLong(CachePeekMode... peekModes) throws ClientException {
+        return ch.service(
+            ClientOperation.CACHE_GET_SIZE,
+            req -> writePeekModes(peekModes, req),
+            res -> res.in().readLong()
+        );
+    }
+
+    /** {@inheritDoc} */
+    @Override public IgniteClientFuture<Long> sizeLongAsync(CachePeekMode... peekModes) throws ClientException {
+        return ch.serviceAsync(
+            ClientOperation.CACHE_GET_SIZE,
+            req -> writePeekModes(peekModes, req),
+            res -> res.in().readLong()
         );
     }
 
@@ -1639,6 +1651,24 @@ public class TcpClientCache<K, V> implements ClientCache<K, V> {
                 serDes.writeObject(out, e.getKey());
                 serDes.writeObject(out, e.getValue());
             });
+    }
+
+    /** */
+    private void writePeekModes(CachePeekMode[] peekModes, PayloadOutputChannel req) {
+        writeCacheInfo(req);
+        ClientUtils.collection(peekModes, req.out(), (out, m) -> out.writeByte((byte)m.ordinal()));
+    }
+
+    /** */
+    private int readCacheSizeInt(PayloadInputChannel res) {
+        long size = res.in().readLong();
+
+        if (size <= Integer.MAX_VALUE)
+            return (int)size;
+        else {
+            throw new ClientException("Cache size exceeded maximum value for int type, use " +
+                "sizeLong/sizeLongAsync methods to get correct value");
+        }
     }
 
     /**
