5e193b69247c87665d7ced5d5162f52eb9515f96
Maksim Timonin <timoninmaxim@apache.org>
2026-02-16 16:09:21 +0300

    IGNITE-27842 Remove redundant IgniteExperimental annotations (#12739)

--- modules/core/src/main/java/org/apache/ignite/internal/management/tracing/TracingConfigurationResetCommand.java
+++ modules/core/src/main/java/org/apache/ignite/internal/management/tracing/TracingConfigurationResetCommand.java
@@ -18,10 +18,8 @@
 package org.apache.ignite.internal.management.tracing;
 
 import org.apache.ignite.internal.management.tracing.TracingConfigurationCommand.TracingConfigurationResetCommandArg;
-import org.apache.ignite.lang.IgniteExperimental;
 
 /** */
-@IgniteExperimental
 public class TracingConfigurationResetCommand extends AbstractTracingConfigurationCommand {
     /** {@inheritDoc} */
     @Override public String description() {
