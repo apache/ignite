4ce70317425580f148c27aacfe22c578cbf344ae
Vladimir Steshin <vladsz83@gmail.com>
2026-02-23 11:35:12 +0300

    fix

--- modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/messages/TcpDiscoveryCollectionMessage.java
+++ modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/messages/TcpDiscoveryCollectionMessage.java
@@ -19,6 +19,7 @@ package org.apache.ignite.spi.discovery.tcp.messages;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import org.apache.ignite.IgniteCheckedException;
@@ -75,8 +76,6 @@ public class TcpDiscoveryCollectionMessage implements TcpDiscoveryMarshallableMe
 
     /** {@inheritDoc} */
     @Override public void finishUnmarshal(Marshaller marsh, ClassLoader clsLdr) {
-        assert marshallableMsgsBytes != null || marshallableMsgs != null || writableMsgs != null;
-
         if (marshallableMsgsBytes != null && marshallableMsgs == null) {
             try {
                 marshallableMsgs = U.unmarshal(marsh, marshallableMsgsBytes, clsLdr);
@@ -118,7 +117,8 @@ public class TcpDiscoveryCollectionMessage implements TcpDiscoveryMarshallableMe
      * @return Pending messages from previous node.
      */
     public Collection<TcpDiscoveryAbstractMessage> messages() {
-        assert !F.isEmpty(writableMsgs) || !F.isEmpty(marshallableMsgs);
+        if (F.isEmpty(writableMsgs) && F.isEmpty(marshallableMsgs))
+            return Collections.emptyList();
 
         int totalSz = (F.isEmpty(writableMsgs) ? 0 : writableMsgs.size())
             + (F.isEmpty(marshallableMsgs) ? 0 : marshallableMsgs.size());
