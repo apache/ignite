8233e7e8774daa76b510ebfe963653c47f90c73d
Vladimir Steshin <vladsz83@gmail.com>
2026-02-26 23:40:24 +0300

    + master

--- modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/messages/TcpDiscoveryNodeAddedMessage.java
+++ modules/core/src/main/java/org/apache/ignite/spi/discovery/tcp/messages/TcpDiscoveryNodeAddedMessage.java
@@ -251,7 +251,7 @@ public class TcpDiscoveryNodeAddedMessage extends TcpDiscoveryAbstractTraceableM
 
     /** {@inheritDoc} */
     @Override public short directType() {
-        return 22;
+        return 24;
     }
 
     /** {@inheritDoc} */
