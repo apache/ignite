049a977ce048d2c369bf1dd49bb2c9851fdb6538
Nikita Amelchev <nsamelchev@gmail.com>
2026-03-06 14:12:55 +0300

    IGNITE-28082 Refactor DiscoveryCustomMessage default methods usages (#12865)

--- modules/core/src/main/java/org/apache/ignite/internal/processors/authentication/UserProposedMessage.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/authentication/UserProposedMessage.java
@@ -18,11 +18,8 @@
 package org.apache.ignite.internal.processors.authentication;
 
 import org.apache.ignite.internal.Order;
-import org.apache.ignite.internal.managers.discovery.DiscoCache;
 import org.apache.ignite.internal.managers.discovery.DiscoveryCustomMessage;
 import org.apache.ignite.internal.managers.discovery.DiscoveryServerOnlyCustomMessage;
-import org.apache.ignite.internal.managers.discovery.GridDiscoveryManager;
-import org.apache.ignite.internal.processors.affinity.AffinityTopologyVersion;
 import org.apache.ignite.internal.util.tostring.GridToStringInclude;
 import org.apache.ignite.internal.util.typedef.internal.S;
 import org.apache.ignite.lang.IgniteUuid;
@@ -74,17 +71,6 @@ public class UserProposedMessage implements DiscoveryServerOnlyCustomMessage, Me
      */
     @Nullable @Override public DiscoveryCustomMessage ackMessage() {
         return null;
-    }
-
-    /** {@inheritDoc} */
-    @Override public boolean isMutable() {
-        return false;
-    }
-
-    /** {@inheritDoc} */
-    @Nullable @Override public DiscoCache createDiscoCache(GridDiscoveryManager mgr,
-        AffinityTopologyVersion topVer, DiscoCache discoCache) {
-        throw new UnsupportedOperationException();
     }
 
     /**
