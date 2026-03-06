8c8a0453863763d7eba94dc32e1b89e08b004da4
Vladimir Steshin <vladsz83@gmail.com>
2026-03-06 11:52:44 +0300

    IGNITE-27887 Use message serializer for TcpConnectionRequestDiscoveryMessage (#12753)

--- modules/core/src/main/java/org/apache/ignite/spi/communication/tcp/internal/TcpConnectionRequestDiscoveryMessage.java
+++ modules/core/src/main/java/org/apache/ignite/spi/communication/tcp/internal/TcpConnectionRequestDiscoveryMessage.java
@@ -18,13 +18,16 @@
 package org.apache.ignite.spi.communication.tcp.internal;
 
 import java.util.UUID;
+import org.apache.ignite.internal.Order;
 import org.apache.ignite.internal.managers.discovery.DiscoCache;
 import org.apache.ignite.internal.managers.discovery.DiscoveryCustomMessage;
+import org.apache.ignite.internal.managers.discovery.DiscoveryMessageFactory;
 import org.apache.ignite.internal.managers.discovery.GridDiscoveryManager;
 import org.apache.ignite.internal.processors.affinity.AffinityTopologyVersion;
 import org.apache.ignite.internal.util.tostring.GridToStringInclude;
 import org.apache.ignite.internal.util.typedef.internal.S;
 import org.apache.ignite.lang.IgniteUuid;
+import org.apache.ignite.plugin.extensions.communication.Message;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -35,25 +38,37 @@ import org.jetbrains.annotations.Nullable;
  * node B receives request and opens communication connection to node A
  * thus allowing both nodes to communicate to each other.
  */
-public class TcpConnectionRequestDiscoveryMessage implements DiscoveryCustomMessage {
+public class TcpConnectionRequestDiscoveryMessage implements DiscoveryCustomMessage, Message {
     /** */
     private static final long serialVersionUID = 0L;
 
-    /** */
-    private final IgniteUuid id = IgniteUuid.randomUuid();
+    /** Message id. */
+    @Order(0)
+    IgniteUuid id;
 
-    /** */
+    /** Receiver node id. */
+    @Order(1)
     @GridToStringInclude
-    private final UUID receiverNodeId;
+    UUID receiverNodeId;
 
-    /** */
+    /** Connection index. */
+    @Order(2)
     @GridToStringInclude
-    private final int connIdx;
+    int connIdx;
 
-    /** */
+    /**
+     * @param receiverNodeId Receiver node id.
+     * @param connIdx Connection index.
+     */
     public TcpConnectionRequestDiscoveryMessage(UUID receiverNodeId, int connIdx) {
+        id = IgniteUuid.randomUuid();
         this.receiverNodeId = receiverNodeId;
         this.connIdx = connIdx;
+    }
+
+    /** Constructor for {@link DiscoveryMessageFactory}. */
+    public TcpConnectionRequestDiscoveryMessage() {
+        // No-op.
     }
 
     /** {@inheritDoc} */
@@ -88,6 +103,11 @@ public class TcpConnectionRequestDiscoveryMessage implements DiscoveryCustomMess
         DiscoCache discoCache
     ) {
         throw new UnsupportedOperationException("createDiscoCache");
+    }
+
+    /** {@inheritDoc} */
+    @Override public short directType() {
+        return 23;
     }
 
     /** {@inheritDoc} */
