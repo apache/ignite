13113129a14d780dd24d8fcf71d340f3beb81fd5
Anton Vinogradov <av@apache.org>
2026-03-12 16:34:53 +0300

    WIP

--- modules/core/src/main/java/org/apache/ignite/internal/managers/communication/GridIoManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/managers/communication/GridIoManager.java
@@ -449,7 +449,7 @@ public class GridIoManager extends GridManagerAdapter<CommunicationSpi<Object>> 
 
         List<MessageFactoryProvider> compMsgs = new ArrayList<>();
 
-        compMsgs.add(new GridIoMessageFactory(marsh, U.resolveClassLoader(ctx.config())));
+        compMsgs.add(new GridIoMessageFactory(marsh, U.gridClassLoader()));
 
         for (IgniteComponentType compType : IgniteComponentType.values()) {
             MessageFactoryProvider f = compType.messageFactory();
