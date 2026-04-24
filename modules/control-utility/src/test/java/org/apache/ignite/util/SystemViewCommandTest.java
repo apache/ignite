20d2381696fd3e4edf15102dcecf3b1a530f79fa
zstan <zstan@apache.org>
2026-04-24 15:14:33 +0300

    IGNITE-28605 Fix flaky SystemViewCommandTest.testSchemas

--- modules/control-utility/src/test/java/org/apache/ignite/util/SystemViewCommandTest.java
+++ modules/control-utility/src/test/java/org/apache/ignite/util/SystemViewCommandTest.java
@@ -348,6 +348,8 @@ public class SystemViewCommandTest extends GridCommandHandlerClusterByClassAbstr
         assertEquals(DummyService.class.getName(), sysView.get(2)); // serviceClass
         assertEquals(Integer.toString(srvcCfg.getMaxPerNodeCount()), sysView.get(6)); // maxPerNodeCount
         assertEquals(F.first(ignite0.services().serviceDescriptors()).topologySnapshot().toString(), sysView.get(10));
+
+        ignite0.services().cancel("service");
     }
 
     /** */
