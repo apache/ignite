28d57d011b189f4a6371aad09b1171d647b76b19
Sergey Chugunov <sergey.chugunov@gmail.com>
2026-05-06 15:00:31 +0300

    IGNITE-28242 Add missing readlock to IgniteTxAdapted.batchStoreCommit

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/transactions/IgniteTxAdapter.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/transactions/IgniteTxAdapter.java
@@ -1430,8 +1430,16 @@ public abstract class IgniteTxAdapter extends GridMetadataAwareAdapter implement
 
                         boolean intercept = e.context().config().getInterceptor() != null;
 
-                        if (intercept || !F.isEmpty(e.entryProcessors()))
-                            e.cached().unswap(false);
+                        if (intercept || !F.isEmpty(e.entryProcessors())) {
+                            cctx.database().checkpointReadLock();
+
+                            try {
+                                e.cached().unswap(false);
+                            }
+                            finally {
+                                cctx.database().checkpointReadUnlock();
+                            }
+                        }
 
                         IgniteBiTuple<GridCacheOperation, CacheObject> res = applyTransformClosures(e, false, null);
 
