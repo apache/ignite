24e8f0d61a7aa57cc2851212f4e8f289bf6cfb39
Valuyskiy.O.Y <valuyskiy.o.y@sbertech.ru>
2026-05-16 09:51:20 +1000

    IGNITE-28661 Refactor IgniteBuildInfoPropertiesTest

--- modules/core/src/test/java/org/apache/ignite/internal/util/IgniteBuildInfoPropertiesTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/util/IgniteBuildInfoPropertiesTest.java
@@ -19,18 +19,34 @@ package org.apache.ignite.internal.util;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
 import org.apache.ignite.internal.IgniteProperties;
 import org.apache.ignite.internal.IgniteVersionUtils;
 import org.apache.ignite.internal.util.typedef.internal.U;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
+import org.jetbrains.annotations.Nullable;
 import org.junit.Test;
 
 /** */
 public class IgniteBuildInfoPropertiesTest extends GridCommonAbstractTest {
+    /** */
+    private static final String PROPS_CLASS = "org.apache.ignite.internal.IgniteProperties";
+
+    /** */
+    private static final String VER_UTILS_CLASS = "org.apache.ignite.internal.IgniteVersionUtils";
+
     /** */
     @Test
     public void testLoadsBuildInfoProperties() {
@@ -48,24 +64,53 @@ public class IgniteBuildInfoPropertiesTest extends GridCommonAbstractTest {
     /** */
     @Test
     public void testUserIgnitePropertiesDoesNotOverrideBuildMetadata() throws Exception {
+        String wrongVerStr = "WRONG_VERSION";
+
         File tmpDir = Files.createTempDirectory("ignite-props-test-").toFile();
 
         try {
             File userProps = new File(tmpDir, "ignite.properties");
 
             try (FileWriter writer = new FileWriter(userProps)) {
-                writer.write("ignite.version=WRONG_VERSION" + U.nl());
+                writer.write("ignite.version=" + wrongVerStr + U.nl());
                 writer.write("some.custom.property=123" + U.nl());
             }
 
-            URL[] urls = new URL[] {tmpDir.toURI().toURL()};
+            URL tmpUrl = tmpDir.toURI().toURL();
+
+            URL igniteCoreUrl = IgniteProperties.class.getProtectionDomain().getCodeSource().getLocation();
+
+            URL[] urls = new URL[] {tmpUrl, igniteCoreUrl};
 
             ClassLoader ldr = IgniteProperties.class.getClassLoader();
 
-            try (URLClassLoader testLdr = new URLClassLoader(urls, ldr)) {
-                Class<?> cls = Class.forName("org.apache.ignite.internal.IgniteProperties", true, testLdr);
+            Set<String> urlLoadedClasses = Set.of(PROPS_CLASS, VER_UTILS_CLASS);
 
-                Method getMtd = cls.getMethod("get", String.class);
+            try (TestURLClassLoader testLdr = new TestURLClassLoader(urls, ldr, urlLoadedClasses)) {
+                URL userPropsUrl = testLdr.getResource("ignite.properties");
+
+                assertNotNull(userPropsUrl);
+
+                Properties props = new Properties();
+
+                try (InputStream in = userPropsUrl.openStream()) {
+                    props.load(in);
+                }
+
+                assertEquals(2, props.size());
+                assertEquals(wrongVerStr, props.getProperty("ignite.version"));
+                assertEquals("123", props.getProperty("some.custom.property"));
+
+                Class<?> propsCls = Class.forName(PROPS_CLASS, true, testLdr);
+                Class<?> utilsCls = Class.forName(VER_UTILS_CLASS, false, testLdr);
+
+                assertSame(testLdr, propsCls.getClassLoader());
+                assertSame(testLdr, utilsCls.getClassLoader());
+
+                assertNotNull(testLdr.getResource("ignite.properties"));
+                assertNotNull(testLdr.getResource("ignite-build-info.properties"));
+
+                Method getMtd = propsCls.getMethod("get", String.class);
 
                 String ver = (String)getMtd.invoke(null, "ignite.version");
 
@@ -75,6 +120,66 @@ public class IgniteBuildInfoPropertiesTest extends GridCommonAbstractTest {
         }
         finally {
             U.delete(tmpDir);
+        }
+    }
+
+    /** */
+    private static class TestURLClassLoader extends URLClassLoader {
+        /** */
+        private final Set<String> urlLoadedClasses;
+
+        /** */
+        public TestURLClassLoader(URL[] urls, ClassLoader parent, Set<String> urlLoadedClasses) {
+            super(urls, parent);
+
+            this.urlLoadedClasses = new HashSet<>(urlLoadedClasses);
+        }
+
+        /** {@inheritDoc} */
+        @Override protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+            if (urlLoadedClasses.contains(name)) {
+                synchronized (getClassLoadingLock(name)) {
+                    Class<?> cls = findLoadedClass(name);
+
+                    if (cls == null)
+                        cls = findClass(name);
+
+                    if (resolve)
+                        resolveClass(cls);
+
+                    return cls;
+                }
+            }
+
+            return super.loadClass(name, resolve);
+        }
+
+        /** {@inheritDoc} */
+        @Override public @Nullable URL getResource(String name) {
+            URL url = findResource(name);
+
+            return url != null ? url : super.getResource(name);
+        }
+
+        /** {@inheritDoc} */
+        @Override public Enumeration<URL> getResources(String name) throws IOException {
+            List<URL> res = new ArrayList<>();
+
+            Enumeration<URL> childRes = findResources(name);
+
+            while (childRes.hasMoreElements())
+                res.add(childRes.nextElement());
+
+            ClassLoader parent = getParent();
+
+            if (parent != null) {
+                Enumeration<URL> parentRes = parent.getResources(name);
+
+                while (parentRes.hasMoreElements())
+                    res.add(parentRes.nextElement());
+            }
+
+            return Collections.enumeration(res);
         }
     }
 }
