bf711bf14ce512520247409c316dc1b1d5cda3e5
Vladimir Steshin <vladsz83@gmail.com>
2026-05-22 14:36:02 +0300

    IGNITE-28637 Revise using the origin message class loader and serializer (#13167)

--- modules/core/src/main/java/org/apache/ignite/internal/CoreMessagesProvider.java
+++ modules/core/src/main/java/org/apache/ignite/internal/CoreMessagesProvider.java
@@ -448,8 +448,7 @@ public class CoreMessagesProvider extends AbstractMarshallableMessageFactoryProv
         withNoSchema(ExchangeFailureMessage.class);
         withNoSchema(CacheStatisticsClearMessage.class);
         withNoSchema(ClientCacheChangeDummyDiscoveryMessage.class);
-        // TODO: revise using resolved class loader, https://issues.apache.org/jira/browse/IGNITE-28637
-        withNoSchemaResolvedClassLoader(DynamicCacheChangeBatch.class);
+        withNoSchema(DynamicCacheChangeBatch.class);
 
         // [10000 - 10200]: Transaction and lock related messages. Most of them originally comes from Communication.
         msgIdx = 10000;
