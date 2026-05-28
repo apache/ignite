788527e83e98bb3450e5eead7310c4ecec4ee048
Anton Vinogradov <av@apache.org>
2026-05-28 19:18:59 +0300

    WIP

--- modules/core/src/main/java/org/apache/ignite/internal/managers/communication/CompressedMessageSerializer.java
+++ modules/core/src/main/java/org/apache/ignite/internal/managers/communication/CompressedMessageSerializer.java
@@ -27,6 +27,10 @@ import static org.apache.ignite.internal.managers.communication.CompressedMessag
 
 /** Message serializer for compressed message. */
 public class CompressedMessageSerializer implements MessageSerializer<CompressedMessage> {
+    /** */
+    public CompressedMessageSerializer(ClassLoader clsLdr) {
+    }
+
     /** {@inheritDoc} */
     @Override public boolean writeTo(CompressedMessage msg, MessageWriter writer) {
         if (!writer.isHeaderWritten()) {
