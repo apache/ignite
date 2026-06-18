d2e169c764fb3a37e01928718ebfeabd802459d6
Anton Vinogradov <av@apache.org>
2026-06-18 14:37:22 +0300

    WIP

--- modules/core/src/main/java/org/apache/ignite/internal/GridJobSiblingsResponse.java
+++ modules/core/src/main/java/org/apache/ignite/internal/GridJobSiblingsResponse.java
@@ -23,13 +23,13 @@ import org.apache.ignite.compute.ComputeJobSibling;
 import org.apache.ignite.internal.util.typedef.internal.S;
 import org.apache.ignite.internal.util.typedef.internal.U;
 import org.apache.ignite.marshaller.Marshaller;
-import org.apache.ignite.plugin.extensions.communication.CacheMarshallableMessage;
+import org.apache.ignite.plugin.extensions.communication.MarshallableMessage;
 import org.jetbrains.annotations.Nullable;
 
 /**
  * Job siblings response.
  */
-public class GridJobSiblingsResponse implements CacheMarshallableMessage {
+public class GridJobSiblingsResponse implements MarshallableMessage {
     /** */
     private @Nullable Collection<ComputeJobSibling> siblings;
 
