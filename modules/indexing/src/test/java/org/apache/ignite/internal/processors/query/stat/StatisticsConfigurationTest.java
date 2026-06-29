197c8d9666e0576b8fd7d186348aa2640e1fd33d
Dmitry Werner <grimekillah@gmail.com>
2026-06-29 13:15:53 +0500

    fix

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/stat/StatisticsConfigurationTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/query/stat/StatisticsConfigurationTest.java
@@ -153,6 +153,15 @@ public class StatisticsConfigurationTest extends StatisticsAbstractTest {
     protected void stopGridAndChangeBaseline(int nodeIdx) {
         stopGrid(nodeIdx);
 
+        // Wait for topology to stabilize before setting new baseline to avoid race condition
+        // where baseline is set while discovery messages about the stopped node are still propagating.
+        try {
+            awaitPartitionMapExchange();
+        }
+        catch (InterruptedException e) {
+            // No-op.
+        }
+
         if (persist)
             F.first(G.allGrids()).cluster().setBaselineTopology(F.first(G.allGrids()).cluster().topologyVersion());
 
