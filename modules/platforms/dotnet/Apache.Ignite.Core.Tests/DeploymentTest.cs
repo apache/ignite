accbfdaa2b16df81c0abc360689b54eb2cfe30b5
Dmitry Werner <grimekillah@gmail.com>
2026-07-02 14:31:13 +0500

    try to fix v3

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/DeploymentTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/DeploymentTest.cs
@@ -201,7 +201,7 @@ namespace Apache.Ignite.Core.Tests
             // Copy jars.
             var home = IgniteHome.Resolve();
 
-            var jarNames = new[] {@"\ignite-core-", @"\cache-api-1.0.0.jar", @"\ignite-spring-"};
+            var jarNames = new[] {@"\ignite-core-", @"\cache-api-1.0.0.jar", @"\modules\spring\", @"\ignite-spring-"};
 
             var jars = Directory.GetFiles(home, "*.jar", SearchOption.AllDirectories)
                 .Where(jarPath => jarNames.Any(jarPath.Contains)).ToArray();
