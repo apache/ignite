1ac29570802c8f9d1294da6d7c5055aa625e858c
NSAmelchev <namelchev@apache.org>
2026-07-29 17:16:32 +0300

    IGNITE-28736 Preserve initial order of fields in BinaryType#fieldNames

--- modules/binary/api/src/main/java/org/apache/ignite/binary/BinaryType.java
+++ modules/binary/api/src/main/java/org/apache/ignite/binary/BinaryType.java
@@ -43,6 +43,10 @@ public interface BinaryType {
     /**
      * Gets collection of all field names for this binary type in the order in which the fields were initially
      * registered.
+     * <p>
+     * Older Ignite versions did not preserve field registration order. If such a version created or processed the
+     * metadata, the original order may already be lost. In that case, this method returns the order available in the
+     * metadata.
      *
      * @return Collection of all field names for this binary type.
      */
