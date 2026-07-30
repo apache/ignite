5191df0596328a9a725e30d0dae6a4599b93c916
Nikolay Izhikov <nizhikov@apache.org>
2026-07-30 10:57:23 +0300

    IGNITE-28929 Move message constructor to generated code instead of reflection

--- modules/core/src/main/java/org/apache/ignite/internal/UseBinaryMarshaller.java
+++ modules/core/src/main/java/org/apache/ignite/internal/UseBinaryMarshaller.java
@@ -26,7 +26,8 @@ import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * Mark {@link Message} class with this annotation to serialize it with {@link BinaryMarshaller}.
+ * In case message CAN contain user provided classes it must be marshalled with the {@link BinaryMarshaller}.
+ * Mark {@link Message} class with annotation to serialize it with {@link BinaryMarshaller}.
  */
 @Documented
 @Target(value = TYPE)
