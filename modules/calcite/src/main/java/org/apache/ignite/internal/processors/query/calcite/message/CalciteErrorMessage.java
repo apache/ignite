109627e8173ea6daed3b7a13d59ddf340e11ba69
Vladimir Steshin <vladsz83@gmail.com>
2026-08-07 14:55:07 +0300

    IGNITE-28912 Move ErrorMessage to a common internal package (#13443)

--- modules/calcite/src/main/java/org/apache/ignite/internal/processors/query/calcite/message/CalciteErrorMessage.java
+++ modules/calcite/src/main/java/org/apache/ignite/internal/processors/query/calcite/message/CalciteErrorMessage.java
@@ -20,7 +20,7 @@ package org.apache.ignite.internal.processors.query.calcite.message;
 import java.util.UUID;
 import org.apache.ignite.internal.DeferredUnmarshalMessage;
 import org.apache.ignite.internal.Order;
-import org.apache.ignite.internal.managers.communication.ErrorMessage;
+import org.apache.ignite.internal.util.ErrorMessage;
 
 /** */
 public class CalciteErrorMessage extends ErrorMessage implements DeferredUnmarshalMessage {
