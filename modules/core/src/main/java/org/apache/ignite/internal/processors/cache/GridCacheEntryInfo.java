f588f629661d73bab7f177479454ed6757562f6c
Steshin Vladimir <vladsz83@gmail.com>
2026-08-09 19:36:54 +0300

    more minor comment

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheEntryInfo.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheEntryInfo.java
@@ -54,9 +54,9 @@ public class GridCacheEntryInfo implements CacheIdAware, Message {
     long initTime;
 
     /**
-     * Expiration time delta to transfer. In theory, we can get the calculating time delta thread paused causing
-     * a negative delta value. This shouldn't be treated as disabled expiration. Correct behavior is expired timeout.
-     * {@link Long#MIN_VALUE} is taken as one with unrealistic chance to appear.
+     * Expiration time delta to transfer. {@link Long#MIN_VALUE} means no expiration enabled. In theory, we can get
+     * the calculating time delta thread paused causing a negative delta value. This shouldn't be treated as disabled
+     * expiration. Correct behavior is expired timeout. {@link Long#MIN_VALUE} is taken as one with unrealistic chance to appear.
      */
     @Order(4)
     long expireTimeDelta = Long.MIN_VALUE;
