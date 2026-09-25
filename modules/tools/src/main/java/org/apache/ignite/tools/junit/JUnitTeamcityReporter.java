432df7997cf915ce832737bd21aa1613f3c3e167
zstan <zstan@apache.org>
2026-09-25 16:10:52 +0300

    fix after review 6

--- modules/tools/src/main/java/org/apache/ignite/tools/junit/JUnitTeamcityReporter.java
+++ modules/tools/src/main/java/org/apache/ignite/tools/junit/JUnitTeamcityReporter.java
@@ -220,7 +220,11 @@ public class JUnitTeamcityReporter extends RunListener {
         return "test-" + prevSuite + prevFlush + ".xml";
     }
 
-    /** */
+    /**
+     * @param msg Message.
+     *
+     * @return Escaped string.
+     */
     public static String escapeForTeamcity(String msg) {
         return (msg == null ? "null" : msg)
             .replace("|", "||")
